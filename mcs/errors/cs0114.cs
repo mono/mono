8ed037b7944a7d2518da908fbf5bd5326e315512
Miguel de Icaza <miguel@gnome.org>
2001-06-13 23:29:34 +0000

    Add test case for cs0114
    svn path=/trunk/mcs/; revision=6

---
+++ mcs/errors/cs0114.cs
@@ -0,0 +1,9 @@
+// cs0114.cs: Y.XX hides inherited member X.XX.  Must use `override' or `new' keyword
+// Line: 8
+public abstract class X {
+	public abstract void XX ();
+}
+
+class Y : X {
+	void XX ();
+}
