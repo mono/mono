3dfc511a3313ac0e08be4402eb11564c0d788ca4
Sean MacIsaac <macisaac@mono-cvs.ximian.com>
2001-07-15 05:45:23 +0000

    * StringCollection.cs: Added so that CodeDom.* would compile.
    svn path=/trunk/mcs/; revision=149

---
+++ mcs/class/System/System.Collections.Specialized/StringCollection.cs
@@ -0,0 +1,14 @@
+//
+// System.Collections.Specialized.StringCollection.cs
+//
+// Author:
+//   Sean MacIsaac (macisaac@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Collections.Specialized {
+	
+	public class StringCollection {
+	}
+}
