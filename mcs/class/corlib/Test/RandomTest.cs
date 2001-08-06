70ec9199855641e3041d54565a0cfe7903bf6b4b
Bob Smith <bobsmith@mono-cvs.ximian.com>
2001-08-06 18:24:35 +0000

    Added System.Random NUnit test.
    svn path=/trunk/mcs/; revision=417

---
+++ mcs/class/corlib/Test/RandomTest.cs
@@ -0,0 +1,67 @@
+//
+// System.Random Test Cases
+//
+// Author: Bob Smith <bob@thestuff.net>
+//
+
+using NUnit.Framework;
+using System;
+
+public class RandomTest : TestCase
+{
+        public RandomTest(string name): base(name){}
+        public void TestDouble()
+        {
+                Random r;
+                int i;
+                double c=0;
+                for (i=0; i<20; i++) c+=r.NextDouble();
+                c/=i;
+                Assert (c < .7 && c > .3);
+        }
+        public void TestSeed()
+        {
+                Random r(42), r2(42);
+                int i;
+                double c=0, c2=0;
+                for (i=0; i<20; i++)
+                {
+                        c += r.NextDouble();
+                        c2 += r2.NextDouble();
+                }
+                AssertEquals(c, c2);
+        }
+        public void TestNext()
+        {
+                Random r;
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next();
+                        Assert (c <= Random.MaxValue && c >= Random.MinValue)
+                }
+        }
+        public void TestNextMax()
+        {
+                Random r;
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next(10);
+                        Assert (c <= 10 && c >= Random.MinValue)
+                }
+        }
+        public void TestNextMinMax()
+        {
+                Random r;
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next(1, 10);
+                        Assert (c <= 10 && c >= 1)
+                }
+        }
+}
