76a9d42012945d51a4a2e6b20d05624065f5059d
Nick Drochak <nickd@mono-cvs.ximian.com>
2001-08-07 16:04:06 +0000

    2001-08-08  Nick Drochak <nick@jobdragon.com>
             * CollectionBase.cs: Add
             * ReadOnlyCollectionBase.cs: Add
             * CollectionBaseTest.cs: Add
             * ReadOnlyCollectionBaseTest.cs: Add
    
    
    --------------------------------------------------------------------
    
    svn path=/trunk/mcs/; revision=424

---
+++ mcs/class/corlib/System.Collections/ReadOnlyCollectionBase.cs
@@ -0,0 +1,42 @@
+//
+// System.Collections.ReadOnlyCollectionBase.cs
+//
+// Author:
+//   Nick Drochak II (ndrochak@gol.com)
+//
+// (C) 2001 Nick Drochak II
+//
+
+using System;
+
+namespace System.Collections {
+
+	public abstract class ReadOnlyCollectionBase : ICollection,	IEnumerable {
+
+		// private instance properties
+		private System.Collections.ArrayList myList;
+		
+		// public instance properties
+		public virtual int Count { get { return InnerList.Count; } }
+		
+		// Public Instance Methods
+		public virtual System.Collections.IEnumerator GetEnumerator() { return InnerList.GetEnumerator(); }
+		
+		// Protected Instance Constructors
+		protected ReadOnlyCollectionBase() { }
+		
+		// Protected Instance Properties
+		protected virtual System.Collections.ArrayList InnerList {get { return this.myList; } }
+		
+		// ICollection methods
+		void ICollection.CopyTo(Array array, int index) {
+			lock (InnerList) { InnerList.CopyTo(array, index); }
+		}
+		object ICollection.SyncRoot {
+				get { return InnerList.SyncRoot; }
+			}
+		bool ICollection.IsSynchronized {
+			get { return InnerList.IsSynchronized; }
+		}
+	}
+}
