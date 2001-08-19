88799614569e78b0077312233bd0333a3cb68997
Miguel de Icaza <miguel@gnome.org>
2001-08-19 22:59:11 +0000

    Add a couple of attributes -miguel
    svn path=/trunk/mcs/; revision=494

---
+++ mcs/class/corlib/System/CLSCompliantAttribute.cs
@@ -0,0 +1,33 @@
+//
+// System.CLSCompliantAttribute.cs
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System {
+
+	/// <summary>
+	///   Used to indicate if an element of a program is CLS compliant.
+	/// </summary>
+	///
+	/// <remarks>
+	/// </remarks>
+	public class CLSCompliant : Attribute {
+
+		bool is_compliant;
+
+		public CLSCompliant (bool is_compliant)
+		{
+			this.is_compliant = is_compliant;
+		}
+
+		public bool IsCompliant {
+			get {
+				return is_compliant;
+			}
+		}
+	}
+}
---
+++ mcs/class/corlib/System/ContextStaticAttribute.cs
@@ -0,0 +1,24 @@
+//
+// System.ContextStatic.cs
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System {
+
+	/// <summary>
+	///   The ContextStatic attribute is used to flag fields as being unique
+	/// </summary>
+	///
+	/// <remarks>
+	///   
+	/// </remarks>
+	public class ContextStatic : Attribute {
+
+		// No methods.
+		
+	}
+}
