134538f13a093f4670067c36d6c70e6af4e04006
Bob Smith <bobsmith@mono-cvs.ximian.com>
2001-08-20 20:24:35 +0000

    Control.cs: More implementation.
    svn path=/trunk/mcs/; revision=522

--- mcs/class/System.Web/System.Web.UI/Control.cs
+++ mcs/class/System.Web/System.Web.UI/Control.cs
@@ -8,6 +8,9 @@
 //
 
 //notes: view state only tracks changes after OnInit method is executed for the page request. You can read from it at any time, but cant write to it during rendering.
+//more notes: look at the private on* methods for initialization order. they will help.
+//even more notes: view state info in trackviewstate method description. read later.
+//Ok, enough notes: what the heck is different between enable view state, and track view state.
 
 using System;
 using System.Web;
@@ -29,6 +32,7 @@ namespace System.Web.UI
                 private HttpContext _context = null;
                 private bool _childControlsCreated = false;
                 private StateBag _viewState; //TODO: help me.
+                private bool _trackViewState = false; //TODO: I think this is right. Verify. Also modify other methods to use this.
                 private bool _viewStateIgnoreCase = true;
                 private EventHandlerList _events;
                 public Control()
@@ -36,6 +40,7 @@ namespace System.Web.UI
                         _namingContainer = _parent;
                         _viewState = new StateBag(_viewStateIgnoreCase);
                         _events = new EventHandlerList();
+                        _controls = this.CreateControlCollection(); //FIXME: this goes here?
                 }
                 public virtual string ClientID
                 {
@@ -140,6 +145,7 @@ namespace System.Web.UI
                                 return _visible;
                         }
                         set
+                        {
                                 _visible = value;
                         }
                 }
@@ -179,14 +185,16 @@ namespace System.Web.UI
                 protected bool HasChildViewState
                 {
                         get
-                        {
-                                //TODO: look through children for any that have saved view state info.
+                        {       //FIXME: Relook over this. not sure!
+                                foreach (control c in _controls)
+                                        if (c.IsTrackingViewState) return true;
+                                return false;
                         }
                 }
                 protected bool IsTrackingViewState
                 {
                         get
-                        {
+                        {       //FIXME: ME TO!
                                 return _enableViewState;
                         }
                 }
@@ -204,6 +212,71 @@ namespace System.Web.UI
                                 return _viewStateIgnoreCase;
                         }
                 }
-        }
+                protected virtual void AddParsedSubObject(object obj)
+                {
+                        _controls.Add(obj);
+                }
+                protected void BuildProfileTree(string parentId, bool calcViewState)
+                {
+                        //TODO
+                }
+                protected void ClearChildViewState()
+                {
+                        //TODO
+                        //Not quite sure about this. an example clears children then calls this, so I think
+                        //view state is local to the current object, not children.
+                }
+                protected virtual void CreateChildControls() {}
+                protected virtual ControlCollection CreateControlCollection()
+                {
+                        _controls = new ControlCollection(this);
+                }
+                protected virtual void EnsureChildControls() {} //FIXME: I think this should be empty.
+                public virtual Control FindControl(string id)
+                {
+                        int i;
+                        for (i = 0; i < _controls.Count; i++)
+                                if (_controls[i].ID == id) return _controls[i].ID;
+                        return null;
+                }
+                protected virtual Control FindControl(string id, int pathOffset)
+                {
+                        int i;
+                        for (i = pathOffset; i < _controls.Count; i++)
+                                if (_controls[i].ID == id) return _controls[i].ID;
+                        return null;
+                }
+                protected virtual void LoadViewState(object savedState)
+                {
+                        //TODO: What should I do by default?
+                }
+                protected string MapPathSecure(string virtualPath)
+                {
+                        //TODO: Need to read up on security+web.
+                }
+                protected virtual bool OnBubbleEvent(object source, EventArgs args)
+                {
+                        return false; //FIXME: It might throw "ItemCommand". not sure.
+                }
+                protected virtual void OnDataBinding(EventArgs e) {} //FIXME: I think this should be empty.
+                protected virtual void OnInit(EventArgs e) {} //FIXME: This one too.controls 
+                protected virtual void OnPreRender(EventArgs e) {} //FIXME: Me to.
+                protected virtual void OnUnload(EventArgs e) {} //TODO: Ok, I'm missing something. Read up on the event system.
+                protected void RaiseBubbleEvent(object source, EventArgs args)
+                {
+                        _parent.OnBubbleEvent(source, args); //FIXME: I think this is right. Check though.
+                }
+                protected virtual void Render(HtmlTextWriter writer) {} //FIXME: Default?
+                protected virtual void RenderChildren(HtmlTextWriter writer)
+                {
+                        //if render method delegate is set, call it here. otherwise,
+                        //render any child controls. just a for loop?
+                }
+                protected virtual void TrackViewState()
+                {
+                        _trackViewState = true;
+                }
 
+
+        }
 }
