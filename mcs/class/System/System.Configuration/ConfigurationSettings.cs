c868a9d062b0de54cb085033648470f03603604d
Miguel de Icaza <miguel@gnome.org>
2001-08-21 23:37:54 +0000

    Add System.Configuration classes from Chris Podurgiel
    svn path=/trunk/mcs/; revision=545

---
+++ mcs/class/System/System.Configuration/ConfigurationSettings.cs
@@ -0,0 +1,61 @@
+//
+// System.Configuration.ConfigurationSettings.cs
+//
+// Author:
+//   Christopher Podurgiel (cpodurgiel@msn.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Collections.Specialized;
+
+namespace System.Configuration
+{
+	/// <summary>
+	///		Component class.
+	/// </summary>
+	/// <remarks>
+	///		Longer description
+	/// </remarks>
+
+	public sealed class ConfigurationSettings
+	{
+
+		private NameValueCollection appsettings;
+
+
+		/// <summary>
+		///		ConfigurationSettings Constructor.
+		/// </summary>
+		public ConfigurationSettings ()
+		{
+			appsettings = new NameValueCollection();
+		}
+
+		/// <summary>
+		///		Returns configuration settings for a user-defined configuration 
+section.
+		/// </summary>
+		/// <param name="sectionName"></param>
+		/// <returns></returns>
+		public static object GetConfig(	string sectionName)
+		{
+			//FIXME: Not sure how to determine the correct .config file to parse.
+			return null;
+		}
+
+		/// <summary>
+		///		Get the Application Configuration Settings.
+		/// </summary>
+		public NameValueCollection AppSettings
+		{
+			get
+			{
+				return appsettings;
+			}
+		}
+	}
+}
+
+
---
+++ mcs/class/System/System.Configuration/NameValueSectionHandler.cs
@@ -0,0 +1,74 @@
+//
+// System.Configuration.NameValueSectionHandler.cs
+//
+// Author:
+//   Christopher Podurgiel (cpodurgiel@msn.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+using System;
+
+namespace System.Configuration
+{
+	/// <summary>
+	/// Summary description for NameValueSectionHandler.
+	/// </summary>
+	public class NameValueSectionHandler : IConfigurationSectionHander
+	{
+
+		private static string _stringKey;
+		private static string _stringValue;
+
+		/// <summary>
+		///		NameValueSectionHandler Constructor
+		/// </summary>
+		public NameValueSectionHandler()
+		{
+			_stringKey = "key";
+			_stringValue = "value";
+		}
+
+		/// <summary>
+		///		Creates a new configuration handler and adds the specified 
+configuration object to the collection.
+		/// </summary>
+		/// <param name="parent">Composed from the configuration settings in a 
+corresponding parent configuration section.</param>
+		/// <param name="configContext">Provides access to the virtual path for 
+which the configuration section handler computes configuration values. 
+Normally this parameter is reserved and is null.</param>
+		/// <param name="section">The XML node that contains the configuration 
+information to be handled. section provides direct access to the XML 
+contents of the configuration section.</param>
+		/// <returns></returns>
+		public object Create(object parent, object context, XmlNode section)
+		{
+			//FIXME: Add Implemetation code here.
+		}
+
+		/// <summary>
+		///		Gets the name of the key in the key-value pair.
+		/// </summary>
+		protected virtual string KeyAttributeName
+		{
+			get
+			{
+				return _stringKey;
+			}
+		}
+
+		/// <summary>
+		///		Gets the value for the key in the key-value pair.
+		/// </summary>
+		protected virtual string ValueAttributeName
+		{
+			get
+			{
+				return _stringValue;
+			}
+		}
+
+	}
+}
+
+
