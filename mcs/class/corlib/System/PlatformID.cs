49c06dd26d26e64cdddd1539dfdf204004f18f51
Jim Richardson <jimr@mono-cvs.ximian.com>
2001-08-31 08:57:30 +0000

    Changed enum value for first mono platform to allow growth for windows systems
    svn path=/trunk/mcs/; revision=690

--- mcs/class/corlib/System/PlatformID.cs
+++ mcs/class/corlib/System/PlatformID.cs
@@ -15,12 +15,11 @@ namespace System
 	public enum PlatformID
 	{	// TODO: determine what definitions to incorporate
 		//       possibilities are quite varied
-		//		 my redhat example is just an idea that
-		//		 can / will be determined at a later date
-		//		 I chose Redhat as an ex since that is the distro I am familiar with
 		Win32NT,
 		Win32S,
 		Win32Windows,
-		Linux86Redhat
+		Linux = 128 // that should leave plenty for 
+                            // windows growth and still not foul
+                            // up my OperatingSystem GetHashCode
 	}
 }
