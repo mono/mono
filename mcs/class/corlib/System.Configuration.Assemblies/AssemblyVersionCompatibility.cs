33f04f45a608b2383cc44b3ee0b0ba3ea5237324
Sergey Chaban <serge@mono-cvs.ximian.com>
2001-09-07 16:01:44 +0000

    Removed auto-generated comments.
    svn path=/trunk/mcs/; revision=759

--- mcs/class/corlib/System.Configuration.Assemblies/AssemblyVersionCompatibility.cs
+++ mcs/class/corlib/System.Configuration.Assemblies/AssemblyVersionCompatibility.cs
@@ -2,7 +2,8 @@
 //
 // This code was automatically generated from
 // ECMA CLI XML Library Specification.
-// Generator: libgen.xsl
+// Generator: libgen.xsl [1.0; (C) Sergey Chaban (serge@wildwestsoftware.com)]
+// Created: Wed, 5 Sep 2001 06:38:13 UTC
 // Source file: all.xml
 // URL: http://devresource.hp.com/devresource/Docs/TechPapers/CSharp/all.xml
 //
@@ -13,39 +14,19 @@ namespace System.Configuration.Assemblies {
 
 
 	/// <summary>
-	/// <para>
-	///                   Defines the different flavors of assembly version compatibility.
-	///                   The AssemblyVersionCompatibility defines the compatibility of an
-	///                   assembly with other versions of the same assembly, indicating if it cannot
-	///                   execute side-by-side with other versions (e.g., due to conflicts over a device
-	///                   driver).
-	///                </para>
-	/// <para>
-	///                   If no compatibility is specified, an assembly is side-by-side
-	///                   compatible in all scopes.
-	///                </para>
-	/// <para>
-	///                   An assembly cannot be more specific with regard to exactly which
-	///                   previous versions it is not side-by-side compatible with. Hence, if the AssemblyVersionCompatibility
-	///                   is specified, it means the assembly is non side-by-side with all know versions.
-	///                   If not specified, it means it is side-by-side with all known versions.
-	///                </para>
 	/// </summary>
 	public enum AssemblyVersionCompatibility {
 
 		/// <summary>
-		///             The assembly cannot execute with other versions if they are executing in the same process.
-		///             </summary>
+		/// </summary>
 		SameMachine = 1,
 
 		/// <summary>
-		///             The assembly cannot execute with other versions if they are executing in the same process.
-		///             </summary>
+		/// </summary>
 		SameProcess = 2,
 
 		/// <summary>
-		///             The assembly cannot execute with other versions if they executing in the same application domain.
-		///             </summary>
+		/// </summary>
 		SameDomain = 3,
 	} // AssemblyVersionCompatibility
 
--- mcs/class/corlib/System.Configuration.Assemblies/ProcessorID.cs
+++ mcs/class/corlib/System.Configuration.Assemblies/ProcessorID.cs
@@ -2,7 +2,8 @@
 //
 // This code was automatically generated from
 // ECMA CLI XML Library Specification.
-// Generator: libgen.xsl
+// Generator: libgen.xsl [1.0; (C) Sergey Chaban (serge@wildwestsoftware.com)]
+// Created: Wed, 5 Sep 2001 06:38:23 UTC
 // Source file: all.xml
 // URL: http://devresource.hp.com/devresource/Docs/TechPapers/CSharp/all.xml
 //
@@ -13,111 +14,86 @@ namespace System.Configuration.Assemblies {
 
 
 	/// <summary>
-	/// <para>
-	///                   Defines identification numbers for supported processors.
-	///                </para>
 	/// </summary>
 	public enum ProcessorID {
 
 		/// <summary>
-		///             Intel 386 or later and compatible processor.
-		///             </summary>
+		/// </summary>
 		INTEL_386 = 386,
 
 		/// <summary>
-		///             Intel 486 or later and compatible processor.
-		///             </summary>
+		/// </summary>
 		INTEL_486 = 486,
 
 		/// <summary>
-		///             Intel Pentium or later and compatible processor.
-		///             </summary>
+		/// </summary>
 		INTEL_PENTIUM = 586,
 
 		/// <summary>
-		///             MIPS R4000 Little-Endian processor.
-		///             </summary>
+		/// </summary>
 		MIPS_R4000 = 4000,
 
 		/// <summary>
-		///             Alpha 21064 processor.
-		///             </summary>
+		/// </summary>
 		ALPHA_21064 = 21064,
 
 		/// <summary>
-		///             PPC 601 processor.
-		///             </summary>
+		/// </summary>
 		PPC_601 = 601,
 
 		/// <summary>
-		///             PPC 603 processor.
-		///             </summary>
+		/// </summary>
 		PPC_603 = 603,
 
 		/// <summary>
-		///             PPC 604 processor.
-		///             </summary>
+		/// </summary>
 		PPC_604 = 604,
 
 		/// <summary>
-		///             PPC 620 processor.
-		///             </summary>
+		/// </summary>
 		PPC_620 = 620,
 
 		/// <summary>
-		///             Hitachi SH3 processor.
-		///             </summary>
+		/// </summary>
 		HITACHI_SH3 = 10003,
 
 		/// <summary>
-		///             Hitachi SH3E processor.
-		///             </summary>
+		/// </summary>
 		HITACHI_SH3E = 10004,
 
 		/// <summary>
-		///             Hitachi SH4 processor.
-		///             </summary>
+		/// </summary>
 		HITACHI_SH4 = 10005,
 
 		/// <summary>
-		///             Motoroloa 821 processor.
-		///             </summary>
+		/// </summary>
 		MOTOROLA_821 = 821,
 
 		/// <summary>
-		///             SHX SH3 Little-Endian processor.
-		///             </summary>
+		/// </summary>
 		SHx_SH3 = 103,
 
 		/// <summary>
-		///             SHX SH4 Little-Endian processor.
-		///             </summary>
+		/// </summary>
 		SHx_SH4 = 104,
 
 		/// <summary>
-		///             STRONGARM Little-Endian processor.
-		///             </summary>
+		/// </summary>
 		STRONGARM = 2577,
 
 		/// <summary>
-		///             ARM 720 processor.
-		///             </summary>
+		/// </summary>
 		ARM720 = 1824,
 
 		/// <summary>
-		///             ARM 820 processor.
-		///             </summary>
+		/// </summary>
 		ARM820 = 2080,
 
 		/// <summary>
-		///             ARM 920 processor.
-		///             </summary>
+		/// </summary>
 		ARM920 = 2336,
 
 		/// <summary>
-		/// <para>
-		///                   ARM 7TDMI processor.
-		///                </para>
 		/// </summary>
 		ARM_7TDMI = 70001,
 	} // ProcessorID
