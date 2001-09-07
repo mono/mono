720a2aa448bd5fe0efa71d15f5932687a7593b5f
Paolo Molaro <lupus@oddwiz.org>
2001-09-07 06:32:33 +0000

    Fri Sep 7 12:28:01 CEST 2001 Paolo Molaro <lupus@ximian.com>
    	* AsyncCallback.cs, common.src: add AsyncCallback delegate.
    
    svn path=/trunk/mcs/; revision=737

---
+++ mcs/class/corlib/System/AsyncCallback.cs
@@ -0,0 +1,13 @@
+//
+// System/AsyncCallback.cs
+//
+// Author:
+//   Paolo Molaro (lupus@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System {
+	public delegate void AsyncCallback (IAsyncResult ar);
+}
+
