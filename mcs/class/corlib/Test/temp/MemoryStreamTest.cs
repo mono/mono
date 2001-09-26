a3cfee175fa32908ed0ab2d41982d4277410dfda
Sean MacIsaac <macisaac@mono-cvs.ximian.com>
2001-09-26 09:25:02 +0000

    NAnt build files and test cases that CVS is giving me problems with.
    svn path=/trunk/mcs/; revision=974

---
+++ mcs/class/corlib/Test/temp/MemoryStreamTest.cs
@@ -0,0 +1,128 @@
+//
+// System.IO.StringWriter
+//
+// Author: Marcin Szczepanski (marcins@zipworld.com.au)
+//
+// TODO: Add some testing for exceptions
+//
+// TODO: Add some testing for the CanXXX properties, exceptions,
+// various different constructors.
+//
+
+using NUnit.Framework;
+using System.IO;
+using System;
+using System.Text;
+
+public class MemoryStreamTest : TestCase {
+	
+        private MemoryStream testStream;
+        private byte[] testStreamData;
+        
+	public MemoryStreamTest( string name ): base(name) { }
+
+        protected override void SetUp() {
+                testStreamData = new byte[100];
+
+                for( int i = 0; i < 100; i++ ) {
+                        testStreamData[i] = (byte)(100 - i);
+                }
+
+                testStream = new MemoryStream( testStreamData );
+        }
+
+	public void TestConstructors() {
+                MemoryStream ms = new MemoryStream();
+
+                AssertEquals( 0, ms.Length );
+                AssertEquals( 0, ms.Capacity );
+                AssertEquals( true, ms.CanWrite );
+                
+                ms = new MemoryStream( 10 );
+
+                // FIXME: Should ms.Length be 0 if there is no data?
+                // the spec is a little unclear.  If this is changed then
+                // the code will probably need to change
+
+                AssertEquals( 10, ms.Length );
+                AssertEquals( 10, ms.Capacity );
+        }
+
+        // 
+        // Verify that the first count bytes in testBytes are the same as
+        // the count bytes from index start in testStreamData
+        //
+        private void VerifyTestData( byte[] testBytes, int start, int count) {
+                if( testBytes == null ) {
+                        throw new ArgumentNullException();
+                } else if( ( start < 0 || count < 0 ) || start + count > testStreamData.Length || start > testStreamData.Length ) {
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                for( int test = 0; test < count; test++ ) {
+                        if( testBytes[ test ] != testStreamData[ start + test ] ) {
+                                string failStr = String.Format( "testByte {0} (testStream {1}) was <{2}>, expecting <{3}>", test, start+test, 
+                                        testBytes[ test ], testStreamData[ start+test] );
+                                Fail( failStr );
+                        }
+                }
+        }
+
+        public void TestRead() {
+                byte[] readBytes = new byte[20];
+
+                /* Test simple read */
+                testStream.Read( readBytes, 0, 10 );
+                VerifyTestData( readBytes, 0, 10 );
+
+                /* Seek back to beginning */
+
+                testStream.Seek( 0, SeekOrigin.Begin );
+ 
+                /* Read again, bit more this time */
+                testStream.Read( readBytes, 0, 20 );
+                VerifyTestData( readBytes, 0, 20 );
+
+                /* Seek to 20 bytes from End */
+                testStream.Seek( -20, SeekOrigin.End );
+                testStream.Read( readBytes, 0, 20);
+                VerifyTestData( readBytes, 80, 20);
+
+                int readByte = testStream.ReadByte();
+                AssertEquals( -1, readByte );
+                                      
+        }
+
+        public void TestWriteBytes() {
+                byte[] readBytes = new byte[100];
+                MemoryStream ms = new MemoryStream( 100 );
+
+                for( int i = 0; i < 100; i++ ) {
+                        ms.WriteByte( testStreamData[i] );
+                }
+
+                ms.Seek( 0, SeekOrigin.Begin); 
+                
+                testStream.Read( readBytes, 0, 100 );
+
+                VerifyTestData( readBytes, 0, 100 );
+        }               
+
+        public void TestWriteBlock() {
+                byte[] readBytes = new byte[100];
+                MemoryStream ms = new MemoryStream( 100 );
+
+                ms.Write( testStreamData, 0, 100 );
+
+                ms.Seek( 0, SeekOrigin.Begin); 
+                
+                testStream.Read( readBytes, 0, 100 );
+
+                VerifyTestData( readBytes, 0, 100 );
+
+                byte[] arrayBytes = testStream.ToArray();
+
+                VerifyTestData( arrayBytes, 0, 100 );
+
+        }
+}
---
+++ mcs/class/corlib/Test/temp/RandomTest.cs
@@ -0,0 +1,68 @@
+//
+// System.Random Test Cases
+//
+// Author: Bob Smith <bob@thestuff.net>
+//
+
+using NUnit.Framework;
+using System;
+
+public class RandomTest : TestCase
+{
+        public RandomTest(string name): base(name){}
+        public void TestDouble()
+        {
+                Random r = new Random();
+                int i;
+                double c=0;
+                for (i=0; i<20; i++) c+=r.NextDouble();
+                c/=i;
+                Assert (c < .7 && c > .3);
+        }
+        public void TestSeed()
+        {
+                Random r = new Random(42);
+                Random r2 = new Random(42);
+                int i;
+                double c=0, c2=0;
+                for (i=0; i<20; i++)
+                {
+                        c += r.NextDouble();
+                        c2 += r2.NextDouble();
+                }
+                AssertEquals(c, c2);
+        }
+        public void TestNext()
+        {
+                Random r = new Random();
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next();
+                        Assert (c < Int32.MaxValue && c >= 0);
+                }
+        }
+        public void TestNextMax()
+        {
+                Random r = new Random();
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next(10);
+                        Assert (c < 10 && c >= 0);
+                }
+        }
+        public void TestNextMinMax()
+        {
+                Random r = new Random();
+                int i;
+                long c;
+                for (i=0; i<20; i++)
+                {
+                        c = r.Next(1, 10);
+                        Assert (c < 10 && c >= 1);
+                }
+        }
+}
