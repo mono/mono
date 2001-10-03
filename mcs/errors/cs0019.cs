914532ab8ac1cc13525cb0383874ae9521150639
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-10-03 13:01:18 +0000

    Added files for errors CS0019 and CS0023
    svn path=/trunk/mcs/; revision=1064

---
+++ mcs/errors/cs0019.cs
@@ -0,0 +1,14 @@
+// cs0019.cs : Operator + cannot be applied to operands of type `Foo' and `int'
+// Line : 11
+
+public class Foo {
+
+	public static void Main ()
+	{
+		
+		Foo k = new Foo ();
+
+		int i = k + 6;
+		
+	}
+}
---
+++ mcs/errors/cs0023.cs
@@ -0,0 +1,13 @@
+// cs0023.cs : Operator ~ cannot be applied to operand of type `Foo'
+// Line : 10
+
+public class Foo {
+
+	public static void Main ()
+	{
+		Foo k = new Foo ();
+		
+		int i = ~ k;
+
+	}
+}
