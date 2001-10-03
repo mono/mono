b717c0c9eb18319a24b00c2e626e02353ab7a4fd
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-10-03 12:59:16 +0000

    2001-10-03  Ravi Pratap  <ravi@ximian.com>
    	* test-16.cs : Add.
    
    	* makefile : update accordingly.
    
    svn path=/trunk/mcs/; revision=1063

---
+++ mcs/tests/test-16.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Mine {
+
+	public class Blah {
+
+		public static int operator + (Blah i, Blah j)
+		{
+			Console.WriteLine ("Base class binary + operator");
+			return 2; 
+		}
+
+		public static implicit operator int (Blah i)
+		{
+			Console.WriteLine ("Converting from Blah->int");
+			return 3;
+		}
+	}
+
+	public class Foo : Blah {
+
+		public static int Main ()
+		{
+			int number = new Foo () + new Foo () ;
+			Console.WriteLine (number);
+
+			int k = new Foo ();
+
+			if (number == 2)
+				return 0;
+			else
+				return 1;
+		}
+	}
+}
