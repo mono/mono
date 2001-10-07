c12461cc6fd9e27329fde0f3dec76d27795c9086
Miguel de Icaza <miguel@gnome.org>
2001-10-07 02:13:01 +0000

    Add AllTests file from Mario
    svn path=/trunk/mcs/; revision=1115

---
+++ mcs/class/System.XML/Test/AllTests.cs
@@ -0,0 +1,27 @@
+// Author:
+//   Mario Martinez (mariom925@home.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+using NUnit.Framework;
+namespace Ximian.Mono.Tests {
+ /// <summary>
+ ///   Combines all unit tests for the System.XML.dll assembly
+ ///   into one test suite.
+ /// </summary>
+ public class AllTests : TestCase {
+  public AllTests(string name) : base(name) {}
+
+  public static ITest Suite
+
+
+   get
+   {
+    TestSuite suite =  new TestSuite();
+    suite.AddTest(new TestSuite(typeof(System.Xml.Test)));
+    return suite;
+   }
+  }
+ }
+}
---
+++ mcs/class/System/Test/AllTests.cs
@@ -0,0 +1,35 @@
+// Ximian.Mono.Tests.AllTests, System.dll
+//
+// Author:
+//   Mario Martinez (mariom925@home.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+using NUnit.Framework;
+namespace Ximian.Mono.Tests {
+ /// <summary>
+ ///   Combines all unit tests for the System.dll assembly
+ ///   into one test suite.
+ /// </summary>
+ public class AllTests : TestCase {
+  public AllTests(string name) : base(name) {}
+
+  public static ITest Suite
+
+
+   get
+   {
+    TestSuite suite =  new TestSuite();
+    suite.AddTest(DnsTest.Suite);
+    suite.AddTest(IPHostEntryTest.Suite);
+
+suite.AddTest(Testsuite.System.Collections.Specialized.NameValueCollectionTe
+st.Suite);
+    suite.AddTest(new
+TestSuite(typeof(System.Collections.Specialized.StringCollectionTest)));
+    return suite;
+   }
+  }
+ }
+}
