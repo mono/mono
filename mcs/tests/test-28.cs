ccd28af88744bb0d6c35412ec419ad8d091e6390
Miguel de Icaza <miguel@gnome.org>
2001-10-21 17:00:24 +0000

    Add indexer test, does not work yet
    svn path=/trunk/mcs/; revision=1190

---
+++ mcs/tests/test-28.cs
@@ -0,0 +1,34 @@
+class X {
+	int v1, v2;
+	
+	int this [int a] {
+		get {
+			if (a == 0)
+				return v1;
+			else
+				return v2;
+		}
+
+		set {
+			if (a == 0)
+				v1 = value;
+			else
+				v2 = value;
+		}
+	}
+
+	static int Main ()
+	{
+		X x = new X ();
+
+		x [0] = 1;
+		if (x.v1 != 1)
+			return 1;
+
+		if (x [0] != 1)
+			return 2;
+
+		return 0;
+		
+	}
+}
