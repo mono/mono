8edceaee2168867e70ad66a119cf09cd14c230b7
Miguel de Icaza <miguel@gnome.org>
2001-10-25 19:34:35 +0000

    Add a bunch of new classes to reduce warnings from Mint
    svn path=/trunk/mcs/; revision=1202

---
+++ mcs/class/corlib/System.Security.Principal/PrincipalPolicy.cs
@@ -0,0 +1,17 @@
+//
+// System.Security.Principal.PrincipalPolicy.cs
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Security.Principal {
+
+	enum PrincipalPolicy {
+		UnauthenticatedPrincipal,
+		NoPrincipal,
+		WindowsPrincipal
+	}
+}
---
+++ mcs/class/corlib/System.Security.Principal/WindowsAccountType.cs
@@ -0,0 +1,18 @@
+//
+// System.Security.Principal.WindowsAccountType.cs
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Security.Principal {
+
+	enum WindowsAccountType {
+		Normal,
+		Guest,
+		System,
+		Anonymous
+	}
+}
