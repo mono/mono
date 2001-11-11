2a7159dbd8ce4a1adbf1a1e3e440de202196819c
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-11-11 08:01:48 +0000

    Add files for the new errors we catch.
    svn path=/trunk/mcs/; revision=1322

---
+++ mcs/errors/cs1008.cs
@@ -0,0 +1,14 @@
+// cs1008.cs : Type byte, sbyte, short, ushort, int, uint, long, or ulong expected
+// Line : 7
+
+public class Blah {
+
+	public enum MyEnum {
+		Foo = "foo",
+		Bar
+	}
+
+	public static void Main ()
+	{
+	}
+}
