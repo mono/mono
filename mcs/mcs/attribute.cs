a1843f59b54c2a60229f53a13b5eb8deb185873f
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-11-12 13:29:05 +0000

    2001-12-11  Ravi Pratap  <ravi@ximian.com>
    	* attribute.cs (Attribute::Resolve): Expand to handle named arguments too.
    
    	* ../errors/cs0617.cs : Add.
    
    svn path=/trunk/mcs/; revision=1333

--- mcs/mcs/attribute.cs
+++ mcs/mcs/attribute.cs
@@ -30,6 +30,21 @@ namespace CIR {
 			Location = loc;
 		}
 
+		void error617 (string name)
+		{
+			Report.Error (617, Location, "'" + name + "' is not a valid named attribute " +
+				      "argument. Named attribute arguments must be fields which are not " +
+				      "readonly, static or const, or properties with a set accessor which "+
+				      "are not static.");
+		}
+
+		void error182 ()
+		{
+			Report.Error (182, Location,
+				      "An attribute argument must be a constant expression, typeof " +
+				      "expression or array creation expression");
+		}
+
 		public CustomAttributeBuilder Resolve (EmitContext ec)
 		{
 			string name = Name;
@@ -49,21 +64,23 @@ namespace CIR {
 			
 			// Now we extract the positional and named arguments
 			
-			// FIXME : For now we handle only positional arguments
-
-			if (Arguments != null)
-				if (Arguments.Count != 1)
-					Console.WriteLine ("Warning : Cannot handle named arguments in attributes yet");
-			
-			
 			ArrayList pos_args = new ArrayList ();
+			ArrayList named_args = new ArrayList ();
 			
-			if (Arguments != null)
+			if (Arguments != null) {
 				pos_args = (ArrayList) Arguments [0];
+				if (Arguments.Count > 1)
+					named_args = (ArrayList) Arguments [1];
+			}
 				
-			object [] values = new object [pos_args.Count];
+			object [] pos_values = new object [pos_args.Count];
+
+			//
+			// First process positional arguments 
+			//
 			
-			for (int i = 0; i < pos_args.Count; i++) {
+			int i;
+			for (i = 0; i < pos_args.Count; i++) {
 
 				Argument a = (Argument) pos_args [i];
 
@@ -73,12 +90,81 @@ namespace CIR {
 				Expression e = Expression.Reduce (ec, a.Expr);
 
 				if (e is Literal) 
-					values [i] = ((Literal) e).GetValue ();
+					pos_values [i] = ((Literal) e).GetValue ();
 				else { 
-					Report.Error (182, Location,
-						      "An attribute argument must be a constant expression, typeof " +
-						      "expression or array creation expression");
+					error182 ();
 					return null;
+				}
+			}
+
+			//
+			// Now process named arguments
+			//
+
+			ArrayList field_infos = new ArrayList ();
+			ArrayList prop_infos  = new ArrayList ();
+			ArrayList field_values = new ArrayList ();
+			ArrayList prop_values = new ArrayList ();
+
+			for (i = 0; i < named_args.Count; i++) {
+
+				DictionaryEntry de = (DictionaryEntry) named_args [i];
+
+				string member_name = (string) de.Key;
+				Argument a  = (Argument) de.Value;
+
+				if (!a.Resolve (ec))
+					return null;
+
+				Expression member = Expression.MemberLookup (ec, attribute_type, member_name, false,
+									     MemberTypes.Field | MemberTypes.Property,
+									     BindingFlags.Public | BindingFlags.Instance,
+									     Location);
+
+				if (member == null || !(member is PropertyExpr || member is FieldExpr)) {
+					error617 (member_name);
+					return null;
+				}
+
+				if (member is PropertyExpr) {
+					PropertyExpr pe = (PropertyExpr) member;
+					PropertyInfo pi = pe.PropertyInfo;
+
+					if (!pi.CanWrite) {
+						error617 (member_name);
+						return null;
+					}
+
+					Expression e = Expression.Reduce (ec, a.Expr);
+					
+					if (e is Literal) 
+						prop_values.Add (((Literal) e).GetValue ());
+					else { 
+						error182 ();
+						return null;
+					}
+					
+					prop_infos.Add (pi);
+					
+				} else if (member is FieldExpr) {
+					FieldExpr fe = (FieldExpr) member;
+					FieldInfo fi = fe.FieldInfo;
+
+					if (fi.IsInitOnly) {
+						error617 (member_name);
+						return null;
+					}
+
+					Expression e = Expression.Reduce (ec, a.Expr);
+					
+					if (e is Literal) 
+						field_values.Add (((Literal) e).GetValue ());
+					else { 
+						error182 ();
+						return null;
+					}
+					
+					field_infos.Add (fi);
 				}
 			}
 			
@@ -98,9 +184,22 @@ namespace CIR {
 				Report.Error (-6, Location, "Could not find a constructor for this argument list.");
 				return null;
 			}
+			
+			PropertyInfo [] prop_info_arr = new PropertyInfo [prop_infos.Count];
+			FieldInfo [] field_info_arr = new FieldInfo [field_infos.Count];
+			object [] field_values_arr = new object [field_values.Count];
+			object [] prop_values_arr = new object [prop_values.Count];
 
-			CustomAttributeBuilder cb = new CustomAttributeBuilder ((ConstructorInfo) constructor, values); 
+			field_infos.CopyTo  (field_info_arr, 0);
+			field_values.CopyTo (field_values_arr, 0);
 
+			prop_values.CopyTo  (prop_values_arr, 0);
+			prop_infos.CopyTo   (prop_info_arr, 0);
+			
+			CustomAttributeBuilder cb = new CustomAttributeBuilder ((ConstructorInfo) constructor, pos_values,
+										prop_info_arr, prop_values_arr,
+										field_info_arr, field_values_arr); 
+			
 			return cb;
 		}
 
