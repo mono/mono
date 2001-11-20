398cb6574ee4af9604eceabfcd37e68ace7d47ed
Daniel Weber <dweber@mono-cvs.ximian.com>
2001-11-20 19:02:06 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=1396

---
+++ mcs/class/System.XML/System.Xml.XPath/IXPathNavigable.cs
@@ -0,0 +1,17 @@
+// -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+//
+// System.Xml.IXmlLineInfo.cs
+//
+// Author:
+//   Jason Diamond (jason@injektilo.org)
+//
+// (C) 2001 Jason Diamond  http://injektilo.org/
+//
+
+namespace System.Xml.XPath
+{
+	public interface IXPathNavigable
+	{
+		XPathNavigator CreateNavigator();
+	}
+}
---
+++ mcs/class/System.XML/System.Xml.XPath/XPathNavigator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace System.Xml.XPath
+{
+	/// <summary>
+	/// Summary description for XPathNavigator.
+	/// </summary>
+	public class XPathNavigator
+	{
+		public XPathNavigator()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
