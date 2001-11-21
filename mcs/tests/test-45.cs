04b82a2101a31eb605c8be078f84ebbc64cc3f73
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-11-21 05:04:52 +0000

    Fix up to do double-dimension array access too !
    svn path=/trunk/mcs/; revision=1403

--- mcs/tests/test-45.cs
+++ mcs/tests/test-45.cs
@@ -10,11 +10,10 @@ public class Blah {
 
 		if (i [2] != 2)
 			return 1;
-
-		// The following doesn't work as of now. Is it supposed to ?
-		//if (j [1,2] != 3)
-		//	return 1;
-
+		
+		if (j [1,1] != 3)
+			return 1;
+		
 		Console.WriteLine ("Array initialization test okay.");
 				   
 		return 0;
