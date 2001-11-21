7a86c63770aa1beb76227983da6b4bd973358af4
Nick Drochak <nickd@mono-cvs.ximian.com>
2001-11-21 18:49:08 +0000

    2001-11-22  Nick Drochak <ndrochak@gol.com>
    	* IPAddress.cs: Fix constructor bug, properly name Address property,
    	and use triple-slash for comments.
    
    svn path=/trunk/mcs/; revision=1407

--- mcs/class/System/System.Net/IPAddress.cs
+++ mcs/class/System/System.Net/IPAddress.cs
@@ -9,11 +9,11 @@
 
 namespace System.Net {
 
-	// <remarks>
-	//   Encapsulates an IP Address.
-	// </remarks>
+	/// <remarks>
+	///   Encapsulates an IP Address.
+	/// </remarks>
 	public class IPAddress {
-		public uint address;
+		public uint Address;
 
 		public static readonly IPAddress Any;
 		public static readonly IPAddress Broadcast;
@@ -27,23 +27,23 @@ namespace System.Net {
 
 		static IPAddress ()
 		{
-			Any.address = new IPAddress (InaddrAny);
-			Broadcast.address = new IPAddress (InaddrBroadcast);
-			Loopback.address = new IPAddress (InaddrLoopback);
-			None.address = new IPAddress (InaddrNone);
+			Any.Address = InaddrAny;
+			Broadcast.Address = InaddrBroadcast;
+			Loopback.Address = InaddrLoopback;
+			None.Address = InaddrNone;
 		}
 		
-		// <summary>
-		//   Constructor from a 32-bit constant.
-		// </summary>
+		/// <summary>
+		///   Constructor from a 32-bit constant.
+		/// </summary>
 		public IPAddress (uint address)
 		{
-			this.address = address;
+			this.Address = address;
 		}
 
-		// <summary>
-		//   Constructor from a dotted quad notation. 
-		// </summary>
+		/// <summary>
+		///   Constructor from a dotted quad notation. 
+		/// </summary>
 		public IPAddress (string ip)
 		{
 			string[] ips = ip.Split (new char[] {'.'});
@@ -53,31 +53,31 @@ namespace System.Net {
 			for (i = 0; i < ips.Length; i++)
 				a = (a << 8) |  (UInt16.Parse(ips [i]));
 
-			address = a;
+			Address = a;
 		}
 
-		// <summary>
-		//   Used to tell whether an address is a loopback.
-		// </summary>
-		// <param name="addr">Address to compare</param>
-		// <returns></returns>
+		/// <summary>
+		///   Used to tell whether an address is a loopback.
+		/// </summary>
+		/// <param name="addr">Address to compare</param>
+		/// <returns></returns>
 		public static bool IsLoopback (IPAddress addr)
 		{
-			return addr.address == InaddrLoopback;
+			return addr.Address == InaddrLoopback;
 		}
 
-		// <summary>
-		//   Overrides System.Object.ToString to return
-		//   this object rendered in a quad-dotted notation
-		// </summary>
+		/// <summary>
+		///   Overrides System.Object.ToString to return
+		///   this object rendered in a quad-dotted notation
+		/// </summary>
 		public override string ToString ()
 		{
-			return ToString (address);
+			return ToString (Address);
 		}
 
-		// <summary>
-		//   Returns this object rendered in a quad-dotted notation
-		// </summary>
+		/// <summary>
+		///   Returns this object rendered in a quad-dotted notation
+		/// </summary>
 		public static string ToString (uint addr)
 		{
 			return  (addr >> 24).ToString () + "." +
@@ -86,20 +86,20 @@ namespace System.Net {
 				(addr & 0xff).ToString ();
 		}
 
-		// <returns>
-		//   Whether both objects are equal.
-		// </returns>
+		/// <returns>
+		///   Whether both objects are equal.
+		/// </returns>
 		public override bool Equals (object other)
 		{
 			if (other is System.Net.IPAddress){
-				return address == ((System.Net.IPAddress) other).address;
+				return Address == ((System.Net.IPAddress) other).Address;
 			}
 			return false;
 		}
 
 		public override int GetHashCode ()
 		{
-			return (int)address;
+			return (int)Address;
 		}
 	}
 	
