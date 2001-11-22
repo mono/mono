ccb0c3703c35f57f0ef340095d294a7f3d72a28c
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-11-22 06:58:36 +0000

    2001-11-22  Ravi Pratap  <ravi@ximian.com>
    	* test-45.cs : Augment some more to test shorthand declaration
    	of variables/fields that are array types.
    
    svn path=/trunk/mcs/; revision=1418

--- mcs/tests/test-45.cs
+++ mcs/tests/test-45.cs
@@ -2,19 +2,32 @@ using System;
 
 public class Blah {
 
+	private static int[] array = {0, 1, 2, 3};
+	
 	public static int Main ()
 	{
 		int [] i = new int [] { 0, 1, 2, 3 };
 
 		int [,] j = new int [,] { {0,1}, {2,3}, {4,5}, {6,7} };
-		int a = 5;
+		
+		int [] a = { 4, 5, 6, 7 };
 
-		int [] k = new int [] { a };
+		// int foo = 1;
+		// int [] k = new int [] { foo };
+
 		if (i [2] != 2)
 			return 1;
 		
 		if (j [1,1] != 3)
 			return 1;
+		
+		for (int t = 0; t < 4; ++t) {
+			if (array [t] != t)
+				return 1;
+			
+			if (a [t] != (t + 4))
+				return 1;
+		}
 		
 		Console.WriteLine ("Array initialization test okay.");
 				   
