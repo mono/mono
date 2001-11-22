dcc4b74a9141d08440bc82c3497f897b6fb6f4b7
Daniel Weber <dweber@mono-cvs.ximian.com>
2001-11-22 01:19:33 +0000

    Add copyright information
    svn path=/trunk/mcs/; revision=1411

--- mcs/class/System.XML/System.Xml/XmlException.cs
+++ mcs/class/System.XML/System.Xml/XmlException.cs
@@ -1,3 +1,12 @@
+// -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+//
+// System.Xml.XmlException
+//
+// Author:
+//   Daniel Weber (daniel-weber@austin.rr.com)
+//
+// (C) 2001 Daniel Weber
+
 using System;
 using System.Runtime.Serialization;
 
