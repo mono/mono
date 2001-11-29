c0a6c6e3e3342fe3929dd07fc4646b27610a801a
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2001-11-29 15:48:48 +0000

    2001-11-30       Gaurav Vaish <gvaish@iitk.ac.in>
    * CheckBox.cs      --- Oops! I forgot OnPreRender. The last method left, well, still supposedly. Looks pretty fine now.
    
    svn path=/trunk/mcs/; revision=1473

--- mcs/class/System.Web/System.Web.UI.WebControls/CheckBox.cs
+++ mcs/class/System.Web/System.Web.UI.WebControls/CheckBox.cs
@@ -1,20 +1,21 @@
 /**
- * Namespace: System.Web.UI.WebControls
- * Class:     CheckBox
- * 
- * Author:  Gaurav Vaish
- * Maintainer: gvaish@iitk.ac.in
- * Contact: <my_scripts2001@yahoo.com>, <gvaish@iitk.ac.in>
- * Status:  100%
- * 
- * (C) Gaurav Vaish (2001)
- * Thanks to Leen Toelen (toelen@hotmail.com)'s classes that helped me
- * to write the contents of the function LoadPostData(...)
- */
+* Namespace: System.Web.UI.WebControls
+* Class:     CheckBox
+*
+* Author:  Gaurav Vaish
+* Maintainer: gvaish@iitk.ac.in
+* Contact: <my_scripts2001@yahoo.com>, <gvaish@iitk.ac.in>
+* Status:  100%
+*
+* (C) Gaurav Vaish (2001)
+* Thanks to Leen Toelen (toelen@hotmail.com)'s classes that helped me
+* to write the contents of the function LoadPostData(...)
+*/
 
 using System;
 using System.Collections;
 using System.Collections.Specialized;
+using System.Globalization;
 using System.Web;
 using System.Web.UI;
 
@@ -23,7 +24,7 @@ namespace System.Web.UI.WebControls
 	public class CheckBox : WebControl, IPostBackDataHandler
 	{
 		private static readonly object CheckedChangedEvent = new object();
-
+		
 		public CheckBox()
 		{
 			//
@@ -58,7 +59,7 @@ namespace System.Web.UI.WebControls
 				ViewState["Checked"] = value;
 			}
 		}
-
+		
 		public virtual string Text
 		{
 			get
@@ -71,6 +72,28 @@ namespace System.Web.UI.WebControls
 			set
 			{
 				ViewState["Text"] = value;
+			}
+		}
+		
+		private bool SaveCheckedViewState
+		{
+			get
+			{
+				if(Events[CheckedChangedEvent] != null)
+				{
+					if(!Enabled)
+						return true;
+					if(GetType() == typeof(CheckBox))
+					{
+						return false;
+					}
+					if(GetType() == typeof(RadioButton))
+					{
+						return false;
+					}
+				}
+				return true;
+				
 			}
 		}
 		
@@ -106,66 +129,79 @@ namespace System.Web.UI.WebControls
 		protected virtual void OnCheckedChanged(EventArgs e)
 		{
 			if(Events!=null)
-			{
-				EventHandler eh = (EventHandler)(Events[CheckedChangedEvent]);
-				if(eh!=null)
-					eh(this, e);
-			}
+				{
+					EventHandler eh = (EventHandler)(Events[CheckedChangedEvent]);
+					if(eh!=null)
+						eh(this, e);
+				}
 		}
 		
-		protected virtual void OnPreRender(EventArgs e)
+		protected override void OnPreRender(EventArgs e)
 		{
-			throw new NotImplementedException();
+			if(Page!=null)
+				{
+					if(Enabled)
+						{
+							Page.RegisterRequiresPostBack(this);
+						}
+				}
+				if(SaveCheckedViewState)
+					{
+						ViewState.SetItemDirty("checked", false);
+					}
 		}
 		
 		protected override void Render(HtmlTextWriter writer)
 		{
-			//TODO: THE LOST WORLD!
 			bool hasBeginRendering = false;
 			if(ControlStyleCreated)
-			{
-				if(!ControlStyle.IsEmpty)
 				{
-					hasBeginRendering = true;
-					ControlStyle.AddAttributesToRender(writer, this);
+					//TODO: Uncomment this in final version
+					/*
+					if(!ControlStyle.IsEmpty)
+						{
+							hasBeginRendering = true;
+							ControlStyle.AddAttributesToRender(writer, this);
+						}
+						*/
 				}
-			}
-			if(!Enabled)
-			{
-				hasBeginRendering = true;
-				writer.AddAttribute(HtmlTextWriterAttribute.Disbled, "disabled");
-			}
-			if(ToolTip.Length > 0)
-			{
-				hasBeginRendering = true;
-				writer.AddAttribute(HtmlTextWriterAttribute.Title, ToolTip);
-			}
-			if(Attributes.Count > 0)
-			{
-				hasBeginRendering = true;
-				Attributes.AddAttributes(writer);
-			}
-			if(hasBeginRendering)
-				writer.RenderBeginTag(HtmlTextWriterTag.Span);
+				if(!Enabled)
+					{
+						hasBeginRendering = true;
+						writer.AddAttribute(HtmlTextWriterAttribute.Disabled, "disabled");
+					}
+					if(ToolTip.Length > 0)
+						{
+							hasBeginRendering = true;
+							writer.AddAttribute(HtmlTextWriterAttribute.Title, ToolTip);
+						}
+						if(Attributes.Count > 0)
+							{
+								hasBeginRendering = true;
+								Attributes.AddAttributes(writer);
+							}
+							if(hasBeginRendering)
+								writer.RenderBeginTag(HtmlTextWriterTag.Span);
 			if(Text.Length > 0)
-			{
-				if(TextAlign == TextAlign.Right)
 				{
-					writer.AddAttribute(HtmlTextWriterAttribute.For, ClientID);
-					writer.RenderBeginTag(HtmlTextWriterTag.Label);
-					writer.Write(Text);
-					writer.RenderEngTag();
-					RenderInputTag(writer, ClientID);
-				} else
-				{
-					RenderInputTag(writer, ClientID);
-					writer.AddAttribute(HtmlTextWriterAttribute.For, ClientID);
-					writer.RenderBeginTag(HtmlTextWriterTag.Label);
-					writer.Write(Text);
+					if(TextAlign == TextAlign.Right)
+						{
+							writer.AddAttribute(HtmlTextWriterAttribute.For, ClientID);
+							writer.RenderBeginTag(HtmlTextWriterTag.Label);
+							writer.Write(Text);
+							writer.RenderEndTag();
+							RenderInputTag(writer, ClientID);
+						} else
+							{
+								RenderInputTag(writer, ClientID);
+								writer.AddAttribute(HtmlTextWriterAttribute.For, ClientID);
+								writer.RenderBeginTag(HtmlTextWriterTag.Label);
+								writer.Write(Text);
+							}
 				}
-			}
-			if(hasBeginRendering)
-				writer.RenderEndTag();
+				if(hasBeginRendering)
+					writer.RenderEndTag();
+			throw new NotImplementedException("Calling some internal functions");
 		}
 		
 		internal virtual void RenderInputTag(HtmlTextWriter writer, string clientId)
@@ -174,23 +210,23 @@ namespace System.Web.UI.WebControls
 			writer.AddAttribute(HtmlTextWriterAttribute.Type, "checkbox");
 			writer.AddAttribute(HtmlTextWriterAttribute.Name, UniqueID);
 			if(Checked)
-			{
-				writer.AddAttribute(HtmlTextWriterAttribute.Checked, "checked");
-			}
-			if(AutoPostBack)
-			{
-				writer.AddAttribute(HtmlTextWriterAttribute.OnClick,Page.GetPostBackClientEvent(this, String.Empty));
-				writer.AddAttribute("language", "javascript");
-			}
-			if(AccessKey.Length > 0)
-			{
-				writer.AddAttribute(HtmlTextWriterAttribute.AccessKey, AccessKey);
-			}
-			if(TabIndex != 0)
-			{
-				writer.AddAttribute(HtmlTextWriterAttribute.TabIndex, TabIndex.ToString(NumberFormatInfo.InvariantInfo));
-			}
-			writer.RenderBeginTag(HtmlTextWriterTag.Input);
+				{
+					writer.AddAttribute(HtmlTextWriterAttribute.Checked, "checked");
+				}
+				if(AutoPostBack)
+					{
+						writer.AddAttribute(HtmlTextWriterAttribute.Onclick,Page.GetPostBackClientEvent(this, String.Empty));
+						writer.AddAttribute("language", "javascript");
+					}
+					if(AccessKey.Length > 0)
+						{
+							writer.AddAttribute(HtmlTextWriterAttribute.Accesskey, AccessKey);
+						}
+						if(TabIndex != 0)
+							{
+								writer.AddAttribute(HtmlTextWriterAttribute.Tabindex, TabIndex.ToString(NumberFormatInfo.InvariantInfo));
+							}
+							writer.RenderBeginTag(HtmlTextWriterTag.Input);
 			writer.RenderEndTag();
 		}
 		
@@ -199,10 +235,10 @@ namespace System.Web.UI.WebControls
 			string postedVal = postCollection[postDataKey];
 			bool   postChecked = false;
 			if(postedVal != null)
-			{
-				postChecked = postedVal.Length > 0;
-			}
-			Checked = postChecked;
+				{
+					postChecked = postedVal.Length > 0;
+				}
+				Checked = postChecked;
 			return (postChecked == Checked == false);
 		}
 		
