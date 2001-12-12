77ab22144d4a3abf7afd8db9e9164fd2550af4f7
Miguel de Icaza <miguel@gnome.org>
2001-12-12 23:21:15 +0000

    More bugs
    svn path=/trunk/mcs/; revision=1563

---
+++ mcs/errors/bug4.cs
@@ -0,0 +1,15 @@
+using System;
+
+class X {
+	static void Main ()
+	{
+		try {
+			throw new Exception ();
+		} catch (Exception e) {
+			Console.WriteLine ("Caught");
+			throw;
+		} catch {
+			Console.WriteLine ("Again");
+		}
+	}
+}
