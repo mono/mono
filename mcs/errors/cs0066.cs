3d91144ae3edbc8f6f2edb0f084eae96925a09f7
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2001-12-16 00:14:23 +0000

    Add as we now catch this error.
    svn path=/trunk/mcs/; revision=1585

---
+++ mcs/errors/cs0066.cs
@@ -0,0 +1,25 @@
+// cs0066.cs : 'Button.Click' : event must be of a delegate type 
+// Line : 10
+
+using System;
+
+public delegate void EventHandler (object sender, EventArgs e);
+
+public class Button {
+
+	public event Blah Click;
+
+	public void Reset ()
+	{
+		Click = null;
+	}
+}
+
+public class Blah {
+
+	public static void Main ()
+	{
+		Blah b = new Blah ();
+	}
+	
+}
