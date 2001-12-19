5cdecdb151208ae29306b72630ec7ac2e4fe2008
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2001-12-19 12:04:23 +0000

    2001-12-19    Gaurav Vaish <gvaish@iitk.ac.in>
    * AUTHORS: Ha ha, having fun time.
    
    * ListItem.cs  -- needed because of visibility issues(?)
    
    svn path=/trunk/mcs/; revision=1641

---
+++ mcs/class/System.Web/System.Web.UI.WebControls/ListItem.cs
@@ -0,0 +1,25 @@
+/**
+ * Namespace: System.Web.UI.WebControls
+ * Class:     ListItem
+ *
+ * Author:  Gaurav Vaish
+ * Maintainer: gvaish@iitk.ac.in
+ * Contact: <my_scripts2001@yahoo.com>, <gvaish@iitk.ac.in>
+ * Implementation: yes
+ * Status:  10%
+ *
+ * (C) Gaurav Vaish (2001)
+ */
+
+using System;
+using System.Collections;
+using System.Web;
+using System.Web.UI;
+
+namespace System.Web.UI.WebControls
+{
+	public class ListItem : IStateManager, IParserAccessor, IAttributeAccessor
+	{
+		
+	}
+}
