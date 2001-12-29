804504f50c73d519a0485b61146576032e3a8fae
Nick Drochak <nickd@mono-cvs.ximian.com>
2001-12-29 11:57:58 +0000

    2001-12-29  Nick Drochak  <ndrochak@gol.com>
    	* PolicyLevel.cs: New file
    
    svn path=/trunk/mcs/; revision=1746

---
+++ mcs/class/corlib/System.Security.Policy/PolicyLevel.cs
@@ -0,0 +1,12 @@
+// System.Security.Policy.PolicyLevel
+//
+// Nick Drochak (ndrochak@gol.com)
+//
+// (C) 2001 Nick Drochak
+
+namespace System.Security.Policy
+{
+	public sealed class PolicyLevel
+	{
+	}
+}
