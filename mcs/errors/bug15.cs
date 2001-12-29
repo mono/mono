6e5e0cc3fdac47c55fa4cab2d539c1a3a4430d5a
Miguel de Icaza <miguel@gnome.org>
2001-12-29 20:26:01 +0000

    Added bug15
    svn path=/trunk/mcs/; revision=1752

---
+++ mcs/errors/bug15.cs
@@ -0,0 +1,22 @@
+using System;
+
+class X {
+
+	void A ()
+	{
+	}
+				
+	static void Main ()
+	{
+		int loop = 0;
+		
+		goto a;
+	b:
+		loop++;
+	a:
+		Console.WriteLine ("Hello");
+		for (;;){
+			goto b;
+		}
+	}
+}
