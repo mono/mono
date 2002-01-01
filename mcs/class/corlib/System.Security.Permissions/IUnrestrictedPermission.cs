709ffdcbe82633679dccfee4658fe2f90a4641a8
Nick Drochak <nickd@mono-cvs.ximian.com>
2002-01-01 19:33:28 +0000

    2002-01-02  Nick Drochak  <ndrochak@gol.com>
    	* IUnrestrictedPermission.cs: Added missing interface class
    
    svn path=/trunk/mcs/; revision=1770

---
+++ mcs/class/corlib/System.Security.Permissions/IUnrestrictedPermission.cs
@@ -0,0 +1,15 @@
+//
+// System.Security.Permissions.IUnrestrictedPermission.cs
+//
+// Author:
+//   Nick Drochak(ndrochak@gol.com)
+//
+// (C) Nick Drochak
+//
+
+namespace System.Security.Permissions {
+
+	public interface IUnrestrictedPermission {
+		bool IsUnrestricted();
+	}
+}
