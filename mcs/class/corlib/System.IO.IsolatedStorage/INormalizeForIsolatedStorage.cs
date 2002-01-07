894a74d809ef3c06bd61be590c283d26346249c4
Duco Fijma <duco@mono-cvs.ximian.com>
2002-01-07 17:59:34 +0000

    Created INormalizeForIsolatedStorage
    svn path=/trunk/mcs/; revision=1901

---
+++ mcs/class/corlib/System.IO.IsolatedStorage/INormalizeForIsolatedStorage.cs
@@ -0,0 +1,18 @@
+//
+// System.IO.IsolatedStorage.INormalizeForIsolatedStorage
+//
+// author:
+//   Duco Fijma (duco@lorentz.xs4all.nl)
+//
+//   (c) 2002 Duco Fijma
+//
+
+namespace System.IO.IsolatedStorage {
+
+public interface INormalizeForIsolatedStorage {
+
+	object Normalize ();
+
+}
+
+}
