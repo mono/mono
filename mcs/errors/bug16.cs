80b3679465237e5ed7bc85d4fe003d657e28e52c
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2002-01-08 13:15:33 +0000

    Add although we have just fixed it :-)
    svn path=/trunk/mcs/; revision=1910

---
+++ mcs/errors/bug16.cs
@@ -0,0 +1,24 @@
+//
+// Fixed
+//
+
+using System;
+
+public class Blah {
+	
+	public static void Main ()
+	{
+		ushort i;
+		sbyte  j;
+		
+		i = 10;
+		j = 20;
+
+		Console.WriteLine (i);
+		Console.WriteLine (j);
+		
+		Console.WriteLine ("Hello there !");
+	}	
+}
+
+		
