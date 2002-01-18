9071a1fd56aa6a904f25ff8adeb8cc6c36bc9d0a
Miguel de Icaza <miguel@gnome.org>
2002-01-18 02:10:10 +0000

    Add new error
    svn path=/trunk/mcs/; revision=2041

---
+++ mcs/errors/cs1524.cs
@@ -0,0 +1,15 @@
+// cs1524.cs: expected catch or finally
+// Line: 12
+
+namespace Test {
+        public class Test {
+                public static int Main () {
+                        int a;
+                        try {
+                                a = 1;
+                        }
+
+                        return 0;
+                }
+        }
+}
