b675c77a6899cd50483773132fc46d13fbbffc75
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-01-18 21:29:35 +0000

    Forgot about DllNotFoundException
    svn path=/trunk/mcs/; revision=2054

---
+++ mcs/class/corlib/System/DllNotFoundException.cs
@@ -0,0 +1,42 @@
+//	
+// System.DllNotFoundException.cs
+//
+// Author:
+//	Duncan Mak (duncan@ximian.com)
+//
+// 2002 (C) Ximian, Inc. http://www.ximian.com
+//
+
+using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace System
+{
+	   [Serializable]
+	   public class DllNotFoundException : SystemException
+	   {
+			 // Constructors
+			 public DllNotFoundException ()
+				    : base (Locale.GetText ("DLL not found."));
+			 {
+			 }
+
+			 public DllNotFoundException (string message)
+				    : base (message)
+			 {
+			 }
+			 
+			 public DllNotFoundException (SerializationInfo info,
+				    StreamingContext context)
+				    : base (info, context)
+			 {
+			 }
+
+			 public DllNotFoundException (string message, Exception innerException)
+				    :base (message, innerException)
+			 {
+			 }
+				    
+	   }
+}
