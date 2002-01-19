83e508f297cac823c2f4e39dc99df90956d5c4b1
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-01-19 01:27:37 +0000

    Added missing Exceptions in System.IO and System.Security.Policy
    svn path=/trunk/mcs/; revision=2061

---
+++ mcs/class/corlib/System.IO/EndOfStreamException.cs
@@ -0,0 +1,42 @@
+//	
+// System.EndOfStreamException.cs
+//
+// Author:
+//	Duncan Mak (duncan@ximian.com)
+//
+// 2002 (C) Ximian, Inc. http://www.ximian.com
+//
+
+using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace System
+{
+	   [Serializable]
+	   public class EndOfStreamException : SystemException
+	   {
+			 // Constructors
+			 public EndOfStreamException ()
+				    : base (Locale.GetText ("Failed to read past end of stream."))
+			 {
+			 }
+
+			 public EndOfStreamException (string message)
+				    : base (message)
+			 {
+			 }
+			 
+			 protected EndOfStreamException (SerializationInfo info,
+				    StreamingContext context)
+				    : base (info, context)
+			 {
+			 }
+
+			 public EndOfStreamException (string message, Exception innerException)
+				    :base (message, innerException)
+			 {
+			 }
+				    
+	   }
+}
