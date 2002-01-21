3cc87e2203944ad1c5747268c5d46bd1e5bba26a
Veronica De Santis <veronica@mono-cvs.ximian.com>
2002-01-21 22:50:33 +0000

    2002-01-22 Veronica De Santis <veron78@interfree.it>
    	in mono/mono/metadata/
            * icall.c : Added mapping of internal calls for Manual and Auto reset events
            * threads.c : Added the implementation of internal calls for events
            * threads.h : Added prototypes of internal calls for events
    
    	in mcs/class/corlib/System.Threading/
            * NativeEventCalls : Class that contains internal calls shared by Auto
                                 and Manual Reset Events
            * AutoResetEvents.cs : Added class AutoResetEvents and its implementation
            * ManualResetEvents.cs : Added class ManualResetEvents and its implementation
    
    	also added 2 tests in mono/mono/tests/
    	* autoresetevents.cs
    	* manualresetevents.cs
    
    svn path=/trunk/mcs/; revision=2091

--- mcs/class/corlib/System.Threading/AutoResetEvent.cs
+++ mcs/class/corlib/System.Threading/AutoResetEvent.cs
@@ -3,18 +3,33 @@
 //
 // Author:
 //   Dick Porter (dick@ximian.com)
+//   Veronica De Santis (veron78@interfree.it)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
 //
 
+using System;
+using System.Runtime.CompilerServices;
 
 namespace System.Threading 
 {
-	public sealed class AutoResetEvent : WaitHandle 
+
+ 	public sealed class AutoResetEvent : WaitHandle 
 	{
-		[MonoTODO]
+		// Constructor
 		public AutoResetEvent(bool initialState) {
-			// FIXME
+			os_handle = NativeEventCalls.CreateEvent_internal(false,initialState,null);
 		}
+
+		// Methods
+
+		public bool Set() {
+			return(NativeEventCalls.SetEvent_internal(os_handle));
+		}
+
+		public bool Reset() {
+			return(NativeEventCalls.ResetEvent_internal(os_handle));
+		}
+
 	}
 }
--- mcs/class/corlib/System.Threading/ManualResetEvent.cs
+++ mcs/class/corlib/System.Threading/ManualResetEvent.cs
@@ -3,15 +3,33 @@
 //
 // Author:
 //   Dick Porter (dick@ximian.com)
+//   Veronica De Santis (veron78@interfree.it)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
 //
 
-namespace System.Threading
+using System;
+using System.Runtime.CompilerServices;
+
+namespace System.Threading 
 {
-	[MonoTODO]
-	public sealed class ManualResetEvent : WaitHandle
+
+ 	public sealed class ManualResetEvent : WaitHandle 
 	{
-		// FIXME
+		// Constructor
+		public ManualResetEvent(bool initialState) {
+			os_handle = NativeEventCalls.CreateEvent_internal(true,initialState,null);
+		}
+
+		// Methods
+
+		public bool Set() {
+			return(NativeEventCalls.SetEvent_internal(os_handle));
+		}
+
+		public bool Reset() {
+			return(NativeEventCalls.ResetEvent_internal(os_handle));
+		}
+
 	}
 }
