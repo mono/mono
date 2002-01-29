fbaf4fd8475c0334280e59b49f9cf12d7b1691c4
Nick Drochak <nickd@mono-cvs.ximian.com>
2002-01-29 13:33:52 +0000

    2002-01-30  Nick Drochak  <ndrochak@gol.com>
    	* CodeGroup.cs: New file.  Not nearly done with this, but saving it
    	in cvs just in case.
    	* PolicyLevel.cs: Sarted just a bit on this then relized i need CodeGroup
    	first (among others).  Checking in for disaster recovery.
    
    2002-01-29  Nick Drochak  <ndrochak@gol.com>
    
    	* PolicyStatementAttribute.cs: New file.
    	* PolicyException.cs: Use correct namespace
    	* PolicyLevel.cs: Add [Serializable] to class
    
    svn path=/trunk/mcs/; revision=2189

---
+++ mcs/class/corlib/System.Security.Policy/CodeGroup.cs
@@ -0,0 +1,49 @@
+// System.Security.Policy.CodeGroup
+//
+// Nick Drochak (ndrochak@gol.com)
+//
+// (C) 2001 Nick Drochak
+
+using System.Security.Policy;
+using System.Security.Permissions;
+
+namespace System.Security.Policy
+{
+	[MonoTODO]
+	[Serializable]
+	public abstract class CodeGroup
+	{
+		PolicyStatement m_policy;
+		IMembershipCondition m_membershipCondition;
+
+		public CodeGroup(IMembershipCondition membershipCondition,
+					PolicyStatement policy)
+		{
+			if (null == membershipCondition)
+				throw new ArgumentNullException("Value cannot be null.");
+			m_policy = policy;
+			m_membershipCondition = membershipCondition;
+		}
+
+		public abstract CodeGroup Copy();
+		public abstract string MergeLogic {get;}
+		public abstract PolicyStatement Resolve(	Evidence evidence);
+		public abstract CodeGroup ResolveMatchingCodeGroups(Evidence evidence);
+
+		public PolicyStatement PolicyStatement
+		{
+			get
+			{
+				return m_policy;
+			}
+			set
+			{
+				m_policy = value;
+			}
+		}
+
+	}  // public abstract class CodeGroup
+
+	// FIXME: remove after done testing in VS.NET
+	public class MonoTODO : Attribute {}
+}  // namespace System.Security.Policy
--- mcs/class/corlib/System.Security.Policy/PolicyException.cs
+++ mcs/class/corlib/System.Security.Policy/PolicyException.cs
@@ -11,7 +11,7 @@ using System;
 using System.Globalization;
 using System.Runtime.Serialization;
 
-namespace System
+namespace System.Security.Policy
 {
 	   [Serializable]
 	   public class PolicyException : SystemException
