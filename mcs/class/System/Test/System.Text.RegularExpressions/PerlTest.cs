b862e0ee3b36a4e52df7fb4be73a4c495eb7f721
Dan Lewis <dan@mono-cvs.ximian.com>
2002-01-31 21:47:17 +0000

    Removed /nowarn:1595, added /noconfig flag to build file (CSC was using the Microsoft assemblies in preference.)
    Removed checksum from Perl regex trial descriptions. Now using a descriptive
    string.
    
    svn path=/trunk/mcs/; revision=2216

--- mcs/class/System/Test/System.Text.RegularExpressions/PerlTest.cs
+++ mcs/class/System/Test/System.Text.RegularExpressions/PerlTest.cs
@@ -23,142 +23,18 @@ namespace MonoTests.System.Text.RegularExpressions {
 
 		public void TestTrials () {
 			foreach (RegexTrial trial in PerlTrials.trials) {
-				string msg = trial.Execute ();
-				if (msg != null)
-					Assertion.Fail (msg);
+				string actual = trial.Execute ();
+				if (actual != trial.Expected) {
+					Assertion.Fail (
+						trial.ToString () +
+						"Expected " + trial.Expected +
+						" but got " + actual
+					);
+				}
 			}
 		}
 
 		protected override void SetUp () { }
 		protected override void TearDown () { }
-	}
-
-	class RegexTrial {
-		public string pattern;
-		public RegexOptions options;
-		public string input;
-
-		public Result expected;
-		public uint checksum;
-
-		public RegexTrial (string pattern, RegexOptions options, string input, Result expected, uint checksum) {
-			this.pattern = pattern;
-			this.options = options;
-			this.input = input;
-			this.expected = expected;
-			this.checksum = checksum;
-		}
-
-		public string Execute () {
-			try {
-				Regex re = new Regex (pattern, options);
-				Match m = re.Match (input);
-
-				if (m.Success) {
-					uint sum = CreateChecksum (re, m);
-					return Report (Result.Pass, sum);
-				}
-
-				return Report (Result.Fail, 0);
-			}
-			catch (Exception) {
-				return Report (Result.Error, 0);
-			}
-		}
-
-		public override string ToString () {
-			return
-				"Matching input '" + input +
-				"' against pattern '" + pattern +
-				"' with options '" + options + "'.";
-		}
-
-		// private
-
-		private static uint CreateChecksum (Regex re, Match m) {
-			Checksum sum = new Checksum ();
-		
-			// group name mapping
-
-			string[] names = re.GetGroupNames ();
-			foreach (string name in re.GetGroupNames ()) {
-				sum.Add (name);
-				sum.Add ((uint)re.GroupNumberFromName (name));
-			}
-
-			// capture history
-
-			foreach (Group group in m.Groups) {
-				foreach (Capture cap in group.Captures) {
-					sum.Add ((uint)cap.Index);
-					sum.Add ((uint)cap.Length);
-				}
-			}
-
-			return sum.Value;
-		}
-
-		private string Report (Result actual, uint sum) {
-			if (actual == expected && sum == checksum)
-				return null;
-
-			string msg = this.ToString ();
-			if (actual != expected) {
-				msg +=
-					" Expected " + expected +
-					", but result was " + actual + ".";
-			}
-
-			if (sum != checksum)
-				msg += " Bad checksum.";
-
-			return msg;
-		}
-
-	}
-
-	enum Result {
-		Pass,
-		Fail,
-		Error
-	}
-
-	class Checksum {
-		public Checksum () {
-			this.sum = 0;
-		}
-
-		public uint Value {
-			get { return sum; }
-		}
-
-		public void Add (string str) {
-			for (int i = 0; i < str.Length; ++ i)
-				Add (str[i], 16);
-		}
-
-		public void Add (uint n) {
-			Add (n, 32);
-		}
-
-		public void Add (ulong n, int bits) {
-			ulong mask = 1ul << (bits - 1);
-			for (int i = 0; i < bits; ++ i) {
-				Add ((n & mask) != 0);
-				mask >>= 1;
-			}
-		}
-
-		public void Add (bool bit) {
-			bool top = (sum & 0x80000000) != 0;
-			sum <<= 1;
-			sum ^= bit ? (uint)1 : (uint)0;
-
-			if (top)
-				sum ^= key;
-		}
-
-		private uint sum;
-		private readonly uint key = 0x04c11db7;
 	}
 }
