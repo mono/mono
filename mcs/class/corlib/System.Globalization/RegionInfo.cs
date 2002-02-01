440865cf1b0c32a278a30fe79c8664d70b377380
Radek Doulik <rodo@mono-cvs.ximian.com>
2002-02-01 16:55:01 +0000

    more RegionInfo work
    svn path=/trunk/mcs/; revision=2229

--- mcs/class/corlib/System.Globalization/RegionInfo.cs
+++ mcs/class/corlib/System.Globalization/RegionInfo.cs
@@ -184,7 +184,7 @@ namespace System.Globalization {
 			case "AQ": // Antarctica
 				NLS_id = 010;
 				break;
-			case "AG": // Antigua And Barbuda
+			case "AG": // Antigua and Barbuda
 				NLS_id = 028;
 				break;
 			case "AR": // Argentina
@@ -238,7 +238,7 @@ namespace System.Globalization {
 			case "BO": // Bolivia
 				NLS_id = 068;
 				break;
-			case "BA": // Bosnia And Herzegowina
+			case "BA": // Bosnia and Herzegowina
 				NLS_id = 070;
 				break;
 			case "BW": // Botswana
@@ -439,7 +439,7 @@ namespace System.Globalization {
 			case "HT": // Haiti
 				NLS_id = 332;
 				break;
-			case "HM": // Heard And Mc Donald Islands
+			case "HM": // Heard and Mc Donald Islands
 				NLS_id = 334;
 				break;
 			case "VA": // Holy See (Vatican City State)
@@ -463,7 +463,7 @@ namespace System.Globalization {
 			case "ID": // Indonesia
 				NLS_id = 360;
 				break;
-			case "IR": // Iran (Islamic Republic Of)
+			case "IR": // Iran (Islamic Republic of)
 				NLS_id = 364;
 				break;
 			case "IQ": // Iraq
@@ -496,10 +496,10 @@ namespace System.Globalization {
 			case "KI": // Kiribati
 				NLS_id = 296;
 				break;
-			case "KP": // Korea, Democratic People'S Republic Of
+			case "KP": // Korea, Democratic People's Republic of
 				NLS_id = 408;
 				break;
-			case "KR": // Korea, Republic Of
+			case "KR": // Korea, Republic of
 				NLS_id = 410;
 				break;
 			case "KW": // Kuwait
@@ -508,7 +508,7 @@ namespace System.Globalization {
 			case "KG": // Kyrgyzstan
 				NLS_id = 417;
 				break;
-			case "LA": // Lao People'S Democratic Republic
+			case "LA": // Lao People's Democratic Republic
 				NLS_id = 418;
 				break;
 			case "LV": // Latvia
@@ -538,7 +538,7 @@ namespace System.Globalization {
 			case "MO": // Macau
 				NLS_id = 446;
 				break;
-			case "MK": // Macedonia, The Former Yugoslav Republic Of
+			case "MK": // Macedonia, The Former Yugoslav Republic of
 				NLS_id = 807;
 				break;
 			case "MG": // Madagascar
@@ -577,10 +577,10 @@ namespace System.Globalization {
 			case "MX": // Mexico
 				NLS_id = 484;
 				break;
-			case "FM": // Micronesia, Federated States Of
+			case "FM": // Micronesia, Federated States of
 				NLS_id = 583;
 				break;
-			case "MD": // Moldova, Republic Of
+			case "MD": // Moldova, Republic of
 				NLS_id = 498;
 				break;
 			case "MC": // Monaco
@@ -694,13 +694,13 @@ namespace System.Globalization {
 			case "RW": // Rwanda
 				NLS_id = 646;
 				break;
-			case "KN": // Saint Kitts And Nevis
+			case "KN": // Saint Kitts and Nevis
 				NLS_id = 659;
 				break;
 			case "LC": // Saint Lucia
 				NLS_id = 662;
 				break;
-			case "VC": // Saint Vincent And The Grenadines
+			case "VC": // Saint Vincent and The Grenadines
 				NLS_id = 670;
 				break;
 			case "WS": // Samoa
@@ -709,7 +709,7 @@ namespace System.Globalization {
 			case "SM": // San Marino
 				NLS_id = 674;
 				break;
-			case "ST": // Sao Tome And Principe
+			case "ST": // Sao Tome and Principe
 				NLS_id = 678;
 				break;
 			case "SA": // Saudi Arabia
@@ -742,7 +742,7 @@ namespace System.Globalization {
 			case "ZA": // South Africa
 				NLS_id = 710;
 				break;
-			case "GS": // South Georgia And The South Sandwich Islands
+			case "GS": // South Georgia and The South Sandwich Islands
 				NLS_id = 239;
 				break;
 			case "ES": // Spain
@@ -754,7 +754,7 @@ namespace System.Globalization {
 			case "SH": // St. Helena
 				NLS_id = 654;
 				break;
-			case "PM": // St. Pierre And Miquelon
+			case "PM": // St. Pierre and Miquelon
 				NLS_id = 666;
 				break;
 			case "SD": // Sudan
@@ -763,7 +763,7 @@ namespace System.Globalization {
 			case "SR": // Suriname
 				NLS_id = 740;
 				break;
-			case "SJ": // Svalbard And Jan Mayen Islands
+			case "SJ": // Svalbard and Jan Mayen Islands
 				NLS_id = 744;
 				break;
 			case "SZ": // Swaziland
@@ -778,13 +778,13 @@ namespace System.Globalization {
 			case "SY": // Syrian Arab Republic
 				NLS_id = 760;
 				break;
-			case "TW": // Taiwan, Province Of China
+			case "TW": // Taiwan, Province of China
 				NLS_id = 158;
 				break;
 			case "TJ": // Tajikistan
 				NLS_id = 762;
 				break;
-			case "TZ": // Tanzania, United Republic Of
+			case "TZ": // Tanzania, United Republic of
 				NLS_id = 834;
 				break;
 			case "TH": // Thailand
@@ -799,7 +799,7 @@ namespace System.Globalization {
 			case "TO": // Tonga
 				NLS_id = 776;
 				break;
-			case "TT": // Trinidad And Tobago
+			case "TT": // Trinidad and Tobago
 				NLS_id = 780;
 				break;
 			case "TN": // Tunisia
@@ -811,7 +811,7 @@ namespace System.Globalization {
 			case "TM": // Turkmenistan
 				NLS_id = 795;
 				break;
-			case "TC": // Turks And Caicos Islands
+			case "TC": // Turks and Caicos Islands
 				NLS_id = 796;
 				break;
 			case "TV": // Tuvalu
@@ -856,7 +856,7 @@ namespace System.Globalization {
 			case "VI": // Virgin Islands (U.S.)
 				NLS_id = 850;
 				break;
-			case "WF": // Wallis And Futuna Islands
+			case "WF": // Wallis and Futuna Islands
 				NLS_id = 876;
 				break;
 			case "EH": // Western Sahara
@@ -886,7 +886,527 @@ namespace System.Globalization {
 			get {
 				switch (NLS_id) {
 				default:
-					throw new Exception ();
+					throw new Exception ("Dunno what is currency symbol for " + NLS_id + " Region. FIXME.");
+				}
+			}
+		}
+
+		[MonoTODO]
+		public static RegionInfo CurrentRegion {
+			get {
+				return null;
+			}
+		}
+
+		public virtual string DisplayName {
+			get {
+				switch (NLS_id) {
+				case 203: // Czech republic
+					return "Èeská republika";
+				case 840: // United States
+					return "United States";
+				default:
+					throw new Exception ("FIXME. Please add your region name in language used in this region.");
+				}
+			}
+		}
+
+		public virtual string EnglishName {
+			get {
+				switch (NLS_id) {
+				case 004:
+					return "Afghanistan";
+				case 008:
+					return "Albania";
+				case 012:
+					return "Algeria";
+				case 016:
+					return "American Samoa";
+				case 020:
+					return "Andorra";
+				case 024:
+					return "Angola";
+				case 660:
+					return "Anguilla";
+				case 010:
+					return "Antarctica";
+				case 028:
+					return "Antigua and Barbuda";
+				case 032:
+					return "Argentina";
+				case 051:
+					return "Armenia";
+				case 533:
+					return "Aruba";
+				case 036:
+					return "Australia";
+				case 040:
+					return "Austria";
+				case 031:
+					return "Azerbaijan";
+				case 044:
+					return "Bahamas";
+				case 048:
+					return "Bahrain";
+				case 050:
+					return "Bangladesh";
+				case 052:
+					return "Barbados";
+				case 112:
+					return "Belarus";
+				case 056:
+					return "Belgium";
+				case 084:
+					return "Belize";
+				case 204:
+					return "Benin";
+				case 060:
+					return "Bermuda";
+				case 064:
+					return "Bhutan";
+				case 068:
+					return "Bolivia";
+				case 070:
+					return "Bosnia and Herzegowina";
+				case 072:
+					return "Botswana";
+				case 074:
+					return "Bouvet Island";
+				case 076:
+					return "Brazil";
+				case 086:
+					return "British Indian Ocean Territory";
+				case 096:
+					return "Brunei Darussalam";
+				case 100:
+					return "Bulgaria";
+				case 854:
+					return "Burkina Faso";
+				case 108:
+					return "Burundi";
+				case 116:
+					return "Cambodia";
+				case 120:
+					return "Cameroon";
+				case 124:
+					return "Canada";
+				case 132:
+					return "Cape Verde";
+				case 136:
+					return "Cayman Islands";
+				case 140:
+					return "Central African Republic";
+				case 148:
+					return "Chad";
+				case 152:
+					return "Chile";
+				case 156:
+					return "China";
+				case 162:
+					return "Christmas Island";
+				case 166:
+					return "Cocos (Keeling) Islands";
+				case 170:
+					return "Colombia";
+				case 174:
+					return "Comoros";
+				case 178:
+					return "Congo";
+				case 184:
+					return "Cook Islands";
+				case 188:
+					return "Costa Rica";
+				case 384:
+					return "Cote D'Ivoire";
+				case 191:
+					return "Croatia (Local Name: Hrvatska)";
+				case 192:
+					return "Cuba";
+				case 196:
+					return "Cyprus";
+				case 203:
+					return "Czech Republic";
+				case 208:
+					return "Denmark";
+				case 262:
+					return "Djibouti";
+				case 212:
+					return "Dominica";
+				case 214:
+					return "Dominican Republic";
+				case 626:
+					return "East Timor";
+				case 218:
+					return "Ecuador";
+				case 818:
+					return "Egypt";
+				case 222:
+					return "El Salvador";
+				case 226:
+					return "Equatorial Guinea";
+				case 232:
+					return "Eritrea";
+				case 233:
+					return "Estonia";
+				case 231:
+					return "Ethiopia";
+				case 238:
+					return "Falkland Islands (Malvinas)";
+				case 234:
+					return "Faroe Islands";
+				case 242:
+					return "Fiji";
+				case 246:
+					return "Finland";
+				case 250:
+					return "France";
+				case 249:
+					return "France, Metropolitan";
+				case 254:
+					return "French Guiana";
+				case 258:
+					return "French Polynesia";
+				case 260:
+					return "French Southern Territories";
+				case 266:
+					return "Gabon";
+				case 270:
+					return "Gambia";
+				case 268:
+					return "Georgia";
+				case 276:
+					return "Germany";
+				case 288:
+					return "Ghana";
+				case 292:
+					return "Gibraltar";
+				case 300:
+					return "Greece";
+				case 304:
+					return "Greenland";
+				case 308:
+					return "Grenada";
+				case 312:
+					return "Guadeloupe";
+				case 316:
+					return "Guam";
+				case 320:
+					return "Guatemala";
+				case 324:
+					return "Guinea";
+				case 624:
+					return "Guinea-Bissau";
+				case 328:
+					return "Guyana";
+				case 332:
+					return "Haiti";
+				case 334:
+					return "Heard and Mc Donald Islands";
+				case 336:
+					return "Holy See (Vatican City State)";
+				case 340:
+					return "Honduras";
+				case 344:
+					return "Hong Kong";
+				case 348:
+					return "Hungary";
+				case 352:
+					return "Iceland";
+				case 356:
+					return "India";
+				case 360:
+					return "Indonesia";
+				case 364:
+					return "Iran (Islamic Republic of)";
+				case 368:
+					return "Iraq";
+				case 372:
+					return "Ireland";
+				case 376:
+					return "Israel";
+				case 380:
+					return "Italy";
+				case 388:
+					return "Jamaica";
+				case 392:
+					return "Japan";
+				case 400:
+					return "Jordan";
+				case 398:
+					return "Kazakhstan";
+				case 404:
+					return "Kenya";
+				case 296:
+					return "Kiribati";
+				case 408:
+					return "Korea, Democratic People's Republic of";
+				case 410:
+					return "Korea, Republic of";
+				case 414:
+					return "Kuwait";
+				case 417:
+					return "Kyrgyzstan";
+				case 418:
+					return "Lao People's Democratic Republic";
+				case 428:
+					return "Latvia";
+				case 422:
+					return "Lebanon";
+				case 426:
+					return "Lesotho";
+				case 430:
+					return "Liberia";
+				case 434:
+					return "Libyan Arab Jamahiriya";
+				case 438:
+					return "Liechtenstein";
+				case 440:
+					return "Lithuania";
+				case 442:
+					return "Luxembourg";
+				case 446:
+					return "Macau";
+				case 807:
+					return "Macedonia, The Former Yugoslav Republic of";
+				case 450:
+					return "Madagascar";
+				case 454:
+					return "Malawi";
+				case 458:
+					return "Malaysia";
+				case 462:
+					return "Maldives";
+				case 466:
+					return "Mali";
+				case 470:
+					return "Malta";
+				case 584:
+					return "Marshall Islands";
+				case 474:
+					return "Martinique";
+				case 478:
+					return "Mauritania";
+				case 480:
+					return "Mauritius";
+				case 175:
+					return "Mayotte";
+				case 484:
+					return "Mexico";
+				case 583:
+					return "Micronesia, Federated States of";
+				case 498:
+					return "Moldova, Republic of";
+				case 492:
+					return "Monaco";
+				case 496:
+					return "Mongolia";
+				case 500:
+					return "Montserrat";
+				case 504:
+					return "Morocco";
+				case 508:
+					return "Mozambique";
+				case 104:
+					return "Myanmar";
+				case 516:
+					return "Namibia";
+				case 520:
+					return "Nauru";
+				case 524:
+					return "Nepal";
+				case 528:
+					return "Netherlands";
+				case 530:
+					return "Netherlands Antilles";
+				case 540:
+					return "New Caledonia";
+				case 554:
+					return "New Zealand";
+				case 558:
+					return "Nicaragua";
+				case 562:
+					return "Niger";
+				case 566:
+					return "Nigeria";
+				case 570:
+					return "Niue";
+				case 574:
+					return "Norfolk Island";
+				case 580:
+					return "Northern Mariana Islands";
+				case 578:
+					return "Norway";
+				case 512:
+					return "Oman";
+				case 586:
+					return "Pakistan";
+				case 585:
+					return "Palau";
+				case 591:
+					return "Panama";
+				case 598:
+					return "Papua New Guinea";
+				case 600:
+					return "Paraguay";
+				case 604:
+					return "Peru";
+				case 608:
+					return "Philippines";
+				case 612:
+					return "Pitcairn";
+				case 616:
+					return "Poland";
+				case 620:
+					return "Portugal";
+				case 630:
+					return "Puerto Rico";
+				case 634:
+					return "Qatar";
+				case 638:
+					return "Reunion";
+				case 642:
+					return "Romania";
+				case 643:
+					return "Russian Federation";
+				case 646:
+					return "Rwanda";
+				case 659:
+					return "Saint Kitts and Nevis";
+				case 662:
+					return "Saint Lucia";
+				case 670:
+					return "Saint Vincent and The Grenadines";
+				case 882:
+					return "Samoa";
+				case 674:
+					return "San Marino";
+				case 678:
+					return "Sao Tome and Principe";
+				case 682:
+					return "Saudi Arabia";
+				case 686:
+					return "Senegal";
+				case 690:
+					return "Seychelles";
+				case 694:
+					return "Sierra Leone";
+				case 702:
+					return "Singapore";
+				case 703:
+					return "Slovakia (Slovak Republic)";
+				case 705:
+					return "Slovenia";
+				case 090:
+					return "Solomon Islands";
+				case 706:
+					return "Somalia";
+				case 710:
+					return "South Africa";
+				case 239:
+					return "South Georgia and The South Sandwich Islands";
+				case 724:
+					return "Spain";
+				case 144:
+					return "Sri Lanka";
+				case 654:
+					return "St. Helena";
+				case 666:
+					return "St. Pierre and Miquelon";
+				case 736:
+					return "Sudan";
+				case 740:
+					return "Suriname";
+				case 744:
+					return "Svalbard and Jan Mayen Islands";
+				case 748:
+					return "Swaziland";
+				case 752:
+					return "Sweden";
+				case 756:
+					return "Switzerland";
+				case 760:
+					return "Syrian Arab Republic";
+				case 158:
+					return "Taiwan, Province of China";
+				case 762:
+					return "Tajikistan";
+				case 834:
+					return "Tanzania, United Republic of";
+				case 764:
+					return "Thailand";
+				case 768:
+					return "Togo";
+				case 772:
+					return "Tokelau";
+				case 776:
+					return "Tonga";
+				case 780:
+					return "Trinidad and Tobago";
+				case 788:
+					return "Tunisia";
+				case 792:
+					return "Turkey";
+				case 795:
+					return "Turkmenistan";
+				case 796:
+					return "Turks and Caicos Islands";
+				case 798:
+					return "Tuvalu";
+				case 800:
+					return "Uganda";
+				case 804:
+					return "Ukraine";
+				case 784:
+					return "United Arab Emirates";
+				case 826:
+					return "United Kingdom";
+				case 840:
+					return "United States";
+				case 581:
+					return "United States Minor Outlying Islands";
+				case 858:
+					return "Uruguay";
+				case 860:
+					return "Uzbekistan";
+				case 548:
+					return "Vanuatu";
+				case 862:
+					return "Venezuela";
+				case 704:
+					return "Viet Nam";
+				case 092:
+					return "Virgin Islands (British)";
+				case 850:
+					return "Virgin Islands (U.S.)";
+				case 876:
+					return "Wallis and Futuna Islands";
+				case 732:
+					return "Western Sahara";
+				case 887:
+					return "Yemen";
+				case 891:
+					return "Yugoslavia";
+				case 180:
+					return "Zaire";
+				case 894:
+					return "Zambia";
+				case 716:
+					return "Zimbabwe";
+				default:
+					throw new Exception ("This code should not be reached.");
+				}
+			}
+		}
+
+		public virtual bool IsMetric {
+			get {
+				switch (NLS_id) {
+				case 203: // Czech Republic
+					return true;
+				case 840: // United States
+					return false;
+				default:
+					throw new Exception ("FIXME. Please define.");
 				}
 			}
 		}
