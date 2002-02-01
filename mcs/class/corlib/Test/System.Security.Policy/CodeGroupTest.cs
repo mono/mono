fcfb9958fdea72350ee55e99bd0f91d15006e904
Nick Drochak <nickd@mono-cvs.ximian.com>
2002-02-01 20:39:18 +0000

    2002-02-01  Nick Drochak  <ndrochak@gol.com>
    	* CodeGroupTest.cs: Still not done, checking in now just in case
    
    svn path=/trunk/mcs/; revision=2231

--- mcs/class/corlib/Test/System.Security.Policy/CodeGroupTest.cs
+++ mcs/class/corlib/Test/System.Security.Policy/CodeGroupTest.cs
@@ -88,12 +88,31 @@ namespace MonoTests.System.Security.Policy
 			try
 			{
 				cg = new MyCodeGroup(new AllMembershipCondition(), new PolicyStatement(new PermissionSet(PermissionState.None)));
-				Assert("Property not set correctly by constructor.", cg.PolicyStatement != null);
+				Assert("PolicyStatement property not set correctly by constructor.", cg.PolicyStatement != null);
+				Assert("MembershipCondition property not set correctly by constructor.", cg.MembershipCondition != null);
 			}
 			catch(Exception e)
 			{
 				Fail("Constructor failed. Exception caught was: " + e.ToString());
 			}
+		}
+
+		public void TestDescriptionProperty()
+		{
+			MyCodeGroup cg = null;
+			const string description = "Test Description";
+			cg = new MyCodeGroup(new AllMembershipCondition(), new PolicyStatement(new PermissionSet(PermissionState.None)));
+			cg.Description = description;
+			Assert("Description not the expected value", cg.Description == description);
+		}
+
+		public void TestNameProperty()
+		{
+			MyCodeGroup cg = null;
+			const string name = "Test Name";
+			cg = new MyCodeGroup(new AllMembershipCondition(), new PolicyStatement(new PermissionSet(PermissionState.None)));
+			cg.Name = name;
+			Assert("Description not the expected value", cg.Name == name);
 		}
 
 	}  // public class CodeGroupTest 
