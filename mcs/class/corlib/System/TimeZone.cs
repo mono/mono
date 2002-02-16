95085eda0f43894bb915ec6c800757fa32441354
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-02-16 20:34:15 +0000

    2002-02-16  Duncan Mak  <duncan@ximian.com>
    	* TimeZone.cs: patch from Ajay Kumar Dwivedi (adwiv@yahoo.com) to
    	make IsDaylightSavingTime (DateTime) call
    	IsDaylightSavingTime (DateTime, DaylightTime).
    
    	Added internal class CurrentTimeZone from Ajay. It needs more work
    	to fill in the appropriate internal calls.
    
    svn path=/trunk/mcs/; revision=2451

--- mcs/class/corlib/System/TimeZone.cs
+++ mcs/class/corlib/System/TimeZone.cs
@@ -2,19 +2,21 @@
 // System.TimeZone.cs
 //
 // Author: Duncan Mak (duncan@ximian.com)
+// 	   Ajay Kumar Dwivedi (adwiv@yahoo.com)
 //
 // (C) Ximian, Inc.
 //
 
+using System.Collections;
 using System.Globalization;
 
 namespace System {
-
+	
 	[Serializable]
 	public abstract class TimeZone
 	{
 		// Fields
-		static TimeZone local;
+		private static TimeZone currentTimeZone;
 		
 		// Constructor
 		protected TimeZone ()
@@ -24,9 +26,14 @@ namespace System {
 		// Properties
 		public static TimeZone CurrentTimeZone
 		{
-			get { return local; }
-		}
+			get {
+				if (currentTimeZone == null)
+					currentTimeZone = new CurrentTimeZone ();
 
+				return currentTimeZone;
+			}
+		}
+		
 		public abstract string DaylightName
 		{
 			get;
@@ -46,19 +53,21 @@ namespace System {
 		{
 			int currentYear = CultureInfo.CurrentCulture.Calendar.GetYear (time);
 			DaylightTime daylight = GetDaylightChanges (currentYear);
-			if ((daylight.Start <= time) && (time < daylight.End))
-				return true;
-			else
-				return false;
+
+			return IsDaylightSavingTime (time, daylight);
 		}
 
 		public virtual bool IsDaylightSavingTime (DateTime time, DaylightTime daylightTimes)
 		{
 			if (daylightTimes == null)
 				throw new ArgumentNullException ("daylightTimes is null.");
+
+			if (daylightTimes.Delta == TimeSpan.Zero)
+				return false; 
+
 			if ((daylightTimes.Start <= time) && (time < daylightTimes.End))
 				return true;
-			else
+			else // time doesn't not fall in the span of daylightTimes
 				return false;
 		}
 
@@ -71,5 +80,64 @@ namespace System {
 		{
 			return time - GetUtcOffset (time);
 		}
+	}
+
+	internal class CurrentTimeZone : TimeZone
+	{		
+		// Fields
+		private static string daylightName;
+                private static string standardName;
+
+                // A yearwise cache of DaylightTime.                 
+                private static Hashtable daylightCache = new Hashtable (1);
+		
+                // the offset when daylightsaving is not on.
+                private static TimeSpan utcOffsetWithOutDLS;
+
+                // the offset when daylightsaving is on.
+                private static TimeSpan utcOffsetWithDLS;
+
+		// Constructor
+		[MonoTODO ("Add internal calls to initialize the fields")]
+		internal CurrentTimeZone ()
+			: base ()
+		{
+		}
+
+		// Properties
+		public override string DaylightName
+                {
+                        get { return daylightName; }
+                }
+                
+                public override string StandardName 
+                {
+                        get { return standardName; }
+                }
+
+		// Methods
+		[MonoTODO]
+		public override DaylightTime GetDaylightChanges (int year)
+                {
+                        if (year < 1 || year > 9999)
+                                throw new ArgumentOutOfRangeException (year + " is not in a range between 1 and 9999.");
+
+                        if (daylightCache [year] == null) {
+                                //TODO: do something and put the DaylightTime corresponding to the time in cache.
+                                DaylightTime dlt = new DaylightTime (new DateTime (0),
+						     new DateTime (0), new TimeSpan (0));
+				daylightCache.Add (year, dlt);
+                        }
+
+			return (DaylightTime) daylightCache [year];
+                }
+
+		public override TimeSpan GetUtcOffset(DateTime time)
+		{
+                        if (IsDaylightSavingTime (time))
+                                return utcOffsetWithDLS;
+                        
+                        return utcOffsetWithOutDLS;
+                }
 	}
 }
