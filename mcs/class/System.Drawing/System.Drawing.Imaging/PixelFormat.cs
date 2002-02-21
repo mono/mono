a3c83bb2215ea3a23639101338a1783234001eb3
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2002-02-21 14:32:45 +0000

    added files from chris
    svn path=/trunk/mcs/; revision=2570

---
+++ mcs/class/System.Drawing/System.Drawing.Imaging/PixelFormat.cs
@@ -0,0 +1,36 @@
+// created on 20.02.2002 at 21:18
+//
+// Image.cs
+//
+// Author: Christian Meyer
+// eMail: Christian.Meyer@cs.tum.edu
+//
+//
+namespace System.Drawing.Imaging {
+
+	public enum PixelFormat {
+		Alpha,
+		Canonical,
+		DontCare,
+		Extended,
+		Format16bppArgb1555,
+		Format16bppGrayScale,
+		Format16bppRgb555,
+		Format16bppRgb565,
+		Format1bppIndexed,
+		Format24bppRgb,
+		Format32bppArgb,
+		Format32bppPArgb,
+		Format32bppRgb,
+		Format48bppRgb, 
+		Format4bppIndexed,
+		Format64bppArgb,
+		Format64bppPArgb,
+		Format8bppIndexed,
+		Gdi,
+		Indexed,
+		Max,
+		PAlpha,
+		Undefined
+	}
+}
