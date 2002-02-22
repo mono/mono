3045c04f15846e8675fa65a33c3b4389fd9c85f4
Paolo Molaro <lupus@oddwiz.org>
2002-02-22 10:45:18 +0000

    Fri Feb 22 15:36:19 CET 2002 Paolo Molaro <lupus@ximian.com>
    	* RuntimeHelpers.cs: added OffsetToStringData() property.
    
    svn path=/trunk/mcs/; revision=2585

--- mcs/class/corlib/System.Runtime.CompilerServices/RuntimeHelpers.cs
+++ mcs/class/corlib/System.Runtime.CompilerServices/RuntimeHelpers.cs
@@ -1,6 +1,7 @@
 // System.Runtime.CompilerServices.RuntimeHelpers
 //
 // Sean MacIsaac (macisaac@ximian.com)
+// Paolo Molaro (lupus@ximian.com)
 //
 // (C) Ximian, Inc. 2001
 
@@ -11,5 +12,12 @@ namespace System.Runtime.CompilerServices
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void InitializeArray (Array array, RuntimeFieldHandle fldHandle);
 
+		public static int OffsetToStringData {
+			get {
+				// FIXME: this requires the reimplementation of String
+				// as of my mail a few months ago, dum de dum
+				throw new NotImplementedException ();
+			}
+		}
 	}
 }
