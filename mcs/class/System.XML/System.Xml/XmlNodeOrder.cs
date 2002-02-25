d334bf4514ea9ccc3ba78438e50efce67a8a74bf
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-02-25 01:24:51 +0000

    2002-02-24  Duncan Mak  <duncan@ximian.com>
    	    * XmlNodeOrder.cs: Added to CVS.
    
    svn path=/trunk/mcs/; revision=2621

---
+++ mcs/class/System.XML/System.Xml/XmlNodeOrder.cs
@@ -0,0 +1,19 @@
+//
+// System.Xml.XmlNodeOrder.cs
+//
+// Author: Duncan Mak (duncan@ximian.com)
+//
+// (C) Ximian, Inc.
+//
+
+namespace System.Xml
+{
+	[Serializable]
+	public enum XmlNodeOrder
+	{
+		Before = 0,
+		After = 1,
+		Same = 2,
+		Unknown = 3,		
+	}
+}
