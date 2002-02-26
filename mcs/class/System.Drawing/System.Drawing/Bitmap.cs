9afbda4accf4c942e0696597fead1e553cc41767
Christian Meyer <christian@mono-cvs.ximian.com>
2002-02-26 19:14:33 +0000

    Added method headers (Bitmap.cs).
    svn path=/trunk/mcs/; revision=2689

--- mcs/class/System.Drawing/System.Drawing/Bitmap.cs
+++ mcs/class/System.Drawing/System.Drawing/Bitmap.cs
@@ -5,8 +5,12 @@
 // Author: Christian Meyer
 // eMail: Christian.Meyer@cs.tum.edu
 //
+// No implementation has been done yet. I first want to write the method
+// headers of every System.Drawing.
+//
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace System.Drawing {
 
@@ -17,10 +21,10 @@ namespace System.Drawing {
 			this.original = original;
 		}
 
-		public Bitmap (Stream stream) {
+/*		public Bitmap (Stream stream) {
 			this.stream = stream;
 		}
-
+*/
 		public Bitmap (string filename) {
 			this.filename = filename;
 		}
@@ -35,11 +39,11 @@ namespace System.Drawing {
 			this.heigth = heigth;
 		}
 
-		public Bitmap (Stream stream, bool useIcm) {
+/*		public Bitmap (Stream stream, bool useIcm) {
 			this.stream = stream;
 			this.useIcm = useIcm;
-		}
-
+		} 
+*/
 		public Bitmap (string filename, bool useIcm) {
 			this.filename = filename;
 			this.useIcm = useIcm;
@@ -61,8 +65,11 @@ namespace System.Drawing {
 			this.heigth = heigth;
 			this.g = g;
 		}
-
+/*
 		public Bitmap (int width, int heigth, PixelFormat format) {
+			this.width = width;
+			this.heigth = heigth;
+			this.format = format;
 		}
 
 		public Bitmap (int width, int height, int stride,
@@ -74,7 +81,66 @@ namespace System.Drawing {
 			this.scan0 = scan0;
 		}
 
-		// properties
 		// methods
+		public Bitmap Clone (Rectangle rect,PixelFormat format) {
+			throw new NotImplementedException ();
+		}
+		
+		public Bitmap Clone (RectangleF rect, PixelFormat format) {
+			throw new NotImplementedException ();
+		}
+*/
+		public static Bitmap FromHicon (IntPtr hicon) {
+			throw new NotImplementedException ();
+		}
+		
+		public static Bitmap FromResource (IntPtr hinstance,
+		                                   string bitmapName) {
+			throw new NotImplementedException ();
+		}
+		
+		public IntPtr GetHbitmap () {
+			throw new NotImplementedException ();
+		}
+		
+		public IntPtr GetHbitmap (Color background) {
+			throw new NotImplementedException ();
+		}
+		
+		public IntPtr GetHicon () {
+			throw new NotImplementedException ();
+		}
+		
+		public Color GetPixel (int x, int y) {
+			throw new NotImplementedException ();
+		}
+		
+/*		public BitmapData LockBits (Rectangle rect, ImageLockMode flags,
+		                            PixelFormat format) {
+			throw new NotImplementedException ();
+		} */
+		
+		public void MakeTransparent () {
+			throw new NotImplementedException ();
+		}
+		
+		public void MakeTransparent (Color transparentColor) {
+			throw new NotImplementedException ();
+		}
+		
+		public void SetPixel (int x, int y, Color color) {
+			throw new NotImplementedException ();
+		}
+		
+		public void SetResolution (float xDpi, float yDpi) {
+			throw new NotImplementedException ();
+		}
+		
+/*		public void UnlockBits (BitmapData bitmapdata) {
+			throw new NotImplementedException ();
+		} */
+		
+		// properties
+		// needs to be done ###FIXME###
 	}
 }
