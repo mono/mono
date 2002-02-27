7f5d6c4db85ee55e974210d5295081c198e8cd11
Martin Baulig <martin@novell.com>
2002-02-27 09:57:11 +0000

    Updated.
    svn path=/trunk/mcs/; revision=2720

--- mcs/class/corlib/Test/TheTests.cs
+++ mcs/class/corlib/Test/TheTests.cs
@@ -5,126 +5,29 @@ using System.Globalization;
 
 namespace MonoTests.System
 {
-	public class RunBooleanTest : BooleanTest
-	{
-		protected override void RunTest ()
-		{
-			TestStrings ();
-			TestCompareTo ();
-			TestEquals ();
-			TestGetHashCode ();
-			TestGetType ();
-			TestGetTypeCode ();
-			TestParse ();
-			TestToString ();
-		}
-	}
-	public class RunStringTest : StringTest
-	{
-		protected override void RunTest ()
-		{
-			TestLength ();
-			TestCompare ();
-			TestCompareOrdinal ();
-			TestCompareTo ();
-			TestConcat ();
-			TestCopy ();
-			TestCopyTo ();
-			TestEndsWith ();
-			TestEquals ();
-			TestFormat ();
-			TestGetEnumerator ();
-			TestGetHashCode ();
-			TestGetType ();
-			TestGetTypeCode ();
-			TestIndexOf ();
-			TestIndexOfAny ();
-			TestInsert ();
-			TestIntern ();
-			TestIsInterned ();
-			TestJoin ();
-			TestLastIndexOf ();
-			TestLastIndexOfAny ();
-			TestPadLeft ();
-			TestPadRight ();
-			TestRemove ();
-			TestReplace ();
-			TestSplit ();
-			TestStartsWith ();
-			TestSubstring ();
-			TestToCharArray ();
-			TestToLower ();
-			TestToString ();
-			TestToUpper ();
-			TestTrim ();
-			TestTrimEnd ();
-			TestTrimStart ();
-		}
-	}
-	public class RunTimeSpanTest : TimeSpanTest
-	{
-		protected override void RunTest ()
-		{
-			TestCtors ();
-			TestProperties ();
-			TestAdd ();
-			TestCompare ();
-			TestNegateAndDuration ();
-			TestEquals ();
-			TestFromXXXX ();
-			TestGetHashCode ();
-			TestParse ();
-			TestSubstract ();
-			TestToString ();
-		}
-	}
-	public class RunDoubleTest : DoubleTest
-	{
-		protected override void RunTest ()
-		{
-			TestPublicFields ();
-			TestCompareTo ();
-			TestEquals ();
-			TestGetHasCode ();
-			TestTypeCode ();
-			TestIsInfinity ();
-			TestIsNan ();
-			TestIsNegativeInfinity ();
-			TestIsPositiveInfinity ();
-			TestParse ();
-			TestToString ();
-		}
-	}
-	public class RunTimeZoneTest : TimeZoneTest
-	{
-		protected override void RunTest ()
-		{
-			TestCtors ();
-		}
-	}
-	public class RunDateTimeTest : DateTimeTest
-	{
-		protected override void RunTest ()
-		{
-			TestCtors ();
-			TestToString ();
-			TestParse ();
-		}
-	}
 }
 
 namespace MonoTests
 {
 	public class RunAllTests
-{
+	{
 		public static void AddAllTests (TestSuite suite)
 		{
-			suite.AddTest (new MonoTests.System.RunBooleanTest ());
-			suite.AddTest (new MonoTests.System.RunStringTest ());
-			suite.AddTest (new MonoTests.System.RunTimeSpanTest ());
-			suite.AddTest (new MonoTests.System.RunDoubleTest ());
-			suite.AddTest (new MonoTests.System.RunTimeZoneTest ());
-			suite.AddTest (new MonoTests.System.RunDateTimeTest ());
 		}
 	}
 }
+
+class MainApp
+{
+	public static void Main()
+	{
+		Thread.CurrentThread.CurrentCulture = new CultureInfo ("en-US");
+
+		TestResult result = new TestResult ();
+		TestSuite suite = new TestSuite ();
+		MonoTests.RunAllTests.AddAllTests (suite);
+		suite.Run (result);
+		MonoTests.MyTestRunner.Print (result);
+	}
+}
+
