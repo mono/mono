f414ffd111cc7f239df1d068af7f7bd6a56482b2
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2002-03-04 05:05:37 +0000

    2002-03-03     Gaurav Vaish <gvaish@iitk.ac.in>
    * Repeater.cs  -- File added to CVS. Will work on it sometime later. Feeling
     sleepy - at 2:30 in the afternoon. ;-)
    
    svn path=/trunk/mcs/; revision=2870

---
+++ mcs/class/System.Web/System.Web.UI.WebControls/Repeater.cs
@@ -0,0 +1,24 @@
+/**
+ * Namespace: System.Web.UI.WebControls
+ * Class:     Repeater
+ * 
+ * Author:  Gaurav Vaish
+ * Maintainer: gvaish@iitk.ac.in
+ * Contact: <my_scripts2001@yahoo.com>, <gvaish@iitk.ac.in>
+ * Implementation: yes
+ * Status:  ??%
+ * 
+ * (C) Gaurav Vaish (2002)
+ */
+
+using System;
+using System.Web;
+using System.Web.UI;
+
+namespace System.Web.UI.WebControls
+{
+	public class Repeater : Control, INamingContainer
+	{
+		
+	}
+}
