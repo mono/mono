0582dd0ad57a2337ebf5aa4933236d09d5d20f85
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2002-03-05 01:22:53 +0000

    2002-03-05    Gaurav Vaish <gvaish@iitk.ac.in>
    * *.cs: First work, of delegates.
    
    * ChangeLog, TODO: Details about the namespace.
    
    svn path=/trunk/mcs/; revision=2892

---
+++ mcs/class/System.Web/System.Web.Security/DefaultAuthenticationEventHandler.cs
@@ -0,0 +1,16 @@
+/**
+ * Namespace: System.Web.UI.Security
+ *
+ * Author: Gaurav Vaish
+ * Maintainer: gvaish@iitk.ac.in
+ * Contact:  <my_scripts2001@yahoo.com>, <gvaish@iitk.ac.in>
+ * Implementation: yes
+ * Status: 100%
+ *
+ * (C) Gaurav Vaish (2002)
+ */
+
+namespace System.Web.UI.Security
+{
+	public delegate void DefaultAuthenticationEventHandler(object sender, DefaultAuthenticationEventArgs e);
+}
---
+++ mcs/class/System.Web/System.Web.Security/FormsAuthenticationEventHandler.cs
@@ -0,0 +1,16 @@
+/**
+ * Namespace: System.Web.UI.Security
+ *
+ * Author: Gaurav Vaish
+ * Maintainer: gvaish@iitk.ac.in
+ * Contact:  <my_scripts2002@yahoo.com>, <gvaish@iitk.ac.in>
+ * Implementation: yes
+ * Status: 100%
+ *
+ * (C) Gaurav Vaish (2002)
+ */
+
+namespace System.Web.UI.Security
+{
+	public delegate void FormsAuthenticationEventHandler(object sender, FormsAuthenticationEventArgs e);
+}
