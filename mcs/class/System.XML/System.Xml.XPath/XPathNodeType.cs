a1cd780140a7e3c63762d2b90bb1d8fb466aa8a3
Jason Diamond <injektilo@mono-cvs.ximian.com>
2002-03-08 14:34:13 +0000

    Removed some enumerations that might have been in Beta2 but aren't visible anymore. Moved some enumerations from the System.Xml namespace to System.Xml.XPath because that's where they belong (now?).
    svn path=/trunk/mcs/; revision=2998

---
+++ mcs/class/System.XML/System.Xml.XPath/XPathNodeType.cs
@@ -0,0 +1,25 @@
+//
+// System.Xml.XPath.XPathNodeType
+//
+// Author:
+//   Jason Diamond (jason@injektilo.org)
+//
+// (C) 2002 Jason Diamond  http://injektilo.org/
+//
+
+namespace System.Xml.XPath
+{
+	public enum XPathNodeType
+	{
+		All,
+		Attribute,
+		Comment,
+		Element,
+		Namespace,
+		ProcessingInstruction,
+		Root,
+		SignificantWhitespace,
+		Text,
+		Whitespace
+	}
+}
---
+++ mcs/class/System.XML/System.Xml.XPath/XmlDataType.cs
@@ -0,0 +1,17 @@
+//
+// System.Xml.XPath.XmlDataType
+//
+// Author:
+//   Jason Diamond (jason@injektilo.org)
+//
+// (C) 2002 Jason Diamond  http://injektilo.org/
+//
+
+namespace System.Xml.XPath
+{
+	public enum XmlDataType
+	{
+		Number,
+		Text
+	}
+}
