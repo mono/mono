83dbcb0e9a4ace2a8518a68e1dd38a9dfc56165f
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-03-14 01:53:43 +0000

    2002-03-14  Duncan Mak  <duncan@ximian.com>
    	* XmlNodeChangedEventHandler.cs: Added to CVS.
    
    svn path=/trunk/mcs/; revision=3098

---
+++ mcs/class/System.XML/System.Xml/XmlNodeChangedEventHandler.cs
@@ -0,0 +1,13 @@
+//
+// System.Xml.XmlNodeChangedEventHandler.cs
+//
+// Author:
+//	Duncan Mak  (duncan@ximian.com)
+//
+// (C) Ximian, Inc.
+//
+
+namespace System.Xml
+{
+	public delegate void XmlNodeChangedEventHandler (object sender, XmlNodeChangedEventArgs e);
+}
