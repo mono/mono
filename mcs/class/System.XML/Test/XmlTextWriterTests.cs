83dd3960d0c160ed11cefc1c10ca76340fa002b4
Kral Ferch <kral@mono-cvs.ximian.com>
2002-03-15 01:41:17 +0000

    initial checkin.
    svn path=/trunk/mcs/; revision=3116

---
+++ mcs/class/System.XML/Test/XmlTextWriterTests.cs
@@ -0,0 +1,26 @@
+//
+// System.Xml.XmlTextWriterTests
+//
+// Author:
+//   Kral Ferch <kral_ferch@hotmail.com>
+//
+// (C) 2002 Kral Ferch
+//
+
+using System;
+
+using NUnit.Framework;
+
+namespace Ximian.Mono.Tests
+{
+	public class XmlTextWriterTests : TestCase
+	{
+		public XmlTextWriterTests () : base ("Ximian.Mono.Tests.XmlTextWriterTests testsuite") {}
+		public XmlTextWriterTests (string name) : base (name) {}
+
+		public void TestSomething ()
+		{
+
+		}
+	}
+}
