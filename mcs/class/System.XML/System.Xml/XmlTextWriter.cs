cab7abeb74bae6afc8acb02ecda4c5c58349e3ec
Kral Ferch <kral@mono-cvs.ximian.com>
2002-03-15 01:48:10 +0000

    initial checkin.
    svn path=/trunk/mcs/; revision=3120

---
+++ mcs/class/System.XML/System.Xml/XmlTextWriter.cs
@@ -0,0 +1,267 @@
+//
+// System.Xml.XmlTextWriter
+//
+// Author:
+//   Kral Ferch <kral_ferch@hotmail.com>
+//
+// (C) 2002 Kral Ferch
+//
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace System.Xml
+{
+	public class XmlTextWriter : XmlWriter
+	{
+		#region Constructors
+
+		[MonoTODO]
+		public XmlTextWriter (TextWriter w) : base ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XmlTextWriter (Stream w,	Encoding encoding) : base ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XmlTextWriter (string filename, Encoding encoding) : base ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		#region Properties
+
+		[MonoTODO]
+		public Stream BaseStream {
+			get { throw new NotImplementedException(); }
+		}
+
+
+		[MonoTODO]
+		public Formatting Formatting {
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public int Indentation {
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public char IndentChar {
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public bool Namespaces {
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public char QuoteChar {
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public override WriteState WriteState {
+			get { throw new NotImplementedException(); }
+		}
+		
+		[MonoTODO]
+		public override string XmlLang {
+			get { throw new NotImplementedException(); }
+		}
+
+		[MonoTODO]
+		public override XmlSpace XmlSpace {
+			get { throw new NotImplementedException(); }
+		}
+
+		#endregion
+
+		#region Methods
+
+		[MonoTODO]
+		public override void Close ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void Flush ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override string LookupPrefix (string ns)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteBase64 (byte[] buffer, int index, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteBinHex (byte[] buffer, int index, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteCData (string text)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteCharEntity (char ch)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteChars (char[] buffer, int index, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteComment (string text)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteDocType (string name, string pubid, string sysid, string subset)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteEndAttribute ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteEndDocument ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteEndElement ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteEntityRef (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteFullEndElement ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteName (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteNmToken (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteProcessingInstruction (string name, string text)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteQualifiedName (string localName, string ns)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteRaw (string data)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteRaw (char[] buffer, int index, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteStartAttribute (string prefix, string localName, string ns)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteStartDocument ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteStartDocument (bool standalone)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteStartElement (string prefix, string localName, string ns)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteString (string text)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteSurrogateCharEntity (char lowChar, char highChar)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override void WriteWhitespace (string ws)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+	}
+}
