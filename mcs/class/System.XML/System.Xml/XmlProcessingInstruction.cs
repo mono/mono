9d8e6b51e997e5a7f10188dca01eaa1ebb9084db
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-03-15 18:48:31 +0000

    2002-03-15  Duncan Mak  <duncan@ximian.com>
    	* XmlProcessingInstruction.cs (Value): Added the missing Set
    	block.
    	(InnerText): Added in, but not implemented.
    
    svn path=/trunk/mcs/; revision=3132

--- mcs/class/System.XML/System.Xml/XmlProcessingInstruction.cs
+++ mcs/class/System.XML/System.Xml/XmlProcessingInstruction.cs
@@ -18,7 +18,8 @@ namespace System.Xml
 
 		#region Constructors
 
-		protected internal XmlProcessingInstruction(string target, string data, XmlDocument doc) : base(doc)
+		protected internal XmlProcessingInstruction (string target, string data, XmlDocument doc)
+			: base(doc)
 		{
 			this.target = target;
 			this.data = data;
@@ -30,47 +31,46 @@ namespace System.Xml
 
 		public string Data
 		{
-			get {
-				return data;
-			}
+			get { return data; }
 
-			set {
-				data = value;
-			}
+			set { data = value; }
+		}
+
+		[MonoTODO]
+		public override string InnerText
+		{
+			get { throw new NotImplementedException (); }
+			set { throw new NotImplementedException (); }
 		}
 
 		public override string LocalName
 		{
-			get {
-				return target;
-			}
+			get { return target; }
 		}
 
 		public override string Name
 		{
-			get {
-				return target;
-			}
+			get { return target; }
 		}
 
 		public override XmlNodeType NodeType
 		{
-			get {
-				return XmlNodeType.ProcessingInstruction;
-			}
+			get { return XmlNodeType.ProcessingInstruction; }
 		}
 		
 		public string Target
 		{
-			get {
-				return target;
-			}
+			get { return target; }
 		}
 
 		public override string Value
 		{
-			get {
-				return data;
+			get { return data; }
+			set {
+				if (this.IsReadOnly)
+					throw new ArgumentException ("This node is read-only.");
+				else
+					data = value;
 			}
 		}
 
@@ -78,19 +78,19 @@ namespace System.Xml
 
 		#region Methods
 
-		public override XmlNode CloneNode(bool deep)
+		public override XmlNode CloneNode (bool deep)
 		{
-			return new XmlProcessingInstruction(target, data, OwnerDocument);
+			return new XmlProcessingInstruction (target, data, OwnerDocument);
 		}
 
 		[MonoTODO]
-		public override void WriteContentTo(XmlWriter w)
+		public override void WriteContentTo (XmlWriter w)
 		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public override void WriteTo(XmlWriter w)
+		public override void WriteTo (XmlWriter w)
 		{
 			throw new NotImplementedException ();
 		}
