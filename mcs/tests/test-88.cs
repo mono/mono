de994bf91b4a81863e92d8c1ae35f64f5e91f972
Miguel de Icaza <miguel@gnome.org>
2002-03-17 12:47:41 +0000

    Expand test
    svn path=/trunk/mcs/; revision=3166

--- mcs/tests/test-88.cs
+++ mcs/tests/test-88.cs
@@ -1,15 +1,17 @@
 class X {
 
+static void f (string s)
+{
+s. Split ('a');
+}
+
 	static int Main ()
 	{
-		string s;
-
-		s.Split ('a');
-		try {
-			s.Split ();
-		} catch {
-		}
+		string s = "";
 		
+		s.Split ('a');
+		s.Split ();
+		s.Split ('a', 'b', 'c');
 		return 0;
 	}
 }
