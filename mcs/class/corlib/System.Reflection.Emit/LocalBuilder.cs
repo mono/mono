c83e8154db10874da51f54653605c8575fef5d78
Miguel de Icaza <miguel@gnome.org>
2002-03-24 18:15:56 +0000

    2002-03-23  Miguel de Icaza  <miguel@ximian.com>
    	* LocalBuilder.cs: Drop the symbol_writer as LocalBuilder fields.
    	Do this lazily in SetLocalSymInfo.
    
    svn path=/trunk/mcs/; revision=3309

--- mcs/class/corlib/System.Reflection.Emit/LocalBuilder.cs
+++ mcs/class/corlib/System.Reflection.Emit/LocalBuilder.cs
@@ -2,10 +2,12 @@
 //
 // System.Reflection.Emit/LocalBuilder.cs
 //
-// Author:
+// Authors:
 //   Paolo Molaro (lupus@ximian.com)
+//   Martin Baulig (martin@gnome.org)
+//   Miguel de Icaza (miguel@ximian.com)
 //
-// (C) 2001 Ximian, Inc.  http://www.ximian.com
+// (C) 2001, 2002 Ximian, Inc.  http://www.ximian.com
 //
 
 using System;
@@ -17,20 +19,28 @@ using System.Diagnostics.SymbolStore;
 
 namespace System.Reflection.Emit {
 	public sealed class LocalBuilder {
+		//
+		// These are kept in sync with reflection.h
+		//
+		#region Sync with reflection.h
 		private Type type;
 		private string name;
+		#endregion
+		
+		//
+		// Order does not matter after here
+		//
 		private ModuleBuilder module;
-		private ISymbolWriter symbol_writer;
 		internal int position;
 
 		internal LocalBuilder (ModuleBuilder m, Type t)
 		{
 			this.module = m;
-			this.symbol_writer = module.GetSymWriter ();
 			this.type = t;
 		}
 		public void SetLocalSymInfo (string lname, int startOffset, int endOffset)
 		{
+			ISymbolWriter symbol_writer = module.GetSymWriter ();
 			name = lname;
 
 			if (symbol_writer == null)
@@ -56,7 +66,9 @@ namespace System.Reflection.Emit {
 
 		public Type LocalType
 		{
-			get {return type;}
+			get {
+				return type;
+			}
 		}
 	}
 }
