44f6e8a3317327eba8d07c50832f5e939729c3b5
Patrik Torstensson <totte@mono-cvs.ximian.com>
2002-04-10 08:42:30 +0000

    2002-04-10  Patrik Torstensson <patrik.torstensson@labs2.com>
            * ApplicationHost.cs: Test implementation to support testing.
            * SimpleWorkerRequest.cs: Rewrite and a almost full implementation.
    
    svn path=/trunk/mcs/; revision=3733

--- mcs/class/System.Web/System.Web.Hosting/ApplicationHost.cs
+++ mcs/class/System.Web/System.Web.Hosting/ApplicationHost.cs
@@ -1,17 +1,33 @@
 //
-// System.Web.Hosting.ApplicationHost.cs
+// System.Web.Hosting.ApplicationHost
 //
 // Author:
-//   Bob Smith <bob@thestuff.net>
-//
-// (C) Bob Smith
+//   Patrik Torstensson (Patrik.Torstensson@labs2.com)
+//   (class signature from Bob Smith <bob@thestuff.net> (C) )
 //
 
 using System;
+using System.Runtime.Remoting;
 
-namespace System.Web.Hosting
-{
-        public sealed class ApplicationHost
-                public static object CreateApplicationHost(Type hostType, string virtualDir, string physicalDir);
-        }
+namespace System.Web.Hosting {
+   public sealed class ApplicationHost {
+      [MonoTODO("object CreateApplicationHost() Implement (dummy implementation right now)")]
+      public static object CreateApplicationHost(Type HostType, string VirtualPath, string PhysicalPath) {
+         // Construct and own AppDomain via DomainFactory? Can be good to have control over the web appdomain
+         // Dummy impl: just return a init object..
+
+         // TODO: Save in the created app domain....
+         System.Threading.Thread.GetDomain().SetData(".ASP.Net.App.VirtualPath", VirtualPath);
+         System.Threading.Thread.GetDomain().SetData(".ASP.Net.App.Path", PhysicalPath);
+         
+         // TODO: Set to the install path of the runtime engine....
+         System.Threading.Thread.GetDomain().SetData(".ASP.Net.App.InstallPath", "");
+
+         // TODO: Create a name and id for the application...
+         // TODO: Copy all of the domain info to our new domain
+
+         ObjectHandle obj = System.Threading.Thread.GetDomain().CreateInstance(HostType.Module.Assembly.FullName, HostType.FullName);
+         return obj.Unwrap();
+      }
+   }
 }
--- mcs/class/System.Web/System.Web.Hosting/SimpleWorkerRequest.cs
+++ mcs/class/System.Web/System.Web.Hosting/SimpleWorkerRequest.cs
@@ -1,46 +1,231 @@
-//
-// System.Web.Hosting.SimpleWorkerRequest.cs
+// 
+// System.Web.Hosting
 //
 // Author:
-//   Bob Smith <bob@thestuff.net>
+//   Patrik Torstensson (Patrik.Torstensson@labs2.com)
+//   (class signature from Bob Smith <bob@thestuff.net> (C) )
 //
-// (C) Bob Smith
-//
-
 using System;
+using System.IO;
+using System.Text;
 
-namespace System.Web.Hosting
-{
-        public class SimpleWorkerRequest : HttpWorkerRequest
-        {
-                public SimpleWorkerRequest(string page, string query, TextWriter output);
-                public SimpleWorkerRequest(string appVirtualDir, string appPhysicalDir, string page, string query, TextWriter output);
-                public override string MachineConfigPath {get;}
-                public override void EndOfRequest();
-                public override void FlushResponse(bool finalFlush);
-                public override string GetAppPath();
-                public override string GetAppPathTranslated();
-                public override string GetFilePath();
-                public override string GetFilePathTranslated();
-                public override string GetHttpVerbName();
-                public override string GetHttpVersion();
-                public override string GetLocalAddress();
-                public override int GetLocalPort();
-                public override string GetPathInfo();
-                public override string GetQueryString();
-                public override string GetRawUrl();
-                public override string GetRemoteAddress();
-                public override int GetRemotePort();
-                public override string GetServerVariable(string name);
-                public override string GetUriPath();
-                public override IntPtr GetUserToken();
-                public override string MapPath(string path);
-                public override void SendKnownResponseHeader(int index, string value);
-                public override void SendResponseFromFile(IntPtr handle, long offset, long length);
-                public override void SendResponseFromFile(string filename, long offset, long length);
-                public override void SendResponseFromMemory(byte[] data, int length);
-                public virtual void SendResponseFromMemory(IntPtr data, int length);
-                public override void SendStatus(int statusCode, string statusDescription);
-                public override void SendUnknownResponseHeader(string name, string value);
-        }
+namespace System.Web.Hosting {
+   [MonoTODO("Implement security demands on the path usage functions (and review)")]
+   public class SimpleWorkerRequest : HttpWorkerRequest {
+      private string _Page;
+      private string _Query;
+      private string _PathInfo;
+      private string _AppVirtualPath;
+      private string _AppPhysicalPath;
+      private string _AppInstallPath;
+      private TextWriter   _Output;
+      private bool _HasInstallInfo;
+
+      private SimpleWorkerRequest() {
+      }
+
+      public SimpleWorkerRequest(string Page, string Query, TextWriter Output) {
+         _Page = Page;
+         _Query = Query;
+
+         _AppVirtualPath = System.Threading.Thread.GetDomain().GetData(".ASP.Net.App.VirtualPath").ToString();
+         _AppInstallPath = AppDomain.CurrentDomain.GetData(".ASP.Net.App.InstallPath").ToString();
+         _AppPhysicalPath = CheckAndAddSlash(AppDomain.CurrentDomain.GetData(".ASP.Net.App.Path").ToString());
+
+         _Output = Output;
+
+         if (_AppPhysicalPath == null) {
+            // needs to be in a initialized app domain
+            throw new HttpException("Invalid app domain");
+         }
+
+         _HasInstallInfo = true;
+      }
+
+      public SimpleWorkerRequest(string AppVirtualPath, string AppPhysicalPath, string Page, string Query, TextWriter Output) {
+         if (AppDomain.CurrentDomain.GetData(".ASP.Net.App.Path") == null) {
+            // needs to be in a initialized app domain
+            throw new HttpException("Invalid app domain");
+         }
+
+         _Page = Page;
+         _Query = Query;
+         _AppVirtualPath = AppVirtualPath;
+         _AppPhysicalPath = CheckAndAddSlash(AppPhysicalPath);
+         _Output = Output;
+
+         _HasInstallInfo = true;
+      }
+
+      [MonoTODO("Implement security")]
+      public override string MachineInstallDirectory {
+         get {
+            if (_HasInstallInfo) {
+               return _AppInstallPath;
+            }
+
+            return null;
+         }
+      }
+
+      [MonoTODO("Get config path from Web.Config class")]
+      public override string MachineConfigPath {
+         get {
+            return MachineConfigPath;
+         }
+      }
+
+      public override void EndOfRequest() {
+      }
+
+      public override void FlushResponse(bool finalFlush) {
+         _Output.Flush();
+      }
+
+      public override string GetAppPath() {
+         return _AppVirtualPath;
+      }
+
+      [MonoTODO("Implement security")]
+      public override string GetAppPathTranslated() {
+         return _AppPhysicalPath;
+      }
+
+      public override string GetFilePath() {
+         return CreatePath(false);
+      }
+
+      [MonoTODO("Implement security")]
+      public override string GetFilePathTranslated() {
+         return _AppPhysicalPath + _Page.Replace('/', '\\');
+      }
+
+      public override string GetHttpVerbName() {
+         return "GET";
+      }
+
+      public override string GetHttpVersion() {
+         return "HTTP/1.0";
+      }
+
+      public override string GetLocalAddress() {
+         return "127.0.0.1";
+      }
+
+      public override int GetLocalPort() {
+         return 80;
+      }
+
+      [MonoTODO("Implement security")]
+      public override string GetPathInfo() {
+         if (null != _PathInfo) {
+            return _PathInfo;
+         }
+
+         return System.String.Empty;
+      }
+
+      public override string GetQueryString() {
+         return _Query;
+      }
+
+      public override string GetRawUrl() {
+         if (null != _Query && _Query.Length > 0) {
+            return CreatePath(true) + "?" + _Query;
+         }
+
+         return CreatePath(true);
+      }
+
+      public override string GetRemoteAddress() {
+         return "127.0.0.1";
+      }
+
+      public override int GetRemotePort() {
+         return 0;
+      }
+
+      public override string GetServerVariable(string name) {
+         return System.String.Empty;
+      }
+
+      public override string GetUriPath() {
+         return CreatePath(true);
+      }
+
+      public override IntPtr GetUserToken() {
+         return System.IntPtr.Zero;
+      }
+
+      public override string MapPath(string path) {
+         string sPath = _AppPhysicalPath.Substring(0, _AppPhysicalPath.Length - 1);
+         if (path != null && path.Length > 0 && path != "/") {
+            return sPath;
+         }
+
+         if (path.StartsWith(_AppVirtualPath)) {
+            return sPath + path.Substring(_AppVirtualPath.Length).Replace('/', '\\');
+         }
+
+         return null;
+      }
+
+      public override void SendKnownResponseHeader(int index, string value) {
+      }
+
+      public override void SendResponseFromFile(IntPtr handle, long offset, long length) {
+      }
+
+      public override void SendResponseFromFile(string filename, long offset, long length) {
+      }
+
+      public override void SendResponseFromMemory(byte[] data, int length) {
+         _Output.Write(Encoding.Default.GetChars(data, 0, length));
+      }
+
+      public override void SendStatus(int statusCode, string statusDescription) {
+      }
+
+      public override void SendUnknownResponseHeader(string name, string value) {
+      }
+
+      // Create's a path string
+      private string CheckAndAddSlash(string sPath) {
+         if (null == sPath) {
+            return null;
+         }
+
+         if (!sPath.EndsWith("\\")) {
+            return sPath + "\\";
+         }
+
+         return sPath;
+      }
+
+      // Create's a path string
+      private string CreatePath(bool bIncludePathInfo) {
+         string sPath;
+
+         if ("/" == _AppVirtualPath) {
+            sPath = _AppVirtualPath + "/" + _Page;
+         } else {
+            sPath = "/" + _Page;
+         }
+
+         if (bIncludePathInfo && null != _PathInfo) {
+            return sPath + _PathInfo;
+         }
+
+         return sPath;
+      }
+
+      // Parses out the string after / know as the "path info"
+      private void ParsePathInfo() {
+         int iPos = _Page.IndexOf("/");
+         if (iPos >= 0) {
+            _PathInfo = _Page.Substring(iPos);
+            _Page = _Page.Substring(0, iPos);
+         }
+      }
+   }
 }
