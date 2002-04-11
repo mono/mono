a896c8c81ce4e74289bebb7184cf92881e675d8d
Patrik Torstensson <totte@mono-cvs.ximian.com>
2002-04-11 14:01:36 +0000

    2002-04-11 Patrik Torstensson
    	* Forgot to add new files (again...)
    
    svn path=/trunk/mcs/; revision=3763

---
+++ mcs/class/System.Web/System.Web/HttpPostedFile.cs
@@ -0,0 +1,20 @@
+// 
+// System.Web.HttpPostedFile
+//
+// Author:
+//   Patrik Torstensson (Patrik.Torstensson@labs2.com)
+//
+using System;
+
+namespace System.Web {
+   /// <summary>
+   /// Summary description for HttpPostedFile.
+   /// </summary>
+   public class HttpPostedFile {
+      public HttpPostedFile() {
+         //
+         // TODO: Add constructor logic here
+         //
+      }
+   }
+}
---
+++ mcs/class/System.Web/System.Web/HttpStaticObjectsCollection.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+
+namespace System.Web {
+   [MonoTODO()]
+   public class HttpStaticObjectsCollection {
+      [MonoTODO()]
+      public HttpStaticObjectsCollection() {
+      }
+
+      [MonoTODO()]
+      public object GetObject(string name) {
+         throw new NotImplementedException();
+      }
+
+      [MonoTODO()]
+      public virtual IEnumerator GetEnumerator() {
+         throw new NotImplementedException();
+      }
+      
+      [MonoTODO()]
+      public virtual void CopyTo(Array array, int index) {
+         throw new NotImplementedException();
+      }   
+   }
+}
