bb15492dc5417864350b1e7edcc0cdf088d2c71c
Christian Meyer <christian@mono-cvs.ximian.com>
2002-04-14 13:14:57 +0000

    Added Metafile.cs. No impl. Not finished, just stubs. 50% of ctors are done.
    svn path=/trunk/mcs/; revision=3806

---
+++ mcs/class/System.Drawing/System.Drawing.Imaging/Metafile.cs
@@ -0,0 +1,110 @@
+//
+// Metafile.cs
+//
+// (C) 2002 Christian Meyer
+// Author: Christian Meyer
+// eMail: Christian.Meyer@cs.tum.edu
+//
+using System;
+using System.Reflection;
+
+namespace System.Drawing.Imaging {
+
+	public sealed class Metafile : Image {
+
+		// constructors
+		[MonoTODO]
+		public Metafile (Stream stream) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string filename) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr henhmetafile, bool deleteEmf) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHtc, EmfType emfType) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHtc, Rectangle frameRect) {
+			throw new NotImplemtedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHtc, RectangleF frameRect) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader) {
+			throw new NotImplemtedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHtc) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (String fileName, IntPtr referenceHtc) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, EmfType emfType, string description) {
+			throw new NotImplemtedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader, bool deleteWmf) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, EmfType type) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, EmfType type) {
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect) {
+			throw new NotImplementedException ();
+		}
+
+		// methods
+		// properties
+	}
+
+}
