4c23dee643824302430f2831ce3e0f9895b38459
Daniel Morgan <monodanmorg@yahoo.com>
2002-04-15 23:07:23 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=3841

---
+++ mcs/class/Mono.Data.PostgreSqlClient/Mono.Data.PostgreSqlClient/PgSqlErrorCollection.cs
@@ -0,0 +1,83 @@
+//
+// System.Data.SqlClient.SqlErrorCollection
+//
+// Author:
+//   Rodrigo Moya (rodrigo@ximian.com)
+//   Daniel Morgan (danmorg@sc.rr.com)
+//
+// (C) Ximian, Inc 2002
+//
+using System;
+using System.Collections;
+using System.Data;
+
+namespace System.Data.SqlClient
+{
+	/// <summary>
+	/// Provides a means of reading one or more forward-only streams of result sets obtained by executing a command at a SQL database.
+	/// </summary>
+	public sealed class SqlErrorCollection : ICollection, IEnumerable
+	{
+		#region Properties
+
+		[MonoTODO]
+		public int Count 
+		{
+			get 
+			{ 
+				throw new NotImplementedException ();
+			}
+		}
+
+		bool ICollection.IsSynchronized {
+			get 
+			{ 
+				throw new NotImplementedException ();
+			}
+		}
+
+		object ICollection.SyncRoot {
+			get 
+			{ 
+				throw new NotImplementedException ();
+			}
+		}
+		
+		[MonoTODO]
+		public SqlError this[int index]
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void CopyTo (Array array, int index)
+		{
+			throw new NotImplementedException ();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		#region Deconstructors
+
+		// FIXME: do the deconstructor
+/*
+		[MonoTODO]
+		~SqlErrorCollection()
+		{
+			throw new NotImplementedException ();
+		}
+*/
+		#endregion
+	}
+}
---
+++ mcs/class/System.Data/System.Data.SqlClient/SqlInfoMessageEventArgs.cs
@@ -0,0 +1,45 @@
+//
+// System.Data.SqlClient.SqlInfoMessageEventArgs.cs
+//
+// Author:
+//   Rodrigo Moya (rodrigo@ximian.com)
+//   Daniel Morgan (danmorg@sc.rr.com)
+//
+// (C) Ximian, Inc 2002
+//
+
+using System;
+using System.Data;
+
+namespace System.Data.SqlClient
+{
+	public sealed class SqlInfoMessageEventArgs : EventArgs
+	{
+		public SqlErrorCollection Errors 
+		{
+			get 
+			{ 
+				throw new NotImplementedException (); 
+			}
+		}	
+
+		public string Message 
+		{
+			get 
+			{ 
+				throw new NotImplementedException (); 
+			}
+		}	
+
+		public string Source 
+		{
+			get 
+			{ 
+				throw new NotImplementedException (); 
+			}
+		}
+
+		public override string ToString();
+		// representation of InfoMessage event
+	}
+}
