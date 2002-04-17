e0f6a9c96b70031ac0fc5bcb273cfe95fb6d4d7a
Rodrigo Moya <rodrigo@mono-cvs.ximian.com>
2002-04-17 09:15:46 +0000

    2002-04-16  Rodrigo Moya <rodrigo@ximian.com>
    	* System.Data.Common/DataColumnMappingCollection.cs: added basic
    	implementation. Still missing some stuff.
    
    svn path=/trunk/mcs/; revision=3873

--- mcs/class/System.Data/System.Data.Common/DataColumnMappingCollection.cs
+++ mcs/class/System.Data/System.Data.Common/DataColumnMappingCollection.cs
@@ -16,98 +16,121 @@ namespace System.Data.Common
 		MarshalByRefObject, IColumnMappingCollection, IList,
 		ICollection, IEnumerable
 	{
-		[MonoTODO]
-		public DataColumnMappingCollection()
-		{
-			throw new NotImplementedException ();
+		private DataColumnMapping[] mappings;
+		private int size = 0;
+		
+		public DataColumnMappingCollection () {
+		}
+
+		public int Add (object obj) {
+			DataColumnMapping[] tmp = new DataColumnMapping[size + 1];
+
+			Array.Copy (mappings, tmp, size);
+			size++;
+			mappings = tmp;
+			mappings[size - 1] = obj;
+
+			return size;
+		}
+
+		public void AddRange (DataColumnMapping[] values) {
+			DataColumnMapping[] tmp = new DataColumnMapping[size + values.Length];
+
+			Array.Copy (mappings, tmp, size);
+			for (int i = 0; i < values.Length; i++) {
+				tmp[i + size] = values[i];
+			}
+			
+			size += values.Length;
+			mappings = tmp;
+		}
+
+		public void Clear () {
+			/* FIXME */
+			for (int i = 0; i < size; i++)
+				mappings[i] = null;
+
+			size = 0;
+		}
+
+		public bool Contains (object obj) {
+			for (int i = 0; i < size; i++) {
+				if (obj.Equals (mappings[i])
+				    return true;
+			}
+
+			return false;
+		}
+
+		public void CopyTo (Array array, int index) {
+			DataColumnMapping[] tmp = new DataColumnMapping[size];
+			Array.Copy (mappings, tmp, size);
+		}
+
+		public DataColumnMapping GetByDataSetColumn (string value) {
+			for (int i = 0; i < size; i++) {
+				if (mappings[i].DataSetColumn == value)
+					return mappings[i];
+			}
+
+			return null;
 		}
 
 		[MonoTODO]
-		public int Add(object)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void AddRange(DataColumnMapping[] values)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void Clear()
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public bool Contains(object)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void CopyTo(Array array, int index)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public DataColumnMapping GetByDataSetColumn(string value)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public static DataColumnMapping GetColumnMappingBySchemaAction(
+		public static DataColumnMapping GetColumnMappingBySchemaAction (
 			DataColumnMappingCollection columnMappings,
 			string sourceColumn,
-			MissingMappingAction mappingAction)
-		{
+			MissingMappingAction mappingAction) {
+			throw new NotImplementedException ();
+		}
+
+		public int IndexOf (object obj) {
+			for (int i = 0; i < size; i++) {
+				if (obj.Equals (mappings[i]))
+					return i;
+			}
+
+			return -1;
+		}
+
+		public int IndexOfDataSetColumn (string value) {
+			for (int i = 0; i < size; i++) {
+				if (mappings[i].DataSetColumn == value)
+					return i;
+			}
+
+			return -1;
+		}
+
+		[MonoTODO]
+		public void Insert (int index, object value) {
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public int IndexOf(object)
-		{
+		public void Remove (object value) {
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public int IndexOfDataSetColumn(string dataSetColumn)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void Insert(int index, object value)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void Remove(object value)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public void RemoveAt(int index)
-		{
+		public void RemoveAt (int index) {
 			throw new NotImplementedException ();
 		}
 		
-		[MonoTODO]
-		public int Count
-		{
-			get { throw new NotImplementedException (); }
+		public int Count {
+			get { return size; }
 		}
 
-		[MonoTODO]
-		public DataColumnMapping this[int]
-		{
-			get { throw new NotImplementedException (); }
-			set { throw new NotImplementedException (); }
+		public DataColumnMapping this[int index] {
+			get {
+				if (value < size)
+					return mappings[index];
+				return null;
+			}
+			set {
+				if (value < size)
+					mappings[index] = value;
+			}
 		}
 	}
 }
