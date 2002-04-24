faa5b3692a00b1ef6e4093834e0c670bf1dd7dde
Paolo Molaro <lupus@oddwiz.org>
2002-04-24 15:27:21 +0000

    Wed Apr 24 21:15:44 CEST 2002 Paolo Molaro <lupus@ximian.com>
    	* GC.cs: implement most of the methods as icalls.
    
    svn path=/trunk/mcs/; revision=4027

--- mcs/class/corlib/System/GC.cs
+++ mcs/class/corlib/System/GC.cs
@@ -10,17 +10,56 @@
 //
 
 using System;
+using System.Runtime.CompilerServices;
 
 namespace System {
 	public sealed class GC {
 
 		private GC () {}
-		
-		[MonoTODO]
-		public static void SuppressFinalize (object obj)
-		{
-			//throw new NotImplementedException ();
+
+		/* as long as we use Boehm... */
+		public static int MaxGeneration {
+			get {return 0;}
 		}
 
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		extern static void InternalCollect (int generation);
+		
+		public static void Collect () {
+			InternalCollect (MaxGeneration);
+		}
+
+		public static void Collect (int generation) {
+			if (generation < 0 || generation > MaxGeneration)
+				throw new ArgumentOutOfRangeException ("generation");
+			InternalCollect (generation);
+		}
+
+		public static int GetGeneration (object obj) {
+			return 0;
+		}
+
+		public static int GetGeneration (WeakReference wo) {
+			if (!wo.IsAlive)
+				throw new ArgumentException ();
+			return 0;
+		}
+
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public extern static long GetTotalMemory (bool forceFullCollection);
+
+		/* this icall has weird semantics check the docs... */
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public extern static void KeepAlive (object obj);
+		
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public extern static void ReRegisterForFinalize (object obj);
+		
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public extern static void SuppressFinalize (object obj);
+
+		[MethodImplAttribute (MethodImplOptions.InternalCall)]
+		public extern static void WaitForPendingFinalizers();
+		
 	}
 }
