cbfe3aa7d377aa0c6d174158eee438ebf8557b20
Lawrence Pit <lawrence@mono-cvs.ximian.com>
2002-04-26 15:02:02 +0000

    Mail interfaces, not yet sending out mails though
    svn path=/trunk/mcs/; revision=4063

---
+++ mcs/class/System.Web/System.Web.Mail/MailFormat.cs
@@ -0,0 +1,26 @@
+//
+// System.Web.Mail.MailFormat.cs
+//
+// Author:
+//    Lawrence Pit (loz@cable.a2000.nl)
+//
+
+namespace System.Web.Mail 
+{
+
+	/// <summary>
+	/// </summary>
+	public enum MailFormat 
+	{
+
+		/// <summary>
+		/// </summary>
+		Text = 0,
+	
+		/// <summary>
+		/// </summary>
+		Html = 1
+
+	} 
+	
+} //namespace System.Web.Mail
