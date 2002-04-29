fd1141aa7dda3d8bb3d46cf43b2f9a9a77b5fb87
Daniel Morgan <monodanmorg@yahoo.com>
2002-04-29 03:31:13 +0000

    2002-04-29  Daniel Morgan <danmorg@sc.rr.com>
    	* Test/ReadPostgresData.cs: added - Uses the
    	PostgresLibrary to retrieve a recordset.
    	This is not meant to be used in Production, but as a
    	learning aid in coding
    	class System.Data.SqlClient.SqlDataReader.
    	This sample does work.
    
    	* Test/TestSqlDataReader.cs: added - used
    	to test SqlDataReader (does not work yet)
    	Forgot to add to ChangeLog on last commit.
    
    svn path=/trunk/mcs/; revision=4129

---
+++ mcs/class/System.Data/Test/ReadPostgresData.cs
@@ -0,0 +1,310 @@
+//
+// ReadPostgresData.cs
+//
+// Uses the PostgresLibrary to retrieve a recordset.
+// This is not meant to be used in Production, but as a
+// learning aid in coding class System.Data.SqlClient.SqlDataReader.
+//
+// Author:
+//	Daniel Morgan <danmorg@sc.rr.com>
+//
+// (C) 2002 Daniel Morgan
+//
+
+using System;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace LearnToCreateSqlDataReader
+{
+	sealed public class PostgresLibrary {
+
+		public enum ConnStatusType {
+			CONNECTION_OK,
+			CONNECTION_BAD,
+			CONNECTION_STARTED,
+			CONNECTION_MADE,
+			CONNECTION_AWAITING_RESPONSE,
+			CONNECTION_AUTH_OK,			 
+			CONNECTION_SETENV		
+		} 
+
+		public enum PostgresPollingStatusType {
+			PGRES_POLLING_FAILED = 0,
+			PGRES_POLLING_READING,
+			PGRES_POLLING_WRITING,
+			PGRES_POLLING_OK,
+			PGRES_POLLING_ACTIVE
+		}
+
+		public enum ExecStatusType {
+			PGRES_EMPTY_QUERY = 0,
+			PGRES_COMMAND_OK,			
+			PGRES_TUPLES_OK,			
+			PGRES_COPY_OUT,				
+			PGRES_COPY_IN,				
+			PGRES_BAD_RESPONSE,			
+			PGRES_NONFATAL_ERROR,
+			PGRES_FATAL_ERROR
+		}
+
+		[DllImport("pq")]
+		public static extern string PQerrorMessage (IntPtr conn);
+		// char *PQerrorMessage(const PGconn *conn);
+
+		[DllImport("pq")]
+		public static extern IntPtr PQconnectdb(String conninfo);
+		// PGconn *PQconnectdb(const char *conninfo)
+
+		[DllImport("pq")]
+		public static extern void PQfinish(IntPtr conn);
+		// void PQfinish(PGconn *conn)
+		
+		[DllImport("pq")]
+		public static extern IntPtr PQexec(IntPtr conn,
+			String query);
+		// PGresult *PQexec(PGconn *conn,	const char *query);
+
+		[DllImport("pq")]
+		public static extern int PQntuples (IntPtr res);
+		// int PQntuples(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern int PQnfields (IntPtr res);
+		// int PQnfields(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern ConnStatusType PQstatus (IntPtr conn);
+		// ConnStatusType PQstatus(const PGconn *conn);
+		[DllImport("pq")]
+		public static extern ExecStatusType PQresultStatus (IntPtr res);
+		// ExecStatusType PQresultStatus(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern string PQresStatus (ExecStatusType status);
+		// char *PQresStatus(ExecStatusType status);
+
+		[DllImport("pq")]
+		public static extern string PQresultErrorMessage (IntPtr res);
+		// char *PQresultErrorMessage(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern int PQbinaryTuples (IntPtr res);
+		// int PQbinaryTuples(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern string PQfname (IntPtr res,
+			int field_num);
+		// char *PQfname(const PGresult *res,
+		//      int field_num);
+
+		[DllImport("pq")]
+		public static extern int PQfnumber (IntPtr res,
+			string field_name);
+		// int PQfnumber(const PGresult *res, 
+		//      const char *field_name);
+
+
+		[DllImport("pq")]
+		public static extern int PQfmod (IntPtr res, int field_num);
+		// int PQfmod(const PGresult *res, int field_num);
+
+		[DllImport("pq")]
+		public static extern int PQftype (IntPtr res,
+			int field_num);
+		// Oid PQftype(const PGresult *res,
+		//      int field_num);
+
+		[DllImport("pq")]
+		public static extern int PQfsize (IntPtr res,
+			int field_num);
+		// int PQfsize(const PGresult *res,
+		//      int field_num);
+
+		[DllImport("pq")]
+		public static extern string PQcmdStatus (IntPtr res);
+		// char *PQcmdStatus(PGresult *res);
+
+		[DllImport("pq")]
+		public static extern string PQoidStatus (IntPtr res);
+		// char *PQoidStatus(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern int PQoidValue (IntPtr res);
+		// Oid PQoidValue(const PGresult *res);
+
+		[DllImport("pq")]
+		public static extern string PQcmdTuples (IntPtr res);
+		// char *PQcmdTuples(PGresult *res);
+
+		[DllImport("pq")]
+		public static extern string PQgetvalue (IntPtr res,
+			int tup_num, int field_num);
+		// char *PQgetvalue(const PGresult *res,
+		//      int tup_num, int field_num);
+
+		[DllImport("pq")]
+		public static extern int PQgetlength (IntPtr res,
+			int tup_num, int field_num);
+		// int PQgetlength(const PGresult *res,
+		//      int tup_num, int field_num);
+
+		[DllImport("pq")]
+		public static extern int PQgetisnull (IntPtr res,
+			int tup_num, int field_num);
+		// int PQgetisnull(const PGresult *res,
+		//      int tup_num, int field_num);
+
+		[DllImport("pq")]
+		public static extern void PQclear (IntPtr res);
+		// void PQclear(PGresult *res);
+
+
+	}
+
+	public class ReadPostgresData
+	{
+
+		static void Test() {
+			String errorMessage;
+
+			IntPtr pgConn;
+			String sConnInfo;
+			PostgresLibrary.ConnStatusType connStatus;
+
+			String sQuery;
+			IntPtr pgResult;
+
+			sConnInfo = "host=localhost dbname=test user=danmorg password=viewsonic";
+			
+			sQuery = 
+				"select tid, tdesc " +
+				"from sometable ";
+		
+			pgConn = PostgresLibrary.PQconnectdb (sConnInfo);
+
+			connStatus = PostgresLibrary.PQstatus (pgConn);
+			if(connStatus == 
+				PostgresLibrary.
+				ConnStatusType.CONNECTION_OK) {
+
+				Console.WriteLine("CONNECTION_OK");
+
+				pgResult = PostgresLibrary.PQexec(pgConn, sQuery);
+
+				PostgresLibrary.ExecStatusType execStatus;
+
+				execStatus = PostgresLibrary.
+					PQresultStatus (pgResult);
+
+				if(execStatus == 
+					PostgresLibrary.
+					ExecStatusType.PGRES_TUPLES_OK) 
+				{
+					Console.WriteLine("PGRES_TUPLES_OK");
+					
+					int nRows = PostgresLibrary.
+						PQntuples(pgResult);
+					Console.WriteLine("Rows: " + nRows);
+
+					int nFields = PostgresLibrary.
+						PQnfields(pgResult);
+					Console.WriteLine("Columns: " + nFields);
+
+
+					String fieldName;
+					
+					// get meta data fromm result set (schema)
+					// for each column (field)
+					for(int fieldIndex = 0; 
+						fieldIndex < nFields; 
+						fieldIndex ++) {
+
+						// get column name
+						fieldName = PostgresLibrary.
+							PQfname(pgResult, fieldIndex);
+
+						Console.WriteLine("Field " + 
+							fieldIndex + ": " +
+							fieldName);
+
+						int oid;
+						// get PostgreSQL data type (OID)
+						oid = PostgresLibrary.
+							PQftype(pgResult, fieldIndex);
+
+						int definedSize;
+						// get defined size of column
+						definedSize = PostgresLibrary.
+							PQfsize(pgResult, fieldIndex);
+					}
+
+					// for each row and column, get the data value
+					for(int row = 0; 
+						row < nRows; 
+						row++) {
+
+						for(int col = 0; 
+							col < nFields; 
+							col++) {
+
+							String value;
+							// get data value
+							value = PostgresLibrary.
+									PQgetvalue(
+										pgResult,
+										row, col);
+
+							Console.WriteLine("Row: " + row +
+									" Col: " + col +
+									" " + value);
+
+							int columnIsNull;
+							// is column NULL?
+							columnIsNull = PostgresLibrary.
+								PQgetisnull(pgResult,
+									row, col);
+
+							int actualLength;
+							actualLength = PostgresLibrary.
+								PQgetlength(pgResult,
+									row, col);
+						}
+					}
+
+					// close result set
+					PostgresLibrary.PQclear (pgResult);
+				}
+				else {
+					// display connection error				
+					errorMessage = PostgresLibrary.
+						PQresStatus(execStatus);
+
+					errorMessage += " " + PostgresLibrary.
+						PQresultErrorMessage(pgResult);
+
+					Console.WriteLine(errorMessage);
+				}
+
+				// close database conneciton
+				PostgresLibrary.PQfinish(pgConn);
+
+			}
+			else {
+				errorMessage = PostgresLibrary.
+					PQerrorMessage (pgConn);
+				errorMessage += ": Could not connect to database.";
+				Console.WriteLine(errorMessage);
+			}
+		
+			
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Test();
+		}
+	}
+}
