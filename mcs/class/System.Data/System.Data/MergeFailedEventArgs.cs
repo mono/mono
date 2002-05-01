2becf838ae700b412c165375ce9a8b8af36e380e
Miguel de Icaza <miguel@gnome.org>
2002-05-01 20:11:34 +0000

    Miggy version
    svn path=/trunk/mcs/; revision=4205

--- mcs/class/System.Data/System.Data/MergeFailedEventArgs.cs
+++ mcs/class/System.Data/System.Data/MergeFailedEventArgs.cs
@@ -1,90 +1,38 @@
 //
 // System.Data.MergeFailedEventArgs.cs
 //
-// Authors:
-//   John Dugaw <jdugaw@unizenconsulting.com>
-//   Rodrigo Moya <rodrigo@ximian.com>
+// Author:
+//   Miguel de Icaza <miguel@ximian.com>
 //
-// (C) John Dugaw
+// (C) Ximian, Inc 2002
 //
 
 using System;
-using System.Data;
 
 namespace System.Data
 {
-	public class MergeFailedEventArgs : EventArgs
-	{
-		private DataTable table;
-		private string conflict;
+	public class MergeFailedEventArgs : EventArgs {
+		DataTable data_table;
+		string conflict;
+		Exception errors;
+		bool f_continue;
 
-		public MergeFailedEventArgs ( DataTable newTable, string newConflict) {
-			table = newTable;
-			conflict = newConflict;
+		public FillErrorEventArgs (DataTable dataTable, string conflict)
+		{
+			this.data_table = dataTable;
+			this.conflict = conflict;
+		}
+
+		public DataTable DataTable {
+			get {
+				return data_table;
+			}
 		}
 
 		public string Conflict {
 			get {
 				return conflict;
 			}
-			/* TODO
-			 * set included as some versions of mcs fail without them 
-			 * must be removed before going live
-			 */
-			set {
-				System.Console.WriteLine("ERROR: <instance> System.Data.MerFailedEventArgs.Conflict is a read only property!\n");
-			}
-
 		}
-		
-		public DataTable Table {
-			get {
-				return table;
-			}
-			/* TODO
-			 * set included as some versions of mcs fail without them 
-			 *    should be removed
-			 */
-			set {
-				System.Console.WriteLine("ERROR: <instance> System.Data.MergeFailedEventArgs.Table is a read only property!\n");
-			}
-		}
-
-		[Serializable]
-		[ClassInterface(ClassInterfaceType.AutoDual)]
-		public override bool Equals (MergeFailedEventsArgs eq) {
-			throw new NotImplementedException ();
-		}
-
-		[Serializable]
-		[ClassInterface(ClassInterfaceType.AutoDual)]
-		public static override bool Equals(MergeFailedEventArgs a, MergeFailedEventArgs b) {
-			throw new NotImplementedException ();
-		}
-
-		/* TODO
-		 * These can be uncommented and should be implemented or removed
-		 * before production.
-		 [Serializable]
-		 [ClassInterface(ClassInterfaceType.AutoDual)]
-		 public override int GetHashCode() {
-		 System.Console.WriteLine("<instance> System.Data.MergeFailedEventArgs.GetHashCode() called");
-		 }
-
-		 [Serializable]
-		 [ClassInterface(ClassInterfaceType.AutoDual)]
-		 public override Type GetType()
-		 {
-		 System.Console.WriteLine("<instance> System.Data.MergeFailedEventArgs.GetType() called");
-		 }
-		*/
-
-		public override string ToString() {
-			throw new NotImplementedException ();
-		}
-
-		protected object MemberwiseClone() {
-			throw new NotImplementedException ();
-		}
-   }
-} // end namespace declaration
+	}
+}
