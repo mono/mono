7af2cda1d7a0d933afa94f361797589623257ab0
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-05-06 22:22:16 +0000

    * Dennis Hayes <dennish@raytek.com> * Added enums * 	GridItemType.cs * 	HelpNavigator.cs * 	HorizontalAlignment.cs * 	ImeMode.cs * 	ItemActivation.cs * 	ItemBoundsPortion.cs * 	Keys.cs * All correct execpt for Keys.cs which is missing a half dozen * duplicate entries that need correcting.
    svn path=/trunk/mcs/; revision=4351

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/HorizontalAlignment.cs
@@ -0,0 +1,20 @@
+//
+// System.Windows.Forms.HorizontalAlignment.cs
+//
+// Author:
+//   Dennis Hayes (dennish@raytek.com)
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// </summary>
+	public enum HorizontalAlignment {
+		Center = 2,
+		Left = 0,
+		Right = 1
+	}
+}
