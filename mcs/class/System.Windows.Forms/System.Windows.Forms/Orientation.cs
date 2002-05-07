48a3bee4ab9bf3b67959d8ca0dfd01ea2be40f11
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-05-07 22:41:49 +0000

    * 5-6-2002 Dennis Hayes <dennish@raytek.com> * LeftRightAlignment.cs * LinkBehavior.cs * LinkState.cs * ListViewAlignment.cs * MdiLayout.cs * MenuGlyph.cs * MenuMerge.cs * MessageBoxButtons.cs * MessageBoxDefaultButton.cs * MessageBoxIcon.cs * MessageBoxOptions.cs * MouseButtons.cs * Orientation.cs * PictureBoxSizeMode.cs * PropertySort.cs * Added some more enums. All have correct values.
    svn path=/trunk/mcs/; revision=4397

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/Orientation.cs
@@ -0,0 +1,19 @@
+//
+// System.Windows.Forms.Orientation.cs
+//
+// Author:
+//   Dennis Hayes (dennish@raytek.com)
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// </summary>
+	public enum Orientation {
+		Horizontal = 0,
+		Vertical = 1
+	}
+}
