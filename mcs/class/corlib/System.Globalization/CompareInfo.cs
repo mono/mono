032e6cb39ef8a9cb9f8804c8a22d1f8cd177aa9a
Rodrigo Moya <rodrigo@mono-cvs.ximian.com>
2002-05-07 19:15:10 +0000

    2002-05-07  Rodrigo Moya <rodrigo@ximian.com>
    	* CompareInfo.cs: new basic stub (compiles on Linux, so I assume
    	it does not break the build on windows. Sorry if it does).
    
    svn path=/trunk/mcs/; revision=4386

---
+++ mcs/class/corlib/System.Globalization/CompareInfo.cs
@@ -0,0 +1,23 @@
+//
+// System.Globalization.CompareInfo
+//
+// Authors:
+//   Rodrigo Moya (rodrigo@ximian.com)
+//
+// (C) Ximian, Inc. 2002
+//
+
+using System.Runtime.Serialization;
+
+namespace System.Globalization
+{
+	public class CompareInfo : IDeserializationCallback
+	{
+		[MonoTODO]
+		void IDeserializationCallback.OnDeserialization(object sender)
+		{
+			throw new NotImplementedException ();
+		}
+		       
+	}
+}
