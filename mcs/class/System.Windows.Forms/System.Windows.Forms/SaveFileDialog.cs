fcea18896ed2f0b622017f25ee53ffc28084e0d3
Daniel Carrera <daniel@mono-cvs.ximian.com>
2002-05-07 13:24:44 +0000

    Added new classes.
    svn path=/trunk/mcs/; revision=4373

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/SaveFileDialog.cs
@@ -0,0 +1,83 @@
+//
+// System.Windows.Forms.SaveFileDialog
+//
+// Author:
+//   stubbed out by Daniel Carrera (dcarrera@math.toronto.edu)
+//
+// (C) 2002 Ximian, Inc
+//
+
+namespace System.Windows.Forms {
+
+	// <summary>
+	//	This is only a template.  Nothing is implemented yet.
+	//
+	// </summary>
+
+        //public sealed class SaveFileDialog : FileDialog {
+
+		//
+		//  --- Constructor
+		//
+		//[MonoTODO]
+		//public SaveFileDialog()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+
+		//
+		//  --- Public Properties
+		//
+		//[MonoTODO]
+		//public bool CreatePrompt {
+		//	get {
+		//		throw new NotImplementedException ();
+		//	}
+		//	set {
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+		//[MonoTODO]
+		//public bool OverwritePrompt {
+		//	get {
+		//		throw new NotImplementedException ();
+		//	}
+		//	set {
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+
+		//
+		//  --- Public Methods
+		//
+		//[MonoTODO]
+		//public void Dispose()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public Stream OpenFile()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public override void Reset()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public DialogResult ShowDialog()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+
+		//
+		//  --- Protected Methods
+		//
+		//[MonoTODO]
+		//protected virtual void Dispose(bool val)
+		//{
+		//	throw new NotImplementedException ();
+		//}
+	// }
+}
