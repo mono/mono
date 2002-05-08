723cea72b77f74c0bee560ebe3787e74991c5025
Tim Coleman <tim@mono-cvs.ximian.com>
2002-05-08 00:50:45 +0000

    2002-05-08  Tim Coleman <tim@timcoleman.com>         * XPathDocument.cs:         * XPathException.cs: New stub files added.
    svn path=/trunk/mcs/; revision=4402

---
+++ mcs/class/System.XML/System.Xml.XPath/XPathDocument.cs
@@ -0,0 +1,66 @@
+//
+// System.Xml.XPath.XPathDocument
+//
+// Author:
+//   Tim Coleman (tim@timcoleman.com)
+//
+// (C) Copyright 2002 Tim Coleman
+//
+
+using System.IO;
+
+namespace System.Xml.XPath
+{
+	public class XPathDocument : IXPathNavigable
+	{
+		#region Constructors
+
+		[MonoTODO]
+		public XPathDocument (Stream stream)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XPathDocument (string uri)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XPathDocument (TextReader reader)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XPathDocument (XmlReader reader)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XPathDocument (string uri, XmlSpace space)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public XPathDocument (XmlReader reader, XmlSpace space)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		#region Methods
+
+		[MonoTODO]
+		public XPathNavigator CreateNavigator ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		#endregion
+	}
+}
