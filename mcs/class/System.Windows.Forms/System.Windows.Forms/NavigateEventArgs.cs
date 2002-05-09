04b4d3a9e6807a5c0889de38c8a942f705216205
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-05-09 00:33:27 +0000

    2002-5-8 Dennis Hayes <dennish@raytek.com> Dennis Hayes <dennish@raytek.com> Checking in new System.Windows.forms classe for Paul Osman <paul.osmand@sympatico.ca>
    * NavigateEventArgs.cs
    * NodeLabelEditEventArgs.cs
    * NotifyIcon.cs
    Stubbed and commented out
    
    svn path=/trunk/mcs/; revision=4433

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/NavigateEventArgs.cs
@@ -0,0 +1,84 @@
+////
+//// System.Windows.Forms.NavigateEventArgs
+////
+//// Author:
+////   stubbed out by Paul Osman (paul.osman@sympatico.ca)
+////
+//// (C) 2002 Ximian, Inc
+////
+//
+//namespace System.Windows.Forms {
+//
+//	// <summary>
+//	//	This is only a template.  Nothing is implemented yet.
+//	//
+//	// </summary>
+//
+//        public class NavigateEventArgs : EventArgs {
+//
+//		//
+//		//  --- Constructor
+//		//
+//		[MonoTODO]
+//		public NavigateEventArgs(bool isForward)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Public Properties
+//		//
+//		[MonoTODO]
+//		public bool Forward {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		//
+//		//  --- Public Methods
+//		//
+//		[MonoTODO]
+//		public virtual bool Equals(object o)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public static bool Equals(object o1, object o2)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual int GetHashCode()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public Type GetType()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual string ToString()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Protected Methods
+//		//
+//		[MonoTODO]
+//		protected object MemberwiseClone()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		//
+//		//  --- DeConstructor
+//		//
+//		[MonoTODO]
+//		~NavigateEventArgs()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//	 }
+//}
---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/NodeLabelEditEventArgs.cs
@@ -0,0 +1,105 @@
+////
+//// System.Windows.Forms.NodeLabelEditEventArgs
+////
+//// Author:
+////   stubbed out by Paul Osman (paul.osman@sympatico.ca)
+////
+//// (C) 2002 Ximian, Inc
+////
+//
+//namespace System.Windows.Forms {
+//
+//	// <summary>
+//	//	This is only a template.  Nothing is implemented yet.
+//	//
+//	// </summary>
+//
+//        public class NodeLabelEditEventArgs : EventArgs {
+//
+//		//
+//		//  --- Constructor
+//		//
+//		[MonoTODO]
+//		public NodeLabelEditEventArgs(TreeNode)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public NodeLabelEditEventArgs(TreeNode, string)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Public Properties
+//		//
+//		[MonoTODO]
+//		public bool CancelEdit {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//		[MonoTODO]
+//		public string Label {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//		[MonoTODO]
+//		public TreeNode Node {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		//
+//		//  --- Public Methods
+//		//
+//		[MonoTODO]
+//		public virtual bool Equals(object o)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public static bool Equals(object o1, object o2)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual int GetHashCode()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public Type GetType()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual string ToString()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Protected Methods
+//		//
+//		[MonoTODO]
+//		protected object MemberwiseClone()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- DeConstructor
+//		//
+//		[MonoTODO]
+//		~NodeLabelEditEventArgs()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//	 }
+//}
