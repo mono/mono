b04cb804fe1d7768090fd0015fccb9f4d96ec2eb
Lawrence Pit <lawrence@mono-cvs.ximian.com>
2002-05-09 13:28:33 +0000

    Moved StringCollectionTest.cs and NameValueCollectionTest to this dir
    svn path=/trunk/mcs/; revision=4445

---
+++ mcs/class/System/Test/System.Collections.Specialized/AllTests.cs
@@ -0,0 +1,29 @@
+//
+// MonoTests.System.Collections.Specialized.AllTests, System.dll
+//
+// Author:
+//   Lawrence Pit <loz@cable.a2000nl>
+//
+
+using NUnit.Framework;
+using System;
+
+namespace MonoTests.System.Collections.Specialized  {
+	
+	public class AllTests : TestCase {
+
+		public AllTests (string name) : base (name)
+		{
+		}
+
+		public static ITest Suite {
+			get {
+				TestSuite suite = new TestSuite ();
+				suite.AddTest (NameValueCollectionTest.Suite);
+				suite.AddTest (StringCollectionTest.Suite);
+				return suite;
+			}
+		}
+	}
+}
+
