7ceca15dc7eec177c106ca49120f77109b4da46d
Chris J. Breisch <cjbreisch@mono-cvs.ximian.com>
2002-05-12 17:16:37 +0000

    Fixed to conform to Mono standards for incomplete functionality.
    svn path=/trunk/mcs/; revision=4566

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/ShortType.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/ShortType.cs
@@ -2,21 +2,25 @@
 // ShortType.cs
 //
 // Author:
-//   Chris J Breisch (cjbreisch@altavista.net)
+//   Chris J Breisch (cjbreisch@altavista.net) 
 //
 // (C) 2002 Chris J Breisch
 //
-namespace Microsoft.VisualBasic.CompilerServices {
+
+using System;
+
+namespace Microsoft.VisualBasic.CompilerServices 
+{
 	[Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute] 
 	[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)] 
-	[System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Auto)] 
 	sealed public class ShortType {
 		// Declarations
 		// Constructors
 		// Properties
 		// Methods
-		public static System.Int16 FromString (System.String Value) { return 0;}
-		public static System.Int16 FromObject (System.Object Value) { return 0;}
+		public static System.Int16 FromString (System.String Value) { return System.Int16.Parse(Value); }
+		[MonoTODO]
+		public static System.Int16 FromObject (System.Object Value) { throw new NotImplementedException (); }
 		// Events
 	};
 }
