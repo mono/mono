f13cac4c18b1d161cfc1bcd81691cce36c810aaa
Mike Gray <mikegray@mono-cvs.ximian.com>
2002-05-13 19:07:52 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=4602

--- mcs/class/corlib/Test/System.IO/FileTest.cs
+++ mcs/class/corlib/Test/System.IO/FileTest.cs
@@ -31,6 +31,7 @@ namespace MonoTests.System.IO
 		protected override void TearDown ()
 		{
 		        File.Delete ("resources" + Path.DirectorySeparatorChar + "baz");
+		        File.Delete ("resources" + Path.DirectorySeparatorChar + "foo");
 		}
 
 		public static ITest Suite
@@ -220,6 +221,48 @@ namespace MonoTests.System.IO
 		
 		public void TestDelete ()
 		{
+
+			/* exception test: File.Delete(null) */
+			try {
+				File.Delete (null);
+				Fail ("File.Delete(null) should throw ArgumentNullException");
+			} catch (ArgumentNullException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Delete(null) unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Delete("") */
+			try {
+				File.Delete ("");
+				Fail ("File.Delete('') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Delete('') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Delete(" ") */
+			try {
+				File.Delete (" ");
+				Fail ("File.Delete(' ') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Delete(' ') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Delete(directory_not_found) */
+			try {
+				File.Delete ("directory_does_not_exist" + Path.DirectorySeparatorChar + "foo");
+				Fail ("File.Delete(directory_does_not_exist) should throw DirectoryNotFoundException");
+			} catch (DirectoryNotFoundException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Delete(directory_does_not_exist) unexpected exception caught: e=" + e.ToString());
+			}
+
+
                         Assert ("File resources" + Path.DirectorySeparatorChar + "foo should exist for TestDelete to succeed", File.Exists ("resources" + Path.DirectorySeparatorChar + "foo"));
                         try {
                                 File.Delete ("resources" + Path.DirectorySeparatorChar + "foo");
@@ -229,8 +272,128 @@ namespace MonoTests.System.IO
 			Assert ("File resources" + Path.DirectorySeparatorChar + "foo should not exist after File.Delete", !File.Exists ("resources" + Path.DirectorySeparatorChar + "foo"));
 		}
 
+
 		public void TestMove ()
 		{
+			/* exception test: File.Move(null, b) */
+			try {
+				File.Move (null, "b");
+				Fail ("File.Move(null, 'b') should throw ArgumentNullException");
+			} catch (ArgumentNullException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move(null, 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(a, null) */
+			try {
+				File.Move ("a", null);
+				Fail ("File.Move('a', null) should throw ArgumentNullException");
+			} catch (ArgumentNullException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('a', null) unexpected exception caught: e=" + e.ToString());
+			}
+
+
+			/* exception test: File.Move("", b) */
+			try {
+				File.Move ("", "b");
+				Fail ("File.Move('', 'b') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('', 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(a, "") */
+			try {
+				File.Move ("a", "");
+				Fail ("File.Move('a', '') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('a', '') unexpected exception caught: e=" + e.ToString());
+			}
+
+
+			/* exception test: File.Move(" ", b) */
+			try {
+				File.Move (" ", "b");
+				Fail ("File.Move(' ', 'b') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move(' ', 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(a, " ") */
+			try {
+				File.Move ("a", " ");
+				Fail ("File.Move('a', ' ') should throw ArgumentException");
+			} catch (ArgumentException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('a', ' ') unexpected exception caught: e=" + e.ToString());
+			}
+
+
+			/* exception test: File.Move(doesnotexist, b) */
+			try {
+				File.Move ("doesnotexist", "b");
+				Fail ("File.Move('doesnotexist', 'b') should throw FileNotFoundException");
+			} catch (FileNotFoundException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('doesnotexist', 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(resources/foo, doesnotexist/b) */
+			File.Copy("resources" + Path.DirectorySeparatorChar + "AFile.txt", "resources" + Path.DirectorySeparatorChar + "foo", true);
+			try {
+				File.Move ("resources" + Path.DirectorySeparatorChar + "foo", "doesnotexist" + Path.DirectorySeparatorChar + "b");
+				Fail ("File.Move('resources/foo', 'b') should throw DirectoryNotFoundException");
+			} catch (DirectoryNotFoundException) {
+				// do nothing, this is what we expect
+			} catch (FileNotFoundException) {
+				// LAMESPEC
+				// do nothing, this is (kind of) what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('resources/foo', 'doesnotexist/b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(doesnotexist/foo, b) */
+			try {
+				File.Move ("doesnotexist" + Path.DirectorySeparatorChar + "foo", "b");
+				Fail ("File.Move('doesnotexist/foo', 'b') should throw DirectoryNotFoundException");
+			} catch (DirectoryNotFoundException) {
+				// do nothing, this is what we expect
+			} catch (FileNotFoundException) {
+				// LAMESPEC
+				// do nothing, this is (kind of) what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('doesnotexist/foo', 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* exception test: File.Move(resources/foo, resources) */
+			try {
+				File.Move ("resources" + Path.DirectorySeparatorChar + "foo", "resources");
+				Fail ("File.Move('resources/foo', 'resources') should throw IOException");
+			} catch (IOException) {
+				// do nothing, this is what we expect
+			} catch (Exception e) {
+				Fail ("File.Move('resources/foo', 'resources') unexpected exception caught: e=" + e.ToString());
+			}
+
+			/* positive test: File.Move(a, a) shouldn't throw exception */
+			try {
+				File.Move ("resources" + Path.DirectorySeparatorChar + "foo", "resources" + Path.DirectorySeparatorChar + "foo");
+			} catch (Exception e) {
+				Fail ("File.Move('doesnotexist/foo', 'b') unexpected exception caught: e=" + e.ToString());
+			}
+
+
+
 			Assert ("File resources" + Path.DirectorySeparatorChar + "bar should exist", File.Exists ("resources" + Path.DirectorySeparatorChar + "bar"));
 			File.Move ("resources" + Path.DirectorySeparatorChar + "bar", "resources" + Path.DirectorySeparatorChar + "baz");
 			Assert ("File resources" + Path.DirectorySeparatorChar + "bar should not exist", !File.Exists ("resources" + Path.DirectorySeparatorChar + "bar"));
