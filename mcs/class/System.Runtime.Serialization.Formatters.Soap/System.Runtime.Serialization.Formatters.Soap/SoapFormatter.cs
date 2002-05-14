e8de62d68776dbd4de8af4a4ecde06f2071e03bc
Miguel de Icaza <miguel@gnome.org>
2002-05-14 18:02:26 +0000

    Add System.Runtime.Serialization.Formatters.Soap from Jesus
    svn path=/trunk/mcs/; revision=4637

---
+++ mcs/class/System.Runtime.Serialization.Formatters.Soap/System.Runtime.Serialization.Formatters.Soap/SoapFormatter.cs
@@ -0,0 +1,59 @@
+/****************************************************/
+/*Soapformatter class implementation                */
+/*Author: Jesús M. Rodríguez de la Vega             */
+/*gsus@brujula.net                                  */
+/****************************************************/
+
+using System;
+using System.Reflection;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+
+namespace System.Runtime.Serialization.Formatters.soap
+{
+	public class SoapFormatter
+	{
+		private ObjectSerializer ObjSerializer;		
+		/*this is the soapformater's properties               
+		  the Binder, Context and SurrogateSelector properties
+		  have not been declared yet*/
+
+		public FormatterAssemblyStyle AssemblyFormat
+		{
+			get{return AssemblyFormat;}
+			set{AssemblyFormat= value;}
+		}
+
+		public ISoapMessage TopObject
+		{
+			get{return TopObject;}
+			set{TopObject= value;}
+		}
+
+		public FormatterTypeStyle TypeFormat
+		{
+			get{return TypeFormat;}
+			set{TypeFormat= value;}
+		}
+
+		//the other constructor are not supplied yet
+		public SoapFormatter()
+		{			
+		}
+        		
+		public void Serialize(Stream serializationStream, object graph)
+		{
+			ObjSerializer= new ObjectSerializer(serializationStream);
+			ObjSerializer.BeginWrite();
+			ObjSerializer.Serialize(graph);
+		}
+
+		public object Deserialize(Stream serializationStream)			
+		{
+			return null;
+			//not implented yet
+		}
+		
+	}
+}
