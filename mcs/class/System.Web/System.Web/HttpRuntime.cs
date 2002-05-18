c49ad0fb652dd490b8e9e904b36c6bee0fd622c0
Miguel de Icaza <miguel@gnome.org>
2002-05-18 19:08:45 +0000

    2002-05-18  Miguel de Icaza  <miguel@ximian.com>
    	* HttpRuntime.cs: Reformat file.
    
    svn path=/trunk/mcs/; revision=4748

--- mcs/class/System.Web/System.Web/HttpRuntime.cs
+++ mcs/class/System.Web/System.Web/HttpRuntime.cs
@@ -14,241 +14,265 @@ using System.Web.Utils;
 using System.Web.Caching;
 
 namespace System.Web {
-   [MonoTODO("Make corrent right now this is a simple impl to give us a base for testing... the methods here are not complete or valid")]
-   public sealed class HttpRuntime {
-      // Security permission helper objects
-      private static IStackWalk appPathDiscoveryStackWalk;
-      private static IStackWalk ctrlPrincipalStackWalk;
-      private static IStackWalk sensitiveInfoStackWalk;
-      private static IStackWalk unmgdCodeStackWalk;
-      private static IStackWalk unrestrictedStackWalk;
-      private static IStackWalk reflectionStackWalk;
 
-      private static HttpRuntime _Runtime;
-      private Cache _Cache;
+	[MonoTODO ("Make corrent right now this is a simple impl to give us a base for testing... the methods here are not complete or valid")]	
+	public sealed class HttpRuntime {
+		// Security permission helper objects
+		private static IStackWalk appPathDiscoveryStackWalk;
+		private static IStackWalk ctrlPrincipalStackWalk;
+		private static IStackWalk sensitiveInfoStackWalk;
+		private static IStackWalk unmgdCodeStackWalk;
+		private static IStackWalk unrestrictedStackWalk;
+		private static IStackWalk reflectionStackWalk;
 
-      // TODO: Temp to test the framework..
-      IHttpHandler   _Handler;
+		private static HttpRuntime runtime;
+		private Cache cache;
 
-      static HttpRuntime() {
-         appPathDiscoveryStackWalk = null;
-         ctrlPrincipalStackWalk    = null;
-         sensitiveInfoStackWalk    = null;
-         unmgdCodeStackWalk        = null;
-         unrestrictedStackWalk     = null;
+		// TODO: Temp to test the framework..
+		IHttpHandler   handler;
+
+		static HttpRuntime ()
+		{
+			appPathDiscoveryStackWalk = null;
+			ctrlPrincipalStackWalk    = null;
+			sensitiveInfoStackWalk    = null;
+			unmgdCodeStackWalk        = null;
+			unrestrictedStackWalk     = null;
          
-         _Runtime = new HttpRuntime();
-      }
+			runtime = new HttpRuntime ();
+		}
 
-      public HttpRuntime() {
-         Init();
-      }
+		public HttpRuntime ()
+		{
+			Init ();
+		}
 
-      private void Init() {
-         _Cache = new Cache();
-      }
+		private void Init ()
+		{
+			cache = new Cache ();
+		}
 
-      public static IHttpHandler Handler {
-         get {
-            return _Runtime._Handler;
-         }
+		public static IHttpHandler Handler {
+			get {
+				return runtime.handler;
+			}
 
-         set {
-            _Runtime._Handler = value;
-         }
-      }
+			set {
+				runtime.handler = value;
+			}
+		}
 
-      public static void ProcessRequest(HttpWorkerRequest Request) {
-         if (null == _Runtime._Handler) {
-            throw new ArgumentException("No handler");
-         }
+		public static void ProcessRequest (HttpWorkerRequest Request)
+		{
+			if (runtime.handler == null) 
+				throw new ArgumentException ("No handler");
 
-         // just a test method to test the framework
+			// just a test method to test the framework
 
-         HttpContext oContext = new HttpContext(Request);
-         _Runtime._Handler.ProcessRequest(oContext);
+			HttpContext oContext = new HttpContext (Request);
+			runtime.handler.ProcessRequest (oContext);
 
-         oContext.Response.FlushAtEndOfRequest();
-         Request.EndOfRequest();
-      }
+			oContext.Response.FlushAtEndOfRequest ();
+			Request.EndOfRequest ();
+		}
 
-      public static Cache Cache {
-         get {
-            return _Runtime._Cache;
-         }
-      }      
+		public static Cache Cache {
+			get {
+				return runtime.cache;
+			}
+		}      
 
-      [MonoTODO]
-      public static string AppDomainAppId {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string AppDomainAppId {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string AppDomainAppPath {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string AppDomainAppPath {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string AppDomainAppVirtualPath {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string AppDomainAppVirtualPath {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string AppDomainId {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string AppDomainId {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string AspInstallDirectory {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string AspInstallDirectory {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string BinDirectory {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string BinDirectory {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string ClrInstallDirectory {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string ClrInstallDirectory {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string CodegenDir {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string CodegenDir {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static bool IsOnUNCShare {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static bool IsOnUNCShare {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static string MachineConfigurationDirectory {
-         get {
-            throw new NotImplementedException();
-         }
-      }
+		[MonoTODO]
+		public static string MachineConfigurationDirectory {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-      [MonoTODO]
-      public static void Close() {
-         throw new NotImplementedException();
-      }
+		[MonoTODO]
+		public static void Close ()
+		{
+			throw new NotImplementedException ();
+		}
 
-      internal static string FormatResourceString(string key) {         return GetResourceString(key);      }
-      internal static string FormatResourceString(string key, string arg0) {
-         string format = GetResourceString(key);
-         if(format==null)
-            return null;
-         return String.Format(format, arg0);
-      }
+		internal static string FormatResourceString (string key)
+		{
+			return GetResourceString (key);
+		}
 
-      [MonoTODO("FormatResourceString(string, string, string)")]
-      internal static string FormatResourceString(string key, string arg0, string type) {
-         // String.Format(string, object, object);
-         throw new NotImplementedException();
-      }
+		internal static string FormatResourceString (string key, string arg0)
+		{
+			string format = GetResourceString (key);
 
-      [MonoTODO("FormatResourceString(string, string, string, string)")]
-      internal static string FormatResourceString(string key, string arg0, string arg1, string arg2) {
-         // String.Format(string, object, object, object);
-         throw new NotImplementedException();
-      }
+			if (format == null)
+				return null;
+			
+			return String.Format (format, arg0);
+		}
 
-      [MonoTODO("FormatResourceString(string, string[]")]
-      internal static string FormatResourceString(string key, string[] args) {
-         // String.Format(string, object[]);
-         throw new NotImplementedException();
-      }
+		[MonoTODO ("FormatResourceString (string, string, string)")]
+		internal static string FormatResourceString (string key, string arg0, string type) {
+			// String.Format (string, object, object);
+			throw new NotImplementedException ();
+		}
 
-      private static string GetResourceString(string key) {
-         return _Runtime.GetResourceStringFromResourceManager(key);
-      }
+		[MonoTODO ("FormatResourceString (string, string, string, string)")]
+		internal static string FormatResourceString (string key, string arg0,
+							     string arg1, string arg2)
+		{
+			// String.Format (string, object, object, object);
+			throw new NotImplementedException ();
+		}
 
-      [MonoTODO("GetResourceStringFromResourceManager(string)")]
-      private string GetResourceStringFromResourceManager(string key) {
-         throw new NotImplementedException();
-      }
+		[MonoTODO ("FormatResourceString (string, string[]")]
+		internal static string FormatResourceString (string key, string[] args)
+		{
+			// String.Format (string, object[]);
+			throw new NotImplementedException ();
+		}
 
-      [MonoTODO("Get Application path from the appdomain object")]
-      internal static IStackWalk AppPathDiscovery {
-         get {
-            if(appPathDiscoveryStackWalk == null) {
-               appPathDiscoveryStackWalk = new FileIOPermission(FileIOPermissionAccess.PathDiscovery, "<apppath>");
-            }
-            return appPathDiscoveryStackWalk;
-         }
-      }
+		private static string GetResourceString (string key) {
+			return runtime.GetResourceStringFromResourceManager (key);
+		}
 
-      internal static IStackWalk ControlPrincipal {
-         get {
-            if(ctrlPrincipalStackWalk == null) {
-               ctrlPrincipalStackWalk = new SecurityPermission(SecurityPermissionFlag.ControlPrincipal);
-            }
-            return ctrlPrincipalStackWalk;
-         }
-      }
+		[MonoTODO ("GetResourceStringFromResourceManager (string)")]
+		private string GetResourceStringFromResourceManager (string key) {
+			throw new NotImplementedException ();
+		}
 
-      internal static IStackWalk Reflection {
-         get {
-            if(reflectionStackWalk == null) {
-               reflectionStackWalk = new ReflectionPermission(ReflectionPermissionFlag.TypeInformation | ReflectionPermissionFlag.MemberAccess);
-            }
-            return reflectionStackWalk;
-         }
-      }
+		[MonoTODO ("Get Application path from the appdomain object")]
+		internal static IStackWalk AppPathDiscovery {
+			get {
+				if (appPathDiscoveryStackWalk == null) {
+					appPathDiscoveryStackWalk = new FileIOPermission (
+						FileIOPermissionAccess.PathDiscovery, "<apppath>");
+				}
+				return appPathDiscoveryStackWalk;
+			}
+		}
 
-      internal static IStackWalk SensitiveInformation {
-         get {
-            if(sensitiveInfoStackWalk == null) {
-               sensitiveInfoStackWalk = new EnvironmentPermission(PermissionState.Unrestricted);
-            }
-            return sensitiveInfoStackWalk;
-         }
-      }
+		internal static IStackWalk ControlPrincipal {
+			get {
+				if (ctrlPrincipalStackWalk == null) {
+					ctrlPrincipalStackWalk = new SecurityPermission (
+						SecurityPermissionFlag.ControlPrincipal);
+				}
+				return ctrlPrincipalStackWalk;
+			}
+		}
 
-      internal static IStackWalk UnmanagedCode {
-         get {
-            if(unmgdCodeStackWalk == null) {
-               unmgdCodeStackWalk = new SecurityPermission(SecurityPermissionFlag.UnmanagedCode);
-            }
-            return unmgdCodeStackWalk;
-         }
-      }
+		internal static IStackWalk Reflection {
+			get {
+				if (reflectionStackWalk == null) {
+					reflectionStackWalk = new ReflectionPermission (
+						ReflectionPermissionFlag.TypeInformation |
+						ReflectionPermissionFlag.MemberAccess);
+				}
+				return reflectionStackWalk;
+			}
+		}
 
-      internal static IStackWalk Unrestricted {
-         get {
-            if(unrestrictedStackWalk == null) {
-               unrestrictedStackWalk = new PermissionSet(PermissionState.Unrestricted);
-            }
-            return unrestrictedStackWalk;
-         }
-      }
+		internal static IStackWalk SensitiveInformation {
+			get {
+				if (sensitiveInfoStackWalk == null) {
+					sensitiveInfoStackWalk = new EnvironmentPermission (
+						PermissionState.Unrestricted);
+				}
+				return sensitiveInfoStackWalk;
+			}
+		}
 
-      internal static IStackWalk FileReadAccess(string file) {
-         return new FileIOPermission(FileIOPermissionAccess.Read, file);
-      }
+		internal static IStackWalk UnmanagedCode {
+			get {
+				if (unmgdCodeStackWalk == null) {
+					unmgdCodeStackWalk = new SecurityPermission (
+						SecurityPermissionFlag.UnmanagedCode);
+				}
+				return unmgdCodeStackWalk;
+			}
+		}
 
-      internal static IStackWalk PathDiscoveryAccess(string path) {
-         return new FileIOPermission(FileIOPermissionAccess.PathDiscovery, path);
-      }
-   }
+		internal static IStackWalk Unrestricted {
+			get {
+				if (unrestrictedStackWalk == null) {
+					unrestrictedStackWalk = new PermissionSet (
+						PermissionState.Unrestricted);
+				}
+				return unrestrictedStackWalk;
+			}
+		}
+
+		internal static IStackWalk FileReadAccess (string file)
+		{
+			return new FileIOPermission (FileIOPermissionAccess.Read, file);
+		}
+
+		internal static IStackWalk PathDiscoveryAccess (string path)
+		{
+			return new FileIOPermission (FileIOPermissionAccess.PathDiscovery, path);
+		}
+	}
 }
