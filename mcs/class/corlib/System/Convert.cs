0442a5fa459fccfbacb574efdabf5e69cfe1937c
Martin Baulig <martin@novell.com>
2002-05-19 12:28:30 +0000

    2002-05-19  Martin Baulig  <martin@gnome.org>
    	* Convert.cs (FromBase64CharArray): Do correct exception handling.
    
    svn path=/trunk/mcs/; revision=4762

--- mcs/class/corlib/System/Convert.cs
+++ mcs/class/corlib/System/Convert.cs
@@ -94,11 +94,10 @@ namespace System {
 			if (inArray == null)
 				throw new ArgumentNullException();
 			
-			if ((offset < 0) || (offset + length > inArray.Length))
+			if ((offset < 0) || (length < 0) || (offset + length > inArray.Length))
 				throw new ArgumentOutOfRangeException();
 			
-			int len = inArray.Length;
-			if (len < 4 || len % 4 != 0)
+			if (length < 4 || length % 4 != 0)
 				throw new FormatException();
 				
 			byte[] inArr = new System.Text.UTF8Encoding().GetBytes(inArray, offset, length);
