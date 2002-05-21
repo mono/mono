949331335f90f2f345e3b1ce7e10c18eaf9dbbff
Paolo Molaro <lupus@oddwiz.org>
2002-05-21 11:39:20 +0000

    Tue May 21 17:25:49 CEST 2002 Paolo Molaro <lupus@ximian.com>
    	* MonoCustomAttrs.cs: handle inherit argument.
    
    svn path=/trunk/mcs/; revision=4823

--- mcs/class/corlib/System/MonoCustomAttrs.cs
+++ mcs/class/corlib/System/MonoCustomAttrs.cs
@@ -28,32 +28,52 @@ namespace System {
 		}
 
 		internal static object[] GetCustomAttributes (ICustomAttributeProvider obj, Type attributeType, bool inherit) {
-			// handle inherit
 			object[] res = from_cache (obj);
 			// shortcut
 			if (res.Length == 1 && res[0].GetType () == attributeType)
 				return (object[])res.Clone ();
 			ArrayList a = new ArrayList ();
-			foreach (object attr in res) {
-				if (attributeType.Equals (attr.GetType ()))
-					a.Add (attr);
-			}
+			Type btype = obj as Type;
+			do {
+				foreach (object attr in res) {
+					if (attributeType.Equals (attr.GetType ()))
+						a.Add (attr);
+				}
+				if (btype != null && ((btype = btype.BaseType) != null)) {
+					res = from_cache (btype);
+				} else {
+					break;
+				}
+			} while (inherit && btype != null && ((btype = btype.BaseType) != null));
 			Attribute[] r = new Attribute [a.Count];
 			a.CopyTo (r);
 			return r;
 		}
 
 		internal static object[] GetCustomAttributes (ICustomAttributeProvider obj, bool inherit) {
-			// handle inherit
-			return (Object[])from_cache (obj).Clone ();
+			Type btype = obj as Type;
+			if (!inherit || btype == null) {
+				return (Object[])from_cache (obj).Clone ();
+			} else {
+				ArrayList a = new ArrayList ();
+				a.AddRange (from_cache (obj));
+				while ((btype = btype.BaseType) != null) {
+					a.AddRange (from_cache (btype));
+				}
+				Attribute[] r = new Attribute [a.Count];
+				a.CopyTo (r);
+				return (object[])r;
+			}
 		}
 		internal static bool IsDefined (ICustomAttributeProvider obj, Type attributeType, bool inherit) {
-			// handle inherit
 			object[] res = from_cache (obj);
 			foreach (object attr in res) {
 				if (attributeType.Equals (attr.GetType ()))
 					return true;
 			}
+			Type btype = obj as Type;
+			if (inherit && (btype != null) && ((btype = btype.BaseType) != null))
+				return IsDefined (btype, attributeType, inherit);
 			return false;
 		}
 	}
