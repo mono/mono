9b6181fd86944590c2bb3c61e952963426aac554
Martin Baulig <martin@novell.com>
2002-05-22 08:09:03 +0000

    2002-05-22  Martin Baulig  <martin@gnome.org>
    	* AssemblerWriterI386.cs: Don't use GNU extensions and produce assembler
    	output which is free of comments and extra whitespaces so that it's suitable
    	for `as -f'.
    
    svn path=/trunk/mcs/; revision=4851

--- mcs/class/Mono.CSharp.Debugger/AssemblerWriterI386.cs
+++ mcs/class/Mono.CSharp.Debugger/AssemblerWriterI386.cs
@@ -18,10 +18,12 @@ namespace Mono.CSharp.Debugger
 	{
 		public AssemblerWriterI386 (StreamWriter writer) {
 			this.writer = writer;
+			writer.WriteLine ("#NOAPP");
 		}
 
 		private StreamWriter writer;
 		private static int next_anon_label_idx = 0;
+		private bool in_section = false;
 
 		public void WriteLabel (string label)
 		{
@@ -70,13 +72,13 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteUInt8 (int value)
 		{
-			char[] output = { '\t', '.', 'b', 'y', 't', 'e', '\t', '\t',
+			char[] output = { '\t', '.', 'b', 'y', 't', 'e', ' ',
 					  '0', 'x', '\0', '\0',
 					  '\n' };
 
 			unchecked {
-				output [10] = hex [(value & 0xf0) >> 4];
-				output [11] = hex [value & 0x0f];
+				output [9] = hex [(value & 0xf0) >> 4];
+				output [10] = hex [value & 0x0f];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -84,14 +86,14 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteInt8 (int value)
 		{
-			char[] output = { '\t', '.', 'b', 'y', 't', 'e', '\t', '\t',
+			char[] output = { '\t', '.', 'b', 'y', 't', 'e', ' ',
 					  '0', 'x', '\0', '\0',
 					  '\n' };
 
 			unchecked {
 				uint uvalue = (uint) value;
-				output [10] = hex [(uvalue & 0xf0) >> 4];
-				output [11] = hex [uvalue & 0x0f];
+				output [9] = hex [(uvalue & 0xf0) >> 4];
+				output [10] = hex [uvalue & 0x0f];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -99,7 +101,7 @@ namespace Mono.CSharp.Debugger
 
 		public void Write2Bytes (int a, int b)
 		{
-			char[] output = { '\t', '.', 'b', 'y', 't', 'e', '\t', '\t',
+			char[] output = { '\t', '.', 'b', 'y', 't', 'e', ' ',
 					  '0', 'x', '\0', '\0', ',', ' ',
 					  '0', 'x', '\0', '\0',
 					  '\n' };
@@ -107,10 +109,10 @@ namespace Mono.CSharp.Debugger
 			unchecked {
 				uint ua = (uint) a;
 				uint ub = (uint) b;
-				output [10] = hex [(ua & 0xf0) >> 4];
-				output [11] = hex [ua & 0x0f];
-				output [16] = hex [(ub & 0xf0) >> 4];
-				output [17] = hex [ub & 0x0f];
+				output [9] = hex [(ua & 0xf0) >> 4];
+				output [10] = hex [ua & 0x0f];
+				output [15] = hex [(ub & 0xf0) >> 4];
+				output [16] = hex [ub & 0x0f];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -118,29 +120,29 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteUInt16 (int value)
 		{
-			writer.WriteLine ("\t.2byte\t\t" + value);
+			writer.WriteLine ("\t.word " + value);
 		}
 
 		public void WriteInt16 (int value)
 		{
-			writer.WriteLine ("\t.2byte\t\t" + value);
+			writer.WriteLine ("\t.word " + value);
 		}
 
 		public void WriteUInt32 (int value)
 		{
-			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', '\t', '\t',
+			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', ' ',
 					  '0', 'x', '\0', '\0', '\0', '\0', '\0', '\0',
 					  '\0', '\0', '\n' };
 
 			unchecked {
-				output [10] = hex [(value & 0xf0000000) >> 28];
-				output [11] = hex [(value & 0x0f000000) >> 24];
-				output [12] = hex [(value & 0x00f00000) >> 20];
-				output [13] = hex [(value & 0x000f0000) >> 16];
-				output [14] = hex [(value & 0x0000f000) >> 12];
-				output [15] = hex [(value & 0x00000f00) >>  8];
-				output [16] = hex [(value & 0x000000f0) >>  4];
-				output [17] = hex [(value & 0x0000000f)];
+				output [9] = hex [(value & 0xf0000000) >> 28];
+				output [10] = hex [(value & 0x0f000000) >> 24];
+				output [11] = hex [(value & 0x00f00000) >> 20];
+				output [12] = hex [(value & 0x000f0000) >> 16];
+				output [13] = hex [(value & 0x0000f000) >> 12];
+				output [14] = hex [(value & 0x00000f00) >>  8];
+				output [15] = hex [(value & 0x000000f0) >>  4];
+				output [16] = hex [(value & 0x0000000f)];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -148,20 +150,20 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteInt32 (int value)
 		{
-			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', '\t', '\t',
+			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', ' ',
 					  '0', 'x', '\0', '\0', '\0', '\0', '\0', '\0',
 					  '\0', '\0', '\n' };
 
 			unchecked {
 				uint uvalue = (uint) value;
-				output [10] = hex [(uvalue & 0xf0000000) >> 28];
-				output [11] = hex [(uvalue & 0x0f000000) >> 24];
-				output [12] = hex [(uvalue & 0x00f00000) >> 20];
-				output [13] = hex [(uvalue & 0x000f0000) >> 16];
-				output [14] = hex [(uvalue & 0x0000f000) >> 12];
-				output [15] = hex [(uvalue & 0x00000f00) >>  8];
-				output [16] = hex [(uvalue & 0x000000f0) >>  4];
-				output [17] = hex [(uvalue & 0x0000000f)];
+				output [9] = hex [(uvalue & 0xf0000000) >> 28];
+				output [10] = hex [(uvalue & 0x0f000000) >> 24];
+				output [11] = hex [(uvalue & 0x00f00000) >> 20];
+				output [12] = hex [(uvalue & 0x000f0000) >> 16];
+				output [13] = hex [(uvalue & 0x0000f000) >> 12];
+				output [14] = hex [(uvalue & 0x00000f00) >>  8];
+				output [15] = hex [(uvalue & 0x000000f0) >>  4];
+				output [16] = hex [(uvalue & 0x0000000f)];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -169,58 +171,61 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteSLeb128 (int value)
 		{
-			writer.WriteLine ("\t.sleb128\t" + value);
+			writer.WriteLine ("\t.sleb128 " + value);
 		}
 
 		public void WriteULeb128 (int value)
 		{
-			writer.WriteLine ("\t.uleb128\t" + value);
+			writer.WriteLine ("\t.uleb128 " + value);
 		}
 
 		public void WriteAddress (int value)
 		{
 			if (value == 0)
-				writer.WriteLine ("\t.long\t\t0");
+				writer.WriteLine ("\t.long 0");
 			else
-				writer.WriteLine ("\t.long\t\t" + value);
+				writer.WriteLine ("\t.long " + value);
 		}
 
 		public void WriteString (string value)
 		{
-			writer.WriteLine ("\t.string\t\t\"" + value + "\"");
+			writer.WriteLine ("\t.string \"" + value + "\"");
 		}
 
 		public void WriteSectionStart (String section)
 		{
-			writer.WriteLine ("\t.section\t." + section);
+			if (in_section)
+				throw new Exception ();
+			in_section = true;
+			writer.WriteLine ("\t.section ." + section);
 		}
 
 		public void WriteSectionEnd ()
 		{
-			writer.WriteLine ("\t.previous\n");
+			in_section = false;
 		}
 
 		public void WriteRelativeOffset (int start_label, int end_label)
 		{
-			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', '\t', '\t',
+			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', ' ',
 					  '.', 'L', '_', '\0', '\0', '\0', '\0', '\0', '\0',
 					  ' ', '-', ' ',
 					  '.', 'L', '_', '\0', '\0', '\0', '\0', '\0', '\0',
 					  '\n' };
 
 			unchecked {
-				output [11] = hex [(end_label & 0xf00000) >> 20];
-				output [12] = hex [(end_label & 0x0f0000) >> 16];
-				output [13] = hex [(end_label & 0x00f000) >> 12];
-				output [14] = hex [(end_label & 0x000f00) >>  8];
-				output [15] = hex [(end_label & 0x0000f0) >>  4];
-				output [16] = hex [(end_label & 0x00000f)];
-				output [23] = hex [(start_label & 0xf00000) >> 20];
-				output [24] = hex [(start_label & 0x0f0000) >> 16];
-				output [25] = hex [(start_label & 0x00f000) >> 12];
-				output [26] = hex [(start_label & 0x000f00) >>  8];
-				output [27] = hex [(start_label & 0x0000f0) >>  4];
-				output [28] = hex [(start_label & 0x00000f)];
+				output [10] = hex [(end_label & 0xf00000) >> 20];
+				output [11] = hex [(end_label & 0x0f0000) >> 16];
+				output [12] = hex [(end_label & 0x00f000) >> 12];
+				output [13] = hex [(end_label & 0x000f00) >>  8];
+				output [14] = hex [(end_label & 0x0000f0) >>  4];
+				output [15] = hex [(end_label & 0x00000f)];
+				output [22] = hex [(start_label & 0xf00000) >> 20];
+				output [23] = hex [(start_label & 0x0f0000) >> 16];
+				output [24] = hex [(start_label & 0x00f000) >> 12];
+				output [25] = hex [(start_label & 0x000f00) >>  8];
+				output [26] = hex [(start_label & 0x0000f0) >>  4];
+				output [27] = hex [(start_label & 0x00000f)];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -228,25 +233,25 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteShortRelativeOffset (int start_label, int end_label)
 		{
-			char[] output = { '\t', '.', 'b', 'y', 't', 'e', '\t', '\t',
+			char[] output = { '\t', '.', 'b', 'y', 't', 'e', ' ',
 					  '.', 'L', '_', '\0', '\0', '\0', '\0', '\0', '\0',
 					  ' ', '-', ' ',
 					  '.', 'L', '_', '\0', '\0', '\0', '\0', '\0', '\0',
 					  '\n' };
 
 			unchecked {
-				output [11] = hex [(end_label & 0xf00000) >> 20];
-				output [12] = hex [(end_label & 0x0f0000) >> 16];
-				output [13] = hex [(end_label & 0x00f000) >> 12];
-				output [14] = hex [(end_label & 0x000f00) >>  8];
-				output [15] = hex [(end_label & 0x0000f0) >>  4];
-				output [16] = hex [(end_label & 0x00000f)];
-				output [23] = hex [(start_label & 0xf00000) >> 20];
-				output [24] = hex [(start_label & 0x0f0000) >> 16];
-				output [25] = hex [(start_label & 0x00f000) >> 12];
-				output [26] = hex [(start_label & 0x000f00) >>  8];
-				output [27] = hex [(start_label & 0x0000f0) >>  4];
-				output [28] = hex [(start_label & 0x00000f)];
+				output [10] = hex [(end_label & 0xf00000) >> 20];
+				output [11] = hex [(end_label & 0x0f0000) >> 16];
+				output [12] = hex [(end_label & 0x00f000) >> 12];
+				output [13] = hex [(end_label & 0x000f00) >>  8];
+				output [14] = hex [(end_label & 0x0000f0) >>  4];
+				output [15] = hex [(end_label & 0x00000f)];
+				output [22] = hex [(start_label & 0xf00000) >> 20];
+				output [23] = hex [(start_label & 0x0f0000) >> 16];
+				output [24] = hex [(start_label & 0x00f000) >> 12];
+				output [25] = hex [(start_label & 0x000f00) >>  8];
+				output [26] = hex [(start_label & 0x0000f0) >>  4];
+				output [27] = hex [(start_label & 0x00000f)];
 			}
 
 			writer.Write (output, 0, output.Length);
@@ -254,22 +259,22 @@ namespace Mono.CSharp.Debugger
 
 		public void WriteAbsoluteOffset (string label)
 		{
-			writer.WriteLine ("\t.long\t\t.L_" + label);
+			writer.WriteLine ("\t.long .L_" + label);
 		}
 
 		public void WriteAbsoluteOffset (int index)
 		{
-			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', '\t', '\t',
+			char[] output = { '\t', '.', 'l', 'o', 'n', 'g', ' ',
 					  '.', 'L', '_', '\0', '\0', '\0', '\0', '\0', '\0',
 					  '\n' };
 
 			unchecked {
-				output [11] = hex [(index & 0xf00000) >> 20];
-				output [12] = hex [(index & 0x0f0000) >> 16];
-				output [13] = hex [(index & 0x00f000) >> 12];
-				output [14] = hex [(index & 0x000f00) >>  8];
-				output [15] = hex [(index & 0x0000f0) >>  4];
-				output [16] = hex [(index & 0x00000f)];
+				output [10] = hex [(index & 0xf00000) >> 20];
+				output [11] = hex [(index & 0x0f0000) >> 16];
+				output [12] = hex [(index & 0x00f000) >> 12];
+				output [13] = hex [(index & 0x000f00) >>  8];
+				output [14] = hex [(index & 0x0000f0) >>  4];
+				output [15] = hex [(index & 0x00000f)];
 			}
 
 			writer.Write (output, 0, output.Length);
