61a7a7d98404226ef0a9c026ba762aa59c808416
Paolo Molaro <lupus@oddwiz.org>
2002-05-23 11:31:05 +0000

    Thu May 23 17:17:28 CEST 2002 Paolo Molaro <lupus@ximian.com>
    	* Activator.cs: implemented CreateInstance ().
    
    svn path=/trunk/mcs/; revision=4880

--- mcs/class/corlib/System/Activator.cs
+++ mcs/class/corlib/System/Activator.cs
@@ -65,37 +65,48 @@ namespace System
 			throw new NotImplementedException(); 
 		}
 		
-		[MonoTODO]
-		public static object CreateInstance(Type type)  { throw new NotImplementedException(); }
+		public static object CreateInstance(Type type) {
+			return CreateInstance (type, false);
+		}
 		
-		[MonoTODO]
-		public static object CreateInstance(Type type, 
-			object[] args) { throw new NotImplementedException(); }
+		public static object CreateInstance(Type type, object[] args) {
+			return CreateInstance (type, args, new object [0]);
+		}
 
 		[MonoTODO]
-		public static object CreateInstance(Type type, 
-			object[] args, object[] activationAttributes) { 
-			throw new NotImplementedException(); 
+		public static object CreateInstance(Type type, object[] args, object[] activationAttributes) {
+			Type[] atypes = new Type [args.Length];
+			for (int i = 0; i < args.Length; ++i) {
+				atypes [i] = args [i].GetType ();
+			}
+			ConstructorInfo ctor = type.GetConstructor (atypes);
+			return ctor.Invoke (args);
+
 		}
 
 		[MonoTODO]
 		public static object CreateInstance(Type type, 
 			BindingFlags bindingAttr, Binder binder, object[] args, 
 			CultureInfo culture) { 
-			throw new NotImplementedException(); 
+			return CreateInstance (type, bindingAttr, binder, args, culture, new object [0]);
 		}
 
 		[MonoTODO]
 		public static object CreateInstance(Type type, 
-			BindingFlags bindingAttr, Binder binder, object[] args, 
-			CultureInfo culture, object[] activationAttributes) { 
-			throw new NotImplementedException(); 
+				BindingFlags bindingAttr, Binder binder, object[] args, 
+				CultureInfo culture, object[] activationAttributes) { 
+			Type[] atypes = new Type [args.Length];
+			for (int i = 0; i < args.Length; ++i) {
+				atypes [i] = args [i].GetType ();
+			}
+			ConstructorInfo ctor = type.GetConstructor (bindingAttr, binder, atypes, null);
+			return ctor.Invoke (args, bindingAttr, binder, args, culture);
 		}
 
 		[MonoTODO]
-		public static object CreateInstance(Type type, 
-			bool nonPublic) { 
-			throw new NotImplementedException(); 
+		public static object CreateInstance(Type type, bool nonPublic) { 
+			ConstructorInfo ctor = type.GetConstructor (Type.EmptyTypes);
+			return ctor.Invoke (null);
 		}
 
 		[MonoTODO]
