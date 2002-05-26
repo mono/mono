9e1a32a8ff13f8fb6b533f6d76585635935ff304
Miguel de Icaza <miguel@gnome.org>
2002-05-26 03:01:15 +0000

    Add small stub to allow for the assembly to compile
    svn path=/trunk/mcs/; revision=4942

---
+++ mcs/class/System.XML/System.Xml/XmlConvert.cs
@@ -0,0 +1,17 @@
+//
+// XmlConvert.cs: Xml data type conversion
+//
+// Only a stub for now
+//
+using System;
+
+namespace System.Xml {
+
+	public class XmlConvert {
+
+		public static string VerifyNCName (string name)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
