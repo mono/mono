56903331e836941b50715abb2831d692a67cee4f
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-05-28 20:31:16 +0000

    2002-5-27 DennisHayes <dennish@raytek.com> 		Check in for Paul Osman <paul.osman@sympatico.ca> * Menu.cs * Menu.MenuItemCollection.cs * MenuItem.cs * MessageBox.cs * MonthCalendar.cs * Stubbed and commented out * MonthCalendar.cs has bad extra byte from old skeleton, need new corrected version. * MonthCalendar.HitTestInfo.cs needs to be incorporated into MonthCalendar. * Menu.MenuItemCollection.cs needs to be incorporated into Menu.cs.
    svn path=/trunk/mcs/; revision=4997

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/Menu.cs
@@ -0,0 +1,212 @@
+////
+//// System.Windows.Forms.Menu
+////
+//// Author:
+////   stubbed out by Paul Osman (paul.osman@sympatico.ca)
+////
+//// (C) 2002 Ximian, Inc
+////
+//
+//using System;
+//using System.Reflection;
+//using System.Globalization;
+////using System.Windows.Forms.AccessibleObject.IAccessible;
+//using System.Drawing;
+//
+//namespace System.Windows.Forms 
+//{
+//
+//	/// <summary>
+//	/// ToDo note:
+//	///  - Nothing is implemented
+//	/// </summary>
+//	
+//	public abstract class Menu : Component
+//	{
+//		//
+//		// -- Public Methods
+//		//
+//
+//		public virtual ObjRef CreateObjRef(Type t) 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public void Dispose() 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		protected virtual void Dispose(bool b)  
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public virtual bool Equals(object o) 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public static bool Equals(object o, object o) 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public ContextMenu GetContextMenu()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public virtual int GetHashCode()
+//		{
+//			throw new NotImplementedException();
+//		}
+//		
+//		public object GetLifetimeService()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public MainMenu GetMainMenu()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public Type GetType()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public virtual object InitializeLifetimeService()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public virtual void MergeMenu(Menu menuSrc)
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public override string ToString()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		//
+//		// -- Protected Methods
+//		//
+//
+//		protected void CloneMenu(Menu menuSrc) 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		protected override void Dispose(bool b)
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		public void Dispose()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		~Menu() 
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		protected virtual object GetService(Type service)
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		protected object MemberwiseClone()
+//		{
+//			throw new NotImplementedException();
+//		}
+//
+//		//
+//		// -- Public Events
+//		//
+//
+//		public event EventHandler Disposed;
+//
+//		//
+//		// -- Public Properties
+//		//
+//
+//		public IContainer Container
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		public IntPtr Handle
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		public virtual bool IsParent
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		public MenuItem MdiListItem
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		public Menu.MenuItemCollection MenuItems
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		public virtual ISite Site
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//			set
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		//
+//		// -- Protected Properties
+//		//
+//
+//		protected bool DesignMode
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//
+//		protected EventHandlerList Events
+//		{
+//			get
+//			{
+//				throw new NotImplementedException();
+//			}
+//		}
+//	}
+//}
+//			
