bf28b612207a6eb1040107e4472131d2633fbe9f
Nick Drochak <nickd@mono-cvs.ximian.com>
2002-05-30 12:45:30 +0000

    2002-05-31  Nick Drochak  <ndrochak@gol.com>
    	* BugTest.cs: Small file to show NullReferenceException bug
    	* AllTests.cs: Include BugTest.cs in the suite.
    
    svn path=/trunk/mcs/; revision=5027

--- mcs/class/corlib/Test/System/AllTests.cs
+++ mcs/class/corlib/Test/System/AllTests.cs
@@ -24,20 +24,24 @@ namespace MonoTests.System {
 				suite.AddTest (AttributeTest.Suite);
 				suite.AddTest (BitConverterTest.Suite);
 				suite.AddTest (BooleanTest.Suite);
+                                suite.AddTest (BugTest.Suite);
                                 suite.AddTest (ByteTest.Suite);
 				suite.AddTest (CharEnumeratorTest.Suite);
                                 suite.AddTest (CharTest.Suite);
                                 suite.AddTest (ConsoleTest.Suite);
 				suite.AddTest (ConvertTest.Suite);
 				suite.AddTest (DateTimeTest.Suite);
+				suite.AddTest (DecimalTest.Suite);
 				suite.AddTest (DoubleTest.Suite);
 				suite.AddTest (EnumTest.Suite);
+				suite.AddTest (ExceptionTest.Suite);
 				suite.AddTest (GuidTest.Suite);
                                 suite.AddTest (Int16Test.Suite);
                                 suite.AddTest (Int32Test.Suite);
                                 suite.AddTest (Int64Test.Suite);
                                 suite.AddTest (IntegerFormatterTest.Suite);
 				suite.AddTest (MathTest.Suite);
+				suite.AddTest (MulticastDelegateTest.Suite);
                                 suite.AddTest (ObjectTest.Suite);
                                 suite.AddTest (RandomTest.Suite);
                                 suite.AddTest (ResolveEventArgsTest.Suite);
@@ -49,8 +53,6 @@ namespace MonoTests.System {
                                 suite.AddTest (UInt32Test.Suite);
                                 suite.AddTest (UInt64Test.Suite);
 				suite.AddTest (VersionTest.Suite);
-				suite.AddTest (MulticastDelegateTest.Suite);
-				suite.AddTest (ExceptionTest.Suite);
 				return suite;
                         }
                 }
