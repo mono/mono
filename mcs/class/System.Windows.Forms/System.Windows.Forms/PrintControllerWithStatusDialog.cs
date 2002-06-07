ee6c568cc4786b53d11c9d5a0802553af25ccd9c
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-06-07 20:38:02 +0000

    2002-6-5 DennisHayes <dennish@raytek.com> * ErrorProvider.cs * PrintControllerWithStatusDialog.cs * PrintDialog.cs * added new files for paul
    svn path=/trunk/mcs/; revision=5164

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/PrintControllerWithStatusDialog.cs
@@ -0,0 +1,100 @@
+////
+//// System.Windows.Forms.PrintControllerWithStatusDialog
+////
+//// Author:
+////   stubbed out by Paul Osman (paul.osman@sympatico.ca)
+////
+//// (C) 2002 Ximian, Inc
+////
+//
+//namespace System.Windows.Forms {
+//
+//	// <summary>
+//	//	This is only a template.  Nothing is implemented yet.
+//	//
+//	// </summary>
+//
+//        public class PrintControllerWithStatusDialog : PrintController {
+//
+//		//
+//		//  --- Constructor
+//		//
+//		[MonoTODO]
+//		public PrintControllerWithStatusDialog(PrintController)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public PrintControllerWithStatusDialog(PrintController, string)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Public Methods
+//		//
+//		[MonoTODO]
+//		public virtual bool Equals(object o)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public static bool Equals(object o1, object o2)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual int GetHashCode()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public Type GetType()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public override void OnEndPage(PrintDocument document, PrintPageEventArgs e)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public override void OnEndPrint(PrintDocument document, PrintEventArgs e)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public override Graphics OnStartPage(PrintDocument document, PrintPageEventArgs e)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public override void OnStartPrint(PrintDocument document, PrintEventArgs e)
+//		{
+//			throw new NotImplementedException ();
+//		}
+//		[MonoTODO]
+//		public virtual string ToString()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Protected Methods
+//		//
+//		[MonoTODO]
+//		protected object MemberwiseClone()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//
+//		//
+//		//  --- Destructor
+//		//
+//		[MonoTODO]
+//		~PrintControllerWithStatusDialog()
+//		{
+//			throw new NotImplementedException ();
+//		}
+//	 }
+//}
