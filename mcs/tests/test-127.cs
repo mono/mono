c8c967a78915317dac1539c5a5ee63881d132e2f
Martin Baulig <martin@novell.com>
2002-06-08 12:47:02 +0000

    2002-06-08  Martin Baulig  <martin@gnome.org>
    	* test-127.cs: New test for Enum.IsDefined().
    
    svn path=/trunk/mcs/; revision=5171

---
+++ mcs/tests/test-127.cs
@@ -0,0 +1,23 @@
+//
+// It is possible to invoke Enum methods on an enum type.
+//
+using System;
+
+enum Test {
+	A,
+	B,
+	C
+}
+
+class X {
+
+	static int Main ()
+	{
+		Test test = Test.A;
+
+		if (!Test.IsDefined (typeof (Test), test))
+			return 1;
+
+		return 0;
+	}
+}
