649e909cdce97d16c9067c51ca712496d427651e
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-06-11 21:57:12 +0000

    2002-6-5 DennisHayes <dennish@raytek.com> * checked in  for Stefan Warnke (StefanW@POBox.com) * UICuesEventArgs.cs * UpDownBase.cs * Just stubbs * New files, work in progress, needs cleanup and mono style
    svn path=/trunk/mcs/; revision=5235

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/UICuesEventArgs.cs
@@ -0,0 +1,91 @@
+//
+// System.Windows.Forms.UICuesEventArgs
+//
+// Author:
+//	 stubbed out by Stefan Warnke (StefanW@POBox.com)
+//	 impelemented by ...
+//
+// (C) Ximian, Inc., 2002
+//
+using System;
+
+
+namespace Sytem.Windows.Forms
+{
+	// <summary>
+	//	This is only a template.  Nothing is implemented yet.
+	// </summary>
+
+
+	/// <summary>
+	/// UICuesEventArgs specifies which user interface feature changed and its new value.
+	/// </summary>
+	public class UICuesEventArgs : EventArgs
+	{
+//		public enum UICues {
+//			Changed,ChangeFocus,ChangeKeyboard,None,ShowFocus,ShowKeyboard,Shown
+//		};
+//
+//		/// --- Constructor ---
+//		public UICuesEventArgs(UICues uicues) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		/// --- Properties ---
+//		public UICues Changed {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//		
+//		public bool ChangeFocus {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public bool ChangeKeyboard {
+//			get 
+//			{
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public bool ShowFocus {
+//			get 
+//			{
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public bool ShowKeyboard {
+//			get 
+//			{
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		/// --- Public Methods ---
+//		public virtual bool Equals(object obj) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		public virtual int GetHashCode() {
+//			throw new NotImplementedException ();
+//		}
+//
+//		public Type GetType() {
+//			throw new NotImplementedException ();
+//		}
+//
+//		public virtual string ToString() {			
+//			throw new NotImplementedException ();
+//		}
+//
+//		/// --- Protected Methods ---
+//		protected object MemberwiseClone() {
+//			throw new NotImplementedException ();
+//		}
+
+	}
+}
---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/UpDownBase.cs
@@ -0,0 +1,228 @@
+//
+// System.Windows.Forms.UICuesEventArgs
+//
+// Author:
+//	 stubbed out by Stefan Warnke (StefanW@POBox.com)
+//	 impelemented by ...
+//
+// (C) Ximian, Inc., 2002
+//
+using System;
+
+namespace Sytem.Windows.Forms
+{
+	// <summary>
+	//	This is only a template.  Nothing is implemented yet.
+	// </summary>
+
+
+	/// <summary>
+	/// The up-down control consists of a text box and a small vertical scroll bar, 
+	/// commonly referred to as a spinner control.
+	/// </summary>
+	public abstract class UpDownBase : ContainerControl
+	{
+//		public UpDownBase()	{
+//			throw new NotImplementedException ();
+//		}
+//
+//		/// --- Properties ---
+//		public override Color BackColor {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public override Image BackgroundImage {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public BorderStyle BorderStyle {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public override ContextMenu ContextMenu {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public override bool Focused {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//		
+//		public override Color ForeColor {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public bool InterceptArrowKeys {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public int PreferredHeight {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public bool ReadOnly {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public override string Text {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public HorizontalAlignment TextAlign {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		public LeftRightAlignment UpDownAlign {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		/// --- Public Methods ---
+//		public abstract void DownButton() {
+//			throw new NotImplementedException ();
+//		}
+//
+//		public void Select(int start,int length) {
+//			throw new NotImplementedException ();
+//		}
+//		
+//		public abstract void UpButton() {
+//			throw new NotImplementedException ();
+//		}
+//	
+//
+//		/// --- Protected Properties ---
+//		protected bool ChangingText {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		protected override CreateParams CreateParams {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//		protected override Size DefaultSize {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//	
+//		protected bool UserEdit {
+//			get {
+//				throw new NotImplementedException ();
+//			}
+//			set {
+//				throw new NotImplementedException ();
+//			}
+//		}
+//
+//	
+//		/// --- Protected Methods ---
+//		protected override void OnFontChanged(EventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected override void OnHandleCreated(EventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected override void OnLayout(LayoutEventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//	
+//		protected override void OnMouseWheel(MouseEventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected virtual void OnTextBoxKeyDown(object source, KeyEventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected virtual void OnTextBoxKeyPress(object source, KeyPressEventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected virtual void OnTextBoxLostFocus(object source, EventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected virtual void OnTextBoxResize(object source, EventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//		
+//		protected virtual void OnTextBoxTextChanged(object source, EventArgs e) {
+//			throw new NotImplementedException ();
+//		}
+//		
+//		protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified) {
+//			throw new NotImplementedException ();
+//		}
+//		
+//		protected abstract void UpdateEditText() {
+//			throw new NotImplementedException ();
+//		}
+//
+//		protected virtual void ValidateEditText() {
+//			throw new NotImplementedException ();
+//		}
+
+	}
+}
