6e9c5492906ea465e7d6a55555f98df89de73288
Paolo Molaro <lupus@oddwiz.org>
2002-06-20 12:12:07 +0000

    Test error case.
    svn path=/trunk/mcs/; revision=5365

---
+++ mcs/errors/cs0528.cs
@@ -0,0 +1,10 @@
+// cs0528:  `A' is already listed in interface list
+// Line: 6
+interface A {
+        void stuff ();
+}
+class C: A, A {
+        public void stuff () {}
+        static void Main() {}
+}
+
