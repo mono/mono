5d82fee5c1cc9816b720d9f1b5e3a7050c37bbc3
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-06-24 11:34:14 +0000

    2002-06-24  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* AllTests.cs: New file to make 'make test' work.
    	* ChangeLog: New file.
    
    svn path=/trunk/mcs/; revision=5429

---
+++ mcs/class/corlib/Test/System.Runtime.Serialization/AllTests.cs
@@ -0,0 +1,28 @@
+//
+// MonoTests.System.Runtime.Serialization.AllTests
+//
+// Authors:
+// 	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// (C) 2002 Ximian, Inc.  (http://www.ximian.com)
+// 
+
+using System;
+using NUnit.Framework;
+
+namespace MonoTests.System.Runtime.Serialization {
+        public class AllTests : TestCase
+	{
+                public AllTests (string name) : base(name) {}
+                
+                public static ITest Suite 
+                { 
+                        get {
+                                TestSuite suite =  new TestSuite();
+                                suite.AddTest(ObjectIDGeneratorTests.Suite);
+                                return suite;
+                        }
+                }
+        }
+}
+
