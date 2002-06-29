622f22afeaf9e1753c5055eb7f14ba797819814a
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-06-29 02:01:29 +0000

    2002-6-28 DennisHayes <dennish@raytek.com> * Checked in for Jackson Harper (jackson@latitudegeo.com) * Tooltip.cs * new class
    svn path=/trunk/mcs/; revision=5495

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/tooltip.cs
@@ -0,0 +1,133 @@
+//
+// System.Windows.Forms.ToolTip
+//
+// Author:
+//   stubbed out by Jackson Harper (jackson@latitudegeo.com)
+//
+// (C) 2002 Ximian, Inc
+//
+
+namespace System.Windows.Forms {
+
+	// <summary>
+	//	This is only a template.  Nothing is implemented yet.
+	//
+	// </summary>
+
+        //public sealed class ToolTip : Component, IExtenderProvider {
+
+		//
+		//  --- Public Constructors
+		//
+		//[MonoTODO]
+		//public ToolTip() 
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public ToolTip(IContainer cont)
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//
+		// --- Public Properties
+		//
+		//[MonoTODO]
+		//public bool Active {
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}	
+		//}
+		//[MonoTODO]
+		//public int AutomaticDelay {
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}	
+		//}
+		//[MonoTODO]
+		//public int AutoPopDelay{
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+		//[MonoTODO]
+		//public int InitialDelay {
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+		//[MonoTODO]
+		//public int ReshowDelay {
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+		//[MonoTODO]
+		//public bool ShowAlways {
+		//	get
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//	set
+		//	{
+		//		throw new NotImplementedException ();
+		//	}
+		//}
+		//
+		// --- Public Methods
+		//
+		//[MonoTODO]
+		//public void RemoveAll()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public void SetToolTip(Control control, string caption)
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//public override string ToString()
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//
+		// --- Protected Methods
+		//
+		//[MonoTODO]
+		//protected override void Dispose(bool disposing)
+		//{
+		//	throw new NotImplementedException ();
+		//}
+		//[MonoTODO]
+		//~ToolTip() 
+		//{
+		//	throw new NotImplementedException ();
+		//}
+	//}
+}
