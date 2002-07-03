5be830f1dec41799d2962b69268c36db42891d8d
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-07-03 02:36:43 +0000

    2002-7-2  DennisHayes <dennish@raytek.com> * Application.cs * ControlEventArgs.cs * Form.cs * merged changes made by miguel and hestilow in gkt# to system.windows.forms
    svn path=/trunk/mcs/; revision=5568

--- mcs/class/System.Windows.Forms/System.Windows.Forms/ControlEventArgs.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/ControlEventArgs.cs
@@ -3,30 +3,40 @@
 //
 // Author:
 //   stubbed out by Jaak Simm (jaaksimm@firm.ee)
-//	Completed by Dennis Hayes (dennish@raytek.com)
+//	  implemented for Gtk+ by Rachel Hestilow (hestilow@ximian.com)
+
 // (C) Ximian, Inc., 2002
-//
+
 
 namespace System.Windows.Forms
 {
 	/// <summary>
 	/// Provides data for the ControlAdded and ControlRemoved events.
+	/// ToDo note:
+	///  - no methods are implemented
 	/// </summary>
+
 	public class ControlEventArgs : EventArgs
 	{
-		private Control control;
+		Control control;
 
+		#region Constructors
 		public ControlEventArgs(Control control) {
 			this.control = control;
 		}
+		#endregion
+		
+		#region Properties
 		public Control Control {
 			get { return control; }
 		}
+		#endregion
 		public string ToString() {
 			return base.ToString + control.ToString();
 		}
 		public int GetHashCode() {
 			return unchecked(base.GetHashCode() * control.ToString());
 		}
+
 	}
 }
