ce71f4631c7b573137d011c733af7163a019aa48
Miguel de Icaza <miguel@gnome.org>
2002-07-03 01:30:48 +0000

    Add new tests from Andrew
    svn path=/trunk/mcs/; revision=5563

---
+++ mcs/errors/cs1021-2.cs
@@ -0,0 +1,8 @@
+//
+// cs1021: Integral constant is too large
+
+class X {
+	public static void Main() {
+		int h = 0xffffffffffffffffffffffffff;
+	}
+}
---
+++ mcs/errors/cs1021.cs
@@ -0,0 +1,8 @@
+//
+// cs1021: Integral constant is too large
+
+class X {
+	public static void Main() {
+		int i = 123821738792178327198;
+	}
+}
