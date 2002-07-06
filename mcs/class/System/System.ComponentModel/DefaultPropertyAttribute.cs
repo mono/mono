21c3934525b8e496ff0ec533492b6429470dced7
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-07-06 17:52:51 +0000

    2002-07-06  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* DefaultPropertyAttribute.cs: New file.
    
    svn path=/trunk/mcs/; revision=5627

---
+++ mcs/class/System/System.ComponentModel/DefaultPropertyAttribute.cs
@@ -0,0 +1,43 @@
+//
+// System.ComponentModel.DefaultPropertyAttribute
+//
+// Authors:
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// (C) 2002 Ximian, Inc (http://www.ximian.com)
+//
+
+using System;
+
+namespace System.ComponentModel
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class DefaultPropertyAttribute : Attribute
+	{
+		private string property_name;
+
+		public DefaultPropertyAttribute (string name)
+		{
+			property_name = name;
+		}
+
+		public string Name
+		{
+			get { return property_name; }
+		}
+
+		public override bool Equals (object o)
+		{
+			if (!(o is DefaultPropertyAttribute))
+				return false;
+
+			return (((DefaultPropertyAttribute) o).Name == property_name);
+		}
+
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+	}
+}
+
