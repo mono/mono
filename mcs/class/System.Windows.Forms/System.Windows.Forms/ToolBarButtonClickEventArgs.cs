3019c87e04a2a6972c093653779b0941a7abca48
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-07-08 02:53:25 +0000

    2002-7-7  DennisHayes <dennish@raytek.com>
    * ToolBarButtonClickEventArgs.cs 
    * UpDownEventArgs.cs 
    * KeyPressEventHandler.cs
    * Added new files.
    
    svn path=/trunk/mcs/; revision=5647

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/ToolBarButtonClickEventArgs.cs
@@ -0,0 +1,25 @@
+//
+// System.Windows.Forms.ToolBarButtonClickEventArgs
+//
+// Author:
+//	 stubbed out by Dennis Hayes(dennish@raytek.com)
+//
+// (C) Ximian, Inc., 2002
+//
+using System;
+
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// Summary description for ToolBarButtonClickEventArgs.
+	/// </summary>
+	public class ToolBarButtonClickEventArgs {
+
+		public ToolBarButtonClickEventArgs()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/UpDownEventArgs.cs
@@ -0,0 +1,18 @@
+//
+// System.Windows.Forms.UpDownEventArgs
+//
+// Author:
+//	 stubbed out by Dennis Hayes(dennish@raytek.com)
+//
+// (C) Ximian, Inc., 2002
+//
+using System;
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// Summary description for UpDownEventArgs.
+	/// </summary>
+	public class UpDownEventArgs {
+
+	}
+}
