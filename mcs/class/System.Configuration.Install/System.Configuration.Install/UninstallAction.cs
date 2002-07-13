6adde675c7c224852d21e68e38d465b9cdd8de4f
Jonathan Pryor <jpryor@novell.com>
2002-07-13 18:58:23 +0000

    Added new assembly (System.Configuration.Install.dll).
    Currently the only class I've seen that belongs here is
    System.Configuration.Install.UninstallAction, but there are sure to be more.
    
    Set up NAnt build files to build System.Configuration.Install.Dll, Test cases.
    
    svn path=/trunk/mcs/; revision=5778

---
+++ mcs/class/System.Configuration.Install/System.Configuration.Install/UninstallAction.cs
@@ -0,0 +1,21 @@
+//
+// System.Configuration.Install.UninstallAction.cs
+//
+// Authors:
+//   Jonathan Pryor (jonpryor@vt.edu)
+//
+// (C) 2002
+//
+
+using System;
+using System.Diagnostics;
+
+namespace System.Diagnostics {
+
+	[Serializable]
+	public enum UninstallAction {
+		NoAction=0x01,
+		Remove=0x00
+	}
+}
+
