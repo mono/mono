f371254896f75b87b1de40e5437480b0c5d4143c
Tim Coleman <tim@mono-cvs.ximian.com>
2002-07-19 16:58:22 +0000

    2002-07-19 Tim Coleman  <tim@timcoleman.com> 	* ChangeLog: 	* LogicalMethodInfo.cs: 	* LogicalMethodTypes.cs: 		Add required classes to make System.Web.Services.Description 		buildable.
    svn path=/trunk/mcs/; revision=5930

---
+++ mcs/class/System.Web.Services/System.Web.Services.Protocols/LogicalMethodTypes.cs
@@ -0,0 +1,17 @@
+// 
+// System.Web.Services.Protocols.LogicalMethodTypes.cs
+//
+// Author:
+//   Tim Coleman (tim@timcoleman.com)
+//
+// Copyright (C) Tim Coleman, 2002
+//
+
+using System.Web.Services;
+
+namespace System.Web.Services.Protocols {
+	public enum LogicalMethodTypes {
+		Async,
+		Sync
+	}
+}
