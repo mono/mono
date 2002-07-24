6c9f96c11acc67b0307ddaa503f75ebe8ac7d0a3
Martin Baulig <martin@novell.com>
2002-07-24 00:02:27 +0000

    2002-07-24  Martin Baulig  <martin@gnome.org>
    	* test-150.cs: New test.
    
    	* test-149.cs: New test.
    
    svn path=/trunk/mcs/; revision=6100

---
+++ mcs/tests/test-150.cs
@@ -0,0 +1,8 @@
+using System;
+class T {
+        public static int Main() {
+                if (Int32.MinValue == 0x80000000)
+                        return 1;
+                return 0;
+        }
+}
