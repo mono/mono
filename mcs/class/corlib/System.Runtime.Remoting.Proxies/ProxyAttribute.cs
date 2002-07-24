44dd238adde690f5de31814db118251f32a593f4
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-07-24 04:24:35 +0000

    2002-07-24  Duncan Mak  <duncan@ximian.com>
    	* ProxyAttribute.cs: Revert the last commit.
    
    svn path=/trunk/mcs/; revision=6109

--- mcs/class/corlib/System.Runtime.Remoting.Proxies/ProxyAttribute.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Proxies/ProxyAttribute.cs
@@ -14,7 +14,7 @@ using System.Runtime.Remoting.Contexts;
 namespace System.Runtime.Remoting.Proxies {
 
 	[AttributeUsage (AttributeTargets.Class)]
-	public class ProxyAttribute : IContextAttribute
+	public class ProxyAttribute : Attribute
 	{
 		public ProxyAttribute ()
 		{
