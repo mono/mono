5285cef890c7718f523e6bafb150ea03afe2a267
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-07-31 15:13:50 +0000

    2002-07-31  Duncan Mak  <duncan@ximian.com>
    	* BinaryServerFormatterSink.Protocol.cs:
    	* SoapServerFormatterSink.Protocol.cs: Added.
    
    svn path=/trunk/mcs/; revision=6288

---
+++ mcs/class/corlib/System.Runtime.Remoting.Channels/SoapServerFormatterSink.Protocol.cs
@@ -0,0 +1,17 @@
+//
+// System.Runtime.Remoting.Channels.BinaryServerFormatterSink.Protocol.cs
+//
+// Author: Duncan Mak (duncan@ximian.com)
+//
+// 2002 (C) Copyright, Ximian, Inc.
+//
+
+namespace System.Runtime.Remoting.Channels {
+
+	[Serializable]
+	public enum SoapServerFormatterSink.Protocol
+	{
+		Http = 0,
+		Other = 1,
+	}
+}
