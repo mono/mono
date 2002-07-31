ba240dad7a47d041fe3e5defee211754fb7d2bdb
Duncan Mak <duncan@mono-cvs.ximian.com>
2002-07-31 15:58:34 +0000

    2002-07-31  Duncan Mak  <duncan@ximian.com>
    	* BinaryServerFormatterSink.cs:
    	* SoapServerFormatterSink.cs: Added.
    
    svn path=/trunk/mcs/; revision=6293

---
+++ mcs/class/corlib/System.Runtime.Remoting.Channels/BinaryServerFormatterSink.cs
@@ -0,0 +1,65 @@
+//
+// System.Runtime.Remoting.Channels.BinaryServerFormatterSink.cs
+//
+// Author: Duncan Mak (duncan@ximian.com)
+//
+// 2002 (C) Copyright, Ximian, Inc.
+//
+
+using System.Collections;
+
+namespace System.Runtime.Remoting.Channels {
+
+	public class BinaryServerFormatterSink : IServerChannelSink, IChannelSinkBase
+	{
+		IServerChannelSink next_sink;
+		
+		[MonoTODO]
+		public BinaryServerFormatterSink (BinaryServerFormatterSink.Protocol protocol,
+						  IServerChannelSink nextSink,
+						  IChannelReceiver receiver)
+		{
+			this.next_sink = nextSink;
+		}
+
+		public IServerChannelSink NextChannelSink {
+			get {
+				return next_sink;
+			}
+		}
+
+		[MonoTODO]
+		public IDictionary Properties {
+			get {
+				throw new NonImplementedException ();
+			}
+		}
+
+		[MonoTODO]
+		public void AsyncProcessResponse (IServerResponseChannelSinkStack sinkStack, object state,
+					  ITransportHeaders headers, Stream stream)
+		{
+			throw new NonImplementedException ();
+		}
+
+		[MonoTODO]
+		public Stream GetReponseStream (IMessage msg, ITransportHeaders headers)
+		{
+			throw new NonImplementedException ();
+		}
+		
+		[MonoTODO]
+		void ProcessMessage (IMessage msg, ITransportHeaders requestHeaders, Stream requestStream,
+				     out ITransportHeaders responseHeaders, out Stream responseStream)
+		{
+			throw new NonImplementedException ();
+		}
+		
+		[Serializable]
+		public enum Protocol
+		{
+			Http = 0,
+			Other = 1,
+		}
+	}
+}
---
+++ mcs/class/corlib/System.Runtime.Remoting.Channels/SoapServerFormatterSink.cs
@@ -0,0 +1,63 @@
+//
+// System.Runtime.Remoting.Channels.BinaryServerFormatterSink.cs
+//
+// Author: Duncan Mak (duncan@ximian.com)
+//
+// 2002 (C) Copyright, Ximian, Inc.
+//
+
+namespace System.Runtime.Remoting.Channels {
+
+	public class SoapServerFormatterSink : IServerChannelSink, IChannelSinkBase
+	{
+		IServerChannelSink next_sink;
+		
+		[MonoTODO]
+		public SoapServerFormatterSink (SoapServerFormatterSink.Protocol protocol,
+						IServerChannelSink nextSink,
+						IChannelReceiver receiver)
+		{
+			this.next_sink = nextSink;
+		}
+
+		public IServerChannelSink NextChannelSink {
+			get {
+				return next_sink;
+			}
+		}
+
+		[MonoTODO]
+		public IDictionary Properties {
+			get {
+				throw new NonImplementedException ();
+			}
+		}
+
+		[MonoTODO]
+		public void AsyncProcessResponse (IServerResponseChannelSinkStack sinkStack, object state,
+					  ITransportHeaders headers, Stream stream)
+		{
+			throw new NonImplementedException ();
+		}
+
+		[MonoTODO]
+		public Stream GetReponseStream (IMessage msg, ITransportHeaders headers)
+		{
+			throw new NonImplementedException ();
+		}
+		
+		[MonoTODO]
+		void ProcessMessage (IMessage msg, ITransportHeaders requestHeaders, Stream requestStream,
+				     out ITransportHeaders responseHeaders, out Stream responseStream)
+		{
+			throw new NonImplementedException ();
+		}
+
+		[Serializable]
+		public enum Protocol
+		{
+			Http = 0,
+			Other = 1,
+		}
+	}
+}
