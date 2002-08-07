14f740684648f908ac043e0e95bab6ef98f56857
Martin Baulig <martin@novell.com>
2002-08-07 23:05:42 +0000

    2002-08-08  Martin Baulig  <martin@gnome.org>
    	* test-161.cs: New test.
    
    svn path=/trunk/mcs/; revision=6522

---
+++ mcs/tests/test-161.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class ZipEntry
+{
+	DateTime cal = DateTime.Now;
+
+	public ZipEntry(string name)
+	{
+	}
+
+	public ZipEntry(ZipEntry e)
+	{
+	}
+
+	public DateTime DateTime {
+		get {
+			return cal;
+		}
+	}
+
+	static int Main () {
+		// Compilation only test.
+		return 0;
+	}
+}
