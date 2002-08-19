465c49f4b8d734cba89854ec654648d109b0bf76
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-08-19 10:50:31 +0000

    2002-08-19  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* FieldInfo.cs: implemented SetValue.
    
    svn path=/trunk/mcs/; revision=6745

--- mcs/class/corlib/System.Reflection/FieldInfo.cs
+++ mcs/class/corlib/System.Reflection/FieldInfo.cs
@@ -110,8 +110,13 @@ namespace System.Reflection {
 
 		public abstract void SetValue (object obj, object val, BindingFlags invokeAttr, Binder binder, CultureInfo culture);
 
-		[MonoTODO]
-		public void SetValue( object obj, object value) {
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		private static extern void SetValueInternal (FieldInfo fi, object obj, object value);
+
+		public void SetValue (object obj, object value)
+		{
+			//FIXME: when Binder.BindToField works, use it to check conversion
+			SetValueInternal (this, obj, value);
 		}
 	}
 }
