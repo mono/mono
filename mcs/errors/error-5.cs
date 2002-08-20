ed2785c27d03f55e30772fca6ca0d86a8ee245c4
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2002-08-20 03:25:54 +0000

    2002-08-19  Ravi Pratap  <ravi@ximian.com>
    	* error-5.cs : New test.
    
    svn path=/trunk/mcs/; revision=6785

---
+++ mcs/errors/error-5.cs
@@ -0,0 +1,17 @@
+// The event access is illegal and it should bail out
+
+using System;
+
+delegate void MyEventHandler();
+
+class MyEvent {
+	public event MyEventHandler SomeEvent;
+}
+
+class EventDemo {
+
+	public static void Main(){
+		MyEvent evt = new MyEvent();
+                evt.SomeEvent();
+        }
+}
