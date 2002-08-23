9a22266fe9ce7fd7e3c983d872783a428b7c32a5
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-08-23 06:24:31 +0000

    2002-08-23  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* CompilerError.cs: implemented.
    	* CompilerErrorCollection.cs: stubbed out and fix inheritance.
    	* GeneratorSupport.cs: fixed values.
    
    svn path=/trunk/mcs/; revision=6931

--- mcs/class/System/System.CodeDom.Compiler/CompilerErrorCollection.cs
+++ mcs/class/System/System.CodeDom.Compiler/CompilerErrorCollection.cs
@@ -1,16 +1,111 @@
 //
 // System.CodeDom.Compiler CompilerErrorCollection Class implementation
 //
-// Author:
-//   Daniel Stodden (stodden@in.tum.de)
+// Authors:
+// 	Daniel Stodden (stodden@in.tum.de)
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
 //
-// (C) 2002 Ximian, Inc.
+// (C) 2002 Ximian, Inc. (http://www.ximian.com)
 //
-
+using System.Collections;
 namespace System.CodeDom.Compiler
 {
-	public class CompilerErrorCollection
+	[MonoTODO]
+	public class CompilerErrorCollection : CollectionBase
 	{
-		// FIXME
+		[MonoTODO]
+		public CompilerErrorCollection ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public CompilerErrorCollection (CompilerErrorCollection value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public CompilerErrorCollection (CompilerError[] value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public int Add (CompilerError value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void AddRange (CompilerError[] value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void AddRange (CompilerErrorCollection value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public bool Contains (CompilerError value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void CopyTo (CompilerError[] array, int index)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public int IndexOf (CompilerError value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void Insert (int index, CompilerError value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void Remove (CompilerError value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public CompilerError this [int index]
+		{
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoTODO]
+		public bool HasErrors
+		{
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoTODO]
+		public bool HasWarnings
+		{
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 	}
+
 }
+
--- mcs/class/System/System.CodeDom.Compiler/GeneratorSupport.cs
+++ mcs/class/System/System.CodeDom.Compiler/GeneratorSupport.cs
@@ -12,27 +12,27 @@ namespace System.CodeDom.Compiler
 	[Flags]
 	[Serializable]
 	public enum GeneratorSupport {
-		ArraysOfArrays,
-		EntryPointMethod,
-		GotoStatements,
-		MultidimensionalArrays,
-		StaticConstructors,
-		TryCatchStatements,
-		ReturnTypeAttributes,
-		DeclareValueTypes,
-		DeclareEnums,
-		DeclareDelegates,
-		DeclareInterfaces,
-		DeclareEvents,
-		AssemblyAttributes,
-		ParameterAttributes,
-		ReferenceParameters,
-		ChainedConstructorArguments,
-		NestedTypes,
-		MultipleInterfaceMembers,
-		PublicStaticMembers,
-		ComplexExpressions,
-		Win32Resources
+		ArraysOfArrays = 1,
+		EntryPointMethod = 1 << 1,
+		GotoStatements = 1 << 2,
+		MultidimensionalArrays = 1 << 3,
+		StaticConstructors = 1 << 4,
+		TryCatchStatements = 1 << 5,
+		ReturnTypeAttributes = 1 << 6,
+		DeclareValueTypes = 1 << 7,
+		DeclareEnums = 1 << 8,
+		DeclareDelegates = 1 << 9,
+		DeclareInterfaces = 1 << 10,
+		DeclareEvents = 1 << 11,
+		AssemblyAttributes = 1 << 12,
+		ParameterAttributes = 1 << 13,
+		ReferenceParameters = 1 << 14,
+		ChainedConstructorArguments = 1 << 15,
+		NestedTypes = 1 << 16,
+		MultipleInterfaceMembers = 1 << 17,
+		PublicStaticMembers = 1 << 18,
+		ComplexExpressions = 1 << 19,
+		Win32Resources = 1 << 20
 	}
 }
 
