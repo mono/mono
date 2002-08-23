ef88f0cf1ae968d387dbeea3324b13d5d6a278e2
Martin Baulig <martin@novell.com>
2002-08-23 14:08:23 +0000

    Reverted this for the moment, it's causing too many problems. 2002-08-12  Martin Baulig  <martin@gnome.org>
    	* attribute.cs (Attribute.Resolve): Call System.Enum.ToObject() on
    	enum constants so that we encode the value with the enum type and
    	not its underlying type.
    
    svn path=/trunk/mcs/; revision=6949

--- mcs/mcs/attribute.cs
+++ mcs/mcs/attribute.cs
@@ -155,10 +155,7 @@ namespace Mono.CSharp {
 					return null;
 
 				e = a.Expr;
-				if ((e is EnumConstant) && !(e.Type is TypeBuilder)) {
-					pos_values [i] = System.Enum.ToObject (
-						e.Type, ((Constant) e).GetValue ());
-				} else if (e is Constant) {
+				if (e is Constant) {
 					pos_values [i] = ((Constant) e).GetValue ();
 				} else if (e is TypeOf) {
 					pos_values [i] = ((TypeOf) e).TypeArg;
