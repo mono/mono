ed8437dd7a26486ee98f1863112ae8f0defb4359
Martin Baulig <martin@novell.com>
2002-08-23 12:08:31 +0000

    2002-08-23  Martin Baulig  <martin@gnome.org>
    	* interface.cs (Interface.GetMembers): Added static check for events.
    
    svn path=/trunk/mcs/; revision=6939

--- mcs/mcs/interface.cs
+++ mcs/mcs/interface.cs
@@ -919,9 +919,8 @@ namespace Mono.CSharp {
 
 		MemberList IMemberContainer.GetMembers (MemberTypes mt, BindingFlags bf)
 		{
-			// Interfaces only contain instance members.  However, when querying for
-			// events, neither BindingFlags.Instance nor BindingFlags.Static will be set.
-			if (((bf & BindingFlags.Instance) == 0) && ((mt & MemberTypes.Event) == 0))
+			// Interfaces only contain instance members.
+			if ((bf & BindingFlags.Instance) == 0)
 				return MemberList.Empty;
 			if ((bf & BindingFlags.Public) == 0)
 				return MemberList.Empty;
