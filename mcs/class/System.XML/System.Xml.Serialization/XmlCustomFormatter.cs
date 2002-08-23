e2f62c9a84a35f590e2e0c28bd3b145cdd922995
Tim Coleman <tim@mono-cvs.ximian.com>
2002-08-23 03:46:58 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=6923

--- mcs/class/System.XML/System.Xml.Serialization/XmlCustomFormatter.cs
+++ mcs/class/System.XML/System.Xml.Serialization/XmlCustomFormatter.cs
@@ -16,6 +16,54 @@ namespace System.Xml.Serialization {
 
 		#region Methods
 
+		[MonoTODO]
+		public static byte[] FromByteArrayBase64 (byte[] value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static string FromByteArrayHex (byte[] value)
+		{
+			StringBuilder output = new StringBuilder ();
+			foreach (byte val in value)
+				output.Append (val.ToString ("X2"));
+			return output.ToString ();
+		}
+
+		public static string FromChar (char value)
+		{
+			return ((int) value).ToString ();
+		}
+
+		public static string FromDate (DateTime value)
+		{
+			return XmlConvert.ToString (value, "HH':'mm':'ss'.'fffffffzzz");
+		}
+
+		public static string FromDateTime (DateTime value)
+		{
+			return XmlConvert.ToString (value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz");
+		}
+
+		public static string FromTime (DateTime value)
+		{
+			return XmlConvert.ToString (value, "yyyy'-'MM'-'dd");
+		}
+
+		public static string FromEnum (long value, string[] values, long[] ids)
+		{
+			int length = ids.Length;
+
+			for (int i = 0; i < length; i ++) {
+				if (ids[i] == value) 
+					if (i >= values.Length)
+						return String.Empty;
+					else
+						return values[i].ToString ();
+			}
+			return value.ToString ();
+		}
+
 		public static string FromXmlName (string name)
 		{
 			return XmlConvert.EncodeName (name);
@@ -38,6 +86,54 @@ namespace System.Xml.Serialization {
 			foreach (string token in tokens)
 				output.Append (FromXmlNmToken (token));
 			return output.ToString ();
+		}
+
+		[MonoTODO]
+		public static char ToChar (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static DateTime ToDate (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static DateTime ToDateTime (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static DateTime ToTime (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static string ToXmlName (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static string ToXmlNCName (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static string ToXmlNmToken (string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static string ToXmlNmTokens (string value)
+		{
+			throw new NotImplementedException ();
 		}
 
 		#endregion // Methods
