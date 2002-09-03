b1313d9e7f28d50c18f97d2b38a3cedb830c1e64
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2002-09-03 13:04:37 +0000

    revert my last change - it needs to be public
    svn path=/trunk/mcs/; revision=7205

--- mcs/class/corlib/System.Runtime.Remoting.Messaging/MonoMethodMessage.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Messaging/MonoMethodMessage.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 
 namespace System.Runtime.Remoting.Messaging {
 	
-	internal class MonoMethodMessage : IMethodCallMessage, IMethodReturnMessage {
+	public class MonoMethodMessage : IMethodCallMessage, IMethodReturnMessage {
 
 		MonoMethod method;
 
