281e05c86279b25822205abea0c7e6b7c569d65e
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-09-04 02:35:26 +0000

    2002-9-2  DennisHayes <dennish@raytek.com>
    	* Brushes.cs : stubbed, added colors. Not sure how to implement,
    				 but this should at least save someone some typing.
    	* CharacterRange.cs :Implmented
    	* Region.cs : stub needed for system.windows.forms
    
    svn path=/trunk/mcs/; revision=7227

---
+++ mcs/class/System.Drawing/System.Drawing/Brushes.cs
@@ -0,0 +1,838 @@
+//
+// System.Windows.drawing.Brushes.cs
+//
+// Author:
+//   Dennis Hayes (dennish@Raytek.com)
+//	Suesan Chaney
+//
+// (C) Ximian, Inc., 2002
+//
+using System;
+
+namespace System.Drawing {
+	/// <summary>
+	/// Summary description for Brushes.
+	/// </summary>
+	public sealed class Brushes {
+		public static Brush AliceBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+	
+		public static Brush AntiqueWhite{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Aqua{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Aquamarine{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Azure{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Beige{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Bisque{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Black{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush blanchedAlmond{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Blue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush BlueViolet{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Brown{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush BurlyWood{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush CadetBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Chartreuse{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Chocolate{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Coral{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush CornFlowerBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush cornsilk{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Crimsion{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Cyan{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkCyan{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Darkgoldenrod{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkKhaki{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkMagenta{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush OliveGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkOrange{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkOrchard{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkRed{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkSalmon{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkSeaGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkSlateBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkSlateGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkTurquoise{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DarkViolet{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DeepPink{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DeepSkyBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DimGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush DodgerBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Firebrick{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush FloralWhite{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush ForestGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Fuchsia{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Gainsboro {
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush GhostWhite{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Gold{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Goldenrod{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Gray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Green{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush GreenYellow{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Honeydew{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush HotPink{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush IndianRed{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Indigo{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Ivory{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Khaki{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Lavender{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LavenderBlush{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LawnGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LemonChiffon{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightCoral{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightCyan{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightGoldenrodYellow{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightPink{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightSalmon{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightSeaGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightSkyBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightSlateGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightSteelBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush LightYellow{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Lime{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Linen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Magenta{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Maroon{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumAquamarine{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumOrchid{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumPurple{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumSeaGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumSpringGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumTurquoise{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MediumVioletRed{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MidnightBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MintCream{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush MistyRose{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Moccasin{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush NavajoWhite{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Navy{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush OldLace{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Olive{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush OliveDrab{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Orange{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush OrangeRed{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Orchid{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PaleGoldenrod{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PaleGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PaleTurquoise{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+		public static Brush PalevioletRed{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PapayaWhip{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PeachPuff{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Peru{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Pink{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Plum{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush PowderBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Purple{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Red{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush RosyBrown{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush RoyalBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SaddleBrown{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SandyBrown{ 
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SeaGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SeaShell{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Silver{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SkyBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SlateBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SlateGray{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+		public static Brush Snow{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SpringGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush SteelBlue{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Tan{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Teal{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Thistle{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Tomato{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Transparent{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Turquoise{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Violet{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Wheat{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush White{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush WhiteSmoke{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush Yellow{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static Brush YellowGreen{
+			get{
+				throw new NotImplementedException ();
+			}
+		}
+	
+	}
+}
---
+++ mcs/class/System.Drawing/System.Drawing/Region.cs
@@ -0,0 +1,25 @@
+//
+// System.Windows.Drawing.Region.cs
+//
+// Author:
+//	Dennis Hayes (dennish@raytek.com)
+// (C) 2002 Ximian, Inc
+//
+using System;
+using System.Drawing2d;
+namespace System.Drawing
+{
+	/// <summary>
+	/// Summary description for Region.
+	/// </summary>
+	/// 
+	//[ComVisible(false)]
+	public sealed class Region// : MarshalByRefObject, IDisposable
+	{
+		public Region() {
+		}
+		//[comVisible(false)]
+		public Region(GraphicsPath path) {
+		}
+	}
+}
