abdd012125b11fd8e396b41123873be0adb82e59
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-09-04 02:39:21 +0000

    2002-9-2  DennisHayes <dennish@raytek.com> 	* GraphicsPath.cs 	* added stub needed for system.windows.forms
    svn path=/trunk/mcs/; revision=7228

---
+++ mcs/class/System.Drawing/System.Drawing.Drawing2D/GraphicsPath.cs
@@ -0,0 +1,28 @@
+using System;
+namespace System.Drawing2d
+{
+	/// <summary>
+	/// Summary description for GraphicsPath.
+	/// </summary>
+	public sealed class GraphicsPath //: MarshalByRefObject, ICloneable, IDisposable
+	{
+		//[MonoTODO]
+		public GraphicsPath() {
+		}
+		//[MonoTODO]
+		//public GraphicsPath(FillMode fillMode) : this() {
+		//}
+		//[MonoTODO]
+		//public GraphicsPath(Point[] pts, byte[] types) : this() {
+		//}
+		//[MonoTODO]
+		//public GraphicsPath(PointF[] pts, byte[] types) : this() {
+		//}
+		//[MonoTODO]
+		//public GraphicsPath(Point[] pts, byte[] types, FillMode fillmode) : this(pts, types) {
+		//}
+		//[MonoTODO]
+		//public GraphicsPath(PointF[] pts, byte[] types, FillMode fillmode) : this(pts, types) {
+		//}
+	}
+}
