cc1d059ed082678c41ae73f4f25ac3744cff1e3f
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-09-12 22:47:25 +0000

    2002-09-13  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* list.unix: added new files.
    	* System.Drawing.Imaging/MetafileType.cs: implemented missing enum.
    
    svn path=/trunk/mcs/; revision=7415

---
+++ mcs/class/System.Drawing/System.Drawing.Imaging/MetafileType.cs
@@ -0,0 +1,22 @@
+//
+// System.Drawing.Imaging.MetafileType
+//
+// Authors:
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// (C) 2002 Ximian, Inc (http://www.ximian.com)
+//
+
+namespace System.Drawing.Imaging
+{
+	public enum MetafileType
+	{
+		Invalid = 0,
+		Wmf = 1,
+		WmfPlaceable = 2,
+		Emf = 3,
+		EmfPlusOnly = 4,
+		EmfPlusDual = 5
+	}
+}
+
