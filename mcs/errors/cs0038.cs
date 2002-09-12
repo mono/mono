d01a931d9fcf0616b1736ddc7835d903664f9640
Martin Baulig <martin@novell.com>
2002-09-12 15:45:40 +0000

    2002-09-12  Martin Baulig  <martin@gnome.org>
    	* cs0038.cs: New test.
    
    svn path=/trunk/mcs/; revision=7397

---
+++ mcs/errors/cs0038.cs
@@ -0,0 +1,22 @@
+// cs0038.cs: Cannot access a nonstatic member of outer type `X' via nested type `X.Y'.
+// line: 15
+using System;
+
+class X
+{
+	int a = 5;
+
+	class Y
+	{
+		public long b;
+
+		public Y ()
+		{
+			Console.WriteLine (a);
+		}
+	}
+
+	static void Main ()
+	{
+	}
+}
