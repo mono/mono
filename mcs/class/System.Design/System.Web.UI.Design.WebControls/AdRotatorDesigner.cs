f2bce6f4cee966012e3f2922c2607e3a50d10ca1
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2002-09-16 12:25:16 +0000

    2002-09-16     Gaurav Vaish <gvaish_mono@lycos.com>
    * AdRotatorDesigner.cs    : Initial implementation.
    * TODOAttribute.cs        : The TODO-Attribute class.
    * TODO                    : Added file
    * ChangeLog               : Added file
    
    svn path=/trunk/mcs/; revision=7499

---
+++ mcs/class/System.Design/System.Web.UI.Design.WebControls/AdRotatorDesigner.cs
@@ -0,0 +1,23 @@
+/**
+ * Namespace:   System.Web.UI.Design.WebControls
+ * Class:       AdRotatorDesigner
+ *
+ * Author:      Gaurav Vaish
+ * Maintainer:  mastergaurav AT users DOT sf DOT net
+ *
+ * (C) Gaurav Vaish (2002)
+ */
+
+using System;
+using System.Web.UI.Design;
+
+namespace System.Web.UI.Design.WebControls
+{
+	public class AdRotatorDesigner : ControlDesigner
+	{
+		public AdRotatorDesigner() : base()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
