053b98da026216f1e62e177e019bfbb45b169fc6
Sebastien Pouliot <sebastien@ximian.com>
2002-10-13 18:38:54 +0000

    2002-10-13  Sebastien Pouliot  <spouliot@videotron.ca>
    	* HashAlgorithm.cs: Added Dispose() to HashAlgorithm because it
    	inherits ICryptoTransform
    	* KeyedHashAlgorithm.cs: New implementation
    	* HMACSHA1.cs: New (include a generic HMACAlgorithm as internal class)
    	* MACTripleDES.cs: New (missing core implementation on generic MACAlgorithm)
    	* CryptoStream.cs: Added limited functionalities to support HMACSHA1
    
    svn path=/trunk/mcs/; revision=8221

--- mcs/class/corlib/System.Security.Cryptography/CryptoStream.cs
+++ mcs/class/corlib/System.Security.Cryptography/CryptoStream.cs
@@ -3,118 +3,135 @@
 //
 // Author:
 //   Thomas Neidhart (tome@sbox.tugraz.at)
+//   Sebastien Pouliot (spouliot@motus.com)
+//
+// Portions (C) 2002 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System;
 using System.IO;
 
-namespace System.Security.Cryptography
-{
+namespace System.Security.Cryptography {
 
-	public class CryptoStream : Stream
+public class CryptoStream : Stream {
+	private Stream _stream;
+	private ICryptoTransform _transform;
+	private CryptoStreamMode _mode;
+	
+	public CryptoStream(Stream stream, ICryptoTransform transform, CryptoStreamMode mode) 
 	{
-		private CryptoStreamMode _mode;
-		
-		public CryptoStream(Stream stream, ICryptoTransform transform, CryptoStreamMode mode) 
-		{
-			_mode = mode;
-		}
-		
-		public override bool CanRead
-		{
-			get {
-				switch (_mode) {
-					case CryptoStreamMode.Read:
-						return true;
-					
-					case CryptoStreamMode.Write:
-						return false;
-					
-					default:
-						return false;
-				}
-			}
-		}
+		_stream = stream;
+		_transform = transform;
+		_mode = mode;
+	}
 
-		public override bool CanSeek
-		{
-			get {
-				return false;
-			}
-		}
+	~CryptoStream () 
+	{
+		Dispose (false);
+	}
+	
+	public override bool CanRead {
+		get { return (_mode == CryptoStreamMode.Read); }
+	}
 
-		public override bool CanWrite
-		{
-			get {
-				switch (_mode) {
-					case CryptoStreamMode.Read:
-						return false;
-					
-					case CryptoStreamMode.Write:
-						return true;
-					
-					default:
-						return false;
-				}
-			}
-		}
-		
-		public override long Length
-		{
-			get {
-				throw new NotSupportedException("Length property not supported by CryptoStream");
-			}
-		}
+	public override bool CanSeek {
+		get { return false; }
+	}
 
-		public override long Position
-		{
-			get {
-				throw new NotSupportedException("Position property not supported by CryptoStream");
-			}
-			set {
-				throw new NotSupportedException("Position property not supported by CryptoStream");
-			}
+	public override bool CanWrite {
+		get { return (_mode == CryptoStreamMode.Write); }
+	}
+	
+	public override long Length {
+		get {
+			throw new NotSupportedException("Length property not supported by CryptoStream");
 		}
+	}
 
-		[MonoTODO]
-		public override int Read(byte[] buffer, int offset, int count)
-		{
-			// TODO: implement
-			return 0;
+	public override long Position {
+		get {
+			throw new NotSupportedException("Position property not supported by CryptoStream");
 		}
+		set {
+			throw new NotSupportedException("Position property not supported by CryptoStream");
+		}
+	}
 
-		[MonoTODO]
-		public override void Write(byte[] buffer, int offset, int count)
-		{
-			// TODO: implement
-		}
+	public void Clear () 
+	{
+		Dispose (true);
+	}
 
-		[MonoTODO]
-		public override void Flush()
-		{
-			// TODO: implement
-		}
+	[MonoTODO("Limited support for HMACSHA1")]
+	public override void Close () 
+	{
+		if (_mode != CryptoStreamMode.Write)
+			throw new NotSupportedException ();
+		// TODO: limited implemention for HMACSHA1
+		byte[] buffer = new byte [0];
+		_transform.TransformFinalBlock (buffer, 0, 0);
+		if (_stream != null)
+			_stream.Close();
+	}
 
-		[MonoTODO]
-		public void FlushFinalBlock()
-		{
-			if (_mode != CryptoStreamMode.Write)
-				throw new NotSupportedException("cannot flush a non-writeable CryptoStream");
-			
-			// TODO: implement
-		}
+	[MonoTODO]
+	public override int Read (byte[] buffer, int offset, int count)
+	{
+		if (_mode != CryptoStreamMode.Read)
+			throw new NotSupportedException ();
+		if ((offset < 0) || (count < 0))
+			throw new ArgumentOutOfRangeException ();
+		if (offset + count > buffer.Length)
+			throw new ArgumentException ();
+		// TODO: implement
+		return 0;
+	}
 
-		public override long Seek(long offset, SeekOrigin origin)
-		{
-			throw new NotSupportedException("cannot seek a CryptoStream");
-		}
-		
-		public override void SetLength(long value)
-		{
-			// LAMESPEC: should throw NotSupportedException like Seek??
-			return;
-		}
-		
-	} // CryptoStream
+	[MonoTODO("Limited support for HMACSHA1")]
+	public override void Write (byte[] buffer, int offset, int count)
+	{
+		if (_mode != CryptoStreamMode.Write)
+			throw new NotSupportedException ();
+		if ((offset < 0) || (count < 0))
+			throw new ArgumentOutOfRangeException ();
+		if (offset + count > buffer.Length)
+			throw new ArgumentException ();
+		// TODO: limited implemention for HMACSHA1
+		byte[] output = new byte [count];
+		_transform.TransformBlock (buffer, offset, count, output, 0);
+	}
+
+	[MonoTODO]
+	public override void Flush ()
+	{
+		if (_mode != CryptoStreamMode.Write)
+			throw new NotSupportedException ("cannot flush a non-writeable CryptoStream");
+		// TODO: implement
+	}
+
+	[MonoTODO]
+	public void FlushFinalBlock ()
+	{
+		if (_mode != CryptoStreamMode.Write)
+			throw new NotSupportedException ("cannot flush a non-writeable CryptoStream");
+		// TODO: implement
+	}
+
+	public override long Seek (long offset, SeekOrigin origin)
+	{
+		throw new NotSupportedException ("cannot Seek a CryptoStream");
+	}
+	
+	// LAMESPEC: Exception NotSupportedException not documented
+	public override void SetLength (long value)
+	{
+		throw new NotSupportedException ("cannot SetLength a CryptoStream");
+	}
+
+	protected virtual void Dispose (bool disposing) 
+	{
+	}
+	
+} // CryptoStream
 	
 } // System.Security.Cryptography
---
+++ mcs/class/corlib/System.Security.Cryptography/HMACSHA1.cs
@@ -0,0 +1,221 @@
+//
+// HMACSHA1.cs: Handles HMAC with SHA-1
+//
+// Author:
+//	Sebastien Pouliot (spouliot@motus.com)
+//
+// (C) 2002 Motus Technologies Inc. (http://www.motus.com)
+//
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace System.Security.Cryptography {
+
+// References:
+// a.	FIPS PUB 198: The Keyed-Hash Message Authentication Code (HMAC), 2002 March.
+//	http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
+// b.	Internet RFC 2104, HMAC, Keyed-Hashing for Message Authentication
+//	(include C source for HMAC-MD5)
+//	http://www.ietf.org/rfc/rfc2104.txt
+// c.	IETF RFC2202: Test Cases for HMAC-MD5 and HMAC-SHA-1
+//	(include C source for HMAC-MD5 and HAMAC-SHA1)
+//	http://www.ietf.org/rfc/rfc2202.txt
+// d.	ANSI X9.71, Keyed Hash Message Authentication Code.
+//	not free :-(
+//	http://webstore.ansi.org/ansidocstore/product.asp?sku=ANSI+X9%2E71%2D2000
+
+// Generic HMAC mechanisms - most of HMAC work is done in here.
+// It should work with any hash function e.g. MD5 for HMACMD5 (RFC2104)
+internal class HMACAlgorithm {
+	private byte[] key;
+	private byte[] hash;
+	private HashAlgorithm algo;
+	private string hashName;
+	private CryptoStream stream;
+
+	public HMACAlgorithm (string algoName) 
+	{
+		CreateHash (algoName);
+	}
+
+	~HMACAlgorithm () 
+	{
+		Dispose ();
+	}
+
+	private void CreateHash (string algoName) 
+	{
+		algo = (HashAlgorithm) CryptoConfig.CreateFromName (algoName);
+		hashName = algoName;
+	}
+
+	public void Dispose () 
+	{
+		ZeroizeKey ();
+	}
+
+	public HashAlgorithm Algo {
+		get { return algo; }
+	}
+
+	public string HashName {
+		get { return hashName; }
+		set { 
+			// only if its not too late for a change
+			if (stream == null)
+				CreateHash (value);
+		}
+	}
+
+	public byte[] HashValue {
+		get { return hash; }
+	}
+
+	public byte[] Key {
+		get { return key; }
+		set {
+			if ((value != null) && (value.Length > 64))
+				key = algo.ComputeHash (value);
+			else
+				key = (byte[]) value.Clone();
+		}
+	}
+
+	public void Initialize () 
+	{
+		hash = null;
+	}
+
+	private byte[] KeySetup (byte[] key, byte padding) 
+	{
+		byte[] buf = new byte [64];
+
+		for (int i = 0; i < key.Length; ++i)
+			buf [i] = (byte) ((byte) key [i] ^ padding);
+
+		for (int i = key.Length; i < 64; ++i)
+			buf [i] = padding;
+		
+		return buf;
+	}
+
+	public void Core (byte[] rgb, int ib, int cb) 
+	{
+		if (stream == null) {
+			byte[] buf = KeySetup (key, 0x36);
+			algo.Initialize ();
+			stream = new CryptoStream (Stream.Null, algo, CryptoStreamMode.Write);
+			stream.Write (buf, 0, buf.Length);
+		}
+		stream.Write (rgb, ib, cb);
+	}
+
+	public byte[] Final () 
+	{
+		stream.Close ();
+		stream = null;
+		byte[] intermediate = algo.Hash;
+		byte[] buf = KeySetup (key, 0x5C);
+
+		algo.Initialize ();
+		stream = new CryptoStream (Stream.Null, algo, CryptoStreamMode.Write);
+		stream.Write (buf, 0, buf.Length);
+		stream.Write (intermediate, 0, intermediate.Length);
+		stream.Close ();
+		stream = null;
+
+		hash = algo.Hash;
+		algo.Clear ();
+		return hash;
+	}
+
+	// Note: this key is different (well most of the time) from the key
+	// used in KeyHashAlgorithm (this one may be padded or hashed). So
+	// it need to be zeroized independently.
+	public void ZeroizeKey () 
+	{
+		if (key != null)
+			Array.Clear (key, 0, key.Length);
+	}
+}
+
+public class HMACSHA1: KeyedHashAlgorithm {
+	private HMACAlgorithm hmac;
+
+	public HMACSHA1 () : base ()
+	{
+		hmac = new HMACAlgorithm ("SHA1");
+		HashSizeValue = 160;
+		GenerateKey();
+	}
+
+	public HMACSHA1 (byte[] rgbKey) 
+	{
+		hmac = new HMACAlgorithm ("SHA1");
+		HashSizeValue = 160;
+		hmac.Key = rgbKey;
+	}
+
+	~HMACSHA1 () 
+	{
+		Dispose (false);
+	}
+
+	public override byte[] Key {
+		get { return base.Key; }
+		set { 
+			hmac.Key = value; 
+			base.Key = value;
+		}
+	} 
+
+	public string HashName {
+		get { return hmac.HashName; }
+		set { hmac.HashName = value; }
+	}
+
+	protected override void Dispose (bool disposing) 
+	{
+		if (hmac != null)
+			hmac.Dispose();
+		base.Dispose (disposing);
+	}
+
+	// generate a random 64 bits key
+	private void GenerateKey () 
+	{
+		KeyValue = new byte[8];
+		RandomNumberGenerator rng = RandomNumberGenerator.Create();
+		rng.GetBytes (KeyValue);
+		hmac.Key = KeyValue;
+	}
+
+	public override void Initialize ()
+	{
+		State = 0;
+		hmac.Initialize ();
+	}
+
+        protected override void HashCore (byte[] rgb, int ib, int cb)
+	{
+		if (State == 0) {
+			// let us throw an exception if hash name is invalid
+			// for HMACSHA1 (obviously this can't be done by the 
+			// generic HMAC class) 
+			if (! (hmac.Algo is SHA1))
+				throw new InvalidCastException ();
+		}
+		State = 1;
+		hmac.Core (rgb, ib, cb);
+	}
+
+	protected override byte[] HashFinal ()
+	{
+		State = 0;
+		return hmac.Final ();
+	}
+}
+
+}
