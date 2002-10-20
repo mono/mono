e96beff775b5bd6d1a5e74a0f8ac913df919ea73
Tim Coleman <tim@mono-cvs.ximian.com>
2002-10-20 06:46:11 +0000

    2002-10-20  Tim Coleman (tim@timcoleman.com)         * TdsConnection.cs:                 Lots of stuff to get this working somewhat.
    svn path=/trunk/mcs/; revision=8417

--- mcs/class/Mono.Data.TdsClient/Mono.Data.TdsClient/TdsConnection.cs
+++ mcs/class/Mono.Data.TdsClient/Mono.Data.TdsClient/TdsConnection.cs
@@ -10,8 +10,10 @@
 using Mono.Data.TdsClient.Internal;
 using System;
 using System.Collections;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
+using System.Text;
 
 namespace Mono.Data.TdsClient {
         public class TdsConnection : Component, ICloneable, IDbConnection
@@ -23,17 +25,13 @@ namespace Mono.Data.TdsClient {
 		int connectionTimeout = 15;
 		string database;
 		IsolationLevel isolationLevel = IsolationLevel.ReadCommitted;
-		int packetSize = 512;
 		ConnectionState state = ConnectionState.Closed;
 
-		string user;
-		string password;
-
-		ArrayList tdsPool;
+		ArrayList tdsPool = null;
 
 		TdsServerType serverType = TdsServerType.Generic;
 		TdsVersion tdsVersion = TdsVersion.tds42;
-		TdsConnectionParameters parms;
+		TdsConnectionParameters parms = new TdsConnectionParameters ();
 
 		#endregion // Fields
 
@@ -46,6 +44,7 @@ namespace Mono.Data.TdsClient {
 
 		public TdsConnection (string connectionString)
 		{
+			parms.PacketSize = 512;
 			SetConnectionString (connectionString);
 		}
 			
@@ -63,6 +62,11 @@ namespace Mono.Data.TdsClient {
 			set { parms.Database = value; }
 		}
 
+		public string Host {
+			get { return parms.Host; }
+			set { parms.Host = value; }
+		}
+
 		public ConnectionState State {
 			get { return state; }
 		}
@@ -70,6 +74,21 @@ namespace Mono.Data.TdsClient {
 		public int ConnectionTimeout {
 			get { return connectionTimeout; }
 			set { connectionTimeout = value; }
+		}
+
+		public int PacketSize {
+			get { return parms.PacketSize; }
+			set { parms.PacketSize = value; }
+		}
+
+		public string User {
+			get { return parms.User; }
+			set { parms.User = value; }
+		}
+
+		public string Password {
+			get { return parms.Password; }
+			set { parms.Password = value; }
 		}
 		
 		#endregion // Properties
@@ -82,6 +101,9 @@ namespace Mono.Data.TdsClient {
 
 			int index = FindAnAvailableTds ();
 			if (index == -1) {
+				Console.WriteLine ("Making new tds");
+				if (parms == null)
+					Console.WriteLine ("fucked up again");
 				Tds tmpTds = new Tds (parms);
 				TdsInstance tmp = new TdsInstance (tmpTds);
 				tdsPool.Add (tmp);
@@ -184,11 +206,11 @@ namespace Mono.Data.TdsClient {
 
 		public void Open ()
 		{
-			if (user == String.Empty || user == null)
+			if (User == String.Empty || User == null)
 			{
 				throw new ArgumentException ();
 			}
-			if (password == null)
+			if (Password == null)
 			{
 				throw new ArgumentException ();
 			}
@@ -197,7 +219,6 @@ namespace Mono.Data.TdsClient {
 				tdsPool = new ArrayList ();
 
 			Tds tmpTds = AllocateTds ();
-			tdsVersion = tmpTds.TdsVersion;
 			database = tmpTds.Database;
 			FreeTds (tmpTds);
 			tmpTds.Logon (parms);
@@ -206,12 +227,183 @@ namespace Mono.Data.TdsClient {
 		[System.MonoTODO]
 		private void SetConnectionString (string connectionString)
 		{
+			connectionString += ";";
+			NameValueCollection parameters = new NameValueCollection ();
+
 			if (connectionString == String.Empty)
 				return;
 
-			throw new NotImplementedException ();
+			bool inQuote = false;
+			bool inDQuote = false;
+
+			string name = String.Empty;
+			string value = String.Empty;
+			StringBuilder sb = new StringBuilder ();
+
+			foreach (char c in connectionString)
+			{
+				switch (c) {
+				case '\'':
+					inQuote = !inQuote;
+					break;
+				case '"' :
+					inDQuote = !inDQuote;
+					break;
+				case ';' :
+					if (!inDQuote && !inQuote) {
+						value = sb.ToString ();
+						sb = new StringBuilder ();
+					}
+					else
+						sb.Append (c);
+					break;
+				case '=' :
+					if (!inDQuote && !inQuote) {
+						name = sb.ToString ();
+						sb = new StringBuilder ();
+					}
+					else
+						sb.Append (c);
+					break;
+				default:
+					sb.Append (c);
+					break;
+				}
+				if (value != String.Empty) {
+					parameters [name.ToUpper ().Trim ()] = value.Trim ();
+					name = String.Empty;
+					value = String.Empty;
+				} 
+			}
+
+			foreach (string key in parameters)
+			{
+				Console.WriteLine ("{0}:{1}", key, parameters[key]);
+			}
+
+			if (this.ConnectionString == String.Empty)
+			{
+				SetDefaultConnectionParameters (parameters);
+			}
+
+			SetProperties (parameters);
+
+			this.connectionString = connectionString;
 		}
 
+
+		private void SetDefaultConnectionParameters (NameValueCollection parameters)
+		{
+                        if (null == parameters.Get ("APPLICATION NAME"))
+                                parameters["APPLICATION NAME"] = ".Net SqlClient Data Provider";
+                        if (null == parameters.Get ("CONNECT TIMEOUT") && null == parameters.Get ("CONNECTION TIMEOUT"))
+                                parameters["CONNECT TIMEOUT"] = "15";
+                        if (null == parameters.Get ("CONNECTION LIFETIME"))
+                                parameters["CONNECTION LIFETIME"] = "0";
+                        if (null == parameters.Get ("CONNECTION RESET"))
+                                parameters["CONNECTION RESET"] = "true";
+                        if (null == parameters.Get ("ENLIST"))
+                                parameters["ENLIST"] = "true";
+                        if (null == parameters.Get ("INTEGRATED SECURITY") && null == parameters.Get ("TRUSTED_CONNECTION"))
+                                parameters["INTEGRATED SECURITY"] = "false";
+                        if (null == parameters.Get ("MAX POOL SIZE"))
+                                parameters["MAX POOL SIZE"] = "100";
+                        if (null == parameters.Get ("MIN POOL SIZE"))
+                                parameters["MIN POOL SIZE"] = "0";
+                        if (null == parameters.Get ("NETWORK LIBRARY") && null == parameters.Get ("NET"))
+                                parameters["NETWORK LIBRARY"] = "dbmssocn";
+                        if (null == parameters.Get ("PACKET SIZE"))
+                                parameters["PACKET SIZE"] = "8192";
+                        if (null == parameters.Get ("PERSIST SECURITY INFO"))
+                                parameters["PERSIST SECURITY INFO"] = "false";
+                        if (null == parameters.Get ("POOLING"))
+                                parameters["POOLING"] = "true";
+		}
+
+                private void SetProperties (NameValueCollection parameters)
+                {
+                        string value;
+
+                        foreach (string name in parameters) {
+                                value = parameters[name];
+
+                                switch (name) {
+                                case "APPLICATION NAME" :
+                                        break;
+                                case "ATTACHDBFILENAME" :
+                                        break;
+                                case "EXTENDED PROPERTIES" :
+                                        break;
+                                case "INITIAL FILE NAME" :
+                                        break;
+                                case "CONNECT TIMEOUT" :
+                                        ConnectionTimeout = Int32.Parse (value);
+                                        break;
+                                case "CONNECTION TIMEOUT" :
+                                        ConnectionTimeout = Int32.Parse (value);
+                                        break;
+                                case "CONNECTION LIFETIME" :
+                                        break;
+                                case "CONNECTION RESET" :
+                                        break;
+                                case "CURRENT LANGUAGE" :
+                                        break;
+                                case "DATA SOURCE" :
+                                        Host = value;
+                                        break;
+                                case "SERVER" :
+                                        Host = value;
+                                        break;
+                                case "ADDRESS" :
+                                        Host = value;
+                                        break;
+                                case "ADDR" :
+                                        Host = value;
+                                        break;
+                                case "NETWORK ADDRESS" :
+                                        Host = value;
+                                        break;
+                                case "ENLIST" :
+                                        break;
+                                case "INITIAL CATALOG" :
+                                        Database = value;
+                                        break;
+                                case "DATABASE" :
+                                        Database = value;
+                                        break;
+                                case "INTEGRATED SECURITY" :
+                                        break;
+                                case "TRUSTED_CONNECTION" :
+                                        break;
+                                case "MAX POOL SIZE" :
+                                        break;
+                                case "MIN POOL SIZE" :
+                                        break;
+                                case "NETWORK LIBRARY" :
+                                        break;
+                                case "NET" :
+                                        break;
+                                case "PACKET SIZE" :
+					PacketSize = Int32.Parse (value);
+                                        break;
+                                case "PASSWORD" :
+                                        Password = value;
+                                        break;
+                                case "PWD" :
+                                        Password = value;
+                                        break;
+                                case "PERSIST SECURITY INFO" :
+                                        break;
+                                case "POOLING" :
+                                        break;
+                                case "USER ID" :
+                                        User = value;
+                                        break;
+                                case "WORKSTATION ID" :
+                                        break;
+				}
+			}
+		}
 		#endregion // Methods
 
 		private class TdsInstance 
