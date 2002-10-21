120099cd4bd47a33698d59173bd7300953e2c208
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-10-21 03:15:13 +0000

    2002-10-18  DennisHayes <dennish@raytek.com>
    	* KeyPressEventHandler.cs
    	* Keys.cs
    	* LabelEditEventArgs.cs
    	* LabelEditEventHandler.cs
    	* LayoutEventArgs.cs
    	* LayoutEventHandler.cs
    	* LeftRightAlignment.cs
    	* LinkArea.cs
    	* LinkBehavior.cs
    	* LinkClickedEventArgs.cs
    	* LinkClickedEventHandler.cs
    	* LinkLabel.cs
    	* LinkLabelLinkClickedEventArgs.cs
    	* LinkLabelLinkClickedEventHandler.cs
    	* ListViewAlignment.cs
    	* MainMenu.cs
    	* MdiLayout.cs
    	* MeasureItemEventArgs.cs
    	* MeasureItemEventHandler.cs
    	* Menu.cs
    	* Message.cs
    	* MessageBoxButtons.cs
    	* MessageBoxDefaultButton.cs
    	* MessageBoxIcon.cs
    	* MessageBoxOptions.cs
    	* MouseButtons.cs
    	* MouseEventArgs.cs
    	* MouseEventHandler.cs
    	* NavigateEventArgs.cs
    	* NavigateEventHandler.cs
    	* NodeLabelEditEventArgs.cs
    	* NodeLabelEditEventHandler.cs
    	* PaintEventArgs.cs
    
    svn path=/trunk/mcs/; revision=8437

---
+++ mcs/class/System.Windows.Forms/WINELib/MeasureItemEventHandler.cs
@@ -0,0 +1,19 @@
+//
+// System.Windows.Forms.MeasureItemEventHandler.cs
+//
+// Authors:
+//   Jaak Simm (jaaksimm@firm.ee)
+//   Dennis Hayes (dennish@Raytek.com)
+//
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// Represents the method that will handle the MeasureItem event of the 
+	/// ListBox, ComboBox, CheckedListBox, or MenuItem controls.
+	/// </summary>
+	//[Serializable]
+	public delegate void MeasureItemEventHandler(object sender, MeasureItemEventArgs e);
+}
