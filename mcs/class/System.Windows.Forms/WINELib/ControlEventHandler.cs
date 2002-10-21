e648a7e8dc5b802b795c3c8aca47f9f55dec518e
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-10-21 03:02:52 +0000

    2002-10-18  DennisHayes <dennish@raytek.com> 	* Application.cs 	* Button.cs 	* ButtonBase.cs 	* changelog 	* ContainerControl.cs 	* Control.cs 	* DrawItemEventArgs.cs 	* DrawItemEventHandler.cs 	* Font.cs 	* Form.cs 	* IAccessible.cs 	* IContainerControl.cs 	* Label.cs 	* MenuItem.cs 	* MessageBox.cs 	* NativeWindow.cs 	* ScrollableControl.cs 	* Win32.cs
    	* Added some implmentation. Uncommented some stuff. 
    	* About to add files to implment stuff uncommented.
    
    svn path=/trunk/mcs/; revision=8434

---
+++ mcs/class/System.Windows.Forms/WINELib/ControlEventHandler.cs
@@ -0,0 +1,16 @@
+//
+// System.Windows.Forms.ControlEventHandler.cs
+//
+// Author:
+//   Dennis hayes (dennish@raytek.com)
+//
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Windows.Forms {
+	//[Serializable]
+	public delegate void ControlEventHandler (
+	object sender,
+	ControlEventArgs e
+	);
+}
