52d3e9e129de065bbd0b41fd44669f3d6bd76eb1
Tim Coleman <tim@mono-cvs.ximian.com>
2002-10-25 18:49:16 +0000

    2002-10-25  Tim Coleman (tim@timcoleman.com) 	* SqlDataReader.cs: 		Missed checking in this file before.
    svn path=/trunk/mcs/; revision=8560

--- mcs/class/System.Data/System.Data.SqlClient/SqlDataReader.cs
+++ mcs/class/System.Data/System.Data.SqlClient/SqlDataReader.cs
@@ -4,25 +4,13 @@
 // Author:
 //   Rodrigo Moya (rodrigo@ximian.com)
 //   Daniel Morgan (danmorg@sc.rr.com)
+//   Tim Coleman (tim@timcoleman.com)
 //
 // (C) Ximian, Inc 2002
 // (C) Daniel Morgan 2002
-//
-// Credits:
-//    SQL and concepts were used from libgda 0.8.190 (GNOME Data Access)
-//    http://www.gnome-db.org/
-//    with permission from the authors of the
-//    PostgreSQL provider in libgda:
-//        Michael Lausch <michael@lausch.at>
-//        Rodrigo Moya <rodrigo@gnome-db.org>
-//        Vivien Malerba <malerba@gnome-db.org>
-//        Gonzalo Paniagua Javier <gonzalo@gnome-db.org>
-//
+// Copyright (C) Tim Coleman, 2002
 
-// *** uncomment #define to get debug messages, comment for production ***
-//#define DEBUG_SqlDataReader
-
-
+using Mono.Data.TdsClient.Internal;
 using System;
 using System.Collections;
 using System.ComponentModel;
@@ -34,389 +22,302 @@ namespace System.Data.SqlClient {
 	/// of result sets obtained by executing a command 
 	/// at a SQL database.
 	/// </summary>
-	//public sealed class SqlDataReader : MarshalByRefObject,
-	//	IEnumerable, IDataReader, IDisposable, IDataRecord
-	public sealed class SqlDataReader : IEnumerable, 
-		IDataReader, IDataRecord {
+	public sealed class SqlDataReader : MarshalByRefObject, IEnumerable, IDataReader, IDisposable, IDataRecord
+	{
 		#region Fields
 
-		private SqlCommand cmd;
-		private DataTable table = null;
+		int fieldCount;
+		bool hasRows;
+		bool isClosed;
+		int recordsAffected;
 
-		// columns in a row
-		private object[] fields; // data value in a .NET type
-		private string[] types; // PostgreSQL Type
-		private bool[] isNull; // is NULL?
-		private int[] actualLength; // ActualLength of data
-		private DbType[] dbTypes; // DB data type
-		// actucalLength = -1 is variable-length
-				
-		private bool open = false;
-		IntPtr pgResult; // PGresult
-		private int rows;
-		private int cols;
+		SqlCommand command;
+		DataTable schemaTable;
 
-		private int recordsAffected = -1; // TODO: get this value
-
-		private int currentRow = -1; // no Read() has been done yet
-
-		#endregion // Fields
+		#endregion // eields
 
 		#region Constructors
 
-		internal SqlDataReader (SqlCommand sqlCmd) {
-
-			cmd = sqlCmd;
-			open = true;
-			cmd.OpenReader(this);
+		internal SqlDataReader (SqlCommand command)
+		{
+			schemaTable = ConstructSchemaTable ();
+			this.command = command;
+			this.fieldCount = 0;
+			this.isClosed = false;
+			NextResult ();
 		}
 
 		#endregion
 
-		#region Public Methods
-
-		[MonoTODO]
-		public void Close() {
-			open = false;
-			
-			// free SqlDataReader resources in SqlCommand
-			// and allow SqlConnection to be used again
-			cmd.CloseReader();
-
-			// TODO: get parameters from result
-
-			// clear unmanaged PostgreSQL result set
-			PostgresLibrary.PQclear (pgResult);
-			pgResult = IntPtr.Zero;
-		}
-
-		[MonoTODO]
-		public DataTable GetSchemaTable() {
-			return table;
-		}
-
-		[MonoTODO]
-		public bool NextResult() {
-			SqlResult res;
-			currentRow = -1;
-			bool resultReturned;
-			
-			// reset
-			table = null;
-			pgResult = IntPtr.Zero;
-			rows = 0;
-			cols = 0;
-			types = null;
-			recordsAffected = -1;
-
-			res = cmd.NextResult();
-			resultReturned = res.ResultReturned;
-
-			if(resultReturned == true) {
-				table = res.Table;
-				pgResult = res.PgResult;
-				rows = res.RowCount;
-				cols = res.FieldCount;
-				types = res.PgTypes;
-				recordsAffected = res.RecordsAffected;
-			}
-			
-			res = null;
-			return resultReturned;
-		}
-
-		[MonoTODO]
-		public bool Read() {
-			
-			string dataValue;
-			int c = 0;
-			
-			if(currentRow < rows - 1)  {
-				
-				currentRow++;
-			
-				// re-init row
-				fields = new object[cols];
-				//dbTypes = new DbType[cols];
-				actualLength = new int[cols];
-				isNull = new bool[cols];
-			
-				for(c = 0; c < cols; c++) {
-
-					// get data value
-					dataValue = PostgresLibrary.
-						PQgetvalue(
-						pgResult,
-						currentRow, c);
-
-					// is column NULL?
-					//isNull[c] = PostgresLibrary.
-					//	PQgetisnull(pgResult,
-					//	currentRow, c);
-
-					// get Actual Length
-					actualLength[c] = PostgresLibrary.
-						PQgetlength(pgResult,
-						currentRow, c);
-
-					DbType dbType;	
-					dbType = PostgresHelper.
-						TypnameToSqlDbType(types[c]);
-
-					if(dataValue == null) {
-						fields[c] = null;
-						isNull[c] = true;
-					}
-					else if(dataValue.Equals("")) {
-						fields[c] = null;
-						isNull[c] = true;
-					}
-					else {
-						isNull[c] = false;
-						fields[c] = PostgresHelper.
-							ConvertDbTypeToSystem (
-							dbType,
-							dataValue);
-					}
-				}
-				return true;
-			}
-			return false; // EOF
-		}
-
-		[MonoTODO]
-		public byte GetByte(int i) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public long GetBytes(int i, long fieldOffset, 
-			byte[] buffer, int bufferOffset, 
-			int length) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public char GetChar(int i) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public long GetChars(int i, long fieldOffset, 
-			char[] buffer, int bufferOffset, 
-			int length) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public IDataReader GetData(int i) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public string GetDataTypeName(int i) {
-			return types[i];
-		}
-
-		[MonoTODO]
-		public DateTime GetDateTime(int i) {
-			return (DateTime) fields[i];
-		}
-
-		[MonoTODO]
-		public decimal GetDecimal(int i) {
-			return (decimal) fields[i];
-		}
-
-		[MonoTODO]
-		public double GetDouble(int i) {
-			return (double) fields[i];
-		}
-
-		[MonoTODO]
-		public Type GetFieldType(int i) {
-
-			DataRow row = table.Rows[i];
-			return Type.GetType((string)row["DataType"]);
-		}
-
-		[MonoTODO]
-		public float GetFloat(int i) {
-			return (float) fields[i];
-		}
-
-		[MonoTODO]
-		public Guid GetGuid(int i) {
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
-		public short GetInt16(int i) {
-			return (short) fields[i];
-		}
-
-		[MonoTODO]
-		public int GetInt32(int i) {
-			return (int) fields[i];
-		}
-
-		[MonoTODO]
-		public long GetInt64(int i) {
-			return (long) fields[i];
-		}
-
-		[MonoTODO]
-		public string GetName(int i) {
-
-			DataRow row = table.Rows[i];
-			return (string) row["ColumnName"];
-		}
-
-		[MonoTODO]
-		public int GetOrdinal(string name) {
-
-			int i;
-			DataRow row;
-
-			for(i = 0; i < table.Rows.Count; i++) {
-				row = table.Rows[i];
-				if(((string) row["ColumnName"]).Equals(name))
-					return i;
-			}
-
-			for(i = 0; i < table.Rows.Count; i++) {
-				string ta;
-				string n;
-					
-				row = table.Rows[i];
-				ta = ((string) row["ColumnName"]).ToUpper();
-				n = name.ToUpper();
-						
-				if(ta.Equals(n)) {
-					return i;
-				}
-			}
-			
-			throw new MissingFieldException("Missing field: " + name);
-		}
-
-		[MonoTODO]
-		public string GetString(int i) {
-			return (string) fields[i];
-		}
-
-		[MonoTODO]
-		public object GetValue(int i) {
-			return fields[i];
-		}
-
-		[MonoTODO]
-		public int GetValues(object[] values) 
-		{
-			Array.Copy (fields, values, fields.Length);
-			return fields.Length;
-		}
-
-		[MonoTODO]
-		public bool IsDBNull(int i) {
-			return isNull[i];
-		}
-
-		[MonoTODO]
-		public bool GetBoolean(int i) {
-			return (bool) fields[i];
-		}
-
-		[MonoTODO]
-		public IEnumerator GetEnumerator() {
-			throw new NotImplementedException ();
-		}
-
-		#endregion // Public Methods
-
-		#region Destructors
-
-		[MonoTODO]
-		public void Dispose () {
-		}
-
-		//[MonoTODO]
-		//~SqlDataReader() {
-		//}
-
-		#endregion // Destructors
-
 		#region Properties
 
 		public int Depth {
-			[MonoTODO]
-			get { 
-				return 0; // always return zero, unless
-				          // this provider will allow
-				          // nesting of a row
-			}
+			get { return 0; }
+		}
+
+		public int FieldCount {
+			get { return fieldCount; }
+		}
+
+		public bool HasRows {
+			get { return hasRows; }
 		}
 
 		public bool IsClosed {
-			[MonoTODO]
-			get {
-				if(open == false)
-					return true;
-				else
-					return false;
-			}
+			get { return isClosed; }
+		}
+
+		public object this [int i] {
+			get { return GetValue (i); }
+		}
+
+		public object this [string name] {
+			get { return GetValue (GetOrdinal (name)); }
 		}
 
 		public int RecordsAffected {
-			[MonoTODO]
-			get { 
-				return recordsAffected;
-			}
-		}
-	
-		public int FieldCount {
-			[MonoTODO]
-			get { 
-				return cols;
-			}
-		}
-
-		public object this[string name] {
-			[MonoTODO]
-			get { 
-				int i;
-				DataRow row;
-
-				for(i = 0; i < table.Rows.Count; i++) {
-					row = table.Rows[i];
-					if(row["ColumnName"].Equals(name))
-						return fields[i];
-				}
-
-				for(i = 0; i < table.Rows.Count; i++) {
-					string ta;
-					string n;
-					
-					row = table.Rows[i];
-					ta = ((string) row["ColumnName"]).ToUpper();
-					n = name.ToUpper();
-						
-					if(ta.Equals(n)) {
-						return fields[i];
-					}
-				}
-			
-				throw new MissingFieldException("Missing field: " + name);
-			}
-		}
-
-		public object this[int i] {
-			[MonoTODO]
-			get { 
-				return fields[i];
-			}
+			get { return recordsAffected; }
 		}
 
 		#endregion // Properties
+
+		#region Methods
+
+		public void Close()
+		{
+			isClosed = true;
+			command.Connection.DataReaderOpen = false;
+		}
+
+		private static DataTable ConstructSchemaTable ()
+		{
+			Type booleanType = Type.GetType ("System.Boolean");
+			Type stringType = Type.GetType ("System.String");
+			Type intType = Type.GetType ("System.Int32");
+			Type typeType = Type.GetType ("System.Type");
+
+			DataTable schemaTable = new DataTable ("SchemaTable");
+			schemaTable.Columns.Add ("ColumnName", stringType);
+			schemaTable.Columns.Add ("ColumnOrdinal", intType);
+			schemaTable.Columns.Add ("ColumnSize", intType);
+			schemaTable.Columns.Add ("NumericPrecision", intType);
+			schemaTable.Columns.Add ("NumericScale", intType);
+			schemaTable.Columns.Add ("IsUnique", booleanType);
+			schemaTable.Columns.Add ("IsKey", booleanType);
+			schemaTable.Columns.Add ("BaseServerName", stringType);
+			schemaTable.Columns.Add ("BaseCatalogName", stringType);
+			schemaTable.Columns.Add ("BaseColumnName", stringType);
+			schemaTable.Columns.Add ("BaseSchemaName", stringType);
+			schemaTable.Columns.Add ("BaseTableName", stringType);
+			schemaTable.Columns.Add ("DataType", typeType);
+			schemaTable.Columns.Add ("AllowDBNull", booleanType);
+			schemaTable.Columns.Add ("ProviderType", booleanType);
+			schemaTable.Columns.Add ("IsAliased", booleanType);
+			schemaTable.Columns.Add ("IsExpression", booleanType);
+			schemaTable.Columns.Add ("IsIdentity", booleanType);
+			schemaTable.Columns.Add ("IsAutoIncrement", booleanType);
+			schemaTable.Columns.Add ("IsRowVersion", booleanType);
+			schemaTable.Columns.Add ("IsHidden", booleanType);
+			schemaTable.Columns.Add ("IsLong", booleanType);
+			schemaTable.Columns.Add ("IsReadOnly", booleanType);
+
+			return schemaTable;
+		}
+
+		[MonoTODO]
+		public bool GetBoolean (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public byte GetByte (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public long GetBytes (int i, long dataIndex, byte[] buffer, int bufferIndex, int length)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public char GetChar (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public long GetChars (int i, long dataIndex, char[] buffer, int bufferIndex, int length)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public IDataReader GetData (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public string GetDataTypeName (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public DateTime GetDateTime (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is DateTime))
+				throw new InvalidCastException ();
+			return (DateTime) value;
+		}
+
+		public decimal GetDecimal (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is decimal))
+				throw new InvalidCastException ();
+			return (decimal) value;
+		}
+		public double GetDouble (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is double))
+				throw new InvalidCastException ();
+			return (double) value;
+		}
+
+		public Type GetFieldType (int i)
+		{
+			return GetValue (i).GetType ();
+		}
+
+		public float GetFloat (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is float))
+				throw new InvalidCastException ();
+			return (float) value;
+		}
+
+		[MonoTODO]
+		public Guid GetGuid (int i)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public short GetInt16 (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is short))
+				throw new InvalidCastException ();
+			return (short) value;
+		}
+
+		public int GetInt32 (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is int))
+				throw new InvalidCastException ();
+			return (int) value;
+		}
+
+		public long GetInt64 (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is long))
+				throw new InvalidCastException ();
+			return (long) value;
+		}
+
+		public string GetName (int i)
+		{
+			return (string) schemaTable.Rows[i]["ColumnName"];
+		}
+
+		[MonoTODO]
+		public int GetOrdinal (string name)
+		{
+			foreach (DataRow schemaRow in schemaTable.Rows)
+				if (schemaRow ["ColumnName"] == name)
+					return (int) schemaRow ["ColumnOrdinal"];
+			foreach (DataRow schemaRow in schemaTable.Rows)
+				if (String.Compare (((string) schemaRow ["ColumnName"]), name, true) == 0)
+					return (int) schemaRow ["ColumnOrdinal"];
+			throw new IndexOutOfRangeException ();
+		}
+
+		public DataTable GetSchemaTable ()
+		{
+			if (schemaTable.Rows != null && schemaTable.Rows.Count > 0)
+				return schemaTable;
+			fieldCount = 0;
+
+			foreach (TdsColumnSchema schemaObject in command.Tds.ColumnInfo) {
+				DataRow schemaRow = schemaTable.NewRow ();
+				schemaRow ["ColumnName"] = schemaObject.ColumnName;
+				schemaRow ["ColumnOrdinal"] = schemaObject.ColumnOrdinal;
+				schemaRow ["BaseTableName"] = schemaObject.TableName;
+				schemaRow ["AllowDBNull"] = schemaObject.Nullable;
+				schemaRow ["IsReadOnly"] = !schemaObject.Writable;
+				schemaTable.Rows.Add (schemaRow);
+				fieldCount += 1;
+			}
+			return schemaTable;
+		}		
+
+		public int GetInt32 (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is int))
+				throw new InvalidCastException ();
+			return (int) value;
+		}
+
+		public long GetInt64 (int i)
+		{
+			object value = GetValue (i);
+			if (!(value is long))
+				throw new InvalidCastException ();
+			return (long) value;
+		}
+
+		public string GetName (int i)
+		{
+			return (string) schemaTable.Rows[i]["ColumnName"];
+		}
+
+		[MonoTODO]
+		public int GetOrdinal (string name)
+		{
+			foreach (DataRow schemaRow in schemaTable.Rows)
+				if (schemaRow ["ColumnName"] == name)
+					return (int) schemaRow ["ColumnOrdinal"];
+			foreach (DataRow schemaRow in schemaTable.Rows)
+				if (String.Compare (((string) schemaRow ["ColumnName"]), name, true) == 0)
+					return (int) schemaRow ["ColumnOrdinal"];
+			throw new IndexOutOfRangeException ();
+		}
+
+		public DataTable GetSchemaTable ()
+		{
+			if (schemaTable.Rows != null && schemaTable.Rows.Count > 0)
+				return schemaTable;
+			fieldCount = 0;
+
+			foreach (TdsColumnSchema schemaObject in command.Tds.ColumnInfo) {
+				DataRow schemaRow = schemaTable.NewRow ();
+				schemaRow ["ColumnName"] = schemaObject.ColumnName;
+				schemaRow ["ColumnOrdinal"] = schemaObject.ColumnOrdinal;
+				schemaRow ["BaseTableName"] = schemaObject.TableName;
+				schemaRow ["AllowDBNull"] = schemaObject.Nullable;
+				schemaRow ["IsReadOnly"] = !schemaObject.Writable;
+				schemaTable.Rows.Add (schemaRow);
+				fieldCount += 1;
+			}
+			return schemaTable;
+		}
 	}
 }
