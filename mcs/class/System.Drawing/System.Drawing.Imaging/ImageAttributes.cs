8e590adc3108e7b063d22b61f6ab43c6b76e0f6e
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-10-28 17:43:05 +0000

    added and implmented
    svn path=/trunk/mcs/; revision=8629

---
+++ mcs/class/System.Drawing/System.Drawing.Imaging/ImageAttributes.cs
@@ -0,0 +1,34 @@
+//
+// System.Windows.Forms.FormBorderStyle.cs
+//
+// Author:
+//   Dennis Hayes (dennish@raytek.com)
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Drawing;
+
+namespace System.Drawing.Imaging
+{
+	/// <summary>
+	/// Summary description for ImageAttributes.
+	/// </summary>
+	public class ImageAttributes {
+		public ImageAttributes() {
+		}
+
+		//Clears the color keys for all GDI+ objects
+		public void ClearColorKey(){
+		}
+
+		//Sets the color keys for all GDI+ objects
+		public void SetColorKey(COLOR colorLow, COLOR colorHigh){
+
+		}
+
+		public ~ImageAttributes() {
+		}
+
+	}
+}
