09a10d4caa171327a31b85f726b014c08a73f984
Sebastien Pouliot <sebastien@ximian.com>
2002-10-29 02:49:43 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=8641

---
+++ mcs/class/corlib/Test/System.Security.Cryptography/SHA256ManagedTest.cs
@@ -0,0 +1,69 @@
+//
+// SHA256ManagedTest.cs - NUnit Test Cases for SHA256Managed
+//
+// Author:
+//	Sebastien Pouliot (spouliot@motus.com)
+//
+// (C) 2002 Motus Technologies Inc. (http://www.motus.com)
+//
+
+using NUnit.Framework;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MonoTests.System.Security.Cryptography {
+
+// References:
+// a.	FIPS PUB 180-2: Secure Hash Standard
+//	http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf
+
+// we inherit from SHA256Test because all SHA256 implementation must return the 
+// same results (hence should run a common set of unit tests).
+public class SHA256ManagedTest : SHA256Test {
+
+	public SHA256ManagedTest () : base ("System.Security.Cryptography.SHA256Managed testsuite") {}
+	public SHA256ManagedTest (string name) : base (name) {}
+
+	protected override void SetUp () 
+	{
+		hash = new SHA256Managed ();
+	}
+
+	protected override void TearDown () {}
+
+	public static new ITest Suite {
+		get { 
+			return new TestSuite (typeof (SHA256ManagedTest)); 
+		}
+	}
+
+	public override void TestCreate () 
+	{
+		// no need to repeat this test
+	}
+
+	// none of those values changes for a particuliar implementation of SHA256
+	public override void TestStaticInfo () 
+	{
+		// test all values static for SHA256
+		base.TestStaticInfo ();
+		string className = hash.ToString ();
+		AssertEquals (className + ".CanReuseTransform", true, hash.CanReuseTransform);
+		AssertEquals (className + ".CanTransformMultipleBlocks", true, hash.CanTransformMultipleBlocks);
+		AssertEquals (className + ".ToString()", "System.Security.Cryptography.SHA256Managed", className);
+	}
+
+	public void TestSHA256forFIPSCompliance () 
+	{
+		SHA256 sha = (SHA256) hash;
+		// First test, we hash the string "abc"
+		FIPS186_Test1 (sha);
+		// Second test, we hash the string "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"
+		FIPS186_Test2 (sha);
+		// Third test, we hash 1,000,000 times the character "a"
+		FIPS186_Test3 (sha);
+	}
+}
+
+}
---
+++ mcs/class/corlib/Test/System.Security.Cryptography/SHA384ManagedTest.cs
@@ -0,0 +1,69 @@
+//
+// SHA384ManagedTest.cs - NUnit Test Cases for SHA384Managed
+//
+// Author:
+//	Sebastien Pouliot (spouliot@motus.com)
+//
+// (C) 2002 Motus Technologies Inc. (http://www.motus.com)
+//
+
+using NUnit.Framework;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MonoTests.System.Security.Cryptography {
+
+// References:
+// a.	FIPS PUB 180-2: Secure Hash Standard
+//	http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf
+
+// we inherit from SHA384Test because all SHA384 implementation must return the 
+// same results (hence should run a common set of unit tests).
+public class SHA384ManagedTest : SHA384Test {
+
+	public SHA384ManagedTest () : base ("System.Security.Cryptography.SHA384Managed testsuite") {}
+	public SHA384ManagedTest (string name) : base (name) {}
+
+	protected override void SetUp () 
+	{
+		hash = new SHA384Managed ();
+	}
+
+	protected override void TearDown () {}
+
+	public static new ITest Suite {
+		get { 
+			return new TestSuite (typeof (SHA384ManagedTest)); 
+		}
+	}
+
+	public override void TestCreate () 
+	{
+		// no need to repeat this test
+	}
+
+	// none of those values changes for a particuliar implementation of SHA384
+	public override void TestStaticInfo () 
+	{
+		// test all values static for SHA384
+		base.TestStaticInfo ();
+		string className = hash.ToString ();
+		AssertEquals (className + ".CanReuseTransform", true, hash.CanReuseTransform);
+		AssertEquals (className + ".CanTransformMultipleBlocks", true, hash.CanTransformMultipleBlocks);
+		AssertEquals (className + ".ToString()", "System.Security.Cryptography.SHA384Managed", className);
+	}
+
+	public void TestSHA384forFIPSCompliance () 
+	{
+		SHA384 sha = (SHA384) hash;
+		// First test, we hash the string "abc"
+		FIPS186_Test1 (sha);
+		// Second test, we hash the string "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"
+		FIPS186_Test2 (sha);
+		// Third test, we hash 1,000,000 times the character "a"
+		FIPS186_Test3 (sha);
+	}
+}
+
+}
