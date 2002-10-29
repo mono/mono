1580b7655de42d38f22cd644a5abfda2e8a25014
Jackson Harper <jackson@novell.com>
2002-10-29 04:51:58 +0000

    FirstMatchCodeGroup.cs, Hash.cs: New classes, somewhat finished
    svn path=/trunk/mcs/; revision=8652

---
+++ mcs/class/corlib/System.Security.Policy/FirstMatchCodeGroup.cs
@@ -0,0 +1,105 @@
+// System.Security.Policy.FirstMatchCodeGroup
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+
+using System;
+
+namespace System.Security.Policy {
+
+	public class FirstMatchCodeGroup : CodeGroup {
+		
+		public FirstMatchCodeGroup(IMembershipCondition membershipCondition, PolicyStatement policy) :
+			base (membershipCondition, policy)
+		{
+		}
+
+		//
+		// Public Properties
+		//
+
+		public override string MergeLogic
+		{
+			get { return "First Match"; }
+		}
+
+		//
+		// Public Methods
+		//
+		
+		public override CodeGroup Copy()
+		{
+			FirstMatchCodeGroup copy = CopyNoChildren ();
+
+			foreach (CodeGroup group in Children) {
+				copy.AddChild ( group );
+			}
+
+			return copy;
+		}
+
+		public override PolicyStatement Resolve(Evidence evidence)
+		{
+			PolicyStatement policy = null;
+			PolicyStatement child_policy;
+
+			if (null == evidence)
+				throw new ArgumentNullException ();
+
+			if (MembershipCondition.Check (evidence)) {
+				if (null != PolicyStatement) {
+					policy = PolicyStatement;
+				} else {
+					// Loop through all children breaking on the first one that resolves
+					foreach (CodeGroup child in Children) {
+						if (null == (child_policy = child.Resolve (evidence)))
+							continue;
+						policy = child_policy;
+						break;
+					}
+				}
+			}
+			
+			return policy;
+		}
+		
+		public override CodeGroup ResolveMatchingCodeGroups(Evidence evidence)
+		{
+			CodeGroup group = null;
+
+			if (null == evidence)
+				throw new ArgumentNullException ();
+			
+			if (MembershipCondition.Check (evidence)) {
+				group = CopyNoChildren ();
+				
+				// Add the first child that resolves
+				foreach (CodeGroup child in Children) {
+					if ( null == child.Resolve (evidence))
+						continue;
+					group.AddChild (child);
+					break;
+				}
+			}
+
+			return group;
+		}
+	
+		//
+		// Private Methods
+		//
+	
+		private FirstMatchCodeGroup CopyNoChildren()
+		{
+			FirstMatchCodeGroup copy = new FirstMatchCodeGroup (MembershipCondition, PolicyStatement);
+
+			copy.Name = Name;
+			copy.Description = Description;
+
+			return copy;
+		}
+	}
+
+}
+
---
+++ mcs/class/corlib/System.Security.Policy/Hash.cs
@@ -0,0 +1,86 @@
+// System.Security.Policy.Hash
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+
+namespace System.Security.Policy {
+
+	[MonoTODO]
+	public sealed class Hash {
+		
+		private Assembly assembly;
+
+		public Hash(Assembly assembly)
+		{
+			this.assembly = assembly;
+		}
+
+		//
+		// Public Properties
+		//
+		
+		public byte[] MD5 
+		{
+			get {
+				return GenerateHash (new MD5CryptoServiceProvider ());
+			}
+		}
+
+		public byte[] SHA1
+		{
+			get {
+				return GenerateHash (SHA1CryptoServiceProvider ());
+			}
+		}
+
+		//
+		// Public Methods
+		//
+		
+		public byte[] GenerateHash(HashAlgorithm hashAlg)
+		{
+			return hashAlg.ComputeHash (GetData ());
+		}
+
+		[MonoTODO]
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder();
+
+			builder.Append ("<System.Security.Policy.Hash version=\"1\">");
+			builder.AppendFormat ("<RawData>{0}</RawData>", GetData ());
+			builder.Append ("</System.Security.Policy.Hash>");
+
+			return builder.ToString ();
+		}
+
+		//
+		// Private Methods
+		//
+
+		[MonoTODO("This doesn't match the MS version perfectly.")]
+		private byte[] GetData()
+		{
+			FileStream stream = new FileStream( assembly.Location, FileMode.Open, FileAccess.Read );
+			byte[] buf = new byte[stream.Length];
+
+			stream.Read( buf, 0, (int)stream.Length );
+
+			return buf;
+		}
+	}
+
+}
+
