d29f13f83c151e4edf5700a8b8a8ae0674f4545c
Jackson Harper <jackson@novell.com>
2002-10-30 15:39:25 +0000

    Added class
    svn path=/trunk/mcs/; revision=8680

---
+++ mcs/class/System/Microsoft.CSharp/CompilerError.cs
@@ -0,0 +1,114 @@
+// Microsoft.CSharp.CompilerError
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+// (C) 2001 Jackson Harper, All rights reserved.
+//
+
+using System;
+using System.Text;
+
+namespace Microsoft.CSharp {
+
+	public enum ErrorLevel {
+		Error,
+		FatalError,
+		None,
+		Warning
+	}
+
+	public class CompilerError {
+  
+		private ErrorLevel level = ErrorLevel.None;
+		private string message = null;
+		private string file = null;
+		private int number = 0;
+		private int column = 0;
+		private int line = 0;
+
+  		public CompilerError()
+    		{
+    		}
+
+		~CompilerError()
+		{
+			message = null;
+			file = null;
+		}
+
+
+		//
+		// Public Properties
+		//
+
+		public ErrorLevel ErrorLevel {
+			get { return level; }
+			set { level = value; }
+		}
+
+		public string ErrorMessage {
+			get {
+				return (null == message ? "" : message);
+			}
+			set {
+				message = value;
+			}
+		}
+
+		public int ErrorNumber {
+			get { return number; }
+			set { number = value; }
+		}
+
+		public int SourceColumn {
+			get { return column; }
+			set { column = value; }      
+		}
+
+		public string FileName {
+			get { 
+				return (null == file ? "" : file);
+			}
+			set { file = value; }
+  		}
+
+		public int SourceLine {
+			get { return line; }
+			set { line = value; }
+		}
+
+		//
+		// Public Methods
+		//
+
+		/// <summary>
+		///   Error message in form: 
+		///   [filename(line,column):] AAAAA CSXXX: message
+  		/// </summary>
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder ();
+
+			builder.AppendFormat ("[{0}({1},{2}):] ", 
+				FileName, SourceLine, SourceColumn);
+			builder.AppendFormat ("{0} CS{1}: {2}", 
+				ErrorLevelString, ErrorNumber, ErrorMessage);
+			
+			return builder.ToString ();
+		}
+
+		//
+		// Private Properties
+		//
+
+		private string ErrorLevelString {
+			get {
+				if (ErrorLevel.FatalError == ErrorLevel)
+					return "Error Fatal";
+				return ErrorLevel.ToString ();
+			}
+		}
+	}
+}
+
