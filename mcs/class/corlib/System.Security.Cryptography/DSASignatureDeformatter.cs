6d1f1618694423eff9378c17608408080a74a0a2
Sebastien Pouliot <sebastien@ximian.com>
2002-10-31 02:14:39 +0000

    2002-10-30  Sebastien Pouliot  <spouliot@videotron.ca>
    	* DSASignatureDeformatter.cs: Fully implemented - however it
    	requires a functionnal DSA implementation to work.
    	* DSASignatureFormatter.cs: Fully implemented - however it
    	requires a functionnal DSA implementation to work.
    
    svn path=/trunk/mcs/; revision=8688

--- mcs/class/corlib/System.Security.Cryptography/DSASignatureDeformatter.cs
+++ mcs/class/corlib/System.Security.Cryptography/DSASignatureDeformatter.cs
@@ -1,51 +1,62 @@
 //
 // System.Security.Cryptography DSASignatureDeformatter.cs
 //
-// Author:
-//   Thomas Neidhart (tome@sbox.tugraz.at)
+// Authors:
+//	Thomas Neidhart (tome@sbox.tugraz.at)
+//	Sebastien Pouliot (spouliot@motus.com)
+//
+// Portions (C) 2002 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System;
 
-namespace System.Security.Cryptography
-{
+namespace System.Security.Cryptography {
 
-	/// <summary>
-	/// DSA Signature Deformatter
-	/// </summary>
-	public class DSASignatureDeformatter : AsymmetricSignatureDeformatter
+/// <summary>
+/// DSA Signature Deformatter
+/// </summary>
+public class DSASignatureDeformatter : AsymmetricSignatureDeformatter {
+	
+	private DSA dsa;
+	
+	public DSASignatureDeformatter () {}
+
+	public DSASignatureDeformatter (AsymmetricAlgorithm key)
 	{
-		[MonoTODO]
-		public DSASignatureDeformatter()
-		{
-			// TODO: implement
-		}
+		SetKey (key);
+	}
 
-		[MonoTODO]
-		public DSASignatureDeformatter(AsymmetricAlgorithm key)
-		{
-			// TODO: implement
-		}
+	public override void SetHashAlgorithm (string strName)
+	{
+		if (strName == null)
+			throw new ArgumentNullException ("strName");
 
-		
-		public override void SetHashAlgorithm(string strName)
-		{
-			throw new CryptographicException("This method is not used");
+		try {
+			// just to test, we don't need the object
+			SHA1 hash = SHA1.Create (strName);
 		}
+		catch {
+			throw new CryptographicUnexpectedOperationException ("DSA requires SHA1");
+		}
+	}
 
-		[MonoTODO]
-		public override void SetKey(AsymmetricAlgorithm key)
-		{
-			// TODO: implement
+	public override void SetKey (AsymmetricAlgorithm key)
+	{
+		if (key != null) {
+			// this will throw a InvalidCastException if this isn't
+			// a DSA keypair
+			dsa = (DSA) key;
 		}
+		// here null is accepted!
+	}
 
-		[MonoTODO]
-		public override bool VerifySignature(byte[] rgbHash, byte[] rgbSignature)
-		{
-			// TODO: implement
-			return false;
-		}
-		
-	} // DSASignatureDeformatter
+	public override bool VerifySignature (byte[] rgbHash, byte[] rgbSignature)
+	{
+		if (dsa == null)
+			throw new CryptographicUnexpectedOperationException ("missing key");
+		return dsa.VerifySignature (rgbHash, rgbSignature);
+	}
+	
+} // DSASignatureDeformatter
 	
 } // System.Security.Cryptography
--- mcs/class/corlib/System.Security.Cryptography/DSASignatureFormatter.cs
+++ mcs/class/corlib/System.Security.Cryptography/DSASignatureFormatter.cs
@@ -1,52 +1,62 @@
 //
 // System.Security.Cryptography DSASignatureFormatter.cs
 //
-// Author:
-//   Thomas Neidhart (tome@sbox.tugraz.at)
+// Authors:
+//	Thomas Neidhart (tome@sbox.tugraz.at)
+//	Sebastien Pouliot (spouliot@motus.com)
+//
+// Portions (C) 2002 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System;
 
-namespace System.Security.Cryptography
-{
+namespace System.Security.Cryptography {
 
-	/// <summary>
-	/// DSA Signature Formatter
-	/// </summary>
-	public class DSASignatureFormatter : AsymmetricSignatureFormatter
+/// <summary>
+/// DSA Signature Formatter
+/// </summary>
+public class DSASignatureFormatter : AsymmetricSignatureFormatter {
+	
+	private DSA dsa;
+
+	public DSASignatureFormatter () {}
+
+	public DSASignatureFormatter (AsymmetricAlgorithm key)
 	{
+		SetKey (key);
+	}
 
-		[MonoTODO]
-		public DSASignatureFormatter() 
-		{
-			// TODO: implement
-		}
+	public override byte[] CreateSignature (byte[] rgbHash)
+	{
+		if (dsa == null)
+			throw new CryptographicUnexpectedOperationException ("missing key");
+		return dsa.CreateSignature (rgbHash);
+	}
 
-		[MonoTODO]
-		public DSASignatureFormatter(AsymmetricAlgorithm key)
-		{
-			// TODO: implement
-		}
+	public override void SetHashAlgorithm (string strName)
+	{
+		if (strName == null)
+			throw new ArgumentNullException ("strName");
 
-		[MonoTODO]
-		public override byte[] CreateSignature(byte[] rgbHash)
-		{
-			// TODO: implement
-			return null;
+		try {
+			// just to test, we don't need the object
+			SHA1 hash = SHA1.Create (strName);
 		}
+		catch {
+			throw new CryptographicUnexpectedOperationException ("DSA requires SHA1");
+		}
+	}
 
-		[MonoTODO]
-		public override void SetHashAlgorithm(string strName)
-		{
-			// TODO: implement
+	public override void SetKey (AsymmetricAlgorithm key)
+	{
+		if (key != null) {
+			// this will throw a InvalidCastException if this isn't
+			// a DSA keypair
+			dsa = (DSA) key;
 		}
-
-		[MonoTODO]
-		public override void SetKey(AsymmetricAlgorithm key)
-		{
-			// TODO: implement
-		}
-		
-	} // DSASignatureFormatter
+		// here null is accepted!
+	}
+	
+} // DSASignatureFormatter
 	
 } // System.Security.Cryptography
