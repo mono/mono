148b8a1428367299627699828d3074b6e718e767
Tim Coleman <tim@mono-cvs.ximian.com>
2002-11-01 01:37:53 +0000

    2002-10-31  Tim Coleman (tim@timcoleman.com)         * TdsDataReader.cs:                 D'oh!  Now use TdsSchemaInfo because SchemaInfo                 is internal to System.Data.dll
    svn path=/trunk/mcs/; revision=8720

--- mcs/class/Mono.Data.TdsClient/Mono.Data.TdsClient/TdsDataReader.cs
+++ mcs/class/Mono.Data.TdsClient/Mono.Data.TdsClient/TdsDataReader.cs
@@ -251,13 +251,13 @@ namespace Mono.Data.TdsClient {
 				return schemaTable;
 			fieldCount = 0;
 
-			foreach (SchemaInfo schemaObject in command.Tds.Schema) {
+			foreach (TdsSchemaInfo schemaObject in command.Tds.Schema) {
 				DataRow schemaRow = schemaTable.NewRow ();
 				schemaRow ["ColumnName"] = schemaObject.ColumnName;
 				schemaRow ["ColumnOrdinal"] = schemaObject.ColumnOrdinal;
 				schemaRow ["BaseTableName"] = schemaObject.TableName;
-				schemaRow ["AllowDBNull"] = schemaObject.Nullable;
-				schemaRow ["IsReadOnly"] = !schemaObject.Writable;
+				schemaRow ["AllowDBNull"] = schemaObject.AllowDBNull;
+				schemaRow ["IsReadOnly"] = schemaObject.IsReadOnly;
 				schemaTable.Rows.Add (schemaRow);
 				fieldCount += 1;
 			}
