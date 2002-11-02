7b2e93e576dedadc8d67916716f7cf69c5d421cf
Miguel de Icaza <miguel@gnome.org>
2002-11-02 20:09:52 +0000

    Add Accessibility stuff
    svn path=/trunk/mcs/; revision=8758

---
+++ mcs/class/Accessibility/Accessibility/IAccessible.cs
@@ -0,0 +1,10 @@
+//
+// IAccessible.cs: Interface for accessible elements
+//
+
+namespace Accessibility {
+	
+	public interface IAccessible {
+		// Opaque for now.
+	}
+}
---
+++ mcs/class/Accessibility/Accessibility/IAccessibleHandler.cs
@@ -0,0 +1,3 @@
+interface IAccessibleHandler {
+	// Opaque for now.
+}
