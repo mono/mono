b16779770d3967dfa3603e8686dcf8c12b3f3ed7
Miguel de Icaza <miguel@gnome.org>
2002-11-02 21:11:55 +0000

    Update
    svn path=/trunk/mcs/; revision=8774

--- mcs/class/Accessibility/Accessibility/IAccessible.cs
+++ mcs/class/Accessibility/Accessibility/IAccessible.cs
@@ -1,10 +1,35 @@
 //
 // IAccessible.cs: Interface for accessible elements
 //
+// What a horrible interface.
+//
 
 namespace Accessibility {
 	
 	public interface IAccessible {
-		// Opaque for now.
+		
+		void   accDoDefaultAction (object childID);
+		object accHitTest (int xLeft, int yTop);
+		void   accLocation (out int pxLeft, out int pyTop, out int pcxWidth, out int pcyHeight, object childID);
+		object accNavigate(int navDir, object childID);
+		void   accSelect(int flagsSelect, object childID);
+		object get_accChild(object childID);
+		string get_accDefaultAction(object childID);
+		string get_accDescription(object childID);
+		string get_accHelp(object childID);
+		int    get_accHelpTopic(out string pszHelpFile,object childID);
+		string get_accKeyboardShortcut(object childID);
+		string get_accName(object childID);
+		object get_accRole(object childID);
+		object get_accState(object childID);
+		string get_accValue(object childID);
+		void   set_accName(object childID, string newName);
+		void   set_accValue(object childID, string newValue);
+
+
+		int    accChildCount { get; }
+		object accFocus { get; }
+		object accParent { get;}
+		object accSelection { get; } 
 	}
 }
