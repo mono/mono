120c054725db233f5658bffd732d98185f553b66
Dennis Hayes <dennis@mono-cvs.ximian.com>
2002-11-02 02:16:45 +0000

    stub for picturebox.cs - last unstubbed function in swf
    svn path=/trunk/mcs/; revision=8742

---
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/PictureBox.cs
@@ -0,0 +1,136 @@
+//
+// System.Windows.Forms.PicureBox
+//
+// Author:
+//   stubbed out by Hossein Safavi (hsafavi@purdue.edu)
+//	Dennis Hayes (dennish@raytek.com)
+//
+// (C) 2002 Ximian, Inc
+//
+
+namespace System.Windows.Forms {
+
+	//<summary>
+	//	Skeleton. Nothing is implemented
+	//
+	//</summary>
+	using System.ComponentModel {
+
+
+		//
+		// --- Public Constructor
+		//
+		[MONOTODO]
+		public PictureBox () 
+		{
+			throw new NotImplementedException ();
+		}
+		//
+		// --- Public Properties
+		//
+		[MONOTODO]
+		public Image Image {
+			get {
+				throw new NotImplementedException();
+			}
+			set {
+				throw new NotImplementedException();
+			}
+		}
+		public new ImeMode ImeMode {
+			get {
+				throw new NotImplementedException();
+			}
+			set {
+				throw new NotImplementedException();
+			}
+		}
+		//
+		// --- Protected Properties
+		//
+		[MONOTODO]
+		protected override CreateParams CreateParams {
+			get {
+				throw new NotImplementedException();
+			}
+		}
+		[MONOTODO]
+		protected override ImeMode DefaultImeMode {
+			get {
+				throw new NotImplementedException();
+			}
+		}
+		[MONOTODO]
+		protected override Size DefaultSize {
+			get {
+				throw new NotImplementedException();
+			}
+		}
+		//
+		// --- Public Methods
+		//
+		[MONOTODO]
+		public override string ToString()
+		{
+			throw new NotImplementedException();
+		}
+		//
+		// --- Protected Methods
+		//
+		[MONOTODO]
+		protected override void Dispose(bool disposing) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void OnEnabledChanged(EventArgs e) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void OnPaint(PaintEventArgs pe) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void OnParentChanged(EventArgs e) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void OnResize(EventArgs e) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected virtual void OnSizeModeChanged(EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void OnVisibleChanged(EventArgs e) 
+		{
+			throw new NotImplementedException();
+		}
+		[MONOTODO]
+		protected override void SetBoundsCore(int x,int y,int width,int height,BoundsSpecified specified) 
+		{
+			throw new NotImplementedException();
+		}
+		//
+		// --- Public Events
+		//
+		[MONOTODO]
+		public new event EventHandler Enter;
+		[MONOTODO]
+		public new event KeyEventHandler KeyDown;
+		[MONOTODO]
+		public new event KeyPressEventHandler KeyPress;
+		[MONOTODO]
+		public new event KeyEventHandler KeyUp;
+		[MONOTODO]
+		public new event EventHandler Leave;
+		[MONOTODO]
+		public event EventHandler SizeModeChanged;
+	}
+}
