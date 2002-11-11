33a0d2930fbb62fa73176b06946425c4864c42fd
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-11-11 18:37:18 +0000

    2002-11-11  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* Timer.cs: implemented more stuff. It works now.
    
    svn path=/trunk/mcs/; revision=8921

--- mcs/class/corlib/System.Threading/Timer.cs
+++ mcs/class/corlib/System.Threading/Timer.cs
@@ -1,10 +1,11 @@
 //
 // System.Threading.Timer.cs
 //
-// Author:
-//   Dick Porter (dick@ximian.com)
+// Authors:
+// 	Dick Porter (dick@ximian.com)
+// 	Gonzalo Paniagua Javier (gonzalo@ximian.com)
 //
-// (C) Ximian, Inc.  http://www.ximian.com
+// (C) 2001, 2002 Ximian, Inc.  http://www.ximian.com
 //
 
 
@@ -12,101 +13,198 @@ namespace System.Threading
 {
 	public sealed class Timer : MarshalByRefObject, IDisposable
 	{
-		[MonoTODO]
-		public Timer(TimerCallback callback, object state, int dueTime, int period) {
-			if(dueTime < -1) {
-				throw new ArgumentOutOfRangeException("Due time < -1");
+		sealed class Runner : MarshalByRefObject, IDisposable
+		{
+			ManualResetEvent wait;
+			TimerCallback callback;
+			object state;
+			int dueTime;
+			int period;
+			bool disposed;
+
+			public Runner (TimerCallback callback, object state)
+			{
+				this.callback = callback;
+				this.state = state;
 			}
-			if(period < -1) {
-				throw new ArgumentOutOfRangeException("Period < -1");
+
+			public int DueTime {
+				get { return dueTime; }
+				set { dueTime = value; }
 			}
-			
-			// FIXME
+
+			public int Period {
+				get { return period; }
+				set { period = value; }
+			}
+
+			bool WaitForDueTime ()
+			{
+				if (dueTime > 0) {
+					bool signaled;
+					do {
+						wait.Reset ();
+						signaled = wait.WaitOne (dueTime, false);
+					} while (signaled == true && !disposed);
+
+					if (disposed)
+						return false;
+				}
+
+				return true;
+			}
+
+			public void Start ()
+			{
+				if (dueTime == Timeout.Infinite)
+					return;
+
+				wait = new ManualResetEvent (false);
+				if (!WaitForDueTime ())
+					return;
+
+				bool signaled = false;
+				while (true) {
+					signaled = wait.WaitOne (period, false);
+					if (disposed)
+						break;
+
+					if (!signaled) {
+						callback (state);
+					} else if (!WaitForDueTime ()) {
+							return;
+					}
+				}
+			}
+
+			public void Dispose ()
+			{
+				Dispose (true);
+			}
+
+			void Dispose (bool disposing)
+			{
+				disposed = true;
+				if (wait != null) {
+					wait.Set ();
+					Thread.Sleep (100);
+					((IDisposable) wait).Dispose ();
+					wait = null;
+				}
+
+				if (disposing)
+					GC.SuppressFinalize (this);
+			}
+
+			~Runner ()
+			{
+				Dispose (false);
+			}
 		}
 
-		[MonoTODO]
-		public Timer(TimerCallback callback, object state, long dueTime, long period) {
-			if(dueTime < -1) {
-				throw new ArgumentOutOfRangeException("Due time < -1");
-			}
-			if(period < -1) {
-				throw new ArgumentOutOfRangeException("Period < -1");
-			}
-			// FIXME
+		Runner runner;
+
+		public Timer (TimerCallback callback, object state, int dueTime, int period)
+		{
+			if (dueTime < -1)
+				throw new ArgumentOutOfRangeException ("dueTime");
+
+			if (period < -1)
+				throw new ArgumentOutOfRangeException ("period");
+
+			Init (callback, state, dueTime, period);
 		}
 
-		[MonoTODO]
-		public Timer(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period) {
-			if(dueTime.Milliseconds < 0 || dueTime.Milliseconds > Int32.MaxValue) {
-				throw new ArgumentOutOfRangeException("Due time out of range");
-			}
-			if(period.Milliseconds < 0 || period.Milliseconds > Int32.MaxValue) {
-				throw new ArgumentOutOfRangeException("Period out of range");
-			}
-			// FIXME
+		public Timer (TimerCallback callback, object state, long dueTime, long period)
+		{
+			if (dueTime < -1)
+				throw new ArgumentOutOfRangeException ("dueTime");
+
+			if (period < -1)
+				throw new ArgumentOutOfRangeException ("period");
+
+			Init (callback, state, (int) dueTime, (int) period);
 		}
 
-		[CLSCompliant(false)][MonoTODO]
-		public Timer(TimerCallback callback, object state, uint dueTime, uint period) {
-			// FIXME
+		public Timer (TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+			: this (callback, state, dueTime.Milliseconds, period.Milliseconds)
+		{
 		}
 
-		[MonoTODO]
-		public bool Change(int dueTime, int period) {
-			if(dueTime < -1) {
-				throw new ArgumentOutOfRangeException("Due time < -1");
-			}
-			if(period < -1) {
-				throw new ArgumentOutOfRangeException("Period < -1");
-			}
-			// FIXME
-			return(false);
+		[CLSCompliant(false)]
+		public Timer (TimerCallback callback, object state, uint dueTime, uint period)
+			: this (callback, state, (long) dueTime, (long) period)
+		{
 		}
 
-		[MonoTODO]
-		public bool Change(long dueTime, long period) {
-			if(dueTime < -1) {
-				throw new ArgumentOutOfRangeException("Due time < -1");
-			}
-			if(period < -1) {
-				throw new ArgumentOutOfRangeException("Period < -1");
-			}
-			if(dueTime > 4294967294) {
-				throw new NotSupportedException("Due time too large");
-			}
-			if(period > 4294967294) {
-				throw new NotSupportedException("Period too large");
-			}
-			// FIXME
-			return(false);
+		void Init (TimerCallback callback, object state, int dueTime, int period)
+		{
+			runner = new Runner (callback, state);
+			Change (dueTime, period);
+			Thread t = new Thread (new ThreadStart (runner.Start));
+			t.Start ();
 		}
 
-		[MonoTODO]
-		public bool Change(TimeSpan dueTime, TimeSpan period) {
-			// FIXME
-			return(false);
+		[MonoTODO("false return?")]
+		public bool Change (int dueTime, int period)
+		{
+			if (dueTime < -1)
+				throw new ArgumentOutOfRangeException ("dueTime");
+
+			if (period < -1)
+				throw new ArgumentOutOfRangeException ("period");
+
+			runner.DueTime = dueTime;
+			runner.Period = period;
+			return true;
 		}
 
-		[CLSCompliant(false)][MonoTODO]
-		public bool Change(uint dueTime, uint period) {
-			// FIXME
-			return(false);
+		public bool Change (long dueTime, long period)
+		{
+			if(dueTime > 4294967294)
+				throw new NotSupportedException ("Due time too large");
+
+			if(period > 4294967294)
+				throw new NotSupportedException ("Period too large");
+
+			return Change ((int) dueTime, (int) period);
 		}
 
-		[MonoTODO]
-		public void Dispose() {
-			// FIXME
+		public bool Change (TimeSpan dueTime, TimeSpan period)
+		{
+			return Change (dueTime.Milliseconds, period.Milliseconds);
 		}
 
-		[MonoTODO]
-		public bool Dispose(WaitHandle notifyObject) {
-			// FIXME
-			return(false);
+		[CLSCompliant(false)]
+		public bool Change (uint dueTime, uint period)
+		{
+			if (dueTime > Int32.MaxValue)
+				throw new NotSupportedException ("Due time too large");
+
+			if (period > Int32.MaxValue)
+				throw new NotSupportedException ("Period too large");
+
+			return Change ((int) dueTime, (int) period);
 		}
 
-		[MonoTODO]
-		~Timer() {
-			// FIXME
+		public void Dispose ()
+		{
+			runner.Dispose ();
+			runner = null;
+			GC.SuppressFinalize (this);
+		}
+
+		[MonoTODO("How do we signal the handler?")]
+		public bool Dispose (WaitHandle notifyObject)
+		{
+			Dispose ();
+			return true; //FIXME
+		}
+
+		~Timer ()
+		{
+			runner = null;
 		}
 	}
 }
+
