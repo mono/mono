6c005235b31ddd06179ca2837dd89ca26b3f756f
Martin Baulig <martin@novell.com>
2002-11-14 23:03:45 +0000

    2002-11-14  Martin Baulig  <martin@ximian.com>
    	* class.cs (Constructor.Emit): Added the extern/block check.
    	Fixes bug #33678.
    
    svn path=/trunk/mcs/; revision=8990

--- mcs/mcs/class.cs
+++ mcs/mcs/class.cs
@@ -2599,6 +2599,37 @@ namespace Mono.CSharp {
 			ILGenerator ig = ConstructorBuilder.GetILGenerator ();
 			EmitContext ec = new EmitContext (parent, Location, ig, null, ModFlags, true);
 
+			//
+			// abstract or extern methods have no bodies
+			//
+			if ((ModFlags & Modifiers.EXTERN) != 0){
+				if (block == null)
+					return;
+
+				//
+				// extern methods have no bodies.
+				//
+				if ((ModFlags & Modifiers.EXTERN) != 0)
+					Report.Error (
+						179, Location, "External constructor `" +
+						TypeManager.CSharpSignature (ConstructorBuilder) +
+						"' can not have a body");
+
+				return;
+			}
+
+			//
+			// Methods must have a body unless they're extern
+			//
+			if (block == null) {
+				Report.Error (
+					501, Location, "Constructor `" +
+					TypeManager.CSharpSignature (ConstructorBuilder) +
+					"' must declare a body since it is not marked extern");
+				return;
+			}
+
+
 			if ((ModFlags & Modifiers.STATIC) == 0){
 				if (parent is Class && Initializer == null)
 					Initializer = new ConstructorBaseInitializer (
