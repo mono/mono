e38d6661e86951b3aca2699141cbf87f3ca3a124
Alejandro Sánchez Acosta <asanchez@mono-cvs.ximian.com>
2002-11-19 14:16:35 +0000

    System.ComponentModel.DesignerAttribute.cs Added: Alejandro S�nchez Acosta <raciel@es.gnu.org>
    svn path=/trunk/mcs/; revision=9074

---
+++ mcs/class/System/System.ComponentModel/DesignerAttribute.cs
@@ -0,0 +1,93 @@
+//
+// System.ComponentModel.DesignerAttribute.cs
+//
+// Author:
+//   Alejandro Sánchez Acosta (raciel@es.gnu.org)
+//
+// (C) Alejandro Sánchez Acosta
+//
+
+namespace System.ComponentModel {
+
+	/// <summary>
+	///   Designer Attribute for classes. 
+	/// </summary>
+	
+	/// <remarks>
+	/// </remarks>
+	
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+		public sealed class DesignerAttribute : Attribute
+		{
+			string name;
+			string basetypename;
+			Type type;
+			Type basetype;
+			
+			public DesignerAttribute (string designerTypeName)
+			{
+				name  = designerTypeName;
+			}
+
+			public DesignerAttribute(Type designerType)
+			{
+				type = designerType;
+			}
+
+			public DesignerAttribute(string designerTypeName, string designerBaseTypeName)
+			{
+				name = designerTypeName;
+				basetypename = designerBaseTypeName;
+			}
+
+			public DesignerAttribute(string designerTypeName, Type designerBaseType)
+			{
+				name = designerTypeName;
+				basetype = designerBaseType;
+			}
+
+			public DesignerAttribute(Type designerType, Type designerBaseType)
+			{
+				type = designerType;
+				basetype = designerBaseType;
+			}
+
+			public string DesignerBaseTypeName 
+			{
+				get
+				{
+					return basetype;
+				}
+			}
+
+			public string DesignerTypeName 
+			{
+				get
+				{
+					return name;
+				}
+			}
+
+			public override object TypeId 
+			{
+				get 
+				{
+					return this.GetType();
+				}
+			}
+			
+			public override bool Equals(object obj)
+			{
+	                        if (!(o is DesignerAttribute))
+	                                return false;
+	                        return (((DesignerAttribute) o).name == name) && 
+					(((DesignerAttribute) o).basetype == basetype) &&
+					(((DesignerAttribute) o).type == type) &&
+					(((DesignerAttribute) o).basetypename == basetypename);
+			}				
+
+			public override int GetHashCode()
+			{
+				return base.GetHashCode ();
+			}
+}
