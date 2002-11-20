8d573bf64063c5691844828e4b5d9d40210e1bde
Alejandro Sánchez Acosta <asanchez@mono-cvs.ximian.com>
2002-11-20 21:20:46 +0000

    IExpando added.
    svn path=/trunk/mcs/; revision=9118

---
+++ mcs/class/corlib/System.Runtime.InteropServices.Expando/IExpando.cs
@@ -0,0 +1,26 @@
+//
+// System.Runtime.InteropServices.Expando.IExpando.cs
+//
+// Author:
+//    Alejandro Sánchez Acosta (raciel@es.gnu.org)
+// 
+// (C) Alejandro Sánchez Acosta
+// 
+
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace System.Runtime.InteropServices.Expando
+{
+	[Guid("")]
+	public interface IExpando : IReflect
+	{
+		FieldInfo AddField (string name);
+
+		MethodInfo AddMethod (string name, Delegate method);
+
+		PropertyInfo AddProperty(string name);
+
+		void RemoveMember(MemberInfo m);
+	}
+}
