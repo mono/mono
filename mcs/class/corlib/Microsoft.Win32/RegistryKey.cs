4ab843536c16e3751759508f70a5fc31b649100a
Miguel de Icaza <miguel@gnome.org>
2002-11-20 02:41:17 +0000

    Add new file, used for Windows.Forms compilation
    svn path=/trunk/mcs/; revision=9107

---
+++ mcs/class/corlib/Microsoft.Win32/RegistryKey.cs
@@ -0,0 +1,55 @@
+//
+// RegistryKey.cs: a single node in the Windows registry
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+
+using System;
+
+namespace Microsoft.Win32 {
+	
+	public sealed class RegistryKey : MarshalByRefObject, IDisposable {
+
+		void IDisposable.Dispose ()
+		{
+		}
+
+		[MonoTODO]
+		public void SetValue (string name, object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public RegistryKey OpenSubKey (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public RegistryKey OpenSubKey (string name, bool writtable)
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public object GetValue (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public object Getvalue (string name, object defaultvalue)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public RegistryKey CreateSubKey (string subkey)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
