c1a9681be58745203bae2443cd0372336ccfa2f3
Sebastien Pouliot <sebastien@ximian.com>
2002-11-21 00:50:50 +0000

    2002-11-20  Sebastien Pouliot  <spouliot@videotron.ca>
    	* RSAPKCS1SignatureDeformatterTest.cs: Removed patch from Nick -
    	as it doesn't seems to work on my system and the exception behavior
    	isn't logical (only MD5 not SHA1, ...).
    
    svn path=/trunk/mcs/; revision=9123

--- mcs/class/corlib/Test/System.Security.Cryptography/RSAPKCS1SignatureDeformatterTest.cs
+++ mcs/class/corlib/Test/System.Security.Cryptography/RSAPKCS1SignatureDeformatterTest.cs
@@ -285,14 +285,7 @@ public class RSAPKCS1SignatureDeformatterTest : TestCase {
 
 		// bad signature
 		md5Signature [0] = (byte) ~md5Signature [0];
-		try {
-		    fmt.VerifySignature (hash, md5Signature);
-		    Fail ("Should have thrown at VerifySignature(MD5, badSign)");
-		} catch (CryptographicException) {
-			// do nothing, this is what we expect
-		} catch (Exception e) {
-			Fail ("Unexpected exception thrown at VerifySignature(MD5, badSign). e=" + e);
-		}
+		Assert ("VerifySignature(MD5, badSign)", !fmt.VerifySignature (hash, md5Signature));
 
 		// wrong signature length
 		md5Signature = new byte [md5Signature.Length-1];
