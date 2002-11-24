80358cd0401aef1610e6a7becae99fad2e80edd8
Martin Baulig <martin@novell.com>
2002-11-24 18:30:01 +0000

    2002-11-24  Martin Baulig  <martin@ximian.com>
    	* verify-6.cs: New test for bug #32269.
    
    svn path=/trunk/mcs/; revision=9171

---
+++ mcs/tests/verify-6.cs
@@ -0,0 +1,24 @@
+using System;
+
+class X
+{
+	enum Foo {
+		A, B
+	}
+
+	enum Bar {
+		C, D
+	}
+
+	public static void Main ()
+	{
+		Foo foo = Foo.A;
+		Enum se = (Enum) foo;
+		Enum sc = (Enum) Foo.A;
+		object obj1 = (object) foo;
+		object obj2 = (object) Foo.A;
+
+		Bar bar = (Bar) se;
+		Foo blah = (Foo) obj1;
+	}
+}
