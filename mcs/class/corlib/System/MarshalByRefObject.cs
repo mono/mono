043561857c11156e7a0c8a87859b94218acccece
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2002-11-29 19:26:11 +0000

    2002-11-29  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* MarshalByRefObject.cs: undo latest changes. It breaks the build by
    	some obscure reasons (try make -f makefile.gnu using a corlib which has
    	the modified version).
    
    svn path=/trunk/mcs/; revision=9263

--- mcs/class/corlib/System/MarshalByRefObject.cs
+++ mcs/class/corlib/System/MarshalByRefObject.cs
@@ -3,49 +3,32 @@
 //
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
-//   Jaime Anguiano Olarra (jaime@gnome.org)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
 //
 
 using System.Runtime.Remoting;
-using System.Runtime.Remoting.Lifetime;
 
 namespace System {
 
 	[Serializable]
 	public abstract class MarshalByRefObject {
-		private object life_ctrl;
-		private ILease ilife_ctrl;
 
-		[MonoTODO]
 		public virtual ObjRef CreateObjRef (Type type)
 		{
 			throw new NotImplementedException ();
 		}
 
-		// LAME SPEC??: It does not say what happens if there is not a current lifetime
-		// server object. Should we return null or throw an exception?.
 		public object GetLifetimeService ()
 		{
-			return ilife_ctrl;
+			return null;
 		}
 
-		[MonoTODO]
 		public virtual object InitializeLifetimeService ()
 		{
-			life_ctrl = new object ();
-			ilife_ctrl = life_ctrl as ILease;
-			
-			if (ilife_ctrl.CurrentState == LeaseState.Initial) 
-				ilife_ctrl.InitialLeaseTime = new TimeSpan ((long) 0);
-			else 
-				ilife_ctrl.InitialLeaseTime = LifetimeServices.LeaseManagerPollTime;
-
-			return ilife_ctrl;
+			return null;
 		}
 
-		[MonoTODO]
 		protected MarshalByRefObject ()
 		{
 		}
