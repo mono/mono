b6cc22a0903088ca028f05e409628199cdde7248
Jaime Anguiano Olarra <jaime@mono-cvs.ximian.com>
2002-12-01 13:06:28 +0000

    Test for C# Compiler error CS0036. Attribute [In] can't accompany an out parameter.
    svn path=/trunk/mcs/; revision=9311

---
+++ mcs/errors/cs0036.cs
@@ -0,0 +1,20 @@
+// cs0036.cs: Attibute [In] cannot be accompaning the an 'out' parameter.
+// Line:
+
+using System;
+using System.Runtime.InteropServices;
+
+class ErrorCS0036 {
+	int i;
+
+	static void SetInteger ([In] out int i) {
+		i = 10;
+	}
+
+	public static void Main () {
+		int x;
+		SetInteger (out x);
+		Console.WriteLine ("The compiler should say: ErrorCS0036: {0}", x);
+	}
+}
+
