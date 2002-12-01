c57f10190e329bf2e3d2088735ba1d272fc8c51c
Jackson Harper <jackson@novell.com>
2002-12-01 07:01:58 +0000

    Added stub
    svn path=/trunk/mcs/; revision=9306

---
+++ mcs/class/System/System.CodeDom.Compiler/CodeCompiler.cs
@@ -0,0 +1,142 @@
+//
+// System.CodeDom.Compiler.CodeCompiler
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+// (C) 2002 Jackson Harper, All rights reserved
+//
+
+namespace System.CodeDom.Compiler {
+
+	public abstract class CodeCompiler : CodeGenerator, ICodeCompiler
+	{
+
+		[MonoTODO]
+		protected CodeCompiler ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected abstract string CompilerName {
+			get;
+		}
+	
+		[MonoTODO]
+		protected abstract string FileExtension {
+			get;
+		}
+
+		[MonoTODO]
+		protected abstract string CmdArgsFromParameters (
+			CompilerParameters options);
+
+		[MonoTODO]
+		protected virtual CompilerResults FromDom (
+			CompilerParameters options, CodeCompileUnit e)
+		{
+			throw new NotImplementedException ();
+		}
+	
+		[MonoTODO]
+		protected virtual CompilerResults FromDomBatch(
+			CompilerParameters options,CodeCompileUnit[] ea)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected virtual CompilerResults FromFile(
+			CompilerParameters options,string fileName)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected virtual CompilerResults FromFileBatch(
+			CompilerParameters options,string[] fileNames)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected virtual CompilerResults FromSource(
+			CompilerParameters options,string source)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected virtual CompilerResults FromSourceBatch(
+			CompilerParameters options,string[] sources)
+		{
+			throw new NotImplementedException ();
+		}
+
+
+		[MonoTODO]
+		protected virtual string GetResponseFileCmdArgs(
+			CompilerParameters options,string cmdArgs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromDom(
+			CompilerParameters options,CodeCompileUnit e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromDomBatch(
+			CompilerParameters options,CodeCompileUnit[] ea)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromFile(
+			CompilerParameters options, string fileName)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromFileBatch(
+			CompilerParameters options, string[] fileNames)
+		{
+			throw new NotImplementedException ();
+		}
+
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromSource(
+			CompilerParameters options, string source)
+		{
+			throw new NotImplementedException ();
+		}
+
+
+		[MonoTODO]
+		CompilerResults ICodeCompiler.CompileAssemblyFromSourceBatch(
+			CompilerParameters options, string[] sources)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected static string JoinStringArray(string[] sa, 
+			string separator)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		protected abstract void ProcessCompilerOutputLine(
+			CompilerResults results, string line);
+
+	}
+}
+
