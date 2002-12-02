283bc9387af3b02ae75ac26af77eb2a19393f8f0
Jaime Anguiano Olarra <jaime@mono-cvs.ximian.com>
2002-12-02 17:06:22 +0000

    Test for C# Compiler ErrorCS0059. Inconsisten accessibility. Parameter type less accessible than delegate.
    svn path=/trunk/mcs/; revision=9350

---
+++ mcs/errors/cs0059.cs
@@ -0,0 +1,15 @@
+// cs0059.cs: Iconsisten accessibility. Parameter type less accessible than delegate.
+// Line: 10
+
+using System;
+
+class ErrorCS0059 {
+}
+
+class Foo {
+	public delegate void ErrorCS0059Delegate (ErrorCS0059 e);
+
+	public static void Main () {
+	}
+}
+
