1e0036634e465abb26e3da19186fb49a0372535b
Jaime Anguiano Olarra <jaime@mono-cvs.ximian.com>
2002-12-03 14:47:45 +0000

    Test for C# Compiler Error CS0071. NOTE: Very strange behavior. See later Bugzilla report.
    svn path=/trunk/mcs/; revision=9366

---
+++ mcs/errors/cs0071.cs
@@ -0,0 +1,17 @@
+// cs0071.cs: An explicit implementation of an event must use property syntax.
+// Line: 13
+
+using System;
+
+public delegate void Foo (object source);
+
+interface IFoo {
+	event Foo OnFoo;
+}
+	
+class ErrorCS0071 : IFoo {
+	event Foo IFoo.OnFoo () { }
+	public static void Main () {
+	}
+}
+
