cec172518bfa3a7ea5379bc543183a8104e0018d
Sebastien Pouliot <sebastien@ximian.com>
2002-12-08 20:59:17 +0000

    2002-12-07  Sebastien Pouliot <spouliot@videotron.ca>
    	* AssemblyName.cs: Added missing methods/interfaces. Fixed some
    	code to match the MS Framework.
    	* StrongNameKeyPair.cs: Completed the "visible" implementation.
    	There must be some internal methods to allow signing with the key.
    
    svn path=/trunk/mcs/; revision=9453

--- mcs/class/corlib/System.Reflection/AssemblyName.cs
+++ mcs/class/corlib/System.Reflection/AssemblyName.cs
@@ -1,35 +1,45 @@
-
 //
 // System.Reflection/AssemblyName.cs
 //
-// Author:
-//   Paolo Molaro (lupus@ximian.com)
+// Authors:
+//	Paolo Molaro (lupus@ximian.com)
+//	Sebastien Pouliot (spouliot@motus.com)
 //
 // (C) 2001 Ximian, Inc.  http://www.ximian.com
+// Portions (C) 2002 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System;
-using System.Reflection;
-using System.Globalization;
 using System.Configuration.Assemblies;
+using System.Globalization;
+using System.Reflection;
 using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace System.Reflection {
 
+// References:
+// a.	Uniform Resource Identifiers (URI): Generic Syntax
+//	http://www.ietf.org/rfc/rfc2396.txt
+
 	[Serializable]
-	public sealed class AssemblyName  : ISerializable // ICloneable, , IDeserializationCallback
-	{
-		string name = "";
+	public sealed class AssemblyName  : ICloneable, ISerializable, IDeserializationCallback {
+		string name;
 		string codebase;
 		int major, minor, build, revision;
 		CultureInfo cultureinfo;
 		AssemblyNameFlags flags;
 		AssemblyHashAlgorithm hashalg;
 		StrongNameKeyPair keypair;
+		byte[] publicKey;
+		byte[] keyToken;
 		AssemblyVersionCompatibility versioncompat;
 		
 		public AssemblyName ()
 		{
+			// defaults
+			versioncompat = AssemblyVersionCompatibility.SameMachine;
 		}
 
 		internal AssemblyName (SerializationInfo si, StreamingContext sc)
@@ -40,80 +50,73 @@ namespace System.Reflection {
 		}
 
 		public string Name {
-			get {
-				return name;
-			}
-			set {
-				name = value;
-			}
+			get { return name; }
+			set { name = value; }
 		}
 
 		public string CodeBase {
-			get {
-				return codebase;
-			}
-
-			set {
-				codebase = value;
-			}
+			get { return codebase; }
+			set { codebase = value; }
 		}
 
-		[MonoTODO]
+		[MonoTODO("RFC 2396")]
+		private string Escape (string url) 
+		{
+			// we already have code in mcs\class\System\System\Uri.cs
+			// but Uri class ins't part of corlib !
+			// TODO
+			return url;
+		}
+
 		public string EscapedCodeBase {
-			get {
-				return codebase;
-			}
+			get { return Escape (codebase); }
 		}
 
 		public CultureInfo CultureInfo {
-			get {
-				return cultureinfo;
-			}
-
-			set {
-				cultureinfo = value;
-			}
+			get { return cultureinfo; }
+			set { cultureinfo = value; }
 		}
 
 		public AssemblyNameFlags Flags {
-			get {
-				return flags;
-			}
-
-			set {
-				flags = value;
-			}
+			get { return flags; }
+			set { flags = value; }
 		}
 
-		[MonoTODO]
+		[MonoTODO("incomplete")]
 		public string FullName {
 			get {
-				return name;
+				if (name == null)
+					return null;
+				StringBuilder fname = new StringBuilder ();
+				fname.Append (name);
+				fname.Append (", Version=");
+				fname.Append (Version.ToString ());
+				fname.Append (", Culture=");
+				if (CultureInfo.LCID == CultureInfo.InvariantCulture.LCID)
+					fname.Append ("neutral");
+				else
+					fname.Append (CultureInfo.ToString ()); // ???
+				if (keypair == null)
+					fname.Append (", PublicKeyToken=null");
+				// TODO
+				return fname.ToString ();
 			}
 		}
 
 		public AssemblyHashAlgorithm HashAlgorithm {
-			get {
-				return hashalg;
-			}
-
-			set {
-				hashalg = value;
-			}
+			get { return hashalg; }
+			set { hashalg = value; }
 		}
 
 		public StrongNameKeyPair KeyPair {
-			get {
-				return keypair;
-			}
-
-			set {
-				keypair = value;
-			}
+			get { return keypair; }
+			set { keypair = value; }
 		}
 
 		public Version Version {
 			get {
+				if (name == null)
+					return null;
 				return new Version (major, minor, build, revision);
 			}
 
@@ -126,16 +129,11 @@ namespace System.Reflection {
 		}
 
 		public AssemblyVersionCompatibility VersionCompatibility {
-			get {
-				return versioncompat;
-			}
-
-			set {
-				versioncompat = value;
-			}
+			get { return versioncompat; }
+			set { versioncompat = value; }
 		}
 		
-		public override int GetHashCode ()
+/*		public override int GetHashCode ()
 		{
 			return name.GetHashCode ();
 		}
@@ -151,7 +149,7 @@ namespace System.Reflection {
 				return true;
 			
 			return false;
-		}
+		}*/
 
 		public override string ToString ()
 		{
@@ -159,9 +157,46 @@ namespace System.Reflection {
 			return (name != null) ? name : base.ToString ();
 		}
 
-		[MonoTODO]
-		public byte[] GetPublicKeyToken() {
-			return new byte[0];
+		public byte[] GetPublicKey() 
+		{
+			return publicKey;
+		}
+
+		public byte[] GetPublicKeyToken() 
+		{
+			if (keyToken != null)
+				return keyToken;
+			else if (publicKey == null)
+				return null;
+			else {
+				HashAlgorithm ha = null;
+				switch (hashalg) {
+					case AssemblyHashAlgorithm.MD5:
+						ha = MD5.Create ();
+						break;
+					default:
+						// None default to SHA1
+						ha = SHA1.Create ();
+						break;
+				}
+				byte[] hash = ha.ComputeHash (publicKey);
+				// we need the last 8 bytes in reverse order
+				keyToken = new byte [8];
+				Array.Copy (hash, (hash.Length - 8), keyToken, 0, 8);
+				Array.Reverse (keyToken, 0, 8);
+				return keyToken;
+			}
+		}
+
+		public void SetPublicKey (byte[] publicKey) 
+		{
+			flags = AssemblyNameFlags.PublicKey;
+			this.publicKey = publicKey;
+		}
+
+		public void SetPublicKeyToken (byte[] publicKeyToken) 
+		{
+			keyToken = publicKeyToken;
 		}
 
 		public void GetObjectData (SerializationInfo info, StreamingContext context)
@@ -169,6 +204,25 @@ namespace System.Reflection {
 			info.AddValue ("_Name", name);
 			info.AddValue ("_CodeBase", codebase);
 			info.AddValue ("_Version", Version);
+		}
+
+		// required to implement ICloneable
+		[MonoTODO()]
+		public object Clone() 
+		{
+			return null;
+		}
+
+		// required to implement IDeserializationCallback
+		[MonoTODO()]
+		public void OnDeserialization (object sender) 
+		{
+		}
+
+		public static AssemblyName GetAssemblyName (string assemblyFile) 
+		{
+			Assembly a = Assembly.LoadFrom (assemblyFile);
+			return a.GetName ();
 		}
 	}
 }
--- mcs/class/corlib/System.Reflection/StrongNameKeyPair.cs
+++ mcs/class/corlib/System.Reflection/StrongNameKeyPair.cs
@@ -1,39 +1,114 @@
 //
 // System.Reflection.StrongNameKeyPair.cs
 //
-// Author:
-//   Kevin Winchester (kwin@ns.sympatico.ca)
+// Authors:
+//	Kevin Winchester (kwin@ns.sympatico.ca)
+//	Sebastien Pouliot (spouliot@motus.com)
 //
 // (C) 2002 Kevin Winchester
+// Portions (C) 2002 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System.IO;
 
 namespace System.Reflection {
 
-	[Serializable]
-	public class StrongNameKeyPair 
-	{		
-		private byte[] keyPair;
+[Serializable]
+public class StrongNameKeyPair 
+{		
+	private byte[] keyPair;
+	private byte[] publicKey;
+	private int bitLen;
+	private bool keypairValid;
 
-		public StrongNameKeyPair (byte[] keyPairArray) 
-		{
-			keyPair = keyPairArray;
-		}
-		
-		public StrongNameKeyPair (FileStream keyPairFile) 
-		{
-			
-		}
-		
-		public StrongNameKeyPair (string keyPairContainer) 
-		{
-			
-		}
-		
-		public byte[] PublicKey 
-		{
-			get { return keyPair; }
+	public StrongNameKeyPair (byte[] keyPairArray) 
+	{
+		if (keyPairArray == null)
+			throw new ArgumentNullException ("keyPairArray");
+		keypairValid = Validate (keyPairArray);
+	}
+	
+	public StrongNameKeyPair (FileStream keyPairFile) 
+	{
+		if (keyPairFile == null)
+			throw new ArgumentNullException ("keyPairFile");
+		byte[] input = new byte [keyPairFile.Length];
+		keyPairFile.Read (input, 0, input.Length);
+		keypairValid = Validate (input);
+	}
+	
+	[MonoTODO("We do not, yet, support keypair persistance")]
+	public StrongNameKeyPair (string keyPairContainer) 
+	{
+		// named key container
+		if (keyPairContainer == null)
+			throw new ArgumentNullException ("keyPairContainer");
+		// only RSA ? or both RSA and DSA ?
+		throw new NotImplementedException ();
+	}
+
+	private bool Validate (byte[] keypair) {
+		// Type - PRIVATEKEYBLOB (0x07)
+		if (keypair[0] != 0x07)
+			return false;
+		// Version - Always CUR_BLOB_VERSION (0x02)
+		if (keypair[1] != 0x02)
+			return false;
+		// RESERVED - Always 0
+		if ((keypair[2] != 0x00) || (keypair[3] != 0x00))
+			return false;
+		// ALGID - Always 00 24 00 00 (for CALG_RSA_SIGN)
+		if ((keypair[4] != 0x00) || (keypair[5] != 0x24) || (keypair[6] != 0x00) || (keypair[7] != 0x00))
+			return false;
+		// Magic - RSA2 (ASCII in hex)
+		if ((keypair[8] != 0x52) || (keypair[9] != 0x53) || (keypair[10] != 0x41) || (keypair[11] != 0x32))
+			return false;
+		// bitlen - ex: 1024 - must be a multiple of 8
+		bitLen = (keypair[15] << 24) + (keypair[14] << 16) + (keypair[13] << 8) + keypair[12];
+		if (bitLen % 8 != 0)
+			return false;
+		// public exponent (DWORD)
+		// modulus
+		// private key
+		keyPair = keypair;
+		return true;
+	}
+
+	public byte[] PublicKey {
+		get { 
+			if (!keypairValid)
+				throw new ArgumentException ("invalid keypair");
+			// first call (will be cached for all subsequent calls)
+			if (publicKey == null) {
+				publicKey = new byte [(bitLen >> 3) + 32];
+				// The first 12 bytes are documented at:
+				// http://msdn.microsoft.com/library/en-us/cprefadd/html/grfungethashfromfile.asp
+				// ALG_ID - Signature
+				publicKey[0] = keyPair[4];
+				publicKey[1] = keyPair[5];	
+				publicKey[2] = keyPair[6];	
+				publicKey[3] = keyPair[7];	
+				// ALG_ID - Hash
+				publicKey[4] = 0x04;
+				publicKey[5] = 0x80;
+				publicKey[6] = 0x00;
+				publicKey[7] = 0x00;
+				// Length of Public Key (in bytes)
+				int lastPart = publicKey.Length - 12;
+				publicKey[8] = (byte)(lastPart % 256);
+				publicKey[9] = (byte)(lastPart / 256); // just in case
+				publicKey[10] = 0x00;
+				publicKey[11] = 0x00;
+				// Ok from here - Same structure as keypair - expect for public key
+				publicKey[12] = 0x06;		// PUBLICKEYBLOB
+				// we can copy this part
+				Array.Copy (keyPair, 1, publicKey, 13, publicKey.Length - 13);
+				// and make a small adjustment 
+				publicKey[23] = 0x31;		// (RSA1 not RSA2)
+			}
+			return publicKey; 
 		}
 	}
+}
+
 }
