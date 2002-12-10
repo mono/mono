45714addb231520c36b230c4c3c18cdf5aa55236
Jaime Anguiano Olarra <jaime@mono-cvs.ximian.com>
2002-12-10 18:34:49 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=9537

---
+++ mcs/class/corlib/System.Runtime.Remoting.Services/TrackingServices.cs
@@ -0,0 +1,32 @@
+//
+// System.Runtime.Remoting.Services.TrackingServices.cs
+//
+// Author:
+// 	Jaime Anguiano Olarra (jaime@gnome.org)
+//
+// (C) 2002, Jaime Anguiano Olarra
+//
+// FIXME: This is just a skeleton for practical purpouses.
+//
+
+using System;
+using System.Runtime.Remoting;
+
+namespace System.Runtime.Remoting.Services 
+{
+	[MonoTODO]
+	public class TrackingServices 
+	{
+		public TrackingServices () {
+		}
+
+		public static void RegisterTrackingHandler (ITrackingHandler handler) {
+			throw new NotImplementedException ();
+		}
+
+		public static void UnregisterTrackingHandler (ITrackingHandler handler) {
+			throw new NotImplementedException ();
+		}
+	}
+}
+
