ef43ef882a282bffcace369d0557918163dcca40
Eduardo Garcia Cebollero <egarcia@mono-cvs.ximian.com>
2002-12-15 23:14:30 +0000

    2002-12-14  Eduardo Garcia Cebollero <kiwnix@yahoo.es>
    	* Directory.cs: Some fixes related to correct some exceptions thrown
    
    2002-12-11  Eduardo Garcia Cebollero <kiwnix@yahoo.es>
    
    	* Directory.cs: Some Exceptions added, fixed GetParent(),
    	CreateDirectory() should work with unix, native windows and
    	windows samba shares. Converted end-lines from dos-mode to unix-mode
    
    2002-12-08  Eduardo Garcia Cebollero <kiwnix@yahoo.es>
    
    	* Directory.cs: CreateDirectory  works now with Absolute paths
    	too, not only with relative ones.
    
    svn path=/trunk/mcs/; revision=9693

--- mcs/class/corlib/System.IO/Directory.cs
+++ mcs/class/corlib/System.IO/Directory.cs
@@ -5,6 +5,7 @@
 //   Jim Richardson  (develop@wtfo-guru.com)
 //   Miguel de Icaza (miguel@ximian.com)
 //   Dan Lewis       (dihlewis@yahoo.co.uk)
+//   Eduardo Garcia  (kiwnix@yahoo.es)
 //
 // Copyright (C) 2001 Moonlight Enterprises, All Rights Reserved
 // Copyright (C) 2002 Ximian, Inc.
@@ -16,15 +17,18 @@
 using System;
 using System.Security.Permissions;
 using System.Collections;
+using System.Text;
 
 namespace System.IO
 {
 	public sealed class Directory : Object
 	{
 		private Directory () {}
-
+		
 		public static DirectoryInfo CreateDirectory (string path)
 		{
+			DirectoryInfo tmpinfo = null;
+			
 			if (path == null) {
 				throw new ArgumentNullException ("path");
 			}
@@ -37,6 +41,97 @@ namespace System.IO
 				throw new ArgumentException ("Path contains invalid chars");
 			}
 
+			if (path.Trim().Length == 0){
+				throw new ArgumentException ("Only blank characters in path");
+			}
+			
+			if (path == ":")
+				throw new NotSupportedException ("Only ':' In path");
+			
+			string[] pathcomponents = path.Split(new char[] { Path.DirectorySeparatorChar });
+			
+			
+			if (pathcomponents.Length == 1){
+				tmpinfo = Directory.RealCreateDirectory(path);
+			} 
+			else {
+				if ((path[0]== Path.DirectorySeparatorChar) ||
+				    ((path[1] == ':') && (path[2] == Path.DirectorySeparatorChar))) //Absolute Path						
+				{
+					//Should Work in Unix, Win* native Directoryes and Samba Shares
+					//FIXME: This is not thread safe
+				    
+					
+					string actual_path = Directory.GetCurrentDirectory();
+					
+					if (Environment.OSVersion.Platform == PlatformID.Unix) //Is Unix
+					{
+						StringBuilder pathsumm = new StringBuilder(path);
+						Directory.SetCurrentDirectory("/");
+						pathsumm.Remove(0,1);
+						tmpinfo = Directory.CreateDirectory(pathsumm.ToString());
+					}
+					else //We asume is Win*
+					{
+						if ((path[1] == ':') || (path[0] == Path.DirectorySeparatorChar)) //Is a regular path
+						{
+							StringBuilder pathsumm = new StringBuilder(path);
+							Directory.SetCurrentDirectory(path.Substring(0,2));
+							pathsumm.Remove(0,2);
+							tmpinfo = Directory.CreateDirectory(pathsumm.ToString());							
+						}								
+						else if((path[0] == '\\') && (path[1] == '\\')) //Is a Samba Share
+						{
+							if (Directory.Exists(pathcomponents[0] + "\\"
+									     + pathcomponents[1] + "\\"
+									     + pathcomponents[2]))
+							{
+								StringBuilder pathsumm = new StringBuilder();	
+								Directory.SetCurrentDirectory(pathcomponents[0] + 
+											      "\\" + pathcomponents[1] +
+											      "\\" + pathcomponents[2]);
+								pathcomponents[0] = ""; pathcomponents[1] = ""; pathcomponents[2] = "";
+								foreach(string dir in pathsumm.ToString())
+								{
+									pathsumm.Append(dir + "\\");
+								}		
+								Directory.CreateDirectory(pathsumm.ToString());
+							}
+							else
+							{
+								throw new DirectoryNotFoundException("The samba share do not Exists");
+							}
+						}
+						
+					}
+					Directory.SetCurrentDirectory(actual_path);	
+				}
+				else //Relative Path
+				{
+					StringBuilder pathsumm = new StringBuilder();
+					
+					foreach(string dir in pathcomponents)
+					{
+						if (dir.Length != 0) {
+							if (pathsumm.Length == 0) {
+								pathsumm.Append (dir);
+							} 
+							else {
+								pathsumm.Append (Path.DirectorySeparatorChar + dir);
+							}
+							
+							if (!Directory.Exists (pathsumm.ToString())) {
+								tmpinfo = Directory.RealCreateDirectory (pathsumm.ToString());
+							}
+						}
+					}
+				}
+			}
+			return tmpinfo;
+		}
+		
+		private static DirectoryInfo RealCreateDirectory (string path)
+		{
 			MonoIOError error;
 			
 			if (!MonoIO.CreateDirectory (path, out error)) {
@@ -59,6 +154,12 @@ namespace System.IO
 			if (path.IndexOfAny (Path.InvalidPathChars) != -1) {
 				throw new ArgumentException ("Path contains invalid chars");
 			}
+			if (path.Trim().Length == 0)
+				throw new ArgumentException ("Only blank characters in path");
+			if (path == ":")
+				throw new NotSupportedException ("Only ':' In path");
+
+			
 
 			MonoIOError error;
 			
@@ -82,9 +183,16 @@ namespace System.IO
 		{
 			if (path == null)
 				throw new ArgumentNullException ();
+			if (path == "")
+				throw new System.ArgumentException("Path is Empty");
 			if (path.IndexOfAny (Path.InvalidPathChars) != -1)
 				throw new ArgumentException ("Path contains invalid characters");
-
+			if (path.Trim().Length == 0)
+				throw new ArgumentException ("Only blank characters in path");
+			if (path == ":")
+				throw new NotSupportedException ("Only ':' In path");			
+			
+			
 			if (recurse == false){
 				Delete (path);
 				return;
@@ -95,6 +203,9 @@ namespace System.IO
 
 		public static bool Exists (string path)
 		{
+			if (path == null)
+				return false;
+				
 			MonoIOError error;
 			
 			return MonoIO.ExistsDirectory (path, out error);
@@ -112,6 +223,19 @@ namespace System.IO
 		
 		public static DateTime GetCreationTime (string path)
 		{
+			if (path == null)
+				throw new System.ArgumentNullException("Path is Null");
+			if (path == "")
+				throw new System.ArgumentException("Path is Empty");
+			if (path.Trim().Length == 0)
+				throw new ArgumentException ("Only blank characters in path");
+			if (path.IndexOfAny (Path.InvalidPathChars) != -1)
+				throw new ArgumentException ("Path contains invalid chars");
+			if (path == ":")
+				throw new NotSupportedException ("Only ':' In path");
+
+			
+
 			return File.GetLastWriteTime (path);
 		}
 		
@@ -139,7 +263,7 @@ namespace System.IO
 		
 		public static string GetDirectoryRoot (string path)
 		{
-			return "" + Path.DirectorySeparatorChar;
+			return new String(Path.DirectorySeparatorChar,1);
 		}
 		
 		public static string [] GetFiles (string path)
@@ -153,23 +277,41 @@ namespace System.IO
 		}
 
 		public static string [] GetFileSystemEntries (string path)
-		{	
+		{
 			return GetFileSystemEntries (path, "*");
 		}
 
 		public static string [] GetFileSystemEntries (string path, string pattern)
 		{
+			if (path == null)
+				throw new ArgumentNullException ();
+			if (path.IndexOfAny (Path.InvalidPathChars) != -1)
+				throw new ArgumentException ("Path contains invalid characters");
+			if (path == "")
+				throw new ArgumentException ("The Path do not have a valid format");
+
 			return GetFileSystemEntries (path, pattern, 0, 0);
 		}
 		
 		public static string[] GetLogicalDrives ()
-		{	
-			return new string [] { "A:\\", "C:\\" };
+		{ 
+			//FIXME: Hardcoded Paths
+			if (Environment.OSVersion.Platform == PlatformID.Unix)
+				return new string[] { "/" };
+			else
+				return new string [] { "A:\\", "C:\\" };
 		}
 
 		public static DirectoryInfo GetParent (string path)
 		{
-			return new DirectoryInfo (Path.GetDirectoryName (path));
+			if (path == null)
+				throw new ArgumentNullException ();
+			if (path.IndexOfAny (Path.InvalidPathChars) != -1)
+				throw new ArgumentException ("Path contains invalid characters");
+			if (path == "")
+				throw new ArgumentException ("The Path do not have a valid format");
+			
+			return new DirectoryInfo (Path.GetDirectoryName (path + Path.DirectorySeparatorChar + ".."));
 		}
 
 		public static void Move (string src, string dest)
