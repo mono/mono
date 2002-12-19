df5afd9d377b95bc93ccaeaca7a163b2a36d0413
César Natarén <cesar@mono-cvs.ximian.com>
2002-12-19 21:56:08 +0000

    Added file cs0230.cs
    svn path=/trunk/mcs/; revision=9782

---
+++ mcs/errors/cs0230.cs
@@ -0,0 +1,17 @@
+//
+// CS0230.cs: foreach statement must contain the type and the variable identifier
+//
+
+using System;
+
+class X
+{
+	public static void Main()
+	{
+		int [] a = new int [5] {5, 4, 3, 2, 1};
+		
+		foreach (int in a) {
+			Console.WriteLine (x);
+		}
+	}
+}
