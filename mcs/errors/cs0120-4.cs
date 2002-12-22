686fdaac384b0e6ae9e616eb95a4bf68cdc021f1
Miguel de Icaza <miguel@gnome.org>
2002-12-22 06:39:28 +0000

    Add another one
    svn path=/trunk/mcs/; revision=9832

---
+++ mcs/errors/cs0120-4.cs
@@ -0,0 +1,23 @@
+using System;
+
+class X {
+	// Public properties and variables.
+	public string Y;
+
+	// Constructors.
+	public X()
+	{
+	}
+
+	// Public static methods.
+	public static void Main(string[] Arguments)
+	{
+		X.Y = "";
+	}
+}
+
+
+
+
+
+
