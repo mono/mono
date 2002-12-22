390086fddc1c255c71a207c287933e9414a19436
Miguel de Icaza <miguel@gnome.org>
2002-12-22 04:34:21 +0000

    Add this change as well
    svn path=/trunk/mcs/; revision=9827

--- mcs/class/corlib/System/MarshalByRefObject.cs
+++ mcs/class/corlib/System/MarshalByRefObject.cs
@@ -3,6 +3,7 @@
 //
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
+//   Lluis Sanchez Gual (lsg@ctv.es)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
 //
@@ -12,11 +13,21 @@ using System.Runtime.Remoting;
 namespace System {
 
 	[Serializable]
-	public abstract class MarshalByRefObject {
+	public abstract class MarshalByRefObject 
+	{
+		Identity _identity;		// Holds marshalling iformation of the object
+
+		internal Identity ObjectIdentity
+		{
+			get { return _identity; }
+			set { _identity = value; }
+		}
 
 		public virtual ObjRef CreateObjRef (Type type)
 		{
-			throw new NotImplementedException ();
+			// This method can only be called when this object has been marshalled
+			if (_identity == null) throw new RemotingException ("No remoting information was found for the object");
+			return _identity.CreateObjRef (type);
 		}
 
 		public object GetLifetimeService ()
