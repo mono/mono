c2b988119c974c5768b8fd7d86bb61acb637b78f
Alejandro Sánchez Acosta <asanchez@mono-cvs.ximian.com>
2002-12-23 12:54:57 +0000

    Added cs0170 test error.
    svn path=/trunk/mcs/; revision=9841

---
+++ mcs/errors/cs0170.cs
@@ -0,0 +1,27 @@
+// CS0170: Use of possibly unassigned field `a'
+// Line: 23
+
+using System;
+
+namespace cs0170
+{
+	public struct Foo {
+		public int a;
+	}
+
+	public class Bar
+	{
+		public void Inc (int x)
+		{
+			++x;
+		}		
+
+		static void Main ()
+		{
+			Foo f;
+			Bar b = new Bar();
+			b.Inc (f.a);
+			Console.WriteLine (f.a);
+		}
+	}
+}
