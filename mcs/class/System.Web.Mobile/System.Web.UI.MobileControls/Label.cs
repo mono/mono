f7708e260d433e39c3212908c81719e70d65aac1
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2003-01-13 16:52:22 +0000

    2003-01-13	Gaurav Vaish <gvaish_mono AT lycos.com>
    	* Label.cs               : The smallest class on this earth.
    
    svn path=/trunk/mcs/; revision=10464

---
+++ mcs/class/System.Web.Mobile/System.Web.UI.MobileControls/Label.cs
@@ -0,0 +1,21 @@
+/**
+ * Project   : Mono
+ * Namespace : System.Web.UI.MobileControls
+ * Class     : Label
+ * Author    : Gaurav Vaish
+ *
+ * Copyright : 2002 with Gaurav Vaish, and with
+ *             Ximian Inc
+ */
+
+using System.Web.Mobile;
+
+namespace System.Web.UI.MobileControls
+{
+	public class Label : MobileControl
+	{
+		public Label()
+		{
+		}
+	}
+}
