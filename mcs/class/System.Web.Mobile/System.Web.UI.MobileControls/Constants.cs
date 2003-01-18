77ed3351854f80cc345f537ea5a90fa44839087b
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2003-01-18 18:12:10 +0000

    2003-01-18	Gaurav Vaish <gvaish_mono AT lycos.com>
    	* Constants.cs           : Undocumented class. Reflection revealed.
    	                           Implemented.
    
    svn path=/trunk/mcs/; revision=10681

---
+++ mcs/class/System.Web.Mobile/System.Web.UI.MobileControls/Constants.cs
@@ -0,0 +1,44 @@
+/**
+ * Project   : Mono
+ * Namespace : System.Web.UI.MobileControls
+ * Class     : Constants
+ * Author    : Gaurav Vaish
+ *
+ * Copyright : 2003 with Gaurav Vaish, and with
+ *             Ximian Inc
+ */
+
+namespace System.Web.UI.MobileControls
+{
+	public class Constants
+	{
+		public const string AlternatingItemTemplateTag =
+		                                         "AlternatingItemTemplate";
+		public const string ContentTemplateTag = "ContentTemplate";
+		public const int    DefaultSessionsStateHistorySize = 5;
+		public const string EventArgumentID =    "__EA";
+		public const string EventSourceID =      "__ET";
+		public const string FooterTemplateTag =  "FooterTemplate";
+		public const string FormIDPrefix =       "#";
+		public const string HeaderTemplateTag =  "HeaderTemplate";
+		public const string ItemDetailsTemplateTag =
+		                                         "ItemDetailsTemplate";
+		public const string ItemTemplateTag =    "ItemTemplate";
+		public const string LabelTemplateTag =   "LabelTemplate";
+		public const string OptimumPageWeightParameter =
+		                                         "optimumPageWeight";
+		public const string PagePrefix =         "__PG_";
+		public const string ScreenCharactersHeightParameter =
+		                                         "screenCharactersHeight";
+		public const string ScriptTemplateTag =  "ScriptTemplate";
+		public const char   SelectionListSpecialCharacter = '*';
+		public const string SeparatorTempalteTag =
+		                                         "SeparatorTemplate";
+		public const string SymbolProtocol =     "symbol:";
+		public const string UniqueFilePathSuffixVariable = "__ufps=";
+		public const string UniqueFilePathSuffixVariableWithoutEqual =
+		                                         "__ufps";
+
+		internal const string ErrorStyle = "error";
+	}
+}
