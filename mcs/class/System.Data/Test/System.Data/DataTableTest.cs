46fa64be6ec83e02324e8f14d0357c2238257d0b
Ville Palo <ville@mono-cvs.ximian.com>
2003-01-27 13:56:48 +0000

    2003-27-01  Ville Palo <vi64pa@koti.soon.fi>
    	* DataTableTest.cs: Added test for DataTable.Select ()
    
    svn path=/trunk/mcs/; revision=10945

--- mcs/class/System.Data/Test/System.Data/DataTableTest.cs
+++ mcs/class/System.Data/Test/System.Data/DataTableTest.cs
@@ -55,6 +55,184 @@ namespace MonoTests.System.Data
 			
 		}
 
+                public void TestSelect ()
+                {
+			DataSet Set = new DataSet ();
+			DataTable Mom = new DataTable ("Mom");
+			DataTable Child = new DataTable ("Child");			
+			Set.Tables.Add (Mom);
+			Set.Tables.Add (Child);
+			
+			DataColumn Col = new DataColumn ("Name");
+			DataColumn Col2 = new DataColumn ("ChildName");
+			Mom.Columns.Add (Col);
+			Mom.Columns.Add (Col2);
+			
+			DataColumn Col3 = new DataColumn ("Name");
+			DataColumn Col4 = new DataColumn ("Age");
+			Col4.DataType = Type.GetType ("System.Int16");
+			Child.Columns.Add (Col3);
+			Child.Columns.Add (Col4);
+                	
+                	DataRelation Relation = new DataRelation ("Rel", Mom.Columns [1], Child.Columns [0]);
+			Set.Relations.Add (Relation);
+
+                	DataRow Row = Mom.NewRow ();
+                	Row [0] = "Laura";
+                	Row [1] = "Nick";
+                	Mom.Rows.Add (Row);
+                	
+                	Row = Mom.NewRow ();
+                	Row [0] = "Laura";
+                	Row [1] = "Dick";
+                	Mom.Rows.Add (Row);
+                	
+                	Row = Mom.NewRow ();
+                	Row [0] = "Laura";
+                	Row [1] = "Mick";
+                	Mom.Rows.Add (Row);
+
+                	Row = Mom.NewRow ();
+                	Row [0] = "Teresa";
+                	Row [1] = "Jack";
+                	Mom.Rows.Add (Row);
+                	
+                	Row = Mom.NewRow ();
+                	Row [0] = "Teresa";
+                	Row [1] = "Mack";
+                	Mom.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Nick";
+                	Row [1] = 15;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Dick";
+                	Row [1] = 25;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Mick";
+                	Row [1] = 35;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Jack";
+                	Row [1] = 10;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Mack";
+                	Row [1] = 19;
+                	Child.Rows.Add (Row);
+                
+                	Row = Child.NewRow ();
+                	Row [0] = "Mack";
+                	Row [1] = 99;
+                	Child.Rows.Add (Row);
+                	
+                	DataRow [] Rows = Mom.Select ("Name = 'Teresa'");
+                	AssertEquals ("test#01", 2, Rows.Length);
+                	
+                	Rows = Mom.Select ("Name = 'Teresa' and ChildName = 'Nick'");
+                	AssertEquals ("test#02", 0, Rows.Length);
+
+                	Rows = Mom.Select ("Name = 'Teresa' and ChildName = 'Jack'");
+                	AssertEquals ("test#03", 1, Rows.Length);
+
+                	Rows = Mom.Select ("Name = 'Teresa' and ChildName <> 'Jack'");
+                	AssertEquals ("test#04", "Mack", Rows [0] [1]);
+                	
+                	Rows = Mom.Select ("Name = 'Teresa' or ChildName <> 'Jack'");
+                	AssertEquals ("test#05", 5, Rows.Length);
+
+                	Rows = Child.Select ("age = 20 - 1");
+                	AssertEquals ("test#06", 1, Rows.Length);
+
+                	Rows = Child.Select ("age <= 20");
+                	AssertEquals ("test#07", 3, Rows.Length);
+
+                	Rows = Child.Select ("age >= 20");
+                	AssertEquals ("test#08", 3, Rows.Length);
+			
+                	Rows = Child.Select ("age >= 20 and name = 'Mack' or name = 'Nick'");
+                	AssertEquals ("test#09", 2, Rows.Length);
+
+                	Rows = Child.Select ("age >= 20 and (name = 'Mack' or name = 'Nick')");
+                	AssertEquals ("test#10", 1, Rows.Length);
+                	AssertEquals ("test#11", "Mack", Rows [0] [0]);
+
+                }
+                
+                public void TestSelect2 ()
+                {
+			DataSet Set = new DataSet ();
+			DataTable Child = new DataTable ("Child");						
+			//Set.Tables.Add (Child);
+						
+			DataColumn Col3 = new DataColumn ("Name");
+			DataColumn Col4 = new DataColumn ("Age");
+			Col4.DataType = Type.GetType ("System.Int16");
+			Child.Columns.Add (Col3);
+			Child.Columns.Add (Col4);
+                	
+                	DataRow Row = Child.NewRow ();
+                	Row [0] = "Nick";
+                	Row [1] = 15;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Dick";
+                	Row [1] = 25;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Mick";
+                	Row [1] = 35;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Jack";
+                	Row [1] = 10;
+                	Child.Rows.Add (Row);
+                	
+                	Row = Child.NewRow ();
+                	Row [0] = "Mack";
+                	Row [1] = 19;
+                	Child.Rows.Add (Row);
+                
+                	Row = Child.NewRow ();
+                	Row [0] = "Mack";
+                	Row [1] = 99;
+                	Child.Rows.Add (Row);
+
+			DataRow [] Rows = Child.Select ("age >= 20", "age DESC");
+                	AssertEquals ("test#01", 3, Rows.Length);
+                	AssertEquals ("test#02", "Mack", Rows [0] [0]);
+                	AssertEquals ("test#03", "Mick", Rows [1] [0]);                	
+                	AssertEquals ("test#04", "Dick", Rows [2] [0]);                	
+                	
+                	Rows = Child.Select ("age >= 20", "age asc");
+                	AssertEquals ("test#05", 3, Rows.Length);
+                	AssertEquals ("test#06", "Dick", Rows [0] [0]);
+                	AssertEquals ("test#07", "Mick", Rows [1] [0]);                	
+                	AssertEquals ("test#08", "Mack", Rows [2] [0]);                	
+                
+                	Rows = Child.Select ("age >= 20", "name asc");
+                	AssertEquals ("test#09", 3, Rows.Length);
+                	AssertEquals ("test#10", "Dick", Rows [0] [0]);
+                	AssertEquals ("test#11", "Mack", Rows [1] [0]);                	
+                	AssertEquals ("test#12", "Mick", Rows [2] [0]);                	
+
+                	Rows = Child.Select ("age >= 20", "name desc");
+                	AssertEquals ("test#09", 3, Rows.Length);
+                	AssertEquals ("test#10", "Mick", Rows [0] [0]);
+                	AssertEquals ("test#11", "Mack", Rows [1] [0]);                	
+                	AssertEquals ("test#12", "Dick", Rows [2] [0]);                	
+
+                }
+
 		public void TestToString()
 		{
 			DataTable dt = new DataTable();
