2962d0c78a069550e8886f2fb69c7fb203daccb4
Patrik Torstensson <totte@mono-cvs.ximian.com>
2003-01-28 12:15:37 +0000

    2003-01-28  Patrik Torstensson
    	* MarshalByRefObject.cs: implemented GetObjectIdentity
    	* Exception.cs: added support for remote exceptions
    
    svn path=/trunk/mcs/; revision=10976

--- mcs/class/corlib/System/MarshalByRefObject.cs
+++ mcs/class/corlib/System/MarshalByRefObject.cs
@@ -4,18 +4,34 @@
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
 //   Lluis Sanchez Gual (lsg@ctv.es)
+//   Patrik Torstensson (totte_mono@yahoo.com)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
 //
-
+using System.Threading;
 using System.Runtime.Remoting;
 
 namespace System {
 
 	[Serializable]
-	public abstract class MarshalByRefObject 
-	{
-		Identity _identity;		// Holds marshalling iformation of the object
+	public abstract class MarshalByRefObject {
+		private Identity _identity;		// Holds marshalling iformation of the object
+
+		internal Identity GetObjectIdentity(MarshalByRefObject obj, out bool IsClient) {
+			IsClient = false;
+			Identity objId = null;
+
+			if (RemotingServices.IsTransparentProxy(obj)) 
+			{
+				objId = RemotingServices.GetRealProxy(obj).ObjectIdentity;
+				IsClient = true;
+			} else 
+			{
+				objId = obj.ObjectIdentity;
+			}
+
+			return objId;
+		}
 
 		internal Identity ObjectIdentity
 		{
