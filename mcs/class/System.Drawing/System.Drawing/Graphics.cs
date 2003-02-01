dc6a01e022ee8b3b8570a992300c0c737a7fad3f
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-02-01 17:04:11 +0000

    Added a few methods needed to compile s.w.f in *nix
    svn path=/trunk/mcs/; revision=11094

--- mcs/class/System.Drawing/System.Drawing/Graphics.cs
+++ mcs/class/System.Drawing/System.Drawing/Graphics.cs
@@ -9,15 +9,36 @@
 //
 namespace System.Drawing {
 
+	[MonoTODO]
 	public sealed class Graphics : MarshalByRefObject, IDisposable {
 
 		public void Dispose ()
 		{
 		}
 
+		[MonoTODO]
 		public void FillRectangle (Brush b, Rectangle r)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static Graphics FromHwnd (IntPtr hwnd)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static Graphics FromHdc (IntPtr hdc)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static Graphics FromHdc (IntPtr hdc, IntPtr hdevice)
 		{
 			throw new NotImplementedException ();
 		}
 	}
 }
+
