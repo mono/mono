5d67a8951cfdf5e7607d349a41903e4bac4d6a8b
Sebastien Pouliot <sebastien@ximian.com>
2003-02-02 01:42:26 +0000

    2003-02-01  Sebastien Pouliot  <spouliot@videotron.ca>
    	* Modules.cs: Oups - not implemented. Added MonoTODO to most methods
    	so it's real status get reflected correctly on the web site.
    
    svn path=/trunk/mcs/; revision=11106

--- mcs/class/corlib/System.Reflection/Module.cs
+++ mcs/class/corlib/System.Reflection/Module.cs
@@ -13,91 +13,159 @@ using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 
 namespace System.Reflection {
-	[Serializable]
-	public class Module : ISerializable, ICustomAttributeProvider {
 
-		public static readonly TypeFilter FilterTypeName;
-		public static readonly TypeFilter FilterTypeNameIgnoreCase;
+[Serializable]
+public class Module : ISerializable, ICustomAttributeProvider {
 
-		private IntPtr _impl; /* a pointer to a MonoImage */
-		internal Assembly assembly;
-		internal string fqname;
-		internal string name;
-		internal string scopename;
+	public static readonly TypeFilter FilterTypeName;
+	public static readonly TypeFilter FilterTypeNameIgnoreCase;
 
-		internal Module () {}
+	private IntPtr _impl; /* a pointer to a MonoImage */
+	internal Assembly assembly;
+	internal string fqname;
+	internal string name;
+	internal string scopename;
 
-		public Assembly Assembly {get {return assembly;}}
-		public virtual string FullyQualifiedName {get {return fqname;}}
-		public string Name {get {return name;}}
-		public string ScopeName {get {return scopename;}}
+	internal Module () {}
 
-
-		public virtual Type[] FindTypes(TypeFilter filter, object filterCriteria) {
-			return null;
-		}
-		public virtual object[] GetCustomAttributes(bool inherit) {
-			return MonoCustomAttrs.GetCustomAttributes (this, inherit);
-		}
-		public virtual object[] GetCustomAttributes(Type attributeType, bool inherit) {
-			return MonoCustomAttrs.GetCustomAttributes (this, attributeType, inherit);
-		}
-		public FieldInfo GetField(string name) {
-			return null;
-		}
-		public FieldInfo GetField(string name, BindingFlags flags) {
-			return null;
-		}
-		public FieldInfo[] GetFields() {
-			return null;
-		}
-
-		public MethodInfo GetMethod(string name) {
-			return null;
-		}
-		public MethodInfo GetMethod(string name, Type[] types) {
-			return null;
-		}
-		public MethodInfo GetMethod( string name, BindingFlags bindingAttr, Binder binder, CallingConventions callConvention, Type[] types, ParameterModifier[] modifiers) {
-			return null;
-		}
-		protected virtual MethodInfo GetMethodImpl( string name, BindingFlags bindingAttr, Binder binder, CallingConventions callConvention, Type[] types, ParameterModifier[] modifiers) {
-			return null;
-		}
-		public MethodInfo[] GetMethods() {
-			return null;
-		}
-		public virtual void GetObjectData( SerializationInfo info, StreamingContext context) {}
-		public X509Certificate GetSignerCertificate() {
-			return null;
-		}
-		public virtual Type GetType(string className) {
-			return null;
-		}
-		public virtual Type GetType(string className, bool ignoreCase) {
-			return null;
-		}
-		public virtual Type GetType(string className, bool throwOnError, bool ignoreCase) {
-			return null;
-		}
-		public virtual Type[] GetTypes() {
-			return null;
-		}
-		public virtual bool IsDefined( Type attributeType, bool inherit) {
-			return MonoCustomAttrs.IsDefined (this, attributeType, inherit);
-		}
-		public bool IsResource() {
-			return false;
-		}
-		public override string ToString() {
-			return "Reflection.Module: " + name;
-		}
-
-
-
-
-
-
+	public Assembly Assembly {
+		get { return assembly; }
 	}
+
+	public virtual string FullyQualifiedName {
+		get { return fqname; }
+	}
+
+	public string Name {
+		get { return name; }
+	}
+
+	public string ScopeName {
+		get { return scopename; }
+	}
+
+	[MonoTODO]
+	public virtual Type[] FindTypes(TypeFilter filter, object filterCriteria) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual object[] GetCustomAttributes(bool inherit) 
+	{
+		return MonoCustomAttrs.GetCustomAttributes (this, inherit);
+	}
+
+	[MonoTODO]
+	public virtual object[] GetCustomAttributes(Type attributeType, bool inherit) 
+	{
+		return MonoCustomAttrs.GetCustomAttributes (this, attributeType, inherit);
+	}
+
+	[MonoTODO]
+	public FieldInfo GetField (string name) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public FieldInfo GetField (string name, BindingFlags flags) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public FieldInfo[] GetFields () 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public MethodInfo GetMethod (string name) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public MethodInfo GetMethod (string name, Type[] types) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public MethodInfo GetMethod (string name, BindingFlags bindingAttr, Binder binder, CallingConventions callConvention, Type[] types, ParameterModifier[] modifiers) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	protected virtual MethodInfo GetMethodImpl (string name, BindingFlags bindingAttr, Binder binder, CallingConventions callConvention, Type[] types, ParameterModifier[] modifiers) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public MethodInfo[] GetMethods () 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual void GetObjectData (SerializationInfo info, StreamingContext context) 
+	{
+	}
+
+	public X509Certificate GetSignerCertificate ()
+	{
+		try {
+			return X509Certificate.CreateFromSignedFile (assembly.Location);
+		}
+		catch {
+			return null;
+		}
+	}
+
+	[MonoTODO]
+	public virtual Type GetType(string className) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual Type GetType(string className, bool ignoreCase) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual Type GetType(string className, bool throwOnError, bool ignoreCase) 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual Type[] GetTypes() 
+	{
+		return null;
+	}
+
+	[MonoTODO]
+	public virtual bool IsDefined (Type attributeType, bool inherit) 
+	{
+		return MonoCustomAttrs.IsDefined (this, attributeType, inherit);
+	}
+
+	[MonoTODO]
+	public bool IsResource() 
+	{
+		return false;
+	}
+
+	[MonoTODO]
+	public override string ToString () 
+	{
+		return "Reflection.Module: " + name;
+	}
+}
 
 }
