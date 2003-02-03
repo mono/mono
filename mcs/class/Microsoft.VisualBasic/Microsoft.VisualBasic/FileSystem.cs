41d6cb2429b3fe144ace569bbf8c86f9db52346e
Miguel de Icaza <miguel@gnome.org>
2003-02-03 23:31:15 +0000

    New changes from Daniel Campos
    svn path=/trunk/mcs/; revision=11184

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/FileSystem.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/FileSystem.cs
@@ -3,7 +3,7 @@
 //
 // Author:
 //   
-// Daniel Campos ( danielcampos@myway.com )
+// Daniel Campos ( danielcampos@netcourrier.com )
 // 
 //
 
@@ -14,6 +14,8 @@ namespace Microsoft.VisualBasic
         [Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute] 
         sealed public class FileSystem {
                 private static System.IO.FileStream[] FHandle=new FileStream[255];
+		private static Microsoft.VisualBasic.OpenMode[] FMode= new Microsoft.VisualBasic.OpenMode[255];
+		private static string InitialPath=Environment.CurrentDirectory; 
                 // Declarations
                 // Constructors
                 // Properties
@@ -42,8 +44,26 @@ namespace Microsoft.VisualBasic
                 	return Environment.CurrentDirectory;
                 }
                 
-                [MonoTODO]
-                public static System.String CurDir (System.Char Drive) { throw new NotImplementedException (); }
+                [MonoTODO("Needs Testing")]
+                public static System.String CurDir (System.Char Drive)
+		{
+			bool MyOK=false;
+			string MyDrive=(Drive.ToString()).ToLower();
+			string[] buf=System.IO.Directory.GetLogicalDrives ();
+			for (int lookfor=0;lookfor<buf.Length;lookfor++)
+				if ( buf[lookfor].Substring(0,1).ToLower() == MyDrive ) {MyOK=true; break; }
+			if (!MyOK)
+				throw new System.ArgumentException("Invalid drive is specified.");
+			if ( Environment.CurrentDirectory.Substring(0,1).ToLower() == MyDrive )
+				return Environment.CurrentDirectory  ;
+			else
+			{
+				if ( InitialPath.Substring(0,1).ToLower() == MyDrive ) 
+					return InitialPath;
+				else
+					return (MyDrive.ToUpper()  + Path.VolumeSeparatorChar + Path.DirectorySeparatorChar  );
+			}
+		}
                 [MonoTODO]
                 public static System.String Dir () { throw new NotImplementedException (); }
                 [MonoTODO]
@@ -89,8 +109,14 @@ namespace Microsoft.VisualBasic
                  	return System.IO.File.GetLastWriteTime (PathName);
                 }
                 
-                [MonoTODO]
-                public static System.Int64 FileLen (System.String PathName) {  throw new NotImplementedException (); }
+                [MonoTODO("Needs Testing")]
+                public static System.Int64 FileLen(System.String PathName) 
+		{
+			FileInfo MyFile=new FileInfo(PathName);
+			if ( !MyFile.Exists )
+				throw new System.ArgumentException(PathName + " does not exists");
+			return (System.Int64)MyFile.Length;  
+		}
                 [MonoTODO]
                 public static Microsoft.VisualBasic.FileAttribute GetAttr (System.String PathName) { throw new NotImplementedException (); }
                 
@@ -114,13 +140,22 @@ namespace Microsoft.VisualBasic
                 	System.IO.FileMode MyMode;
                 	System.IO.FileAccess MyAccess; 
                 	System.IO.FileShare MyShare;
+                	//
+                	// exceptions
+                	//
                 	if ( RecordLength < -1 )
                 		throw new System.ArgumentException("Record Length is negative (nad not equal to -1)");
                 	if ( RecordLength > 32767)
                 		throw new System.ArgumentException ("Invalid Record Length");
                 	if (FileNumber <0 || FileNumber > 255)
                    		throw new System.IO.IOException(FileNumber.ToString() + " is invalid (<-1 or >255)",5); 
-                	
+                	if ( (Mode == OpenMode.Output) && ( Access != OpenAccess.Default ) && ( Access != OpenAccess.Write ) )
+						throw new System.ArgumentException("To use Output Mode, you have to use OpenAccess.Write or OpenAccess.Default");
+  					if ( (Mode == OpenMode.Input) && ( Access != OpenAccess.Default ) && ( Access != OpenAccess.Read ) )
+						throw new System.ArgumentException("To use Input Mode, you have to use OpenAccess.Read or OpenAccess.Default");
+                	//
+                	// implementation
+                	//
                 	FileNumber--;
             		if (FHandle[FileNumber] != null)
             			throw new System.IO.IOException (FileNumber.ToString() + " is in use",5); 	
@@ -179,7 +214,7 @@ namespace Microsoft.VisualBasic
         					throw new System.ArgumentException("Invalid Share");
         			}
         			FHandle[FileNumber]=new System.IO.FileStream (FileName,MyMode,MyAccess,MyShare);
-            			
+            			FMode[FileNumber]=Mode;
             		
                 		
                 	
@@ -312,13 +347,159 @@ namespace Microsoft.VisualBasic
                 public static void Input (System.Int32 FileNumber, ref System.String Value) { throw new NotImplementedException (); }
                 [MonoTODO]
                 public static void Input (System.Int32 FileNumber, ref System.DateTime Value) { throw new NotImplementedException (); }
-                [MonoTODO]
-                public static void Write (System.Int32 FileNumber, params System.Object[] Output) { throw new NotImplementedException (); }
-                [MonoTODO]
-                public static void WriteLine (System.Int32 FileNumber, params System.Object[] Output) { throw new NotImplementedException (); }
-                [MonoTODO]
-                public static System.String InputString (System.Int32 FileNumber, System.Int32 CharCount) { throw new NotImplementedException (); }
-                
+                 [MonoTODO("Needs Testing")]
+                public static void Write (System.Int32 FileNumber, params System.Object[] Output) 
+		{
+			string MyBuf=null;
+			byte[] Separator=new byte[1];
+			byte[] bufout=new Byte[1];
+			Separator[0]=(byte)',';
+			if (FileNumber<1 || FileNumber>255)
+				throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+			if (FHandle[FileNumber - 1]==null)
+				throw new System.IO.IOException(FileNumber + " does not exists",52);
+			if ( FMode[ FileNumber - 1] != OpenMode.Output  )
+				throw new System.IO.IOException ("FileMode is invalid");
+			if (Output.Length == 0)
+			{
+				FHandle[FileNumber - 1].Write(Separator,0,1);
+			}
+			else
+			{
+				for (int MyArgs=0;MyArgs<Output.Length;MyArgs++)
+				{
+					MyBuf=WriteAuxiliar(Output[MyArgs]);
+					for (int PutsData=0;PutsData<MyBuf.Length;PutsData++)
+					{
+						bufout[0]=(byte)MyBuf[PutsData];
+						FHandle[FileNumber-1].Write(bufout,0,1);
+					}
+					FHandle[FileNumber - 1].Write(Separator,0,1);
+				}
+			}
+		}
+                [MonoTODO("Needs Testing")]
+                public static void WriteLine (System.Int32 FileNumber, params System.Object[] Output) 
+		{
+			byte[] Separator=new byte[1];
+			byte[] bufout=new Byte[1];
+			byte[] NewLine=new Byte[2];
+			string MyBuf="";
+			Separator[0]=(byte)',';
+			NewLine[0]=(byte)'\x0D';
+			NewLine[1]=(byte)'\x0A';
+			if (FileNumber<1 || FileNumber>255)
+				throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+			if (FHandle[FileNumber - 1]==null)
+				throw new System.IO.IOException(FileNumber + " does not exists",52);
+			if ( FMode[ FileNumber - 1] != OpenMode.Output  )
+				throw new System.IO.IOException ("FileMode is invalid");
+			if (Output.Length == 0)
+			{
+				FHandle[FileNumber - 1].Write(NewLine,0,2);
+			}
+			else
+			{
+				for (int MyArgs=0;MyArgs<Output.Length;MyArgs++)
+				{
+					MyBuf=WriteAuxiliar(Output[MyArgs]);
+					for (int PutsData=0;PutsData<MyBuf.Length;PutsData++)
+					{
+						bufout[0]=(byte)MyBuf[PutsData];
+						FHandle[FileNumber-1].Write(bufout,0,1);
+					}
+					if (MyArgs < (Output.Length -1))
+					{
+						FHandle[FileNumber - 1].Write(Separator,0,1);
+					}
+					else
+					{
+						FHandle[FileNumber - 1].Write(NewLine,0,2);
+					}
+				}
+			}
+		}
+		private static string WriteAuxiliar(Object Argument)
+		{
+			string retval="";
+			if (Argument==null)
+				retval="#NULL#";
+			else
+			{
+				switch (Argument.GetType().ToString())
+				{
+				case "System.Boolean": 
+					if ( (bool)Argument == true)
+						retval="#TRUE#";
+					else
+						retval="#FALSE#";
+					break;
+				case "System.String":
+					retval="\"" + (string)Argument + "\"";
+					break;
+				case "System.Int64":
+				case "System.UInt64":
+				case "System.Int32":
+				case "System.UInt32":
+				case "System.Int16":
+				case "System.UInt16":
+				case "System.Sbyte":
+				case "System.Byte":
+					retval = Argument.ToString();
+					break;
+				case "System.Single":
+				case "System.Double":
+				case "System.Decimal":
+					string buf= (Argument.ToString()) ;
+					if ( buf.IndexOf(System.Globalization.NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator)>=0)
+						retval=buf.Substring(0, buf.IndexOf(System.Globalization.NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator)) 
+						+ "." + buf.Substring(1+buf.IndexOf(System.Globalization.NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator));
+	 				break;
+				case "System.Exception":
+					retval=((Exception)Argument).ToString();
+					break;
+				case "System.Char":
+					retval=((char)Argument).ToString();
+					break;
+				case "System.DateTime":
+					retval=((System.DateTime )Argument).ToString("u");
+					retval=retval.Substring(0,retval.Length -1);
+					if (retval.Substring(11,8)=="00:00:00")
+						retval=retval.Substring(0,10);
+					retval= "#" + retval + "#";
+					break;
+				default :
+					throw new NotImplementedException ();
+					
+				}
+			}
+			return retval;
+		}
+                [MonoTODO("Needs Testing")]
+                public static System.String InputString (System.Int32 FileNumber, System.Int32 CharCount) 
+                {
+                	byte[] Buf;
+                	string retval="";
+                	//
+                	// exceptions
+                	if ( FileNumber<1 || FileNumber>255)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+                	if ( FHandle[FileNumber - 1] == null)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+                	if ( (FMode[FileNumber - 1] != OpenMode.Input) && (FMode[FileNumber - 1] != OpenMode.Binary ))
+                		throw new System.IO.IOException ("FileMode is invalid");
+                	if (CharCount <0 || CharCount>2e14 )
+                		throw new System.ArgumentException(); 
+                	if ( (CharCount + FHandle[FileNumber - 1].Position) > FHandle[FileNumber - 1].Length)
+                		throw new System.IO.EndOfStreamException();	
+                	//
+                	// implementation
+                	Buf=new byte[CharCount];
+                	FHandle[FileNumber - 1].Read(Buf,0,Buf.Length);
+                	for (int myloop=0;myloop<Buf.Length;myloop++)
+                		retval+=((char)Buf[myloop]).ToString();
+                	return retval;
+                }
                 [MonoTODO("Needs testing")]
                 public static System.String LineInput (System.Int32 FileNumber) 
                 { 
@@ -345,7 +526,6 @@ namespace Microsoft.VisualBasic
                 	if ( retval.Length > 0 )
                 		if ( (buf == '\x0A') && (retval[retval.Length -1 ] == '\x0D') )
                 			retval=retval.Substring(0,retval.Length -1) ;
-                	Console.WriteLine (retval);
                 	return retval;
                 	    
                 }
@@ -403,19 +583,43 @@ namespace Microsoft.VisualBasic
                 [MonoTODO]
                 public static System.Int64 Loc (System.Int32 FileNumber) { throw new NotImplementedException (); }
                 [MonoTODO]
-                public static System.Int64 LOF (System.Int32 FileNumber) { throw new NotImplementedException (); }
+                public static System.Int64 LOF (System.Int32 FileNumber) 
+                {
+                	if (FileNumber<1 || FileNumber>255)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+                	if ( FHandle[FileNumber - 1] == null)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);	
+                	return (System.Int64)FHandle[FileNumber - 1].Length;
+                }
                 [MonoTODO]
                 public static Microsoft.VisualBasic.TabInfo TAB () { throw new NotImplementedException (); }
                 [MonoTODO]
                 public static Microsoft.VisualBasic.TabInfo TAB (System.Int16 Column) { throw new NotImplementedException (); }
                 [MonoTODO]
                 public static Microsoft.VisualBasic.SpcInfo SPC (System.Int16 Count) { throw new NotImplementedException (); }
-                [MonoTODO]
-                public static Microsoft.VisualBasic.OpenMode FileAttr (System.Int32 FileNumber) { throw new NotImplementedException (); }
+                [MonoTODO("Needs Testing")]
+                public static Microsoft.VisualBasic.OpenMode FileAttr (System.Int32 FileNumber) 
+                { 
+                	if (FileNumber<1 || FileNumber>255)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+                	if ( FHandle[FileNumber - 1] == null)
+                		throw new System.IO.IOException (FileNumber.ToString() + " does not exists",52);
+               		return FMode[FileNumber - 1];
+                }
                 [MonoTODO]
                 public static void Reset () { throw new NotImplementedException (); }
-                [MonoTODO]
-                public static void Rename (System.String OldPath, System.String NewPath) { throw new NotImplementedException (); }
+                [MonoTODO("Needs Testing")]
+                public static void Rename (System.String OldPath, System.String NewPath) 
+		{
+			if ( !File.Exists (OldPath) && !Directory.Exists( OldPath))
+				throw new System.ArgumentException ( OldPath + " does not exist");
+			if ( File.Exists ( NewPath) || Directory.Exists ( NewPath))
+				throw new System.IO.IOException ( NewPath + " already exists");
+			if ( File.Exists (OldPath))
+				File.Move (OldPath, NewPath);
+			else
+				Directory.Move (OldPath, NewPath); 
+		}
                 // Events
                 
         };
