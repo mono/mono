8471f1a9c41a79e2e27ab5e25a31582cfbfba750
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-02-05 21:14:53 +0000

    Initial check-in
    svn path=/trunk/mcs/; revision=11253

---
+++ mcs/class/Mono.Posix/Mono.Posix/UnixEndPoint.cs
@@ -0,0 +1,54 @@
+//
+// Mono.Posix.UnixEndPoint: EndPoint derived class for AF_UNIX family sockets.
+//
+// Authors:
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// (C) 2003 Ximian, Inc (http://www.ximian.com)
+//
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Mono.Posix
+{
+	[Serializable]
+	public class UnixEndPoint : EndPoint
+	{
+		string filename;
+		
+		public UnixEndPoint (string filename)
+		{
+			this.filename = filename;
+		}
+		
+		public override AddressFamily AddressFamily {
+			get { return AddressFamily.Unix; }
+		}
+
+		public override EndPoint Create (SocketAddress socketAddress)
+		{
+			int size = socketAddress.Size;
+			byte [] bytes = new byte [size];
+			for (int i = 0; i < size; i++) {
+				bytes [i] = socketAddress [i];
+			}
+
+			string name = Encoding.Default.GetString (bytes);
+			return new UnixEndPoint (name);
+		}
+
+		public override SocketAddress Serialize ()
+		{
+			byte [] bytes = Encoding.Default.GetBytes (filename);
+			SocketAddress sa = new SocketAddress (AddressFamily, bytes.Length + 2);
+			// sa [0] -> family low byte, sa [1] -> family high byte
+			for (int i = 0; i < bytes.Length; i++)
+				sa [i + 2] = bytes [i];
+
+			return sa;
+		}
+	}
+}
+
