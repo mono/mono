9d1953a5b3e916dc614783b7f3b776947e28cbd0
Martin Baulig <martin@novell.com>
2003-02-09 22:23:23 +0000

    2003-02-09  Martin Baulig  <martin@ximian.com>
    	* IMonoSymbolWriter.cs (DefineNamespace, OpenMethod): New methods.
    
    svn path=/trunk/mcs/; revision=11415

--- mcs/class/corlib/System.Reflection.Emit/IMonoSymbolWriter.cs
+++ mcs/class/corlib/System.Reflection.Emit/IMonoSymbolWriter.cs
@@ -8,13 +8,20 @@
 // (C) 2003 Ximian, Inc.  http://www.ximian.com
 //
 
-using System.Diagnostics.SymbolStore;
+using System.Reflection;
 using System.Reflection.Emit;
+using System.Diagnostics.SymbolStore;
 
 namespace Mono.CSharp.Debugger {
 	public interface IMonoSymbolWriter : ISymbolWriter {
 		byte[] CreateSymbolFile (AssemblyBuilder assembly_builder);
 
 		void MarkSequencePoint (int offset, int line, int column);
+
+		int DefineNamespace (string name, ISymbolDocumentWriter document,
+				     string[] using_clauses, int parent);
+
+		void OpenMethod (ISymbolDocumentWriter document, int startLine, int startColumn,
+				 int endLine, int endColumn, MethodBase method, int namespace_id);
 	}
 }
