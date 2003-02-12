c84ac4051fb12308375e330c55a25ded44dbe76c
Zoltan Varga <vargaz@gmail.com>
2003-02-12 17:30:22 +0000

    2003-02-12  Zoltan Varga  <vargaz@freemail.hu>
    	* makefile (TEST_SOURCES): Added test-181.
    
    svn path=/trunk/mcs/; revision=11530

---
+++ mcs/tests/test-181.cs
@@ -0,0 +1,24 @@
+//
+// test-181.cs: Test whenever mcs correctly handles the MethodImplAttributes
+// custom attribute.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+class Test
+{
+	[MethodImplAttribute(MethodImplOptions.Synchronized)]
+	public void test ()
+	{
+	}
+
+	static public int Main ()
+	{
+		MethodImplAttributes iflags = typeof (Test).GetMethod ("test").GetMethodImplementationFlags ();
+		return ((iflags & MethodImplAttributes.Synchronized) != 0 ? 0 : 1);
+	}
+}
+		
+		
