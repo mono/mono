d05bf9fbc2fc138012048c6b7bc5b635fc3d42ff
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2003-02-23 16:02:23 +0000

    2003-02-23	Gaurav Vaish <gvaish_mono AT lycos.com>
    	* MobileCapabilities.cs    : Initial implementation.
    
    svn path=/trunk/mcs/; revision=11870

---
+++ mcs/class/System.Web.Mobile/System.Web.Mobile/MobileCapabilities.cs
@@ -0,0 +1,23 @@
+/**
+ * Project   : Mono
+ * Namespace : System.Web.UI.MobileControls
+ * Class     : MobileCapabilities
+ * Author    : Gaurav Vaish
+ *
+ * Copyright : 2003 with Gaurav Vaish, and with
+ *             Ximian Inc
+ */
+
+using System.Web;
+using System.Web.UI;
+
+namespace System.Web.Mobile
+{
+	public class MobileCapabilities : HttpBrowserCapabilities
+	{
+		public MobileCapabilities()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
