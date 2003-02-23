8381e73ac4293a42ac9335795fb08162ca2189fc
Rafael Teixeira <monoman@gmail.com>
2003-02-23 20:32:42 +0000

    Dummy methods and fields
    svn path=/trunk/mcs/; revision=11895

--- mcs/class/System.ServiceProcess/System.ServiceProcess/ServiceBase.cs
+++ mcs/class/System.ServiceProcess/System.ServiceProcess/ServiceBase.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace System.ServiceProcess
 {
-	class ServiceBase
+	public class ServiceBase
 	{
 		public ServiceBase() { }
+
+		protected virtual void Dispose( bool disposing ) { }
+
+		protected virtual void OnStart(string[] args) { }
+
+		protected virtual void OnStop() { }
+
+		protected string ServiceName;
+
+		public static void Run(ServiceBase[] ServicesToRun) { }
+
 	}
 }
