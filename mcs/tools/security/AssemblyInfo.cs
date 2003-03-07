edd1a2ed0395661a0f3d66f91016425dbd16ecc5
Sebastien Pouliot <sebastien@ximian.com>
2003-03-07 00:30:09 +0000

    2003-03-06  Sebastien Pouliot  <spouliot@videotron.ca>
    	* AssemblyInfo.cs: New. Global assembly attributes (version and
    	  copyright) for security tools.
    	* README: New. Important information about the Mono's security tools
    
    svn path=/trunk/mcs/; revision=12268

---
+++ mcs/tools/security/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// AssemblyTitle - included in tool's source code
+// AssemblyDescription - included in tool's source code
+[assembly: AssemblyCompany("Sébastien Pouliot, Motus Technologies")]
+[assembly: AssemblyProduct("Open Source Tools for .NET")]
+[assembly: AssemblyCopyright("Copyright 2002, 2003 Motus Technologies. Released under BSD license.")]
+[assembly: AssemblyVersion("0.22.99.0")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: AssemblyDelaySign(true)]
+//[assembly: AssemblyKeyFile("sectools.pub")]
+//[assembly: AssemblyKeyName("")]
+
