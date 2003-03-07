b99f1dfa0bf4d123921de151f5ac262ba07d9257
Jackson Harper <jackson@novell.com>
2003-03-07 04:40:00 +0000

    * ClassTable.cs: Add file, this is a 'table' for storing classes 		* Location.cs: Add file, this will eventually be used for marking 	locations in IL files
    svn path=/trunk/mcs/; revision=12284

---
+++ mcs/ilasm/codegen/ClassTable.cs
@@ -0,0 +1,127 @@
+//
+// Mono.ILASM.ClassTable.cs
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+// (C) 2003 Jackson Harper, All rights reserved
+//
+
+using PEAPI;
+using System;
+using System.Collections;
+
+namespace Mono.ILASM {
+
+	public class ClassTable {
+
+		private class ClassTableItem {
+		
+			private static readonly int DefinedFlag = 2;
+	
+			private int flags;
+
+			public ArrayList LocationList;
+			public Class Class;
+
+			public ClassTableItem (Class klass, Location location)
+			{
+				flags = 0;
+				Class = klass;
+				LocationList = new ArrayList ();
+				LocationList.Add (location);
+			}
+		
+			public bool Defined {
+				get { return ((flags & DefinedFlag) != 0); }
+				set {
+					if (value)
+						flags |= DefinedFlag;
+					else
+						flags ^= DefinedFlag;
+				}
+			}
+		}
+
+		protected readonly TypeAttr DefaultAttr;
+		protected Hashtable table;
+		protected PEFile pefile;
+		
+		public ClassTable (PEFile pefile)
+		{
+			DefaultAttr = TypeAttr.Public;
+			this.pefile = pefile;
+			table = new Hashtable ();
+		}
+
+		public Class Get (string full_name)
+		{
+			ClassTableItem item = table[full_name] as ClassTableItem;
+			
+			if (item == null)
+				return null;
+
+			return item.Class;
+		}
+		
+		public Class GetReference (string full_name, Location location)
+		{
+			ClassTableItem item = table[full_name] as ClassTableItem;
+			
+			if (item != null) {
+				item.LocationList.Add (location);
+				return item.Class;
+			}
+			
+			string name_space, name;			
+			GetNameAndNamespace (full_name, out name_space, out name);
+
+			Class klass = pefile.AddClass (DefaultAttr, name_space, name);
+			AddReference (full_name, klass, location);
+			
+			return klass;
+		}
+
+		public void AddDefined (string full_name, Class klass, Location location)
+		{
+			ClassTableItem item = table[full_name] as ClassTableItem;
+			
+			if (item == null) {
+				item = new ClassTableItem (klass, location);
+			} else if (item.Defined) {
+				throw new Exception (String.Format 
+					("Class: {0} defined in multiple locations.", full_name));
+			}
+
+			item.Defined = true;
+		}
+
+		protected void AddReference (string full_name, Class klass, Location location)
+		{
+			if (table.Contains (full_name))
+				return;
+
+			ClassTableItem item = new ClassTableItem (klass, location);
+			
+			table[full_name] = item;
+		}
+
+		protected void GetNameAndNamespace (string full_name,
+			out string name_space, out string name) {
+			
+			int last_dot = full_name.LastIndexOf ('.');
+	
+			if (last_dot < 0) {
+				name_space = String.Empty;
+				name = full_name;
+				return;
+			}
+				
+			name_space = full_name.Substring (0, last_dot);
+			name = full_name.Substring (last_dot + 1);
+		}
+
+	}
+
+}
+
