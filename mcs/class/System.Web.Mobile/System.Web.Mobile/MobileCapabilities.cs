2aab91405192c886bb7f2b23eea667653d9d737e
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2003-03-08 07:40:47 +0000

    2003-03-08	Gaurav Vaish <gvaish_mono AT lycos.com>
    	* MobileCapabilities.cs    : Stub-ing methods / properties.
    
    svn path=/trunk/mcs/; revision=12348

--- mcs/class/System.Web.Mobile/System.Web.Mobile/MobileCapabilities.cs
+++ mcs/class/System.Web.Mobile/System.Web.Mobile/MobileCapabilities.cs
@@ -8,6 +8,7 @@
  *             Ximian Inc
  */
 
+using System;
 using System.Web;
 using System.Web.UI;
 
@@ -17,7 +18,574 @@ namespace System.Web.Mobile
 	{
 		public MobileCapabilities()
 		{
-			throw new NotImplementedException();
+		}
+
+		public bool CanCombineFormsInDeck
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanInitiateVoiceCall
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderAfterInputOrSelectElement
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderEmptySelects
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderInputAndSelectElementsTogether
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderMixedSelects
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderOneventAndPrevElementsTogether
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderPostBackCards
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanRenderSetvarZeroWithMultiSelectionList
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool CanSendMail
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int GatewayMajorVersion
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public double GatewayMinorVersion
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string GatewayVersion
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool HasBackButton
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool HidesRightAlignedMultiselectScrollbars
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string InputType
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsColor
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsMobileDevice
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int MaximumRenderedPageSize
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int MaximumSoftkeyLabelLength
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string MobileDeviceManufacturer
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string MobileDeviceModel
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int NumberOfSoftkeys
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string PreferredImageMime
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string PreferredRenderingMime
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string PreferredRenderingType
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersBreakBeforeWmlSelectAndInput
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersBreaksAfterHtmlLists
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersBreaksAfterWmlAnchor
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersBreaksAfterWmlInput
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersWmlDoAcceptsInline
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RendersWmlSelectsAsMenuCards
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string RequiredMetaTagNameValue
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresAttributeColonSubstitution
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresContentTypeMetaTag
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresDBCSCharacter
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresHtmlAdaptiveErrorReporting
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresLeadingPageBreak
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresNoBreakInFormatting
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresOutputOptimization
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresPhoneNumbersAsPlainText
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresSpecialViewStateEncoding
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresUniqueFilePathSuffix
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresUniqueHtmlCheckboxNames
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool RequiresUrlEncodedPostfieldValues
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int ScreenBitDepth
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int ScreenCharactersHeight
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int ScreenCharactersWidth
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int ScreenPixelsHeight
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int ScreenPixelsWidth
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsAccesskeyAttribute
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsBodyColor
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsBold
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsCacheControlMetaTag
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsCss
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsDivAlign
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsDivNoWrap
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsFontColor
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsFontName
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsFontSize
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsModeSymbols
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsImageSubmit
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsInputStyle
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsInputMode
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsItalic
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsJPhoneMultiMediaAttributes
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsJPhoneSymbols
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsQueryStringInFormAction
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsRedirectWithCookie
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsSelectMultiple
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool SupportsUncheck
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
 		}
 	}
 }
