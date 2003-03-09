134a4d39ffab3f28cf5b7b990caee0e6c5d98f17
Joel Basson <joel@mono-cvs.ximian.com>
2003-03-09 17:40:52 +0000

    2003-03-09  Joel Basson  <jstrike@mweb.co.za>
    * Frame.cs should have been GroupBox.cs
    
    2
    
    svn path=/trunk/mcs/; revision=12380

---
+++ mcs/class/System.Windows.Forms/Gtk/CheckBox.cs
@@ -0,0 +1,41 @@
+//		
+//			System.Windows.Forms.CheckBox
+//
+//			Author: 
+//						Joel Basson		(jstrike@mweb.co.za)
+//
+//
+
+using System.Drawing;
+using System.Drawing.Printing;
+using System.ComponentModel;
+
+namespace System.Windows.Forms {
+
+	/// <summary>
+	/// Represents a Windows CheckBox control.
+	///
+	/// </summary>
+
+	public class CheckBox: ButtonBase{
+
+	public CheckBox () : base ()
+	{
+	}
+
+	internal override Gtk.Widget CreateWidget () {
+		Gtk.CheckButton cbox = new Gtk.CheckButton();
+		cbox.Add (label.Widget);
+		return cbox;	
+	}
+	
+		public bool Checked {
+			get {
+		   	return ((Gtk.CheckButton)Widget).Active; 
+			}
+			set {
+				((Gtk.CheckButton)Widget).Active = value;
+			}
+		}
+	}
+}
---
+++ mcs/class/System.Windows.Forms/Gtk/RadioButton.cs
@@ -0,0 +1,62 @@
+//		
+//			System.Windows.Forms.Frame
+//
+//			Author: 
+//						Joel Basson		(jstrike@mweb.co.za)
+//
+//
+
+using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.ComponentModel;
+
+namespace System.Windows.Forms { 
+
+	
+	/// <summary>
+	/// Represents a Windows RadioButton control.
+	///
+	/// </summary>
+	///	This doesn't work properly. The "else" statement never seems
+	///	to get invoked, causing the grouping to not work.
+	///
+	
+		class RadioButton : ButtonBase {
+
+        	private static bool initialized;
+  				private static Gtk.RadioButton first_radio_button;
+			
+         public RadioButton()
+         {
+             initialized = false;
+         }
+ 
+         private static Gtk.Widget MakeWidget() { 			
+				
+					if ( initialized == false ) {
+                 initialized = true;
+  							
+                 first_radio_button = new Gtk.RadioButton(null, "");
+             		return first_radio_button;
+            } 
+					else {
+         		   	return Gtk.RadioButton.NewWithLabelFromWidget(first_radio_button, "");
+            }
+        	}
+				
+				internal override Gtk.Widget CreateWidget() {
+						
+						return MakeWidget();
+				}
+
+				public override string Text {
+					get {
+		   					return ((Gtk.RadioButton)Widget).Label; 
+					}
+					set {
+								((Gtk.RadioButton)Widget).Label = value;
+					}
+				}
+     }
+}
