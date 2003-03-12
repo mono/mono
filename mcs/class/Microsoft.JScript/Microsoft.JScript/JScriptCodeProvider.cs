c239d683b680e19e966caf4576966a1f5974aa47
César Natarén <cesar@mono-cvs.ximian.com>
2003-03-12 01:22:25 +0000

    Added Microsoft.JScript assembly and namespace, that will be used by the Jscript compiler.
    svn path=/trunk/mcs/; revision=12445

---
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/JScriptCodeProvider.cs
@@ -0,0 +1,43 @@
+//
+// JScriptCodeProvider.cs:
+//
+// Author: Cesar Octavio Lopez Nataren <cesar@ciencias.unam.mx>
+//
+
+namespace Microsoft.JScript
+{
+	using System;
+	using System.CodeDom.Compiler;
+
+	[MonoTODO]
+	public class JScriptCodeProvider : CodeDomProvider
+	{
+		[MonoTODO]
+		public JScriptCodeProvider ()
+		{
+			throw new NotImplementedException ();
+		}
+
+
+		[MonoTODO]
+		public override string FileExtension {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+
+		[MonoTODO]
+		public override ICodeCompiler CreateCompiler ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		
+		[MonoTODO]
+		public override ICodeGenerator CreateGenerator ()
+		{
+			throw new NotImplementedException ();
+		}		
+	}
+}
---
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/TODOAttribute.cs
@@ -0,0 +1,37 @@
+//
+// TODOAttribute.cs
+//
+// Author:
+//   Ravi Pratap (ravi@ximian.com)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+
+namespace System.Runtime.Serialization.Formatters.Soap {
+
+	/// <summary>
+	///   The TODO attribute is used to flag all incomplete bits in our class libraries
+	/// </summary>
+	///
+	/// <remarks>
+	///   Use this to decorate any element which you think is not complete
+	/// </remarks>
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	public class MonoTODOAttribute : Attribute {
+
+		private string comment;
+		
+		public MonoTODOAttribute ()
+		{}
+
+		public MonoTODOAttribute (string comment)
+		{
+			this.comment = comment;
+		}
+
+		public string Comment
+		{
+			get { return comment; }
+		}
+	}
+}
