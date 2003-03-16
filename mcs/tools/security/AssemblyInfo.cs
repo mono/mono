eb26218e6508474537f695b869e2464f59239444
Sebastien Pouliot <sebastien@ximian.com>
2003-03-16 00:44:53 +0000

    2003-03-15  Sebastien Pouliot  <spouliot@videotron.ca>
    	* AssemblyInfo.cs: Updated version for mono 0.23.
    	* MakeCert.cs: New. MakeCert clone utility to create X.509
    	certificates (e.g. for testing Authenticode signatures).
    	* makefile: Added cert2spc, sn & makecert to Windows build.
    	* sectools.build: Added makecert to build.
    
    svn path=/trunk/mcs/; revision=12577

--- mcs/tools/security/AssemblyInfo.cs
+++ mcs/tools/security/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Sébastien Pouliot, Motus Technologies")]
 [assembly: AssemblyProduct("Open Source Tools for .NET")]
 [assembly: AssemblyCopyright("Copyright 2002, 2003 Motus Technologies. Released under BSD license.")]
-[assembly: AssemblyVersion("0.22.99.0")]
+[assembly: AssemblyVersion("0.23.99.0")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyTrademark("")]
 //[assembly: AssemblyCulture("")]
