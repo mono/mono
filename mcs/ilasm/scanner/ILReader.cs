ccefbef64640e3170ffdf2a84e9b27477d7fe603
Jackson Harper <jackson@novell.com>
2003-03-30 08:14:50 +0000

    * ILReader.cs: Instead of mainting a buffer let the reader handle 	that, and use a stack for putting chars back. This is probably 	much slower and not nearly as cool but the old method was a little 	buggy and I was having trouble tracking things down. This can be 	optimized someday.
    svn path=/trunk/mcs/; revision=12923

--- mcs/ilasm/scanner/ILReader.cs
+++ mcs/ilasm/scanner/ILReader.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Collections;
 
 namespace Mono.ILASM {
 
@@ -11,25 +12,19 @@ namespace Mono.ILASM {
 	/// <summary>
 	/// </summary>
 	public class ILReader {
-		internal static readonly int CHUNK_SIZE = 4096;
-
+		
 		private StreamReader reader;
-		private int inUse;
-		private int pos;
-		private int termPos;
+		private Stack putback_stack;
 		private Location location;
 		private Location markedLocation;
-		private char[] buffer;
-
+		
 		public ILReader (StreamReader reader)
 		{
 			this.reader = reader;
+			putback_stack = new Stack ();
+			
 			location = new Location ();
 			markedLocation = Location.Unknown;
-			buffer = new char [CHUNK_SIZE];
-			pos = buffer.Length + 1;
-			termPos = buffer.Length + 2;
-			inUse = 0;
 		}
 
 
@@ -52,98 +47,28 @@ namespace Mono.ILASM {
 			}
 		}
 
-
-		private void EnsureCapacity (int required, int shift)
+		private int DoRead ()
 		{
-			int n = buffer.Length;
-
-			if (shift < 0) shift = 0;
-
-			if (n < required) {
-				for (;n < required; n <<= 1);
-				char [] newBuff = new char [n];
-				Array.Copy (buffer, 0, newBuff, shift, buffer.Length);
-			}
+			if (putback_stack.Count > 0) 
+				return (char) putback_stack.Pop ();
+			
+			return reader.Read ();
 		}
 
-		private void EnsureCapacity (int required)
+		private int DoPeek ()
 		{
-			EnsureCapacity (required, 0);
+			if (putback_stack.Count > 0)
+				return (char) putback_stack.Peek ();
+			
+			return reader.Peek ();
 		}
-
-		private int NormalizeEOL (int len)
-		{
-			if (len <= 0) return -1;
-			int n = len;
-
-			for (int i = 0, j = 0; i < len; i++, j++) {
-				if (buffer [i] == '\r') {
-					if (i == len - 1) {
-						// strip the very last char
-						--n;
-						break;
-					} else {
-						buffer [j] = '\n';
-						if (buffer [i + 1] == '\n') {
-							i++;
-							--n;
-						}
-					}
-				} else if (i != j) {
-					buffer [j] = buffer [i];
-				}
-
-			}
-
-			return n;
-		}
-
-
-
-		private int DoRead (int advance)
-		{
-			int n = 1;
-
-			if (pos == termPos) {
-				n = -1;
-			} else if (pos >= inUse) {
-				pos = 0;
-
-				// read as much as possible
-				n = reader.Read (buffer, 0, buffer.Length);
-				n = NormalizeEOL (n);
-
-				// Manually terminate the buffer if we had
-				// reached the end of the input stream.
-				if (n != -1 && n < buffer.Length) termPos = n + 1;
-				
-				inUse = n;
-			}
-
-			int res = -1;
-			if (n != -1) {
-				res = buffer [pos];
-			}
-			pos += advance;
-
-
-			// Track location
-			if (res == '\n') {
-				location.NewLine();
-			} else {
-				location.NextColumn();
-			}
-
-			return res;
-		}
-
 
 		/// <summary>
 		/// </summary>
 		/// <returns></returns>
 		public int Read ()
 		{
-			return DoRead (1);
+			return DoRead ();
 		}
 
 		/// <summary>
@@ -151,7 +76,7 @@ namespace Mono.ILASM {
 		/// <returns></returns>
 		public int Peek ()
 		{
-			return DoRead (0);
+			return DoPeek ();
 		}
 
 
@@ -159,17 +84,10 @@ namespace Mono.ILASM {
 		/// </summary>
 		public void Unread (char c)
 		{
-			if (pos > 0) {
-				if (buffer [pos - 1] == c) {
-					--pos;
-				} else {
-					throw new Exception ("Invalid putback.");
-				}
-			} else {
-				EnsureCapacity (++inUse, 1);
-				pos = 0;
-				buffer [0] = c;
-			}
+			putback_stack.Push (c);
+
+			if ('\n' == c)
+				location.PreviousLine ();
 
 			location.PreviousColumn ();
 		}
@@ -180,18 +98,8 @@ namespace Mono.ILASM {
 		/// <param name="chars"></param>
 		public void Unread (char [] chars)
 		{
-			int len = chars.Length;
-
-			if (len == 0) return;
-
-			if (pos >= len) {
-				pos -= len;
-			} else {
-				inUse += len;
-				EnsureCapacity (inUse, len);
-				pos -= len;
-				Array.Copy (chars, 0, buffer, pos, len);
-			}
+			for (int i=chars.Length-1; i>=0; i--)
+				Unread (chars[i]);					
 		}
 
 		/// <summary>
@@ -250,3 +158,4 @@ namespace Mono.ILASM {
 	}
 
 }
+
