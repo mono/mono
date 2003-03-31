9c784fbcfcaf0ddd37553b7f92b1cbd7944bdcda
Martin Willemoes Hansen <mwh@mono-cvs.ximian.com>
2003-03-31 14:14:49 +0000

    * System.Drawing.Printing/PrinterUnitConvert.cs stubed
    svn path=/trunk/mcs/; revision=12973

---
+++ mcs/class/System.Drawing/System.Drawing.Printing/PrinterUnitConvert.cs
@@ -0,0 +1,62 @@
+//
+// System.Drawing.Printing.PrinterUnitConvert
+//
+// Authors:
+//      Martin Willemoes Hansen (mwh@sysrq.dk)
+//
+// (C) 2003 Martin Willemoes Hansen
+//
+
+namespace System.Drawing.Printing
+{
+        public sealed class PrinterUnitConvert
+	{
+		[MonoTODO]
+		public static double Convert (double value,
+					      PrinterUnit fromUnit,
+					      PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+
+		[MonoTODO]
+		public static int Convert (int value,
+					   PrinterUnit fromUnit,
+					   PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+
+		[MonoTODO]
+		public static Margins Convert (Margins value,
+					       PrinterUnit fromUnit,
+					       PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+
+		[MonoTODO]
+		public static Point Convert (Point value,
+					     PrinterUnit fromUnit,
+					     PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+
+		[MonoTODO]
+		public static Rectangle Convert (Rectangle value,
+						 PrinterUnit fromUnit,
+						 PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+
+		[MonoTODO]
+		public static Size Convert (Size value,
+					    PrinterUnit fromUnit,
+					    PrinterUnit toUnit)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
