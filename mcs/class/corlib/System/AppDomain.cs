b64fcb264011ca0d451330f6a3ad27c9db431be7
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-04-02 01:43:58 +0000

    2003-04-02  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* AppDomain.cs: fixed InvalidCastException.
    
    svn path=/trunk/mcs/; revision=13012

--- mcs/class/corlib/System/AppDomain.cs
+++ mcs/class/corlib/System/AppDomain.cs
@@ -665,9 +665,9 @@ namespace System {
 			if (AssemblyResolve == null)
 				return null;
 			
-			ResolveEventHandler [] list = (ResolveEventHandler []) AssemblyResolve.GetInvocationList ();
-			foreach (ResolveEventHandler eh in list) {
-				Assembly assembly = eh (this, new ResolveEventArgs (name));
+			foreach (Delegate eh in AssemblyResolve.GetInvocationList ()) {
+				ResolveEventHandler handler = (ResolveEventHandler) eh;
+				Assembly assembly = handler (this, new ResolveEventArgs (name));
 				if (assembly != null)
 					return assembly;
 			}
