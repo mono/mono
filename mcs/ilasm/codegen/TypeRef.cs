ecd10525ee2ebba2c49139649b0bc75c0d664765
Jackson Harper <jackson@novell.com>
2003-04-02 07:45:43 +0000

    * ILParser.jay: Support pinned, modreq, and modopt types.
    svn path=/trunk/mcs/; revision=13016

--- mcs/ilasm/codegen/TypeRef.cs
+++ mcs/ilasm/codegen/TypeRef.cs
@@ -13,22 +13,26 @@ using System;
 
 namespace Mono.ILASM {
 
-	public class TypeRef {
+        public class TypeRef {
 
-		public readonly PEAPI.Type Type;
-		public readonly string FullName;
+                public readonly PEAPI.Type Type;
+                public readonly string FullName;
 
-		public TypeRef (PEAPI.Type type, string full_name)
-		{
-			Type = type;
-			FullName = full_name;
-		}
-	
-		public override string ToString ()
-		{
-			return FullName;
-		}
-	}
+                public bool Pinned;
+
+                public TypeRef (PEAPI.Type type, string full_name)
+                {
+                        Type = type;
+                        FullName = full_name;
+                        Pinned = false;
+                }
+
+                public override string ToString ()
+                {
+                        return FullName;
+                }
+
+        }
 
 }
 
