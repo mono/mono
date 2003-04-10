436abe8b4150ebc86e2401ad8e74079a4e184266
Ville Palo <ville@mono-cvs.ximian.com>
2003-04-10 15:24:45 +0000

    2003-04-10  Ville Palo <vi64pa@kolumbus.fi>
    	* StringWriterTest.cs: Added couple of tests.
    
    svn path=/trunk/mcs/; revision=13470

--- mcs/class/corlib/Test/System.IO/StringWriterTest.cs
+++ mcs/class/corlib/Test/System.IO/StringWriterTest.cs
@@ -41,6 +41,62 @@ public class StringWriterTest : TestCase {
                 writer.Write( testBuffer, 5, 6 );
                 AssertEquals( "A fooTestString", writer.ToString() );
         }
+
+        public void TestNewLine() {
+        	
+        	StringWriter writer = new StringWriter();
+        	
+        	writer.NewLine = "\n\r";
+        	AssertEquals ("NewLine 1", "\n\r", writer.NewLine);
+        	
+        	writer.WriteLine ("first");
+        	AssertEquals ("NewLine 2", "first\n\r", writer.ToString());
+        	
+        	writer.NewLine = "\n";
+        	AssertEquals ("NewLine 3", "first\n\r", writer.ToString());
+        	
+        	writer.WriteLine ("second");
+        	AssertEquals ("NewLine 4", "first\n\rsecond\n", writer.ToString());
+        	
+        }
+        
+        public void TestWriteLine() {
+        	
+        	StringWriter writer = new StringWriter();
+        	writer.NewLine = "\n";
+        	
+        	writer.WriteLine ("first line");
+        	writer.WriteLine ("second line");
+        	        	
+        	AssertEquals ("WriteLine 1", "first line\nsecond line\n", writer.ToString ());
+        	writer.Close ();
+        }
+        
+        public void TestGetStringBuilder() {
+        	
+        	StringWriter writer = new StringWriter ();
+        	writer.Write ("line");
+		StringBuilder builder = writer.GetStringBuilder ();
+        	builder.Append (12);
+        	AssertEquals ("GetStringBuilder 1", "line12", writer.ToString ());
+        	writer.Write ("test");
+        	AssertEquals ("GetStringBuilder 2", "line12test", builder.ToString ());        	        	
+        }
+        
+        public void TestClose() {
+        	
+        	StringWriter writer = new StringWriter ();
+        	writer.Write ("mono");
+        	writer.Close ();
+        	
+        	try {
+        		writer.Write ("kicks ass");
+        		Fail ("Close 1");
+        	} catch (Exception e) {
+        		AssertEquals ("Close 2", typeof (ObjectDisposedException), e.GetType ());
+        	}
+        }
+
 }
 
 }
