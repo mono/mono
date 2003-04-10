fc5a9fcae91daff5cd1634fc8e48764a3a0e9a21
Lluis Sanchez <lluis@novell.com>
2003-04-10 16:29:44 +0000

    * CrossContextChannel.cs: Improved support for OneWay and Async messages.
    svn path=/trunk/mcs/; revision=13478

--- mcs/class/corlib/System.Runtime.Remoting.Contexts/CrossContextChannel.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Contexts/CrossContextChannel.cs
@@ -61,16 +61,28 @@ namespace System.Runtime.Remoting.Contexts
 			{
 				Context.NotifyGlobalDynamicSinks (true, msg, false, true);
 				Thread.CurrentContext.NotifyDynamicSinks (true, msg, false, false);
-				replySink = new ContextRestoreSink (replySink, oldContext, msg);
-				return identity.AsyncObjectProcessMessage (msg, replySink);
+				if (replySink != null) replySink = new ContextRestoreSink (replySink, oldContext, msg);
+
+				IMessageCtrl res = identity.AsyncObjectProcessMessage (msg, replySink);
+
+				if (replySink == null)
+				{
+					Context.NotifyGlobalDynamicSinks (false, msg, false, false);
+					Thread.CurrentContext.NotifyDynamicSinks (false, msg, false, false);
+				}
+
+				return res;
 			}
-			catch
+			catch (Exception ex)
+			{
+				if (replySink != null)
+					replySink.SyncProcessMessage (new ReturnMessage (ex, (IMethodCallMessage)msg));
+				return null;
+			}
+			finally
 			{
 				if (oldContext != null)
 					Context.SwitchToContext (oldContext);
-
-				// TODO: return an exception
-				return null;
 			}
 		}
 
@@ -98,17 +110,17 @@ namespace System.Runtime.Remoting.Contexts
 				{
 					Context.NotifyGlobalDynamicSinks (false, msg, false, false);
 					Thread.CurrentContext.NotifyDynamicSinks (false, msg, false, false);
+					return _next.SyncProcessMessage (msg);
 				}
 				catch (Exception ex)
 				{
-					msg = new ReturnMessage (ex, (IMethodCallMessage)_call);
+					return new ReturnMessage (ex, (IMethodCallMessage)_call);
 				}
 				finally
 				{
 					if (_context != null)
 						Context.SwitchToContext (_context);
-				}
-				return _next.SyncProcessMessage (msg);
+				}		
 			}
 
 			public IMessageCtrl AsyncProcessMessage (IMessage msg, IMessageSink replySink)
