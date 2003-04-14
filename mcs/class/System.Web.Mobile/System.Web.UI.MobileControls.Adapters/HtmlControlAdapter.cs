087e75b3369a1ca45e85cb832022cf916b3e3c3e
Gaurav Vaish <gvaish@mono-cvs.ximian.com>
2003-04-14 08:39:34 +0000

    2003-04-08	Gaurav Vaish <gvaish_mono AT lycos.com>
    * HtmlControlAdapter.cs     : Initial implementation.
    
    svn path=/trunk/mcs/; revision=13585

---
+++ mcs/class/System.Web.Mobile/System.Web.UI.MobileControls.Adapters/HtmlControlAdapter.cs
@@ -0,0 +1,24 @@
+/**
+ * Project   : Mono
+ * Namespace : System.Web.UI.MobileControls.Adapters
+ * Class     : HtmlControlAdapter
+ * Author    : Gaurav Vaish
+ *
+ * Copyright : 2003 with Gaurav Vaish, and with
+ *             Ximian Inc
+ */
+
+using System;
+using System.Collections;
+using System.Web;
+using System.Web.UI;
+
+namespace System.Web.Mobile
+{
+	public class HtmlControlAdapter //: ControlAdapter
+	{
+		public HtmlControlAdapter()
+		{
+		}
+	}
+}
