17a8392827dd34a37bee9b80f7d62fcff5689241
Eduardo Garcia Cebollero <egarcia@mono-cvs.ximian.com>
2003-04-15 03:30:01 +0000

    2003-04-15 Eduardo Garcia Cebollero <kiwnix@yahoo.es>
            * CaseInsensitiveComparerTest.cs: Fixed Compare Method (now works
            like MS.NET) and implemented the
            CaseInsensitiveComparerTest(culture) constructor.
    
    svn path=/trunk/mcs/; revision=13633

--- mcs/class/corlib/Test/System.Collections/CaseInsensitiveComparerTest.cs
+++ mcs/class/corlib/Test/System.Collections/CaseInsensitiveComparerTest.cs
@@ -11,11 +11,13 @@ namespace MonoTests.System.Collections {
 
 
 	/// <summary>CaseInsensitiveComparer test suite.</summary>
+        [TestFixture]
 	public class CaseInsensitiveComparerTest : TestCase {
 		protected override void SetUp ()
 		{
 		}
 
+		[Test]
 		public void TestDefaultInstance ()
 		{
 			// Make sure the instance returned by Default
@@ -24,15 +26,81 @@ namespace MonoTests.System.Collections {
 			        as CaseInsensitiveComparer) != null);
 		}
 
+		[Test]
 		public void TestCompare () {
 			CaseInsensitiveComparer cic = new CaseInsensitiveComparer ();
 
-			Assert(cic.Compare ("WILD WEST", "Wild West") == 0);
-			Assert(cic.Compare ("WILD WEST", "wild west") == 0);
+			AssertEquals(cic.Compare ("WILD WEST", "Wild West"),0);
+			AssertEquals(cic.Compare ("WILD WEST", "wild west"),0);
 			Assert(cic.Compare ("Zeus", "Mars") > 0);
 			Assert(cic.Compare ("Earth", "Venus") < 0);
 		}
-			
+
+		[Test]
+		public void TestIntsNEq()
+		{
+			int a =1;
+			int b =2;                                   
+			AssertEquals("#01",Comparer.Default.Compare(a,b),CaseInsensitiveComparer.Default.Compare(a,b));
+		}
+		
+		[Test]
+		public void TestIntsEq()
+		{
+			int a =1;
+			int b =1;                                                                                
+		
+			AssertEquals("#02",Comparer.Default.Compare(a,b),CaseInsensitiveComparer.Default.Compare(a,b));
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void CtorNull()
+		{
+		    new CaseInsensitiveComparer(null);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void TestObject()
+		{
+		    CaseInsensitiveComparer.Default.Compare(a,b);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void TestDiffArgs()
+		{
+		    CaseInsensitiveComparer.Default.Compare(a,b);
+		}
+
+		[Test]
+		public void TestNull1()
+		{
+			string a = null;
+			string b = "5";
+
+			AssertEquals("#04 Failed",Comparer.Default.Compare(a,b),CaseInsensitiveComparer.Default.Compare(a,b));
+		}
+
+		[Test]
+		public void TestNull2()
+		{
+			string a = null;
+			string b = null;
+
+			AssertEquals("#05 Failed",Comparer.Default.Compare(a,b),CaseInsensitiveComparer.Default.Compare(a,b));
+		}
+		
+		[Test]
+		public void TestStringsCaps()
+		{
+			string a = "AA";
+			string b = "aa";
+
+			Assert("#06 Failed",CaseInsensitiveComparer.Default.Compare(a,b),0);
+		}
+      
 	}
 
 }
