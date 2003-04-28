c831579fe6342965066c777ebf58bffc21c3b0bd
Alexandre Pigolkine <pigolkine@mono-cvs.ximian.com>
2003-04-28 18:28:45 +0000

    small improvements
    svn path=/trunk/mcs/; revision=14074

--- mcs/class/System.Windows.Forms/System.Windows.Forms/monogc.c
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/monogc.c
@@ -1,3 +1,13 @@
+//
+// monogc.c
+//
+// Author:
+// implementation started by John Sohn (jsohn@columbus.rr.com)
+// Alexandre Pigolkine (pigolkine@gmx.de)
+// Jonathan Gilbert
+//
+// (C) Ximian, Inc., 2002/2003
+//
 #include <windows.h>
 #include <stdio.h>
 #include <gc/gc.h>
@@ -8,6 +18,37 @@ int GC_finalize_on_demand = -1;
 void (* GC_finalizer_notifier)() = (void (*) GC_PROTO((void)))0;
 HMODULE moduleGC;
 
+typedef GC_PTR (*gc_malloc_ptrt)(size_t lb);
+typedef int (*gc_push_all_stack_ptrt)(GC_PTR b, GC_PTR t);
+typedef int (*gc_init_gcj_malloc_ptrt)(int mp_index, void * mp);
+typedef void* (* gc_gcj_malloc_ptrt)(size_t lb, void * ptr_to_struct_containing_descr);
+typedef int (*gc_make_descriptor_ptrt)(GC_bitmap b, size_t t);
+typedef int (*gc_should_invoke_finalizers_ptrt)(void);
+typedef void (*gc_register_finalizer_ptrt)(GC_PTR obj, GC_finalization_proc fn,
+        		                   GC_PTR cd, GC_finalization_proc *ofn, GC_PTR *ocd);
+typedef GC_PTR (*gc_malloc_atomic_ptrt)(size_t lb);
+typedef GC_PTR (*gc_realloc_ptrt)(GC_PTR old_object, size_t new_size_in_bytes);
+typedef GC_PTR (*gc_base_ptrt)(GC_PTR displaced_pointer);
+typedef GC_PTR (*gc_free_ptrt)(GC_PTR object_addr);
+typedef GC_PTR (*gc_gcollect_ptrt)();
+typedef size_t (*gc_get_heap_size_ptrt)();
+typedef int (*gc_invoke_finalizers_ptrt)();
+
+gc_malloc_ptrt             gc_malloc;
+gc_push_all_stack_ptrt     gc_push_all_stack;
+gc_init_gcj_malloc_ptrt    gc_init_gcj_malloc;
+gc_gcj_malloc_ptrt         gc_gcj_malloc;
+gc_make_descriptor_ptrt    gc_make_descriptor;
+gc_should_invoke_finalizers_ptrt  gc_should_invoke_finalizers;
+gc_register_finalizer_ptrt gc_register_finalizer;
+gc_malloc_atomic_ptrt      gc_malloc_atomic;
+gc_realloc_ptrt            gc_realloc;
+gc_base_ptrt               gc_base;
+gc_free_ptrt               gc_free;
+gc_gcollect_ptrt           gc_gcollect;
+gc_get_heap_size_ptrt      gc_get_heap_size;
+gc_invoke_finalizers_ptrt  gc_invoke_finalizers;
+
 static int printf_stub(const char *format,...)
 {
 	return 0;
@@ -17,60 +58,70 @@ static int printf_stub(const char *format,...)
 
 void InitGC ()
 {
+	printf_stub(0); // suppress warning
 	if(moduleGC == 0){
 		printf ("Initializing Boehm GC library...\n");
 		moduleGC = LoadLibraryA ("gc.dll");
 
-		if (moduleGC == 0) {
+		if (moduleGC == NULL) {
+			printf ("Boehm GC library cannot be loaded");
 			exit (1);
 		}
-	}
 
+		gc_malloc = (gc_malloc_ptrt)GetProcAddress (moduleGC, "GC_malloc");
+		gc_push_all_stack = (gc_push_all_stack_ptrt) GetProcAddress (moduleGC, "GC_push_all_stack");
+		gc_init_gcj_malloc = (gc_init_gcj_malloc_ptrt) GetProcAddress (moduleGC, "GC_init_gcj_malloc");
+		gc_gcj_malloc = (gc_gcj_malloc_ptrt) GetProcAddress (moduleGC, "GC_gcj_malloc");
+		gc_make_descriptor = (gc_make_descriptor_ptrt) GetProcAddress (moduleGC, "GC_make_descriptor");
+		gc_should_invoke_finalizers = (gc_should_invoke_finalizers_ptrt) 
+					GetProcAddress (moduleGC, "GC_should_invoke_finalizers");
+		gc_register_finalizer =
+			(gc_register_finalizer_ptrt)GetProcAddress (moduleGC, "GC_register_finalizer");
+		gc_malloc_atomic = (gc_malloc_atomic_ptrt)GetProcAddress (moduleGC, "GC_malloc_atomic");
+		gc_realloc = (gc_realloc_ptrt)GetProcAddress (moduleGC, "GC_realloc");
+		gc_base = (gc_base_ptrt)GetProcAddress (moduleGC, "GC_base");
+		gc_free = (gc_free_ptrt) GetProcAddress (moduleGC, "GC_free");
+		gc_gcollect = (gc_gcollect_ptrt) GetProcAddress (moduleGC, "GC_gcollect");
+		gc_get_heap_size = (gc_get_heap_size_ptrt) GetProcAddress (moduleGC, "GC_get_heap_size");
+		gc_invoke_finalizers =
+			(gc_invoke_finalizers_ptrt) GetProcAddress (moduleGC, "GC_invoke_finalizers");
+
+		printf("done!\n");
+	}
 }
 
 GC_PTR GC_malloc (size_t lb)
 {
-	GC_PTR (*gc_malloc)(size_t lb);
 	GC_PTR status = NULL;
 	InitGC ();
-	gc_malloc = GetProcAddress (moduleGC, "GC_malloc");
 
 	TRACE ("GC_malloc start\n");
 	status = gc_malloc (lb);
-	TRACE ("GC_malloc end status %p\n",status);
+	TRACE ("GC_malloc end\n");
 	return status;
 }
 
 void GC_push_all_stack (GC_PTR b, GC_PTR t)
 {
-	int (*gc_push_all_stack)(GC_PTR b, GC_PTR t);
 	InitGC ();
 	TRACE ("GC_push_all_stack\n");
-	gc_push_all_stack =
-		GetProcAddress (moduleGC, "GC_push_all_stack");
 	gc_push_all_stack (b,t);
 	TRACE ("GC_push_all_stack end\n");
 }
 
 void GC_init_gcj_malloc(int mp_index, void * /* really GC_mark_proc */mp)
 {
-	int (*gc_init_gcj_malloc)(int mp_index, void * mp);
 	InitGC ();
 	TRACE ("GC_init_gcj_malloc\n");
-	gc_init_gcj_malloc =
-		GetProcAddress (moduleGC, "GC_init_gcj_malloc");
 	gc_init_gcj_malloc (mp_index,mp);
 	TRACE ("GC_init_gcj_malloc end\n");
 }
 
 void * GC_gcj_malloc(size_t lb, void * ptr_to_struct_containing_descr)
 {
-	void* (* gc_gcj_malloc)(size_t lb, void * ptr_to_struct_containing_descr);
 	void *result = 0;
 	InitGC ();
 	TRACE ("gc_gcj_malloc bytes %d\n", lb);
-	gc_gcj_malloc =
-		GetProcAddress (moduleGC, "GC_gcj_malloc");
 	result = gc_gcj_malloc (lb,ptr_to_struct_containing_descr);
 	TRACE ("gc_gcj_malloc end %p\n", result);
 	return result;
@@ -78,12 +129,9 @@ void * GC_gcj_malloc(size_t lb, void * ptr_to_struct_containing_descr)
 
 GC_descr GC_make_descriptor(GC_bitmap bm, size_t len)
 {
-	int (*gc_make_descriptor)(GC_bitmap b, size_t t);
 	GC_descr result = 0;
 	InitGC ();
 	TRACE ("GC_make_descriptor\n");
-	gc_make_descriptor =
-		GetProcAddress (moduleGC, "GC_make_descriptor");
 	result = gc_make_descriptor (bm,len);
 	TRACE ("GC_make_descriptor end\n");
 	return result;
@@ -91,12 +139,9 @@ GC_descr GC_make_descriptor(GC_bitmap bm, size_t len)
 
 int GC_should_invoke_finalizers(void)
 {
-	int (*gc_should_invoke_finalizers)(void);
 	int result = 0;
 	InitGC ();
 	TRACE ("GC_should_invoke_finalizers\n");
-	gc_should_invoke_finalizers =
-		GetProcAddress (moduleGC, "GC_should_invoke_finalizers");
 	result = gc_should_invoke_finalizers ();
 	TRACE ("GC_should_invoke_finalizers end\n");
 	return result;
@@ -105,12 +150,8 @@ int GC_should_invoke_finalizers(void)
 void GC_register_finalizer (GC_PTR obj, GC_finalization_proc fn, GC_PTR cd,
 		GC_finalization_proc *ofn, GC_PTR *ocd)
 {
-	void (*gc_register_finalizer)(GC_PTR obj, GC_finalization_proc fn,
-			GC_PTR cd, GC_finalization_proc *ofn, GC_PTR *ocd);
 	InitGC ();
 	TRACE ("GC_register_finalizer\n");
-	gc_register_finalizer =
-		GetProcAddress (moduleGC, "GC_register_finalizer");
 	gc_register_finalizer (obj, fn, cd, ofn, ocd);
 	TRACE ("GC_register_finalizer end\n");
 }
@@ -119,14 +160,9 @@ void GC_register_finalizer_no_order (GC_PTR obj, GC_finalization_proc fn,
 				     GC_PTR cd, GC_finalization_proc *ofn,
 				     GC_PTR *ocd)
 {
-	void (*gc_register_finalizer_no_order)(GC_PTR obj,
-			GC_finalization_proc fn, GC_PTR cd,
-			GC_finalization_proc *ofn, GC_PTR *ocd);
 	InitGC ();
-	gc_register_finalizer_no_order =
-		GetProcAddress (moduleGC, "GC_register_finalizer");
 	TRACE ("GC_register_finalizer_no_order\n");
-	gc_register_finalizer_no_order (obj, fn, cd, ofn, ocd);
+	gc_register_finalizer (obj, fn, cd, ofn, ocd);
 	TRACE ("GC_register_finalizer_no_order end\n");
 }
 
@@ -138,11 +174,8 @@ GC_PTR GC_debug_malloc (size_t size_in_bytes, GC_EXTRA_PARAMS)
 
 GC_PTR GC_malloc_atomic (size_t size_in_bytes)
 {
-	GC_PTR (*gc_malloc_atomic)(size_t lb);
 	GC_PTR status = NULL;
 	InitGC ();
-	gc_malloc_atomic = GetProcAddress (moduleGC, "GC_malloc_atomic");
-
 	TRACE ("GC_malloc_atomic start\n");
 	status = gc_malloc_atomic (size_in_bytes);
 	TRACE ("GC_malloc_atomic end\n");
@@ -151,11 +184,8 @@ GC_PTR GC_malloc_atomic (size_t size_in_bytes)
 
 GC_PTR GC_realloc (GC_PTR old_object, size_t new_size_in_bytes)
 {
-	GC_PTR (*gc_realloc)(GC_PTR old_object, size_t new_size_in_bytes);
 	GC_PTR status = NULL;
 	InitGC ();
-	gc_realloc = GetProcAddress (moduleGC, "GC_realloc");
-
 	TRACE ("GC_realloc start\n");
 	status = gc_realloc (old_object, new_size_in_bytes);
 	TRACE ("GC_realloc end\n");
@@ -164,12 +194,9 @@ GC_PTR GC_realloc (GC_PTR old_object, size_t new_size_in_bytes)
 
 GC_PTR GC_base (GC_PTR displaced_pointer)
 {
-	GC_PTR (*gc_base)(GC_PTR displaced_pointer);
 	GC_PTR status = NULL;
 	InitGC ();
 
-	TRACE ("in GC_base\n");
-	gc_base = GetProcAddress (moduleGC, "GC_base");
 	TRACE ("GC_base start\n");
 	status = gc_base (displaced_pointer);
 	TRACE ("GC_base end\n");
@@ -178,32 +205,25 @@ GC_PTR GC_base (GC_PTR displaced_pointer)
 
 void GC_free (GC_PTR object_addr)
 {
-	GC_PTR (*gc_free)(GC_PTR object_addr);
 	InitGC ();
 	TRACE ("GC_free start\n");
-	gc_free = GetProcAddress (moduleGC, "GC_free");
 	gc_free (object_addr);
 	TRACE ("GC_free end\n");
 }
 
 void GC_gcollect ()
 {
-	GC_PTR (*gc_gcollect)();
 	InitGC ();
 	TRACE ("GC_gcollect start\n");
-	gc_gcollect = GetProcAddress (moduleGC, "GC_gcollect");
 	gc_gcollect ();
 	TRACE ("GC_gcollect end\n");
 }
 
 size_t GC_get_heap_size ()
 {
-	size_t (*gc_get_heap_size)();
 	size_t status = 0;
 	InitGC ();
 
-	TRACE ("in GC_get_heap_size\n");
-	gc_get_heap_size = GetProcAddress (moduleGC, "GC_get_heap_size");
 	TRACE ("GC_get_heap_size start\n");
 	status = gc_get_heap_size ();
 	TRACE ("GC_get_heap_size end\n");
@@ -212,11 +232,9 @@ size_t GC_get_heap_size ()
 
 int GC_invoke_finalizers ()
 {
-	int (*gc_invoke_finalizers)();
 	int status = 0;
 	InitGC ();
 
-	gc_invoke_finalizers = GetProcAddress (moduleGC, "GC_invoke_finalizers");
 	TRACE ("GC_invoke_finalizers start\n");
 	status = gc_invoke_finalizers ();
 	TRACE ("GC_invoke_finalizers end\n");
@@ -261,5 +279,3 @@ int GC_pthread_detach (pthread_t thread)
 	TRACE ("GC_pthread_detach\n");
 	return pthread_detach (thread);
 }
-
-
