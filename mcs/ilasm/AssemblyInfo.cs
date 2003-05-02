880551b90b21f9236222a403fc143e51058ab2da
Jackson Harper <jackson@novell.com>
2003-05-02 14:49:19 +0000

    * AssemblyInfo.cs: Bump up version number
    svn path=/trunk/mcs/; revision=14210

--- mcs/ilasm/AssemblyInfo.cs
+++ mcs/ilasm/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.0.4")]
+[assembly: AssemblyVersion("0.0.5")]
 [assembly: AssemblyTitle ("Mono ILasm Compiler")]
 [assembly: AssemblyDescription ("Mono ILasm Compiler")]
 [assembly: AssemblyCopyright ("Sergey Chaban and Jackson Harper")]
