e76a490f7e9abb2307f6b23e1038d5eafa6c0af1
Miguel de Icaza <miguel@gnome.org>
2003-05-06 19:03:08 +0000

    Improve test
    svn path=/trunk/mcs/; revision=14342

--- mcs/tests/test-128.cs
+++ mcs/tests/test-128.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 public class SimpleAttribute : Attribute {
 
@@ -8,6 +9,38 @@ public class SimpleAttribute : Attribute {
 	{
 		n = name;
 	}
+}
+
+[AttributeUsage (AttributeTargets.All)]
+public class MineAttribute : Attribute {
+        public MineAttribute (params Type [] t)
+        {
+                types = t;
+        }
+        public Type[] types;
+}
+
+[Mine(typeof(int), typeof (string), typeof(object[]))]
+public class Foo {
+        public static int MM ()
+        {
+                object[] attrs = typeof (Foo).GetCustomAttributes (typeof(MineAttribute), true);
+                MineAttribute ma = (MineAttribute) attrs [0];
+                if (ma.types [0] != typeof (int)){
+                        Console.WriteLine ("failed");
+			return 1;
+		}
+                if (ma.types [1] != typeof (string)){
+                        Console.WriteLine ("failed");
+			return 2;
+		}
+                if (ma.types [2] != typeof (object [])){
+                        Console.WriteLine ("failed");
+			return 3;
+		}
+		Console.WriteLine ("OK");
+                return 0;
+        }
 }
 
 public class Blah {
@@ -40,7 +73,7 @@ public class Blah {
 		// attributes have actually been applied etc.
 		//
 
-		return 0;
+		return Foo.MM ();
 	}
 
 }
