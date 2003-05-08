0d5a6bfb8a317c9d22e4cec6bfbed9d303a60cea
Jackson Harper <jackson@novell.com>
2003-05-08 02:56:52 +0000

    * TypeInstr.cs: New file - implementation of instructions that 	take a single type instruction.
    svn path=/trunk/mcs/; revision=14380

---
+++ mcs/ilasm/codegen/TypeInstr.cs
@@ -0,0 +1,36 @@
+//
+// Mono.ILASM.TypeInstr
+//
+// Author(s):
+//  Jackson Harper (Jackson@LatitudeGeo.com)
+//
+// (C) 2003 Jackson Harper, All rights reserved
+//
+
+
+using System;
+
+namespace Mono.ILASM {
+
+        public class TypeInstr : IInstr {
+
+                private PEAPI.TypeOp op;
+                private ITypeRef operand;
+
+                public TypeInstr (PEAPI.TypeOp op, ITypeRef operand)
+                {
+                        this.op = op;
+                        this.operand = operand;
+                }
+
+                public void Emit (CodeGen code_gen, PEAPI.CILInstructions cil)
+                {
+                        operand.Resolve (code_gen);
+
+                        cil.TypeInst (op, operand.PeapiType);
+                }
+
+        }
+
+}
+
