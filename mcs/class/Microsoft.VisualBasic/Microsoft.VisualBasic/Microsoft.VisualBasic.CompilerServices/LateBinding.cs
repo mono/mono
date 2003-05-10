94bc5ac1b0e1a86bd3c3b7f7066c8d483f1d2e5a
Marco Ridoni <marco@mono-cvs.ximian.com>
2003-05-10 10:42:11 +0000

    First implementation for LateIndexGet/Set. Probably missing several special cases
    svn path=/trunk/mcs/; revision=14450

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/LateBinding.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/LateBinding.cs
@@ -3,16 +3,20 @@
 //
 // Author:
 //   Chris J Breisch (cjbreisch@altavista.net) 
+//   Marco Ridoni    (marco.ridoni@virgilio.it)
 //
 // (C) 2002 Chris J Breisch
+// (C) 2003 Marco Ridoni
 //
 
 using System;
+using System.Reflection;
+using Microsoft.VisualBasic;
 
-namespace Microsoft.VisualBasic.CompilerServices 
+namespace Microsoft.VisualBasic.CompilerServices
 {
-	[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)] 
-	[Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute] 
+	[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+	[Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute]
 	sealed public class LateBinding {
 		// Declarations
 		// Constructors
@@ -33,18 +37,125 @@ namespace Microsoft.VisualBasic.CompilerServices
 		[MonoTODO]
 		[System.Diagnostics.DebuggerStepThroughAttribute] 
 		[System.Diagnostics.DebuggerHiddenAttribute] 
-		public static System.Object LateIndexGet (System.Object o, System.Object[] args, System.String[] paramnames) { throw new NotImplementedException (); }
+		public static System.Object LateIndexGet (System.Object o, System.Object[] args, System.String[] paramnames)
+		{
+			Type objType;
+			Object binderState = null;
+	
+			if (o == null || args == null)
+				throw new ArgumentException();
+	
+			objType = o.GetType();
+			if (objType.IsArray) {
+				Array a = (Array) o;
+				int[] idxs = new int[args.Length];
+				Array.Copy (args, idxs, args.Length);
+	
+				return a.GetValue(idxs);
+			}
+			else
+			{
+				MemberInfo[] defaultMembers = objType.GetDefaultMembers();
+				if (defaultMembers == null)
+					throw new Exception();  // FIXME: Which exception should we throw?
+					
+				// We try to find a default method/property/field we can invoke/use
+				VBBinder MyBinder = new VBBinder();
+				BindingFlags bindingFlags = BindingFlags.IgnoreCase |
+						BindingFlags.Instance |
+						BindingFlags.Static |
+						BindingFlags.Public |
+						BindingFlags.GetProperty |
+						BindingFlags.GetField |
+						BindingFlags.InvokeMethod;
+	
+				MethodBase[] mb = new MethodBase[defaultMembers.Length];
+				try {
+					for (int x = 0; x < defaultMembers.Length; x++)
+						if (defaultMembers[x].MemberType == MemberTypes.Property)
+							mb[x] = ((PropertyInfo) defaultMembers[x]).GetGetMethod();
+						else
+							mb[x] = (MethodBase) defaultMembers[x];
+				} catch (Exception e) {	}
+	
+				MethodBase TheMethod = MyBinder.BindToMethod (bindingFlags,
+										mb,
+										ref args,
+										null,
+										null,
+										paramnames,
+										out binderState);
+				if (TheMethod == null)
+					throw new TargetInvocationException(new ArgumentNullException());
+				
+				return TheMethod.Invoke (o, args);		
+			}
+		}
 		[MonoTODO]
-		[System.Diagnostics.DebuggerHiddenAttribute] 
-		[System.Diagnostics.DebuggerStepThroughAttribute] 
+		[System.Diagnostics.DebuggerHiddenAttribute]
+		[System.Diagnostics.DebuggerStepThroughAttribute]
 		public static void LateIndexSetComplex (System.Object o, System.Object[] args, System.String[] paramnames, System.Boolean OptimisticSet, System.Boolean RValueBase) { throw new NotImplementedException (); }
 		[MonoTODO]
-		[System.Diagnostics.DebuggerStepThroughAttribute] 
-		[System.Diagnostics.DebuggerHiddenAttribute] 
-		public static void LateIndexSet (System.Object o, System.Object[] args, System.String[] paramnames) { throw new NotImplementedException (); }
+		[System.Diagnostics.DebuggerStepThroughAttribute]
+		[System.Diagnostics.DebuggerHiddenAttribute]
+		public static void LateIndexSet (System.Object o, System.Object[] args, System.String[] paramnames) 
+		{
+			Type objType;
+			Object binderState = null;
+			Object myValue;
+
+			if (o == null || args == null)
+				throw new ArgumentException();
+	
+			myValue = args[args.Length - 1];
+			objType = o.GetType();
+			if (objType.IsArray) {
+				Array a = (Array) o;
+				int[] idxs = new int[args.Length - 1];
+				Array.Copy (args, idxs, args.Length -1);
+				a.SetValue(myValue, idxs);
+			}
+			else
+			{
+				MemberInfo[] defaultMembers = objType.GetDefaultMembers();
+				if (defaultMembers == null)
+					throw new Exception();  // FIXME: Which exception should we throw?
+									
+				// We try to find a default method/property/field we can invoke/use
+				VBBinder MyBinder = new VBBinder();
+				BindingFlags bindingFlags = BindingFlags.IgnoreCase |
+						BindingFlags.Instance |
+						BindingFlags.Static |
+						BindingFlags.Public |
+						BindingFlags.GetProperty |
+						BindingFlags.GetField |
+						BindingFlags.InvokeMethod;
+
+				MethodBase[] mb = new MethodBase[defaultMembers.Length];
+				try {
+					for (int x = 0; x < defaultMembers.Length; x++)
+						if (defaultMembers[x].MemberType == MemberTypes.Property)
+							mb[x] = ((PropertyInfo) defaultMembers[x]).GetSetMethod();
+						else
+							mb[x] = (MethodBase) defaultMembers[x];
+				} catch (Exception e) {	}
+	
+				MethodBase TheMethod = MyBinder.BindToMethod (bindingFlags,
+										mb,
+										ref args,
+										null,
+										null,
+										paramnames,
+										out binderState);
+				if (TheMethod == null)
+					throw new TargetInvocationException(new ArgumentNullException());
+				
+				TheMethod.Invoke (o, args);	
+			}	
+		}
 		[MonoTODO]
-		[System.Diagnostics.DebuggerStepThroughAttribute] 
-		[System.Diagnostics.DebuggerHiddenAttribute] 
+		[System.Diagnostics.DebuggerStepThroughAttribute]
+		[System.Diagnostics.DebuggerHiddenAttribute]
 		public static void LateCall (System.Object o, System.Type objType, System.String name, System.Object[] args, System.String[] paramnames, System.Boolean[] CopyBack) { throw new NotImplementedException (); }
 		// Events
 	};
