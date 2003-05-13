dc91409cfd0d52cb7c62a0066b11aeba50c4652d
Nick Drochak <nickd@mono-cvs.ximian.com>
2003-05-13 15:28:26 +0000

    2003-05-14  Nick Drochak  <ndrochak@gol.com>
    	* UTF7EncodingTest.cs:
    	* UTF8EncodingTest.cs: added from Patrick Kalkman <kalkman@cistron.nl>
    
    svn path=/trunk/mcs/; revision=14538

---
+++ mcs/class/corlib/Test/System.Text/UTF7EncodingTest.cs
@@ -0,0 +1,167 @@
+﻿// UTF7EncodingTest.cs - NUnit Test Cases for System.Text.UTF7Encoding
+//
+// Patrick Kalkman  kalkman@cistron.nl
+//
+// (C) 2003 Patrick Kalkman
+// 
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace MonoTests.System.Text
+{
+
+        [TestFixture]
+        public class UTF7EncodingTest 
+        {
+                [SetUp]
+                public void GetReady() {}
+                
+                [TearDown]
+                public void Clean() {}
+                
+                [Test]
+                public void TestDirectlyEncoded1() 
+                {
+                        // Unicode characters a-z, A-Z, 0-9 and '()_./:? are directly encoded.
+                        string UniCodeString = "\u0061\u007A\u0041\u005A\u0030\u0039\u0027\u003F";
+                        byte[] UTF7Bytes = null;
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        
+                        UTF7Bytes = UTF7enc.GetBytes (UniCodeString);
+                        
+                        Assertion.AssertEquals ("UTF7 #1", 0x61, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #2", 0x7A, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #3", 0x41, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #4", 0x5A, UTF7Bytes [3]);
+                        Assertion.AssertEquals ("UTF7 #5", 0x30, UTF7Bytes [4]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x39, UTF7Bytes [5]);
+                        Assertion.AssertEquals ("UTF7 #7", 0x27, UTF7Bytes [6]);
+                        Assertion.AssertEquals ("UTF7 #8", 0x3F, UTF7Bytes [7]);
+                }
+        
+                [Test]
+                public void TestDirectlyEncoded2()
+                {
+                        // Unicode characters a-z, A-Z, 0-9 and '()_./:? are directly encoded.
+                        string UniCodeString = "\u0061\u007A\u0041\u005A\u0030\u0039\u0027\u003F";
+                        byte[] UTF7Bytes = new byte [8];
+                        int Length = UniCodeString.Length;
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        
+                        int Cnt = UTF7enc.GetBytes (UniCodeString.ToCharArray(), 0, Length, UTF7Bytes, 0);
+                
+		        Assertion.AssertEquals ("UTF7 #1", 0x61, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #2", 0x7A, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #3", 0x41, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #4", 0x5A, UTF7Bytes [3]);
+                        Assertion.AssertEquals ("UTF7 #5", 0x30, UTF7Bytes [4]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x39, UTF7Bytes [5]);
+                        Assertion.AssertEquals ("UTF7 #7", 0x27, UTF7Bytes [6]);
+                        Assertion.AssertEquals ("UTF7 #8", 0x3F, UTF7Bytes [7]);
+		}
+        
+                [Test]
+                public void TestEncodeOptionalEncoded()
+                {
+                        string UniCodeString = "\u0021\u0026\u002A\u003B";
+                        byte[] UTF7Bytes = null;
+                        
+                        //Optional Characters are allowed.	
+                        UTF7Encoding UTF7enc = new UTF7Encoding (true); 
+                        UTF7Bytes = UTF7enc.GetBytes (UniCodeString);
+                        
+                        Assertion.AssertEquals ("UTF7 #1", 0x21, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #2", 0x26, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #3", 0x2A, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #4", 0x3B, UTF7Bytes [3]);
+                        
+                        //Optional characters are not allowed.
+                        UTF7enc = new UTF7Encoding (false);
+                        UTF7Bytes = UTF7enc.GetBytes (UniCodeString);
+                        
+                        Assertion.AssertEquals ("UTF7 #5", 0x2B, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x41, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #7", 0x43, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #8", 0x45, UTF7Bytes [3]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x41, UTF7Bytes [1]);
+                }
+        
+                [Test]
+                public void TestEncodeUnicodeShifted1()
+                {
+                        string UniCodeString = "\u0041\u2262\u0391\u002E";
+                        byte[] UTF7Bytes = null;
+                        
+                        UTF7Encoding UTF7enc = new UTF7Encoding();
+                        UTF7Bytes = UTF7enc.GetBytes (UniCodeString);
+                        
+                        //"A<NOT IDENTICAL TO><ALPHA>." is encoded as A+ImIDkQ-. see RFC 1642
+                        Assertion.AssertEquals ("UTF7 #1", 0x41, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #2", 0x2B, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #3", 0x49, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #4", 0x6D, UTF7Bytes [3]);
+                        Assertion.AssertEquals ("UTF7 #5", 0x49, UTF7Bytes [4]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x44, UTF7Bytes [5]);
+                        Assertion.AssertEquals ("UTF7 #7", 0x6B, UTF7Bytes [6]);
+                        Assertion.AssertEquals ("UTF7 #8", 0x51, UTF7Bytes [7]);
+                        Assertion.AssertEquals ("UTF7 #9", 0x2D, UTF7Bytes [8]);
+                        Assertion.AssertEquals ("UTF7 #10", 0x2E, UTF7Bytes [9]);
+                }
+        
+                [Test]
+                public void TestEncodeUnicodeShifted2()
+                {
+                        string UniCodeString = "\u0041\u2262\u0391\u002E";
+                        byte[] UTF7Bytes = new byte [10];
+                        int Length = UniCodeString.Length;
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        
+                        int Cnt = UTF7enc.GetBytes (UniCodeString.ToCharArray(), 0, Length, UTF7Bytes, 0);
+                        
+                        //"A<NOT IDENTICAL TO><ALPHA>." is encoded as A+ImIDkQ-. see RFC 1642
+                        Assertion.AssertEquals ("UTF7 #1", 0x41, UTF7Bytes [0]);
+                        Assertion.AssertEquals ("UTF7 #2", 0x2B, UTF7Bytes [1]);
+                        Assertion.AssertEquals ("UTF7 #3", 0x49, UTF7Bytes [2]);
+                        Assertion.AssertEquals ("UTF7 #4", 0x6D, UTF7Bytes [3]);
+                        Assertion.AssertEquals ("UTF7 #5", 0x49, UTF7Bytes [4]);
+                        Assertion.AssertEquals ("UTF7 #6", 0x44, UTF7Bytes [5]);
+                        Assertion.AssertEquals ("UTF7 #7", 0x6B, UTF7Bytes [6]);
+                        Assertion.AssertEquals ("UTF7 #8", 0x51, UTF7Bytes [7]);
+                        Assertion.AssertEquals ("UTF7 #9", 0x2D, UTF7Bytes [8]);
+                        Assertion.AssertEquals ("UTF7 #10", 0x2E, UTF7Bytes [9]);
+                }
+        
+                [Test]
+                public void TestDecodeUnicodeShifted1()
+                {
+                        string UniCodeString = "\u0041\u2262\u0391\u002E";
+                        byte[] UTF7Bytes = new byte [] {0x41, 0x2B, 0x49, 0x6D, 0x49, 0x44, 0x6B,  0x51, 0x2D, 0x2E};
+                        
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        char[] UniCodeChars = UTF7enc.GetChars (UTF7Bytes);
+                        //A+ImIDkQ-. is decoded as "A<NOT IDENTICAL TO><ALPHA>." see RFC 1642
+                        Assertion.AssertEquals ("UTF #1", UniCodeString.ToCharArray() [0], UniCodeChars [0]);
+                        Assertion.AssertEquals ("UTF #2", UniCodeString.ToCharArray() [1], UniCodeChars [1]);
+                        Assertion.AssertEquals ("UTF #3", UniCodeString.ToCharArray() [2], UniCodeChars [2]);
+                        Assertion.AssertEquals ("UTF #4", UniCodeString.ToCharArray() [3], UniCodeChars [3]);
+                }
+        
+                [Test]
+                public void TestMaxCharCount()
+                {
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        Assertion.AssertEquals ("UTF #1", 50, UTF7enc.GetMaxCharCount(50));
+                }
+        
+                [Test]
+                public void TestMaxByteCount()
+                {
+                        UTF7Encoding UTF7enc = new UTF7Encoding ();
+                        Assertion.AssertEquals ("UTF #1", 136, UTF7enc.GetMaxByteCount(50));
+                }
+        }
+}
+
+
+
---
+++ mcs/class/corlib/Test/System.Text/UTF8EncodingTest.cs
@@ -0,0 +1,102 @@
+﻿// UTF8EncodingTest.cs - NUnit Test Cases for System.Text.UTF8Encoding
+//
+// Patrick Kalkman  kalkman@cistron.nl
+//
+// (C) 2003 Patrick Kalkman
+// 
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace MonoTests.System.Text
+{
+
+        [TestFixture]
+        public class UTF8EncodingTest 
+        {
+                [SetUp]
+                public void GetReady() {}
+                
+                [TearDown]
+                public void Clean() {}
+                
+                [Test]
+                public void TestEncodingGetBytes1()
+                {
+                        UTF8Encoding utf8Enc = new UTF8Encoding ();
+                        string UniCode = "\u0041\u2262\u0391\u002E";
+                        
+                        // "A<NOT IDENTICAL TO><ALPHA>." may be encoded as 41 E2 89 A2 CE 91 2E 
+                        // see (RFC 2044)
+                        byte[] utf8Bytes = utf8Enc.GetBytes (UniCode);
+                        
+                        Assertion.AssertEquals ("UTF #1", 0x41, utf8Bytes [0]);
+                        Assertion.AssertEquals ("UTF #2", 0xE2, utf8Bytes [1]);
+                        Assertion.AssertEquals ("UTF #3", 0x89, utf8Bytes [2]);
+                        Assertion.AssertEquals ("UTF #4", 0xA2, utf8Bytes [3]);
+                        Assertion.AssertEquals ("UTF #5", 0xCE, utf8Bytes [4]);
+                        Assertion.AssertEquals ("UTF #6", 0x91, utf8Bytes [5]);
+                        Assertion.AssertEquals ("UTF #7", 0x2E, utf8Bytes [6]);
+                }
+        
+                [Test]
+                public void TestEncodingGetBytes2()
+                {
+                        UTF8Encoding utf8Enc = new UTF8Encoding ();
+                        string UniCode = "\u0048\u0069\u0020\u004D\u006F\u006D\u0020\u263A\u0021";
+                        
+                        // "Hi Mom <WHITE SMILING FACE>!" may be encoded as 48 69 20 4D 6F 6D 20 E2 98 BA 21 
+                        // see (RFC 2044)
+                        byte[] utf8Bytes = new byte [11];
+                        
+                        int ByteCnt = utf8Enc.GetBytes (UniCode.ToCharArray(), 0, UniCode.Length, utf8Bytes, 0);
+                        
+                        Assertion.AssertEquals ("UTF #1", 11, ByteCnt);
+                        Assertion.AssertEquals ("UTF #2", 0x48, utf8Bytes [0]);
+                        Assertion.AssertEquals ("UTF #3", 0x69, utf8Bytes [1]);
+                        Assertion.AssertEquals ("UTF #4", 0x20, utf8Bytes [2]);
+                        Assertion.AssertEquals ("UTF #5", 0x4D, utf8Bytes [3]);
+                        Assertion.AssertEquals ("UTF #6", 0x6F, utf8Bytes [4]);
+                        Assertion.AssertEquals ("UTF #7", 0x6D, utf8Bytes [5]);
+                        Assertion.AssertEquals ("UTF #8", 0x20, utf8Bytes [6]);
+                        Assertion.AssertEquals ("UTF #9", 0xE2, utf8Bytes [7]);
+                        Assertion.AssertEquals ("UTF #10", 0x98, utf8Bytes [8]);
+                        Assertion.AssertEquals ("UTF #11", 0xBA, utf8Bytes [9]);
+                        Assertion.AssertEquals ("UTF #12", 0x21, utf8Bytes [10]);
+                }
+        
+                [Test]
+                public void TestDecodingGetChars1()
+                {
+                        UTF8Encoding utf8Enc = new UTF8Encoding ();
+                        // 41 E2 89 A2 CE 91 2E may be decoded as "A<NOT IDENTICAL TO><ALPHA>." 
+                        // see (RFC 2044)
+                        byte[] utf8Bytes = new byte [] {0x41, 0xE2, 0x89, 0xA2, 0xCE, 0x91, 0x2E};
+                        char[] UniCodeChars = utf8Enc.GetChars(utf8Bytes);
+                             
+                        Assertion.AssertEquals ("UTF #1", 0x0041, UniCodeChars [0]);
+                        Assertion.AssertEquals ("UTF #2", 0x2262, UniCodeChars [1]);
+                        Assertion.AssertEquals ("UTF #3", 0x0391, UniCodeChars [2]);
+                        Assertion.AssertEquals ("UTF #4", 0x002E, UniCodeChars [3]);
+                }
+                
+                [Test]
+                public void TestMaxCharCount()
+                {
+                        UTF8Encoding UTF8enc = new UTF8Encoding ();
+                        Assertion.AssertEquals ("UTF #1", 50, UTF8enc.GetMaxCharCount(50));
+                }
+        
+                [Test]
+                public void TestMaxByteCount()
+                {
+                        UTF8Encoding UTF8enc = new UTF8Encoding ();
+                        Assertion.AssertEquals ("UTF #1", 200, UTF8enc.GetMaxByteCount(50));
+                }
+
+                
+        }
+}
+
+
+
