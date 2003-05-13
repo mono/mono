64ebc6935247a09872b5134b05f489e006c00293
Sebastien Pouliot <sebastien@ximian.com>
2003-05-13 01:50:58 +0000

    2003-05-12  Sebastien Pouliot  <spouliot@videotron.ca>
    	* PKCS1MaskGenerationMethodTest.cs: Uncommented PKCS#1 v.2.1 vector
    	unit test (will work on Mono but fails with MS implementation).
    	* RSAOAEPKeyExchangeDeformatterTest.cs: Updated tests vectors for
    	both Mono and MS (under Windows XP) generated OAEP results.
    	* RSAOAEPKeyExchangeFormatterTest.cs: Update exception handling.
    
    svn path=/trunk/mcs/; revision=14524

--- mcs/class/corlib/Test/System.Security.Cryptography/RSAOAEPKeyExchangeFormatterTest.cs
+++ mcs/class/corlib/Test/System.Security.Cryptography/RSAOAEPKeyExchangeFormatterTest.cs
@@ -4,7 +4,7 @@
 // Author:
 //	Sebastien Pouliot (spouliot@motus.com)
 //
-// (C) 2002 Motus Technologies Inc. (http://www.motus.com)
+// (C) 2002, 2003 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using NUnit.Framework;
@@ -13,122 +13,120 @@ using System.Security.Cryptography;
 
 namespace MonoTests.System.Security.Cryptography {
 
-public class RSAOAEPKeyExchangeFormatterTest : TestCase {
-	protected static RSA key;
+	[TestFixture]
+	public class RSAOAEPKeyExchangeFormatterTest {
 
-	protected override void SetUp () 
-	{
-		// generating a keypair is REALLY long and the framework
-		// makes sure that we generate one (even if create an object
-		// to import an exsting key)
-		if (key == null) {
-			key = RSA.Create ();
-			key.ImportParameters (AllTests.GetRsaKey (true));
+		protected static RSA key;
+
+		[SetUp]
+		void SetUp () 
+		{
+			// generating a keypair is REALLY long and the MS framework
+			// makes sure that we generate one (even if create an object
+			// to import an existing key). Mono is smarter in this case
+			if (key == null) {
+				key = RSA.Create ();
+				key.ImportParameters (AllTests.GetRsaKey (true));
+			}
 		}
-	}
 
-	protected override void TearDown () {}
+		public void AssertEquals (string msg, byte[] array1, byte[] array2) 
+		{
+			AllTests.AssertEquals (msg, array1, array2);
+		}
 
-	public void AssertEquals (string msg, byte[] array1, byte[] array2) 
-	{
-		AllTests.AssertEquals (msg, array1, array2);
-	}
+		[Test]
+		public void Properties () 
+		{
+			RSAOAEPKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter ();
+			keyex.SetKey (key);
+			Assertion.AssertNull ("RSAOAEPKeyExchangeFormatter.Parameter", keyex.Parameter);
+			Assertion.AssertNull ("RSAOAEPKeyExchangeFormatter.Parameters", keyex.Parameters);
+			Assertion.AssertNull ("RSAOAEPKeyExchangeFormatter.Rng", keyex.Rng);
+			Assertion.AssertEquals ("RSAOAEPKeyExchangeFormatter.ToString()", "System.Security.Cryptography.RSAOAEPKeyExchangeFormatter", keyex.ToString ());
+		}
 
-	public void TestProperties () 
-	{
-		RSAOAEPKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter ();
-		keyex.SetKey (key);
-		AssertEquals("RSAOAEPKeyExchangeFormatter.Parameters", null, keyex.Parameters);
-		// null (default)
-		AssertNull("RSAOAEPKeyExchangeFormatter.Rng", keyex.Rng);
-		AssertEquals("RSAOAEPKeyExchangeFormatter.ToString()", "System.Security.Cryptography.RSAOAEPKeyExchangeFormatter", keyex.ToString ());
-	}
+		// ExchangeMin (1)
+		[Test]
+		public void ExchangeMin() 
+		{
+			AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
+			byte[] M = { 0x01 };
+			try {
+				byte[] EM = keyex.CreateKeyExchange (M);
+				AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
+				byte[] Mback = keyback.DecryptKeyExchange (EM);
+				AssertEquals ("RSAOAEPKeyExchangeFormatter Min", M, Mback);
+			}
+			catch (CryptographicException ce) {
+				// not supported by every version of Windows - Minimum: Windows XP
+				Console.WriteLine (ce.Message + " (" + Environment.OSVersion.ToString () + ")");
+			}
+		}
 
-	// TestExchangeMin (1)
-	public void TestExchangeMin() 
-	{
-		AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
-		byte[] M = { 0x01 };
-		try {
+		// test with a message 128 bits (16 bytes) long
+		[Test]
+		public void Exchange128() 
+		{
+			AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
+			byte[] M = { 0xd4, 0x36, 0xe9, 0x95, 0x69, 0xfd, 0x32, 0xa7, 0xc8, 0xa0, 0x5b, 0xbc, 0x90, 0xd3, 0x2c, 0x49 };
+			try {
+				byte[] EM = keyex.CreateKeyExchange (M);
+				AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
+				byte[] Mback = keyback.DecryptKeyExchange (EM);
+				AssertEquals ("RSAOAEPKeyExchangeFormatter 128", M, Mback);
+			}
+			catch (CryptographicException ce) {
+				// not supported by every version of Windows - Minimum: Windows XP
+				Console.WriteLine (ce.Message + " (" + Environment.OSVersion.ToString () + ")");
+			}
+		}
+
+		// test with a message 160 bits (20 bytes) long
+		[Test]
+		public void Exchange192() 
+		{
+			AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
+			byte[] M = { 0xd4, 0x36, 0xe9, 0x95, 0x69, 0xfd, 0x32, 0xa7, 0xc8, 0xa0, 0x5b, 0xbc, 0x90, 0xd3, 0x2c, 0x49, 0x00, 0x00, 0x00, 0x00 };
+			try {
+				byte[] EM = keyex.CreateKeyExchange (M);
+				AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
+				byte[] Mback = keyback.DecryptKeyExchange (EM);
+				AssertEquals ("RSAOAEPKeyExchangeFormatter 192", M, Mback);
+			}
+			catch (CryptographicException ce) {
+				// not supported by every version of Windows - Minimum: Windows XP
+				Console.WriteLine (ce.Message + " (" + Environment.OSVersion.ToString () + ")");
+			}
+		}
+
+		// Max = (key size in bytes) - 2 * (hash length) - 2
+		[Test]
+		public void ExchangeMax() 
+		{
+			AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
+			// use SHA1 internaly
+			byte[] M = new byte [(key.KeySize >> 3) - 2 * 20 - 2];
+			try {
+				byte[] EM = keyex.CreateKeyExchange (M);
+				AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
+				byte[] Mback = keyback.DecryptKeyExchange (EM);
+				AssertEquals ("RSAOAEPKeyExchangeFormatter Max", M, Mback);
+			}
+			catch (CryptographicException ce) {
+				// not supported by every version of Windows - Minimum: Windows XP
+				Console.WriteLine (ce.Message + " (" + Environment.OSVersion.ToString () + ")");
+			}
+		}
+
+		// TestExchangeTooBig
+		[Test]
+		[ExpectedException (typeof (CryptographicException))]
+		public void ExchangeTooBig() 
+		{
+			AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
+			byte[] M = new byte [(key.KeySize >> 3)- 10];
 			byte[] EM = keyex.CreateKeyExchange (M);
-			AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
-			byte[] Mback = keyback.DecryptKeyExchange (EM);
-			AssertEquals ("RSAOAEPKeyExchangeFormatter Min", M, Mback);
-		}
-		catch (CryptographicException) {
-			// not supported by every version of Windows
-			// Minimum: Windows 2000 + High Encryption Pack
 		}
 	}
-
-	// test with a message 128 bits (16 bytes) long
-	public void TestExchange128() 
-	{
-		AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
-		byte[] M = { 0xd4, 0x36, 0xe9, 0x95, 0x69, 0xfd, 0x32, 0xa7, 0xc8, 0xa0, 0x5b, 0xbc, 0x90, 0xd3, 0x2c, 0x49 };
-		try {
-			byte[] EM = keyex.CreateKeyExchange (M);
-			AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
-			byte[] Mback = keyback.DecryptKeyExchange (EM);
-			AssertEquals ("RSAOAEPKeyExchangeFormatter 128", M, Mback);
-		}
-		catch (CryptographicException) {
-			// not supported by every version of Windows
-			// Minimum: Windows 2000 + High Encryption Pack
-		}
-	}
-
-	// test with a message 160 bits (20 bytes) long
-	public void TestExchange192() 
-	{
-		AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
-		byte[] M = { 0xd4, 0x36, 0xe9, 0x95, 0x69, 0xfd, 0x32, 0xa7, 0xc8, 0xa0, 0x5b, 0xbc, 0x90, 0xd3, 0x2c, 0x49, 0x00, 0x00, 0x00, 0x00 };
-		try {
-			byte[] EM = keyex.CreateKeyExchange (M);
-			AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
-			byte[] Mback = keyback.DecryptKeyExchange (EM);
-			AssertEquals ("RSAOAEPKeyExchangeFormatter 192", M, Mback);
-		}
-		catch (CryptographicException) {
-			// not supported by every version of Windows
-			// Minimum: Windows 2000 + High Encryption Pack
-		}
-	}
-
-	// Max = (key size in bytes) - 2 * (hash length) - 2
-	public void TestExchangeMax() 
-	{
-		AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
-		// use SHA1 internaly
-		byte[] M = new byte [(key.KeySize >> 3) - 2 * 20 - 2];
-		try {
-			byte[] EM = keyex.CreateKeyExchange (M);
-			AsymmetricKeyExchangeDeformatter keyback = new RSAOAEPKeyExchangeDeformatter (key);
-			byte[] Mback = keyback.DecryptKeyExchange (EM);
-			AssertEquals ("RSAOAEPKeyExchangeFormatter Max", M, Mback);
-		}
-		catch (CryptographicException) {
-			// not supported by every version of Windows
-			// Minimum: Windows 2000 + High Encryption Pack
-		}
-	}
-
-	// TestExchangeTooBig
-	public void TestExchangeTooBig() 
-	{
-		AsymmetricKeyExchangeFormatter keyex = new RSAOAEPKeyExchangeFormatter (key);
-		byte[] M = new byte [(key.KeySize >> 3)- 10];
-		try {
-			byte[] EM = keyex.CreateKeyExchange (M);
-			Fail ("Expected CryptographicException but got none");
-		}
-		catch (CryptographicException) {
-			// this is what we expect
-		}
-		catch (Exception e) {
-			Fail ("Expected CryptographicException but got : " + e.ToString ());
-		}
-	}
-}
-
 }
