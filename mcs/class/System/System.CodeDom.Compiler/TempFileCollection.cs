7e502bc93687808a6641bc578b037df809047877
Dick Porter <dick@acm.org>
2003-05-16 12:15:07 +0000

    2003-05-16  Dick Porter  <dick@ximian.com>
    	* TempFileCollection.cs: Implement
    
    svn path=/trunk/mcs/; revision=14629

--- mcs/class/System/System.CodeDom.Compiler/TempFileCollection.cs
+++ mcs/class/System/System.CodeDom.Compiler/TempFileCollection.cs
@@ -2,15 +2,162 @@
 // System.CodeDom.Compiler TempFileCollection Class implementation
 //
 // Author:
-//   Daniel Stodden (stodden@in.tum.de)
+//	Dick Porter (dick@ximian.com)
 //
-// (C) 2002 Ximian, Inc.
+// (C) 2003 Ximian, Inc.
 //
+
+using System.IO;
+using System.Collections;
 
 namespace System.CodeDom.Compiler
 {
-	public class TempFileCollection
+	public class TempFileCollection:ICollection, IEnumerable, IDisposable
 	{
-		// FIXME
+		Hashtable filehash;
+		string tempdir;
+		bool keepfiles;
+		
+		public TempFileCollection(): this(null, false)
+		{
+		}
+
+		public TempFileCollection(string tempDir): this(tempDir, false)
+		{
+		}
+
+		public TempFileCollection(string tempDir, bool keepFiles)
+		{
+			filehash=new Hashtable();
+			tempdir=tempDir;
+			keepfiles=keepFiles;
+		}
+
+		private string basepath=null;
+		
+		public string BasePath
+		{
+			get {
+				if(basepath==null) {
+					if(tempdir==null) {
+						/* Get the system temp dir */
+						MonoIO.GetTempPath(out tempdir);
+					}
+
+					string random=new Random().Next(10000,99999).ToString();
+					
+					if(tempdir.EndsWith("\\") ||
+					   tempdir.EndsWith("/")) {
+						basepath=tempdir+random;
+					} else {
+						basepath=tempdir+"/"+random;
+					}
+				}
+
+				return(basepath);
+			}
+		}
+
+		public int Count
+		{
+			get {
+				return(filehash.Count);
+			}
+		}
+
+		public bool KeepFiles
+		{
+			get {
+				return(keepfiles);
+			}
+			set {
+				keepfiles=value;
+			}
+		}
+
+		public string TempDir
+		{
+			get {
+				if(tempdir==null) {
+					return(String.Empty);
+				} else {
+					return(tempdir);
+				}
+			}
+		}
+
+		public string AddExtension(string fileExtension)
+		{
+			return(AddExtension(fileExtension, keepfiles));
+		}
+
+		public string AddExtension(string fileExtension, bool keepFile)
+		{
+			string filename=BasePath+"."+fileExtension;
+			AddFile(filename, keepFile);
+			return(filename);
+		}
+
+		public void AddFile(string fileName, bool keepFile)
+		{
+			filehash.Add(fileName, keepFile);
+		}
+
+		public void CopyTo(string[] fileNames, int start)
+		{
+			filehash.Keys.CopyTo(fileNames, start);
+		}
+
+		void ICollection.CopyTo(Array array, int start)
+		{
+			filehash.Keys.CopyTo(array, start);
+		}
+
+		[MonoTODO("What goes here?")]
+		object ICollection.SyncRoot {
+			get {
+				return(null);
+			}
+		}
+
+		bool ICollection.IsSynchronized {
+			get {
+				return(false);
+			}
+		}
+		
+		void IDisposable.Dispose() 
+		{
+			Dispose(true);
+		}
+		
+		public void Delete()
+		{
+			string[] filenames=new string[filehash.Count];
+			filehash.Keys.CopyTo(filenames, 0);
+
+			foreach(string file in filenames) {
+				if((bool)filehash[file]==true) {
+					File.Delete(file);
+					filehash.Remove(file);
+				}
+			}
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return(filehash.Keys.GetEnumerator());
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			Delete();
+		}
+
+		~TempFileCollection()
+		{
+			Dispose(false);
+		}
+		
 	}
 }
