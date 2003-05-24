e7f5c90645f07be191be732ea91e1aad9851375e
Joel Basson <joel@mono-cvs.ximian.com>
2003-05-24 15:13:33 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=14845

---
+++ mcs/class/System.Windows.Forms/Gtk/MessageBoxButtons.cs
@@ -0,0 +1,18 @@
+//		
+//			System.Windows.Forms.ComboBox
+//
+//			Author: 
+//						Joel Basson		(jstrike@mweb.co.za)
+//
+//
+
+using System;
+
+namespace System.Windows.Forms
+{
+	public enum MessageBoxButtons{
+
+		OK, OKCancel, RetryCancel, YesNo, YesNoCancel, AbortRetryIgnore
+
+	}
+}
