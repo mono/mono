92a430bf6d9a18fc8343cfa1ffed299356808e2a
Joel Basson <joel@mono-cvs.ximian.com>
2003-05-24 15:29:49 +0000

    *** empty log message ***
    svn path=/trunk/mcs/; revision=14849

--- mcs/class/System.Windows.Forms/Gtk/Demo2.cs
+++ mcs/class/System.Windows.Forms/Gtk/Demo2.cs
@@ -16,10 +16,16 @@ namespace demo2
 	public class GtkForm : System.Windows.Forms.Form
   	{
 	    	private Button button1 = new Button(); 
-			private Button button2 = new Button();
-			private ColorDialog color1 = new ColorDialog();
-			private ComboBox combo1 = new ComboBox();
-
+		private Button button2 = new Button();
+		private ColorDialog color1 = new ColorDialog();
+		private ComboBox combo1 = new ComboBox();
+		private MainMenu mainMenu1 = new MainMenu();
+		private MenuItem menuItem1 = new MenuItem();
+		private MenuItem menuItem2 = new MenuItem();
+		private MenuItem menuItem3 = new MenuItem();
+		private MenuItem menuItem4 = new MenuItem();
+		private MenuItem menuItem5 = new MenuItem();
+ 
 	    	private void InitializeWidgets()
 	    	{
     		  	button1.Location = new Point(150, 28);
@@ -29,14 +35,25 @@ namespace demo2
     	  		button1.Click += new EventHandler(this.button1_Click);    
          	button1.Enabled = true;
 
-  				button2.Location = new Point(150, 80);
+  			button2.Location = new Point(150, 80);
        	  	button2.Name = "button2";
     	  	  	button2.Size = new Size(128, 44);
        	 	button2.Text = "Add to ComboBox";
     	  		button2.Click += new EventHandler(this.button2_Click);    
          	button2.Enabled = true;
-				
-				combo1.Location = new Point(150, 150);
+
+		menuItem1.Text = "File";
+		menuItem2.Text = "Exit";
+		menuItem3.Text = "Edit";
+		menuItem4.Text = "Stop";
+		menuItem5.Text = "Start";
+		menuItem1.MenuItems.Add(menuItem2);
+		menuItem3.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {menuItem4,
+							   menuItem5});	
+   		mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {menuItem1,
+							menuItem3});
+
+		combo1.Location = new Point(150, 150);
 		this.combo1.Items.AddRange(new object[] {"Item 1",
                         "Item 2",
                         "Item 3",
@@ -48,7 +65,8 @@ namespace demo2
 									this.button2,
 									this.color1,
 									this.combo1});
-          this.Size = new Size(512, 250);
+          	this.Size = new Size(512, 250);
+		this.Menu = mainMenu1;	
     		}
     	
     	public GtkForm()
@@ -62,7 +80,10 @@ namespace demo2
   		}
 
 		private void button2_Click(object sender, EventArgs e){ 
-			combo1.Items.Add("Joel");		
+			combo1.BeginUpdate();
+			for (int i = 1; i < 5; i++) {combo1.Items.Add("Joel");}
+			combo1.EndUpdate();	
+ 	
   		}
 
 	 }
