4f307969a8a02143fe527630632101aca84f41c5
Joel Basson <joel@mono-cvs.ximian.com>
2003-05-25 13:26:10 +0000

    2003-05-25  Joel Basson  <jstrike@mweb.co.za>
    * Shortcut.cs
    
    svn path=/trunk/mcs/; revision=14861

---
+++ mcs/class/System.Windows.Forms/Gtk/Shortcut.cs
@@ -0,0 +1,24 @@
+//		
+//			System.Windows.Forms.ComboBox
+//
+//			Author: 
+//						Joel Basson		(jstrike@mweb.co.za)
+//
+//
+
+using System;
+using Gtk;
+using GtkSharp;
+using Gdk;
+using GLib;
+using Gnome;
+
+namespace System.Windows.Forms
+{
+	public enum Shortcut : char {
+ 	
+		CtrlN 	
+
+
+	}
+}
