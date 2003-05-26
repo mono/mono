5fcaed74225d5104771858c513ea0781cb839870
Miguel de Icaza <miguel@gnome.org>
2003-05-26 18:45:46 +0000

    Improve test
    svn path=/trunk/mcs/; revision=14894

--- mcs/tests/unsafe-2.cs
+++ mcs/tests/unsafe-2.cs
@@ -8,6 +8,9 @@ unsafe class X {
 	{
 		char *ptr = stackalloc char [10];
 		int i;
+		long l = 0;
+		ulong ul = 0;
+		byte b = 0;
 		
 		for (i = 0; i < 10; i++)
 			ptr [i] = (char) (i + 10);
@@ -17,6 +20,16 @@ unsafe class X {
 				return 200 + i;
 			ptr++;
 		}
+
+
+		// Now test index access with longs
+		if (ptr [l] != 10)
+			return 1;
+		if (ptr [ul] != 10)
+			return 2;
+		if (ptr [b] != 10)
+			return 3;
+		
 		Console.WriteLine ("Ok");
 		return 0;
 	}
