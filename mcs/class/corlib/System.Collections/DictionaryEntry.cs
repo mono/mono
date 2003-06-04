74c9dc45547f9156f2c9cf8d91196760c5a67f28
Ben Maurer <benm@mono-cvs.ximian.com>
2003-06-04 22:29:05 +0000

    2003-06-04  Ben Maurer <bmaurer@users.sourceforge.net> 	* DictionaryEntry.cs: Throw exception if key == null. Fixes Rotor 	failures
    svn path=/trunk/mcs/; revision=15112

--- mcs/class/corlib/System.Collections/DictionaryEntry.cs
+++ mcs/class/corlib/System.Collections/DictionaryEntry.cs
@@ -1,3 +1,13 @@
+//
+// DictionaryEntry.cs
+//
+// Authors:
+// 	Paolo Molaro  (lupus@ximian.com)
+// 	Ben Maurer (bmaurer@users.sourceforge.net)
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+// (C) 2003 Ben Maurer
+
 using System;
 
 namespace System.Collections {
@@ -7,14 +17,21 @@ namespace System.Collections {
 		private object key;
 		private object val;
 
-		public DictionaryEntry( object k, object value) {
+		public DictionaryEntry (object k, object value) {
+			if (k == null)
+				throw new ArgumentNullException ("k");
+			
 			key = k;
 			val = value;
 		}
 		
 		public object Key {
 			get {return key;}
-			set {key = value;}
+			set {
+				if (value == null)
+					throw new ArgumentNullException ("value");
+				key = value;
+			}
 		}
 		public object Value {
 			get {return val;}
