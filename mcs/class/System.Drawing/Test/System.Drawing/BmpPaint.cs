d520d70774a88bd5c7f00eea8cf26d621111e75c
Alexandre Pigolkine <pigolkine@mono-cvs.ximian.com>
2003-06-06 22:02:48 +0000

    sample added
    svn path=/trunk/mcs/; revision=15171

---
+++ mcs/class/System.Drawing/Test/System.Drawing/BmpPaint.cs
@@ -0,0 +1,33 @@
+//
+// BmpPaint.cs test application
+//
+// Author:
+//   Alexandre Pigolkine(pigolkine@gmx.de)
+// 
+//
+// (C) Ximian, Inc.  http://www.ximian.com
+//
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class BmpPaint {
+	static public void Main( string[] args) {
+		Bitmap	bmp = new Bitmap(100,100);
+		Console.WriteLine("Bitmap created OK {0}", bmp != null);
+		if( bmp != null) {
+			Console.WriteLine("Bitmap Pixelformat {0}", bmp.PixelFormat);
+		}
+		Graphics gr = Graphics.FromImage(bmp);
+		Console.WriteLine("Graphics created OK {0}", gr != null);
+		if( gr != null) {
+			Pen p = new Pen(Color.Red, 2);
+			gr.DrawLine(p, 10.0F, 10.0F, 90.0F, 90.0F);
+			gr.DrawRectangle(p, 10.0F, 10.0F, 80.0F, 80.0F);
+			p.Dispose();
+		}
+		bmp.Save("file.bmp");
+		gr.Dispose();
+		bmp.Dispose();
+	}
+};
