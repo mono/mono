234536090b5655dc3c8896906e6ce9fc674a3cea
Jackson Harper <jackson@novell.com>
2003-06-09 02:58:45 +0000

    * EventDef.cs: Only add one method for addon, fire, other, and 	removeon
    svn path=/trunk/mcs/; revision=15216

--- mcs/ilasm/codegen/EventDef.cs
+++ mcs/ilasm/codegen/EventDef.cs
@@ -21,10 +21,10 @@ namespace Mono.ILASM {
                 private PEAPI.Event event_def;
                 private bool is_resolved;
 
-                private ArrayList addon_list;
-                private ArrayList fire_list;
-                private ArrayList other_list;
-                private ArrayList removeon_list;
+                private MethodRef addon;
+                private MethodRef fire;
+                private MethodRef other;
+                private MethodRef removeon;
 
                 public EventDef (FeatureAttr attr, ITypeRef type, string name)
                 {
@@ -58,65 +58,45 @@ namespace Mono.ILASM {
                         if (!is_resolved)
                                 Resolve (code_gen, classdef);
 
-                        if (addon_list != null) {
-                                foreach (MethodRef methodref in addon_list) {
-                                        methodref.Resolve (code_gen);
-                                        event_def.AddAddon ((PEAPI.MethodDef) methodref.PeapiMethod);
-                                }
+                        if (addon != null) {
+                                addon.Resolve (code_gen);
+                                event_def.AddAddon ((PEAPI.MethodDef) addon.PeapiMethod);
                         }
 
-                        if (fire_list != null) {
-                                foreach (MethodRef methodref in fire_list) {
-                                        methodref.Resolve (code_gen);
-                                        event_def.AddFire ((PEAPI.MethodDef) methodref.PeapiMethod);
-                                }
+                        if (fire != null) {
+                                fire.Resolve (code_gen);
+                                event_def.AddFire ((PEAPI.MethodDef) fire.PeapiMethod);
                         }
 
-                        if (other_list != null) {
-                                foreach (MethodRef methodref in other_list) {
-                                        methodref.Resolve (code_gen);
-                                        event_def.AddOther ((PEAPI.MethodDef) methodref.PeapiMethod);
-                                }
+                        if (other != null) {
+                                other.Resolve (code_gen);
+                                event_def.AddOther ((PEAPI.MethodDef) other.PeapiMethod);
                         }
 
-                        if (removeon_list != null) {
-                                foreach (MethodRef methodref in removeon_list) {
-                                        methodref.Resolve (code_gen);
-                                        event_def.AddRemoveOn ((PEAPI.MethodDef) methodref.PeapiMethod);
-                                }
+                        if (removeon != null) {
+                                removeon.Resolve (code_gen);
+                                event_def.AddRemoveOn ((PEAPI.MethodDef) removeon.PeapiMethod);
                         }
                 }
 
                 public void AddAddon (MethodRef method_ref)
                 {
-                        if (addon_list == null)
-                                addon_list = new ArrayList ();
-
-                        addon_list.Add (method_ref);
+                        addon = method_ref;
                 }
 
                 public void AddFire (MethodRef method_ref)
                 {
-                        if (fire_list == null)
-                                fire_list = new ArrayList ();
-
-                        fire_list.Add (method_ref);
+                        fire = method_ref;
                 }
 
                 public void AddOther (MethodRef method_ref)
                 {
-                        if (other_list == null)
-                                other_list = new ArrayList ();
-
-                        other_list.Add (method_ref);
+                        other = method_ref;
                 }
 
                 public void AddRemoveon (MethodRef method_ref)
                 {
-                        if (removeon_list == null)
-                                removeon_list = new ArrayList ();
-
-                        removeon_list.Add (method_ref);
+                        removeon = method_ref;
                 }
 
         }
