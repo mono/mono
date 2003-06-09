d215be3d88ee5f9448ef42b99d296f76990a8fef
César Natarén <cesar@mono-cvs.ximian.com>
2003-06-09 05:45:10 +0000

    added initial support for Semantic Analysis
    svn path=/trunk/mcs/; revision=15217

--- mcs/class/Microsoft.JScript/Microsoft.JScript/ASTList.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/ASTList.cs
@@ -1,7 +1,9 @@
 //
-// ASTList.cs:
+// ASTList.cs: Representation of a collection of source elements 
+//             that form an Ecmascript program.
 //
-// Author: Cesar Octavio Lopez Nataren
+// Author: 
+//	Cesar Octavio Lopez Nataren
 //
 // (C) 2003, Cesar Octavio Lopez Nataren, <cesar@ciencias.unam.mx>
 //
@@ -9,9 +11,40 @@
 namespace Microsoft.JScript
 {
 	using System.Collections;
+	using System.Text;
+	using System;
 
 	public class ASTList : AST
 	{
-		private ArrayList elems;
+		internal ArrayList elems;
+
+		internal ASTList ()
+		{
+			elems = new ArrayList ();
+		}
+
+
+		internal ASTList Add (AST elem)
+		{
+			elems.Add (elem);
+			return this;
+		}
+
+		
+		public override object Visit (Visitor v, object args)
+		{
+			return v.VisitASTList (this, args);
+		}
+
+
+		public override string ToString ()
+		{
+			StringBuilder sb = new StringBuilder ();
+
+			foreach (AST ast in elems)
+				sb.Append (ast.ToString () + "\n");
+
+			return sb.ToString ();
+		}
 	}
 }
