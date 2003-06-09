67980e78813b7602ee793ec33a02a5d21f43d8a1
César Natarén <cesar@mono-cvs.ximian.com>
2003-06-09 06:08:04 +0000

    header style correction
    svn path=/trunk/mcs/; revision=15220

--- mcs/class/Microsoft.JScript/Microsoft.JScript/FunctionObject.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/FunctionObject.cs
@@ -1,7 +1,10 @@
 //
 // FunctionObject.cs:
 //
-// Author: Cesar Octavio Lopez Nataren
+// Author:
+//	Cesar Octavio Lopez Nataren
+//
+// (C) 2003, Cesar Octavio Lopez Nataren, <cesar@ciencias.unam.mx>
 //
 
 namespace Microsoft.JScript
