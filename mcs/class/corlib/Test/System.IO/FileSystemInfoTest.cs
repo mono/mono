e4bdec4b6a43dce6a3abf1d31a2580ab8973a280
Nick Drochak <nickd@mono-cvs.ximian.com>
2003-06-18 03:09:41 +0000

    2003-06-18  Nick Drochak <ndrochak@gol.com>
    	* FileSystemInfoTest.cs: Works on .NET 1.1 now. If these values are
    	different on 1.0 then we need to wrap with a #if NET_1_1.
    
    svn path=/trunk/mcs/; revision=15469

--- mcs/class/corlib/Test/System.IO/FileSystemInfoTest.cs
+++ mcs/class/corlib/Test/System.IO/FileSystemInfoTest.cs
@@ -148,7 +148,7 @@ namespace MonoTests.System.IO
 				AssertEquals ("test#01", 1601, time.Year);
 				AssertEquals ("test#02", 1, time.Month);
 				AssertEquals ("test#03", 1, time.Day);
-				AssertEquals ("test#04", 2, time.Hour);
+				AssertEquals ("test#04", 9, time.Hour);
 				AssertEquals ("test#05", 0, time.Minute);
 				AssertEquals ("test#06", 0, time.Second);
 				AssertEquals ("test#07", 0, time.Millisecond);
@@ -159,7 +159,7 @@ namespace MonoTests.System.IO
 				AssertEquals ("test#08", 1601, time.Year);
 				AssertEquals ("test#09", 1, time.Month);
 				AssertEquals ("test#10", 1, time.Day);
-				AssertEquals ("test#11", 2, time.Hour);
+				AssertEquals ("test#11", 9, time.Hour);
 				AssertEquals ("test#12", 0, time.Minute);
 				AssertEquals ("test#13", 0, time.Second);
 				AssertEquals ("test#14", 0, time.Millisecond);
@@ -197,7 +197,7 @@ namespace MonoTests.System.IO
 				AssertEquals ("test#01", 1601, time.Year);
 				AssertEquals ("test#02", 1, time.Month);
 				AssertEquals ("test#03", 1, time.Day);
-				AssertEquals ("test#04", 2, time.Hour);
+				AssertEquals ("test#04", 9, time.Hour);
 				AssertEquals ("test#05", 0, time.Minute);
 				AssertEquals ("test#06", 0, time.Second);
 				AssertEquals ("test#07", 0, time.Millisecond);
@@ -263,7 +263,7 @@ namespace MonoTests.System.IO
 				AssertEquals ("test#01", 1601, time.Year);
 				AssertEquals ("test#02", 1, time.Month);
 				AssertEquals ("test#03", 1, time.Day);
-				AssertEquals ("test#04", 2, time.Hour);
+				AssertEquals ("test#04", 9, time.Hour);
 				AssertEquals ("test#05", 0, time.Minute);
 				AssertEquals ("test#06", 0, time.Second);
 				AssertEquals ("test#07", 0, time.Millisecond);
