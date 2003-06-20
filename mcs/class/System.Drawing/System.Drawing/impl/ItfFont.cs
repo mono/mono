a547e00d1dc9f47f40b28ecd3cc366564b43962e
Alexandre Pigolkine <pigolkine@mono-cvs.ximian.com>
2003-06-20 23:51:46 +0000

    properties added
    svn path=/trunk/mcs/; revision=15527

--- mcs/class/System.Drawing/System.Drawing/impl/ItfFont.cs
+++ mcs/class/System.Drawing/System.Drawing/impl/ItfFont.cs
@@ -1,5 +1,5 @@
 //
-// System.Drawing.Brush.cs
+// System.Drawing.IFont.cs
 //
 // Author:
 //   Alexandre Pigolkine (pigolkine@gmx.de)
@@ -10,13 +10,39 @@
 namespace System.Drawing 
 {
 	internal interface IFontFactory {
-		IFont Font(FontFamily family, float size, FontStyle style);
-		IFont Font(string familyName, float size, FontStyle style);
+		IFont Font(FontFamily family, float emSize, FontStyle style, GraphicsUnit unit, byte charSet, bool isVertical);
+		IFont Font(string familyName, float emSize, FontStyle style, GraphicsUnit unit, byte charSet, bool isVertical);
 		IFont FromHfont(IntPtr font);
 	}
 
 	internal interface IFont : IDisposable
 	{
 		IntPtr ToHfont ();
+		
+		bool Bold { get ; }
+		
+		FontFamily FontFamily { get ;}
+		
+		byte GdiCharSet { get; }
+		
+		bool GdiVerticalFont { get; }
+		
+		int Height { get; }
+
+		bool Italic { get; }
+
+		string Name { get; }
+
+		float Size { get; }
+
+		float SizeInPoints { get; }
+
+		bool Strikeout { get; }
+		
+		FontStyle Style { get; }
+
+		bool Underline { get; }
+
+		GraphicsUnit Unit { get; }
 	}
 }
