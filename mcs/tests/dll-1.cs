8f8683e01928c0fb74b543b1973f059ca473f7f8
Miguel de Icaza <miguel@gnome.org>
2003-06-29 05:40:13 +0000

    Add new Qt tests
    svn path=/trunk/mcs/; revision=15721

---
+++ mcs/tests/dll-1.cs
@@ -0,0 +1,10 @@
+namespace Qt
+{
+	public class QtSupport
+	{
+		protected static event MousePressEvent mousePressEvent;
+
+		protected delegate void MousePressEvent ();
+	}
+
+}
---
+++ mcs/tests/prog-1.cs
@@ -0,0 +1,28 @@
+namespace QtSamples
+{
+	using Qt;
+
+	public class QtClass: QtSupport
+	{
+		public QtClass()
+		{
+			mousePressEvent += new MousePressEvent( pressEvent );
+		}
+		
+		public void pressEvent() { }
+	}
+
+
+	public class Testing
+	{
+		public static int Main()
+		{
+			QtClass q = new QtClass();
+
+			return 0;
+		}
+	}
+}
+
+
+
