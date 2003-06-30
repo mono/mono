9decfe615a35e9f3bb32c26e0d09e8906297e3f6
Zoltan Varga <vargaz@gmail.com>
2003-06-30 14:56:48 +0000

    2003-06-30  Zoltan Varga  <vargaz@freemail.hu>
    	* run_test.sh: Added 'all' option to run all tests. Add class libraries
    	to MONO_PATH.
    
    svn path=/trunk/mcs/; revision=15757

--- mcs/class/corlib/Test/run_test.sh
+++ mcs/class/corlib/Test/run_test.sh
@@ -5,16 +5,23 @@ if [ $# -eq 0 ]; then
 	echo "$0 System.IO.FileTest System.Text.StringBuilderTest"
 	echo "or"
 	echo "$0 System.AllTests"
+	echo "or"
+	echo "$0 all"
 	echo "and so on..."
 	exit 1
 fi
 
 topdir=../../..
 NUNITCONSOLE=$topdir/nunit20/nunit-console.exe
-MONO_PATH=$topdir/nunit20:.
+MONO_PATH=$topdir/nunit20:$topdir/class/lib:.
 
 for i in $@; do
+	if [ "$i" = "all" ]; then
+		fixture=""
+	else
+		fixture="/fixture:MonoTests.${i}"
+	fi
 	MONO_PATH=$MONO_PATH \
-		mono --debug ${NUNITCONSOLE} corlib_test.dll /fixture:MonoTests.${i}
+		mono --debug ${NUNITCONSOLE} corlib_test.dll $fixture
 done
 
