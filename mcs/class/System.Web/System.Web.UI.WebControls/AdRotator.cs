c7d6a3b7c856a4857ceac44125563929c09d57bd
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-06-30 14:56:44 +0000

    2003-06-30  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* AdRotator.cs: fixed bug #44271 and a few others bugs. Mono-stylized.
    
    svn path=/trunk/mcs/; revision=15756

--- mcs/class/System.Web/System.Web.UI.WebControls/AdRotator.cs
+++ mcs/class/System.Web/System.Web.UI.WebControls/AdRotator.cs
@@ -2,13 +2,14 @@
  * Namespace: System.Web.UI.WebControls
  * Class:     AdRotator
  *
- * Author:  Gaurav Vaish
- * Maintainer: gvaish@iitk.ac.in
+ * Authors:  Gaurav Vaish, Gonzalo Paniagua
+ * Maintainer: gvaish@iitk.ac.in, gonzalo@ximian.com
  * Implementation: yes
  * Contact: <gvaish@iitk.ac.in>
  * Status:  100%
  *
  * (C) Gaurav Vaish (2001)
+ * (C) 2003 Ximian, Inc. (http://www.ximian.com)
  */
 
 using System;
@@ -28,217 +29,168 @@ namespace System.Web.UI.WebControls
 	[DefaultProperty("AdvertisementFile")]
 	//TODO: [Designer("??")]
 	[ToolboxData("<{0}:AdRotator runat=\"server\" Height=\"60px\" "
-	             + "Width=\"468\"></{0}:AdRotator>")]
+	+ "Width=\"468\"></{0}:AdRotator>")]
 	public class AdRotator: WebControl
 	{
-
-		private string advertisementFile;
-		private static readonly object AdCreatedEvent = new object();
+		string advertisementFile;
+		static readonly object AdCreatedEvent = new object();
 
 		// Will be set values during (On)PreRender-ing
-		private string alternateText;
-		private string imageUrl;
-		private string navigateUrl;
+		string alternateText;
+		string imageUrl;
+		string navigateUrl;
+		string fileDirectory;
+		Random random;
 
-		private string fileDirectory;
-
-		class AdRecord
+		public AdRotator ()
 		{
-			public IDictionary adProps;
-			public int         hits; // or impressions or clicks
-			public string      keyword;
-
-			public AdRecord(IDictionary adProps)
-			{
-				this.adProps = adProps;
-				hits         = 0;
-				keyword      = String.Empty;
-			}
+			advertisementFile = "";
+			fileDirectory     = null;
 		}
 
-/*
- * Loading / Saving data from/to ad file and all the manipulations wrt to the URL...
- * are incorporated by the following functions.
- * GetData(string)
- * LoadAdFile(string)
- * IsAdMatching(AdRecord)
- * ResolveAdUrl(string)
- * SelectAd()
- * The exact control flow will be detailed. Let me first write the functions
- */
-
-		private AdRecord[] LoadAdFile(string file)
+		AdRecord[] LoadAdFile (string file)
 		{
-			Stream      fStream;
-			ArrayList   list;
-			XmlReader   reader;
-			XmlDocument document;
-			XmlNode     topNode, innerNode;
-			IDictionary hybridDict = null;
-			AdRecord[]  adsArray = null;
-			try
-			{
-				fStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
-			} catch(Exception e)
-			{
-				throw new HttpException("AdRotator: Unable to open file");
+			Stream fStream;
+			try {
+				fStream = new FileStream (file, FileMode.Open, FileAccess.Read, FileShare.Read);
+			} catch (Exception e) {
+				throw new HttpException("AdRotator: Unable to open file", e);
 			}
-			try
-			{
-				list = new ArrayList();
-				reader = new XmlTextReader(fStream);
-				document = new XmlDocument();
-				document.Load(reader);
-				if(document.DocumentElement!=null)
-				{
-					if(document.DocumentElement.LocalName=="Advertisements")
-					{
-						topNode = document.DocumentElement.FirstChild;
-						while(topNode!=null)
-						{
-							if(topNode.LocalName=="Ad")
-							{
-								innerNode = topNode.FirstChild;
-								while(innerNode!=null)
-								{
-									if(innerNode.NodeType==XmlNodeType.Element)
-									{
-										if(hybridDict==null)
-										{
-											hybridDict = new HybridDictionary();
-										}
-										hybridDict.Add(innerNode.LocalName, innerNode.InnerText);
-									}
-									innerNode = innerNode.NextSibling;
-								}
-								if (hybridDict!=null) {
-									list.Add (hybridDict);
-									hybridDict = null;
-								}
+
+			ArrayList list = new ArrayList ();
+			try {
+				IDictionary hybridDict = null;
+				XmlDocument document = new XmlDocument ();
+				document.Load (fStream);
+
+				XmlElement docElem = document.DocumentElement;
+
+				if (docElem == null)
+					throw new HttpException ("No advertisements found");
+
+				if (docElem.LocalName != "Advertisements")
+					throw new HttpException ("No advertisements found: invalid document element");
+
+				XmlNode node = docElem.FirstChild;
+				while (node != null) {
+					if (node.LocalName == "Ad") {
+						XmlNode innerNode = node.FirstChild;
+						while (innerNode != null) {
+							if (node.NodeType == XmlNodeType.Element) {
+								if (hybridDict == null)
+									hybridDict = new HybridDictionary ();
+
+								hybridDict.Add (innerNode.LocalName, innerNode.InnerText);
 							}
-							topNode = topNode.NextSibling;
+							innerNode = innerNode.NextSibling;
+						}
+
+						if (hybridDict != null) {
+							list.Add (hybridDict);
+							hybridDict = null;
 						}
 					}
+					node = node.NextSibling;
 				}
-				if(list.Count>0)
-				{
-					adsArray = new AdRecord[list.Count];
-					for(int i=0; i < list.Count; i++)
-					{
-						adsArray[i] = new AdRecord((IDictionary)list[i]);
-					}
-				}
-			} catch(Exception e)
-			{
-				throw new HttpException("AdRotator_Parse_Error" + file);
-			} finally
-			{
-				fStream.Close();
+
+			} catch(Exception e) {
+				throw new HttpException("Parse error:" + file, e);
+			} finally {
+				if (fStream != null)
+					fStream.Close();
 			}
-			if(adsArray == null)
-			{
-				throw new HttpException("AdRotator_No_Advertisements_Found");
-			}
+
+			if (list.Count == 0)
+				throw new HttpException ("No advertisements found");
+
+			AdRecord [] adsArray = new AdRecord [list.Count];
+			int count = list.Count;
+			for (int i = 0; i < count; i++)
+				adsArray [i] = new AdRecord ((IDictionary) list [i]);
+
 			return adsArray;
 		}
 
-		[MonoTODO("CacheDependency does nothing")]
-		private AdRecord[] GetData(string file)
+		AdRecord [] GetData (string file)
 		{
-			string physPath = MapPathSecure(file);
+			string physPath = MapPathSecure (file);
 			string AdKey = "AdRotatorCache: " + physPath;
-			fileDirectory = UrlUtils.GetDirectory(UrlUtils.Combine(TemplateSourceDirectory, file));
+			fileDirectory = UrlUtils.GetDirectory (UrlUtils.Combine (TemplateSourceDirectory, file));
 			Cache cache = HttpRuntime.Cache;
-			AdRecord[] records = (AdRecord[])cache[AdKey];
-			if(records==null)
-			{
-				records = LoadAdFile(physPath);
-				if(records==null)
-				{
-					return null;
-				}
-				cache.Insert(AdKey, records, new CacheDependency(physPath));
+			AdRecord[] records = (AdRecord[]) cache [AdKey];
+			if (records == null) {
+				records = LoadAdFile (physPath);
+				cache.Insert (AdKey, records, new CacheDependency (physPath));
 			}
+
 			return records;
 		}
 
-		private IDictionary SelectAd()
+		IDictionary SelectAd ()
 		{
-			AdRecord[] records = GetData(AdvertisementFile);
-			if(records!=null && records.Length!=0)
-			{
-				int impressions = 0;
-				for(int i=0 ; i < records.Length; i++)
-				{
-					if(IsAdMatching(records[i]))
-						impressions += records[i].hits;
-				}
-				if(impressions!=0)
-				{
-					int rnd = (new Random()).Next(impressions) + 1;
-					int counter = 0;
-					int index = 0;
-					for(int i=0; i < records.Length; i++)
-					{
-						if(IsAdMatching(records[i]))
-						{
-							if(rnd <= (counter + records[i].hits))
-							{
-								index = i;
-								break;
-							}
-							counter += records[i].hits;
-						}
-					}
-					return records[index].adProps;
-				}
-				//FIXME: the line below added. Where should i init hits to make
-				//impressions not be 0?
-				return records [0].adProps;
+			AdRecord[] records = GetData (AdvertisementFile);
+			if (records == null || records.Length ==0)
+				return null;
+
+			int impressions = 0;
+			int rlength = records.Length;
+			for (int i=0 ; i < rlength; i++) {
+				if (IsAdMatching (records [i]))
+					impressions += records [i].Hits;
 			}
-			return null;
+
+			if (impressions == 0)
+				return null;
+
+			if (random == null)
+				random = new Random ();
+
+			int rnd = random.Next (impressions) + 1;
+			int counter = 0;
+			int index = 0;
+			for (int i = 0; i < rlength; i++) {
+				if(IsAdMatching(records[i])) {
+					if (rnd <= (counter + records [i].Hits)) {
+						index = i;
+						break;
+					}
+					counter += records [i].Hits;
+				}
+			}
+
+			return records [index].Properties;
 		}
 
-		private bool IsAdMatching(AdRecord currAd)
+		private bool IsAdMatching (AdRecord currAd)
 		{
 			if (KeywordFilter != String.Empty)
-				return (0 == String.Compare (currAd.keyword, KeywordFilter, true));
+				return (0 == String.Compare (currAd.Keyword, KeywordFilter, true));
 
 			return true;
 		}
 
-		private string ResolveAdUrl(string relativeUrl)
+		private string ResolveAdUrl (string relativeUrl)
 		{
-			if(relativeUrl.Length==0 || !UrlUtils.IsRelativeUrl(relativeUrl))
+			if (relativeUrl.Length==0 || !UrlUtils.IsRelativeUrl (relativeUrl))
 				return relativeUrl;
-			string fullUrl = String.Empty;
-			if(fileDirectory != null)
+
+			string fullUrl;
+			if (fileDirectory != null)
 				fullUrl = fileDirectory;
-			if(fullUrl.Length == 0)
+			else
 				fullUrl = TemplateSourceDirectory;
-			if(fullUrl.Length == 0)
+
+			if (fullUrl.Length == 0)
 				return relativeUrl;
-			return (fullUrl + relativeUrl);
+
+			return UrlUtils.Combine (fullUrl, relativeUrl);
 		}
 
 		[WebCategory("Action")]
 		[WebSysDescription("AdRotator_OnAdCreated")]
-		public event AdCreatedEventHandler AdCreated
-		{
-			add
-			{
-				Events.AddHandler(AdCreatedEvent, value);
-			}
-			remove
-			{
-				Events.RemoveHandler(AdCreatedEvent, value);
-			}
-		}
-
-		public AdRotator(): base()
-		{
-			advertisementFile = string.Empty;
-			fileDirectory     = null;
+		public event AdCreatedEventHandler AdCreated {
+			add { Events.AddHandler (AdCreatedEvent, value); }
+			remove { Events.RemoveHandler (AdCreatedEvent, value); }
 		}
 
 		[Bindable(true)]
@@ -246,43 +198,30 @@ namespace System.Web.UI.WebControls
 		//[Editor("??")]
 		[WebCategory("Behaviour")]
 		[WebSysDescription("AdRotator_AdvertisementFile")]
-		public string AdvertisementFile
-		{
-			get
-			{
-				return advertisementFile;
-			}
-			set
-			{
-				advertisementFile = value;
-			}
+		public string AdvertisementFile {
+			get { return ((advertisementFile != null) ? advertisementFile : ""); }
+			set { advertisementFile = value; }
 		}
 
-		public override FontInfo Font
-		{
-			get
-			{
-				return Font;
-			}
+		public override FontInfo Font {
+			get { return base.Font; }
 		}
 
 		[Bindable(true)]
 		[DefaultValue("")]
 		[WebCategory("Behaviour")]
 		[WebSysDescription("AdRotator_KeywordFilter")]
-		public string KeywordFilter
-		{
-			get
-			{
-				object o = ViewState["KeywordFilter"];
-				if(o!=null)
-					return (string)o;
+		public string KeywordFilter {
+			get {
+				object o = ViewState ["KeywordFilter"];
+				if (o != null)
+					return (string) o;
+
 				return String.Empty;
 			}
-			set
-			{
-				if(value!=null)
-					ViewState["KeywordFilter"] = value.Trim();
+			set {
+				if(value != null)
+					ViewState ["KeywordFilter"] = value.Trim ();
 			}
 		}
 
@@ -291,74 +230,98 @@ namespace System.Web.UI.WebControls
 		[TypeConverter(typeof(TargetConverter))]
 		[WebCategory("Behaviour")]
 		[WebSysDescription("AdRotator_Target")]
-		public string Target
-		{
-			get
-			{
-				object o = ViewState["Target"];
-				if(o!=null)
-					return (string)o;
-				return String.Empty;
+		public string Target {
+			get {
+				object o = ViewState ["Target"];
+				if (o != null)
+					return (string) o;
+
+				return "_top";
 			}
-			set
-			{
+			set {
 				ViewState["Target"] = value;
 			}
 		}
 
-		protected override ControlCollection CreateControlCollection()
+		protected override ControlCollection CreateControlCollection ()
 		{
-			return new EmptyControlCollection(this);
+			return new EmptyControlCollection (this);
 		}
 
-		protected virtual void OnAdCreated(AdCreatedEventArgs e)
+		protected virtual void OnAdCreated (AdCreatedEventArgs e)
 		{
-			if(Events!=null)
-			{
-				AdCreatedEventHandler aceh = (AdCreatedEventHandler)(Events[AdCreatedEvent]);
-				if(aceh!=null)
-					aceh(this, e);
-			}
+			if (Events == null)
+				return;
+
+			AdCreatedEventHandler aceh = (AdCreatedEventHandler) Events [AdCreatedEvent];
+			if (aceh != null)
+				aceh (this, e);
 		}
 
-		protected override void OnPreRender(EventArgs e)
+		protected override void OnPreRender (EventArgs e)
 		{
-			if(AdvertisementFile!=String.Empty)
-			{
-				AdCreatedEventArgs acea = new AdCreatedEventArgs(SelectAd());
-				imageUrl      = acea.ImageUrl;
-				navigateUrl   = acea.NavigateUrl;
-				alternateText = acea.AlternateText;
-			}
+			if(AdvertisementFile == String.Empty)
+				return;
+
+			AdCreatedEventArgs acea = new AdCreatedEventArgs (SelectAd ());
+			imageUrl = acea.ImageUrl;
+			navigateUrl = acea.NavigateUrl;
+			alternateText = acea.AlternateText;
 		}
 
-		protected override void Render(HtmlTextWriter writer)
+		protected override void Render (HtmlTextWriter writer)
 		{
-			HyperLink hLink = new HyperLink();
+			HyperLink hLink = new HyperLink ();
 			Image adImage = new Image();
-			foreach(IEnumerable current in Attributes.Keys)
-			{
-				hLink.Attributes[(string)current] = Attributes[(string)current];
-			}
-			if(ID != null && ID.Length > 0)
+			foreach (string current in Attributes.Keys)
+				hLink.Attributes [current] = Attributes [current];
+
+			if (ID != null && ID.Length > 0)
 				hLink.ID = ID;
-			hLink.Target    = Target;
+
+			hLink.Target = Target;
 			hLink.AccessKey = AccessKey;
-			hLink.Enabled   = Enabled;
-			hLink.TabIndex  = TabIndex;
+			hLink.Enabled  = Enabled;
+			hLink.TabIndex = TabIndex;
 			if (navigateUrl != null && navigateUrl.Length != 0)
 				hLink.NavigateUrl = ResolveAdUrl (navigateUrl);
-			hLink.RenderBeginTag(writer);
-			if(ControlStyleCreated)
-			{
+
+			hLink.RenderBeginTag (writer);
+			if (ControlStyleCreated)
 				adImage.ApplyStyle(ControlStyle);
-			}
+
 			if(imageUrl!=null && imageUrl.Length > 0)
-				adImage.ImageUrl = ResolveAdUrl(imageUrl);
+				adImage.ImageUrl = ResolveAdUrl (imageUrl);
+
 			adImage.AlternateText = alternateText;
-			adImage.ToolTip       = ToolTip;
-			adImage.RenderControl(writer);
-			hLink.RenderEndTag(writer);
+			adImage.ToolTip = ToolTip;
+			adImage.RenderControl (writer);
+			hLink.RenderEndTag (writer);
+		}
+
+		class AdRecord
+		{
+			public IDictionary Properties;
+			public int Hits; // or impressions or clicks
+			public string Keyword;
+
+			public AdRecord (IDictionary adProps)
+			{
+				this.Properties = adProps;
+				Keyword = Properties ["Keyword"] as string;
+				if (Keyword == null)
+					Keyword = "";
+
+				string imp = Properties ["Impressions"] as string;
+				Hits = 1;
+				if (imp != null) {
+					try {
+						Hits = Int32.Parse (imp);
+					} catch {
+					}
+				}
+			}
 		}
 	}
 }
+
