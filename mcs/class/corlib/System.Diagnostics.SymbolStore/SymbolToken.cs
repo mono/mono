034d6ff128dd6b79cc69efd5efc2892271a86ca5
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-06 08:27:00 +0000

    2003-07-06  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* SymbolToken.cs: Implemented missing members, style changes
    
    svn path=/trunk/mcs/; revision=15979

--- mcs/class/corlib/System.Diagnostics.SymbolStore/SymbolToken.cs
+++ mcs/class/corlib/System.Diagnostics.SymbolStore/SymbolToken.cs
@@ -1,29 +1,41 @@
 //
-// System.Diagnostics.SymbolStore.SymbolToken
+// System.Diagnostics.SymbolStore.SymbolToken.cs
 //
-// Author:
+// Authors:
 //   Duco Fijma (duco@lorentz.xs4all.nl)
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
 //
-//   (c) 2002 Duco Fijma
+// (c) 2002 Duco Fijma
+// (C) 2003 Andreas Nahr
 // 
 
 namespace System.Diagnostics.SymbolStore
 {
+	public struct SymbolToken 
+	{
 
-public struct SymbolToken {
+		private int _val;
 
-	private int _val;
+		public SymbolToken (int val)
+		{
+			_val = val;
+		}
 
-	public SymbolToken (int val) { _val = val; }
+		public override bool Equals (object obj) 
+		{
+			if (!(obj is SymbolToken))
+				return false;
+			return ((SymbolToken) obj).GetToken() == _val;
+		}
 
-	[MonoTODO]
-	public override bool Equals (object obj) { return false; }
+		public override int GetHashCode()
+		{
+			return _val.GetHashCode(); 
+		}
 
-	[MonoTODO]
-	public override int GetHashCode() { return 0; }
-
-	public int GetToken() { return _val; }
-	
-}
-
+		public int GetToken()
+		{
+			return _val; 
+		}
+	}
 }
