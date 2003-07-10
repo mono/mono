74cee0a54f52a5a450fbde8cf48f585827adb512
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-10 16:55:22 +0000

    2003-07-10  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* SystemEvents.cs: Implemented preliminary timer support, added private constructor, fixed events
    
    svn path=/trunk/mcs/; revision=16106

--- mcs/class/System/Microsoft.Win32/SystemEvents.cs
+++ mcs/class/System/Microsoft.Win32/SystemEvents.cs
@@ -1,110 +1,141 @@
 //
-// SystemEvents.cs
+// Microsoft.Win32.SystemEvents.cs
 //
-// Author:
-//  Johannes Roith (johannes@jroith.de)
+// Authors:
+//   Johannes Roith (johannes@jroith.de)
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
 //
 // (C) 2002 Johannes Roith
+// (C) 2003 Andreas Nahr
 //
 
-namespace Microsoft.Win32 {
-	/// <summary>
-	/// </summary>
-public sealed class SystemEvents {
+using System;
+using System.Timers;
+using System.Collections;
 
-	[MonoTODO]
-	public static void InvokeOnEventsThread(System.Delegate method)
+namespace Microsoft.Win32 
+{
+	public sealed class SystemEvents 
 	{
-		throw new System.NotImplementedException ();
-	}
+		private static Hashtable TimerStore = new Hashtable ();
 
+		private SystemEvents ()
+		{
+		}
 
+		// You can use timers using the CreateTimer, KillTimer methods and the Timerelapsed event
+		// This is only a partial solution, as it only works in managed code.
+		// TODO implement this on OS level
+		// Till done this solution should work if you are only using the mentioned members
 
-	[MonoTODO]
-	public static event System.EventHandler DisplaySettingsChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-		
-	[MonoTODO]
-	public static event System.EventHandler EventsThreadShutdown {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event System.EventHandler InstalledFontsChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event System.EventHandler LowMemory {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event System.EventHandler PaletteChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event  PowerModeChangedEventHandler PowerModeChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-		public static event SessionEndedEventHandler SessionEnded {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event System.EventHandler SessionEnding {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
+		public static IntPtr CreateTimer (int interval)
+		{
+			Guid Ident = Guid.NewGuid ();
+			int IdentValue = Ident.GetHashCode ();
+			Timer t = new System.Timers.Timer (interval);
+			t.Elapsed += new ElapsedEventHandler (InternalTimerElapsed);
+			TimerStore.Add (IdentValue, t);
+			return new IntPtr (IdentValue);
+		}
 
-	[MonoTODO]
-	public static event System.EventHandler TimeChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event UserPreferenceChangedEventHandler TimerElapsed {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event UserPreferenceChangedEventHandler UserPreferenceChanged {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
-	
-	[MonoTODO]
-	public static event  UserPreferenceChangingEventHandler UserPreferenceChanging {
-		add 	{ throw new System.NotImplementedException ();}
-		remove 	{ throw new System.NotImplementedException ();}
-	}
-	
+		public static void KillTimer (IntPtr timerId)
+		{
+			Timer t = (Timer) TimerStore[timerId.GetHashCode()];
+			t.Stop ();
+			t.Elapsed -= new ElapsedEventHandler (InternalTimerElapsed);
+			t.Dispose ();
+			TimerStore.Remove (timerId.GetHashCode());
+		}
 
-}
+		private static void InternalTimerElapsed (object e, ElapsedEventArgs args)
+		{
+			if (TimerElapsed != null)
+				TimerElapsed (null, new TimerElapsedEventArgs (new IntPtr(0)));
+		}
 
+		[MonoTODO]
+		public static void InvokeOnEventsThread(Delegate method)
+		{
+			throw new System.NotImplementedException ();
+		}
 
+		[MonoTODO]
+		public static event System.EventHandler DisplaySettingsChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event System.EventHandler EventsThreadShutdown 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event System.EventHandler InstalledFontsChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event System.EventHandler LowMemory 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event System.EventHandler PaletteChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event PowerModeChangedEventHandler PowerModeChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event SessionEndedEventHandler SessionEnded 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event SessionEndingEventHandler SessionEnding 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event System.EventHandler TimeChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		public static event TimerElapsedEventHandler TimerElapsed;
+
+		[MonoTODO]
+		public static event UserPreferenceChangedEventHandler UserPreferenceChanged 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+
+		[MonoTODO]
+		public static event UserPreferenceChangingEventHandler UserPreferenceChanging 
+		{
+			add 	{ throw new System.NotImplementedException ();}
+			remove 	{ throw new System.NotImplementedException ();}
+		}
+	}
 }
