314fb3819149c0ea37694d6d1cb4cd7e2eef313e
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-10 14:17:36 +0000

    2003-07-10  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added file
    
    svn path=/trunk/mcs/; revision=16093

---
+++ mcs/class/System/Assembly/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2002 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Runtime.CompilerServices;
+
+// General Information about the system assembly
+
+[assembly: AssemblyVersion("1.0.5000.0")]
+
+[assembly: AssemblyTitle("System.dll")]
+[assembly: AssemblyDescription("System.dll")]
+[assembly: AssemblyConfiguration("Development version")]
+[assembly: AssemblyCompany("MONO dev team")]
+[assembly: AssemblyProduct("MONO CLI")]
+[assembly: AssemblyCopyright("Various Authors")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("System.dll")]
+[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: SatelliteContractVersion("1.0.5000.0")]
+[assembly: AllowPartiallyTrustedCallers]
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
