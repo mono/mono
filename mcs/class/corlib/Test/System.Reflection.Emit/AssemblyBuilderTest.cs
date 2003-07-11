06538b8da84c35ab0dcdcdfcc270438c08803e7b
Zoltan Varga <vargaz@gmail.com>
2003-07-11 17:29:05 +0000

    2003-07-11  Zoltan Varga  <vargaz@freemail.hu>
    	* AssemblyBuilderTest.cs: Added error handling tests.
    
    svn path=/trunk/mcs/; revision=16123

--- mcs/class/corlib/Test/System.Reflection.Emit/AssemblyBuilderTest.cs
+++ mcs/class/corlib/Test/System.Reflection.Emit/AssemblyBuilderTest.cs
@@ -13,6 +13,7 @@ using System.Threading;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.IO;
+using System.Configuration.Assemblies;
 
 using NUnit.Framework;
 
@@ -28,6 +29,10 @@ public class AssemblyBuilderTest : Assertion
 		public FooAttribute (string arg)
 		{
 		}
+
+		public FooAttribute ()
+		{
+		}
 	}
 
 	static int nameIndex = 0;
@@ -36,11 +41,32 @@ public class AssemblyBuilderTest : Assertion
 
 	static AssemblyBuilder ab;
 
+	string tempDir = Path.Combine (Path.GetTempPath (), "MonoTests.System.Reflection.Emit.AssemblyBuilderTest");
+
 	[SetUp]
 	protected void SetUp () {
+		if (Directory.Exists (tempDir))
+			Directory.Delete (tempDir, true);
+
+		Directory.CreateDirectory (tempDir);
+
+		for (int i = 1; i < 3; ++i) {
+			string resFile = Path.Combine (tempDir, "res" + i + ".txt");
+			using (StreamWriter sw = new StreamWriter (resFile)) {
+				sw.WriteLine ("FOO");
+			}
+		}
+
 		domain = Thread.GetDomain ();
 		ab = genAssembly ();
+		ab.DefineDynamicModule ("def_module");
 	}
+
+	[TearDown]
+	protected void TearDown () {
+		if (Directory.Exists (tempDir))
+			Directory.Delete (tempDir, true);
+	}		
 
 	private AssemblyName genAssemblyName () {
 		AssemblyName assemblyName = new AssemblyName();
@@ -50,7 +76,8 @@ public class AssemblyBuilderTest : Assertion
 
 	private AssemblyBuilder genAssembly () {
 		return domain.DefineDynamicAssembly (genAssemblyName (),
-											 AssemblyBuilderAccess.RunAndSave);
+											 AssemblyBuilderAccess.RunAndSave,
+											 tempDir);
 	}
 
 	private MethodInfo genEntryFunction (AssemblyBuilder assembly) {
@@ -62,13 +89,14 @@ public class AssemblyBuilderTest : Assertion
 		return mb;
 	}
 
+	[ExpectedException (typeof (NotSupportedException))]
 	public void TestCodeBase () {
-		try {
-			string codebase = ab.CodeBase;
-			Fail ();
-		}
-		catch (NotSupportedException) {
-		}
+		string codebase = ab.CodeBase;
+	}
+
+	[ExpectedException (typeof (NotSupportedException))]
+	public void TestLocation () {
+		string location = ab.Location;
 	}
 
 	public void TestEntryPoint () {
@@ -168,6 +196,224 @@ public class AssemblyBuilderTest : Assertion
 
 		AssertNull (ab.GetDynamicModule ("FOO4"));
 	}
+
+#if NET_1_1
+	public void TestImageRuntimeVersion () {
+		string version = ab.ImageRuntimeVersion;
+		Assert (version.Length > 0);
+	}
+#endif
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestAddResourceFileNullName () {
+		ab.AddResourceFile (null, "foo.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestAddResourceFileNullFilename () {
+		ab.AddResourceFile ("foo", null);
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestAddResourceFileEmptyName () {
+		ab.AddResourceFile ("", "foo.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestAddResourceFileEmptyFilename () {
+		ab.AddResourceFile ("foo", "");
+	}
+
+	[ExpectedException (typeof (FileNotFoundException))]
+	public void TestAddResourceFileNonexistentFile () {
+		ab.AddResourceFile ("foo", "not-existent.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestAddResourceFileDuplicateFileName () {
+		ab.AddResourceFile ("foo", "res1.txt");
+		ab.AddResourceFile ("foo2", "res1.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestAddResourceFileDuplicateName () {
+		ab.AddResourceFile ("foo", "res1.txt");
+		ab.AddResourceFile ("foo", "res2.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestAddResourceFileFilenameIncludesPath () {
+		ab.AddResourceFile ("foo", "/tmp/res1.txt");
+	}
+
+	public void TestAddResourceFile () {
+		ab.AddResourceFile ("foo", "res2.txt", ResourceAttributes.Public);
+
+		ab.Save ("TestAddResourceFile.dll");
+
+		// TODO: Test reading back
+	}
+
+	public void TestDefineResource () {
+		ab.DefineResource ("foo", "FOO", "foo.txt", ResourceAttributes.Public);
+		ab.DefineResource ("foo2", "FOO", "foo2.txt");
+
+		ab.Save ("TestDefineResource.dll");
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestDefineDynamicModuleNullName () {
+		ab.DefineDynamicModule (null, "foo.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestDefineDynamicModuleNullFilename () {
+		ab.DefineDynamicModule ("foo", null);
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModuleEmptyName () {
+		ab.DefineDynamicModule ("", "foo.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModuleEmptyFilename () {
+		ab.DefineDynamicModule ("foo", "");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModuleDuplicateFileName () {
+		ab.DefineDynamicModule ("foo", "res1.txt");
+		ab.DefineDynamicModule ("foo2", "res1.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModuleDuplicateName () {
+		ab.DefineDynamicModule ("foo", "res1.txt");
+		ab.DefineDynamicModule ("foo", "res2.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModuleFilenameIncludesPath () {
+		ab.DefineDynamicModule ("foo", "/tmp/res1.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModule5 () {
+		// Filename without extension
+		ab.DefineDynamicModule ("foo", "foo");
+	}
+
+	/*
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineDynamicModule6 () {
+		// Name too long
+		string name = "";
+		for (int i = 0; i < 259; ++i)
+			name = name + "A";
+
+		try {
+			ab.DefineDynamicModule (name);
+		}
+		catch (Exception) {
+			Fail ();
+		}
+
+		name = name + "A";
+		// LAMESPEC: According to MSDN, this should throw an ArgumentException
+
+		ab.DefineDynamicModule (name);
+	}
+	*/
+
+	[ExpectedException (typeof (InvalidOperationException))]
+	public void TestDefineDynamicModule7 () {
+		// Called when assembly was already saved
+		ab.Save ("TestDefineDynamicModule7.dll");
+		ab.DefineDynamicModule ("foo", "foo.dll");
+	}
+
+	[ExpectedException (typeof (NotSupportedException))]
+	public void TestDefineDynamicModule8 () {
+		// Called on an assembly defined with the Run attribute
+		AssemblyBuilder ab = 
+			domain.DefineDynamicAssembly (genAssemblyName (),
+										  AssemblyBuilderAccess.Run,
+										  tempDir);
+		ab.DefineDynamicModule ("foo", "foo.dll");
+	}
+
+	public void TestDefineDynamicModule () {
+		ab.DefineDynamicModule ("foo", "foo.dll");
+		ab.DefineDynamicModule ("foo2", true);
+		ab.DefineDynamicModule ("foo3", "foo3.dll");
+		ab.DefineDynamicModule ("foo4", "foo4.dll", true);
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestDefineUnmanagedResource1 () {
+		// Null argument
+		ab.DefineUnmanagedResource ((byte[])null);
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestDefineUnmanagedResource2 () {
+		// Null argument
+		ab.DefineUnmanagedResource ((string)null);
+	}
+
+	[ExpectedException (typeof (ArgumentException))]
+	public void TestDefineUnmanagedResource3 () {
+		// Empty filename
+		ab.DefineUnmanagedResource ("");
+	}
+
+	[ExpectedException (typeof (FileNotFoundException))]
+	public void TestDefineUnmanagedResource4 () {
+		// Nonexistent file
+		ab.DefineUnmanagedResource ("not-exists.txt");
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestSetCustomAttribute1 () {
+		// Null argument
+		ab.SetCustomAttribute (null);
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestSetCustomAttribute2 () {
+		// Null constructor
+		ab.SetCustomAttribute (null, new byte [0]);
+	}
+
+	[ExpectedException (typeof (ArgumentNullException))]
+	public void TestSetCustomAttribute3 () {
+		// Null blob
+		ab.SetCustomAttribute (typeof(AssemblyCompanyAttribute).GetConstructor (new Type [] { typeof (String) }), null);
+	}
+
+	public void TestSetCustomAttribute () {
+		// Test common custom attributes
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyVersionAttribute).GetConstructor (new Type [] { typeof (string) }), new object [] { "1.2.3.4"}));
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyKeyFileAttribute).GetConstructor (new Type [] { typeof (string) }), new object [] { "foo"}));
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyCultureAttribute).GetConstructor (new Type [] { typeof (string) }), new object [] { "bar"}));
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyAlgorithmIdAttribute).GetConstructor (new Type [] { typeof (AssemblyHashAlgorithm) }), new object [] { AssemblyHashAlgorithm.MD5 }));
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyFlagsAttribute).GetConstructor (new Type [] { typeof (uint) }), new object [] { (uint)0x0100 }));
+
+		ab.SetCustomAttribute (new CustomAttributeBuilder (typeof (AssemblyDelaySignAttribute).GetConstructor (new Type [] { typeof (bool) }), new object [] { true }));
+
+		ab.SetCustomAttribute (typeof (FooAttribute).GetConstructor (new Type [] {}), new byte [0]);
+
+		ab.Save ("TestSetCustomAttribute.dll");
+
+		/* We should read back the assembly and check the attributes ... */
+	}
+
 }
 }
 
