06de67ba2db5c6769a6d5b3691e028ba5bc5d51d
Rafael Teixeira <monoman@gmail.com>
2003-07-14 03:54:55 +0000

    mbas version now is 0.25.0.0 in sync with mono
    svn path=/trunk/mcs/; revision=16186

--- mcs/mbas/AssemblyInfo.cs
+++ mcs/mbas/AssemblyInfo.cs
@@ -56,7 +56,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.25.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
