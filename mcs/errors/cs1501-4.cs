a24b2739ef59565c196b8bb7af18cd9fc18ee3aa
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2003-07-14 15:40:34 +0000

    Add this error test case
    svn path=/trunk/mcs/; revision=16211

---
+++ mcs/errors/cs1501-4.cs
@@ -0,0 +1,12 @@
+// cs1501-4.cs : No overload for method `NUnit.Framework.IgnoreAttribute' takes `0' arguments
+// Line: 8 
+
+using NUnit.Framework;
+
+class C
+{
+	[Ignore]
+	public void Method ()
+	{
+	}
+}
