32ab64c0936f1ff98155c93a17365e04c826467f
Jerome Laban <jerome@mono-cvs.ximian.com>
2003-07-14 16:01:37 +0000

    2003-07-14  Jerome Laban <jlaban@wanadoo.fr>
            * NetConfigurationHandler.cs: New file that handles
            system.net/settings
    
    svn path=/trunk/mcs/; revision=16216

---
+++ mcs/class/System/System.Net.Configuration/NetConfigurationHandler.cs
@@ -0,0 +1,32 @@
+//
+// System.Net.Configuration.NetConfigurationHandler.cs
+//
+// Authors:
+//	Jerome Laban (jlaban@wanadoo.fr)
+//
+// (C) 2003 Ximian, Inc (http://www.ximian.com)
+//
+
+using System.Collections;
+using System.Configuration;
+using System.Xml;
+
+namespace System.Net.Configuration
+{
+	class NetConfigurationHandler : IConfigurationSectionHandler
+	{
+		public virtual object Create (object parent, object configContext, XmlNode section)
+		{
+			NetConfig config = new NetConfig();
+
+			XmlNode node = section.SelectSingleNode("ipv6");
+			if(node != null) {
+				XmlAttribute attrib = node.Attributes["enabled"];
+				if(attrib != null)
+					config.ipv6Enabled = String.Compare (attrib.Value, "true", true) == 0;
+			}
+
+			return config;
+		}
+	}
+}
