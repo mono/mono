ba7287d57d9a92847e741861fac4cf574c39779c
Jerome Laban <jerome@mono-cvs.ximian.com>
2003-07-14 09:51:39 +0000

    2003-07-14 Jerome Laban <jlaban@wanadoo.fr>
            * IComponent.cs: Removed duplicate Designer attribute.
    
    svn path=/trunk/mcs/; revision=16192

--- mcs/class/System/System.ComponentModel/IComponent.cs
+++ mcs/class/System/System.ComponentModel/IComponent.cs
@@ -19,12 +19,10 @@ namespace System.ComponentModel
 	[ComVisible (true), TypeConverter (typeof (System.ComponentModel.ComponentConverter))]
 	#if (NET_1_0)
     		[Designer ("System.Windows.Forms.Design.ComponentDocumentDesigner, System.Design, Version=1.0.3300.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof (IRootDesigner)),
-		Designer ("System.Componentmodel.Design.ComponentDesigner, System.Design, Version=1.0.3300.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof (IDesigner)),
 		RootDesignerSerializer ("System.ComponentModel.Design.Serialization.RootCodeDomSerializer, System.Design, Version=1.0.3300.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", "System.ComponentModel.Design.Serialization.CodeDomSerializer, System.Design, Version=1.0.3300.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", true)]
 	#endif
 	#if (NET_1_1)
     		[Designer ("System.Windows.Forms.Design.ComponentDocumentDesigner, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof (IRootDesigner)),
-		Designer ("System.Componentmodel.Design.ComponentDesigner, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof (IDesigner)),
 		RootDesignerSerializer ("System.ComponentModel.Design.Serialization.RootCodeDomSerializer, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", "System.ComponentModel.Design.Serialization.CodeDomSerializer, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", true)]
 	#endif
 	public interface IComponent : IDisposable
