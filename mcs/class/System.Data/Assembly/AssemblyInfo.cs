4baaf7d13513a0c0597018a910566b8d84cc5510
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-18 19:14:54 +0000

    2003-07-18  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added
    	* ChangeLog: Added
    	* Locale.cs: Added
    
    svn path=/trunk/mcs/; revision=16412

---
+++ mcs/class/System.Data/Assembly/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about the system assembly
+
+#if (NET_1_0)
+	[assembly: AssemblyVersion("1.0.3300.0")]
+	[assembly: SatelliteContractVersion("1.0.3300.0")]
+#endif
+#if (NET_1_1)
+	[assembly: AssemblyVersion("1.0.5000.0")]
+	[assembly: SatelliteContractVersion("1.0.5000.0")]
+	[assembly: ComCompatibleVersion(1, 0, 3300, 0)]
+	[assembly: TypeLibVersion(1, 10)]
+#endif
+
+[assembly: AssemblyTitle("System.Data.dll")]
+[assembly: AssemblyDescription("System.Data.dll")]
+[assembly: AssemblyConfiguration("Development version")]
+[assembly: AssemblyCompany("MONO development team")]
+[assembly: AssemblyProduct("MONO CLI")]
+[assembly: AssemblyCopyright("(c) 2003 Various Authors")]
+[assembly: AssemblyTrademark("")]
+
+// FIXME Add once compliance is reached
+//[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("System.Data.dll")]
+[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: ComVisible(false)]
+[assembly: AllowPartiallyTrustedCallers]
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
