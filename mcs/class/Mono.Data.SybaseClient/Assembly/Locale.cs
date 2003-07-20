3b9113adc28719823d1f77754aa721aa89b3527c
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-20 08:20:55 +0000

    Updated to recent version
    svn path=/trunk/mcs/; revision=16439

--- mcs/class/Mono.Data.SybaseClient/Assembly/Locale.cs
+++ mcs/class/Mono.Data.SybaseClient/Assembly/Locale.cs
@@ -1,18 +1,24 @@
 //
-// System.Globalization.Locale.cs
+// Locale.cs
 //
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
 //
-// (C) 2001 Ximian, Inc (http://www.ximian.com)
+// (C) 2001 - 2003 Ximian, Inc (http://www.ximian.com)
 //
-	internal class Locale {
 
-		/// <summary>
-		///   Returns the translated message for the current locale
-		/// </summary>
-		public static string GetText (string msg)
-		{
-			return msg;
-		}
+internal sealed class Locale {
+
+	private Locale ()
+	{
 	}
+
+	/// <summary>
+	///   Returns the translated message for the current locale
+	/// </summary>
+	public static string GetText (string msg)
+	{
+		return msg;
+	}
+}
--- mcs/class/Mono.Data.TdsClient/Assembly/Locale.cs
+++ mcs/class/Mono.Data.TdsClient/Assembly/Locale.cs
@@ -1,19 +1,24 @@
 //
-// System.Globalization.Locale.cs
+// Locale.cs
 //
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
 //
-// (C) 2001 Ximian, Inc (http://www.ximian.com)
+// (C) 2001 - 2003 Ximian, Inc (http://www.ximian.com)
 //
 
-	internal class Locale {
+internal sealed class Locale {
 
-		/// <summary>
-		///   Returns the translated message for the current locale
-		/// </summary>
-		public static string GetText (string msg)
-		{
-			return msg;
-		}
+	private Locale ()
+	{
 	}
+
+	/// <summary>
+	///   Returns the translated message for the current locale
+	/// </summary>
+	public static string GetText (string msg)
+	{
+		return msg;
+	}
+}
