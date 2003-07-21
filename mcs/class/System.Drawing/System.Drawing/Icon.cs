ca93428b4aac4d2ca7f1496dc756517758e8903e
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-21 21:02:38 +0000

    2003-07-21  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* Icon.cs: Stubed out (fix for System.Drawing.Design)
    
    svn path=/trunk/mcs/; revision=16501

--- mcs/class/System.Drawing/System.Drawing/Icon.cs
+++ mcs/class/System.Drawing/System.Drawing/Icon.cs
@@ -1,44 +1,128 @@
 //
 // System.Drawing.Icon.cs
 //
-// Author:
+// Authors:
 //   Dennis Hayes (dennish@Raytek.com)
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
 //
 // (C) 2002 Ximian, Inc
 //
 using System;
+using System.IO;
 using System.Runtime.Serialization;
 
 namespace System.Drawing
 {
-	/// <summary>
-	/// Summary description for Icon.
-	/// </summary>
+
 	[Serializable]
-	public class Icon : MarshalByRefObject, ISerializable, ICloneable, IDisposable
+	public sealed class Icon : MarshalByRefObject, ISerializable, ICloneable, IDisposable
 	{
-		public Icon()
+
+		private Icon ()
 		{
-			//
-			// TODO: Add constructor logic here
-			//
 		}
 
+		[MonoTODO ("Implement")]
+		public Icon (Icon original, int width, int height)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Icon (Icon original, Size size)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Icon (Stream stream)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Icon (Stream stream, int width, int height)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Icon (string fileName)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Icon (Type type, string resource)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
         	private Icon (SerializationInfo info, StreamingContext context)
 		{
+			throw new NotImplementedException ();
 		}
 
+		[MonoTODO ("Implement")]
 		void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
 		{
+			throw new NotImplementedException ();
 		}
 
+		[MonoTODO ("Implement")]
 		public void Dispose ()
 		{
 		}
 
-		object ICloneable.Clone()
+		[MonoTODO ("Implement")]
+		public object Clone ()
 		{
 			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public static Icon FromHandle (IntPtr handle)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public void Save (Stream outputStream)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public Bitmap ToBitmap ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public override string ToString ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO ("Implement")]
+		public IntPtr Handle {
+			get {throw new NotImplementedException ();}
+		}
+
+		[MonoTODO ("Implement")]
+		public int Height {
+			get {throw new NotImplementedException ();}
+		}
+
+		[MonoTODO ("Implement")]
+		public Size Size {
+			get {throw new NotImplementedException ();}
+		}
+
+		[MonoTODO ("Implement")]
+		public int Width {
+			get {throw new NotImplementedException ();}
 		}
 	}
 }
