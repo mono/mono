ee3e23fd5dbe43b26ab4e7acd43b1aca2792d04e
Miguel de Icaza <miguel@gnome.org>
2003-07-22 04:13:55 +0000

    Add chmod
    svn path=/trunk/mcs/; revision=16511

--- mcs/class/Mono.Posix/Mono.Posix/Syscall.cs
+++ mcs/class/Mono.Posix/Mono.Posix/Syscall.cs
@@ -25,5 +25,23 @@ namespace Mono.Posix {
 				
 			return Encoding.UTF8.GetString (buf, 0, res);
 		}
+
+		[Flags]
+		public enum FileMode {
+			S_ISUID   = 04000,
+			S_ISGID   = 02000,
+			S_ISVTX   = 01000,
+			S_IRUSR   = 00400,
+			S_IWUSR   = 00200,
+			S_IXUSR   = 00100,
+			S_IRGRP   = 00040,
+			S_IWGRP   = 00020,
+			S_IXGRP   = 00010,
+			S_IROTH   = 00004,
+			S_IWOTH   = 00002,
+			S_IXOTH   = 00001
+		}
+		
+		public static int chmod (string file, FileMode mode)
 	}
 }
