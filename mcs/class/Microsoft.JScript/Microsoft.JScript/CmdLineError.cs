8a4e2f604dd13670936c9f6896f1a6e870852597
César Natarén <cesar@mono-cvs.ximian.com>
2003-07-22 18:18:51 +0000

    oops! forgot to add this files
    svn path=/trunk/mcs/; revision=16531

---
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/CmdLineError.cs
@@ -0,0 +1,54 @@
+//
+// CmdLineError.cs:
+//
+// Author:
+//	Cesar Lopez Nataren (cesar@ciencias.unam.mx)
+//
+// (C) 2003, Cesar Lopez Nataren
+//
+
+namespace Microsoft.JScript
+{
+	public enum CmdLineError {
+		AssemblyNotFound,
+		CannotCreateEngine,
+		CompilerConstant,
+		DuplicateFileAsSourceAndAssembly,
+		DuplicateResourceFile,
+		DuplicateResourceName,
+		DuplicateSourceFile,
+		ErrorSavingCompiledState,
+		IncompatibleTargets,
+		InvalidAssembly,
+		InvalidCharacters,
+		InvalidCodePage,
+		InvalidDefinition,
+		InvalidForCompilerOptions,
+		InvalidLocaleID,
+		InvalidTarget,
+		InvalidSourceFile,
+		InvalidVersion,
+		InvalidWarningLevel,
+		ManagedResourceNotFound,
+		MissingDefineArgument,
+		MissingExtension,
+		MissingLibArgument,
+		MissingReference,
+		MissingVersionInfo,
+		MultipleOutputNames,
+		MultipleTargets,
+		MultipleWin32Resources,
+		NestedResponseFiles,
+		NoCodePage,
+		NoFileName,
+		NoInputSourcesSpecified,
+		NoLocaleID,
+		NoWarningLevel,
+		ResourceNotFound,
+		SourceFileTooBig,
+		SourceNotFound,
+		UnknownOption,
+		Unspecified,
+		LAST
+	}
+}
