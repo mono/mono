144012324e2e7bcc13e68f9b052cc584433490dd
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2003-07-25 17:10:36 +0000

    Add this error test case
    svn path=/trunk/mcs/; revision=16675

---
+++ mcs/errors/cs0592-3.cs
@@ -0,0 +1,9 @@
+// cs0592 :  Attribute 'IndexerName' is not valid on this declaration type. It is valid on 'property' declarations only.
+// Line : 6
+
+using System.Runtime.CompilerServices;
+
+[IndexerName("XXX")]
+class A
+{
+}
