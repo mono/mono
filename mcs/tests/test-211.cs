e2cbdf5c45bd4534eeca9bca18c3a4d8b5c3728f
Martin Baulig <martin@novell.com>
2003-07-28 17:29:29 +0000

    New test.
    svn path=/trunk/mcs/; revision=16777

---
+++ mcs/tests/test-211.cs
@@ -0,0 +1,51 @@
+class X
+{
+	public readonly int value;
+
+	public X (int value)
+	{
+		this.value = value;
+	}
+
+	public static implicit operator X (int y)
+	{
+		return new X (y);
+	}
+}
+
+class Y
+{
+	public readonly X x;
+
+	public Y (X x)
+	{
+		this.x = x;
+	}
+
+	public static implicit operator Y (X x)
+	{
+		return new Y (x);
+	}
+}
+
+class Z
+{
+	public readonly Y y;
+
+	public Z (Y y)
+	{
+		this.y = y;
+	}
+
+	public static implicit operator Z (Y y)
+	{
+		return new Z (y);
+	}
+
+	public static int Main ()
+	{
+		int a = 5;
+		Y y = (Y) (X) a;
+		return 0;
+	}
+}
