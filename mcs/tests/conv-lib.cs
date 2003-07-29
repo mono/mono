0dc4ad622395570b4c2260cf10378bc163052c44
Ravi Pratap M <ravi@mono-cvs.ximian.com>
2003-07-29 20:35:21 +0000

    2003-07-29  Ravi Pratap  <ravi@ximian.com>
    	* conv-lib.cs: New Multi stage compilation test for bug #47043.
    
    	* conv-main.cs: Main for the above.
    
    	* Makefile: Update.
    
    svn path=/trunk/mcs/; revision=16856

---
+++ mcs/tests/conv-lib.cs
@@ -0,0 +1,28 @@
+public class A
+{
+	public static bool operator==(A a1, A a2)
+	{
+		return true;
+	}
+
+	public static bool operator!=(A a1, A a2)
+	{
+		return false;
+	}
+
+	public override bool Equals (object o)
+	{
+		return true;
+	}
+
+	public override int GetHashCode ()
+	{
+		return base.GetHashCode ();
+	}
+
+	public int KK () { return 1; }
+}
+
+public class B : A {
+}
+
---
+++ mcs/tests/conv-main.cs
@@ -0,0 +1,11 @@
+public class MainClass
+{
+	public static int Main ()
+	{
+		A a = new A ();
+		B b = new B ();
+		bool r = (a == b);
+
+                return 0;
+	}
+}
