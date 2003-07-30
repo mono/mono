7796bb4e359f0b9d8c1fb2c085a7d8d048567209
Alexandre Pigolkine <pigolkine@mono-cvs.ximian.com>
2003-07-30 02:26:47 +0000

    BmpPaint.cs 	modified
    svn path=/trunk/mcs/; revision=16868

--- mcs/class/System.Drawing/Test/System.Drawing/BmpPaint.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/BmpPaint.cs
@@ -12,8 +12,9 @@ using System.Drawing;
 using System.Drawing.Imaging;
 
 public class BmpPaint {
-	static public void Main( string[] args) {
-		Bitmap	bmp = new Bitmap(100,100);
+	static public void CreateBitmap (string filename, ImageFormat imgFmt) 
+	{
+		Bitmap	bmp = new Bitmap(100,100, PixelFormat.Format24bppRgb);
 		Console.WriteLine("Bitmap created OK {0}", bmp != null);
 		if( bmp != null) {
 			Console.WriteLine("Bitmap Pixelformat {0}", bmp.PixelFormat);
@@ -26,8 +27,40 @@ public class BmpPaint {
 			gr.DrawRectangle(p, 10.0F, 10.0F, 80.0F, 80.0F);
 			p.Dispose();
 		}
-		bmp.Save("file.bmp", ImageFormat.Bmp);
+		bmp.Save(filename, imgFmt);
 		gr.Dispose();
 		bmp.Dispose();
+		Console.WriteLine("Bitmap stored to " + filename);
+	}
+	
+	static public void PaintOnBitmap (string filename) 
+	{
+		Bitmap	bmp = new Bitmap(filename);
+		Console.WriteLine("Bitmap readed OK {0}", bmp != null);
+		if( bmp != null) {
+			Console.WriteLine("Bitmap Pixelformat {0}", bmp.PixelFormat);
+		}
+		Graphics gr = Graphics.FromImage(bmp);
+		Console.WriteLine("Graphics created OK {0}", gr != null);
+		if( gr != null) {
+			Pen p = new Pen(Color.Blue, 2);
+			gr.DrawLine(p, 20.0F, 20.0F, 80.0F, 80.0F);
+			gr.DrawRectangle(p, 20.0F, 20.0F, 60.0F, 60.0F);
+			p.Dispose();
+		}
+		bmp.Save(filename);
+		gr.Dispose();
+		bmp.Dispose();
+		Console.WriteLine("Modified Bitmap stored to " + filename);
+	}
+	
+	static public void Main( string[] args) 
+	{
+		CreateBitmap ("file.bmp", ImageFormat.Bmp);
+		PaintOnBitmap ("file.bmp");
+		CreateBitmap ("file.jpg", ImageFormat.Jpeg);
+		PaintOnBitmap ("file.jpg");
+		CreateBitmap ("file.png", ImageFormat.Png);
+		PaintOnBitmap ("file.png");
 	}
 };
