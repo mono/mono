0834cb59fbe8675361848ecf5dce3749438caaf2
Andreas N <andreas@mono-cvs.ximian.com>
2003-07-30 18:09:43 +0000

    2003-07-30  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* PersistenceModeAttribute.cs: Fixed wrong AttributeUsage
    
    svn path=/trunk/mcs/; revision=16905

--- mcs/class/System.Web/System.Web.UI/PersistenceModeAttribute.cs
+++ mcs/class/System.Web/System.Web.UI/PersistenceModeAttribute.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace System.Web.UI {
 
-	[AttributeUsage (AttributeTargets.Class)]
+	[AttributeUsage (AttributeTargets.All)]
 	public sealed class PersistenceModeAttribute : Attribute
 	{
 		PersistenceMode mode;
