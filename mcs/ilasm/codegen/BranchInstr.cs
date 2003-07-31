ada2cc76d5626b943617bdc296e85061333ce542
Jackson Harper <jackson@novell.com>
2003-07-31 06:03:18 +0000

    * BranchInstr.cs: Allow explicit offsets for labels
    svn path=/trunk/mcs/; revision=16928

--- mcs/ilasm/codegen/BranchInstr.cs
+++ mcs/ilasm/codegen/BranchInstr.cs
@@ -17,17 +17,30 @@ namespace Mono.ILASM {
 
                 private PEAPI.BranchOp op;
                 private string label;
+		private uint offset;
+		private bool use_offset;
 
                 public BranchInstr (PEAPI.BranchOp op, string label)
                 {
                         this.op = op;
                         this.label = label;
+			use_offset = false;
                 }
+
+		public BranchInstr (PEAPI.BranchOp op, uint offset)
+		{
+			this.op = op;
+			this.offset = offset;
+			use_offset = true;
+		}
 
                 public void Emit (CodeGen code_gen, MethodDef meth,
 				  PEAPI.CILInstructions cil)
                 {
-                        cil.Branch (op, meth.GetLabelDef (label));
+			if (use_offset)
+				cil.Branch (op, meth.GetLabelDef (offset));
+			else
+                        	cil.Branch (op, meth.GetLabelDef (label));
                 }
         }
 
