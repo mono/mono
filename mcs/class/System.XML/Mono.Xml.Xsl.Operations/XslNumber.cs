f787103a0995df45ed619826a33bde0ea3fb7a6a
Ben Maurer <benm@mono-cvs.ximian.com>
2003-08-01 02:17:28 +0000

    2003-07-31 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* XslNumber.cs: Stub for parsing xsl:number.
    
    svn path=/trunk/mcs/; revision=16975

--- mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslNumber.cs
+++ mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslNumber.cs
@@ -17,23 +17,63 @@ using System.Xml.Xsl;
 
 namespace Mono.Xml.Xsl.Operations {
 	public class XslNumber : XslCompiledElement {
+		
+		// <xsl:number
+		//   level = "single" | "multiple" | "any"
 		XslNumberingLevel level;
+		//   count = pattern
 		XPathExpression count;
+		//   from = pattern
 		XPathExpression from;
+		//   value = number-expression
 		XPathExpression value;
-		string format;
-		string XmlLang;
-		string letterValue;
-		XPathExpression groupingSeparator;
-		XPathExpression groupingSize;
+		//   format = { string }
+		XslAvt format;
+		//   lang = { nmtoken }
+		XslAvt lang;
+		//   letter-value = { "alphabetic" | "traditional" }
+		XslAvt letterValue;
+		//   grouping-separator = { char }
+		XslAvt groupingSeparator;
+		//   grouping-size = { number } />
+		XslAvt groupingSize;
 		
 		public XslNumber (Compiler c) : base (c) {}
 		
 		protected override void Compile (Compiler c)
 		{
-			throw new NotImplementedException ();
+			switch (c.GetAttribute ("level"))
+			{
+			case "single":
+				level = XslNumberingLevel.Single;
+				break;
+			case "multiple":
+				level = XslNumberingLevel.Multiple;
+				break;
+			case "any":
+				level = XslNumberingLevel.Any;
+				break;
+			case null:
+			case "":
+			default:
+				level = XslNumberingLevel.Single; // single == default
+				break;
+			}
+			
+			count = c.CompilePattern (c.GetAttribute ("count"));
+			from = c.CompilePattern (c.GetAttribute ("from"));
+			value = c.CompileExpression (c.GetAttribute ("value"));
+			
+			if (value.ReturnType != XPathResultType.Number && value.ReturnType != XPathResultType.Any)
+				throw new Exception ("The expression for attribute 'value' must return a number");
+			
+			format = c.ParseAvtAttribute ("format");
+			lang = c.ParseAvtAttribute ("lang");
+			letterValue = c.ParseAvtAttribute ("letter-value");
+			groupingSeparator = c.ParseAvtAttribute ("grouping-separator");
+			groupingSize = c.ParseAvtAttribute ("grouping-size");
 		}
-		// TODO: parse this
+
 		public override void Evaluate (XslTransformProcessor p)
 		{
 			throw new NotImplementedException ();
