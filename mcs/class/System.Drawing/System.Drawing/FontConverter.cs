6f4ec0838e89af65347cec995def1a175535c044
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-08-01 10:55:29 +0000

    2003-08-01  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* FontConverter.cs: stub it out so that system.web compiles.
    
    svn path=/trunk/mcs/; revision=16987

--- mcs/class/System.Drawing/System.Drawing/FontConverter.cs
+++ mcs/class/System.Drawing/System.Drawing/FontConverter.cs
@@ -1,22 +1,116 @@
 //
 // System.Drawing.FontConverter.cs
 //
-// Author:
-//   Dennis Hayes (dennish@Raytek.com)
+// Authors:
+// 	Dennis Hayes (dennish@Raytek.com)
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
 //
-// (C) 2002 Ximian, Inc
+// (C) 2002,2003 Ximian, Inc
 //
 using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace System.Drawing
 {
-	/// <summary>
-	/// Summary description for FontConverter.
-	/// </summary>
-	public class FontConverter
+	public class FontConverter : TypeConverter
 	{
 		public FontConverter()
 		{
 		}
+
+		[MonoTODO]
+		public override bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override bool CanConvertTo (ITypeDescriptorContext context, Type destinationType)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override object ConvertTo (ITypeDescriptorContext context,
+						  CultureInfo culture,
+						  object value,
+						  Type destinationType)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override object ConvertFrom (ITypeDescriptorContext context,
+						    CultureInfo culture,
+						    object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override object CreateInstance (ITypeDescriptorContext context,
+						       IDictionary propertyValues)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override bool GetCreateInstanceSupported (ITypeDescriptorContext context)
+		{
+			return true;
+		}
+
+		[MonoTODO]
+		public override PropertyDescriptorCollection GetProperties (ITypeDescriptorContext context,
+									    object value,
+									    Attribute [] attributes)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override bool GetPropertiesSupported (ITypeDescriptorContext context)
+		{
+			return true;
+		}
+
+		public sealed class FontNameConverter : TypeConverter
+		{
+			public FontNameConverter ()
+			{
+			}
+
+			[MonoTODO]
+			public override bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[MonoTODO]
+			public override object ConvertFrom (ITypeDescriptorContext context,
+							    CultureInfo culture,
+							    object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[MonoTODO]
+			public override StandardValuesCollection GetStandardValues (ITypeDescriptorContext context)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[MonoTODO]
+			public override bool GetStandardValuesExclusive (ITypeDescriptorContext context)
+			{
+				throw new NotImplementedException ();
+			}
+
+			public override bool GetStandardValuesSupported (ITypeDescriptorContext context)
+			{
+				return true;
+			}
+		}
 	}
 }
+
