42f84478b96626760e2e69a61089a5635e799424
Ben Maurer <benm@mono-cvs.ximian.com>
2003-08-01 00:58:53 +0000

    2003-07-31 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* XsltCompiledContext.cs: Implement more functions.
    
    svn path=/trunk/mcs/; revision=16970

--- mcs/class/System.XML/Mono.Xml.Xsl/XsltCompiledContext.cs
+++ mcs/class/System.XML/Mono.Xml.Xsl/XsltCompiledContext.cs
@@ -15,6 +15,7 @@ using System.Xml;
 using System.Xml.Schema;
 using System.Xml.XPath;
 using System.Xml.Xsl;
+using System.Text;
 using System.IO;
 using Mono.Xml.Xsl.Functions;
 using Mono.Xml.Xsl.Operations;
@@ -95,11 +96,14 @@ namespace Mono.Xml.Xsl {
 			MethodInfo [] mi = t.GetMethods (BF.Public | BF.Instance | BF.Static);
 			if (mi.Length == 0)
 				return null;
+			
+			if (argTypes == null)
+				return mi [0]; // if we dont have info on the arg types, nothing we can do
 
 
 			free = 0;
 			// filter on name + num args
-			int numArgs = argTypes == null ? 0 : argTypes.Length;
+			int numArgs = argTypes.Length;
 			for (int i = 0; i < mi.Length; i ++) {
 				if (mi [i].Name == name && mi [i].GetParameters ().Length == numArgs) 
 					mi [free++] = mi [i];
@@ -287,7 +291,33 @@ namespace Mono.Xml.Xsl.Functions {
 		
 		public override object Invoke (XsltCompiledContext xsltContext, object [] args, XPathNavigator docContext)
 		{
-			throw new NotImplementedException ();
+			QName name = XslNameUtil.FromString ((string)args [0], xsltContext);
+
+			return (
+				(name.Namespace == Compiler.XsltNamespace) &&
+				(
+					//
+					// A list of all the instructions (does not include top-level-elements)
+					//
+					name.Name == "apply-imports" ||
+					name.Name == "apply-templates" ||
+					name.Name == "call-template" ||
+					name.Name == "choose" ||
+					name.Name == "comment" ||
+					name.Name == "copy" ||
+					name.Name == "copy-of" ||
+					name.Name == "element" ||
+					name.Name == "fallback" ||
+					name.Name == "for-each" ||
+					name.Name == "message" ||
+					name.Name == "number" ||
+					name.Name == "processing-instruction" ||
+					name.Name == "text" ||
+					name.Name == "value-of" ||
+					name.Name == "variable"
+				)
+			);
+			
 		}
 	}
 
@@ -305,7 +335,49 @@ namespace Mono.Xml.Xsl.Functions {
 		
 		public override object Invoke (XsltCompiledContext xsltContext, object [] args, XPathNavigator docContext)
 		{
-			throw new NotImplementedException ();
+			
+			string name = (string)args [0];
+			int colon = name.IndexOf (':');
+			// extension function
+			if (colon > 0)
+				return xsltContext.ResolveFunction (
+					name.Substring (0, colon), 
+					name.Substring (colon, name.Length - colon), 
+					null) != null;
+			
+			return (
+				//
+				// XPath
+				//
+                                name == "boolean" ||
+                                name == "ceiling" ||
+                                name == "concat" ||
+                                name == "contains" ||
+                                name == "count" ||
+                                name == "false" ||
+                                name == "floor" ||
+                                name == "id"||
+                                name == "lang" ||
+                                name == "last" ||
+                                name == "local-name" ||
+                                name == "name" ||
+                                name == "namespace-uri" ||
+                                name == "normalize-space" ||
+                                name == "not" ||
+                                name == "number" ||
+                                name == "position" ||
+                                name == "round" ||
+                                name == "starts-with" ||
+                                name == "string" ||
+                                name == "string-length" ||
+                                name == "substring" ||
+                                name == "substring-after" ||
+                                name == "substring-before" ||
+                                name == "sum" ||
+                                name == "translate" ||
+                                name == "true" ||
+				xsltContext.ResolveFunction ("", name, null) != null // rest of xslt functions
+			);
 		}
 	} 
 
@@ -313,8 +385,39 @@ namespace Mono.Xml.Xsl.Functions {
 		public XsltGenerateId () : base (0, 1, XPathResultType.String , new XPathResultType [] { XPathResultType.NodeSet }) {}
 		public override object Invoke (XsltCompiledContext xsltContext, object [] args, XPathNavigator docContext)
 		{
-			throw new NotImplementedException ();
+			XPathNavigator n;
+			if (args.Length == 1) {
+				XPathNodeIterator itr = (XPathNodeIterator) args [0];
+				if (itr.MoveNext ())
+					n = itr.Current.Clone ();
+				else
+					return string.Empty; // empty nodeset == empty string
+			} else
+				n = docContext.Clone ();
+			
+			StringBuilder sb = new StringBuilder ("Mono"); // Ensure begins with alpha
+			sb.Append (XmlConvert.EncodeLocalName (n.BaseURI));
+			sb.Replace ('_', 'm'); // remove underscores from EncodeLocalName
+			
+			do {
+				sb.Append (IndexInParent (n));
+				sb.Append ('m');
+			} while (n.MoveToParent ());
+			
+			return sb.ToString ();
 		}
+		
+		int IndexInParent (XPathNavigator nav)
+		{
+			nav = nav.Clone();
+			
+			int n = 0;
+			while (nav.MoveToPrevious ())
+				n++;
+			
+			return n;
+		}
+		
 	} 
 	
 	class XsltKey : XPFuncImpl {
@@ -382,7 +485,17 @@ namespace Mono.Xml.Xsl.Functions {
 		
 		public override object Invoke (XsltCompiledContext xsltContext, object [] args, XPathNavigator docContext)
 		{
-			throw new NotImplementedException ();
+			QName name = XslNameUtil.FromString ((string)args [0], xsltContext);
+			
+			if (name.Namespace == Compiler.XsltNamespace) {
+				switch (name.Name) {
+					case "version": return "1.0";
+					case "vendor": return "Mono";
+					case "vendor-url": return "http://www.go-mono.com/";
+				}
+			}
+			
+			return "";
 		}
 	} 
 
