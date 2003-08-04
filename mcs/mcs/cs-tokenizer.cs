ba38a99cbe6be681ccce158d501efbc927d779f6
Martin Baulig <martin@novell.com>
2003-08-04 13:45:52 +0000

    2003-08-04  Martin Baulig  <martin@ximian.com>
    	* cs-parser.jay (CLOSE_PARENS_CAST, CLOSE_PARENS_NO_CAST,
    	CLOSE_PARENS_OPEN_PARENS, CLOSE_PARENS_MINUS): New tokens to
    	deambiguate casts and delegate invocations.
    	(parenthesized_expression): Use the new tokens to ensure this is
    	not a cast of method invocation.
    
    	* cs-tokenizer.cs (is_punct): Return one of the new special tokens
    	when reading a `)' and Deambiguate_CloseParens () was previously
    	called.
    
    	* expression.cs (ParenthesizedExpression): New class.  This is
    	just used for the CS0075 test.
    	(Binary.DoResolve): Check for CS0075.
    
    svn path=/trunk/mcs/; revision=17052

--- mcs/mcs/cs-tokenizer.cs
+++ mcs/mcs/cs-tokenizer.cs
@@ -383,8 +383,30 @@ namespace Mono.CSharp
 				return Token.CLOSE_BRACKET;
 			case '(':
 				return Token.OPEN_PARENS;
-			case ')':
-				return Token.CLOSE_PARENS;
+			case ')': {
+				if (deambiguate_close_parens == 0)
+					return Token.CLOSE_PARENS;
+
+				--deambiguate_close_parens;
+
+				// Save current position and parse next token.
+				reader.DiscardBufferedData ();
+				long old = reader.BaseStream.Position;
+				int new_token = token ();
+				reader.DiscardBufferedData ();
+				reader.BaseStream.Position = old;
+				putback_char = -1;
+
+				if (new_token == Token.OPEN_PARENS)
+					return Token.CLOSE_PARENS_OPEN_PARENS;
+				else if (new_token == Token.MINUS)
+					return Token.CLOSE_PARENS_MINUS;
+				else if (IsCastToken (new_token))
+					return Token.CLOSE_PARENS_CAST;
+				else
+					return Token.CLOSE_PARENS_NO_CAST;
+			}
+
 			case ',':
 				return Token.COMMA;
 			case ':':
@@ -526,6 +548,14 @@ namespace Mono.CSharp
 				return Token.OP_GT;
 			}
 			return Token.ERROR;
+		}
+
+		int deambiguate_close_parens = 0;
+
+		public void Deambiguate_CloseParens ()
+		{
+			putback (')');
+			deambiguate_close_parens++;
 		}
 
 		void Error_NumericConstantTooLong ()
@@ -998,12 +1028,44 @@ namespace Mono.CSharp
 		{
 			return val;
 		}
-		
-		public int token ()
+
+		bool IsCastToken (int token)
 		{
-			current_token = xtoken ();
-			return current_token;
+			switch (token) {
+			case Token.BANG:
+			case Token.TILDE:
+			case Token.IDENTIFIER:
+			case Token.LITERAL_INTEGER:
+			case Token.LITERAL_FLOAT:
+			case Token.LITERAL_DOUBLE:
+			case Token.LITERAL_DECIMAL:
+			case Token.LITERAL_CHARACTER:
+			case Token.LITERAL_STRING:
+			case Token.BASE:
+			case Token.CHECKED:
+			case Token.FALSE:
+			case Token.FIXED:
+			case Token.NEW:
+			case Token.NULL:
+			case Token.SIZEOF:
+			case Token.THIS:
+			case Token.THROW:
+			case Token.TRUE:
+			case Token.TYPEOF:
+			case Token.UNCHECKED:
+			case Token.UNSAFE:
+				return true;
+
+			default:
+				return false;
+			}
 		}
+
+		public int token ()
+                {
+			current_token = xtoken ();
+                        return current_token;
+                }
 
 		static StringBuilder static_cmd_arg = new System.Text.StringBuilder ();
 		
