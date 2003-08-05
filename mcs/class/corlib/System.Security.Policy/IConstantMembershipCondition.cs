58cd72173c8d462aeed0398fded42e520d9fa155
Duncan Mak <duncan@mono-cvs.ximian.com>
2003-08-05 04:27:47 +0000

    2003-08-04  Duncan Mak  <duncan@ximian.com>
    	* IConstantMembershipCondition.cs: added.
    
    svn path=/trunk/mcs/; revision=17067

---
+++ mcs/class/corlib/System.Security.Policy/IConstantMembershipCondition.cs
@@ -0,0 +1,13 @@
+//
+// System.Security.Policy.IConstantMembershipCondition.cs
+// 
+// Author: Duncan Mak <duncan@ximian.com>
+// 
+// (C) Ximian, Inc. 2003.
+//
+// Note: this is just a marker interface and exposes no methods.
+//
+namespace System.Security.Policy {
+
+        internal interface IConstantMembershipCondition {}
+}
