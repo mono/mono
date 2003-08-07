d6f7ecbb6915099fe2e6bf0039c92e88378ac227
Atsushi Eno <atsushieno@gmail.com>
2003-08-07 22:04:46 +0000

    2003-08-07  Atsushi Enomoto <ginga@kit.hi-ho.ne.jp>
    	* added IHasXmlParserContext.cs.
    
    svn path=/trunk/mcs/; revision=17165

---
+++ mcs/class/System.XML/Mono.Xml/IHasXmlParserContext.cs
@@ -0,0 +1,19 @@
+//
+// Mono.Xml.IHasParserContext.cs
+//
+// Author:
+//	Atsushi Enomoto  (ginga@kit.hi-ho.ne.jp)
+//
+// (C) 2003 Atsushi Enomoto
+//
+// This interface is used to support ResolveEntity for custom XmlReader.
+//
+using System.Xml;
+
+namespace Mono.Xml
+{
+	public interface IHasXmlParserContext
+	{
+		XmlParserContext ParserContext { get; }
+	}
+}
