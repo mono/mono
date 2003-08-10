93d64e7d1ed5e21d2379d259eecb884c1e799bd9
Dennis Hayes <dennis@mono-cvs.ximian.com>
2003-08-10 07:54:44 +0000

    signiture fixes. added control as base class for ComponentEditorForm.cs
    svn path=/trunk/mcs/; revision=17220

--- mcs/class/System.Windows.Forms/System.Windows.Forms.Design/ComponentEditorForm.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms.Design/ComponentEditorForm.cs
@@ -7,45 +7,51 @@
 //
 using System;
 using System.Windows.Forms;
-namespace System.Windows.Forms.Design
-{
+namespace System.Windows.Forms.Design {
 	/// <summary>
 	/// Summary description for ComponentEditorForm.
 	/// </summary>
-	public class ComponentEditorForm { //: Form {
-		public ComponentEditorForm()
-		{
+	public class ComponentEditorForm : Form {
+		public ComponentEditorForm() {
 			//
 			// TODO: Add constructor logic here
 			//
 			throw new NotImplementedException ();
 		}
+
 		public ComponentEditorForm(object component, Type[] pageTypes){
 			//
 			// TODO: Add constructor logic here
 			//
 			throw new NotImplementedException ();
 		}
-//		protected override void OnActivated(EventArgs e){
-//			throw new NotImplementedException ();
-//		}
-//		public override bool PreProcessMessage(ref Message msg){
-//			throw new NotImplementedException ();
-//		}
-//		public virtual DialogResult ShowForm(){
-//			throw new NotImplementedException ();
-//		}
-//		public virtual DialogResult ShowForm(int page){
-//			throw new NotImplementedException ();
-//		}
-//		public virtual DialogResult ShowForm(IWin32Window owner){
-//			throw new NotImplementedException ();
-//		}
-//		public virtual DialogResult ShowForm(IWin32Window owner, int page){
-//			throw new NotImplementedException ();
-//		}
-//		protected override viod onHelpRequested(HelpEventArgs e){
-//			throw new NotImplementedException ();
-//		}
+
+		protected override void OnActivated(EventArgs e){
+			throw new NotImplementedException ();
+		}
+
+		public override bool PreProcessMessage(ref Message msg){
+			throw new NotImplementedException ();
+		}
+
+		public virtual DialogResult ShowForm(){
+			throw new NotImplementedException ();
+		}
+
+		public virtual DialogResult ShowForm(int page){
+			throw new NotImplementedException ();
+		}
+
+		public virtual DialogResult ShowForm(IWin32Window owner){
+			throw new NotImplementedException ();
+		}
+
+		public virtual DialogResult ShowForm(IWin32Window owner, int page){
+			throw new NotImplementedException ();
+		}
+		// can't override the function in control. bug in compiler
+		//protected override void onHelpRequested(HelpEventArgs e){
+		//	throw new NotImplementedException ();
+		//}
 	}
 }
