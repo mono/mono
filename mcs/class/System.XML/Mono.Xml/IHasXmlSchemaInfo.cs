2fc2cb8972c04653a1520401388070dbba50ca35
Atsushi Eno <atsushieno@gmail.com>
2003-08-14 12:51:28 +0000

    2003-08-14  Atsushi Enomoto <ginga@kit.hi-ho.ne.jp>
    	* added IHasXmlSchemaInfo.cs.
    
    svn path=/trunk/mcs/; revision=17320

---
+++ mcs/class/System.XML/Mono.Xml/IHasXmlSchemaInfo.cs
@@ -0,0 +1,19 @@
+//
+// Mono.Xml.IHasXmlSchemaInfo.cs
+//
+// Author:
+//	Atsushi Enomoto  (ginga@kit.hi-ho.ne.jp)
+//
+// (C) 2003 Atsushi Enomoto
+//
+// This interface is used to support XmlSchema type information.
+//
+using System.Xml;
+
+namespace Mono.Xml
+{
+	public interface IHasXmlSchemaInfo
+	{
+		object SchemaType { get; }
+	}
+}
