a8d038fdf2973b24e83f9114e28637f7b8f4146a
Ben Maurer <benm@mono-cvs.ximian.com>
2003-08-19 22:34:09 +0000

    2003-08-19 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* XslNumber.cs: Initial support.
    
    svn path=/trunk/mcs/; revision=17419

--- mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslNumber.cs
+++ mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslNumber.cs
@@ -14,6 +14,7 @@ using System.Collections;
 using System.Xml;
 using System.Xml.XPath;
 using System.Xml.Xsl;
+using System.Text;
 
 namespace Mono.Xml.Xsl.Operations {
 	public class XslNumber : XslCompiledElement {
@@ -114,36 +115,231 @@ namespace Mono.Xml.Xsl.Operations {
 			
 			switch (this.level) {
 				case XslNumberingLevel.Single:
-					throw new NotImplementedException ();
+					return nf.Format (NumberSingle (p));
 				case XslNumberingLevel.Multiple:
 					throw new NotImplementedException ();
 				case XslNumberingLevel.Any:
-					throw new NotImplementedException ();
+					return nf.Format (NumberAny (p));
 				default:
 					throw new Exception ("Should not get here");
 			}
 		}
+		int NumberAny (XslTransformProcessor p)
+		{
+			int i = 0;
+			XPathNavigator n = p.CurrentNode.Clone ();
+			do {
+				do {
+					if (MatchesCount (n, p)) i++;
+					if (MatchesFrom (n, p)) return i;
+				} while (n.MoveToPrevious ());
+			} while (n.MoveToParent ());
+			return 0;
+		}
+		int NumberSingle (XslTransformProcessor p)
+		{
+			XPathNavigator n = p.CurrentNode.Clone ();
+		
+			while (!MatchesCount (n, p)) {
+				if (from != null && MatchesFrom (n, p))
+					return 0;
+				
+				if (!n.MoveToParent ())
+					return 0;
+			}
+			
+			if (from != null) {
+				XPathNavigator tmp = n.Clone ();
+				if (MatchesFrom (tmp, p))
+					// Was not desc of closest matches from
+					return 0;
+				
+				bool found = false;
+				while (tmp.MoveToParent ())
+					if (MatchesFrom (tmp, p)) {
+						found = true; break;
+					}
+				if (!found)
+					// not desc of matches from
+					return 0;
+			}
+			
+			int i = 1;
+				
+			while (n.MoveToPrevious ()) {
+				if (MatchesCount (n, p)) i++;
+			}
+				
+			return i;
+		}
+		
+		bool MatchesCount (XPathNavigator item, XslTransformProcessor p)
+		{
+			if (count == null)
+				return item.LocalName == p.CurrentNode.LocalName &&
+					item.NamespaceURI == p.CurrentNode.NamespaceURI;
+			else
+				return item.Matches (count);
+		}
+		
+		bool MatchesFrom (XPathNavigator item, XslTransformProcessor p)
+		{
+			if (from == null)
+				return item.NodeType == XPathNodeType.Root;
+			else
+				return item.Matches (from);
+		}
 		
 		class XslNumberFormatter {
+			string firstSep = "", lastSep = "";
+			ArrayList fmtList = new ArrayList ();
+			
 			public XslNumberFormatter (string format, string lang, string letterValue, char groupingSeparator, int groupingSize)
 			{
-				throw new NotImplementedException ();
+				// We dont do any i18n now, so we ignore lang and letterValue.
+				if (format == null || format == "")
+					fmtList.Add (FormatItem.GetItem (null, "1", groupingSeparator, groupingSize));
+				else {
+					NumberFormatterScanner s = new NumberFormatterScanner (format);
+					
+					string sep, itm;
+					
+					sep = s.Advance (false);
+					itm = s.Advance (true);
+					
+					if (itm == null) {
+						lastSep = sep;
+						fmtList.Add (FormatItem.GetItem (null, "1", groupingSeparator, groupingSize));
+					} else {
+						firstSep = sep;
+						sep = null;
+					
+						while (itm != null) {
+							fmtList.Add (FormatItem.GetItem (sep, itm, groupingSeparator, groupingSize));
+							sep = s.Advance (false);
+							itm = s.Advance (true);
+						}
+						
+						lastSep = sep;
+					}
+				}
 			}
 			
 			public int NumbersNeeded {
-				get { throw new NotImplementedException (); }
+				get { return fmtList.Count; }
 			}
 			
 			// return the format for a single value, ie, if using Single or Any
 			public string Format (int value)
 			{
-				throw new NotImplementedException ();
+				StringBuilder b = new StringBuilder ();
+				if (firstSep != null) b.Append (firstSep);
+				((FormatItem)fmtList [0]).Format (b, value);
+				if (lastSep != null) b.Append (lastSep);
+				
+				return b.ToString ();
 			}
 			
 			// format for an array of numbers.
 			public string Format (int [] values)
 			{
 				throw new NotImplementedException ();
+			}
+			
+			class NumberFormatterScanner {
+				int pos = 0, len;
+				string fmt;
+				
+				public NumberFormatterScanner (string fmt) {
+					this.fmt = fmt;
+					len = fmt.Length;
+				}
+				
+				public string Advance (bool alphaNum)
+				{
+					int start = pos;
+					while ((pos < len) && (char.IsLetterOrDigit (fmt, pos) == alphaNum))
+						pos++;
+					
+					if (pos == start)
+						return null;
+					else
+						return fmt.Substring (start, pos - start);
+				}
+			}
+			
+			abstract class FormatItem {
+				public readonly string sep;
+				public FormatItem (string sep)
+				{
+					this.sep = sep;
+				}
+				
+				public abstract void Format (StringBuilder b, int num);
+					
+				public static FormatItem GetItem (string sep, string item, char gpSep, int gpSize)
+				{
+					switch (item [0])
+					{
+						case '0': case '1':
+							return new DigitItem (sep, item.Length, gpSep, gpSize);
+						case 'a':
+							return new AlphaItem (sep, false);
+						case 'A':
+							return new AlphaItem (sep, true);
+						case 'i':
+							return new RomanItem (sep, false);
+						case 'I':
+							return new RomanItem (sep, true);
+						
+						default:
+							throw new Exception ();
+					}
+				}
+			}
+			
+			class AlphaItem : FormatItem {
+				bool uc;
+				public AlphaItem (string sep, bool uc) : base (sep)
+				{
+					this.uc = uc;
+				}
+				
+				public override void Format (StringBuilder b, int num)
+				{
+					throw new NotImplementedException ();
+				}
+			}
+			
+			class RomanItem : FormatItem {
+				bool uc;
+				public RomanItem (string sep, bool uc) : base (sep)
+				{
+					this.uc = uc;
+				}
+				
+				public override void Format (StringBuilder b, int num)
+				{
+					throw new NotImplementedException ();
+				}
+			}
+			
+			class DigitItem : FormatItem {
+				int len, gpSize;
+				char gpSep;
+				
+				public DigitItem (string sep, int len, char gpSep, int gpSize) : base (sep)
+				{
+					this.len = len;
+					this.gpSep = gpSep;
+					this.gpSize = gpSize;
+				}
+				
+				public override void Format (StringBuilder b, int num)
+				{
+					// TODO Formatting
+					b.Append (num);
+				}
 			}
 		}
 	}
