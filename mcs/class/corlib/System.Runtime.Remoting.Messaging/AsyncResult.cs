309488f3ab44b2ca97e4e9ce7b435e0f1df01199
Lluis Sanchez <lluis@novell.com>
2003-08-20 14:22:17 +0000

    * MethodCall.cs: Changed exception text to match MS.NET (more or less). 	* AsyncResult.cs: Added new fields to store async calls results.
    svn path=/trunk/mcs/; revision=17444

--- mcs/class/corlib/System.Runtime.Remoting.Messaging/AsyncResult.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Messaging/AsyncResult.cs
@@ -25,7 +25,11 @@ public class AsyncResult : IAsyncResult, IMessageSink {
 	bool sync_completed;
 	bool completed;
 	bool endinvoke_called;
+	object[] out_args;
+	object ret_val;
+	Exception exception;
 	IMessageCtrl message_ctrl;
+	IMessage reply_message;
 		
 	public virtual object AsyncState
 	{
@@ -84,10 +88,9 @@ public class AsyncResult : IAsyncResult, IMessageSink {
 		throw new NotSupportedException ();
 	}
 
-	[MonoTODO]
 	public virtual IMessage GetReplyMessage()
 	{
-		throw new NotImplementedException ();
+		return reply_message;
 	}
 
 	public virtual void SetMessageCtrl (IMessageCtrl mc)
@@ -95,8 +98,19 @@ public class AsyncResult : IAsyncResult, IMessageSink {
 		message_ctrl = mc;
 	}
 
+	internal void SetCompletedSynchronously (bool completed)
+	{
+		sync_completed = completed;
+	}
+
 	public virtual IMessage SyncProcessMessage (IMessage msg)
 	{
+		IMethodReturnMessage retMsg = (IMethodReturnMessage) msg;
+		reply_message = msg;
+		out_args = retMsg.OutArgs;
+		ret_val = retMsg.ReturnValue;
+		exception = retMsg.Exception;
+
 		completed = true;
 		NativeEventCalls.SetEvent_internal (handle.Handle);
 		// TODO: invoke callback
--- mcs/class/corlib/System.Runtime.Remoting.Messaging/MethodCall.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Messaging/MethodCall.cs
@@ -264,7 +264,7 @@ namespace System.Runtime.Remoting.Messaging {
 			if (_uri != null)
 			{
 				Type type = RemotingServices.GetServerTypeForUri (_uri);
-				if (type == null) throw new RemotingException ("No receiver for uri " + _uri);
+				if (type == null) throw new RemotingException ("Requested service not found. No receiver for uri " + _uri);
 
 				if (CanCastTo (_typeName, type))
 				{
