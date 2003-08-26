a95dbdef4134ad30ea6e487a5f432c55301360eb
Martin Baulig <martin@novell.com>
2003-08-26 18:59:49 +0000

    2003-08-26  Martin Baulig  <martin@ximian.com>
    	* test-214.cs: New test for bug #47853.
    
    svn path=/trunk/mcs/; revision=17628

---
+++ mcs/tests/test-214.cs
@@ -0,0 +1,23 @@
+using T1 = N1.C1;
+
+namespace N2
+{
+	class Test : T1
+	{
+		static int Main()
+		{
+			// Compilation-only test.
+			Foo ();
+			return 0;
+		}
+	}
+}
+
+namespace N1
+{
+	public class C1
+	{        
+		public static void Foo ()
+		{ }
+	}
+}
