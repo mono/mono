82de61aee3d02928784b64ff9fe00432ab233da7
Alexandre Pigolkine <pigolkine@mono-cvs.ximian.com>
2003-08-31 00:58:45 +0000

    stubs added
    svn path=/trunk/mcs/; revision=17759

--- mcs/class/System.Windows.Forms/System.Windows.Forms.Design/ComponentEditor.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms.Design/ComponentEditor.cs
@@ -6,20 +6,49 @@
 // (C) 2002 Ximian, Inc.  http://www.ximian.com
 //
 using System;
-
+using System.ComponentModel;
 namespace System.Windows.Forms.Design
 {
 	/// <summary>
 	/// Summary description for WindowsFormsComponentEditor.
 	/// </summary>
-	public class ComponentEditor
+	public abstract class ComponentEditor
 	{
 		public ComponentEditor()
 		{
 			//
 			// TODO: Add constructor logic here
 			//
-			throw new NotImplementedException ();
+		}
+		
+		public bool EditComponent(object obj)
+		{
+			return false;
+		}
+		
+		public abstract bool EditComponent(ITypeDescriptorContext ctx, object obj);
+	}
+	
+	public class WindowsFormsComponentEditor : ComponentEditor
+	{
+		public WindowsFormsComponentEditor()
+		{
+		}
+		
+		public override bool EditComponent(ITypeDescriptorContext ctx, object obj)
+		{
+			return false;
+		}
+		
+		public bool EditComponent(object obj, IWin32Window iwnd)
+		{
+			return false;
+		}
+		
+		public virtual bool EditComponent(ITypeDescriptorContext ctx, object obj, IWin32Window iwnd)
+		{
+			return false;
 		}
 	}
+
 }
--- mcs/class/System.Windows.Forms/System.Windows.Forms.Design/IUIService.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms.Design/IUIService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 namespace System.Windows.Forms.Design
 {
@@ -7,5 +8,19 @@ namespace System.Windows.Forms.Design
 	/// </summary>
 	public interface IUIService
 	{
+		IDictionary Styles {get;}
+		
+		bool CanShowComponentEditor( object component);		
+		IWin32Window GetDialogOwnerWindow();
+		void SetUIDirty();
+		bool ShowComponentEditor( object component, IWin32Window parent);		
+		void ShowError(Exception ex);
+		void ShowError(string str);
+		void ShowError(Exception ex, string str);
+		DialogResult ShowDialog(Form form);		
+		void ShowMessage(string str);
+		void ShowMessage(string str1, string str2);
+		DialogResult ShowMessage(string str1, string str2, MessageBoxButtons btn);
+		bool ShowToolWindow( Guid toolWindow);
 	}
 }
