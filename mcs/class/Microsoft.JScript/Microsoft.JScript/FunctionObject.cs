ed1460d4801bb3b1d24c24007fbc53e9f08b0c60
César Natarén <cesar@mono-cvs.ximian.com>
2003-09-20 06:56:31 +0000

    jaja, forgot to commit this
    svn path=/trunk/mcs/; revision=18205

--- mcs/class/Microsoft.JScript/Microsoft.JScript/FunctionObject.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/FunctionObject.cs
@@ -13,6 +13,11 @@ namespace Microsoft.JScript.Tmp
 
 	public class FunctionObject : ScriptFunction
 	{
+		internal string Name;
+		internal string ReturnType;
+		internal FormalParameterList Params;
+		internal Block Body;
+	    
 		public override string ToString ()
 		{
 			throw new NotImplementedException ();
