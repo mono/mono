0cfb0dcccfcf6fdf4d44479d50cb08b6c56fa462
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-09-25 16:42:53 +0000

    2003-09-25  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* ObjectHandle.cs: simplified and fix InitializeLifetimeService.
    
    svn path=/trunk/mcs/; revision=18295

--- mcs/class/corlib/System.Runtime.Remoting/ObjectHandle.cs
+++ mcs/class/corlib/System.Runtime.Remoting/ObjectHandle.cs
@@ -18,35 +18,21 @@ namespace System.Runtime.Remoting {
 
 	public class ObjectHandle : MarshalByRefObject, IObjectHandle {
 		private object _wrapped;
-		private object life_ctrl;
 		
 		public ObjectHandle (object o)
 		{
 			_wrapped = o;
 		}
 
-		[MonoTODO]
 		public override object InitializeLifetimeService ()
 		{
-			life_ctrl = base.InitializeLifetimeService ();
-			ILease ilife_ctrl = life_ctrl as ILease;
-			
-			if (ilife_ctrl != null)
-
-				// I can't see in the .NET docs if the lifetime counter
-				// must be initialized to the time the object is created
-				// or if a relativistic time is enough (as both differ in
-				// fact just in a constant. In the meantime I'll use 0.
-			
-				ilife_ctrl.InitialLeaseTime = new TimeSpan ((long) 0);
-			
-			return ilife_ctrl;
+			return base.InitializeLifetimeService ();
 		}
 
 		public object Unwrap ()
 		{
 			return _wrapped;
 		}
-
 	}
 }
+
