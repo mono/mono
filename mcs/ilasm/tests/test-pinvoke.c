cda05294fd2427a38ba0fd725ccf873704578598
Jackson Harper <jackson@novell.com>
2003-10-06 16:08:14 +0000

    New tests for pinvoke
    svn path=/trunk/mcs/; revision=18677

---
+++ mcs/ilasm/tests/test-pinvoke.c
@@ -0,0 +1,19 @@
+/** 
+ * test-pinvoke.c: native functions for testing ilasm pinvoke
+ *
+ * Author(s):
+ *  Jackson Harper (jackson@ximian.com)
+ *
+ * (C) 2003 Ximian, Inc (http://www.ximian.com)
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+ret_one ()
+{
+        return 1;
+}
