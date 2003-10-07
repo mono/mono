2571ce223352e264c2e90e11d9bb01359177862d
Miguel de Icaza <miguel@gnome.org>
2003-10-07 05:40:04 +0000

    Improve test to check array initializations from enumerations
    svn path=/trunk/mcs/; revision=18695

--- mcs/tests/test-165.cs
+++ mcs/tests/test-165.cs
@@ -4,6 +4,15 @@ public class Testing
 {   
         enum Fruit { Apple, Banana, Cherry };   
 
+	static int TestEnumInit (Fruit f)
+	{
+		Fruit [] testedFruits = { f };
+
+		if (f != Fruit.Apple)
+			return 1;
+		return 0;
+	}
+
         public static int Main()   
         {   
                 Fruit[] pieFillings = { Fruit.Apple, Fruit.Banana, Fruit.Cherry };
@@ -14,6 +23,9 @@ public class Testing
 			return 2;
 		if (pieFillings [2] != Fruit.Cherry)
 			return 3;
+
+		if (TestEnumInit (Fruit.Apple) != 0)
+			return 4;
 
 		return 0;
         }          
