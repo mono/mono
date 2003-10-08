bac4c7895c75ac41b633f58d79b29ec5cae38c82
Martin Baulig <martin@novell.com>
2003-10-08 20:11:16 +0000

    Simplify this a bit.
    svn path=/trunk/mcs/; revision=18770

--- mcs/tests/gen-14.cs
+++ mcs/tests/gen-14.cs
@@ -1,16 +1,10 @@
-using System;
-
 public class Stack<S>
 {
 	public Stack (S s)
-	{
-		Console.WriteLine ("STACK CTOR: {0} {1}", s, s.GetType ());
-	}
+	{ }
 
 	public void Push (S s)
-	{
-		Console.WriteLine ("STACK PUSH: {0} {1}", s, s.GetType ());
-	}
+	{ }
 }
 
 public class X
@@ -18,11 +12,9 @@ public class X
 	static void Main ()
 	{
 		Stack<int> s1 = new Stack<int> (3);
-		Stack<string> s2 = new Stack<string> ("Hello");
-
-		Console.WriteLine (s1);
 		s1.Push (4);
-		Console.WriteLine (s2);
+
+		Stack<string> s2 = new Stack<string> ("Hello");
 		s2.Push ("Test");
 	}
 }
