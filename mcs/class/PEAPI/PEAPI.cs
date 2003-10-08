44bcf820ba4540273a2d2eb9ac63ad1e6914b87a
Jackson Harper <jackson@novell.com>
2003-10-08 00:17:29 +0000

    * PEAPI.cs: Add unbox.any instruction.
    svn path=/trunk/mcs/; revision=18730

--- mcs/class/PEAPI/PEAPI.cs
+++ mcs/class/PEAPI/PEAPI.cs
@@ -632,7 +632,7 @@ namespace PEAPI
                       unbox = 0x79, stobj = 0x81, box = 0x8C, newarr, 
                       ldelema = 0x8F, refanyval = 0xC2, mkrefany = 0xC6, 
                       ldtoken = 0xD0, initobj = 0xFE15, sizeOf = 0xFE1C,
-                      ldelem = 0xA3, stelem = 0xA4 }
+                      ldelem = 0xA3, stelem = 0xA4, unbox_any }
 
   /// <summary>
   /// CIL branch instructions
