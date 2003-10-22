2bc272925ffc77abeb9aa5fdcd121f5e68c196eb
Todd Berman <tberman@mono-cvs.ximian.com>
2003-10-22 18:10:05 +0000

    2003-10-22  Todd Berman <tberman@gentoo.org>
            * RoutingOutputFilter.cs: Quick WSE2 hack for the To and Action fields,
            still needs a lot of implementation.
    
    svn path=/trunk/mcs/; revision=19304

--- mcs/class/Microsoft.Web.Services/Microsoft.Web.Services.Routing/RoutingOutputFilter.cs
+++ mcs/class/Microsoft.Web.Services/Microsoft.Web.Services.Routing/RoutingOutputFilter.cs
@@ -21,6 +21,16 @@ namespace Microsoft.Web.Services.Routing {
 			if (envelope == null)
 				throw new ArgumentNullException ("envelope");
 			// TODO
+#if WSE2
+//Quick quick quick Hack for some Addressing stuff
+			if(envelope.Context.To != null) {	
+				envelope.CreateHeader ().AppendChild (envelope.Context.To.GetXml (envelope));
+			}
+			if(envelope.Context.Action != null) {
+				envelope.CreateHeader ().AppendChild (envelope.Context.Action.GetXml (envelope));
+			}
+		
+#endif
 		}
 	}
 }
