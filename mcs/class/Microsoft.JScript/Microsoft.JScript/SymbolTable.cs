1eb8317b22ac2e5ec322e413a9ca3f4f62173cf6
César Natarén <cesar@mono-cvs.ximian.com>
2003-11-03 17:03:27 +0000

    * SymbolTable.cs: Added Add method that receives the id and the function declaration class. Retrieve changed to return an AST. Added ToString method.
    svn path=/trunk/mcs/; revision=19586

--- mcs/class/Microsoft.JScript/Microsoft.JScript/SymbolTable.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/SymbolTable.cs
@@ -7,14 +7,14 @@
 // (C) 2003, Cesar Lopez Nataren, <cesar@ciencias.unam.mx>
 //
 
-
 using System;
 using System.Collections;
+using  System.Text;
 
-namespace Microsoft.JScript.Tmp
-{
-	internal class SymbolTable
-	{
+namespace Microsoft.JScript {
+
+	internal class SymbolTable {
+
 		internal Hashtable symbols;
 
 		internal SymbolTable ()
@@ -28,16 +28,30 @@ namespace Microsoft.JScript.Tmp
 			symbols.Add (id, d);
 		}
 
-
-		internal VariableDeclaration Retrieve (string id)
+		internal void Add (string id, FunctionDeclaration fd)
 		{
-			return ((VariableDeclaration) symbols [id]);
+			symbols.Add (id, fd);
 		}
 
-		
+		internal AST Retrieve (string id)
+		{
+			return ((AST) symbols [id]);
+		}
+
 		internal bool Contains (string id)
 		{
 			return symbols.ContainsKey (id);
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder sb = new StringBuilder ();
+			ICollection keys = symbols.Keys;
+
+			foreach (object o in keys)
+				sb.Append (o.ToString ());
+
+			return sb.ToString ();
 		}
 	}
 }
