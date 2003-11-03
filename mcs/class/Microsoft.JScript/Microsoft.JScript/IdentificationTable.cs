06ea3d23e7678c15d30826e1e4a53003938b3501
César Natarén <cesar@mono-cvs.ximian.com>
2003-11-03 16:58:12 +0000

    * IdentificationTable.cs: Added ToString method. Retrieve now returns an AST not a VariableDeclaration.
    svn path=/trunk/mcs/; revision=19583

--- mcs/class/Microsoft.JScript/Microsoft.JScript/IdentificationTable.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/IdentificationTable.cs
@@ -8,11 +8,12 @@
 //
 
 using System.Collections;
+using System.Text;
 
-namespace Microsoft.JScript.Tmp
-{
-	internal class IdentificationTable
-	{
+namespace Microsoft.JScript {
+
+	internal class IdentificationTable {
+
 		internal Stack stack;
 
 		internal IdentificationTable ()
@@ -20,35 +21,43 @@ namespace Microsoft.JScript.Tmp
 			stack = new Stack ();
 			stack.Push (new SymbolTable ());
 		}
-
 		
 		internal void OpenBlock ()
 		{
 			stack.Push (new SymbolTable ());
 		}
 
-
 		internal void CloseBlock ()
 		{
 			stack.Pop ();
 		}
 
-
 		internal void Enter (string id, VariableDeclaration decl)
 		{
 			((SymbolTable) stack.Peek ()).Add (id , decl);
+			System.Console.WriteLine ("IdentificationTable::Enter");
 		}
 
-
-		internal VariableDeclaration Retrieve (string id)
+		internal AST Retrieve (string id)
 		{
 			return ((SymbolTable) stack.Peek ()).Retrieve (id);
 		}
 
-
 		internal bool Contains (string id)
 		{
 			return ((SymbolTable) stack.Peek ()).Contains (id);
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder sb = new StringBuilder ();
+
+			int i, size = stack.Count;
+
+			for (i = 0; i < size; i++)
+				sb.Append (stack.Pop ().ToString ());
+
+			return sb.ToString ();
 		}
 	}
 }
