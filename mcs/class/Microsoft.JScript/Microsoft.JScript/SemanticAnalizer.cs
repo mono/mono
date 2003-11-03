730c7e436750c0638a269504b224e3d149207c5d
César Natarén <cesar@mono-cvs.ximian.com>
2003-11-03 17:01:06 +0000

    * SemanticAnalizer.cs: Fixed the file name (maybe I should erase it and add it again with the correct name). Not based on the Visitor pattern anymore. Deleted all the VisitFOO methods. Added a IdentificationTable field. Added Run, Dump methods.
    svn path=/trunk/mcs/; revision=19584

--- mcs/class/Microsoft.JScript/Microsoft.JScript/SemanticAnalizer.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/SemanticAnalizer.cs
@@ -1,6 +1,5 @@
 //
-// SemanticAnaliser.cs: A implementation of the Visitor interface.
-//			It performs the Semantic Analysis for EcmaScript
+// SemanticAnalyser.cs: Initiate the type check and identification phases.
 //
 // Author:
 //	Cesar Lopez Nataren
@@ -8,113 +7,30 @@
 // (C) 2003, Cesar Lopez Nataren, <cesar@ciencias.unam.mx>
 //
 
-namespace Microsoft.JScript.Tmp
-{
-	using System;
+using System;
 
-	internal class SemanticAnaliser : Visitor
-	{
-		private IdentificationTable symTab;
+namespace Microsoft.JScript {
 
-		public object VisitASTList (ASTList prog, object obj)
-		{
-			throw new NotImplementedException ();
-		}
+	public class SemanticAnalyser {
 
-
-		public object VisitVariableDeclaration (VariableDeclaration decl, 
-							object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitFunctionDeclaration (FunctionDeclaration decl,
-							object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitArrayLiteral (ArrayLiteral al, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-		
-		public object VisitBlock (Block b, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-		
-		public object VisitEval (Eval e, object args)
-		{	
-			throw new NotImplementedException ();
-		}
-
-		
-		public object VisitForIn (ForIn forIn, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitFunctionExpression (FunctionExpression fexp,	
-						       object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitImport (Import imp, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitPackage (Package pkg, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitScriptBlock (ScriptBlock sblock, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitThrow (Throw t, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitTry (Try t, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitWith (With w, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public object VisitPrint (Print p, object args)
-		{
-			throw new NotImplementedException ();
-		}
-
-	
 		//
-		// We dont need to check anything on literals.
+		// We must include the default 'Global Object',
+		// which contains the built-in objects: Math, String, ...
+		// static void init_default_global_object ()
 		//
-		public object VisitStringLiteral (StringLiteral sl, object args)
+
+		static IdentificationTable context;
+
+		public static bool Run (ScriptBlock prog)
 		{
-			return null;
-		}		
+			context = new IdentificationTable ();
+
+			return prog.Resolve (context);
+		}
+
+		public static void Dump ()
+		{
+			Console.WriteLine (context.ToString ());
+		}
 	}
 }
