ac122271f9deb2e559613c68cfb1f5773656c3aa
Andreas N <andreas@mono-cvs.ximian.com>
2003-11-13 19:29:30 +0000

    2003-11-13  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* IExpando.cs: Added attribute
    
    svn path=/trunk/mcs/; revision=19953

--- mcs/class/corlib/System.Runtime.InteropServices.Expando/IExpando.cs
+++ mcs/class/corlib/System.Runtime.InteropServices.Expando/IExpando.cs
@@ -12,7 +12,7 @@ using System.Reflection.Emit;
 
 namespace System.Runtime.InteropServices.Expando
 {
-	//[Guid("")]
+	[Guid("afbf15e6-c37c-11d2-b88e-00a0c9b471b8")]
 	public interface IExpando : IReflect
 	{
 		FieldInfo AddField (string name);
