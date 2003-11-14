68723379f435e2aa4d3066cd280010cefb32c859
Sebastien Pouliot <sebastien@ximian.com>
2003-11-14 01:37:33 +0000

    2003-11-13  Sebastien Pouliot  <spouliot@videotron.ca>
    	* ProtectedData.cs: Added exceptions - core is still TODO.
    	* ProtectedMemory.cs: Added exceptions - core is still TODO.
    
    svn path=/trunk/mcs/; revision=19978

--- mcs/class/corlib/System.Security.Cryptography/ProtectedData.cs
+++ mcs/class/corlib/System.Security.Cryptography/ProtectedData.cs
@@ -13,18 +13,33 @@ using System;
 
 namespace System.Security.Cryptography {
 
+	// References:
+	// a.	Windows Data Protection
+	//	http://msdn.microsoft.com/library/en-us/dnsecure/html/windataprotection-dpapi.asp?frame=true
+
 	public sealed class ProtectedData {
+
+		// FIXME: interop could be important under windows - if one application protect some data using
+		// mono and another one unprotects it using ms.net
 
 		[MonoTODO ("interop with MS implementation ?")]
 		public static byte[] Protect (byte[] userData, byte[] optionalEntropy, DataProtectionScope scope) 
 		{
-			return userData;
+			if (userData == null)
+				throw new ArgumentNullException ("userData");
+
+			// on Windows this is supported only under 2000 and later OS
+			throw new PlatformNotSupportedException ();
 		}
 
 		[MonoTODO ("interop with MS implementation ?")]
 		public static byte[] Unprotect (byte[] encryptedData, byte[] optionalEntropy, DataProtectionScope scope) 
 		{
-			return encryptedData;
+			if (encryptedData == null)
+				throw new ArgumentNullException ("encryptedData");
+
+			// on Windows this is supported only under 2000 and later OS
+			throw new PlatformNotSupportedException ();
 		}
  	} 
 }
