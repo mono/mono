3d5de3815031731e5c34aa344822da2a37f2adc0
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-11-14 07:36:29 +0000

    2003-11-14  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* corlib_test.dll.sources: added new ValueTypeTest.cs
    
    	* System/ValueType.cs:
    	(Equals): compare the fields of structs too.
    	(GetHashCode): combine the hash code of all the fields.
    	Fixes bug #50901 (will remove the icall in a couple of days).
    
    	* Test/corlib_linux_test.args: Removed file.
    	* Test/corlib_test.args: Removed file.
    	* Test/System/ValueTypeTest.cs: New file.
    
    svn path=/trunk/mcs/; revision=19994

--- mcs/class/corlib/System/ValueType.cs
+++ mcs/class/corlib/System/ValueType.cs
@@ -3,10 +3,13 @@
 //
 // Author:
 //   Miguel de Icaza (miguel@ximian.com)
+//   Gonzalo Paniagua Javier (gonzalo@ximian.com)
 //
 // (C) Ximian, Inc.  http://www.ximian.com
+// (C) 2003 Novell, Inc.  http://www.novell.com
 //
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace System {
@@ -25,16 +28,65 @@ namespace System {
 		//   True if this instance and o represent the same type
 		//   and have the same value.
 		// </summary>
-		public override bool Equals (object o) {
-			return InternalEquals (this, o);
+		public override bool Equals (object o)
+		{
+			if (o == null)
+				return false;
+
+			bool result = InternalEquals (this, o);
+			if (result)
+				return result;
+
+			Type me = GetType ();
+			Type you = o.GetType ();
+			if (me != you)
+				return false;
+
+			BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+					     BindingFlags.Instance;
+
+			FieldInfo [] meFlds = me.GetFields (flags);
+			FieldInfo [] youFlds = you.GetFields (flags);
+			for (int i = meFlds.Length - 1; i >= 0; i--) {
+				object meVal = meFlds [i].GetValue (this);
+				object youVal = youFlds [i].GetValue (o);
+				if (meVal == null) {
+					if (youVal == null)
+						continue;
+
+					return false;
+				}
+
+				result = meVal.Equals (youVal);
+				if (!result)
+					return false;
+			}
+
+			return true;
 		}
 
 		// <summary>
 		//   Gets a hashcode for this value type using the
 		//   bits in the structure
 		// </summary>
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		public extern override int GetHashCode ();
+		public override int GetHashCode ()
+		{
+			Type me = GetType ();
+			BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+					     BindingFlags.Instance;
+
+			FieldInfo [] meFlds = me.GetFields (flags);
+			if (meFlds.Length == 0)
+				return base.GetHashCode ();
+
+			int result = 0;
+			foreach (object o in meFlds) {
+				if (o != null)
+					result ^= o.GetHashCode ();
+			}
+
+			return result;
+		}
 
 		// <summary>
 		//   Stringified representation of this ValueType.
