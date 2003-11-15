641b45f1bc30e909c475e5f27cd9183637598569
Martin Baulig <martin@novell.com>
2003-11-15 11:02:52 +0000

    Two new tests .....
    svn path=/trunk/mcs/; revision=20029

---
+++ mcs/tests/gen-32.cs
@@ -0,0 +1,21 @@
+interface Foo<S>
+{
+	void Hello (S s);
+}
+
+interface Bar<T,U> : Foo<U>
+{
+	void Test (T t, U u);
+}
+
+class X
+{
+	static void Test (Bar<int,string> bar)
+	{
+		bar.Hello ("Test");
+		bar.Test (7, "Hello");
+	}
+
+	static void Main ()
+	{ }
+}
