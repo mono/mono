022323677325fad15ea5cc44a697592b995fb502
Miguel de Icaza <miguel@gnome.org>
2003-11-17 21:37:48 +0000

    Add new files
    svn path=/trunk/mcs/; revision=20139

---
+++ mcs/class/corlib/System.Runtime.InteropServices/BestFitMappingAttribute.cs
@@ -0,0 +1,31 @@
+//
+// System.Runtime.InteropServices.BestFitMappingAttribute.cs
+//
+// Author:
+//   Miguel de Icaza (miguel@ximian.com)
+//
+// (C) Novell, Inc.  http://www.ximian.com
+//
+using System;
+
+namespace System.Runtime.InteropServices {
+
+	[AttributeUsage (AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+	[Serializable]
+	public class BestFitMappingAttribute : Attribute {
+		bool bfm;
+		
+		public BestFitMappingAttribute (bool bfm)
+		{
+			this.bfm = bfm;
+		}
+
+		public bool ThrowOnUnmappableChar = false;
+		
+		public bool BestFitMapping {
+			get {
+				return bfm;
+			}
+		}
+	}
+}
