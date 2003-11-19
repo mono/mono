a6ef74259e980fb9666fb122a5ec2604e3d5068c
Lluis Sanchez <lluis@novell.com>
2003-11-19 17:03:18 +0000

    InternalRemotingServices.cs: Added missing attributes.
    svn path=/trunk/mcs/; revision=20230

--- mcs/class/corlib/System.Runtime.Remoting/InternalRemotingServices.cs
+++ mcs/class/corlib/System.Runtime.Remoting/InternalRemotingServices.cs
@@ -8,18 +8,20 @@
 //
 
 using System;
+using System.Diagnostics;
 using System.Runtime.Remoting.Metadata;
 using System.Runtime.Remoting.Messaging;
 
 namespace System.Runtime.Remoting
 {
-	public sealed class InternalRemotingServices 
+	public class InternalRemotingServices 
 	{
 		public InternalRemotingServices ()
 		{
 		}
 		
 		[MonoTODO]
+		[Conditional("_LOGGING")]
 		public static void DebugOutChnl (string s)
 		{
 			throw new NotImplementedException ();
@@ -32,12 +34,14 @@ namespace System.Runtime.Remoting
 		}
 		
 		[MonoTODO]
+		[Conditional("_DEBUG")]
 		public static void RemotingAssert (bool condition, string message)
 		{
 			throw new NotImplementedException ();
 		}
 		
 		[MonoTODO]
+		[Conditional("_LOGGING")]
 		public static void RemotingTrace (params object[] messages)
 		{
 			throw new NotImplementedException ();
