af0830eb583480729c8770be1b82de6b6e77f58f
Miguel de Icaza <miguel@gnome.org>
2003-11-22 20:39:16 +0000

    Apply patch from Wojciech Polak <polak@gnu.org>, updates errno values
    svn path=/trunk/mcs/; revision=20337

--- mcs/class/corlib/Unix/Errno.cs
+++ mcs/class/corlib/Unix/Errno.cs
@@ -8,54 +8,150 @@ namespace System.Private {
 
 		internal static string Message (int code)
 		{
-			switch (code){
+			switch (code) {
 			case Wrapper.EPERM:
-				return "No permission";
+				return "Operation not permitted";
 			case Wrapper.ENOENT:
-				return "The name does not exist";
+				return "No such file or directory";
 			case Wrapper.EISDIR:
-				return "error: Is a directory";
+				return "Is a directory";
 			case Wrapper.EBADF:
 				return "Bad file descriptor";
 			case Wrapper.ENOMEM:
-				return "Out of memory";
+				return "Cannot allocate memory";
 			case Wrapper.EEXIST:
 				return "File already exists";
 			case Wrapper.ENOTEMPTY:
-				return "Directory is not empty";
-				
+				return "Directory not empty";
 			case Wrapper.ESRCH:
+				return "No such process";
 			case Wrapper.EINTR:
+				return "Interrupted function call";
 			case Wrapper.EIO:
+				return "Input/output error";
 			case Wrapper.ENXIO:
+				return "No such device or address";
 			case Wrapper.E2BIG:
+				return "Argument list too long";
 			case Wrapper.ENOEXEC:
+				return "Exec format error";
 			case Wrapper.ECHILD:
+				return "No child processes";
 			case Wrapper.EAGAIN:
+				return "Resource temporarily unavailable";
 			case Wrapper.EACCES:
+				return "Permission denied";
 			case Wrapper.EFAULT:
+				return "Bad address";
 			case Wrapper.ENOTBLK:
+				return "Block device required";
 			case Wrapper.EBUSY:
+				return "Device or resource busy";
 			case Wrapper.EXDEV:
+				return "Invalid cross-device link";
 			case Wrapper.ENODEV:
+				return "No such device";
+			case Wrapper.ENOTDIR:
+				return "Not a directory";
 			case Wrapper.EINVAL:
+				return "Invalid argument";
 			case Wrapper.ENFILE:
+				return "Too many open files in system";
 			case Wrapper.EMFILE:
+				return "Too many open files";
 			case Wrapper.ENOTTY:
+				return "Inappropriate ioctl for device";
 			case Wrapper.ETXTBSY:
+				return "Text file busy";
 			case Wrapper.EFBIG:
+				return "File too large";
 			case Wrapper.ENOSPC:
+				return "No space left on device";
 			case Wrapper.ESPIPE:
+				return "Illegal seek";
 			case Wrapper.EROFS:
+				return "Read-only file system";
 			case Wrapper.EMLINK:
+				return "Too many links";
 			case Wrapper.EPIPE:
+				return "Broken pipe";
 			case Wrapper.EDOM:
+				return "Numerical argument out of domain";
 			case Wrapper.ERANGE:
+				return "Numerical result out of range";
 			case Wrapper.EDEADLK:
+				return "Resource deadlock avoided";
 			case Wrapper.ENAMETOOLONG:
+				return "File name too long";
 			case Wrapper.ENOLCK:
+				return "No locks available";
 			case Wrapper.ENOSYS:
+				return "Function not implemented";
 			case Wrapper.ELOOP:
+				return "Too many levels of symbolic links";
+			case Wrapper.EUSERS:
+				return "Too many users";
+			case Wrapper.ENOTSOCK:
+				return "Socket operation on non-socket";
+			case Wrapper.EDESTADDRREQ:
+				return "Destination address required";
+			case Wrapper.EMSGSIZE:
+				return "Message too long";
+			case Wrapper.EPROTOTYPE:
+				return "Protocol wrong type for socket";
+			case Wrapper.ENOPROTOOPT:
+				return "Protocol not available";
+			case Wrapper.EPROTONOSUPPORT:
+				return "Protocol not supported";
+			case Wrapper.ESOCKTNOSUPPORT:
+				return "Socket type not supported";
+			case Wrapper.EOPNOTSUPP:
+				return "Operation not supported";
+			case Wrapper.EPFNOSUPPORT:
+				return "Protocol family not supported";
+			case Wrapper.EAFNOSUPPORT:
+				return "Address family not supported by protocol";
+			case Wrapper.EADDRINUSE:
+				return "Address already in use";
+			case Wrapper.EADDRNOTAVAIL:
+				return "Cannot assign requested address";
+			case Wrapper.ENETDOWN:
+				return "Network is down";
+			case Wrapper.ENETUNREACH:
+				return "Network is unreachable";
+			case Wrapper.ENETRESET:
+				return "Network dropped connection on reset";
+			case Wrapper.ECONNABORTED:
+				return "Software caused connection abort";
+			case Wrapper.ECONNRESET:
+				return "Connection reset by peer";
+			case Wrapper.ENOBUFS:
+				return "No buffer space available";
+			case Wrapper.EISCONN:
+				return "Transport endpoint is already connected";
+			case Wrapper.ENOTCONN:
+				return "Transport endpoint is not connected";
+			case Wrapper.ESHUTDOWN:
+				return "Cannot send after transport endpoint shutdown";
+			case Wrapper.ETOOMANYREFS:
+				return "Too many references: cannot splice";
+			case Wrapper.ETIMEDOUT:
+				return "Connection timed out";
+			case Wrapper.ECONNREFUSED:
+				return "Connection refused";
+			case Wrapper.EHOSTDOWN:
+				return "Host is down";
+			case Wrapper.EHOSTUNREACH:
+				return "No route to host";
+			case Wrapper.EALREADY:
+				return "Operation already in progress";
+			case Wrapper.EINPROGRESS:
+				return "Operation now in progress";
+			case Wrapper.ESTALE:
+				return "Stale NFS file handle";
+			case Wrapper.EDQUOT:
+				return "Disk quota exceeded";
+			case Wrapper.ENOMEDIUM:
 			case Wrapper.ENOMSG:
 			case Wrapper.EIDRM:
 			case Wrapper.ECHRNG:
@@ -96,42 +192,7 @@ namespace System.Private {
 			case Wrapper.ELIBSCN:
 			case Wrapper.ELIBMAX:
 			case Wrapper.ELIBEXEC:
-			case Wrapper.EUSERS:
-			case Wrapper.ENOTSOCK:
-			case Wrapper.EDESTADDRREQ:
-			case Wrapper.EMSGSIZE:
-			case Wrapper.EPROTOTYPE:
-			case Wrapper.ENOPROTOOPT:
-			case Wrapper.EPROTONOSUPPORT:
-			case Wrapper.ESOCKTNOSUPPORT:
-			case Wrapper.EOPNOTSUPP:
-			case Wrapper.EPFNOSUPPORT:
-			case Wrapper.EAFNOSUPPORT:
-			case Wrapper.EADDRINUSE:
-			case Wrapper.EADDRNOTAVAIL:
-			case Wrapper.ENETDOWN:
-			case Wrapper.ENETUNREACH:
-			case Wrapper.ENETRESET:
-			case Wrapper.ECONNABORTED:
-			case Wrapper.ECONNRESET:
-			case Wrapper.ENOBUFS:
-			case Wrapper.EISCONN:
-			case Wrapper.ENOTCONN:
-			case Wrapper.ESHUTDOWN:
-			case Wrapper.ETOOMANYREFS:
-			case Wrapper.ETIMEDOUT:
-			case Wrapper.ECONNREFUSED:
-			case Wrapper.EHOSTDOWN:
-			case Wrapper.EHOSTUNREACH:
-			case Wrapper.EALREADY:
-			case Wrapper.EINPROGRESS:
-			case Wrapper.ESTALE:
-			case Wrapper.EDQUOT:
-			case Wrapper.ENOMEDIUM:
 				break;
-				
-			case Wrapper.ENOTDIR:
-				return "Not a directory";
 			}
 			return String.Format ("Errno code={0}", code);
 		}
