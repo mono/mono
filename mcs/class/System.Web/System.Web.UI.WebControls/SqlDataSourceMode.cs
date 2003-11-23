069539d2b064d27f2ecc90464dbacbfc4e7e0e0a
Ben Maurer <benm@mono-cvs.ximian.com>
2003-11-23 02:44:44 +0000

    2003-11-22 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* SqlDataSource*: Implement almost everything. Data access is
    	still missing.
    
    svn path=/trunk/mcs/; revision=20349

---
+++ mcs/class/System.Web/System.Web.UI.WebControls/SqlDataSourceMode.cs
@@ -0,0 +1,22 @@
+//
+// System.Web.UI.WebControls.SqlDataSourceMode
+//
+// Authors:
+//	Ben Maurer (bmaurer@users.sourceforge.net)
+//
+// (C) 2003 Ben Maurer
+//
+
+#if NET_1_2
+using System.Collections;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace System.Web.UI.WebControls {
+	public enum SqlDataSourceMode {
+		DataReader,
+		DataSet
+	}
+}
+#endif
+
---
+++ mcs/class/System.Web/System.Web.UI.WebControls/SqlDataSourceStatusEventHandler.cs
@@ -0,0 +1,15 @@
+//
+// System.Web.UI.WebControls.SqlDataSourceStatusEventHandler
+//
+// Authors:
+//	Ben Maurer (bmaurer@users.sourceforge.net)
+//
+// (C) 2003 Ben Maurer
+//
+
+#if NET_1_2
+namespace System.Web.UI.WebControls {
+	public delegate void SqlDataSourceStatusEventHandler (object source, SqlDataSourceStatusEventArgs e);
+}
+#endif
+
