0be4ac62daf18e6a1fb323759178aad1469d9908
Jackson Harper <jackson@novell.com>
2003-11-25 21:42:21 +0000

    * HttpStaticObjectsCollection.cs: Add methods for serialization 	and conversion to/from byte arrays.
    svn path=/trunk/mcs/; revision=20438

--- mcs/class/System.Web/System.Web/HttpStaticObjectsCollection.cs
+++ mcs/class/System.Web/System.Web/HttpStaticObjectsCollection.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections;
 
 namespace System.Web {
@@ -55,5 +56,73 @@ namespace System.Web {
             return this;
          }
       }
+
+           private void Set (string name, object obj)
+           {
+                   _Objects [name] = obj;
+           }
+           
+           internal void Serialize (BinaryWriter w)
+           {
+                   lock (_Objects) {
+                           w.Write (Count);
+                           foreach (string key in _Objects.Keys) {
+                                   w.Write (key);
+                                   object value = _Objects [key];
+                                   if (value == null) {
+                                           w.Write (System.Web.Util.AltSerialization.NullIndex);
+                                           continue;
+                                   }
+                                   
+                                   System.Web.Util.AltSerialization.SerializeByType (w, value);
+                           }
+                   }
+           }
+
+           internal static HttpStaticObjectsCollection Deserialize (BinaryReader r)
+           {
+                   HttpStaticObjectsCollection result = new HttpStaticObjectsCollection ();
+                   for (int i = r.ReadInt32 (); i > 0; i--) {
+                           string key = r.ReadString ();
+                           int index = r.ReadInt32 ();
+                           if (index == System.Web.Util.AltSerialization.NullIndex)
+                                   result.Set (key, null);
+                           else
+                                   result.Set (key, System.Web.Util.AltSerialization.DeserializeFromIndex (index, r));
+                   }
+                   
+   		   return result;
+	   }
+
+	   internal byte [] ToByteArray ()
+	   {
+		   MemoryStream stream = null;
+		   try {
+			   stream = new MemoryStream ();
+			   Serialize (new BinaryWriter (stream));
+			   return stream.GetBuffer ();
+		   } catch {
+			   throw;
+		   } finally {
+			   if (stream != null)
+				   stream.Close ();
+		   }
+	   }
+
+	   internal static HttpStaticObjectsCollection FromByteArray (byte [] data)
+	   {
+		   HttpStaticObjectsCollection objs = null;
+		   MemoryStream stream = null;
+		   try {
+			   stream = new MemoryStream (data);
+			   objs = Deserialize (new BinaryReader (stream));
+		   } catch {
+			   throw;
+		   } finally {
+			   if (stream != null)
+				   stream.Close ();
+		   }
+		   return objs;
+	   }
    }
 }
