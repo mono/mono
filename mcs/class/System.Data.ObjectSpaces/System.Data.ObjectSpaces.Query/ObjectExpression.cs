a85f0c4bde4ae9588744a632201bcfcd5fee92ea
Tim Coleman <tim@mono-cvs.ximian.com>
2003-11-28 03:45:04 +0000

    2003-11-27  Tim Coleman <tim@timcoleman.com>         * System.Data.ObjectSpaces.Query/ObjectExpression.cs:         * System.Data.ObjectSpaces.Query/ObjectSpaceNode.cs:         * System.Data.ObjectSpaces_test.dll.sources:                 Various updates for completion
    svn path=/trunk/mcs/; revision=20572

--- mcs/class/System.Data.ObjectSpaces/System.Data.ObjectSpaces.Query/ObjectExpression.cs
+++ mcs/class/System.Data.ObjectSpaces/System.Data.ObjectSpaces.Query/ObjectExpression.cs
@@ -3,16 +3,63 @@
 //
 //
 // Author:
-//	Richard Thombs (stony@stony.org)
+//     Richard Thombs (stony@stony.org)
+//     Tim Coleman (tim@timcoleman.com)
+//
+// Copyright (C) Tim Coleman, 2003
 //
 
 #if NET_1_2
 
-namespace System.Data.ObjectSpaces.Query
-{
-	[MonoTODO()]
+using System.Data.ObjectSpaces;
+
+namespace System.Data.ObjectSpaces.Query {
 	public class ObjectExpression
 	{
+		#region Fields
+
+		Expression expression;
+		ObjectSchema objectSchema;
+
+		#endregion // Fields
+
+		#region Constructors
+
+		[MonoTODO]
+		public ObjectExpression (Expression expression, ObjectSchema objectSchama)
+		{
+			this.expression = expression;
+			this.objectSchema = objectSchema;
+		}
+
+		#endregion // Constructors
+
+		#region Properties
+
+		public Expression Expression {
+			get { return expression; }
+		}
+
+		public ObjectSchema ObjectSchema {
+			get { return objectSchema; }
+		}
+
+		[MonoTODO]
+		public Type ObjectType {
+			get { throw new NotImplementedException (); }
+		}
+
+		#endregion // Properties
+
+		#region Methods
+
+		[MonoTODO]
+		public CompiledQuery Compile (MappingSchema map)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion // Methods
 	}
 }
 
--- mcs/class/System.Data.ObjectSpaces/System.Data.ObjectSpaces.Query/ObjectSpaceNode.cs
+++ mcs/class/System.Data.ObjectSpaces/System.Data.ObjectSpaces.Query/ObjectSpaceNode.cs
@@ -3,21 +3,35 @@
 //
 //
 // Author:
-//	Richard Thombs (stony@stony.org)
+//     Richard Thombs (stony@stony.org)
+//     Tim Coleman (tim@timcoleman.com)
+//
+// Copyright (C) Tim Coleman, 2003
 //
 
 #if NET_1_2
 
-namespace System.Data.ObjectSpaces.Query
-{
-	[MonoTODO()]
+namespace System.Data.ObjectSpaces.Query {
 	public class ObjectSpaceNode : Expression
 	{
+		#region Constructors
+
+		public ObjectSpaceNode ()
+			: base ()
+		{
+		}
+
+		#endregion
+
+		#region Methods
+
 		[MonoTODO()]
 		public override object Clone()
 		{
 			throw new NotImplementedException();
 		}
+
+		#endregion // Methods
 	}
 }
 
