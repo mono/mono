aa1b31d4a4e8624cf292d2f43ca9ebeecd00be58
Patrik Torstensson <totte@mono-cvs.ximian.com>
2003-12-09 12:03:02 +0000

    2003-12-09  Patrik Torstensson  <p@rxc.se>
    	* CryptAPI.cs: Added stdcall callconv to all Win32
    	  calls, default in Mono is CDECL
    
    svn path=/trunk/mcs/; revision=20928

--- mcs/class/Mono.Security.Win32/Mono.Security.Cryptography/CryptoAPI.cs
+++ mcs/class/Mono.Security.Win32/Mono.Security.Cryptography/CryptoAPI.cs
@@ -14,29 +14,29 @@ namespace Mono.Security.Cryptography {
 
 internal class CryptoAPI {
 
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptAcquireContextA (ref IntPtr phProv, string pszContainer, string pszProvider, int dwProvType, uint dwFlags);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptCreateHash (IntPtr hProv, uint Algid, IntPtr hKey, uint dwFlags, ref IntPtr phHash);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptDecrypt (IntPtr hKey, IntPtr hHash, bool Final, uint dwFlags, byte[] pbData, ref uint pdwDataLen);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptDestroyHash (IntPtr hHash);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptDestroyKey (IntPtr hKey);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptEncrypt (IntPtr hKey, IntPtr hHash, bool Final, uint dwFlags, byte[] pbData, ref uint pdwDataLen, uint dwBufLen);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptGenKey (IntPtr hProv, uint Algid, uint dwFlags, ref IntPtr phKey);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptGenRandom (IntPtr hProv, uint dwLen, byte[] pbBuffer);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptGetHashParam (IntPtr hHash, uint dwParam, byte[] pbData, ref uint pdwDataLen, uint dwFlags);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptHashData (IntPtr hHash, byte[] pbData, uint dwDataLen, uint dwFlags);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptImportKey (IntPtr hProv, byte[] pbData, uint dwDataLen, IntPtr hPubKey, uint dwFlags, ref IntPtr phKey);
-	[DllImport ("advapi32.dll", SetLastError=true)]
+	[DllImport ("advapi32.dll", CallingConvention=CallingConvention.StdCall, SetLastError=true)]
 	public static extern bool CryptReleaseContext (IntPtr hProv, uint dwFlags);
 
 	public static readonly uint CRYPT_VERIFYCONTEXT  = 0xF0000000;
