5862c7a7834bdf523515bcda26c3fc393757e9ce
Lluis Sanchez <lluis@novell.com>
2003-12-10 16:40:59 +0000

    SoapTypeAttribute.cs: Use full namespace name.
    svn path=/trunk/mcs/; revision=20974

--- mcs/class/corlib/System.Runtime.Remoting.Metadata/SoapTypeAttribute.cs
+++ mcs/class/corlib/System.Runtime.Remoting.Metadata/SoapTypeAttribute.cs
@@ -129,7 +129,7 @@ namespace System.Runtime.Remoting.Metadata {
 			{
 				string na;
 				if (type.Assembly == typeof (object).Assembly) na = string.Empty;
-				else na = type.Assembly.GetName().Name;
+				else na = type.Assembly.FullName;
 				_xmlTypeNamespace = SoapServices.CodeXmlNamespaceForClrTypeNamespace (type.Namespace, na);
 			}
 			
