7becefbc103befd988d16d8b93eca4f94acbd35c
Sebastien Pouliot <sebastien@ximian.com>
2003-12-15 12:18:41 +0000

    2003-12-15  Sebastien Pouliot  <spouliot@videotron.ca>
    	* MACAlgorithm.cs: Fixed difference between 1.0 and 1.1 framework.
    	The 1.0 framework is adding an additional  padding block (empty)
    	to MAC when MACing an exact multiple of the TripleDES block size.
    
    svn path=/trunk/mcs/; revision=21163

--- mcs/class/corlib/Mono.Security.Cryptography/MACAlgorithm.cs
+++ mcs/class/corlib/Mono.Security.Cryptography/MACAlgorithm.cs
@@ -23,40 +23,75 @@ namespace Mono.Security.Cryptography {
 
 		private SymmetricAlgorithm algo;
 		private ICryptoTransform enc;
-		private BlockProcessor block;
-	
-		public MACAlgorithm (SymmetricAlgorithm algorithm)
+		private byte[] block;
+		private int blockSize;
+		private int blockCount;
+
+		public MACAlgorithm (SymmetricAlgorithm algorithm) 
 		{
 			algo = (SymmetricAlgorithm) algorithm;
 			algo.Mode = CipherMode.CBC;
 			algo.Padding = PaddingMode.Zeros;
-			algo.IV = new byte [(algo.BlockSize >> 3)];
+			blockSize = (algo.BlockSize >> 3); // in bytes
+			algo.IV = new byte [blockSize];
+			block = new byte [blockSize];
 		}
 	
 		public void Initialize (byte[] key) 
 		{
 			algo.Key = key;
-			// note: the encryptor transform amy be reused - see Final
+			// note: the encryptor transform may be reusable - see Final
 			if (enc == null) {
 				enc = algo.CreateEncryptor ();
-				block = new BlockProcessor (enc);
 			}
-			block.Initialize ();
+			Array.Clear (block, 0, blockSize);
+			blockCount = 0;
 		}
 		
 		public void Core (byte[] rgb, int ib, int cb) 
 		{
-			block.Core (rgb, ib, cb);
+			// 1. fill the rest of the "block"
+			int n = System.Math.Min (blockSize - blockCount, cb);
+			Array.Copy (rgb, ib, block, blockCount, n); 
+			blockCount += n;
+	
+			// 2. if block is full then transform it
+			if (blockCount == blockSize) {
+				enc.TransformBlock (block, 0, blockSize, block, 0);
+	
+				// 3. transform any other full block in specified buffer
+				int b = (int) ((cb - n) / blockSize);
+				for (int i=0; i < b; i++) {
+					enc.TransformBlock (rgb, n, blockSize, block, 0);
+					n += blockSize;
+				}
+	
+				// 4. if data is still present fill the "block" with the remainder
+				blockCount = cb - n;
+				if (blockCount > 0)
+					Array.Copy (rgb, n, block, 0, blockCount);
+			}
 		}
 	
 		public byte[] Final () 
 		{
-			byte[] mac = block.Final ();
+			byte[] result;
+			if (blockCount > 0)
+				result = enc.TransformFinalBlock (block, 0, blockCount);
+			else {
+#if NET_1_0
+				// add an empty (zeros) block for MAC padding
+				byte[] emptyBlock = new byte [blockSize];
+				result = enc.TransformFinalBlock (emptyBlock, 0, blockSize);
+#else
+				result = (byte[]) block.Clone ();
+#endif
+			}
 			if (!enc.CanReuseTransform) {
-				enc.Dispose();
+				enc.Dispose ();
 				enc = null;
 			}
-			return mac;
+			return result;
 		}
 	}
 }
