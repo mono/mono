80778f5c068f8c7f23b5b02ddaa3771a679459a2
Atsushi Eno <atsushieno@gmail.com>
2003-12-15 20:03:43 +0000

    2003-12-15  Atsushi Enomoto <atsushi@novell.com> 	* UpdateEventHandler.cs, 	  XmlAdapter.cs, 	  XmlDataSourceResolve.cs, 	  XmlUpdateEventArgs.cs : Initial checkin.
    svn path=/trunk/mcs/; revision=21200

---
+++ mcs/class/System.Data.SqlXml/System.Xml/UpdateEventHandler.cs
@@ -0,0 +1,15 @@
+//
+// UpdateEventHandler.cs
+//
+// Author:
+//	Atsushi Enomoto <ginga@kit.hi-ho.ne.jp>
+//
+//
+
+#if NET_1_2
+namespace System.Xml
+{
+	public delegate void UpdateEventHandler (object sender, XmlUpdateEventArgs e);
+}
+
+#endif
