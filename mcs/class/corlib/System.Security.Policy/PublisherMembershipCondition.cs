94b570f1f6715c6e8e28d95fa7a6f0930c2630d2
Sebastien Pouliot <sebastien@ximian.com>
2003-12-16 02:06:55 +0000

    2003-12-15  Sebastien Pouliot  <spouliot@videotron.ca>
    	* PublisherMembershipCondition.cs: Completed TODO.
    
    svn path=/trunk/mcs/; revision=21210

--- mcs/class/corlib/System.Security.Policy/PublisherMembershipCondition.cs
+++ mcs/class/corlib/System.Security.Policy/PublisherMembershipCondition.cs
@@ -4,11 +4,13 @@
 // Author:
 //	Sebastien Pouliot (spouliot@motus.com)
 //
-// (C) 2002 Motus Technologies Inc. (http://www.motus.com)
+// (C) 2002, 2003 Motus Technologies Inc. (http://www.motus.com)
 //
 
 using System;
+using System.Collections;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace System.Security.Policy {
 
@@ -29,6 +31,7 @@ namespace System.Security.Policy {
 			x509 = certificate;
 		}
 	
+		// LAMESPEC: Undocumented ArgumentNullException exception
 		public X509Certificate Certificate {
 			get { return x509; }
 			set { 
@@ -38,10 +41,16 @@ namespace System.Security.Policy {
 			}
 		}
 	
-		[MonoTODO()]
 		public bool Check (Evidence evidence) 
 		{
-			return true;
+			IEnumerator e = evidence.GetHostEnumerator ();
+			while (e.MoveNext ()) {
+				if (e.Current is Publisher) {
+					if (x509.Equals ((e.Current as Publisher).Certificate))
+						return true;
+				}
+			}
+			return false;
 		}
 	
 		public IMembershipCondition Copy () 
@@ -56,14 +65,38 @@ namespace System.Security.Policy {
 			return x509.Equals ((o as PublisherMembershipCondition).Certificate);
 		}
 	
-		[MonoTODO()]
 		public void FromXml (SecurityElement e) 
 		{
+			FromXml (e, null);
 		}
 	
-		[MonoTODO()]
+		private byte FromHexChar (char c) 
+		{
+			if ((c >= 'A') && (c <= 'F'))
+				return (byte) (c - 'A' + 10);
+			if ((c >= '0') && (c <= '9'))
+				return (byte) (c - '0');
+			throw new ArgumentException ("invalid hex char");
+		}
+
 		public void FromXml (SecurityElement e, PolicyLevel level) 
 		{
+			if (e == null)
+				throw new ArgumentNullException ("e");
+			if (e.Tag != "IMembershipCondition")
+				throw new ArgumentException ("Not IMembershipCondition", "e");
+			// PolicyLevel isn't used as there's no need to resolve NamedPermissionSet references
+			string cert = e.Attribute ("X509Certificate");
+			if (cert != null) {
+				byte[] rawcert = new byte [cert.Length >> 1];
+				int n = 0;
+				int i = 0;
+				while (n < rawcert.Length) {
+					rawcert [n] = (byte) (FromHexChar (cert[i++]) << 4);
+					rawcert [n++] += FromHexChar (cert[i++]);
+				}
+				x509 = new X509Certificate (rawcert);
+			}
 		}
 	
 		public override int GetHashCode () 
@@ -75,17 +108,30 @@ namespace System.Security.Policy {
 		{
 			return "Publisher - " + x509.GetPublicKeyString ();
 		}
-	
-		[MonoTODO()]
-		public SecurityElement ToXml () 
+
+		// snippet moved from FileIOPermission (nickd) to be reused in all derived classes
+		internal SecurityElement Element (object o, int version) 
 		{
-			return null;
+			SecurityElement se = new SecurityElement ("IMembershipCondition");
+			Type type = this.GetType ();
+			StringBuilder asmName = new StringBuilder (type.Assembly.ToString ());
+			asmName.Replace ('\"', '\'');
+			se.AddAttribute ("class", type.FullName + ", " + asmName);
+			se.AddAttribute ("version", version.ToString ());
+			return se;
 		}
 	
-		[MonoTODO()]
+		public SecurityElement ToXml () 
+		{
+			return ToXml (null);
+		}
+	
 		public SecurityElement ToXml (PolicyLevel level) 
 		{
-			return null;
+			// PolicyLevel isn't used as there's no need to resolve NamedPermissionSet references
+			SecurityElement se = Element (this, 1);
+			se.AddAttribute ("X509Certificate", x509.GetRawCertDataString ());
+			return se;
 		}
 	}
 }
