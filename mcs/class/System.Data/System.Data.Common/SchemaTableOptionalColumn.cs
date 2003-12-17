d2d5721bf9570b95f0f3fad3c57cb79531faea4d
Jackson Harper <jackson@novell.com>
2003-12-17 09:49:42 +0000

    * SchemaTableOptionalColumn.cs: Add to fix default build
    svn path=/trunk/mcs/; revision=21266

---
+++ mcs/class/System.Data/System.Data.Common/SchemaTableOptionalColumn.cs
@@ -0,0 +1,23 @@
+//
+// System.Data.Common.SchemaTableOptionalColumn
+//
+// Author(s):
+//  Jackson Harper (jackson@ximian.com)
+//
+// (C) 2003 Novell, Inc (http://www.novell.com)
+//
+
+
+#if NET_1_2
+
+namespace System.Data.Common {
+
+        // Just here to fix the build
+        public class SchemaTableOptionalColumn {
+
+        }
+        
+}
+
+#endif
+
