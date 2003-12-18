f044dd148f9ea2133ed8e889141299c244fd2ccf
Tim Coleman <tim@mono-cvs.ximian.com>
2003-12-18 21:27:49 +0000

    2003-12-18  Tim Coleman <tim@timcoleman.com> 	SchemaTableOptionalColumn.cs: 		Forgot to commit before.  Oops.
    svn path=/trunk/mcs/; revision=21334

--- mcs/class/System.Data/System.Data.Common/SchemaTableOptionalColumn.cs
+++ mcs/class/System.Data/System.Data.Common/SchemaTableOptionalColumn.cs
@@ -1,23 +1,28 @@
 //
-// System.Data.Common.SchemaTableOptionalColumn
+// System.Data.Common.SchemaTableOptionalColumn.cs
 //
-// Author(s):
-//  Jackson Harper (jackson@ximian.com)
+// Author:
+//   Tim Coleman (tim@timcoleman.com)
 //
-// (C) 2003 Novell, Inc (http://www.novell.com)
+// Copyright (C) Tim Coleman, 2003
 //
-
 
 #if NET_1_2
 
 namespace System.Data.Common {
+	public sealed class SchemaTableOptionalColumn 
+	{
+		#region Fields
+			public static readonly string BaseCatalogName = "BaseCatalogName";
+			public static readonly string BaseServerName = "BaseServerName";
+			public static readonly string IsAutoIncrement = "IsAutoIncrement";
+			public static readonly string IsHidden = "IsHidden";
+			public static readonly string IsReadOnly = "IsReadOnly";
+			public static readonly string IsRowVersion = "IsRowVersion";
+			public static readonly string ProviderSpecificDataType = "ProviderSpecificDataType";
 
-        // Just here to fix the build
-        public class SchemaTableOptionalColumn {
-
-        }
-        
+		#endregion // Fields
+	}
 }
 
-#endif
-
+#endif // NET_1_2
