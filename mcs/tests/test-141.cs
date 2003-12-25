278452729f87d914bd2e8ef5ee412c4e60784417
Ben Maurer <benm@mono-cvs.ximian.com>
2003-12-25 23:29:54 +0000

    2003-12-25 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* test-141.cs: add tests for #38674.
    
    svn path=/trunk/mcs/; revision=21497

--- mcs/tests/test-141.cs
+++ mcs/tests/test-141.cs
@@ -3,23 +3,59 @@ using System;
 class X {
 	public static int Main()
 	{
+		if (! Test1 ()) return 1;
+		if (! Test2 ()) return 2;
+		if (! Test3 ()) return 3;
+		
+		return 0;
+	}
+	
+	static bool Test1 ()
+	{
 		byte num1 = 105;
 		byte num2 = 150;
-		byte sum;
 
-		bool ok = false;
+		// should generate OverflowException
+		try {
+			checked {
+				byte sum = (byte) (num1 - num2);
+			}
+			
+			return false;
+		} catch (OverflowException) {
+			return true;
+		}
+	}
+	
+	static bool Test2 ()
+	{
+		long l = long.MinValue;
 		
 		// should generate OverflowException
 		try {
 			checked {
-				sum = (byte) (num1 - num2);
+				l = - l;
 			}
-		} catch (OverflowException){
-			ok = true;
+			
+			return false;
+		} catch (OverflowException) {
+			return true;
 		}
-
-		if (ok)
-			return 0;
-		return 1;
+	}
+	
+	static bool Test3 ()
+	{
+		int i = int.MinValue;
+		
+		// should generate OverflowException
+		try {
+			checked {
+				i = - i;
+			}
+			
+			return false;
+		} catch (OverflowException) {
+			return true;
+		}
 	}
 }
