08bfb7aba68233371d8bdb8b1014f44b47a6fa70
Sebastien Pouliot <sebastien@ximian.com>
2004-01-03 01:35:06 +0000

    2004-01-02  Sebastien Pouliot  <spouliot@videotron.ca>
    	* EvidenceTest.cs: Upgraded tests to NUnit2. Added new tests.
    	* StrongNameTest.cs: Class now inherits from Assertion.
    
    svn path=/trunk/mcs/; revision=21613

--- mcs/class/corlib/Test/System.Security.Policy/EvidenceTest.cs
+++ mcs/class/corlib/Test/System.Security.Policy/EvidenceTest.cs
@@ -1,29 +1,26 @@
 //
 // MonoTests.System.Security.Policy.EvidenceTest
 //
-// Author(s):
-//   Jackson Harper (Jackson@LatitudeGeo.com)
+// Authors:
+//	Jackson Harper (Jackson@LatitudeGeo.com)
+//	Sebastien Pouliot (spouliot@motus.com)
 //
 // (C) 2001 Jackson Harper, All rights reserved.
-
+// Portions (C) 2003, 2004 Motus Technologies Inc. (http://www.motus.com)
+//
 
 using System;
 using System.Collections;
 using System.Security.Policy;
 using NUnit.Framework;
 
+namespace MonoTests.System.Security.Policy {
 
-namespace MonoTests.System.Security.Policy
-{
-
-	public class EvidenceTest : TestCase 
-	{
+	[TestFixture]
+	public class EvidenceTest : Assertion {
 		
-		protected override void SetUp()
-		{
-		}
-
-		public void TestDefaultConstructor()
+		[Test]
+		public void DefaultConstructor ()
 		{
 			Evidence evidence = new Evidence ();
 			
@@ -36,34 +33,33 @@ namespace MonoTests.System.Security.Policy
 				evidence.GetEnumerator().MoveNext(), false);
 		}
 
-		public void TestMultipleConstructor() {
-			Evidence evidence;
+		[Test]
+		public void MultipleConstructor ()
+		{
 			object[] hostarray = new object[10];
 			object[] assemarray = new object[10];
-
-			evidence = new Evidence ( hostarray, assemarray );
+			Evidence evidence = new Evidence ( hostarray, assemarray );
 
 			AssertEquals ( "Count of multiple arg constructor should equal 20", evidence.Count, 20 );
 		}
 
-		public void TestCopyConstructor() 
+		[Test]
+		public void CopyConstructor ()
 		{
-			Evidence evidence1, evidence2;
 			object[] hostlist = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmblist = { "asmb-1", "asmb-2", "asmb-3", "asmb-4" };
-
-			evidence1 = new Evidence (hostlist, asmblist);
-			evidence2 = new Evidence (evidence1);
+			Evidence evidence1 = new Evidence (hostlist, asmblist);
+			Evidence evidence2 = new Evidence (evidence1);
 			
 			AssertEquals("Copy constructor counts do not match", evidence1.Count, evidence2.Count);
 		}
 
-		public void TestAddAssembly() 
+		[Test]
+		public void AddAssembly ()
 		{
 			Evidence evidence = new Evidence ();
 			object[] comparray = new object[100];
 			string obj;
-			int index;
 
 			for (int i=0; i<100; i++) {
 				obj = String.Format ("asmb-{0}", i+1);
@@ -72,19 +68,19 @@ namespace MonoTests.System.Security.Policy
 				AssertEquals (evidence.Count, i+1);
 			}
 			
-			index = 0;
+			int index = 0;
 			foreach (object compobj in evidence) {
 				AssertEquals ("Comparison object does not equal evidence assembly object", 
 					comparray[index++], compobj);
 			}
 		}
 
-		public void TestAddHost()
+		[Test]
+		public void AddHost ()
 		{
 			Evidence evidence = new Evidence ();
 			object[] comparray = new object[100];
 			string obj;
-			int index;
 
 			for (int i=0; i<100; i++) {
 				obj = String.Format ("asmb-{0}", i+1);
@@ -93,44 +89,42 @@ namespace MonoTests.System.Security.Policy
 				AssertEquals (evidence.Count, i+1);
 			}
 
-			index = 0;
+			int index = 0;
 			foreach (object compobj in evidence) {
 				AssertEquals ("Comparison object does not equal evidence host object", 
 					comparray[index++], compobj);
 			}
 		}
 
-		public void TestMultiArgConstructorForEach() 
+		[Test]
+		public void MultiArgConstructorForEach ()
 		{
 			object[] hostarray = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmbarray = { "asmb-1", "asmb-2", "asmb-3", "asmb-4" };
 			ArrayList compare = new ArrayList (); 
 			Evidence evidence = new Evidence (hostarray, asmbarray);
-			int i;
 		
 			compare.AddRange (hostarray);
 			compare.AddRange (asmbarray);
 	
-			i = 0;		
+			int i = 0;		
 			foreach (object obj in evidence) {
 				AssertEquals (obj, compare[i++]);
 			}
 		}
 
-		public void TestEnumeratorReset() 
+		[Test]
+		public void EnumeratorReset ()
 		{
 			object[] hostarray = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmbarray = { "asmb-1", "asmb-2", "asmb-3", "asmb-4" };
 			ArrayList compare = new ArrayList (); 
 			Evidence evidence = new Evidence (hostarray, asmbarray);
-			IEnumerator enumerator;
-			int i;
-			
 			compare.AddRange (hostarray);
 			compare.AddRange (asmbarray);
 
-			i = 0;
-			enumerator = evidence.GetEnumerator ();	
+			int i = 0;
+			IEnumerator enumerator = evidence.GetEnumerator ();	
 			while (enumerator.MoveNext ()) {
 				AssertEquals (enumerator.Current, compare[i++]);
 			}
@@ -142,25 +136,21 @@ namespace MonoTests.System.Security.Policy
 			}
 		}
 
-		public void TestGetHostEnumerator() 
+		[Test]
+		public void GetHostEnumerator ()
 		{
 			object[] hostarray = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmbarray = { "asmb-1", "asmb-2" };
-			Evidence evidence;
-			IEnumerator enumerator;
-			int i;
-
-			evidence = new Evidence (hostarray, asmbarray);
-			enumerator = evidence.GetHostEnumerator ();
-			
-			i = 0;
+			Evidence evidence = new Evidence (hostarray, asmbarray);
+			IEnumerator enumerator = evidence.GetHostEnumerator ();
+			int i = 0;
 			while (enumerator.MoveNext ()) {
                			AssertEquals (enumerator.Current, hostarray[i++]);
 			}
 		}
 
-
-		public void TestGetHostAssemblyEnumerator()
+		[Test]
+		public void GetHostAssemblyEnumerator ()
 		{
 			object[] hostarray = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmbarray = { "asmb-1", "asmb-2", "asmb-3", "asmb-4" };
@@ -175,16 +165,14 @@ namespace MonoTests.System.Security.Policy
 			while (enumerator.MoveNext()) {
         	        	AssertEquals (enumerator.Current, asmbarray[i++]);
 			}
-		
 		}
 
-		public void TestCount() 
+		[Test]
+		public void Count ()
 		{
 			object[] hostarray = { "host-1", "host-2", "host-3", "host-4" };
 			object[] asmbarray = { "asmb-1", "asmb-2", "asmb-3", "asmb-4" };
-			Evidence evidence;
-		
-			evidence = new Evidence (hostarray, asmbarray);
+			Evidence evidence = new Evidence (hostarray, asmbarray);
 			Assertion.AssertEquals (evidence.Count, 8);
 
 			for( int i=0; i<100; i++ ) {
@@ -197,69 +185,69 @@ namespace MonoTests.System.Security.Policy
 			}
 		}
 
-		public void TestNullCopyToException() 
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void NullCopyToException() 
 		{
-			Evidence evidence;
-	
-			evidence = new Evidence ();
+			Evidence evidence = new Evidence ();
 			evidence.AddHost ("host-1");
-			
-			try {
-				evidence.CopyTo (null, 100);
-				Fail ("CopyTo should throw exception when recieving a null array");
-			} catch (Exception e) {
-				Assert ("Should have caught an ArgumentNull Exception", e is ArgumentNullException);
-			}
-		}
-
-		/// <summary>
-		///    No Exception will be generated because the copy won't run because the evidence list is empty
-		/// </summary>
-		public void TestCopyToNoException() 
-		{
-			Evidence evidence = new Evidence ();;
-
 			evidence.CopyTo (null, 100);
 		}
 
-		public void TestArgOutOfRangeCopyToException() 
+		/// <summary>
+		///    No Exception will be generated because the copy won't run because the evidence list is empty
+		/// </summary>
+		[Test]
+		public void CopyToNoException() 
+		{
+			Evidence evidence = new Evidence ();;
+			evidence.CopyTo (null, 100);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentOutOfRangeException))]
+		public void ArgOutOfRangeCopyToException() 
 		{
 			Evidence evidence = new Evidence (new object[10], new object[10]);
-			
-			try {
-				evidence.CopyTo (new object[10], -100);
-				Fail ("CopyTo should throw exception when recieving a negative index");
-			} catch (Exception e) {
-				Assert("Should have caught an ArgumentOutOfRangeException Exception", 
-					e is ArgumentOutOfRangeException);
-			}
+			evidence.CopyTo (new object[10], -100);
 		}
 
 		/// <summary>
 		///    No Exception will be generated because the copy won't run because the evidence list is empty
 		/// </summary>
-		public void TestArgOutOfRangeCopyToNoException() 
+		[Test]
+		public void ArgOutOfRangeCopyToNoException() 
 		{
 			Evidence evidence = new Evidence ();
-
 			evidence.CopyTo (new object[10], -100);
 		}
 
-		public void BadMergeTest() {
-			Evidence evidence, evidence2;
-		
-			evidence = new Evidence (null, null);
-			evidence2 = new Evidence ();
+		[Test]
+		public void BadMerge ()
+		{
+			Evidence evidence = new Evidence (null, null);
+			Evidence evidence2 = new Evidence ();
+			evidence2.Merge (evidence);
 		}
 
-		public void MergeTest() {
-			Evidence evidence, evidence2;
-
-			evidence = new Evidence (new object[10], new object[10]);
-		  	evidence2 = new Evidence ();
-
+		[Test]
+		public void Merge ()
+		{
+			Evidence evidence = new Evidence (new object[10], new object[10]);
+		  	Evidence evidence2 = new Evidence ();
 			evidence2.Merge (evidence);
+		}
+
+		[Test]
+		public void DefaultProperties () 
+		{
+			Evidence e = new Evidence ();
+			AssertEquals ("Count", 0, e.Count);
+			Assert ("IsReadOnly", !e.IsReadOnly);
+			// LAMESPEC: Always TRUE (not FALSE)
+			Assert ("IsSynchronized", e.IsSynchronized);
+			Assert ("Locked", !e.Locked);
+			AssertNotNull ("SyncRoot", e.SyncRoot);
 		}
 	}
 }
-
