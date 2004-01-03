cbaebb87c42ce1fd7a127a78cb592266952213ec
Atsushi Eno <atsushieno@gmail.com>
2004-01-03 16:12:50 +0000

    2004-01-03  Atsushi Enomoto <atsushi@ximian.com>
    	* XmlSchemaTests.cs : fixed file path. Added more tests.
    	* added XmlSchemaCollectionTests.cs and XmlSchemaDatatypeTests.cs (the
    	  latter one's purpose overlaps with XmlSchemaBuiltInDatatypeTests ;)
    
    svn path=/trunk/mcs/; revision=21639

--- mcs/class/System.XML/Test/System.Xml.Schema/XmlSchemaTests.cs
+++ mcs/class/System.XML/Test/System.Xml.Schema/XmlSchemaTests.cs
@@ -1,5 +1,5 @@
 //
-// System.Xml.XmlSchema.cs
+// System.Xml.XmlSchemaTests.cs
 //
 // Author:
 //   Atsushi Enomoto <ginga@kit.hi-ho.ne.jp>
@@ -8,6 +8,7 @@
 //
 
 using System;
+using System.IO;
 using System.Xml;
 using System.Xml.Schema;
 using NUnit.Framework;
@@ -98,7 +99,7 @@ namespace MonoTests.System.Xml
 		[Test]
 		public void TestRead ()
 		{
-			XmlSchema schema = GetSchema ("XmlFiles/xsd/1.xsd");
+			XmlSchema schema = GetSchema ("Test/XmlFiles/xsd/1.xsd");
 			AssertEquals (6, schema.Items.Count);
 
 			bool fooValidated = false;
@@ -126,6 +127,30 @@ namespace MonoTests.System.Xml
 		}
 
 		[Test]
+		public void TestReadFlags ()
+		{
+			XmlSchema schema = GetSchema ("Test/XmlFiles/xsd/2.xsd");
+			schema.Compile (null);
+			XmlSchemaElement el = schema.Items [0] as XmlSchemaElement;
+			AssertNotNull (el);
+			AssertEquals (XmlSchemaDerivationMethod.Extension, el.Block);
+
+			el = schema.Items [1] as XmlSchemaElement;
+			AssertNotNull (el);
+			AssertEquals (XmlSchemaDerivationMethod.Extension |
+				XmlSchemaDerivationMethod.Restriction, el.Block);
+		}
+
+		[Test]
+		public void TestWriteFlags ()
+		{
+			XmlSchema schema = GetSchema ("Test/XmlFiles/xsd/2.xsd");
+			StringWriter sw = new StringWriter ();
+			XmlTextWriter xtw = new XmlTextWriter (sw);
+			schema.Write (xtw);
+		}
+
+		[Test]
 		public void TestCompile ()
 		{
 			XmlQualifiedName qname;
@@ -133,9 +158,16 @@ namespace MonoTests.System.Xml
 			XmlSchemaComplexType cType;
 			XmlSchemaSequence seq;
 
-			XmlSchema schema = GetSchema ("XmlFiles/xsd/1.xsd");
+			XmlSchema schema = GetSchema ("Test/XmlFiles/xsd/1.xsd");
+//			Assert (!schema.IsCompiled);
 			schema.Compile (null);
+			Assert (schema.IsCompiled);
 			string ns = "urn:bar";
+
+			XmlSchemaElement foo = (XmlSchemaElement) schema.Elements [QName ("Foo", ns)];
+			AssertNotNull (foo);
+			XmlSchemaDatatype stringDatatype = foo.ElementType as XmlSchemaDatatype;
+			AssertNotNull (stringDatatype);
 
 			// HogeType
 			qname = QName ("HogeType", ns);
@@ -148,8 +180,7 @@ namespace MonoTests.System.Xml
 			AssertNotNull (seq);
 			AssertEquals (2, seq.Items.Count);
 			XmlSchemaElement refFoo = seq.Items [0] as XmlSchemaElement;
-			AssertCompiledElement (refFoo, QName ("Foo", ns),
-				((XmlSchemaElement) schema.Elements [QName ("Foo", ns)]).ElementType);
+			AssertCompiledElement (refFoo, QName ("Foo", ns), stringDatatype);
 
 			// FugaType
 			qname = QName ("FugaType", ns);
@@ -169,12 +200,58 @@ namespace MonoTests.System.Xml
 			AssertEquals (1, seq.Items.Count);
 			XmlSchemaElement refBaz = seq.Items [0] as XmlSchemaElement;
 			AssertNotNull (refBaz);
-			AssertCompiledElement (refBaz, QName ("Baz", ""),
-				((XmlSchemaElement) schema.Elements [QName ("Foo", ns)]).ElementType);
+			AssertCompiledElement (refBaz, QName ("Baz", ""), stringDatatype);
 
 			qname = QName ("Bar", ns);
 			XmlSchemaElement element = schema.Elements [qname] as XmlSchemaElement;
 			AssertCompiledElement (element, qname, cType);
+		}
+
+		[Test]
+		[ExpectedException (typeof (XmlSchemaException))]
+		public void TestCompileNonSchema ()
+		{
+			XmlTextReader xtr = new XmlTextReader ("<root/>", XmlNodeType.Document, null);
+			XmlSchema schema = XmlSchema.Read (xtr, null);
+		}
+
+		[Test]
+		public void TestSimpleImport ()
+		{
+			XmlSchema schema = XmlSchema.Read (new XmlTextReader ("Test/XmlFiles/xsd/3.xsd"), null);
+			AssertEquals ("urn:foo", schema.TargetNamespace);
+			XmlSchemaImport import = schema.Includes [0] as XmlSchemaImport;
+			AssertNotNull (import);
+
+			schema.Compile (null);
+			AssertEquals (4, schema.Elements.Count);
+			AssertNotNull (schema.Elements [QName ("Foo", "urn:foo")]);
+			AssertNotNull (schema.Elements [QName ("Bar", "urn:foo")]);
+			AssertNotNull (schema.Elements [QName ("Foo", "urn:bar")]);
+			AssertNotNull (schema.Elements [QName ("Bar", "urn:bar")]);
+			
+		}
+
+		[Test]
+		public void TestQualification ()
+		{
+			XmlSchema schema = XmlSchema.Read (new XmlTextReader ("Test/XmlFiles/xsd/5.xsd"), null);
+			schema.Compile (null);
+			XmlSchemaElement el = schema.Elements [QName ("Foo", "urn:bar")] as XmlSchemaElement;
+			AssertNotNull (el);
+			XmlSchemaComplexType ct = el.ElementType as XmlSchemaComplexType;
+			XmlSchemaSequence seq = ct.ContentTypeParticle as XmlSchemaSequence;
+			XmlSchemaElement elp = seq.Items [0] as XmlSchemaElement;
+			AssertEquals (QName ("Bar", ""), elp.QualifiedName);
+
+			schema = XmlSchema.Read (new XmlTextReader ("Test/XmlFiles/xsd/6.xsd"), null);
+			schema.Compile (null);
+			el = schema.Elements [QName ("Foo", "urn:bar")] as XmlSchemaElement;
+			AssertNotNull (el);
+			ct = el.ElementType as XmlSchemaComplexType;
+			seq = ct.ContentTypeParticle as XmlSchemaSequence;
+			elp = seq.Items [0] as XmlSchemaElement;
+			AssertEquals (QName ("Bar", "urn:bar"), elp.QualifiedName);
 		}
 	}
 }
