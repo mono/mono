f1ab9475616aeacb9cf1d325a61e34189136be8d
Sebastien Pouliot <sebastien@ximian.com>
2004-01-04 03:08:38 +0000

    2004-01-03  Sebastien Pouliot  <spouliot@videotron.ca>
    	* SecurityManager.cs: Added some basic stuff to make some security unit
    	tests works (mostly when using PolicyHierarchy).
    
    svn path=/trunk/mcs/; revision=21666

--- mcs/class/corlib/System.Security/SecurityManager.cs
+++ mcs/class/corlib/System.Security/SecurityManager.cs
@@ -1,14 +1,18 @@
 //
 // System.Security.SecurityManager.cs
 //
-// Author:
+// Authors:
 //	Nick Drochak(ndrochak@gol.com)
+//	Sebastien Pouliot (spouliot@motus.com)
 //
 // (C) Nick Drochak
+// Portions (C) 2004 Motus Technologies Inc. (http://www.motus.com)
 //
 
-using System.Security.Policy;
 using System.Collections;
+using System.IO;
+using System.Security.Permissions;
+using System.Security.Policy;
 
 namespace System.Security {
 
@@ -16,24 +20,45 @@ namespace System.Security {
 
 		private static bool checkExecutionRights;
 		private static bool securityEnabled;
+		static private object _lockObject;
+		private static ArrayList _hierarchy;
+
+		static SecurityManager () 
+		{
+			// lock(this) is bad
+			// http://msdn.microsoft.com/library/en-us/dnaskdr/html/askgui06032003.asp?frame=true
+			_lockObject = new object ();
+		}
 
 		private SecurityManager () {}
 
+		// properties
+
 		public static bool CheckExecutionRights {
 			get { return checkExecutionRights; }
-			set { checkExecutionRights = value; }
+			set { 
+				// throw a SecurityException if we don't have ControlPolicy permission
+				new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+				checkExecutionRights = value; 
+			}
 		}
 
 		public static bool SecurityEnabled {
 			get { return securityEnabled; }
-			set { securityEnabled = value; }
+			set { 
+				// throw a SecurityException if we don't have ControlPolicy permission
+				new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+				securityEnabled = value; 
+			}
 		}
+
+		// methods
 
 		[MonoTODO("Incomplete")]
 		public static bool IsGranted (IPermission perm)
 		{
 			if (perm == null)
-				return false;
+				return true;
 			if (!securityEnabled)
 				return true;
 			return false;
@@ -42,6 +67,17 @@ namespace System.Security {
 		[MonoTODO()]
 		public static PolicyLevel LoadPolicyLevelFromFile (string path, PolicyLevelType type)
 		{
+			if (path == null)
+				throw new ArgumentNullException ("path");
+			if (!File.Exists (path))
+				throw new ArgumentException ("file do not exist");
+			// throw a SecurityException if we don't have ControlPolicy permission
+			new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+			// throw a SecurityException if we don't have Read, Write and PathDiscovery permissions
+			FileIOPermissionAccess access = FileIOPermissionAccess.Read | FileIOPermissionAccess.Write | FileIOPermissionAccess.PathDiscovery;
+			new FileIOPermission (access, path).Demand ();
+
+			// TODO
 			return null;
 		}
 
@@ -50,13 +86,24 @@ namespace System.Security {
 		{
 			if (null == str)
 				throw new ArgumentNullException("str");
+			// throw a SecurityException if we don't have ControlPolicy permission
+			new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+
+			// TODO
 			return null;
 		}
 
-		[MonoTODO()]
+		[MonoTODO("InitializePolicyHierarchy not implemented")]
 		public static IEnumerator PolicyHierarchy ()
 		{
-			return null;
+			// throw a SecurityException if we don't have ControlPolicy permission
+			new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+			if (_hierarchy == null) {
+				lock (_lockObject) {
+					InitializePolicyHierarchy ();
+				}
+			}
+			return _hierarchy.GetEnumerator ();
 		}
 
 		[MonoTODO()]
@@ -78,10 +125,44 @@ namespace System.Security {
 			return null;
 		}
 
-		[MonoTODO()]
-		public static void SavePolicy () {}
+		[MonoTODO ("InternalSavePolicyLevel isn't complete")]
+		public static void SavePolicy () 
+		{
+			// throw a SecurityException if we don't have ControlPolicy permission
+			// done by using PolicyHierarchy (no need to duplicate)
+			IEnumerator e = PolicyHierarchy ();
+			while (e.MoveNext ()) {
+				PolicyLevel level = (e.Current as PolicyLevel);
+				InternalSavePolicyLevel (level);
+			}
+		}
 
-		[MonoTODO()]
-		public static void SavePolicyLevel (PolicyLevel level) {}
+		[MonoTODO ("InternalSavePolicyLevel isn't complete")]
+		public static void SavePolicyLevel (PolicyLevel level) 
+		{
+			// throw a SecurityException if we don't have ControlPolicy permission
+			new SecurityPermission (SecurityPermissionFlag.ControlPolicy).Demand ();
+
+			InternalSavePolicyLevel (level);
+		}
+
+		// internal stuff
+
+		internal static void InternalSavePolicyLevel (PolicyLevel level) 
+		{
+			// without the security checks (to avoid checks in loops)
+		}
+
+		[MonoTODO ("Incomplete")]
+		internal static void InitializePolicyHierarchy ()
+		{
+			ArrayList al = new ArrayList ();
+			// minimum: Machine, Enterprise and User
+			// FIXME: Incomplete
+			al.Add (new PolicyLevel ("Enterprise"));
+			al.Add (new PolicyLevel ("Machine"));
+			al.Add (new PolicyLevel ("User"));
+			_hierarchy = ArrayList.Synchronized (al);
+		}
 	}
 }
