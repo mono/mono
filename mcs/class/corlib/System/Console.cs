fbcf76b01ef622eef74aa027e671f899b91689e3
Miguel de Icaza <miguel@gnome.org>
2004-01-06 22:34:40 +0000

    2004-01-06  Miguel de Icaza  <miguel@ximian.com>
    	* Console.cs: Test for UTF-8 being present anywhere on the
    	string, also do ToUpper instead of ToLower, which will work even
    	around the ICU bug with different locales (#52065), and addresses #52101
    
    svn path=/trunk/mcs/; revision=21759

--- mcs/class/corlib/System/Console.cs
+++ mcs/class/corlib/System/Console.cs
@@ -21,15 +21,22 @@ namespace System {
 		static Console ()
 		{
 			string codepage = Encoding.InternalCodePage ();
-			Encoding encoding = Encoding.Default;
+			Encoding encoding;
 
-			if (((int) Environment.OSVersion.Platform) != 128){
+			if (((int) Environment.Platform) != 128){
 				//
 				// On Unix systems (128), do not output the
 				// UTF-8 ZWNBSP (zero-width non-breaking space).
 				//
 				if (codepage.ToUpper ().Replace ('-', '_').IndexOf ("UTF_8") != -1)
 					encoding = Encoding.UTF8Unmarked;
+				else
+					encoding = Encoding.Default;
+			} else {
+				//
+				// On Windows, follow the Windows tradition
+				//
+				encoding = Encoding.Default;
 			}
 			
 			stderr = new StreamWriter (OpenStandardError (), encoding);
