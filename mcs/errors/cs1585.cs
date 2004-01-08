b994d376d9f282b9a7c8a092f2125ef8790e421b
Miguel de Icaza <miguel@gnome.org>
2004-01-08 21:29:09 +0000

    Add
    svn path=/trunk/mcs/; revision=21856

---
+++ mcs/errors/cs1585.cs
@@ -0,0 +1,11 @@
+// cs1585: Modifier `static' should appear before type
+// Line:  7
+using System; 
+ 
+class t 
+{ 
+        public void static Main (string[] args) 
+        { 
+                Console.WriteLine("Hello"); 
+        } 
+} 
