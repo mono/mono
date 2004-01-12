49442da6c80605caf72343eaeee22e67a6d7e538
Sebastien Pouliot <sebastien@ximian.com>
2004-01-12 01:28:16 +0000

    2004-01-11  Sebastien Pouliot <spouliot@videotron.ca>
    	* AssemblyInfo.cs: New. Copied and ajusted from corlib.
    	* Locale.cs: New. Copied (verbatim) from corlib.
    	* ChangeLog: New.
    
    svn path=/trunk/mcs/; revision=21963

---
+++ mcs/class/System.Security/Assembly/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about the system assembly
+
+#if (NET_1_0)
+	[assembly: AssemblyVersion ("1.0.3300.0")]
+	[assembly: SatelliteContractVersion ("1.0.3300.0")]
+#endif
+#if (NET_1_1)
+	[assembly: AssemblyVersion ("1.0.5000.0")]
+	[assembly: SatelliteContractVersion ("1.0.5000.0")]
+#endif
+
+[assembly: AssemblyTitle ("System.Security.dll")]
+[assembly: AssemblyDescription ("System.Security.dll")]
+[assembly: AssemblyCompany ("MONO development team")]
+[assembly: AssemblyProduct ("MONO CLI")]
+[assembly: AssemblyCopyright ("(c) 2003-2004 Various Authors")]
+
+[assembly: CLSCompliant (true)]
+[assembly: NeutralResourcesLanguage ("en-US")]
+
+//[assembly: AssemblyDelaySign (false)]
+//[assembly: AssemblyKeyFile ("")]
