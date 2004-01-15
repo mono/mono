ffedde1b7191eb29d2787eb870e4dd3d1dd711b6
Andreas N <andreas@mono-cvs.ximian.com>
2004-01-15 22:43:29 +0000

    2004-01-15  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* DirectoryEntryConverter.cs: Added, stubbed minimal version
    	* ChangeLog: Added
    
    svn path=/trunk/mcs/; revision=22129

---
+++ mcs/class/System.DirectoryServices/System.DirectoryServices.Design/DirectoryEntryConverter.cs
@@ -0,0 +1,24 @@
+//
+// System.DirectoryServices.Design.DirectoryEntryConverter.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2004 Andreas Nahr
+//
+//
+
+using System.ComponentModel;
+
+namespace System.DirectoryServices.Design
+{
+
+	internal class DirectoryEntryConverter : TypeConverter
+	{
+
+		//TODO IMPLEMENT
+		public DirectoryEntryConverter ()
+		{
+		}
+	}
+}
