b32d91d1727595cf0a17cdbf7008efe0ad00b799
César Natarén <cesar@mono-cvs.ximian.com>
2004-01-16 09:17:38 +0000

    initial set of tests
    svn path=/trunk/mcs/; revision=22159

---
+++ mcs/jtests/global-assign-sum.js
@@ -0,0 +1,4 @@
+var a, b, c, d, e, f, g, h, i, j, k, l;
+var x;
+
+x = a + b + c + d + e + f + g + h + i + j + k + l;
---
+++ mcs/jtests/global-sum.js
@@ -0,0 +1,3 @@
+var a, b, c, d, e, f, g, h, i, j, k, l;
+
+a + b + c + d + e + f + g + h + i + j + k + l;
