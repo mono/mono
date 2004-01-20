73472f0d348e0b7bf040b41e0a2c150a59351d0a
Atsushi Eno <atsushieno@gmail.com>
2004-01-20 16:51:07 +0000

    2004-01-20 Atsushi Enomoto <atsushi@ximian.com>
    	* Added missing TypedDataSetGenerator.cs.
    
    svn path=/trunk/mcs/; revision=22304

---
+++ mcs/class/System.Data/System.Data/TypedDataSetGenerator.cs
@@ -0,0 +1,37 @@
+﻿//
+// TypedDataSetGenerator.cs
+//
+// Author:
+//	Atsushi Enomoto <atsushi@ximian.com>
+//
+// (C)2004 Novell Inc.
+//
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Data;
+
+namespace System.Data
+{
+	public class TypedDataSetGenerator
+	{
+		public TypedDataSetGenerator ()
+		{
+		}
+
+		[MonoTODO]
+		public static void Generate (DataSet dataSet,
+			CodeNamespace codeNamespace,
+			ICodeGenerator codeGen)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static string GenerateIdName (string name, ICodeGenerator codeGen)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
