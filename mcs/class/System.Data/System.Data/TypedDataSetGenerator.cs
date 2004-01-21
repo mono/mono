0c7b31589e90da50dc4123b482d31d1c1122e2e6
Atsushi Eno <atsushieno@gmail.com>
2004-01-21 14:34:56 +0000

    2004-01-21 Atsushi Enomoto <atsushi@ximian.com>
    	* TypedDataSetGenerator.cs : Implemented GenerateIdName().
    
    svn path=/trunk/mcs/; revision=22340

--- mcs/class/System.Data/System.Data/TypedDataSetGenerator.cs
+++ mcs/class/System.Data/System.Data/TypedDataSetGenerator.cs
@@ -11,6 +11,7 @@ using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.Collections;
 using System.Data;
+using System.Text;
 
 namespace System.Data
 {
@@ -28,10 +29,41 @@ namespace System.Data
 			throw new NotImplementedException ();
 		}
 
-		[MonoTODO]
 		public static string GenerateIdName (string name, ICodeGenerator codeGen)
 		{
-			throw new NotImplementedException ();
+			if (name == null || codeGen == null)
+				throw new NullReferenceException ();
+
+			name = codeGen.CreateValidIdentifier (name);
+			// ... however, in fact this method is so insufficient
+			// that we have to modify the name.
+
+			if (name.Length == 0)
+				return "_";
+
+			StringBuilder sb = null;
+			if (!Char.IsLetter (name, 0) && name [0] != '_') {
+				sb = new StringBuilder ();
+				sb.Append ('_');
+			}
+
+			int start = 0;
+			for (int i = 0; i < name.Length; i++) {
+				if (!Char.IsLetterOrDigit (name, i)) {
+					if (sb == null)
+						sb = new StringBuilder ();
+					sb.Append (name, start, i - start);
+					sb.Append ('_');
+					start = i + 1;
+				}
+			}
+
+			if (sb != null) {
+				sb.Append (name, start, name.Length - start);
+				return sb.ToString ();
+			}
+			else
+				return name;
 		}
 	}
 }
