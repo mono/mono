7f0eeb366cea03bee25bd3cf2300f7f90c1979a3
Rafael Teixeira <monoman@gmail.com>
2004-01-22 13:53:27 +0000

    2004-01-22  Rafael Teixeira <rafaelteixeirabr@hotmail.com> 	* Utils.cs: 		First Try Implementation for CopyArray(): NET_1_1-aware
    svn path=/trunk/mcs/; revision=22396

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/Utils.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/Utils.cs
@@ -3,8 +3,10 @@
 //
 // Author:
 //   Chris J Breisch (cjbreisch@altavista.net) 
+//   Rafael Teixeira (rafaelteixeirabr@hotmail.com)
 //
 // (C) 2002 Chris J Breisch
+// (C) 2004 Rafael Teixeira
 //
 
 using System;
@@ -14,18 +16,40 @@ namespace Microsoft.VisualBasic.CompilerServices
 	[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)] 
 	[Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute] 
 	sealed public class Utils {
-		// Declarations
-		// Constructors
-		// Properties
-		// Methods
+
+		static Utils() {
+		}
+
 		[MonoTODO]
-		public static void ThrowException (System.Int32 hr) { throw new NotImplementedException (); }
+		public static void ThrowException (System.Int32 hr) { 
+			throw new NotImplementedException (); 
+		}
+
 		[MonoTODO]
-		public static System.Object SetCultureInfo (System.Globalization.CultureInfo Culture) { throw new NotImplementedException (); }
+		public static System.Object SetCultureInfo (System.Globalization.CultureInfo Culture) { 
+			throw new NotImplementedException (); 
+		}
+
 		[MonoTODO]
-		public static System.Array CopyArray (System.Array arySrc, System.Array aryDest) { throw new NotImplementedException (); }
+		public static System.Array CopyArray (System.Array SourceArray, System.Array DestinationArray) { 
+#if NET_1_1
+			long SourceLength = SourceArray.LongLength;
+			long DestinationLength = DestinationArray.LongLength;
+			long LengthToCopy = (SourceLength < DestinationLength) ? SourceLength : DestinationLength;
+			Array.Copy(SourceArray, DestinationArray, LengthToCopy); 
+#else
+			int SourceLength = SourceArray.Length;
+			int DestinationLength = DestinationArray.Length;
+			int LengthToCopy = (SourceLength < DestinationLength) ? SourceLength : DestinationLength;
+			Array.Copy(SourceArray, DestinationArray, LengthToCopy); 
+#endif
+			return DestinationArray;
+		}
+
 		[MonoTODO]
-		public static System.String MethodToString (System.Reflection.MethodBase Method) { throw new NotImplementedException (); }
+		public static System.String MethodToString (System.Reflection.MethodBase Method) {
+			 throw new NotImplementedException (); 
+		}
 		// Events
 	};
 }
