b29346f49cc09a3aae9b2b9998b9dc1612d48608
Sebastien Pouliot <sebastien@ximian.com>
2004-01-26 16:00:25 +0000

    2004-01-26  Sebastien Pouliot  <spouliot@videotron.ca>
    	* PermissionSetAttribute.cs: Implemented CreatePermissionSet.
    
    svn path=/trunk/mcs/; revision=22504

--- mcs/class/corlib/System.Security.Permissions/PermissionSetAttribute.cs
+++ mcs/class/corlib/System.Security.Permissions/PermissionSetAttribute.cs
@@ -1,12 +1,19 @@
 //
 // System.Security.Permissions.PermissionSetAttribute.cs
 //
-// Duncan Mak <duncan@ximian.com>
+// Authors
+//	Duncan Mak <duncan@ximian.com>
+//	Sebastien Pouliot  <spouliot@videotron.ca>
 //
 // (C) 2002 Ximian, Inc. http://www.ximian.com
 //
 
 using System;
+using System.IO;
+using System.Security.Policy;
+using System.Text;
+
+using Mono.Xml;
 
 namespace System.Security.Permissions {
 
@@ -59,7 +66,31 @@ namespace System.Security.Permissions {
 			return null; 	  // Not used, used for inheritance from SecurityAttribute
 		}
 
-		[MonoTODO]
+		private PermissionSet CreateFromXml (string xml) 
+		{
+			SecurityParser sp = new SecurityParser ();
+			sp.LoadXml (xml);
+			SecurityElement se = sp.ToXml ();
+
+			string className = se.Attribute ("class");
+			if (className == null)
+				return null;
+
+			PermissionState state = PermissionState.None;
+			if (se.Attribute ("Unrestricted") == "true")
+				state = PermissionState.Unrestricted;
+
+			if (className.EndsWith ("NamedPermissionSet")) {
+				NamedPermissionSet nps = new NamedPermissionSet (se.Attribute ("Name"), state);
+				return (PermissionSet) nps;
+			}
+			else if (className.EndsWith ("PermissionSet")) {
+				PermissionSet ps = new PermissionSet (state);
+				return ps;
+			}
+			return null;
+		}
+
 		public PermissionSet CreatePermissionSet ()
 		{
 			PermissionSet pset = null;
@@ -68,10 +99,16 @@ namespace System.Security.Permissions {
 			else {
 				pset = new PermissionSet (PermissionState.None);
 				if (name != null) {
+					return PolicyLevel.CreateAppDomainLevel ().GetNamedPermissionSet (name);
 				}
 				else if (file != null) {
+					Encoding e = ((isUnicodeEncoded) ? System.Text.Encoding.Unicode : System.Text.Encoding.ASCII);
+					using (StreamReader sr = new StreamReader (file, e)) {
+						pset = CreateFromXml (sr.ReadToEnd ());
+					}
 				}
 				else if (xml != null) {
+					pset = CreateFromXml (xml);
 				}
 			}
 			return pset;
