230ac4914fd1902bc8f5bfd33171e75097150f6a
Atsushi Eno <atsushieno@gmail.com>
2004-01-28 07:32:55 +0000

    2004-01-28  Atsushi Enomoto  <atsushi@ximian.com>
    	* xslttest.cs : Now it tests against XPathDocument. Reduced error msgs.
    
    svn path=/trunk/mcs/; revision=22571

--- mcs/class/System.XML/Test/System.Xml.Xsl/standalone_tests/xslttest.cs
+++ mcs/class/System.XML/Test/System.Xml.Xsl/standalone_tests/xslttest.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Text;
 using System.Xml;
+using System.Xml.XPath;
 using System.Xml.Xsl;
 
 namespace XsltTest
@@ -14,7 +15,7 @@ namespace XsltTest
 // output22,77: not-supported encoding, but MS passes...?
 // output72.xsl: should not pass
 			ArrayList expectedExceptions = new ArrayList
-(new string [] {"lre12.xsl", "namespace40.xsl", "namespace42.xsl", "namespace43.xsl",
+(new string [] {"attribset15.xsl", "lre12.xsl", "namespace40.xsl", "namespace42.xsl", "namespace43.xsl",
 "namespace48.xsl", "namespace60.xsl", "namespace73.xsl", "namespace106.xsl",
 "output22.xsl", "output72.xsl", "output77.xsl"});
 
@@ -37,17 +38,17 @@ if (expectedExceptions.Contains (stylesheetBase))
 	continue;
 						XmlTextReader stylextr = new XmlTextReader (stylesheet);
 						XmlValidatingReader stylexvr = new XmlValidatingReader (stylextr);
-						XmlDocument styledoc = new XmlDocument ();
-						styledoc.Load (stylesheet);
+//						XmlDocument styledoc = new XmlDocument ();
+//						styledoc.Load (stylesheet);
 						trans.Load (stylesheet);
 //						trans.Load (styledoc);
 						XmlTextReader xtr = new XmlTextReader (srcxml);
 						XmlValidatingReader xvr = new XmlValidatingReader (xtr);
 						xvr.ValidationType = ValidationType.None;
-						XmlDocument input = new XmlDocument ();
-						input.Load (xvr);
+//						XmlDocument input = new XmlDocument ();
+//						input.Load (xvr);
 //						input.Load (xtr);
-//						XPathDocument input = new XPathDocument (xtr);
+						XPathDocument input = new XPathDocument (xvr);
 						StringWriter sw = new StringWriter ();
 						trans.Transform (input, null, sw);
 						string outfile = path + scenario.SelectSingleNode ("output-file[@role='principal']").InnerText;
@@ -59,7 +60,7 @@ if (expectedExceptions.Contains (stylesheetBase))
 						string reference_out = sr.ReadToEnd ();
 						string actual_out = sw.ToString ();
 						if (reference_out != actual_out)
-#if false
+#if true
 							Console.WriteLine ("Different: " + testCase.GetAttribute ("id"));
 #else
 							Console.WriteLine ("Different: " +
