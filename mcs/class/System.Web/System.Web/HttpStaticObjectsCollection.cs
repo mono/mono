5bf6135ce30cd407d61b0342089b4c6da783996c
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2004-02-09 10:36:07 +0000

    stylized
    svn path=/trunk/mcs/; revision=22897

--- mcs/class/System.Web/System.Web/HttpStaticObjectsCollection.cs
+++ mcs/class/System.Web/System.Web/HttpStaticObjectsCollection.cs
@@ -3,126 +3,123 @@ using System.IO;
 using System.Collections;
 
 namespace System.Web {
-   public sealed class HttpStaticObjectsCollection : ICollection, IEnumerable {
-      private Hashtable _Objects;
+	public sealed class HttpStaticObjectsCollection : ICollection, IEnumerable {
+		private Hashtable _Objects;
 
-      // Needs to hold object items that can be latebound and can be serialized
-      public HttpStaticObjectsCollection() {
-         _Objects = new Hashtable();
-      }
+		// Needs to hold object items that can be latebound and can be serialized
+		public HttpStaticObjectsCollection ()
+		{
+			_Objects = new Hashtable();
+		}
 
-      public object GetObject(string name) {
-         return this[name];
-      }
+		public object GetObject (string name)
+		{
+			return this [name];
+		}
 
-      public IEnumerator GetEnumerator() {
-         return _Objects.GetEnumerator ();
-      }
-      
-      public void CopyTo(Array array, int index) {
-	 _Objects.CopyTo (array, index);
-      }   
+		public IEnumerator GetEnumerator ()
+		{
+			return _Objects.GetEnumerator ();
+		}
 
-      internal IDictionary GetObjects() {
-         return _Objects;
-      }
+		public void CopyTo (Array array, int index)
+		{
+			_Objects.CopyTo (array, index);
+		}   
 
-      public object this[string name] {
-         get {
-            return _Objects [name];
-         }
-      }
+		internal IDictionary GetObjects ()
+		{
+			return _Objects;
+		}
 
-      public int Count {
-         get {
-            return _Objects.Count;
-         }
-      }
+		public object this [string name] {
+			get { return _Objects [name]; }
+		}
 
-      public bool IsReadOnly {
-         get {
-            return true;
-         }
-      }
+		public int Count {
+			get { return _Objects.Count; }
+		}
 
-      public bool IsSynchronized {
-         get {
-            return false;
-         }
-      }
+		public bool IsReadOnly {
+			get { return true; }
+		}
 
-      public object SyncRoot {
-         get {
-            return this;
-         }
-      }
+		public bool IsSynchronized {
+			get { return false; }
+		}
 
-           private void Set (string name, object obj)
-           {
-                   _Objects [name] = obj;
-           }
-           
-           internal void Serialize (BinaryWriter w)
-           {
-                   lock (_Objects) {
-                           w.Write (Count);
-                           foreach (string key in _Objects.Keys) {
-                                   w.Write (key);
-                                   object value = _Objects [key];
-                                   if (value == null) {
-                                           w.Write (System.Web.Util.AltSerialization.NullIndex);
-                                           continue;
-                                   }
-                                   
-                                   System.Web.Util.AltSerialization.SerializeByType (w, value);
-                           }
-                   }
-           }
+		public object SyncRoot {
+			get { return this; }
+		}
 
-           internal static HttpStaticObjectsCollection Deserialize (BinaryReader r)
-           {
-                   HttpStaticObjectsCollection result = new HttpStaticObjectsCollection ();
-                   for (int i = r.ReadInt32 (); i > 0; i--) {
-                           string key = r.ReadString ();
-                           int index = r.ReadInt32 ();
-                           if (index == System.Web.Util.AltSerialization.NullIndex)
-                                   result.Set (key, null);
-                           else
-                                   result.Set (key, System.Web.Util.AltSerialization.DeserializeFromIndex (index, r));
-                   }
-                   
-   		   return result;
-	   }
 
-	   internal byte [] ToByteArray ()
-	   {
-		   MemoryStream stream = null;
-		   try {
-			   stream = new MemoryStream ();
-			   Serialize (new BinaryWriter (stream));
-			   return stream.GetBuffer ();
-		   } catch {
-			   throw;
-		   } finally {
-			   if (stream != null)
-				   stream.Close ();
-		   }
-	   }
+		private void Set (string name, object obj)
+		{
+			_Objects [name] = obj;
+		}
 
-	   internal static HttpStaticObjectsCollection FromByteArray (byte [] data)
-	   {
-		   HttpStaticObjectsCollection objs = null;
-		   MemoryStream stream = null;
-		   try {
-			   stream = new MemoryStream (data);
-			   objs = Deserialize (new BinaryReader (stream));
-		   } catch {
-			   throw;
-		   } finally {
-			   if (stream != null)
-				   stream.Close ();
-		   }
-		   return objs;
-	   }
-   }
+		internal void Serialize (BinaryWriter w)
+		{
+			lock (_Objects) {
+				w.Write (Count);
+				foreach (string key in _Objects.Keys) {
+					w.Write (key);
+					object value = _Objects [key];
+					if (value == null) {
+						w.Write (System.Web.Util.AltSerialization.NullIndex);
+						continue;
+					}
+
+					System.Web.Util.AltSerialization.SerializeByType (w, value);
+				}
+			}
+		}
+
+		internal static HttpStaticObjectsCollection Deserialize (BinaryReader r)
+		{
+			HttpStaticObjectsCollection result = new HttpStaticObjectsCollection ();
+			for (int i = r.ReadInt32 (); i > 0; i--) {
+				string key = r.ReadString ();
+				int index = r.ReadInt32 ();
+				if (index == System.Web.Util.AltSerialization.NullIndex)
+					result.Set (key, null);
+				else
+					result.Set (key, System.Web.Util.AltSerialization.DeserializeFromIndex (index, r));
+			}
+
+			return result;
+		}
+
+		internal byte [] ToByteArray ()
+		{
+			MemoryStream stream = null;
+			try {
+				stream = new MemoryStream ();
+				Serialize (new BinaryWriter (stream));
+				return stream.GetBuffer ();
+			} catch {
+				throw;
+			} finally {
+				if (stream != null)
+					stream.Close ();
+			}
+		}
+
+		internal static HttpStaticObjectsCollection FromByteArray (byte [] data)
+		{
+			HttpStaticObjectsCollection objs = null;
+			MemoryStream stream = null;
+			try {
+				stream = new MemoryStream (data);
+				objs = Deserialize (new BinaryReader (stream));
+			} catch {
+				throw;
+			} finally {
+				if (stream != null)
+					stream.Close ();
+			}
+			return objs;
+		}
+	}
 }
+
