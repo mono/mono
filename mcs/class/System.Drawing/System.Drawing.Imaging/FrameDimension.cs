2531d682a4a9fad966526f3194507bae0e4102bc
Sanja Gupta <sanjay@mono-cvs.ximian.com>
2004-02-13 10:11:10 +0000

    ChangeLog: Updated ChangeLog FrameDimension.cs: Implemented missing functionalities.
    svn path=/trunk/mcs/; revision=23053

--- mcs/class/System.Drawing/System.Drawing.Imaging/FrameDimension.cs
+++ mcs/class/System.Drawing/System.Drawing.Imaging/FrameDimension.cs
@@ -5,6 +5,7 @@
 // Author: Christian Meyer
 // eMail: Christian.Meyer@cs.tum.edu
 // Dennis Hayes (dennish@raytek.com)
+// Sanjay Gupta <gsanjay@novell.com)
 //
 
 namespace System.Drawing.Imaging {
@@ -13,35 +14,39 @@ using System;
 
 public sealed class FrameDimension {
 
+	internal Guid guid;
 	// constructor
-	public FrameDimension (Guid guid) {}
+	public FrameDimension (Guid guid) 
+	{
+		this.guid = guid;
+	}
 
 	//properties
 	public Guid Guid {
 		get {
-			throw new NotImplementedException ();
+			return guid;
 		}
 	}
-	
+																	   																							 
 	public static FrameDimension Page {
 		get {
-			throw new NotImplementedException ();
+			return new FrameDimension ( new Guid ( "7462dc86-6180-4c7e-8e3f-ee7333a7a483" ) );
 		}
 	}
 	
 	public static FrameDimension Resolution {
 		get {
-			throw new NotImplementedException ();
+			return new FrameDimension ( new Guid ( "84236f7b-3bd3-428f-8dab-4ea1439ca315" ) );			
 		}
 	}
 	
 	public static FrameDimension Time {
 		get {
-			throw new NotImplementedException ();
+			return new FrameDimension ( new Guid ( "6aedbd6d-3fb5-418a-83a6-7f45229dc872" ) );			
 		}
 	}
 	
-	//methods
+    //methods
 	public override bool Equals (object o) {
 		throw new NotImplementedException ();
 	}
