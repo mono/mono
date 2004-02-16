8f1072d4da06d08a066c4128bc4ab85f3387b8a4
Marek Safar <marek.safar@gmail.com>
2004-02-16 09:35:07 +0000

    2004-02-16 Marek Safar <marek.safar@seznam.cz>
    	* test-230.cs: test for module attributes.
    
    svn path=/trunk/mcs/; revision=23120

---
+++ mcs/tests/test-230.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+
+[module: DebuggableAttribute (false, false)] 
+
+class TestClass {
+        static int Main()
+        {
+            Module[] moduleArray;
+            moduleArray = Assembly.GetExecutingAssembly().GetModules(false);
+
+            Module myModule = moduleArray[0];
+            object[] attributes;
+            
+            attributes = myModule.GetCustomAttributes(typeof (DebuggableAttribute), false);
+            if (attributes[0] != null)
+            {
+                Console.WriteLine ("Succeeded");
+                return 0;
+            }
+            return 1;
+        }
+}
