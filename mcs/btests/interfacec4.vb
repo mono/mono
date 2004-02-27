9d19dc3c984404fdaeed40373974cbfa06b4ee5a
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-02-27 05:50:58 +0000

    Deleting files starting with lowercase 'i', 'l' and 'm'
    svn path=/trunk/mcs/; revision=23520

--- mcs/btests/interfacec4.vb
+++
@@ -1,8 +0,0 @@
-Imports System
-Interface I
-	Public Sub S()
-End Interface
-Module InterfaceC4
-	Sub Main()
-	End Sub
-End Module
--- mcs/btests/moduletestc1.vb
+++
@@ -1,7 +0,0 @@
-Imports System
-Module ModuleTest
-	Module ModuleInner
-	End Module
-	Sub Main()
-	End Sub
-End Module
