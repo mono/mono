1726cd69e350633407a4742666b4e03989460a24
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-02-27 06:00:52 +0000

    Deleting vb files starting with lowercase
    svn path=/trunk/mcs/; revision=23521

--- mcs/btests/overridec2.vb
+++
@@ -1,9 +0,0 @@
-Class C1
-	Public Notoverridable Sub S() 'methods that do not override any other method should not be declared 'notoverridable'
-
-	End Sub
-End Class
-Module OverrideC2
-	Sub Main()
-	End Sub
-End Module
