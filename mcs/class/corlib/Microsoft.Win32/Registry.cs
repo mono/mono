e6e48d4872ac1c43c3f11bdac8d6c6ff09af5e4a
Jackson Harper <jackson@novell.com>
2004-03-03 22:40:13 +0000

    * Win32RegistryApi.cs: New file - pinvokes to the win32 	registry. Patch  by Jose Cornado. 	* IRegistryApi.cs: New file - Interface to a registry backend 	(win32 on windows, no implementation on unix). This interface will 	probably change when we write a Linux backend. 	* Win32ResultCode.cs: New file - Win32 result codes needed to 	check pinvoke. Patch by Jose Cornado. 	* Registry.cs: Create the keys. Patch by Jose Cornado. 	* RegistryKey.cs: Implement. Patch by Jose Cornado.
    svn path=/trunk/mcs/; revision=23666

--- mcs/class/corlib/Microsoft.Win32/Registry.cs
+++ mcs/class/corlib/Microsoft.Win32/Registry.cs
@@ -3,6 +3,7 @@
 //
 // Author:
 //   stubbed out by Alexandre Pigolkine (pigolkine@gmx.de)
+//
 
 using System;
 
@@ -11,12 +12,19 @@ namespace Microsoft.Win32
 	public sealed class Registry
 	{
 		private Registry () { }
-		public static readonly RegistryKey ClassesRoot;
-		public static readonly RegistryKey CurrentConfig;
-		public static readonly RegistryKey CurrentUser;
-		public static readonly RegistryKey DynData;
-		public static readonly RegistryKey LocalMachine;
-		public static readonly RegistryKey PerformanceData;
-		public static readonly RegistryKey Users;
+		public static readonly RegistryKey ClassesRoot = new RegistryKey (
+				RegistryHive.ClassesRoot, "HKEY_CLASSES_ROOT");
+		public static readonly RegistryKey CurrentConfig = new RegistryKey (
+				RegistryHive.CurrentConfig, "HKEY_CURRENT_CONFIG");
+		public static readonly RegistryKey CurrentUser = new RegistryKey (
+				RegistryHive.CurrentUser, "HKEY_CURRENT_USER");
+		public static readonly RegistryKey DynData = new RegistryKey (
+				RegistryHive.DynData, "HKEY_DYN_DATA");
+		public static readonly RegistryKey LocalMachine = new RegistryKey (
+				RegistryHive.LocalMachine, "HKEY_DYN_DATA");
+		public static readonly RegistryKey PerformanceData = new RegistryKey (
+				RegistryHive.PerformanceData, "HKEY_PERFORMANCE_DATA");
+		public static readonly RegistryKey Users = new RegistryKey (
+				RegistryHive.Users, "HKEY_USERS");
 	}
 }
