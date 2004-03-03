568fb4eaa8207b85c60f386701514cdc36e46b7b
Sanja Gupta <sanjay@mono-cvs.ximian.com>
2004-03-03 14:37:04 +0000

    ChangeLog: Updated ChangeLog gdipFunctions.cs: Added ImageAttributes functions
    svn path=/trunk/mcs/; revision=23652

--- mcs/class/System.Drawing/System.Drawing/gdipFunctions.cs
+++ mcs/class/System.Drawing/System.Drawing/gdipFunctions.cs
@@ -860,6 +860,36 @@ namespace System.Drawing {
                 internal static extern Status GdipSetImageAttributesColorMatrix (IntPtr imageattr,
                                 ColorAdjustType type, bool enableFlag, ColorMatrix colorMatrix,
                                 ColorMatrix grayMatrix,  ColorMatrixFlag flags);                                
+
+		[DllImport ("gdiplus.dll")]     
+		internal static extern Status GdipSetImageAttributesGamma (IntPtr imageattr, 
+												ColorAdjustType type, bool enableFlag,  
+																			float gamma);
+		
+		[DllImport ("gdiplus.dll")]     
+		internal static extern Status GdipSetImageAttributesNoOp (IntPtr imageattr, 
+												ColorAdjustType type, bool enableFlag);
+		
+		[DllImport ("gdiplus.dll")]     
+		internal static extern Status GdipSetImageAttributesOutputChannel (IntPtr imageattr,
+												ColorAdjustType type, bool enableFlag, 
+														ColorChannelFlag channelFlags);
+		
+		[DllImport ("gdiplus.dll", CharSet=CharSet.Auto)]     
+		internal static extern Status GdipSetImageAttributesOutputChannelColorProfile (IntPtr imageattr,
+												ColorAdjustType type, bool enableFlag, 
+										[MarshalAs (UnmanagedType.LPWStr)] string profileName);
+				
+		[DllImport ("gdiplus.dll")]     
+		internal static extern Status GdipSetImageAttributesRemapTable (IntPtr imageattr,
+												ColorAdjustType type, bool enableFlag, 
+													uint mapSize, IntPtr colorMap);
+		
+		[DllImport ("gdiplus.dll")]     
+		internal static extern Status GdipSetImageAttributesThreshold (IntPtr imageattr, 
+												ColorAdjustType type, bool enableFlag, 
+																		float thresHold);
+		
                                
 		// Font		
 		[DllImport("gdiplus.dll")]                   
