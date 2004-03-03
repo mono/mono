16e0d2166afe3182eed8db39326c9ff9dc340ed8
Sanja Gupta <sanjay@mono-cvs.ximian.com>
2004-03-03 14:48:31 +0000

    ChangeLog: Updated ChangeLog ImageAttributes.cs: Added few missing functions Metafile.cs: Added stubs for missing constructors and functions
    svn path=/trunk/mcs/; revision=23653

--- mcs/class/System.Drawing/System.Drawing.Imaging/ImageAttributes.cs
+++ mcs/class/System.Drawing/System.Drawing.Imaging/ImageAttributes.cs
@@ -4,6 +4,8 @@
 // Author:
 //   Dennis Hayes (dennish@raytek.com) (stubbed out)
 //	 Jordi Mas i Hernàndez (jmas@softcatala.org)
+//	 Sanjay Gupta (gsanjay@novell.com)
+//
 // (C) 2002-4 Ximian, Inc.  http://www.ximian.com
 //
 
@@ -36,17 +38,119 @@ namespace System.Drawing.Imaging
 			Console.WriteLine("ImageAttributes().ImageAttributes()" + nativeImageAttr);
 		}
 
+		public void ClearBrushRemapTable()
+		{
+			ClearRemapTable (ColorAdjustType.Brush);			
+		}
+
 		//Clears the color keys for all GDI+ objects
-		public void ClearColorKey(){			                    
+		public void ClearColorKey() 
+		{			                 
+			ClearColorKey (ColorAdjustType.Default);
+		}
+
+		public void ClearColorKey(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesColorKeys (nativeImageAttr, 
+																	type, false, 0, 0);
 			
-			Status status = GDIPlus.GdipSetImageAttributesColorKeys(nativeImageAttr,  ColorAdjustType.Default,  false, 0,0);
+			GDIPlus.CheckStatus (status);			
+		}
+
+		public void ClearColorMatrix()
+		{
+			ClearColorMatrix (ColorAdjustType.Default);
+		}
+		
+		public void ClearColorMatrix(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesColorMatrix (nativeImageAttr, 
+										type, false, null, null, ColorMatrixFlag.Default);
 			
-			if (status != Status.Ok)
-				throw new Exception ("Error calling GDIPlus.ClearColorKey:" +status);
+			GDIPlus.CheckStatus (status);			
+		}
+		
+		public void ClearGamma()
+		{
+			ClearGamma (ColorAdjustType.Default);
+		}
+		
+		public void ClearGamma(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesGamma (nativeImageAttr, 
+																	type, false, 0);
+			
+			GDIPlus.CheckStatus (status);
+		}
+		
+		public void ClearNoOp()
+		{
+			ClearNoOp (ColorAdjustType.Default);
+		}
+		
+		public void ClearNoOp(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesNoOp (nativeImageAttr, 
+																		type, false);
+			
+			GDIPlus.CheckStatus (status);
+		}
+
+		public void ClearOutputChannel()
+		{
+			ClearOutputChannel (ColorAdjustType.Default);
+		}
+		
+		public void ClearOutputChannel(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesOutputChannel (nativeImageAttr, 
+																		type, false, ColorChannelFlag.ColorChannelLast );
+			
+			GDIPlus.CheckStatus (status);
+		}
+
+		public void ClearOutputChannelColorProfile()
+		{
+			ClearOutputChannelColorProfile (ColorAdjustType.Default);
+		}
+		
+		public void ClearOutputChannelColorProfile(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesOutputChannelColorProfile (nativeImageAttr, 
+																	type, false, null);
+			
+			GDIPlus.CheckStatus (status);
+		}
+
+		public void ClearRemapTable()
+		{
+			ClearRemapTable (ColorAdjustType.Default);
+		}
+		
+		public void ClearRemapTable(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesRemapTable (nativeImageAttr, 
+															type, false, 0, IntPtr.Zero);
+			
+			GDIPlus.CheckStatus (status);
+		}
+
+		public void ClearThreshold()
+		{
+			ClearThreshold (ColorAdjustType.Default);
+		}
+		
+		public void ClearThreshold(ColorAdjustType type)
+		{
+			Status status = GDIPlus.GdipSetImageAttributesThreshold (nativeImageAttr, 
+																		type, false, 0);
+			
+			GDIPlus.CheckStatus (status);
 		}
 
 		//Sets the color keys for all GDI+ objects
-		public void SetColorKey(Color colorLow, Color colorHigh){
+		public void SetColorKey(Color colorLow, Color colorHigh)
+		{
 			
 			Status status = GDIPlus.GdipSetImageAttributesColorKeys(nativeImageAttr,
             				ColorAdjustType.Default, true,  colorLow.ToArgb(), colorHigh.ToArgb());
--- mcs/class/System.Drawing/System.Drawing.Imaging/Metafile.cs
+++ mcs/class/System.Drawing/System.Drawing.Imaging/Metafile.cs
@@ -19,96 +19,284 @@ namespace System.Drawing.Imaging {
 
 		// constructors
 		[MonoTODO]
-		public Metafile (Stream stream) {
+		public Metafile (Stream stream) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (string filename) {
+		public Metafile (string filename) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr henhmetafile, bool deleteEmf) {
+		public Metafile (IntPtr henhmetafile, bool deleteEmf) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHtc, EmfType emfType) {
+		public Metafile (IntPtr referenceHtc, EmfType emfType) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHtc, Rectangle frameRect) {
+		public Metafile (IntPtr referenceHtc, Rectangle frameRect) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHtc, RectangleF frameRect) {
-			throw new NotImplementedException ();
-		}
-
-		//[MonoTODO]
-		//public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader) {
-		//	throw new NotImplementedException ();
-		//}
-
-		[MonoTODO]
-		public Metafile (Stream stream, IntPtr referenceHtc) {
+		public Metafile (IntPtr referenceHtc, RectangleF frameRect) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (String fileName, IntPtr referenceHtc) {
+		public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHdc, EmfType emfType, string description) {
+		public Metafile (Stream stream, IntPtr referenceHtc) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit) {
+		public Metafile (String fileName, IntPtr referenceHtc) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit) {
-			throw new NotImplementedException ();
-		}
-
-//		[MonoTODO]
-//		public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader, bool deleteWmf) {
-//			throw new NotImplementedException ();
-//		}
-
-		[MonoTODO]
-		public Metafile (Stream stream, IntPtr referenceHdc, EmfType type) {
+		public Metafile (IntPtr referenceHdc, EmfType emfType, string description) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect) {
+		public Metafile (IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect) {
+		public Metafile (IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (string fileName, IntPtr referenceHdc, EmfType type) {
+		public Metafile (IntPtr hmetafile, WmfPlaceableFileHeader wmfHeader, bool deleteWmf) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
-		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect) {
+		public Metafile (Stream stream, IntPtr referenceHdc, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, RectangleF frameRect) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHtc, EmfType type, string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, EmfType type, string description)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type, string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, EmfType type, string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, EmfType type) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, string description) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type,
+															string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (Stream stream, IntPtr referenceHdc, RectangleF frameRect, MetafileFrameUnit frameUnit, EmfType type,
+															string description) 
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type,
+															string description) 
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public Metafile (string fileName, IntPtr referenceHdc, Rectangle frameRect, MetafileFrameUnit frameUnit, EmfType type,
+															string description) 
+		{
 			throw new NotImplementedException ();
 		}
 
 		// methods
+		[MonoTODO]
+		public IntPtr GetHenhmetafile()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public MetafileHeader GetMetafileHeader()
+		{
+			return GetMetafileHeader (GetHenhmetafile () );
+		}
+
+		[MonoTODO]
+		public static MetafileHeader GetMetafileHeader(IntPtr henhmetafile)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static MetafileHeader GetMetafileHeader(Stream stream)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static MetafileHeader GetMetafileHeader(string fileName)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static MetafileHeader GetMetafileHeader(IntPtr henhmetafile, WmfPlaceableFileHeader wmfHeader)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public void PlayRecord(EmfPlusRecordType recordType, int flags, int dataSize, byte[] datawmfHeader)
+		{
+			throw new NotImplementedException ();
+		}
 		// properties
 	}
 
