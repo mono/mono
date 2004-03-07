e0a97826d7b914619f2081c85f21acc84167b7fa
César Natarén <cesar@mono-cvs.ximian.com>
2004-03-07 22:39:05 +0000

    * Statement.cs: added parent param to If constructor. Implement         Resolve and Emit for If statement.
    svn path=/trunk/mcs/; revision=23772

--- mcs/class/Microsoft.JScript/Microsoft.JScript/Statement.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/Statement.cs
@@ -7,8 +7,10 @@
 // (C) 2003, Cesar Octavio Lopez Nataren, <cesar@ciencias.unam.mx>
 //
 
-using System.Text;
 using System;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Microsoft.JScript {
 
@@ -16,7 +18,7 @@ namespace Microsoft.JScript {
 
 		internal AST cond, true_stm, false_stm;
 
-		internal If (AST condition, AST true_stm, AST false_stm)
+		internal If (AST parent, AST condition, AST true_stm, AST false_stm)
 		{
 			this.cond = condition;
 			this.true_stm = true_stm;
@@ -36,19 +38,65 @@ namespace Microsoft.JScript {
 			
 			return sb.ToString ();
 		}
-
+		
 		internal override bool Resolve (IdentificationTable context)
 		{
-			cond.Resolve (context);
-			true_stm.Resolve (context);
-			false_stm.Resolve (context);
+			bool r = true;
 
-			return true;
+			if (cond != null)
+				if (cond is Exp)
+					r &= ((Exp) cond).Resolve (context, false);
+
+			if (true_stm != null)
+				if (true_stm is Exp)
+					r &= ((Exp) true_stm).Resolve (context, true);
+				else
+					r &= true_stm.Resolve (context);			
+
+			if (false_stm != null)
+				if (false_stm is Exp)
+					r &= ((Exp) false_stm).Resolve (context, true);
+				else
+					r &= false_stm.Resolve (context);			
+			return r;
 		}
 
 		internal override void Emit (EmitContext ec)
 		{
-			throw new NotImplementedException ();
+			ILGenerator ig = ec.ig;
+			Label a, b;
+
+			a = ig.DefineLabel ();
+			b = ig.DefineLabel ();
+
+			if (cond != null)
+				cond.Emit (ec);
+
+			Expression e = cond as Expression;
+			AST tmp = (AST) e.exprs [e.exprs.Count - 1];
+
+			if (tmp is Relational)
+				//ig.Emit (OpCodes.Bge, a);
+				ig.Emit (OpCodes.Brfalse, a);
+			else {
+				Console.WriteLine ("cond is not relational");
+				ig.Emit (OpCodes.Ldc_I4_1);
+				ig.Emit (OpCodes.Call, 
+					 typeof (Convert).GetMethod ("ToBoolean", 
+								     new Type [] { typeof (object), typeof (Boolean)}));
+				ig.Emit (OpCodes.Brfalse, a);
+			}
+			
+			if (true_stm != null)
+				true_stm.Emit (ec);
+			
+			ig.Emit (OpCodes.Br, b);
+			ig.MarkLabel (a);
+
+			if (false_stm != null)
+				false_stm.Emit (ec);
+
+			ig.MarkLabel (b);
 		}
 	}
 
