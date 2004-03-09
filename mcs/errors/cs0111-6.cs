a50335c7110eb1b29d1e5f06c08bd975e6051369
Ben Maurer <benm@mono-cvs.ximian.com>
2004-03-09 03:35:38 +0000

    new tests
    svn path=/trunk/mcs/; revision=23822

---
+++ mcs/errors/cs0111-6.cs
@@ -0,0 +1,11 @@
+// cs111-6.cs : Class `T' already contains a definition with the same return value and parameter types for method `Foo'
+// Line : 6
+
+class T {
+	T () {}
+	T () {}
+
+	public static void Main ()
+	{
+	}
+}
