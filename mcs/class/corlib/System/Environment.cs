0ea5378cf8a07ea25d9db082cb83145f0b6092b8
Martin Baulig <martin@novell.com>
2004-03-10 01:00:17 +0000

    Update corlib version number.
    svn path=/trunk/mcs/; revision=23864

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -29,7 +29,7 @@ namespace System
 		 * increment this variable. Also increment the
 		 * pair of this variable in the runtime in metadata/appdomain.c.
 		 */
-		private const int mono_corlib_version = 12;
+		private const int mono_corlib_version = 13;
 
 		private Environment ()
 		{
