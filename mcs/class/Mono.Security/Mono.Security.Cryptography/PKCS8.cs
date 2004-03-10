471fba111dc9be8539315c90554b37109240269f
Sebastien Pouliot <sebastien@ximian.com>
2004-03-10 11:58:45 +0000

    2004-03-10  Sebastien Pouliot  <sebastien@ximian.com>
    	* PKCS8.cs: Added EncodeRSA, EncodeDSA and completed GetBytes() so it
    	is now possible to encode PKCS8 files (this is required to encode
    	PKCS12 files).
    
    svn path=/trunk/mcs/; revision=23872

--- mcs/class/Mono.Security/Mono.Security.Cryptography/PKCS8.cs
+++ mcs/class/Mono.Security/Mono.Security.Cryptography/PKCS8.cs
@@ -145,10 +145,26 @@ namespace Mono.Security.Cryptography {
 				}
 			}
 
-			// TODO
 			public byte[] GetBytes () 
 			{
-				return null;
+				ASN1 privateKeyAlgorithm = new ASN1 (0x30);
+				privateKeyAlgorithm.Add (ASN1Convert.FromOID (_algorithm));
+				privateKeyAlgorithm.Add (new ASN1 (0x05)); // ASN.1 NULL
+
+				ASN1 pki = new ASN1 (0x30);
+				pki.Add (new ASN1 (0x02, new byte [1] { (byte) _version }));
+				pki.Add (privateKeyAlgorithm);
+				pki.Add (new ASN1 (0x04, _key));
+
+				if (_list.Count > 0) {
+					ASN1 attributes = new ASN1 (0xA0);
+					foreach (ASN1 attribute in _list) {
+						attributes.Add (attribute);
+					}
+					pki.Add (attributes);
+				}
+
+				return pki.GetBytes ();
 			}
 
 			// static methods
@@ -194,9 +210,9 @@ namespace Mono.Security.Cryptography {
 			 *	otherPrimeInfos   OtherPrimeInfos OPTIONAL 
 			 * }
 			 */
-			static public RSA DecodeRSA (byte[] encryptedKeypair) 
+			static public RSA DecodeRSA (byte[] keypair) 
 			{
-				ASN1 privateKey = new ASN1 (encryptedKeypair);
+				ASN1 privateKey = new ASN1 (keypair);
 				if (privateKey.Tag != 0x30)
 					throw new CryptographicException ("invalid private key format");
 
@@ -226,22 +242,70 @@ namespace Mono.Security.Cryptography {
 				return rsa;
 			}
 
+			/*
+			 * RSAPrivateKey ::= SEQUENCE {
+			 *	version           Version, 
+			 *	modulus           INTEGER,  -- n
+			 *	publicExponent    INTEGER,  -- e
+			 *	privateExponent   INTEGER,  -- d
+			 *	prime1            INTEGER,  -- p
+			 *	prime2            INTEGER,  -- q
+			 *	exponent1         INTEGER,  -- d mod (p-1)
+			 *	exponent2         INTEGER,  -- d mod (q-1) 
+			 *	coefficient       INTEGER,  -- (inverse of q) mod p
+			 *	otherPrimeInfos   OtherPrimeInfos OPTIONAL 
+			 * }
+			 */
+			static public byte[] Encode (RSA rsa) 
+			{
+				RSAParameters param = rsa.ExportParameters (true);
+
+				ASN1 rsaPrivateKey = new ASN1 (0x30);
+				rsaPrivateKey.Add (new ASN1 (0x02, new byte [1] { 0x00 }));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.Modulus));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.Exponent));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.D));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.P));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.Q));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.DP));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.DQ));
+				rsaPrivateKey.Add (ASN1Convert.FromUnsignedBigInteger (param.InverseQ));
+
+				return rsaPrivateKey.GetBytes ();
+			}
+
 			// DSA only encode it's X private key inside an ASN.1 INTEGER (Hint: Tag == 0x02)
 			// which isn't enough for rebuilding the keypair. The other parameters
 			// can be found (98% of the time) in the X.509 certificate associated
 			// with the private key or (2% of the time) the parameters are in it's
 			// issuer X.509 certificate (not supported in the .NET framework).
-			static public DSA DecodeDSA (byte[] encryptedPrivateKey, DSAParameters dsaParameters) 
+			static public DSA DecodeDSA (byte[] privateKey, DSAParameters dsaParameters) 
 			{
-				ASN1 privateKey = new ASN1 (encryptedPrivateKey);
-				if (privateKey.Tag != 0x02)
+				ASN1 pvk = new ASN1 (privateKey);
+				if (pvk.Tag != 0x02)
 					throw new CryptographicException ("invalid private key format");
 
 				// X is ALWAYS 20 bytes (no matter if the key length is 512 or 1024 bits)
-				dsaParameters.X = Normalize (encryptedPrivateKey, 20);
+				dsaParameters.X = Normalize (privateKey, 20);
 				DSA dsa = DSA.Create ();
 				dsa.ImportParameters (dsaParameters);
 				return dsa;
+			}
+
+			static public byte[] Encode (DSA dsa) 
+			{
+				DSAParameters param = dsa.ExportParameters (true);
+				return ASN1Convert.FromUnsignedBigInteger (param.X).GetBytes ();
+			}
+
+			static public byte[] Encode (AsymmetricAlgorithm aa) 
+			{
+				if (aa is RSA)
+					return Encode ((RSA)aa);
+				else if (aa is DSA)
+					return Encode ((DSA)aa);
+				else
+					throw new CryptographicException ("Unknown asymmetric algorithm {0}", aa.ToString ());
 			}
 		}
 
@@ -285,18 +349,33 @@ namespace Mono.Security.Cryptography {
 
 			public string Algorithm {
 				get { return _algorithm; }
+				set { _algorithm = value; }
 			}
 
 			public byte[] EncryptedData {
-				get { return (byte[]) _data.Clone (); }
+				get { return (_data == null) ? null : (byte[]) _data.Clone (); }
+				set { _data = (value == null) ? null : (byte[]) value.Clone (); }
 			}
 
 			public byte[] Salt {
-				get { return (byte[]) _salt.Clone (); }
+				get { 
+					if (_salt == null) {
+						RandomNumberGenerator rng = RandomNumberGenerator.Create ();
+						_salt = new byte [8];
+						rng.GetBytes (_salt);
+					}
+					return (byte[]) _salt.Clone (); 
+				}
+				set { _salt = (byte[]) value.Clone (); }
 			}
 
 			public int IterationCount {
 				get { return _iterations; }
+				set { 
+					if (value < 0)
+						throw new ArgumentOutOfRangeException ("IterationCount", "Negative");
+					_iterations = value; 
+				}
 			}
 
 			// methods
@@ -341,10 +420,33 @@ namespace Mono.Security.Cryptography {
 			// so you're on your own. Just don't try to copy the big guys too much ;)
 			// Netscape:	http://www.cs.auckland.ac.nz/~pgut001/pubs/netscape.txt
 			// Microsoft:	http://www.cs.auckland.ac.nz/~pgut001/pubs/breakms.txt
-			public byte[] GetBytes (byte[] encryptedPrivateKey)
+			public byte[] GetBytes ()
 			{
-				// TODO
-				return null;
+				if (_algorithm == null)
+					throw new CryptographicException ("No algorithm OID specified");
+
+				ASN1 encryptionAlgorithm = new ASN1 (0x30);
+				encryptionAlgorithm.Add (ASN1Convert.FromOID (_algorithm));
+
+				// parameters ANY DEFINED BY algorithm OPTIONAL
+				if ((_iterations > 0) || (_salt != null)) {
+					ASN1 salt = new ASN1 (0x04, _salt);
+					ASN1 iterations = ASN1Convert.FromInt32 (_iterations);
+
+					ASN1 parameters = new ASN1 (0x30);
+					parameters.Add (salt);
+					parameters.Add (iterations);
+					encryptionAlgorithm.Add (parameters);
+				}
+
+				// encapsulates EncryptedData into an OCTET STRING
+				ASN1 encryptedData = new ASN1 (0x04, _data);
+
+				ASN1 encryptedPrivateKeyInfo = new ASN1 (0x30);
+				encryptedPrivateKeyInfo.Add (encryptionAlgorithm);
+				encryptedPrivateKeyInfo.Add (encryptedData);
+
+				return encryptedPrivateKeyInfo.GetBytes ();
 			}
 		}
 	}
