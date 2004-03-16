ad9727b88a42dddfd3cc4eb7188997bad1ff7baa
César Natarén <cesar@mono-cvs.ximian.com>
2004-03-16 23:52:33 +0000

    Plus.cs: added rule for string addition.
    svn path=/trunk/mcs/; revision=24186

--- mcs/class/Microsoft.JScript/Microsoft.JScript/Plus.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/Plus.cs
@@ -19,7 +19,10 @@ namespace Microsoft.JScript {
 
 		public  object EvaluatePlus (object v1, object v2)
 		{
-			return new object ();
+			if (v1 is String && v2 is String)
+				return String.Concat (v1, v2);
+			else
+				return new object ();
 		}
 
 		public static object DoOp (object v1, object v2)
