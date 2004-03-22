193b3d3665091657e6735159c4ecb984ba9c87ca
Martin Baulig <martin@novell.com>
2004-03-22 23:10:46 +0000

    delegate test ....
    svn path=/trunk/mcs/; revision=24442

---
+++ mcs/tests/gen-46.cs
@@ -0,0 +1,30 @@
+using System;
+
+delegate void Test<T> (T t);
+
+class Foo<T>
+{
+	public event Test<T> MyEvent;
+
+	public void Hello (T t)
+	{
+		if (MyEvent != null)
+			MyEvent (t);
+	}
+}
+
+class X
+{
+	void do_hello (string hello)
+	{
+		Console.WriteLine ("Hello: {0}", hello);
+	}
+
+	static void Main ()
+	{
+		Foo<string> foo = new Foo<string> ();
+		foo.MyEvent += new Test<string> (do_hello);
+		foo.MyEvent += new Test (do_hello);
+		foo.Hello ();
+	}
+}
