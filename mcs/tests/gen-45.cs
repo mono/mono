b4a3bf5b129a523e49198f769430cfbfdbcb6918
Martin Baulig <martin@novell.com>
2004-03-23 19:03:16 +0000

    Make this actually work.
    svn path=/trunk/mcs/; revision=24492

--- mcs/tests/gen-45.cs
+++ mcs/tests/gen-45.cs
@@ -6,7 +6,7 @@ class Test<A,B>
 	public void Foo<V> (V[] v, V w)
 	{ }
 
-	public void Hello<V,W> (Test<V,B> v, Test<A,W> w)
+	public void Hello<V,W> (V v, W w, Test<V,W> x)
 	{ }
 
 	public void ArrayMethod<V> (params V[] args)
@@ -20,7 +20,7 @@ class X
 		Test<float,int> test = new Test<float,int> ();
 		test.Foo ("Hello World");
 		test.Foo (new long[] { 3, 4, 5 }, 9L);
-		test.Hello (test, test);
+		test.Hello (3.14F, 9, test);
 		test.ArrayMethod (3.14F, (float) 9 / 3);
 	}
 }
