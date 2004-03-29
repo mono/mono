67593bde698f19d48f08c13f2139412042ee9359
Miguel de Icaza <miguel@gnome.org>
2004-03-29 21:40:49 +0000

    Add new test
    svn path=/trunk/mcs/; revision=24729

---
+++ mcs/tests/unsafe-7.cs
@@ -0,0 +1,17 @@
+//Compile with /unsafe
+
+struct Obsolete {
+	int a;
+}
+struct A {
+	int a, b;
+}
+
+class MainClass {
+        unsafe public static void Main ()
+        {
+                System.Console.WriteLine (sizeof (Obsolete));
+        }
+}
+
+
