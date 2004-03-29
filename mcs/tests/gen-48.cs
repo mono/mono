a02966ad517245108965d8721294fa3d0924a45a
Martin Baulig <martin@novell.com>
2004-03-29 01:58:26 +0000

    New test.
    svn path=/trunk/mcs/; revision=24691

---
+++ mcs/tests/gen-48.cs
@@ -0,0 +1,29 @@
+public class Stack<T>
+{
+	T[] data;
+
+	public Stack ()
+	{
+		data = new T [10];
+	}
+
+	public void Add (T t)
+	{
+		data [0] = t;
+	}
+}
+
+struct Foo
+{
+	int a;
+}
+
+class X
+{
+	static void Main ()
+	{
+		Foo foo = new Foo ();
+		Stack<Foo> stack = new Stack<Foo> ();
+		stack.Add (foo);
+	}
+}
