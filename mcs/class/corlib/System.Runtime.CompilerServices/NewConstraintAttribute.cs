d3d04925918196f6758c6ae3d1d314e4c2d53592
Martin Baulig <martin@novell.com>
2004-03-30 20:03:00 +0000

    2004-03-30  Martin Baulig  <martin@ximian.com>
    	* NewConstraintAttribute.cs: New file.
    
    svn path=/trunk/mcs/; revision=24800

---
+++ mcs/class/corlib/System.Runtime.CompilerServices/NewConstraintAttribute.cs
@@ -0,0 +1,20 @@
+//
+// System.Runtime.CompilerServices/NewConstraintAttribute.cs
+//
+// Author:
+//   Martin Baulig (martin@ximian.com)
+//
+// (C) 2004 Novell, Inc.
+//
+
+using System.Runtime.InteropServices;
+
+namespace System.Runtime.CompilerServices
+{
+	public sealed class NewConstraintAttribute : Attribute
+	{
+		public NewConstraintAttribute ()
+		{
+		}
+	}
+}
