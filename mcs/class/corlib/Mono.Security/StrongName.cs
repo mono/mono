846df6fec5e011a03a6285e109ad53e7d7d61175
Sebastien Pouliot <sebastien@ximian.com>
2004-03-31 19:52:07 +0000

    2004-03-31  Sebastien Pouliot  <sebastien@ximian.com>
    	* StrongName.cs: Removed #if for NET_1_2
    
    svn path=/trunk/mcs/; revision=24870

--- mcs/class/corlib/Mono.Security/StrongName.cs
+++ mcs/class/corlib/Mono.Security/StrongName.cs
@@ -120,7 +120,7 @@ namespace Mono.Security {
 			get {
 				if (rsa == null)
 					return false;
-#if INSIDE_CORLIB || NET_1_2
+#if INSIDE_CORLIB
 				// the easy way
 				if (RSA is RSACryptoServiceProvider) {
 					// available as internal for corlib
