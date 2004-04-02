05d7ee5919a550060853ab39f9d5ff792661de06
Martin Baulig <martin@novell.com>
2004-04-02 23:25:15 +0000

    New test.
    svn path=/trunk/mcs/; revision=24984

---
+++ mcs/tests/gen-55.cs
@@ -0,0 +1,28 @@
+// Using constructed types in a namespace alias.
+
+namespace N1
+{
+	class A<T>
+	{
+		public class B { }
+
+		public class C<U> { }
+	}
+
+	class C { }
+}
+
+namespace N2
+{
+	using Y = N1.A<int>;
+
+	class X
+	{
+		static void Main ()
+		{
+			Y y = new Y ();
+			Y.B b = new Y.B ();
+			Y.C<long> c = new Y.C<long> ();
+		}
+	}
+}
