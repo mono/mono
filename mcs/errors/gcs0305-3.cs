5fd937e4e0faf273b58ed9dd95f1341fed52793c
Martin Baulig <martin@novell.com>
2004-04-03 03:25:37 +0000

    New tests.
    svn path=/trunk/mcs/; revision=24987

---
+++ mcs/errors/gcs0305-3.cs
@@ -0,0 +1,20 @@
+// CS0305: Using the generic type `N1.A<T>' requires 1 type arguments
+// Line: 12
+namespace N1
+{
+	class A<T>
+	{
+	}
+}
+
+namespace N3
+{
+	using W = N1.A;
+
+	class X
+	{
+		static void Main ()
+		{
+		}
+	}
+}
