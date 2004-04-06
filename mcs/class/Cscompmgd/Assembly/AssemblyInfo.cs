6e2cdc3d95fd806840f0406669a10c67b828afed
Andreas N <andreas@mono-cvs.ximian.com>
2004-04-06 20:20:39 +0000

    2004-04-06  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added
    	* ChangeLog: Added
    	* Locale.cs: Added
    
    svn path=/trunk/mcs/; revision=25115

---
+++ mcs/class/Cscompmgd/Assembly/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about the System.Web.Mobile assembly
+
+#if (NET_1_0)
+	[assembly: AssemblyVersion ("7.0.3300.0")]
+#endif
+#if (NET_1_1)
+	[assembly: AssemblyVersion ("7.0.5000.0")]
+#endif
+
+[assembly: AssemblyTitle ("Managed C# Compiler")]
+[assembly: AssemblyDescription ("Interface for the managed C# compiler")]
+
+[assembly: CLSCompliant (true)]
+[assembly: AssemblyFileVersion ("0.0.0.1")]
+
+[assembly: ComVisible (false)]
+
+//[assembly: AssemblyDelaySign (true)]
+//[assembly: AssemblyKeyFile ("")]
