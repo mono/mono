8a979ffd252431e57e786926d039540dd742bb78
Miguel de Icaza <miguel@gnome.org>
2004-04-07 16:46:30 +0000

    Add new errors
    svn path=/trunk/mcs/; revision=25163

---
+++ mcs/errors/cs1601.cs
@@ -0,0 +1,13 @@
+// cs1601.cs: ArgIterator and TypedReference are invalid as ref/out arguments
+//
+using System;
+
+class X {
+	static void Main ()
+	{
+	}
+
+	static void M (ref TypedReference a)
+	{
+	}
+}
