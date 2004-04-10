1abd7ec48eb7615461a846a3e5fe115d3a9dcb64
Miguel de Icaza <miguel@gnome.org>
2004-04-10 18:58:44 +0000

    errors
    svn path=/trunk/mcs/; revision=25308

---
+++ mcs/errors/cs0633-2.cs
@@ -0,0 +1,15 @@
+public class MonthDays {
+   [System.Runtime.CompilerServices.IndexerName ("")]
+   public int this [int a] {
+      get {
+         return 0;
+      }
+   }
+
+   public static void Main ()
+   {
+	int i = new MonthDays () [1];
+   }
+}
+
+
---
+++ mcs/errors/cs0681.cs
@@ -0,0 +1,7 @@
+// cs0681: Fields can not be abstract
+// Line: 4
+class X {
+	abstract int name;
+
+	static void Main () {}
+}
