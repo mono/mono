38f5601f7d35b9a8a29b34bbf7039ce1adc22553
Gert Driesen <drieseng@users.sourceforge.net>
2004-04-12 09:19:24 +0000

    * FileSystemInfo.cs: Implemented ISerializable.
    svn path=/trunk/mcs/; revision=25354

--- mcs/class/corlib/System.IO/FileSystemInfo.cs
+++ mcs/class/corlib/System.IO/FileSystemInfo.cs
@@ -11,11 +11,24 @@
 //------------------------------------------------------------------------------
 
 using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 namespace System.IO {
 	
 	[Serializable]
-	public abstract class FileSystemInfo : MarshalByRefObject {
+	public abstract class FileSystemInfo : MarshalByRefObject, ISerializable {
+		#region Implementation of ISerializable
+
+		[ComVisible(false)]
+		public virtual void GetObjectData (SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue ("OriginalPath", OriginalPath, typeof(string));
+			info.AddValue ("FullPath", FullPath, typeof(string));
+		}
+
+		#endregion Implementation of ISerializable
+
 		// public properties
 
 		public abstract bool Exists { get; }
@@ -153,6 +166,17 @@ namespace System.IO {
 		{
 			this.valid = false;
 			this.FullPath = null;
+		}
+
+		protected FileSystemInfo (SerializationInfo info, StreamingContext context)
+		{
+			if (info == null)
+			{
+				throw new ArgumentNullException("info");
+			}
+
+			FullPath = info.GetString("FullPath");
+			OriginalPath = info.GetString("OriginalPath");
 		}
 
 		protected string FullPath;
