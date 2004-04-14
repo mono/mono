5d903c17e5c2de939fc42137c81137c824aaa065
Andreas N <andreas@mono-cvs.ximian.com>
2004-04-14 21:58:19 +0000

    2004-04-14  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Removed
    	* TODOAttribute.cs: Removed (duplicate)
    
    	* System.Windows.Forms.Design.csproj.user
    	* System.Windows.Forms.Design.csproj: Removed (outdated)
    
    	* WindowsFormsComponentEditor.cs
    	* ComponentEditorPage.cs: Added, mostly implemented
    
    	* ComponentEditor.cs: Removed (does not belong here)
    
    svn path=/trunk/mcs/; revision=25509

--- mcs/class/System.Windows.Forms/System.Windows.Forms.Design/IUIService.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms.Design/IUIService.cs
@@ -1,26 +1,31 @@
-using System;
+//
+// System.Windows.Forms.Design.IUIService.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+
+using System.Runtime.InteropServices;
 using System.Collections;
 
 namespace System.Windows.Forms.Design
 {
-	/// <summary>
-	/// Summary description for IUIService.
-	/// </summary>
+	[Guid ("06a9c74b-5e32-4561-be73-381b37869f4f")]
 	public interface IUIService
 	{
 		IDictionary Styles {get;}
-		
-		bool CanShowComponentEditor( object component);		
-		IWin32Window GetDialogOwnerWindow();
-		void SetUIDirty();
-		bool ShowComponentEditor( object component, IWin32Window parent);		
-		void ShowError(Exception ex);
-		void ShowError(string message);
-		void ShowError(Exception ex, string message);
-		DialogResult ShowDialog(Form form);		
-		void ShowMessage(string message);
-		void ShowMessage(string message, string caption);
-		DialogResult ShowMessage(string message, string caption, MessageBoxButtons buttons);
-		bool ShowToolWindow( Guid toolWindow);
+
+		bool CanShowComponentEditor (object component);
+		IWin32Window GetDialogOwnerWindow ();
+		void SetUIDirty ();
+		bool ShowComponentEditor (object component, IWin32Window parent);
+		void ShowError (Exception ex);
+		void ShowError (string message);
+		void ShowError (Exception ex, string message);
+		DialogResult ShowDialog (Form form);
+		void ShowMessage (string message);
+		void ShowMessage (string message, string caption);
+		DialogResult ShowMessage (string message, string caption, MessageBoxButtons buttons);
+		bool ShowToolWindow (Guid toolWindow);
 	}
 }
