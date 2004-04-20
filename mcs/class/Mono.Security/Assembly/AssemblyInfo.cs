439b1d780031ef73f50af69d58a36be22e009a32
Sebastien Pouliot <sebastien@ximian.com>
2004-04-20 12:17:29 +0000

    2004-04-20  Sebastien Pouliot <sebastien@ximian.com>
    	* AssemblyInfo.cs: New. Copied and ajusted from System.Security.
    	* Locale.cs: New. Copied (verbatim) from System.Security.
    	* ChangeLog: New.
    
    svn path=/trunk/mcs/; revision=25720

---
+++ mcs/class/Mono.Security/Assembly/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+//
+// AssemblyInfo.cs
+//
+// Authors:
+//	Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//	Sebastien Pouliot  (sebastien@ximian.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+// (C) 2004 Novell (http://www.novell.com)
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Security.Permissions;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about the system assembly
+
+#if (NET_1_0)
+	[assembly: AssemblyVersion ("1.0.3300.0")]
+	[assembly: SatelliteContractVersion ("1.0.3300.0")]
+#elif (NET_1_1)
+	[assembly: AssemblyVersion ("1.0.5000.0")]
+	[assembly: SatelliteContractVersion ("1.0.5000.0")]
+#elif (NET_1_2)
+	[assembly: AssemblyVersion ("1.2.3400.0")]
+	[assembly: SatelliteContractVersion ("1.2.3400.0")]
+#endif
+
+[assembly: AssemblyCompany ("MONO development team")]
+[assembly: AssemblyCopyright ("(c) 2003-2004 Various Authors")]
+[assembly: AssemblyDescription ("Mono.Security.dll")]
+[assembly: AssemblyProduct ("MONO CLI")]
+[assembly: AssemblyTitle ("Mono.Security.dll")]
+//[assembly: CLSCompliant (true)]
+[assembly: ComVisible (false)]
+[assembly: NeutralResourcesLanguage ("en-US")]
+
+
+// BigInteger use unsafe code
+[assembly:SecurityPermission(SecurityAction.RequestOptional, UnmanagedCode=true)]
+
+
+//[assembly: AssemblyDelaySign (true)]
+//[assembly: AssemblyKeyFile ("")]
