bd1e4b847d37491f3c4d80f781ccf4bbf443b1f8
Atsushi Eno <atsushieno@gmail.com>
2004-04-23 04:19:38 +0000

    2004-04-23  Atsushi Enomoto  <atsushi@ximian.com>
    	* ConstructorBuilder.cs, DynamicMethod.cs, EnumBuilder.cs,
    	  GenericTypeParameterBuilder.cs, MethodBuilder.cs, ModuleBuilder.cs,
    	  OpCodes.cs, TypeBuilder.cs :
    	  NET_2_0 related build fix.
    
    svn path=/trunk/mcs/; revision=25881

--- mcs/class/corlib/System.Reflection.Emit/DynamicMethod.cs
+++ mcs/class/corlib/System.Reflection.Emit/DynamicMethod.cs
@@ -8,7 +8,7 @@
 // (C) 2003 Ximian, Inc.  http://www.ximian.com
 //
 
-#if NET_2_0 | BOOTSTRAP_NET_2_0
+#if BOOTSTRAP_NET_2_0//NET_2_0 | BOOTSTRAP_NET_2_0
 
 using System;
 using System.Reflection;
--- mcs/class/corlib/System.Reflection.Emit/GenericTypeParameterBuilder.cs
+++ mcs/class/corlib/System.Reflection.Emit/GenericTypeParameterBuilder.cs
@@ -13,7 +13,7 @@ using System.Runtime.CompilerServices;
 using System.Globalization;
 using System.Runtime.Serialization;
 
-#if NET_2_0 | BOOTSTRAP_NET_2_0
+#if BOOTSTRAP_NET_2_0//NET_2_0 | BOOTSTRAP_NET_2_0
 namespace System.Reflection.Emit
 {
 	public sealed class GenericTypeParameterBuilder : Type
