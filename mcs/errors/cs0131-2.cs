b04808f58e14905a2d1441437934bc765332f64a
Martin Baulig <martin@novell.com>
2004-04-28 23:46:27 +0000

    New test.
    svn path=/trunk/mcs/; revision=26221

---
+++ mcs/errors/cs0131-2.cs
@@ -0,0 +1,12 @@
+// CS0131: The left-hand side of an assignment must be a variable, property or indexer
+// Line: 10
+using System;
+
+class X
+{
+	static void Main ()
+	{
+		int a = 7;
+		+a = 9;
+	}
+}
