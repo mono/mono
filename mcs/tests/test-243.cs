c5cbe53e8782c22a6ca1d1c4d638fe8f66248b35
Martin Baulig <martin@novell.com>
2004-04-28 16:31:50 +0000

    2004-04-27  Martin Baulig  <martin@ximian.com>
    	* test-154.cs: Added test case for #52625.
    
    svn path=/trunk/mcs/; revision=26174

---
+++ mcs/tests/test-243.cs
@@ -0,0 +1,21 @@
+// Bug #57014.
+using System;
+
+public class X {
+	public const string Address = null;
+	
+	public static bool Resolve (string addr)
+	{
+		return true;
+	}
+
+	static string Test ()
+	{
+		return Address;
+	}
+
+	static void Main ()
+	{
+		Resolve (Address);
+	}
+}
