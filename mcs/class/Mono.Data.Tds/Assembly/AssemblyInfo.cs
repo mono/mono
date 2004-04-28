3870af671b5107a2978d085424e27d7974badf31
Sebastien Pouliot <sebastien@ximian.com>
2004-04-28 19:24:01 +0000

    2004-04-28  Sebastien Pouliot  <sebastien@ximian.com>
    	* AssemblyInfo.cs: Added
    	* ChangeLog: Added
    	* Locale.cs: Added
    
    svn path=/trunk/mcs/; revision=26194

---
+++ mcs/class/Mono.Data.Tds/Assembly/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/* TODO COMPLETE INFORMATION
+
+#if (NET_1_0)
+	[assembly: AssemblyVersion ("1.0.3300.0")]
+#endif
+#if (NET_1_1)
+	[assembly: AssemblyVersion ("1.0.5000.0")]
+#endif
+
+[assembly: AssemblyTitle ("")]
+[assembly: AssemblyDescription ("")]
+
+[assembly: CLSCompliant (true)]
+[assembly: AssemblyFileVersion ("0.0.0.1")]
+
+[assembly: ComVisible (false)]
+
+*/
+
+[assembly: AssemblyDelaySign (true)]
+[assembly: AssemblyKeyFile("../mono.pub")]
+
