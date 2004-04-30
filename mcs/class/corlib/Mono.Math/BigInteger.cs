3761545f6ab7258cae87b9a74886e6d0eb640bbb
Ben Maurer <benm@mono-cvs.ximian.com>
2004-04-30 20:13:36 +0000

    2004-04-30 Ben Maurer  <bmaurer@users.sourceforge.net>
    	* BigInteger.cs: use readonly for prime array.
    
    svn path=/trunk/mcs/; revision=26454

--- mcs/class/corlib/Mono.Math/BigInteger.cs
+++ mcs/class/corlib/Mono.Math/BigInteger.cs
@@ -62,7 +62,7 @@ namespace Mono.Math {
 		///			</code>
 		///		</para>
 		/// </remarks>
-		internal static uint [] smallPrimes = {
+		internal static readonly uint [] smallPrimes = {
 			2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
 			73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
 			157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
