be564a8e30979541d77de9efd5675304d9438f55
Sebastien Pouliot <sebastien@ximian.com>
2004-05-02 19:19:13 +0000

    2004-05-02  Sebastien Pouliot  <sebastien@ximian.com>
    	* AssemblyInfo.cs: Added missing using clause. Fixed the build.
    
    svn path=/trunk/mcs/; revision=26556

--- mcs/class/Microsoft.VisualC/Assembly/AssemblyInfo.cs
+++ mcs/class/Microsoft.VisualC/Assembly/AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 #if (NET_2_0)
 	[assembly: AssemblyVersion ("2.0.3600.0")]
@@ -24,13 +25,6 @@ using System.Runtime.InteropServices;
 #endif
 
 /* TODO COMPLETE INFORMATION
-
-#if (NET_1_0)
-	[assembly: AssemblyVersion ("1.0.3300.0")]
-#endif
-#if (NET_1_1)
-	[assembly: AssemblyVersion ("1.0.5000.0")]
-#endif
 
 [assembly: AssemblyTitle ("")]
 [assembly: AssemblyDescription ("")]
