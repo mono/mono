546a1fdca5c987cc043f8c744e2c9f2d928a9d25
Ben Maurer <benm@mono-cvs.ximian.com>
2004-05-02 16:21:35 +0000

    version
    svn path=/trunk/mcs/; revision=26546

--- mcs/class/I18N/Assembly/AssemblyInfo.cs
+++ mcs/class/I18N/Assembly/AssemblyInfo.cs
@@ -12,12 +12,12 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if (NET_1_0)
-	[assembly: AssemblyVersion ("1.0.3300.0")]
+#if (NET_2_0)
+	[assembly: AssemblyVersion ("2.0.3600.0")]
 #elif (NET_1_1)
 	[assembly: AssemblyVersion ("1.0.5000.0")]
-#elif (NET_2_0)
-	[assembly: AssemblyVersion ("2.0.3600.0")]
+#else
+	[assembly: AssemblyVersion ("1.0.3300.0")]
 #endif
 
 /* TODO COMPLETE INFORMATION
