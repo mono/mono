c1fb15a59e1981655f336aa7d3fbd7ca8eba5cbd
Marek Safar <marek.safar@gmail.com>
2004-05-03 07:21:41 +0000

    2004-05-03 Marek Safar <marek.safar@seznam.cz>
    	* cs0579-3.cs,
    	* cs0592-7.cs,
    	* cs0626.cs: 3 new error tests.
    
    svn path=/trunk/mcs/; revision=26579

---
+++ mcs/errors/cs0579-3.cs
@@ -0,0 +1,12 @@
+// cs0579.cs : Duplicate 'Obsolete' attribute
+// Line : 8
+
+using System;
+
+class MainClass {
+        [Obsolete]
+        [Obsolete]
+        static void Main()
+        {
+        }
+}
---
+++ mcs/errors/cs0592-7.cs
@@ -0,0 +1,15 @@
+// cs0592-7.cs: Attribute 'EnumAttribute' is not valid on this declaration type. It is valid on 'method' declarations only.
+// Line: 13
+
+using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class EnumAttribute : Attribute {}
+
+public enum E
+{
+        e_1,
+        [EnumAttribute]
+        e_2
+}
