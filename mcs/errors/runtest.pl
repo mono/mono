9335a6b796b1207287fa7adb0a80888ff4713e68
Raja R Harinath <harinath@hurrynot.org>
2004-05-11 07:22:56 +0000

    * Makefile (run-test-local): Run gmcs and multi tests too. (test-multi-local): Add logging of errors. * runtest.pl: Change $errors and $warnings to keep track of lines on which errors are supposed to occur.
    svn path=/trunk/mcs/; revision=27068

--- mcs/errors/runtest.pl
+++ mcs/errors/runtest.pl
@@ -3,14 +3,25 @@
 use strict;
 use IPC::Open3;
 
+sub remove ($$) {
+    my ($val, $arr) = @_;
+    my $had = 0;
+    my @hasnt = ();
+    
+    for my $v (@$arr) {
+	if ($v == $val) { $had = 1; }
+	else { push @hasnt, $v; }
+    }
+    return ($had, \@hasnt);
+}
+
 # Setting $strict to 1 enables line number checks, setting it to 2 makes
 # line number mismatches fatal.
 my $strict = 2;
 my $failures = 0;
-my $mcs = (defined $ENV{MCS}) ? $ENV{MCS} : 'mcs';
 
-unless ($#ARGV == 0) {
-    print STDERR "Usage: $0 testcase.cs\n";
+if ($#ARGV != 1) {
+    print STDERR "Usage: $0 testcase.cs testcase.out\n";
     exit 1;
 }
 
@@ -18,12 +29,13 @@ my %errors = ();
 my %warnings = ();
 my %lines = ();
 
-my $filename = $ARGV [0];
+my $csfile = $ARGV [0];
+my $mcsout = $ARGV [1];
 my $input;
 
 my $line = 0;
 
-open (INPUT, "<$filename") or die
+open (INPUT, "<$csfile") or die
     "Can't open testcase: $!";
 while (defined ($input = <INPUT>)) {
     ++$line;
@@ -31,75 +43,54 @@ while (defined ($input = <INPUT>)) {
     next unless $input =~ m,^\s*//\s*(error|warning)?\s*(CS\d+),;
 
     if ((defined $1) and ($1 eq 'warning')) {
-	++$warnings{$2};
+	push @{$warnings{$2}}, $line+1;
     } else {
-	++$errors{$2};
+	push @{$errors{$2}}, $line+1;
     }
 
     $lines{$line+1} = $2;
 }
+
 close INPUT;
 
-open (MCS, "$mcs $filename|") or die
-    "Can't open mcs pipe: $!";
+open (MCS, "$mcsout") or die
+    "Can't open compiler output file: $!";
 
 while (defined ($input = <MCS>)) {
     chop $input;
     next unless $input =~ m,\((\d+)\)\s+(warning|error)\s+(CS\d+):,;
+    my $had;
 
     if ($2 eq 'warning') {
-	--$warnings{$3};
+	($had, $warnings{$3}) = remove $1, $warnings{$3};
+	if ($strict && ! $had) {
+	    print "Didn't expect any warnings on line $1, but got $2 $3.\n";
+	    $failures++ if $strict == 2;
+	}
     } else {
-	--$errors{$3};
+	($had, $errors{$3}) = remove $1, $errors{$3};
+	if ($strict && ! $had) {
+	    print "Didn't expect any errors on line $1, but got $2 $3.\n";
+	    $failures++ if $strict == 2;
+	}
     }
-
-    next unless $strict;
-
-    if (!defined $lines{$1}) {
-	print "Didn't expect any warnings or errors in line $1, but got $2 $3.\n";
-	$failures++ if $strict == 2;
-    } elsif ($lines{$1} ne $3) {
-	print "Expected to find ".$lines{$1}." on line $1, but got $3.\n";
-	$failures++ if $strict == 2;
-    }
+    
+    print "Expected to find ".$lines{$1}." on line $1, but got $3.\n"
+	if $strict and $lines{$1} ne $3;
 }
 
 close MCS;
 
 foreach my $error (keys %errors) {
-    my $times = $errors{$error};
-
-    if ($times == -1) {
-	print "Unexpected error $error.\n";
-    } elsif ($times < 0) {
-	print "Unexpected error $error (reported ".(-$times)." times).\n";
-    } elsif ($times == 1) {
-	print "Failed to report error $error.\n";
-    } elsif ($times > 0) {
-	print "Failed to report error $error $times times.\n";
-    }
-
-    $failures++ unless $times == 0;    
+    print "Failed to report error $error on line $_\n"
+	for @{$errors{$error}};
+    $failures += @{$errors{$error}};
 }
 
 foreach my $warning (keys %warnings) {
-    my $times = $warnings{$warning};
-
-    if ($times == -1) {
-	print "Unexpected warning $warning.\n";
-    } elsif ($times < 0) {
-	print "Unexpected warning $warning (reported ".(-$times)." times).\n";
-    } elsif ($times == 1) {
-	print "Failed to report warning $warning.\n";
-    } elsif ($times > 0) {
-	print "Failed to report warning $warning $times times.\n";
-    }
-
-    $failures++ unless $times == 0;    
+    print "Failed to report warning $warning on line $_\n"
+	for @{$warnings{$warning}};
+    $failures += @{$warnings{$warning}};
 }
 
-if ($failures == 0) {
-    exit 0;
-} else {
-    exit 1;
-}
+exit ($failures != 0);
