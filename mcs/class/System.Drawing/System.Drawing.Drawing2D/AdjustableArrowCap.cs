8627dfa65b4ff1fb90f67dacf1445a71aac39fb5
Ravindra <ravindra@mono-cvs.ximian.com>
2004-05-11 05:22:47 +0000

    Implemented AdjustableArrowCap.cs and CustomLineCap.cs.
    svn path=/trunk/mcs/; revision=27063

--- mcs/class/System.Drawing/System.Drawing.Drawing2D/AdjustableArrowCap.cs
+++ mcs/class/System.Drawing/System.Drawing.Drawing2D/AdjustableArrowCap.cs
@@ -1,11 +1,14 @@
 //
 // System.Drawing.Drawing2D.AdjustableArrowCap.cs
 //
-// Author:
-//   Dennis Hayes (dennish@Raytek.com)
+// Authors:
+//	Dennis Hayes (dennish@Raytek.com)
+//	Ravindra (rkumar@novell.com)
 //
-// (C) 2002/3 Ximian, Inc
+// Copyright (C) 2002/3 Ximian, Inc. http://www.ximian.com
+// Copyright (C) 2004 Novell, Inc. http://www.novell.com
 //
+
 using System;
 
 namespace System.Drawing.Drawing2D
@@ -15,67 +18,82 @@ namespace System.Drawing.Drawing2D
 	/// </summary>
 	public sealed class AdjustableArrowCap : CustomLineCap
 	{
-		private bool isFilled;
-		private float height;
-		private float width;
-		private float middleInset;
-		//Constructors
-		public AdjustableArrowCap(float width, float height, bool isFilled) {
-			this.isFilled = isFilled;
-			this.height = height;
-			this.width = width;
-			middleInset = 0;
+		// Constructors
+
+		internal AdjustableArrowCap (IntPtr ptr) : base (ptr)
+		{
 		}
 
-		public AdjustableArrowCap(float width, float height) {
-			isFilled = true;
-			this.height = height;
-			this.width = width;
-			middleInset = 0;
-			//AdjustableArrowCap(width, height, true);
+		public AdjustableArrowCap (float width, float height) : this (width, height, true)
+		{
 		}
 
-		//Public Properities
-		[MonoTODO] //redraw on set
+		public AdjustableArrowCap (float width, float height, bool isFilled)
+		{
+			Status status = GDIPlus.GdipCreateAdjustableArrowCap (height, width, isFilled, out nativeObject);
+			GDIPlus.CheckStatus (status);
+		}
+
+		// Public Properities
+
 		public bool Filled {
 			get {
+				bool isFilled;
+				Status status = GDIPlus.GdipGetAdjustableArrowCapFillState (nativeObject, out isFilled);
+				GDIPlus.CheckStatus (status);
+
 				return isFilled;
 			}
+
 			set {
-				isFilled = value;
-				//Redraw!
-			}
-		}
-		[MonoTODO] //redraw on set
-		public float Width {
-			get {
-				return width;
-			}
-			set {
-				width = value;
-				//Redraw!
-			}
-		}
-		[MonoTODO] //redraw on set
-		public float Height {
-			get {
-				return height;
-			}
-			set {
-				height = value;
-				//Redraw!
-			}
-		}
-		[MonoTODO] //redraw on set
-		public float MiddleInset {
-			get {
-				return middleInset;
-			}
-			set {
-				middleInset = value;
-				//Redraw!
+				Status status = GDIPlus.GdipSetAdjustableArrowCapFillState (nativeObject, value);
+				GDIPlus.CheckStatus (status);
 			}
 		}
 
+		public float Width {
+			get {
+				float width;
+				Status status = GDIPlus.GdipGetAdjustableArrowCapWidth (nativeObject, out width);
+				GDIPlus.CheckStatus (status);
+
+				return width;
+			}
+
+			set {
+				Status status = GDIPlus.GdipSetAdjustableArrowCapWidth (nativeObject, value);
+				GDIPlus.CheckStatus (status);
+			}
+		}
+
+		public float Height {
+			get {
+				float height;
+				Status status = GDIPlus.GdipGetAdjustableArrowCapHeight (nativeObject, out height);
+				GDIPlus.CheckStatus (status);
+
+				return height;
+			}
+
+			set {
+				Status status = GDIPlus.GdipSetAdjustableArrowCapHeight (nativeObject, value);
+				GDIPlus.CheckStatus (status);
+			}
+		}
+
+		public float MiddleInset {
+			get {
+				float middleInset;
+				Status status = GDIPlus.GdipGetAdjustableArrowCapMiddleInset (nativeObject, out middleInset);
+				GDIPlus.CheckStatus (status);
+
+				return middleInset;
+			}
+
+			set {
+				Status status = GDIPlus.GdipSetAdjustableArrowCapMiddleInset (nativeObject, value);
+				GDIPlus.CheckStatus (status);
+			}
+		}
 	}
 }
--- mcs/class/System.Drawing/System.Drawing.Drawing2D/CustomLineCap.cs
+++ mcs/class/System.Drawing/System.Drawing.Drawing2D/CustomLineCap.cs
@@ -2,90 +2,150 @@
 // System.Drawing.Drawing2D.CustomLineCap.cs
 //
 // Authors:
-//   Dennis Hayes (dennish@Raytek.com)
-//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//	Dennis Hayes (dennish@Raytek.com)
+//	Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//	Ravindra (rkumar@novell.com)
 //
-// (C) 2002/3 Ximian, Inc
+// Copyright (C) 2002/3 Ximian, Inc. http://www.ximian.com
+// Copyright (C) 2004 Novell, Inc. http://www.novell.com
 //
+
 using System;
 
-namespace System.Drawing.Drawing2D {
+namespace System.Drawing.Drawing2D
+{
 	/// <summary>
 	/// Summary description for CustomLineCap.
 	/// </summary>
-	public class CustomLineCap : MarshalByRefObject, ICloneable, IDisposable {
-		private LineCap baseCap;
-		private float baseInset;
-		private LineJoin strokeJoin;
-		private float widthScale;
+	public class CustomLineCap : MarshalByRefObject, ICloneable, IDisposable
+	{
+		private bool disposed;
+		internal IntPtr nativeObject;
 
 		// Constructors
-		// Constructor with no parameters is not part of spec. It was needed to get to compile. Bug in compiler?
-		protected CustomLineCap() {
-		}
-		public CustomLineCap(GraphicsPath fillPAth, GraphicsPath strokePath, LineCap baseCap, float baseInset) {
-			this.baseInset = baseInset;
-		}
-		public CustomLineCap(GraphicsPath fillPAth, GraphicsPath strokePAth, LineCap baseCap) {
-			this.baseCap = baseCap;
-		}
-		public CustomLineCap(GraphicsPath fillPAth, GraphicsPath strokePAth) {
+
+		internal CustomLineCap () { }
+
+		internal CustomLineCap (IntPtr ptr)
+		{
+			nativeObject = ptr;
 		}
 
-		public LineCap BaseCap{
+		public CustomLineCap (GraphicsPath fillPath, GraphicsPath strokePath) : this (fillPath, strokePath, LineCap.Flat, 0)
+		{
+		}
+
+		public CustomLineCap (GraphicsPath fillPath, GraphicsPath strokePath, LineCap baseCap) : this (fillPath, strokePath, baseCap, 0)
+		{
+		}
+
+		public CustomLineCap(GraphicsPath fillPath, GraphicsPath strokePath, LineCap baseCap, float baseInset)
+		{
+			Status status = GDIPlus.GdipCreateCustomLineCap (fillPath.nativePath, strokePath.nativePath, baseCap, baseInset, out nativeObject);
+			GDIPlus.CheckStatus (status);
+		}
+
+		public LineCap BaseCap {
 			get {
+				LineCap baseCap;
+				Status status = GDIPlus.GdipGetCustomLineCapBaseCap (nativeObject, out baseCap);
+				GDIPlus.CheckStatus (status);
+
 				return baseCap;
 			}
+
 			set {
-				baseCap = value;
+				Status status = GDIPlus.GdipSetCustomLineCapBaseCap (nativeObject, value);
+				GDIPlus.CheckStatus (status);
 			}
 		}
-		public LineJoin StrokeJoin{
+
+		public LineJoin StrokeJoin {
 			get {
+				LineJoin strokeJoin;
+				Status status = GDIPlus.GdipGetCustomLineCapStrokeJoin (nativeObject, out strokeJoin);
+				GDIPlus.CheckStatus (status);
+
 				return strokeJoin;
 			}
+
 			set {
-				strokeJoin = value;
+				Status status = GDIPlus.GdipSetCustomLineCapStrokeJoin (nativeObject, value);
+				GDIPlus.CheckStatus (status);
 			}
 		}
-		public float BaseInset{
+
+		public float BaseInset {
 			get {
+				float baseInset;
+				Status status = GDIPlus.GdipGetCustomLineCapBaseInset (nativeObject, out baseInset);
+				GDIPlus.CheckStatus (status);
+
 				return baseInset;
 			}
+
 			set {
-				baseInset = value;
+				Status status = GDIPlus.GdipSetCustomLineCapBaseInset (nativeObject, value);
+				GDIPlus.CheckStatus (status);
 			}
 		}
 
-		public float WidthScale{
+		public float WidthScale {
 			get {
+				float widthScale;
+				Status status = GDIPlus.GdipGetCustomLineCapWidthScale (nativeObject, out widthScale);
+				GDIPlus.CheckStatus (status);
+
 				return widthScale;
 			}
+
 			set {
-				widthScale = value;
+				Status status = GDIPlus.GdipSetCustomLineCapWidthScale (nativeObject, value);
+				GDIPlus.CheckStatus (status);
 			}
 		}
 
-		//Public Methods
+		// Public Methods
 
-		// Implment IConeable.Clone
-		public object Clone(){
-			//FIXME
-			return new CustomLineCap();
+		public virtual object Clone ()
+		{
+			IntPtr clonePtr;
+			Status status = GDIPlus.GdipCloneCustomLineCap (nativeObject, out clonePtr);
+			GDIPlus.CheckStatus (status);
+
+			return new CustomLineCap (clonePtr);
 		}
 		
-		public virtual void Dispose(){
-			Dispose(true);
-		}
-		public virtual void Dispose(bool disposing){
-		}
-		
-		public void GetStrokeCaps(out LineCap startCap, out LineCap endCap){
-			startCap = baseCap;
-			endCap = baseCap;
+		public virtual void Dispose ()
+		{
+			Dispose (true);
 		}
 
-		public void SetStrokeCaps(LineCap startCap, LineCap endCap){
+		protected virtual void Dispose (bool disposing)
+		{
+			if (! disposed) {
+				Status status = GDIPlus.GdipDeleteCustomLineCap (nativeObject);
+				GDIPlus.CheckStatus (status);
+				disposed = true;
+				nativeObject = IntPtr.Zero;
+			}
+		}
+		
+		~CustomLineCap ()
+		{
+			Dispose (false);
+		}
+
+		public void GetStrokeCaps (out LineCap startCap, out LineCap endCap)
+		{
+			Status status = GDIPlus.GdipGetCustomLineCapStrokeCaps (nativeObject, out startCap, out endCap);
+			GDIPlus.CheckStatus (status);
+		}
+
+		public void SetStrokeCaps(LineCap startCap, LineCap endCap)
+		{
+			Status status = GDIPlus.GdipSetCustomLineCapStrokeCaps (nativeObject, startCap, endCap);
+			GDIPlus.CheckStatus (status);
 		}
 	}
 }
