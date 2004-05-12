e21949fa8e09baa2d2ae7d476310a353c95e2922
Zoltan Varga <vargaz@gmail.com>
2004-05-12 16:00:10 +0000

    2004-05-12  Zoltan Varga  <vargaz@freemail.hu>
    	* CompressedStack.cs: New file.
    
    svn path=/trunk/mcs/; revision=27173

---
+++ mcs/class/corlib/System.Threading/CompressedStack.cs
@@ -0,0 +1,14 @@
+//
+// System.Threading.Thread.cs
+//
+// Author:
+//   Zoltan Varga (vargaz@freemail.hu)
+//
+// (C) 2004 Novell (http://www.novell.com)
+//
+
+namespace System.Threading
+{
+	public class CompressedStack {
+	}
+}		
