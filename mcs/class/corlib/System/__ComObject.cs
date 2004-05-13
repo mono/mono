2384356529af1271f95a3bc9773df131f8e2ea91
Sebastien Pouliot <sebastien@ximian.com>
2004-05-13 19:02:30 +0000

    2004-05-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* __ComObject.cs: To please corcompare (no implementation).
    
    svn path=/trunk/mcs/; revision=27303

---
+++ mcs/class/corlib/System/__ComObject.cs
@@ -0,0 +1,19 @@
+//
+// System.__ComObject
+//
+// Author:
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2004 Novell (http://www.novell.com)
+//
+
+namespace System {
+
+	[MonoTODO ("Is there someone in there ?")]
+	public class __ComObject : MarshalByRefObject {
+	
+		private __ComObject ()
+		{
+		}
+	}
+}
