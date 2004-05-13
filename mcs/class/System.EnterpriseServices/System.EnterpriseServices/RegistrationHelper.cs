724a9774efc312139e378e318e1c14305271d7ca
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2004-05-13 00:32:08 +0000

    2004-05-13  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* RegistrationHelper.cs: fixed the build on windows. I will file a bug
    	report on mcs for this.
    
    svn path=/trunk/mcs/; revision=27204

--- mcs/class/System.EnterpriseServices/System.EnterpriseServices/RegistrationHelper.cs
+++ mcs/class/System.EnterpriseServices/System.EnterpriseServices/RegistrationHelper.cs
@@ -24,7 +24,7 @@ namespace System.EnterpriseServices {
 
 		#region Methods
 
-		public void InstallAssembly (string assembly, out string application, out string tlb, InstallationFlags installFlags)
+		public void InstallAssembly (string assembly, ref string application, ref string tlb, InstallationFlags installFlags)
 		{
 			application = String.Empty;
 			tlb = String.Empty;
