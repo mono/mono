fd587ba8f7f2683e804cfe6293b4c6dae79614cb
Marek Safar <marek.safar@gmail.com>
2004-05-14 08:42:05 +0000

    2004-05-14 Marek Safar <marek.safar@seznam.cz>
    	* cs3008-7.cs: New test for bug #58478.
    
    svn path=/trunk/mcs/; revision=27352

---
+++ mcs/errors/cs3008-7.cs
@@ -0,0 +1,13 @@
+// cs3008.cs: Identifier 'System.Error.__ComObject' is not CLS-compliant
+// Line: 8
+
+using System;
+[assembly:CLSCompliant(true)]
+
+namespace System.Error {
+	public class __ComObject : MarshalByRefObject {
+		private __ComObject ()
+		{
+		}
+	}
+}
