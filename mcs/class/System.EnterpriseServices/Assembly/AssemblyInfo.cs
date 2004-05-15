3496b6439adc185ff069b0c75f02ac90400f15bb
Gert Driesen <drieseng@users.sourceforge.net>
2004-05-15 15:19:42 +0000

    * AssemblyInfo.cs: fixed assembly attributes to match MS frameworks
    svn path=/trunk/mcs/; revision=27450

--- mcs/class/System.EnterpriseServices/Assembly/AssemblyInfo.cs
+++ mcs/class/System.EnterpriseServices/Assembly/AssemblyInfo.cs
@@ -12,13 +12,24 @@ using System.Resources;
 using System.Security;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.EnterpriseServices;
 
 #if (NET_1_0)
 	[assembly: AssemblyVersion("1.0.3300.0")]
 	[assembly: SatelliteContractVersion("1.0.3300.0")]
 #elif (NET_2_0)
-        [assembly: AssemblyVersion("2.0.3600.0")]
+	[assembly: AssemblyVersion("2.0.3600.0")]
 	[assembly: SatelliteContractVersion("2.0.3600.0")]
+	[assembly: ComVisible(true)]
+	[assembly: AssemblyTitle("System.EnterpriseServices.dll")]
+	[assembly: AssemblyDescription("System.EnterpriseServices.dll")]
+	[assembly: AssemblyConfiguration("Development version")]
+	[assembly: AssemblyCompany("MONO development team")]
+	[assembly: AssemblyProduct("MONO CLI")]
+	[assembly: AssemblyCopyright("(c) 2004 Various Authors")]
+	[assembly: AssemblyTrademark("")]
+	[assembly: AssemblyInformationalVersion("2.0.40301.9")]
+	[assembly: AssemblyFileVersion("2.0.40301.9")]
 #elif (NET_1_1)
 	[assembly: AssemblyVersion("1.0.5000.0")]
 	[assembly: SatelliteContractVersion("1.0.5000.0")]
@@ -26,22 +37,12 @@ using System.Runtime.InteropServices;
 	[assembly: TypeLibVersion(1, 10)]
 #endif
 
-[assembly: AssemblyTitle("System.EnterpriseServices.dll")]
-[assembly: AssemblyDescription("System.EnterpriseServices.dll")]
-[assembly: AssemblyConfiguration("Development version")]
-[assembly: AssemblyCompany("MONO development team")]
-[assembly: AssemblyProduct("MONO CLI")]
-[assembly: AssemblyCopyright("(c) 2004 Various Authors")]
-[assembly: AssemblyTrademark("")]
-
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyDefaultAlias("System.EnterpriseServices.dll")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: ComVisible(false)]
+[assembly: ApplicationID("1e246775-2281-484f-8ad4-044c15b86eb7")]
+[assembly: ApplicationName(".NET Utilities")]
 [assembly: Guid("4fb2d46f-efc8-4643-bcd0-6e5bfa6a174c")]
-[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: AssemblyDelaySign(true)]
 [assembly: AssemblyKeyFile("../msfinal.pub")]
