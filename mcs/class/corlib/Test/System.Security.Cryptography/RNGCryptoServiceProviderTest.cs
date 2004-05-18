be18a0f207fcda9647fbb707bcc85efd78e97c7f
Sebastien Pouliot <sebastien@ximian.com>
2004-05-18 01:46:19 +0000

    2004-05-17  Sebastien Pouliot  <sebastien@ximian.com>
    	* RNGCryptoServiceProviderTest.cs: Added new tests to see how the
    	constructor behave with null seeds or if they modify the seed.
    
    svn path=/trunk/mcs/; revision=27553

--- mcs/class/corlib/Test/System.Security.Cryptography/RNGCryptoServiceProviderTest.cs
+++ mcs/class/corlib/Test/System.Security.Cryptography/RNGCryptoServiceProviderTest.cs
@@ -1,8 +1,11 @@
 //
 // TestSuite.System.Security.Cryptography.RNGCryptoServiceProviderTest.cs
 //
-// Author:
+// Authors:
 //      Mark Crichton (crichton@gimp.org)
+//	Sebastien Pouliot  (sebastien@ximian.com)
+//
+// Copyright (C) 2004 Novell (http://www.novell.com)
 //
 
 using System;
@@ -21,6 +24,44 @@ namespace MonoTests.System.Security.Cryptography {
 		private void SetUp () 
 		{
 			_algo = new RNGCryptoServiceProvider ();
+		}
+
+		[Test]
+		public void ConstructorByteArray () 
+		{
+			byte[] array = new byte [16];
+			byte[] seed = (byte[]) array.Clone ();
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider (seed);
+			AssertEquals ("Seed", BitConverter.ToString (array), BitConverter.ToString (seed));
+		}
+
+		[Test]
+		public void ConstructorByteArray_Null () 
+		{
+			byte[] array = null;
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider (array);
+		}
+
+		[Test]
+		public void ConstructorCsp_Null () 
+		{
+			CspParameters csp = null;
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider (csp);
+		}
+
+		[Test]
+		public void ConstructorString () 
+		{
+			string s = "Mono seed";
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider (s);
+			AssertEquals ("Seed", "Mono seed", s);
+		}
+
+		[Test]
+		public void ConstructorString_Null () 
+		{
+			string s = null;
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider (s);
 		}
 
 		[Test]
