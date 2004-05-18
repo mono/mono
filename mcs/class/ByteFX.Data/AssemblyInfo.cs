1da941cf42841578175bb79c09e6ebefc1a05a9d
Duncan Mak <duncan@mono-cvs.ximian.com>
2004-05-18 03:45:54 +0000

    * Makefile: Add reference to Mono.Posix.dll.
    * ByteFX.Data.dll.sources: Update to latest listing of source
    files.
    
    * AssemblyInfo.cs: Revert back to the previous version, because we
    don't have bytefx.snk.
    
    * mysqlclient/transcaction.cs: Removed, replaced by the new
    transaction.cs.
    
    svn path=/trunk/mcs/; revision=27557

--- mcs/class/ByteFX.Data/AssemblyInfo.cs
+++ mcs/class/ByteFX.Data/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("ByteFX.MySqlClient.dll")]
-[assembly: AssemblyDescription("ADO.Net driver for MySql")]
+[assembly: AssemblyTitle("ByteFX.Data.dll")]
+[assembly: AssemblyDescription("ADO.Net drivers for MySql & PostgreSQL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ByteFX, Inc.")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright 2002-2004, ByteFX, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2002-2003, ByteFX, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -26,35 +26,13 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.7.6.*")]
-
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
-//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-[assembly: AssemblyDelaySign(false)]
-#if WINDOWS || !DEBUG
-[assembly: AssemblyKeyFile(@"d:\projects\ByteFX\bytefx.snk")]
+#if (NET_2_0)
+	[assembly: AssemblyVersion ("0.7.4.2")]
+#elif (NET_1_1)
+	[assembly: AssemblyVersion ("0.7.4.1")]
+#else
+	[assembly: AssemblyVersion ("0.7.4.0")]
 #endif
-[assembly: AssemblyKeyName("")]
+
+[assembly: AssemblyDelaySign (true)]
+[assembly: AssemblyKeyFile ("../mono.pub")]
