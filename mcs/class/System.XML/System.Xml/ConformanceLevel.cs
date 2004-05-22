4a8aca0670c43b4f0100a3ca6cd063409ae36014
Atsushi Eno <atsushieno@gmail.com>
2004-05-22 19:11:34 +0000

    2004-05-22  Atsushi Enomoto <atsushi@ximian.com>
    	* ConformanceLevel.cs : added.
    
    svn path=/trunk/mcs/; revision=27900

---
+++ mcs/class/System.XML/System.Xml/ConformanceLevel.cs
@@ -0,0 +1,17 @@
+//
+// ConformanceLevel.cs
+//
+// Author:
+//	Atsushi Enomoto <atsushi@ximian.com>
+//
+#if NET_2_0
+namespace System.Xml
+{
+	public enum ConformanceLevel
+	{
+		Auto,
+		Document,
+		Fragment
+	}
+}
+#endif
