46972b8ca16446c90e791dbdc5dc69fee0acaac0
Martin Baulig <martin@novell.com>
2004-05-23 19:10:51 +0000

    New test.
    svn path=/trunk/mcs/; revision=27932

---
+++ mcs/errors/cs0246-8.cs
@@ -0,0 +1,9 @@
+// CS0246: Cannot find type `Reflection.ConstructorInfo'
+// Line: 7
+using System;
+
+class foo {
+    static void Main(string[] args ) {    
+        Reflection.ConstructorInfo ci;        
+    }
+} 
