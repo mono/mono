90224d7d1fb0f5296acf989d14e6f51245295589
Martin Baulig <martin@novell.com>
2004-05-24 16:35:10 +0000

    2004-05-24  Martin Baulig  <martin@ximian.com>
    	* test-259.cs: New test for #55251.
    
    svn path=/trunk/mcs/; revision=27980

---
+++ mcs/tests/test-259.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Testing
+{
+	public enum INT : int { Zero }
+	public const INT JPEG_SUSPENDED = (INT)0;
+	public const INT JPEG_HEADER_OK = (INT)1;
+ 
+	public static void Main()
+	{ }
+}
