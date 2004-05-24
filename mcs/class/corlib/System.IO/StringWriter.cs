2740d6eb84d4cb4c54055f32805cc0a264a8ccfc
Sebastien Pouliot <sebastien@ximian.com>
2004-05-24 18:24:29 +0000

    2004-05-24  Sebastien Pouliot  <sebastien@ximian.com>
    	* StringWriter.cs: Fixed possible integer overflow in Write. Changed
    	spaces for tabs.
    
    svn path=/trunk/mcs/; revision=27986

--- mcs/class/corlib/System.IO/StringWriter.cs
+++ mcs/class/corlib/System.IO/StringWriter.cs
@@ -1,47 +1,56 @@
 //
 // System.IO.StringWriter
 //
-// Author: Marcin Szczepanski (marcins@zipworld.com.au)
+// Authors
+//	Marcin Szczepanski (marcins@zipworld.com.au)
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2004 Novell (http://www.novell.com)
 //
 
+using System.Globalization;
 using System.Text;
 
 namespace System.IO {
-		[Serializable]
+
+	[Serializable]
         public class StringWriter : TextWriter {
                 
                 private StringBuilder internalString;
 		private bool disposed = false;
 
-                public StringWriter() : this (new StringBuilder ()) {
-			
-                }
+                public StringWriter () : this (new StringBuilder ())
+		{
+		}
 
-                public StringWriter( IFormatProvider formatProvider ) : this (new StringBuilder (), formatProvider)  {
+                public StringWriter (IFormatProvider formatProvider) :
+			this (new StringBuilder (), formatProvider)
+		{
+		}
 
-                }
+                public StringWriter (StringBuilder sb) :
+			this (sb, null)
+		{
+		}
 
-                public StringWriter( StringBuilder sb ) : this (sb, null) {
-
-                }
-
-                public StringWriter( StringBuilder sb, IFormatProvider formatProvider ) {
-			
+                public StringWriter (StringBuilder sb, IFormatProvider formatProvider)
+		{
 			if (sb == null)
-				throw new ArgumentNullException ();
+				throw new ArgumentNullException ("sb");
 
-                        internalString = sb;
-                        internalFormatProvider = formatProvider;
-                }
+			internalString = sb;
+			internalFormatProvider = formatProvider;
+		}
 
                 public override System.Text.Encoding Encoding {
                         get {
                                 return System.Text.Encoding.Unicode;
                         }
-                }
+		}
 
-                public override void Close() {
-                        Dispose( true );
+                public override void Close ()
+		{
+			Dispose (true);
 			disposed = true;
                 }
 
@@ -53,51 +62,54 @@ namespace System.IO {
 			disposed = true;
 		}
 
-                public virtual StringBuilder GetStringBuilder() {
-                        return internalString;
+                public virtual StringBuilder GetStringBuilder ()
+		{
+			return internalString;
+		}
+
+                public override string ToString ()
+		{
+			return internalString.ToString ();
                 }
 
-                public override string ToString() {
-                        return internalString.ToString();
-                }
+                public override void Write (char value)
+		{
+			if (disposed) {
+				throw new ObjectDisposedException ("StringReader", 
+					Locale.GetText ("Cannot write to a closed StringWriter"));
+			}
 
-                public override void Write( char value ) {
+                        internalString.Append (value);
+		}
 
-			if (disposed) 
-				throw new ObjectDisposedException ("StringWriter", "Cannot write to a closed StringWriter");
+                public override void Write (string value)
+		{
+			if (disposed) {
+				throw new ObjectDisposedException ("StringReader", 
+					Locale.GetText ("Cannot write to a closed StringWriter"));
+			}
 
-                        internalString.Append( value );
-                }
+			internalString.Append (value);
+		}
 
-                public override void Write( string value ) {
+                public override void Write (char[] buffer, int index, int count)
+		{
+			if (disposed) {
+				throw new ObjectDisposedException ("StringReader", 
+					Locale.GetText ("Cannot write to a closed StringWriter"));
+			}
+			if (buffer == null)
+				throw new ArgumentNullException ("buffer");
+			if (index < 0)
+				throw new ArgumentOutOfRangeException ("index", "< 0");
+			if (count < 0)
+				throw new ArgumentOutOfRangeException ("count", "< 0");
+			// re-ordered to avoid possible integer overflow
+                        if (index > buffer.Length - count)
+				throw new ArgumentException ("index + count > buffer.Length");
 
-			if (disposed) 
-				throw new ObjectDisposedException ("StringWriter", "Cannot write to a closed StringWriter");
-
-                        internalString.Append( value );
-                }
-
-                public override void Write( char[] buffer, int index, int count ) {
-
-			if (disposed) 
-				throw new ObjectDisposedException ("StringReader", "Cannot write to a closed StringWriter");
-
-                        if( buffer == null ) {
-                                throw new ArgumentNullException();
-                        } else if( index < 0 || count < 0 ) {
-                                throw new ArgumentOutOfRangeException();
-                        } else if( index > buffer.Length || index + count > buffer.Length ) {
-                                throw new ArgumentException();
-                        }
-                        
-                        char[] writeBuffer = new char[ count ];
-
-                        Array.Copy( buffer, index, writeBuffer, 0, count );
-
-                        internalString.Append( writeBuffer );
-                }
-                          
-        }
+			internalString.Append (buffer, index, count);
+		}
+	}
 }
-                        
-                        
+              
