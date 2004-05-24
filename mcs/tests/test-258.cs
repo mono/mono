8b049ae6041f36849e0f89fecde8eb6f8cd5541d
Martin Baulig <martin@novell.com>
2004-05-24 16:17:38 +0000

    2004-05-24  Martin Baulig  <martin@ximian.com>
    	* test-258.cs: New test for #58584.
    
    svn path=/trunk/mcs/; revision=27978

---
+++ mcs/tests/test-258.cs
@@ -0,0 +1,24 @@
+using System;
+
+// Without namespace, this error does not happen.
+namespace MyTest
+{
+        public class Test
+        {
+                public interface Inner
+                {
+                        void Foo ();
+                }
+        }
+
+        public class Test2 : MarshalByRefObject, Test.Inner
+        {
+                // This is OK: public void Foo ()
+                void Test.Inner.Foo ()
+                {
+                }
+
+		static void Main ()
+		{ }
+        }
+}
