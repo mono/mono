8a6749f11e18dd855410f6d3d2b4e424a8f1a27d
Marek Safar <marek.safar@gmail.com>
2004-05-25 08:35:08 +0000

    New tests
    svn path=/trunk/mcs/; revision=28027

---
+++ mcs/errors/cs0626-4.cs
@@ -0,0 +1,8 @@
+// cs0626.cs: Method, operator, or accessor 'ExternClass.ExternMethod()' is marked external and has no attributes on it. Consider adding a DllImport attribute to specify the external implementation
+// Line: 6
+// Compiler options: -warnaserror -warn:1
+
+class ExternClass {
+        [System.Obsolete]
+        public static extern void ExternMethod();
+}
