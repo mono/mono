27c19dba877ae453e53d7c3b8b3a1f849402bd48
César Natarén <cesar@mono-cvs.ximian.com>
2004-05-25 00:23:59 +0000

    2004-05-24  Cesar Lopez Nataren  <cesar@ciencias.unam.mx> 	* Added switch.js
    svn path=/trunk/mcs/; revision=28014

---
+++ mcs/jtests/switch.js
@@ -0,0 +1,16 @@
+var x = "b";
+
+switch (x) {
+case "hola":
+    print ("matched hola");
+    print ("Oh i rock");
+case "a":
+    print ("matched a");
+case "b":
+    print ("matched b");
+case 1:
+    print ("matched 1");
+default:
+    print ("blah this is default");
+    print ("this is default too ;-)");
+}
