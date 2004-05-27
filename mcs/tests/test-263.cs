246078296014ace72a87c4fa3094e6d1e77c529e
Marek Safar <marek.safar@gmail.com>
2004-05-27 19:16:56 +0000

    2004-05-27  Marek Safar <marek.safar@seznam.cz>
    	* test-263.cs: New test.
    
    svn path=/trunk/mcs/; revision=28304

---
+++ mcs/tests/test-263.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Test {
+	public String GetString (byte[] bytes)
+	{
+                unsafe {
+                    int* i;
+                }
+                
+		unsafe {
+			fixed (byte *ss = &bytes [0]) {
+                                int* i;
+				return new String ((sbyte*)ss, 0, bytes.Length);
+			}
+		}
+	}
+        
+        public static void Main () {}
+}
