78693a6be5ea1add04dad74bd2b2d475d9fa60fc
Ravindra <ravindra@mono-cvs.ximian.com>
2004-05-27 07:20:18 +0000

    Temporarily ignoring a failing test case.
    svn path=/trunk/mcs/; revision=28231

--- mcs/class/System.Drawing/Test/System.Drawing/TestImageConverter.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/TestImageConverter.cs
@@ -406,6 +406,7 @@ namespace MonoTests.System.Drawing
 		}
 
 		[Test]
+		[Ignore ("This test fails because of bug #58435")]
 		public void TestGetProperties ()
 		{
 			PropertyDescriptorCollection propsColl;
