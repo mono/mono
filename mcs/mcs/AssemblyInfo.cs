749975515bf3743138836a1e9d57db06113a59c0
Miguel de Icaza <miguel@gnome.org>
2004-05-31 18:22:28 +0000

    version
    svn path=/trunk/mcs/; revision=28574

--- mcs/mcs/AssemblyInfo.cs
+++ mcs/mcs/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.94")]
+[assembly: AssemblyVersion("")]
 [assembly: AssemblyTitle ("Mono C# Compiler")]
 [assembly: AssemblyDescription ("Mono C# Compiler")]
 [assembly: AssemblyCopyright ("2001, 2002, 2003 Ximian, Inc.")]
