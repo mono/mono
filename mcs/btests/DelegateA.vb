99bc5750c0b2c29dfbf54fbcf626436f2e711109
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-06-01 06:12:41 +0000

    test cases for Delegate
    svn path=/trunk/mcs/; revision=28612

---
+++ mcs/btests/DelegateA.vb
@@ -0,0 +1,36 @@
+Imports System
+
+
+
+Module M
+	Delegate Sub SD()
+	sub f()
+	End sub
+
+	Public Delegate Function SF(a as integer) as Integer
+	Public Function f1(a as integer) as Integer
+		return a
+	End Function
+
+	Sub Main()
+		dim d1 as SD 
+		d1 = new SD(AddressOf f)
+		d1.Invoke()
+
+		'dim d2 as SD
+		'd2 = AddressOf f  ''//this invocation is not functional yet
+		'd2.Invoke()
+
+		dim d3 as new SD(AddressOf f)
+		d3.Invoke()
+
+		dim d4 as SF
+		d4 = new SF(AddressOf f1)
+		Dim i as Integer = d4.Invoke(10)
+		if i <> 10 then
+			Throw new System.Exception ("#A1, Unexpected result")
+		end if
+
+		
+	End Sub
+End Module
