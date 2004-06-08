d2d6aae795324713413536cea38f9909e3f65ef1
Martin Baulig <martin@novell.com>
2004-06-08 02:26:41 +0000

    Incremented version number.
    svn path=/trunk/mcs/; revision=29001

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -32,7 +32,7 @@ namespace System
 		 * Changes which are already detected at runtime, like the addition
 		 * of icalls, do not require an increment.
 		 */
-		private const int mono_corlib_version = 21;
+		private const int mono_corlib_version = 22;
 
 		private Environment ()
 		{
