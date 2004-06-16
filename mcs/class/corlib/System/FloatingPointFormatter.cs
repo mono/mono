4a5457ed457efd92ffd51de92975fefa9517f19f
Sebastien Pouliot <sebastien@ximian.com>
2004-06-16 18:12:24 +0000

    2004-06-16  Sebastien Pouliot  <sebastien@ximian.com>
    	* FloatingPointFormatter.cs: Implemented "R" format using Jon Skeet
    	source code (with permission). Fix (biggest) part of bug #60110.
    	http://www.yoda.arachsys.com/csharp/floatingpoint.html
    
    svn path=/trunk/mcs/; revision=29704

--- mcs/class/corlib/System/FloatingPointFormatter.cs
+++ mcs/class/corlib/System/FloatingPointFormatter.cs
@@ -1,10 +1,13 @@
 //
 // System.FloatingPointFormatter.cs
 //
-// Author:
-//   Pedro Martinez Julia <yoros@wanadoo.es>
+// Authors:
+//	Pedro Martinez Julia <yoros@wanadoo.es>
+//	Jon Skeet <skeet@pobox.com>
+//	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // Copyright (C) 2003 Pedro Martíez Juliá <yoros@wanadoo.es>
+// Copyright (C) 2004 Jon Skeet
 // Copyright (C) 2004 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -107,7 +110,7 @@ namespace System {
 			case 'P':
 				return FormatPercent (formatData, value, nfi, precision);
 			case 'R':
-				return FormatReversible (formatData, value, nfi, precision);
+				return FormatReversible (value, nfi, precision);
 			case 'X':
 				throw new FormatException(Locale.GetText(
 					"The specified format is invalid") + ": " + format);
@@ -603,11 +606,65 @@ namespace System {
 			return sb.ToString();
 		}
 
+		// from http://www.yoda.arachsys.com/csharp/floatingpoint.html
+		// used with permission from original author
+		private string FormatReversible (double value, NumberFormatInfo nfi, int precision)
+		{
+			// Translate the double into sign, exponent and mantissa.
+			long bits = BitConverter.DoubleToInt64Bits (value);
+			bool negative = (bits >> 63)==1;
+			int exponent = (int) ((bits >> 52) & 0x7ffL);
+			long mantissa = bits & 0xfffffffffffffL;
 
-		[MonoTODO]
-		private string FormatReversible (Format formatData, double value, NumberFormatInfo nfi,
-				int precision) {
-			return FormatGeneral (formatData, value, nfi, precision);
+			// Subnormal numbers; exponent is effectively one higher,
+			// but there's no extra normalisation bit in the mantissa
+			if (exponent == 0) {
+				exponent++;
+			}
+			// Normal numbers; leave exponent as it is but add extra
+			// bit to the front of the mantissa
+			else {
+				mantissa = mantissa | (1L<<52);
+			}
+        
+			// Bias the exponent. It's actually biased by 1023, but we're
+			// treating the mantissa as m.0 rather than 0.m, so we need
+			// to subtract another 52 from it.
+			exponent -= 1075;
+        
+			if (mantissa == 0) {
+				return "0";
+			}
+        
+			// Normalize
+			while((mantissa & 1) == 0) {
+				//  i.e., Mantissa is even
+				mantissa >>= 1;
+				exponent++;
+			}
+        
+			// Construct a new decimal expansion with the mantissa
+        		ArbitraryDecimal ad = new ArbitraryDecimal (mantissa);
+        
+			// If the exponent is less than 0, we need to repeatedly
+			// divide by 2 - which is the equivalent of multiplying
+			// by 5 and dividing by 10.
+			if (exponent < 0) {
+				for (int i=0; i < -exponent; i++)
+					ad.MultiplyBy (5);
+				ad.Shift (-exponent);
+			}
+			// Otherwise, we need to repeatedly multiply by 2
+			else {
+				for (int i=0; i < exponent; i++)
+					ad.MultiplyBy(2);
+			}
+        
+			// Finally, return the string with an appropriate sign
+			if (negative)
+				return nfi.NegativeSign + ad.ToString (nfi);
+			else
+				return ad.ToString (nfi);
 		}
 
 		private string FormatCustom (Format formatData, double value,
@@ -982,4 +1039,122 @@ namespace System {
 
 	}
 
+	// from http://www.yoda.arachsys.com/csharp/floatingpoint.html
+	// used with permission from original author
+	internal class ArbitraryDecimal {
+		/// <summary>Digits in the decimal expansion, one byte per digit
+		byte[] digits;
+		/// <summary> 
+		/// How many digits are *after* the decimal point
+		/// </summary>
+		int decimalPoint=0;
+
+		/// <summary> 
+		/// Constructs an arbitrary decimal expansion from the given long.
+		/// The long must not be negative.
+		/// </summary>
+		internal ArbitraryDecimal (long x)
+		{
+			string tmp = x.ToString (CultureInfo.InvariantCulture);
+			digits = new byte [tmp.Length];
+			for (int i=0; i < tmp.Length; i++)
+				digits[i] = (byte) (tmp[i] - '0');
+			Normalize ();
+		}
+        
+		/// <summary>
+		/// Multiplies the current expansion by the given amount, which should
+		/// only be 2 or 5.
+		/// </summary>
+		internal void MultiplyBy (int amount)
+		{
+			byte[] result = new byte [digits.Length+1];
+			for (int i=digits.Length-1; i >= 0; i--) {
+				int resultDigit = digits [i] * amount + result [i+1];
+				result [i] = (byte)(resultDigit / 10);
+				result [i+1] = (byte)(resultDigit % 10);
+			}
+			if (result [0] != 0) {
+				digits = result;
+			}
+			else {
+				Array.Copy (result, 1, digits, 0, digits.Length);
+			}
+			Normalize ();
+		}
+        
+		/// <summary>
+		/// Shifts the decimal point; a negative value makes
+		/// the decimal expansion bigger (as fewer digits come after the
+		/// decimal place) and a positive value makes the decimal
+		/// expansion smaller.
+		/// </summary>
+		internal void Shift (int amount)
+		{
+			decimalPoint += amount;
+		}
+
+		/// <summary>
+		/// Removes leading/trailing zeroes from the expansion.
+		/// </summary>
+		internal void Normalize ()
+		{
+			int first;
+			for (first=0; first < digits.Length; first++) {
+				if (digits [first] != 0)
+					break;
+			}
+
+			int last;
+			for (last = digits.Length - 1; last >= 0; last--) {
+				if (digits [last] != 0)
+					break;
+			}
+            
+			if ((first == 0) && (last == digits.Length - 1))
+				return;
+            
+			byte[] tmp = new byte [last-first+1];
+			for (int i=0; i < tmp.Length; i++)
+				tmp [i] = digits [i + first];
+            
+			decimalPoint -= digits.Length - (last + 1);
+			digits = tmp;
+		}
+
+		/// <summary>
+		/// Converts the value to a proper decimal string representation.
+		/// </summary>
+		public string ToString (NumberFormatInfo nfi)
+		{
+			char[] digitString = new char [digits.Length];            
+			for (int i=0; i < digits.Length; i++)
+				digitString [i] = (char)(digits [i] + '0');
+            
+			// Simplest case - nothing after the decimal point,
+			// and last real digit is non-zero, eg value=35
+			if (decimalPoint == 0) {
+				return new string (digitString);
+			}
+            
+			// Fairly simple case - nothing after the decimal
+			// point, but some 0s to add, eg value=350
+			if (decimalPoint < 0) {
+				return new string (digitString) + new string ('0', -decimalPoint);
+			}
+            
+			// Nothing before the decimal point, eg 0.035
+			if (decimalPoint >= digitString.Length) {
+				return "0" + nfi.NumberDecimalSeparator + 
+					new string ('0',(decimalPoint-digitString.Length))+ new string (digitString);
+			}
+
+			// Most complicated case - part of the string comes
+			// before the decimal point, part comes after it,
+			// eg 3.5
+			return new string (digitString, 0, digitString.Length - decimalPoint) +
+				nfi.NumberDecimalSeparator + 
+				new string (digitString, digitString.Length - decimalPoint, decimalPoint);
+		}
+	}
 }
