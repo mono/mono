ce459e6e2a151e4c45b67bccadc5a63b1bbbece3
Sachin Kumar <sachin@mono-cvs.ximian.com>
2004-06-16 12:52:47 +0000

    Java code converted from Mainsoft
    svn path=/trunk/mcs/; revision=29683

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/SpcInfo.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/SpcInfo.cs
@@ -6,15 +6,63 @@
 //
 // (C) 2002 Chris J Breisch
 //
+
+using System.ComponentModel;
+
 namespace Microsoft.VisualBasic {
-	[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)] 
-	[MonoTODO]
+	[EditorBrowsableAttribute(EditorBrowsableState.Never)] 
 	public struct SpcInfo {
 		// Declarations
-		public System.Int16 Count;
+		public short Count;
+	
 		// Constructors
-		// Properties
-		// Methods
-		// Events
+		public SpcInfo(short value) { Count = value ; }
+		
+		// methods
+		public void __Ctor__(short value)
+    		{
+        		Count = value;
+    		}
+     		
+		// clones the current instance.
+		public SpcInfo __Clone__()
+    		{
+        		return new SpcInfo(Count);
+    		}
+     		
+		// Initializes current instance to zero
+		public void __ZeroInit__()
+    		{
+        		Count = 0;
+    		}
+     
+    		// Copies the given value to current
+		public void __Copy__(SpcInfo other)
+    		{
+        		Count = other.Count;
+    		}
+
+		public bool equals(System.Object obj)
+    		{
+        		if (obj == null)
+            			return false;
+
+        		if (obj.Equals(this))
+            			return true;
+
+        		if (obj is SpcInfo)
+        		{
+            			SpcInfo int16 = (SpcInfo) obj;
+            			return Count == int16.Count;
+        		}
+
+        		return false;
+    		}
+
+    		public int hashCode()
+    		{
+        		return Count | (Count << 16);
+    		}
+	
 	};
 }
--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/TabInfo.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/TabInfo.cs
@@ -6,18 +6,65 @@
 //
 // (C) 2002 Chris J Breisch
 //
+
 using System.Runtime.InteropServices;
 using System.ComponentModel;
 
 namespace Microsoft.VisualBasic {
 	[EditorBrowsable(EditorBrowsableState.Never)] 
-	[MonoTODO]
-	public struct TabInfo {
+		public struct TabInfo {
 		// Declarations
-		public System.Int16 Column;
+		public short Column;
+
 		// Constructors
+
+		public TabInfo(short value) { Column = value ; }
 		// Properties
 		// Methods
-		// Events
+		public void __Ctor__(short value)
+    		{
+        		Column = value;
+    		}
+		
+		// clones current instance
+		public TabInfo __Clone__()
+    		{
+        		return new TabInfo(Column);
+    		}
+
+		// Initializes current instance to zero
+		public void __ZeroInit__()
+    		{
+		        Column = 0;
+	        }
+		
+		// Copies the given value to current
+		public void __Copy__(TabInfo other)
+		{
+        		Column = other.Column;
+    		}
+
+		public bool equals(System.Object obj)
+    		{
+        		if (obj == null)
+            			return false;
+
+		        if (obj.Equals(this))
+            			return true;
+
+		        if (obj is TabInfo)
+        		{
+		            TabInfo Int16 = (TabInfo) obj;
+		            return Column == Int16.Column;
+        		}
+
+		        return false;
+  		}
+
+		public int hashCode()
+    		{
+		        return Column | (Column << 16);
+	        }
+	
 	};
 }
