1bdec1beee6497b8b2f2184a122a0f529f606d73
Gert Driesen <drieseng@users.sourceforge.net>
2004-06-17 18:28:49 +0000

    * InvalidComObjectException.cs: marked serializable to match MS.NET * InvalidOleVarianTypeException.cs: marked serializable to match MS.NET
    svn path=/trunk/mcs/; revision=29800

--- mcs/class/corlib/System.Runtime.InteropServices/InvalidOleVariantTypeException.cs
+++ mcs/class/corlib/System.Runtime.InteropServices/InvalidOleVariantTypeException.cs
@@ -33,6 +33,7 @@ using System.Runtime.Serialization;
 
 namespace System.Runtime.InteropServices
 {
+	[Serializable]
 	public class InvalidOleVariantTypeException : SystemException
 	{
 		private const int ErrorCode = -2146233039; // = 0x80131531
