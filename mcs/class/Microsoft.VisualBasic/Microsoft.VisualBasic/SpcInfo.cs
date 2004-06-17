f9504dbf49f7a549154ab7433e0b4c0df001e4c0
Sachin Kumar <sachin@mono-cvs.ximian.com>
2004-06-17 11:51:54 +0000

    Made corrections as suggested by Hari
    svn path=/trunk/mcs/; revision=29762

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/SpcInfo.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/SpcInfo.cs
@@ -16,53 +16,6 @@ namespace Microsoft.VisualBasic {
 		public short Count;
 	
 		// Constructors
-		public SpcInfo(short value) { Count = value ; }
-		
 		// methods
-		public void __Ctor__(short value)
-    		{
-        		Count = value;
-    		}
-     		
-		// clones the current instance.
-		public SpcInfo __Clone__()
-    		{
-        		return new SpcInfo(Count);
-    		}
-     		
-		// Initializes current instance to zero
-		public void __ZeroInit__()
-    		{
-        		Count = 0;
-    		}
-     
-    		// Copies the given value to current
-		public void __Copy__(SpcInfo other)
-    		{
-        		Count = other.Count;
-    		}
-
-		public bool equals(System.Object obj)
-    		{
-        		if (obj == null)
-            			return false;
-
-        		if (obj.Equals(this))
-            			return true;
-
-        		if (obj is SpcInfo)
-        		{
-            			SpcInfo int16 = (SpcInfo) obj;
-            			return Count == int16.Count;
-        		}
-
-        		return false;
-    		}
-
-    		public int hashCode()
-    		{
-        		return Count | (Count << 16);
-    		}
-	
 	};
 }
--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/TabInfo.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/TabInfo.cs
@@ -12,59 +12,12 @@ using System.ComponentModel;
 
 namespace Microsoft.VisualBasic {
 	[EditorBrowsable(EditorBrowsableState.Never)] 
-		public struct TabInfo {
+	public struct TabInfo {
 		// Declarations
 		public short Column;
 
 		// Constructors
-
-		public TabInfo(short value) { Column = value ; }
 		// Properties
 		// Methods
-		public void __Ctor__(short value)
-    		{
-        		Column = value;
-    		}
-		
-		// clones current instance
-		public TabInfo __Clone__()
-    		{
-        		return new TabInfo(Column);
-    		}
-
-		// Initializes current instance to zero
-		public void __ZeroInit__()
-    		{
-		        Column = 0;
-	        }
-		
-		// Copies the given value to current
-		public void __Copy__(TabInfo other)
-		{
-        		Column = other.Column;
-    		}
-
-		public bool equals(System.Object obj)
-    		{
-        		if (obj == null)
-            			return false;
-
-		        if (obj.Equals(this))
-            			return true;
-
-		        if (obj is TabInfo)
-        		{
-		            TabInfo Int16 = (TabInfo) obj;
-		            return Column == Int16.Column;
-        		}
-
-		        return false;
-  		}
-
-		public int hashCode()
-    		{
-		        return Column | (Column << 16);
-	        }
-	
 	};
 }
