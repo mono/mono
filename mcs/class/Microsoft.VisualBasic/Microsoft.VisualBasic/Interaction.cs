16f6cb8223b6ebe3cfaaecb1f51d3b52477a1ead
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-06-18 06:08:10 +0000

    2004-06-18 Anirban Bhattacharjee <banirban@novell.com>         * Interaction.cs: Few methods implemented - Contribution by Jambunathan and Anirban
    svn path=/trunk/mcs/; revision=29850

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Interaction.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic/Interaction.cs
@@ -8,7 +8,14 @@
 // (C) 2002 Chris J Breisch
 // (C) 2004 Joerg Rosenkranz
 //
+
 using System;
+using System.Reflection;
+using System.Collections;
+//using Windows.Drawing;
+//using System.Windows.Forms;
+
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Microsoft.VisualBasic {
 	[Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute] 
@@ -36,58 +43,11 @@ namespace Microsoft.VisualBasic {
 			throw new NotImplementedException ();
 		}
 			
-		public static System.String Command ()
-		{ 
-			string [] args = Environment.GetCommandLineArgs ();
-
-			if (args != null && args.Length > 1) {
-				return string.Join (" ", args, 1, args.Length - 1);
-			} else {
-				return "";
-			}
-		}
 			
-		[MonoTODO]
-		public static System.String Environ (System.Int32 Expression)
-		{ 
-			throw new NotImplementedException ();
-		}
 			
-		public static System.String Environ (System.String Expression)
-		{ 
-			if (Expression == null)
-				return "";
-			
-			string var = Environment.GetEnvironmentVariable (Expression);
-			return var != null ? var : "";
-		}
-			
-		[MonoTODO]
-		public static void Beep ()
-		{ 
-			throw new NotImplementedException ();
-		}
 			
 		[MonoTODO]
 		public static System.String InputBox (System.String Prompt, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue("")] System.String Title, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue("")] System.String DefaultResponse, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue(-1)] System.Int32 XPos, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue(-1)] System.Int32 YPos)
-		{ 
-			throw new NotImplementedException ();
-		}
-			
-		[MonoTODO]
-		public static Microsoft.VisualBasic.MsgBoxResult MsgBox (System.Object Prompt, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue(0)] Microsoft.VisualBasic.MsgBoxStyle Buttons, [System.Runtime.InteropServices.Optional] [System.ComponentModel.DefaultValue(null)] System.Object Title)
-		{ 
-			throw new NotImplementedException ();
-		}
-			
-		[MonoTODO]
-		public static System.Object CallByName (System.Object ObjectRef, System.String ProcName, Microsoft.VisualBasic.CallType UseCallType, params System.Object[] Args)
-		{ 
-			throw new NotImplementedException ();
-		}
-			
-		[MonoTODO]
-		public static System.Object Choose (System.Double Index, params System.Object[] Choice)
 		{ 
 			throw new NotImplementedException ();
 		}
@@ -97,16 +57,83 @@ namespace Microsoft.VisualBasic {
 			return Expression ? TruePart : FalsePart;
 		}
 			
-		[MonoTODO]
-		public static System.String Partition (System.Int64 Number, System.Int64 Start, System.Int64 Stop, System.Int64 Interval)
+		public static System.String Partition (System.Int64 number, System.Int64 start, System.Int64 stop, System.Int64 interval)
 		{ 
-			throw new NotImplementedException ();
+			String stopStr = "";
+			String startStr = "";
+			long startNumber = 0;
+			int spacesCount = 0;
+			long endNumber = 0;
+
+			if (start < 0)
+				throw new ArgumentException(
+					Utils.GetResourceString("Argument_InvalidValue1", "Start"));
+			if (stop <= start)
+				throw new ArgumentException(
+					Utils.GetResourceString("Argument_InvalidValue1", "Stop"));
+			if (interval < 1)
+				throw new ArgumentException(
+					Utils.GetResourceString("Argument_InvalidValue1", "Interval"));
+
+			if (number < start)
+				endNumber = start - 1;
+			else {
+				if (number > stop)
+					startNumber = stop + 1;
+				else {
+					if (interval == 1){
+						startNumber = number;
+						endNumber = number;
+					}
+					else {
+						endNumber = start-1;
+						while (endNumber < number)
+							endNumber += interval;
+						startNumber = endNumber - interval + 1;
+
+						if (endNumber > stop)
+							endNumber = stop;
+						if (startNumber < start)
+							startNumber = start;
+					}
+				}
+			}
+			
+			startStr = startNumber.ToString();
+			stopStr = endNumber.ToString();
+
+			if (stopStr.Length  > startStr.Length)
+				spacesCount = stopStr.Length;
+			else
+				spacesCount = startStr.Length;
+	
+			return startStr.PadLeft(spacesCount) + ":" + stopStr.PadRight(spacesCount);
 		}
 			
-		[MonoTODO]
 		public static System.Object Switch (params System.Object[] VarExpr)
 		{ 
-			throw new NotImplementedException ();
+			int counter;
+			int index;
+
+			if (VarExpr == null)
+				return null;
+
+			counter = VarExpr.Length;
+			index = 0;
+
+			if (counter % 2 != 0)
+				throw new ArgumentException(
+					Utils.GetResourceString("Argument_InvalidValue1", "VarExpr"));
+
+			do {
+				if((bool)VarExpr[index])
+					return VarExpr[index + 1];
+				index += 2;
+				counter = counter - 2;
+			}
+			while (counter > 0);
+
+			return null;
 		}
 			
 		[MonoTODO]
@@ -145,6 +172,230 @@ namespace Microsoft.VisualBasic {
 			throw new NotImplementedException ();
 		}
 		
-		// Events
-	};
+
+		public static Object CallByName (Object objRef, String name, CallType userCallType, Object[] args)
+		{
+			Object retVal = null;
+			Type[] argsType = null;
+
+
+			if(args != null && args.Length != 0) {
+				argsType = new Type[args.Length];
+
+				for(int i = 0; i < args.Length; i++) 
+					argsType[i] = args[i].GetType();
+			}
+
+			Type objType = objRef.GetType();
+        
+			try
+			{
+				MethodInfo methodInfo = null;
+
+				if(userCallType == CallType.Method) {
+					Console.WriteLine("Method");
+						methodInfo = objType.GetMethod(name, argsType);
+				}
+				else if(userCallType == CallType.Get) {
+					Console.WriteLine("GetMethod");
+					methodInfo = objType.GetProperty(name).GetGetMethod();
+				}
+				else if(userCallType == CallType.Set) {
+
+					Console.WriteLine("SetMethod");
+					methodInfo = objType.GetProperty(name).GetSetMethod();
+				}
+
+				return methodInfo.Invoke(objRef, args);
+
+			}
+			catch (Exception exp)
+			{
+				throw new ArgumentException();
+			}
+
+		}
+
+
+		public static System.Object Choose (System.Double Index, System.Object[] Choice)
+		{ 
+			int i;
+
+			i = (int) Math.Round(Conversion.Fix(Index) - 1.0);
+			if(Choice.Rank != 1) 
+				throw new ArgumentException(Utils.GetResourceString("Argument_RankEQOne1", "Choice"));
+        
+			if(i < 0 || i > Choice.GetUpperBound(0)) 
+				return null;
+			else
+				return Choice[i];
+		}
+
+
+		public static System.String Environ (System.Int32 Expression)
+		{ 
+			int index = 0;
+			Exception e;
+
+	//		Console.WriteLine("Coming Here"+Expression);
+
+			IDictionary envVars = Environment.GetEnvironmentVariables();
+
+			foreach(DictionaryEntry de in envVars) {
+				if(++index == Expression) {
+					if( (object) de.Value == null)
+						return "";
+					else
+						return String.Concat(de.Key, "=" , de.Value);
+				}
+			}
+	//		Console.WriteLine("Exiting the loop");
+
+			return "";
+
+		}
+			
+		public static System.String Environ (System.String Expression)
+		{ 
+			Exception e;
+			if (Expression == null) {
+				e = ExceptionUtils.VbMakeExceptionEx(5, Utils.GetResourceString("Argument_InvalidValue1", Expression));
+				throw e;
+			}
+			
+			string var = Environment.GetEnvironmentVariable (Expression);
+			return var != null ? var : "";
+		}
+
+		public static void Beep ()
+		{ 
+			Console.WriteLine("\a");
+		}
+
+
+		public static System.String Command ()
+		{ 
+			string [] args = Environment.GetCommandLineArgs ();
+
+			if (args != null && args.Length > 1) {
+				return string.Join (" ", args, 2, args.Length - 1);
+			} else {
+				return "";
+			}
+		}
+
+		[MonoTODO]
+		public static Microsoft.VisualBasic.MsgBoxResult MsgBox (System.Object Prompt, 
+									 [System.Runtime.InteropServices.Optional] 
+									 [System.ComponentModel.DefaultValue(0)] 
+									 Microsoft.VisualBasic.MsgBoxStyle Buttons, 
+									 [System.Runtime.InteropServices.Optional] 
+									 [System.ComponentModel.DefaultValue(null)] System.Object Title)
+		{ 
+			throw new NotImplementedException ();
+		/*	//MessageButtons msgBoxButtons = 0;
+			MessageBoxIcon msgBoxIcon = 0;
+			MessageBoxDefaultButton msgBoxDefaultButton = 0;
+			MessageBoxOptions msgBoxOptions = 0;
+			
+
+			int IconsMask = MsgBoxStyle.Critical | MsgBoxStyle.Question | MsgBoxStyle.Exclamation | MsgBoxStyle.Information;
+
+			int ButtonsMask = MsgBoxStyle.OKOnly |MsgBoxStyle.OKCancel | MsgBoxStyle.AbortRetryIgnore |
+						MsgBoxStyle.YesNoCancel |
+						MsgBoxStyle.YesNo | MsgBoxStyle.RetryCancel;
+
+			int DefaultButtonMask = MsgBoxStyle.DeafultButton1 | MsgBoxStyle.DefaultButton2 | 
+						MsgBoxStyle.DefaultButton3;
+
+			int OptionsMask =  MsgBoxStyle.MsgBoxRight | MsgBoxStyle.MsgBoxRtlReading;
+
+
+			switch(Buttons & IconMask) {
+			case MsgBoxStyle.OKOnly:
+				msgBoxButtons = MessageBoxButtons.OK;
+				break;
+
+			case MsgBoxStyle.OKCancel:
+				msgBoxButtons = MessageBoxButtons.OK;
+				break;
+
+			case MsgBoxStyle.AbortRetryIgnore:
+				msgBoxButtons = MessageBoxButtons.OKCancel;
+				break;
+
+			case MsgBoxStyle.YesNoCancel:
+				msgBoxButtons = MessageBoxButtons.YesNoCancel;
+				break;
+
+			case MsgBoxStyle.YesNo:
+				msgBoxButtons = MessageBoxButtons.YesNo;
+				break;
+
+			case MsgBoxStyle.RetryCancel:
+				msgBoxButtons = MessageBoxButtons.RetryCancel;
+				break;
+
+			default:
+				// handle error
+				break;
+			}
+
+
+
+			switch(Buttons & IconMask) {
+
+			case MsgBoxStyle.Critical:
+				msgBoxIcon = MessageBoxIcon.Error;
+				break;
+
+			case MsgBoxStyle.Question:
+				msgBoxIcon = MessageBoxIcon.Question;
+				break;
+
+			case MsgBoxStyle.Exclamation:
+				msgBoxIcon = MessageBoxIcon.Exclamation;
+				break;
+
+			case MsgBoxStyle.Information:
+				msgBoxIcon = MessageBoxIcon.Information;
+				break;
+
+			default:
+				// handle error
+				break;
+			}
+
+			switch(Buttons & DefaultButtonMask) {
+			case MsgBoxStyle.DefaultButton1:
+				msgBoxDefaultButton = MessageBoxDefaultButton.Button1;
+				break;
+			case MsgBoxStyle.DefaultButton2:
+				msgBoxDefaultButton = MessageBoxDefaultButton.Button2;
+				break;
+			case MsgBoxStyle.DefaultButton3:
+				msgBoxDefaultButton = MessageBoxDefaultButton.Button3;
+				break;
+			default:
+				//handle error
+				break;
+			}
+
+			switch(Buttons & OptionsMask) {
+			default:
+				break;
+				
+
+			
+			}
+
+
+
+
+
+	*/	}
+
+
+	}
 }
+
