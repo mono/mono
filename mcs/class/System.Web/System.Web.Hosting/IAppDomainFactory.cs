3f09099bcd518bbe63442e6821195efba00f60cf
Gert Driesen <drieseng@users.sourceforge.net>
2004-06-18 17:22:44 +0000

    * IAppDomainFactory.cs: added missing marshalling attributes * IISAPIRuntime.cs: added missing marshalling attributes
    svn path=/trunk/mcs/; revision=29900

--- mcs/class/System.Web/System.Web.Hosting/IAppDomainFactory.cs
+++ mcs/class/System.Web/System.Web.Hosting/IAppDomainFactory.cs
@@ -14,14 +14,15 @@ namespace System.Web.Hosting
 {
 	[Guid ("e6e21054-a7dc-4378-877d-b7f4a2d7e8ba")]
 	[InterfaceType (ComInterfaceType.InterfaceIsIUnknown)]
-        public interface IAppDomainFactory
-        {
-                object Create ([In] string module,
-			       [In] string typeName,
-			       [In] string appId,
-			       [In] string appPath,
-			       [In] string strUrlOfAppOrigin,
-			       [In] int iZone);
-        }
+	public interface IAppDomainFactory
+	{
+		[return: MarshalAs (UnmanagedType.Interface)]
+		object Create ([In, MarshalAs(UnmanagedType.BStr)] string module,
+			       [In, MarshalAs(UnmanagedType.BStr)] string typeName,
+			       [In, MarshalAs(UnmanagedType.BStr)] string appId,
+			       [In, MarshalAs(UnmanagedType.BStr)] string appPath,
+			       [In, MarshalAs(UnmanagedType.BStr)] string strUrlOfAppOrigin,
+			       [In, MarshalAs(UnmanagedType.I4)] int iZone);
+	}
 }
 
--- mcs/class/System.Web/System.Web.Hosting/IISAPIRuntime.cs
+++ mcs/class/System.Web/System.Web.Hosting/IISAPIRuntime.cs
@@ -14,11 +14,12 @@ namespace System.Web.Hosting
 {
 	[Guid ("c4918956-485b-3503-bd10-9083e3f6b66c")]
 	[InterfaceType (ComInterfaceType.InterfaceIsIUnknown)]
-        public interface IISAPIRuntime
-        {
-                void DoGCCollect ();
-                int ProcessRequest ([In] IntPtr ecb, [In] int useProcessModel);
-                void StartProcessing ();
-                void StopProcessing ();
-        }
+	public interface IISAPIRuntime
+	{
+		void DoGCCollect ();
+		[return: MarshalAs (UnmanagedType.I4)]
+		int ProcessRequest ([In] IntPtr ecb, [In, MarshalAs(UnmanagedType.I4)] int useProcessModel);
+		void StartProcessing ();
+		void StopProcessing ();
+	}
 }
