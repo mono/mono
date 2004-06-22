df6cb78b0c2789aaa837ef05f67555f8bf86fa8f
Miguel de Icaza <miguel@gnome.org>
2004-06-22 15:06:48 +0000

    Add Marek to the about section
    svn path=/trunk/mcs/; revision=30123

--- mcs/mcs/driver.cs
+++ mcs/mcs/driver.cs
@@ -263,7 +263,7 @@ namespace Mono.CSharp
 				"For more information on Mono, visit the project Web site\n" +
 				"   http://www.go-mono.com\n\n" +
 
-				"The compiler was written by Miguel de Icaza, Ravi Pratap and Martin Baulig");
+				"The compiler was written by Miguel de Icaza, Ravi Pratap, Martin Baulig and Marek Safar");
 			Environment.Exit (0);
 		}
 
