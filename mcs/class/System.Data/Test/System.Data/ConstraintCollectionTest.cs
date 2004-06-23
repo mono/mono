899c87a1482795613f568020be030df51a6ee59b
Umadevi S <uma@mono-cvs.ximian.com>
2004-06-23 03:44:36 +0000

    2004-06-23 Umadevi S <sumadevi@novell.com>         * ConstraintCollectionTest.cs : Corrected AddRange testcases
    svn path=/trunk/mcs/; revision=30155

--- mcs/class/System.Data/Test/System.Data/ConstraintCollectionTest.cs
+++ mcs/class/System.Data/Test/System.Data/ConstraintCollectionTest.cs
@@ -400,15 +400,11 @@ namespace MonoTests.System.Data
 
 			// After EndInit is called the constraints associated with most recent call to AddRange() must be
 			// added to the ConstraintCollection
-                        Assertion.AssertNull ("#A03", constraints [2].Table);
-//                        Assertion.Assert ("#A03", constraints [2].Table.ToString().Equals ("Table"));
+                        Assertion.Assert ("#A03", constraints [2].Table.ToString().Equals ("Table"));
                         Assertion.Assert ("#A04", table.Constraints.Contains ("Unique1"));
                         Assertion.Assert ("#A05", table.Constraints.Contains ("Unique2"));
-#if NET_1_1 // really?
-                        Assertion.Assert ("#A06", !table.Constraints.Contains ("Unique3"));
-#else
                         Assertion.Assert ("#A06", table.Constraints.Contains ("Unique3"));
-#endif
+
                 }
         
 
