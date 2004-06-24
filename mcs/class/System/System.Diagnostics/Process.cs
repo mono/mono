d4cbaa798fdee26712f0d9e27ca620c84cdbd015
Atsushi Eno <atsushieno@gmail.com>
2004-06-24 11:57:36 +0000

    2004-06-24  Atsushi Enomoto  <atsushi@ximian.com>
    	* Process.cs : quick fix for UseShellExecute=false on windows. See
    	  the code comment.
    
    svn path=/trunk/mcs/; revision=30290

--- mcs/class/System/System.Diagnostics/Process.cs
+++ mcs/class/System/System.Diagnostics/Process.cs
@@ -770,7 +770,11 @@ namespace System.Diagnostics {
 					cmdline = startInfo.FileName + " " + startInfo.Arguments.Trim ();
 			} else {
 				appname = startInfo.FileName;
-				cmdline = startInfo.Arguments.Trim ();
+				// FIXME: There seems something wrong in process.c. We should not require extraneous command line
+				if (Path.DirectorySeparatorChar == '\\')
+					cmdline = appname + " " + startInfo.Arguments.Trim ();
+				else
+					cmdline = startInfo.Arguments.Trim ();
 			}
 
 			ret=Start_internal(appname,
