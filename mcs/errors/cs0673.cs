f4d4ebdc7e3e6943d4afefcbc6a7c134395cd62a
Marek Safar <marek.safar@gmail.com>
2004-06-24 13:19:22 +0000

    fixed test
    svn path=/trunk/mcs/; revision=30302

--- mcs/errors/cs0673.cs
+++ mcs/errors/cs0673.cs
@@ -1,11 +1,10 @@
-//
 // cs0673.cs: System.Void cannot be used from C# -- use typeof (void) to get the void type object.
-// 
+// Line: 8
 
 public class X
 {
 	public static void Main()
 	{
-		Type t = typeof (System.Void);
+		System.Type t = typeof (System.Void);
 	}
 }
