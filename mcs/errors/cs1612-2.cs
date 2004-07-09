aec32df817b8a5af83ad1641797da1779449c22f
Miguel de Icaza <miguel@gnome.org>
2004-07-09 19:15:52 +0000

    New errors
    svn path=/trunk/mcs/; revision=30964

---
+++ mcs/errors/cs1612-2.cs
@@ -0,0 +1,22 @@
+using System;
+class X {
+	static void Main ()
+	{
+
+		P.x = 10;
+		Console.WriteLine ("Got: " + P.x);
+	}
+
+	static G P {
+	 get {
+		return g;
+	 }
+	}
+
+	static G g = new G ();
+
+	struct G {
+		public int x;
+	}
+}
+		
