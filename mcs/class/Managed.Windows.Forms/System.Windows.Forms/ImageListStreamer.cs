f8c8d81e7ffbd526866aa1bab86406b5140e1df6
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2004-07-15 20:05:28 +0000

    - Implemented ImageList and ImageList.ImageCollection classes - Added ColorDepth enumeration - Updated SWF VS.Net project
    svn path=/trunk/mcs/; revision=31198

---
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ImageListStreamer.cs
@@ -0,0 +1,50 @@
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Copyright (c) 2004 Novell, Inc.
+//
+// Authors:
+//	Peter Bartok	(pbartok@novell.com)
+//
+//
+// $Revision: 1.1 $
+// $Modtime: $
+// $Log: ImageListStreamer.cs,v $
+// Revision 1.1  2004/07/15 20:05:28  pbartok
+// - Implemented ImageList and ImageList.ImageCollection classes
+// - Added ColorDepth enumeration
+// - Updated SWF VS.Net project
+//
+//
+//
+
+// NOT COMPLETE
+
+using System.Runtime.Serialization;
+
+namespace System.Windows.Forms {
+	public sealed class ImageListStreamer : ISerializable {
+		#region Public Instance Methods
+		[MonoTODO("Need to learn about streaming first - Peter")]
+		public void GetObjectData(SerializationInfo si, StreamingContext context) {
+			throw new NotImplementedException();
+		}
+		#endregion	// Public Instance Methods
+	}
+}
