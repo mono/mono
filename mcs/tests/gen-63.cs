3e70582560bf967038756a0a6f3409b8037cc236
Martin Baulig <martin@novell.com>
2004-07-17 21:49:49 +0000

    2004-07-18  Martin Baulig  <martin@ximian.com>
    	* gen-63.cs: New test.
    
    svn path=/trunk/mcs/; revision=31267

---
+++ mcs/tests/gen-63.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Test
+{
+	public int Length {
+		get {
+			return 1;
+		}
+	}
+
+	public static implicit operator object[] (Test test)
+	{
+		return new object[1] { test };
+	}
+
+	public static int IndexOf (Test array, object value)
+	{
+		// The non-generic function is better than the generic one whose type
+		// arguments have been infered.
+		return IndexOf (array, value, 0, array.Length);
+	}
+
+	public static int IndexOf (Array array, object value, int startIndex, int count)
+	{
+		return 2;
+	}
+
+	public static int IndexOf<T> (T[] array, T value, int startIndex, int count)
+	{
+		return 1;
+	}
+}
+
+class X
+{
+	static int Main ()
+	{
+		Test test = new Test ();
+		int result = Test.IndexOf (test, test);
+		if (result == 2)
+			return 0;
+		else
+			return 1;
+	}
+}
