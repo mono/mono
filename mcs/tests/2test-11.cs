3d3dd9eb8e95c42b0bcf0ee2cc8a6d8fd384935d
Martin Baulig <martin@novell.com>
2004-07-23 01:54:28 +0000

    2004-07-23  Martin Baulig  <martin@ximian.com>
    	* 2test-10.cs, 1test-11.cs: New tests for partial classes.
    
    svn path=/trunk/mcs/; revision=31395

---
+++ mcs/tests/2test-11.cs
@@ -0,0 +1,29 @@
+namespace Test1
+{
+	public class Base
+	{ }
+
+	public partial class Foo : Base
+	{ }
+
+	public partial class Foo : Base
+	{ }
+}
+
+namespace Test2
+{
+	public interface Base
+	{ }
+
+	public partial class Foo : Base
+	{ }
+
+	public partial class Foo : Base
+	{ }
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
