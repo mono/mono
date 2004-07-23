4e6f20adb3e4b23e6a34589baaac2d81d349b9f5
Martin Baulig <martin@novell.com>
2004-07-23 02:00:59 +0000

    Another test for partial classes.
    svn path=/trunk/mcs/; revision=31396

---
+++ mcs/errors/cs0102-6.cs
@@ -0,0 +1,17 @@
+// CS0102: The container `Test' already contains a definition for `x'
+// Line: 10
+partial class Test
+{
+	int x;
+}
+
+partial class Test
+{
+	int x;
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
