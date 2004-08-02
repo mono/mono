5556cbeef30d06a748c65e660abddc1a24a8e925
Sebastien Pouliot <sebastien@ximian.com>
2004-08-02 22:45:49 +0000

    2003-08-02  Sebastien Pouliot  <sebastien@ximian.com>
    	* UnionCodeGroupTest.cs: Added tests for ResolveMatchingCodeGroups.
    
    svn path=/trunk/mcs/; revision=31752

--- mcs/class/corlib/Test/System.Security.Policy/UnionCodeGroupTest.cs
+++ mcs/class/corlib/Test/System.Security.Policy/UnionCodeGroupTest.cs
@@ -2,9 +2,29 @@
 // MonoTests.System.Security.Policy.UnionCodeGroupTest
 //
 // Author:
-//	Sebastien Pouliot (spouliot@motus.com)
+//	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // (C) 2004 Motus Technologies Inc. (http://www.motus.com)
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using NUnit.Framework;
@@ -88,6 +108,59 @@ namespace MonoTests.System.Security.Policy {
 		{
 			UnionCodeGroup cg = new UnionCodeGroup (new AllMembershipCondition (), new PolicyStatement (new PermissionSet (PermissionState.None)));
 			cg.ResolveMatchingCodeGroups (null);
+		}
+
+		[Test]
+		public void ResolveMatchingCodeGroups_NoMatch ()
+		{
+			UnionCodeGroup cg = new UnionCodeGroup (new ZoneMembershipCondition (SecurityZone.Untrusted), new PolicyStatement (new PermissionSet (PermissionState.Unrestricted)));
+			AssertNull (cg.ResolveMatchingCodeGroups (new Evidence ()));
+		}
+
+		[Test]
+		public void ResolveMatchingCodeGroups_OneLevel ()
+		{
+			UnionCodeGroup level1 = new UnionCodeGroup (new AllMembershipCondition (), new PolicyStatement (new PermissionSet (PermissionState.None)));
+			CodeGroup match = level1.ResolveMatchingCodeGroups (new Evidence ());
+			AssertNotNull ("Match", match);
+			Assert ("Equals(false)", match.Equals (level1, false));
+			Assert ("Equals(true)", match.Equals (level1, true));
+		}
+
+		[Test]
+		public void ResolveMatchingCodeGroups_TwoLevel ()
+		{
+			UnionCodeGroup level1 = new UnionCodeGroup (new AllMembershipCondition (), new PolicyStatement (new PermissionSet (PermissionState.None)));
+			CodeGroup level2 = level1.Copy ();
+			level1.AddChild (level2);
+
+			CodeGroup match = level1.ResolveMatchingCodeGroups (new Evidence ());
+			AssertNotNull ("Match", match);
+			Assert ("Equals(false)", match.Equals (level1, false));
+			Assert ("Equals(true)", match.Equals (level1, true));
+
+			UnionCodeGroup level2b = new UnionCodeGroup (new ZoneMembershipCondition (SecurityZone.Untrusted), new PolicyStatement (new PermissionSet (PermissionState.Unrestricted)));
+			level1.AddChild (level2b);
+			CodeGroup match2 = level1.ResolveMatchingCodeGroups (new Evidence ());
+			AssertNotNull ("Match2", match2);
+			Assert ("Equals(false)2", match2.Equals (level1, false));
+			Assert ("Equals(true)2", !match2.Equals (level1, true));
+		}
+
+		[Test]
+		public void ResolveMatchingCodeGroups_ThreeLevel ()
+		{
+			UnionCodeGroup level1 = new UnionCodeGroup (new AllMembershipCondition (), new PolicyStatement (new PermissionSet (PermissionState.None)));
+			CodeGroup level2 = level1.Copy ();
+			level1.AddChild (level2);
+			UnionCodeGroup level3 = new UnionCodeGroup (new ZoneMembershipCondition (SecurityZone.Untrusted), new PolicyStatement (new PermissionSet (PermissionState.Unrestricted)));
+			level2.AddChild (level3);
+
+			CodeGroup match = level1.ResolveMatchingCodeGroups (new Evidence ());
+			AssertNotNull ("Match", match);
+			Assert ("Equals(false)", match.Equals (level1, false));
+			// Equals (true) isn't a deep compare (just one level)
+			Assert ("Equals(true)", match.Equals (level1, true));
 		}
 
 		[Test]
