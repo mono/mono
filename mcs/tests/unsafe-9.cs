13686008cb2accd82cb77f738f5a8566958a9829
Ben Maurer <benm@mono-cvs.ximian.com>
2004-08-03 15:43:40 +0000

    2004-08-03  Ben Maurer  <bmaurer@ximian.com>
    	* unsafe-9.cs: new test for #62263.
    
    svn path=/trunk/mcs/; revision=31809

---
+++ mcs/tests/unsafe-9.cs
@@ -0,0 +1,20 @@
+//
+// Test for http://bugzilla.ximian.com/show_bug.cgi?id=62263
+//
+// We need to make sure that pointer arth uses the size of X
+// not the size of X*
+//
+
+using System;
+unsafe struct X {
+	int x, y, z;
+	
+	static int Main ()
+	{
+		X* foo = null;
+		
+		if ((int) (foo + 1) != sizeof (foo))
+			return 1;
+		return 0;
+	}
+}
