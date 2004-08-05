e5c41d9c85d74669b0f27e8ea19efc058b2ba386
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-08-05 12:32:22 +0000

    2004-08-05 Anirban Bhattacharjee <banirban@novell.com>         * InterfaceA.vb: Test now covers all Interface members.
    svn path=/trunk/mcs/; revision=31925

--- mcs/btests/InterfaceA.vb
+++ mcs/btests/InterfaceA.vb
@@ -1,5 +1,8 @@
 Interface I
     Function F()
+    Sub S (i as integer)
+    Property P
+    Event e (i as integer)
 End Interface
 
 Class C
@@ -7,6 +10,18 @@ Class C
 
     Function F() Implements I.F
     End Function
+
+    Sub S(i as integer) Implements I.S
+    End Sub
+
+   Property P Implements I.P
+	Get
+	End Get
+	Set
+	End Set
+   End Property
+
+   Event e(i as integer) Implements I.e
 End Class
 
 Module InterfaceA
