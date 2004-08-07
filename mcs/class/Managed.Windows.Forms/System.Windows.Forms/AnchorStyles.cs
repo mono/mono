6a472f7b51bcf6b4a4a6aa53abe70e7b48956af4
Jackson Harper <jackson@novell.com>
2004-08-07 00:02:04 +0000

    Add flags and serializable attributes.
    svn path=/trunk/mcs/; revision=32008

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/AnchorStyles.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/AnchorStyles.cs
@@ -23,9 +23,12 @@
 //	Peter Bartok	pbartok@novell.com
 //
 //
-// $Revision: 1.1 $
+// $Revision: 1.2 $
 // $Modtime: $
 // $Log: AnchorStyles.cs,v $
+// Revision 1.2  2004/08/07 00:02:04  jackson
+//         Add flags and serializable attributes.
+//
 // Revision 1.1  2004/07/09 05:21:25  pbartok
 // - Initial check-in
 //
@@ -34,6 +37,9 @@
 // COMPLETE
 
 namespace System.Windows.Forms {
+
+	[Flags]
+	[Serializable]
 	public enum AnchorStyles {
 		None	= 0x00000000,
 		Top	= 0x00000001,
