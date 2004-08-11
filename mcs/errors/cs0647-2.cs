dee7307e2a9db5368bba65d0ec9b4870e1624c8e
Marek Safar <marek.safar@gmail.com>
2004-08-11 17:03:41 +0000

    new error tests
    svn path=/trunk/mcs/; revision=32224

---
+++ mcs/errors/cs0647-2.cs
@@ -0,0 +1,8 @@
+// cs0647.cs : Error emitting 'System.Security.Permissions.SecurityPermissionAttribute' attribute -- 'SecurityAction RequestMinimum is not valid on this declaration
+// Line : 10
+
+using System;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: SecurityPermission (SecurityAction.Demand, SkipVerification=true)]
