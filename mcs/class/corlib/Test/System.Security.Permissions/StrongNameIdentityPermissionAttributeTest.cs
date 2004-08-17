7651e338a77ad848f68c8f3e1fd086cfb11165da
Sebastien Pouliot <sebastien@ximian.com>
2004-08-17 19:39:04 +0000

    2003-08-17  Sebastien Pouliot  <sebastien@ximian.com>
    	* StrongNameIdentityPermissionTest.cs: New. Unit tests for
    	StrongNameIdentityPermission.
    	* StrongNameIdentityPermissionAttributeTest.cs: Added unit test to
    	check the behavior of using an invalid SecurityAction. Added tests
    	for CreatePermission. Converted existing tests to NUnit 2.2 format.
    
    svn path=/trunk/mcs/; revision=32457

--- mcs/class/corlib/Test/System.Security.Permissions/StrongNameIdentityPermissionAttributeTest.cs
+++ mcs/class/corlib/Test/System.Security.Permissions/StrongNameIdentityPermissionAttributeTest.cs
@@ -2,9 +2,29 @@
 // StrongNameIdentityPermissionAttributeTest.cs - NUnit Test Cases for StrongNameIdentityPermissionAttribute
 //
 // Author:
-//	Sebastien Pouliot (spouliot@motus.com)
+//	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // (C) 2003 Motus Technologies Inc. (http://www.motus.com)
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using NUnit.Framework;
@@ -15,7 +35,7 @@ using System.Security.Permissions;
 namespace MonoTests.System.Security.Permissions {
 
 	[TestFixture]
-	public class StrongNameIdentityPermissionAttributeTest : Assertion {
+	public class StrongNameIdentityPermissionAttributeTest {
 
 		static string pk = "00240000048000009400000006020000002400005253413100040000010001003DBD7208C62B0EA8C1C058072B635F7C9ABDCB22DB20B2A9DADAEFE800642F5D8DEB7802F7A5367728D7558D1468DBEB2409D02B131B926E2E59544AAC18CFC909023F4FA83E94001FC2F11A27477D1084F514B861621A0C66ABD24C4B9FC90F3CD8920FF5FFCED76E5C6FB1F57DD356F96727A4A5485B079344004AF8FFA4CB";
 
@@ -23,39 +43,54 @@ namespace MonoTests.System.Security.Permissions {
 		public void Default () 
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
-			AssertNull ("Name", a.Name);
-			AssertNull ("PublicKey", a.PublicKey);
-			AssertNull ("Version", a.Version);
-			AssertEquals ("TypeId", a.ToString (), a.TypeId.ToString ());
-			Assert ("Unrestricted", !a.Unrestricted);
+			Assert.IsNull (a.Name, "Name");
+			Assert.IsNull (a.PublicKey, "PublicKey");
+			Assert.IsNull (a.Version, "Version");
+			Assert.AreEqual (a.ToString (), a.TypeId.ToString (), "TypeId");
+			Assert.IsTrue (!a.Unrestricted, "Unrestricted");
 
 			StrongNameIdentityPermission perm = (StrongNameIdentityPermission) a.CreatePermission ();
-			AssertEquals ("CreatePermission.Name", String.Empty, perm.Name);
-			AssertNull ("CreatePermission.PublicKey", perm.PublicKey);
-			AssertEquals ("CreatePermission.Version", "0.0", perm.Version.ToString ());
+			Assert.AreEqual (String.Empty, perm.Name, "CreatePermission.Name");
+			Assert.IsNull (perm.PublicKey, "CreatePermission.PublicKey");
+			Assert.AreEqual ("0.0", perm.Version.ToString (), "CreatePermission.Version");
 		}
 
 		[Test]
 		public void Action () 
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
-			AssertEquals ("Action=Assert", SecurityAction.Assert, a.Action);
+			Assert.AreEqual (SecurityAction.Assert, a.Action, "Action=Assert");
 			a.Action = SecurityAction.Demand;
-			AssertEquals ("Action=Demand", SecurityAction.Demand, a.Action);
+			Assert.AreEqual (SecurityAction.Demand, a.Action, "Action=Demand");
 			a.Action = SecurityAction.Deny;
-			AssertEquals ("Action=Deny", SecurityAction.Deny, a.Action);
+			Assert.AreEqual (SecurityAction.Deny, a.Action, "Action=Deny");
 			a.Action = SecurityAction.InheritanceDemand;
-			AssertEquals ("Action=InheritanceDemand", SecurityAction.InheritanceDemand, a.Action);
+			Assert.AreEqual (SecurityAction.InheritanceDemand, a.Action, "Action=InheritanceDemand");
 			a.Action = SecurityAction.LinkDemand;
-			AssertEquals ("Action=LinkDemand", SecurityAction.LinkDemand, a.Action);
+			Assert.AreEqual (SecurityAction.LinkDemand, a.Action, "Action=LinkDemand");
 			a.Action = SecurityAction.PermitOnly;
-			AssertEquals ("Action=PermitOnly", SecurityAction.PermitOnly, a.Action);
+			Assert.AreEqual (SecurityAction.PermitOnly, a.Action, "Action=PermitOnly");
 			a.Action = SecurityAction.RequestMinimum;
-			AssertEquals ("Action=RequestMinimum", SecurityAction.RequestMinimum, a.Action);
+			Assert.AreEqual (SecurityAction.RequestMinimum, a.Action, "Action=RequestMinimum");
 			a.Action = SecurityAction.RequestOptional;
-			AssertEquals ("Action=RequestOptional", SecurityAction.RequestOptional, a.Action);
+			Assert.AreEqual (SecurityAction.RequestOptional, a.Action, "Action=RequestOptional");
 			a.Action = SecurityAction.RequestRefuse;
-			AssertEquals ("Action=RequestRefuse", SecurityAction.RequestRefuse, a.Action);
+			Assert.AreEqual (SecurityAction.RequestRefuse, a.Action, "Action=RequestRefuse");
+#if NET_2_0
+			a.Action = SecurityAction.DemandChoice;
+			Assert.AreEqual (SecurityAction.DemandChoice, a.Action, "Action=DemandChoice");
+			a.Action = SecurityAction.InheritanceDemandChoice;
+			Assert.AreEqual (SecurityAction.InheritanceDemandChoice, a.Action, "Action=InheritanceDemandChoice");
+			a.Action = SecurityAction.LinkDemandChoice;
+			Assert.AreEqual (SecurityAction.LinkDemandChoice, a.Action, "Action=LinkDemandChoice");
+#endif
+		}
+
+		[Test]
+		public void Action_Invalid ()
+		{
+			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute ((SecurityAction)Int32.MinValue);
+			// no validation in attribute
 		}
 
 		[Test]
@@ -63,9 +98,11 @@ namespace MonoTests.System.Security.Permissions {
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
 			a.Name = "mono";
-			AssertEquals ("Name", "mono", a.Name);
+			Assert.AreEqual ("mono", a.Name, "Name");
 			a.Name = null;
-			AssertNull ("Name", a.Name);
+			Assert.IsNull (a.Name, "Name-null");
+			a.Name = String.Empty;
+			Assert.AreEqual (String.Empty, a.Name, "Name-empty");
 		}
 
 		[Test]
@@ -73,9 +110,9 @@ namespace MonoTests.System.Security.Permissions {
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
 			a.PublicKey = pk;
-			AssertEquals ("PublicKey", pk, a.PublicKey);
+			Assert.AreEqual (pk, a.PublicKey, "PublicKey");
 			a.PublicKey = null;
-			AssertNull ("PublicKey", a.PublicKey);
+			Assert.IsNull (a.PublicKey, "PublicKey");
 		}
 
 		[Test]
@@ -83,9 +120,9 @@ namespace MonoTests.System.Security.Permissions {
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
 			a.Version = "1.2.3.4";
-			AssertEquals ("Version", "1.2.3.4", a.Version);
+			Assert.AreEqual ("1.2.3.4", a.Version, "Version");
 			a.Version = null;
-			AssertNull ("Version", a.Version);
+			Assert.IsNull (a.Version, "Version");
 		}
 
 		[Test]
@@ -102,7 +139,10 @@ namespace MonoTests.System.Security.Permissions {
 		{
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
 			a.PublicKey = pk;
-			IPermission perm = a.CreatePermission ();
+			StrongNameIdentityPermission p = (StrongNameIdentityPermission)a.CreatePermission ();
+			Assert.IsNull (p.Name, "Name");
+			Assert.AreEqual (pk, p.PublicKey.ToString (), "PublicKey");
+			Assert.IsNull (p.Version, "Version");
 		}
 
 		[Test]
@@ -120,7 +160,10 @@ namespace MonoTests.System.Security.Permissions {
 			StrongNameIdentityPermissionAttribute a = new StrongNameIdentityPermissionAttribute (SecurityAction.Assert);
 			a.Name = "mono";
 			a.PublicKey = pk;
-			IPermission perm = a.CreatePermission ();
+			StrongNameIdentityPermission p = (StrongNameIdentityPermission) a.CreatePermission ();
+			Assert.AreEqual ("mono", p.Name, "Name");
+			Assert.AreEqual (pk, p.PublicKey.ToString (), "PublicKey");
+			Assert.IsNull (p.Version, "Version");
 		}
 
 		[Test]
@@ -140,6 +183,10 @@ namespace MonoTests.System.Security.Permissions {
 			a.PublicKey = pk;
 			a.Version = "1.2.3.4";
 			IPermission perm = a.CreatePermission ();
+			StrongNameIdentityPermission p = (StrongNameIdentityPermission)a.CreatePermission ();
+			Assert.IsNull (p.Name, "Name");
+			Assert.AreEqual (pk, p.PublicKey.ToString (), "PublicKey");
+			Assert.AreEqual ("1.2.3.4", p.Version.ToString (), "Version");
 		}
 
 		[Test]
@@ -149,7 +196,10 @@ namespace MonoTests.System.Security.Permissions {
 			a.Name = "mono";
 			a.PublicKey = pk;
 			a.Version = "1.2.3.4";
-			IPermission perm = a.CreatePermission ();
+			StrongNameIdentityPermission p = (StrongNameIdentityPermission)a.CreatePermission ();
+			Assert.AreEqual ("mono", p.Name, "Name");
+			Assert.AreEqual (pk, p.PublicKey.ToString (), "PublicKey");
+			Assert.AreEqual ("1.2.3.4", p.Version.ToString (), "Version");
 		}
 
 		[Test]
