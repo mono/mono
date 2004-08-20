750d36191ec686addf471c98e6d8f71b703a4f17
Sureshkumar T <suresh@mono-cvs.ximian.com>
2004-08-20 10:44:48 +0000

    2004-08-20 Sureshkumar T (tsureshkumar@novell.com) 	* OdbcConnection.cs - correct handles are passed to OdbcError exception to trap the correct error
    svn path=/branches/mono-1-0/mcs/; revision=32563

--- mcs/class/System.Data/System.Data.Odbc/OdbcConnection.cs
+++ mcs/class/System.Data/System.Data.Odbc/OdbcConnection.cs
@@ -198,20 +198,20 @@ namespace System.Data.Odbc
 				// disconnect
 				ret = libodbc.SQLDisconnect (hdbc);
 				if ( (ret!=OdbcReturn.Success) && (ret!=OdbcReturn.SuccessWithInfo)) 
-					throw new OdbcException (new OdbcError ("SQLConnect", OdbcHandleType.Dbc,hdbc));
+					throw new OdbcException (new OdbcError ("SQLDisconnect", OdbcHandleType.Dbc,hdbc));
 
 				// free handles
 				if (hdbc != IntPtr.Zero) {
 					ret = libodbc.SQLFreeHandle ( (ushort) OdbcHandleType.Dbc, hdbc);	
 					if ( (ret!=OdbcReturn.Success) && (ret!=OdbcReturn.SuccessWithInfo)) 
-						throw new OdbcException (new OdbcError ("SQLConnect", OdbcHandleType.Dbc,hdbc));
+						throw new OdbcException (new OdbcError ("SQLFreeHandle", OdbcHandleType.Dbc,hdbc));
 				}
 				hdbc = IntPtr.Zero;
 
 				if (henv != IntPtr.Zero) {
 					ret = libodbc.SQLFreeHandle ( (ushort) OdbcHandleType.Env, henv);	
 					if ( (ret!=OdbcReturn.Success) && (ret!=OdbcReturn.SuccessWithInfo)) 
-						throw new OdbcException (new OdbcError ("SQLConnect", OdbcHandleType.Dbc,hdbc));
+						throw new OdbcException (new OdbcError ("SQLFreeHandle", OdbcHandleType.Env,henv));
 				}
 				henv = IntPtr.Zero;
 
