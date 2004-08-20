b1213a660e6a2df587332f260292058298a0e385
Sebastien Pouliot <sebastien@ximian.com>
2004-08-20 13:25:32 +0000

    2004-08-20  Sebastien Pouliot  <sebastien@ximian.com>
    	* SiteIdentityPermission.cs: Basic (without wildcards) implementation
    	for Intersect, IsSubsetOf and Union. Fixed to pass all new unit tests.
    
    svn path=/trunk/mcs/; revision=32573

--- mcs/class/corlib/System.Security.Permissions/SiteIdentityPermission.cs
+++ mcs/class/corlib/System.Security.Permissions/SiteIdentityPermission.cs
@@ -48,25 +48,32 @@ namespace System.Security.Permissions {
 
 		public SiteIdentityPermission (string site) 
 		{
-			_site = site;
+			Site = site;
 		}
 
 		// Properties
 
 		public string Site {
 			get { 
-				if (_site == null)
+				if (IsEmpty ())
 					throw new NullReferenceException ("Site");
 				return _site; 
 			}
-			set { _site = value; }
+			set {
+				if (!IsValid (value))
+					throw new ArgumentException ("Site");
+				_site = value;
+			}
 		}
 
 		// Methods
 
 		public override IPermission Copy () 
 		{
-			return new SiteIdentityPermission (_site);
+			if (IsEmpty ())
+				return new SiteIdentityPermission (PermissionState.None);
+			else
+				return new SiteIdentityPermission (_site);
 		}
 
 		public override void FromXml (SecurityElement esd) 
@@ -76,32 +83,50 @@ namespace System.Security.Permissions {
 			// Note: we do not (yet) care about the return value 
 			// as we only accept version 1 (min/max values)
 
-			this.Site = esd.Attribute ("Site");
+			string s = esd.Attribute ("Site");
+			if (s != null)
+				Site = s;
 		}
 
-		[MonoTODO]
+		[MonoTODO ("do not support wildcard")]
 		public override IPermission Intersect (IPermission target)
 		{
+			SiteIdentityPermission sip = Cast (target);
+			if ((sip == null) || (IsEmpty ()))
+				return null;
+			if (_site == sip._site)
+				return Copy ();
 			return null;
 		}
 
-		[MonoTODO]
+		[MonoTODO ("do not support wildcard")]
 		public override bool IsSubsetOf (IPermission target) 
 		{
-			return false;
+			SiteIdentityPermission sip = Cast (target);
+			if (sip == null)
+				return IsEmpty ();
+			return (_site == sip._site);
 		}
 
 		public override SecurityElement ToXml ()
 		{
 			SecurityElement e = Element (version);
-			e.AddAttribute ("Site", _site);
+			if (_site != null)
+				e.AddAttribute ("Site", _site);
                         return e;
 		}
 
-		[MonoTODO]
+		[MonoTODO ("do not support wildcard")]
 		public override IPermission Union (IPermission target) 
 		{
-			return null;
+			SiteIdentityPermission sip = Cast (target);
+			if ((sip == null) || (_site == sip._site) || sip.IsEmpty ())
+				return Copy ();
+			if (IsEmpty ())
+				return sip.Copy ();
+
+			throw new ArgumentException (Locale.GetText (
+				"Cannot union two different sites."), "target");
 		}
 
 		// IBuiltInPermission
@@ -111,6 +136,11 @@ namespace System.Security.Permissions {
 		}
 
 		// helpers
+
+		private bool IsEmpty ()
+		{
+			return (_site == null);
+		}
 
 		private SiteIdentityPermission Cast (IPermission target)
 		{
@@ -123,6 +153,53 @@ namespace System.Security.Permissions {
 			}
 
 			return sip;
+		}
+
+		private static bool[] valid = new bool [94] {
+			/*  33 */ true,  false, true,  true,  true,  true,  true,  true,  true,  true,
+			/*  43 */ false, false, true,  true,  false, true,  true,  true,  true,  true,
+			/*  53 */ true,  true,  true,  true,  false, false, false, false, false, false,
+			/*  63 */ false, true,  true,  true,  true,  true,  true,  true,  true,  true,
+			/*  73 */ true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+			/*  83 */ true,  true,  true,  true,  true,  true,  true,  true,  false, false,
+			/*  93 */ false, true,  true,  false, true,  true,  true,  true,  true,  true,
+			/* 103 */ true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+			/* 113 */ true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+			/* 123 */ true,  false, true,  true
+		};
+
+		private bool IsValid (string s)
+		{
+			if (s == null)
+				return false;
+
+			for (int i = 0; i < s.Length; i++) {
+				ushort x = (ushort) s [i];
+				if ((x < 33) || (x > 126)) {
+					return false;
+				}
+				if (x == 42) {
+					// special case for wildcards (*)
+					// must be alone or followed by a dot
+					if ((s.Length > 1) && (s [i + 1] != '.'))
+						return false;
+				}
+				if (!valid [x - 33]) {
+					return false;
+				}
+			}
+
+			// a lone dot isn't valid
+			if (s.Length == 1)
+				return (s [0] != '.');
+			return true;
+		}
+
+		private bool HasWildcard ()
+		{
+			if (_site == null)
+				return false;
+			return (_site.IndexOf ('*') >= 0);
 		}
 	}
 }
