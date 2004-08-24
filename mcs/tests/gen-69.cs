dca709d67684b308db07d4edd258cf5d7564b0a2
Martin Baulig <martin@novell.com>
2004-08-24 16:19:22 +0000

    2004-08-24  Martin Baulig  <martin@ximian.com>
    	* gen-69.cs, gen-70.cs: New tests.
    
    svn path=/trunk/mcs/; revision=32763

---
+++ mcs/tests/gen-69.cs
@@ -0,0 +1,28 @@
+public interface IFoo
+{
+	int GetHashCode ();
+}
+
+public interface IFoo<T>
+{
+	int GetHashCode ();
+}
+
+public class Test<T>
+{
+	public int Foo (IFoo<T> foo)
+	{
+		return foo.GetHashCode ();
+	}
+
+	public int Foo (IFoo foo)
+	{
+		return foo.GetHashCode ();
+	}
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
