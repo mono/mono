51ec6d4a7124307e9b2bf548f976efd05bee9069
Martin Baulig <martin@novell.com>
2004-08-24 19:08:06 +0000

    New test.
    svn path=/trunk/mcs/; revision=32785

---
+++ mcs/tests/gen-71.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Foo<T>
+{
+	public T Test<U> (U u)
+		where U : T
+	{
+		return u;
+	}
+}
+
+class X
+{
+	static void Main ()
+	{
+		Foo<X> foo = new Foo<X> ();
+
+		Y y = new Y ();
+		X x = foo.Test<Y> (y);
+	}
+}
+
+class Y : X
+{
+}
