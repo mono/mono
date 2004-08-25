bcc825763c24dc496d0dec5bc36235cda705fd4b
Sachin Kumar <sachin@mono-cvs.ximian.com>
2004-08-25 09:44:58 +0000

    Added few more tests for Conditional Statements
    svn path=/trunk/mcs/; revision=32820

--- mcs/btests/ConditionalStatementsC.vb
+++ mcs/btests/ConditionalStatementsC.vb
@@ -6,6 +6,7 @@ Module ConditionalStatementsC
 
         Dim i As Integer
         Dim arr(10) As Integer
+        Dim flag As Boolean
 
         For i = 0 To 10
 
@@ -14,7 +15,15 @@ Module ConditionalStatementsC
                     arr(i) = 1
                 Case Is < 2, 3, 6 To 7
                     arr(i) = 2
-                Case <= 8, >= 7
+                    Select Case i           ' Nested Select Case Statement
+                        Case 0 To 2
+                            flag = False
+                        Case 3, 6 To 7
+                            flag = True
+                        Case Else
+                            flag = False
+                    End Select
+                Case Is <= 8, Is >= 7
                     arr(i) = 3
                 Case Else
                     arr(i) = 4
@@ -23,11 +32,13 @@ Module ConditionalStatementsC
         Next
 
         If arr(0) <> 1 Or arr(1) <> 1 Or arr(2) <> 1 Then
-            Throw New Exception("#CSC1 - Switch Statement failed")
+            Throw New Exception("#CSC1 - Select Case Statement failed")
         ElseIf arr(3) <> 2 Or arr(6) <> 2 Or arr(7) <> 2 Then
-            Throw New Exception("#CSC2 - Switch Statement failed")
+            Throw New Exception("#CSC2 - Select Case Statement failed")
         ElseIf arr(4) <> 3 Or arr(5) <> 3 Or arr(8) <> 3 Or arr(9) <> 3 Or arr(10) <> 3 Then
-            Throw New Exception("#CSC3 - Switch Statement failed")
+            Throw New Exception("#CSC3 - Select Case Statement failed")
+        ElseIf flag = False Then
+            Throw New Exception("#CSC4 - Nested Select Case Statement failed")
         Else
             Console.WriteLine("OK")
         End If
