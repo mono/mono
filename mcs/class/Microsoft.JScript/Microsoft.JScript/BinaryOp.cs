e89b5866629ac812223ce2436621fa24771d7eaa
César Natarén <cesar@mono-cvs.ximian.com>
2004-08-26 01:30:24 +0000

    2004-08-25  Cesar Lopez Nataren  <cesar@ciencias.unam.mx>
    	* BinaryOp.cs: renamed current_op to simply op as with the new
    	parser we no longer need to keep track of the old_op.
    
    svn path=/trunk/mcs/; revision=32853

--- mcs/class/Microsoft.JScript/Microsoft.JScript/BinaryOp.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/BinaryOp.cs
@@ -28,28 +28,8 @@
 //
 
 namespace Microsoft.JScript {
-
 	public abstract class BinaryOp : Exp {
-
 		internal AST left, right;
-
-		//
-		// We transform rules of the form:
-		//
-		// 	E -> E Op R
-		//	E -> R
-		//
-		// to rules of the form:
-		//
-		//	E -> R E_aux
-		//	E_aux -> (Op R E_aux | )
-		//
-		// so we must keep track of the 
-		// two operators (in the case where Op can be
-		// more than one operator).
-		//
-
-		internal JSToken old_op;
-		internal JSToken current_op;
+		internal JSToken op;
 	}
 }
