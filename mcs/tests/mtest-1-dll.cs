ba74774bb7653b8fd9d3bfb97a99125f0cd45847
Raja R Harinath <harinath@hurrynot.org>
2004-09-01 12:15:56 +0000

    new tests from bug 61128
    svn path=/trunk/mcs/; revision=33148

---
+++ mcs/tests/mtest-1-dll.cs
@@ -0,0 +1,9 @@
+// Compiler options: -t:library
+
+namespace Q {
+ public class A {
+  public static new string ToString() {
+   return "Hello world!";
+  }
+ }
+}
---
+++ mcs/tests/mtest-1-exe.cs
@@ -0,0 +1,10 @@
+// Compiler options: -r:mtest-1-dll
+
+using System;
+using Q;
+
+public class B {
+ public static void Main( string[] args ) {
+  Console.WriteLine( A.ToString() );
+ }
+}
