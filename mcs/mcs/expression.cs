54c1054514c577643c7f52431faceb293b6f328a
Miguel de Icaza <miguel@gnome.org>
2004-09-06 20:19:54 +0000

    2004-09-06  Miguel de Icaza  <miguel@ximian.com>
    	* expression.cs: Report errors with unsafe pointers, fixes #64896
    
    svn path=/trunk/mcs/; revision=33446

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -6772,6 +6772,10 @@ namespace Mono.CSharp {
 				return null;
 			}
 
+			if (typearg.IsPointer && !ec.InUnsafe){
+				UnsafeError (loc);
+				return null;
+			}
 			CheckObsoleteAttribute (typearg);
 
 			type = TypeManager.type_type;
