a8069eda9496233b69e91abbb6c6fe188ad9e52e
Miguel de Icaza <miguel@gnome.org>
2004-09-07 15:31:28 +0000

    2004-09-07  Miguel de Icaza  <miguel@ximian.com>
    	* expression.cs: Return false if we fail to resolve the inner
    	expression.
    
    svn path=/branches/mono-1-0/mcs/; revision=33525

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -5841,7 +5841,7 @@ namespace Mono.CSharp {
 					Expression tmp = (Expression) o;
 					tmp = tmp.Resolve (ec);
 					if (tmp == null)
-						continue;
+						return false;
 
 					// Console.WriteLine ("I got: " + tmp);
 					// Handle initialization from vars, fields etc.
