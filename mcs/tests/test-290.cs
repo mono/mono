f0d8f97a976c7527e502259307ae36f8cc4dfd2b
Raja R Harinath <harinath@hurrynot.org>
2004-09-07 08:23:51 +0000

    New test.
    svn path=/trunk/mcs/; revision=33481

---
+++ mcs/tests/test-290.cs
@@ -0,0 +1,13 @@
+// Distilled from report in http://lists.ximian.com/archives/public/mono-devel-list/2004-September/007777.html
+
+using System;
+
+class EntryPoint {
+	delegate void EventHandler (object sender);
+	static event EventHandler FooEvent;
+	static void bar_f (object sender) {}
+	static void Main () {
+		FooEvent (null);
+		object bar = new EventHandler (bar_f);
+	}
+}
