24150f30a102522e06a3f2c7f5e8fbc85f0ab1e2
Marek Safar <marek.safar@gmail.com>
2004-09-13 21:16:25 +0000

    new test
    svn path=/trunk/mcs/; revision=33822

---
+++ mcs/errors/cs0672-3.cs
@@ -0,0 +1,18 @@
+// cs0672.cs: Member 'B.Warning' overrides obsolete member 'A.Warning'. Add the Obsolete attribute to 'B.Warning'
+// Line: 15
+// Compiler options: -warnaserror
+
+using System;
+
+public class A {
+	[Obsolete()]
+	public virtual string Warning {
+		get { return ""; }
+	}
+}
+
+public class B : A {
+	public override string Warning {
+		get { return ""; }
+	}
+}
