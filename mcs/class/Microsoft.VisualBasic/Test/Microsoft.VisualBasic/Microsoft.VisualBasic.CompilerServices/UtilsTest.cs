1d8d7bbab0210cd76e1118e99d3f918a9a289728
Rafael Teixeira <monoman@gmail.com>
2004-09-14 15:01:04 +0000

    2004-09-14  Rafael Teixeira <rafaelteixeirabr@hotmail.com> 	* UtilsTest.cs: upgraded to use nunit Assert class, also corrected testing for null that 	  was giving an exception when run.
    svn path=/trunk/mcs/; revision=33881

--- mcs/class/Microsoft.VisualBasic/Test/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/UtilsTest.cs
+++ mcs/class/Microsoft.VisualBasic/Test/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/UtilsTest.cs
@@ -36,7 +36,7 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 {
 
 	[TestFixture]
-	public class UtilsTest : Assertion {
+	public class UtilsTest {
 		
 		[SetUp]
 		public void GetReady() {}
@@ -49,9 +49,9 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 			string[] source = new string[] { "First", "Second", "Third" };
 			string[] destination = new string[2];
 			string[] result = (string[])Utils.CopyArray(source, destination);
-			AssertEquals ("ResultIsDestination", destination, result);
-			AssertEquals ("First", source[0], destination[0]);
-			AssertEquals ("Second", source[1], destination[1]);
+			Assert.AreSame (destination, result, "ResultIsDestination");
+			Assert.AreEqual (source[0], destination[0], "First");
+			Assert.AreEqual (source[1], destination[1], "Second");
 		}
 
 		[Test]
@@ -59,10 +59,10 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 			string[] source = new string[] { "First", "Second" };
 			string[] destination = new string[3];
 			string[] result = (string[])Utils.CopyArray(source, destination);
-			AssertEquals ("ResultIsDestination", destination, result);
-			AssertEquals ("First", source[0], destination[0]);
-			AssertEquals ("Second", source[1], destination[1]);
-			AssertEquals ("EmptyThird", null, destination[2]);
+			Assert.AreSame (destination, result, "ResultIsDestination");
+			Assert.AreEqual (source[0], destination[0], "First");
+			Assert.AreEqual (source[1], destination[1], "Second");
+			Assert.IsNull (destination[2], "EmptyThird");
 		}
 	
 		[Test]
@@ -74,9 +74,9 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 			source[1,1] = "Fourth";
 			string[,] destination = new string[2,1];
 			string[,] result = (string[,])Utils.CopyArray(source, destination);
-			AssertEquals ("ResultIsDestination", destination, result);
-			AssertEquals ("First", source[0,0], destination[0,0]);
-			AssertEquals ("Third", source[1,0], destination[1,0]);
+			Assert.AreSame (destination, result, "ResultIsDestination");
+			Assert.AreEqual (source[0,0], destination[0,0], "First");
+			Assert.AreEqual (source[1,0], destination[1,0], "Third");
 		}
 
 		[Test]
@@ -88,13 +88,13 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 			source[1,1] = "Fourth";
 			string[,] destination = new string[2,3];
 			string[,] result = (string[,])Utils.CopyArray(source, destination);
-			AssertEquals ("ResultIsDestination", destination, result);
-			AssertEquals ("First", source[0,0], destination[0,0]);
-			AssertEquals ("Second", source[0,1], destination[0,1]);
-			AssertEquals ("Third", source[1,0], destination[1,0]);
-			AssertEquals ("Fourth", source[1,1], destination[1,1]);
-			AssertEquals ("EmptyFifth", null, destination[0,2]);
-			AssertEquals ("EmptySixth", null, destination[1,2]);
+			Assert.AreSame (destination, result, "ResultIsDestination");
+			Assert.AreEqual (source[0,0], destination[0,0], "First");
+			Assert.AreEqual (source[0,1], destination[0,1], "Second");
+			Assert.AreEqual (source[1,0], destination[1,0], "Third");
+			Assert.AreEqual (source[1,1], destination[1,1], "Fourth");
+			Assert.IsNull (destination[0,2], "EmptyFifth");
+			Assert.IsNull (destination[1,2], "EmptySixth");
 		}
 	
 		// An nice way to test for exceptions the class under test should 
@@ -106,7 +106,7 @@ namespace MonoTests.Microsoft.VisualBasic.CompilerServices
 			ConcreteCollection myCollection;
 			myCollection = new ConcreteCollection();
 			....
-			AssertEquals ("#UniqueID", expected, actual);
+			Assert.AreEqual ("#UniqueID", expected, actual);
 			....
 			Fail ("Message");
 		}
