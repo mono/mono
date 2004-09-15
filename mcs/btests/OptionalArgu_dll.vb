0308e240655c745f2a5f5d6803214d87349f0f98
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-09-15 13:37:22 +0000

    2004-09-15 Anirban Bhattacharjee <banirban@novell.com>         * OptionalArgu_dll.vb:         * OptionalArgu_exe.vb:                 Tests for optional argument accross assembly         * Test.Sources : updated         * Makefile: Added dll name
    svn path=/trunk/mcs/; revision=33934

---
+++ mcs/btests/OptionalArgu_dll.vb
@@ -0,0 +1,9 @@
+Public Class c
+	public Function s (i As Integer, _
+				 Optional j As Integer = 10, _
+				 Optional k As String = "aaa") _
+				 as string
+
+		Return ("s : " + i.ToString() + " - " + j.ToString() + " - " + k)
+	End Function
+End Class
---
+++ mcs/btests/OptionalArgu_exe.vb
@@ -0,0 +1,17 @@
+Module M
+	Sub Main()
+		Dim x As New c()
+		
+		Dim res as String
+
+		res = x.s (10)
+		If (res <> "s : 10 - 10 - aaa")
+			Throw New System.Exception ("#A1, Unexpected result returned by OptionalArgu_dll.dll")
+		End If
+		
+		res = x.s (5, 5)
+		If (res <> "s : 5 - 5 - aaa")
+			Throw New System.Exception ("#A2, Unexpected result returned by OptionalArgu_dll.dll")
+		End If
+	End Sub
+End Module
