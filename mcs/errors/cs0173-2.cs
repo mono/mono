531b9d6b4ef9eb50249fb5a58c92322b2ee43539
Miguel de Icaza <miguel@gnome.org>
2004-09-17 23:17:12 +0000

    Up
    svn path=/trunk/mcs/; revision=34035

--- mcs/errors/cs0173-2.cs
+++ mcs/errors/cs0173-2.cs
@@ -1,2 +1,8 @@
-// cs0173.cs: Type of conditional expression can't be determined because there is no implicit conversion between 'int' and '<null>'// Line: 29public class MainClass {        public static void Main() {
-                bool result = false;                System.Console.WriteLine (result ? 1 : null);   }}
+// cs0173.cs: Type of conditional expression can't be determined because there is no implicit conversion between 'int' and '<null>'// Line: 29
+
+public class MainClass {
+        public static void Main() {
+                bool result = false;
+                System.Console.WriteLine (result ? 1 : null);
+	}
+}
