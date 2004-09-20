ba2df53441df3ae8c818962a170cbcaca02f7540
César Natarén <cesar@mono-cvs.ximian.com>
2004-09-20 23:14:45 +0000

    * SemanticAnalizer.cs: added hash global_env and         IdentificationTable label_set for keeping track of global methods and         label sets. Added static constructor, method BuildGlobalEnv,         ImplementationName, ObjectSystemContains, AddLabel, ContainsLabel,         GetLabel, RemoveLabel.
    svn path=/trunk/mcs/; revision=34122

--- mcs/class/Microsoft.JScript/Microsoft.JScript/SemanticAnalizer.cs
+++ mcs/class/Microsoft.JScript/Microsoft.JScript/SemanticAnalizer.cs
@@ -4,7 +4,7 @@
 // Author:
 //	Cesar Lopez Nataren
 //
-// (C) 2003, Cesar Lopez Nataren, <cesar@ciencias.unam.mx>
+// (C) 2003, 2004 Cesar Lopez Nataren, <cesar@ciencias.unam.mx>
 //
 
 //
@@ -29,6 +29,8 @@
 //
 
 using System;
+using System.Reflection;
+using System.Collections;
 
 namespace Microsoft.JScript {
 
@@ -36,13 +38,65 @@ namespace Microsoft.JScript {
 
 		internal static bool print = true;
 		public static bool allow_member_expr_as_function_name;
-		//
-		// We must include the default 'Global Object',
-		// which contains the built-in objects: Math, String, ...
-		// static void init_default_global_object ()
-		//
-
+		static Hashtable global_env = new Hashtable ();
 		static IdentificationTable context;
+		static IdentificationTable label_set;
+
+		static SemanticAnalyser ()
+		{
+			label_set = new IdentificationTable ();
+			BuildGlobalEnv ();
+		}
+
+		static void BuildGlobalEnv ()
+		{
+			/* value properties of the Global Object */
+			global_env.Add ("NaN", new BuiltIn ("NaN", false, false));
+			global_env.Add ("Infinity", new BuiltIn ("Infinity", false, false));
+			global_env.Add ("undefined", new BuiltIn ("undefined", false, false));
+			
+			/* function properties of the Global Object */
+			object [] custom_attrs;
+			Type global_object = typeof (GlobalObject);
+			MethodInfo [] methods = global_object.GetMethods (BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+			foreach (MethodInfo mi in methods) {
+				custom_attrs = mi.GetCustomAttributes (typeof (JSFunctionAttribute), false);
+				foreach (JSFunctionAttribute attr in custom_attrs)
+					if (attr.IsBuiltIn)
+						global_env.Add (mi.Name, new BuiltIn (ImplementationName (attr.BuiltIn.ToString ()), false, true));
+			}
+
+			/* built in objects */
+			global_env.Add ("Object", new BuiltIn ("Object", true, true));
+			global_env.Add ("Function", new BuiltIn ("Function", true, true));
+			global_env.Add ("Array", new BuiltIn ("Array", true, true));
+			global_env.Add ("String", new BuiltIn ("String", true, true));
+			global_env.Add ("Boolean", new BuiltIn ("Boolean", true, true));
+			global_env.Add ("Number", new BuiltIn ("Number", true, true));
+			global_env.Add ("Math", new BuiltIn ("Math", false, false));
+			global_env.Add ("Date", new BuiltIn ("Date", true, true));
+			global_env.Add ("RegExp", new BuiltIn ("RegExp", true, true));
+
+			/* built in Error objects */
+			global_env.Add ("Error", new BuiltIn ("Error", true, true));
+			global_env.Add ("EvalError", new BuiltIn ("EvalError", true, true));
+			global_env.Add ("RangeError", new BuiltIn ("RangeError", true, true));
+			global_env.Add ("ReferenceError", new BuiltIn ("ReferenceError", true, true));
+			global_env.Add ("SyntaxError", new BuiltIn ("SyntaxError", true, true));
+			global_env.Add ("TypeError", new BuiltIn ("TypeError", true, true));
+			global_env.Add ("URIError", new BuiltIn ("URIError", true, true));
+		}
+
+		static string ImplementationName (string name)
+		{
+			int i = name.LastIndexOf ('_');
+			return name.Substring (i + 1);
+		}
+
+		internal static object ObjectSystemContains (string x)
+		{
+			return global_env [x];
+		}
 
 		public static bool Run (ScriptBlock prog)
 		{
@@ -66,6 +120,27 @@ namespace Microsoft.JScript {
 
 		internal static string CurrentAnonymousMethod {
 			get { return "anonymous " + anon_method_counter; }
+		}
+
+		internal static void AddLabel (string name, AST binding)
+		{
+			label_set.Enter (name, binding);
+		}
+		
+		internal static bool ContainsLabel (string name)
+		{
+			object r = label_set.Contains (name);
+			return r != null;
+		}
+
+		internal static object GetLabel (string name) 
+		{
+			return label_set.Contains (name);
+		}
+
+		internal static void RemoveLabel (string name)
+		{
+			label_set.Remove (name);
 		}
 	}
 }
