b405ec44927caecad48fbe6e247d5db68cd9c0bb
Sebastien Pouliot <sebastien@ximian.com>
2004-09-22 12:06:46 +0000

    2004-09-22  Sebastien Pouliot  <sebastien@ximian.com>
    	* RandomTest.cs: Commented test CompareWithMS following constants
    	changes in Mono implementation (to use Knuth's constants).
    
    svn path=/trunk/mcs/; revision=34209

--- mcs/class/corlib/Test/System/RandomTest.cs
+++ mcs/class/corlib/Test/System/RandomTest.cs
@@ -3,7 +3,28 @@
 //
 // Authors: 
 //	Bob Smith <bob@thestuff.net>
-//	Sebastien Pouliot <spouliot@motus.com>
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using NUnit.Framework;
@@ -79,6 +100,7 @@ namespace MonoTests.System {
 			}
 		}
 
+/* Mono implementation is now compatible with Knuth (not MS) implementation (choice of constants)
 		[Test]
 		public void CompareWithMS () 
 		{
@@ -95,6 +117,6 @@ namespace MonoTests.System {
 			AssertEquals ("Seed(8)", "E7-2A-5C-44-D1-8C-7D-74", r [2]);
 			AssertEquals ("Seed(2)", "C5-67-2A-FC-1B-4E-CD-72", r [1]);
 			AssertEquals ("Seed(0)", "B9-D1-C4-8E-34-8F-E7-71", r [0]);
-		}
+		}*/
 	}
 }
