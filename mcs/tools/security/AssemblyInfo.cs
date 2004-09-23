20cb643a975497ff4afe17a56d44a301f64f6e4b
Sebastien Pouliot <sebastien@ximian.com>
2004-09-23 13:25:53 +0000

    2004-09-23  Sebastien Pouliot  <sebastien@ximian.com>
    	* AssemblyInfo.cs: Updated version to 1.0.3 for next stable release.
    	* MakeCert.cs: Hack for date parsing problem with Mono.
    
    svn path=/branches/mono-1-0/mcs/; revision=34281

--- mcs/tools/security/AssemblyInfo.cs
+++ mcs/tools/security/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Motus Technologies, Novell")]
 [assembly: AssemblyProduct("Mono Security Tools")]
 [assembly: AssemblyCopyright("Copyright 2002, 2003 Motus Technologies. Copyright 2004 Novell. BSD licensed.")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyTrademark("")]
 //[assembly: AssemblyCulture("")]
