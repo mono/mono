5df1776250b006624077241be458711b5bb5d402
Marek Safar <marek.safar@gmail.com>
2004-09-23 16:39:17 +0000

    new test
    svn path=/trunk/mcs/; revision=34290

---
+++ mcs/errors/cs0642-2.cs
@@ -0,0 +1,13 @@
+// cs0642.cs: Possible mistaken empty statement
+// Line: 9
+
+public class C
+{
+    public static void Main ()
+    {
+        int i= 5;
+        while (i++ < 100);
+            { }
+    }
+}
+
--- mcs/errors/cs0642.cs
+++ mcs/errors/cs0642.cs
@@ -1,3 +1,6 @@
+// cs0642.cs: Possible mistaken empty statement
+// Line: 7
+
 class X{
 	static void Main ()
 	{
