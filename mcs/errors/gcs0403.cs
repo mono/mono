4aa24650014721496873147f59a04ce4df559a6e
Martin Baulig <martin@novell.com>
2004-09-24 02:52:18 +0000

    New test.
    svn path=/trunk/mcs/; revision=34323

---
+++ mcs/errors/gcs0403.cs
@@ -0,0 +1,15 @@
+// CS0403: Cannot convert null to the type parameter `T' becaues it could be a value type.  Consider using `default (T)' instead.
+// Line: 7
+class Test<T>
+{
+	public T Null ()
+	{
+		return null;
+	}
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
