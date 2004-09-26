551d466cb4267622773e66280fd6fe9164edfd2e
Zoltan Varga <vargaz@gmail.com>
2004-09-26 14:44:43 +0000

    2004-09-26  Zoltan Varga  <vargaz@freemail.hu>
    	* corlib_test.dll.sources: Add System.Reflection/MethodInfoTest.
    
    svn path=/trunk/mcs/; revision=34407

---
+++ mcs/class/corlib/Test/System.Reflection/MethodInfoTest.cs
@@ -0,0 +1,62 @@
+//
+// System.Reflection.MethodInfo Test Cases
+//
+// Authors:
+//  Zoltan Varga (vargaz@gmail.com)
+//
+// (c) 2003 Ximian, Inc. (http://www.ximian.com)
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace MonoTests.System.Reflection
+{
+	[TestFixture]
+	public class MethodInfoTest : Assertion
+	{
+		[DllImport ("libfoo", EntryPoint="foo", CharSet=CharSet.Unicode, ExactSpelling=false, PreserveSig=true, SetLastError=true, BestFitMapping=true, ThrowOnUnmappableChar=true)]
+		public static extern void dllImportMethod ();
+
+#if NET_2_0
+		[Test]
+		public void PseudoCustomAttributes ()
+		{
+			DllImportAttribute attr = (DllImportAttribute)((typeof (MethodInfoTest).GetMethod ("dllImportMethod").GetCustomAttributes (typeof (DllImportAttribute), true)) [0]);
+
+			AssertEquals (CallingConvention.Winapi, attr.CallingConvention);
+			AssertEquals ("foo", attr.EntryPoint);
+			AssertEquals ("libfoo", attr.Value);
+			AssertEquals (CharSet.Unicode, attr.CharSet);
+			AssertEquals (false, attr.ExactSpelling);
+			AssertEquals (true, attr.PreserveSig);
+			AssertEquals (true, attr.SetLastError);
+			AssertEquals (true, attr.BestFitMapping);
+			AssertEquals (true, attr.ThrowOnUnmappableChar);
+		}
+#endif
+	}
+}
+
