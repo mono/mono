19a415ecec07c10cf8e830b0317a8ddd22e18d78
Zoltan Varga <vargaz@gmail.com>
2004-09-27 13:03:02 +0000

    2004-09-27  Zoltan Varga  <vargaz@freemail.hu>
    	* MethodBody.cs LocalVariableInfo.cs ExceptionHandlingClause.cs
    	ExceptionHandlingClauseFlags.cs: New files.
    
    svn path=/trunk/mcs/; revision=34435

---
+++ mcs/class/corlib/System.Reflection/LocalVariableInfo.cs
@@ -0,0 +1,65 @@
+//
+// System.Reflection/LocalVariableInfo.cs
+//
+// Author:
+//   Zoltan Varga (vargaz@gmail.com)
+//
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#if NET_2_0
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System.Reflection {
+
+	public class LocalVariableInfo {
+		#region Sync with reflection.h
+		Type local_type;
+		bool is_pinned;
+		int local_index;
+		#endregion
+
+		public virtual bool IsPinned {
+			get {
+				return is_pinned;
+			}
+		}
+
+		public virtual int LocalIndex {
+			get {
+				return local_index;
+			}
+		}
+
+		public virtual Type LocalType {
+			get {
+				return local_type;
+			}
+		}
+	}
+
+}
+
+#endif
