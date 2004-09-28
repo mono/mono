a13f2334e028b1b792681958cf798c4c71f0324b
Umadevi S <uma@mono-cvs.ximian.com>
2004-09-28 04:17:39 +0000

    2004-09-28 Umadevi S <sumadevi@novell.com>         * DataRow.cs - checked for Enforceconstraints, while using indices to search for related rows
    svn path=/trunk/mcs/; revision=34458

--- mcs/class/System.Data/System.Data/DataRow.cs
+++ mcs/class/System.Data/System.Data/DataRow.cs
@@ -1037,7 +1037,9 @@ namespace System.Data {
 			int numColumn = parentColumns.Length;
 			if (HasVersion(version)) {
 				Index indx = relation.ParentTable.GetIndexByColumns (parentColumns);
-				if (indx != null) { // get the child rows from the index
+				if (indx != null && 
+    (Table == null || Table.DataSet == null || 
+     Table.DataSet.EnforceConstraints)) { // get the child rows from the index
 					Node[] childNodes = indx.FindAllSimple(childColumns, IndexFromVersion(version));
 					for (int i = 0; i < childNodes.Length; i++) {
 						rows.Add (childNodes[i].Row);
@@ -1046,7 +1048,6 @@ namespace System.Data {
 				else { // no index so we have to search manualy.
 					int curIndex = IndexFromVersion(DataRowVersion.Current);
 					int tmpRecord = relation.ParentTable.RecordCache.NewRecord();
-
 					try {
 						for (int i = 0; i < numColumn; i++) {
 							// according to MSDN: the DataType value for both columns must be identical.
