c3ea191f0ea46b26add57aca3c808d04ffd2553a
Miguel de Icaza <miguel@gnome.org>
2004-10-05 14:32:51 +0000

    Improve test to handle constants of classes whose value is null
    svn path=/trunk/mcs/; revision=34723

--- mcs/tests/test-259.cs
+++ mcs/tests/test-259.cs
@@ -5,6 +5,9 @@ public class Testing
 	public enum INT : int { Zero }
 	public const INT JPEG_SUSPENDED = (INT)0;
 	public const INT JPEG_HEADER_OK = (INT)1;
+
+	// Test that we can have a null value here
+	public const Testing testing = null;
  
 	public static void Main()
 	{ }
