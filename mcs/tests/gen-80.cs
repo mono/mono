1e66ee58e315f04cb04ae8643be11df88e9bf3b9
Martin Baulig <martin@novell.com>
2004-10-11 15:18:14 +0000

    Slightly modified version of gen-79.cs.
    svn path=/trunk/mcs/; revision=34865

---
+++ mcs/tests/gen-80.cs
@@ -0,0 +1,19 @@
+public interface IFoo<X>
+{ }
+
+public class Test
+{
+	public void Hello<T> (IFoo<T> foo)
+	{
+		InsertAll (foo);
+	}
+
+	public void InsertAll<U> (IFoo<U> foo)
+	{ }
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
