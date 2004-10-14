73ecfff599f67edf4585150c6b78e8349fbf079b
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-10-14 06:47:04 +0000

    2004-10-14 Anirban Bhattacharjee <banirban@novell.com>         * ForB.vb: Correction in test case         * ForC.vb: New test case         * Test.Sources: ForC.vb got included
    svn path=/trunk/mcs/; revision=34971

--- mcs/btests/ForB.vb
+++ mcs/btests/ForB.vb
@@ -1,15 +1,11 @@
-REM LineNo: 19
-REM ExpectedError: BC30064
-REM ErrorMessage: 'ReadOnly' variable cannot be target of an assignment.
-
 Imports System
 
 Module ForB
 
     Class C1
-        Public ReadOnly index As Integer = 0
+        Public index As Integer = 0
 
-        Sub New()
+        Sub x()
             For index = 0 To 2
                 Console.WriteLine(index)
             Next
@@ -19,6 +15,7 @@ Module ForB
 
     Sub main()
         Dim c As New C1()
+	c.x ()
         If c.index <> 3 Then
             Throw New Exception("#ForB1")
         End If
