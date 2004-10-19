946ba6c4167ea968684e70699741455888ef1018
Anirban Bhattacharjee <anirban@mono-cvs.ximian.com>
2004-10-19 08:00:17 +0000

    Modified test
    svn path=/trunk/mcs/; revision=35122

--- mcs/btests/ForEachB.vb
+++ mcs/btests/ForEachB.vb
@@ -3,22 +3,22 @@ Imports System
 Module ForEachB
 
     Class C1
-        Public ReadOnly index As Integer = 0
+'        public index As Integer = 0
 
-        Sub New()
+        Sub w()
             Dim arr() As Integer = {1, 2, 3}
-            For Each index In arr
+            For Each index as integer In arr
                 Console.WriteLine(index)
             Next
         End Sub
-
     End Class
 
     Sub main()
         Dim c As New C1()
-        If c.index <> 3 Then
-            Throw New Exception("#FEB1")
-        End If
+        c.w()
+'	If c.index <> 3 Then
+ '           Throw New Exception("#FEB1")
+  '      End If
     End Sub
 
 End Module
