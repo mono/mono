a063d6ec1ec77debb52293ab6c4645ecd3494b68
Marek Safar <marek.safar@gmail.com>
2004-10-20 20:21:54 +0000

    new test
    svn path=/trunk/mcs/; revision=35186

---
+++ mcs/errors/cs0534-2.cs
@@ -0,0 +1,10 @@
+// cs0534.cs: 'DerivedClass' does not implement inherited abstract member 'BaseClass.Value.set'
+// Line: 8
+
+abstract class BaseClass {
+        protected abstract int Value { get; set; }
+}
+
+class DerivedClass: BaseClass {
+        protected override int Value { get {} }
+}
