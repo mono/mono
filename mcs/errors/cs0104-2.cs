f6b088d2ed5038c45fc7c8e735506a0aa8281521
Miguel de Icaza <miguel@gnome.org>
2004-10-28 15:45:38 +0000

    Add another test case, this was a recent regression
    svn path=/trunk/mcs/; revision=35401

---
+++ mcs/errors/cs0104-2.cs
@@ -0,0 +1,25 @@
+using Gdk;
+using System.Drawing;
+
+public class Plot {
+	void M ()
+	{
+		Graphics g;
+	}
+	
+	
+	static void Main ()
+	{
+	}
+}
+
+
+namespace Gdk {
+	public class Graphics {
+	}
+}
+
+namespace System.Drawing {
+	public class Graphics {
+	}
+}
