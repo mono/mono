997fee31ef73a3ff9595dbe7a896c1908a26e0e8
Sebastien Pouliot <sebastien@ximian.com>
2004-10-28 12:30:08 +0000

    2004-10-28  Sebastien Pouliot  <sebastien@ximian.com>
    	* AuthenticodeDeformatterTest.cs: Check timestamp as an UTC value so
    	that the test can work anywhere in the world (and not just at home ;)
    
    svn path=/trunk/mcs/; revision=35396

--- mcs/class/Mono.Security/Test/Mono.Security.Authenticode/AuthenticodeDeformatterTest.cs
+++ mcs/class/Mono.Security/Test/Mono.Security.Authenticode/AuthenticodeDeformatterTest.cs
@@ -702,7 +702,7 @@ namespace MonoTests.Mono.Security.Authenticode {
 			// mean it's root is trusted on the current system
 			Assert.IsTrue (((ad.Reason == 0) || (ad.Reason == 6)), "Reason");
 			Assert.AreEqual ("35-A5-21-3B-FC-FE-FA-40-97-AA-BB-DE-3B-52-15-6F", BitConverter.ToString (ad.Hash), "Hash");
-			Assert.AreEqual (632011226700000000, ad.Timestamp.Ticks, "Timestamp");
+			Assert.AreEqual (632011370700000000, ad.Timestamp.ToUniversalTime ().Ticks, "Timestamp");
 			Assert.AreEqual (4, ad.Certificates.Count, "#Certificates");
 			Assert.AreEqual ("C=ZA, S=Western Cape, L=Cape Town, O=Thawte Consulting cc, OU=Certification Services Division, CN=Thawte Server CA, E=server-certs@thawte.com", ad.SigningCertificate.IssuerName, "IssuerName");
 			Assert.AreEqual ("C=CA, S=Quebec, L=Quebec, O=Motus Technologies Inc., OU=Secure Application Development, CN=Motus Technologies Inc.", ad.SigningCertificate.SubjectName, "SubjectName");
