fd3d6a523bc9339b490fada435b7f2f81508c4af
Sebastien Pouliot <sebastien@ximian.com>
2004-11-05 18:17:48 +0000

    2004-11-05  Sebastien Pouliot  <sebastien@ximian.com>
    	* KeyPersistanceTest.cs: Merge fixes from HEAD when machine store
    	isn't created (and can't be created by the current user).
    
    svn path=/branches/mono-1-0/mcs/; revision=35715

--- mcs/class/Mono.Security/Test/Mono.Security.Cryptography/KeyPairPersistenceTest.cs
+++ mcs/class/Mono.Security/Test/Mono.Security.Cryptography/KeyPairPersistenceTest.cs
@@ -123,15 +123,22 @@ namespace MonoTests.Mono.Security.Cryptography {
 			CspParameters cp = new CspParameters (-6, "Provider", "Container");
 			cp.Flags = CspProviderFlags.UseMachineKeyStore;
 			KeyPairPersistence kpp = new KeyPairPersistence (cp, "<keypair/>");
-			kpp.Save ();
+			try {
+				kpp.Save ();
 
-			Assert ("Save-Exists", File.Exists (kpp.Filename));
-			KeyPairPersistence kpp2 = new KeyPairPersistence (cp);
-			Assert ("Load", kpp2.Load ());
+				Assert ("Save-Exists", File.Exists (kpp.Filename));
+				KeyPairPersistence kpp2 = new KeyPairPersistence (cp);
+				Assert ("Load", kpp2.Load ());
 
-			Compare (kpp, kpp2);
-			kpp.Remove ();
-			Assert ("Remove-!Exists", !File.Exists (kpp.Filename));
+				Compare (kpp, kpp2);
+				kpp.Remove ();
+				Assert ("Remove-!Exists", !File.Exists (kpp.Filename));
+			}
+			catch (CryptographicException ce) {
+				// not everyone can write to the machine store
+				if (!(ce.InnerException is UnauthorizedAccessException))
+					throw;
+			}
 		}
 
 		[Test]
@@ -140,15 +147,22 @@ namespace MonoTests.Mono.Security.Cryptography {
 			CspParameters cp = new CspParameters (-7, "Provider", "Container");
 			cp.Flags = CspProviderFlags.UseDefaultKeyContainer | CspProviderFlags.UseMachineKeyStore;
 			KeyPairPersistence kpp = new KeyPairPersistence (cp, "<keypair/>");
-			kpp.Save ();
+			try {
+				kpp.Save ();
 
-			Assert ("Save-Exists", File.Exists (kpp.Filename));
-			KeyPairPersistence kpp2 = new KeyPairPersistence (cp);
-			Assert ("Load", kpp2.Load ());
+				Assert ("Save-Exists", File.Exists (kpp.Filename));
+				KeyPairPersistence kpp2 = new KeyPairPersistence (cp);
+				Assert ("Load", kpp2.Load ());
 
-			Compare (kpp, kpp2);
-			kpp.Remove ();
-			Assert ("Remove-!Exists", !File.Exists (kpp.Filename));
+				Compare (kpp, kpp2);
+				kpp.Remove ();
+				Assert ("Remove-!Exists", !File.Exists (kpp.Filename));
+			}
+			catch (CryptographicException ce) {
+				// not everyone can write to the machine store
+				if (!(ce.InnerException is UnauthorizedAccessException))
+					throw;
+			}
 		}
 
 		[Test]
