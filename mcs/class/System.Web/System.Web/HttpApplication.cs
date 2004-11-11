1aeab7743666f2cb32d8eac2b783f19060188ea6
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2004-11-11 22:02:30 +0000

    2004-11-11 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* HttpApplication.cs: undo the TimeoutManager.(Add|Remove) shuffling.
    	It causes troubles under heavy load.
    
    
    svn path=/trunk/mcs/; revision=36042

--- mcs/class/System.Web/System.Web/HttpApplication.cs
+++ mcs/class/System.Web/System.Web/HttpApplication.cs
@@ -639,7 +639,6 @@ namespace System.Web
 			{
 				bool ready_sync = false;
 				IStateHandler handler;
-				bool timeoutPossible = false;
 
 				lock (_app) {
 					_app.OnStateExecuteEnter ();
@@ -663,18 +662,9 @@ namespace System.Web
 
 							handler = _handlers [_currentStateIdx];
 							_countSteps++;
-							timeoutPossible = handler.PossibleToTimeout;
-							try {
-								if (timeoutPossible)
-									HttpRuntime.TimeoutManager.Add (_app.Context);
-	
-								lasterror = ExecuteState (handler, ref ready_sync);
-								if (ready_sync) 
-									_countSyncSteps++;
-							} finally {
-								if (timeoutPossible)
-									HttpRuntime.TimeoutManager.Remove (_app.Context);
-							}
+							lasterror = ExecuteState (handler, ref ready_sync);
+							if (ready_sync) 
+								_countSyncSteps++;
 						} while (_currentStateIdx < _endStateIdx);
 
 						if (null != lasterror)
@@ -903,12 +893,14 @@ namespace System.Web
 			SaveThreadCulture ();
 			_savedContext = HttpContext.Context;
 			HttpContext.Context = _Context;
+			HttpRuntime.TimeoutManager.Add (_Context);
 			SetPrincipal (Context.User);
 		}
 
 		internal void OnStateExecuteLeave ()
 		{
 			RestoreThreadCulture ();
+			HttpRuntime.TimeoutManager.Remove (_Context);
 			HttpContext.Context = _savedContext;
 			RestorePrincipal ();
 		}
