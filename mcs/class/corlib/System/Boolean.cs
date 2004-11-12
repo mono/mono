4a8f6325d8bd830620f0501851416deda5cd6d64
Miguel de Icaza <miguel@gnome.org>
2004-11-12 22:19:35 +0000

    Commit
    svn path=/trunk/mcs/; revision=36076

--- mcs/class/corlib/System/Boolean.cs
+++ mcs/class/corlib/System/Boolean.cs
@@ -29,7 +29,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-
+// Testing SVN 
 //
 // I guess this is the Boolean class. This was written word for word
 // off the Library specification for System.Boolean in the ECMA
