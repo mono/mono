71937b0f476fe89e0d6d98fdfd4370a4d48cd98d
Martin Baulig <martin@novell.com>
2004-11-16 04:54:20 +0000

    2004-11-16  Martin Baulig  <martin@ximian.com>
    	* test-316.cs: New test for #69082.
    
    	* test-317.cs; New test for #63705.
    
    
    svn path=/trunk/mcs/; revision=36160

---
+++ mcs/tests/test-317.cs
@@ -0,0 +1,16 @@
+// Compiler options: /warn:4 /warnaserror
+interface IFoo
+{
+	void Test (int t);
+}
+
+interface IBar : IFoo
+{
+	new int Test (int t);
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
