5b5ea83f1ebaf1cfa93d1521bedefa5934fef972
Miguel de Icaza <miguel@gnome.org>
2004-11-17 04:45:23 +0000

    Add new test from for bug #69361
    svn path=/trunk/mcs/; revision=36191

---
+++ mcs/tests/pover-a.cs
@@ -0,0 +1,9 @@
+//
+// First file of a 2-test file to check for #69361
+// 
+public class AA
+{
+	internal protected virtual string Data {
+		get { return null; }
+	}
+}
---
+++ mcs/tests/pover-b.cs
@@ -0,0 +1,6 @@
+public class BB : AA
+{
+	protected override string Data {
+		get { return null; }
+	}
+}
