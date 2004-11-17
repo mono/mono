10e907bd41984461b3c0b46da8acfbe9e6c0d919
Marek Safar <marek.safar@gmail.com>
2004-11-17 17:07:46 +0000

    missing unsafe
    svn path=/trunk/mcs/; revision=36225

--- mcs/errors/cs0214-2.cs
+++ mcs/errors/cs0214-2.cs
@@ -1,7 +1,7 @@
 // cs0214: pointers can only be used in an unsafe context
 // Line: 9
-// This error shows how cs214 is produced at the *call site*
-//
+// Compiler options: -unsafe
+
 public class Test
 {
         public void Foo ()
