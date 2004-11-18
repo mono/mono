9ff80c83d59adcc47c39185d6092aaa5957eb625
Jonathan Pryor <jpryor@novell.com>
2004-11-18 14:49:53 +0000

    * stdio.c: Add fread(3) and fwrite(3) wrappers.
    svn path=/trunk/mono/; revision=36278

--- support/stdio.c
+++ support/stdio.c
@@ -25,6 +25,24 @@ Mono_Posix_Syscall_L_cuserid (void)
 	return L_cuserid;
 }
 
+mph_size_t
+Mono_Posix_Stdlib_fread (void *ptr, mph_size_t size, mph_size_t nmemb, FILE *stream)
+{
+	mph_return_if_size_t_overflow (size);
+	mph_return_if_size_t_overflow (nmemb);
+
+	return fread (ptr, (size_t) size, (size_t) nmemb, stream);
+}
+
+mph_size_t
+Mono_Posix_Stdlib_fwrite (const void *ptr, mph_size_t size, mph_size_t nmemb, FILE *stream)
+{
+	mph_return_if_size_t_overflow (size);
+	mph_return_if_size_t_overflow (nmemb);
+
+	return fwrite (ptr, (size_t) size, (size_t) nmemb, stream);
+}
+
 G_END_DECLS
 
 /*
