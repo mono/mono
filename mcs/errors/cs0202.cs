70ac5f6eefa4c37a7001a16112ac61fa729baced
Marek Safar <marek.safar@gmail.com>
2004-11-19 15:32:41 +0000

    new test
    svn path=/trunk/mcs/; revision=36324

---
+++ mcs/errors/cs0202.cs
@@ -0,0 +1,23 @@
+// cs0202.cs: foreach requires that the return type 'Foo.E[]' of 'Foo.P.GetEnumerator()' must have a suitable public MoveNext method and public Current property
+// Line: 18
+
+public class Foo
+{
+        public class E {}
+            
+        public class P
+        {
+            public E[] GetEnumerator ()
+            {
+                return null;
+            }
+        }
+       
+        public static void Main ()
+        {
+            P o = new P ();
+            foreach (P p in o)
+            {
+            }
+        }
+}
