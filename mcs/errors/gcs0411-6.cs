b6788a9e9e0133b4e601964e6196228e7bdc922a
Martin Baulig <martin@novell.com>
2004-11-20 16:28:50 +0000

    New tests.
    svn path=/trunk/mcs/; revision=36341

---
+++ mcs/errors/gcs0411-6.cs
@@ -0,0 +1,25 @@
+// CS0411: The type arguments for method `World' cannot be infered from the usage. Try specifying the type arguments explicitly.
+// Line: 16
+public interface IFoo<T>
+{ }
+
+public class Foo : IFoo<int>, IFoo<string>
+{ }
+
+public class Hello
+{
+	public void World<U> (U u, IFoo<U> foo)
+	{ }
+
+	public void Test (Foo foo)
+	{
+		World ("Canada", foo);
+	}
+}
+
+class X
+{
+	static void Main ()
+	{
+	}
+}
