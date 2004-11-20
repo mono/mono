87da17c9abcddde70aa25cef3073f719b82fe6a0
Martin Baulig <martin@novell.com>
2004-11-20 16:35:12 +0000

    New test.
    svn path=/trunk/mcs/; revision=36343

---
+++ mcs/tests/gen-103.cs
@@ -0,0 +1,27 @@
+public interface IFoo<T>
+{ }
+
+public class Foo : IFoo<string>
+{ }
+
+public class Hello
+{
+	public void World<U> (U u, IFoo<U> foo)
+	{ }
+
+	public void World<V> (IFoo<V> foo)
+	{ }
+
+	public void Test (Foo foo)
+	{
+		World ("Canada", foo);
+		World (foo);
+	}
+}
+
+class X
+{
+	static void Main ()
+	{
+	}
+}
