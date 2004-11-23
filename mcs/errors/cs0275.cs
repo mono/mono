6e9876be7497c8c275a8bd6a0cc5e126ad40dec7
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2004-11-23 15:48:42 +0000

    2004-11-23  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* cs0275.cs: New
    	
    
    svn path=/trunk/mcs/; revision=36441

---
+++ mcs/errors/cs0275.cs
@@ -0,0 +1,12 @@
+// cs0275.cs(9) error CS0275: Accessibility modifiers can not be used on accessors in interfaces
+// Line: 7
+
+ public interface Error0275
+ {
+	 string Message {
+		 protected get;
+		 set;
+	 }
+
+ }
+
