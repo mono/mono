37471f7e84252328f00acc9f58f50beb46164faa
Zoltan Varga <vargaz@gmail.com>
2004-11-24 13:13:31 +0000

    2004-11-24  Zoltan Varga  <vargaz@freemail.hu>
    	* TypeTest.cs: Add regression test for #69787.
    
    svn path=/trunk/mcs/; revision=36480

--- mcs/class/corlib/Test/System/TypeTest.cs
+++ mcs/class/corlib/Test/System/TypeTest.cs
@@ -253,6 +253,17 @@ namespace MonoTests.System
 		public void IsPrimitive () {
 			Assert (typeof (IntPtr).IsPrimitive);
 		}
+
+		[Test]
+		public void GetTypeWithWhitespace () {
+			AssertNotNull (Type.GetType
+						   (@"System.Configuration.NameValueSectionHandler,
+			System,
+Version=1.0.5000.0,
+Culture=neutral
+,
+PublicKeyToken=b77a5c561934e089"));
+		}
 	}
 }
 
