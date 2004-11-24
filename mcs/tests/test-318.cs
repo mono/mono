f6884a5a870eda3d6ed65043721285edeceac96a
Marek Safar <marek.safar@gmail.com>
2004-11-24 12:08:54 +0000

    improved test
    svn path=/trunk/mcs/; revision=36470

--- mcs/tests/test-318.cs
+++ mcs/tests/test-318.cs
@@ -1,19 +1,37 @@
 // This code must be compilable without any warning
-// Compiler options: -warnaserror -warn:4
+// Compiler options: -warnaserror -warn:4 -unsafe
+// Test of wrong CS0219 warning
 
-public class C
-{
-        public static void my_from_fixed(out int val)
-        {
-            val = 3;
-        }
+public class C {
+    
+	public static void my_from_fixed(out int val)
+	{
+		val = 3;
+	}
         
-        public static void month_from_fixed(int date) {
+	public static void month_from_fixed(int date)
+	{
 		int year;
 		my_from_fixed(out year);
 	}
         
-        public static void Main ()
-        {
-        }
+	internal static int CreateFromString (int arg)
+	{
+		int major = 0;
+		int number = 5;
+
+		major = number;
+		number = -1;
+                    
+		return major;
+	}   
+        
+        public unsafe double* GetValue (double value)
+	{
+		double d = value;
+		return &d;
+	}        
+        
+	public static void Main () {
+	}
 }
