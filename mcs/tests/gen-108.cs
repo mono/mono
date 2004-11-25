34e3d2fea3bba97cebbd4fe553a21d3cfa541fb3
Martin Baulig <martin@novell.com>
2004-11-25 12:12:54 +0000

    New test.
    svn path=/trunk/mcs/; revision=36541

---
+++ mcs/tests/gen-108.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Test<T>
+{
+	protected T item;
+
+	public Test (T item)
+	{
+		this.item = item;
+	}
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		yield return item;
+	}
+}
+
+class X
+{
+	static void Main ()
+	{
+		Test<int> test = new Test<int> (3);
+		foreach (int a in test)
+			;
+	}
+}
