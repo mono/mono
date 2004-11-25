ba7129ff520580a35febe0e5fdc69aae20b17e7b
Raja R Harinath <harinath@hurrynot.org>
2004-11-25 10:53:49 +0000

    * Assembly/Consts.cs (UnmanageType_80) [BOOTSTRAP_WITH_OLDLIB]: Update to changed name of bootstrap-only constant. * System.Runtime.InteropServices/UnmanagedType.cs (mono_bootstrap_NativeTypeMax): Rename from __mono_bootstrap_NativeTypeMax. CSC doesn't like [CLSCompliant(false)] on enum member.
    svn path=/trunk/mcs/; revision=36535

--- mcs/class/corlib/Assembly/Consts.cs
+++ mcs/class/corlib/Assembly/Consts.cs
@@ -77,7 +77,7 @@ internal sealed class Consts
 #endif
 
 #if BOOTSTRAP_WITH_OLDLIB
-	public const UnmanagedType UnmanagedType_80 = UnmanagedType.__mono_bootstrap_NativeTypeMax;
+	public const UnmanagedType UnmanagedType_80 = UnmanagedType.mono_bootstrap_NativeTypeMax;
 #else
 	public const UnmanagedType UnmanagedType_80 = (UnmanagedType) 80;
 #endif
