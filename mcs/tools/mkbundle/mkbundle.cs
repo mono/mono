83b6541e4014d34ff959cfa2d4726baec63fe94b
Miguel de Icaza <miguel@gnome.org>
2004-11-27 20:21:55 +0000

    Add new mkbundle tool
    svn path=/trunk/mcs/; revision=36691

---
+++ mcs/tools/mkbundle/mkbundle.cs
@@ -0,0 +1,277 @@
+//
+// mkbundle: tool to create bundles.
+//
+// Based on the `make-bundle' Perl script written by Paolo Molaro (lupus@debian.org)
+//
+// Author:
+//   Miguel de Icaza
+//
+// (C) Novell, Inc 2004
+//
+using System;
+using System.Xml;
+using System.Collections;
+using System.Reflection;
+using System.IO;
+using System.Runtime.InteropServices;
+using Mono.Posix;
+
+class MakeBundle {
+	static string output = "a.out";
+	static ArrayList link_paths = new ArrayList ();
+	static bool autodeps = true;
+	static bool keeptemp = false;
+	
+	static int Main (string [] args)
+	{
+		ArrayList sources = new ArrayList ();
+		int top = args.Length;
+		link_paths.Add (".");
+		
+		for (int i = 0; i < top; i++){
+
+			switch (args [i]){
+			case "-h":
+				Help ();
+				return 1;
+			}
+			
+			// Now process options that take an argument
+			switch (args [i]){
+			case "-o": 
+				if (i+1 == top){
+					Help (); 
+					return 1;
+				}
+				output = args [++i];
+				break;
+
+			case "-L":
+				if (i+1 == top){
+					Help (); 
+					return 1;
+				}
+				link_paths.Add (args [i]);
+				break;
+
+			case "--nodeps":
+				autodeps = false;
+				break;
+
+			case "--keeptemp":
+				keeptemp = true;
+				break;
+				
+			default:
+				sources.Add (args [i]);
+				break;
+			}
+		}
+
+		Console.WriteLine ("Sources: {0} Auto-dependencies: {1}", sources.Count, autodeps);
+		if (sources.Count == 0 || output == null) {
+			Help ();
+			Environment.Exit (1);
+		}
+
+		ArrayList assemblies = LoadAssemblies (sources);
+		ArrayList files = new ArrayList ();
+		foreach (Assembly a in assemblies)
+			QueueAssembly (files, a.CodeBase);
+
+		GenerateBundles (files);
+		//GenerateJitWrapper ();
+		
+		return 0;
+	}
+
+	static void GenerateBundles (ArrayList files)
+	{
+		string temp_s = "temp.s"; // Path.GetTempFileName ();
+		string temp_c = "temp.c";
+		string temp_o = Path.GetTempFileName () + ".o";
+		
+		try {
+			ArrayList c_bundle_names = new ArrayList ();
+			byte [] buffer = new byte [8192];
+
+			StreamWriter ts = new StreamWriter (File.Create (temp_s));
+			StreamWriter tc = new StreamWriter (File.Create (temp_c));
+
+			tc.WriteLine ("#include <mono/metadata/assembly.h>\n");
+			foreach (string url in files){
+				string fname = url.Substring (7);
+				string aname = fname.Substring (fname.LastIndexOf ("/") + 1);
+				string encoded = aname.Replace ("-", "_").Replace (".", "_");
+
+				Console.WriteLine ("   embedding: " + fname);
+				
+				FileInfo fi = new FileInfo (fname);
+				FileStream fs = File.OpenRead (fname);
+
+				ts.WriteLine (
+			        	".globl assembly_data_{0}\n" +
+					"\t.section .rodata\n" +
+					"\t.align 32\n" +
+					"\t.type assembly_data_{0}, @object\n" +
+					"\t.size assembly_data_{0}, {1}\n" +
+					"assembly_data_{0}:\n",
+					encoded, fi.Length);
+
+
+				int n;
+				while ((n = fs.Read (buffer, 0, 8192)) != 0){
+					for (int i = 0; i < n; i++){
+						ts.Write ("\t.byte {0}\n", buffer [i]);
+					}
+				}
+				ts.WriteLine ();
+
+				tc.WriteLine ("extern const unsigned char assembly_data_{0} [];", encoded);
+				tc.WriteLine ("static const MonoBundledAssembly assembly_bundle_{0} = {{\"{1}\", assembly_data_{0}, {2}}};",
+					      encoded, aname, fi.Length);
+
+				c_bundle_names.Add ("assembly_bundle_" + encoded);
+			}
+			ts.Close ();
+			Console.WriteLine ("Compiling:");
+			string cmd = String.Format ("as -o {0} {1} ", temp_o, temp_s);
+			Console.WriteLine (cmd);
+			int ret = system (cmd);
+			if (ret != 0){
+				Error ("[Fail]");
+				return;
+			}
+
+			tc.WriteLine ("\nstatic const MonoBundledAssembly *bundled [] = {");
+			foreach (string c in c_bundle_names){
+				tc.WriteLine ("\t&{0},", c);
+			}
+			tc.WriteLine ("\tNULL\n};\n");
+
+			tc.WriteLine ("int mono_main (int argc, char* argv[]);");
+			tc.WriteLine ("int main (int argc, char* argv[])\n" +
+				      "{\n" +
+				      "\tmono_register_bundled_assemblies(bundled);\n" +
+				      "\treturn mono_main (argc, argv);\n" +
+				      "}");
+			tc.Close ();
+
+			cmd = String.Format ("cc -o {2} -Wall {0} `pkg-config --cflags --libs mono` {1}",
+					     temp_c, temp_o, output);
+			Console.WriteLine (cmd);
+			ret = system (cmd);
+			if (ret != 0){
+				Error ("[Fail]");
+				return;
+			}
+			Console.WriteLine ("Done");
+		} finally {
+			if (!keeptemp){
+				//File.Delete (temp_s);
+				//File.Delete (temp_c);
+				//File.Delete (temp_o);
+			}
+		}
+	}
+	
+	static ArrayList LoadAssemblies (ArrayList sources)
+	{
+		ArrayList assemblies = new ArrayList ();
+		bool error = false;
+		
+		foreach (string name in sources){
+			Assembly a = LoadAssembly (name);
+
+			if (a == null){
+				error = true;
+				continue;
+			}
+			
+			assemblies.Add (a);
+		}
+
+		if (error)
+			Environment.Exit (1);
+
+		return assemblies;
+	}
+	
+	static void QueueAssembly (ArrayList files, string codebase)
+	{
+		if (files.Contains (codebase))
+			return;
+
+		files.Add (codebase);
+		Assembly a = Assembly.LoadFrom (codebase);
+
+		if (!autodeps)
+			return;
+		
+		foreach (AssemblyName an in a.GetReferencedAssemblies ())
+			QueueAssembly (files, an.CodeBase);
+	}
+
+	static Assembly LoadAssembly (string assembly)
+	{
+		Assembly a;
+		
+		try {
+			char[] path_chars = { '/', '\\' };
+			
+			if (assembly.IndexOfAny (path_chars) != -1) {
+				a = Assembly.LoadFrom (assembly);
+			} else {
+				string ass = assembly;
+				if (ass.EndsWith (".dll"))
+					ass = assembly.Substring (0, assembly.Length - 4);
+				a = Assembly.Load (ass);
+			}
+			return a;
+		} catch (FileNotFoundException){
+			string total_log = "";
+			
+			foreach (string dir in link_paths){
+				string full_path = Path.Combine (dir, assembly);
+				if (!assembly.EndsWith (".dll") && !assembly.EndsWith (".exe"))
+					full_path += ".dll";
+				
+				try {
+					a = Assembly.LoadFrom (full_path);
+					return a;
+				} catch (FileNotFoundException ff) {
+					total_log += ff.FusionLog;
+					continue;
+				}
+			}
+			Error ("Cannot find assembly `" + assembly + "'" );
+			Console.WriteLine ("Log: \n" + total_log);
+		} catch (BadImageFormatException f) {
+			Error ("Cannot load assembly (bad file format)" + f.FusionLog);
+		} catch (FileLoadException f){
+			Error ("Cannot load assembly " + f.FusionLog);
+		} catch (ArgumentNullException){
+			Error("Cannot load assembly (null argument)");
+		}
+		return null;
+	}
+
+	static void Error (string msg)
+	{
+		Console.Error.WriteLine (msg);
+		Environment.Exit (1);
+	}
+
+	static void Help ()
+	{
+		Console.WriteLine ("Usage is: mkbundle [options] -o output assembly1 [assembly2...]\n\n" +
+				   "Options:\n" + 
+				   "    -L path     Adds `path' to the search path for assemblies" +
+				   "    --nodeps    Turns off automatic dependency embedding" +
+				   "    --keeptemp  Keeps the temporary files");
+	}
+
+	[DllImport ("libc")]
+	static extern int system (string s);
+}
+
