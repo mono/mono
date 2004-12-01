079eb0780d153bb214741c4c96cddcbb07dd5582
Marek Safar <marek.safar@gmail.com>
2004-12-01 10:34:48 +0000

    2004-12-01  Marek Safar  <marek.safar@seznam.cz>
    	Fix bug #68790
    	* ecore.cs: CheckMarshallByRefAccess new virtual method for testing
    	MarshallByReference members access.
    
    	* expression.cs: Use CheckMarshallByRefAccess;
    	Better error CS0197 message.
    
    	* report.cs: Print whole related error message.
    
    svn path=/trunk/mcs/; revision=36893

--- mcs/mcs/ecore.cs
+++ mcs/mcs/ecore.cs
@@ -192,6 +192,9 @@ namespace Mono.CSharp {
 			Report.Warning (code, loc, format, args);
 		}
 
+		// Not nice but we have broken hierarchy
+		public virtual void CheckMarshallByRefAccess (Type container) {}
+
 		/// <summary>
 		/// Tests presence of ObsoleteAttribute and report proper error
 		/// </summary>
@@ -2844,6 +2847,14 @@ namespace Mono.CSharp {
 		{
 			Emit (ec, false);
 		}
+
+		public override void CheckMarshallByRefAccess (Type container)
+		{
+			if (!container.IsSubclassOf (TypeManager.mbr_type) && DeclaringType.IsSubclassOf (TypeManager.mbr_type)) {
+				Report.SymbolRelatedToPreviousError (DeclaringType);
+				Report.Error (1690, loc, "Cannot call '{0}' method, property, or indexer because it is a value type member of a marshal-by-reference class", Name);
+			}
+		}
 		
 		public void AddressOf (EmitContext ec, AddressOp mode)
 		{
@@ -3100,6 +3111,8 @@ namespace Mono.CSharp {
 				instance_expr = instance_expr.DoResolve (ec);
 				if (instance_expr == null)
 					return false;
+
+				instance_expr.CheckMarshallByRefAccess (ec.ContainerType);
 			}
 
 			if (must_do_cs1540_check && (instance_expr != null)) {
@@ -3115,8 +3128,8 @@ namespace Mono.CSharp {
 					return false;
 				}
 			}
-
 			return true;
+
 		}
 		
 		override public Expression DoResolve (EmitContext ec)
