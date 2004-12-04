8d4173dff6adaca3d923bed7b91958c8fa2769ee
Atsushi Eno <atsushieno@gmail.com>
2004-12-04 14:12:18 +0000

    2004-12-04  Atsushi Enomoto <atsushi@ximian.com>
    	* DTMXPathNavigator.cs : added more constant fields to utility class.
    
    
    
    svn path=/trunk/mcs/; revision=37079

--- mcs/class/System.XML/Mono.Xml.XPath/DTMXPathNavigator.cs
+++ mcs/class/System.XML/Mono.Xml.XPath/DTMXPathNavigator.cs
@@ -626,5 +626,7 @@ namespace Mono.Xml.XPath
 	{
 		public const string XML = "http://www.w3.org/XML/1998/namespace";
 		public const string XMLNS = "http://www.w3.org/2000/xmlns/";
+		public const int IndexXML = 2;
+		public const int IndexXMLNS = 3;
 	}
 }
