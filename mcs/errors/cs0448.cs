3a5fbe3cfae94f4c8d7806dedb27cbafda502ccd
Marek Safar <marek.safar@gmail.com>
2004-12-07 16:14:57 +0000

    reformating
    svn path=/trunk/mcs/; revision=37318

--- mcs/errors/cs0448.cs
+++ mcs/errors/cs0448.cs
@@ -1,2 +1,7 @@
-// cs0448.cs: The return type for ++ or -- operator must be the containing type or derived from the containing type// Line: 5
-class SampleClass {        public static int operator ++ (SampleClass value) {                return new SampleClass();        }}
+// cs0448.cs: The return type for ++ or -- operator must be the containing type or derived from the containing type
+// Line: 5
+class SampleClass {
+    public static int operator ++ (SampleClass value) {
+        return new SampleClass();        
+    }
+}
