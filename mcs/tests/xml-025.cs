10da3f974cc313489719e2ee0d136eb4dccb9eb6
Raja R Harinath <harinath@hurrynot.org>
2004-12-07 11:30:27 +0000

    * Makefile (TEST_HARNESS_EXTRAS): New.  List of files that need to be copied to the test harness directory. (test-harness-run): Use it. * xml-025.cs, xml-025-ref.xml: Don't use ../ to refer to xml-025.inc.
    svn path=/trunk/mcs/; revision=37310

--- mcs/tests/xml-025.cs
+++ mcs/tests/xml-025.cs
@@ -1,23 +1,21 @@
 // Compiler options: -doc:xml-025.xml
-// Note that it could not be compiled to generate reference output as it is.
-// csc needs '\\' instead of '/' for file specification.
 
 namespace Testing
 {
-   /// <include file='../xml-025.inc' path='/foo' />
+   /// <include file='xml-025.inc' path='/foo' />
    public class Test
    {
 	public static void Main ()
 	{
 	}
 
-	/// <include file='../xml-025.inc' path='/root'/>
+	/// <include file='xml-025.inc' path='/root'/>
 	public string S1;
 
-	/// <include file='../xml-025.inc' path='/root/child'/>
+	/// <include file='xml-025.inc' path='/root/child'/>
 	public string S2;
 
-	/// <include file='../xml-025.inc' path='/root/@attr'/>
+	/// <include file='xml-025.inc' path='/root/@attr'/>
 	public string S3;
    }
 }
