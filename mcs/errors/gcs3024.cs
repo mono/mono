ad934a4e832173b320419585362cc3caa052cb7c
Martin Baulig <martin@novell.com>
2004-12-08 21:05:07 +0000

    2004-12-08  Martin Baulig  <martin@ximian.com>
    	* gcs3024.cs: New test.
    
    
    svn path=/trunk/mcs/; revision=37423

---
+++ mcs/errors/gcs3024.cs
@@ -0,0 +1,14 @@
+// CS3024: 'IList2<T>': type parameters are not CLS-compliant
+// Line: 7
+using System;
+
+[assembly: CLSCompliant(true)]
+
+public interface IList2<T>
+{ }
+
+class X
+{
+	static void Main ()
+	{ }
+}
