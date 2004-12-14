2b514180c504c968dfb044b6d78d390c6ee75f17
Miguel de Icaza <miguel@gnome.org>
2004-12-14 19:05:35 +0000

    Remove warning
    svn path=/trunk/mcs/; revision=37756

--- mcs/mcs/anonymous.cs
+++ mcs/mcs/anonymous.cs
@@ -1009,6 +1009,11 @@ namespace Mono.CSharp {
 			}
 			Block invocation_block = ec.CurrentBlock;
 			CapturedParameter par_info = (CapturedParameter) captured_parameters [name];
+			if (par_info != null){
+				// 
+				// FIXME: implementing this.
+				//
+			}
 			ILGenerator ig = ec.ig;
 
 			ScopeInfo si;
@@ -1041,6 +1046,11 @@ namespace Mono.CSharp {
 			}
 			EmitParameterInstance (ec, name);
 			CapturedParameter par_info = (CapturedParameter) captured_parameters [name];
+			if (par_info != null){
+				// 
+				// FIXME: implementing this.
+				//
+			}
 			ec.ig.Emit (OpCodes.Ldfld, par_info.FieldBuilder);
 		}
 
