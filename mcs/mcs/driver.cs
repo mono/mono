a13f69418592020769af1ab8aebc996ddb4aacc0
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2004-12-17 20:43:00 +0000

    2004-12-17  Carlos Cortez <calberto.cortez@gmail.com>
    	* driver.cs: Patch to handle a xsp bug that prevents to reference an .exe
    	assembly, by JB Evain.
    
    
    svn path=/trunk/mcs/; revision=37907

--- mcs/mcs/driver.cs
+++ mcs/mcs/driver.cs
@@ -295,7 +295,7 @@ namespace Mono.CSharp
 					a = Assembly.LoadFrom (assembly);
 				} else {
 					string ass = assembly;
-					if (ass.EndsWith (".dll"))
+					if (ass.EndsWith (".dll") || ass.EndsWith (".exe"))
 						ass = assembly.Substring (0, assembly.Length - 4);
 					a = Assembly.Load (ass);
 				}
@@ -304,7 +304,7 @@ namespace Mono.CSharp
 			} catch (FileNotFoundException){
 				foreach (string dir in link_paths){
 					string full_path = Path.Combine (dir, assembly);
-					if (!assembly.EndsWith (".dll"))
+					if (!assembly.EndsWith (".dll") && !assembly.EndsWith (".exe"))
 						full_path += ".dll";
 
 					try {
