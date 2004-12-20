ae7dfcb85eed2acb71e46ea8c4f730cd63247a26
Sebastien Pouliot <sebastien@ximian.com>
2004-12-20 15:29:42 +0000

    2004-12-20  Sebastien Pouliot  <sebastien@ximian.com>
    	* Environment.cs: Bump corlib version.
    
    
    svn path=/trunk/mcs/; revision=37976

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -54,7 +54,7 @@ namespace System
 		 * Changes which are already detected at runtime, like the addition
 		 * of icalls, do not require an increment.
 		 */
-		private const int mono_corlib_version = 31;
+		private const int mono_corlib_version = 32;
 
 		private Environment ()
 		{
