a543c11300df06bdba7f837a1ef9accf444734f5
Ben Maurer <benm@mono-cvs.ximian.com>
2004-12-27 23:35:31 +0000

    2004-12-27  Ben Maurer  <bmaurer@ximian.com>
    	* List.cs: My new (mostly untested ;-) impl of List
    	<T>. Implements most of the API.
    
    
    svn path=/trunk/mcs/; revision=38117

--- mcs/class/corlib/System.Collections.Generic/List.cs
+++ mcs/class/corlib/System.Collections.Generic/List.cs
@@ -40,271 +40,513 @@ namespace System.Collections.Generic
 {
 	[CLSCompliant(false)]
 	[ComVisible(false)]
-	public class List<T> : IList<T>, ICollection<T>, IEnumerable<T>,
-		IList, ICollection, IEnumerable
+	public class List<T> : IList<T>, ICollection<T>, IEnumerable<T>, IList, ICollection, IEnumerable
 	{
-		protected int count;
-		protected int capacity;
-		protected T[] contents;
-		protected int modified;
-
+		T [] data;
+		int size;
+		int version;
+			
+		const int DefaultCapacity = 4;
+		
 		public List ()
 		{
 		}
-
+		
+		public List (IEnumerable <T> collection)
+		{
+			AddRange (collection);
+		}
+		
 		public List (int capacity)
 		{
-			this.capacity = capacity;
-			contents = new T [capacity];
+			data = new T [capacity];
 		}
-
-		public List (ICollection collection)
-			: this (collection.Count)
-		{
-			collection.CopyTo (contents, 0);
-			count = collection.Count;
-		}
-
-		protected void Resize (int size)
-		{
-			if (size < capacity)
-				return;
-
-			if (size < 10)
-				size = 10;
-
-			T[] ncontents = new T [size];
-			if (count > 0)
-				Array.Copy (contents, 0, ncontents, 0, count);
-
-			modified++;
-			contents = ncontents;
-			capacity = size;
-		}
-
+		
 		public void Add (T item)
 		{
-			if (count >= capacity)
-				Resize (2 * capacity);
-
-			contents [count] = item;
-			count++;
+			if (data == null)
+				Capacity = DefaultCapacity;
+			else if (size == data.Length)
+				Capacity = Math.Max (Capacity * 2, DefaultCapacity);
+			
+			data [size ++] = item;
 		}
-
-		int IList.Add (object item)
+		
+		public void CheckRange (int idx, int count)
 		{
-			if (count >= capacity)
-				Resize (2 * capacity);
-
-			contents [count] = (T) item;
-			return count++;
+			if (idx < 0 || count < 0 || idx + count < size)
+				throw new ArgumentOutOfRangeException ();
 		}
-
+		
+		[MonoTODO ("PERFORMANCE: fix if it is an IList <T>")]
+		public void AddRange(IEnumerable<T> collection)
+		{
+			foreach (T t in collection)
+				Add (t);
+		}
+		
+		[MonoTODO]
+		public IList<T> AsReadOnly ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		public int BinarySearch(T item)
+		{
+			return BinarySearch (item, Comparer <T>.Default);
+		}
+		
+		public int BinarySearch(T item, IComparer<T> comparer)
+		{
+			return BinarySearch (0, size, item, comparer);
+		}
+		
+		public int BinarySearch(int index, int count, T item, IComparer<T> comparer)
+		{
+			CheckRange (index, count);
+			return Array.BinarySearch (data, index, size, item, comparer);
+		}
+		
 		public void Clear ()
 		{
-			count = 0;
+			if (data == null)
+				return;
+			
+			Array.Clear (data, 0, data.Length);
 		}
-
+		
 		public bool Contains (T item)
 		{
-			for (int i = 0; i < count; i++)
-				if (contents [i] == item)
-					return true;
-
-			return false;
+			return IndexOf (item) != -1;
+		}
+		
+		public List <U> ConvertAll <U> (Converter<T, U> converter)
+		{
+			List <U> u = new List <U> (size);
+			int i = 0;
+			foreach (T t in this)
+				u [i ++] = converter (t);
+			
+			return u;
+		}
+		
+		public void CopyTo (T [] array)
+		{
+			CopyTo (array, 0);
+		}
+		
+		public void CopyTo (T [] array, int arrayIndex)
+		{
+			CopyTo (0, array, arrayIndex, size);
+		}
+		
+		public void CopyTo (int index, T[] array, int arrayIndex, int count)
+		{
+			CheckRange (index, count);
+			Array.Copy (data, index, array, arrayIndex, count);
 		}
 
+		public bool Exists (Predicate<T> match)
+		{
+			foreach (T t in this)
+				if (match (t))
+					return true;
+			
+			return false;
+		}
+		
+		public T Find (Predicate<T> match)
+		{
+			foreach (T t in this)
+				if (match (t))
+					return t;
+			
+			return default (T);
+		}
+		
+		// Maybe we could make this faster. For example, you could
+		// make a bit set with stackalloc for which elements to copy
+		// then you could size the array correctly.
+		public List<T> FindAll (Predicate<T> match)
+		{
+			List <T> f = new List <T> ();
+			
+			foreach (T t in this)
+				if (match (t))
+					f.Add (t);
+			
+			return f;
+		}
+		
+		public int FindIndex (Predicate <T> match)
+		{
+			return FindIndex (0, match);
+		}
+		
+		public int FindIndex (int startIndex, Predicate <T> match)
+		{
+			return FindIndex (startIndex, size - startIndex, match);
+		}
+		
+		public int FindIndex (int startIndex, int count, Predicate <T> match)
+		{
+			CheckRange (startIndex, count);
+			
+			for (int i = startIndex; i < startIndex + count; i ++)
+				if (match (data [i]))
+					return i;
+				
+			return -1;
+		}
+		
+		public T FindLast (Predicate <T> match)
+		{
+			int i = FindLastIndex (match);
+			return i == -1 ? default (T) : this [i];
+		}
+		
+		public int FindLastIndex (Predicate <T> match)
+		{
+			return FindLastIndex (0, match);
+		}
+		
+		public int FindLastIndex (int startIndex, Predicate <T> match)
+		{
+			return FindLastIndex (startIndex, size - startIndex, match);
+		}
+		
+		public int FindLastIndex (int startIndex, int count, Predicate <T> match)
+		{
+			CheckRange (startIndex, count);
+			for (int i = startIndex + count; i != startIndex;)
+				if (match (data [--i]))
+					return i;
+				
+			return -1;	
+		}
+		
+		public void ForEach (Action <T> action)
+		{
+			foreach (T t in this)
+				action (t);
+		}
+		
+		public Enumerator <T> GetEnumerator ()
+		{
+			return new Enumerator <T> (this);
+		}
+		
+		[MonoTODO]
+		public List <T> GetRange (int index, int count)
+		{
+			throw new NotImplementedException ();
+		}
+		
+		public int IndexOf (T item)
+		{
+			return IndexOf (item, 0);
+		}
+		
+		public int IndexOf (T item, int index)
+		{
+			return IndexOf (item, index, size - index);
+		}
+		
+		public int IndexOf (T item, int index, int count)
+		{
+			CheckRange (index, count);
+			if (data == null)
+				return -1;
+			
+			return Array.IndexOf (data, item, index, count);
+		}
+		
+		void Shift (int start, int delta)
+		{
+			Array.Copy (data, start, data, start + delta, size - start);
+		}
+		
+		public void Insert (int index, T item)
+		{
+			if ((uint) index < (uint) size)
+				throw new ArgumentOutOfRangeException ();
+			
+			Shift (index, 1);
+			size ++;
+			this [index] = item;
+				
+		}
+		[MonoTODO ("Performance for collection")]
+		public void InsertRange (int index, IEnumerable<T> collection)
+		{
+			foreach (T t in collection)
+				Insert (index ++, t);
+		}
+		
+		public int LastIndexOf (T item)
+		{
+			return LastIndexOf  (item, 0);
+		}
+		
+		public int LastIndexOf  (T item, int index)
+		{
+			return LastIndexOf  (item, index, size - index);
+		}
+		
+		public int LastIndexOf (T item, int index, int count)
+		{
+			CheckRange (index, count);
+			if (data == null)
+				return -1;
+			
+			return Array.LastIndexOf (data, item, index, count);
+		}
+		
+		public bool Remove (T item)
+		{
+			int loc = IndexOf (item);
+			if (loc != -1)
+				RemoveAt (loc);
+			
+			return loc != -1;
+		}
+		
+		[MonoTODO ("I can make it faster than this...")]
+		public int RemoveAll (Predicate<T> match)
+		{
+			int index = 0;
+			int c = 0;
+			while ((index = FindIndex (index, match)) != -1) {
+				RemoveAt (index);
+				c ++;
+			}
+			
+			return c;
+		}
+		
+		public void RemoveAt (int index)
+		{
+			RemoveRange (index, 1);
+		}
+		
+		public void RemoveRange (int index, int count)
+		{
+			CheckRange (index, count);
+			Shift (index, -count);
+		}
+		
+		public void Reverse ()
+		{
+			Reverse (0, size);
+		}
+		public void Reverse (int index, int count)
+		{
+			CheckRange (index, count);
+			Array.Reverse (data, index, count);
+		}
+		
+		public void Sort ()
+		{
+			Sort (Comparer <T>.Default);
+		}
+		public void Sort (IComparer<T> comparer)
+		{
+			Sort (0, size, comparer);
+		}
+		
+		// Waiting on Array
+		[MonoTODO]
+		public void Sort (Comparison<T> comparison)
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[MonoTODO]
+		public void Sort (int index, int count, IComparer<T> comparer)
+		{
+			CheckRange (index, count);
+			throw new NotImplementedException ();
+		}
+
+		public T [] ToArray ()
+		{
+			T [] t = new T [size];
+			if (data != null)
+				data.CopyTo (t, 0);
+			
+			return t;
+		}
+		
+		public void TrimToSize ()
+		{
+			Capacity = size;
+		}
+		
+		public bool TrueForAll (Predicate <T> match)
+		{
+			foreach (T t in this)
+				if (!match (t))
+					return false;
+				
+			return true;
+		}
+		
+		public int Capacity {
+			get { 
+				if (data == null)
+					return DefaultCapacity;
+				return data.Length;
+			}
+			set {
+				if ((uint) value < (uint) size)
+					throw new ArgumentOutOfRangeException ();
+				
+				Array.Resize (ref data, value);
+			}
+		}
+		
+		public int Count {
+			get { return size; }
+		}
+		
+		public T this [int index] {
+			get {
+				if ((uint) index >= (uint) size)
+					throw new IndexOutOfRangeException ();
+				return data [index];
+			}
+			set {
+				if ((uint) index >= (uint) size)
+					throw new IndexOutOfRangeException ();
+				data [index] = value;
+			}
+		}
+		
+#region Interface Crap
+		IEnumerator <T> IEnumerable <T>.GetEnumerator()
+		{
+			return GetEnumerator ();
+		}
+		
+		void ICollection.CopyTo (Array array, int arrayIndex)
+		{
+			Array.Copy (data, 0, data, arrayIndex, size);
+		}
+		
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator ();
+		}
+		
+		int IList.Add (object item)
+		{
+			Add ((T) item);
+			return size - 1;
+		}
+		
 		bool IList.Contains (object item)
 		{
 			return Contains ((T) item);
 		}
-
-		public int IndexOf (T item)
-		{
-			for (int i = 0; i < count; i++)
-				if (contents [i] == item)
-					return i;
-
-			return -1;
-		}
-
+		
 		int IList.IndexOf (object item)
 		{
 			return IndexOf ((T) item);
 		}
-
-		public void Insert (int index, T item)
-		{
-			if (index < 0)
-				throw new ArgumentException ();
-			if (index > count)
-				index = count;
-
-			Resize (index);
-			int rest = count - index;
-			if (rest > 0)
-				Array.Copy (contents, index, contents, index+1, rest);
-			contents [index] = item;
-		}
-
+		
 		void IList.Insert (int index, object item)
 		{
 			Insert (index, (T) item);
 		}
-
-		public bool Remove (T item)
-		{
-			int index = IndexOf (item);
-			if (index < 0)
-				return false;
-
-			RemoveAt (index);
-			return true;
-		}
-
+		
 		void IList.Remove (object item)
 		{
 			Remove ((T) item);
 		}
-
-		public void RemoveAt (int index)
-		{
-			if ((index < 0) || (count == 0))
-				throw new ArgumentException ();
-			if (index > count)
-				index = count;
-
-			int rest = count - index;
-			if (rest > 0)
-				Array.Copy (contents, index+1, contents, index, rest);
-
-			count--;
-		}
-
-		public bool IsFixedSize {
-			get {
-				return false;
-			}
-		}
-
-		public bool IsReadOnly {
-			get {
-				return false;
-			}
-		}
-
-		public T this [int index] {
-			get {
-				return contents [index];
-			}
-
-			set {
-				contents [index] = value;
-			}
-		}
-
-		object IList.this [int index] {
-			get {
-				return contents [index];
-			}
-
-			set {
-				// contents [index] = (T) value;
-			}
-		}
-
-		public void CopyTo (T[] array, int arrayIndex)
-		{
-			if (count > 0)
-				Array.Copy (contents, 0, array, arrayIndex, count);
-		}
-
-		void ICollection.CopyTo (Array array, int arrayIndex)
-		{
-			if (count > 0)
-				Array.Copy (contents, 0, array, arrayIndex, count);
-		}
-
-		public int Count {
-			get {
-				return count;
-			}
-		}
-
-		public bool IsSynchronized {
+		
+		bool ICollection <T>.IsReadOnly {
 			get { return false; }
 		}
-
-		public object SyncRoot {
+		bool ICollection.IsSynchronized {
+			get { return false; }
+		}
+		
+		object ICollection.SyncRoot {
 			get { return this; }
 		}
-
-		public Enumerator GetEnumerator ()
-		{
-			return new Enumerator (this);
+		bool IList.IsFixedSize {
+			get { return false; }
 		}
-
-		IEnumerator<T> IEnumerable<T>.GetEnumerator ()
-		{
-			return new Enumerator (this);
+		
+		bool IList.IsReadOnly {
+			get { return false; }
 		}
-
-		IEnumerator IEnumerable.GetEnumerator ()
-		{
-			return new Enumerator (this);
+		
+		object IList.this [int index] {
+			get { return this [index]; }
+			set { this [index] = (T) value; }
 		}
-
-		public struct Enumerator : IEnumerator<T>, IEnumerator
-		{
-			List<T> list;
-			int modified;
-			int current;
-
-			public Enumerator (List<T> list)
+#endregion
+		
+		
+		public struct Enumerator <T> : IEnumerator <T>, IEnumerator, IDisposable {
+			const int NOT_STARTED = -2;
+			
+			// this MUST be -1, because we depend on it in move next.
+			// we just decr the size, so, 0 - 1 == FINISHED
+			const int FINISHED = -1;
+			
+			List <T> l;
+			int idx;
+			int ver;
+			
+			internal Enumerator (List <T> l)
 			{
-				this.list = list;
-				this.modified = list.modified;
-				this.current = -1;
+				this.l = l;
+				idx = NOT_STARTED;
+				ver = l.version;
 			}
-
-			public T Current {
-				get {
-					if (current < 0)
-						throw new InvalidOperationException ("Enumeration has not been initialized; call MoveNext first.");
-					if (current >= list.count)
-						throw new InvalidOperationException ("Enumeration already finished.");
-					
-					return list.contents [current];
-				}
-			}
-
-			object IEnumerator.Current {
-				get {
-					return Current;
-				}
-			}
-
-			public bool MoveNext ()
-			{
-				if (list.modified != modified)
-					throw new InvalidOperationException ("List was modified while enumerating.");
-
-				current++;
-				return current < list.count;
-			}
-
-			void IEnumerator.Reset ()
-			{
-				if (list.modified != modified)
-					throw new InvalidOperationException ("List was modified while enumerating.");
-
-				current = -1;
-			}
-
+			
+			// for some fucked up reason, MSFT added a useless dispose to this class
+			// It means that in foreach, we must still do a try/finally. Broken, very
+			// broken.
 			public void Dispose ()
 			{
-				modified = -1;
+				idx = NOT_STARTED;
 			}
+			
+			public bool MoveNext ()
+			{
+				if (ver != l.version)
+					throw new InvalidOperationException ();
+				
+				if (idx == NOT_STARTED)
+					idx = l.size;
+				
+				return idx != FINISHED && -- idx != FINISHED;
+			}
+			
+			public T Current {
+				get {
+					if (idx < 0)
+						throw new InvalidOperationException ();
+					
+					return l.data [l.size - 1 - idx];
+				}
+			}
+			
+			void IEnumerator.Reset ()
+			{
+				if (ver != l.version)
+					throw new InvalidOperationException ();
+				
+				idx = NOT_STARTED;
+			}
+			
+			object IEnumerator.Current {
+				get { return Current; }
+			}
+			
 		}
 	}
 }
