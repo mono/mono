41bba174ebfc49d0215989ce7b4c73ad252835eb
Duncan Mak <duncan@mono-cvs.ximian.com>
2004-12-29 04:19:23 +0000

    2004-12-28  Duncan Mak  <duncan@ximian.com>
    	* libodbc.cs: Change all the DllImport attributes to point to
    	"odbc32.dll" instead of "odbc32", because /etc/mono/config lists
    	"odbc32.dll" (all other entries in the file have the .dll suffix
    	as well, for consistency's sake, let's rename it here.)
    
    	This fixes #70727.
    	
    
    svn path=/trunk/mcs/; revision=38151

--- mcs/class/System.Data/System.Data.Odbc/libodbc.cs
+++ mcs/class/System.Data/System.Data.Odbc/libodbc.cs
@@ -124,107 +124,107 @@ namespace System.Data.Odbc
 			}
 		}
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLAllocHandle (OdbcHandleType HandleType, IntPtr InputHandle, ref IntPtr OutputHandlePtr);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLSetEnvAttr (IntPtr EnvHandle, OdbcEnv Attribute, IntPtr Value, int StringLength);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLConnect (IntPtr ConnectionHandle, string ServerName, short NameLength1, string UserName, short NameLength2, string Authentication, short NameLength3);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn  SQLDriverConnect(IntPtr ConnectionHandle, IntPtr WindowHandle, string InConnectionString, short StringLength1, string OutConnectionString, short BufferLength,	ref short StringLength2Ptr,	ushort DriverCompletion);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLExecDirect (IntPtr StatementHandle, string StatementText, int TextLength);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLRowCount (IntPtr StatementHandle, ref int RowCount);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLNumResultCols (IntPtr StatementHandle, ref short ColumnCount);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLFetch (IntPtr StatementHandle);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref bool TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref double TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref long TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref short TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref float TargetPtr, int BufferLen, ref int Len);
 	
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref OdbcTimestamp TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, ref int TargetPtr, int BufferLen, ref int Len);
 	
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetData (IntPtr StatementHandle, ushort ColumnNumber, OdbcType TargetType, byte[] TargetPtr, int BufferLen, ref int Len);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLDescribeCol(IntPtr StatementHandle, ushort ColumnNumber, byte[] ColumnName, short BufferLength, ref short NameLength, ref short DataType, ref uint ColumnSize, ref short DecimalDigits, ref short Nullable);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLFreeHandle(ushort HandleType, IntPtr SqlHandle);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLDisconnect(IntPtr ConnectionHandle);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLPrepare(IntPtr StatementHandle, string Statement, int TextLength);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLExecute(IntPtr StatementHandle);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLSetConnectAttr(IntPtr ConnectionHandle, OdbcConnectionAttribute Attribute, IntPtr Value, int Length);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLEndTran(int HandleType, IntPtr Handle, short CompletionType);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLBindParameter(IntPtr StatementHandle, ushort ParamNum, 
 				short InputOutputType, short ValueType, short ParamType, uint ColSize, 
 				short DecimalDigits, byte[] ParamValue, int BufLen, int StrLen);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLBindParameter(IntPtr StatementHandle, ushort ParamNum, 
 				short InputOutputType, short ValueType, short ParamType, uint ColSize, 
 				short DecimalDigits, ref int ParamValue, int BufLen, int StrLen);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLCancel(IntPtr StatementHandle);
 		
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLCloseCursor(IntPtr StatementHandle);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLError(IntPtr EnvironmentHandle,
 							   IntPtr ConnectionHandle, IntPtr StatementHandle,
 							   byte[] Sqlstate, ref int NativeError,
 							   byte[] MessageText, short BufferLength,
 							   ref short TextLength);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetStmtAttr(IntPtr StatementHandle,
 								int Attribute, ref IntPtr Value, int BufLen, int StrLen);
 
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLSetDescField(IntPtr DescriptorHandle,
 			short RecNumber, short FieldIdentifier, byte[] Value, int BufLen);
                         
-		[DllImport("odbc32")]
+		[DllImport("odbc32.dll")]
 		internal static extern OdbcReturn SQLGetDiagRec (OdbcHandleType HandleType,
 							   IntPtr Handle, ushort RecordNumber,
 							   byte [] Sqlstate, ref int NativeError,
