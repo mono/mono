c5976e500f77d42da4bd500d9e1270b12679b814
Jonathan Pryor <jpryor@novell.com>
2004-12-30 20:19:22 +0000

    * stdio.c: More wrapper functions.  This should complete C89's <stdio.h>     requirements.
    svn path=/trunk/mono/; revision=38202

--- support/stdio.c
+++ support/stdio.c
@@ -43,6 +43,94 @@ Mono_Posix_Stdlib_fwrite (const void *ptr, mph_size_t size, mph_size_t nmemb, FI
 	return fwrite (ptr, (size_t) size, (size_t) nmemb, stream);
 }
 
+gint32
+Mono_Posix_Stdlib__IOFBF (void)
+{
+	return _IOFBF;
+}
+
+gint32
+Mono_Posix_Stdlib__IOLBF (void)
+{
+	return _IOLBF;
+}
+
+gint32
+Mono_Posix_Stdlib__IONBF (void)
+{
+	return _IONBF;
+}
+
+gint32
+Mono_Posix_Stdlib_BUFSIZ (void)
+{
+	return BUFSIZ;
+}
+
+gint32
+Mono_Posix_Stdlib_L_tmpnam (void)
+{
+	return L_tmpnam;
+}
+
+FILE*
+Mono_Posix_Stdlib_stdin (void)
+{
+	return stdin;
+}
+
+FILE*
+Mono_Posix_Stdlib_stdout (void)
+{
+	return stdout;
+}
+
+FILE*
+Mono_Posix_Stdlib_stderr (void)
+{
+	return stderr;
+}
+
+gint32
+Mono_Posix_Stdlib_setvbuf (FILE *stream, char *buf, int mode, mph_size_t size)
+{
+	mph_return_if_size_t_overflow (size);
+	return setvbuf (stream, buf, mode, (size_t) size);
+}
+
+gint32
+Mono_Posix_Stdlib_fseek (FILE* stream, gint64 offset, int origin)
+{
+	mph_return_if_long_overflow (offset);
+
+	return fseek (stream, offset, origin);
+}
+
+gint64
+Mono_Posix_Stdlib_ftell (FILE* stream)
+{
+	return ftell (stream);
+}
+
+fpos_t*
+Mono_Posix_Stdlib_CreateFilePosition ()
+{
+	fpos_t* pos = malloc (sizeof(fpos_t));
+	return pos;
+}
+
+gint32
+Mono_Posix_Stdlib_fgetpos (FILE* stream, fpos_t *pos)
+{
+	return fgetpos (stream, pos);
+}
+
+gint32
+Mono_Posix_Stdlib_fsetpos (FILE* stream, fpos_t *pos)
+{
+	return fsetpos (stream, pos);
+}
+
 G_END_DECLS
 
 /*
