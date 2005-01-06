3ad471aad25665306a81fbce9731b866845d2b69
Sebastien Pouliot <sebastien@ximian.com>
2005-01-06 19:16:22 +0000

    2005-01-06  Sebastien Pouliot  <sebastien@ximian.com>
    	* DESTest.cs: Updated weak key and semi-weak key tests with more 
    	exhaustive versions.
    	* TripleDESTest.cs: Added tests when including a DES weak key into
    	a 2DES or 3DES key.
    
    
    svn path=/trunk/mcs/; revision=38455

--- mcs/class/corlib/Test/System.Security.Cryptography/DESCryptoServiceProviderTest.cs
+++ mcs/class/corlib/Test/System.Security.Cryptography/DESCryptoServiceProviderTest.cs
@@ -4,7 +4,26 @@
 // Author:
 //      Sebastien Pouliot  <sebastien@ximian.com>
 //
-// (C) 2004 Novell (http://www.novell.com)
+// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using System;
@@ -27,16 +46,16 @@ namespace MonoTests.System.Security.Cryptography {
 		public void KeyChecks () 
 		{
 			byte[] key = des.Key;
-			AssertEquals ("Key", 8, key.Length);
-			Assert ("IsWeakKey", !DES.IsWeakKey (key));
-			Assert ("IsSemiWeakKey", !DES.IsSemiWeakKey (key));
+			Assert.AreEqual (8, key.Length, "Key");
+			Assert.IsFalse (DES.IsWeakKey (key), "IsWeakKey");
+			Assert.IsFalse (DES.IsSemiWeakKey (key), "IsSemiWeakKey");
 		}
 
 		[Test]
 		public void IV () 
 		{
 			byte[] iv = des.IV;
-			AssertEquals ("IV", 8, iv.Length);
+			Assert.AreEqual (8, iv.Length, "IV");
 		}
 
 		// other tests (test vectors) are inherited from DESFIPS81Test
