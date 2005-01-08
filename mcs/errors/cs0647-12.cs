de73422a1efe750e0b6d03e8bbe82823cbe00f50
Miguel de Icaza <miguel@gnome.org>
2005-01-08 17:54:02 +0000

    New errors
    svn path=/trunk/mcs/; revision=38526

---
+++ mcs/errors/cs0647-12.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using System;
+
+class X {
+	[DllImport ("")]
+	extern static void Blah ();
+
+    static void Main (string [] args)
+    {
+    }
+
+}
---
+++ mcs/errors/cs0647-13.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using System;
+
+class X {
+	[DllImport (null)]
+	extern static void Blah ();
+
+    static void Main (string [] args)
+    {
+    }
+
+}
