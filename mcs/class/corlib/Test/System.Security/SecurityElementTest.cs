d01f7544c9cc2878f228f2d72ca61fe067bb9d22
Sebastien Pouliot <sebastien@ximian.com>
2005-01-10 15:31:38 +0000

    2005-01-10  Sebastien Pouliot  <sebastien@ximian.com>
    	* SecurityElementTest.cs: Add new tests for 2.0 methods. Adapted 
    	existing tests to deal with the fact that 2.0 doesn't indent the XML
    	attributes and child elements.
    
    
    svn path=/trunk/mcs/; revision=38625

--- mcs/class/corlib/Test/System.Security/SecurityElementTest.cs
+++ mcs/class/corlib/Test/System.Security/SecurityElementTest.cs
@@ -3,9 +3,29 @@
 //
 // Authors:
 //	Lawrence Pit (loz@cable.a2000.nl)
-//	Sebastien Pouliot (spouliot@motus.com)
+//	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // Portions (C) 2004 Motus Technologies Inc. (http://www.motus.com)
+// Copyright (C) 2005 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using NUnit.Framework;
@@ -322,8 +342,67 @@ namespace MonoTests.System.Security {
 			SecurityElement se = new SecurityElement ("Multiple");
 			se.AddAttribute ("Attribute1", "One");
 			se.AddAttribute ("Attribute2", "Two");
+#if NET_2_0
+			string expected = String.Format ("<Multiple Attribute1=\"One\"{0}Attribute2=\"Two\"/>{0}", Environment.NewLine);
+#else
 			string expected = String.Format ("<Multiple Attribute1=\"One\"{0}          Attribute2=\"Two\"/>{0}", Environment.NewLine);
+#endif
 			AssertEquals ("ToString()", expected, se.ToString ());
 		}
+
+#if NET_2_0
+		[Test]
+		public void Copy ()
+		{
+			SecurityElement se = SecurityElement.FromString ("<tag attribute=\"value\"><child attr=\"1\">mono</child><child/></tag>");
+			SecurityElement copy = se.Copy ();
+			Assert ("se!ReferenceEquals", !Object.ReferenceEquals (se, copy));
+			Assert ("c1=ReferenceEquals", Object.ReferenceEquals (se.Children [0], copy.Children [0]));
+			Assert ("c2=ReferenceEquals", Object.ReferenceEquals (se.Children [1], copy.Children [1]));
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void FromString_Null () 
+		{
+			SecurityElement.FromString (null);
+		}
+
+		[Test]
+		[ExpectedException (typeof (XmlSyntaxException))]
+		public void FromString_Empty ()
+		{
+			SecurityElement.FromString (String.Empty);
+		}
+
+		[Test]
+		[ExpectedException (typeof (XmlSyntaxException))]
+		public void FromString_NonXml ()
+		{
+			SecurityElement.FromString ("mono");
+		}
+
+		[Test]
+		public void FromString ()
+		{
+			SecurityElement se = SecurityElement.FromString ("<tag attribute=\"value\"><child attr=\"1\">mono</child><child/></tag>");
+			AssertEquals ("Tag", "tag", se.Tag);
+			AssertNull ("Text", se.Text);
+			AssertEquals ("Attribute.Count", 1, se.Attributes.Count);
+			AssertEquals ("Attribute", "value", se.Attribute ("attribute"));
+			AssertEquals ("Children.Count", 2, se.Children.Count);
+
+			SecurityElement child = (SecurityElement) se.Children [0];
+			AssertEquals ("Child1.Tag", "child", child.Tag);
+			AssertEquals ("Child1.Text", "mono", child.Text);
+			AssertEquals ("Child1.Attribute.Count", 1, child.Attributes.Count);
+			AssertEquals ("Child1.Attribute", "1", child.Attribute ("attr"));
+
+			child = (SecurityElement) se.Children [1];
+			AssertEquals ("Child2.Tag", "child", child.Tag);
+			AssertNull ("Child2.Text", child.Text);
+			AssertNull ("Child2.Attribute", child.Attributes);
+		}
+#endif
 	}
 }
