d466d8caffe29167e19c928275272106f1d57774
Raja R Harinath <harinath@hurrynot.org>
2005-01-11 13:49:17 +0000

    * test-334.cs: New test for #69519.
    svn path=/trunk/mcs/; revision=38693

---
+++ mcs/tests/test-334.cs
@@ -0,0 +1,18 @@
+namespace Test {
+	namespace Foo {
+		namespace Store {
+			public class Directory { }
+		}
+	}
+}
+
+namespace Test {
+	using Directory = Foo.Store.Directory;
+	namespace Foo {
+		namespace Index {
+			public class CompoundFileReader : Directory {
+				static void Main () { }
+			}
+		}
+	}
+}
