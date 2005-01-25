d02ebfe751a98d8436489ef9cf1c97548e2f4643
Martin Baulig <martin@novell.com>
2005-01-25 00:41:01 +0000

    Avoid CS0441: A static class cannot be sealed.
    svn path=/trunk/mcs/; revision=39447

--- mcs/class/corlib/System.Globalization/CharUnicodeInfo.cs
+++ mcs/class/corlib/System.Globalization/CharUnicodeInfo.cs
@@ -35,7 +35,7 @@ using System;
 namespace System.Globalization
 {
 	// FIXME: might be better to have them as icall.
-	public static sealed class CharUnicodeInfo
+	public static class CharUnicodeInfo
 	{
 		public static int GetDecimalDigitValue (char c)
 		{
