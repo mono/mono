48843d089a8fda57e752ffc54f9542de918c9d0e
Atsushi Eno <atsushieno@gmail.com>
2005-02-09 09:21:55 +0000

    2005-02-09  Atsushi Enomoto  <atsushi@ximian.com>
    	* XslForEach.cs : Don't bork at <xsl:for-each/>. Patch by Andrew Skiba.
    
    
    svn path=/trunk/mcs/; revision=40329

--- mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslForEach.cs
+++ mcs/class/System.XML/Mono.Xml.Xsl.Operations/XslForEach.cs
@@ -72,6 +72,9 @@ namespace Mono.Xml.Xsl.Operations {
 		
 		public override void Evaluate (XslTransformProcessor p)
 		{
+			if (children == null)
+				return;
+
 			p.PushNodeset (p.Select (select));
 			p.PushForEachContext ();
 			
