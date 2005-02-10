312544c4eff8d9da6f297a9c316b9f5b7ce59b57
Atsushi Eno <atsushieno@gmail.com>
2005-02-10 11:26:30 +0000

    2005-02-10  Atsushi Enomoto <atsushi@ximian.com>
    	* XmlWriterEmitter.cs : String.Replace() was insufficient for
    	  sequential candidates for replacement. Patch by Andrew Skiba.
    
    
    svn path=/trunk/mcs/; revision=40388

--- mcs/class/System.XML/Mono.Xml.Xsl/XmlWriterEmitter.cs
+++ mcs/class/System.XML/Mono.Xml.Xsl/XmlWriterEmitter.cs
@@ -115,13 +115,14 @@ namespace Mono.Xml.Xsl
 		}		
 
 		public override void WriteComment (string text) {
-			if (text.IndexOf ("--") >= 0)
+			//FIXME: horrible performance!!!
+			while (text.IndexOf ("--")>=0)
 				text = text.Replace ("--", "- -");
 
-			if (text.EndsWith ("-"))
-				writer.WriteComment (text + ' ');
-			else
-				writer.WriteComment (text);
+			if ((text.EndsWith("-")))
+				text += ' ';
+
+			writer.WriteComment (text);
 		}
 
 		public override void WriteProcessingInstruction (string name, string text)
