31be035834a5398aefd52017252793c35b949a8d
Zoltan Varga <vargaz@gmail.com>
2005-02-11 18:47:59 +0000

    Merge from HEAD.
    svn path=/branches/mono-1-0/mcs/; revision=40494

--- mcs/class/corlib/System/Type.cs
+++ mcs/class/corlib/System/Type.cs
@@ -32,6 +32,7 @@
 
 using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
@@ -562,6 +563,9 @@ namespace System {
 
 			if (Equals (c))
 				return true;
+
+			if (c is TypeBuilder)
+				return ((TypeBuilder)c).IsAssignableTo (this);
 
 			return type_is_assignable_from (this, c);
 		}
