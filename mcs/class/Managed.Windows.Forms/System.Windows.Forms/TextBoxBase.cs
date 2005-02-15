2f281fd20568a2d740aa525ee369489ea2bcdffd
Alexander Olk <aolk@mono-cvs.ximian.com>
2005-02-15 18:11:12 +0000

    Changing local method variables doesn't make sense. Fixes TextBox.Width being always 100.
    svn path=/trunk/mcs/; revision=40711

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextBoxBase.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextBoxBase.cs
@@ -5,10 +5,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -678,7 +678,7 @@ namespace System.Windows.Forms {
 				if (!multiline) {
 					if (height > PreferredHeight) {
 						text_bounds = new Rectangle(x, y, width, height);
-						height = PreferredHeight;
+						text_bounds.Height = PreferredHeight;
 					}
 				}
 			}
