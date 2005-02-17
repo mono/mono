154da2550a73d804fc67edbfd03966167d055aa1
Marek Safar <marek.safar@gmail.com>
2005-02-17 07:47:52 +0000

    2005-02-17  Marek Safar <marek.safar@seznam.cz>
    	* test-74.cs: Fixed to work with LF & CRLF.
    
    svn path=/trunk/mcs/; revision=40792

--- mcs/tests/test-74.cs
+++ mcs/tests/test-74.cs
@@ -1,7 +1,11 @@
+
 //
 // This test excercises #pre-processor directives in non-1 column
 // as well as the literal strings
-//
+// Warning: The first empty line is important
+
+using System.IO;
+
 class X {
 	#if Blah
 	#else
@@ -18,12 +22,20 @@ class X {
 		if (e != f)
 			return 2;
 
-		string g = "Hello" + System.Environment.NewLine + "world";
+		string g = "Hello\nworld";
+
+		using (StreamReader sr = new StreamReader("test-74.cs")) {
+			int i = sr.Read ();
+			if (sr.Read () <= 13)
+				g = g.Replace ("\n", "\r\n");
+		}
+
 		string h = @"Hello
 world";
 		if (g != h) 
 			return 3;
 
+		System.Console.WriteLine ("OK");
 		return 0;
 	}
 }
