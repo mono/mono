111b20366a5d1606268e23b6be0710990e951109
Paolo Molaro <lupus@oddwiz.org>
2005-02-17 18:44:34 +0000

    Added test case.
    svn path=/trunk/mcs/; revision=40817

---
+++ mcs/class/corlib/Test/System.Runtime.InteropServices/GCHandleTest.cs
@@ -0,0 +1,29 @@
+//
+// System.Runtime.InteropServices.GCHandle Test Cases
+//
+// Authors:
+// 	Paolo Molaro (lupus@ximian.com)
+//
+// (c) 2005 Novell, Inc. (http://www.novell.com)
+//
+
+using NUnit.Framework;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MonoTests.System.Runtime.InteropServices
+{
+	[TestFixture]
+	public class GCHandleTest : Assertion
+	{
+		static GCHandle handle;
+
+		[Test]
+		public void DefaultZeroValue ()
+		{
+			AssertEquals (false, handle.IsAllocated);
+		}
+
+	}
+}
+
