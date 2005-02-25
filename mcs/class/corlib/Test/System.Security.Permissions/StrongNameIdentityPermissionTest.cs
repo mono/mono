b8a9fe561370d8569f44c9b7983e753f8a810bdd
Sebastien Pouliot <sebastien@ximian.com>
2005-02-25 02:48:31 +0000

    2005-02-24  Sebastien Pouliot  <sebastien@ximian.com>
    	* StrongNameIdentityPermissionTest.cs: Fixed some bad tests and 
    	re-enabled the tests on NET_2_0 profile.
    
    
    svn path=/trunk/mcs/; revision=41173

--- mcs/class/corlib/Test/System.Security.Permissions/StrongNameIdentityPermissionTest.cs
+++ mcs/class/corlib/Test/System.Security.Permissions/StrongNameIdentityPermissionTest.cs
@@ -40,8 +40,16 @@ namespace MonoTests.System.Security.Permissions {
 		static byte[] ecma = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 #if NET_2_0
-		[Category ("NotWorking")]
+		private StrongNameIdentityPermission GetUnion ()
+		{
+			StrongNamePublicKeyBlob blob = new StrongNamePublicKeyBlob (ecma);
+			StrongNameIdentityPermission snip = new StrongNameIdentityPermission (blob, "mono", new Version (1, 2, 3, 4));
+			StrongNamePublicKeyBlob blob2 = new StrongNamePublicKeyBlob (new byte[16]);
+			StrongNameIdentityPermission diffPk = new StrongNameIdentityPermission (blob2, "mono", new Version (1, 2, 3, 4));
+			return (StrongNameIdentityPermission)snip.Union (diffPk);
+		}
 #endif
+
 		[Test]
 		public void PermissionStateNone ()
 		{
@@ -73,7 +81,6 @@ namespace MonoTests.System.Security.Permissions {
 		}
 #if NET_2_0
 		[Test]
-		[Category ("NotWorking")]
 		public void PermissionStateUnrestricted ()
 		{
 			// In 2.0 Unrestricted are permitted for identity permissions
@@ -86,12 +93,10 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.IsNull (se.Attribute ("PublicKeyBlob"), "Xml-PublicKeyBlob");
 			Assert.IsNull (se.Attribute ("AssemblyVersion"), "Xml-AssemblyVersion");
 			StrongNameIdentityPermission copy = (StrongNameIdentityPermission)snip.Copy ();
-			// Strangely once copied the Name becomes equals to String.Empty in 2.0 [FDBK19351]
-			Assert.AreEqual (String.Empty, copy.Name, "Copy-Name");
-			Assert.IsNull (copy.PublicKey, "Copy-PublicKey");
-			Assert.IsNull (se.Attribute ("AssemblyVersion"), "Copy-Version");
+			Assert.IsTrue (snip.Equals (copy), "snip Equals copy");
+			Assert.IsTrue (copy.Equals (snip), "copy Equals snip");
 			// and they aren't equals to None
-			Assert.IsFalse (snip.Equals (new StrongNameIdentityPermission (PermissionState.None)));
+			Assert.IsFalse (snip.Equals (new StrongNameIdentityPermission (PermissionState.None)), "Not Equals None");
 		}
 #else
 		[Test]
@@ -130,6 +135,7 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.AreEqual ("1.2", se.Attribute ("AssemblyVersion"), "Xml-AssemblyVersion");
 
 			StrongNameIdentityPermission copy = (StrongNameIdentityPermission)snip.Copy ();
+			se = copy.ToXml ();
 			Assert.IsNull (se.Attribute ("Name"), "Copy-Name");
 			Assert.AreEqual ("00000000000000000400000000000000", se.Attribute ("PublicKeyBlob"), "Copy-PublicKeyBlob");
 			Assert.AreEqual ("1.2", se.Attribute ("AssemblyVersion"), "Copy-AssemblyVersion");
@@ -143,9 +149,6 @@ namespace MonoTests.System.Security.Permissions {
 		{
 			StrongNamePublicKeyBlob blob = new StrongNamePublicKeyBlob (ecma);
 			StrongNameIdentityPermission snip = new StrongNameIdentityPermission (blob, String.Empty, new Version (1, 2));
-#if !NET_2_0
-			// TODO
-#endif
 		}
 
 		[Test]
@@ -214,6 +217,26 @@ namespace MonoTests.System.Security.Permissions {
 #endif
 		}
 
+#if NET_2_0
+		[Test]
+		[ExpectedException (typeof (NotSupportedException))]
+		public void Name_Multiple_Get ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			string s = union.Name;
+		}
+
+		[Test]
+		public void Name_Multiple_Set ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			union.Name = "oops";
+			Assert.AreEqual ("oops", union.Name, "Name");
+			Assert.IsNull (union.PublicKey, "PublicKey");
+			Assert.AreEqual ("0.0", union.Version.ToString (), "Version");
+		}
+#endif
+
 		[Test]
 		[ExpectedException (typeof (ArgumentNullException))]
 		public void PublicKey_Null ()
@@ -222,7 +245,25 @@ namespace MonoTests.System.Security.Permissions {
 			StrongNameIdentityPermission snip = new StrongNameIdentityPermission (blob, "mono", new Version (1, 2, 3, 4));
 			snip.PublicKey = null;
 		}
+#if NET_2_0
+		[Test]
+		[ExpectedException (typeof (NotSupportedException))]
+		public void PublicKey_Multiple_Get ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			StrongNamePublicKeyBlob snpkb = union.PublicKey;
+		}
 
+		[Test]
+		public void PublicKey_Multiple_Set ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			union.PublicKey = new StrongNamePublicKeyBlob (ecma);
+			Assert.AreEqual (String.Empty, union.Name, "Name");
+			Assert.IsNotNull (union.PublicKey, "PublicKey");
+			Assert.AreEqual ("0.0", union.Version.ToString (), "Version");
+		}
+#endif
 		[Test]
 		public void Version ()
 		{
@@ -234,9 +275,24 @@ namespace MonoTests.System.Security.Permissions {
 			snip.Version = new Version (1, 2, 3);
 			Assert.AreEqual ("1.2.3", snip.Version.ToString (), "Version-3");
 		}
-
 #if NET_2_0
-		[Category ("NotWorking")]
+		[Test]
+		[ExpectedException (typeof (NotSupportedException))]
+		public void Version_Multiple_Get ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			Version v = union.Version;
+		}
+
+		[Test]
+		public void Version_Multiple_Set ()
+		{
+			StrongNameIdentityPermission union = GetUnion ();
+			union.Version = new Version ("1.2.3.4");
+			Assert.AreEqual (String.Empty, union.Name, "Name");
+			Assert.IsNull (union.PublicKey, "PublicKey");
+			Assert.AreEqual ("1.2.3.4", union.Version.ToString (), "Version");
+		}
 #endif
 		[Test]
 		public void Copy_NameEmpty ()
@@ -246,12 +302,12 @@ namespace MonoTests.System.Security.Permissions {
 			snip.Version = new Version ("1.2.3.4");
 
 			// because Name == String.Empty, which is illegal using the other constructor
-			// but (somewhat) required to copy the two other informations
+			// but (somewhat) required to copy the teo other informations
 			StrongNameIdentityPermission copy = (StrongNameIdentityPermission)snip.Copy ();
 #if NET_2_0
 			Assert.IsTrue (copy.Equals (snip), "Equals");
 #else
-			Assert.IsFalse (copy.Equals (snip), "Equals");
+			Assert.AreEqual (copy.ToXml ().ToString (), snip.ToXml ().ToString (), "Equals-XML");
 #endif
 		}
 
@@ -262,10 +318,7 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.AreEqual (p1.Version, p2.Version, prefix + ".Version");
 			Assert.IsFalse (Object.ReferenceEquals (p1, p2), "ReferenceEquals");
 		}
-		
-#if NET_2_0
-		[Category ("NotWorking")]
-#endif
+
 		[Test]
 		public void Intersect ()
 		{
@@ -294,7 +347,6 @@ namespace MonoTests.System.Security.Permissions {
 		[Test]
 #if NET_2_0
 		[ExpectedException (typeof (ArgumentException))]
-		[Category ("NotWorking")]
 #endif
 		public void Intersect_DifferentPermissions ()
 		{
@@ -312,6 +364,26 @@ namespace MonoTests.System.Security.Permissions {
 
 			StrongNameIdentityPermission empty = new StrongNameIdentityPermission (PermissionState.None);
 			Assert.IsTrue (empty.IsSubsetOf (null), "empty.IsSubsetOf (null)");
+		}
+
+		[Test]
+		public void IsSubsetOf_Corlib ()
+		{
+			Version fx11 = new Version (1, 0, 5000, 0);
+			StrongNamePublicKeyBlob blob = new StrongNamePublicKeyBlob (ecma);
+			StrongNameIdentityPermission granted = new StrongNameIdentityPermission (blob, "mscorlib", fx11);
+
+			StrongNameIdentityPermission demanded = new StrongNameIdentityPermission (blob, null, null);
+			Assert.IsTrue (demanded.IsSubsetOf (granted), "pk");
+
+			demanded = new StrongNameIdentityPermission (blob, "mscorlib", null);
+			Assert.IsTrue (demanded.IsSubsetOf (granted), "pk+name");
+
+			demanded = new StrongNameIdentityPermission (blob, null, fx11);
+			Assert.IsTrue (demanded.IsSubsetOf (granted), "pk+v");
+
+			demanded = new StrongNameIdentityPermission (blob, "mscorlib", fx11);
+			Assert.IsTrue (demanded.IsSubsetOf (granted), "pk+name+v");
 		}
 
 		[Test]
@@ -357,9 +429,6 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.IsTrue (samePk.IsSubsetOf (union), "snip.IsSubsetOf (union)");
 		}
 
-#if NET_2_0
-		[Category ("NotWorking")]
-#endif
 		[Test]
 		public void Union_DifferentPk ()
 		{
@@ -377,12 +446,11 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.AreEqual ("00000000000000000000000000000000", (se.Children [1] as SecurityElement).Attribute ("PublicKeyBlob"), "Blob#2");
 			// strangely it is still versioned as 'version="1"'.
 			Assert.AreEqual ("1", se.Attribute ("version"), "Version");
+#else
+			Assert.IsNull (result, "DifferentPk");
 #endif
 		}
 
-#if NET_2_0
-		[Category ("NotWorking")]
-#endif
 		[Test]
 		public void Union_SamePublicKey_DifferentName ()
 		{
@@ -399,12 +467,11 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.AreEqual ("novell", (se.Children [1] as SecurityElement).Attribute ("Name"), "Name#2");
 			// strangely it is still versioned as 'version="1"'.
 			Assert.AreEqual ("1", se.Attribute ("version"), "Version");
+#else
+			Assert.IsNull (result, "DifferentName");
 #endif
 		}
 
-#if NET_2_0
-		[Category ("NotWorking")]
-#endif
 		[Test]
 		public void Union_SamePublicKey_DifferentVersion ()
 		{
@@ -421,6 +488,8 @@ namespace MonoTests.System.Security.Permissions {
 			Assert.AreEqual ("1.2", (se.Children [1] as SecurityElement).Attribute ("AssemblyVersion"), "AssemblyVersion#2");
 			// strangely it is still versioned as 'version="1"'.
 			Assert.AreEqual ("1", se.Attribute ("version"), "Version");
+#else
+			Assert.IsNull (result, "DifferentVersion");
 #endif
 		}
 
