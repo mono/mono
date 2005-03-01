14975eec5bdbb65da5258468e3b36a37d8a5e4f6
Marek Safar <marek.safar@gmail.com>
2005-03-01 08:45:38 +0000

    2005-03-01  Marek Safar <marek.safar@seznam.cz>
    	* test-352.cs: New test.
    	
    	* test-353.cs, test-353-2: Bug #73117
    
    svn path=/trunk/mcs/; revision=41310

---
+++ mcs/tests/test-353-2.cs
@@ -0,0 +1,8 @@
+using System;
+ 
+public class Three {
+        public static void Main ()
+        {
+                One o = new One ();
+        }
+}
---
+++ mcs/tests/test-353.cs
@@ -0,0 +1,12 @@
+// Compiler options: ../test-353-2.cs
+
+using System;
+ 
+[Obsolete]
+public class One { }
+ 
+#pragma warning disable 612
+ 
+public class Two {
+        private One one;
+}
