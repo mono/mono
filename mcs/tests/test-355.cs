76584b8e4117858141aaad379071d33278ea3eb1
Marek Safar <marek.safar@gmail.com>
2005-03-08 12:45:09 +0000

    2005-03-08  Marek Safar <marek.safar@seznam.cz>
    	* test-355.cs, test-356.cs: New tests.
    
    svn path=/trunk/mcs/; revision=41564

---
+++ mcs/tests/test-355.cs
@@ -0,0 +1,8 @@
+namespace Level1 {
+        namespace Level1.Level2 {
+                public class MainClass {
+                        public static void Main() {
+                        }
+                }
+        }
+}
---
+++ mcs/tests/test-356.cs
@@ -0,0 +1,6 @@
+public class MainClass {
+        static void Main () {
+                const int size = sizeof(int);
+        }
+}
+
