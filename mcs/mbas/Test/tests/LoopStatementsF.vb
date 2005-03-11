63a77d291cfe680c518171bfc8b1e47bf2c30bb6
Manjula GHM <manjula@mono-cvs.ximian.com>
2005-03-11 09:21:28 +0000

    Reverting previous checkin as it was wrong file
    svn path=/trunk/mcs/; revision=41683

--- mcs/mbas/Test/tests/LoopStatementsF.vb
+++ mcs/mbas/Test/tests/LoopStatementsF.vb
@@ -8,8 +8,8 @@ Module Test
 		Dim i 
 		For i = 9 to 0 step 1			
 		Next 
-		if i<>10 then
-			throw new System.Exception("For loop not working. Expected 9 but got "&i)
+		if i<>9 then
+			throw new System.Exception("For loop not working. Expected 9 but got " &i)
 		End if
     End Sub
 End Module
