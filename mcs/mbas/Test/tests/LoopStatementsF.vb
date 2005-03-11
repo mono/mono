70de884daa13b7acb88af00c5c099d8d09baf5e5
Manjula GHM <manjula@mono-cvs.ximian.com>
2005-03-11 09:12:54 +0000

    Minor fix in the LoopStatement test case
    svn path=/trunk/mcs/; revision=41682

--- mcs/mbas/Test/tests/LoopStatementsF.vb
+++ mcs/mbas/Test/tests/LoopStatementsF.vb
@@ -8,7 +8,7 @@ Module Test
 		Dim i 
 		For i = 9 to 0 step 1			
 		Next 
-		if i<>9 then
+		if i<>10 then
 			throw new System.Exception("For loop not working. Expected 9 but got "&i)
 		End if
     End Sub
