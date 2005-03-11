1a71008473826bdc3cbba78efc48577e5f4a67eb
Lluis Sanchez <lluis@novell.com>
2005-03-11 13:23:59 +0000

    2005-03-11  Lluis Sanchez Gual <lluis@novell.com>
    	* AssemblyInfo.cs: Registered GridView.js script.
    
    
    svn path=/trunk/mcs/; revision=41695

--- mcs/class/System.Web/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Web/Assembly/AssemblyInfo.cs
@@ -110,6 +110,7 @@ using System.Web.UI;
 
 [assembly: WebResource ("TreeView.js", "text/javascript")]
 [assembly: WebResource ("Menu.js", "text/javascript")]
+[assembly: WebResource ("GridView.js", "text/javascript")]
 [assembly: WebResource ("callback.js", "text/javascript")]
 [assembly: WebResource ("webform.js", "text/javascript")]
 
