2039ef93cee397037beb99724cc3ec8eb87cc596
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-03-15 01:40:13 +0000

    2005-03-14 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* PropertyValueCollection.cs: the Value property for an empty collection
    	is now null and setting Value to null clears the collection. Patch by
    	Boris Kirzner (Mainsoft).
    
    svn path=/branches/mono-1-0/mcs/; revision=41816

--- mcs/class/System.DirectoryServices/System.DirectoryServices/PropertyValueCollection.cs
+++ mcs/class/System.DirectoryServices/System.DirectoryServices/PropertyValueCollection.cs
@@ -157,25 +157,27 @@ namespace System.DirectoryServices
 		{
 			get
 			{
-				if(this.Count==1)
-				{
-					return (object) List[0];
-				}
-				else
-				{
+				switch (Count) {
+					case 0 : 
+						return null;
+					case 1 :
+						return (object) List[0];
+					default :
 //					System.Object[] oArray= new System.Object[this.Count];
 //					object[] oArray= new object[this.Count];
 //					Array.Copy((System.Array)List,0,(System.Array)oArray,0,this.Count);
-					Array LArray = Array.CreateInstance( Type.GetType("System.Object"), this.Count );
-					for ( int i = LArray.GetLowerBound(0); i <= LArray.GetUpperBound(0); i++ )
-						LArray.SetValue( List[i], i );
-					return LArray;
+						Array LArray = Array.CreateInstance( Type.GetType("System.Object"), this.Count );
+						for ( int i = LArray.GetLowerBound(0); i <= LArray.GetUpperBound(0); i++ )
+							LArray.SetValue( List[i], i );
+						return LArray;
 				}
 			}
 			set
 			{
 				List.Clear();
-				Add(value);
+				if (value != null) {
+					Add(value);
+				}
 			}
 		}
 
