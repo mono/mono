c18c4cf88a1368d1cb3d052fc4b80acb27d63838
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-03-15 01:44:02 +0000

    2005-03-14 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* DirectoryEntry.cs: username and password are now initialized to null.
    	Patch by Boris Kirzner (Mainsoft).
    
    
    svn path=/branches/mono-1-0/mcs/; revision=41818

--- mcs/class/System.DirectoryServices/System.DirectoryServices/DirectoryEntry.cs
+++ mcs/class/System.DirectoryServices/System.DirectoryServices/DirectoryEntry.cs
@@ -54,8 +54,8 @@ namespace System.DirectoryServices
 		private string _Path="";
 		private string _Name=null;
 		private DirectoryEntry _Parent=null;
-		private string _Username="";
-		private string _Password="";
+		private string _Username;
+		private string _Password;
 		//private string _Nativeguid;
 		private PropertyCollection _Properties = null;
 		private string _SchemaClassName=null;
