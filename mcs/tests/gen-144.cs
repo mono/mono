275414c348a0697e26b1eaea3b829d28d2cf2c2f
Martin Baulig <martin@novell.com>
2005-03-17 10:31:47 +0000

    New test.
    svn path=/trunk/mcs/; revision=41933

---
+++ mcs/tests/gen-144.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+	public class Application
+	{
+		public static void Main()
+		{
+			string[] array = new string[10];
+
+			System.Collections.Generic.IEnumerable<string> enumer = array;
+		}
+	}
+}
