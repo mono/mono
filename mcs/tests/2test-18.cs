1ca27704b51188d072e5d32dff7ed727610aee97
Martin Baulig <martin@novell.com>
2005-03-17 16:54:10 +0000

    Ooops.
    svn path=/trunk/mcs/; revision=41951

--- mcs/tests/2test-18.cs
+++ mcs/tests/2test-18.cs
@@ -1,11 +1,11 @@
 public class Foo {
 	delegate Inner foo_fn(string s);
-	public static void Main(string[] args)
+	public static void Main()
 	{
 		foo_fn f = delegate (string s) {
 			return new Inner(s + s);
 		};
-		f (args[0]);
+		f ("Test");
 	}
 
 	class Inner
