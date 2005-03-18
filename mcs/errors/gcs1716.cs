6a1e4ec9ead8fd9733cb00a658bc987ecca0b3eb
Marek Safar <marek.safar@gmail.com>
2005-03-18 13:32:27 +0000

    new tests
    svn path=/trunk/mcs/; revision=41981

---
+++ mcs/errors/gcs1716.cs
@@ -0,0 +1,8 @@
+// cs1716.cs: Do not use 'System.Runtime.CompilerServices.FixedBuffer' attribute. Use the 'fixed' field modifier instead
+// Line: 6
+
+public class C
+{
+    [System.Runtime.CompilerServices.FixedBuffer (typeof (int), 1)]
+    public int i;
+}
