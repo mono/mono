fd45e86ed902456e1b49aff1841e1817f14df185
Marek Safar <marek.safar@gmail.com>
2005-03-22 11:01:33 +0000

    new error tests
    svn path=/trunk/mcs/; revision=42096

---
+++ mcs/errors/cs1717-3.cs
@@ -0,0 +1,13 @@
+// cs1717.cs: Assignment made to same variable; did you mean to assign something else?
+// Line: 9
+// Compiler options: -warnaserror -warn:3
+
+class A
+{    
+    int i;
+    
+    void C ()
+    {
+	i = i;
+    }
+}
