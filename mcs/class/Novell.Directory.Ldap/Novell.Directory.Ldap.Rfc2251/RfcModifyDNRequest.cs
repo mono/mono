c0eeb5609fb904207eda169bcf7c29c70936ffd7
Boris Kirzner <borisk@mono-cvs.ximian.com>
2005-03-29 13:27:20 +0000

    According to RFC 2251 newSuperior should be sent as context-specific 0 type rather than octet string.
    svn path=/trunk/mcs/; revision=42336

--- mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Rfc2251/RfcModifyDNRequest.cs
+++ mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Rfc2251/RfcModifyDNRequest.cs
@@ -52,6 +52,16 @@ namespace Novell.Directory.Ldap.Rfc2251
 		//*************************************************************************
 		// Constructors for ModifyDNRequest
 		//*************************************************************************
+
+		// according to RFC 2251 :
+		// ModifyDNRequest ::= [APPLICATION 12] SEQUENCE {
+		//   entry           LDAPDN,
+		//   newrdn          RelativeLDAPDN,
+		//   deleteoldrdn    BOOLEAN,
+		//   newSuperior     [0] LDAPDN OPTIONAL
+		// }
+		// i.e. newSuperior is a context-specific 0.
+		static readonly Asn1Identifier superiorId = new Asn1Identifier(Asn1Identifier.CONTEXT,false,0x0);
 		
 		/// <summary> </summary>
 		public RfcModifyDNRequest(RfcLdapDN entry, RfcRelativeLdapDN newrdn, Asn1Boolean deleteoldrdn):this(entry, newrdn, deleteoldrdn, null)
@@ -64,8 +74,10 @@ namespace Novell.Directory.Ldap.Rfc2251
 			add(entry);
 			add(newrdn);
 			add(deleteoldrdn);
-			if (newSuperior != null)
+			if (newSuperior != null) {
+				newSuperior.setIdentifier(superiorId);
 				add(newSuperior);
+			}
 		}
 		
 		/// <summary> Constructs a new Delete Request copying from the ArrayList of
