d157b40c98b7805d5509c4599e080900e0af68f6
Marek Safar <marek.safar@gmail.com>
2005-03-30 12:14:32 +0000

    new 2.0 test
    svn path=/trunk/mcs/; revision=42368

---
+++ mcs/errors/gcs1689.cs
@@ -0,0 +1,8 @@
+// cs1689.cs: Attribute 'System.Diagnostics.ConditionalAttribute' is only valid on methods or attribute classes
+// Line: 6
+
+using System.Diagnostics;
+
+[Conditional("DEBUG")]
+public class Test {}
+    
