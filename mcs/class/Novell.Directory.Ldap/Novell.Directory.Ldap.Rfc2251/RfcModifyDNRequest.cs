3123437ec767d1ba498d1a9ec272b24d13712d64
Boris Kirzner <borisk@mono-cvs.ximian.com>
2005-04-05 15:22:59 +0000

    Fix for : According to RFC 2251 newSuperior should be sent as context-specific 0 type rather than octet string. RfcLdapSuperDN.cs : added new class that represents context-specific 0 type. RfcModifyDNRequest.cs : constructor now receives RfcLdapSuperDN.
    svn path=/trunk/mcs/; revision=42574

--- mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Rfc2251/RfcModifyDNRequest.cs
+++ mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Rfc2251/RfcModifyDNRequest.cs
@@ -52,30 +52,19 @@ namespace Novell.Directory.Ldap.Rfc2251
 		//*************************************************************************
 		// Constructors for ModifyDNRequest
 		//*************************************************************************
-
-		// according to RFC 2251 :
-		// ModifyDNRequest ::= [APPLICATION 12] SEQUENCE {
-		//   entry           LDAPDN,
-		//   newrdn          RelativeLDAPDN,
-		//   deleteoldrdn    BOOLEAN,
-		//   newSuperior     [0] LDAPDN OPTIONAL
-		// }
-		// i.e. newSuperior is a context-specific 0.
-		static readonly Asn1Identifier superiorId = new Asn1Identifier(Asn1Identifier.CONTEXT,false,0x0);
-		
+	
 		/// <summary> </summary>
 		public RfcModifyDNRequest(RfcLdapDN entry, RfcRelativeLdapDN newrdn, Asn1Boolean deleteoldrdn):this(entry, newrdn, deleteoldrdn, null)
 		{
 		}
 		
 		/// <summary> </summary>
-		public RfcModifyDNRequest(RfcLdapDN entry, RfcRelativeLdapDN newrdn, Asn1Boolean deleteoldrdn, RfcLdapDN newSuperior):base(4)
+		public RfcModifyDNRequest(RfcLdapDN entry, RfcRelativeLdapDN newrdn, Asn1Boolean deleteoldrdn, RfcLdapSuperDN newSuperior):base(4)
 		{
 			add(entry);
 			add(newrdn);
 			add(deleteoldrdn);
 			if (newSuperior != null) {
-				newSuperior.setIdentifier(superiorId);
 				add(newSuperior);
 			}
 		}
