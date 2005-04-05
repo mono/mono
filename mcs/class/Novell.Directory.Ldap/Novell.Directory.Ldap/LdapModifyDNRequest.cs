c15225ae386b6ce29e95505d6b71ebe8b20e4f41
Boris Kirzner <borisk@mono-cvs.ximian.com>
2005-04-05 15:24:45 +0000

    LdapModifyDNRequest.cs : create RfcModifyDNRequest with RfcLdapSuperDN instead of RfcLdapDN.
    svn path=/trunk/mcs/; revision=42575

--- mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap/LdapModifyDNRequest.cs
+++ mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap/LdapModifyDNRequest.cs
@@ -139,7 +139,7 @@ namespace Novell.Directory.Ldap
 		/// <param name="cont">           Any controls that apply to the modifyDN request,
 		/// or null if none.
 		/// </param>
-		public LdapModifyDNRequest(System.String dn, System.String newRdn, System.String newParentdn, bool deleteOldRdn, LdapControl[] cont):base(LdapMessage.MODIFY_RDN_REQUEST, new RfcModifyDNRequest(new RfcLdapDN(dn), new RfcRelativeLdapDN(newRdn), new Asn1Boolean(deleteOldRdn), ((System.Object) newParentdn != null)?new RfcLdapDN(newParentdn):null), cont)
+		public LdapModifyDNRequest(System.String dn, System.String newRdn, System.String newParentdn, bool deleteOldRdn, LdapControl[] cont):base(LdapMessage.MODIFY_RDN_REQUEST, new RfcModifyDNRequest(new RfcLdapDN(dn), new RfcRelativeLdapDN(newRdn), new Asn1Boolean(deleteOldRdn), ((System.Object) newParentdn != null)?new RfcLdapSuperDN(newParentdn):null), cont)
 		{
 			return ;
 		}
