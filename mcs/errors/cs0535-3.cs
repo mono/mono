b49aafaf624d34c10d9952b1360e3fb3819c73f0
Raja R Harinath <harinath@hurrynot.org>
2005-04-08 08:22:49 +0000

    * cs0535-3.cs: New test from #58413.
    svn path=/trunk/mcs/; revision=42675

---
+++ mcs/errors/cs0535-3.cs
@@ -0,0 +1,18 @@
+// cs0535.cs: 'Test' does not implement interface member 'X.Hola(ref string)'
+// Line: 9
+
+using System;
+interface X {
+	void Hola (ref string name);
+}
+
+class Test : X {
+	static void Main ()
+	{
+	}
+
+	public void Hola (out string name)
+	{
+		name = null;
+	}
+}
