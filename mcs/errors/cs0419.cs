dc8e09188d98d1011816c3703c3053ec894356ea
Atsushi Eno <atsushieno@gmail.com>
2005-04-17 12:39:24 +0000

    2005-04-17  Atsushi Enomoto  <atsushi@ximian.com>
    	* cs-1574-6.cs, cs-0419.cs : added new tests (bug #71603 and #71605).
    
    	* xml-037.cs, xml-037-ref.xml,
    	  xml-038.cs, xml-038-ref.xml : added new tests for bug #74633.
    
    
    svn path=/trunk/mcs/; revision=43144

---
+++ mcs/errors/cs0419.cs
@@ -0,0 +1,7 @@
+// Compiler options: -doc:dummy.xml -warn:3 -warnaserror
+/// <summary>
+/// Exposes <see cref="System.String.Replace"/> to XSLT
+/// </summary>
+public class Test {
+}
+
---
+++ mcs/errors/cs1574-6.cs
@@ -0,0 +1,11 @@
+// Compiler options: -doc:dummy.xml -warnaserror -warn:1
+/// <summary>
+/// <see cref="MyDelegate(int)" />
+/// </summary>
+public class Test {
+        /// <summary>
+        /// whatever
+        /// </summary>
+        public delegate void MyDelegate(int i);
+}
+
