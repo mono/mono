4fd1ddf2b9b79c696755d7547b88756d2f4a0faf
Sebastien Pouliot <sebastien@ximian.com>
2005-04-18 12:57:51 +0000

    2005-04-18  Sebastien Pouliot  <sebastien@ximian.com>
    	* SymmetricTransform.cs: Fixed a division by zero if someone changes 
    	the feedback value to 0.
    
    
    svn path=/branches/mono-1-0/mcs/; revision=43212

--- mcs/class/Mono.Security/Mono.Security.Cryptography/SymmetricTransform.cs
+++ mcs/class/Mono.Security/Mono.Security.Cryptography/SymmetricTransform.cs
@@ -6,32 +6,7 @@
 //	Sebastien Pouliot <sebastien@ximian.com>
 //
 // Portions (C) 2002, 2003 Motus Technologies Inc. (http://www.motus.com)
-// (C) 2004 Novell (http://www.novell.com)
-//
-
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-
-//
-// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -90,7 +65,8 @@ namespace Mono.Security.Cryptography {
 			Buffer.BlockCopy (rgbIV, 0, temp, 0, BlockSizeByte);
 			temp2 = new byte [BlockSizeByte];
 			FeedBackByte = (algo.FeedbackSize >> 3);
-			FeedBackIter = (int) BlockSizeByte / FeedBackByte;
+			if (FeedBackByte != 0)
+				FeedBackIter = (int) BlockSizeByte / FeedBackByte;
 			// transform buffers
 			workBuff = new byte [BlockSizeByte];
 			workout =  new byte [BlockSizeByte];
