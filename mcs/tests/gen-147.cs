70969f5958a0dc7eecbc84721bb8a773c1756c16
Martin Baulig <martin@novell.com>
2005-04-22 16:08:45 +0000

    New tests.
    svn path=/trunk/mcs/; revision=43449

---
+++ mcs/tests/gen-147.cs
@@ -0,0 +1,38 @@
+namespace A
+{
+    interface IFoo
+    {
+        void Hello (IFoo foo);
+    }
+}
+
+namespace B
+{
+  partial class Test <T> : IDoo, A.IFoo where T : A.IFoo
+    { }
+}
+
+namespace B
+{
+    using A;
+
+    partial class Test <T> : Y, IFoo where T : IFoo
+    {
+        void IFoo.Hello (IFoo foo)
+        { }
+    }
+}
+
+interface IDoo { }
+
+class Y { }
+
+class X
+{
+    static void Main ()
+    { }
+}
+
+
+
+
