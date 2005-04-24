9e6c973211abf8ce2625aaf8fd6e713ee9826743
Sebastien Pouliot <sebastien@ximian.com>
2005-04-24 15:42:31 +0000

    2005-04-24  Sebastien Pouliot  <sebastien@ximian.com>
    	* SubjectIdentifierType.cs: Removed (really) obsolete enum value and
    	[Serializable] attribute.
    
    
    svn path=/trunk/mcs/; revision=43513

--- mcs/class/System.Security/System.Security.Cryptography.Pkcs/SubjectIdentifierType.cs
+++ mcs/class/System.Security/System.Security.Cryptography.Pkcs/SubjectIdentifierType.cs
@@ -5,7 +5,7 @@
 //	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // (C) 2003 Motus Technologies Inc. (http://www.motus.com)
-// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -29,17 +29,13 @@
 
 #if NET_2_0
 
-using System;
-
 namespace System.Security.Cryptography.Pkcs {
 
-	[Serializable]
 	public enum SubjectIdentifierType {
 		Unknown,
 		IssuerAndSerialNumber,
 		SubjectKeyIdentifier,
-		[Obsolete] HashOnly,
-		NoSignature = 3
+		NoSignature
 	}
 }
 
