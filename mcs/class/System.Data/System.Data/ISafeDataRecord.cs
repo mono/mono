702e06babf559ad900bf769960866a3811a6d1ac
Boris Kirzner <borisk@mono-cvs.ximian.com>
2005-04-26 17:18:11 +0000

    Drop of Mainsoft.System.Data
    svn path=/branches/Mainsoft.System.Data/mcs/; revision=43605

---
+++ mcs/class/System.Data/System.Data/ISafeDataRecord.cs
@@ -0,0 +1,63 @@
+//
+// System.Data.ISafeDataRecord
+//
+// Author:
+//   Boris Kirzner (borisk@mainsoft.com)
+//
+using System;
+
+namespace System.Data
+{
+	internal interface ISafeDataRecord
+	{
+		bool GetBooleanSafe(int i);
+
+		byte GetByteSafe(int i);
+
+		//long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferOffset, int length);
+
+		char GetCharSafe(int i);
+
+		//long GetChars(int i, long fieldOffset, char[] buffer, int bufferOffset, int length);
+
+		//IDataReader GetData(int i);
+
+		//string GetDataTypeName(int i);
+
+		//DateTime GetDateTime(int i);
+
+		decimal GetDecimalSafe(int i);
+
+		double GetDoubleSafe(int i);
+
+		//Type GetFieldType(int i);
+
+		float GetFloatSafe(int i);
+
+		//Guid GetGuid(int i);
+
+		short GetInt16Safe(int i);
+
+		int GetInt32Safe(int i);
+
+		long GetInt64Safe(int i);
+
+		//string GetName(int i);
+
+		//int GetOrdinal(string name);
+
+		string GetStringSafe(int i);
+
+		//object GetValue(int i);
+
+		//int GetValues(object[] values);
+
+		//bool IsDBNull(int i);
+
+		//int FieldCount{get;}
+
+		//object this[string name]{get;}
+		
+		//object this[int i]{get;}
+	}
+}
