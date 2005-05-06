592950b2b6dac6bffcc4bc5ca697ff0b50584196
Sebastien Pouliot <sebastien@ximian.com>
2005-05-06 17:59:03 +0000

    Avoid filling the GAC with almost identical versions of the same assembly.
    svn path=/trunk/mcs/; revision=44153

--- mcs/class/IBM.Data.DB2/Assembly/AssemblyInfo.cs
+++ mcs/class/IBM.Data.DB2/Assembly/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AssemblyDelaySign (true)]
 [assembly: AssemblyKeyFile ("ibm.pub")]
