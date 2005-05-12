9f99d665cb608d1b0586edb7794d120ff7c0feb3
Martin Baulig <martin@novell.com>
2005-05-12 22:08:32 +0000

    **** Merged r44473 from GMCS ****
    svn path=/trunk/mcs/; revision=44477

--- mcs/mcs/statement.cs
+++ mcs/mcs/statement.cs
@@ -1917,6 +1917,7 @@ namespace Mono.CSharp {
 		}
 
 		bool unreachable_shown;
+		bool unreachable;
 
 		public override bool Resolve (EmitContext ec)
 		{
@@ -1936,8 +1937,6 @@ namespace Mono.CSharp {
 			// from the beginning of the function.  The outer Resolve() that detected the unreachability is
 			// responsible for handling the situation.
 			//
-			bool unreachable = false;
-
 			int statement_count = statements.Count;
 			for (int ix = 0; ix < statement_count; ix++){
 				Statement s = (Statement) statements [ix];
@@ -2020,6 +2019,8 @@ namespace Mono.CSharp {
 
 			if (Implicit)
 				return Resolve (ec);
+
+			unreachable = true;
 
 			ec.StartFlowBranching (FlowBranching.BranchingType.Block, loc);
 			bool ok = Resolve (ec);
