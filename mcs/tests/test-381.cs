af9d56f3bbea0be29134eaad0215d672a43677ed
Martin Baulig <martin@novell.com>
2005-05-14 04:35:18 +0000

    2005-05-14  Martin Baulig  <martin@ximian.com>
    	* test-381.cs: Slightly modified version of test-360.
    
    
    svn path=/trunk/mcs/; revision=44514

---
+++ mcs/tests/test-381.cs
@@ -0,0 +1,19 @@
+public class Application
+{
+	public static void Main(string[] args)
+	{
+		if (true)
+		{
+			string thisWorks = "nice";
+			System.Console.WriteLine(thisWorks);
+		}
+		else
+		{
+			string thisDoesnt;
+			System.Console.WriteLine();
+			thisDoesnt = "not so";
+			System.Console.WriteLine(thisDoesnt);
+		}
+	}
+}
+
