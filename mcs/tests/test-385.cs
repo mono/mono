7d44c5c518b9efb94d88e3e1dba972dd830216b5
Martin Baulig <martin@novell.com>
2005-05-18 13:44:35 +0000

    New test.
    svn path=/trunk/mcs/; revision=44680

---
+++ mcs/tests/test-385.cs
@@ -0,0 +1,17 @@
+class Test {
+        static int Main ()
+        {
+                int i = 5;
+                switch (i) {
+                case 5:
+                        if (i == 5)
+                                break;
+                        return 1;
+                default:
+                        return 2;
+                }
+                System.Console.WriteLine (i);
+		return 0;
+        }
+}
+
