87e307aa19742a372856c52c7f27be89a04b464c
Satya Sudha K <sudha@mono-cvs.ximian.com>
2005-05-23 06:28:22 +0000

    VBBinder.cs : Support for named arguments in late binding
    svn path=/trunk/mcs/; revision=44903

--- mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/VBBinder.cs
+++ mcs/class/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/VBBinder.cs
@@ -55,7 +55,7 @@ namespace Microsoft.VisualBasic.CompilerServices
 
 		private class BinderState
 		{
-			public object[] args;
+			public object[] originalArgs;
 			public bool[] byRefFlags;
 		}
 
@@ -81,6 +81,35 @@ namespace Microsoft.VisualBasic.CompilerServices
 		private Type objectType;
 		private string bindToName;
 
+		// This method selects a suitable method for the given arguments
+		// Step 1 : Filter out all the methods that are not applicable
+		// Step 2 : Out of the remaining methods, retain only the methods of the most derived types
+		// Step 3 : Get the kind of conversions (Widening, Exact, Narrowing) to be made to the 
+		//          arguments to make them applicable. Remember the best match found so far. 
+		//	    If two or more candidates require narrowing conversions - Report error
+		//          If two candidates X and Y require widening conversions, then
+		//		* select X if conversions from parameters of X to correspodning parameters
+		//		  of Y are all widening.
+		//		* select Y if conversions from parameters of Y to correspodning parameters
+		//		  of X are all widening.
+		//		* otherwise ambiguous
+		// Step 4: Once a best match of found, contruct a new argument list for that method using 
+		//         the given parameters :
+		//		* If ParamArray parameters are present then put all the corresponding arguments
+		//                into an Array
+		//		  For example, while invoking a method 
+		//			'F (a As Integer, b as Integer, ParamArray c() As Integer)'
+		//		  like
+		//			'F (1,2,3,4,5,6}'
+		// 		  the new argument list should look like (1,2 {3,4,5,6})
+		//		- Note : Named arguments dont come into picture here as they are not supported
+		//		         with ParamArray parameters
+		// 		* Create an argument order;
+		//		* If an argument is Missing or the corresponding argOrder entry is -1, 
+		//		  create an argument with the parameter's default value.
+		//		* Perform appropriate conversions.
+		//		* set the 'ByRefFlags array properly ('ByRefFlags' is an array to indicate,
+		//		  which arguments matched a 'ByRef' parameter)
 		public override MethodBase BindToMethod(
 			BindingFlags bindingAttr,
 			MethodBase[] match,
@@ -88,36 +117,35 @@ namespace Microsoft.VisualBasic.CompilerServices
 			ParameterModifier[] modifiers,
 			CultureInfo culture,
 			string[] names,
-			out object state
-			)
+			out object state)
 		{
-			// Store the arguments to the method in a state object.
+			// Store the original arguments to the method in a state object.
 			BinderState binderState = new BinderState();
-			object[] arguments = new Object[args.Length];
-			args.CopyTo(arguments, 0);
-			binderState.args = arguments;
+			binderState.originalArgs = args;
+			binderState.byRefFlags = this.byRefFlags;
 			state = binderState;
 
 			MethodBase mbase = null;
-			Type applicable_type = null;
 
 			if(match == null || match.Length == 0)
 				throw new ArgumentNullException();
 
-			/*
-				// Handle delegates
-				if (match [0].Name == "Invoke") {
-					// TODO
-				}
-			*/
-
 			ArrayList candidates = new ArrayList ();
 			// Get the list of all suitable methods
+			string errMsg = "";
+			// Step 1:
 			for (int index = 0; index < match.Length; index ++) {
-				if (IsApplicable (match [index], args))
+				if (IsApplicable (match [index], names, args, ref errMsg))
 					candidates.Add (match [index]);
 			}
 
+			if (candidates.Count == 0) {
+				if (match.Length == 1 && errMsg != "")
+					throw new ArgumentException (errMsg);
+				return null;
+			}
+
+			// Step 2:
 			MemberInfo[] tempMatchList = GetMostDerivedMembers (candidates);
 			MethodBase[] filteredMatchList = new MethodBase [tempMatchList.Length];
 			for (int index = 0; index < tempMatchList.Length; index ++)
@@ -127,10 +155,11 @@ namespace Microsoft.VisualBasic.CompilerServices
 			int numWideningConversions = 0, numNarrowingConversions = 0;
 			ArrayList narrowingConv = new ArrayList ();
 			ArrayList wideningConv = new ArrayList ();
+			// Step 3:
 			for(int x = 0; x < filteredMatchList.Length; x++)
 			{
 				ParameterInfo[] parameters = filteredMatchList [x].GetParameters();
-				ConversionType ctype = GetConversionType (parameters, args);
+				ConversionType ctype = GetConversionType (parameters, args, names);
 				if (ctype == ConversionType.None)
 					continue;
 				if (ctype == ConversionType.Widening)
@@ -145,14 +174,16 @@ namespace Microsoft.VisualBasic.CompilerServices
 						numWideningConversions ++;
 					mbase = filteredMatchList [x];
 				} else if (bestMatch == ctype) {
-					if (bestMatch == ConversionType.Widening || bestMatch == ConversionType.Exact) {
+					if (bestMatch == ConversionType.Widening) {
 						// Got a widening conversion before also.
 						// Find the best among the two
 						int closestMatch = GetClosestMatch (mbase, filteredMatchList [x], args.Length);
-						if (closestMatch == -1) {
+						if (closestMatch == -1) { // Ambiguous
 							numWideningConversions ++;
 						}
-						else if (closestMatch == 1)
+						else 
+							numWideningConversions = 0;
+						if (closestMatch == 1)
 							mbase = filteredMatchList [x];
 					} else {
 						numNarrowingConversions ++;
@@ -164,7 +195,7 @@ namespace Microsoft.VisualBasic.CompilerServices
 				//TODO : print the methods too
 				throw new AmbiguousMatchException ("No overloaded '" + this.objectType + "." + this.bindToName + "' can be called without a narrowing conversion");
 			}
-			if ((bestMatch == ConversionType.Widening || bestMatch == ConversionType.Exact) && numWideningConversions > 1) {
+			if ((bestMatch == ConversionType.Widening) && numWideningConversions > 1) {
 				//TODO : print the methods too
 				throw new AmbiguousMatchException ("No overloaded '" + this.objectType + "." + this.bindToName + "' can be called without a widening conversion");
 			}
@@ -172,12 +203,15 @@ namespace Microsoft.VisualBasic.CompilerServices
 			if (mbase == null)
 				return null;
 
-			int count = 0;
 			ParameterInfo[] pars = mbase.GetParameters ();
 			if (pars.Length == 0)
 				return mbase;
-			int numFixedParams = CountStandardParams (pars);
 
+			int [] argOrder = null;
+			if (!CreateArgOrder (pars, names, args.Length, out argOrder))
+				return null; 
+
+			// Step 4:
 			if (UsesParamArray (pars)) {
 				int index = 0;
 				int paramArrayIndex = pars.GetUpperBound (0);
@@ -185,10 +219,8 @@ namespace Microsoft.VisualBasic.CompilerServices
 				Array paramArgs = Array.CreateInstance (paramArrayType.GetElementType (), args.Length - paramArrayIndex);
 				bool isArgArray = false;
 				if (pars.GetUpperBound (0) + 1 == args.Length) {
-					if (args [pars.GetUpperBound (0)].GetType().IsArray) {
+					if (args [pars.GetUpperBound (0)].GetType().IsArray)
 						isArgArray = true;
-						count ++;
-					}
 				}
 
 				if (!isArgArray) {
@@ -213,46 +245,38 @@ namespace Microsoft.VisualBasic.CompilerServices
 
 			object[] newArguments = new object [pars.Length];
 			args.CopyTo (newArguments, 0);
+			if (!CreateArgOrder (pars, names, args.Length, out argOrder))
+				return null;
 			int numExpectedArgs = pars.Length;
 			if (UsesParamArray (pars))
 				numExpectedArgs --;
+
+			int argIndex = 0;
+			
 			for(int y = 0; y < numExpectedArgs; y++)
 			{
-				if (y < args.Length && (args [y] is Missing || args[y] == null))
+				argIndex = y;
+				if (argIndex < argOrder.Length)
+					argIndex = argOrder [y];
+				if (argIndex == -1 || argIndex >= args.Length) // Missing arguments (trailing)
 					newArguments [y] = pars [y].DefaultValue;
-				else if (y >= args.Length)
-					newArguments [y] = pars [y].DefaultValue;
-				else 
-					newArguments [y] = args [y];
+				else if (argIndex < args.Length) {
+					if (args [argIndex] is Missing)
+						newArguments [y] = pars [y].DefaultValue;
+					else 
+						newArguments [y] = args [argIndex];
+				}
 			}
 
 			for(int y = 0; y < numExpectedArgs; y++) {
 				if (newArguments [y] == null)
-					return null;
+					continue;
 
 				if((newArguments [y] = ObjectType.CTypeHelper (newArguments[y], pars[y].ParameterType)) == null)
 					return null;
 			}
 
-			((BinderState) state).args = newArguments;
-
-			if (byRefFlags == null || pars.Length == 0) {
-				return mbase;
-			}
-
-			int paramIndex = 0;
-			for (int index = 0; index < byRefFlags.Length; index ++) {
-				paramIndex = index;
-				if (index >= pars.Length)
-					paramIndex = pars.GetUpperBound (0);
-				ParameterInfo p = pars [paramIndex];
-				if (p.ParameterType.IsByRef) {
-					if (byRefFlags [index] != false)
-						byRefFlags [index] = true;
-				} else
-					byRefFlags [index] = false;
-			}
-
+			args = newArguments;
 			return mbase;
 		}
 
@@ -264,7 +288,6 @@ namespace Microsoft.VisualBasic.CompilerServices
 			int isBetter = -2;
 			ParameterInfo[] bestMatchParams = bestMatch.GetParameters ();
 			ParameterInfo[] candidateParams = candidate.GetParameters ();
-			int numParams = Math.Min (bestMatchParams.Length, candidateParams.Length);
 			int paramArrayIndex1 = -1, paramArrayIndex2 = -1;
 			if (UsesParamArray (bestMatchParams)) {
 				paramArrayIndex1 = (bestMatchParams.Length > 0) ? (bestMatchParams.Length - 1) : -1;
@@ -330,6 +353,7 @@ namespace Microsoft.VisualBasic.CompilerServices
 			return isBetter;
 		}
 
+		// Determines is there is any 'ParamArray' parameter in the given set of param
 		internal static bool UsesParamArray (ParameterInfo [] pars) {
 			if (pars == null || pars.Length == 0)
 				return false;
@@ -340,9 +364,7 @@ namespace Microsoft.VisualBasic.CompilerServices
 			return true;
 		}
 
-		/*
-		  Gets the number of parameters that are neither optional not ParamArray
-		*/
+		// Gets the number of parameters that are neither optional not ParamArray
 		internal static int CountStandardParams (ParameterInfo [] pars) {
 			if (pars.Length == 0)
 				return 0;
@@ -360,10 +382,10 @@ namespace Microsoft.VisualBasic.CompilerServices
 			return count;
 		}
 
-		private ConversionType GetConversionType (ParameterInfo[] parameters, object[] args) {
+		// Determines the kind of conversions from argument types to parameter types
+		private ConversionType GetConversionType (ParameterInfo[] parameters, object[] args, string[] names) {
 			int numParams = parameters.Length;
 			int numArgs = args.Length;
-			int paramArrayIndex = -1;
 			int numFixedParams = CountStandardParams (parameters);
 
 			if (numParams == 0) {
@@ -382,19 +404,30 @@ namespace Microsoft.VisualBasic.CompilerServices
 
 			ConversionType ctype = ConversionType.None;
 			bool isLastParam = false;
-			int paramIndex = 0;
+			int paramIndex = 0, argIndex = 0;
+			int [] argOrder = null;
+			if (!CreateArgOrder (parameters, names, numArgs, out argOrder))
+				return ConversionType.None;
+
 			for (int index = 0; index < numArgs; index ++) {
-				if (index < numFixedParams)
-					paramIndex = index;
-				else if (usesParamArray) {
-					paramIndex = parameters.GetUpperBound (0);
-					isLastParam = true;
-				}
+				argIndex = argOrder [index];
+				paramIndex = index;
+				if (paramIndex >= numFixedParams)
+					 if (usesParamArray) {
+						paramIndex = parameters.GetUpperBound (0);
+						isLastParam = true;
+					}
 
 				ConversionType currentCType = ConversionType.None;
 				Type type1 = null;
-				if (args [index] != null)
-					type1 = args [index].GetType ();
+				if (argIndex == -1) {
+					currentCType = ConversionType.Exact;
+					if (ctype < currentCType)
+						ctype = currentCType;
+					continue;
+				}
+				if (args [argIndex] != null)
+					type1 = args [argIndex].GetType ();
 				Type type2 = parameters [paramIndex].ParameterType;
 				if (type2.IsByRef)
 					type2 = type2.GetElementType ();
@@ -553,9 +586,8 @@ namespace Microsoft.VisualBasic.CompilerServices
 				}
 			}
 
-			if (name == null || name.Equals ("")) {
+			if (name == null || name.Equals ("")) 
 				throw new MissingMemberException ("No default members defined for type '" + objType + "'");
-			}
 
 			MemberInfo[] memberinfo = GetMembers (objReflect, objType, name, flags);
 
@@ -569,31 +601,53 @@ namespace Microsoft.VisualBasic.CompilerServices
 				MethodBase[] methodbase = new MethodBase [memberinfo.Length];
 				for (int index = 0; index < memberinfo.Length; index ++)
 					methodbase [index] = (MethodBase) memberinfo [index];
-				MethodBase mbase = BindToMethod (flags, methodbase, ref args, modifiers, culture, paramNames, out objState);
-				if (mbase == null) {
-					throw new MissingMemberException ("No member '" + name + "' defined for type '" + objType + "' which takes the given set of arguments");
-				}
 
-				object [] newArgs = args;
-				if (objState != null)
-					newArgs = ((BinderState) objState).args;
+				MethodBase mbase = BindToMethod (flags,
+								 methodbase,
+								 ref args, modifiers,
+								 culture,
+								 paramNames,
+								 out objState);
+				if (mbase == null)
+					throw new MissingMemberException ("No member '" + name +
+						 "' defined for type '" + objType + "' which takes the given set of arguments");
 
 				MethodInfo mi = (MethodInfo) mbase;
-				retVal =  mi.Invoke (target, newArgs);
-				Array.Copy (newArgs, args, args.Length);
-			}
-
-			if (objState != null && ((BinderState)objState).byRefFlags != null) {
-				this.byRefFlags = ((BinderState)objState).byRefFlags; 
+				retVal =  mi.Invoke (target, args);
+				// Modify the byRefFlags approproately
+				// Also copy the modified arguments back
+				if (objState != null && byRefFlags != null) {
+					BinderState state = (BinderState) objState;
+					object [] originalArgs = state.originalArgs;
+					int[] paramOrder = null;
+					ParameterInfo [] pars = mi.GetParameters ();
+					if (!CreateParamOrder (pars, paramNames, args.Length, out paramOrder))
+						return null;	// Should never happen
+					int paramIndex = 0;
+					for (int index = 0; index < originalArgs.Length; index++) {
+						paramIndex = index;
+						if (paramIndex < paramOrder.Length)
+							paramIndex = paramOrder [index];
+						if (paramIndex == -1)
+							continue;	// Should not happen
+						if (paramIndex >= pars.Length)
+							paramIndex = pars.GetUpperBound (0);
+						ParameterInfo p = pars [paramIndex];
+						if (p.ParameterType.IsByRef) {
+							if (byRefFlags [index] != false)
+								byRefFlags [index] = true;
+							originalArgs [index] = args [paramIndex];
+						} else
+							byRefFlags [index] = false;
+					}
+				}
 			}
 
 			return retVal;
 		}
 
-		/*
-		 Determines whether a given method can be invoked with a given set of arguments
-		*/
-		private bool IsApplicable (MethodBase mb, object [] args) {
+		// Determines whether a given method can be invoked with a given set of arguments
+		private bool IsApplicable (MethodBase mb, string [] names, object [] args, ref string errorMsg) {
 			ParameterInfo [] parameters = mb.GetParameters ();
 			int numFixedParams = CountStandardParams (parameters);
 			int numParams = parameters.Length;
@@ -612,22 +666,35 @@ namespace Microsoft.VisualBasic.CompilerServices
 			if (!usesParamArray && (argCount > numParams))
 				return false;
 
-			int paramIndex = 0;
+			if (usesParamArray && (names != null && names.Length > 0)) {
+				errorMsg += "\n\tNamed argument cannot match ParamArray parameters";
+				return false;
+			}
+
+			int paramIndex = 0, argIndex = 0;
 			bool isLastParam = false;
+			int [] argOrder = null;
+			if (!CreateArgOrder (parameters, names, argCount, out argOrder))
+				return false;
+
+			// We know that we have arguments for all fixed parameters atleast
 			for (int index = 0; index < argCount; index ++) {
 				Type argType = null;
 				Type paramType = null;
 				isLastParam = false;
-				if (index < numFixedParams) 
-					paramIndex = index;
-				else if (usesParamArray) {
-					paramIndex = numParams - 1;
-					isLastParam = true;
-				}
+				paramIndex = index;
+				argIndex = argOrder [index];
+				if (argIndex == -1) // Missing argument, we will use the default value.
+					continue;
+				if (index >= numFixedParams)
+					if (usesParamArray) {
+						paramIndex = numParams - 1;
+						isLastParam = true;
+					}
 
 				paramType = parameters [paramIndex].ParameterType;
-				if (args [index] != null) {
-					argType = args [index].GetType ();
+				if (args [argIndex] != null) {
+					argType = args [argIndex].GetType ();
 					if (usesParamArray && isLastParam) {
 						// ParamArray parameter of type 'T'. We can either have an 
 						// argument which is an array of type T, or 'n' number of 
@@ -649,6 +716,86 @@ namespace Microsoft.VisualBasic.CompilerServices
 							return false;
 					}
 	
+				}
+			}
+			return true;
+		}
+
+		
+		// Creates argument order
+		// If argument names are present, then argument order is created according to paramters
+		// For missing arguments, the corresponding entry wil be -1
+		//    For example, if a method 'F (a As Integer,b As Integer,c As Integer,d As Integer)' 
+		//    is invoked like 'F (c := 1, a := 2, d :=3), then the argument order will be 
+		//    {1, -1, 0, 2}
+		// This method also checks for duplicate argument names, missing parameters, etc.
+		//
+		// If no argument names are present, the order of arguments is expected to match that 
+		// of the parameters. So, in this case, the order will be {0, 1, 2, 3...., numArgs - 1}
+		private bool CreateArgOrder (ParameterInfo [] pars, string [] names, int numArgs, out int [] argOrder) 
+		{
+			argOrder = null;
+			if (names == null || names.Length == 0) {
+				argOrder = new int [numArgs];
+				for (int index = 0; index < numArgs; index ++)
+					argOrder [index] = index;
+			} else {
+
+				int numParamsMatched = 0;
+				argOrder = new int [pars.Length];
+				for (int index = 0; index < argOrder.Length; index ++) 
+					argOrder [index] = -1;
+				for (int paramIndex = 0; paramIndex < pars.Length; paramIndex ++) {
+					string paramName = pars [paramIndex].Name.ToLower ();
+					bool foundArgForParam = false;
+					for (int argindex = 0; argindex < names.Length; argindex ++) {
+						string argName = names [argindex].ToLower ();
+						if (paramName.Equals (argName)) {
+							foundArgForParam = true;
+							if (argOrder [paramIndex] != -1)
+								throw new ArgumentException ("Argument '" + 
+									argName + "' specified multiple times");
+							argOrder [paramIndex] = argindex;
+							numParamsMatched ++;
+						}
+					}
+
+					if (!foundArgForParam && !pars [paramIndex].IsOptional)
+						return false;
+				} 
+
+				if (numParamsMatched != numArgs) 
+					return false;	// We have some invalid paramnames
+			}
+			return true;
+		}
+
+		// Creates parameter order
+		// Similar to CreateArgOrder method, but here order is created according to names/arguments
+		private bool CreateParamOrder (ParameterInfo [] pars, string [] names, int numArgs, out int [] paramOrder) 
+		{
+			paramOrder = null;
+			if (names == null || names.Length == 0) {
+				paramOrder = new int [numArgs];
+				for (int index = 0; index < numArgs; index ++)
+					paramOrder [index] = index;
+			} else {
+
+				paramOrder = new int [names.Length];
+				for (int index = 0; index < paramOrder.Length; index ++) 
+					paramOrder [index] = -1;
+				for (int argindex = 0; argindex < names.Length; argindex ++) {
+					string argName = names [argindex].ToLower ();
+					bool foundParamForArg = false;
+					for (int paramIndex = 0; paramIndex < pars.Length; paramIndex ++) {
+						string paramName = pars [paramIndex].Name.ToLower ();
+						if (paramName.Equals (argName)) {
+							paramOrder [argindex] = paramIndex;
+							foundParamForArg = true;
+						}
+						if (! foundParamForArg)
+							return false;
+					}
 				}
 			}
 			return true;
