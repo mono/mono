90b8b7dea75e17b8db48fe33c23ef88fe708b1ed
Andrew Skiba <andrews@mono-cvs.ximian.com>
2005-05-30 16:22:15 +0000

    "no regression" policy support
    svn path=/trunk/mcs/; revision=45193

--- mcs/class/System.XML/Test/System.Xml/W3C/xmlconf.cs
+++ mcs/class/System.XML/Test/System.Xml/W3C/xmlconf.cs
@@ -24,6 +24,12 @@ namespace XmlConfTest {
 			public override string ToString() {
 				return _short.ToString();
 			}
+
+			public string Long {
+				get {
+					return _long;
+				}
+			}
 		}
 
 		static void PrintUsage () {
@@ -86,8 +92,11 @@ namespace XmlConfTest {
 		ArrayList slowTests = new ArrayList ();
 		ArrayList igroredTests = new ArrayList ();
 		ArrayList knownFailures = new ArrayList ();
+		ArrayList fixmeList = new ArrayList ();
+		ArrayList netFailures = new ArrayList ();
 		StreamWriter failedList;
 		StreamWriter fixedList;
+		StreamWriter slowNewList;
 		#endregion
 
 		#region command line option fields
@@ -129,9 +138,12 @@ namespace XmlConfTest {
 			_args = args;
 			failedList = new StreamWriter ("failed.lst", false);
 			fixedList = new StreamWriter ("fixed.lst", false);
+			slowNewList = new StreamWriter ("slow-new.lst", false);
 			ReadStrings (slowTests, "slow.lst");
 			ReadStrings (igroredTests, "ignored.lst");
 			ReadStrings (knownFailures, "knownFailures.lst");
+			ReadStrings (fixmeList, "fixme.lst");
+			ReadStrings (netFailures, "net-failed.lst");
 		}
 
 		bool Run ()
@@ -141,7 +153,7 @@ namespace XmlConfTest {
 				return false;
 
 			XmlDocument catalog = new XmlDocument ();
-			catalog.Load (@"xmlconf\xmlconf.xml");
+			catalog.Load ("xmlconf/xmlconf.xml");
 			
 			foreach (XmlElement test in catalog.SelectNodes ("//TEST")) {
 				++totalCount;
@@ -157,23 +169,38 @@ namespace XmlConfTest {
 				}
 
 				DateTime start = DateTime.Now;
-				res &= PerformTest (test);	
+				res = PerformTest (test) && res;
 				TimeSpan span = DateTime.Now - start;
 				if (span.TotalSeconds > 1) {
 					if (slowTests.Contains (testId))
 						continue;
-					using (StreamWriter wr = new StreamWriter("slow.lst", true))
-						wr.WriteLine (testId);
+					slowNewList.WriteLine (testId);
+					slowNewList.Flush ();
 				}
 			}
 
-			Console.Error.WriteLine ("\n*********");
+			Console.Error.WriteLine ("\n\n*********");
 			Console.Error.WriteLine ("Total:{0}", totalCount);
 			Console.Error.WriteLine ("Performed:{0}", performedCount);
 			Console.Error.WriteLine ("Passed:{0}", passedCount);
 			Console.Error.WriteLine ("Failed:{0}", failedCount);
 			Console.Error.WriteLine ("Regressions:{0}", regressionsCount);
-			Console.Error.WriteLine ("Fixed:{0}", fixedCount);
+			Console.Error.WriteLine ("Fixed:{0}\n", fixedCount);
+
+			if (fixedCount > 0)
+				Console.Error.WriteLine (@"ATTENTION!
+You must delete the fixed tests (those listed in fixed.lst) from
+knownFailures.lst or fixme.lst. If you don't do it, you can miss
+regressions in the future.");
+
+			if (regressionsCount > 0)
+				Console.Error.WriteLine (@"ERROR!!! New regressions!
+If you see this message for the first time, your last changes had
+introduced new bugs! Before you commit, you must do one of the following:
+
+1. Find and fix the bugs, so tests will pass again.
+2. Open new bugs in bugzilla and temporily add the tests to fixme.lst
+3. Write to devlist and confirm adding the new tests to knownFailures.lst");
 
 			return res;
 		}
@@ -209,8 +236,8 @@ namespace XmlConfTest {
 				validatingPassed = false;
 			}
 			bool res = isOK (type, nonValidatingPassed, validatingPassed);
-			Report (test, res, nonValidatingPassed, validatingPassed);
-			return res;
+			
+			return Report (test, res, nonValidatingPassed, validatingPassed);
 		}
 
 		bool isOK (string type, bool nonValidatingPassed, bool validatingPassed)
@@ -229,26 +256,40 @@ namespace XmlConfTest {
 			}
 		}
 
-		void Report (XmlElement test, bool isok, bool nonValidatingPassed, bool validatingPassed)
+		bool Report (XmlElement test, bool isok, bool nonValidatingPassed, bool validatingPassed)
 		{
 			string testId = test.GetAttribute ("ID");
 
 			if (isok) {
 				++passedCount;
-				if (knownFailures.Contains (testId)) {
+				if (fixmeList.Contains (testId) || knownFailures.Contains (testId)) {
 					++fixedCount;
 					fixedList.WriteLine (testId);
+					Console.Error.Write ("!");
+					return true;
+				}
+				if (netFailures.Contains (testId)) {
+					Console.Error.Write (",");
+					return true;
 				}
 
 				Console.Error.Write (".");
-				return;
+				return true;
 			}
 
 			++failedCount;
 
+			if (netFailures.Contains (testId)) {
+				Console.Error.Write ("K");
+				return true;
+			}
 			if (knownFailures.Contains (testId)) {
 				Console.Error.Write ("k");
-				return;
+				return true;
+			}
+			if (fixmeList.Contains (testId)) {
+				Console.Error.Write ("f");
+				return true;
 			}
 
 			++regressionsCount;
@@ -257,6 +298,7 @@ namespace XmlConfTest {
 				testId, test.GetAttribute ("TYPE"), nonValidatingPassed, validatingPassed);
 			failedList.WriteLine (test.InnerXml);
 			failedList.Flush ();
+			return false;
 		}
 	}
 }
