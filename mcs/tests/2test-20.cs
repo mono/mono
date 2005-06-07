634617f1e8d8028e0c1b7ffd686aa5d5bde24ea2
Martin Baulig <martin@novell.com>
2005-06-07 02:22:57 +0000

    New test.
    svn path=/trunk/mcs/; revision=45558

---
+++ mcs/tests/2test-20.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Foo<T>
+{
+	public IEnumerator<T> getEnumerator (int arg)
+	{
+		if (arg == 1) {
+			int foo = arg;
+			Console.WriteLine (foo);
+		}
+
+		if (arg == 2) {
+			int foo = arg;
+			Console.WriteLine (foo);
+		}
+
+		yield break;
+	}
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
