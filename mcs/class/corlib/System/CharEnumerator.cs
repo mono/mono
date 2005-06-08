b8406f8df01d8ae4d04ca146bcadd4935bce269a
Martin Baulig <martin@novell.com>
2005-06-08 17:23:23 +0000

    The CharEnumerator issue is fixed.
    svn path=/trunk/mcs/; revision=45651

--- mcs/class/corlib/System/CharEnumerator.cs
+++ mcs/class/corlib/System/CharEnumerator.cs
@@ -31,11 +31,17 @@
 //
 
 using System.Collections;
+#if NET_2_0
+using System.Collections.Generic;
+#endif
 
 namespace System
 {
 	[Serializable]
 	public sealed class CharEnumerator : IEnumerator, ICloneable
+#if NET_2_0
+	, IEnumerator <char>
+#endif
 	{
 		private string str;
 		private int index;
@@ -102,5 +108,11 @@ namespace System
 		{
 			index = -1;
 		}
+#if NET_2_0
+		void IDisposable.Dispose ()
+		{
+			// nop
+		}
+#endif
 	}
 }
