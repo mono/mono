5c1dc6e8f0fa9130d242cd704e67b26f1a195914
Raja R Harinath <harinath@hurrynot.org>
2005-06-08 05:15:03 +0000

    * mcs/class.cs (GetPartialBases): Fix return value check of part.GetClassBases. * tests/test-388.cs, tests/test-389.cs: New tests from #75160.  They're actually interdependent.  #75160 is a file-ordering bug, and we use the two testcases to drive the two different orderings. * tests/Makefile (TEST_HARNESS_EXTRAS): Add test-388.cs and test-389.cs.
    svn path=/trunk/mcs/; revision=45613

---
+++ mcs/tests/test-388.cs
@@ -0,0 +1,44 @@
+// This is used to debug an ordering dependent bug.  The counterpart is test-389.cs.
+//
+// Compiler options: -t:library test-389.cs
+
+namespace Schemas {
+    [System.Xml.Serialization.XmlType("base-field-type"),
+    System.Xml.Serialization.XmlInclude(typeof(compoundfield)),
+    System.Xml.Serialization.XmlInclude(typeof(fieldtype))]
+    public partial class basefieldtype {
+
+        [System.Xml.Serialization.XmlAttribute(DataType="ID")]
+        public string id;
+
+        [System.Xml.Serialization.XmlAttribute()]
+        public string datatype;
+    }
+
+    [System.Xml.Serialization.XmlType("field-type")]
+    public partial class fieldtype: basefieldtype {}
+
+    [System.Xml.Serialization.XmlType("compound-field")]
+    public partial class compoundfield: basefieldtype {}
+
+    public partial class field {
+
+        [System.Xml.Serialization.XmlAttribute()]
+        public string id;
+
+        [System.Xml.Serialization.XmlAttribute()]
+        public string type;
+    }
+
+    [System.Xml.Serialization.XmlType("form-data")]
+    public partial class formdata {
+
+        [System.Xml.Serialization.XmlArray(ElementName="form-fields"),
+        System.Xml.Serialization.XmlArrayItem(Type=typeof(field),IsNullable=false)]
+        public field[] formfields;
+        
+		[System.Xml.Serialization.XmlElement("field-type",Type=typeof(fieldtype)),
+        System.Xml.Serialization.XmlElement("compound-field",Type=typeof(compoundfield))]
+        public basefieldtype[] Items;
+    }
+}
