262d98239beaaddb1cd6ebcf9628ffbc27c2a31c
Martin Baulig <martin@novell.com>
2005-06-15 13:04:48 +0000

    2005-06-13  Martin Baulig  <martin@ximian.com>
    	* MonoType.cs
    	(MonoType.getFullName): Added `bool assembly_qualified' argument.
    	(MonoType.AssemblyQualifiedName): The interncall now adds the
    	assembly name, so we don't need to do it here.
    	(MonoType.FullName): Use the new getFullName() API.
    
    
    svn path=/trunk/mcs/; revision=46036

--- mcs/class/corlib/System/MonoType.cs
+++ mcs/class/corlib/System/MonoType.cs
@@ -470,12 +470,12 @@ namespace System
 
 		public override string AssemblyQualifiedName {
 			get {
-				return getFullName (false) + ", " + Assembly.UnprotectedGetName ().ToString ();
+				return getFullName (true, true);
 			}
 		}
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		private extern string getFullName(bool full_name);
+		private extern string getFullName(bool full_name, bool assembly_qualified);
 
 		public extern override Type BaseType {
 			[MethodImplAttribute(MethodImplOptions.InternalCall)]
@@ -484,7 +484,7 @@ namespace System
 
 		public override string FullName {
 			get {
-				return getFullName (false);
+				return getFullName (true, false);
 			}
 		}
 
@@ -565,7 +565,7 @@ namespace System
 
 		public override string ToString()
 		{
-			return getFullName (true);
+			return getFullName (false, false);
 		}
 
 #if NET_2_0 || BOOTSTRAP_NET_2_0
