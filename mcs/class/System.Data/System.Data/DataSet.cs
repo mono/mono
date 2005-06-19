528c6eefbf5ca39e857d69d6ba37e256f3653e53
Konstantin Triger <kostat@mono-cvs.ximian.com>
2005-06-19 12:51:26 +0000

    Fixed ReadXml to work with IXmlSerializable
    svn path=/trunk/mcs/; revision=46217

--- mcs/class/System.Data/System.Data/DataSet.cs
+++ mcs/class/System.Data/System.Data/DataSet.cs
@@ -882,219 +882,177 @@ namespace System.Data {
 
 		// LAMESPEC: XmlReadMode.Fragment is far from presisely
 		// documented. MS.NET infers schema against this mode.
-		public XmlReadMode ReadXml (XmlReader input, XmlReadMode mode)
+		public XmlReadMode ReadXml (XmlReader reader, XmlReadMode mode)
 		{
-			switch (input.ReadState) {
+			if (reader == null)
+				return mode;
+
+			switch (reader.ReadState) {
 			case ReadState.EndOfFile:
 			case ReadState.Error:
 			case ReadState.Closed:
 				return mode;
 			}
 			// Skip XML declaration and prolog
-			input.MoveToContent ();
-			if (input.EOF)
+			reader.MoveToContent ();
+			if (reader.EOF)
 				return mode;
 
-			// FIXME: We need more decent code here, but for now
-			// I don't know the precise MS.NET behavior, I just
-			// delegate to specific read process.
-			switch (mode) {
-			case XmlReadMode.IgnoreSchema:
-				return ReadXmlIgnoreSchema (input, mode, true);
-			case XmlReadMode.ReadSchema:
-				return ReadXmlReadSchema (input, mode, true);
-			}
-			// remaining modes are: Auto, InferSchema, Fragment, Diffgram
-
-			XmlReader reader = input;
-
-			int depth = reader.Depth;
-			XmlReadMode result = mode;
-			bool skippedTopLevelElement = false;
-			string potentialDataSetName = null;
-			XmlDocument doc = null;
-			bool shouldReadData = mode != XmlReadMode.DiffGram;
-			bool shouldNotInfer = Tables.Count > 0;
-
-			switch (mode) {
-			case XmlReadMode.Auto:
-			case XmlReadMode.InferSchema:
-				doc = new XmlDocument ();
-				do {
-					doc.AppendChild (doc.ReadNode (reader));
-				} while (!reader.EOF &&
-					doc.DocumentElement == null);
-				reader = new XmlNodeReader (doc);
-				reader.MoveToContent ();
-				break;
-			case XmlReadMode.DiffGram:
-				if (!(reader.LocalName == "diffgram" &&
-					reader.NamespaceURI == XmlConstants.DiffgrNamespace))
-					goto case XmlReadMode.Auto;
-				break;
+			if (reader is XmlTextReader) {
+				// we dont need whitespace
+				((XmlTextReader) reader).WhitespaceHandling = WhitespaceHandling.None;
 			}
 
-			switch (mode) {
-			case XmlReadMode.Auto:
-			case XmlReadMode.InferSchema:
-			case XmlReadMode.ReadSchema:
-				if (!(reader.LocalName == "diffgram" &&
-					reader.NamespaceURI == XmlConstants.DiffgrNamespace) &&
-					!(reader.LocalName == "schema" &&
-					reader.NamespaceURI == XmlSchema.Namespace))
-					potentialDataSetName = reader.LocalName;
-				goto default;
-			case XmlReadMode.Fragment:
-				break;
-			default:
-				if (!(reader.LocalName == "diffgram" &&
-					reader.NamespaceURI == XmlConstants.DiffgrNamespace) &&
-					!(reader.LocalName == "schema" &&
-					reader.NamespaceURI == XmlSchema.Namespace)) {
-					if (!reader.IsEmptyElement) {
-						reader.Read ();
-						reader.MoveToContent ();
-						skippedTopLevelElement = true;
-					}
-					else {
-						switch (mode) {
-						case XmlReadMode.Auto:
-						case XmlReadMode.InferSchema:
-							DataSetName = reader.LocalName;
-							break;
-						}
-						reader.Read ();
-					}
-				}
-				break;
-			}
-
-			// If schema, then read the first element as schema 
-			if (reader.LocalName == "schema" && reader.NamespaceURI == XmlSchema.Namespace) {
-				shouldNotInfer = true;
-				switch (mode) {
-				case XmlReadMode.IgnoreSchema:
-				case XmlReadMode.InferSchema:
-					reader.Skip ();
-					break;
-				case XmlReadMode.Fragment:
-					ReadXmlSchema (reader);
-					break;
-				case XmlReadMode.DiffGram:
-				case XmlReadMode.Auto:
-					if (Tables.Count == 0) {
-						ReadXmlSchema (reader);
-						if (mode == XmlReadMode.Auto)
-							result = XmlReadMode.ReadSchema;
-					} else {
-					// otherwise just ignore and return IgnoreSchema
-						reader.Skip ();
-						result = XmlReadMode.IgnoreSchema;
-					}
-					break;
-				case XmlReadMode.ReadSchema:
-					ReadXmlSchema (reader);
-					break;
-				}
-			}
+			XmlReadMode Result = mode;
+			XmlDiffLoader DiffLoader = null;
 
 			// If diffgram, then read the first element as diffgram 
 			if (reader.LocalName == "diffgram" && reader.NamespaceURI == XmlConstants.DiffgrNamespace) {
 				switch (mode) {
-				case XmlReadMode.Auto:
-				case XmlReadMode.IgnoreSchema:
-				case XmlReadMode.DiffGram:
-					XmlDiffLoader DiffLoader = new XmlDiffLoader (this);
-					DiffLoader.Load (reader);
-					if (mode == XmlReadMode.Auto)
-						result = XmlReadMode.DiffGram;
-					shouldReadData = false;
-					break;
-				case XmlReadMode.Fragment:
-					reader.Skip ();
-					break;
-				default:
-					reader.Skip ();
-					break;
+					case XmlReadMode.Auto:
+					case XmlReadMode.DiffGram:
+						if (DiffLoader == null)
+							DiffLoader = new XmlDiffLoader (this);
+						DiffLoader.Load (reader);
+						// (and leave rest of the reader as is)
+						return  XmlReadMode.DiffGram;
+					case XmlReadMode.Fragment:
+						reader.Skip ();
+						// (and continue to read)
+						break;
+					default:
+						reader.Skip ();
+						// (and leave rest of the reader as is)
+						return mode;
 				}
 			}
 
-			// if schema after diffgram, just skip it.
-			if (!shouldReadData && reader.LocalName == "schema" && reader.NamespaceURI == XmlSchema.Namespace) {
-				shouldNotInfer = true;
+			// If schema, then read the first element as schema 
+			if (reader.LocalName == "schema" && reader.NamespaceURI == XmlSchema.Namespace) {
 				switch (mode) {
-				default:
-					reader.Skip ();
-					break;
-				case XmlReadMode.ReadSchema:
-				case XmlReadMode.DiffGram:
-					if (Tables.Count == 0)
+					case XmlReadMode.IgnoreSchema:
+					case XmlReadMode.InferSchema:
+						reader.Skip ();
+						// (and break up read)
+						return mode;
+					case XmlReadMode.Fragment:
 						ReadXmlSchema (reader);
-					break;
+						// (and continue to read)
+						break;
+					case XmlReadMode.Auto:
+						if (Tables.Count == 0) {
+							ReadXmlSchema (reader);
+							return XmlReadMode.ReadSchema;
+						} else {
+							// otherwise just ignore and return IgnoreSchema
+							reader.Skip ();
+							return XmlReadMode.IgnoreSchema;
+						}
+					default:
+						ReadXmlSchema (reader);
+						// (and leave rest of the reader as is)
+						return mode; // When DiffGram, return DiffGram
 				}
 			}
 
 			if (reader.EOF)
-				return result == XmlReadMode.Auto ?
-					potentialDataSetName != null && !shouldNotInfer ?
-					XmlReadMode.InferSchema :
-					XmlReadMode.IgnoreSchema : result;
+				return mode;
 
-			// Otherwise, read as dataset... but only when required.
-			if (shouldReadData && !shouldNotInfer) {
-				switch (mode) {
-				case XmlReadMode.Auto:
-					if (Tables.Count > 0)
-						goto case XmlReadMode.IgnoreSchema;
+			int depth = (reader.NodeType == XmlNodeType.Element) ? reader.Depth : -1;
+
+			XmlDocument doc = new XmlDocument ();
+			XmlElement root = doc.CreateElement(reader.Prefix, reader.LocalName, reader.NamespaceURI);
+			if (reader.HasAttributes) {
+				for (int i = 0; i < reader.AttributeCount; i++) {
+					reader.MoveToAttribute(i);
+					if (reader.NamespaceURI == XmlConstants.XmlnsNS)
+						root.SetAttribute(reader.Name, reader.GetAttribute(i));
+					else {
+						XmlAttribute attr = root.SetAttributeNode(reader.LocalName, reader.NamespaceURI);
+						attr.Prefix = reader.Prefix;
+						attr.Value = reader.GetAttribute(i);
+					}
+				}
+			}
+
+			reader.Read();
+			XmlReadMode retMode = mode;
+			bool schemaLoaded = false;
+
+			for (;;) {
+				if( reader.Depth == depth ||
+					reader.NodeType == XmlNodeType.EndElement)
+					break;
+
+				if (reader.NodeType != XmlNodeType.Element) {
+					if (!reader.Read())
+						break;
+					continue;
+				}
+
+				if (reader.LocalName == "schema" && reader.NamespaceURI == XmlSchema.Namespace) {
+					switch (mode) {
+						case XmlReadMode.IgnoreSchema:
+						case XmlReadMode.InferSchema:
+							reader.Skip ();
+							break;
+						
+						default:
+							ReadXmlSchema (reader);
+							retMode = XmlReadMode.ReadSchema;
+							schemaLoaded = true;
+							// (and leave rest of the reader as is)
+							break;
+					}
+
+					continue;
+				}
+	
+				if ((reader.LocalName == "diffgram") && (reader.NamespaceURI == XmlConstants.DiffgrNamespace)) {
+					if ((mode == XmlReadMode.DiffGram) || (mode == XmlReadMode.IgnoreSchema)
+						|| mode == XmlReadMode.Auto) {
+						if (DiffLoader == null)
+							DiffLoader = new XmlDiffLoader (this);
+						DiffLoader.Load (reader);
+						// (and leave rest of the reader as is)
+						retMode = XmlReadMode.DiffGram;
+					}
 					else
-						goto case XmlReadMode.InferSchema;
-				case XmlReadMode.InferSchema:
-					InferXmlSchema (doc, null);
-					if (mode == XmlReadMode.Auto)
-						result = XmlReadMode.InferSchema;
-					break;
-				case XmlReadMode.IgnoreSchema:
-				case XmlReadMode.Fragment:
-				case XmlReadMode.DiffGram:
-					break;
-				default:
-					shouldReadData = false;
-					break;
+						reader.Skip();
+
+					continue;
 				}
+
+				//collect data
+				XmlNode n = doc.ReadNode(reader);
+				root.AppendChild(n);
 			}
 
-			if (shouldReadData) {
-				XmlReader dataReader = reader;
-				if (doc != null) {
-					dataReader = new XmlNodeReader (doc);
-					dataReader.MoveToContent ();
-				}
-				if (reader.NodeType == XmlNodeType.Element)
-					XmlDataReader.ReadXml (this, dataReader,
-						mode);
+			if (reader.NodeType == XmlNodeType.EndElement)
+				reader.Read ();
+			reader.MoveToContent();
+
+			if (mode == XmlReadMode.DiffGram) {
+				return retMode;
 			}
 
-			if (skippedTopLevelElement) {
-				switch (result) {
-				case XmlReadMode.Auto:
-				case XmlReadMode.InferSchema:
-//					DataSetName = potentialDataSetName;
-//					result = XmlReadMode.InferSchema;
-					break;
-				}
-				if (reader.NodeType == XmlNodeType.EndElement)
-					reader.ReadEndElement ();
-			}
-//*
-			while (input.Depth > depth)
-				input.Read ();
-			if (input.NodeType == XmlNodeType.EndElement)
-				input.Read ();
-//*/
-			input.MoveToContent ();
+			doc.AppendChild(root);
 
-			return result == XmlReadMode.Auto ?
-				XmlReadMode.IgnoreSchema : result;
+			if (!schemaLoaded &&
+				retMode != XmlReadMode.ReadSchema &&
+				mode != XmlReadMode.IgnoreSchema &&
+				mode != XmlReadMode.Fragment &&
+				Tables.Count == 0) {
+				InferXmlSchema(doc, null);
+				if (mode == XmlReadMode.Auto)
+					retMode = XmlReadMode.InferSchema;
+			}
+
+			reader = new XmlNodeReader (doc);
+			XmlDataReader.ReadXml (this, reader, mode);
+
+			return retMode == XmlReadMode.Auto ?
+				XmlReadMode.IgnoreSchema : retMode;
 		}
 		#endregion // Public Methods
 
@@ -1168,7 +1126,7 @@ namespace System.Data {
 		
 		protected virtual void ReadXmlSerializable (XmlReader reader)
 		{
-			ReadXml (reader);
+			ReadXml (reader, XmlReadMode.DiffGram);
 		}
 
 		void IXmlSerializable.ReadXml (XmlReader reader)
@@ -1184,7 +1142,16 @@ namespace System.Data {
 
 		XmlSchema IXmlSerializable.GetSchema ()
 		{
-			return GetSchemaSerializable ();
+			// FIXME:
+			// actually MS return null if it's not a typed dataset
+			// we are not there yet
+			//if (GetType() == typeof(DataSet))
+			//	return null;
+			MemoryStream stream = new MemoryStream();
+			XmlTextWriter writer = new XmlTextWriter(stream, null);
+			WriteXmlSchema(writer);
+			stream.Position = 0;
+			return XmlSchema.Read(new XmlTextReader(stream), (ValidationEventHandler)null);
 		}
 
 		protected virtual bool ShouldSerializeRelations ()
@@ -1225,58 +1192,6 @@ namespace System.Data {
 		#endregion
 
 		#region Private Methods
-
-		private XmlReadMode ReadXmlIgnoreSchema (XmlReader input, XmlReadMode mode, bool checkRecurse)
-		{
-			if (input.LocalName == "schema" &&
-				input.NamespaceURI == XmlSchema.Namespace) {
-				input.Skip ();
-			}
-			else if (input.LocalName == "diffgram" &&
-				input.NamespaceURI == XmlConstants.DiffgrNamespace) {
-				XmlDiffLoader DiffLoader = new XmlDiffLoader (this);
-				DiffLoader.Load (input);
-			}
-			else if (checkRecurse ||
-				input.LocalName == DataSetName &&
-				input.NamespaceURI == Namespace) {
-				XmlDataReader.ReadXml (this, input, mode);
-			}
-			else if (checkRecurse && !input.IsEmptyElement) {
-				int depth = input.Depth;
-				input.Read ();
-				input.MoveToContent ();
-				ReadXmlIgnoreSchema (input, mode, false);
-				while (input.Depth > depth)
-					input.Skip ();
-				if (input.NodeType == XmlNodeType.EndElement)
-					input.ReadEndElement ();
-			}
-			input.MoveToContent ();
-			return XmlReadMode.IgnoreSchema;
-		}
-
-		private XmlReadMode ReadXmlReadSchema (XmlReader input, XmlReadMode mode, bool checkRecurse)
-		{
-			if (input.LocalName == "schema" &&
-				input.NamespaceURI == XmlSchema.Namespace) {
-				ReadXmlSchema (input);
-			}
-			else if (checkRecurse && !input.IsEmptyElement) {
-				int depth = input.Depth;
-				input.Read ();
-				input.MoveToContent ();
-				ReadXmlReadSchema (input, mode, false);
-				while (input.Depth > depth)
-					input.Skip ();
-				if (input.NodeType == XmlNodeType.EndElement)
-					input.ReadEndElement ();
-			}
-			else
-				input.Skip ();
-			input.MoveToContent ();
-			return XmlReadMode.ReadSchema;
-		}
 
 		internal static string WriteObjectXml (object o)
 		{
@@ -1338,7 +1253,7 @@ namespace System.Data {
 
 		private void WriteTable (XmlWriter writer, DataTable table, XmlWriteMode mode, DataRowVersion version)
 		{
-			DataRow[] rows = new DataRow [table.Rows.Count];
+			DataRow[] rows = table.NewRowArray(table.Rows.Count);
 			table.Rows.CopyTo (rows, 0);
 			WriteTable (writer, rows, mode, version, true);
 		}
