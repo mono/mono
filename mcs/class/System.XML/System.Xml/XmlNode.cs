68bc7c0afbf9e92bbe93c036b2a414c46a48f6a6
Andrew Skiba <andrews@mono-cvs.ximian.com>
2005-06-22 07:32:01 +0000

    remove unnecessary argument from AppendChildValues function
    svn path=/trunk/mcs/; revision=46347

--- mcs/class/System.XML/System.Xml/XmlNode.cs
+++ mcs/class/System.XML/System.Xml/XmlNode.cs
@@ -108,16 +108,16 @@ namespace System.Xml
 		public virtual string InnerText {
 			get {
 				StringBuilder builder = new StringBuilder ();
-				AppendChildValues (this, builder);
+				AppendChildValues (builder);
 				return builder.ToString ();
 			}
 
 			set { throw new InvalidOperationException ("This node is read only. Cannot be modified."); }
 		}
 
-		private void AppendChildValues (XmlNode parent, StringBuilder builder)
+		private void AppendChildValues (StringBuilder builder)
 		{
-			XmlNode node = parent.FirstChild;
+			XmlNode node = FirstChild;
 
 			while (node != null) {
 				switch (node.NodeType) {
@@ -128,7 +128,7 @@ namespace System.Xml
  					builder.Append (node.Value);
 					break;
 				}
-				AppendChildValues (node, builder);
+				node.AppendChildValues (builder);
 				node = node.NextSibling;
 			}
 		}
