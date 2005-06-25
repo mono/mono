a6e0d6f12b8c0de765d583b04790c563e451ad83
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-06-25 19:23:08 +0000

    2005-06-25 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* Cache.cs: 
    	* CacheEntry.cs: style. Removed doc. comments.
    
    
    svn path=/trunk/mcs/; revision=46502

--- mcs/class/System.Web/System.Web.Caching/Cache.cs
+++ mcs/class/System.Web/System.Web.Caching/Cache.cs
@@ -57,13 +57,6 @@ namespace System.Web.Caching {
 			_objExpires = new CacheExpires (this);
 		}
 
-		/// <summary>
-		/// Internal method to create a enumerator and over all public
-		/// items in the cache and is used by GetEnumerator method.
-		/// </summary>
-		/// <returns>
-		/// Returns IDictionaryEnumerator that contains all public items in the cache
-		/// </returns>
 		private IDictionaryEnumerator CreateEnumerator () {
 			Hashtable objTable;
 
@@ -101,42 +94,6 @@ namespace System.Web.Caching {
 			GetEntry (strKey);
 		}
 
-		/// <summary>
-		/// Adds the specified item to the Cache object with
-		/// dependencies, expiration and priority policies, and a
-		/// delegate you can use to notify your application when the
-		/// inserted item is removed from the Cache.
-		/// </summary>
-		/// <param name="strKey">The cache key used to reference the item.</param>
-		/// <param name="objItem">The item to be added to the cache.</param>
-		/// <param name="objDependency">
-		/// The file or cache key dependencies for the item. When any
-		/// dependency changes, the object becomes invalid and is removed
-		/// from the cache. If there are no dependencies, this paramter
-		/// contains a null reference.
-		/// </param>
-		/// <param name="absolutExpiration">
-		/// The time at which the added object expires and is removed from the cache.
-		/// </param>
-		/// <param name="slidingExpiration">
-		/// The interval between the time the added object was last
-		/// accessed and when that object expires. If this value is the
-		/// equivalent of 20 minutes, the object expires and is removed
-		/// from the cache 20 minutes after it is last accessed.
-		/// </param>
-		/// <param name="enumPriority">
-		/// The relative cost of the object, as expressed by the
-		/// CacheItemPriority enumeration. The cache uses this value when
-		/// it evicts objects; objects with a lower cost are removed from
-		/// the cache before objects with a higher cost.
-		/// </param>
-		/// <param name="eventRemoveCallback">
-		/// A delegate that, if provided, is called when an object is
-		/// removed from the cache. You can use this to notify
-		/// applications when their objects are deleted from the
-		/// cache.
-		/// </param>
-		/// <returns>The Object item added to the Cache.</returns>
 		public object Add (string strKey, object objItem, CacheDependency objDependency,
 							DateTime absolutExpiration, TimeSpan slidingExpiration, 
 							CacheItemPriority enumPriority, CacheItemRemovedCallback eventRemoveCallback) {
@@ -171,16 +128,16 @@ namespace System.Web.Caching {
 
 			// todo: check decay and make up the minHit range
 
-			objEntry = new CacheEntry (this,
-										strKey,
-										objItem,
-										objDependency,
-										eventRemoveCallback,
-										absolutExpiration,
-										slidingExpiration,
-										longHitRange,
-										pub,
-										enumPriority);
+			objEntry = new CacheEntry (	this,
+							strKey,
+							objItem,
+							objDependency,
+							eventRemoveCallback,
+							absolutExpiration,
+							slidingExpiration,
+							longHitRange,
+							pub,
+							enumPriority);
 
 			_lockEntries.AcquireWriterLock (-1);
 			try {
@@ -218,54 +175,16 @@ namespace System.Web.Caching {
 			return objEntry.Item;
 		}
 		
-		/// <summary>
-		/// Inserts an item into the Cache object with a cache key to
-		/// reference its location and using default values provided by
-		/// the CacheItemPriority enumeration.
-		/// </summary>
-		/// <param name="strKey">The cache key used to reference the item.</param>
-		/// <param name="objItem">The item to be added to the cache.</param>
 		public void Insert (string strKey, object objItem) {
 			Add (strKey, objItem, null, NoAbsoluteExpiration, NoSlidingExpiration,
 				CacheItemPriority.Default, null, true, true);
 		}
 
-		/// <summary>
-		/// Inserts an object into the Cache that has file or key dependencies.
-		/// </summary>
-		/// <param name="strKey">The cache key used to reference the item.</param>
-		/// <param name="objItem">The item to be added to the cache.</param>
-		/// <param name="objDependency">
-		/// The file or cache key dependencies for the item. When any
-		/// dependency changes, the object becomes invalid and is removed
-		/// from the cache. If there are no dependencies, this paramter
-		/// contains a null reference.
-		/// </param>
 		public void Insert (string strKey, object objItem, CacheDependency objDependency) {
 			Add (strKey, objItem, objDependency, NoAbsoluteExpiration, NoSlidingExpiration,
 				CacheItemPriority.Default, null, true, true);
 		}
 
-		/// <summary>
-		/// Inserts an object into the Cache with dependencies and expiration policies.
-		/// </summary>
-		/// <param name="strKey">The cache key used to reference the item.</param>
-		/// <param name="objItem">The item to be added to the cache.</param>
-		/// <param name="objDependency">
-		/// The file or cache key dependencies for the item. When any
-		/// dependency changes, the object becomes invalid and is removed
-		/// from the cache. If there are no dependencies, this paramter
-		/// contains a null reference.
-		/// </param>
-		/// <param name="absolutExpiration">
-		/// The time at which the added object expires and is removed from the cache.
-		/// </param>
-		/// <param name="slidingExpiration">The interval between the
-		/// time the added object was last accessed and when that object
-		/// expires. If this value is the equivalent of 20 minutes, the
-		/// object expires and is removed from the cache 20 minutes after
-		/// it is last accessed.
-		/// </param>
 		public void Insert (string strKey, object objItem, CacheDependency objDependency,
 							DateTime absolutExpiration, TimeSpan slidingExpiration) {
 
@@ -273,40 +192,6 @@ namespace System.Web.Caching {
 				CacheItemPriority.Default, null, true, true);
 		}
 
-		/// <summary>
-		/// Inserts an object into the Cache object with dependencies,
-		/// expiration and priority policies, and a delegate you can use
-		/// to notify your application when the inserted item is removed
-		/// from the Cache.
-		/// </summary>
-		/// <param name="strKey">The cache key used to reference the item.</param>
-		/// <param name="objItem">The item to be added to the cache.</param>
-		/// <param name="objDependency">
-		/// The file or cache key dependencies for the item. When any
-		/// dependency changes, the object becomes invalid and is removed
-		/// from the cache. If there are no dependencies, this paramter
-		/// contains a null reference.
-		/// </param>
-		/// <param name="absolutExpiration">
-		/// The time at which the added object expires and is removed from the cache.
-		/// </param>
-		/// <param name="slidingExpiration">
-		/// The interval between the time the added object was last
-		/// accessed and when that object expires. If this value is the
-		/// equivalent of 20 minutes, the object expires and is removed
-		/// from the cache 20 minutes after it is last accessed.
-		/// </param>
-		/// <param name="enumPriority">The relative cost of the object,
-		/// as expressed by the CacheItemPriority enumeration. The cache
-		/// uses this value when it evicts objects; objects with a lower
-		/// cost are removed from the cache before objects with a higher
-		/// cost.
-		/// </param>
-		/// <param name="eventRemoveCallback">A delegate that, if
-		/// provided, is called when an object is removed from the cache.
-		/// You can use this to notify applications when their objects
-		/// are deleted from the cache.
-		/// </param>
 		public void Insert (string strKey, object objItem, CacheDependency objDependency,
 							DateTime absolutExpiration, TimeSpan slidingExpiration,
 							CacheItemPriority enumPriority, CacheItemRemovedCallback eventRemoveCallback) {
@@ -325,30 +210,10 @@ namespace System.Web.Caching {
 				enumPriority, eventRemoveCallback, false, true);
 		}
 
-		/// <summary>
-		/// Removes the specified item from the Cache object.
-		/// </summary>
-		/// <param name="strKey">The cache key for the cache item to remove.</param>
-		/// <returns>
-		/// The item removed from the Cache. If the value in the key
-		/// parameter is not found, returns a null reference.
-		/// </returns>
 		public object Remove (string strKey) {
 			return Remove (strKey, CacheItemRemovedReason.Removed);
 		}
 
-		/// <summary>
-		/// Internal method that updates the cache, decremenents the
-		/// number of existing items and call close on the cache entry.
-		/// This method is also used from the ExpiresBuckets class to
-		/// remove an item during GC flush.
-		/// </summary>
-		/// <param name="strKey">The cache key for the cache item to remove.</param>
-		/// <param name="enumReason">Reason why the item is removed.</param>
-		/// <returns>
-		/// The item removed from the Cache. If the value in the key
-		/// parameter is not found, returns a null reference.
-		/// </returns>
 		internal object Remove (string strKey, CacheItemRemovedReason enumReason) {
 			CacheEntry objEntry = null;
 
@@ -375,11 +240,6 @@ namespace System.Web.Caching {
 			return objEntry.Item;
 		}
 
-		/// <summary>
-		/// Retrieves the specified item from the Cache object.
-		/// </summary>
-		/// <param name="strKey">The identifier for the cache item to retrieve.</param>
-		/// <returns>The retrieved cache item, or a null reference.</returns>
 		public object Get (string strKey) {
 			CacheEntry objEntry = GetEntry (strKey);
 
@@ -421,16 +281,10 @@ namespace System.Web.Caching {
 			return objEntry;
 		}
 
-		/// <summary>
-		/// Gets the number of items stored in the cache.
-		/// </summary>
 		public int Count {
 			get { return _nItems; }
 		}
 
-		/// <summary>
-		/// Gets or sets the cache item at the specified key.
-		/// </summary>
 		public object this [string strKey] {
 			get {
 				return Get (strKey);
