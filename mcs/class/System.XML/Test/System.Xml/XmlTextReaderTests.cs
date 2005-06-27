7bcda5b08aff3c45aec7b9327d98be9244133029
Atsushi Eno <atsushieno@gmail.com>
2005-06-27 18:07:29 +0000

    2005-06-27  Atsushi Enomoto <atsushi@ximian.com>
    	* XmlTextReaderTests.cs : added a testcase for not-wf entity 
    	  declaration which is actually not used in the instance.
    
    
    svn path=/trunk/mcs/; revision=46581

--- mcs/class/System.XML/Test/System.Xml/XmlTextReaderTests.cs
+++ mcs/class/System.XML/Test/System.Xml/XmlTextReaderTests.cs
@@ -1100,5 +1100,20 @@ namespace MonoTests.System.Xml
 			AssertEquals ("#1", 0xf090, (int) r.Value [0]);
 			AssertEquals ("#1", 0x8080, (int) r.Value [1]);
 		}
+
+		[Test]
+		[ExpectedException (typeof (XmlException))]
+		public void EntityDeclarationNotWF ()
+		{
+			string xml = @"<!DOCTYPE doc [
+				<!ELEMENT doc (#PCDATA)>
+				<!ENTITY e ''>
+				<!ENTITY e '<foo&>'>
+				]>
+				<doc>&e;</doc> ";
+			XmlTextReader xtr = new XmlTextReader (xml,
+				XmlNodeType.Document, null);
+			xtr.Read ();
+		}
 	}
 }
