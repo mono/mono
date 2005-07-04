b472676cbc0fab2a5d859e93c353012a2289de4b
Rafael Teixeira <monoman@gmail.com>
2005-07-04 19:12:16 +0000

    Harmonizing Makefile with available test sources
    svn path=/trunk/mcs/; revision=46915

--- mcs/mbas/Test/misc/ctest.vb
+++ mcs/mbas/Test/misc/ctest.vb
@@ -1,33 +1,46 @@
 Imports System.Windows.Forms
-Imports System.Drawing
 
-Delegate Sub d_b1_onClick (s, evt)
+' Delegate Sub ClickHandler (s, evt)
 
 Public Class TestClass
 	Inherits Form
 			
 Public withevents button1 as Button
-		
-Public Sub PutButtonOnForm()
 
+
+' TODO: omitting 'as System.EventArgs' in the second parameter gives: 
+' 			error BC30408: Method 'System.Void OneClick (object, object)' does not match 
+'			delegate 'System.Void System.EventHandler (object, System.EventArgs)'
+' This probably isn't following the lax binding rules when Option Strict is Off
+Public Sub OneClick (a, b as System.EventArgs) Handles button1.Click
+	Me.button1.Text = "Clicked"
 End Sub
 
-Public Sub b1_onClick_handler (a,b) Handles button1.Click
-
-End Sub
+'Public Sub AnotherClick (a, b)
+'	Me.button1.Text = "Clicked Another Way"
+'End Sub
 
 Public Sub New(ctest_a as string)   
-	Dim b1_onClick as d_b1_onClick 
-	
-	b1_onClick = New d_b1_onClick(AddressOf Me.b1_onClick_handler)
+	'Dim clicker as ClickHandler
+	 
+	' TODO: this line is giving:
+	'			error BC-0100: Internal error: Could not find delegate constructor!
+	'clicker = New ClickHandler(AddressOf Me.AnotherClick)
 	
 	Me.button1 = New Button()
 	Me.button1.Text = "Click Me"
-	Me.button1.Location = New Point(100, 10)
-	'Me.Click = b1_onClick
+	Me.button1.Location = New System.Drawing.Point(100, 10)
+	
+	'Me.button1.Click = clicker
+
 	Me.Controls.Add(Me.button1)
+
 
 	System.Console.WriteLine (ctest_a)
 End Sub
+
+public shared sub Main(args as string())
+	Application.Run(new TestClass("Just Test It"))
+end sub
 
 End Class
