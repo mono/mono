aa3bd28a19b768d588a884c2fb97b7d0e5ddf3c1
Martin Baulig <martin@novell.com>
2005-07-11 16:05:39 +0000

    New test.
    svn path=/trunk/mcs/; revision=47175

---
+++ mcs/errors/gcs0019-2.cs
@@ -0,0 +1,14 @@
+// CS0019: Operator `==' cannot be applied to operands of type `T' and `T'
+// Line: 5
+public class TestClass<T>
+{
+	public bool Check (T x, T y) { return x == y; }
+}
+
+public class X
+{
+	static void Main ()
+	{ }
+}
+
+
