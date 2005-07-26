20e7b7c84dc341f5cacd7c7386fabd3c61a46681
Miguel de Icaza <miguel@gnome.org>
2005-07-26 04:03:17 +0000

    Fix Jackson's patch
    svn path=/trunk/mcs/; revision=47679

--- mcs/tools/gacutil/driver.cs
+++ mcs/tools/gacutil/driver.cs
@@ -291,9 +291,11 @@ namespace Mono.Tools {
  				if (Path.DirectorySeparatorChar == '/') {
 					string pkg_path = "../gac/" + an.Name + "/" + version_token + "/" + asmb_file;
  					symlink (pkg_path, ref_path);
+
 					foreach (string ext in siblings) {
 						string sibling = String.Concat (pkg_path, ext);
-						symlink (String.Concat (ref_path, ext), sibling);
+						string sref = String.Concat (ref_path, ext);
+						symlink (sibling, sref);
 					}
 					WriteLine ("Package exported to: {0} -> {1}", ref_path, pkg_path);
  				} else {
