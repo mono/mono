61c7010c458aed5e6b9b63466845c47e7f898459
Andrew Skiba <andrews@mono-cvs.ximian.com>
2005-08-09 17:14:54 +0000

    * TestImageCodecInfo.cs: FormatID, CodecName, DllName, FilenameExtension, Flags, FormatDescription, MimeType properties checked
    svn path=/trunk/mcs/; revision=48180

--- mcs/class/System.Drawing/Test/System.Drawing.Imaging/TestImageCodecInfo.cs
+++ mcs/class/System.Drawing/Test/System.Drawing.Imaging/TestImageCodecInfo.cs
@@ -45,24 +45,13 @@ namespace MonoTests.System.Drawing
 		Hashtable decoders;
 		Hashtable encoders;
 
-		ImageCodecInfo GetDecoder (string clsid)
-		{
-			return GetDecoder (new Guid (clsid));
-		}
-
-		ImageCodecInfo GetDecoder (Guid clsid)
-		{
-			return (ImageCodecInfo) decoders [clsid];
-		}
-
-		ImageCodecInfo GetEncoder (string clsid)
-		{
-			return GetEncoder (new Guid (clsid));
-		}
-
 		ImageCodecInfo GetEncoder (Guid clsid)
 		{
 			return (ImageCodecInfo) encoders [clsid];
+		}
+
+		ImageCodecInfo GetDecoder (Guid clsid) {
+			return (ImageCodecInfo) decoders [clsid];
 		}
 
 		[TestFixtureSetUp]
@@ -77,56 +66,88 @@ namespace MonoTests.System.Drawing
 			foreach (ImageCodecInfo encoder in ImageCodecInfo.GetImageEncoders())
 				encoders[encoder.Clsid] = encoder;
 		}
+
+		static void Check (ImageCodecInfo e, ImageCodecInfo d, Guid FormatID, string CodecName, string DllName,
+			string FilenameExtension, ImageCodecFlags Flags, string FormatDescription,
+			string MimeType/*, byte [][] SignatureMasks*/)
+		{
+			Assert.AreEqual (FormatID, e.FormatID, "Encoder.FormatID");
+			Assert.AreEqual (CodecName, e.CodecName, "Encoder.CodecName");
+			Assert.AreEqual (DllName, e.DllName, "Encoder.DllName");
+			Assert.AreEqual (FilenameExtension, e.FilenameExtension, "Encoder.FilenameExtension");
+			Assert.AreEqual (Flags, e.Flags, "Encoder.Flags");
+			Assert.AreEqual (FormatDescription, e.FormatDescription, "Encoder.FormatDescription");
+			Assert.AreEqual (MimeType, e.MimeType, "Encoder.MimeType");
+
+			Assert.AreEqual (FormatID, d.FormatID, "Decoder.FormatID");
+			Assert.AreEqual (CodecName, d.CodecName, "Decoder.CodecName");
+			Assert.AreEqual (DllName, d.DllName, "Decoder.DllName");
+			Assert.AreEqual (FilenameExtension, d.FilenameExtension, "Decoder.FilenameExtension");
+			Assert.AreEqual (Flags, d.Flags, "Decoder.Flags");
+			Assert.AreEqual (FormatDescription, d.FormatDescription, "Decoder.FormatDescription");
+			Assert.AreEqual (MimeType, d.MimeType, "Decoder.MimeType");
+			/*
+			if (SignatureMasks == null) {
+				Assert.AreEqual (null, e.SignatureMasks, "Encoder.SignatureMasks");
+				Assert.AreEqual (null, d.SignatureMasks, "Decoder.SignatureMasks");
+			}
+			else {
+				Assert.AreEqual (SignatureMasks.Length, e.SignatureMasks.Length, "Encoder.SignatureMasks.Length");
+				Assert.AreEqual (SignatureMasks.Length, d.SignatureMasks.Length, "Decoder.SignatureMasks.Length");
+				for (int i = 0; i < SignatureMasks.Length; i++) {
+					Assert.AreEqual (SignatureMasks[i].Length, e.SignatureMasks[i].Length,
+						"Encoder.SignatureMasks["+i.ToString ()+"].Length");
+					Assert.AreEqual (SignatureMasks[i].Length, d.SignatureMasks[i].Length,
+						"Decoder.SignatureMasks["+i.ToString ()+"].Length");
+					for (int j = 0; j < SignatureMasks[i].Length; j++) {
+						Assert.AreEqual (SignatureMasks[i][j], e.SignatureMasks[i][j],
+							"Encoder.SignatureMasks["+i.ToString ()+"]["+j.ToString ()+"]");
+						Assert.AreEqual (SignatureMasks[i][j], d.SignatureMasks[i][j],
+							"Decoder.SignatureMasks["+i.ToString ()+"]["+j.ToString ()+"]");
+					}
+				}
+			}
+			*/
+		}
+
+		[Test]
+		public void BMPCodec()
+		{
+			Guid g = new Guid ("557cf400-1a04-11d3-9a73-0000f81ef32e");
+			Check (GetEncoder (g), GetDecoder (g), ImageFormat.Bmp.Guid,
+				"Built-in BMP Codec", null, "*.BMP;*.DIB;*.RLE",
+				ImageCodecFlags.Builtin|ImageCodecFlags.Encoder|ImageCodecFlags.Decoder|ImageCodecFlags.SupportBitmap,
+				"BMP", "image/bmp");
+		}
+
+		[Test]
+		public void GifCodec()
+		{
+			Guid g = new Guid ("557cf402-1a04-11d3-9a73-0000f81ef32e");
+			Check (GetEncoder (g), GetDecoder (g), ImageFormat.Gif.Guid,
+				"Built-in GIF Codec", null, "*.GIF",
+				ImageCodecFlags.Builtin|ImageCodecFlags.Encoder|ImageCodecFlags.Decoder|ImageCodecFlags.SupportBitmap,
+				"GIF", "image/gif");
+		}
 		
 		[Test]
-		public void BMPDecoder()
+		public void JpegCodec()
 		{
-			Assert.AreEqual (ImageFormat.Bmp.Guid,
-				GetDecoder ("557cf400-1a04-11d3-9a73-0000f81ef32e").FormatID, "BMP");
+			Guid g = new Guid ("557cf401-1a04-11d3-9a73-0000f81ef32e");
+			Check (GetEncoder (g), GetDecoder (g), ImageFormat.Jpeg.Guid,
+				"Built-in JPEG Codec", null, "*.JPG;*.JPEG;*.JPE;*.JFIF",
+				ImageCodecFlags.Builtin|ImageCodecFlags.Encoder|ImageCodecFlags.Decoder|ImageCodecFlags.SupportBitmap,
+				"JPEG", "image/jpeg");
 		}
 
 		[Test]
-		public void GifDecoder()
+		public void PngCodec()
 		{
-			Assert.AreEqual (ImageFormat.Gif.Guid,
-				GetDecoder ("557cf402-1a04-11d3-9a73-0000f81ef32e").FormatID, "GIF");
-		}
-		
-		[Test]
-		public void JpegDecoder()
-		{
-			Assert.AreEqual (ImageFormat.Jpeg.Guid,
-				GetDecoder ("557cf401-1a04-11d3-9a73-0000f81ef32e").FormatID, "JPEG");
-		}
-
-		[Test]
-		public void PngDecoder()
-		{
-			Assert.AreEqual (ImageFormat.Png.Guid,
-				GetDecoder ("557cf406-1a04-11d3-9a73-0000f81ef32e").FormatID, "PNG");
-		}
-		[Test]
-		public void BMPEncoder() {
-			Assert.AreEqual (ImageFormat.Bmp.Guid,
-				GetEncoder ("557cf400-1a04-11d3-9a73-0000f81ef32e").FormatID, "BMP");
-		}
-
-		[Test]
-		public void GifEncoder() {
-			Assert.AreEqual (ImageFormat.Gif.Guid,
-				GetEncoder ("557cf402-1a04-11d3-9a73-0000f81ef32e").FormatID, "GIF");
-		}
-		
-		[Test]
-		public void JpegEncoder() {
-			Assert.AreEqual (ImageFormat.Jpeg.Guid,
-				GetEncoder ("557cf401-1a04-11d3-9a73-0000f81ef32e").FormatID, "JPEG");
-		}
-
-		[Test]
-		public void PngEncoder() {
-			Assert.AreEqual (ImageFormat.Png.Guid,
-				GetEncoder ("557cf406-1a04-11d3-9a73-0000f81ef32e").FormatID, "PNG");
+			Guid g = new Guid ("557cf406-1a04-11d3-9a73-0000f81ef32e");
+			Check (GetEncoder (g), GetDecoder (g), ImageFormat.Png.Guid,
+				"Built-in PNG Codec", null, "*.PNG",
+				ImageCodecFlags.Builtin|ImageCodecFlags.Encoder|ImageCodecFlags.Decoder|ImageCodecFlags.SupportBitmap,
+				"PNG", "image/png");
 		}
 	}
 }
