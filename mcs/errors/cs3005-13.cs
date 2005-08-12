fdb1ce435919db8d1e0f4db253c967df7ba1ea21
Martin Baulig <martin@novell.com>
2005-08-12 16:39:28 +0000

    2005-08-12  Martin Baulig  <martin@ximian.com>
    	* known-issues-gmcs: Ignore cs3005-13.cs since it's supposed to
    	produce a warning and not an error on .NET 2.0.
    
    
    svn path=/trunk/mcs/; revision=48339

--- mcs/errors/cs3005-13.cs
+++ mcs/errors/cs3005-13.cs
@@ -1,6 +1,10 @@
 // cs3005-13.cs: Identifier `CLSEnum.Label' differing only in case is not CLS-compliant
 // Line: 9
 
+//
+// NOTE: This is only an error in MCS - in GMCS, it's just a warning.
+//
+
 using System;
 [assembly:CLSCompliant (true)]
 
