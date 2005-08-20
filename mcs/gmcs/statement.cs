411b145c68db97f504f45a76d50419fc60ed5d78
Martin Baulig <martin@novell.com>
2005-08-20 17:55:44 +0000

    *** Merged r47879 from MCS ***
    svn path=/trunk/mcs/; revision=48604

--- mcs/gmcs/statement.cs
+++ mcs/gmcs/statement.cs
@@ -3786,6 +3786,11 @@ namespace Mono.CSharp {
 						throw new Exception ("Variable does not exist in this block");
 
 					ig.Emit (OpCodes.Stloc, vi.LocalBuilder);
+					if (vi.IsCaptured){
+						ec.EmitCapturedVariableInstance (vi);
+						ig.Emit (OpCodes.Ldloc, vi.LocalBuilder);
+						ig.Emit (OpCodes.Stfld, vi.FieldBuilder);
+					}
 				} else
 					ig.Emit (OpCodes.Pop);
 				
