1390241c0fde045694fe20dea9f84a8fee4d9f74
Atsushi Eno <atsushieno@gmail.com>
2005-08-25 15:28:45 +0000

    2005-08-25  Atsushi Enomoto  <atsushi@ximian.com>
    	* class.cs : (AbstractPropertyEventMethod) SetupName() now takes
    	  Location. Now getter and setter store location correctly.
    	  (errors/cs0111-12.cs now reports the expected location.)
    
    
    svn path=/trunk/mcs/; revision=48844

--- mcs/mcs/class.cs
+++ mcs/mcs/class.cs
@@ -5704,7 +5704,7 @@ namespace Mono.CSharp {
 		ReturnParameter return_attributes;
 
 		public AbstractPropertyEventMethod (MemberBase member, string prefix)
-			: base (null, SetupName (prefix, member), null)
+			: base (null, SetupName (prefix, member, member.Location), null)
 		{
 			this.prefix = prefix;
 			IsDummy = true;
@@ -5712,21 +5712,21 @@ namespace Mono.CSharp {
 
 		public AbstractPropertyEventMethod (MemberBase member, Accessor accessor,
 						    string prefix)
-			: base (null, SetupName (prefix, member),
+			: base (null, SetupName (prefix, member, accessor.Location),
 				accessor.Attributes)
 		{
 			this.prefix = prefix;
 			this.block = accessor.Block;
 		}
 
-		static MemberName SetupName (string prefix, MemberBase member)
+		static MemberName SetupName (string prefix, MemberBase member, Location loc)
 		{
-			return new MemberName (member.MemberName.Left, prefix + member.ShortName);
+			return new MemberName (member.MemberName.Left, prefix + member.ShortName, loc);
 		}
 
 		public void UpdateName (MemberBase member)
 		{
-			SetMemberName (SetupName (prefix, member));
+			SetMemberName (SetupName (prefix, member, Location));
 		}
 
 		#region IMethodData Members
