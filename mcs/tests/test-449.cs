f160b9b93b57e8f72432be213baa152d6fb58b3e
Miguel de Icaza <miguel@gnome.org>
2005-08-30 13:39:14 +0000

    Add new test for bug 75925
    svn path=/trunk/mcs/; revision=49120

---
+++ mcs/tests/test-449.cs
@@ -0,0 +1,30 @@
+//
+// This is a test for bug 75925, to ensure that
+// we do not accidentally introduce regressions on anonymous methods.
+//
+// This bug only needs to be compiled
+//
+
+using System;
+using System.Threading;
+
+class X {
+
+	static void Main () 
+	{
+	}
+	
+	void Z ()
+	{
+		ThreadPool.QueueUserWorkItem (delegate {
+			Z ();
+					
+			ThreadPool.QueueUserWorkItem (delegate {
+				Z ();
+			});	
+		});
+	}	
+}
+
+
+
