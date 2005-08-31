7e84b4434f613eab5d7ad8dc81dfa497867f8006
Martin Baulig <martin@novell.com>
2005-08-31 18:09:30 +0000

    Make this actually work.
    svn path=/trunk/mcs/; revision=49214

--- mcs/class/corlib/System/ConsoleKey.cs
+++ mcs/class/corlib/System/ConsoleKey.cs
@@ -30,13 +30,17 @@
 #if NET_2_0
 namespace System {
 	public enum ConsoleKey {
+#pragma warning disable 3005
 		Backspace = 8,
+#pragma warning restore
 		Tab = 9,
 		Clear = 12,
 		Enter = 13,
 		Pause = 19,
 		Escape = 27,
+#pragma warning disable 3005
 		Spacebar = 32,
+#pragma warning restore
 		PageUp = 33,
 		PageDown = 34,
 		End = 35,
