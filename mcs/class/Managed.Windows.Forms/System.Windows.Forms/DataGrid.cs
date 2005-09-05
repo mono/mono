c589428ae83dcad5025c570c787b5729e026100a
Jordi Mas i Hernandez <jordi@mono-cvs.ximian.com>
2005-09-05 09:59:24 +0000

    fixes build
    svn path=/trunk/mcs/; revision=49460

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGrid.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGrid.cs
@@ -1069,7 +1069,7 @@ namespace System.Windows.Forms
 
 		protected virtual DataGridColumnStyle CreateGridColumn (PropertyDescriptor prop, bool isDefault)
 		{
-			
+			throw new NotImplementedException();
 		}
 
 		protected override void Dispose (bool disposing)
