03e66dec045e561305053e4cb5694f88a8d4301f
Miguel de Icaza <miguel@gnome.org>
2005-09-05 18:37:43 +0000

    Add test case for bug #75852
    svn path=/trunk/mcs/; revision=49502

---
+++ mcs/errors/gcs0309-5.cs
@@ -0,0 +1,8 @@
+// CS0309: The type `System.Int32' must be convertible to `System.EventArgs' in order to use it as parameter `T' in the generic type or method `System.EventHandler`1<T>'
+// Line: 5
+class X {
+	System.EventHandler <int> x;
+	static void Main () {}
+}
+
+
