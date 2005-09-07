776d6b78eb61978beffabd9e799fdbdafc6a90c2
Jonathan Chambers <joncham@gmail.com>
2005-09-07 02:02:11 +0000

    2005-09-06 Jonathan Chambers <jonathan.chambers@ansys.com>
            * PropertiesTab.cs: Added
    
    
    svn path=/trunk/mcs/; revision=49606

---
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/PropertiesTab.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace System.Windows.Forms.PropertyGridInternal
+{
+	/// <summary>
+	/// Summary description for PropertiesTab.
+	/// </summary>
+	public class PropertiesTab : System.Windows.Forms.Design.PropertyTab
+	{
+		public PropertiesTab()
+		{
+		}
+
+		public override System.ComponentModel.PropertyDescriptorCollection GetProperties(object component, Attribute[] attributes)
+		{
+			return null;
+		}
+
+		public override string TabName
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+
+	}
+}
