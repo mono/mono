95477771733b2a489903f259a4305dfc926ca200
Sebastien Pouliot <sebastien@ximian.com>
2005-09-13 23:36:13 +0000

    2005-09-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* HttpApplicationCas.cs: New. CAS unit tests.
    	* HttpApplicationStateCas.cs: New. CAS unit tests.
    	* HttpBrowserCapabilitiesCas.cs: New. CAS unit tests.
    	* HttpCachePolicyCas.cs: New. CAS unit tests.
    	* HttpCacheVaryByHeadersCas.cs: New. CAS unit tests.
    	* HttpCacheVaryByParamsCas.cs: New. CAS unit tests.
    	* HttpClientCertificateCas.cs: New. CAS unit tests.
    	* HttpCompileExceptionCas.cs: New. CAS unit tests.
    	* HttpContextCas.cs: Add method/propertie CAS tests cases.
    	* HttpCookieCas.cs: New. CAS unit tests.
    	* HttpCookieCollectionCas.cs: New. CAS unit tests.
    	* HttpExceptionCas.cs: New. CAS unit tests.
    	* HttpFileCollectionCas.cs: New. CAS unit tests.
    	* HttpModuleCollectionCas.cs: New. CAS unit tests.
    	* HttpParseExceptionCas.cs: New. CAS unit tests.
    	* HttpRequestCas.cs: New. CAS unit tests.
    	* HttpRequestValidationExceptionCas.cs: New. CAS unit tests.
    	* HttpResponseCas.cs: New. CAS unit tests.
    	* HttpRuntimeCas.cs: New. CAS unit tests.
    	* HttpServerUtilityCas.cs: New. CAS unit tests.
    	* HttpStaticObjectsCollectionCas.cs: New. CAS unit tests.
    	* HttpUnhandledExceptionCas.cs: New. CAS unit tests.
    	* HttpUtilityCas.cs: New. CAS unit tests.
    	* HttpWorkerRequestCas.cs: New. CAS unit tests.
    	* HttpWriterCas.cs: New. CAS unit tests.
    	* ProcessInfoCas.cs: New. CAS unit tests.
    	* ProcessModelInfoCas.cs: New. CAS unit tests.
    	* TraceContextCas.cs: New. CAS unit tests.
    
    
    svn path=/trunk/mcs/; revision=49988

--- mcs/class/System.Web/Test/System.Web/HttpContextCas.cs
+++ mcs/class/System.Web/Test/System.Web/HttpContextCas.cs
@@ -29,32 +29,98 @@
 using NUnit.Framework;
 
 using System;
+using System.Globalization;
+using System.IO;
 using System.Reflection;
 using System.Security;
 using System.Security.Permissions;
 using System.Security.Principal;
 using System.Web;
+using System.Web.Handlers;
 
 namespace MonoCasTests.System.Web {
 
 	[TestFixture]
 	[Category ("CAS")]
-	public class HttpContextCas {
+	public class HttpContextCas : AspNetHostingMinimal {
 
 		private HttpContext context;
+		private HttpRequest request;
+		private HttpResponse response;
+		private StringWriter sw;
+		private IHttpHandler handler;
 
 		[TestFixtureSetUp]
 		public void FixtureSetUp ()
 		{
+			// running at fulltrust
 			context = new HttpContext (null);
+			request = new HttpRequest (String.Empty, "http://localhost/", String.Empty);
+			sw = new StringWriter ();
+			response = new HttpResponse (sw);
+			handler = new TraceHandler ();
 		}
 
-		[SetUp]
-		public void SetUp ()
+		[Test]
+		[PermissionSet (SecurityAction.Deny, Unrestricted = true)]
+		public void Constructors_Deny_Unrestricted ()
 		{
-			if (!SecurityManager.SecurityEnabled)
-				Assert.Ignore ("SecurityManager.SecurityEnabled is OFF");
+			Assert.IsNotNull (new HttpContext (null), "ctor(HttpWorkerRequest)");
+			Assert.IsNotNull (new HttpContext (request, response), "ctor(HttpRequest,HttpResponse)");
 		}
+
+		[Test]
+		[PermissionSet (SecurityAction.Deny, Unrestricted = true)]
+		public void Properties_Deny_Unrestricted ()
+		{
+			// AllErrors value depends on the execution order
+			Exception[] exceptions = context.AllErrors;
+			Assert.IsNull (context.ApplicationInstance, "ApplicationInstance");
+			context.ApplicationInstance = new HttpApplication ();
+			Assert.IsNotNull (context.Cache, "Cache");
+			Assert.IsNull (context.Error, "Error");
+			Assert.IsNull (context.Handler, "Handler");
+			context.Handler = handler;
+			// FIXME: Mono returns false, MS returns true
+			bool b = context.IsCustomErrorEnabled;
+			Assert.IsFalse (context.IsDebuggingEnabled, "IsDebuggingEnabled");
+			Assert.IsNotNull (context.Items, "Items");
+			Assert.IsNotNull (context.Request, "Request");
+			Assert.IsNotNull (context.Response, "Response");
+			Assert.IsNotNull (context.Server, "Server");
+			Assert.IsNull (context.Session, "Session");
+			// note: only SkipAuthorization setter is protected
+			Assert.IsFalse (context.SkipAuthorization, "SkipAuthorization");
+			Assert.IsTrue (context.Timestamp < DateTime.MaxValue, "Timestamp");
+			Assert.IsNotNull (context.Trace, "Trace");
+			// note: only User setter is protected
+			Assert.IsNull (context.User, "User");
+#if NET_2_0
+			Assert.IsNotNull (context.Application, "Application");
+			Assert.IsNotNull (context.CurrentHandler, "CurrentHandler");
+			Assert.IsNull (context.PreviousHandler, "PreviousHandler");
+//			Assert.IsNull (context.Profile, "Profile");
+#endif
+			// static properties
+			Assert.IsNull (HttpContext.Current, "Current");
+		}
+
+#if ONLY_1_1
+		[Test]
+		[SecurityPermission (SecurityAction.Deny, UnmanagedCode = true)]
+		[ExpectedException (typeof (SecurityException))]
+		public void Application_Deny_UnmanagedCode ()
+		{
+			Assert.IsNotNull (context.Application, "Application");
+		}
+
+		[Test]
+		[SecurityPermission (SecurityAction.PermitOnly, UnmanagedCode = true)]
+		public void Application_PermitOnly_UnmanagedCode ()
+		{
+			Assert.IsNotNull (context.Application, "Application");
+		}
+#endif
 
 		[Test]
 		[SecurityPermission (SecurityAction.Deny, ControlPrincipal = true)]
@@ -84,6 +150,80 @@ namespace MonoCasTests.System.Web {
 		public void User_Set_PermitOnly ()
 		{
 			context.User = new GenericPrincipal (new GenericIdentity ("me"), null);
+		}
+
+		[Test]
+		[PermissionSet (SecurityAction.Deny, Unrestricted = true)]
+		public void Methods_Deny_Unrestricted ()
+		{
+			context.AddError (new Exception ());
+			context.ClearError ();
+			try {
+				context.GetConfig (String.Empty);
+			}
+			catch (NullReferenceException) {
+			}
+
+			try {
+				context.RewritePath (String.Empty);
+			}
+			catch (NullReferenceException) {
+			}
+
+			try {
+				context.RewritePath (String.Empty, String.Empty, String.Empty);
+			}
+			catch (NullReferenceException) {
+			}
+#if NET_2_0
+			context.GetSection (String.Empty);
+
+			try {
+				context.RewritePath (String.Empty, true);
+			}
+			catch (NullReferenceException) {
+			}
+#endif
+		}
+
+		[Test]
+		[PermissionSet (SecurityAction.Deny, Unrestricted = true)]
+		public void StaticMethods_Deny_Unrestricted ()
+		{
+			HttpContext.GetAppConfig (String.Empty);
+#if NET_2_0
+			HttpContext.GetGlobalResourceObject (String.Empty, String.Empty);
+			HttpContext.GetGlobalResourceObject (String.Empty, String.Empty, CultureInfo.InvariantCulture);
+			try {
+				HttpContext.GetLocalResourceObject ("/", String.Empty);
+			}
+			catch (NotImplementedException) {
+				// mono
+			}
+			catch (NullReferenceException) {
+			}
+			try {
+				HttpContext.GetLocalResourceObject ("/", String.Empty, CultureInfo.InvariantCulture);
+			}
+			catch (NotImplementedException) {
+				// mono
+			}
+			catch (NullReferenceException) {
+			}
+#endif
+		}
+
+		// LinkDemand
+
+		public override object CreateControl (SecurityAction action, AspNetHostingPermissionLevel level)
+		{
+			ConstructorInfo ci = this.Type.GetConstructor (new Type[1] { typeof (HttpWorkerRequest) });
+			Assert.IsNotNull (ci, ".ctor(HttpWorkerRequest)");
+			return ci.Invoke (new object[1] { null });
+		}
+
+		public override Type Type {
+			get { return typeof (HttpContext); }
 		}
 	}
 }
