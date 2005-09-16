7f46017a8b172d280969d716d6fb6dc48164eddf
Hisham Mardam Bey <hisham@mono-cvs.ximian.com>
2005-09-16 09:55:09 +0000

    Work on ScrollBar unit test.
    svn path=/trunk/mcs/; revision=50134

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/ScrollBarTest.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/ScrollBarTest.cs
@@ -1,12 +1,12 @@
 //
-// ScrollBarTest.cs: Test cases for ScrollBar.
+// Copyright (c) 2005 Novell, Inc.
 //
-// Author:
-//   Ritvik Mayank (mritvik@novell.com)
+// Authors:
+//      Ritvik Mayank (mritvik@novell.com)
+//      Hisham Mardam Bey (hisham.mardambey@gmail.com)
 //
-// (C) 2005 Novell, Inc. (http://www.novell.com)
 //
-
+                                                
 
 using System;
 using NUnit.Framework;
@@ -16,94 +16,97 @@ using System.Runtime.Remoting;
 
 namespace MonoTests.System.Windows.Forms
 {
-	[TestFixture]
-	public class ScrollBarTest 
-	{
-
+        [TestFixture]
+        public class ScrollbarTest
+        {
+		
 		[Test]
-		public void ScrollBarPropertyTest ()
-		{	
-			ScrollBar myscrlbar = new HScrollBar (); // Could be checked for VScrollBar as well 
-				
+		public void PubPropTest ()
+		{
+			ScrollBar myscrlbar = new HScrollBar ();
+			
 			// B
-			Assert.AreEqual ("Control", myscrlbar.BackColor.Name, "#B1");
 			myscrlbar.BackColor = Color.Red;
 			Assert.AreEqual (255, myscrlbar.BackColor.R, "#B2");
-			myscrlbar.BackgroundImage = Image.FromFile ("M.gif");
-			Assert.AreEqual (60, myscrlbar.BackgroundImage.Height, "#B3");
-
+			myscrlbar.BackgroundImage = Image.FromFile ("a.png");
+			Assert.AreEqual (16, myscrlbar.BackgroundImage.Height, "#B3");
+			
 			// F
-			Assert.AreEqual ("ControlText", myscrlbar.ForeColor.Name, "#F1");
-	
-			// I 
-			Assert.AreEqual (ImeMode.Disable, myscrlbar.ImeMode, "#I1");
+			Assert.AreEqual ("ff000000", myscrlbar.ForeColor.Name, "#F1");
+			
+			// I
+			//Assert.AreEqual (ImeMode.Disable, myscrlbar.ImeMode, "#I1");
 			
 			// L
-			Assert.AreEqual (10, myscrlbar.LargeChange, "#L1");
+                        Assert.AreEqual (10, myscrlbar.LargeChange, "#L1");
 			
 			// M
 			Assert.AreEqual (100, myscrlbar.Maximum, "#M1");
 			Assert.AreEqual (0, myscrlbar.Minimum, "#M2");
-
+			myscrlbar.Maximum = 300;
+			myscrlbar.Minimum = 100;
+			Assert.AreEqual (300, myscrlbar.Maximum, "#M3");
+			Assert.AreEqual (100, myscrlbar.Minimum, "#M4");
+			
 			// S
 			Assert.AreEqual (null, myscrlbar.Site, "#S1");
 			Assert.AreEqual (1, myscrlbar.SmallChange, "#S2");
+			myscrlbar.SmallChange = 10;
+			Assert.AreEqual (10, myscrlbar.SmallChange, "#S3");
 			
 			// T
-			Assert.AreEqual ("", myscrlbar.Text, "#T1");
+			Assert.AreEqual (false, myscrlbar.TabStop, "#T1");
+			myscrlbar.TabStop = true;
+			Assert.AreEqual (true, myscrlbar.TabStop, "#T2");
+			Assert.AreEqual ("", myscrlbar.Text, "#T3");
 			myscrlbar.Text = "MONO SCROLLBAR";
-			Assert.AreEqual ("MONO SCROLLBAR", myscrlbar.Text, "#T2");
-
+			Assert.AreEqual ("MONO SCROLLBAR", myscrlbar.Text, "#T4");
+			
 			// V
-			Assert.AreEqual (0, myscrlbar.Value, "#V1");
-		}
-
-		[Test]
-		[ExpectedException (typeof (ArgumentException))]
-		public void ExceptionChangeTest ()
-		{
-			ScrollBar myHscrlbar = new HScrollBar ();
-			myHscrlbar.LargeChange = -1; // LargeChange must be greater than 0
-			myHscrlbar.SmallChange = -1; // SmallChange must be greater than 0	
+                        Assert.AreEqual (100, myscrlbar.Value, "#V1");
+			myscrlbar.Value = 150;			
+			Assert.AreEqual (150, myscrlbar.Value, "#V2");
 		}
 		
 		[Test]
 		[ExpectedException (typeof (ArgumentException))]
-		public void ExceptionValTest ()
+		public void ExceptionValueTest ()
 		{
-			ScrollBar myHscrlbar1 = new HScrollBar ();
-			ScrollBar myHscrlbar2 = new HScrollBar ();
-			myHscrlbar1.Value = -1 ;
-			myHscrlbar2.Value = 101 ;
+			ScrollBar myscrlbar = new HScrollBar ();
+			myscrlbar.Minimum = 10;
+			myscrlbar.Maximum = 20;			
+			myscrlbar.Value = 9;
+			myscrlbar.Value = 21;
 		}
 		
 		[Test]
-		public void ToStringMethodTest () 
+		public void PubMethodTest ()
 		{
-			ScrollBar myHscrlbar = new HScrollBar ();
-			myHscrlbar.Text = "New HScrollBar";
+			ScrollBar myscrlbar = new HScrollBar ();
+			myscrlbar.Text = "New HScrollBar";
 			Assert.AreEqual ("System.Windows.Forms.HScrollBar, Minimum: 0, Maximum: 100, Value: 0",
-				         myHscrlbar.ToString (), "#T3");
-		}
+					 myscrlbar.ToString (), "#T5");
+		}				
 	}
-	
-	[TestFixture]
-	public class ScrollBarValueChangedEventClass
-	{
+   
+        [TestFixture]
+        public class ScrollBarValueChangedEventClass
+        {
 		static bool eventhandled = false;
 		public static void ValueChange_EventHandler (object sender, EventArgs e)
 		{
 			eventhandled = true;
 		}
-
+		
 		[Test]
-		public void ValueChangeEventTest ()
-		{
+	        public void ValueChangeEventTest ()
+	        {
 			ScrollBar myHscrlbar = new HScrollBar ();
 			myHscrlbar.Value = 40 ;
 			myHscrlbar.ValueChanged += new EventHandler (ValueChange_EventHandler);
 			myHscrlbar.Value = 50 ;
 			Assert.AreEqual (true, eventhandled, "#1");
 		}
-	}	
+	}   
 }
+	   
