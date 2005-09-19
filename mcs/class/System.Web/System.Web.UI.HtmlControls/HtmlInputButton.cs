937352e462420cbd70710b0eff2544765dfb04c7
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-09-19 21:02:22 +0000

    2005-09-19 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* HtmlInputButton.cs: render the onclick for doing a postback even when
    	there are no validators.
    
    
    svn path=/trunk/mcs/; revision=50258

--- mcs/class/System.Web/System.Web.UI.HtmlControls/HtmlInputButton.cs
+++ mcs/class/System.Web/System.Web.UI.HtmlControls/HtmlInputButton.cs
@@ -142,10 +142,19 @@ namespace System.Web.UI.HtmlControls {
 
 		protected override void RenderAttributes (HtmlTextWriter writer)
 		{
-			if (CausesValidation && Page != null && Page.AreValidatorsUplevel ()) {
+			if (CausesValidation && Page != null) {
+				string oc = null;
 				ClientScriptManager csm = new ClientScriptManager (Page);
-				writer.WriteAttribute ("onclick", csm.GetClientValidationEvent ());
-				writer.WriteAttribute ("language", "javascript");
+				if (Page.AreValidatorsUplevel ()) {
+					oc = csm.GetClientValidationEvent ();
+				} else if (Events [ServerClickEvent] != null) {
+					oc = Attributes ["onclick"] + " " + csm.GetPostBackClientEvent (this, "");
+				}
+				
+				if (oc != null) {
+					writer.WriteAttribute ("language", "javascript");
+					writer.WriteAttribute ("onclick", oc);
+				}
 			}
 
 			Attributes.Remove ("CausesValidation");
