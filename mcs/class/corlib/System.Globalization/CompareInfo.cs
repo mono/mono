b1f8929250eff74bc2a2fa0b9a01c34c63a0d0be
Atsushi Eno <atsushieno@gmail.com>
2005-09-21 20:43:05 +0000

    2005-09-21  Atsushi Enomoto  <atsushi@ximian.com>
    	* CompareInfo.cs : switched to "managed collation by default" mode.
    
    
    svn path=/trunk/mcs/; revision=50415

--- mcs/class/corlib/System.Globalization/CompareInfo.cs
+++ mcs/class/corlib/System.Globalization/CompareInfo.cs
@@ -42,8 +42,8 @@ namespace System.Globalization
 	public class CompareInfo : IDeserializationCallback
 	{
 		static readonly bool useManagedCollation =
-			Environment.internalGetEnvironmentVariable ("MONO_USE_MANAGED_COLLATION")
-			== "yes";
+			Environment.internalGetEnvironmentVariable ("MONO_DISABLE_MANAGED_COLLATION")
+			!= "yes";
 
 		internal static bool UseManagedCollation {
 			get { return useManagedCollation && MSCompatUnicodeTable.IsReady; }
