0d91a7a29c48d44cb8c5d09516a24ef56c74ae46
Sebastien Pouliot <sebastien@ximian.com>
2005-09-21 18:48:01 +0000

    2005-09-21  Sebastien Pouliot  <sebastien@ximian.com>
    	* CheckBoxList.cs: Fix unit test failure (which matched bug #48802).
    
    
    svn path=/trunk/mcs/; revision=50402

--- mcs/class/System.Web/System.Web.UI.WebControls/CheckBoxList.cs
+++ mcs/class/System.Web/System.Web.UI.WebControls/CheckBoxList.cs
@@ -338,7 +338,18 @@ namespace System.Web.UI.WebControls {
 			check_box.Text = item.Text;
 			check_box.AutoPostBack = AutoPostBack;
 			check_box.Checked = item.Selected;
+
+			if (!Enabled) {
+				check_box.Enabled = false;
+				writer.AddAttribute (HtmlTextWriterAttribute.Disabled, "disabled");
+				writer.RenderBeginTag (HtmlTextWriterTag.Span);
+			}
+
 			check_box.RenderControl (writer);
+
+			if (!Enabled) {
+				writer.RenderEndTag ();
+			}
 		}
 
 		void IRepeatInfoUser.RenderItem (ListItemType itemType,
