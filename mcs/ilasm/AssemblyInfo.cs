756031ed1fc68846086a0e415d4835d429492300
Miguel de Icaza <miguel@gnome.org>
2005-09-22 04:30:36 +0000

    Update versions
    svn path=/branches/mono-1-1-9/mcs/; revision=50454

--- mcs/ilasm/AssemblyInfo.cs
+++ mcs/ilasm/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion (Consts.MonoVersion)]
+[assembly: AssemblyVersion("1.1.9.1")]
 [assembly: AssemblyTitle ("Mono ILasm Compiler")]
 [assembly: AssemblyDescription ("Mono ILasm Compiler")]
 [assembly: AssemblyCopyright ("Sergey Chaban and Jackson Harper")]
