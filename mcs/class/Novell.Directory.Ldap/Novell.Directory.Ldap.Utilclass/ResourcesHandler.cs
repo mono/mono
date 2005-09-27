a5793b8f10dbbbd5e55acfa54a2bdf140b208233
Boris Kirzner <borisk@mono-cvs.ximian.com>
2005-09-27 15:43:29 +0000

    Added generated resx files and fixed referencing resources.
    svn path=/trunk/mcs/; revision=50873

--- mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Utilclass/ResourcesHandler.cs
+++ mcs/class/Novell.Directory.Ldap/Novell.Directory.Ldap.Utilclass/ResourcesHandler.cs
@@ -108,7 +108,7 @@ namespace Novell.Directory.Ldap.Utilclass
 		{
 			if (defaultMessages == null)
 			{
-				defaultMessages = new ResourceManager("ExceptionMessages", Assembly.GetExecutingAssembly());
+				defaultMessages = new ResourceManager("Novell.Directory.Ldap.Utilclass.ExceptionMessages", Assembly.GetExecutingAssembly());
 			}
 			
 			if (defaultLocale == null)
@@ -181,7 +181,7 @@ namespace Novell.Directory.Ldap.Utilclass
 			{
 /*
 				defaultResultCodes = ResourceManager.CreateFileBasedResourceManager("ResultCodeMessages", "Resources", null);*/
-				defaultResultCodes = new ResourceManager("ResultCodeMessages", Assembly.GetExecutingAssembly());
+				defaultResultCodes = new ResourceManager("Novell.Directory.Ldap.Utilclass.ResultCodeMessages", Assembly.GetExecutingAssembly());
 			}
 
 			if (defaultLocale == null)
