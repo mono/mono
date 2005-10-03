3969e85a166a689bb3c589542cb501a12657522c
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-10-03 08:32:32 +0000

    2005-10-03 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* InputFilterStream.cs proxy stream for use by filters.
    	* HttpRequest.cs: implement input filtering.
    
    
    svn path=/trunk/mcs/; revision=51107

--- mcs/class/System.Web/System.Web/HttpRequest.cs
+++ mcs/class/System.Web/System.Web/HttpRequest.cs
@@ -70,6 +70,8 @@ namespace System.Web {
 		WebROCollection all_params;
 		WebROCollection headers;
 		Stream input_stream;
+		InputFilterStream input_filter;
+		Stream filter;
 		HttpCookieCollection cookies;
 		string http_method;
 
@@ -328,18 +330,26 @@ namespace System.Web {
 			}
 		}
 
-		[MonoTODO]
 		public Stream Filter {
 			get {
-				throw new NotImplementedException ();
+				if (filter != null)
+					return filter;
+
+				if (input_filter == null)
+					input_filter = new InputFilterStream ();
+
+				return input_filter;
 			}
 
 			set {
-				throw new NotImplementedException ();
+				if (value == null)
+					throw new HttpException ("Invalid filter");
+
+				filter = value;
 			}
 		}
 
-		static Stream StreamCopy (Stream stream)
+		Stream StreamCopy (Stream stream)
 		{
 #if !TARGET_JVM
 			if (stream is IntPtrStream)
@@ -566,10 +576,32 @@ namespace System.Web {
 #else
 		const int INPUT_BUFFER_SIZE = 32*1024;
 
+		void DoFilter (byte [] buffer)
+		{
+			if (input_filter == null)
+				return;
+
+			// Replace the input with the filtered input
+			input_filter.BaseStream = input_stream;
+			MemoryStream ms = new MemoryStream ();
+			while (true) {
+				int n = filter.Read (buffer, 0, buffer.Length);
+				if (n <= 0)
+					break;
+				ms.Write (buffer, 0, n);
+			}
+			// From now on input_stream has the filtered input
+			input_stream = new MemoryStream (ms.GetBuffer (), 0, (int) ms.Length, false, true);
+		}
+
 		void MakeInputStream ()
 		{
+			if (input_stream != null)
+				return;
+
 			if (worker_request == null) {
 				input_stream = new MemoryStream (new byte [0], 0, 0, false, true);
+				DoFilter (new byte [1024]);
 				return;
 			}
 
@@ -593,6 +625,7 @@ namespace System.Web {
 				} else {
 					input_stream = new MemoryStream (buffer, 0, buffer.Length, false, true);
 				}
+				DoFilter (new byte [1024]);
 				return;
 			}
 
@@ -618,7 +651,7 @@ namespace System.Web {
 					total += n;
 				}
 				input_stream = new IntPtrStream (content, total);
-
+				DoFilter (buffer);
 			} else {
 				MemoryStream ms = new MemoryStream ();
 				if (total > 0)
@@ -636,6 +669,7 @@ namespace System.Web {
 					ms.Write (buffer, 0, n);
 				}
 				input_stream = new MemoryStream (ms.GetBuffer (), 0, (int) ms.Length, false, true);
+				DoFilter (buffer);
 			}
 
 			if (total < content_length)
@@ -654,6 +688,7 @@ namespace System.Web {
 			get {
 				if (input_stream == null)
 					MakeInputStream ();
+
 				return input_stream;
 			}
 		}
