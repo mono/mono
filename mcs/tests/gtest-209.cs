6854e6ad236bedbae3531577b4a98b3187eb7469
Martin Baulig <martin@novell.com>
2005-10-04 18:15:17 +0000

    New test.
    svn path=/trunk/mcs/; revision=51192

---
+++ mcs/tests/gtest-209.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary3
+{
+	public class Dictionary1<TKey, TValue> : Dictionary<TKey, TValue>
+	{ }
+
+	public class Test
+	{
+		static void Main ()
+		{
+			Dictionary1<Guid, String> _D = new Dictionary1<Guid, string>();
+			_D.Add(Guid.NewGuid(), "foo");
+		}
+	}
+}
+
