2272cfd02bac4b132109e2838edf2c05e416ddda
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-10-20 04:30:01 +0000

    2005-10-19 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* HttpResponseStream.cs: reworked buffering so that adjacents blocks are
    	written at once. Also fix bug #76460.
    
    
    svn path=/trunk/mcs/; revision=51964

--- mcs/class/System.Web/System.Web/HttpResponseStream.cs
+++ mcs/class/System.Web/System.Web/HttpResponseStream.cs
@@ -50,7 +50,6 @@ namespace System.Web {
 	internal class HttpResponseStream : Stream {
 		Bucket first_bucket;
 		Bucket cur_bucket;
-		Bucket split_after_file;
 		HttpResponse response;
 		internal long total;
 		Stream filter;
@@ -167,207 +166,73 @@ namespace System.Web {
 		}
 
 #else // TARGET_JVM
-		unsafe class Block {
-			byte* data;
-
+		unsafe sealed class BlockManager {
 			const int PreferredLength = 128 * 1024;
-			public const int ChunkSize = 32 * 1024;
-			const int PreferredChunks = PreferredLength / ChunkSize;
+			byte *data;
+			int position;
+			int block_size;
 
-			int actual_length;
-			int nchunks;
-			int taken;
-	
-			public Block Next, Prev;
-		
-	
-			public Block () : this (PreferredLength)
+			public BlockManager ()
 			{
 			}
 
-			public Block (int initial_size)
+			public int Position {
+				get { return position; }
+			}
+
+			void EnsureCapacity (int capacity)
 			{
-				if (initial_size <= PreferredLength) {
-					actual_length = PreferredLength;
-					nchunks = PreferredChunks;
-				} else {
-					actual_length = initial_size;
-					nchunks = 1;
+				if (block_size >= capacity)
+					return;
+
+				capacity += PreferredLength;
+				capacity = (capacity / PreferredLength) * PreferredLength;
+				data = (byte *) Marshal.ReAllocHGlobal ((IntPtr) data, (IntPtr) capacity);
+				block_size = capacity;
+			}
+
+			public void Write (byte [] buffer, int offset, int count)
+			{
+				EnsureCapacity (position + count);
+				Marshal.Copy (buffer, offset, (IntPtr) (data + position), count);
+				position += count;
+			}
+
+			public void Send (HttpWorkerRequest wr, int start, int end)
+			{
+				if (end - start <= 0)
+					return;
+
+				wr.SendResponseFromMemory ((IntPtr) (data + start), end - start);
+			}
+
+			public void Send (Stream stream, int start, int end)
+			{
+				int len = end - start;
+				if (len <= 0)
+					return;
+
+				byte [] buffer = new byte [Math.Min (len, 32 * 1024)];
+				int size = buffer.Length;
+				while (len > 0) {
+					Marshal.Copy ((IntPtr) (data + start), buffer, 0, size);
+					stream.Write (buffer, 0, size);
+					start += size;
+					len -= size;
+					if (len > 0 && len < size)
+						size = len;
 				}
-				data = (byte*) Marshal.AllocHGlobal (actual_length);
 			}
-	
+			
 			public void Dispose ()
 			{
-				Marshal.FreeHGlobal ((IntPtr) data);
-			}
-	
-			public Chunk GetChunk (int size)
-			{
-				Chunk c = new Chunk (size);
-				// (number of chunks to allocate - 1)
-				int gimme = (size / ChunkSize);
-				for (int i = 0; i < nchunks; i ++) {
-					if ((taken & (1 << i)) == 0) {
-						c.block = this;
-						c.block_area = 1 << i;
-						c.data = data + i * (ChunkSize + gimme);
-						int shift = 0;
-						do {
-							taken |= (c.block_area << shift);
-							gimme--;
-							shift++;
-						} while (gimme >= 0);
-						return c;
-					}
-				}
-	
-				throw new Exception ("Internal error: shouldn't get here");
-			}
-	
-			public static Block Unlink (ref Block list, Block b)
-			{
-				if (b.Prev != null)
-					b.Prev.Next = b.Next;
-				if (b.Next != null)
-					b.Next.Prev = b.Prev;
-				if (b == list)
-					list = list.Next;
-				
-				b.Next = b.Prev = null;
-				return b;
-			}
-	
-			public static void Link (ref Block head, Block b)
-			{
-				b.Next = head;
-				if (head != null)
-					head.Prev = b;
-				head = b;
-			}
-	
-			public void Return (Chunk c)
-			{
-				taken &= ~c.block_area;
-			}
-			
-			public bool IsEmpty {
-				get {
-					return taken == 0;
+				if ((IntPtr) data != IntPtr.Zero) {
+					Marshal.FreeHGlobal ((IntPtr) data);
+					data = (byte *) IntPtr.Zero;
 				}
 			}
-	
-			public bool IsFull {
-				get {				
-					return taken == (1 << nchunks) - 1;
-				}
-			}
-	
-			public override string ToString ()
-			{
-				string bitmap = "";
-				for (int i = 0; i < nchunks; i ++) {
-					if ((taken & (1 << i)) == 0)
-						bitmap += ".";
-					else
-						bitmap += "x";
-				}
-	
-				return String.Format ("0x{0:x} {1}", (IntPtr) data, bitmap);
-			}
-			
 		}
-	
-		unsafe struct Chunk {
-			public byte* data;
-			public int size;
-			public int block_area;
-			public Block block;
 
-			public Chunk (int size)
-			{
-				this.size = size;
-				data = (byte *) 0;
-				block_area = 0;
-				block = null;
-			}
-
-			public static void Copy(byte[] buff, int offset, Chunk c, int pos, int len)
-			{
-				Marshal.Copy (buff, offset, (IntPtr) (c.data + pos), len);
-			}
-
-			public static void Copy(Chunk c, int pos, byte[] buff, int offset, int len)
-			{
-				Marshal.Copy ((IntPtr) (c.data + pos), buff, offset, len);
-			}
-	
-			static Block filled;
-			static Block part_filled;
-			static Block empty;
-	
-			public static Chunk GetChunk (int cb)
-			{
-				if (cb < Block.ChunkSize)
-					cb = Block.ChunkSize;
-
-				Chunk c;
-				if (part_filled != null) {
-					c = part_filled.GetChunk (cb);
-					if (part_filled.IsFull)
-						Block.Link (ref filled, Block.Unlink (ref part_filled, part_filled));
-					
-					return c;
-				}
-	
-				if (empty == null)
-					Block.Link (ref empty, new Block (cb));
-				
-				c = empty.GetChunk (cb);
-				if (empty.IsFull) // account for the case where we have 1 chunk/block
-					Block.Link (ref filled, Block.Unlink (ref empty, empty));
-				else
-					Block.Link (ref part_filled, Block.Unlink (ref empty, empty));
-				return c;
-			}
-	
-			public static void DisposeChunk (Chunk c)
-			{
-				Block b = c.block;
-				bool was_full = b.IsFull;
-	
-				b.Return (c);
-	
-				if (was_full)
-					Block.Link (ref part_filled, Block.Unlink (ref filled, b));
-				if (b.IsEmpty)
-					Block.Link (ref empty, Block.Unlink (ref part_filled, b));
-			}
-	
-			public static void DisposeEmptyChunks ()
-			{
-				for (Block b = empty; b != null; b = b.Next)
-					b.Dispose ();
-				empty = null;
-			}
-			
-			/*	
-			public static void PrintState ()
-			{
-				Console.WriteLine ("Filled blocks:");
-				for (Block b = filled; b != null; b = b.Next)
-					Console.WriteLine ("\t{0}", b);
-				Console.WriteLine ("Part Filled blocks:");
-				for (Block b = part_filled; b != null; b = b.Next)
-					Console.WriteLine ("\t{0}", b);
-				Console.WriteLine ("Empty blocks:");
-				for (Block b = empty; b != null; b = b.Next)
-					Console.WriteLine ("\t{0}", b);	
-				
-			}
-			*/
-			
-		}
 #endif
 		abstract class Bucket {
 			public Bucket Next;
@@ -385,89 +250,57 @@ namespace System.Web {
 		unsafe
 #endif
 		class ByteBucket : Bucket {
-			Chunk c;
 			int start;
-			int pos;
-			int rem;
-			bool partial;
-	
-			ByteBucket () {}
-			
-			public ByteBucket (int cb)
+			int length;
+			public BlockManager blocks;
+			public bool Expandable = true;
+
+			public ByteBucket () : this (null)
 			{
-				c = Chunk.GetChunk (cb);
-				start = pos = 0;
-				rem = c.size;
+			}
+
+			public ByteBucket (BlockManager blocks)
+			{
+				if (blocks == null)
+					blocks = new BlockManager ();
+
+				this.blocks = blocks;
+				start = blocks.Position;
 			}
 
 			public override int Length {
-				get { return pos - start; }
+				get { return length; }
 			}
 
 			public int Write (byte [] buf, int offset, int count)
 			{
-				int copy = Math.Min (rem, count);
-				if (copy == 0)
-					return copy;
+				if (Expandable == false)
+					throw new Exception ("This should not happen.");
 
-				Chunk.Copy (buf, offset, c, pos, copy);
-				
-				pos += copy;
-				rem -= copy;
-	
-				return copy;
+				blocks.Write (buf, offset, count);
+				length += count;
+				return count;
 			}
-	
-			public ByteBucket SplitOff ()
-			{
-				// Don't give people a really short bucket.
-				if (rem < 4 * 1024)
-					return null;
-	
-				ByteBucket b = new ByteBucket ();
-				b.partial = true;
-				b.c = c;
-				b.start = b.pos = pos;
-				b.rem = rem;
-				return b;
-			}
-			
-			
+
 			public override void Dispose ()
 			{
-				if (!partial)
-					Chunk.DisposeChunk (c);
+				blocks.Dispose ();
 			}
 
 			public override void Send (HttpWorkerRequest wr)
 			{
-				int len = pos - start;
-#if false
-				for (int i = 0; i < len; i++)
-					Console.Write ("[{0}:{1}]", start [i], start [i]);
-				Console.WriteLine ("Sending {0} bytes", len);
-				Console.WriteLine (Environment.StackTrace);
-#endif
-				
-#if TARGET_JVM
-				if (start == 0)
-					wr.SendResponseFromMemory (c.data, len);
-				else
-				{
-					byte[] buf = new byte[len];
-					Chunk.Copy(c, start, buf, 0, len);
-				}
-#else
-				wr.SendResponseFromMemory ((IntPtr) (c.data + start), len);
-#endif
+				if (length == 0)
+					return;
+
+				blocks.Send (wr, start, length);
 			}
 
 			public override void Send (Stream stream)
 			{
-				int len = (int) (pos - start);
-				byte [] copy = new byte [len];
-				Chunk.Copy (c, start, copy, 0, len);
-				stream.Write (copy, 0, len);
+				if (length == 0)
+					return;
+
+				blocks.Send (stream, start, length);
 			}
 		}
 	
@@ -560,9 +393,10 @@ namespace System.Web {
 
 			if (response.use_chunked) 
 				SendChunkSize (total, false);
-			
-			for (Bucket b = first_bucket; b != null; b = b.Next) 
+
+			for (Bucket b = first_bucket; b != null; b = b.Next) {
 				b.Send (wr);
+			}
 
 			if (response.use_chunked) {
 				SendChunkSize (-1, false);
@@ -594,14 +428,21 @@ namespace System.Web {
 
 		public void WriteFile (string f, long offset, long length)
 		{
+			if (length == 0)
+				return;
+
 			ByteBucket bb = cur_bucket as ByteBucket;
 
-			if (bb != null)
-				split_after_file = bb.SplitOff ();
+			if (bb != null) {
+				bb.Expandable = false;
+				bb = new ByteBucket (bb.blocks);
+			}
 
 			total += length;
 			
 			AppendBucket (new BufferedFileBucket (f, offset, length));
+			if (bb != null)
+				AppendBucket (bb);
 			// Flush () is called from HttpResponse if needed (WriteFile/TransmitFile)
 		}
 
@@ -669,26 +510,11 @@ namespace System.Web {
 
 		void AppendBuffer (byte [] buffer, int offset, int count)
 		{
-			if (cur_bucket == null)
-				AppendBucket (new ByteBucket (count));
+			if (!(cur_bucket is ByteBucket))
+				AppendBucket (new ByteBucket ());
 
 			total += count;
-			while (count > 0) {
-				if (cur_bucket is ByteBucket) {
-					int n = ((ByteBucket) cur_bucket).Write (buffer, offset, count);
-					offset += n;
-					count -= n;
-				}
-	
-				if (split_after_file != null) {
-					AppendBucket (split_after_file);
-					split_after_file = null;
-					continue;
-				}
-				
-				if (count != 0)
-					AppendBucket (new ByteBucket (count));
-			}
+			((ByteBucket) cur_bucket).Write (buffer, offset, count);
 		}
 
 		//
@@ -708,7 +534,6 @@ namespace System.Web {
 
 			first_bucket = null;
 			cur_bucket = null;
-			split_after_file = null;
 		}
 
 		public void Clear ()
