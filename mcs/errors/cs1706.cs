f174cba417d4da8a1e9605c74036fc59cdc62a68
Marek Safar <marek.safar@gmail.com>
2005-10-23 17:40:10 +0000

    new test
    svn path=/trunk/mcs/; revision=52091

---
+++ mcs/errors/cs1706.cs
@@ -0,0 +1,17 @@
+// cs1706.cs: Anonymous methods are not allowed in attribute declaration
+// Line: 13
+
+using System;
+
+delegate void TestDelegate();
+
+class MyAttr : Attribute
+{
+    public MyAttr (TestDelegate d) { }
+}
+
+[MyAttr (delegate {} )]
+class C
+{
+}
+
