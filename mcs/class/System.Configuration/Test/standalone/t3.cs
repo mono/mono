ce7a75680c114579965f626dd30985a168db5095
Chris Toshok <toshok@novell.com>
2005-10-24 17:48:45 +0000

    modify t3 slightly
    svn path=/trunk/mcs/; revision=52138

--- mcs/class/System.Configuration/Test/standalone/t3.cs
+++ mcs/class/System.Configuration/Test/standalone/t3.cs
@@ -18,6 +18,8 @@ class T1
 
 			Console.WriteLine("IsMachineLevel: {0}", config.EvaluationContext.IsMachineLevel);
 
+			Console.WriteLine ("appsettings.Settings.AllKeys.Count = {0}", appsettings.Settings.AllKeys.Length);
+
 			foreach (string key in appsettings.Settings.AllKeys) {
 				Console.WriteLine ("settings[{0}] = {1}", appsettings.Settings[key].Key, appsettings.Settings[key].Value);
 			}
