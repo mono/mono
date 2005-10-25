63a795bf23e4926d7d5d0e3a0010f34f69e09307
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2005-10-25 12:11:20 +0000

    2005-10-25  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* gcs1700.cs, gcs1726.cs, gcs0647.cs, gcs0647-2.cs: 
    	  New tests.
    
    	* InternalVisibleTo.snk: New key file for gcs1726.cs.
    	  
    
    svn path=/trunk/mcs/; revision=52177

---
+++ mcs/errors/gcs1700.cs
@@ -0,0 +1,17 @@
+// gcs1700.cs: Assembly reference `MyAssemblyName, Version=' is invalid and cannot be resolved
+// Line: 8
+// Compiler options: -warnaserror -warn:3
+
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("MyAssemblyName, Version=")]
+
+public class InternalsVisibleToTest 
+{
+	static void Main ()
+	{
+	}
+
+}
+
---
+++ mcs/errors/gcs1726.cs
@@ -0,0 +1,16 @@
+// gcs1726.cs: Friend assembly reference 'MyAssemblyName' is invalid. Strong named assemblies must specify a public key token in their InternalsVisibleTo declarations
+// Line: 8
+// Compiler options: -keyfile:InternalsVisibleTest.snk
+
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("MyAssemblyName")]
+
+public class Test
+{
+	static void Main ()
+	{
+	}
+}
+
