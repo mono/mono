ec1dca6f61c61acc5b45a2c36de3b570116e3ec5
Sebastien Pouliot <sebastien@ximian.com>
2005-11-02 16:17:52 +0000

    2005-11-02  Sebastien Pouliot  <sebastien@ximian.com>
    	* Executor.cs: Reworked (for permissions) and fixed ExitCode (must be 
    	called before Close).
    
    
    svn path=/trunk/mcs/; revision=52477

--- mcs/class/System/System.CodeDom.Compiler/Executor.cs
+++ mcs/class/System/System.CodeDom.Compiler/Executor.cs
@@ -30,6 +30,7 @@
 
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Permissions;
 using System.Security.Principal;
 using System.Threading;
@@ -75,58 +76,30 @@ namespace System.CodeDom.Compiler {
 		{
 			string outputName = null;
 			string errorName = null;
-			ExecWaitWithCapture ((IntPtr)(-1), cmd, Environment.CurrentDirectory, tempFiles, ref outputName, ref errorName);
+			ExecWaitWithCapture (cmd, Environment.CurrentDirectory, tempFiles, ref outputName, ref errorName);
 		}
 
+		[SecurityPermission (SecurityAction.Demand, UnmanagedCode = true)]
+		[SecurityPermission (SecurityAction.Assert, ControlPrincipal = true)] // UnmanagedCode "covers" more than ControlPrincipal
 		public static Int32 ExecWaitWithCapture (IntPtr userToken, string cmd, string currentDir, TempFileCollection tempFiles, ref string outputName, ref string errorName)
 		{
-			if (outputName == null)
-				outputName = tempFiles.AddExtension ("out");
-			
-			if (errorName == null)
-				errorName = tempFiles.AddExtension ("err");
-
-			int exit_code = -1;
-			WindowsImpersonationContext context = null;
+#if NET_2_0
+			// WindowsImpersonationContext implements IDisposable only in 2.0
+			using (WindowsImpersonationContext context = WindowsIdentity.Impersonate (userToken)) {
+				return InternalExecWaitWithCapture (cmd, currentDir, tempFiles, ref outputName, ref errorName);
+			}
+#else
+			int result = -1;
+			WindowsImpersonationContext context = WindowsIdentity.Impersonate (userToken);
 			try {
-				if (userToken != (IntPtr)(-1)) {
-					context = WindowsIdentity.Impersonate (userToken);
-				}
-				
-				Process proc = new Process ();
-				proc.StartInfo.FileName = cmd;
-				proc.StartInfo.CreateNoWindow = true;
-				proc.StartInfo.UseShellExecute = false;
-				proc.StartInfo.RedirectStandardOutput = true;
-				proc.StartInfo.RedirectStandardError = true;
-				proc.StartInfo.WorkingDirectory = currentDir;
-			
-				try {
-					proc.Start();
-				
-					ProcessResultReader outReader = new ProcessResultReader (proc.StandardOutput, outputName);
-					ProcessResultReader errReader = new ProcessResultReader (proc.StandardError, errorName);
-				
-					Thread t = new Thread (new ThreadStart (errReader.Read));
-					t.Start ();
-				
-					outReader.Read ();
-					t.Join ();
-				
-					proc.WaitForExit();
-				} 
-				finally  {
-					proc.Close ();
-					exit_code = proc.ExitCode;
-				}
+				result = InternalExecWaitWithCapture (cmd, currentDir, tempFiles, ref outputName, ref errorName);
 			}
 			finally {
-				if (context != null) {
-					context.Undo ();
-					context = null;
-				}
+				context.Undo ();
+				context = null;
 			}
-			return exit_code;
+			return result;
+#endif
 		}
 		
 		public static Int32 ExecWaitWithCapture (IntPtr userToken, string cmd, TempFileCollection tempFiles, ref string outputName, ref string errorName)
@@ -134,14 +107,58 @@ namespace System.CodeDom.Compiler {
 			return ExecWaitWithCapture (userToken, cmd, Environment.CurrentDirectory, tempFiles, ref outputName, ref errorName);
 		}
 
+		[SecurityPermission (SecurityAction.Demand, UnmanagedCode = true)]
 		public static Int32 ExecWaitWithCapture (string cmd, string currentDir, TempFileCollection tempFiles, ref string outputName, ref string errorName )
 		{
-			return ExecWaitWithCapture ((IntPtr)(-1), cmd, currentDir, tempFiles, ref outputName, ref errorName);
+			return InternalExecWaitWithCapture (cmd, currentDir, tempFiles, ref outputName, ref errorName);
 		}
 
+		[SecurityPermission (SecurityAction.Demand, UnmanagedCode = true)]
 		public static Int32 ExecWaitWithCapture (string cmd, TempFileCollection tempFiles, ref string outputName, ref string errorName)
 		{
-			return ExecWaitWithCapture ((IntPtr)(-1), cmd, Environment.CurrentDirectory, tempFiles, ref outputName, ref errorName);
+			return InternalExecWaitWithCapture (cmd, Environment.CurrentDirectory, tempFiles, ref outputName, ref errorName);
+		}
+
+		private static int InternalExecWaitWithCapture (string cmd, string currentDir, TempFileCollection tempFiles, ref string outputName, ref string errorName)
+		{
+			if ((cmd == null) || (cmd.Length == 0))
+				throw new ExternalException (Locale.GetText ("No command provided for execution."));
+
+			if (outputName == null)
+				outputName = tempFiles.AddExtension ("out");
+			
+			if (errorName == null)
+				errorName = tempFiles.AddExtension ("err");
+
+			int exit_code = -1;
+			Process proc = new Process ();
+			proc.StartInfo.FileName = cmd;
+			proc.StartInfo.CreateNoWindow = true;
+			proc.StartInfo.UseShellExecute = false;
+			proc.StartInfo.RedirectStandardOutput = true;
+			proc.StartInfo.RedirectStandardError = true;
+			proc.StartInfo.WorkingDirectory = currentDir;
+			
+			try {
+				proc.Start();
+			
+				ProcessResultReader outReader = new ProcessResultReader (proc.StandardOutput, outputName);
+				ProcessResultReader errReader = new ProcessResultReader (proc.StandardError, errorName);
+				
+				Thread t = new Thread (new ThreadStart (errReader.Read));
+				t.Start ();
+			
+				outReader.Read ();
+				t.Join ();
+				
+				proc.WaitForExit();
+			} 
+			finally  {
+				exit_code = proc.ExitCode;
+				// the handle is cleared in Close (so no ExitCode)
+				proc.Close ();
+			}
+			return exit_code;
 		}
 	}
 }
