12f532abd1360fcfc512a5d761e06c3e8215f0e9
Jackson Harper <jackson@novell.com>
2005-11-03 22:39:36 +0000

    * TreeNodeConverter.cs: Implement to get off my class-status           back.
    svn path=/trunk/mcs/; revision=52536

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeNodeConverter.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeNodeConverter.cs
@@ -27,22 +27,25 @@
 // NOT COMPLETE
 
 using System.ComponentModel;
+using System.Globalization;
 
 namespace System.Windows.Forms {
 	public class TreeNodeConverter : TypeConverter {
-		#region Public Constructors
-		public TreeNodeConverter() {
-		}
-		#endregion	// Public Constructors
 
 		#region Public Instance Methods
-		[MonoTODO]
-		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) {
+		public override bool CanConvertTo (ITypeDescriptorContext context, Type destinationType)
+		{
+			if (destinationType == typeof (string))
+				return true;
 			return base.CanConvertTo (context, destinationType);
 		}
 
-		[MonoTODO]
-		public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType) {
+		public override object ConvertTo (ITypeDescriptorContext context, CultureInfo culture,
+				object value, Type destinationType)
+		{
+			// Regardless of the type of object passed in this does a ToString
+			if (destinationType == typeof (string) && value != null)
+				return value.ToString ();
 			return base.ConvertTo (context, culture, value, destinationType);
 		}
 
