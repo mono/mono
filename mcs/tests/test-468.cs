fcacd1a1fa3fdfec286eaedb054ea116603cd094
Marek Safar <marek.safar@gmail.com>
2005-11-08 09:57:51 +0000

    update
    svn path=/trunk/mcs/; revision=52689

--- mcs/tests/test-468.cs
+++ mcs/tests/test-468.cs
@@ -1,21 +1,29 @@
 using System;
 using System.Runtime.InteropServices;
 
-    [ComImport, GuidAttribute("E5CB7A31-7512-11D2-89CE-0080C792E5D8")]
-    public class CorMetaDataDispenserExClass { }
+	[ComImport, GuidAttribute("E5CB7A31-7512-11D2-89CE-0080C792E5D8")]
+	public class CorMetaDataDispenserExClass { }
 
-    [ComImport, GuidAttribute("31BCFCE2-DAFB-11D2-9F81-00C04F79A0A3"),
-CoClass(typeof(CorMetaDataDispenserExClass))]
-    public interface MetaDataDispenserEx { }
+	[ComImport, GuidAttribute("31BCFCE2-DAFB-11D2-9F81-00C04F79A0A3"),
+	CoClass(typeof(CorMetaDataDispenserExClass))]
+	public interface IMetaDataDispenserEx { }
 
-public class Test {
 	
-	public static void XXX () {
-		Object o = new MetaDataDispenserEx ();
+public class Test
+{
+	public static void XXX ()
+	{
+		IMetaDataDispenserEx o = new IMetaDataDispenserEx();
 	}
 
-	static void Main ()
+	public static void Main()
 	{
-}
-	
+		/* It doesn't work on Mono runtime
+		IMetaDataDispenserEx o = new IMetaDataDispenserEx();
+		if (o.GetType () != typeof (CorMetaDataDispenserExClass))
+			return 1;
+		Console.WriteLine ("OK");
+		return 0;
+		*/
+	}
 }
