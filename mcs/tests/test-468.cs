e97c1feefc01df0f6f0d5e57dc07dc0d62dc6d38
Miguel de Icaza <miguel@gnome.org>
2005-11-08 00:56:44 +0000

    Add tests
    svn path=/trunk/mcs/; revision=52672

---
+++ mcs/tests/test-468.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices;
+
+    [ComImport, GuidAttribute("E5CB7A31-7512-11D2-89CE-0080C792E5D8")]
+    public class CorMetaDataDispenserExClass { }
+
+    [ComImport, GuidAttribute("31BCFCE2-DAFB-11D2-9F81-00C04F79A0A3"),
+CoClass(typeof(CorMetaDataDispenserExClass))]
+    public interface MetaDataDispenserEx { }
+
+public class Test {
+	
+	public static void Main () {
+		Object o = new MetaDataDispenserEx ();
+	}
+}
