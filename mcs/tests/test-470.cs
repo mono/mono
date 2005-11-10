0a0ff8e30e9810b4349f0843f9733213d68f5a9e
Marek Safar <marek.safar@gmail.com>
2005-11-10 10:27:18 +0000

    New test
    svn path=/trunk/mcs/; revision=52840

---
+++ mcs/tests/test-470.cs
@@ -0,0 +1,19 @@
+// This code must be compilable without any warning
+// Compiler options: -warnaserror -warn:4
+
+class X
+{
+	public string ASTNodeTypeName
+	{
+		get 
+		{ 
+			return typeof(int).FullName;; 
+		}
+	}
+}
+
+class Demo {
+	static void Main ()
+	{
+	}
+}
