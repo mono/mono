1a68584d0c3f8374d2250303ff90d56502624d14
Atsushi Eno <atsushieno@gmail.com>
2005-11-15 22:04:32 +0000

    2005-11-15  Atsushi Enomoto  <atsushi@ximian.com>
    	* typemanager.cs : (GetFullNameSignature) differentiate indexers
    	  with different parameters. Fixed the second problem in #76685.
    
    	* cs0419-2.cs : test to check error message problem in #76685.
    
    
    svn path=/trunk/mcs/; revision=53091

--- mcs/mcs/typemanager.cs
+++ mcs/mcs/typemanager.cs
@@ -587,6 +587,14 @@ public class TypeManager {
 	/// </summary>
 	static public string GetFullNameSignature (MemberInfo mi)
 	{
+		PropertyInfo pi = mi as PropertyInfo;
+		if (pi != null) {
+			MethodBase pmi = pi.GetGetMethod ();
+			if (pmi == null)
+				pmi = pi.GetSetMethod ();
+			if (GetParameterData (pmi).Count > 0)
+				mi = pmi;
+		}
 		return (mi is MethodBase)
 			? CSharpSignature (mi as MethodBase) 
 			: CSharpName (mi.DeclaringType) + '.' + mi.Name;
@@ -629,14 +637,17 @@ public class TypeManager {
 
 		// Is indexer
 		if (mb.IsSpecialName && !mb.IsConstructor) {
-			if (iparams.Count > 1) {
+			if (iparams.Count > (mb.Name.StartsWith ("get_") ? 0 : 1)) {
 				sig.Append ("this[");
 				if (show_accessor) {
 					sig.Append (parameters.Substring (1, parameters.Length - 2));
 				}
 				else {
 					int before_ret_val = parameters.LastIndexOf (',');
-					sig.Append (parameters.Substring (1, before_ret_val - 1));
+					if (before_ret_val < 0)
+						sig.Append (parameters.Substring (1, parameters.Length - 2));
+					else
+						sig.Append (parameters.Substring (1, before_ret_val - 1));
 				}
 				sig.Append (']');
 			} else {
