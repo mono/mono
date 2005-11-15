3e21ee3660d9d6a73fca789e599c2b9d6f09f3a4
Martin Baulig <martin@novell.com>
2005-11-15 14:30:40 +0000

    New test.
    svn path=/trunk/mcs/; revision=53058

---
+++ mcs/tests/gtest-225.cs
@@ -0,0 +1,29 @@
+public class Set<Element>
+{
+	protected readonly Node[] sub;
+	public Set () { }
+
+	public struct Locator
+	{
+		public delegate void Replace (Node node);
+		public Locator (Replace put) { }
+	}
+
+	public class Node : Set<Element>
+	{ }
+
+	protected Locator locate (Element x)
+	{
+		Set<Element> parent = this;
+		return new Locator (new Locator.Replace (delegate (Node n) {
+			parent.sub[0] = n;
+		}));
+	}
+}
+
+static class SetTest
+{
+	static void Main ()
+	{
+	}
+}
