7503917f4f8a2da3f029a0bca8a924668924de1d
Konstantin Triger <kostat@mono-cvs.ximian.com>
2005-11-30 15:08:44 +0000

    merge -r 53682:53683
    svn path=/branches/mainsoft/gh1.7/mcs/; revision=53685

--- mcs/class/System.Data/jay.bat
+++ mcs/class/System.Data/jay.bat
@@ -1,2 +1,3 @@
-echo "processing Parser.jay..."
-%1\..\..\jay\jay.exe -ct < %1\..\..\jay\skeleton.cs %1\Mono.Data.SqlExpressions\Parser.jay > %1\Mono.Data.SqlExpressions\Parser.cs
+echo on
+@echo "processing Parser.jay..."
+"%1\..\..\jay\jay.exe" -ct < "%1..\..\jay\skeleton.cs" "%1Mono.Data.SqlExpressions\Parser.jay" > "%1Mono.Data.SqlExpressions\Parser.cs"
