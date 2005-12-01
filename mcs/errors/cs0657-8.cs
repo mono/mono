cb5cd9b69b7c10e4402953ce9b87d2dbeef9df38
Marek Safar <marek.safar@gmail.com>
2005-12-01 12:02:40 +0000

    Fixed.
    svn path=/trunk/mcs/; revision=53757

--- mcs/errors/cs0657-8.cs
+++ mcs/errors/cs0657-8.cs
@@ -1,5 +1,7 @@
 // cs0657-8.cs: `param' is not a valid attribute location for this declaration. Valid attribute locations for this declaration are `method, return'
-// Line : 4
+// Line : 9
+
+using System;
 
 struct S
 {
