fb3d387f8cc1bc92ddd11558a10d89795dd204ad
Marek Safar <marek.safar@gmail.com>
2005-12-05 14:23:25 +0000

    2005-12-05  Marek Safar <marek.safar@seznam.cz>
    	* test-475.cs: Event initializer.
    	* test-465.cs: One more resource test.
    
    svn path=/trunk/mcs/; revision=53927

--- mcs/tests/test-349.cs
+++ mcs/tests/test-349.cs
@@ -1,18 +1,25 @@
 // Compiler options: -optimize+
 
 // TODO: I will have to investigate how to test that ctor is really empty
+using System;
 
 class C
 {
     public C () {}
 
     int i = new int ();
+	int i2 = 1 - 1;
     double d = new double ();
     char c = new char ();
     bool b = new bool ();
     decimal dec2 = new decimal ();
-    object o = new object ();
-	    
+	object o = null;
+	ValueType BoolVal = (ValueType)null;
+		
+	int[] a_i = null;
+	object[] a_o = null;
+	ValueType[] a_v = null;
+	
     public static void Main ()
     {
     }
