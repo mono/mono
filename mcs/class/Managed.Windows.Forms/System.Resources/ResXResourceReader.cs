a04980c512ae98e1f62d39516f8407eb0409a670
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-12-06 04:11:14 +0000

    2005-12-05  Peter Bartok  <pbartok@novell.com>
    	* ResXResourceReader.cs: Removed previous patch. More than just Byte[]
    	  has that mime type and the patch was causing things to fail.
    	  Since our writer no longer emits the mimetype this should be fine.
    
    
    svn path=/trunk/mcs/; revision=53966

--- mcs/class/Managed.Windows.Forms/System.Resources/ResXResourceReader.cs
+++ mcs/class/Managed.Windows.Forms/System.Resources/ResXResourceReader.cs
@@ -200,7 +200,7 @@ namespace System.Resources
 						object v = null;
 						string val = get_value (reader, "value");
 
-						if (mt != null && tt != null && mt != ResXResourceWriter.ByteArraySerializedObjectMimeType) {
+						if (mt != null && tt != null) {
 							TypeConverter c = TypeDescriptor.GetConverter (tt);
 							v = c.ConvertFrom (Convert.FromBase64String (val));
 						} else if (tt != null) {
