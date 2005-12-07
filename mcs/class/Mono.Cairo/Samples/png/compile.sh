bc46339a0477517ad1172489f19987280699517b
John Luke <jluke@mono-cvs.ximian.com>
2005-12-07 03:05:54 +0000

    replace circles sample with knockout
    svn path=/trunk/mcs/; revision=54019

--- mcs/class/Mono.Cairo/Samples/png/compile.sh
+++ mcs/class/Mono.Cairo/Samples/png/compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp circles.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp arc.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp arcneg.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp clip.cs 
@@ -10,4 +9,5 @@ mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp fillstroke.cs
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp gradient.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp image.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp image_pattern.cs 
+mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp knockout.cs 
 mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp text.cs 
