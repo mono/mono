b089a9f47c7ece3de267e80498d5967be43b7047
Miguel de Icaza <miguel@gnome.org>
2005-12-14 16:19:29 +0000

    New test, for bug 76914
    svn path=/trunk/mcs/; revision=54367

---
+++ mcs/tests/test-478.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+
+public class Position {
+}
+
+public class Board {
+    public enum Stone : int {
+	None = 0,
+	    Empty = 1,
+	    Black = 2,
+	    White = 3
+    }
+
+    public Stone Get(Position p)
+    {
+	return (Stone)p;
+    }
+
+    public static void Main() {
+    }
+    
+}
+
+
+
+
+
+
