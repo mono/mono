9e246be799d505af15157b042be0e54e33463986
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-12-21 09:32:29 +0000

    2005-12-21  Peter Dennis Bartok  <pbartok@novell.com>
    	* TextControl.cs (RecalculateLine): Only wrap if the wrap point is
    	  neither the beginning nor the end of the line (Fixes bug #76479)
    
    
    svn path=/trunk/mcs/; revision=54685

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextControl.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextControl.cs
@@ -452,7 +452,7 @@ namespace System.Windows.Forms {
 				}
 
 				if (doc.wrap) {
-					if ((widths[pos] + w) + 27 > (doc.viewport_width - this.right_indent)) {
+					if ((wrap_pos > 0) && (wrap_pos != len) && (widths[pos] + w) + 27 > (doc.viewport_width - this.right_indent)) {
 						pos = wrap_pos;
 						tag.width = wrap_width;
 						doc.Split(this, tag, pos, true);
