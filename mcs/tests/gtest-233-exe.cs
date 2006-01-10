4dd432d3cedbc615d44bc6954dac670074337c4b
Martin Baulig <martin@novell.com>
2006-01-10 21:03:44 +0000

    New test.
    svn path=/trunk/mcs/; revision=55342

---
+++ mcs/tests/gtest-233-exe.cs
@@ -0,0 +1,17 @@
+// Compiler options: /r:gtest-233-lib.dll
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+class Program
+{
+	static void Main (string[] args)
+	{
+		MyClass<int> list = new MyClass<int>();
+
+		list.ListChanged += new ListChangedEventHandler (list_ListChanged);
+	}
+
+	static void list_ListChanged (object sender, ListChangedEventArgs e) { }
+}
