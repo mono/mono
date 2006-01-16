9f2c01ae30404ca1ed7868559c63a8bdf1969b15
Raja R Harinath <harinath@hurrynot.org>
2006-01-16 14:07:40 +0000

    In mcs: 	Fix #76874. 	* ecore.cs (MemberAccess.CheckIntermediateModification): Remove. 	(UnboxCast.DoResolveLValue): New.  Move CS0445 check from 	CheckIntermediateModification. 	(FieldExpr.DoResolve): Add new two-argument version that 	allows us to resolve the InstanceExpression as an lvalue. 	The one-argument variant is now just a wrapper. 	(FieldExpr.DoResolveLValue): Use two-argument DoResolve. 	Resolve the lhs as an lvalue if the it has a value type. 	(FieldExpr.AssignToReadonly): Move CS1648 and CS1650 checks 	from Assign.DoResolve. 	(PropertyExpr.InstanceResolve): Allow InstanceExpression to be 	resolved as an lvalue. 	(PropertyExpr.DoResolve): Update. 	(PropertyExpr.DoResolveLValue): Resolve the lhs as an lvalue if it 	has a value type.  Move CS1612 check here from 	CheckIntermediateModification. 	* assign.cs (Assign.DoResolve): Remove CS1648 and CS1650 checks. 	* expression.cs (EmptyExpression.OutAccess): New.  Used as the 	'right_side' of a ResolveLValue on an 'out' argument. 	(EmptyExpression.LValueMemberAccess): New.  Used as the 	'right_side' of a propagated ResolveLValue on a value type. 	(LocalVariableReference.DoResolveBase): Recognize 	EmptyExpression.OutAccess and EmptyExpression.LValueMemberAccess. 	Add CS1654 check. 	(Argument.Resolve): Use EmptyExpression.OutAccess rather than 	EmptyExpression.Null.
    In tests:
    	* test-488.cs: New test based on #76874.
    
    In errors:
    	* cs1654.cs, cs1654-2, cs1656-4: New tests from #76874.
    
    svn path=/trunk/mcs/; revision=55631

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -3410,7 +3410,7 @@ namespace Mono.CSharp {
 				local_info = Block.GetLocalInfo (Name);
 
 				// is out param
-				if (lvalue_right_side == EmptyExpression.Null)
+				if (lvalue_right_side == EmptyExpression.OutAccess)
 					local_info.Used = true;
 
 				is_readonly = local_info.ReadOnly;
@@ -3421,8 +3421,11 @@ namespace Mono.CSharp {
 			VariableInfo variable_info = local_info.VariableInfo;
 			if (lvalue_right_side != null){
 				if (is_readonly){
-					if (lvalue_right_side is LocalVariableReference || lvalue_right_side == EmptyExpression.Null)
+					if (lvalue_right_side is LocalVariableReference || lvalue_right_side == EmptyExpression.OutAccess)
 						Report.Error (1657, loc, "Cannot pass `{0}' as a ref or out argument because it is a `{1}'",
+							Name, local_info.GetReadOnlyContext ());
+					else if (lvalue_right_side == EmptyExpression.LValueMemberAccess)
+						Report.Error (1654, loc, "Cannot assign to members of `{0}' because it is a `{1}'",
 							Name, local_info.GetReadOnlyContext ());
 					else
 						Report.Error (1656, loc, "Cannot assign to `{0}' because it is a `{1}'",
@@ -3989,7 +3992,7 @@ namespace Mono.CSharp {
 					Error_LValueRequired (loc);
 			} else if (ArgType == AType.Out) {
 				ec.InRefOutArgumentResolving = true;
-				Expr = Expr.DoResolveLValue (ec, EmptyExpression.Null);
+				Expr = Expr.DoResolveLValue (ec, EmptyExpression.OutAccess);
 				ec.InRefOutArgumentResolving = false;
 
 				if (Expr == null)
@@ -8211,6 +8214,9 @@ namespace Mono.CSharp {
 	/// </summary>
 	public class EmptyExpression : Expression {
 		public static readonly EmptyExpression Null = new EmptyExpression ();
+
+		public static readonly EmptyExpression OutAccess = new EmptyExpression ();
+		public static readonly EmptyExpression LValueMemberAccess = new EmptyExpression ();
 
 		static EmptyExpression temp = new EmptyExpression ();
 		public static EmptyExpression Grab ()
