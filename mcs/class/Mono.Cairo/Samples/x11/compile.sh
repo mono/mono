bd40397ddb5d71120d476e7ef613e70f98e0f502
Alp Toker <alp@mono-cvs.ximian.com>
2006-01-17 01:58:24 +0000

    2006-01-17  Alp Toker  <alp@atoker.com>
      * Samples/x11/compile.sh: Remove unused gtk-sharp reference
    
    svn path=/trunk/mcs/; revision=55663

--- mcs/class/Mono.Cairo/Samples/x11/compile.sh
+++ mcs/class/Mono.Cairo/Samples/x11/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp arc.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp arcneg.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp clip.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp clip_img.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp curve_rect.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp curve_to.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp fillstroke.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp gradient.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp image.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp image_pattern.cs x11.cs
-mcs -r:System.Drawing -r:Mono.Cairo -pkg:gtk-sharp text.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo arc.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo arcneg.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo clip.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo clip_img.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo curve_rect.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo curve_to.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo fillstroke.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo gradient.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo image.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo image_pattern.cs x11.cs
+mcs -r:System.Drawing -r:Mono.Cairo text.cs x11.cs
