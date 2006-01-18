522d101247c1ac20773ecb441401f97b6cb428a6
Martin Baulig <martin@novell.com>
2006-01-18 17:07:03 +0000

    2006-01-18  Martin Baulig  <martin@ximian.com>
    	* class.cs (TypeContainer.AddToMemberContainer): Use
    	`symbol.MemberName.MethodName' instead of just `symbol.Name';
    	fixes #77124.
    
    
    svn path=/trunk/mcs/; revision=55737

---
+++ mcs/tests/gtest-240.cs
@@ -0,0 +1,28 @@
+using System;
+
+interface IMyInterface<T>
+{
+	event EventHandler MyEvent;
+}
+
+public class MyClass: IMyInterface<string>, IMyInterface<int>
+{
+	event EventHandler IMyInterface<string>.MyEvent
+	{
+	add {}
+	remove {}
+	}
+
+	event EventHandler IMyInterface<int>.MyEvent
+	{
+	add {}
+	remove {}
+	}
+	
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
