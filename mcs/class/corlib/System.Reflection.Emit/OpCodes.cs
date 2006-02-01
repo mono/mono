1e886d33ae2b3180f5b6122fe92d494d03248a30
Zoltan Varga <vargaz@gmail.com>
2006-02-01 07:06:05 +0000

    2006-02-01  Zoltan Varga  <vargaz@gmail.com>
    	* OpCodes.cs: Readonly is a net 2.0 only field.
    
    svn path=/trunk/mcs/; revision=56374

--- mcs/class/corlib/System.Reflection.Emit/OpCodes.cs
+++ mcs/class/corlib/System.Reflection.Emit/OpCodes.cs
@@ -925,9 +925,11 @@ namespace System.Reflection.Emit {
 			0xFE << 0 | 0x1D << 8 | (byte) StackBehaviour.Pushi << 16 | (byte) StackBehaviour.Pop1 << 24,
 			2 << 0 | (byte) OpCodeType.Primitive << 8 | (byte) OperandType.InlineNone << 16 | (byte) FlowControl.Next << 24);
 
+#if NET_2_0 || BOOTSTRAP_NET_2_0
 		public static readonly OpCode Readonly = new OpCode (
 			0xFE << 0 | 0x1E << 8 | (byte) StackBehaviour.Push0 << 16 | (byte) StackBehaviour.Pop0 << 24,
 			2 << 0 | (byte) OpCodeType.Primitive << 8 | (byte) OperandType.InlineNone << 16 | (byte) FlowControl.Next << 24);
+#endif
 
                 public static bool TakesSingleByteArgument (OpCode inst)
                 {
