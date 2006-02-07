739ac835fa2ff1f38a1036bdc835a45d765a2a1a
Chris Toshok <toshok@novell.com>
2006-02-07 16:56:30 +0000

    fix typo
    svn path=/trunk/mcs/; revision=56624

--- mcs/class/System.Web/System.Web.UI/UserControl.cs
+++ mcs/class/System.Web/System.Web.UI/UserControl.cs
@@ -205,7 +205,8 @@ namespace System.Web.UI {
 				return;
 			initialized = true;
 			this.Page = page;
-			InitializeAsUserControlInternal ();
+			WireupAutomaticEvents ();
+			FrameworkInitialize ();
 		}
 
 		[MonoTODO]
