70b4a2710bfd70b94efe5c6052c893b6f6fc4695
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2006-02-10 08:03:16 +0000

    2006-02-10  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* SerialError.cs:
    	* SerialData.cs:
    	* SerialPinChange.cs: New enums.
    	
    
    svn path=/trunk/mcs/; revision=56745

---
+++ mcs/class/System/System.IO.Ports/SerialData.cs
@@ -0,0 +1,22 @@
+//
+// System.IO.Ports.SerialData.cs
+//
+// Authors:
+//	Carlos Alberto Cortez (calberto.cortez@gmail.com)
+//
+// (c) Copyright 2006 Novell, Inc. (http://www.novell.com)
+//
+
+#if NET_2_0
+
+namespace System.IO.Ports 
+{
+	public enum SerialData 
+	{
+		Chars = 1,
+		Eof
+	} 
+}
+
+#endif
+
