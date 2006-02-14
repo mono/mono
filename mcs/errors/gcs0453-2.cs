0fa5071b3fc3d7d297d7f991dfdbe45de39132ea
Martin Baulig <martin@novell.com>
2006-02-14 17:33:56 +0000

    Small fix.
    svn path=/trunk/mcs/; revision=56862

--- mcs/errors/gcs0453-2.cs
+++ mcs/errors/gcs0453-2.cs
@@ -1,4 +1,4 @@
-// CS0453: The type `Foo' must be a non-nullable value type in order to use it as parameter `T' in the generic type or method `System.Nullable<T>'
+// CS0453: The type `Foo' must be a non-nullable value type in order to use it as type parameter `T' in the generic type or method `System.Nullable<T>'
 // Line: 10
 public class Foo
 { }
