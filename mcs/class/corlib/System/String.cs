0fafec9282cf6d13222a7e5db8979a44a14683e0
Marek Safar <marek.safar@gmail.com>
2006-02-21 21:59:39 +0000

    2006-02-21  Marek Safar  <marek.safar@seznam.cz>
    	* String.cs (Equals): Optimized for speed.
    
    svn path=/trunk/mcs/; revision=57112

--- mcs/class/corlib/System/String.cs
+++ mcs/class/corlib/System/String.cs
@@ -72,28 +72,42 @@ namespace System
 			if (len != b.length)
 				return false;
 
-			if (len == 0)
-				return true;
+			fixed (char* s1 = &a.start_char, s2 = &b.start_char) {
+				char* s1_ptr = s1;
+				char* s2_ptr = s2;
 
-			fixed (char * s1 = &a.start_char, s2 = &b.start_char) {
-				// it must be one char, because 0 len is done above
-				if (len < 2)
-					return *s1 == *s2;
-
-				// check by twos
-				int * sint1 = (int *) s1, sint2 = (int *) s2;
-				int n2 = len >> 1;
-				do {
-					if (*sint1++ != *sint2++)
+				while (len >= 8) {
+					if (((int*)s1_ptr)[0] != ((int*)s2_ptr)[0] ||
+						((int*)s1_ptr)[1] != ((int*)s2_ptr)[1] ||
+						((int*)s1_ptr)[2] != ((int*)s2_ptr)[2] ||
+						((int*)s1_ptr)[3] != ((int*)s2_ptr)[3])
 						return false;
-				} while (--n2 != 0);
 
-				// nothing left
-				if ((len & 1) == 0)
-					return true;
+					s1_ptr += 8;
+					s2_ptr += 8;
+					len -= 8;
+				}
 
-				// check the last one
-				return *(char *) sint1 == *(char *) sint2;
+				if (len >= 4) {
+					if (((int*)s1_ptr)[0] != ((int*)s2_ptr)[0] ||
+						((int*)s1_ptr)[1] != ((int*)s2_ptr)[1])
+						return false;
+
+					s1_ptr += 4;
+					s2_ptr += 4;
+					len -= 4;
+				}
+
+				if (len > 1) {
+					if (((int*)s1_ptr)[0] != ((int*)s2_ptr)[0])
+						return false;
+
+					s1_ptr += 2;
+					s2_ptr += 2;
+					len -= 2;
+				}
+
+				return len == 0 || *s1_ptr == *s2_ptr;
 			}
 		}
 
