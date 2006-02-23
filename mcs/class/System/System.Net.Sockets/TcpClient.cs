6e1f6757b7159d24f5b1e1ff52564c0f3a3abd51
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2006-02-23 21:30:14 +0000

    2006-02-23 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* TcpClient.cs: when the socket is not connected, don't try to set the
    	socket options, as they are ignored on linux. Wait until Connect
    	succeeds and then apply the options.
    
    
    svn path=/trunk/mcs/; revision=57216

--- mcs/class/System/System.Net.Sockets/TcpClient.cs
+++ mcs/class/System/System.Net.Sockets/TcpClient.cs
@@ -1,10 +1,12 @@
 // System.Net.Sockets.TcpClient.cs
 //
 // Author:
-//    Phillip Pearson (pp@myelin.co.nz)
+// 	Phillip Pearson (pp@myelin.co.nz)
+//	Gonzalo Paniagua Javier (gonzalo@novell.com)
 //
 // Copyright (C) 2001, Phillip Pearson
 //    http://www.myelin.co.nz
+// Copyright (c) 2006 Novell, Inc. (http://www.novell.com)
 //
 
 //
@@ -28,31 +30,32 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-// NB: This is untested (probably buggy) code - take care if using it
-
 using System;
 using System.Net;
 
 namespace System.Net.Sockets
 {
-	/// <remarks>
-	/// A slightly more abstracted way to create an
-	/// outgoing network connections than a Socket.
-	/// </remarks>
-	public class TcpClient : IDisposable
-	{
-		// private data
-		
-		private NetworkStream stream;
-		private bool active;
-		private Socket client;
-		private bool disposed = false;
-		
-		// constructor
+	public class TcpClient : IDisposable {
+		enum Properties : uint {
+			LingerState = 1,
+			NoDelay = 2,
+			ReceiveBufferSize = 4,
+			ReceiveTimeout = 8,
+			SendBufferSize = 16,
+			SendTimeout = 32
+		}
 
-		/// <summary>
-		/// Some code that is shared between the constructors.
-		/// </summary>
+		// private data
+		NetworkStream stream;
+		bool active;
+		Socket client;
+		bool disposed;
+		Properties values;
+		int recv_timeout, send_timeout;
+		int recv_buffer_size, send_buffer_size;
+		LingerOption linger_state;
+		bool no_delay;
+		
 		private void Init (AddressFamily family)
 		{
 			active = false;
@@ -65,9 +68,6 @@ namespace System.Net.Sockets
 			client = new Socket(family, SocketType.Stream, ProtocolType.Tcp);
 		}
 
-		/// <summary>
-		/// Constructs a new TcpClient with no connection set up
-		/// </summary>
 		public TcpClient ()
 		{
 			Init(AddressFamily.InterNetwork);
@@ -87,44 +87,22 @@ namespace System.Net.Sockets
 		}
 #endif
 		
-		/// <summary>
-		/// Constructs a new TcpClient with a specified local endpoint.
-		/// Use this if you want to have your connections originating
-		/// from a certain port, or a certain IP (on a multi homed
-		/// system).
-		/// </summary>
-		/// <param name="local_end_point">The aforementioned local endpoint</param>
 		public TcpClient (IPEndPoint local_end_point)
 		{
 			Init(local_end_point.AddressFamily);
 			client.Bind(local_end_point);
 		}
 		
-		/// <summary>
-		/// Constructs a new TcpClient and connects to a specified
-		/// host on a specified port.  A quick way to set up a network
-		/// connection.
-		/// </summary>
-		/// <param name="hostname">The host to connect to, e.g.
-		/// 192.168.0.201 or www.myelin.co.nz</param>
-		/// <param name="port">The port to connect to, e.g. 80 for HTTP</param>
 		public TcpClient (string hostname, int port)
 		{
 			Connect(hostname, port);
 		}
 				
-		/// <summary>
-		/// A flag that is 'true' if the TcpClient has an active connection
-		/// </summary>
-		protected bool Active
-		{
+		protected bool Active {
 			get { return active; }
 			set { active = value; }
 		}
 		
-		/// <summary>
-		/// The socket that all network comms passes through
-		/// </summary>
 #if NET_2_0
 		public Socket Client {
 #else
@@ -137,121 +115,150 @@ namespace System.Net.Sockets
 			}
 		}
 
-		/// <summary>
-		/// Internal function to allow TcpListener.AcceptTcpClient
-		/// to work (it needs to be able to set protected property
-		/// 'Client')
-		/// </summary>
-		/// <param name="s"></param>
+#if NET_2_0
+		public int Available {
+			get { return client.Available; }
+		}
+
+		public bool Connected {
+			get { return client.Connected; }
+		}
+
+		public bool ExclusiveAddressUse {
+			get {
+				return !((bool) client.GetSocketOption (SocketOptionLevel.Socket,
+								SocketOptionName.ReuseAddress));
+			}
+			set {
+				client.SetSocketOption (SocketOptionLevel.Socket,
+							SocketOptionName.ReuseAddress, !value);
+			}
+		}
+#endif
 		internal void SetTcpClient (Socket s) 
 		{
 			Client = s;
 		}
-		
-		/// <summary>
-		/// If set, the socket will remain open after it has been
-		/// instructed to close, in order to send data that remains
-		/// in the buffer.
-		/// </summary>
-		public LingerOption LingerState
-		{
+
+		public LingerOption LingerState {
 			get {
-				return (LingerOption)client.GetSocketOption(
-					SocketOptionLevel.Socket,
-					SocketOptionName.Linger);
+				if ((values & Properties.LingerState) != 0)
+					return linger_state;
+
+				return (LingerOption) client.GetSocketOption (SocketOptionLevel.Socket,
+									SocketOptionName.Linger);
 			}
 			set {
+				if (!client.Connected) {
+					linger_state = value;
+					values |= Properties.LingerState;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.Linger, value);
 			}
 		}
 				
-		/// <summary>
-		/// <p>If set, outbound data will be sent at once rather than collected
-		/// until enough is available to fill a packet.</p>
-		/// 
-		/// <p>This is the TCP_NODELAY sockopt from BSD sockets and WinSock.
-		/// For more information, look up the Nagle algorithm.</p>
-		/// </summary>
-		public bool NoDelay
-		{
+		public bool NoDelay {
 			get {
+				if ((values & Properties.NoDelay) != 0)
+					return no_delay;
+
 				return (bool)client.GetSocketOption(
 					SocketOptionLevel.Tcp,
 					SocketOptionName.NoDelay);
 			}
 			set {
+				if (!client.Connected) {
+					no_delay = value;
+					values |= Properties.NoDelay;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Tcp,
 					SocketOptionName.NoDelay, value ? 1 : 0);
 			}
 		}
 				
-		/// <summary>
-		/// How big the receive buffer is (from the connection socket)
-		/// </summary>
-		public int ReceiveBufferSize
-		{
+		public int ReceiveBufferSize {
 			get {
+				if ((values & Properties.ReceiveBufferSize) != 0)
+					return recv_buffer_size;
+
 				return (int)client.GetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.ReceiveBuffer);
 			}
 			set {
+				if (!client.Connected) {
+					recv_buffer_size = value;
+					values |= Properties.ReceiveBufferSize;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.ReceiveBuffer, value);
 			}
 		}
 			
-		/// <summary>
-		/// How long before the socket will time out on a 
-		/// Receive() call
-		/// </summary>
-		public int ReceiveTimeout
-		{
+		public int ReceiveTimeout {
 			get {
+				if ((values & Properties.ReceiveTimeout) != 0)
+					return recv_timeout;
+
 				return (int)client.GetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.ReceiveTimeout);
 			}
 			set {
+				if (!client.Connected) {
+					recv_timeout = value;
+					values |= Properties.ReceiveTimeout;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.ReceiveTimeout, value);
 			}
 		}
 		
-		/// <summary>
-		/// How big the send buffer is (from the connection socket)
-		/// </summary>
-		public int SendBufferSize
-		{
+		public int SendBufferSize {
 			get {
+				if ((values & Properties.SendBufferSize) != 0)
+					return send_buffer_size;
+
 				return (int)client.GetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.SendBuffer);
 			}
 			set {
+				if (!client.Connected) {
+					send_buffer_size = value;
+					values |= Properties.SendBufferSize;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.SendBuffer, value);
 			}
 		}
 		
-		/// <summary>
-		/// How long before the socket will time out on a
-		/// Send() call
-		/// </summary>
-		public int SendTimeout
-		{
+		public int SendTimeout {
 			get {
+				if ((values & Properties.SendTimeout) != 0)
+					return send_timeout;
+
 				return (int)client.GetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.SendTimeout);
 			}
 			set {
+				if (!client.Connected) {
+					send_timeout = value;
+					values |= Properties.SendTimeout;
+					return;
+				}
 				client.SetSocketOption(
 					SocketOptionLevel.Socket,
 					SocketOptionName.SendTimeout, value);
@@ -261,24 +268,11 @@ namespace System.Net.Sockets
 		
 		// methods
 		
-		/// <summary>
-		/// Closes the socket and disposes of all managed resources.
-		/// 
-		/// Throws SocketException if something goes wrong while
-		/// closing the socket.
-		/// </summary>
 		public void Close ()
 		{
 			((IDisposable) this).Dispose ();
 		}
 		
-		/// <summary>
-		/// Connects to a specified remote endpoint
-		/// 
-		/// Throws SocketException if something goes wrong while
-		/// connecting.
-		/// </summary>
-		/// <param name="remote_end_point">The aforementioned endpoint</param>
 		public void Connect (IPEndPoint remote_end_point)
 		{
 			try {
@@ -290,37 +284,37 @@ namespace System.Net.Sockets
 			}
 		}
 		
-		/// <summary>
-		/// Connects to an IP address on a port
-		/// 
-		/// Throws SocketException if something goes wrong while
-		/// connecting.
-		/// </summary>
-		/// <param name="address">The IP address (get it from Dns.GetHostByName)</param>
-		/// <param name="port">The port to connect to, e.g. 80 for HTTP</param>
 		public void Connect (IPAddress address, int port)
 		{
 			Connect(new IPEndPoint(address, port));
 		}
-		
-		/// <summary>
-		/// Resolves a fully qualified domain name to an IP address
-		/// and connects to it on a specified port
-		/// 
-		/// Throws SocketException if something goes wrong while
-		/// connecting.
-		/// </summary>
-		/// <param name="hostname">The hostname, e.g. www.myelin.co.nz</param>
-		/// <param name="port">The port, e.g. 80 for HTTP</param>
-		[MonoTODO]
+
+		void SetOptions ()
+		{
+			Properties props = values;
+			values = 0;
+
+			if ((props & Properties.LingerState) != 0)
+				LingerState = linger_state;
+			if ((props & Properties.NoDelay) != 0)
+				NoDelay = no_delay;
+			if ((props & Properties.ReceiveBufferSize) != 0)
+				ReceiveBufferSize = recv_buffer_size;
+			if ((props & Properties.ReceiveTimeout) != 0)
+				ReceiveTimeout = recv_timeout;
+			if ((props & Properties.SendBufferSize) != 0)
+				SendBufferSize = send_buffer_size;
+			if ((props & Properties.SendTimeout) != 0)
+				SendTimeout = send_timeout;
+		}
+
 		public void Connect (string hostname, int port)
 		{
 			CheckDisposed ();
 
 			IPHostEntry host = Dns.GetHostByName(hostname);
 
-			for(int i=0; i<host.AddressList.Length; i++)
-			{
+			for(int i=0; i<host.AddressList.Length; i++) {
 				try {
 					Init(host.AddressList[i].AddressFamily);
 
@@ -332,9 +326,10 @@ namespace System.Net.Sockets
 #endif
 
 					Connect(new IPEndPoint(host.AddressList[i], port));
+					if (values != 0)
+						SetOptions ();
 					break;
-				}
-				catch(Exception e) {
+				} catch (Exception e) {
 					if(client != null) {
 						client.Close();
 						client = null;
@@ -347,20 +342,12 @@ namespace System.Net.Sockets
 			}
 		}
 		
-		/// <summary>
-		/// Gets rid of all managed resources
-		/// </summary>
 		void IDisposable.Dispose ()
 		{
 			Dispose (true);
 			GC.SuppressFinalize (this);
 		}
 
-		/// <summary>
-		/// Gets rid of all unmanaged resources
-		/// </summary>
-		/// <param name="disposing">If this is true, it gets rid of all
-		/// managed resources as well</param>
 		protected virtual void Dispose (bool disposing)
 		{
 			if (disposed)
@@ -384,23 +371,16 @@ namespace System.Net.Sockets
 			}
 		}
 		
-		/// <summary>
-		/// Destructor - just calls Dispose()
-		/// </summary>
 		~TcpClient ()
 		{
 			Dispose (false);
 		}
 		
-		/// <returns>A NetworkStream object connected to the
-		/// connection socket</returns>
 		public NetworkStream GetStream()
 		{
 			try {
 				if (stream == null)
-				{
 					stream = new NetworkStream (client, true);
-				}
 				return stream;
 			}
 			finally { CheckDisposed (); }
@@ -413,3 +393,4 @@ namespace System.Net.Sockets
 		}		
 	}
 }
+
