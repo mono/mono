3e764dd8f8a3414b83eda8208e1697c7b83da44e
Marek Safar <marek.safar@gmail.com>
2006-03-04 20:03:08 +0000

    2006-03-04  Marek Safar <marek.safar@seznam.cz>
    	* test-494.cs: New test for #77353.
    
    svn path=/trunk/mcs/; revision=57586

---
+++ mcs/tests/test-494.cs
@@ -0,0 +1,11 @@
+using System.Text;
+
+namespace Agresso.Foundation {
+	public class Function
+	{
+		static void Main () {}
+	}
+
+	public delegate void Translate(Function callback, 
+		ref StringBuilder result);
+	}
