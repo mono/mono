7710888a868f622254a4f37e95bf00b13cb6a8db
Martin Baulig <martin@novell.com>
2006-03-07 05:29:15 +0000

    New tests.
    svn path=/trunk/mcs/; revision=57646

---
+++ mcs/tests/gtest-252.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class EqualityComparer<T>
+{
+	readonly static Type sequencedequalityComparer = typeof (SequencedEqualityComparer<,>);
+
+	public static void Test ()
+	{ }
+}
+
+public class SequencedEqualityComparer<T,W>
+{ }
+
+class X
+{
+	static void Main ()
+	{
+		EqualityComparer<int>.Test ();
+	}
+}
