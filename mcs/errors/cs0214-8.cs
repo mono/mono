e1827ff1ba527728e5682fcbe93d70793dd1e8c3
Marek Safar <marek.safar@gmail.com>
2006-03-09 23:11:09 +0000

    new test case
    svn path=/trunk/mcs/; revision=57751

---
+++ mcs/errors/cs0214-8.cs
@@ -0,0 +1,19 @@
+// cs0214-8.cs: Pointers and fixed size buffers may only be used in an unsafe context
+// Line: 17
+// Compiler options: -unsafe
+
+public unsafe delegate void Bar (int* x);
+
+class X
+{
+	public X (Bar bar)
+	{ }
+
+	unsafe static void Test (int* b)
+	{ }
+
+	static void Main ()
+	{
+		X x = new X (Test);
+	}
+}
