959812631d0d6286b1b1083821cba14e44a5cbdb
Miguel de Icaza <miguel@gnome.org>
2006-03-12 20:58:06 +0000

    2006-03-12  Robert Jordan <robertj@gmx.net>
    	* UnixRegistryApi.cs (DeleteValue): Avoid crash, fixes bug #77772
    
    svn path=/branches/mono-1-1-13/mcs/; revision=57880

--- mcs/class/corlib/Microsoft.Win32/UnixRegistryApi.cs
+++ mcs/class/corlib/Microsoft.Win32/UnixRegistryApi.cs
@@ -366,18 +366,14 @@ namespace Microsoft.Win32 {
 			return self.values.Keys.Count;
 		}
 		
-		public void DeleteValue (RegistryKey rkey, string value, bool throw_if_missing)
+		public void DeleteValue (RegistryKey rkey, string name, bool throw_if_missing)
 		{
 			KeyHandler self = KeyHandler.Lookup (rkey);
 
-			foreach (DictionaryEntry de in self.values){
-				if ((string)de.Value == value){
-					self.values.Remove (de.Key);
-					return;
-				}
-			}
-			if (throw_if_missing)
-				throw new ArgumentException ("the given value does not exist", "value");
+			if (throw_if_missing && !self.values.Contains (name))
+				throw new ArgumentException ("the given value does not exist", "name");
+
+			self.values.Remove (name);
 		}
 		
 		public void DeleteKey (RegistryKey rkey, string keyname, bool throw_if_missing)
