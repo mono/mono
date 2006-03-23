e1839917904d237a0e4ec2414906b48fb70e701c
Martin Baulig <martin@novell.com>
2006-03-23 16:06:40 +0000

    2006-03-23  Martin Baulig  <martin@ximian.com>
    	* convert.cs (Convert.ImplicitTypeParameterConversion): Do not
    	blindly allow all conversions if we do not have any constraints.
    
    
    svn path=/trunk/mcs/; revision=58363

---
+++ mcs/errors/gcs0030.cs
@@ -0,0 +1,16 @@
+// CS0030: Cannot convert type `T' to `X'.
+// Line: 8
+class Foo<T>
+	where T : System.ICloneable
+{
+	public X Test (T t)
+	{
+		return (X) t;
+	}
+}
+
+class X
+{
+	static void Main ()
+	{ }
+}
