7e0d2fa737442ce988b5e1efab4873aad8895b92
Mike Kestner <mkestner@gmail.com>
2006-04-12 23:16:47 +0000

    2006-04-12  Mike Kestner  <mkestner@novell.com>
    	* MenuAPI.cs: ignore clicks on disabled menu items. Thanks to Alex for
    	reporting.
    
    svn path=/trunk/mcs/; revision=59430

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/MenuAPI.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/MenuAPI.cs
@@ -119,6 +119,9 @@ namespace System.Windows.Forms {
 		{
 			MenuItem item = GetItemAtXY (args.X, args.Y);
 
+			if (!item.Enabled)
+				return;
+
 			if (item == null || (active && item.IsPopup && (item.Parent is MainMenu))) {
 				Deactivate ();
 				return;
