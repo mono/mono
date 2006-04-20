6dcaf35e8d35b7cd3ebea5037af1a954d31b85d5
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2006-04-20 22:51:04 +0000

    2006-04-20 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* WebProxy.cs: a .ctor was not forwarding the 'credentials' argument.
    	This fixes bug #78150. Also fix the bypass check for local requests.
    
    
    svn path=/branches/mono-1-1-7/mcs/; revision=59717

--- mcs/class/System/System.Net/WebProxy.cs
+++ mcs/class/System/System.Net/WebProxy.cs
@@ -70,7 +70,7 @@ namespace System.Net
 		
 		public WebProxy (string address, bool bypassOnLocal,
 				 string[] bypassList, ICredentials credentials)
-			: this (ToUri (address), bypassOnLocal, bypassList, null) {}
+			: this (ToUri (address), bypassOnLocal, bypassList, credentials) {}
 
 		public WebProxy (Uri address, bool bypassOnLocal, 
 				 string[] bypassList, ICredentials credentials)
@@ -152,7 +152,7 @@ namespace System.Net
 			if (address == null)
 				return true;
 			
-			if (host.IsLoopback)
+			if (bypassOnLocal && host.IsLoopback)
 				return true;
 				
 			if (bypassOnLocal && host.Host.IndexOf ('.') == -1)
