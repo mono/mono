eaaa129b389f168399a7c708f2dc568c408ec360
Chris Toshok <toshok@novell.com>
2006-05-08 19:22:41 +0000

    add 76818's testcase
    svn path=/trunk/mcs/; revision=60422

---
+++ mcs/class/System.Web/Test/standalone/masterpages/76818/Default.aspx.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
