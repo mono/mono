5973334267c96b181e1ddec71b70e9c7ca1d2d23
Chris Toshok <toshok@novell.com>
2006-05-11 20:10:00 +0000

    we still need a guard here, for invalid_list.Count == 0
    svn path=/trunk/mcs/; revision=60605

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/Hwnd.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/Hwnd.cs
@@ -399,6 +399,9 @@ namespace System.Windows.Forms {
 
 		public Rectangle Invalid {
 			get {
+				if (invalid_list.Count == 0)
+					return Rectangle.Empty;
+
 				Rectangle result = (Rectangle)invalid_list[0];
 				for (int i = 1; i < invalid_list.Count; i ++) {
 					result = Rectangle.Union (result, (Rectangle)invalid_list[i]);
