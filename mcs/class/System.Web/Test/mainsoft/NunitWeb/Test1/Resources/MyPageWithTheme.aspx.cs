5b21f1c1dc06b985ce5ac1853708f80ed145f6a8
Andrew Skiba <andrews@mono-cvs.ximian.com>
2006-05-24 14:12:02 +0000

    test themes
    svn path=/trunk/mcs/; revision=61060

---
+++ mcs/class/System.Web/Test/mainsoft/NunitWeb/Test1/Resources/MyPageWithTheme.aspx.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web.UI;
+
+public partial class MyPageWithTheme : System.Web.UI.Page
+{
+	protected void Page_Load (object sender, EventArgs e)
+	{
+		NunitWeb.MyHost.RunDelegate (Context, this);
+	}
+	public override void VerifyRenderingInServerForm (Control c)
+	{
+
+	}
+}
