31649a8bd9da851a6f059edfef878bfde78362c5
Andrew Skiba <andrews@mono-cvs.ximian.com>
2006-05-28 08:54:18 +0000

    add PageDelegate structure for passing delegates between appdomains
    svn path=/trunk/mcs/; revision=61200

---
+++ mcs/class/System.Web/Test/mainsoft/NunitWeb/NunitWeb/PageDelegates.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitWeb
+{
+	[Serializable]
+	public struct PageDelegates
+	{
+		public Helper.AnyMethod MyHandlerCallback;
+		public Helper.AnyMethodInPage LoadComplete;
+		public Helper.AnyMethodInPage PreInit;
+		public Helper.AnyMethodInPage PreLoad;
+		public Helper.AnyMethodInPage PreRenderComplete;
+		public Helper.AnyMethodInPage InitComplete;
+		public Helper.AnyMethodInPage SaveStateComplete;
+		public Helper.AnyMethodInPage CommitTransaction;
+		public Helper.AnyMethodInPage AbortTransaction;
+		public Helper.AnyMethodInPage Error;
+		public Helper.AnyMethodInPage Disposed;
+		public Helper.AnyMethodInPage DataBinding;
+		public Helper.AnyMethodInPage Init;
+		public Helper.AnyMethodInPage Load;
+		public Helper.AnyMethodInPage PreRender;
+		public Helper.AnyMethodInPage Unload;
+		public object Param;
+	}
+}
