2cb792105c091b47d178e9343e25a1cfce3b8497
Marek Safar <marek.safar@gmail.com>
2006-08-13 13:50:20 +0000

    2006-08-13  Marek Safar  <marek.safar@seznam.cz>
    	A fix for #79056
    	* cs-parser.jay: Don't destroy current array type by typeof of array's.
    
    svn path=/trunk/mcs/; revision=63681

---
+++ mcs/tests/test-528.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MicroFocus.MONO.Bugs
+{
+	public class Odd
+	{
+		private static readonly Type[] _argTypes = { typeof(object[]) };
+
+		public static void Main()
+		{
+		}
+	}
+}
