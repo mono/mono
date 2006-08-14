8cf2d70efea3434beeb0e8d56ea96163a9d0e1e7
Sebastien Pouliot <sebastien@ximian.com>
2006-08-14 18:32:24 +0000

    2006-08-14  Sebastien Pouliot  <sebastien@ximian.com>
    	* GDIPlusTest.cs: Re-add LOGONT definition as a class (instead of a
    	struct like it's currently defined inside SD).
    
    
    svn path=/trunk/mcs/; revision=63733

--- mcs/class/System.Drawing/Test/System.Drawing/GDIPlusTest.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/GDIPlusTest.cs
@@ -38,6 +38,26 @@ namespace MonoTests.System.Drawing {
 	[TestFixture]
 	public class GDIPlusTest {
 
+		// for the moment this LOGFONT is different (and ok) from the one defined internally in SD
+		[StructLayout (LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		public class LOGFONT {
+			public int lfHeight = 0;
+			public int lfWidth = 0;
+			public int lfEscapement = 0;
+			public int lfOrientation = 0;
+			public int lfWeight = 0;
+			public byte lfItalic = 0;
+			public byte lfUnderline = 0;
+			public byte lfStrikeOut = 0;
+			public byte lfCharSet = 0;
+			public byte lfOutPrecision = 0;
+			public byte lfClipPrecision = 0;
+			public byte lfQuality = 0;
+			public byte lfPitchAndFamily = 0;
+			[MarshalAs (UnmanagedType.ByValTStr, SizeConst = 32)]
+			public string lfFaceName = null;
+		}
+
 		// FontFamily
 		[Test]
 		public void DeleteFontFamily ()
