2172720260b98d002f6e49d2a7200578949f464c
Miguel de Icaza <miguel@gnome.org>
2006-08-19 05:47:30 +0000

    remove unused comments
    svn path=/trunk/mcs/; revision=64039

--- mcs/class/corlib/System.Threading/ThreadState.cs
+++ mcs/class/corlib/System.Threading/ThreadState.cs
@@ -38,46 +38,20 @@ using System.Runtime.Serialization;
 
 namespace System.Threading {
 
-
-	/// <summary>
-	/// </summary>
 	[Flags]
 #if NET_2_0
 	[Serializable]
 #endif
 	public enum ThreadState {
-
-		/// <summary>
-		/// </summary>
 		Running = 0x00000000,
-
 		StopRequested = 0x00000001,
 		SuspendRequested = 0x00000002,
-		
-		/// <summary>
-		/// </summary>
 		Background = 0x00000004,
-
-		/// <summary>
-		/// </summary>
 		Unstarted = 0x00000008,
-
-		/// <summary>
-		/// </summary>
 		Stopped = 0x00000010,
-
-		/// <summary>
-		/// </summary>
 		WaitSleepJoin = 0x00000020,
-
 		Suspended = 0x00000040,
-
-		/// <summary>
-		/// </summary>
 		AbortRequested = 0x00000080,
-
-		/// <summary>
-		/// </summary>
 		Aborted = 0x00000100,
    } // ThreadState
 
