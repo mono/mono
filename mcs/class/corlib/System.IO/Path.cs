f36b6a460c36297e26e8756354b0c87b2780a458
Sebastien Pouliot <sebastien@ximian.com>
2006-08-21 14:28:27 +0000

    2006-08-21  Sebastien Pouliot  <sebastien@ximian.com>
    	* Path.cs: Added internal method IsPathSubsetOf required to implement
    	correctly FileIOPermission (better located here).
    
    
    svn path=/trunk/mcs/; revision=64144

--- mcs/class/corlib/System.IO/Path.cs
+++ mcs/class/corlib/System.IO/Path.cs
@@ -586,5 +586,29 @@ namespace System.IO {
 				return ret;
 			}
 		}
+
+		// required for FileIOPermission (and most proibably reusable elsewhere too)
+		// both path MUST be "full paths"
+		static internal bool IsPathSubsetOf (string subset, string path)
+		{
+			if (subset.Length > path.Length)
+				return false;
+
+			// check that everything up to the last separator match
+			int slast = subset.LastIndexOfAny (PathSeparatorChars);
+			if (String.Compare (subset, 0, path, 0, slast) != 0)
+				return false;
+
+			slast++;
+			// then check if the last segment is identical
+			int plast = path.IndexOfAny (PathSeparatorChars, slast);
+			if (plast >= slast) {
+				return String.Compare (subset, slast, path, slast, path.Length - plast) == 0;
+			}
+			if (subset.Length != path.Length)
+				return false;
+
+			return String.Compare (subset, slast, path, slast, subset.Length - slast) == 0;
+		}
 	}
 }
