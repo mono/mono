d46d59be242b2881107e8fb0bbdfbdba7b77c6c8
Sebastien Pouliot <sebastien@ximian.com>
2006-08-25 13:01:01 +0000

    2006-08-25  Sebastien Pouliot  <sebastien@ximian.com>
    	* TestBmpCodec.cs: Add pixel value tests to a well known bitmap.
    
    
    svn path=/trunk/mcs/; revision=64352

--- mcs/class/System.Drawing/Test/System.Drawing.Imaging/TestBmpCodec.cs
+++ mcs/class/System.Drawing/Test/System.Drawing.Imaging/TestBmpCodec.cs
@@ -104,7 +104,48 @@ namespace MonoTests.System.Drawing.Imaging
 			Assert.AreEqual (183, rect.Height);					
 			
 			Assert.AreEqual (173, bmp.Size.Width);
-			Assert.AreEqual (183, bmp.Size.Height);					
+			Assert.AreEqual (183, bmp.Size.Height);	
+
+			// sampling values from a well known bitmap
+			Assert.AreEqual (-1645353, bmp.GetPixel (0, 32).ToArgb (), "0,32");
+			Assert.AreEqual (-461332, bmp.GetPixel (0, 64).ToArgb (), "0,64");
+			Assert.AreEqual (-330005, bmp.GetPixel (0, 96).ToArgb (), "0,96");
+			Assert.AreEqual (-2237489, bmp.GetPixel (0, 128).ToArgb (), "0,128");
+			Assert.AreEqual (-1251105, bmp.GetPixel (0, 160).ToArgb (), "0,160");
+			Assert.AreEqual (-3024947, bmp.GetPixel (32, 0).ToArgb (), "32,0");
+			Assert.AreEqual (-2699070, bmp.GetPixel (32, 32).ToArgb (), "32,32");
+			Assert.AreEqual (-2366734, bmp.GetPixel (32, 64).ToArgb (), "32,64");
+			Assert.AreEqual (-4538413, bmp.GetPixel (32, 96).ToArgb (), "32,96");
+			Assert.AreEqual (-6116681, bmp.GetPixel (32, 128).ToArgb (), "32,128");
+			Assert.AreEqual (-7369076, bmp.GetPixel (32, 160).ToArgb (), "32,160");
+			Assert.AreEqual (-13024729, bmp.GetPixel (64, 0).ToArgb (), "64,0");
+			Assert.AreEqual (-7174020, bmp.GetPixel (64, 32).ToArgb (), "64,32");
+			Assert.AreEqual (-51, bmp.GetPixel (64, 64).ToArgb (), "64,64");
+			Assert.AreEqual (-16053503, bmp.GetPixel (64, 96).ToArgb (), "64,96");
+			Assert.AreEqual (-8224431, bmp.GetPixel (64, 128).ToArgb (), "64,128");
+			Assert.AreEqual (-16579326, bmp.GetPixel (64, 160).ToArgb (), "64,160");
+			Assert.AreEqual (-2502457, bmp.GetPixel (96, 0).ToArgb (), "96,0");
+			Assert.AreEqual (-9078395, bmp.GetPixel (96, 32).ToArgb (), "96,32");
+			Assert.AreEqual (-12696508, bmp.GetPixel (96, 64).ToArgb (), "96,64");
+			Assert.AreEqual (-70772, bmp.GetPixel (96, 96).ToArgb (), "96,96");
+			Assert.AreEqual (-4346279, bmp.GetPixel (96, 128).ToArgb (), "96,128");
+			Assert.AreEqual (-11583193, bmp.GetPixel (96, 160).ToArgb (), "96,160");
+			Assert.AreEqual (-724763, bmp.GetPixel (128, 0).ToArgb (), "128,0");
+			Assert.AreEqual (-7238268, bmp.GetPixel (128, 32).ToArgb (), "128,32");
+			Assert.AreEqual (-2169612, bmp.GetPixel (128, 64).ToArgb (), "128,64");
+			Assert.AreEqual (-3683883, bmp.GetPixel (128, 96).ToArgb (), "128,96");
+			Assert.AreEqual (-12892867, bmp.GetPixel (128, 128).ToArgb (), "128,128");
+			Assert.AreEqual (-3750464, bmp.GetPixel (128, 160).ToArgb (), "128,160");
+			Assert.AreEqual (-3222844, bmp.GetPixel (160, 0).ToArgb (), "160,0");
+			Assert.AreEqual (-65806, bmp.GetPixel (160, 32).ToArgb (), "160,32");
+			Assert.AreEqual (-2961726, bmp.GetPixel (160, 64).ToArgb (), "160,64");
+			Assert.AreEqual (-2435382, bmp.GetPixel (160, 96).ToArgb (), "160,96");
+			Assert.AreEqual (-2501944, bmp.GetPixel (160, 128).ToArgb (), "160,128");
+			Assert.AreEqual (-9211799, bmp.GetPixel (160, 160).ToArgb (), "160,160");
+/*			for (int x = 0; x <bmp.Width; x += 32) {
+				for (int y = 0; y < bmp.Height; y += 32)
+					Console.WriteLine ("\t\t\tAssert.AreEqual ({0}, bmp.GetPixel ({1}, {2}).ToArgb (), \"{1},{2}\");", bmp.GetPixel (x, y).ToArgb (), x, y);
+			}*/
 		}
 
 		
