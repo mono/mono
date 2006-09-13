c1cb3b26dd222576bf16984bd4bd91fb7559aa69
Gert Driesen <drieseng@users.sourceforge.net>
2006-09-13 17:47:44 +0000

    * DynamicMethodTest.cs: Added ctor tests for name null check. Added tests for bug #79367 (marked NotWorking).
    svn path=/trunk/mcs/; revision=65363

--- mcs/class/corlib/Test/System.Reflection.Emit/DynamicMethodTest.cs
+++ mcs/class/corlib/Test/System.Reflection.Emit/DynamicMethodTest.cs
@@ -20,6 +20,106 @@ namespace MonoTests.System.Reflection.Emit
 	{
 		private delegate int HelloInvoker (string msg);
 
+		[Test]
+		public void Constructor1_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest).Module);
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
+		[Test]
+		public void Constructor2_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest));
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
+		[Test]
+		public void Constructor3_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest).Module, true);
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
+		[Test]
+		public void Constructor4_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest), true);
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
+		[Test]
+		public void Constructor5_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					MethodAttributes.Public | MethodAttributes.Static,
+					CallingConventions.Standard,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest).Module, true);
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
+		[Test]
+		public void Constructor6_Name_Null ()
+		{
+			try {
+				new DynamicMethod (null,
+					MethodAttributes.Public | MethodAttributes.Static,
+					CallingConventions.Standard,
+					typeof (void),
+					new Type[] { typeof (string) },
+					typeof (DynamicMethodTest), true);
+				Assert.Fail ("#1");
+			} catch (ArgumentNullException ex) {
+				Assert.AreEqual (typeof (ArgumentNullException), ex.GetType (), "#2");
+				Assert.AreEqual ("name", ex.ParamName, "#3");
+				Assert.IsNull (ex.InnerException, "#4");
+			}
+		}
+
 		[Test] // bug #78253
 		public void DynamicMethodReference ()
 		{
@@ -82,6 +182,7 @@ namespace MonoTests.System.Reflection.Emit
 		}
 
 		private delegate string ReturnString (string msg);
+		private delegate void DoNothing (string msg);
 
 		private static string private_method (string s) {
 			return s;
@@ -105,6 +206,62 @@ namespace MonoTests.System.Reflection.Emit
 			Assert.AreEqual ("ABCD", del ("ABCD"));
 		}
 
+		[Test]
+		[Category ("NotWorking")] // bug #79367
+		public void ReturnType_Null ()
+		{
+			DynamicMethod hello = new DynamicMethod ("Hello",
+				null,
+				new Type[] { typeof (string) },
+				typeof (DynamicMethodTest).Module, true);
+			Assert.AreEqual (typeof (void), hello.ReturnType, "#1");
+
+			ILGenerator helloIL = hello.GetILGenerator ();
+			helloIL.Emit (OpCodes.Ret);
+
+			DoNothing dn = (DoNothing) hello.CreateDelegate (typeof (DoNothing));
+			dn ("whatever");
+
+			object[] invokeArgs = { "Hello, World!" };
+			object objRet = hello.Invoke (null, invokeArgs);
+			Assert.IsNull (objRet, "#2");
+		}
+
+		[Test]
+		[Category ("NotWorking")] // bug #79367
+		public void Name_Empty ()
+		{
+			DynamicMethod hello = new DynamicMethod (string.Empty,
+				typeof (int),
+				new Type[] { typeof (string) },
+				typeof (DynamicMethodTest).Module);
+			Assert.AreEqual (string.Empty, hello.Name, "#1");
+
+			DynamicMethod write = new DynamicMethod ("Write",
+				typeof (int),
+				new Type[] { typeof (string) },
+				typeof (DynamicMethodTest));
+
+			MethodInfo invokeWrite = write.GetBaseDefinition ();
+
+			ILGenerator helloIL = hello.GetILGenerator ();
+			helloIL.Emit (OpCodes.Ldarg_0);
+			helloIL.EmitCall (OpCodes.Call, invokeWrite, null);
+			helloIL.Emit (OpCodes.Ret);
+
+			ILGenerator writeIL = write.GetILGenerator ();
+			writeIL.Emit (OpCodes.Ldc_I4_2);
+			writeIL.Emit (OpCodes.Ret);
+
+			HelloInvoker hi =
+				(HelloInvoker) hello.CreateDelegate (typeof (HelloInvoker));
+			int ret = hi ("Hello, World!");
+			Assert.AreEqual (2, ret, "#2");
+
+			object[] invokeArgs = { "Hello, World!" };
+			object objRet = hello.Invoke (null, invokeArgs);
+			Assert.AreEqual (2, objRet, "#3");
+		}
 	}
 }
 
