4a6fc3eab94ad2e86ec4259a572aea905bf87a01
Boris Kirzner <borisk@mono-cvs.ximian.com>
2006-09-13 13:30:05 +0000

    Small fixes for TARGET_JVM test scripts.
    svn path=/trunk/mcs/; revision=65351

--- mcs/class/System.DirectoryServices/run-tests.test.bat
+++ mcs/class/System.DirectoryServices/run-tests.test.bat
@@ -12,12 +12,17 @@ REM running Test.Sys.Drawing fixture
 REM with output files named GhTests.Net.xml and GhTests.GH.xml
 REM ********************************************************
 
-
 IF "%1"=="" GOTO USAGE
 
-IF "%JAVA_HOME%"=="" GOTO ENVIRONMENT_EXCEPTION
+IF "%VMW_HOME%"=="" GOTO ENVIRONMENT_EXCEPTION
 
-IF "%GHROOT%"=="" GOTO ENVIRONMENT_EXCEPTION
+
+
+IF "%1"=="" (
+	set BUILD_OPTION=rebuild
+) ELSE (
+	set BUILD_OPTION=%1
+)
 
 REM ********************************************************
 REM Set parameters
@@ -31,7 +36,9 @@ set TEST_SOLUTION=Test\System.DirectoryServices.Test20.sln
 set TEST_ASSEMBLY=System.DirectoryServices.Test20.jar
 set PROJECT_CONFIGURATION=Debug_Java20
 
-set DATEL=%date:~4,2%_%date:~7,2%_%date:~10,4%
+set OUTPUT_FILE_PREFIX=%OUTPUT_FILE_PREFIX%.%SECURE_MODE%
+
+set DATEL=%date:~10,4%_%date:~4,2%_%date:~7,2%%
 set TIMEL=%time:~0,2%_%time:~3,2%
 set TIMESTAMP=%DATEL%_%TIMEL%
 
@@ -40,22 +47,34 @@ REM ********************************************************
 REM @echo Set environment
 REM ********************************************************
 
-set JGAC_PATH=%GHROOT%\jgac\vmw4j2ee_110\
+set JGAC_PATH=%VMW_HOME%\jgac\vmw4j2ee_110\
+set JAVA_HOME=%VMW_HOME%\jre5
 
-set RUNTIME_CLASSPATH=%JGAC_PATH%mscorlib.jar;%JGAC_PATH%System.jar;%JGAC_PATH%System.Xml.jar;%JGAC_PATH%System.DirectoryServices.jar;%JGAC_PATH%Novell.Directory.Ldap.jar;%JGAC_PATH%J2SE.Helpers.jar
+set RUNTIME_CLASSPATH=%JGAC_PATH%mscorlib.jar
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%System.jar
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%System.Xml.jar
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%System.Configuration.jar
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%J2SE.Helpers.jar
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%System.DirectoryServices.jar;
+set RUNTIME_CLASSPATH=%RUNTIME_CLASSPATH%;%JGAC_PATH%Novell.Directory.Ldap.jar;
 set NUNIT_OPTIONS=/exclude=NotWorking
 
-set GH_OUTPUT_XML=%OUTPUT_FILE_PREFIX%.GH.%SECURE_MODE%.%TIMESTAMP%.xml
-set BUILD_LOG=%OUTPUT_FILE_PREFIX%.GH.%SECURE_MODE%.%RUNNING_FIXTURE%_build.log.%TIMESTAMP%.txt
-set RUN_LOG=%OUTPUT_FILE_PREFIX%.GH.%SECURE_MODE%.%RUNNING_FIXTURE%_run.log.%TIMESTAMP%.txt
+set GH_OUTPUT_XML=%TIMESTAMP%.%OUTPUT_FILE_PREFIX%.GH.xml
+set BUILD_LOG=%TIMESTAMP%.%OUTPUT_FILE_PREFIX%.GH.%RUNNING_FIXTURE%.build.log
+set RUN_LOG=%TIMESTAMP%.%OUTPUT_FILE_PREFIX%.GH.%RUNNING_FIXTURE%.run.log
 
-set NUNIT_PATH=%BACK_TO_ROOT_DIR%..\..\nunit20\
-set NUNIT_CLASSPATH=%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.framework.jar;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.util.jar;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.core.jar;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit-console.jar;.
+set NUNIT_PATH=..\..\nunit20\
+set NUNIT_CLASSPATH=%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.framework.jar
+set NUNIT_CLASSPATH=%NUNIT_CLASSPATH%;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.util.jar
+set NUNIT_CLASSPATH=%NUNIT_CLASSPATH%;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit.core.jar
+set NUNIT_CLASSPATH=%NUNIT_CLASSPATH%;%NUNIT_PATH%nunit-console\bin\%PROJECT_CONFIGURATION%\nunit-console.jar
+set NUNIT_CLASSPATH=%NUNIT_CLASSPATH%;.
+set NUNIT_CLASSPATH=%NUNIT_CLASSPATH%;%TEST_ASSEMBLY%
+
 set CLASSPATH="%RUNTIME_CLASSPATH%;%NUNIT_CLASSPATH%"
 
-
 REM ********************************************************
-REM @echo Building GH solution...
+@echo Building GH solution...
 REM ********************************************************
 
 rem devenv Test\System.DirectoryServices.sln /%BUILD_OPTION% Debug_Java >>%RUNNING_FIXTURE%_build.log.txt 2<&1
@@ -64,7 +83,7 @@ msbuild %TEST_SOLUTION% /t:%BUILD_OPTION% /p:Configuration=%PROJECT_CONFIGURATIO
 IF %ERRORLEVEL% NEQ 0 GOTO BUILD_EXCEPTION
 
 REM ********************************************************
-REM @echo Building NUnit solution...
+@echo Building NUnit solution...
 REM ********************************************************
 
 if "%NUNIT_BUILD%" == "DONE" goto NUNITSKIP
@@ -83,11 +102,11 @@ set NUNIT_BUILD=DONE
 IF %ERRORLEVEL% NEQ 0 GOTO BUILD_EXCEPTION
 
 REM ********************************************************
-REM @echo Running GH tests...
+@echo Running GH tests...
 REM ********************************************************
 
 REM ********************************************************
-REM @echo Running fixture "%RUNNING_FIXTURE%"
+@echo Running fixture "%RUNNING_FIXTURE%"
 REM ********************************************************
 
 copy Test\bin\Debug_Java20\%TEST_ASSEMBLY% .
@@ -104,9 +123,8 @@ REM @echo on
 "%JAVA_HOME%\bin\java" %JVM_OPTIONS%  -cp %CLASSPATH% NUnit.Console.ConsoleUi %TEST_ASSEMBLY% /fixture=%RUNNING_FIXTURE%  %NUNIT_OPTIONS% /xml=%GH_OUTPUT_XML% >>%RUN_LOG% 2<&1
 REM @echo off
 
-
 REM ********************************************************
-REM @echo Build XmlTool
+@echo Build XmlTool
 REM ********************************************************
 set XML_TOOL_PATH=..\..\tools\mono-xmltool
 rem devenv %XML_TOOL_PATH%\XmlTool.sln /%BUILD_OPTION% Debug_Java >>%RUNNING_FIXTURE%_build.log.txt 2<&1
@@ -118,22 +136,17 @@ copy %XML_TOOL_PATH%\bin\%PROJECT_CONFIGURATION%\xmltool.exe .
 copy %XML_TOOL_PATH%\nunit_transform.xslt .
 
 REM ********************************************************
-REM @echo Analyze and print results
+@echo Analyze and print results
 REM ********************************************************
-REM @echo on
+@echo on
 xmltool.exe --transform nunit_transform.xslt %GH_OUTPUT_XML%
-REM @echo off
-
-copy %RUN_LOG% ..\
-copy %BUILD_LOG% ..\
-copy %GH_OUTPUT_XML% ..\
-
+@echo off
 
 :FINALLY
 GOTO END
 
 :ENVIRONMENT_EXCEPTION
-@echo This test requires environment variables JAVA_HOME and GHROOT to be defined
+@echo This test requires environment variable VMW_HOME to be defined
 GOTO END
 
 :BUILD_EXCEPTION
@@ -151,4 +164,8 @@ GOTO END
 GOTO END
 
 :END
+copy %RUN_LOG% ..\
+copy %BUILD_LOG% ..\
+copy %GH_OUTPUT_XML% ..\
+
 REM EXIT 0
