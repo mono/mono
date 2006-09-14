a0d83c896008c01cac5b48c122994b64a15c21b1
Martin Baulig <martin@novell.com>
2006-09-14 18:09:45 +0000

    New tests for anonymous methods.
    svn path=/trunk/mcs/; revision=65424

--- mcs/errors/cs0136-6.cs
+++ mcs/errors/cs0136-6.cs
@@ -1,4 +1,4 @@
-// cs0136-6.cs: A local variable named `top' cannot be declared in this scope because it would give a different meaning to `top', which is already used in a `parent or current' scope
+// cs0136-6.cs: A local variable named `top' cannot be declared in this scope because it would give a different meaning to `top', which is already used in a `parent or current' scope to denote something else
 // Line: 19
 
 using System.Collections;
