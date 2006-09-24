0328d54986f8a8b4a7478197c5aad438a40ca5a4
Marek Safar <marek.safar@gmail.com>
2006-09-24 13:03:14 +0000

    New tests.
    svn path=/trunk/mcs/; revision=65851

---
+++ mcs/errors/cs1503.cs
@@ -0,0 +1,17 @@
+// CS1503: Argument 1: Cannot convert from `int' to `bool'
+// Line: 15
+
+class A
+{
+	public static void Foo (bool test)
+	{
+	}
+}
+
+class B
+{
+	public static void Main()
+	{
+		A.Foo (1);
+	}
+}
