0a0b9700035b83064ab455cdc1ae6bd1727e1507
Martin Baulig <martin@novell.com>
2006-10-04 18:36:40 +0000

    Cosmetic fix.
    svn path=/trunk/mcs/; revision=66230

--- mcs/errors/cs0136-15.cs
+++ mcs/errors/cs0136-15.cs
@@ -1,4 +1,4 @@
-// CS0136: A local variable named `i' cannot be declared in this scope because it would give a different meaning to `i', which is already used in a 1parent or current' scope to denote something else
+// CS0136: A local variable named `i' cannot be declared in this scope because it would give a different meaning to `i', which is already used in a `parent or current' scope to denote something else
 // Line: 10
 delegate string Fun (int i);
 
