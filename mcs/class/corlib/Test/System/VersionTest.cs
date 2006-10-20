a277170b8e1cf1f1ff978b323c6a7d491579d87e
Gert Driesen <drieseng@users.sourceforge.net>
2006-10-20 10:27:23 +0000

    * VersionTest.cs: Reworked tests for NUnit 2.2.
    svn path=/trunk/mcs/; revision=66828

--- mcs/class/corlib/Test/System/VersionTest.cs
+++ mcs/class/corlib/Test/System/VersionTest.cs
@@ -7,236 +7,198 @@
 //   (C) 2002 Duco Fijma
 //
 
-using NUnit.Framework;
 using System;
+
+using NUnit.Framework;
 
 namespace MonoTests.System
 {
-
-public class VersionTest : TestCase
-{
-	public VersionTest () {}
-
-	public void TestCtors ()
+	[TestFixture]
+	public class VersionTest
 	{
-		Version v1;
-		bool exception;
+		[Test]
+		public void TestCtors ()
+		{
+			Version v1 = new Version ();
+			Assert.AreEqual (0, v1.Major, "#A1");
+			Assert.AreEqual (0, v1.Minor, "#A2");
+			Assert.AreEqual (-1, v1.Build, "#A3");
+			Assert.AreEqual (-1, v1.Revision, "#A4");
+
+			v1 = new Version (23, 7);
+			Assert.AreEqual (23, v1.Major, "#B1");
+			Assert.AreEqual (7, v1.Minor, "#B2");
+			Assert.AreEqual (-1, v1.Build, "#B3");
+			Assert.AreEqual (-1, v1.Revision, "#B4");
+
+			v1 = new Version (23, 7, 99);
+			Assert.AreEqual (23, v1.Major, "#C1");
+			Assert.AreEqual (7, v1.Minor, "#C2");
+			Assert.AreEqual (99, v1.Build, "#C3");
+			Assert.AreEqual (-1, v1.Revision, "#C4");
+
+			v1 = new Version (23, 7, 99, 42);
+			Assert.AreEqual (23, v1.Major, "#D1");
+			Assert.AreEqual (7, v1.Minor, "#D2");
+			Assert.AreEqual (99, v1.Build, "#D3");
+			Assert.AreEqual (42, v1.Revision, "#D4");
 		
-		/*
-		v1 = new Version ();
-		AssertEquals ("A1", 0, v1.Major);
-		AssertEquals ("A2", 0, v1.Minor);
-		AssertEquals ("A3", -1, v1.Build);
-		AssertEquals ("A4", -1, v1.Revision);
-	*/
+			try {
+				v1 = new Version (23, -1);
+				Assert.Fail ("#E");
+			} catch (ArgumentOutOfRangeException) {
+			}
 
-		v1 = new Version (23, 7);
-		AssertEquals ("A5", 23, v1.Major);
-		AssertEquals ("A6", 7, v1.Minor);
-		AssertEquals ("A7", -1, v1.Build);
-		AssertEquals ("A8", -1, v1.Revision);
+			try {
+				v1 = new Version (23, 7, -1);
+				Assert.Fail ("#F");
+			} catch (ArgumentOutOfRangeException) {
+			}
 
-		v1 = new Version (23, 7, 99);
-		AssertEquals ("A9", 23, v1.Major);
-		AssertEquals ("A10", 7, v1.Minor);
-		AssertEquals ("A11", 99, v1.Build);
-		AssertEquals ("A12", -1, v1.Revision);
+			try {
+				v1 = new Version (23, 7, 99, -1);
+				Assert.Fail ("#G");
+			} catch (ArgumentOutOfRangeException) {
+			}
+		}
 
-		v1 = new Version (23, 7, 99, 42);
-		AssertEquals ("A13", 23, v1.Major);
-		AssertEquals ("A14", 7, v1.Minor);
-		AssertEquals ("A15", 99, v1.Build);
-		AssertEquals ("A16", 42, v1.Revision);
-		
-		try {	
-			v1 = new Version (23, -1);
-			exception = false;
-		}
-		catch (ArgumentOutOfRangeException) {
-			exception = true;
-		}
-		Assert ("A17", exception);
+		[Test]
+		public void TestStringCtor () 
+		{
+			Version v1 = new Version ("1.42");
+			Assert.AreEqual (1, v1.Major, "#A1");
+			Assert.AreEqual (42, v1.Minor, "#A2");
+			Assert.AreEqual (-1, v1.Build, "#A3");
+			Assert.AreEqual (-1, v1.Revision, "#A4");
 
-		try {	
-			v1 = new Version (23, 7, -1);
-			exception = false;
-		}
-		catch (ArgumentOutOfRangeException) {
-			exception = true;
-		}
-		Assert ("A18", exception);
+			v1 = new Version ("1.42.79");
+			Assert.AreEqual (1, v1.Major, "#B1");
+			Assert.AreEqual (42, v1.Minor, "#B2");
+			Assert.AreEqual (79, v1.Build, "#B3");
+			Assert.AreEqual (-1, v1.Revision, "#B4");
 
-		try {	
-			v1 = new Version (23, 7, 99, -1);
-			exception = false;
+			v1 = new Version ("1.42.79.66");
+			Assert.AreEqual (1, v1.Major, "#C1");
+			Assert.AreEqual (42, v1.Minor, "#C2");
+			Assert.AreEqual (79, v1.Build, "#C3");
+			Assert.AreEqual (66, v1.Revision, "#C4");
+
+			try {
+				v1 = new Version ("1.42.-79");
+				Assert.Fail ("#D");
+			} catch (ArgumentOutOfRangeException) {
+			}
+
+			try {
+				v1 = new Version ("1");
+				Assert.Fail ("#E");
+			} catch (ArgumentException) {
+			}
+
+			try {
+				v1 = new Version ("1.2.3.4.5");
+				Assert.Fail ("#F");
+			} catch (ArgumentException) {
+			}
+
+			try {
+				v1 = new Version ((string) null);
+				Assert.Fail ("#G");
+			} catch (ArgumentNullException) {
+			}
 		}
-		catch (ArgumentOutOfRangeException) {
-			exception = true;
+
+		[Test]
+		public void TestClone () 
+		{
+			Version v1 = new Version (1, 2, 3, 4);
+			Version v2 = (Version) v1.Clone ();
+
+			Assert.IsTrue (v1.Equals (v2), "#A1");
+			Assert.IsFalse (object.ReferenceEquals (v1, v2), "#A2");
+
+			Version v3 = new Version (); // 0.0
+			v2 = (Version) v3.Clone ();
+
+			Assert.IsTrue (v3.Equals (v2), "#B1");
+			Assert.IsFalse (object.ReferenceEquals (v3, v2), "#B2");
 		}
-		Assert ("A19", exception);
-		
+
+		[Test]
+		public void TestCompareTo ()
+		{
+			Assert.AreEqual (1, new Version (1, 2).CompareTo (new Version (1, 1)), "#A1");
+			Assert.AreEqual (-1, new Version (1, 2, 3).CompareTo (new Version (2, 2, 3)), "#A2");
+			Assert.AreEqual (1, new Version (1, 2, 3, 4).CompareTo (new Version (1, 2, 2, 1)), "#A3");
+			Assert.AreEqual (1, new Version (1, 3).CompareTo (new Version (1, 2, 2, 1)), "#A4");
+			Assert.AreEqual (-1, new Version (1, 2, 2).CompareTo (new Version (1, 2, 2, 1)), "#A5");
+			Assert.AreEqual (0, new Version (1, 2).CompareTo (new Version (1, 2)), "#A6");
+			Assert.AreEqual (0, new Version (1, 2, 3).CompareTo (new Version (1, 2, 3)), "#A7");
+			Assert.IsTrue (new Version (1, 1) < new Version (1, 2), "#A8");
+			Assert.IsTrue (new Version (1, 2) <= new Version (1, 2), "#A9");
+			Assert.IsTrue (new Version (1, 2) <= new Version (1, 3), "#A10");
+			Assert.IsTrue (new Version (1, 2) >= new Version (1, 2), "#A11");
+			Assert.IsTrue (new Version (1, 3) >= new Version (1, 2), "#A12");
+			Assert.IsTrue (new Version (1, 3) > new Version (1, 2), "#A13");
+
+			Version v1 = new Version(1, 2);
+			bool exception;
+
+			Assert.AreEqual (1, v1.CompareTo (null), "#B");
+
+			try {
+				v1.CompareTo ("A string is not a version");
+				Assert.Fail ("#C");
+			} catch (ArgumentException) {
+			}
+		}
+
+		[Test]
+		public void TestEquals ()
+		{
+			Version v1 = new Version (1, 2);
+			Version v2 = new Version (1, 2, 3, 4);
+			Version v3 = new Version (1, 2, 3, 4);
+
+			Assert.IsTrue (v2.Equals (v3), "#A1");
+			Assert.IsTrue (v2.Equals (v2), "#A2");
+			Assert.IsFalse (v1.Equals (v3), "#A3");
+
+			Assert.IsTrue (v2 == v3, "#B1");
+			Assert.IsTrue (v2 == v2, "#B2");
+			Assert.IsFalse (v1 == v3, "#B3");
+
+			Assert.IsFalse (v2.Equals ((Version) null), "#C1");
+			Assert.IsFalse (v2.Equals ("A string"), "#C2");
+		}
+
+		[Test]
+		public void TestToString ()
+		{
+			Version v1 = new Version (1,2);
+			Version v2 = new Version (1,2,3);
+			Version v3 = new Version (1,2,3,4);
+
+			Assert.AreEqual ("1.2", v1.ToString (), "#A1");
+			Assert.AreEqual ("1.2.3", v2.ToString (), "#A2");
+			Assert.AreEqual ("1.2.3.4", v3.ToString (), "#A3");
+			Assert.AreEqual (string.Empty, v3.ToString (0), "#A4");
+			Assert.AreEqual ("1", v3.ToString (1), "#A5");
+			Assert.AreEqual ("1.2", v3.ToString (2), "#A6");
+			Assert.AreEqual ("1.2.3", v3.ToString (3), "#A7");
+			Assert.AreEqual ("1.2.3.4", v3.ToString (4), "#A8");
+
+			try {
+				v2.ToString (4);
+				Assert.Fail ("#B");
+			} catch (ArgumentException) {
+			}
+
+			try {
+				v3.ToString (42);
+				Assert.Fail ("#C");
+			} catch (ArgumentException) {
+			}
+		}
 	}
-
-	public void TestStringCtor () 
-	{
-		Version v1;
-		bool exception;
-		
-		v1 = new Version("1.42.79");
-		AssertEquals ("A1", 1, v1.Major);
-		AssertEquals ("A2", 42, v1.Minor);
-		AssertEquals ("A3", 79, v1.Build);
-		AssertEquals ("A4", -1, v1.Revision);
-		
-
-		try {	
-			v1 = new Version ("1.42.-79");
-			exception = false;
-		}
-		catch (ArgumentOutOfRangeException) {
-			exception = true;
-		}
-		Assert ("A5", exception);
-
-		try {	
-			v1 = new Version ("1");
-			exception = false;
-		}
-		catch (ArgumentException) {
-			exception = true;
-		}
-		Assert ("A6", exception);
-
-		try {	
-			v1 = new Version ("1.2.3.4.5");
-			exception = false;
-		}
-		catch (ArgumentException) {
-			exception = true;
-		}
-		Assert ("A7", exception);
-
-		try {	
-			v1 = new Version ((string) null);
-			exception = false;
-		}
-		catch (ArgumentNullException) {
-			exception = true;
-		}
-		Assert ("A6", exception);
-
-
-	}
-
-	public void TestClone () 
-	{
-		Version v1 = new Version (1, 2, 3, 4);
-		Version v2 = (Version) v1.Clone ();
-
-		Assert ("A1", v1.Equals (v2));
-		Assert ("A2", !ReferenceEquals (v1, v2));
-
-		Version v3 = new Version (); // 0.0
-		v2 = (Version) v3.Clone ();
-
-		Assert ("A3", v3.Equals (v2));
-		Assert ("A4", !ReferenceEquals (v3, v2));
-	}
-
-	public void TestCompareTo ()
-	{
-		Assert ("A1", new Version (1, 2).CompareTo (new Version (1, 1)) > 0);
-		Assert ("A2", new Version (1, 2, 3).CompareTo (new Version (2, 2, 3)) < 0);
-		Assert ("A3", new Version (1, 2, 3, 4).CompareTo (new Version (1, 2, 2, 1)) > 0);
-
-		Assert ("A4", new Version (1, 3).CompareTo (new Version (1, 2, 2, 1)) > 0);
-		Assert ("A5", new Version (1, 2, 2).CompareTo (new Version (1, 2, 2, 1)) < 0);
-
-		Assert ("A6", new Version (1, 2).CompareTo (new Version (1, 2)) == 0);
-		Assert ("A7", new Version (1, 2, 3).CompareTo (new Version (1, 2, 3)) == 0);
-
-		Assert ("A8", new Version (1, 1) < new Version (1, 2));
-		Assert ("A9", new Version (1, 2) <= new Version (1, 2));
-		Assert ("A10", new Version (1, 2) <= new Version (1, 3));
-		Assert ("A11", new Version (1, 2) >= new Version (1, 2));
-		Assert ("A12", new Version (1, 3) >= new Version (1, 2));
-		Assert ("A13", new Version (1, 3) > new Version (1, 2));
-
-		Version v1 = new Version(1, 2);
-		bool exception;
-
-		// LAMESPEC: Docs say this should throw a ArgumentNullException,
-		// but it simply works. Seems any version is subsequent to null
-		Assert ("A14:", v1.CompareTo (null) > 0);
-
-		try {
-			v1.CompareTo ("A string is not a version");
-			exception = false;
-		}
-		catch (ArgumentException) {
-			exception = true;
-		}
-		Assert ("A15", exception);
-		
-	}
-	
-
-	public void TestEquals ()
-	{
-		Version v1 = new Version (1, 2);
-		Version v2 = new Version (1, 2, 3, 4);
-		Version v3 = new Version (1, 2, 3, 4);
-
-		AssertEquals ("A1", true, v2.Equals (v3));
-		AssertEquals ("A2", true, v2.Equals (v2));
-		AssertEquals ("A3", false, v1.Equals (v3));
-
-		AssertEquals ("A4", true, v2 == v3);
-		AssertEquals ("A5", true, v2 == v2);
-		AssertEquals ("A6", false, v1 == v3);
-
-		AssertEquals ("A7", false, v2.Equals ((Version) null));
-		AssertEquals ("A8", false, v2.Equals ("A string"));
-		
-	}
-
-	public void TestToString ()
-	{
-		Version v1 = new Version (1,2);
-		Version v2 = new Version (1,2,3);
-		Version v3 = new Version (1,2,3,4);
-		
-		AssertEquals ("A1", "1.2", v1.ToString ());
-		AssertEquals ("A2", "1.2.3", v2.ToString ());
-		AssertEquals ("A3", "1.2.3.4", v3.ToString ());
-
-		AssertEquals ("A4", "" , v3.ToString (0));
-		AssertEquals ("A5", "1" , v3.ToString (1));
-		AssertEquals ("A6", "1.2" , v3.ToString (2));
-		AssertEquals ("A7", "1.2.3" , v3.ToString (3));
-		AssertEquals ("A8", "1.2.3.4" , v3.ToString (4));
-
-		bool exception;
-
-		try {
-			v2.ToString (4);
-			exception = false;
-		}
-		catch (ArgumentException) {
-			exception = true;
-		}
-		Assert ("A9", exception);
-
-		try {
-			v3.ToString (42);
-			exception = false;
-		}
-		catch (ArgumentException) {
-			exception = true;
-		}
-		Assert ("A10", exception);
-	}
-
-}
-
 }
