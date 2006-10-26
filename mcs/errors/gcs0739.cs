069d1c2ba89eb43287d5bf17087cdaec56c19172
Marek Safar <marek.safar@gmail.com>
2006-10-26 18:02:47 +0000

    New test.
    svn path=/trunk/mcs/; revision=66998

---
+++ mcs/errors/gcs0739.cs
@@ -0,0 +1,8 @@
+// CS0739: A duplicate type forward of type `int'
+// Line: 8
+
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof (int))]
+[assembly: TypeForwardedTo(typeof (int))]
