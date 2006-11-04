6470311b57eabe10537b670d9bd49e3f2935e050
Gert Driesen <drieseng@users.sourceforge.net>
2006-11-04 13:19:12 +0000

    * ComboBoxTests.cs: Moved to correct namespace. Added test for bug #79812. Added (NotWorking) SelectedText tests.
    svn path=/trunk/mcs/; revision=67351

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/ComboBoxTests.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/ComboBoxTests.cs
@@ -26,73 +26,151 @@
 //      Matt Hargett  <matt@use.net>
 //
 
-
-using System.Windows.Forms;
 using System;
+using System.Windows.Forms;
+
 using NUnit.Framework;
 
-[TestFixture]
-public class ComboBoxTests
+namespace MonoTests.System.Windows.Forms
 {
-	ComboBox comboBox;
-	bool textChanged, layoutUpdated;
-
-	[SetUp]
-	public void SetUp()
+	[TestFixture]
+	public class ComboBoxTests
 	{
-		comboBox = new ComboBox();
-		textChanged = false;
-		layoutUpdated = false;
-		comboBox.TextChanged += new EventHandler(textChangedEventHandler);
-		comboBox.Layout += new LayoutEventHandler(layoutEventHandler);
-	}
+		ComboBox comboBox;
+		bool textChanged, layoutUpdated;
 
-	private void textChangedEventHandler(object sender, EventArgs e)
-	{
-		textChanged = true;
-	}
+		[SetUp]
+		public void SetUp ()
+		{
+			comboBox = new ComboBox ();
+			textChanged = false;
+			layoutUpdated = false;
+			comboBox.TextChanged += new EventHandler (textChangedEventHandler);
+			comboBox.Layout += new LayoutEventHandler (layoutEventHandler);
+		}
 
-	private void layoutEventHandler(object sender, LayoutEventArgs e)
-	{
-		layoutUpdated = true;
-	}
+		private void textChangedEventHandler (object sender, EventArgs e)
+		{
+			textChanged = true;
+		}
 
+		private void layoutEventHandler (object sender, LayoutEventArgs e)
+		{
+			layoutUpdated = true;
+		}
 
-	[Test]
-	public void InitialPropertyValues()
-	{
-		
-		Assert.AreEqual(String.Empty, comboBox.Text);
-		Assert.AreEqual(-1, comboBox.SelectedIndex);
-		Assert.IsNull(comboBox.SelectedItem);
-		Assert.AreEqual(121, comboBox.Size.Width);
-		//Note: it is environment dependent
-		//Assert.AreEqual(20, comboBox.Size.Height);
-		Assert.IsFalse(textChanged);
-		Assert.IsFalse(layoutUpdated);
-	}
+		[Test]
+		public void InitialPropertyValues ()
+		{
 
-	[Test]
-	public void SetNegativeOneSelectedIndex()
-	{
-		comboBox.SelectedIndex = -1;
-		Assert.AreEqual(String.Empty, comboBox.Text);
-		Assert.IsFalse(textChanged);
-	}
+			Assert.AreEqual (String.Empty, comboBox.Text);
+			Assert.AreEqual (-1, comboBox.SelectedIndex);
+			Assert.IsNull (comboBox.SelectedItem);
+			Assert.AreEqual (121, comboBox.Size.Width);
+			//Note: it is environment dependent
+			//Assert.AreEqual(20, comboBox.Size.Height);
+			Assert.IsFalse (textChanged);
+			Assert.IsFalse (layoutUpdated);
+		}
 
-	[Test]
-	public void SetDifferentText()
-	{
-		comboBox.Text = "foooooooooooooooooooooooooo";
-		Assert.IsTrue(textChanged);
-		Assert.IsFalse(layoutUpdated);
-	}
+		[Test]
+		public void SetNegativeOneSelectedIndex ()
+		{
+			comboBox.SelectedIndex = -1;
+			Assert.AreEqual (String.Empty, comboBox.Text);
+			Assert.IsFalse (textChanged);
+		}
 
-	[Test]
-	public void SetSameText()
-	{
-		comboBox.Text = String.Empty;
-		Assert.IsFalse(textChanged);
-		Assert.IsFalse(layoutUpdated);
+		[Test]
+		public void SetDifferentText ()
+		{
+			comboBox.Text = "foooooooooooooooooooooooooo";
+			Assert.IsTrue (textChanged);
+			Assert.IsFalse (layoutUpdated);
+		}
+
+		[Test]
+		public void SetSameText ()
+		{
+			comboBox.Text = String.Empty;
+			Assert.IsFalse (textChanged);
+			Assert.IsFalse (layoutUpdated);
+		}
+
+		[Test] // bug #79812
+		public void Add_Item_NonString ()
+		{
+			comboBox.Sorted = true;
+			comboBox.Items.Add (new Person ("B"));
+			comboBox.Items.Add (new Person ("A"));
+			comboBox.Items.Add (new Person ("C"));
+			Assert.AreEqual (string.Empty, comboBox.Text, "#1");
+			comboBox.SelectedIndex = 0;
+			Assert.AreEqual ("A", comboBox.Text, "#2");
+			comboBox.SelectedIndex = 2;
+			Assert.AreEqual ("C", comboBox.Text, "#3");
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void SelectedText ()
+		{
+			Form form = new Form ();
+			form.ShowInTaskbar = false;
+			form.Visible = false;
+			form.Controls.Add (comboBox);
+
+			comboBox.Items.Add ("Bar");
+			comboBox.Items.Add ("Foo");
+			Assert.AreEqual (-1, comboBox.SelectedIndex, "#A1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#A2");
+			comboBox.SelectedIndex = 0;
+			Assert.AreEqual (0, comboBox.SelectedIndex, "#B1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#B2");
+			form.Show ();
+			Assert.AreEqual (0, comboBox.SelectedIndex, "#C1");
+			Assert.AreEqual ("Bar", comboBox.SelectedText, "#C2");
+			comboBox.SelectedIndex = 1;
+			Assert.AreEqual (1, comboBox.SelectedIndex, "#D1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#D2");
+			comboBox.SelectedText = "Ba";
+			Assert.AreEqual (-1, comboBox.SelectedIndex, "#E1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#E2");
+			comboBox.SelectedText = "Bar";
+			Assert.AreEqual (-1, comboBox.SelectedIndex, "#F1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#F2");
+			comboBox.SelectedText = "doesnotexist";
+			Assert.AreEqual (-1, comboBox.SelectedIndex, "#G1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#G2");
+			comboBox.SelectedIndex = 0;
+			Assert.AreEqual (0, comboBox.SelectedIndex, "#H1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#H2");
+			comboBox.SelectedText = "Foo";
+			Assert.AreEqual (-1, comboBox.SelectedIndex, "#I1");
+			Assert.AreEqual (string.Empty, comboBox.SelectedText, "#I2");
+		}
+
+		public class Person
+		{
+			private readonly string _name;
+
+			public Person (string name)
+			{
+				_name = name;
+			}
+
+			public string Name
+			{
+				get
+				{
+					return _name;
+				}
+			}
+
+			public override string ToString ()
+			{
+				return Name;
+			}
+		}
 	}
 }
