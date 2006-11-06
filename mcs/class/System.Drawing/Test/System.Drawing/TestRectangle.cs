b2c5f7996234cc79cab02cfeb8b7d24f4dcd7f24
Sebastien Pouliot <sebastien@ximian.com>
2006-11-06 15:40:04 +0000

    2006-11-06  Sebastien Pouliot  <sebastien@ximian.com>
    	* TestRectangle.cs: Add unit tests for == and != operators. Update 
    	tests to NUnit 2.2 syntax.
    	* TestRectangleF.cs: Add unit tests for == and != operators. Update 
    	tests to NUnit 2.2 syntax.
    
    
    svn path=/trunk/mcs/; revision=67406

--- mcs/class/System.Drawing/Test/System.Drawing/TestRectangle.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/TestRectangle.cs
@@ -1,6 +1,6 @@
 // Tests for System.Drawing.Rectangle.cs
 
-// Copyright (C) 2005 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2005, 2006 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -34,17 +34,14 @@ namespace MonoTests.System.Drawing
 
 	[TestFixture]
 	[SecurityPermission (SecurityAction.Deny, UnmanagedCode = true)]
-	public class TestRectangle : Assertion
-	{
+	public class TestRectangle {
+
 		Rectangle rect_0;
 		Rectangle rect_1;
 		Rectangle rect_2;
 		Rectangle rect_3;
 		Rectangle rect_4;
 		Rectangle rect_5;
-
-		[TearDown]
-		public void Clean () {}
 
 		[SetUp]
 		public void GetReady ()
@@ -60,152 +57,176 @@ namespace MonoTests.System.Drawing
 		[Test]
 		public void Contains ()
 		{
-			AssertEquals (false, rect_0.Contains (5, 5));
-			AssertEquals (true, rect_0.Contains (12, 12));
-			AssertEquals (true, rect_0.Contains (new Point (10, 10)));
-			AssertEquals (false, rect_0.Contains (new Point (10, 50)));
-			AssertEquals (false, rect_0.Contains (50, 10));
-			AssertEquals (true, rect_0.Contains (new Rectangle (20, 20, 15, 15)));
-			AssertEquals (false, rect_0.Contains (new Rectangle (5, 5, 20, 20)));
-			AssertEquals (true, rect_2.Contains (rect_2));
+			Assert.IsFalse (rect_0.Contains (5, 5), "a");
+			Assert.IsTrue (rect_0.Contains (12, 12), "b");
+			Assert.IsTrue (rect_0.Contains (10, 10), "c");
+			Assert.IsFalse (rect_0.Contains (10, 50), "d");
+			Assert.IsFalse (rect_0.Contains (50, 10), "e");
+			Assert.IsTrue (rect_0.Contains (new Rectangle (20, 20, 15, 15)), "f");
+			Assert.IsFalse (rect_0.Contains (new Rectangle (5, 5, 20, 20)), "g");
+			Assert.IsTrue (rect_2.Contains (rect_2), "h");
 		}
 
 		[Test]
 		public void Empty ()
 		{
-			AssertEquals (rect_2.X, 0);
-			AssertEquals (rect_2.Y, 0);
-			AssertEquals (rect_2.Width, 0);
-			AssertEquals (rect_2.Height, 0);
+			Assert.AreEqual (rect_2.X, 0, "X");
+			Assert.AreEqual (rect_2.Y, 0, "Y");
+			Assert.AreEqual (rect_2.Width, 0, "Width");
+			Assert.AreEqual (rect_2.Height, 0, "Height");
 		}
 
 		[Test]
 		public void IsEmpty ()
 		{
-			AssertEquals (rect_0.IsEmpty, false);
-			AssertEquals (rect_2.IsEmpty, true);
-			AssertEquals (rect_3.IsEmpty, false);
+			Assert.IsFalse (rect_0.IsEmpty, "0");
+			Assert.IsTrue (rect_2.IsEmpty, "2");
+			Assert.IsFalse (rect_3.IsEmpty, "3");
 		}
 
 		[Test]
 		public void GetContents ()
 		{
-			AssertEquals (rect_4.Right, rect_4.X + rect_4.Width);
-			AssertEquals (rect_4.Left, rect_4.X);
-			AssertEquals (rect_4.Bottom, rect_4.Y + rect_4.Height);
-			AssertEquals (rect_4.Top, rect_4.Y);
+			Assert.AreEqual (rect_4.Right, rect_4.X + rect_4.Width, "Right");
+			Assert.AreEqual (rect_4.Left, rect_4.X, "Left");
+			Assert.AreEqual (rect_4.Bottom, rect_4.Y + rect_4.Height, "Bottom");
+			Assert.AreEqual (rect_4.Top, rect_4.Y, "Top");
 		}
 
 		[Test]
 		public void IntersectsWith  ()
 		{						
-			AssertEquals (rect_0.IntersectsWith (rect_1), false);
-			AssertEquals (rect_0.IntersectsWith (rect_2), false);
-			AssertEquals (rect_0.IntersectsWith (rect_5), true);
-			AssertEquals (rect_5.IntersectsWith (rect_0), true);
-			AssertEquals (rect_0.IntersectsWith (rect_4), false);
+			Assert.IsFalse (rect_0.IntersectsWith (rect_1), "0 N 1");
+			Assert.IsFalse (rect_0.IntersectsWith (rect_2), "0 N 2");
+			Assert.IsTrue (rect_0.IntersectsWith (rect_5), "0 N 5");
+			Assert.IsTrue (rect_5.IntersectsWith (rect_0), "5 N 0");
+			Assert.IsFalse (rect_0.IntersectsWith (rect_4), "0 N 4");
 		}
 
 		[Test]
 		public void Location ()
 		{
-			AssertEquals (new Point (25, 252), rect_4.Location);
+			Assert.AreEqual (new Point (25, 252), rect_4.Location, "Location-1");
 			Point p = new Point (11, 121);
 			rect_4.Location = p;
-			AssertEquals (p, rect_4.Location);
-			AssertEquals (rect_4.X, 11);
-			AssertEquals (rect_4.Y, 121);
+			Assert.AreEqual (p, rect_4.Location, "Location-2");
+			Assert.AreEqual (rect_4.X, 11, "X");
+			Assert.AreEqual (rect_4.Y, 121, "Y");
 			rect_4.X = 10;
 			rect_4.Y = 15;
-			AssertEquals (new Point (10, 15), rect_4.Location);
+			Assert.AreEqual (new Point (10, 15), rect_4.Location, "Location-3");
 		}
 
 		[Test]
 		public void Size ()
 		{
-			AssertEquals (rect_4.Width, 10);
-			AssertEquals (rect_4.Height, 20);
+			Assert.AreEqual (rect_4.Width, 10, "X");
+			Assert.AreEqual (rect_4.Height, 20, "Y");
 			rect_4.Width = 40;
 			rect_4.Height = 100;
-			AssertEquals (rect_4.Size, new Size (40, 100));
+			Assert.AreEqual (rect_4.Size, new Size (40, 100), "Size");
 			rect_4.Size = new Size (1, 2);
-			AssertEquals (rect_4.Width, 1);
-			AssertEquals (rect_4.Height, 2);
+			Assert.AreEqual (rect_4.Width, 1, "Width");
+			Assert.AreEqual (rect_4.Height, 2, "Height");
 		}
 
 		[Test]
 		public void ConvertFromRectangleF ()
 		{
-			AssertEquals (rect_0, Rectangle.Ceiling (
-				new RectangleF (9.9F, 9.1F, 39.04F, 39.999F)));
-			AssertEquals (rect_0, Rectangle.Round  (
-				new RectangleF (9.5F, 10.499F, 40.01F, 39.6F)));
-			AssertEquals (rect_0, Rectangle.Truncate (
-				new RectangleF (10.999F, 10.01F, 40.3F, 40.0F)));
+			Assert.AreEqual (rect_0, Rectangle.Ceiling (
+				new RectangleF (9.9F, 9.1F, 39.04F, 39.999F)), "Ceiling");
+			Assert.AreEqual (rect_0, Rectangle.Round (
+				new RectangleF (9.5F, 10.499F, 40.01F, 39.6F)), "Round");
+			Assert.AreEqual (rect_0, Rectangle.Truncate (
+				new RectangleF (10.999F, 10.01F, 40.3F, 40.0F)), "Truncate");
 		}
 
 		[Test]
 		public void GetHashCodeTest ()
 		{
-			Assert ("GHC#1", rect_0.GetHashCode () != rect_1.GetHashCode ());
+			Assert.IsTrue (rect_0.GetHashCode () != rect_1.GetHashCode ());
 		}
 
 		[Test]
 		public void Inflate ()
 		{
 			rect_0.Inflate (new Size (8, 5));
-			AssertEquals ("INF#1", new Rectangle (2, 5, 56, 50), rect_0);
+			Assert.AreEqual (new Rectangle (2, 5, 56, 50), rect_0, "INF#1");
 			rect_1.Inflate (4, 4);
-			AssertEquals ("INF#2", new Rectangle (1, 1, 13, 13), rect_1);
-			AssertEquals ("INF#3", new Rectangle (30, 20, 70, 90),
-				Rectangle.Inflate (rect_5, 10, 20));
-			AssertEquals ("INF#4", new Rectangle (40, 40, 50, 50), rect_5);
+			Assert.AreEqual (new Rectangle (1, 1, 13, 13), rect_1, "INF#2");
+			Assert.AreEqual (new Rectangle (30, 20, 70, 90),
+				Rectangle.Inflate (rect_5, 10, 20), "INF#3");
+			Assert.AreEqual (new Rectangle (40, 40, 50, 50), rect_5, "INF#4");
 		}
 
 		[Test]
 		public void Intersect ()
 		{
-			AssertEquals ("INT#1", new Rectangle (40, 40, 10, 10), 
-				Rectangle.Intersect (rect_0, rect_5));
-			AssertEquals ("INT#2", new Rectangle (10, 10, 40, 40), rect_0);
+			Assert.AreEqual (new Rectangle (40, 40, 10, 10),
+				Rectangle.Intersect (rect_0, rect_5), "INT#1");
+			Assert.AreEqual (new Rectangle (10, 10, 40, 40), rect_0, "INT#2");
 			rect_0.Intersect (rect_5);
-			AssertEquals ("INT#3", new Rectangle (40, 40, 10, 10), rect_0);
-			AssertEquals ("INT#4", Rectangle.Empty, Rectangle.Intersect (rect_1, rect_5));
+			Assert.AreEqual (new Rectangle (40, 40, 10, 10), rect_0, "INT#3");
+			Assert.AreEqual (Rectangle.Empty, Rectangle.Intersect (rect_1, rect_5), "INT#4");
 
 			// Two rectangles touching each other
-			AssertEquals ("INT#5", new Rectangle (3, 0, 0, 7), Rectangle.Intersect (new Rectangle (0, 0, 3, 7), new Rectangle (3, 0, 8, 14)));
+			Assert.AreEqual (new Rectangle (3, 0, 0, 7), Rectangle.Intersect (new Rectangle (0, 0, 3, 7), new Rectangle (3, 0, 8, 14)), "INT#5");
 		}
 
 		[Test]
 		public void Offset ()
 		{
 			rect_0.Offset (5, 5);
-			AssertEquals ("OFS#1", new Rectangle (15, 15, 40, 40), rect_0);
+			Assert.AreEqual (new Rectangle (15, 15, 40, 40), rect_0, "OFS#1");
 			rect_1.Offset (new Point (7, 0));
-			AssertEquals ("OFS#2", new Rectangle (12, 5, 5, 5), rect_1);
+			Assert.AreEqual (new Rectangle (12, 5, 5, 5), rect_1, "OFS#2");
 		}
 
 		[Test]
 		public void ToStringTest ()
 		{
-			AssertEquals ("{X=10,Y=10,Width=40,Height=40}", rect_0.ToString ());
-			AssertEquals ("{X=5,Y=5,Width=5,Height=5}", rect_1.ToString ());
-			AssertEquals ("{X=0,Y=0,Width=0,Height=0}", rect_2.ToString ());
-			AssertEquals ("{X=25,Y=25,Width=0,Height=0}", rect_3.ToString ());
+			Assert.AreEqual ("{X=10,Y=10,Width=40,Height=40}", rect_0.ToString (), "0");
+			Assert.AreEqual ("{X=5,Y=5,Width=5,Height=5}", rect_1.ToString (), "1");
+			Assert.AreEqual ("{X=0,Y=0,Width=0,Height=0}", rect_2.ToString (), "2");
+			Assert.AreEqual ("{X=25,Y=25,Width=0,Height=0}", rect_3.ToString (), "3");
 		}
 
 		[Test]
 		public void FromTRLB ()
 		{
-			AssertEquals (rect_0, Rectangle.FromLTRB (10, 10, 50, 50));
-			AssertEquals (rect_1, Rectangle.FromLTRB (5, 5, 10, 10));
-			AssertEquals (rect_2, Rectangle.FromLTRB (0, 0, 0, 0));
+			Assert.AreEqual (rect_0, Rectangle.FromLTRB (10, 10, 50, 50), "0");
+			Assert.AreEqual (rect_1, Rectangle.FromLTRB (5, 5, 10, 10), "1");
+			Assert.AreEqual (rect_2, Rectangle.FromLTRB (0, 0, 0, 0), "2");
 		}
 
 		[Test]
 		public void Union ()
 		{
-			AssertEquals (Rectangle.FromLTRB (5, 5, 50, 50), Rectangle.Union (rect_0, rect_1));
+			Assert.AreEqual (RectangleF.FromLTRB (5, 5, 50, 50), RectangleF.Union (rect_0, rect_1));
+		}
+
+		[Test]
+		public void Operator_Equal ()
+		{
+			RectangleF r0 = new RectangleF (1, 2, 3, 4);
+			RectangleF r1 = r0;
+			Assert.IsTrue (r0 == r1, "self");
+			Assert.IsFalse (r0 == new RectangleF (0, 2, 3, 4), "X");
+			Assert.IsFalse (r0 == new RectangleF (1, 0, 3, 4), "Y");
+			Assert.IsFalse (r0 == new RectangleF (1, 2, 0, 4), "Width");
+			Assert.IsFalse (r0 == new RectangleF (1, 2, 3, 0), "Height");
+		}
+
+		[Test]
+		public void Operator_NotEqual ()
+		{
+			RectangleF r0 = new RectangleF (1, 2, 3, 4);
+			RectangleF r1 = r0;
+			Assert.IsFalse (r0 != r1, "self");
+			Assert.IsTrue (r0 != new RectangleF (0, 2, 3, 4), "X");
+			Assert.IsTrue (r0 != new RectangleF (1, 0, 3, 4), "Y");
+			Assert.IsTrue (r0 != new RectangleF (1, 2, 0, 4), "Width");
+			Assert.IsTrue (r0 != new RectangleF (1, 2, 3, 0), "Height");
 		}
 	}
 }
--- mcs/class/System.Drawing/Test/System.Drawing/TestRectangleF.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/TestRectangleF.cs
@@ -1,6 +1,6 @@
 // Tests for System.Drawing.RectangleF.cs
 
-// Copyright (C) 2005 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2005, 2006 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -34,8 +34,8 @@ namespace MonoTests.System.Drawing
 
 	[TestFixture]
 	[SecurityPermission (SecurityAction.Deny, UnmanagedCode = true)]
-	public class TestRectangleF : Assertion
-	{
+	public class TestRectangleF {
+
 		RectangleF rect_0;
 		RectangleF rect_1;
 		RectangleF rect_2;
@@ -43,9 +43,6 @@ namespace MonoTests.System.Drawing
 		RectangleF rect_4;
 		RectangleF rect_5;
 		RectangleF rect_6;
-
-		[TearDown]
-		public void Clean () {}
 
 		[SetUp]
 		public void GetReady ()
@@ -62,115 +59,124 @@ namespace MonoTests.System.Drawing
 		[Test]
 		public void Contains ()
 		{
-			AssertEquals (false, rect_0.Contains (5, 5));
-			AssertEquals (true, rect_0.Contains (12, 12));
-			AssertEquals (true, rect_0.Contains (10, 10));
-			AssertEquals (false, rect_0.Contains (10, 50));
-			AssertEquals (false, rect_0.Contains (10, 50F-float.Epsilon));
-			AssertEquals (true, rect_0.Contains (10, 49.9F));
-			AssertEquals (false, rect_0.Contains (50, 10));
+			Assert.IsFalse (rect_0.Contains (5, 5), "a");
+			Assert.IsTrue (rect_0.Contains (12, 12), "b");
+			Assert.IsTrue (rect_0.Contains (10, 10), "c");
+			Assert.IsFalse (rect_0.Contains (10, 50), "d");
+			Assert.IsFalse (rect_0.Contains (10, 50F - float.Epsilon), "e");
+			Assert.IsTrue (rect_0.Contains (10, 49.9F), "f");
+			Assert.IsFalse (rect_0.Contains (50, 10), "g");
 		}
 
 		[Test]
 		public void Empty ()
 		{
-			AssertEquals (rect_2.X, 0);
-			AssertEquals (rect_2.Y, 0);
-			AssertEquals (rect_2.Width, 0);
-			AssertEquals (rect_2.Height, 0);
+			Assert.AreEqual (rect_2.X, 0, "X");
+			Assert.AreEqual (rect_2.Y, 0, "Y");
+			Assert.AreEqual (rect_2.Width, 0, "Width");
+			Assert.AreEqual (rect_2.Height, 0, "Height");
 		}
 
 		[Test]
 		public void IsEmpty ()
 		{
-			AssertEquals (rect_0.IsEmpty, false);
-			AssertEquals (rect_2.IsEmpty, true);
-			AssertEquals (rect_3.IsEmpty, true);
-			AssertEquals (rect_6.IsEmpty, true);
+			Assert.IsFalse (rect_0.IsEmpty, "0");
+			Assert.IsTrue (rect_2.IsEmpty, "2");
+			Assert.IsTrue (rect_3.IsEmpty, "3");
+			Assert.IsTrue (rect_6.IsEmpty, "6");
 		}
 
 		[Test]
-		public void GetContents () {
-			AssertEquals (rect_4.Right, rect_4.X + rect_4.Width);
-			AssertEquals (rect_4.Left, rect_4.X);
-			AssertEquals (rect_4.Bottom, rect_4.Y + rect_4.Height);
-			AssertEquals (rect_4.Top, rect_4.Y);
+		public void GetContents () 
+		{
+			Assert.AreEqual (rect_4.Right, rect_4.X + rect_4.Width, "Right");
+			Assert.AreEqual (rect_4.Left, rect_4.X, "Left");
+			Assert.AreEqual (rect_4.Bottom, rect_4.Y + rect_4.Height, "Bottom");
+			Assert.AreEqual (rect_4.Top, rect_4.Y, "Top");
 		}
 
 		[Test]
-		public void IntersectsWith  () {						
-			AssertEquals (rect_0.IntersectsWith (rect_1), false);
-			AssertEquals (rect_0.IntersectsWith (rect_2), false);
-			AssertEquals (rect_0.IntersectsWith (rect_5), true);
-			AssertEquals (rect_5.IntersectsWith (rect_0), true);
-			AssertEquals (rect_0.IntersectsWith (rect_4), false);
+		public void IntersectsWith () 
+		{
+			Assert.IsFalse (rect_0.IntersectsWith (rect_1), "0 N 1");
+			Assert.IsFalse (rect_0.IntersectsWith (rect_2), "0 N 2");
+			Assert.IsTrue (rect_0.IntersectsWith (rect_5), "0 N 5");
+			Assert.IsTrue (rect_5.IntersectsWith (rect_0), "5 N 0");
+			Assert.IsFalse (rect_0.IntersectsWith (rect_4), "0 N 4");
 		}
 
 		[Test]
-		public void Location () {
-			AssertEquals (new PointF (25, 252), rect_4.Location);
+		public void Location () 
+		{
+			Assert.AreEqual (new PointF (25, 252), rect_4.Location, "Location");
 			PointF p = new PointF (11, 121);
 			rect_4.Location = p;
-			AssertEquals (p, rect_4.Location);
-			AssertEquals (rect_4.X, 11);
-			AssertEquals (rect_4.Y, 121);
+			Assert.AreEqual (p, rect_4.Location, "Localtion-2");
+			Assert.AreEqual (rect_4.X, 11, "X");
+			Assert.AreEqual (rect_4.Y, 121, "Y");
 			rect_4.X = 10;
 			rect_4.Y = 15;
-			AssertEquals (new PointF (10, 15), rect_4.Location);
+			Assert.AreEqual (new PointF (10, 15), rect_4.Location, "Localtion-3");
 		}
 
 		[Test]
-		public void Size () {
-			AssertEquals (rect_4.Width, 10);
-			AssertEquals (rect_4.Height, 20);
+		public void Size () 
+		{
+			Assert.AreEqual (rect_4.Width, 10, "Width-1");
+			Assert.AreEqual (rect_4.Height, 20, "Height-1");
 			rect_4.Width = 40;
 			rect_4.Height = 100;
-			AssertEquals (rect_4.Size, new SizeF (40, 100));
+			Assert.AreEqual (rect_4.Size, new SizeF (40, 100), "Size");
 			rect_4.Size = new SizeF (1, 2);
-			AssertEquals (rect_4.Width, 1);
-			AssertEquals (rect_4.Height, 2);
+			Assert.AreEqual (rect_4.Width, 1, "Width-2");
+			Assert.AreEqual (rect_4.Height, 2, "Height-2");
 		}
 
 		[Test]
-		public void GetHashCodeTest () {
-			Assert ("GHC#1", rect_0.GetHashCode () != rect_1.GetHashCode ());
+		public void GetHashCodeTest () 
+		{
+			Assert.IsTrue (rect_0.GetHashCode () != rect_1.GetHashCode ());
 		}
 
 		[Test]
-		public void Inflate () {
+		public void Inflate () 
+		{
 			rect_0.Inflate (new SizeF (8, 5));
-			AssertEquals ("INF#1", new RectangleF (2, 5, 56, 50), rect_0);
+			Assert.AreEqual (new RectangleF (2, 5, 56, 50), rect_0, "INF#1");
 			rect_1.Inflate (4, 4);
-			AssertEquals ("INF#2", new RectangleF (1, 1, 13, 13), rect_1);
-			AssertEquals ("INF#3", new RectangleF (30, 20, 70, 90),
-				RectangleF.Inflate (rect_5, 10, 20));
-			AssertEquals ("INF#4", new RectangleF (40, 40, 50, 50), rect_5);
+			Assert.AreEqual (new RectangleF (1, 1, 13, 13), rect_1, "INF#2");
+			Assert.AreEqual (new RectangleF (30, 20, 70, 90),
+				RectangleF.Inflate (rect_5, 10, 20), "INF#3");
+			Assert.AreEqual (new RectangleF (40, 40, 50, 50), rect_5, "INF#4");
 		}
 
 		[Test]
-		public void Intersect () {
-			AssertEquals ("INT#1", new RectangleF (40, 40, 10, 10), 
-				RectangleF.Intersect (rect_0, rect_5));
-			AssertEquals ("INT#2", new RectangleF (10, 10, 40, 40), rect_0);
+		public void Intersect () 
+		{
+			Assert.AreEqual (new RectangleF (40, 40, 10, 10),
+				RectangleF.Intersect (rect_0, rect_5), "INT#1");
+			Assert.AreEqual (new RectangleF (10, 10, 40, 40), rect_0, "INT#2");
 			rect_0.Intersect (rect_5);
-			AssertEquals ("INT#3", new RectangleF (40, 40, 10, 10), rect_0);
-			AssertEquals ("INT#4", RectangleF.Empty, RectangleF.Intersect (rect_1, rect_5));
+			Assert.AreEqual (new RectangleF (40, 40, 10, 10), rect_0, "INT#3");
+			Assert.AreEqual (RectangleF.Empty, RectangleF.Intersect (rect_1, rect_5), "INT#4");
 		}
 
 		[Test]
-		public void Offset () {
+		public void Offset () 
+		{
 			rect_0.Offset (5, 5);
-			AssertEquals ("OFS#1", new RectangleF (15, 15, 40, 40), rect_0);
+			Assert.AreEqual (new RectangleF (15, 15, 40, 40), rect_0, "OFS#1");
 			rect_1.Offset (new Point (7, 0));
-			AssertEquals ("OFS#2", new RectangleF (12, 5, 5, 5), rect_1);
+			Assert.AreEqual (new RectangleF (12, 5, 5, 5), rect_1, "OFS#2");
 		}
 
 		[Test]
-		public void ToStringTest () {
-			AssertEquals ("{X=10,Y=10,Width=40,Height=40}", rect_0.ToString ());
-			AssertEquals ("{X=5,Y=5,Width=5,Height=5}", rect_1.ToString ());
-			AssertEquals ("{X=0,Y=0,Width=0,Height=0}", rect_2.ToString ());
-			AssertEquals ("{X=25,Y=25,Width=0,Height=0}", rect_3.ToString ());
+		public void ToStringTest () 
+		{
+			Assert.AreEqual ("{X=10,Y=10,Width=40,Height=40}", rect_0.ToString (), "0");
+			Assert.AreEqual ("{X=5,Y=5,Width=5,Height=5}", rect_1.ToString (), "1");
+			Assert.AreEqual ("{X=0,Y=0,Width=0,Height=0}", rect_2.ToString (), "2");
+			Assert.AreEqual ("{X=25,Y=25,Width=0,Height=0}", rect_3.ToString (), "3");
 		}
 
 		[Test]
@@ -178,12 +184,37 @@ namespace MonoTests.System.Drawing
 		{
 			Rectangle r = new Rectangle (1, 2, 3, 4);
 			RectangleF rf = r;
-			AssertEquals (new RectangleF (1F, 2F, 3F, 4F), rf);
+			Assert.AreEqual (new RectangleF (1F, 2F, 3F, 4F), rf);
 		}
 
 		[Test]
-		public void Union () {
-			AssertEquals (RectangleF.FromLTRB (5, 5, 50, 50), RectangleF.Union (rect_0, rect_1));
+		public void Union () 
+		{
+			Assert.AreEqual (RectangleF.FromLTRB (5, 5, 50, 50), RectangleF.Union (rect_0, rect_1));
+		}
+
+		[Test]
+		public void Operator_Equal ()
+		{
+			RectangleF r0 = new RectangleF (1, 2, 3, 4);
+			RectangleF r1 = r0;
+			Assert.IsTrue (r0 == r1, "self");
+			Assert.IsFalse (r0 == new RectangleF (0, 2, 3, 4), "X");
+			Assert.IsFalse (r0 == new RectangleF (1, 0, 3, 4), "Y");
+			Assert.IsFalse (r0 == new RectangleF (1, 2, 0, 4), "Width");
+			Assert.IsFalse (r0 == new RectangleF (1, 2, 3, 0), "Height");
+		}
+
+		[Test]
+		public void Operator_NotEqual ()
+		{
+			RectangleF r0 = new RectangleF (1, 2, 3, 4);
+			RectangleF r1 = r0;
+			Assert.IsFalse (r0 != r1, "self");
+			Assert.IsTrue (r0 != new RectangleF (0, 2, 3, 4), "X");
+			Assert.IsTrue (r0 != new RectangleF (1, 0, 3, 4), "Y");
+			Assert.IsTrue (r0 != new RectangleF (1, 2, 0, 4), "Width");
+			Assert.IsTrue (r0 != new RectangleF (1, 2, 3, 0), "Height");
 		}
 	}
 }
