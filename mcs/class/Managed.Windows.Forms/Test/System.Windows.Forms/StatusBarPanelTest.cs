70417974a556d2fcc9de5777ad1c542e970c06f5
Chris Toshok <toshok@novell.com>
2006-11-07 22:52:28 +0000

    add two non-working StatusBarPanel tests
    svn path=/trunk/mcs/; revision=67499

---
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/StatusBarPanelTest.cs
@@ -0,0 +1,38 @@
+//
+// StatusBarPanelTest.cs: Test cases for StatusBar.
+//
+// (C) 2006 Novell, Inc. (http://www.novell.com)
+//
+
+using System;
+using NUnit.Framework;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.Remoting;
+
+namespace MonoTests.System.Windows.Forms
+{
+	[TestFixture]
+	public class StatusBarPanelTest 
+	{
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		[Category ("NotWorking")]
+		public void MinimumWidth1 ()
+		{
+			StatusBarPanel p = new StatusBarPanel ();
+			Assert.AreEqual (10, p.MinWidth, "1");
+			p.Width = 9;
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void MinimumWidth2 ()
+		{
+			StatusBarPanel p = new StatusBarPanel ();
+			p.Width = 50;
+			p.MinWidth = 100;
+			Assert.AreEqual (100, p.Width, "1");
+		}
+	}
+}
