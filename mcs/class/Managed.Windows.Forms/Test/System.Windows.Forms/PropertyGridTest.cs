6e2a7025f14dc36a5cf459568c25e05c37119da4
Chris Toshok <toshok@novell.com>
2006-11-08 22:47:11 +0000

    add a bunch more property grid tests (some of which aren't working with mono)
    svn path=/trunk/mcs/; revision=67557

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/PropertyGridTest.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/PropertyGridTest.cs
@@ -50,23 +50,17 @@ namespace MonoTests.System.Windows.Forms
 		[Test] // bug #79615
 		public void SelectedObjects_Multiple ()
 		{
-			Form form = new Form ();
-			form.ShowInTaskbar = false;
 			Button button1 = new Button ();
 			Button button2 = new Button ();
 
 			PropertyGrid pg = new PropertyGrid ();
 			pg.SelectedObjects = new object [] { button1, button2 };
-			form.Controls.Add (pg);
-			form.Controls.Add (button1);
-			form.Controls.Add (button2);
 			Assert.IsNotNull (pg.SelectedObjects, "#1");
 			Assert.AreEqual (2, pg.SelectedObjects.Length, "#2");
 			Assert.AreSame (button1, pg.SelectedObjects [0], "#3");
 			Assert.AreSame (button2, pg.SelectedObjects [1], "#4");
 			Assert.IsNotNull (pg.SelectedObject, "#5");
 			Assert.AreSame (button1, pg.SelectedObject, "#6");
-			form.Dispose ();
 		}
 
 		[Test]
@@ -92,6 +86,112 @@ namespace MonoTests.System.Windows.Forms
 			PropertyGrid pg = new PropertyGrid ();
 			Button button1 = new Button ();
 			pg.SelectedObjects = new object [] { button1, null };
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void PropertyGrid_MergedTest ()
+		{
+			PropertyGrid pg = new PropertyGrid ();
+			pg.SelectedObjects = new object[] { new Button (), new Label () };
+
+			Assert.IsNotNull (pg.SelectedGridItem, "1");
+			Assert.AreEqual ("Accessibility", pg.SelectedGridItem.Label, "2");
+			Assert.AreEqual (GridItemType.Category, pg.SelectedGridItem.GridItemType, "3");
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void PropertyGrid_MergedRootTest ()
+		{
+			object[] selected_objects = new object[] { new Button (), new Label () };
+			PropertyGrid pg = new PropertyGrid ();
+
+			pg.SelectedObjects = selected_objects;
+
+			Assert.IsNotNull (pg.SelectedGridItem.Parent, "1");
+			Assert.AreEqual ("System.Object[]", pg.SelectedGridItem.Parent.Label, "2");
+			Assert.AreEqual (GridItemType.Root, pg.SelectedGridItem.Parent.GridItemType, "3");
+
+			Assert.AreEqual (selected_objects, pg.SelectedGridItem.Parent.Value, "4");
+
+			Assert.IsNull (pg.SelectedGridItem.Parent.Parent, "5");
+		}		
+
+		class ArrayTest_object
+		{
+			int[] array;
+			public ArrayTest_object ()
+			{
+				array = new int[10];
+				for (int i = 0; i < array.Length; i ++)
+					array[i] = array.Length - i;
+			}
+			public int[] Array {
+				get { return array; }
+			}
+		}
+
+		[Test]
+		public void PropertyGrid_ArrayTest ()
+		{
+			PropertyGrid pg = new PropertyGrid ();
+
+			pg.SelectedObject = new ArrayTest_object ();
+
+			// selected object
+			Assert.AreEqual ("Array", pg.SelectedGridItem.Label, "1");
+			Assert.IsTrue (pg.SelectedGridItem.Value is Array, "2");
+			Assert.AreEqual (10, pg.SelectedGridItem.GridItems.Count, "3");
+			Assert.AreEqual (GridItemType.Property, pg.SelectedGridItem.GridItemType, "4");
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void PropertyGrid_ArrayParentTest ()
+		{
+			PropertyGrid pg = new PropertyGrid ();
+
+			pg.SelectedObject = new ArrayTest_object ();
+
+			// parent
+			Assert.IsNotNull (pg.SelectedGridItem.Parent, "1");
+			Assert.AreEqual ("Misc", pg.SelectedGridItem.Parent.Label, "2");
+			Assert.AreEqual (GridItemType.Category, pg.SelectedGridItem.Parent.GridItemType, "3");
+			Assert.AreEqual (1, pg.SelectedGridItem.Parent.GridItems.Count, "4");
+		}
+
+		[Test]
+		[Category ("NotWorking")]
+		public void PropertyGrid_ArrayRootTest ()
+		{
+			ArrayTest_object obj = new ArrayTest_object ();
+			PropertyGrid pg = new PropertyGrid ();
+
+			pg.SelectedObject = obj;
+
+			// grandparent
+			Assert.IsNotNull (pg.SelectedGridItem.Parent.Parent, "1");
+			Assert.AreEqual (typeof(ArrayTest_object).ToString(), pg.SelectedGridItem.Parent.Parent.Label, "2");
+			Assert.AreEqual (GridItemType.Root, pg.SelectedGridItem.Parent.Parent.GridItemType, "3");
+			Assert.AreEqual (1, pg.SelectedGridItem.Parent.Parent.GridItems.Count, "4");
+			Assert.AreEqual (obj, pg.SelectedGridItem.Parent.Parent.Value, "5");
+
+			Assert.IsNull (pg.SelectedGridItem.Parent.Parent.Parent, "6");
+		}
+
+		[Test]
+		public void PropertyGrid_ArrayChildrenTest ()
+		{
+			PropertyGrid pg = new PropertyGrid ();
+
+			pg.SelectedObject = new ArrayTest_object ();
+
+			// children
+			Assert.AreEqual ("[0]", pg.SelectedGridItem.GridItems[0].Label, "1");
+			Assert.AreEqual (GridItemType.Property, pg.SelectedGridItem.GridItems[0].GridItemType, "2");
+			Assert.AreEqual (10, pg.SelectedGridItem.GridItems[0].Value, "3");
+			Assert.AreEqual (0, pg.SelectedGridItem.GridItems[0].GridItems.Count, "4");
 		}
 	}
 }
