773264de94f30dbde7b95783b46e7c966175e061
Robert Jordan <robertj@gmx.net>
2006-11-13 19:14:57 +0000

    2006-11-13  Jensen Somers  <jensen.somers@gmail.com>
    	* Removed [Serializable] from OnDeserializedAttribute,
    	OnDeserializingAttribute, OnSerializedAttribute,
    	OnSerializingAttribute.
    
    
    svn path=/trunk/mcs/; revision=67781

--- mcs/class/corlib/System.Runtime.Serialization/OnDeserializingAttribute.cs
+++ mcs/class/corlib/System.Runtime.Serialization/OnDeserializingAttribute.cs
@@ -33,7 +33,6 @@ using System.Runtime.InteropServices;
 namespace System.Runtime.Serialization {
 
 	[ComVisible(true)]
-	[Serializable]
 	[AttributeUsage (AttributeTargets.Method, Inherited=false)]
 	public sealed class OnDeserializingAttribute : Attribute {
 	}
