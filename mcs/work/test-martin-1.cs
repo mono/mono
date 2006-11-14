1932c619b6cd3527b554f85e1a4fb86fb90e0125
Martin Baulig <martin@novell.com>
2006-11-14 16:16:22 +0000

    Updated.
    svn path=/branches/martin/anonymous-methods2/work/; revision=67852

--- mcs/work/test-martin-1.cs
+++ mcs/work/test-martin-1.cs
@@ -12,24 +12,23 @@ public class X
 		for (int i = 1; i <= 5; i++) {
 			int k = i;
 			TestDelegate temp = delegate {
-				Console.WriteLine (i);
-				Console.WriteLine (k);
+				Console.WriteLine ("i = {0}, k = {1}", i, k);
 				sum_i += 1 << i;
 				sum_k += 1 << k;
 			};
 			temp ();
 			d += temp;
 		}
+		Console.WriteLine ("SUM i = {0}, k = {1}", sum_i, sum_k);
 		Console.WriteLine ();
-		Console.WriteLine ("i = {0}, k = {1}", sum_i, sum_k);
 		if (sum_i != 62)
 			return 1;
 		if (sum_k != 62)
 			return 2;
 		sum_i = sum_k = 0;
 		d();
+		Console.WriteLine ("SUM i = {0}, k = {1}", sum_i, sum_k);
 		Console.WriteLine ();
-		Console.WriteLine ("i = {0}, k = {1}", sum_i, sum_k);
 		if (sum_i != 320)
 			return 3;
 		if (sum_k != 62)
--- mcs/work/test-martin-2.cs
+++ mcs/work/test-martin-2.cs
@@ -12,9 +12,7 @@ public class X
 		for (int i = 1; i <= 5; i++) {
 			int k = i;
 			TestDelegate temp = delegate {
-				Console.WriteLine (p);
-				Console.WriteLine (i);
-				Console.WriteLine (k);
+				Console.WriteLine ("i = {0}, k = {1}, p = {2}", i, k, p);
 				sum_i += 1 << i;
 				sum_k += 1 << k;
 				sum_p += 1 << p;
@@ -23,8 +21,8 @@ public class X
 			temp ();
 			d += temp;
 		}
+		Console.WriteLine ("SUM i = {0}, k = {1}, p = {2}", sum_i, sum_k, sum_p);
 		Console.WriteLine ();
-		Console.WriteLine ("i = {0}, k = {1}, p = {2}", sum_i, sum_k, sum_p);
 		if (sum_i != 62)
 			return 1;
 		if (sum_k != 62)
@@ -33,8 +31,8 @@ public class X
 			return 3;
 		sum_i = sum_k = sum_p = 0;
 		d();
+		Console.WriteLine ("SUM i = {0}, k = {1}, p = {2}", sum_i, sum_k, sum_p);
 		Console.WriteLine ();
-		Console.WriteLine ("i = {0}, k = {1}, p = {2}", sum_i, sum_k, sum_p);
 		if (sum_i != 320)
 			return 4;
 		if (sum_k != 62)
