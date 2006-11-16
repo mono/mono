87b92eec8bed8b4ffeec1896f6b8f34cfe8bd8a0
Chris Toshok <toshok@novell.com>
2006-11-16 01:52:37 +0000

    Application unit tests
    svn path=/trunk/mcs/; revision=67969

---
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/ApplicationTest.cs
@@ -0,0 +1,43 @@
+//
+// ApplicationContextTest.cs
+//
+// Author:
+//   Chris Toshok (toshok@ximian.com)
+//
+// (C) 2006 Novell, Inc. (http://www.novell.com)
+//
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace MonoTests.System.Windows.Forms
+{
+	[TestFixture]
+	public class ApplicationTest
+	{
+		ApplicationContext ctx;
+
+		void form_visible_changed (object sender, EventArgs e)
+		{
+			Assert.AreEqual (sender, ctx.MainForm, "1");
+			((Form)sender).Close();
+		}
+
+		[Test]
+		public void ContextMainFormTest ()
+		{
+			Form f1 = new Form ();
+			ctx = new ApplicationContext (f1);
+
+			f1.VisibleChanged += new EventHandler (form_visible_changed);
+
+			Application.Run (ctx);
+
+			Assert.IsNull (ctx.MainForm, "2");
+		}
+	}
+}
