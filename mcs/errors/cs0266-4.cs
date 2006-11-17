e751442dd90d424990bd7bf49def8d1940141b96
Marek Safar <marek.safar@gmail.com>
2006-11-17 23:13:26 +0000

    Add new tests.
    svn path=/trunk/mcs/; revision=68114

---
+++ mcs/errors/cs0266-4.cs
@@ -0,0 +1,20 @@
+// cs0266.cs: Cannot implicitly convert type `Bar' to `X'. An explicit conversion exists (are you missing a cast?)
+// Line: 18
+
+public enum Bar
+{
+	ABar
+}
+
+class X
+{
+	public static explicit operator X (Bar the_bar)
+	{
+		return new X();
+	}
+	
+	public static void Main ()
+	{
+		X x = Bar.ABar;
+	}
+}
