efa7be74b3351ab8e364cb88700cde93faa9d721
Gert Driesen <drieseng@users.sourceforge.net>
2006-11-19 09:33:51 +0000

    * cs1058.cs: Removed as CS1058 does not apply to 1.0 profile. * gcs1058-1.cs, gcs1058-2.cs: Added tests for bug #79810.
    svn path=/trunk/mcs/; revision=68145

--- mcs/errors/gcs1058-1.cs
+++ mcs/errors/gcs1058-1.cs
@@ -1,6 +1,6 @@
 // cs1058.cs: A previous catch clause already catches all exceptions. All non-exceptions thrown will be wrapped in a `System.Runtime.CompilerServices.RuntimeWrappedException'
 // Line: 11
-// Compiler options: -warnaserror -warn:1
+// Compiler options: -warnaserror -warn:4
 
 class C
 {
