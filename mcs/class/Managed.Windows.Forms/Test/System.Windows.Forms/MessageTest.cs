06f0f58fa5e99189923cb8e8dbbac8b997cc9a0a
Rolf Bjarne Kvinge <RKvinge@novell.com>
2006-11-30 17:47:01 +0000

    2006-11-30  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* MessageTest.cs: Added test for ToString.
    	
    
    svn path=/trunk/mcs/; revision=68761

---
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/MessageTest.cs
@@ -0,0 +1,32 @@
+﻿//
+// MessageTest.cs: Test cases for Message
+//
+// Authors:
+//   Rolf Bjarne Kvinge (RKvinge@novell.com)
+//
+// (C) 2006 Novell, Inc. (http://www.novell.com)
+//
+
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.Windows.Forms
+{
+
+	[TestFixture]
+	public class MessageTest
+	{
+		[Test]
+		public void ToStringTest ()
+		{
+			Message msg = Message.Create (new IntPtr (123), 2, new IntPtr (234), new IntPtr (345));
+			Assert.AreEqual ("msg=0x2 (WM_DESTROY) hwnd=0x7b wparam=0xea lparam=0x159 result=0x0", msg.ToString ());
+			msg.Result = new IntPtr (2);
+			Assert.AreEqual ("msg=0x2 (WM_DESTROY) hwnd=0x7b wparam=0xea lparam=0x159 result=0x2", msg.ToString ());
+		}
+	}
+}
