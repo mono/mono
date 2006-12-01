782d2bc71933ae30bc1fcb0855186e9c17bd36da
Marek Safar <marek.safar@gmail.com>
2006-12-01 18:56:09 +0000

    Correctly reapplied my last changes
    svn path=/trunk/mcs/; revision=68838

---
+++ mcs/errors/cs0031-8.cs
@@ -0,0 +1,7 @@
+// cs0031: Constant value `-97' cannot be converted to a `uint'
+// Line: 9
+
+public class Test
+{
+	const uint b = -'a';
+}
---
+++ mcs/errors/cs0664-4.cs
@@ -0,0 +1,9 @@
+// cs0664: Literal of type double cannot be implicitly converted to type `decimal'. Add suffix `m' to create a literal of this type
+// Line: 7
+
+class X {
+	void A ()
+	{
+		decimal d = -2.0;
+	}
+}
