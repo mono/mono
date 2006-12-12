7e425865267e2cabe1a44db74379909479e75fd0
Marek Safar <marek.safar@gmail.com>
2006-12-12 18:09:54 +0000

    New test.
    svn path=/trunk/mcs/; revision=69421

---
+++ mcs/tests/test-partial-14.cs
@@ -0,0 +1,20 @@
+public partial class partial
+{
+}
+
+public partial class A : partial
+{
+	public void partial (partial partial)
+	{  
+		partial partial_ = partial;
+	}
+}
+
+public class B
+{
+	int partial;
+	
+	public static void Main ()
+	{
+	}
+}
