51bc2f2b05aebc0fcc27730194c331e293e41d6a
Eyal Alalouf <eyala@mainsoft.com>
2006-12-14 17:37:34 +0000

    Adding various attributes inheriting from MonoTODO.
    svn path=/trunk/mcs/; revision=69493

--- mcs/build/common/MonoTODOAttribute.cs
+++ mcs/build/common/MonoTODOAttribute.cs
@@ -33,7 +33,7 @@
 namespace System {
 	
 	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
-	internal sealed class MonoTODOAttribute : Attribute {
+	internal class MonoTODOAttribute : Attribute {
 
 		string comment;
 		
@@ -48,6 +48,51 @@ namespace System {
 
 		public string Comment {
 			get { return comment; }
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	internal class MonoDocumentationNoteAttribute : MonoTODOAttribute {
+
+		public MonoDocumentationNoteAttribute (string comment)
+			: base (comment)
+		{
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	internal class MonoExtensionAttribute : MonoTODOAttribute {
+
+		public MonoExtensionAttribute (string comment)
+			: base (comment)
+		{
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	internal class MonoInternalNoteAttribute : MonoTODOAttribute {
+
+		public MonoInternalNoteAttribute (string comment)
+			: base (comment)
+		{
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	internal class MonoLimitationAttribute : MonoTODOAttribute {
+
+		public MonoLimitationAttribute (string comment)
+			: base (comment)
+		{
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All, AllowMultiple=true)]
+	internal class MonoNotSupportedAttribute : MonoTODOAttribute {
+
+		public MonoNotSupportedAttribute (string comment)
+			: base (comment)
+		{
 		}
 	}
 }
