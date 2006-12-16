423d8b5108e1e64a5a357722ce203cce8bc93317
Miguel de Icaza <miguel@gnome.org>
2006-12-16 16:58:36 +0000

    Add test for the struct constructor fix
    svn path=/trunk/mcs/; revision=69577

---
+++ mcs/tests/test-545.cs
@@ -0,0 +1,15 @@
+//
+// It is legal to call the this() constructor
+// on structures.
+//
+
+struct Dingus {
+	public Dingus (int a) : this ()
+		{
+		}
+	
+}
+
+class X {
+	static void Main () {}
+}
