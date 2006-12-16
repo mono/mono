72421858519cd6efa58b43d30474cc2f3cb7cd7f
Marek Safar <marek.safar@gmail.com>
2006-12-16 12:10:23 +0000

    New tests.
    svn path=/trunk/mcs/; revision=69572

---
+++ mcs/errors/cs0466-2.cs
@@ -0,0 +1,14 @@
+// cs0466: `Base.I.this[params int[]].set': the explicit interface implementation cannot introduce the params modifier
+// Line: 10
+
+interface I
+{
+	int this [int[] p] { set; }
+}
+class Base : I
+{
+	int I.this [params int[] p] { 
+		set {
+		}
+	}
+}
