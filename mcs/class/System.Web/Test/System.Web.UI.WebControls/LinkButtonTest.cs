7a38a9cdcfe590b6ba7484f464d6248c317e0734
Konstantin Triger <kostat@mono-cvs.ximian.com>
2006-12-20 09:31:18 +0000

    Fixed with a new browscap.ini
    svn path=/trunk/mcs/; revision=69801

--- mcs/class/System.Web/Test/System.Web.UI.WebControls/LinkButtonTest.cs
+++ mcs/class/System.Web/Test/System.Web.UI.WebControls/LinkButtonTest.cs
@@ -162,7 +162,6 @@ namespace MonoTests.System.Web.UI.WebControls
 
 		[Test]
 		[Category ("NunitWeb")]
-		[Category ("NotWorking")]
 		public void ValidationGroup ()
 		{
 			WebTest t = new WebTest ("NoEventValidation.aspx");
