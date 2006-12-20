07968ad6b4806f387468bbeda31da31385a9e73e
Jackson Harper <jackson@novell.com>
2006-12-20 19:00:38 +0000

    precedence
    svn path=/trunk/mcs/; revision=69832

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextBoxBase.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TextBoxBase.cs
@@ -460,7 +460,7 @@ namespace System.Windows.Forms {
 		[EditorBrowsable(EditorBrowsableState.Advanced)]
 		public int PreferredHeight {
 			get {
-				return Font.Height + BorderStyle == BorderStyle.None ? 0 : 7;
+				return Font.Height + (BorderStyle == BorderStyle.None ? 0 : 7);
 			}
 		}
 
