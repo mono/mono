7da033a75a92da379a8fbf862ef31a901a1b4c6f
Sebastien Pouliot <sebastien@ximian.com>
2006-12-23 04:34:08 +0000

    Fix build as HttpListener now depends on X509Certificate2, which can't be build on the first time for 2.0... bla bla bla, and lot of other stuff depends on it
    svn path=/trunk/mcs/; revision=69982

--- mcs/class/System/System.Net/HttpListener.cs
+++ mcs/class/System/System.Net/HttpListener.cs
@@ -25,7 +25,9 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-#if NET_2_0
+
+#if NET_2_0 && SECURITY_DEP
+
 using System.Collections.Generic;
 using System.Threading;
 //TODO: logging
