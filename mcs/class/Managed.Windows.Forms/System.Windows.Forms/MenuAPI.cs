17470847b88dd17c9808c768b691e9e860521fa3
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-01-01 10:13:18 +0000

    Revert MenuAPI wrong commit
    svn path=/trunk/mcs/; revision=70315

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/MenuAPI.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/MenuAPI.cs
@@ -217,7 +217,6 @@ namespace System.Windows.Forms {
 			MenuTracker tracker = new MenuTracker (menu);
 			tracker.active = true;
 			tracker.popup_active = true;
-			tracker.grab_control.ActiveTracker = tracker;
 			menu.tracker = tracker;
 
 			menu.Wnd = new PopUpWindow (tracker.grab_control, menu);
