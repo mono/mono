b2e79b43bfb97ff1de6d77f3d612de96d4fa21c8
Miguel de Icaza <miguel@gnome.org>
2007-01-01 06:50:53 +0000

    Add new test
    svn path=/trunk/mcs/; revision=70307

--- mcs/tests/test-542.cs
+++ mcs/tests/test-542.cs
@@ -1,18 +1,27 @@
-class C
-{
-	static void Test (C arg)
+//
+// See bug 78113
+//	
+
+public struct ARec
 	{
+		decimal mVal;
+		public ARec(decimal val)
+		{
+			mVal = Round(val, 1); 
+		}
+
+		decimal Round(int digits)
+		{
+			return Round(mVal, digits);
+		}
+
+		static decimal Round(decimal val, int digits)
+		{
+			return 0;
+		}
 	}
-	
-	public static void Main ()
-	{
-		object value = null;
-		C.Test(
-#if true
-		(C)
-#else
-		no error here
-#endif
-		value);
-	}
+
+class X {
+static void Main () {
+}
 }
