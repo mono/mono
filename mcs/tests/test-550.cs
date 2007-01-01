2f2b072df5ec6e938bb8948d910f19bab2a298a8
Miguel de Icaza <miguel@gnome.org>
2007-01-01 07:51:13 +0000

    Add new test for bug 78259
    svn path=/trunk/mcs/; revision=70309

---
+++ mcs/tests/test-550.cs
@@ -0,0 +1,16 @@
+// Compiler options: -codepage:65001
+// Note; this file is utf8 encoded
+using System;
+
+namespace Bla.Blub
+{
+
+	class Fo‿o
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("bla");
+		}
+	}
+
+}
