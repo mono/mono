ee19d7ac4b4a5923659a7870063262d96f5fc58c
Marek Sieradzki <msierad@mono-cvs.ximian.com>
2007-01-12 09:55:43 +0000

    2007-01-12  Marek Sieradzki  <marek.sieradzki@gmail.com>
            * Build.cs: More tests.
    
    
    svn path=/trunk/mcs/; revision=70906

--- mcs/class/Microsoft.Build.Engine/Test/various/Build.cs
+++ mcs/class/Microsoft.Build.Engine/Test/various/Build.cs
@@ -77,5 +77,65 @@ namespace MonoTests.Microsoft.Build.BuildEngine.Various {
 
 			Assert.IsFalse (project.Build ("1"));
 		}
+
+		[Test]
+		public void TestBuild3 ()
+		{
+			Engine engine;
+			Project project;
+
+			string documentString = @"
+				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+					<Target Name='A'>
+						<Error Text='text' />
+						<OnError ExecuteTargets='B' />
+					</Target>
+					<Target Name='B'>
+						<CreateProperty Value='B'>
+							<Output TaskParameter='Value' PropertyName='B'/>
+						</CreateProperty>
+					</Target>
+				</Project>
+			";
+
+			engine = new Engine (Consts.BinPath);
+			project = engine.CreateNewProject ();
+			project.LoadXml (documentString);
+
+			Assert.IsFalse (project.Build ("A"), "A1");
+			Assert.IsNotNull (project.EvaluatedProperties ["B"], "A2");
+		}
+
+		[Test]
+		public void TestBuild4 ()
+		{
+			Engine engine;
+			Project project;
+
+			string documentString = @"
+				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+					<Target Name='A'>
+						<Error ContinueOnError='true' Text='text' />
+						<CreateProperty Value='A'>
+							<Output TaskParameter='Value' PropertyName='A'/>
+						</CreateProperty>
+						<OnError ExecuteTargets='B' />
+					</Target>
+					<Target Name='B'>
+						<CreateProperty Value='B'>
+							<Output TaskParameter='Value' PropertyName='B'/>
+						</CreateProperty>
+					</Target>
+				</Project>
+			";
+
+			engine = new Engine (Consts.BinPath);
+			project = engine.CreateNewProject ();
+			project.LoadXml (documentString);
+
+			Assert.IsTrue (project.Build ("A"), "A1");
+			Assert.IsNotNull (project.EvaluatedProperties ["A"], "A2");
+			Assert.IsNull (project.EvaluatedProperties ["B"], "A3");
+		}
 	}
 }
