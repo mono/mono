9d1d5fa0892522fec0eee21d95a1f35a6b2511ad
Vladimir Krasnov <krasnov@mono-cvs.ximian.com>
2007-01-18 10:18:45 +0000

    * DerbyDBSchema.cs: added traces to unload handler
    svn path=/trunk/mcs/; revision=71242

--- mcs/class/Mainsoft.Web/Mainsoft.Web.Security/DerbyDBSchema.cs
+++ mcs/class/Mainsoft.Web/Mainsoft.Web.Security/DerbyDBSchema.cs
@@ -279,7 +279,10 @@ namespace Mainsoft.Web.Security
 			try {
 				connection.Open ();
 			}
-			catch (Exception) {
+			catch (Exception ex) {
+#if TRACE
+				Console.Write (ex.ToString ());
+#endif
 			}
 		}
 
