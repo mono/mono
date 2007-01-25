db0224b929a59ffd97bce9e49e080739d45f76d5
Ilya Kharmatsky <ilya@mono-cvs.ximian.com>
2007-01-25 16:37:25 +0000

    Added 'Ignore' attributes, under TARGET_JVM directive in order to workaround Grasshopper's bugs.
    svn path=/trunk/mcs/; revision=71676

--- mcs/class/System/Test/System.CodeDom/CodeTypeReferenceTest.cs
+++ mcs/class/System/Test/System.CodeDom/CodeTypeReferenceTest.cs
@@ -469,6 +469,9 @@ namespace MonoTests.System.CodeDom
 		}
 
 		[Test]
+#if TARGET_JVM
+		[Ignore("TD BUG ID: 7202")]
+#endif
 		public void GenericTypeTest1 () {
 			CodeTypeReference reference = new CodeTypeReference (
 				typeof (Dictionary<int,string>));
@@ -497,6 +500,9 @@ namespace MonoTests.System.CodeDom
 		}
 
 		[Test]
+#if TARGET_JVM
+		[Ignore("TD BUG ID: 7202")]
+#endif
 		public void GenericTypeTest2 () {
 			CodeTypeReference reference = new CodeTypeReference (
 				typeof (Dictionary<List<int>, string>));
@@ -533,6 +539,9 @@ namespace MonoTests.System.CodeDom
 		}
 
 		[Test]
+#if TARGET_JVM
+		[Ignore("TD BUG ID: 7202")]
+#endif		
 		public void GenericTypeTest3 () 
 		{
 			CodeTypeReference reference = new CodeTypeReference (
@@ -575,6 +584,9 @@ namespace MonoTests.System.CodeDom
 		}
 
 		[Test]
+#if TARGET_JVM
+		[Ignore ("TD BUG ID: 7203")]
+#endif		
 		public void GenericTypeTest5 () 
 		{
 			CodeTypeReference reference = new CodeTypeReference (
