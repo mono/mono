ae59f8def777bf6fdfb62a73d23311eaf4945f26
Marek Safar <marek.safar@gmail.com>
2007-01-28 10:52:51 +0000

    bug #80646
    svn path=/trunk/mcs/; revision=71803

---
+++ mcs/tests/gtest-306.cs
@@ -0,0 +1,25 @@
+using System;
+
+public partial class FuParentClass<Trow>
+{
+	public FuParentClass ()
+	{
+	}
+}
+
+public partial class FuParentClass<Trow>
+{
+	public class FuChildClass 
+	{
+		public FuChildClass ()
+		{			
+		}
+	}
+}
+
+class C
+{
+	public static void Main ()
+	{
+	}
+}
