8d6384fae1076a2393e0719c8b0a918585e490a3
Miguel de Icaza <miguel@gnome.org>
2007-01-29 15:38:46 +0000

    Merge with Hari's Lambda
    svn path=/trunk/mcs/; revision=71887

--- mcs/mcs/lambda.cs
+++ mcs/mcs/lambda.cs
@@ -12,10 +12,29 @@ using System;
 using System.Collections;
 
 namespace Mono.CSharp {
-	public class LambdaExpression : Expression {
-		public LambdaExpression (ArrayList parameters, object body, Location loc)
+	public class LambdaExpression : AnonymousMethodExpression {
+		Expression expression;
+		Block block;
+
+		//
+		// The parameter list can either be:
+		//    null: no parameters
+		//    arraylist of Parameter (explicitly typed parameters)
+		//    arraylist of strings (implicitly typed parameters)
+		//
+		public LambdaExpression (ArrayList parameter_list, object expression_or_block_body, Location l)
+			: base (null, null, null, null, null, l)
 		{
-			this.loc = loc;
+			expression = expression_or_block_body as Expression;
+			block = expression_or_block_body as Block;
+
+			if (expression == null && block == null)
+				throw new Exception ("Internal compiler error: only Expression or Block is allowed");
+
+			if (RootContext.Version < LanguageVersion.LINQ){
+				Report.FeatureRequiresLINQ (l, "lambda expressions");
+				return;
+			}
 		}
 
 		public override Expression DoResolve (EmitContext ec)
