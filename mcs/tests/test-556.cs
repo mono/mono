b1172574e838db626c84684f05549b56986705c0
Miguel de Icaza <miguel@gnome.org>
2007-02-02 17:58:06 +0000

    Add
    svn path=/trunk/mcs/; revision=72186

---
+++ mcs/tests/test-556.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+public class S 
+{
+	public void Frobnikator() 
+	{
+		const UInt32 SMALL_MASK = (1U << (24)) - 1; 
+		const ulong BIG_MASK = ~((ulong)SMALL_MASK);  
+		
+	}
+
+	static void Main ()
+	{
+	}	
+}
