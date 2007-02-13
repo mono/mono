33692f5f634ff731a349b20b869b70b761855939
Marek Safar <marek.safar@gmail.com>
2007-02-13 21:36:32 +0000

    Test based on #80418
    svn path=/trunk/mcs/; revision=72796

---
+++ mcs/tests/test-562.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("foo.dll")]
+    public static extern void printf(string format, __arglist);
+
+    static int Main()
+    {
+        if (typeof (Program).GetMethod ("printf").CallingConvention != CallingConventions.VarArgs)
+            return 1;
+        
+        Console.WriteLine ("OK");
+        return 0;
+    }
+}
