810bb583cdf396f29adf2096c49a95f604b1162a
Marek Safar <marek.safar@gmail.com>
2007-02-17 16:34:16 +0000

    Test based on bug #80408
    svn path=/trunk/mcs/; revision=73058

---
+++ mcs/tests/test-563.cs
@@ -0,0 +1,29 @@
+// Compiler options: -warn:4 -warnaserror
+
+using System;
+using System.Collections;
+
+public class Test
+{
+	public static object TestMethod (TestCollection t)
+	{
+		foreach (object x in t)
+		{
+			return x;
+		}
+		return null;
+	}
+
+	public static void Main ()
+	{
+	}
+}
+
+interface ITestCollection : IEnumerable
+{
+	new IEnumerator GetEnumerator ();
+}
+
+interface TestCollection : ITestCollection
+{
+}
