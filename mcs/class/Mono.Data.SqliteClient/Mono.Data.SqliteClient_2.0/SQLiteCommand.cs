471ef1cf1027ecaed382bcc666aab43301bdc5de
Miguel de Icaza <miguel@gnome.org>
2007-02-21 16:52:02 +0000

    Fix
    svn path=/trunk/mcs/; revision=73261

--- mcs/class/Mono.Data.SqliteClient/Mono.Data.SqliteClient_2.0/SQLiteCommand.cs
+++ mcs/class/Mono.Data.SqliteClient/Mono.Data.SqliteClient_2.0/SQLiteCommand.cs
@@ -648,12 +648,10 @@ namespace Mono.Data.SqliteClient
       return new SqliteCommand(this);
     }
 
-#if MONO_BACKWARD_COMPAT
     public int LastInsertRowID ()
     {
 	    return _cnn.LastInsertRowId;
     }
-#endif
   }
 }
 #endif
