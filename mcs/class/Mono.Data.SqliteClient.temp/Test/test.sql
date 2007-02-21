56102ccd783cd272e34dfeb61df5c7eac3fca529
Miguel de Icaza <miguel@gnome.org>
2007-02-21 22:06:32 +0000

    Move this out of the way
    svn path=/trunk/mcs/; revision=73281

--- mcs/class/Mono.Data.SqliteClient.temp/Test/test.sql
+++
@@ -1,8 +0,0 @@
-create table test (
-  id int NOT NULL PRIMARY KEY,
-  name varchar (20)
-);
-
-insert into test values (1, "mono test 1");
-insert into test values (2, "mono test 2");
-insert into test values (3, "mono test 3");
