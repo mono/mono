27b92038cd7ef1ccc2fde0604bc4a5ddbe4a189d
Raja R Harinath <harinath@hurrynot.org>
2007-02-23 08:52:48 +0000

    Fix compilation error * DataGridViewRowTest.cs (System.ComponentModel): Remove unused 'using'.
    svn path=/trunk/mcs/; revision=73357

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/DataGridViewRowTest.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/DataGridViewRowTest.cs
@@ -30,7 +30,6 @@ using NUnit.Framework;
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using System.ComponentModel;
 using System.Collections;
 
 namespace MonoTests.System.Windows.Forms {
