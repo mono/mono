3f0e7b3a250504b30e5c4975262b9f0c555ac3e9
Marek Safar <marek.safar@gmail.com>
2007-02-25 21:59:51 +0000

    New test (bug #80455)
    svn path=/trunk/mcs/; revision=73416

---
+++ mcs/tests/gtest-311.cs
@@ -0,0 +1,18 @@
+// Compiler options: -t:library
+
+using System;
+using System.Runtime.InteropServices;
+
+class A
+{
+	[DllImport ("Dingus", CharSet=CharSet.Auto)]
+	extern static void Do<T> ();
+
+	[DllImport ("Dingus")]
+	extern static void Do2<T> ();
+
+	static void Main ()
+	{
+
+	}
+}
