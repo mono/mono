95d62a8e3cc25da5fbcb3120951969dc59edd826
Sebastien Pouliot <sebastien@ximian.com>
2007-04-11 13:05:30 +0000

    2007-04-11  Sebastien Pouliot  <sebastien@ximian.com> 	* GDIPlusTest.cs: Add a check for locking (LockBits) a bitmap twice 	(without unlocking it first).
    svn path=/trunk/mcs/; revision=75607

--- mcs/class/System.Drawing/Test/System.Drawing/GDIPlusTest.cs
+++ mcs/class/System.Drawing/Test/System.Drawing/GDIPlusTest.cs
@@ -161,6 +161,8 @@ namespace MonoTests.System.Drawing {
 
 			Rectangle rect = new Rectangle (2, 2, 5, 5);
 			Assert.AreEqual (Status.Ok, GDIPlus.GdipBitmapLockBits (bmp, ref rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb, bd), "locked");
+			Assert.AreEqual (Status.Win32Error, GDIPlus.GdipBitmapLockBits (bmp, ref rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb, bd), "second lock");
+
 			Assert.AreEqual (rect.Width, bd.Width, "Width");
 			Assert.AreEqual (rect.Height, bd.Height, "Height");
 			Assert.AreEqual (PixelFormat.Format24bppRgb, bd.PixelFormat, "PixelFormat");
