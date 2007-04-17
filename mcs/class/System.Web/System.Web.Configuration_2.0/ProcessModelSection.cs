dc8dd3e2c270edd55f55090f75ff3a3f674ff1d7
Atsushi Eno <atsushieno@gmail.com>
2007-04-17 01:49:12 +0000

    2007-04-17  Atsushi Enomoto  <atsushi@ximian.com>
    	* ProcessModelSection.cs : cpuMask default value should be int,
    	  not uint.
    
    
    svn path=/trunk/mcs/; revision=75800

--- mcs/class/System.Web/System.Web.Configuration_2.0/ProcessModelSection.cs
+++ mcs/class/System.Web/System.Web.Configuration_2.0/ProcessModelSection.cs
@@ -84,7 +84,7 @@ namespace System.Web.Configuration {
 									       new GenericEnumConverter (typeof (ProcessModelComImpersonationLevel)),
 									       PropertyHelper.DefaultValidator,
 									       ConfigurationPropertyOptions.None);
-			cpuMaskProp = new ConfigurationProperty ("cpuMask", typeof (int), 0xffffffff);
+			cpuMaskProp = new ConfigurationProperty ("cpuMask", typeof (int), (int) (int.MaxValue & 0x80000000));
 			enableProp = new ConfigurationProperty ("enable", typeof (bool), true);
 			idleTimeoutProp = new ConfigurationProperty ("idleTimeout", typeof (TimeSpan), TimeSpan.MaxValue,
 								     PropertyHelper.InfiniteTimeSpanConverter,
@@ -222,7 +222,7 @@ namespace System.Web.Configuration {
 			set { base[comImpersonationLevelProp] = value; }
 		}
 
-		[ConfigurationProperty ("cpuMask", DefaultValue = "0xffffffff")]
+		[ConfigurationProperty ("cpuMask", DefaultValue = (int) (int.MaxValue & 0x80000000))]
 		public int CpuMask {
 			get { return (int) base [cpuMaskProp];}
 			set { base[cpuMaskProp] = value; }
