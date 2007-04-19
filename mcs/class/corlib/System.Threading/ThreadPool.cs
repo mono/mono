f84de0774a6ab3a9ca38933c411a06c3cb8686ee
Paolo Molaro <lupus@oddwiz.org>
2007-04-19 14:47:37 +0000

    Thu Apr 19 16:47:52 CEST 2007 Paolo Molaro <lupus@ximian.com>
    	* ThreadPool.cs: patch from Robert Jordan to implement
    	ThreadPool.SetMaxThreads.
    
    
    svn path=/trunk/mcs/; revision=75954

--- mcs/class/corlib/System.Threading/ThreadPool.cs
+++ mcs/class/corlib/System.Threading/ThreadPool.cs
@@ -75,10 +75,18 @@ namespace System.Threading {
 			
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void GetMinThreads (out int workerThreads, out int completionPortThreads);
-			
+
+		[MonoTODO("The min number of completion port threads is not evaluated.")]
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
 		public static extern bool SetMinThreads (int workerThreads, int completionPortThreads);
+
+#if NET_2_0
+		[MonoTODO("The max number of threads cannot be decremented.")]
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
+		public static extern bool SetMaxThreads (int workerThreads, int completionPortThreads);
+#endif
 			
 		public static bool QueueUserWorkItem (WaitCallback callback)
 		{
