29949267ac8a4df035a863b6d4dd75c4ab1694d9
Jonathan Pobst <monkey@jpobst.com>
2007-04-19 18:17:57 +0000

    2007-04-19  Jonathan Pobst  <monkey@jpobst.com>
    	* GroupBoxTest.cs: Add tests for default properties and DisplayRectangle.
    
    svn path=/trunk/mcs/; revision=75966

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/GroupBoxTest.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/GroupBoxTest.cs
@@ -19,6 +19,25 @@ namespace MonoTests.System.Windows.Forms
 	public class GroupBoxTest
 	{
 		[Test]
+		public void Constructor ()
+		{
+			GroupBox gb = new GroupBox ();
+
+			Assert.AreEqual (false, gb.AllowDrop, "A1");
+			// Top/Height are dependent on font height
+			// Assert.AreEqual (new Rectangle (3, 16, 194, 81), gb.DisplayRectangle, "A2");
+			Assert.AreEqual (FlatStyle.Standard, gb.FlatStyle, "A3");
+			Assert.AreEqual (false, gb.TabStop, "A4");
+			Assert.AreEqual (string.Empty, gb.Text, "A5");
+			
+#if NET_2_0
+			Assert.AreEqual (false, gb.AutoSize, "A6");
+			//Assert.AreEqual (AutoSizeMode.GrowOnly, gb.AutoSizeMode, "A7");
+			Assert.AreEqual (true, gb.UseCompatibleTextRendering, "A8");
+#endif
+		}
+		
+		[Test]
 		public void GroupBoxPropertyTest ()
 		{
 			Form myform = new Form ();
@@ -38,5 +57,27 @@ namespace MonoTests.System.Windows.Forms
 			Assert.AreEqual (FlatStyle.System, mygrpbox.FlatStyle, "#4");
 			myform.Dispose ();
 		}
+
+#if NET_2_0
+		[Test]
+		public void PropertyDisplayRectangle ()
+		{
+			GroupBox gb = new GroupBox ();
+			gb.Size = new Size (200, 200);
+			
+			Assert.AreEqual (new Padding (3), gb.Padding, "A0");
+			gb.Padding = new Padding (25, 25, 25, 25);
+
+			Assert.AreEqual (new Rectangle (0, 0, 200, 200), gb.ClientRectangle, "A1");
+
+			// Basically, we are testing that the DisplayRectangle includes
+			// Padding.  Top/Height are affected by font height, so we aren't
+			// using exact numbers.
+			Assert.AreEqual (25, gb.DisplayRectangle.Left, "A2");
+			Assert.AreEqual (150, gb.DisplayRectangle.Width, "A3");
+			Assert.IsTrue (gb.DisplayRectangle.Top > gb.Padding.Top, "A4");
+			Assert.IsTrue (gb.DisplayRectangle.Height < (gb.Height - gb.Padding.Vertical), "A5");
+		}
+#endif
 	}
 }
