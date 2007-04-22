e291e13d3a538d0e6a28282da69436a2d2c5a083
Gert Driesen <drieseng@users.sourceforge.net>
2007-04-22 16:01:17 +0000

    * CookieContainerTest.cs: Removed usage of deprecated Asssertion class. Added test for bug #81421.
    svn path=/trunk/mcs/; revision=76063

--- mcs/class/System/Test/System.Net/CookieContainerTest.cs
+++ mcs/class/System/Test/System.Net/CookieContainerTest.cs
@@ -7,232 +7,193 @@
 // (c) Copyright 2004 Novell, Inc. (http://www.novell.com)
 //
 
-using NUnit.Framework;
 using System;
 using System.Net;
 using System.Reflection;
 
+using NUnit.Framework;
+
 namespace MonoTests.System.Net
 {
 	[TestFixture]
-	public class CookieContainerTest : Assertion
+	public class CookieContainerTest
 	{
 		[Test]
 		public void TestCtor1 ()
 		{
 			CookieContainer c = new CookieContainer (234);
-			AssertEquals ("#01", 234, c.Capacity);
-			bool passed = false;
+			Assert.AreEqual (234, c.Capacity, "#1");
+
 			try {
 				new CookieContainer (0);
-				passed = false;
+				Assert.Fail ("#2");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#02", true, passed);
 
 			try {
 				new CookieContainer (-10);
-				passed = false;
+				Assert.Fail ("#3");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#03", true, passed);
 		}
 
 		[Test]
 		public void TestCtor3 ()
 		{
 			CookieContainer c = new CookieContainer (100, 50, 1000);
-			AssertEquals ("#01", 100, c.Capacity);
-			AssertEquals ("#02", 50, c.PerDomainCapacity);
-			AssertEquals ("#03", 1000, c.MaxCookieSize);
-			bool passed = false;
+			Assert.AreEqual (100, c.Capacity, "#1");
+			Assert.AreEqual (50, c.PerDomainCapacity, "#2");
+			Assert.AreEqual (1000, c.MaxCookieSize, "#3");
+
 			try {
 				new CookieContainer (100, 0, 1000);
-				passed = false;
+				Assert.Fail ("#4");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#04", true, passed);
 
 			try {
 				new CookieContainer (100, -1, 1000);
-				passed = false;
+				Assert.Fail ("#5");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#05", true, passed);
 
-			try {
-				new CookieContainer (100, Int32.MaxValue, 1000);
-				passed = true;
-			} catch (ArgumentException) {
-				passed = false;
-			}
-			AssertEquals ("#06", true, passed);
+			c = new CookieContainer (100, int.MaxValue, 1000);
+			Assert.AreEqual (int.MaxValue, c.PerDomainCapacity, "#6");
 
 			try {
 				new CookieContainer (100, 50, 0);
-				passed = false;
+				Assert.Fail ("#7");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#07", true, passed);
 
 			try {
 				new CookieContainer (100, 500, -4);
-				passed = false;
+				Assert.Fail ("#8");
 			} catch (ArgumentException) {
-				passed = true;
 			}
-			AssertEquals ("#08", true, passed);
 		}
-		
+
 		[Test]
 		public void TestDefaultLimits ()
 		{
-			AssertEquals ("#01", CookieContainer.DefaultCookieLengthLimit, 4096);
-			AssertEquals ("#02", CookieContainer.DefaultCookieLimit, 300);
-			AssertEquals ("#03", CookieContainer.DefaultPerDomainCookieLimit, 20);
+			Assert.AreEqual (4096, CookieContainer.DefaultCookieLengthLimit, "#1");
+			Assert.AreEqual (300, CookieContainer.DefaultCookieLimit, "#2");
+			Assert.AreEqual (20, CookieContainer.DefaultPerDomainCookieLimit, "#3");
 		}
 
 		[Test]
 		public void TestCapacity ()
 		{
 			CookieContainer c = new CookieContainer ();
-			AssertEquals ("#01", c.Capacity, 300);
+			Assert.AreEqual (300, c.Capacity, "#1");
 			c.Capacity = 200;
-			AssertEquals ("#02", c.Capacity, 200);
-			bool passed = false;
+			Assert.AreEqual (200, c.Capacity, "#2");
+
 			try {
 				c.Capacity = -5;
-				passed = false;
+				Assert.Fail ("#3");
 			} catch (ArgumentOutOfRangeException) {
-				passed = true;
 			}
-
-			AssertEquals ("#03", true, passed);
 
 			try {
 				c.Capacity = 5; // must be >= PerDomainCapacity if PerDomainCapacity != Int32.MaxValue
-				passed = false;
+				Assert.Fail ("#4");
 			} catch (ArgumentOutOfRangeException) {
-				passed = true;
 			}
-
-			AssertEquals ("#04", true, passed);
-			passed = false;
 		}
 
 		[Test]
 		public void TestMaxCookieSize ()
 		{
 			CookieContainer c = new CookieContainer ();
-			AssertEquals ("#01", c.MaxCookieSize, 4096);
-			bool passed = false;
+			Assert.AreEqual (4096, c.MaxCookieSize, "#1");
+
 			try {
 				c.MaxCookieSize = -5;
-				passed = false;
+				Assert.Fail ("#2");
 			} catch (ArgumentOutOfRangeException) {
-				passed = true;
 			}
 
-			AssertEquals ("#02", true, passed);
 			try {
 				c.MaxCookieSize = -1;
-				passed = false;
+				Assert.Fail ("#3");
 			} catch (ArgumentOutOfRangeException) {
-				passed = true;
 			}
 
-			AssertEquals ("#03", true, passed);
 			c.MaxCookieSize = 80000;
-			AssertEquals ("#04", 80000, c.MaxCookieSize);
-			c.MaxCookieSize = Int32.MaxValue;
-			AssertEquals ("#04", Int32.MaxValue, c.MaxCookieSize);
+			Assert.AreEqual (80000, c.MaxCookieSize, "#4");
+			c.MaxCookieSize = int.MaxValue;
+			Assert.AreEqual (int.MaxValue, c.MaxCookieSize, "#5");
 		}
 
 		[Test]
 		public void TestAdd_Args ()
 		{
 			CookieContainer cc = new CookieContainer ();
-			bool passed = false;
+
 			try {
 				cc.Add ((Cookie) null);
-				passed = false;
+				Assert.Fail ("#1");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#01", true, passed);
 
 			try {
 				cc.Add ((CookieCollection) null);
-				passed = false;
+				Assert.Fail ("#2");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#02", true, passed);
 
 			try {
 				cc.Add (null, (Cookie) null);
-				passed = false;
+				Assert.Fail ("#3");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#03", true, passed);
 
 			try {
 				cc.Add (null, (CookieCollection) null);
-				passed = false;
+				Assert.Fail ("#4");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#04", true, passed);
 
 			try {
 				cc.Add (new Uri ("http://www.contoso.com"), (Cookie) null);
-				passed = false;
+				Assert.Fail ("#5");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#05", true, passed);
 
 			try {
 				cc.Add (new Uri ("http://www.contoso.com"), (CookieCollection) null);
-				passed = false;
+				Assert.Fail ("#6");
 			} catch (ArgumentNullException) {
-				passed = true;
 			}
-			AssertEquals ("#06", true, passed);
 		}
 
 		[Test]
 		public void TestAdd_Cookie ()
 		{
 			CookieContainer cc = new CookieContainer ();
-			Uri uri = new Uri ("http://www.contoso.com"); 
+			Uri uri = new Uri ("http://www.contoso.com");
 			cc.Add (uri, new CookieCollection ());
 			DateTime timestamp = DateTime.Now;
 			cc.Add (uri, new Cookie ("hola", "Adios"));
 			CookieCollection coll = cc.GetCookies (uri);
 			Cookie cookie = coll [0];
-			AssertEquals ("#07", "", cookie.Comment);
-			AssertEquals ("#08", null, cookie.CommentUri);
-			AssertEquals ("#09", "www.contoso.com", cookie.Domain);
-			AssertEquals ("#10", false, cookie.Expired);
-			AssertEquals ("#11", DateTime.MinValue, cookie.Expires);
-			AssertEquals ("#12", "hola", cookie.Name);
-			AssertEquals ("#13", "/", cookie.Path);
-			AssertEquals ("#14", "", cookie.Port);
-			AssertEquals ("#15", false, cookie.Secure);
+			Assert.AreEqual ("", cookie.Comment, "#1");
+			Assert.IsNull (cookie.CommentUri, "#2");
+			Assert.AreEqual ("www.contoso.com", cookie.Domain, "#3");
+			Assert.IsFalse (cookie.Expired, "#4");
+			Assert.AreEqual (DateTime.MinValue, cookie.Expires, "#5");
+			Assert.AreEqual ("hola", cookie.Name, "#6");
+			Assert.AreEqual ("/", cookie.Path, "#7");
+			Assert.AreEqual ("", cookie.Port, "#8");
+			Assert.IsFalse (cookie.Secure, "#9");
 			// FIX the next test
 			TimeSpan ts = cookie.TimeStamp - timestamp;
 			if (ts.TotalMilliseconds > 500)
-				AssertEquals ("#16 timestamp", true, false);
+				Assert.Fail ("#10");
 
-			AssertEquals ("#17", "Adios", cookie.Value);
-			AssertEquals ("#18", 0, cookie.Version);
+			Assert.AreEqual ("Adios", cookie.Value, "#11");
+			Assert.AreEqual (0, cookie.Version, "#12");
 		}
 
 		[Test]
@@ -241,7 +202,7 @@ namespace MonoTests.System.Net
 			CookieContainer cc = new CookieContainer ();
 			try {
 				cc.GetCookies (null);
-				AssertEquals ("#01", true, false);
+				Assert.Fail ("#1");
 			} catch (ArgumentNullException) {
 			}
 		}
@@ -252,28 +213,71 @@ namespace MonoTests.System.Net
 			CookieContainer cc = new CookieContainer ();
 			try {
 				cc.SetCookies (null, "");
-				AssertEquals ("#01", true, false);
+				Assert.Fail ("#1");
 			} catch (ArgumentNullException) {
 			}
 
 			try {
 				cc.SetCookies (new Uri ("http://www.contoso.com"), null);
-				AssertEquals ("#02", true, false);
+				Assert.Fail ("#2");
 			} catch (ArgumentNullException) {
 			}
 
 			try {
 				cc.SetCookies (new Uri ("http://www.contoso.com"), "=lalala");
-				AssertEquals ("#03", true, false);
+				Assert.Fail ("#3");
 			} catch (CookieException) {
 			}
 
-			try {
-				cc.SetCookies (new Uri ("http://www.contoso.com"), "");
-			} catch (CookieException) {
-				AssertEquals ("#04", true, false);
-			}
+			cc.SetCookies (new Uri ("http://www.contoso.com"), "");
+		}
+
+		[Test] // bug #81421
+		[Category ("NotWorking")]
+		public void GetCookies ()
+		{
+			CookieContainer container = new CookieContainer ();
+			container.Add (new Cookie ("name", "value1", "/path", "localhost"));
+			container.Add (new Cookie ("name", "value2", "/path/sub", "localhost"));
+
+			CookieCollection cookies = container.GetCookies (
+				new Uri ("http://localhost/path/sub"));
+			Assert.IsNotNull (cookies, "#A1");
+			Assert.AreEqual (2, cookies.Count, "#A2");
+
+			Cookie cookie = cookies [0];
+			Assert.AreEqual ("name", cookie.Name, "#B1");
+			Assert.AreEqual ("value2", cookie.Value, "#B2");
+			Assert.AreEqual ("/path/sub", cookie.Path, "#B3");
+			Assert.AreEqual ("localhost", cookie.Domain, "#B4");
+
+			cookie = cookies [1];
+			Assert.AreEqual ("name", cookie.Name, "#C1");
+			Assert.AreEqual ("value1", cookie.Value, "#C2");
+			Assert.AreEqual ("/path", cookie.Path, "#C3");
+			Assert.AreEqual ("localhost", cookie.Domain, "#C4");
+
+			cookies = container.GetCookies (new Uri ("http://localhost/path"));
+			Assert.IsNotNull (cookies, "#D1");
+			Assert.AreEqual (1, cookies.Count, "#D2");
+
+			cookie = cookies [0];
+			Assert.AreEqual ("name", cookie.Name, "#E1");
+			Assert.AreEqual ("value1", cookie.Value, "#E2");
+			Assert.AreEqual ("/path", cookie.Path, "#E3");
+			Assert.AreEqual ("localhost", cookie.Domain, "#E4");
+
+			cookies = container.GetCookies (new Uri ("http://localhost/whatever"));
+			Assert.IsNotNull (cookies, "#F1");
+			Assert.AreEqual (0, cookies.Count, "#F2");
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void GetCookies_Uri_Null ()
+		{
+			CookieContainer container = new CookieContainer ();
+			container.GetCookies ((Uri) null);
 		}
 	}
 }
-
