3df4f578548cffc9ef11e01c57e521d5a3a01237
Gert Driesen <drieseng@users.sourceforge.net>
2007-05-11 21:41:09 +0000

    * DebuggerDisplayAttributeTest.cs: Make test forward compatible. Replace Assert.AreEqual null comparison with Assert.IsNull. Replaced "" with string.Empty.
    svn path=/trunk/mcs/; revision=77263

--- mcs/class/corlib/Test/System.Diagnostics/DebuggerDisplayAttributeTest.cs
+++ mcs/class/corlib/Test/System.Diagnostics/DebuggerDisplayAttributeTest.cs
@@ -28,18 +28,18 @@ namespace MonoTests.System.Diagnostics
 			DebuggerDisplayAttribute dda;
 
 			dda = new DebuggerDisplayAttribute (null);
-			Assert.AreEqual ("", dda.Value, "A1");
-			Assert.AreEqual (null,dda.Target, "A2");
-			Assert.AreEqual (null, dda.TargetTypeName, "A3");
-			Assert.AreEqual ("", dda.Type, "A4");
-			Assert.AreEqual ("", dda.Name, "A4");
+			Assert.AreEqual (string.Empty, dda.Value, "A1");
+			Assert.IsNull (dda.Target, "A2");
+			Assert.IsNull (dda.TargetTypeName, "A3");
+			Assert.AreEqual (string.Empty, dda.Type, "A4");
+			Assert.AreEqual (string.Empty, dda.Name, "A4");
 
 			dda = new DebuggerDisplayAttribute ("abc");
 			Assert.AreEqual ("abc", dda.Value, "B1");
-			Assert.AreEqual (null,dda.Target, "B2");
-			Assert.AreEqual (null, dda.TargetTypeName, "B3");
-			Assert.AreEqual ("", dda.Type, "B4");
-			Assert.AreEqual ("", dda.Name, "B4");
+			Assert.IsNull (dda.Target, "B2");
+			Assert.IsNull (dda.TargetTypeName, "B3");
+			Assert.AreEqual (string.Empty, dda.Type, "B4");
+			Assert.AreEqual (string.Empty, dda.Name, "B4");
 		}
 		
 		[Test]
@@ -48,26 +48,24 @@ namespace MonoTests.System.Diagnostics
 			DebuggerDisplayAttribute dda;
 
 			dda = new DebuggerDisplayAttribute (null);
-			Assert.AreEqual ("",dda.Value, "A1");
-			Assert.AreEqual (null,dda.Target, "A2");
-			Assert.AreEqual (null, dda.TargetTypeName, "A3");
-			Assert.AreEqual ("", dda.Type, "A4");
-			Assert.AreEqual ("", dda.Name, "A4");
+			Assert.AreEqual (string.Empty, dda.Value, "A1");
+			Assert.IsNull (dda.Target, "A2");
+			Assert.IsNull (dda.TargetTypeName, "A3");
+			Assert.AreEqual (string.Empty, dda.Type, "A4");
+			Assert.AreEqual (string.Empty, dda.Name, "A4");
 
 			dda.Target = typeof(string);
-			Assert.AreEqual ("",dda.Value, "B1");
+			Assert.AreEqual (string.Empty, dda.Value, "B1");
 			Assert.IsNotNull (dda.Target, "B2");
 			Assert.AreSame (typeof(string), dda.Target, "B2-b");
-			Assert.AreEqual ("System.String, mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", dda.TargetTypeName, "B3");
-			Assert.AreEqual ("", dda.Type, "B4");
-			Assert.AreEqual ("", dda.Name, "B4");
+			Assert.AreEqual (typeof (string).AssemblyQualifiedName, dda.TargetTypeName, "B3");
+			Assert.AreEqual (string.Empty, dda.Type, "B4");
+			Assert.AreEqual (string.Empty, dda.Name, "B4");
 
 			try {
 				dda.Target = null;
 				Assert.Fail ("Excepted ArgumentNullException, got no exception.");
 			} catch (ArgumentNullException) {
-			} catch (Exception exception) {
-				Assert.Fail ("Excepted ArgumentNullException, got " + exception.GetType ().Name + ".");
 			}
 		}
 
@@ -77,32 +75,32 @@ namespace MonoTests.System.Diagnostics
 			DebuggerDisplayAttribute dda;
 
 			dda = new DebuggerDisplayAttribute (null);
-			Assert.AreEqual ("",dda.Value, "A1");
-			Assert.AreEqual (null,dda.Target, "A2");
-			Assert.AreEqual (null, dda.TargetTypeName, "A3");
-			Assert.AreEqual ("", dda.Type, "A4");
-			Assert.AreEqual ("", dda.Name, "A4");
+			Assert.AreEqual (string.Empty, dda.Value, "A1");
+			Assert.IsNull (dda.Target, "A2");
+			Assert.IsNull (dda.TargetTypeName, "A3");
+			Assert.AreEqual (string.Empty, dda.Type, "A4");
+			Assert.AreEqual (string.Empty, dda.Name, "A4");
 
 			dda.TargetTypeName = "System.String";
-			Assert.AreEqual ("",dda.Value, "B1");
-			Assert.AreEqual (null,dda.Target, "B2");
+			Assert.AreEqual (string.Empty, dda.Value, "B1");
+			Assert.IsNull (dda.Target, "B2");
 			Assert.AreEqual ("System.String", dda.TargetTypeName, "B3");
-			Assert.AreEqual ("", dda.Type, "B4");
-			Assert.AreEqual ("", dda.Name, "B4");
+			Assert.AreEqual (string.Empty, dda.Type, "B4");
+			Assert.AreEqual (string.Empty, dda.Name, "B4");
 
 			dda.TargetTypeName = null;
-			Assert.AreEqual ("",dda.Value, "C1");
-			Assert.AreEqual (null,dda.Target, "C2");
-			Assert.AreEqual (null, dda.TargetTypeName, "C3");
-			Assert.AreEqual ("", dda.Type, "C4");
-			Assert.AreEqual ("", dda.Name, "C4");
+			Assert.AreEqual (string.Empty, dda.Value, "C1");
+			Assert.IsNull (dda.Target, "C2");
+			Assert.IsNull (dda.TargetTypeName, "C3");
+			Assert.AreEqual (string.Empty, dda.Type, "C4");
+			Assert.AreEqual (string.Empty, dda.Name, "C4");
 
-			dda.TargetTypeName = "";
-			Assert.AreEqual ("",dda.Value, "D1");
-			Assert.AreEqual (null,dda.Target, "D2");
-			Assert.AreEqual ("", dda.TargetTypeName, "D3");
-			Assert.AreEqual ("", dda.Type, "D4");
-			Assert.AreEqual ("", dda.Name, "D4");
+			dda.TargetTypeName = string.Empty;
+			Assert.AreEqual (string.Empty, dda.Value, "D1");
+			Assert.IsNull (dda.Target, "D2");
+			Assert.AreEqual (string.Empty, dda.TargetTypeName, "D3");
+			Assert.AreEqual (string.Empty, dda.Type, "D4");
+			Assert.AreEqual (string.Empty, dda.Name, "D4");
 		}
 
 		[Test]
@@ -111,32 +109,32 @@ namespace MonoTests.System.Diagnostics
 			DebuggerDisplayAttribute dda;
 
 			dda = new DebuggerDisplayAttribute (null);
-			Assert.AreEqual ("", dda.Value, "A1");
-			Assert.AreEqual (null, dda.Target, "A2");
-			Assert.AreEqual (null, dda.TargetTypeName, "A3");
-			Assert.AreEqual ("", dda.Type, "A4");
-			Assert.AreEqual ("", dda.Name, "A4");
+			Assert.AreEqual (string.Empty, dda.Value, "A1");
+			Assert.IsNull (dda.Target, "A2");
+			Assert.IsNull (dda.TargetTypeName, "A3");
+			Assert.AreEqual (string.Empty, dda.Type, "A4");
+			Assert.AreEqual (string.Empty, dda.Name, "A4");
 
 			dda.Type = "System.String";
-			Assert.AreEqual ("", dda.Value, "B1");
-			Assert.AreEqual (null, dda.Target, "B2");
-			Assert.AreEqual (null, dda.TargetTypeName, "B3");
+			Assert.AreEqual (string.Empty, dda.Value, "B1");
+			Assert.IsNull (dda.Target, "B2");
+			Assert.IsNull (dda.TargetTypeName, "B3");
 			Assert.AreEqual ("System.String", dda.Type, "B4");
-			Assert.AreEqual ("", dda.Name, "B4");
+			Assert.AreEqual (string.Empty, dda.Name, "B4");
 
 			dda.Type = null;
-			Assert.AreEqual ("", dda.Value, "C1");
-			Assert.AreEqual (null, dda.Target, "C2");
-			Assert.AreEqual (null, dda.TargetTypeName, "C3");
-			Assert.AreEqual (null, dda.Type, "C4");
-			Assert.AreEqual ("", dda.Name, "C4");
+			Assert.AreEqual (string.Empty, dda.Value, "C1");
+			Assert.IsNull (dda.Target, "C2");
+			Assert.IsNull (dda.TargetTypeName, "C3");
+			Assert.IsNull (dda.Type, "C4");
+			Assert.AreEqual (string.Empty, dda.Name, "C4");
 
-			dda.Type = "";
-			Assert.AreEqual ("", dda.Value, "D1");
-			Assert.AreEqual (null, dda.Target, "D2");
-			Assert.AreEqual (null, dda.TargetTypeName, "D3");
-			Assert.AreEqual ("", dda.Type, "D4");
-			Assert.AreEqual ("", dda.Name, "D4");
+			dda.Type = string.Empty;
+			Assert.AreEqual (string.Empty, dda.Value, "D1");
+			Assert.IsNull (dda.Target, "D2");
+			Assert.IsNull (dda.TargetTypeName, "D3");
+			Assert.AreEqual (string.Empty, dda.Type, "D4");
+			Assert.AreEqual (string.Empty, dda.Name, "D4");
 		}
 
 		[Test]
@@ -145,32 +143,32 @@ namespace MonoTests.System.Diagnostics
 			DebuggerDisplayAttribute dda;
 
 			dda = new DebuggerDisplayAttribute (null);
-			Assert.AreEqual ("", dda.Value, "A1");
-			Assert.AreEqual (null, dda.Target, "A2");
-			Assert.AreEqual (null, dda.TargetTypeName, "A3");
-			Assert.AreEqual ("", dda.Type, "A4");
-			Assert.AreEqual ("", dda.Name, "A4");
+			Assert.AreEqual (string.Empty, dda.Value, "A1");
+			Assert.IsNull (dda.Target, "A2");
+			Assert.IsNull (dda.TargetTypeName, "A3");
+			Assert.AreEqual (string.Empty, dda.Type, "A4");
+			Assert.AreEqual (string.Empty, dda.Name, "A4");
 
 			dda.Name = "who?";
-			Assert.AreEqual ("", dda.Value, "B1");
-			Assert.AreEqual (null, dda.Target, "B2");
-			Assert.AreEqual (null, dda.TargetTypeName, "B3");
-			Assert.AreEqual ("", dda.Type, "B4");
+			Assert.AreEqual (string.Empty, dda.Value, "B1");
+			Assert.IsNull (dda.Target, "B2");
+			Assert.IsNull (dda.TargetTypeName, "B3");
+			Assert.AreEqual (string.Empty, dda.Type, "B4");
 			Assert.AreEqual ("who?", dda.Name, "B4");
 
 			dda.Name = null;
-			Assert.AreEqual ("", dda.Value, "C1");
-			Assert.AreEqual (null, dda.Target, "C2");
-			Assert.AreEqual (null, dda.TargetTypeName, "C3");
-			Assert.AreEqual ("", dda.Type, "C4");
-			Assert.AreEqual (null, dda.Name, "C4");
+			Assert.AreEqual (string.Empty, dda.Value, "C1");
+			Assert.IsNull (dda.Target, "C2");
+			Assert.IsNull (dda.TargetTypeName, "C3");
+			Assert.AreEqual (string.Empty, dda.Type, "C4");
+			Assert.IsNull (dda.Name, "C4");
 
-			dda.Name = "";
-			Assert.AreEqual ("", dda.Value, "D1");
-			Assert.AreEqual (null, dda.Target, "D2");
-			Assert.AreEqual (null, dda.TargetTypeName, "D3");
-			Assert.AreEqual ("", dda.Type, "D4");
-			Assert.AreEqual ("", dda.Name, "D4");
+			dda.Name = string.Empty;
+			Assert.AreEqual (string.Empty, dda.Value, "D1");
+			Assert.IsNull (dda.Target, "D2");
+			Assert.IsNull (dda.TargetTypeName, "D3");
+			Assert.AreEqual (string.Empty, dda.Type, "D4");
+			Assert.AreEqual (string.Empty, dda.Name, "D4");
 		}
 	}
 }
