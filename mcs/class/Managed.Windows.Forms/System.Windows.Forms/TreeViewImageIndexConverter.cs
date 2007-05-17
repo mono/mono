ee8ebaa192f964164f10f4ccb9a83f15f57c61fc
Jonathan Pobst <monkey@jpobst.com>
2007-05-17 15:08:33 +0000

    2007-05-17  Jonathan Pobst  <monkey@jpobst.com>
    	* TreeViewImageIndexConverter.cs: Add 2.0 methods.
    
    2007-05-17  Jonathan Pobst  <monkey@jpobst.com>
    
    	* System.Windows.Forms_test.dll.sources: Add TreeViewImageIndexConverterTest.cs.
    
    2007-05-17  Jonathan Pobst  <monkey@jpobst.com>
    
    	* TreeViewImageIndexConverterTest.cs: Added.
    
    svn path=/trunk/mcs/; revision=77568

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeViewImageIndexConverter.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeViewImageIndexConverter.cs
@@ -26,6 +26,7 @@
 using System;
 using System.Drawing;
 using System.Collections;
+using System.ComponentModel;
 
 
 namespace System.Windows.Forms {
@@ -36,10 +37,55 @@ namespace System.Windows.Forms {
 		{
 		}
 
+		#region Protected Properties
 		protected override bool IncludeNoneAsStandardValue {
 			get { return false; }
 		}
-	}
+		#endregion
 
+		#region Public Methods
+#if NET_2_0
+		public override object ConvertFrom (System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+		{
+			string indexStr;
+			
+			if (value != null && value is string) {
+				indexStr = (string)value;
+
+				if (indexStr.Equals ("(default)", StringComparison.InvariantCultureIgnoreCase))
+					return -1;
+				else if (indexStr.Equals ("(none)", StringComparison.InvariantCultureIgnoreCase))
+					return -2;
+					
+				return Int32.Parse (indexStr);
+			} else
+				return base.ConvertFrom (context, culture, value);
+		}
+
+		public override object ConvertTo (System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+		{
+			if (destinationType == typeof (string)) {
+				if (value == null)
+					return string.Empty;
+				else if (value is int && (int)value == -1)
+					return "(default)";
+				else if (value is int && (int)value == -2)
+					return "(none)";
+				else if (value is string && ((string)value).Length == 0)
+					return string.Empty;
+				else
+					return value.ToString ();
+			} else
+				return base.ConvertTo (context, culture, value, destinationType);
+		}
+
+		public override StandardValuesCollection GetStandardValues (System.ComponentModel.ITypeDescriptorContext context)
+		{
+			int[] stdVal = new int[] { -1, -2 };
+			return new TypeConverter.StandardValuesCollection (stdVal);
+		}
+#endif
+		#endregion
+	}
 }
 
