10d729d210753845c1c53dc0743c671fe3a51684
Nagappan Alagappan <nagappan@gmail.com>
2007-05-25 09:14:01 +0000

    2007-05-25  Nagappan A  <anagappan@novell.com>
    	* OdbcDataReader.cs (GetValue): Sybase returns the fraction values
    	in nano seconds, but DateTime expects the value in milliseconds,
    	so the conversion.
    
    
    svn path=/trunk/mcs/; revision=77937

--- mcs/class/System.Data/System.Data.Odbc/OdbcDataReader.cs
+++ mcs/class/System.Data/System.Data.Odbc/OdbcDataReader.cs
@@ -726,9 +726,14 @@ namespace System.Data.Odbc
 				case OdbcType.Time:
 					OdbcTimestamp ts_data = new OdbcTimestamp();
 					ret = libodbc.SQLGetData (hstmt, ColIndex, col.SqlCType, ref ts_data, 0, ref outsize);
-					if (outsize!=-1) // This means SQL_NULL_DATA 
+					if (outsize!=-1) {// This means SQL_NULL_DATA
+						/* Sybase returns the fraction values in nano seconds,
+						   but DateTime expects the value in milliseconds,
+						   so the conversion. */
+						int fraction = Convert.ToInt32 (ts_data.fraction > 999 ? ts_data.fraction / 1000000 : ts_data.fraction);
 						DataValue = new DateTime(ts_data.year,ts_data.month,ts_data.day,ts_data.hour,
-								       ts_data.minute,ts_data.second,Convert.ToInt32(ts_data.fraction));
+								       ts_data.minute,ts_data.second, fraction);
+					}
 					break;
 				case OdbcType.VarBinary :
 				case OdbcType.Image :
