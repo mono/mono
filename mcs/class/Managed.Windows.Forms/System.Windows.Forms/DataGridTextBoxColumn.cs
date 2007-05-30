9d6c8d2fd0dde74421a687a0ca9ff2d754f7d348
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-05-30 05:13:40 +0000

    * DataGridTextBoxColumn.cs: Fix texbox position to prevent override grid 	lines. Fixes #80285.
    2007-05-29  Everaldo Canuto  <ecanuto@novell.com>
    
    
    svn path=/trunk/mcs/; revision=78168

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridTextBoxColumn.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridTextBoxColumn.cs
@@ -185,8 +185,7 @@ namespace System.Windows.Forms
 			}
 
 			textbox.ReadOnly = ro;
-			textbox.Bounds = new Rectangle (new Point (bounds.X + offset_x, bounds.Y + offset_y),
-							new Size (bounds.Width - offset_x, bounds.Height - offset_y));
+			textbox.Bounds = new Rectangle (bounds.X, bounds.Y, bounds.Width - 1, bounds.Height - 1);
 			textbox.Visible = cellIsVisible;
 			textbox.Focus ();
 			textbox.SelectAll ();
