7dfabf0f4f3984bad07d88b1b3fb401208f24af2
Marek Sieradzki <msierad@mono-cvs.ximian.com>
2007-05-30 16:22:55 +0000

    2007-05-30  Marek Sieradzki  <marek.sieradzki@gmail.com>
            * standalone/projref: Added two projects where one project references
            library.
    
    
    svn path=/trunk/mcs/; revision=78232

---
+++ mcs/tools/xbuild/tests/standalone/projref/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Library.Class1.HelloWorld());
+        }
+    }
+}
---
+++ mcs/tools/xbuild/tests/standalone/projref/Library/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Class1
+    {
+        public static string HelloWorld()
+        {
+            return "Hello world!";
+        }
+    }
+}
