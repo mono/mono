19bd87dacbcc4944006b31a39e0df800f6d7786a
Jonathan Pobst <monkey@jpobst.com>
2007-06-04 20:00:44 +0000

    2007-06-04  Jonathan Pobst  <monkey@jpobst.com>
    	* Button.cs: Completely reformat and a little refactor to bring
    	this closer to Mono circa 2007.
    
    svn path=/trunk/mcs/; revision=78587

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/Button.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/Button.cs
@@ -42,19 +42,130 @@ namespace System.Windows.Forms {
 		#endregion	// Local variables
 
 		#region Public Constructors
-		public Button() {
+		public Button ()
+		{
 			dialog_result = DialogResult.None;
-			SetStyle(ControlStyles.StandardDoubleClick, false);
+			SetStyle (ControlStyles.StandardDoubleClick, false);
 		}
 		#endregion	// Public Constructors
 
-		#region	Internal methods
-		internal override void HaveDoubleClick() {
-			EventHandler eh = (EventHandler)(Events [DoubleClickEvent]);
-			if (eh != null)
-				eh (this, EventArgs.Empty);
+		#region Public Properties
+#if NET_2_0
+		[Localizable (true)]
+		public AutoSizeMode AutoSizeMode {
+			get { return base.GetAutoSizeMode (); }
+			set { base.SetAutoSizeMode (value); }
+		}
+#endif
+
+		[DefaultValue (DialogResult.None)]
+		public virtual DialogResult DialogResult {	// IButtonControl
+			get { return dialog_result; }
+			set { dialog_result = value; }
+		}
+		#endregion	// Public Properties
+
+		#region Protected Properties
+		protected override CreateParams CreateParams {
+			get { return base.CreateParams; }
+		}
+		#endregion	// Protected Properties
+
+		#region Public Methods
+		public virtual void NotifyDefault (bool value)	// IButtonControl
+		{	
+			this.IsDefault = value;
+		}
+
+		public void PerformClick ()			// IButtonControl
+		{			
+			if (CanSelect)
+				OnClick (EventArgs.Empty);
+		}
+
+		public override string ToString ()
+		{
+			return base.ToString () + ", Text: " + this.Text;
+		}
+		#endregion	// Public Methods
+
+		#region	Protected Methods
+		protected override void OnClick (EventArgs e)
+		{
+			if (dialog_result != DialogResult.None) {
+				Form p = FindForm ();
+
+				if (p != null)
+					p.DialogResult = dialog_result;
+			}
+			
+			base.OnClick (e);
+		}
+
+#if NET_2_0
+		protected override void OnFontChanged (EventArgs e)
+		{
+			base.OnFontChanged (e);
 		}
 		
+		protected override void OnMouseEnter (EventArgs e)
+		{
+			base.OnMouseEnter (e);
+		}
+		
+		protected override void OnMouseLeave (EventArgs e)
+		{
+			base.OnMouseLeave (e);
+		}
+#endif
+
+		protected override void OnMouseUp (MouseEventArgs e)
+		{
+			base.OnMouseUp (e);
+		}
+
+#if NET_2_0
+		protected override void OnTextChanged (EventArgs e)
+		{
+			base.OnTextChanged (e);
+		}
+#endif
+
+		protected override bool ProcessMnemonic (char charCode)
+		{
+			if (this.UseMnemonic && IsMnemonic (charCode, Text) == true) {
+				PerformClick ();
+				return true;
+			}
+
+			return base.ProcessMnemonic (charCode);
+		}
+
+		protected override void WndProc (ref Message m)
+		{
+			base.WndProc (ref m);
+		}
+		#endregion	// Protected Methods
+
+		#region Events
+		[Browsable (false)]
+		[EditorBrowsable (EditorBrowsableState.Advanced)]
+		public new event EventHandler DoubleClick {
+			add { base.DoubleClick += value; }
+			remove { base.DoubleClick -= value; }
+		}
+
+#if NET_2_0
+		[Browsable (false)]
+		[EditorBrowsable (EditorBrowsableState.Advanced)]
+		public new event MouseEventHandler MouseDoubleClick {
+			add { base.MouseDoubleClick += value; }
+			remove { base.MouseDoubleClick -= value; }
+		}
+#endif
+		#endregion	// Events
+
+		#region	Internal methods
 #if NET_2_0
 		internal override void Draw (PaintEventArgs pevent)
 		{
@@ -83,116 +194,5 @@ namespace System.Windows.Forms {
 		}
 #endif
 		#endregion	// Internal methods
-
-		#region Public Instance Properties
-		[DefaultValue(DialogResult.None)]
-		public virtual DialogResult DialogResult {		// IButtonControl
-			get {
-				return dialog_result;
-			}
-			set {
-				dialog_result = value;
-			}
-		}
-		#endregion	// Public Instance Properties
-
-		#region Protected Instance Properties
-		protected override CreateParams CreateParams {
-			get {
-				return base.CreateParams;
-			}
-		}
-		#endregion	// Protected Instance Properties
-
-		#region Public Instance Methods
-		public virtual void NotifyDefault(bool value) {	// IButtonControl
-			this.IsDefault = value;
-		}
-
-		public void PerformClick() {			// IButtonControl
-			if (CanSelect)
-				OnClick(EventArgs.Empty);
-		}
-
-		public override string ToString() {
-			return base.ToString() + ", Text: " + this.Text;
-		}
-		#endregion	// Public Instance Methods
-
-		#region	Protected Instance Methods
-		protected override void OnClick(EventArgs e) {
-			if (dialog_result != DialogResult.None) {
-				Form p = FindForm ();
-
-				if (p != null) {
-					p.DialogResult = dialog_result;
-				}
-			}
-			base.OnClick(e);
-		}
-
-		protected override void OnMouseUp(MouseEventArgs e) {
-			base.OnMouseUp (e);
-		}
-
-		protected override bool ProcessMnemonic(char charCode) {
-			if (this.UseMnemonic && IsMnemonic(charCode, Text) == true) {
-				PerformClick();
-				return true;
-			}
-			
-			return base.ProcessMnemonic(charCode);
-		}
-
-		protected override void WndProc(ref Message m) {
-			base.WndProc (ref m);
-		}
-		#endregion	// Protected Instance Methods
-
-		#region Events
-		static object DoubleClickEvent = new object ();
-
-		// XXX this and HaveDoubleClick above need to be
-		// looked at.. it's far more likely that this just
-		// uses base.DoubleClick, and HaveDoubleClick goes
-		// away in favor of calls to OnDoubleClick.
-		[Browsable(false)]
-		[EditorBrowsable (EditorBrowsableState.Advanced)]
-		public new event EventHandler DoubleClick {
-			add { Events.AddHandler (DoubleClickEvent, value); }
-			remove { Events.RemoveHandler (DoubleClickEvent, value); }
-		}
-		
-#if NET_2_0
-		[Browsable (false)]
-		[EditorBrowsable (EditorBrowsableState.Advanced)]
-		public new event MouseEventHandler MouseDoubleClick {
-			add { base.MouseDoubleClick += value; }
-			remove { base.MouseDoubleClick -= value; }
-		}
-#endif
-		#endregion	// Events
-		
-#if NET_2_0
-		protected override void OnFontChanged (EventArgs e)
-		{
-			base.OnFontChanged (e);
-		}
-		
-		protected override void OnMouseEnter (EventArgs e)
-		{
-			base.OnMouseEnter (e);
-		}
-		
-		protected override void OnMouseLeave (EventArgs e)
-		{
-			base.OnMouseLeave (e);
-		}
-
-		protected override void OnTextChanged (EventArgs e)
-		{
-			base.OnTextChanged (e);
-		}
-#endif
 	}
 }
