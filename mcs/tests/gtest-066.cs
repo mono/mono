2cd27ddde97a5a900e3a33f0ddb2d4831d75eae0
Marek Safar <marek.safar@gmail.com>
2007-06-15 21:58:13 +0000

    More tests.
    svn path=/trunk/mcs/; revision=79779

--- mcs/tests/gtest-066.cs
+++ mcs/tests/gtest-066.cs
@@ -1,7 +1,16 @@
 using System;
+using A = Test;
 
 public class Foo<T>
 {
+	public class Bar <U>
+	{
+	}
+}
+
+namespace Test
+{
+	class FooEx<V, W> {}
 }
 
 class X
@@ -9,5 +18,8 @@ class X
 	static void Main ()
 	{
 		Console.WriteLine (typeof (Foo<>));
+//		Console.WriteLine (typeof (Foo<>.Bar<>));
+		Console.WriteLine (typeof (Test.FooEx<,>));		
+		Console.WriteLine (typeof (A::FooEx<,>));
 	}
 }
