f0473bc9f1367c60b3bba8b1c302a805e61860c5
Igor Zelmanovich <igorz@mono-cvs.ximian.com>
2007-06-24 12:26:56 +0000

    Initial ScriptComponentDescriptor implementation
    svn path=/trunk/mcs/; revision=80621

--- mcs/class/System.Web.Extensions/System.Web.UI/ScriptComponentDescriptor.cs
+++ mcs/class/System.Web.Extensions/System.Web.UI/ScriptComponentDescriptor.cs
@@ -30,61 +30,121 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web.Script.Serialization;
 
 namespace System.Web.UI
 {
 	public class ScriptComponentDescriptor : ScriptDescriptor
 	{
+		string _type;
+		string _id;
+		Dictionary<string, string> _properties;
+		Dictionary<string, string> _events;
+		Dictionary<string, string> _references;
+
 		public ScriptComponentDescriptor (string type) {
-			throw new NotImplementedException ();
+			if (String.IsNullOrEmpty (type))
+				throw new ArgumentException ("Value cannot be null or empty.", "type");
+			_type = type;
 		}
 
 		public virtual string ClientID {
 			get {
-				throw new NotImplementedException ();
+				return ID;
 			}
 		}
 
 		public virtual string ID {
 			get {
-				throw new NotImplementedException ();
+				if (_id == null)
+					return String.Empty;
+				return _id;
 			}
 			set {
-				throw new NotImplementedException ();
+				_id = value;
 			}
 		}
 
 		public string Type {
 			get {
-				throw new NotImplementedException ();
+				return _type;
 			}
 			set {
-				throw new NotImplementedException ();
+				if (String.IsNullOrEmpty (value))
+					throw new ArgumentException ("Value cannot be null or empty.", "value");
+				_type = value;
 			}
 		}
 
 		public void AddComponentProperty (string name, string componentID) {
-			throw new NotImplementedException ();
+			if (name == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "name");
+			if (componentID == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "componentID");
+
+			AddEntry (ref _references, String.Format ("\"{0}\"", name), String.Format ("\"{0}\"", componentID));
 		}
 
 		public void AddElementProperty (string name, string elementID) {
-			throw new NotImplementedException ();
+			if (name == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "name");
+			if (elementID == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "elementID");
+
+			AddEntry (ref _properties, String.Format ("\"{0}\"", name), String.Format ("$get(\"{0}\")", elementID));
 		}
 
 		public void AddEvent (string name, string handler) {
-			throw new NotImplementedException ();
+			if (name == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "name");
+			if (handler == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "handler");
+
+			AddEntry (ref _events, String.Format ("\"{0}\"", name), handler);
 		}
 
 		public void AddProperty (string name, object value) {
-			throw new NotImplementedException ();
+			if (name == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "name");
+
+			string valueString;
+			if (value == null)
+				valueString = "null";
+			else
+				valueString = JavaScriptSerializer.DefaultSerializer.Serialize (value);
+
+			AddEntry (ref _properties, String.Format ("\"{0}\"", name), valueString);
 		}
 
 		public void AddScriptProperty (string name, string script) {
-			throw new NotImplementedException ();
+			if (name == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "name");
+			if (script == null)
+				throw new ArgumentException ("Value cannot be null or empty.", "script");
+
+			AddEntry (ref _properties, String.Format ("\"{0}\"", name), script);
+		}
+
+		void AddEntry (ref Dictionary<string, string> dictionary, string key, string value) {
+			if (dictionary == null)
+				dictionary = new Dictionary<string, string> ();
+			dictionary.Add (key, value);
 		}
 
 		protected internal override string GetScript () {
-			throw new NotImplementedException ();
+			return String.Format ("$create({0}, {1}, {2}, {3});", Type, SerializeDictionary (_properties), SerializeDictionary (_events), SerializeDictionary (_references));
+		}
+
+		string SerializeDictionary (Dictionary<string, string> dictionary) {
+			if (dictionary == null || dictionary.Count == 0)
+				return "null";
+			StringBuilder sb = new StringBuilder ("{");
+			foreach (string key in dictionary.Keys) {
+				sb.AppendFormat ("{0}:{1},", key, dictionary [key]);
+			}
+			sb.Length--;
+			sb.Append ("}");
+			return sb.ToString ();
 		}
 	}
 }
