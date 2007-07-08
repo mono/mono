78306b2c7add2a4d7140095e9e31c6d4b315f866
Gert Driesen <drieseng@users.sourceforge.net>
2007-07-08 11:51:48 +0000

    * TableLayoutTest.cs: Added test for bug #82040.
    svn path=/trunk/mcs/; revision=81590

--- mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/TableLayoutTest.cs
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/TableLayoutTest.cs
@@ -1087,6 +1087,31 @@ namespace MonoTests.System.Windows.Forms
 
 			f.Dispose ();
 		}
+
+		[Test] // bug #82040
+		[Category ("NotWorking")]
+		public void ShowNoChildren ()
+		{
+			Form form = new Form ();
+			form.ShowInTaskbar = false;
+
+			TableLayoutPanel tableLayoutPanel = new TableLayoutPanel ();
+			tableLayoutPanel.ColumnCount = 3;
+			tableLayoutPanel.Dock = DockStyle.Fill;
+			tableLayoutPanel.RowCount = 11;
+			form.Controls.Add (tableLayoutPanel);
+
+			Timer timer = new Timer ();
+			timer.Interval = 100;
+			timer.Tick += delegate (object sender, EventArgs e) {
+				form.Close ();
+			};
+
+			form.Load += delegate (object sender, EventArgs e) {
+				timer.Enabled = true;
+			};
+			form.ShowDialog ();
+		}
 	}
 }
 #endif
