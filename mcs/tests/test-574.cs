37917cf5906e511b9a78aec9c104d168a3117f00
Marek Safar <marek.safar@gmail.com>
2007-07-13 13:08:28 +0000

    Test based on #82064 (Miguel's commit)
    svn path=/trunk/mcs/; revision=81942

---
+++ mcs/tests/test-574.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+
+namespace XmlnsAttributeSample
+{
+    public class XmlnsAttribTester
+    {
+        static public void Main (string [] args)
+        {
+            using (StringWriter stringWriter = new StringWriter ())
+            {
+            }
+        }
+    }
+}
