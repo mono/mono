77e68791ddfb53865aabf96bd962f4373836639a
Marek Safar <marek.safar@gmail.com>
2007-07-13 14:13:06 +0000

    2007-07-13  Marek Safar  <marek.safar@gmail.com>
    	* ecore.cs (FieldExpr.AddressOf): Less confusing warning message.
    
    
    svn path=/trunk/mcs/; revision=81946

--- mcs/mcs/ecore.cs
+++ mcs/mcs/ecore.cs
@@ -4370,7 +4370,7 @@ namespace Mono.CSharp {
 			FieldBase f = TypeManager.GetField (FieldInfo);
 			if (f != null){
 				if ((f.ModFlags & Modifiers.VOLATILE) != 0){
-					Report.Warning (420, 1, loc, "`{0}': A volatile fields passed using a ref or out parameter might be problematic: routine does not know about volatile",
+					Report.Warning (420, 1, loc, "`{0}': A volatile field references will not be treated as volatile", 
 							f.GetSignatureForError ());
 				}
 					
