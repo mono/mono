a894b72c6e8646c220e106764361d809180ba2a4
Marek Safar <marek.safar@gmail.com>
2007-07-13 10:28:17 +0000

    re-styled
    svn path=/trunk/mcs/; revision=81936

--- mcs/tests/ltest-3.cs
+++ mcs/tests/ltest-3.cs
@@ -1,13 +1,19 @@
 // Compiler options: -langversion:linq
 
 using System;
+
 public delegate TResult Func<TArg0, TResult> (TArg0 arg0);
-class Demo {
-	static Y F<X,Y>(int a, X value, Func<X,Y> f1) {
-		return f1(value);
+
+class Demo
+{
+	static Y F<X, Y> (int a, X value, Func<X, Y> f1)
+	{
+		return f1 (value);
 	}
 	static int Main ()
-	{ object o = F(1, "1:15:30", s => TimeSpan.Parse(s)); 
-	Console.WriteLine (o);
-return 0;}
+	{
+		object o = F (1, "1:15:30", s => TimeSpan.Parse (s));
+		Console.WriteLine (o);
+		return 0;
+	}
 }
