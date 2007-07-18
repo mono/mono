783ff0464909a6a83cf21e18acf9bd21f42bf980
Marek Safar <marek.safar@gmail.com>
2007-07-18 16:07:39 +0000

    More lambda parser tests.
    svn path=/trunk/mcs/; revision=82217

--- mcs/tests/ltest-07.cs
+++ mcs/tests/ltest-07.cs
@@ -1,13 +1,37 @@
 // Compiler options: -langversion:linq
 
-delegate void E ();
+// Lambda parser tests
+
+delegate void D ();
+delegate void E (bool b);
 
 public class C
 {
+	static void Test (D d)
+	{
+	}
+	
+	static void Test (object o, D d)
+	{
+	}
+	
+	static void Test (D d1, D d2)
+	{
+	}
+
+	static void Test2 (object o, E e)
+	{
+	}
+
 	public static void Main ()
 	{
-		E e = () => { };
-		e = (E)null;
-		e = default (E);
+		D e = () => { };
+		e = (D)null;
+		e = default (D);
+
+		Test (() => { });
+		Test (1, () => { });
+		Test (() => { }, () => { });
+		Test2 (null, (foo) => { });
 	}
 }
