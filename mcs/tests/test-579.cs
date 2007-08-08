a78d10a2ca4ed6ac1fa581e71ce187d00bbdd916
Miguel de Icaza <miguel@gnome.org>
2007-08-08 14:41:33 +0000

    Add test
    svn path=/trunk/mcs/; revision=83671

---
+++ mcs/tests/test-579.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+
+class X {
+	delegate object test (MethodInfo x);
+		
+	static void Main ()
+	{
+		DoCall (delegate(MethodInfo from) {
+                    return from.Invoke (null, new object[] { from });
+                });
+	}
+
+	static void DoCall (test t)
+	{
+	}
+}
