4a0818349768b7779a8952f1a503591f42305e1b
Atsushi Eno <atsushieno@gmail.com>
2007-08-10 06:45:48 +0000

    2007-08-10  Atsushi Enomoto  <atsushi@ximian.com>
    	* ITypeInfo2.cs, ITypeLib2.cs : added missing 2.0 members.
    	  It is so strange that many of them overlap the base interfaces.
    
    
    svn path=/trunk/mcs/; revision=83798

--- mcs/class/corlib/System.Runtime.InteropServices.ComTypes/ITypeInfo2.cs
+++ mcs/class/corlib/System.Runtime.InteropServices.ComTypes/ITypeInfo2.cs
@@ -15,8 +15,24 @@ namespace System.Runtime.InteropServices.ComTypes
 	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 	public interface ITypeInfo2 : ITypeInfo
 	{
+		new void AddressOfMember (int memid, INVOKEKIND invKind, out IntPtr ppv);
+		new void CreateInstance ([MarshalAs (UnmanagedType.IUnknown)] object pUnkOuter, [In] ref Guid riid, [MarshalAs (UnmanagedType.IUnknown)] out object ppvObj);
+		new void GetContainingTypeLib (out ITypeLib ppTLB, out int pIndex);
+		new void GetDllEntry (int memid, INVOKEKIND invKind, IntPtr pBstrDllName, IntPtr pBstrName, IntPtr pwOrdinal);
+		new void GetDocumentation (int index, out string strName, out string strDocString, out int dwHelpContext, out string strHelpFile);
+		new void GetIDsOfNames ([In, MarshalAs(UnmanagedType.LPArray, ArraySubType = (UnmanagedType.LPWStr), SizeParamIndex=1)] string[] rgszNames, int cNames, [Out, MarshalAs (UnmanagedType.LPArray, SizeParamIndex=1)] int[] pMemId);
+
+		new void GetImplTypeFlags (int index, out IMPLTYPEFLAGS pImplTypeFlags);
 		void GetTypeKind (out TYPEKIND pTypeKind);
 		void GetTypeFlags (out int pTypeFlags);
+		new void GetFuncDesc (int index, out IntPtr ppFuncDesc);
+		new void GetMops (int memid, out string pBstrMops);
+		new void GetNames (int memid, [Out, MarshalAs (UnmanagedType.LPArray, SizeParamIndex=2)] string[] rgBstrNames, int cMaxNames, out int pcNames);
+		new void GetRefTypeInfo (int hRef, out ITypeInfo ppTI);
+		new void GetRefTypeOfImplType (int index, out int href);
+		new void GetTypeAttr (out IntPtr ppTypeAttr);
+		new void GetTypeComp (out ITypeComp ppTComp);
+		new void GetVarDesc (int index, out IntPtr ppVarDesc);
 		void GetFuncIndexOfMemId (int memid, INVOKEKIND invKind, out int pFuncIndex);
 		void GetVarIndexOfMemId (int memid, out int pVarIndex);
 		void GetCustData (ref Guid guid, out object pVarVal);
@@ -31,6 +47,13 @@ namespace System.Runtime.InteropServices.ComTypes
 		void GetAllParamCustData(int indexFunc, int indexParam, IntPtr pCustData);
 		void GetAllVarCustData(int index, IntPtr pCustData);
 		void GetAllImplTypeCustData(int index, IntPtr pCustData);
+		new void Invoke ([MarshalAs (UnmanagedType.IUnknown)] object pvInstance, int memid, short wFlags, ref DISPPARAMS pDispParams, IntPtr pVarResult, IntPtr pExcepInfo, out int puArgErr);
+		[PreserveSig]
+		new void ReleaseTypeAttr (IntPtr pTypeAttr);
+		[PreserveSig]
+		new void ReleaseFuncDesc (IntPtr pFuncDesc);
+		[PreserveSig]
+		new void ReleaseVarDesc (IntPtr pVarDesc);
 	}
 }
 #endif
--- mcs/class/corlib/System.Runtime.InteropServices.ComTypes/ITypeLib2.cs
+++ mcs/class/corlib/System.Runtime.InteropServices.ComTypes/ITypeLib2.cs
@@ -15,11 +15,25 @@ namespace System.Runtime.InteropServices.ComTypes
 	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 	public interface ITypeLib2 : ITypeLib
 	{
+		new void FindName ([MarshalAs(UnmanagedType.LPWStr)] string szNameBuf, int lHashVal, [Out, MarshalAs (UnmanagedType.LPArray)] ITypeInfo[] ppTInfo, [Out, MarshalAs (UnmanagedType.LPArray)] int[] rgMemId, ref short pcFound);
 		void GetCustData(ref Guid guid, out object pVarVal);
+		new void GetDocumentation (int index, out string strName, out string strDocString, out int dwHelpContext, out string strHelpFile);
+		new void GetLibAttr (out IntPtr ppTLibAttr);
 		void GetLibStatistics(IntPtr pcUniqueNames, out int pcchUniqueNames);
 		[LCIDConversion(1)]
 		void GetDocumentation2(int index, out string pbstrHelpString, out int pdwHelpStringContext, out string pbstrHelpStringDll);
 		void GetAllCustData(IntPtr pCustData);
+		new void GetTypeComp (out ITypeComp ppTComp);
+		new void GetTypeInfo (int index, out ITypeInfo ppTI);
+		new void GetTypeInfoOfGuid (ref Guid guid, out ITypeInfo ppTInfo);
+		new void GetTypeInfoType (int index, out TYPEKIND pTKind);
+		[return: MarshalAs (UnmanagedType.Bool)]
+		new bool IsName ([MarshalAs(UnmanagedType.LPWStr)] string szNameBuf, int lHashVal);
+		[PreserveSig]
+		new void ReleaseTLibAttr (IntPtr pTLibAttr);
+		// undocumented
+		[PreserveSig]
+		new int GetTypeInfoCount ();
 	}
 }
 #endif
