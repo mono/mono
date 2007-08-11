7907cc14922420d10186ee2e2bbf32cadb4555c6
Marek Safar <marek.safar@gmail.com>
2007-08-11 22:36:59 +0000

    A test for bug #82412
    svn path=/trunk/mcs/; revision=83900

---
+++ mcs/tests/gtest-337.cs
@@ -0,0 +1,15 @@
+using System;
+
+class X {
+        static void SetValue<T> (object o, T x)
+        {
+        }
+
+        static void Main ()
+        {
+                object o = null;
+                double [] d = null;
+
+                SetValue (o, d);
+        }
+}
