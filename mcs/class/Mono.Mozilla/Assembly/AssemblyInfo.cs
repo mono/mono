c4b9f046e36e8e7eebdaa1380e8386e1cc2080ed
Andreia Gaita <avidigal@novell.com>
2007-08-11 23:45:14 +0000

    2007-08-11 Andreia Gaita <avidigal@novell.com>
    * AssemblyInfo.cs: fix version and attributes
    
    svn path=/trunk/mcs/; revision=83903

--- mcs/class/Mono.Mozilla/Assembly/AssemblyInfo.cs
+++ mcs/class/Mono.Mozilla/Assembly/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTitle("Mono.Mozilla")]
+[assembly: AssemblyDescription("Managed Wrapper for xulrunner engine, to support WebBrowser")]
+[assembly: AssemblyConfiguration("Development version")]
+[assembly: AssemblyCompany("MONO development team")]
+[assembly: AssemblyProduct("Mono.Mozilla")]
+[assembly: AssemblyCopyright("(c) 2007 Andreia Gaita")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
 
 //
 // Version information for an assembly consists of the following four values:
@@ -26,7 +25,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
