4b3a2c44dd4ecabf8f1d47dde6d7f0ffab1db7f2
Marek Safar <marek.safar@gmail.com>
2007-08-14 15:43:19 +0000

    More tests.
    svn path=/trunk/mcs/; revision=84088

---
+++ mcs/tests/gtest-collectioninit-02.cs
@@ -0,0 +1,18 @@
+// Compiler options: -langversion:linq
+
+using System.Collections.Generic;
+
+public class C
+{
+	public static int Main ()
+	{
+		var o = new Dictionary<string, int>() { { "Foo", 3 } };
+		if (o ["Foo"] != 3)
+			return 1;
+		
+		o = new Dictionary<string, int>() { { "A", 1 }, { "B", 2 } };
+
+		return 0;
+	}
+}
+
