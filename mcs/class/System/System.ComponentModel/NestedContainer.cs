dadb140299a29bc35a02727bc6ff91e94a2513f0
Ivan Zlatev <ivan@ivanz.com>
2007-08-25 08:11:30 +0000

    2007-08-25  Ivan N. Zlatev  <contact@i-nz.net>
            * NestedContainer.cs: implemented.
            * NestedContainerTest.cs: add.
    
    
    svn path=/trunk/mcs/; revision=84844

--- mcs/class/System/System.ComponentModel/NestedContainer.cs
+++ mcs/class/System/System.ComponentModel/NestedContainer.cs
@@ -1,11 +1,10 @@
 //
-// NestedContainer.cs
+// System.ComponentModel.NestedContainer
 //
-// Author:
-//	Atsushi Enomoto  <atsushi@ximian.com>
+// Authors:		
+//		Ivan N. Zlatev (contact i-nZ.net)
 //
-// Copyright (C) 2007 Novell, Inc. http://www.novell.com
-//
+// (C) 2006-2007 Ivan N. Zlatev
 
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -31,49 +30,151 @@
 #if NET_2_0
 
 using System;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace System.ComponentModel
 {
-	public class NestedContainer : Container, INestedContainer
+	// Differences compared to Container:
+	// * Site's DesignMode property is routed through the owning component's site. Note that even though MSDN 
+	//   says that GetService is routed as well, it actually isn't. The unit tests proof that.
+	// * According to MSDN the site's Name property is a qualified name that includes the owning component's name followed by a
+	//	period (.) and the child component's name. It is not again according to the tests.
+	// * GetService provides support for the INestedContainer as a service.
+	// * When the owning component is disposed, the container is disposed as well.
+	
+	public class NestedContainer : Container, INestedContainer, IContainer, IDisposable
 	{
-		IComponent owner;
 
-		[MonoTODO]
+#region Site : INestedSite, ISite, IServiceProvider
+
+		private class Site : INestedSite, ISite, IServiceProvider
+		{		 
+
+			private IComponent _component;
+			private NestedContainer _nestedContainer;
+			private string _siteName;
+			
+			public Site (IComponent component, NestedContainer container, string name)
+			{
+				_component = component;
+				_nestedContainer = container;
+				_siteName = name;
+			}
+			
+			public IComponent Component {
+				get { return _component; }
+			}
+
+			public IContainer Container {
+				get { return _nestedContainer; }
+			}
+
+			public bool DesignMode {
+				get {
+					if (_nestedContainer.Owner != null
+						&& _nestedContainer.Owner.Site != null) {
+
+						return _nestedContainer.Owner.Site.DesignMode;
+					}
+					else {
+						return false;
+					}
+				}
+			}
+
+			public string Name {
+				get { return _siteName; }				
+				set { _siteName = value; }
+			}
+
+			// [owner].[component]
+			//
+			public string FullName {
+				get {
+					if (_siteName == null) {
+						return null;
+					}
+					if (_nestedContainer.OwnerName == null) {
+						return _siteName;
+					}
+
+					return _nestedContainer.OwnerName + "." + _siteName;
+				}
+			}
+
+			public virtual object GetService (Type service)
+			{
+				if (service == typeof (ISite)) {
+					return this; 
+				}
+				
+				return _nestedContainer.GetService (service);
+			}
+		} // Site
+
+#endregion
+
+		
+		
+		private IComponent _owner;
+		
+		
 		public NestedContainer (IComponent owner)
 		{
 			if (owner == null)
-				throw new ArgumentNullException ("component");
-			this.owner = owner;
-		}
+				throw new ArgumentNullException ("owner");
 
-		[MonoTODO]
+			_owner = owner;
+			_owner.Disposed += new EventHandler (OnOwnerDisposed);
+		}
+		
 		public IComponent Owner {
-			get { return owner; }
+			get { return _owner; }
 		}
 
-		[MonoTODO]
 		protected virtual string OwnerName {
-			get { throw new NotImplementedException (); }
-		}
+			get {
+				if (_owner.Site is INestedSite)
+					return ((INestedSite) _owner.Site).FullName;
+				if (_owner == null || _owner.Site == null)
+					return null;
+			
+				return _owner.Site.Name;
+			}
+		}		
 
-		[MonoTODO]
 		protected override ISite CreateSite (IComponent component, string name)
 		{
-			throw new NotImplementedException ();
+			if (component == null)
+				throw new ArgumentNullException("component");
+			
+			return new NestedContainer.Site (component, this, name);
+		}
+		
+		protected override object GetService (Type service)
+		{
+			if (service == typeof (INestedContainer))
+				return this;
+			
+			return base.GetService (service);
 		}
 
-		[MonoTODO]
 		protected override void Dispose (bool disposing)
 		{
-			throw new NotImplementedException ();
+			if (disposing)
+				_owner.Disposed -= new EventHandler (OnOwnerDisposed);
+			
+			base.Dispose (disposing);
 		}
 
-		[MonoTODO]
-		protected override object GetService (Type serviceType)
+		private void OnOwnerDisposed (object sender, EventArgs e)
 		{
-			throw new NotImplementedException ();
+			this.Dispose ();
 		}
 	}
+	
 }
 
 #endif
