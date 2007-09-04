dff49f5c4ddc42017c3f7a2d68efae1d29f54e97
Marek Safar <marek.safar@gmail.com>
2007-09-04 09:42:17 +0000

    Add mote tests, bug #82563
    svn path=/trunk/mcs/; revision=85263

--- mcs/tests/test-580.cs
+++ mcs/tests/test-580.cs
@@ -8,13 +8,20 @@ public class Bla {
 		label += s + s + s + s;
 	}
 	
+	public static void BuildNode (ref string[] label)
+	{
+		string s = "a";
+		int idx = 0;
+		label[idx++] += s + s + s + s;
+	}
+	
 	public static void BuildNode_B (ref object label)
 	{
 		string s = "b";
 		label += s + s;
 	}
 	
-	public static void BuildNode_C (ref string label)
+	public static string BuildNode_C (ref string label)
 	{
 		string[] a = new string [2];
 		int i = 0;
@@ -22,11 +29,36 @@ public class Bla {
 		string s = "b";
 
 		a [i++] += label + s + s + s;
+		return a [i - 1];
+	}
+	
+	public static string BuildNode_D ()
+	{
+		System.Collections.ArrayList values = new System.Collections.ArrayList ();
+		for (int i = 0; i < 6; i++)
+			values.Add (i);
+		string[] strs = new string [values.Count];
+		int idx = 0;
+		foreach (int val in values) {
+			strs [idx] = "Value:";
+			strs [idx++] += val.ToString ();
+		}
+		
+		return strs [5];
+	}
+	
+	public static void BuildNode_E (ref string[,] label)
+	{
+		string s = "a";
+		int idx = 0;
+		label = new string [1, 1];
+		label[idx++, idx - 1] += s + s + s + s;
 	}
 	
 	public static int Main ()
 	{
 		String str = "test";
+		
 		BuildNode (ref str);
 		Console.WriteLine (str);
 		if (str != "testaaaa")
@@ -39,10 +71,31 @@ public class Bla {
 			return 2;
 		
 		str = "test";
-		BuildNode_C (ref str);
+		string res = BuildNode_C (ref str);
 		Console.WriteLine (str);
 		if (str != "test")
 			return 3;
+		
+		Console.WriteLine (res);
+		if (res != "atestbbb")
+			return 4;
+		
+		string[] sa = new string [1];
+		BuildNode (ref sa);
+		Console.WriteLine (sa [0]);
+		if (sa [0] != "aaaa")
+			return 5;
+		
+		str = BuildNode_D ();
+		Console.WriteLine (str);
+		if (str != "Value:5")
+			return 6;
+		
+		string[,] sa2 = null;
+		BuildNode_E (ref sa2);
+		Console.WriteLine (sa2 [0, 0]);
+		if (sa2 [0,0] != "aaaa")
+			return 7;
 		
 		return 0;
 	}
