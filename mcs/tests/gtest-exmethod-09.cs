82032781a5b3107cc32d789735a546402c81af70
Marek Safar <marek.safar@gmail.com>
2007-09-07 20:26:18 +0000

    New test
    svn path=/trunk/mcs/; revision=85506

---
+++ mcs/tests/gtest-exmethod-09.cs
@@ -0,0 +1,17 @@
+// Compiler options: -langversion:linq
+
+static class Test
+{
+	public static void Foo<T> (this string p1)
+	{
+	}
+}
+
+class C
+{
+	public static void Main ()
+	{
+		//int x = Test.Foo<bool> ("bb");
+		"a".Foo<bool> ();
+	}
+}
