83160199c09e5b2df76bccd3b011bc3d61c31246
Zoltan Varga <vargaz@gmail.com>
2007-09-14 14:41:02 +0000

    2007-09-14  Zoltan Varga  <vargaz@gmail.com>
    	* GCHandleTest.cs: Add some tests.
    
    svn path=/trunk/mcs/; revision=85804

--- mcs/class/corlib/Test/System.Runtime.InteropServices/GCHandleTest.cs
+++ mcs/class/corlib/Test/System.Runtime.InteropServices/GCHandleTest.cs
@@ -31,6 +31,57 @@ namespace MonoTests.System.Runtime.InteropServices
 			GCHandle gch = (GCHandle)ptr;
 		}
 
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void AddrOfPinnedObjectNormal ()
+		{
+			GCHandle handle = GCHandle.Alloc (new Object (), GCHandleType.Normal);
+			try {
+				IntPtr ptr = handle.AddrOfPinnedObject();
+			}
+			finally {
+				handle.Free();
+			}
+		}
+
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void AddrOfPinnedObjectWeak ()
+		{
+			GCHandle handle = GCHandle.Alloc (new Object (), GCHandleType.Weak);
+			try {
+				IntPtr ptr = handle.AddrOfPinnedObject();
+			}
+			finally {
+				handle.Free();
+			}
+		}
+
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void AddrOfPinnedObjectWeakTrackResurrection ()
+		{
+			GCHandle handle = GCHandle.Alloc (new Object (), GCHandleType.WeakTrackResurrection);
+			try {
+				IntPtr ptr = handle.AddrOfPinnedObject();
+			}
+			finally {
+				handle.Free();
+			}
+		}
+
+		[Test]
+		public void AddrOfPinnedObjectNull ()
+		{
+			GCHandle handle = GCHandle.Alloc (null, GCHandleType.Pinned);
+			try {
+				IntPtr ptr = handle.AddrOfPinnedObject();
+				AssertEquals (new IntPtr (0), ptr);
+			}
+			finally {
+				handle.Free();
+			}
+		}
 	}
 }
 
