ff7a12fe632ef5c71969a6ff7e1574adde013879
Gert Driesen <drieseng@users.sourceforge.net>
2007-09-17 17:24:04 +0000

    * System_test.dll.sources: Added ComponentResourceManagerTest.cs. * ComponentResourceManager.cs: In ApplyResources, when culture is null use CurrentUICulture. Use hashtable, which is case-insensitive when IgnoreCase is true, to store the resources of the specified culture and its parent(s). When IgnoreCase is set, perform a case-insensitive lookup of property. Ignore resources that have no value, or where the value is not an instance of the property type. Fixes bug #82674. * ComponentResourceManagerTest.cs: Added tests for ctors, ApplyResources and IgnoreCase.
    svn path=/trunk/mcs/; revision=85910

--- mcs/class/System/System.ComponentModel/ComponentResourceManager.cs
+++ mcs/class/System/System.ComponentModel/ComponentResourceManager.cs
@@ -3,11 +3,11 @@
 //
 // Author:
 //   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//   Gert Driesen (drieseng@users.sourceforge.net)
 //
 // (C) 2003 Andreas Nahr
 //
 //
-
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -29,15 +29,15 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-
 using System;
-using System.Resources;
-using System.Globalization;
 using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
 using System.Reflection;
+using System.Resources;
 
-namespace System.ComponentModel {
-
+namespace System.ComponentModel
+{
 	public class ComponentResourceManager : ResourceManager
 	{
 		public ComponentResourceManager ()
@@ -51,31 +51,59 @@ namespace System.ComponentModel {
 
 		public void ApplyResources (object value, string objectName)
 		{
-			ApplyResources (value, objectName, System.Threading.Thread.CurrentThread.CurrentCulture);
+			ApplyResources (value, objectName, (CultureInfo) null);
 		}
 
 		public virtual void ApplyResources (object value, string objectName, CultureInfo culture)
 		{
 			if (value == null)
-				throw new ArgumentNullException ("value is null.");
-			
+				throw new ArgumentNullException ("value");
 			if (objectName == null)
-				throw new ArgumentNullException ("objectName is null.");
-			
+				throw new ArgumentNullException ("objectName");
+			if (culture == null)
+				culture = CultureInfo.CurrentUICulture;
+
+			Hashtable resources = IgnoreCase ? CollectionsUtil.CreateCaseInsensitiveHashtable ()
+				: new Hashtable ();
+			BuildResources (culture, resources);
+
 			string objKey = objectName + ".";
+			CompareOptions compareOptions = IgnoreCase ?
+				CompareOptions.IgnoreCase : CompareOptions.None;
 			Type type = value.GetType ();
-			PropertyInfo[] propinfos = type.GetProperties ();
-			
-			foreach(PropertyInfo pinfo in propinfos) {
-				if (!pinfo.CanWrite)
+
+			BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+			if (IgnoreCase)
+				bindingFlags |= BindingFlags.IgnoreCase;
+
+			foreach (DictionaryEntry de in resources) {
+				string resName = (string) de.Key;
+				if (!culture.CompareInfo.IsPrefix (resName, objKey, compareOptions))
 					continue;
-				
-				object obj = GetObject (objKey + pinfo.Name, culture);
-					
-				if (obj != null)
-					pinfo.SetValue (value, Convert.ChangeType (obj, pinfo.PropertyType), null);
+
+				string propName = resName.Substring (objKey.Length);
+				PropertyInfo pinfo = type.GetProperty (propName,
+					bindingFlags);
+				if (pinfo == null || !pinfo.CanWrite)
+					continue;
+				object resValue = de.Value;
+				if (resValue == null || pinfo.PropertyType.IsInstanceOfType (resValue))
+					pinfo.SetValue (value, resValue, null);
+			}
+		}
+
+		void BuildResources (CultureInfo culture, Hashtable resources)
+		{
+			// first load resources for less specific culture
+			if (culture != culture.Parent) {
+				BuildResources (culture.Parent, resources);
+			}
+
+			ResourceSet rs = GetResourceSet (culture, true, false);
+			if (rs != null) {
+				foreach (DictionaryEntry de in rs)
+					resources [(string) de.Key] = de.Value;
 			}
 		}
 	}
 }
-
