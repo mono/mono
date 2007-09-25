8e05931259ce9328959effacd0122eec209bbe5a
Marek Habersack <grendel@twistedcode.net>
2007-09-25 16:19:37 +0000

    2007-09-25  Marek Habersack  <mhabersack@novell.com>
    	* System.Design.dll.sources: added
    	System.Web.UI.Design/TemplateDefinition.cs,
    	System.Web.UI.Design/TemplatedEditableDesignerRegion.cs,
    	System.Web.UI.Design.WebControls/DataBoundControlDesigner.cs,
    	System.Web.UI.Design.WebControls/BaseDataBoundControlDesigner.cs,
    	System.Web.UI.Design/IDataBindingSchemaProvider.cs,
    	System.Web.UI.Design/IDataSourceDesigner.cs,
    	System.Web.UI.Design/DesignerDataSourceView.
    
    2007-09-25  Marek Habersack  <mhabersack@novell.com>
    
    	* TemplatedEditableDesignerRegion.cs, TemplateDefinition.cs,
    	IDataBindingSchemaProvider.cs, IDataBindingSchemaProvider.cs: added
    
    2007-09-25  Marek Habersack  <mhabersack@novell.com>
    
    	* DataBoundControlDesigner.cs: added
    
    
    
    svn path=/trunk/mcs/; revision=86343

---
+++ mcs/class/System.Design/System.Web.UI.Design/TemplateDefinition.cs
@@ -0,0 +1,129 @@
+//
+// TemplateDefinition.cs
+//
+// Author:
+//   Marek Habersack <mhabersack@novell.com>
+//
+// (C) 2007 Novell, Inc.
+//
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+#if NET_2_0
+
+using System;
+using System.Collections;
+using System.Web.UI.WebControls;
+
+namespace System.Web.UI.Design
+{
+	public class TemplateDefinition : DesignerObject
+	{		
+		[MonoNotSupported ("")]
+		public TemplateDefinition (ControlDesigner designer, string name, Object templatedObject, string templatePropertyName)
+			: base (designer, name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoNotSupported ("")]
+		public TemplateDefinition (ControlDesigner designer, string name, Object templatedObject, string templatePropertyName,
+					   bool serverControlsOnly)
+			: base (designer, name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoNotSupported ("")]
+		public TemplateDefinition (ControlDesigner designer, string name, Object templatedObject, string templatePropertyName,
+					   Style style)
+			: base (designer, name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoNotSupported ("")]
+		public TemplateDefinition (ControlDesigner designer, string name, Object templatedObject, string templatePropertyName,
+					   Style style, bool serverControlsOnly)
+			: base (designer, name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoNotSupported ("")]
+		public virtual bool AllowEditing {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoNotSupported ("")]
+		public virtual string Content {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+
+			[MonoNotSupported ("")]
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoNotSupported ("")]
+		public Style Style {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoNotSupported ("")]
+		public bool SupportsDataBinding {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+
+			[MonoNotSupported ("")]
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoNotSupported ("")]
+		public object TemplatedObject {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		[MonoNotSupported ("")]
+		public string TemplatePropertyName {
+			[MonoNotSupported ("")]
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+	}
+}
+#endif
