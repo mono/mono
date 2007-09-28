6c30dd234ddd035d41a30e63266727088a647471
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2007-09-28 21:06:26 +0000

    2007-09-28  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* TreeView.cs: When calling ExpandAll, don't move the scroll to the 
    	bottom area if we are in fact not using the vertical scroll bar.
    	Fixes #324824.
    
    
    svn path=/trunk/mcs/; revision=86618

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeView.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/TreeView.cs
@@ -764,7 +764,7 @@ namespace System.Windows.Forms {
 			if (!found)
 				return;
 
-			if (IsHandleCreated) {
+			if (IsHandleCreated && vbar.VisibleInternal) {
 				vbar.Value = vbar.Maximum - VisibleCount + 1;
 			} else {
 				RecalculateVisibleOrder (root_node);
