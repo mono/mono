fcb16861f6ad0887e4ea0b77ec2568947a3852d3
Marek Safar <marek.safar@gmail.com>
2007-10-05 09:03:42 +0000

    New test for bug bug #327520
    svn path=/trunk/mcs/; revision=86961

---
+++ mcs/tests/gtest-exmethod-11.cs
@@ -0,0 +1,22 @@
+// Compiler options: -langversion:linq
+
+using System;
+
+public class Test
+{
+	public static int Main (String [] args)
+	{
+		if (1.OneEleven ())
+			return 0;
+
+		return 1;
+	}
+}
+
+public static class Lol
+{
+	public static bool OneEleven (this object o)
+	{
+		return true;
+	}
+}
