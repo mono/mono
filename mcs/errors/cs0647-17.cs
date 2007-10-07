e8a0384572564882532e63f153fd736ff447cfea
Jb Evain <jbevain@gmail.com>
2007-10-07 02:58:50 +0000

    2007-10-07  Jb Evain  <jbevain@novell.com>
    	* cs0647-17.cs: New test for assembly version verification.
    
    svn path=/trunk/mcs/; revision=87044

---
+++ mcs/errors/cs0647-17.cs
@@ -0,0 +1,15 @@
+// cs0647-17.cs: Error during emitting `System.Reflection.AssemblyVersionAttribute' attribute. The reason is `Specified version `0.0.0.80420' is not valid'
+// Line : 7
+
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion ("0.0.0.80420")]
+
+public class Test {
+
+	public static int Main ()
+	{
+		return 1;
+	}
+}
