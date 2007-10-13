e3d2135ee2be259581fb94f0e5dd39b1869345b3
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2007-10-13 01:09:57 +0000

    2007-10-12  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* X11Dnd.cs: When DragEventArgs.Effect is set to a value not part of
    	AllowedEffect, don't let the drop operation happen. 
    	Fixes #32580.
    
    
    svn path=/trunk/mcs/; revision=87432

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/X11Dnd.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/X11Dnd.cs
@@ -977,7 +977,7 @@ namespace System.Windows.Forms {
 			xevent.ClientMessageEvent.message_type = XdndStatus;
 			xevent.ClientMessageEvent.format = 32;
 			xevent.ClientMessageEvent.ptr1 = toplevel;
-			if (effect != DragDropEffects.None)
+			if (effect != DragDropEffects.None && (effect & drag_data.AllowedEffects) != 0)
 				xevent.ClientMessageEvent.ptr2 = (IntPtr) 1;
 
 			xevent.ClientMessageEvent.ptr5 = ActionFromEffect (effect);
