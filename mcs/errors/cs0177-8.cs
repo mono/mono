8c107c1f578d73f32025c268e5bf524ebf0123e9
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-17 08:42:42 +0000

    * cs0177-8.cs: New test based on #325478.
    svn path=/trunk/mcs/; revision=87638

---
+++ mcs/errors/cs0177-8.cs
@@ -0,0 +1,22 @@
+// cs0177-8.cs: The out parameter `parameterModifiers' must be assigned to before control leaves the current method
+// Line: XXX
+
+using System;
+using System.Reflection;
+
+/// <summary>
+/// MS does not report CS0177 for structs:
+/// https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=304489
+/// </summary>
+class Program
+{
+	bool GetArgsForCall (object [] originalArgs, out ParameterModifier parameterModifiers)
+	{
+		int countOfArgs = originalArgs.Length;
+		if (countOfArgs == 0)
+			return false;
+
+		parameterModifiers = new ParameterModifier (countOfArgs);
+		return true;
+	}
+}
