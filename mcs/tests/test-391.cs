3a18dc780fec39afadf948d6a84cdce64b28257f
Marek Safar <marek.safar@gmail.com>
2007-10-22 16:22:19 +0000

    New test.
    svn path=/trunk/mcs/; revision=87915

---
+++ mcs/tests/test-391.cs
@@ -0,0 +1,25 @@
+class C
+{
+	void Foo (int i)
+	{
+	}
+	
+	void Foo (ref int i)
+	{
+	}
+	
+	void Bar (out bool b)
+	{
+		b = false;
+	}
+	
+	void Bar (bool b)
+	{
+	}
+	
+	public static void Main ()
+	{
+	}
+}
+
+
