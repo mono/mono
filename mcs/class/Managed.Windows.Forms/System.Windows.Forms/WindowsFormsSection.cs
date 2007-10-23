36a8408edca9428e7c562ddd107de7952eb2eb2a
Jonathan Pobst <monkey@jpobst.com>
2007-10-23 21:06:35 +0000

    2007-10-22  Jonathan Pobst  <monkey@jpobst.com>
    	* WindowsFormsSection.cs: Implement.
    
    svn path=/trunk/mcs/; revision=88035

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/WindowsFormsSection.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/WindowsFormsSection.cs
@@ -20,14 +20,35 @@
 // Copyright (c) 2007 Novell, Inc.
 //
 
-using System;
-
 #if NET_2_0
+
+using System;
+using System.Configuration;
 
 namespace System.Windows.Forms
 {
-	public static class WindowsFormsSection {
+	public sealed class WindowsFormsSection : ConfigurationSection
+	{
+		private ConfigurationPropertyCollection properties;
+		private ConfigurationProperty jit_debugging;
+
+		public WindowsFormsSection ()
+		{
+			properties = new ConfigurationPropertyCollection();
+			jit_debugging = new ConfigurationProperty ("jitDebugging", typeof (bool), false);
+
+			properties.Add (jit_debugging);
+		}
+
+		[ConfigurationProperty("jitDebugging", DefaultValue = "False")]
+		public bool JitDebugging {
+			get { return (bool)base[jit_debugging]; }
+			set { base[jit_debugging] = value; }
+		}
+
+		protected override ConfigurationPropertyCollection Properties {
+			get { return properties; }
+		}
 	}
 }
-
 #endif
