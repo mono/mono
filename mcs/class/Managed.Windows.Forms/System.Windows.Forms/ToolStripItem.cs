b4e4cb4e3252387d6d2bea87da426ac6d7baa550
Jonathan Pobst <monkey@jpobst.com>
2007-10-24 20:18:16 +0000

    2007-10-24  Jonathan Pobst  <monkey@jpobst.com>
    	* ToolStripItem.cs: Don't focus a ToolStripControlHost on Select of the
    	parent doesn't have focus.  This was causing ToolStripTextBoxes to take
    	focus on mouse over.
    
    svn path=/trunk/mcs/; revision=88127

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ToolStripItem.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ToolStripItem.cs
@@ -891,7 +891,7 @@ namespace System.Windows.Forms
 			if (!this.is_selected && this.CanSelect) {
 				this.is_selected = true;
 				
-				if (this.Visible && this is ToolStripControlHost)
+				if (this.Visible && this.Parent.Focused && this is ToolStripControlHost)
 					(this as ToolStripControlHost).Focus ();
 					
 				this.Invalidate ();
