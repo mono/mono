e6aa80447c6471801542e2381d949c3d6998284b
Marek Safar <marek.safar@gmail.com>
2007-10-25 16:50:23 +0000

    2007-10-25  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #336766
    	
    
    svn path=/trunk/mcs/; revision=88195

---
+++ mcs/tests/gtest-346.cs
@@ -0,0 +1,10 @@
+public class test
+{
+	public void CreateSimpleCallSite(int x) {}
+	public void CreateSimpleCallSite<A>() {}
+	public void CreateSimpleCallSite<A>(int x) {}
+		
+	public static void Main ()
+	{
+	}
+}
