d0b84e19eb02cbd6ba4bb8d67a36e5f7fa3b8449
Marek Safar <marek.safar@gmail.com>
2007-10-25 15:27:02 +0000

    2007-10-25  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #334737
    	
    
    svn path=/trunk/mcs/; revision=88178

---
+++ mcs/tests/test-595.cs
@@ -0,0 +1,32 @@
+using System;
+
+public struct SymbolId
+{
+}
+
+public interface IAttributesCollection
+{
+	object this [SymbolId name] { get; set; }
+}
+
+class AttributesCollection : IAttributesCollection
+{
+	public object this [SymbolId name] { 
+		get { return null; } 
+		set { }
+	}
+}
+
+class Program
+{
+	public static object SetDictionaryValue (object self, SymbolId name, object value)
+	{
+		IAttributesCollection dict = new AttributesCollection ();
+		return dict [name] = value;
+	}
+
+	static void Main ()
+	{
+		SetDictionaryValue (null, new SymbolId (), 1);
+	}
+}
