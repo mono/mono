5680a0330bb6d51af88a619c9cc7e570f19a3563
Marek Safar <marek.safar@gmail.com>
2007-10-27 21:27:47 +0000

    2007-10-27  Marek Safar  <marek.safar@gmail.com>
    	* AssemblyInfo.cs: Keep up-to-date.
    
    svn path=/trunk/mcs/; revision=88333

--- mcs/mcs/AssemblyInfo.cs
+++ mcs/mcs/AssemblyInfo.cs
@@ -1,12 +1,46 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Marek Safar (marek.safar@gmail.com)
+//
+// Copyright (C) 2007 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion(Consts.MonoVersion)]
 [assembly: AssemblyTitle ("Mono C# Compiler")]
+[assembly: AssemblyProduct ("Mono C# Compiler")]
+[assembly: AssemblyCopyright ("2001 - 2007 Novell, Inc.")]
+[assembly: AssemblyCompany ("Novell, Inc.")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyConfiguration ("")]
+
+[assembly: AssemblyVersion (Consts.MonoVersion)]
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+
 #if GMCS_SOURCE
 [assembly: AssemblyDescription ("Mono C# Compiler with Generics")]
 #else
 [assembly: AssemblyDescription ("Mono C# Compiler")]
 #endif
-[assembly: AssemblyCopyright ("2001, 2002, 2003 Ximian, Inc.")]
-[assembly: AssemblyCompany ("Ximian, Inc.")]
