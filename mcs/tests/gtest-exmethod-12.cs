55737b8d2f87efa157cffdd802037138e7f77e4a
Marek Safar <marek.safar@gmail.com>
2007-10-29 09:32:04 +0000

    2007-10-29  Marek Safar  <marek.safar@gmail.com>
     	A test for bug #334652
    
    
    svn path=/trunk/mcs/; revision=88359

---
+++ mcs/tests/gtest-exmethod-12.cs
@@ -0,0 +1,34 @@
+// Compiler options: -langversion:linq
+
+using System;
+using System.Collections.Specialized;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var chat = new ChatClient();
+		var lines = new StringCollection() { "a", "b", "c" };
+		chat.Say("test", lines);
+	}
+}
+
+class ChatClient
+{
+	public void Say(string to, string message)
+	{
+		Console.WriteLine("{0}: {1}", to, message);
+	}
+}
+
+
+static class ChatExtensions
+{
+	public static void Say(this ChatClient chat, string to, StringCollection lines)
+	{
+		foreach (string line in lines)
+		{
+			chat.Say(to, line);
+		}
+	}
+}
