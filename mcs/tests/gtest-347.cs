ecba202e452d2b6cc3a052873f3340ae20a025d5
Marek Safar <marek.safar@gmail.com>
2007-10-31 18:05:58 +0000

    2007-10-31  Marek Safar  <marek.safar@gmail.com>
     	A test for bug #338102
     	
    
    svn path=/trunk/mcs/; revision=88592

---
+++ mcs/tests/gtest-347.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class X
+{
+	public IEnumerable Enumerable(int query)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public IEnumerable<T> Enumerable<T>(string query)
+	{
+		throw new NotImplementedException();
+	}
+
+	public IEnumerable Enumerable(string query)
+	{
+		throw new NotImplementedException();
+	}
+
+	static void Main ()
+	{
+	}
+}
