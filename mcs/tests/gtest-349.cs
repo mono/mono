b80f30a5e02b5f9f7eab0501fc3d15806a080ef1
Marek Safar <marek.safar@gmail.com>
2007-11-06 16:51:24 +0000

    2007-11-06  Marek Safar  <marek.safar@gmail.com>
     	A test for bugs #337712
     	
    
    svn path=/trunk/mcs/; revision=89006

---
+++ mcs/tests/gtest-349.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System;
+using System.Reflection;
+
+class X
+{
+	public delegate R Function<T1, T2, R>(T1 arg1, T2 arg2);
+
+	static int Main ()
+	{
+		Delegate [] e = new Delegate [] {
+			new Function<IList,IList,int> (f2),
+			new Function<IList,object,int> (f2)
+		};
+		
+		if ((int)e [0].DynamicInvoke (null, null) != 1)
+			return 1;
+
+		if ((int) e [1].DynamicInvoke (null, null) != 2)
+			return 2;
+
+		Console.WriteLine ("OK");
+		return 0;
+	}
+
+	static int f2 (IList self, IList other) { return 1; }
+	static int f2 (IList self, object other) {return 2; }
+}
