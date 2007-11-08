9b164434fcdcefbd18876e5f9188c8ba362865f6
Marek Safar <marek.safar@gmail.com>
2007-11-08 15:59:13 +0000

    New tests.
    svn path=/trunk/mcs/; revision=89205

---
+++ mcs/errors/cs0677-2.cs
@@ -0,0 +1,7 @@
+// CS0677: `X.d': A volatile field cannot be of the type `double'
+// Line: 6
+
+class X
+{
+	public volatile double d;
+}
---
+++ mcs/errors/gcs0677-2.cs
@@ -0,0 +1,7 @@
+// CS0677: `C<T>.t': A volatile field cannot be of the type `T'
+// Line: 8
+
+public class C<T>
+{
+	volatile T t;
+}
