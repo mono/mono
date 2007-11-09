028727f5df64902ce81dd978998a888b317ea9bb
Marek Safar <marek.safar@gmail.com>
2007-11-09 16:09:06 +0000

    New test.
    svn path=/trunk/mcs/; revision=89325

---
+++ mcs/errors/gcs0837.cs
@@ -0,0 +1,17 @@
+// CS0837: The `as' operator cannot be applied to a lambda expression or anonymous method
+// Line: 15
+// Compiler options: -langversion:linq
+
+class X
+{
+	delegate void D ();
+	
+	static void Test (D d)
+	{
+	}
+	
+	static void Main ()
+	{
+		Test ((() => { }) as D);
+	}
+}
