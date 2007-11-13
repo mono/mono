ce8e5e707ecef8a1ca68ed40016f8ef544752f94
Marek Safar <marek.safar@gmail.com>
2007-11-13 14:11:01 +0000

    New test.
    svn path=/trunk/mcs/; revision=89557

---
+++ mcs/errors/cs0176-6.cs
@@ -0,0 +1,17 @@
+// CS0176: Static member `A.X' cannot be accessed with an instance reference, qualify it with a type name instead
+// Line: 12
+using System;
+
+class A
+{
+	public static int X { get { return 2; } }
+}
+
+class T
+	{
+	static void Main ()
+	{
+		A T = new A ();
+		System.Console.WriteLine (T.X);
+	}
+}
