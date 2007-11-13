631dfcbe4a82ebdaa980aa6622ba1c792f47f912
Marek Safar <marek.safar@gmail.com>
2007-11-13 09:46:42 +0000

    New test.
    svn path=/trunk/mcs/; revision=89536

---
+++ mcs/errors/gcs1644-9.cs
@@ -0,0 +1,8 @@
+// CS1644: Feature `automatically implemented properties' cannot be used because it is not part of the C# 2.0 language specification
+// Line: 7
+// Compiler options: -langversion:ISO-2
+
+class P
+{
+	public string Name { get; set; }
+}
