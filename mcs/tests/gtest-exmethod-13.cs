6b8d3a9e1fc9916fccc05fd5121180f8e400bacd
Marek Safar <marek.safar@gmail.com>
2007-11-13 14:09:02 +0000

    New test.
    svn path=/trunk/mcs/; revision=89556

---
+++ mcs/tests/gtest-exmethod-13.cs
@@ -0,0 +1,19 @@
+// Compiler options: -langversion:linq
+
+using System;
+using System.Collections.Generic;
+
+public static class Foo
+{
+        public static IEnumerable<T> Reverse<T> (this IEnumerable<T> self)
+        {
+                return self;
+        }
+
+        public static void Main ()
+        {
+                int [] data = {0, 1, 2};
+
+                var rev = data.Reverse ();
+        }
+}
