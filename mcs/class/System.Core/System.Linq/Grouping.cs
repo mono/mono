dd056bbe6ca6a55ee1522a7043ac2702fe16de50
Jb Evain <jbevain@gmail.com>
2007-11-13 00:53:18 +0000

    clean
    svn path=/trunk/mcs/; revision=89513

--- mcs/class/System.Core/System.Linq/Grouping.cs
+++ mcs/class/System.Core/System.Linq/Grouping.cs
@@ -5,10 +5,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -17,7 +17,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //
 // Authors:
-//        Alejandro Serrano "Serras" (trupill@yahoo.es)
+//	Alejandro Serrano "Serras" (trupill@yahoo.es)
 //
 
 using System;
@@ -26,30 +26,30 @@ using System.Collections.Generic;
 
 namespace System.Linq
 {
-        public class Grouping<K, T> : IGrouping<K, T>
-        {
-                K key;
-                IEnumerable<T> group;
-                
-                public Grouping (K key, IEnumerable<T> group)
-                {
-                        this.group = group;
-                        this.key = key;
-                }
-                
-                public K Key {
-                        get { return key; }
-                        set { key = value; }
-                }
-                
-                public IEnumerator<T> GetEnumerator ()
-                {
-                        return group.GetEnumerator ();
-                }
-                
-                IEnumerator IEnumerable.GetEnumerator ()
-                {
-                        return group.GetEnumerator ();
-                }
-        }
+	public class Grouping<K, T> : IGrouping<K, T>
+	{
+		K key;
+		IEnumerable<T> group;
+
+		public Grouping (K key, IEnumerable<T> group)
+		{
+			this.group = group;
+			this.key = key;
+		}
+
+		public K Key {
+			get { return key; }
+			set { key = value; }
+		}
+
+		public IEnumerator<T> GetEnumerator ()
+		{
+			return group.GetEnumerator ();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return group.GetEnumerator ();
+		}
+	}
 }
