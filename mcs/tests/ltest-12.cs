ded8cf7cffeeadfc37826c54627ccc894f1806c9
Marek Safar <marek.safar@gmail.com>
2007-11-14 13:15:38 +0000

    New test.
    svn path=/trunk/mcs/; revision=89608

---
+++ mcs/tests/ltest-12.cs
@@ -0,0 +1,26 @@
+// Compiler options: -langversion:linq
+
+using System;
+
+public delegate void Func<TA> (TA arg0);
+
+class Demo
+{
+	static void F<T> (T[] values, T value, Func<T> f)
+	{
+		Console.WriteLine (values [0]);
+		f (value);
+		Console.WriteLine (values [0]);
+	}
+	
+	static int Main ()
+	{
+		int[] a = new int [] { 10 };
+		F (a, 5, i => a [0] = i);
+		
+		if (a [0] != 5)
+			return 1;
+			
+		return 0;
+	}
+}
