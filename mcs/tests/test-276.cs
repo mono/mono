ed9802ee5166acfcc2320c08401916ef1b603163
Marek Safar <marek.safar@gmail.com>
2007-11-14 10:10:22 +0000

    More test coverage.
    svn path=/trunk/mcs/; revision=89603

--- mcs/tests/test-276.cs
+++ mcs/tests/test-276.cs
@@ -1,10 +1,10 @@
-// Test for bug #60459
+// Compiler options: -warnaserror -warn:4
 
 using System;
 using System.Reflection;
 
-public class EventTestClass {
-        public event EventHandler Elapsed;
+public class EventTestClass : IEventTest {
+        public event EventHandler Elapsed; // No warning is reported
 }
 
 public interface IEventTest {
@@ -14,7 +14,7 @@ public interface IEventTest {
 
 public class EntryPoint
 {
-	static bool test (Type type) { return type.GetEvent ("Elapsed").IsSpecialName; }	
+	public static bool test (Type type) { return type.GetEvent ("Elapsed").IsSpecialName; }	
         public static int Main ()
         {
                 return (test (typeof (EventTestClass)) 
