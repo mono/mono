2ecafdb7c2410c7813e0b1891697aa2307dacf42
Marek Safar <marek.safar@gmail.com>
2007-11-16 19:49:25 +0000

    2007-11-16  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #325423
    	
    
    svn path=/trunk/mcs/; revision=89785

---
+++ mcs/tests/gtest-352.cs
@@ -0,0 +1,40 @@
+// Compiler options: -o+
+
+using System;
+using System.Reflection;
+
+struct D
+{
+	public static D d1 = new D (1);
+	public int d2;
+
+	public D (int v)
+	{
+		d2 = 0;
+	}
+}
+
+class T
+{
+	static int Main ()
+	{
+		ConstructorInfo mi = typeof(D).GetConstructors (BindingFlags.Instance | BindingFlags.Public)[0];
+        MethodBody mb = mi.GetMethodBody();
+		
+		Console.WriteLine (mb.GetILAsByteArray ().Length);
+		if (mb.GetILAsByteArray ().Length != 8) {
+			return 1;
+		}
+
+		mi = typeof (D).GetConstructors (BindingFlags.Static | BindingFlags.NonPublic) [0];
+		mb = mi.GetMethodBody ();
+
+		Console.WriteLine (mb.GetILAsByteArray ().Length);
+		if (mb.GetILAsByteArray ().Length != 12) {
+			return 2;
+		}
+			
+		Console.WriteLine ("OK");
+		return 0;
+	}
+}
