85d6dd7e6cb48005d1763bd820f78f448e326022
Marek Safar <marek.safar@gmail.com>
2007-11-16 10:01:37 +0000

    New tests.
    svn path=/trunk/mcs/; revision=89746

---
+++ mcs/errors/gcs1644-11.cs
@@ -0,0 +1,8 @@
+// CS1644: Feature `default value expression' cannot be used because it is not part of the C# 1.0 language specification
+// Line: 7
+// Compiler options: -langversion:ISO-1
+
+class Test
+{
+	int i = default (int);
+}
