7f46fae7d2b98bfa0af77d23aa9cd3c22a210739
Marek Safar <marek.safar@gmail.com>
2007-11-19 14:53:16 +0000

    2007-11-19  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #342512
     	
    
    svn path=/trunk/mcs/; revision=89930

---
+++ mcs/tests/test-497.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class PlotMenuItem
+{
+	private EventHandler callback_;
+	static int set;
+
+	public PlotMenuItem ()
+	{
+	}
+
+	public PlotMenuItem (EventHandler callback)
+	{
+		callback_ = callback;
+
+		PlotMenuItem child = new PlotMenuItem ();
+		child.Callback += new EventHandler (callback);
+	}
+
+	static int Main ()
+	{
+		PlotMenuItem pmi = new PlotMenuItem (new EventHandler (MenuItem_Click));
+		pmi.Callback (null, null);
+		
+		if (set != 999)
+			return 1;
+			
+		return 0;
+	}
+
+	static void MenuItem_Click (object sender, EventArgs e)
+	{
+		set = 999;
+	}
+
+	public EventHandler Callback {
+		get { return callback_; }
+		set { callback_ = value; }
+	}
+}
