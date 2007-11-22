caa6501cbd2e5eb49565841c6d7010991faf341c
Marek Safar <marek.safar@gmail.com>
2007-11-22 17:21:44 +0000

    2007-11-22  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #334505
     	
    
    svn path=/trunk/mcs/; revision=90171

---
+++ mcs/tests/gtest-friend-13-lib.cs
@@ -0,0 +1,13 @@
+// Compiler options: -t:library
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("gtest-friend-13")]
+
+public class FriendClass
+{
+	protected internal virtual void Test ()
+	{
+	}
+}
+
---
+++ mcs/tests/gtest-friend-13.cs
@@ -0,0 +1,18 @@
+// Compiler options: -r:gtest-friend-13-lib.dll
+
+using System;
+
+public class B : FriendClass
+{
+	protected internal override void Test ()
+	{
+	}
+}
+
+public class Test
+{
+	static void Main ()
+	{
+	}
+}
+
