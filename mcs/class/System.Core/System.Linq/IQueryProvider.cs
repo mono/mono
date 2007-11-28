654c7f165d9737f00549cff1daea96bb28d8e5e0
Jb Evain <jbevain@gmail.com>
2007-11-28 22:32:46 +0000

    tabs
    svn path=/trunk/mcs/; revision=90411

--- mcs/class/System.Core/System.Linq/IQueryProvider.cs
+++ mcs/class/System.Core/System.Linq/IQueryProvider.cs
@@ -28,14 +28,14 @@
 
 using System.Linq.Expressions;
 
-namespace System.Linq
-{
-	public interface IQueryProvider
-	{
-                IQueryable CreateQuery (Expression expression);
-                object Execute (Expression expression);
-                	
-                IQueryable<TElement> CreateQuery<TElement> (Expression expression);
-                TResult Execute<TResult> (Expression expression);	
+namespace System.Linq {
+
+	public interface IQueryProvider {
+
+		IQueryable CreateQuery (Expression expression);
+		object Execute (Expression expression);
+
+		IQueryable<TElement> CreateQuery<TElement> (Expression expression);
+		TResult Execute<TResult> (Expression expression);
 	}
 }
