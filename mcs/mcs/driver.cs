195226782e448dddb02734fed728b057fcae8ef4
Miguel de Icaza <miguel@gnome.org>
2007-12-16 06:17:17 +0000

    Fix indent
    svn path=/trunk/mcs/; revision=91372

--- mcs/mcs/driver.cs
+++ mcs/mcs/driver.cs
@@ -1388,21 +1388,21 @@ namespace Mono.CSharp
 				
 			case "/langversion":
 				switch (value.ToLower (CultureInfo.InvariantCulture)) {
-					case "iso-1":
-						RootContext.Version = LanguageVersion.ISO_1;
-						return true;
-
-					case "default":
-						SetupV2 ();
-						return true;
-#if GMCS_SOURCE
-					case "iso-2":
-						RootContext.Version = LanguageVersion.ISO_2;
-						return true;
+				case "iso-1":
+					RootContext.Version = LanguageVersion.ISO_1;
+					return true;
 					
-					case "linq":
-						RootContext.Version = LanguageVersion.LINQ;
-						return true;
+				case "default":
+					SetupV2 ();
+					return true;
+#if GMCS_SOURCE
+				case "iso-2":
+					RootContext.Version = LanguageVersion.ISO_2;
+					return true;
+					
+				case "linq":
+					RootContext.Version = LanguageVersion.LINQ;
+					return true;
 #endif
 				}
 				Report.Error (1617, "Invalid option `{0}' for /langversion. It must be either `ISO-1', `ISO-2' or `Default'", value);
