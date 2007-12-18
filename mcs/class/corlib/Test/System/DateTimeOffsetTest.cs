91e18876792bb81f81c97c00c4dadf1e572d840e
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2007-12-18 14:13:41 +0000

    2007-12-18  Stephane Delcroix  <sdelcroix@novell.com>
    	* DateTimeOffsetTest.cs: tests for ToString methods.
    
    svn path=/trunk/mcs/; revision=91544

--- mcs/class/corlib/Test/System/DateTimeOffsetTest.cs
+++ mcs/class/corlib/Test/System/DateTimeOffsetTest.cs
@@ -8,6 +8,7 @@
 //
 
 #if NET_2_0
+using System.Globalization;
 using NUnit.Framework;
 using System;
 
@@ -105,6 +106,75 @@ namespace MonoTests.System {
 			object o = dt.UtcDateTime;
 			Assert.IsTrue (o is DateTime);
 			Assert.IsTrue (dt.UtcDateTime == new DateTime (2007,12,16,14,49,00,DateTimeKind.Utc));
+		}
+
+		[Test]
+		public void ParameterlessToString ()
+		{
+			DateTimeOffset dt = new DateTimeOffset (2007, 12, 18, 12, 16, 30, new TimeSpan (1, 0, 0));
+			Assert.AreEqual ("12/18/2007 12:16:30 PM +01:00", dt.ToString());
+			dt = new DateTimeOffset (2007, 12, 18, 12, 16, 30, new TimeSpan (-5, 0, 0));
+			Assert.AreEqual ("12/18/2007 12:16:30 PM -05:00", dt.ToString());
+			dt = new DateTimeOffset (2007, 12, 18, 12, 16, 30, TimeSpan.Zero);
+			Assert.AreEqual ("12/18/2007 12:16:30 PM +00:00", dt.ToString());
+		}
+
+		[Test]
+		public void ToStringWithCultureInfo ()
+		{
+			DateTimeOffset dto = new DateTimeOffset(2007, 5, 1, 9, 0, 0, TimeSpan.Zero);
+			Assert.AreEqual ("05/01/2007 09:00:00 +00:00", dto.ToString (CultureInfo.InvariantCulture));
+			Assert.AreEqual ("5/1/2007 9:00:00 AM +00:00", dto.ToString (new CultureInfo ("en-us")));
+			Assert.AreEqual ("01/05/2007 09:00:00 +00:00", dto.ToString (new CultureInfo ("fr-fr")));
+			Assert.AreEqual ("01.05.2007 09:00:00 +00:00", dto.ToString (new CultureInfo ("de-DE")));
+			Assert.AreEqual ("01/05/2007 9:00:00 +00:00", dto.ToString (new CultureInfo ("es-ES")));
+		}
+
+		[Test]
+		[ExpectedException (typeof (FormatException))]
+		public void ToStringExceptionalCase1 ()
+		{
+			DateTimeOffset.Now.ToString (";");
+		}
+
+		[Test]
+		[ExpectedException (typeof (FormatException))]
+		public void ToStringExceptionalCase2 ()
+		{
+			DateTimeOffset.Now.ToString ("U");
+		}
+
+		[Test]
+		public void ToStringWithFormat ()
+		{
+			DateTimeOffset dto = new DateTimeOffset (2007, 10, 31, 21, 0, 0, new TimeSpan(-8, 0, 0));
+			Assert.AreEqual ("10/31/2007", dto.ToString ("d"));
+			Assert.AreEqual ("Wednesday, October 31, 2007", dto.ToString ("D"));
+			Assert.AreEqual ("9:00 PM", dto.ToString ("t"));
+			Assert.AreEqual ("9:00:00 PM", dto.ToString ("T"));
+			Assert.AreEqual ("Wednesday, October 31, 2007 9:00 PM", dto.ToString ("f"));
+			Assert.AreEqual ("Wednesday, October 31, 2007 9:00:00 PM", dto.ToString ("F"));
+			Assert.AreEqual ("10/31/2007 9:00 PM", dto.ToString ("g"));
+			Assert.AreEqual ("10/31/2007 9:00:00 PM", dto.ToString ("G"));
+			Assert.AreEqual ("October 31", dto.ToString ("M"));
+			Assert.AreEqual (dto.ToString ("m"), dto.ToString ("M"));
+			Assert.AreEqual ("Thu, 01 Nov 2007 05:00:00 GMT", dto.ToString ("R"));
+			Assert.AreEqual (dto.ToString ("r"), dto.ToString ("R"));
+			Assert.AreEqual ("2007-10-31T21:00:00", dto.ToString ("s"));
+			Assert.AreEqual ("2007-11-01 05:00:00Z", dto.ToString ("u"));
+			Assert.AreEqual ("October, 2007", dto.ToString ("Y"));
+			Assert.AreEqual (dto.ToString ("y"), dto.ToString ("Y"));
+		}
+
+		[Test]
+		public void ToStringWithFormatAndCulture ()
+		{
+			DateTimeOffset dto = new DateTimeOffset (2007, 11, 1, 9, 0, 0, new TimeSpan(-7, 0, 0));
+			string format = "dddd, MMM dd yyyy HH:mm:ss zzz";
+			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, null as DateTimeFormatInfo));
+			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, CultureInfo.InvariantCulture));
+			Assert.AreEqual ("jeudi, nov. 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("fr-FR")));
+			Assert.AreEqual ("jueves, nov 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("es-ES")));
 		}
 	}
 }
