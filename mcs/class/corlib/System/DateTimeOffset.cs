042bb2a771e1b3b6935efd0f11b3bd0836f9c780
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2007-12-18 14:14:09 +0000

    2007-12-18  Stephane Delcroix  <sdelcroix@novell.com>
    	* DateTimeOffset.cs: implementing the 4 ToString overloads by reusing
    	large chunks of System.DateTime code.
    
    svn path=/trunk/mcs/; revision=91546

--- mcs/class/corlib/System/DateTimeOffset.cs
+++ mcs/class/corlib/System/DateTimeOffset.cs
@@ -33,6 +33,7 @@
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace System
 {
@@ -359,28 +360,373 @@ namespace System
 			return new DateTimeOffset (dt - utc_offset + offset, offset);
 		}
 
-		[MonoTODO]
 		public override string ToString ()
 		{
-			throw new NotImplementedException ();
+			return ToString (null, null);
 		}
 
-		[MonoTODO]
 		public string ToString (IFormatProvider formatProvider)
 		{
-			throw new NotImplementedException ();	
+			return ToString (null, formatProvider);
 		}
 
-		[MonoTODO]
 		public string ToString (string format)
 		{
-			throw new NotImplementedException ();
+			return ToString (format, null);
 		}
 
-		[MonoTODO]
 		public string ToString (string format, IFormatProvider formatProvider)
 		{
-			throw new NotImplementedException ();	
+			DateTimeFormatInfo dfi = DateTimeFormatInfo.GetInstance(formatProvider);
+
+			if (format == null || format == String.Empty)
+				format = dfi.ShortDatePattern + " " + dfi.LongTimePattern + " zzz";
+
+			bool to_utc = false;
+			if (format.Length == 1) {
+				char fchar = format [0];
+				try {
+					format = _GetStandardPattern (fchar, dfi, out to_utc);
+				} catch {
+					format = null;
+				}
+		
+				if (format == null)
+					throw new FormatException ("format is not one of the format specifier characters defined for DateTimeFormatInfo");
+			}
+			return to_utc ? ToUniversalTime ()._ToString (format, dfi) : _ToString (format, dfi);
+		}
+
+		static int CountRepeat (string fmt, int p, char c)
+		{
+			int l = fmt.Length;
+			int i = p + 1;
+			while ((i < l) && (fmt [i] == c)) 
+				i++;
+			
+			return i - p;
+		}
+
+		static unsafe void ZeroPad (StringBuilder output, int digits, int len)
+		{
+			// more than enough for an int
+			char* buffer = stackalloc char [16];
+			int pos = 16;
+			
+			do {
+				buffer [-- pos] = (char) ('0' + digits % 10);
+				digits /= 10;
+				len --;
+			} while (digits > 0);
+			
+			while (len -- > 0)
+				buffer [-- pos] = '0';
+			
+			output.Append (new string (buffer, pos, 16 - pos));
+		}
+
+		static int ParseQuotedString (string fmt, int pos, StringBuilder output)
+		{
+			// pos == position of " or '
+			
+			int len = fmt.Length;
+			int start = pos;
+			char quoteChar = fmt [pos++];
+			
+			while (pos < len) {
+				char ch = fmt [pos++];
+				
+				if (ch == quoteChar)
+					return pos - start;
+				
+				if (ch == '\\') {
+					// C-Style escape
+					if (pos >= len)
+						throw new FormatException("Un-ended quote");
+	
+					output.Append (fmt [pos++]);
+				} else {
+					output.Append (ch);
+				}
+			}
+
+			throw new FormatException("Un-ended quote");
+		}
+	
+		string _ToString (string format, DateTimeFormatInfo dfi)
+		{
+
+			// the length of the format is usually a good guess of the number
+			// of chars in the result. Might save us a few bytes sometimes
+			// Add + 10 for cases like mmmm dddd
+			StringBuilder result = new StringBuilder (format.Length + 10);
+
+			// For some cases, the output should not use culture dependent calendar
+			DateTimeFormatInfo inv = DateTimeFormatInfo.InvariantInfo;
+			if (format == inv.RFC1123Pattern)
+				dfi = inv;
+			else if (format == inv.UniversalSortableDateTimePattern)
+				dfi = inv;
+
+			int i = 0;
+
+			while (i < format.Length) {
+				int tokLen;
+				bool omitZeros = false;
+				char ch = format [i];
+
+				switch (ch) {
+
+				//
+				// Time Formats
+				//
+				case 'h':
+					// hour, [1, 12]
+					tokLen = CountRepeat (format, i, ch);
+
+					int hr = this.Hour % 12;
+					if (hr == 0)
+						hr = 12;
+
+					ZeroPad (result, hr, tokLen == 1 ? 1 : 2);
+					break;
+				case 'H':
+					// hour, [0, 23]
+					tokLen = CountRepeat (format, i, ch);
+					ZeroPad (result, this.Hour, tokLen == 1 ? 1 : 2);
+					break;
+				case 'm':
+					// minute, [0, 59]
+					tokLen = CountRepeat (format, i, ch);
+					ZeroPad (result, this.Minute, tokLen == 1 ? 1 : 2);
+					break;
+				case 's':
+					// second [0, 29]
+					tokLen = CountRepeat (format, i, ch);
+					ZeroPad (result, this.Second, tokLen == 1 ? 1 : 2);
+					break;
+				case 'F':
+					omitZeros = true;
+					goto case 'f';
+				case 'f':
+					// fraction of second, to same number of
+					// digits as there are f's
+
+					tokLen = CountRepeat (format, i, ch);
+					if (tokLen > 7)
+						throw new FormatException ("Invalid Format String");
+
+					int dec = (int)((long)(this.Ticks % TimeSpan.TicksPerSecond) / (long) Math.Pow (10, 7 - tokLen));
+					int startLen = result.Length;
+					ZeroPad (result, dec, tokLen);
+
+					if (omitZeros) {
+						while (result.Length > startLen && result [result.Length - 1] == '0')
+							result.Length--;
+						// when the value was 0, then trim even preceding '.' (!) It is fixed character.
+						if (dec == 0 && startLen > 0 && result [startLen - 1] == '.')
+							result.Length--;
+					}
+
+					break;
+				case 't':
+					// AM/PM. t == first char, tt+ == full
+					tokLen = CountRepeat (format, i, ch);
+					string desig = this.Hour < 12 ? dfi.AMDesignator : dfi.PMDesignator;
+
+					if (tokLen == 1) {
+						if (desig.Length >= 1)
+							result.Append (desig [0]);
+					}
+					else
+						result.Append (desig);
+
+					break;
+				case 'z':
+					// timezone. t = +/-h; tt = +/-hh; ttt+=+/-hh:mm
+					tokLen = CountRepeat (format, i, ch);
+
+					if (utc_offset.Ticks >= 0)
+						result.Append ('+');
+					else
+						result.Append ('-');
+
+					switch (tokLen) {
+					case 1:
+						result.Append (Math.Abs (utc_offset.Hours));
+						break;
+					case 2:
+						result.Append (Math.Abs (utc_offset.Hours).ToString ("00"));
+						break;
+					default:
+						result.Append (Math.Abs (utc_offset.Hours).ToString ("00"));
+						result.Append (':');
+						result.Append (Math.Abs (utc_offset.Minutes).ToString ("00"));
+						break;
+					}
+					break;
+				case 'K': // maps to 'zzz'
+					tokLen = 1;
+					if (utc_offset.Ticks >= 0)
+						result.Append ('+');
+					else
+						result.Append ('-');
+					result.Append (Math.Abs (utc_offset.Hours).ToString ("00"));
+					result.Append (':');
+					result.Append (Math.Abs (utc_offset.Minutes).ToString ("00"));
+					break;
+				//
+				// Date tokens
+				//
+				case 'd':
+					// day. d(d?) = day of month (leading 0 if two d's)
+					// ddd = three leter day of week
+					// dddd+ full day-of-week
+					tokLen = CountRepeat (format, i, ch);
+
+					if (tokLen <= 2)
+						ZeroPad (result, dfi.Calendar.GetDayOfMonth (this.DateTime), tokLen == 1 ? 1 : 2);
+					else if (tokLen == 3)
+						result.Append (dfi.GetAbbreviatedDayName (dfi.Calendar.GetDayOfWeek (this.DateTime)));
+					else
+						result.Append (dfi.GetDayName (dfi.Calendar.GetDayOfWeek (this.DateTime)));
+
+					break;
+				case 'M':
+					// Month.m(m?) = month # (with leading 0 if two mm)
+					// mmm = 3 letter name
+					// mmmm+ = full name
+					tokLen = CountRepeat (format, i, ch);
+					int month = dfi.Calendar.GetMonth(this.DateTime);
+					if (tokLen <= 2)
+						ZeroPad (result, month, tokLen);
+					else if (tokLen == 3)
+						result.Append (dfi.GetAbbreviatedMonthName (month));
+					else
+						result.Append (dfi.GetMonthName (month));
+
+					break;
+				case 'y':
+					// Year. y(y?) = two digit year, with leading 0 if yy
+					// yyy+ full year with leading zeros if needed.
+					tokLen = CountRepeat (format, i, ch);
+
+					if (tokLen <= 2)
+						ZeroPad (result, dfi.Calendar.GetYear (this.DateTime) % 100, tokLen);
+					else
+						ZeroPad (result, dfi.Calendar.GetYear (this.DateTime), tokLen);
+					break;
+
+				case 'g':
+					// Era name
+					tokLen = CountRepeat (format, i, ch);
+					result.Append (dfi.GetEraName (dfi.Calendar.GetEra (this.DateTime)));
+					break;
+
+				//
+				// Other
+				//
+				case ':':
+					result.Append (dfi.TimeSeparator);
+					tokLen = 1;
+					break;
+				case '/':
+					result.Append (dfi.DateSeparator);
+					tokLen = 1;
+					break;
+				case '\'': case '"':
+					tokLen = ParseQuotedString (format, i, result);
+					break;
+				case '%':
+					if (i >= format.Length - 1)
+						throw new FormatException ("% at end of date time string");
+					if (format [i + 1] == '%')
+						throw new FormatException ("%% in date string");
+
+					// Look for the next char
+					tokLen = 1;
+					break;
+				case '\\':
+					// C-Style escape
+					if (i >= format.Length - 1)
+						throw new FormatException ("\\ at end of date time string");
+
+					result.Append (format [i + 1]);
+					tokLen = 2;
+
+					break;
+				default:
+					// catch all
+					result.Append (ch);
+					tokLen = 1;
+					break;
+				}
+				i += tokLen;
+			}
+			return result.ToString ();
+		}
+
+		static string _GetStandardPattern (char format, DateTimeFormatInfo dfi, out bool to_utc)
+		{
+			string pattern;
+
+			to_utc = false;
+
+			switch (format) {
+			case 'd':
+				pattern = dfi.ShortDatePattern;
+				break;
+			case 'D':
+				pattern = dfi.LongDatePattern;
+				break;
+			case 'f':
+				pattern = dfi.LongDatePattern + " " + dfi.ShortTimePattern;
+				break;
+			case 'F':
+				pattern = dfi.FullDateTimePattern;
+				break;
+			case 'g':
+				pattern = dfi.ShortDatePattern + " " + dfi.ShortTimePattern;
+				break;
+			case 'G':
+				pattern = dfi.ShortDatePattern + " " + dfi.LongTimePattern;
+				break;
+			case 'm':
+			case 'M':
+				pattern = dfi.MonthDayPattern;
+				break;
+			case 'o':
+				pattern = dfi.RoundtripPattern;
+				break;
+			case 'r':
+			case 'R':
+				pattern = dfi.RFC1123Pattern;
+				to_utc = true;
+				break;
+			case 's':
+				pattern = dfi.SortableDateTimePattern;
+				break;
+			case 't':
+				pattern = dfi.ShortTimePattern;
+				break;
+			case 'T':
+				pattern = dfi.LongTimePattern;
+				break;
+			case 'u':
+				pattern = dfi.UniversalSortableDateTimePattern;
+				to_utc = true;
+				break;
+			case 'U':
+				throw new FormatException (String.Format ("Invalid format pattern: '{0}'", format));
+			case 'y':
+			case 'Y':
+				pattern = dfi.YearMonthPattern;
+				break;
+			default:
+				throw new FormatException (String.Format ("Invalid format pattern: '{0}'", format));
+			}
+
+			return pattern;
 		}
 
 		public DateTimeOffset ToUniversalTime ()
