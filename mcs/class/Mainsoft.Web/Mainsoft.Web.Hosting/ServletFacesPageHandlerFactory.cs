0760c4cb5be1fc66ed9263a1d11efae50823ebec
Igor Zelmanovich <igorz@mono-cvs.ximian.com>
2007-12-18 13:38:48 +0000

    refactoring:
    svn path=/branches/mainsoft/JSF/mcs/; revision=91538

--- mcs/class/Mainsoft.Web/Mainsoft.Web.Hosting/ServletFacesPageHandlerFactory.cs
+++ mcs/class/Mainsoft.Web/Mainsoft.Web.Hosting/ServletFacesPageHandlerFactory.cs
@@ -12,39 +12,11 @@ using javax.faces;
 
 namespace Mainsoft.Web.Hosting
 {
-	public sealed class ServletFacesPageHandlerFactory : IHttpHandlerFactory
+	public sealed class ServletFacesPageHandlerFactory : BaseFacesPageHandlerFactory
 	{
-		readonly ServletConfig _servletConfig;
-		readonly FacesContextFactory _facesContextFactory;
-		readonly Lifecycle _lifecycle;
-
-		string getLifecycleId () {
-			String lifecycleId = _servletConfig.getServletContext ().getInitParameter (FacesServlet.LIFECYCLE_ID_ATTR);
-			return lifecycleId != null ? lifecycleId : LifecycleFactory.DEFAULT_LIFECYCLE;
-		}
-
-		public ServletFacesPageHandlerFactory () {
-
-			HttpWorkerRequest wr = (HttpWorkerRequest) ((IServiceProvider) HttpContext.Current).GetService (typeof (HttpWorkerRequest));
-			HttpServlet servlet = (HttpServlet) ((IServiceProvider) wr).GetService (typeof (HttpServlet));
-
-			_servletConfig = servlet.getServletConfig ();
-			_facesContextFactory = (FacesContextFactory) FactoryFinder.getFactory (FactoryFinder.FACES_CONTEXT_FACTORY);
-			//TODO: null-check for Weblogic, that tries to initialize Servlet before ContextListener
-
-			//Javadoc says: Lifecycle instance is shared across multiple simultaneous requests, it must be implemented in a thread-safe manner.
-			//So we can acquire it here once:
-			LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder.getFactory (FactoryFinder.LIFECYCLE_FACTORY);
-			_lifecycle = lifecycleFactory.getLifecycle (getLifecycleId ());
-
-		}
-
-		public IHttpHandler GetHandler (HttpContext context, string requestType, string url, string path) {
-			IHttpHandler handler = new ServletFacesPageHandler (url, _facesContextFactory, _lifecycle);
+		public override IHttpHandler GetHandler (HttpContext context, string requestType, string url, string path) {
+			IHttpHandler handler = new ServletFacesPageHandler (url, FacesContextFactory, Lifecycle);
 			return SessionWrapper.WrapHandler (handler, context, url);
-		}
-
-		public void ReleaseHandler (IHttpHandler handler) {
 		}
 	}
 }
