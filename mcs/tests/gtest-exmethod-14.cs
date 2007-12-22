cba16624b7825a27f31d7e22cceeba028ed461a8
Marek Safar <marek.safar@gmail.com>
2007-12-22 13:18:15 +0000

    New test.
    svn path=/trunk/mcs/; revision=91809

---
+++ mcs/tests/gtest-exmethod-14.cs
@@ -0,0 +1,29 @@
+// Compiler options: -langversion:linq
+
+using System;
+
+public interface IA
+{
+	void Foo (IA self);
+}
+
+public static class C
+{
+	public static void Foo (this IA self)
+	{
+		self.Foo<int> ();
+	}
+	
+	public static void Bar<U> (this IA self)
+	{
+		self.Foo<U> ();
+	}
+	
+	public static void Foo<T> (this IA self)
+	{
+	}
+	
+	public static void Main ()
+	{
+	}
+}
