83548e99a09492342893d0405247cf125d5d9119
Marek Safar <marek.safar@gmail.com>
2007-12-22 19:32:50 +0000

    New test.
    svn path=/trunk/mcs/; revision=91812

---
+++ mcs/tests/gtest-exmethod-15.cs
@@ -0,0 +1,30 @@
+// Compiler options: -langversion:linq
+
+using System;
+using System.Reflection;
+
+public interface IA
+{
+	void Foo (IA self);
+}
+
+public static class C
+{
+	public static TAttribute GetCustomAttribute<TAttribute> (this ICustomAttributeProvider self)
+	{
+		var attributes = self.GetCustomAttributes<TAttribute> ();
+//		if (attributes == null || attributes.Length == 0)
+//			return null;
+
+		return attributes [0];
+	}
+
+	public static TAttribute [] GetCustomAttributes<TAttribute> (this ICustomAttributeProvider self)
+	{
+		return null;
+	}	
+	
+	public static void Main ()
+	{
+	}
+}
