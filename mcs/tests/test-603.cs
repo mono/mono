81170ffb61fad627f9264d21936ab45fe7f40a27
Marek Safar <marek.safar@gmail.com>
2007-12-23 14:42:13 +0000

    2007-12-23  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #346379
    
    
    svn path=/trunk/mcs/; revision=91834

---
+++ mcs/tests/test-603.cs
@@ -0,0 +1,30 @@
+// Compiler options: -unsafe
+
+using System;
+
+namespace ConsoleApplication1
+{
+	public struct Strct
+	{
+		public uint a;
+		public uint b;
+	}
+	
+	unsafe class Program
+	{
+		static Strct* ptr = null;
+		
+		static int Main ()
+		{
+			Strct* values = ptr;
+			values++;
+			values++;
+			
+			long diff = values - ptr;
+			if (diff != 2)
+				return 1;
+			
+			return 0;
+		}
+	}
+}
