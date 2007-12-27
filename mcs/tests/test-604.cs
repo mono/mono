fb23964c8a352bde8a4e2b35b9a685ef2070bb17
Marek Safar <marek.safar@gmail.com>
2007-12-27 10:42:46 +0000

    2007-12-27  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #343465
    
    
    svn path=/trunk/mcs/; revision=91928

---
+++ mcs/tests/test-604.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+
+class Program {
+	interface Iface1 {
+		void IfaceMethod1 ();
+	}
+
+	interface Iface2 {
+		void IfaceMethod2 ();
+	}
+
+	public class ImplementingExplicitInterfacesMembers : Iface1, Iface2 {
+		void Iface1.IfaceMethod1 ()
+		{
+		}
+
+		void Iface2.IfaceMethod2 ()
+		{
+		}
+	}
+
+	static int Main ()
+	{
+		object[] o = typeof (ImplementingExplicitInterfacesMembers).GetMethods (BindingFlags.NonPublic | BindingFlags.Instance);
+		foreach (MethodInfo mi in o) {
+			if (mi.Name.IndexOf ('+') != -1)
+				return 1;
+			Console.WriteLine (mi.Name);
+		}
+		
+		return 0;
+	}
+}
