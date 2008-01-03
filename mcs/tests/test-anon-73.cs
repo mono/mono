72efe4d3c1be7f5ae5046ebe644f1e6196ee0668
Marek Safar <marek.safar@gmail.com>
2008-01-03 11:56:42 +0000

    2008-01-03  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #351157
     	
    
    svn path=/trunk/mcs/; revision=92164

--- mcs/tests/test-anon-73.cs
+++ mcs/tests/test-anon-73.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Threading;
 
+// Cloning mechanism manual tests
+
 delegate void D (object o);
 
 class T {
-        static void Main () {
+	static void Main ()
+	{
 			D d = delegate (object state) {
 			try {
 			} catch {
@@ -12,7 +15,17 @@ class T {
 			} finally {
 			}
 			};
-		}
+	}
+		
+	static void Test_1 ()
+	{
+		System.Threading.ThreadPool.QueueUserWorkItem(delegate(object o)
+		{
+			using (System.Threading.Timer t = new System.Threading.Timer (null, null, 1, 1))
+			{
+			}
+		});
+	}		
 }
 
 
