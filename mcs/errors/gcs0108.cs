fed8a47f52f617380bc5f9abe516f9dd41e22df9
Marek Safar <marek.safar@gmail.com>
2008-01-04 15:33:31 +0000

    New test
    svn path=/trunk/mcs/; revision=92243

---
+++ mcs/errors/gcs0108.cs
@@ -0,0 +1,17 @@
+// CS0108: `BaseConcrete.InnerDerived<T>()' hides inherited member `BaseGeneric<T>.InnerDerived'. Use the new keyword if hiding was intended
+// Line: 14
+// Compiler options: -warn:2 -warnaserror
+
+class BaseGeneric<T>
+{
+	public class InnerDerived
+	{
+	}
+}
+
+class BaseConcrete : BaseGeneric<string>
+{
+	public void InnerDerived<T> ()
+	{
+	}
+}
