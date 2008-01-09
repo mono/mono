2a2e8f245b7ddb578cdc76c0ce0ebb002f6ce461
Jb Evain <jbevain@gmail.com>
2008-01-09 14:04:52 +0000

    tabs instead of spaces
    svn path=/trunk/mcs/; revision=92507

--- mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_ArrayLength.cs
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_ArrayLength.cs
@@ -1,3 +1,12 @@
+//
+// ExpressionTest_ArrayLength.cs
+//
+// Author:
+//   Federico Di Gregorio <fog@initd.org>
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -5,19 +14,18 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// Authors:
-//        Federico Di Gregorio <fog@initd.org>
 
 using System;
 using System.Reflection;
@@ -27,43 +35,43 @@ using System.Linq.Expressions;
 using NUnit.Framework;
 
 namespace MonoTests.System.Linq.Expressions
-{    
-    [TestFixture]
-    public class ExpressionTest_ArrayLength
-    {
-        [Test]
-        [ExpectedException (typeof (ArgumentNullException))]
-        public void Arg1Null ()
-        {
-            Expression.ArrayLength (null);
-        }
+{
+	[TestFixture]
+	public class ExpressionTest_ArrayLength
+	{
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void Arg1Null ()
+		{
+			Expression.ArrayLength (null);
+		}
 
-        [Test]
-        [ExpectedException (typeof (ArgumentException))]
-        public void Arg1NotArray ()
-        {
-            Expression.ArrayLength (Expression.Constant ("This is not an array!"));
-        }
-
-        [Test]
-        public void Rank1String ()
-        {
-            string[] array = { "a", "b", "c" };
-            
-            UnaryExpression expr = Expression.ArrayLength (Expression.Constant (array));
-            Assert.AreEqual (ExpressionType.ArrayLength, expr.NodeType, "ArrayLength#01");
-            Assert.AreEqual (typeof (int), expr.Type, "ArrayLength#02");
-            Assert.IsNull (expr.Method, "ArrayLength#03");
-            Assert.AreEqual ("ArrayLength(value(System.String[]))", expr.ToString(), "ArrayLength#04");
-        }
-
-        [Test]
+		[Test]
 		[ExpectedException (typeof (ArgumentException))]
-        public void Rank2String ()
-        {
-            string[,] array = {{ }, { }};
-            
-            Expression.ArrayLength (Expression.Constant (array));
-        }
-    }
+		public void Arg1NotArray ()
+		{
+			Expression.ArrayLength (Expression.Constant ("This is not an array!"));
+		}
+
+		[Test]
+		public void Rank1String ()
+		{
+			string[] array = { "a", "b", "c" };
+
+			UnaryExpression expr = Expression.ArrayLength (Expression.Constant (array));
+			Assert.AreEqual (ExpressionType.ArrayLength, expr.NodeType, "ArrayLength#01");
+			Assert.AreEqual (typeof (int), expr.Type, "ArrayLength#02");
+			Assert.IsNull (expr.Method, "ArrayLength#03");
+			Assert.AreEqual ("ArrayLength(value(System.String[]))", expr.ToString(), "ArrayLength#04");
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void Rank2String ()
+		{
+			string[,] array = {{ }, { }};
+
+			Expression.ArrayLength (Expression.Constant (array));
+		}
+	}
 }
