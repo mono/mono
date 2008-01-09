e80511a3939412a2a3ee767330253d7dcbd388a9
Jb Evain <jbevain@gmail.com>
2008-01-09 13:46:50 +0000

    tabs instead of spaces
    svn path=/trunk/mcs/; revision=92502

--- mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_Call.cs
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_Call.cs
@@ -1,3 +1,12 @@
+//
+// ExpressionTest_Call.cs
+//
+// Author:
+//   Federico Di Gregorio <fog@initd.org>
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -5,19 +14,18 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// Authors:
-//        Federico Di Gregorio <fog@initd.org>
 
 using System;
 using System.Reflection;
@@ -27,51 +35,51 @@ using System.Linq.Expressions;
 using NUnit.Framework;
 
 namespace MonoTests.System.Linq.Expressions
-{    
-    // Tests for (type, methodName, ...) version.
+{
+	// Tests for (type, methodName, ...) version.
 
-    [TestFixture]
-    public class ExpressionTest_CallWithType
-    {                
-        [Test]
-        [ExpectedException (typeof (ArgumentNullException))]
-        public void Arg1Null ()
-        {
-            Expression.Call ((Type)null, "TestMethod", null, Expression.Constant (1));
-        }
+	[TestFixture]
+	public class ExpressionTest_CallWithType
+	{
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void Arg1Null ()
+		{
+			Expression.Call ((Type)null, "TestMethod", null, Expression.Constant (1));
+		}
 
-        [Test]
-        [ExpectedException (typeof (ArgumentNullException))]
-        public void Arg2Null ()
-        {
-            Expression.Call (typeof (MemberClass), null, null, Expression.Constant (1));
-        }
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void Arg2Null ()
+		{
+			Expression.Call (typeof (MemberClass), null, null, Expression.Constant (1));
+		}
 
-        [Test]
-        [ExpectedException (typeof (InvalidOperationException))]
-        public void Arg4WrongType ()
-        {
-            Expression.Call (typeof (MemberClass), "StaticMethod", null, Expression.Constant (true));
-        }
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void Arg4WrongType ()
+		{
+			Expression.Call (typeof (MemberClass), "StaticMethod", null, Expression.Constant (true));
+		}
 
-        [Test]
-        [ExpectedException (typeof (InvalidOperationException))]
-        public void InstanceMethod ()
-        {
-            Expression.Call (typeof (MemberClass), "TestMethod", null, Expression.Constant (1));
-        }
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void InstanceMethod ()
+		{
+			Expression.Call (typeof (MemberClass), "TestMethod", null, Expression.Constant (1));
+		}
 
-        [Test]
-        public void StaticMethod ()
-        {
-            Expression.Call (typeof (MemberClass), "StaticMethod", null, Expression.Constant (1));
-        }
+		[Test]
+		public void StaticMethod ()
+		{
+			Expression.Call (typeof (MemberClass), "StaticMethod", null, Expression.Constant (1));
+		}
 
-        //[Test]
-        public void StaticGenericMethod ()
-        {
-            MemberClass.StaticGenericMethod(1);
-            Expression.Call (typeof (MemberClass), "StaticGenericMethod", new Type [1] { typeof (int) }, Expression.Constant (1));
-        }
-    }
+		//[Test]
+		public void StaticGenericMethod ()
+		{
+			MemberClass.StaticGenericMethod(1);
+			Expression.Call (typeof (MemberClass), "StaticGenericMethod", new Type [1] { typeof (int) }, Expression.Constant (1));
+		}
+	}
 }
