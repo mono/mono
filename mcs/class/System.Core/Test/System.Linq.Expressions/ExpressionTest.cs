ca8df1c4a78d4da26cd1313bd187a62cdd00d6fb
Jb Evain <jbevain@gmail.com>
2008-01-09 16:14:57 +0000

    2008-01-09  Jb Evain  <jbevain@novell.com>
    	* ExpressionTest.cs: add new fixture of assorted tests for
    	Expression. Contains tests for GetFuncType for now.
    
    
    svn path=/trunk/mcs/; revision=92521

---
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest.cs
@@ -0,0 +1,81 @@
+//
+// ExpressionTest.cs
+//
+// Author:
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.Linq.Expressions {
+
+	[TestFixture]
+	public class ExpressionTest {
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void GetFuncTypeArgNull ()
+		{
+			Expression.GetFuncType (null);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void GetFuncTypeArgEmpty ()
+		{
+			Expression.GetFuncType (new Type [0]);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void GetFuncTypeArgTooBig ()
+		{
+			Expression.GetFuncType (new Type [6]);
+		}
+
+		[Test]
+		public void GetFuncTypeTest ()
+		{
+			var func = Expression.GetFuncType (new [] {typeof (int)});
+			Assert.AreEqual (typeof (Func<int>), func);
+
+			func = Expression.GetFuncType (new [] {typeof (int), typeof (int)});
+			Assert.AreEqual (typeof (Func<int, int>), func);
+
+			func = Expression.GetFuncType (new [] {typeof (int), typeof (int), typeof (int)});
+			Assert.AreEqual (typeof (Func<int, int, int>), func);
+
+			func = Expression.GetFuncType (new [] {typeof (int), typeof (int), typeof (int), typeof (int)});
+			Assert.AreEqual (typeof (Func<int, int, int, int>), func);
+
+			func = Expression.GetFuncType (new [] {typeof (int), typeof (int), typeof (int), typeof (int), typeof (int)});
+			Assert.AreEqual (typeof (Func<int, int, int, int, int>), func);
+		}
+	}
+}
