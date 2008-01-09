b42057aaea2435c08ca627239ac4f2baf43b0359
Jb Evain <jbevain@gmail.com>
2008-01-09 13:42:31 +0000

    2008-01-09  Jb Evain  <jbevain@novell.com>
    	* ExpressionTest_TypeAs.cs: correct wrong test for numerics.
    	Add test for nullable types.
    
    
    svn path=/trunk/mcs/; revision=92499

--- mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_TypeAs.cs
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_TypeAs.cs
@@ -1,3 +1,12 @@
+//
+// EnumerableTest.cs
+//
+// Author:
+//   Federico Di Gregorio <fog@initd.org>
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -5,19 +14,17 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-//
-// Authors:
-//        Federico Di Gregorio <fog@initd.org>
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
 using System.Reflection;
@@ -44,13 +51,20 @@ namespace MonoTests.System.Linq.Expressions
             Expression.TypeAs (Expression.Constant (1), null);
         }
 
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void Arg2NotReferenceNorNullable ()
+		{
+			Expression.TypeAs (Expression.Constant (1), typeof (int));
+		}
+
         [Test]
-        public void Numeric ()
+        public void NullableNumeric ()
         {
-            UnaryExpression expr = Expression.TypeAs (Expression.Constant (1), typeof (int));
+            UnaryExpression expr = Expression.TypeAs (Expression.Constant (1), typeof (int?));
             Assert.AreEqual (ExpressionType.TypeAs, expr.NodeType, "TypeAs#01");
-            Assert.AreEqual (typeof (int), expr.Type, "TypeAs#02");
-            Assert.AreEqual ("(1 As Int32)", expr.ToString(), "TypeAs#03");
+            Assert.AreEqual (typeof (int?), expr.Type, "TypeAs#02");
+            Assert.AreEqual ("(1 As Nullable`1)", expr.ToString(), "TypeAs#03");
         }
 
         [Test]
