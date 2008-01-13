e8b45ada4c17626ad52c073714c4475154aabc43
Igor Zelmanovich <igorz@mono-cvs.ximian.com>
2008-01-13 13:07:12 +0000

    remove Console.WriteLine
    svn path=/trunk/mcs/; revision=92789

--- mcs/class/System.Web/System.Web/VirtualPathUtility.cs
+++ mcs/class/System.Web/System.Web/VirtualPathUtility.cs
@@ -290,13 +290,7 @@ namespace System.Web {
 
 		public static string ToAppRelative (string virtualPath, string applicationPath)
 		{
-#if DEBUG
-			Console.WriteLine ("VirtualPathUtility.ToAppRelative (\"{0}\", \"{1}\")", virtualPath, applicationPath);
-#endif
 			virtualPath = Normalize (virtualPath);
-#if DEBUG
-			Console.WriteLine ("\tvirtualPath (normalized): {0}", virtualPath);
-#endif
 			if (IsAppRelative (virtualPath))
 				return virtualPath;
 
