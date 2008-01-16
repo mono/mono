b60b0bfba99f732a4dfc5a1d783f159a5ba6f51f
Geoff Norton <grompf@sublimeintervention.com>
2008-01-16 21:27:02 +0000

    In .: 2008-01-16  Geoff Norton  <gnorton@novell.com>
    	* XplatUICarbon.cs: Implement GetAutoScaleSize to fix Reflector on
    	trunk
    
    
    svn path=/trunk/mcs/; revision=93110

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/XplatUICarbon.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/XplatUICarbon.cs
@@ -2085,9 +2085,16 @@ namespace System.Windows.Forms {
 		}
 		#endregion
 
-		[MonoTODO]
 		internal override SizeF GetAutoScaleSize(Font font) {
-			throw new NotImplementedException();
+			Graphics        g;
+			float           width;
+			string          magic_string = "The quick brown fox jumped over the lazy dog.";
+			double          magic_number = 44.549996948242189;
+
+			g = Graphics.FromImage (new Bitmap (1, 1));
+
+			width = (float) (g.MeasureString (magic_string, font).Width / magic_number);
+			return new SizeF(width, font.Height);
 		}
 
 		internal override Point MousePosition {
