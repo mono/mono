58f4da29c32c6164fbb075946975a6732e31c757
Miguel de Icaza <miguel@gnome.org>
2008-01-21 14:18:10 +0000

    Add
    svn path=/trunk/mcs/; revision=93389

---
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_Equal.cs
@@ -0,0 +1,148 @@
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//
+// Authors:
+//    Miguel de Icaza (miguel@novell.com)
+//
+
+using System;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace MonoTests.System.Linq.Expressions
+{
+	[TestFixture]
+	public class ExpressionTest_Equal
+	{
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void Arg1Null ()
+		{
+			Expression.Equal (null, Expression.Constant (1));
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void Arg2Null ()
+		{
+			Expression.Equal (Expression.Constant (1), null);
+		}
+
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void ArgTypesDifferent ()
+		{
+			Expression.Equal (Expression.Constant (1), Expression.Constant (2.0));
+		}
+
+		[Test]
+		public void ReferenceCompare ()
+		{
+			Expression.Equal (Expression.Constant (new NoOpClass ()), Expression.Constant (new NoOpClass ()));
+		}
+
+		public struct D {
+		}
+		
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void NoOperatorClass ()
+		{
+			Expression.Equal (Expression.Constant (new D ()), Expression.Constant (new D ()));
+		}
+
+		[Test]
+		public void Numeric ()
+		{
+			BinaryExpression expr = Expression.Equal (Expression.Constant (1), Expression.Constant (2));
+			Assert.AreEqual (ExpressionType.Equal, expr.NodeType, "Equal#01");
+			Assert.AreEqual (typeof (bool), expr.Type, "Equal#02");
+			Assert.IsNull (expr.Method, "Equal#03");
+		}
+
+		[Test]
+		public void Nullable_LiftToNull_SetToFalse ()
+		{
+			int? a = 1;
+			int? b = 2;
+
+			BinaryExpression expr = Expression.Equal (Expression.Constant (a, typeof(int?)),
+								  Expression.Constant (b, typeof(int?)),
+								  false, null);
+			Assert.AreEqual (ExpressionType.Equal, expr.NodeType, "Add#05");
+			Assert.AreEqual (typeof (bool), expr.Type, "Equal#06");
+			Assert.AreEqual (true, expr.IsLifted, "Equal#07");
+			Assert.AreEqual (false, expr.IsLiftedToNull, "Equal#07");
+			Assert.IsNull (expr.Method, "Equal#08");
+		}
+
+		[Test]
+		public void Nullable_LiftToNull_SetToTrue ()
+		{
+			int? a = 1;
+			int? b = 2;
+
+			BinaryExpression expr = Expression.Equal (Expression.Constant (a, typeof(int?)),
+								  Expression.Constant (b, typeof(int?)),
+								  true, null);
+			Assert.AreEqual (ExpressionType.Equal, expr.NodeType, "Equal#01");
+			Assert.AreEqual (typeof (bool?), expr.Type, "Equal#02");
+			Assert.AreEqual (true, expr.IsLifted, "Equal#03");
+			Assert.AreEqual (true, expr.IsLiftedToNull, "Equal#04");
+			Assert.IsNull (expr.Method, "Equal#04");
+		}
+
+		[Test]
+		[ExpectedException(typeof (InvalidOperationException))]
+		public void Nullable_Mixed ()
+		{
+			int? a = 1;
+			int b = 2;
+
+			BinaryExpression expr = Expression.Equal (Expression.Constant (a, typeof (int?)),
+								  Expression.Constant (b, typeof (int)));
+		}
+		
+		[Test]
+		public void UserDefinedClass ()
+		{
+			// We can use the simplest version of GetMethod because we already know only one
+			// exists in the very simple class we're using for the tests.
+			MethodInfo mi = typeof (OpClass).GetMethod ("op_Equality");
+
+			BinaryExpression expr = Expression.Equal (Expression.Constant (new OpClass ()), Expression.Constant (new OpClass ()));
+			Assert.AreEqual (ExpressionType.Equal, expr.NodeType, "Add#09");
+			Assert.AreEqual (typeof (bool), expr.Type, "Add#10");
+			Assert.AreEqual (mi, expr.Method, "Add#11");
+			Assert.AreEqual ("op_Equality", expr.Method.Name, "Add#12");
+		}
+
+		//
+		// Checks for the behavior when the return type for Equal is not
+		// bool, and its coping with nullable values.
+		//
+		[Test]
+		public void UserDefinedEqual ()
+		{
+			
+		}
+		
+	}
+}
---
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_Lift.cs
@@ -0,0 +1,104 @@
+//
+// ExpressionTest_Lift: this contains tests for the various lifting settings in binary expressions
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//
+// Authors:
+//    Miguel de Icaza (miguel@novell.com)
+//
+
+using System;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace MonoTests.System.Linq.Expressions
+{
+	[TestFixture]
+	public class ExpressionTest_Lifting
+	{
+		[Test]
+		public void TestLiftOnEqual ()
+		{
+			ConstantExpression a = Expression.Constant (1, typeof (int?));
+			ConstantExpression b = Expression.Constant (1, typeof (int?));
+
+			BinaryExpression cmp = Expression.Equal (a, b);
+
+			Assert.AreEqual (true, cmp.IsLifted, "IsLifted");
+			Assert.AreEqual (false, cmp.IsLiftedToNull, "IsLiftedToNull");
+			Assert.AreEqual (typeof(bool), cmp.Type, "type");
+		}
+
+		[Test]
+		public void TestLiftOnEqual_ForcedLifted ()
+		{
+			ConstantExpression a = Expression.Constant (1, typeof (int?));
+			ConstantExpression b = Expression.Constant (1, typeof (int?));
+
+			// Force the lift on equal
+			BinaryExpression cmp = Expression.Equal (a, b, true, null);
+
+			Assert.AreEqual (true, cmp.IsLifted, "IsLifted");
+			Assert.AreEqual (true, cmp.IsLiftedToNull, "IsLiftedToNull");
+			Assert.AreEqual (typeof(bool?), cmp.Type);
+		}
+
+		static MethodInfo GM(string n)
+		{
+			MethodInfo [] m = typeof(ExpressionTest_Lifting).GetMethods(BindingFlags.Static | BindingFlags.Public);
+			
+			foreach (MethodInfo mm in m)
+				if (mm.Name == n)
+					return mm;
+			
+			throw new Exception("No method found: " + n);
+		}
+		
+		static public int MyCompare (OpStruct a, OpStruct b)
+		{
+			return 1;
+		}
+		
+		[Test]
+		public void TestLiftOnEqual_WithMethodInfo ()
+		{
+			ConstantExpression a = Expression.Constant (new OpStruct (), typeof (OpStruct?));
+			ConstantExpression b = Expression.Constant (null, typeof (OpStruct?));
+
+			// Force the lift on equal
+			BinaryExpression cmp = Expression.Equal (a, b, true, GM("MyCompare"));
+
+			Assert.AreEqual (true, cmp.IsLifted, "IsLifted");
+			Assert.AreEqual (true, cmp.IsLiftedToNull, "IsLiftedToNull");
+
+			BinaryExpression cmp2 = Expression.Equal (a, b, false, GM("MyCompare"));
+
+			//
+			// When we use a MethodInfo, that has a non-bool return type, 
+			// the result is always Nullable<returntype> regardless of the
+			// setting of "liftToNull"
+			//
+			Assert.AreEqual (typeof(int?), cmp.Type);
+			Assert.AreEqual (typeof(int?), cmp2.Type);
+			
+		}
+	}
+}
