26e8401ea527b01bccbc0270e01a512e2a8b12b8
Marek Safar <marek.safar@gmail.com>
2008-01-22 10:11:58 +0000

    2008-01-22  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #355161
     
    
    svn path=/trunk/mcs/; revision=93496

---
+++ mcs/tests/gtest-exmethod-16.cs
@@ -0,0 +1,37 @@
+using System;
+
+static class Rocks
+{
+	public static bool Extension (this string self)
+	{
+		return true;
+	}
+	
+	public static bool Extension (this D self)
+	{
+		return true;
+	}
+}
+
+delegate string D ();
+
+class Program
+{
+	event D e;
+	
+	public string this [int index] {
+		get { return "HelloWorld"; }
+	}
+	
+	public string Property {
+		get { return "a"; }
+	}
+	
+	static void Main (string [] args)
+	{
+		Program p = new Program ();
+		p [0].Extension ();
+		p.Property.Extension ();
+		p.e.Extension ();
+	}
+}
