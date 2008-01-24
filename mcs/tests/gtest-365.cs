77cd1bd16f8735a1219f615bc0ab4366483a458a
Marek Safar <marek.safar@gmail.com>
2008-01-24 15:30:42 +0000

    New test.
    svn path=/trunk/mcs/; revision=93803

---
+++ mcs/tests/gtest-365.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace B
+{
+	class Program
+	{
+		static int Main()
+		{
+			int? i = 0;
+			bool b = i == (int?)null;
+			if (b)
+				return 1;
+			
+			if (i == (int?)null)
+				return 2;
+				
+			Console.WriteLine (b);
+			return 0;
+		}
+	}
+}
+
