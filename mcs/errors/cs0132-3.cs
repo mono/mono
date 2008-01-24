1438ddc8f3feedf41c52273897b4fc7cd0c72d67
Marek Safar <marek.safar@gmail.com>
2008-01-24 10:27:46 +0000

    New test.
    svn path=/trunk/mcs/; revision=93782

---
+++ mcs/errors/cs0132-3.cs
@@ -0,0 +1,17 @@
+// CS0132: `Foo.Foo(int)': The static constructor must be parameterless
+// Line: 10
+
+class Foo
+{
+	static Foo ()
+	{
+	}
+
+	static Foo (int x)
+	{
+	}
+
+	static void Main ()
+	{
+	}
+}
