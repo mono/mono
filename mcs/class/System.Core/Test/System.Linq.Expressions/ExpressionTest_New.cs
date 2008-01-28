b4978c582182f7d5f611c20495c7fe405b12fb8e
Jb Evain <jbevain@gmail.com>
2008-01-28 22:27:51 +0000

    add some tests for New
    svn path=/trunk/mcs/; revision=94174

---
+++ mcs/class/System.Core/Test/System.Linq.Expressions/ExpressionTest_New.cs
@@ -0,0 +1,124 @@
+//
+// ExpressionTest_New.cs
+//
+// Author:
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.Linq.Expressions {
+
+	[TestFixture]
+	public class ExpressionTest_New {
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void NullType ()
+		{
+			Expression.New (null as Type);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void NullConstructor ()
+		{
+			Expression.New (null as ConstructorInfo);
+		}
+
+		public class Foo {
+
+			public Foo (string s)
+			{
+			}
+		}
+
+		public class Bar {
+
+			public string Value { get; set; }
+
+			public Bar ()
+			{
+			}
+		}
+
+		public struct Baz {
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void NoParameterlessConstructor ()
+		{
+			Expression.New (typeof (Foo));
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void ConstructorHasTooMuchParameters ()
+		{
+			Expression.New (typeof (Foo).GetConstructor (new [] { typeof (string) }));
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void HasNullArgument ()
+		{
+			Expression.New (typeof (Foo).GetConstructor (new [] { typeof (string) }), null as Expression);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentException))]
+		public void HasWrongArgument ()
+		{
+			Expression.New (typeof (Foo).GetConstructor (new [] { typeof (string) }), Expression.Constant (12));
+		}
+
+		[Test]
+		public void NewFoo ()
+		{
+			var n = Expression.New (typeof (Foo).GetConstructor (new [] { typeof (string) }), Expression.Constant ("foo"));
+
+			Assert.AreEqual (ExpressionType.New, n.NodeType);
+			Assert.AreEqual (typeof (Foo), n.Type);
+			Assert.AreEqual ("new Foo(\"foo\")", n.ToString ());
+		}
+
+		[Test]
+		public void NewBar ()
+		{
+			var n = Expression.New (typeof (Bar));
+
+			Assert.AreEqual ("new Bar()", n.ToString ());
+
+			n = Expression.New (typeof (Bar).GetConstructor (Type.EmptyTypes));
+
+			Assert.AreEqual ("new Bar()", n.ToString ());
+		}
+	}
+}
