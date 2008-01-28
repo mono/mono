1c39f6951286706b986de4539d0e1882d9ee591e
Andreia Gaita <avidigal@novell.com>
2008-01-28 01:41:15 +0000

    * Assembly/AssemblyInfo.cs: Version change
    2008-01-28  Andreia Gaita <avidigal@novell.com>
    
    svn path=/trunk/mcs/; revision=94105

--- mcs/class/Mono.Mozilla/Assembly/AssemblyInfo.cs
+++ mcs/class/Mono.Mozilla/Assembly/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("Development version")]
 [assembly: AssemblyCompany("MONO development team")]
 [assembly: AssemblyProduct("Mono.Mozilla")]
-[assembly: AssemblyCopyright("(c) 2007 Andreia Gaita")]
+[assembly: AssemblyCopyright("(c) 2007, 2008 Andreia Gaita")]
 [assembly: AssemblyTrademark("")]
 
 //
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
