c253cee2fab4b7e998fba88f4b3c243ba2d8ed3d
Marek Safar <marek.safar@gmail.com>
2008-02-08 13:53:06 +0000

    2008-02-08  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #359749
     	
    
    svn path=/trunk/mcs/; revision=95246

---
+++ mcs/tests/gtest-exmethod-18.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Foo {
+	public bool IsBar {
+		get { return false; }
+	}
+}
+
+static class FooExt {
+
+	public static bool IsBar (this Foo f)
+	{
+		return f.IsBar;
+	}
+}
+
+class Repro {
+
+	static void Main ()
+	{
+		var f = new Foo ();
+		Console.WriteLine (f.IsBar ());
+	}
+}
