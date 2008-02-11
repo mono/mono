5858f29d4dcb5c607e447d1a95e9807d97c0a364
Ivan Zlatev <ivan@ivanz.com>
2008-02-11 16:28:45 +0000

    2008-02-11  Ivan N. Zlatev  <contact@i-nz.net>
    	* AnchorEditor.cs: More sensible default width.
    
    
    svn path=/trunk/mcs/; revision=95446

--- mcs/class/System.Design/System.Windows.Forms.Design/AnchorEditor.cs
+++ mcs/class/System.Design/System.Windows.Forms.Design/AnchorEditor.cs
@@ -97,7 +97,7 @@ namespace System.Windows.Forms.Design
 	        {
 	            this.SuspendLayout();
 	            this.Name = "AnchorSelector";
-	            this.Size = new System.Drawing.Size (40, 120);
+	            this.Size = new System.Drawing.Size (150, 120);
 	            this.ResumeLayout(false);
 	        }
 
