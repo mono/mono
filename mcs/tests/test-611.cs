7b2c7d0a5ecc206ff7bc4d4c148ca2cae9deadea
Marek Safar <marek.safar@gmail.com>
2008-02-14 11:24:05 +0000

    2008-02-14  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #361686
    	
    
    svn path=/trunk/mcs/; revision=95614

---
+++ mcs/tests/test-611.cs
@@ -0,0 +1,36 @@
+public class T
+{
+	protected class Protected
+	{
+	}
+}
+
+public class D : T
+{
+	private class Private
+	{
+		public void Stuff (Protected p)
+		{
+		}
+	}
+}
+
+public class D2 : T
+{
+	public class P
+	{
+		private class Private
+		{
+			public void Stuff (Protected p)
+			{
+			}
+		}
+	}
+}
+
+public class Z
+{
+	static void Main ()
+	{
+	}
+}
