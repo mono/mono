21d318379c7e882e228f3a5e0046e01cbcea2b05
Gert Driesen <drieseng@users.sourceforge.net>
2008-02-17 19:08:47 +0000

    * CharConverterTest.cs: Added tests for ConvertFrom and ConvertTo. * CultureInfoConverterTest.cs: Added tests for ConvertFrom and ConvertTo. * CharConverter.cs (ConvertFrom): Avoid NRE when value is null. Provide more info when left of value > 1. * CultureInfoConverter.cs (ConvertFrom): Avoid NRE when value is null. On 2.0 profile, use case-sensitive comparison for (Default) value. Use case-insensitive comparing when value is considered as displayname. Provide more info in exception message. (ConvertTo): Use '(Default)' instead of '(default)' for invariant culture. * System_test.dll.sources: Added CharConverterTest.cs and CultureInfoConverterTest.cs.
    svn path=/trunk/mcs/; revision=95995

---
+++ mcs/class/System/Test/System.ComponentModel/CharConverterTest.cs
@@ -0,0 +1,147 @@
+//
+// System.ComponentModel.CharConverter test cases
+//
+// Authors:
+// 	Gert Driesen (drieseng@users.sourceforge.net)
+//
+// (c) 2008 Gert Driesen
+//
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.ComponentModel
+{
+	[TestFixture]
+	public class CharConverterTest
+	{
+		private CharConverter converter;
+		private string pattern;
+		
+		[SetUp]
+		public void SetUp ()
+		{
+			converter = new CharConverter ();
+
+			DateTimeFormatInfo info = CultureInfo.CurrentCulture.DateTimeFormat;
+			pattern = info.ShortDatePattern + " " + info.ShortTimePattern;
+		}
+
+		[Test]
+		public void CanConvertFrom ()
+		{
+			Assert.IsTrue (converter.CanConvertFrom (typeof (string)), "#1");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (char)), "#2");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (object)), "#3");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (int)), "#4");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (char [])), "#5");
+			Assert.IsTrue (converter.CanConvertFrom (typeof (InstanceDescriptor)), "#6");
+		}
+
+		[Test]
+		public void CanConvertTo ()
+		{
+			Assert.IsTrue (converter.CanConvertTo (typeof (string)), "#1");
+			Assert.IsFalse (converter.CanConvertTo (typeof (char)), "#2");
+			Assert.IsFalse (converter.CanConvertTo (typeof (object)), "#3");
+			Assert.IsFalse (converter.CanConvertTo (typeof (int)), "#4");
+			Assert.IsFalse (converter.CanConvertTo (typeof (char [])), "#5");
+			Assert.IsFalse (converter.CanConvertTo (typeof (InstanceDescriptor)), "#6");
+		}
+
+		[Test]
+		public void ConvertFrom_String ()
+		{
+			char c;
+
+			c = (char) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				String.Empty);
+			Assert.AreEqual ('\0', c, "#1");
+
+			c = (char) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"e");
+			Assert.AreEqual ('e', c, "#2");
+
+			c = (char) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"\t f\r\n ");
+			Assert.AreEqual ('f', c, "#3");
+		}
+
+		[Test]
+		public void ConvertFrom_String_Invalid ()
+		{
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					"ef");
+				Assert.Fail ("#A1");
+			} catch (FormatException ex) {
+				// ef is not a valid value for Char
+				Assert.AreEqual (typeof (FormatException), ex.GetType (), "#A2");
+				Assert.IsNull (ex.InnerException, "#A3");
+				Assert.IsNotNull (ex.Message, "#A4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (char).Name) != -1, "#A5");
+				Assert.IsTrue (ex.Message.IndexOf ("ef") != -1, "#A6");
+			}
+
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					"\ref\n");
+				Assert.Fail ("#B1");
+			} catch (FormatException ex) {
+				// \ref\n is not a valid value for Char
+				Assert.AreEqual (typeof (FormatException), ex.GetType (), "#B2");
+				Assert.IsNull (ex.InnerException, "#B3");
+				Assert.IsNotNull (ex.Message, "#B4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (char).Name) != -1, "#B5");
+				Assert.IsTrue (ex.Message.IndexOf ("\ref\n") != -1, "#B6");
+			}
+		}
+
+		[Test]
+		public void ConvertFrom_Value_Null ()
+		{
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					(string) null);
+				Assert.Fail ("#1");
+			} catch (NotSupportedException ex) {
+				// CharConverter cannot convert from (null)
+				Assert.AreEqual (typeof (NotSupportedException), ex.GetType (), "#2");
+				Assert.IsNull (ex.InnerException, "#3");
+				Assert.IsNotNull (ex.Message, "#4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CharConverter).Name) != -1, "#5");
+				Assert.IsTrue (ex.Message.IndexOf ("(null)") != -1, "#6");
+			}
+		}
+
+		[Test]
+		public void ConvertToString ()
+		{
+			string result;
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				' ');
+			Assert.AreEqual (" ", result, "#1");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				'\0');
+			Assert.AreEqual (string.Empty, result, "#2");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				'f');
+			Assert.AreEqual ("f", result, "#3");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				null);
+			Assert.AreEqual (string.Empty, result, "#4");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				new char [] { 'a', 'f' });
+			Assert.AreEqual ("System.Char[]", result, "#5");
+		}
+	}
+}
---
+++ mcs/class/System/Test/System.ComponentModel/CultureInfoConverterTest.cs
@@ -0,0 +1,211 @@
+//
+// System.ComponentModel.CultureInfoConverter test cases
+//
+// Authors:
+// 	Gert Driesen (drieseng@users.sourceforge.net)
+//
+// (c) 2008 Gert Driesen
+//
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.ComponentModel
+{
+	[TestFixture]
+	public class CultureInfoConverterTest
+	{
+		private CultureInfoConverter converter;
+		
+		[SetUp]
+		public void SetUp ()
+		{
+			converter = new CultureInfoConverter ();
+		}
+
+		[Test]
+		public void CanConvertFrom ()
+		{
+			Assert.IsTrue (converter.CanConvertFrom (typeof (string)), "#1");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (CultureInfo)), "#2");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (object)), "#3");
+			Assert.IsFalse (converter.CanConvertFrom (typeof (int)), "#4");
+			Assert.IsTrue (converter.CanConvertFrom (typeof (InstanceDescriptor)), "#5");
+		}
+
+		[Test]
+		public void CanConvertTo ()
+		{
+			Assert.IsTrue (converter.CanConvertTo (typeof (string)), "#1");
+			Assert.IsFalse (converter.CanConvertTo (typeof (object)), "#2");
+			Assert.IsFalse (converter.CanConvertTo (typeof (CultureInfo)), "#3");
+			Assert.IsFalse (converter.CanConvertTo (typeof (int)), "#4");
+			Assert.IsTrue (converter.CanConvertTo (typeof (InstanceDescriptor)), "#5");
+		}
+
+		[Test]
+		public void ConvertFrom_String ()
+		{
+			CultureInfo c;
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				String.Empty);
+			Assert.AreEqual (CultureInfo.InvariantCulture, c, "#1");
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"nl-BE");
+			Assert.AreEqual (new CultureInfo ("nl-BE"), c, "#2");
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"Dut");
+			Assert.AreEqual (new CultureInfo ("nl"), c, "#3");
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"Dutch (Bel");
+			Assert.AreEqual (new CultureInfo ("nl-BE"), c, "#4");
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"duTcH (Bel");
+			Assert.AreEqual (new CultureInfo ("nl-BE"), c, "#5");
+
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"(Default)");
+			Assert.AreEqual (CultureInfo.InvariantCulture, c, "#6");
+
+#if ONLY_1_1
+			c = (CultureInfo) converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+				"(defAuLt)");
+			Assert.AreEqual (CultureInfo.InvariantCulture, c, "#6");
+#endif
+		}
+
+		[Test]
+		public void ConvertFrom_String_IncompleteName ()
+		{
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					"nl-B");
+				Assert.Fail ("#1");
+			} catch (ArgumentException ex) {
+				// The nl-B culture cannot be converted to a
+				// CultureInfo object on this computer
+				Assert.AreEqual (typeof (ArgumentException), ex.GetType (), "#2");
+				Assert.IsNull (ex.InnerException, "#3");
+				Assert.IsNotNull (ex.Message, "#4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CultureInfo).Name) != -1, "#5");
+				Assert.IsTrue (ex.Message.IndexOf ("nl-B") != -1, "#6");
+				Assert.IsNull (ex.ParamName, "#7");
+			}
+		}
+
+		[Test]
+		public void ConvertFrom_String_InvalidCulture ()
+		{
+#if NET_2_0
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					"(default)");
+				Assert.Fail ("#A1");
+			} catch (ArgumentException ex) {
+				// The (default) culture cannot be converted to
+				// a CultureInfo object on this computer
+				Assert.AreEqual (typeof (ArgumentException), ex.GetType (), "#A2");
+				Assert.IsNull (ex.InnerException, "#A3");
+				Assert.IsNotNull (ex.Message, "#A4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CultureInfo).Name) != -1, "#A5");
+				Assert.IsTrue (ex.Message.IndexOf ("(default)") != -1, "#A6");
+				Assert.IsNull (ex.ParamName, "#A7");
+			}
+#endif
+
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					" ");
+				Assert.Fail ("#B1");
+			} catch (ArgumentException ex) {
+				// The   culture cannot be converted to
+				// a CultureInfo object on this computer
+				Assert.AreEqual (typeof (ArgumentException), ex.GetType (), "#B2");
+				Assert.IsNull (ex.InnerException, "#B3");
+				Assert.IsNotNull (ex.Message, "#B4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CultureInfo).Name) != -1, "#B5");
+				Assert.IsTrue (ex.Message.IndexOf ("   ") != -1, "#B6");
+				Assert.IsNull (ex.ParamName, "#B7");
+			}
+
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					"\r\n");
+				Assert.Fail ("#C1");
+			} catch (ArgumentException ex) {
+				// The \r\n culture cannot be converted to
+				// a CultureInfo object on this computer
+				Assert.AreEqual (typeof (ArgumentException), ex.GetType (), "#C2");
+				Assert.IsNull (ex.InnerException, "#C3");
+				Assert.IsNotNull (ex.Message, "#C4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CultureInfo).Name) != -1, "#C5");
+				Assert.IsTrue (ex.Message.IndexOf ("\r\n") != -1, "#C6");
+				Assert.IsNull (ex.ParamName, "#C7");
+			}
+		}
+
+		[Test]
+		public void ConvertFrom_Value_Null ()
+		{
+			try {
+				converter.ConvertFrom (null, CultureInfo.InvariantCulture,
+					(string) null);
+				Assert.Fail ("#1");
+			} catch (NotSupportedException ex) {
+				// CultureInfoConverter cannot convert from (null)
+				Assert.AreEqual (typeof (NotSupportedException), ex.GetType (), "#2");
+				Assert.IsNull (ex.InnerException, "#3");
+				Assert.IsNotNull (ex.Message, "#4");
+				Assert.IsTrue (ex.Message.IndexOf (typeof (CultureInfoConverter).Name) != -1, "#5");
+				Assert.IsTrue (ex.Message.IndexOf ("(null)") != -1, "#6");
+			}
+		}
+
+		[Test]
+		public void ConvertToString ()
+		{
+			string result;
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				new MyCultureInfo ());
+			Assert.AreEqual ("display", result, "#1");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				null);
+			Assert.AreEqual ("(Default)", result, "#2");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				CultureInfo.InvariantCulture);
+			Assert.AreEqual ("(Default)", result, "#3");
+
+			result = converter.ConvertToString (null, CultureInfo.InvariantCulture,
+				new CultureInfo ("nl-BE"));
+			Assert.AreEqual ("Dutch (Belgium)", result, "#4");
+		}
+
+		[Serializable]
+		private sealed class MyCultureInfo : CultureInfo
+		{
+			internal MyCultureInfo () : base ("nl-BE")
+			{
+			}
+
+			public override string DisplayName {
+				get { return "display"; }
+			}
+
+			public override string EnglishName {
+				get { return "english"; }
+			}
+		}
+	}
+}
