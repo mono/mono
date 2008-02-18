dddefab0181b0c7a8c5c2f0289c344f37876dc67
Marek Safar <marek.safar@gmail.com>
2008-02-18 11:12:34 +0000

    New test.
    svn path=/trunk/mcs/; revision=96045

---
+++ mcs/errors/cs0649-4.cs
@@ -0,0 +1,8 @@
+// CS0649: Field `X.x' is never assigned to, and will always have its default value `0'
+// Line: 4
+// Compiler options: -warnaserror -warn:4
+
+class X
+{
+	public readonly int x;
+}
