40dd58615d1ee52419f659f04dbf34750662e6e0
Ivan Zlatev <ivan@ivanz.com>
2008-03-01 23:16:52 +0000

    2008-03-01  Ivan N. Zlatev  <contact@i-nz.net>
    	* MenuCommandService.cs: implemeneted
    	* DesignerHost.cs: Do not fire ComponentChanging/ed events
    	when removing a component.
    	* DesignModeSite: Naming fixes.
    	* UndoEngine.cs: A lot of bugfixes.
    	* SelectionService: Set root component as selected if the 
    	last remaining component from the current selection has been
    	removed.
    
    
    svn path=/branches/mono-1-9/mcs/; revision=97104

--- mcs/class/System.Design/System.ComponentModel.Design/MenuCommandService.cs
+++ mcs/class/System.Design/System.ComponentModel.Design/MenuCommandService.cs
@@ -3,8 +3,10 @@
 //
 // Author:
 //      Atsushi Enomoto  <atsushi@ximian.com>
+//	Ivan N. Zlatev   <contact@i-nz.net>
 //
 // Copyright (C) 2007 Novell, Inc
+// Copyright (C) Ivan N. Zlatev
 //
 
 //
@@ -27,37 +29,71 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
 #if NET_2_0
+
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.ComponentModel;
 
 namespace System.ComponentModel.Design
 {
 	public class MenuCommandService : IMenuCommandService, IDisposable
 	{
-		[MonoTODO]
+		private IServiceProvider _serviceProvider;
+		private DesignerVerbCollection _globalVerbs;
+		private DesignerVerbCollection _verbs;
+		private Dictionary <CommandID, MenuCommand> _commands;
+
 		public MenuCommandService (IServiceProvider serviceProvider)
 		{
-			throw new NotImplementedException ();
+			if (serviceProvider == null)
+				throw new ArgumentNullException ("serviceProvider");
+			_serviceProvider = serviceProvider;
+			ISelectionService selectionSvc = _serviceProvider.GetService (typeof (ISelectionService)) as ISelectionService;
+			if (selectionSvc != null)
+				selectionSvc.SelectionChanged += OnSelectionChanged;
+		}
+
+		private void OnSelectionChanged (object sender, EventArgs arg)
+		{
+			this.OnCommandsChanged (new MenuCommandsChangedEventArgs (MenuCommandsChangedType.CommandChanged, null));
+			// The commands will be updated whenever they are requested/modified.
 		}
 
 		public event MenuCommandsChangedEventHandler MenuCommandsChanged;
 
-		[MonoTODO]
 		public virtual DesignerVerbCollection Verbs {
-			get { throw new NotImplementedException (); }
+			get {
+				this.EnsureVerbs ();
+				return _verbs;
+			}
 		}
 
-		[MonoTODO]
 		public virtual void AddCommand (MenuCommand command)
 		{
-			throw new NotImplementedException ();
+			if (command == null)
+				throw new ArgumentNullException ("command");
+
+			if (_commands == null)
+				_commands = new Dictionary <CommandID, MenuCommand> ();
+			_commands.Add (command.CommandID, command);
+			this.OnCommandsChanged (new MenuCommandsChangedEventArgs (MenuCommandsChangedType.CommandAdded, command));
 		}
 
-		[MonoTODO]
 		public virtual void AddVerb (DesignerVerb verb)
 		{
-			throw new NotImplementedException ();
+			if (verb == null)
+				throw new ArgumentNullException ("verb");
+			this.EnsureVerbs ();
+			if (!_verbs.Contains (verb)) {
+				if (_globalVerbs == null)
+					_globalVerbs = new DesignerVerbCollection ();
+				_globalVerbs.Add (verb);
+			}
+			this.OnCommandsChanged (new MenuCommandsChangedEventArgs (MenuCommandsChangedType.CommandAdded, verb));
 		}
 
 		public void Dispose ()
@@ -65,51 +101,127 @@ namespace System.ComponentModel.Design
 			Dispose (true);
 		}
 
-		[MonoTODO]
 		protected virtual void Dispose (bool disposing)
 		{
+			if (disposing) {
+				if (_globalVerbs != null) {
+					_globalVerbs.Clear ();
+					_globalVerbs = null;
+				}
+				if (_verbs != null) {
+					_verbs.Clear ();
+					_verbs = null;
+				}
+				if (_commands != null) {
+					_commands.Clear ();
+					_commands = null;
+				}
+				if (_serviceProvider != null) {
+					ISelectionService selectionSvc = _serviceProvider.GetService (typeof (ISelectionService)) as ISelectionService;
+					if (selectionSvc != null)
+						selectionSvc.SelectionChanged -= OnSelectionChanged;
+					_serviceProvider = null;
+				}
+			}
 		}
 
-		[MonoTODO]
 		protected void EnsureVerbs ()
 		{
-			throw new NotImplementedException ();
+			DesignerVerbCollection selectionVerbs = null;
+
+			ISelectionService selectionSvc = this.GetService (typeof (ISelectionService)) as ISelectionService;
+			IDesignerHost host = this.GetService (typeof (IDesignerHost)) as IDesignerHost;
+			if (selectionSvc != null && host != null && selectionSvc.SelectionCount == 1) {
+				IComponent primarySelection = selectionSvc.PrimarySelection as IComponent;
+				if (primarySelection != null) {
+					IDesigner designer = host.GetDesigner (primarySelection);
+					if (designer != null)
+						selectionVerbs = designer.Verbs;
+				}
+			}
+
+			// Designer provided verbs have the higher precedence than the global
+			//
+			Dictionary <string, DesignerVerb> allVerbs = new Dictionary <string, DesignerVerb> ();
+			if (_globalVerbs != null) {
+				foreach (DesignerVerb verb in _globalVerbs)
+					allVerbs[verb.Text] = verb;
+			}
+			if (selectionVerbs != null) {
+				foreach (DesignerVerb verb in selectionVerbs)
+					allVerbs[verb.Text] = verb;
+			}
+
+			if (_verbs == null)
+				_verbs = new DesignerVerbCollection ();
+			else
+				_verbs.Clear ();
+
+			foreach (DesignerVerb verb in allVerbs.Values)
+				_verbs.Add (verb);
 		}
 
-		[MonoTODO]
-		public MenuCommand FindCommand (CommandID commandID)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
 		protected MenuCommand FindCommand (Guid guid, int id)
 		{
-			throw new NotImplementedException ();
+			return this.FindCommand (new CommandID (guid, id));
 		}
 
-		[MonoTODO]
+		public MenuCommand FindCommand (CommandID commandID)
+		{
+			if (commandID == null)
+				throw new ArgumentNullException ("commandID");
+
+			MenuCommand command = null;
+			if (_commands != null)
+				_commands.TryGetValue (commandID, out command);
+			if (command == null) {
+				this.EnsureVerbs ();
+				foreach (DesignerVerb verb in _verbs) {
+					if (verb.CommandID.Equals (commandID)) {
+						command = (MenuCommand) verb;
+						break;
+					}
+				}
+			}
+			return command;
+		}
+
 		protected ICollection GetCommandList (Guid guid)
 		{
-			throw new NotImplementedException ();
+			List<MenuCommand> list = new List<MenuCommand> ();
+			if (_commands != null) {
+				foreach (MenuCommand command in _commands.Values) {
+					if (command.CommandID.Guid == guid)
+						list.Add (command);
+				}
+			}
+			return list;
 		}
 
-		[MonoTODO]
-		protected object GetService (Type serviceType)
-		{
-			throw new NotImplementedException ();
-		}
-
-		[MonoTODO]
 		public virtual bool GlobalInvoke (CommandID commandID)
 		{
-			throw new NotImplementedException ();
+			if (commandID == null)
+				throw new ArgumentNullException ("commandID");
+
+			MenuCommand command = this.FindCommand (commandID);
+			if (command != null) {
+				command.Invoke ();
+				return true;
+			}
+			return false;
 		}
 
-		[MonoTODO]
 		public virtual bool GlobalInvoke (CommandID commandID, object arg)
 		{
-			throw new NotImplementedException ();
+			if (commandID == null)
+				throw new ArgumentNullException ("commandID");
+
+			MenuCommand command = this.FindCommand (commandID);
+			if (command != null) {
+				command.Invoke (arg);
+				return true;
+			}
+			return false;
 		}
 
 		protected virtual void OnCommandsChanged (MenuCommandsChangedEventArgs e)
@@ -118,22 +230,36 @@ namespace System.ComponentModel.Design
 				MenuCommandsChanged (this, e);
 		}
 
-		[MonoTODO]
 		public virtual void RemoveCommand (MenuCommand command)
 		{
-			throw new NotImplementedException ();
+			if (command == null)
+				throw new ArgumentNullException ("command");
+			if (_commands != null)
+				_commands.Remove (command.CommandID);
+
+			this.OnCommandsChanged (new MenuCommandsChangedEventArgs (MenuCommandsChangedType.CommandRemoved, null));
 		}
 
-		[MonoTODO]
 		public virtual void RemoveVerb (DesignerVerb verb)
 		{
-			throw new NotImplementedException ();
+			if (verb == null)
+				throw new ArgumentNullException ("verb");
+
+			if (_globalVerbs.Contains (verb))
+				_globalVerbs.Remove (verb);
+
+			this.OnCommandsChanged (new MenuCommandsChangedEventArgs (MenuCommandsChangedType.CommandRemoved, verb));
 		}
 
-		[MonoTODO]
 		public virtual void ShowContextMenu (CommandID menuID, int x, int y)
 		{
-			throw new NotImplementedException ();
+		}
+
+		protected object GetService (Type serviceType)
+		{
+			if (_serviceProvider != null)
+				return _serviceProvider.GetService (serviceType);
+			return null;
 		}
 	}
 }
--- mcs/class/System.Design/System.ComponentModel.Design/UndoEngine.cs
+++ mcs/class/System.Design/System.ComponentModel.Design/UndoEngine.cs
@@ -108,8 +108,9 @@ namespace System.ComponentModel.Design
 
 		private void OnTransactionClosed (object sender, DesignerTransactionCloseEventArgs args)
 		{
+			// Console.WriteLine ("TransactionClosed: Commited: " + args.TransactionCommitted.ToString ());
 			IDesignerHost host = GetRequiredService (typeof (IDesignerHost)) as IDesignerHost;
-			if (!host.InTransaction) { // the "top-most" transaction was closed (currentUnit one)
+			if (!host.InTransaction) { // the "top-most"/last transaction was closed (currentUnit one)
 				_currentUnit.Close ();
 				if (args.TransactionCommitted) {
 					AddUndoUnit (_currentUnit);
@@ -166,14 +167,18 @@ namespace System.ComponentModel.Design
 		private void OnComponentChanging (object sender, ComponentChangingEventArgs args)
 		{
 			if (_currentUnit == null)
-				_currentUnit = CreateUndoUnit ("Modify " + ((IComponent)args.Component).Site.Name + "." + args.Member.Name, true);
+				_currentUnit = CreateUndoUnit ("Modify " + ((IComponent)args.Component).Site.Name + 
+							       (args.Member != null ? "." + args.Member.Name : ""), 
+							       true);
 			_currentUnit.ComponentChanging (args);
 		}
 
 		private void OnComponentChanged (object sender, ComponentChangedEventArgs args)
 		{
 			if (_currentUnit == null)
-				_currentUnit = CreateUndoUnit ("Modify " + ((IComponent)args.Component).Site.Name + "." + args.Member.Name, true);
+				_currentUnit = CreateUndoUnit ("Modify " + ((IComponent)args.Component).Site.Name + "." + 
+							       (args.Member != null ? "." + args.Member.Name : ""), 
+							       true);
 			_currentUnit.ComponentChanged (args);
 
 			IDesignerHost host = GetRequiredService (typeof (IDesignerHost)) as IDesignerHost;
@@ -217,6 +222,7 @@ namespace System.ComponentModel.Design
 
 		protected virtual UndoEngine.UndoUnit CreateUndoUnit (string name, bool primary)
 		{
+			// Console.WriteLine ("CreateUndoUnit: " + name);
 			return new UndoUnit (this, name);
 		}
 
@@ -274,6 +280,7 @@ namespace System.ComponentModel.Design
 
 		protected virtual void DiscardUndoUnit (UndoEngine.UndoUnit unit)
 		{
+			// Console.WriteLine ("DiscardUndoUnit: " + unit.Name);
 		}
 
 
@@ -293,22 +300,20 @@ namespace System.ComponentModel.Design
 
 				public ComponentRenameAction (string currentName, string oldName)
 				{
+					// Console.WriteLine ("ComponentRenameAction (" + oldName + "): " + currentName);
 					_currentName = currentName;
 					_oldName = oldName;
 				}
 
 				public override void Undo (UndoEngine engine)
 				{
-					IComponentChangeService changeService = engine.GetRequiredService (typeof (IComponentChangeService)) as IComponentChangeService;
+					// Console.WriteLine ("ComponentRenameAction.Undo (" + _currentName + "): " + _oldName);
 					IDesignerHost host = engine.GetRequiredService (typeof (IDesignerHost)) as IDesignerHost;
-
 					IComponent component = host.Container.Components[_currentName];
-					changeService.OnComponentChanging (component, null);
 					component.Site.Name = _oldName;
 					string tmp = _currentName;
 					_currentName = _oldName;
 					_oldName = tmp;
-					changeService.OnComponentChanged (component, null, null, null);
 				}
 			} // ComponentRenameAction
 
@@ -322,6 +327,8 @@ namespace System.ComponentModel.Design
 				{
 					if (component == null)
 						throw new ArgumentNullException ("component");
+					// Console.WriteLine ((added ? "Component*Add*RemoveAction" : "ComponentAdd*Remove*Action") +
+					// 		   " (" + component.Site.Name + ")");
 					ComponentSerializationService serializationService = engine.GetRequiredService (
 						typeof (ComponentSerializationService)) as ComponentSerializationService;
 
@@ -337,9 +344,13 @@ namespace System.ComponentModel.Design
 				{
 					IDesignerHost host = engine.GetRequiredService (typeof (IDesignerHost)) as IDesignerHost;
 					if (_added) {
-						host.DestroyComponent (host.Container.Components[_componentName]);
+						// Console.WriteLine ("Component*Add*RemoveAction.Undo (" + _componentName + ")");
+						IComponent component = host.Container.Components[_componentName];
+						if (component != null) // the component might have been destroyed already
+							host.DestroyComponent (component);
 						_added = false;
 					} else {
+						// Console.WriteLine ("ComponentAdd*Remove*Action.Undo (" + _componentName + ")");
 						ComponentSerializationService serializationService = engine.GetRequiredService (
 							typeof (ComponentSerializationService)) as ComponentSerializationService;
 
@@ -352,8 +363,11 @@ namespace System.ComponentModel.Design
 
 			private class ComponentChangeAction : Action
 			{
-				private SerializationStore _currentState;
-				private SerializationStore _oldState;
+				private string _componentName;
+				private MemberDescriptor _member;
+				private IComponent _component;
+				private SerializationStore _afterChange;
+				private SerializationStore _beforeChange;
 
 				public ComponentChangeAction ()
 				{
@@ -361,40 +375,78 @@ namespace System.ComponentModel.Design
 
 				public void SetOriginalState (UndoEngine engine, IComponent component, MemberDescriptor member)
 				{
+					_member = member;
+					_component = component;
+					_componentName = component.Site != null ? component.Site.Name : null;
+					// Console.WriteLine ("ComponentChangeAction.SetOriginalState (" + (_componentName != null ? (_componentName + ".") : "") +
+					// 		   member.Name + "): " +
+					// 		   (((PropertyDescriptor)member).GetValue (component) == null ? "null" :
+					// 		   ((PropertyDescriptor)member).GetValue (component).ToString ()));
 					ComponentSerializationService serializationService = engine.GetRequiredService (
 						typeof (ComponentSerializationService)) as ComponentSerializationService;
-					_oldState = serializationService.CreateStore ();
-					serializationService.SerializeMember (_oldState, component, member);
-					_oldState.Close ();
+					_beforeChange = serializationService.CreateStore ();
+					serializationService.SerializeMemberAbsolute (_beforeChange, component, member);
+					_beforeChange.Close ();
 				}
 
 
 				public void SetModifiedState (UndoEngine engine, IComponent component, MemberDescriptor member)
 				{
+					// Console.WriteLine ("ComponentChangeAction.SetModifiedState (" + (_componentName != null ? (_componentName + ".") : "") +
+					// 		   member.Name + "): " +
+					// 		   (((PropertyDescriptor)member).GetValue (component) == null ? "null" :
+					// 		   ((PropertyDescriptor)member).GetValue (component).ToString ()));
 					ComponentSerializationService serializationService = engine.GetRequiredService (
 						typeof (ComponentSerializationService)) as ComponentSerializationService;
-					_currentState = serializationService.CreateStore ();
-					serializationService.SerializeMember (_currentState, component, member);
-					_currentState.Close ();
+					_afterChange = serializationService.CreateStore ();
+					serializationService.SerializeMemberAbsolute (_afterChange, component, member);
+					_afterChange.Close ();
 				}
 
+				public bool IsComplete {
+					get { return (_beforeChange != null && _afterChange != null); }
+				}
+
+				public string ComponentName {
+					get { return _componentName; }
+				}
+
+				public IComponent Component {
+					get { return _component; }
+				}
+
+				public MemberDescriptor Member {
+					get { return _member; }
+				}
+
+				// Reminder: _component might no longer be a valid instance
+				// so one should request a new one.
+				// 
 				public override void Undo (UndoEngine engine)
 				{
+					if (_beforeChange == null) {
+						// Console.WriteLine ("ComponentChangeAction.Undo: ERROR: UndoUnit is not complete.");
+						return;
+					}
+
+					// Console.WriteLine ("ComponentChangeAction.Undo (" + _componentName + "." + _member.Name + ")");
+					IDesignerHost host = (IDesignerHost)engine.GetRequiredService (typeof(IDesignerHost));
+					_component = host.Container.Components[_componentName];
+
 					ComponentSerializationService serializationService = engine.GetRequiredService (
 						typeof (ComponentSerializationService)) as ComponentSerializationService;
-					IDesignerHost host = engine.GetRequiredService (typeof (IDesignerHost)) as IDesignerHost;
-					serializationService.Deserialize (_oldState, host.Container);
+					serializationService.DeserializeTo (_beforeChange, host.Container);
 
-					SerializationStore tmp = _currentState;
-					_currentState = _oldState;
-					_oldState = tmp;
+					SerializationStore tmp = _beforeChange;
+					_beforeChange = _afterChange;
+					_afterChange = tmp;
 				}
 			} // ComponentChangeAction
 
 			private UndoEngine _engine;
 			private string _name;
 			private bool _closed;
-			private Stack <Action> _actions;
+			private List<Action> _actions;
 
 			public UndoUnit (UndoEngine engine, string name)
 			{
@@ -402,11 +454,10 @@ namespace System.ComponentModel.Design
 					throw new ArgumentNullException ("engine");
 				if (name == null)
 					throw new ArgumentNullException ("name");
-					//name = String.Empty;
 
 				_engine = engine;
 				_name = name;
-				_actions = new Stack <Action> ();
+				_actions = new List <Action> ();
 			}
 
 			public void Undo ()
@@ -418,8 +469,14 @@ namespace System.ComponentModel.Design
 
 			protected virtual void UndoCore ()
 			{
-				foreach (Action action in _actions)
-					action.Undo (_engine);
+				for (int i = _actions.Count - 1; i >= 0; i--) {
+					// Console.WriteLine ("Undoing action type: " + _actions[i].GetType ().Name);
+					_actions[i].Undo (_engine);
+				}
+				// Also reverses the stack of actions, so that
+				// if Undo is called twice it will Redo in the proper order
+				// 
+				_actions.Reverse ();
 			}
 
 			protected UndoEngine UndoEngine {
@@ -436,13 +493,16 @@ namespace System.ComponentModel.Design
 
 			public virtual void Close ()
 			{
+				// Console.WriteLine ("UndoUnot.Close (" + _name + ")");
 				_closed = true;
 			}
 
 			public virtual void ComponentAdded (ComponentEventArgs e)
 			{
-				if (!_closed)
-					_actions.Push (new ComponentAddRemoveAction (_engine, (IComponent) e.Component, true));
+				if (!_closed) {
+					// Console.WriteLine ("New Action: Component*Add*RemoveAction (" + ((IComponent)e.Component).Site.Name + ")");
+					_actions.Add (new ComponentAddRemoveAction (_engine, (IComponent) e.Component, true));
+				}
 			}
 
 			public virtual void ComponentAdding (ComponentEventArgs e)
@@ -451,36 +511,59 @@ namespace System.ComponentModel.Design
 
 			public virtual void ComponentChanged (ComponentChangedEventArgs e)
 			{
-				if (!_closed) {
-					ComponentChangeAction action = _actions.Peek () as ComponentChangeAction;
-					if (action != null)
-						action.SetModifiedState (_engine, (IComponent) e.Component, e.Member);
+				if (_closed)
+					return;
+
+				// A component starts getting removed. 
+				// ComponentRemoving -> remove component -> ComponentRemoved
+				// The problem is that someone can subscribe to the Removed event after us (the UndoEngine) - e.g
+				// ParentControlDesigner will explicitly request (by setting it to null between Removing and Removed 
+				// the serialization of the Parent property of the removed child.
+				// In the case where someone subscribes after and performs changes to the component, we might get 
+				// ComponentChanged events after we've already created the addremove action, but the componentchangeaction
+				// will be incomplete standing before the addremove one.
+				//
+				ComponentChangeAction changeAction = null;
+				for (int i=0; i < _actions.Count; i++) {
+					changeAction = _actions[i] as ComponentChangeAction;
+					if (changeAction != null && !changeAction.IsComplete &&
+					    changeAction.Component == e.Component &&
+					    changeAction.Member.Equals (e.Member)) {
+						changeAction.SetModifiedState (_engine, (IComponent) e.Component, e.Member);
+						break;
+					}
 				}
 			}
 
 			public virtual void ComponentChanging (ComponentChangingEventArgs e)
 			{
-				if (!_closed) {
-					ComponentChangeAction action = new ComponentChangeAction ();
-					action.SetOriginalState (_engine, (IComponent) e.Component, e.Member);
-					_actions.Push (action);
-				}
+				if (_closed)
+					return;
+
+				// Console.WriteLine ("New Action: ComponentChangeAction (" + ((IComponent)e.Component).Site.Name + ")");
+				ComponentChangeAction action = new ComponentChangeAction ();
+				action.SetOriginalState (_engine, (IComponent) e.Component, e.Member);
+				_actions.Add (action);
 			}
 
 			public virtual void ComponentRemoved (ComponentEventArgs e)
 			{
-				if (!_closed)
-					_actions.Push (new ComponentAddRemoveAction (_engine, e.Component, false));
 			}
 
 			public virtual void ComponentRemoving (ComponentEventArgs e)
 			{
+				if (!_closed) {
+					// Console.WriteLine ("New Action: ComponentAdd*Remove*Action (" + ((IComponent)e.Component).Site.Name + ")");
+					_actions.Add (new ComponentAddRemoveAction (_engine, e.Component, false));
+				}
 			}
 
 			public virtual void ComponentRename (ComponentRenameEventArgs e)
 			{
-				if (!_closed)
-					_actions.Push (new ComponentRenameAction (((IComponent)e.Component).Site.Name, e.OldName));
+				if (!_closed) {
+					// Console.WriteLine ("New Action: ComponentRenameAction (" + ((IComponent)e.Component).Site.Name + ")");
+					_actions.Add (new ComponentRenameAction (e.NewName, e.OldName));
+				}
 			}
 
 			protected object GetService (Type serviceType)
