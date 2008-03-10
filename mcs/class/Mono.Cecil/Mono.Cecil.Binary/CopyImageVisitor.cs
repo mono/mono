87fb45d427b8f33418c81c13b5f5c806e68390c3
Jb Evain <jbevain@gmail.com>
2008-03-10 08:15:36 +0000

    2008-03-08  Jb Evain  <jbevain@novell.com>
    	* Mono.Cecil.Binary/CopyImageVisitor.cs:
    		Copy the cliheaders flags over the new assembly.
    		Patch by Oleksiy Gapotchenko <alex.s.gap@gmail.com>
    
    svn path=/trunk/mcs/; revision=97839

--- mcs/class/Mono.Cecil/Mono.Cecil.Binary/CopyImageVisitor.cs
+++ mcs/class/Mono.Cecil/Mono.Cecil.Binary/CopyImageVisitor.cs
@@ -43,6 +43,8 @@ namespace Mono.Cecil.Binary {
 			m_newImage = img;
 			if (m_originalImage.DebugHeader != null)
 				m_newImage.AddDebugHeader ();
+
+			m_newImage.CLIHeader.Flags = m_originalImage.CLIHeader.Flags;
 		}
 
 		public override void VisitDebugHeader (DebugHeader dbgHeader)
