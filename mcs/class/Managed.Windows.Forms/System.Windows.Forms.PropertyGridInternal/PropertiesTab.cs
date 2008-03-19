e3e6cdd6d5c7a32a112811a682e72cb55b4d1586
Ivan Zlatev <ivan@ivanz.com>
2008-03-19 23:18:16 +0000

    2008-03-19  Ivan N. Zlatev  <contact@i-nz.net>
    	* PropertiesTab.cs: Implement.
    
    
    svn path=/trunk/mcs/; revision=98646

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms.PropertyGridInternal/PropertiesTab.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms.PropertyGridInternal/PropertiesTab.cs
@@ -17,10 +17,11 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// Copyright (c) 2005 Novell, Inc.
+// Copyright (c) 2005-2008 Novell, Inc.
 //
 // Authors:
 //	Jonathan Chambers	jonathan.chambers@ansys.com
+//	Ivan N. Zlatev		contact@i-nz.net
 //
 
 using System;
@@ -31,34 +32,43 @@ namespace System.Windows.Forms.PropertyGridInternal
 {
 	public class PropertiesTab : PropertyTab
 	{
-		public PropertiesTab() {
+		public PropertiesTab () 
+		{
 		}
 
 		public override PropertyDescriptorCollection GetProperties (object component, Attribute[] attributes)
 		{
-			return base.GetProperties(null, component, attributes);
+			return GetProperties (null, component, attributes);
 		}
 
 		public override PropertyDescriptorCollection GetProperties (ITypeDescriptorContext context, object component, Attribute[] attributes)
 		{
-			return base.GetProperties (context, component, attributes);
+			if (component == null)
+				return new PropertyDescriptorCollection (null);
+
+			PropertyDescriptorCollection properties = null;
+			TypeConverter converter = TypeDescriptor.GetConverter (component);
+			if (converter != null && converter.GetPropertiesSupported ())
+				properties = converter.GetProperties (context, component, attributes);
+			if (properties == null)   // try 3: TypeDescriptor
+				properties = TypeDescriptor.GetProperties (component, attributes);
+			return properties;
 		}
 
-		public override PropertyDescriptor GetDefaultProperty (object obj)
+		public override PropertyDescriptor GetDefaultProperty (object component)
 		{
-			return base.GetDefaultProperty (obj);
+			if (component == null)
+				return null;
+
+			return TypeDescriptor.GetDefaultProperty (component);
 		}
 
 		public override string HelpKeyword {
-			get {
-				return "vs.properties";
-			}
+			get { return "vs.properties"; }
 		}
 
 		public override string TabName {
-			get {
-				return "Properties";
-			}
+			get { return "Properties"; }
 		}
 	}
 }
