5f7aee49d371582700238c11960702d895c3121c
Jb Evain <jbevain@gmail.com>
2008-03-20 08:44:46 +0000

    unrevert now that gmcs is fixed
    svn path=/trunk/mcs/; revision=98659

--- mcs/class/System.Core/System.Linq.Expressions/MemberAssignment.cs
+++ mcs/class/System.Core/System.Linq.Expressions/MemberAssignment.cs
@@ -51,16 +51,9 @@ namespace System.Linq.Expressions {
 		{
 			var member = this.Member;
 
-			switch (member.MemberType) {
-			case MemberTypes.Field:
-				EmitFieldAssignment (ec, (FieldInfo) member, local);
-				return;
-			case MemberTypes.Property:
-				EmitPropertyAssignment (ec, (PropertyInfo) member, local);
-				return;
-			default:
-				throw new NotSupportedException (member.MemberType.ToString ());
-			}
+			this.Member.OnFieldOrProperty (
+				field => EmitFieldAssignment (ec, field, local),
+				prop => EmitPropertyAssignment (ec, prop, local));
 		}
 
 		void EmitFieldAssignment (EmitContext ec, FieldInfo field, LocalBuilder local)
