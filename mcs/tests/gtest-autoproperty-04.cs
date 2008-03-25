6cdec39d115a5b4cfc3e4d0df49cedbcc8ab6d8a
Marek Safar <marek.safar@gmail.com>
2008-03-25 11:31:24 +0000

    A test for bug #372846
    svn path=/trunk/mcs/; revision=98954

---
+++ mcs/tests/gtest-autoproperty-04.cs
@@ -0,0 +1,14 @@
+// Compiler options: -unsafe
+
+namespace N
+{
+	public class BufferManager
+	{
+		public unsafe byte *Ptr { get; private set; }
+		
+		public static void Main ()
+		{
+		}
+	}
+}
+
