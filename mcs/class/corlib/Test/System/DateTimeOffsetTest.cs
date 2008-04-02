7e7faf62d4a4fb2c0c5750995ca85a068f51ab2a
Miguel de Icaza <miguel@gnome.org>
2008-04-02 23:24:08 +0000

    Make easier to track
    svn path=/trunk/mcs/; revision=99710

--- mcs/class/corlib/Test/System/DateTimeOffsetTest.cs
+++ mcs/class/corlib/Test/System/DateTimeOffsetTest.cs
@@ -171,10 +171,10 @@ namespace MonoTests.System {
 		{
 			DateTimeOffset dto = new DateTimeOffset (2007, 11, 1, 9, 0, 0, new TimeSpan(-7, 0, 0));
 			string format = "dddd, MMM dd yyyy HH:mm:ss zzz";
-			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, null as DateTimeFormatInfo));
-			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, CultureInfo.InvariantCulture));
-			Assert.AreEqual ("jeudi, nov. 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("fr-FR")));
-			Assert.AreEqual ("jueves, nov 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("es-ES")));
+			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, null as DateTimeFormatInfo), "ts1");
+			Assert.AreEqual ("Thursday, Nov 01 2007 09:00:00 -07:00", dto.ToString (format, CultureInfo.InvariantCulture), "ts2");
+			Assert.AreEqual ("jeudi, nov. 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("fr-FR")), "ts3");
+			Assert.AreEqual ("jueves, nov 01 2007 09:00:00 -07:00", dto.ToString (format, new CultureInfo ("es-ES")), "ts4");
 		}
 
 		[Test]
