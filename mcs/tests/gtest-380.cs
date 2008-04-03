7d43d59a72c2bf1f0949745d5dc595cf77c6caba
Marek Safar <marek.safar@gmail.com>
2008-04-03 14:49:06 +0000

    New test.
    svn path=/trunk/mcs/; revision=99748

---
+++ mcs/tests/gtest-380.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System;
+
+class Test
+{
+	public volatile int field;
+
+	public static int Main ()
+	{
+		FieldInfo fi = typeof (Test).GetField ("field");
+		if (fi.GetCustomAttributes (true).Length != 0)
+			return 1;
+		
+		Type[] t = fi.GetRequiredCustomModifiers ();
+		if (t.Length != 1)
+			return 2;
+		
+		if (t [0] != typeof (System.Runtime.CompilerServices.IsVolatile))
+			return 3;
+		
+		Console.WriteLine ("OK");
+		return 0;
+	}
+}
