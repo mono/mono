b93255ccb4a382cd35b62846da04a186a500bfb0
Jb Evain <jbevain@gmail.com>
2008-04-08 19:12:41 +0000

    in System.CodeDom.Compiler: 2008-04-08  Jb Evain  <jbevain@novell.com>
    	* CodeGeneratorFromTypeTestBase.cs: add a generator for
    	abstract properties.
    
    in Microsoft.CSharp:
    2008-04-08  Jb Evain  <jbevain@novell.com>
    
    	* CodeGeneratorFromTypeTest.cs: add test for abstract properties.
    
    svn path=/trunk/mcs/; revision=100132

--- mcs/class/System/Test/Microsoft.CSharp/CodeGeneratorFromTypeTest.cs
+++ mcs/class/System/Test/Microsoft.CSharp/CodeGeneratorFromTypeTest.cs
@@ -235,6 +235,20 @@ namespace MonoTests.Microsoft.CSharp
 		}
 
 		[Test]
+		public void AbstractPropertyTest ()
+		{
+			string code = GenerateAbstractProperty (Options);
+			Assert.AreEqual (string.Format (CultureInfo.InvariantCulture,
+				"public abstract class Test1 {{{0}" +
+				"    {0}" +
+				"    public abstract string Name {{{0}" +
+				"        get;{0}" +
+				"        set;{0}" +
+				"    }}{0}" +
+				"}}{0}", NewLine), code);
+		}
+
+		[Test]
 		public override void PropertyMembersTypeTest1 ()
 		{
 			string code = GeneratePropertyMembersAttributes (Options);
