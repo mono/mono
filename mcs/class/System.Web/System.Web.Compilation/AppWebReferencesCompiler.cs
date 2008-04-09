d8b86dd4f0c3bd1d3d6c59f180ddc819e2f9330e
Marek Habersack <grendel@twistedcode.net>
2008-04-09 21:01:40 +0000

    Fix the build
    svn path=/trunk/mcs/; revision=100278

--- mcs/class/System.Web/System.Web.Compilation/AppWebReferencesCompiler.cs
+++ mcs/class/System.Web/System.Web.Compilation/AppWebReferencesCompiler.cs
@@ -28,7 +28,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if NET_2_0
+#if NET_2_0 && WEBSERVICES_DEP
 using System;
 using System.CodeDom;
 using System.CodeDom.Compiler;
