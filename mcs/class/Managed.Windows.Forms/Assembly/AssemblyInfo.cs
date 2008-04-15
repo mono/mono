19337e535637a797fef0bc891c4a1bd8f0600ec5
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-15 15:43:08 +0000

    2008-04-15  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Use existing consts where possible
    
    svn path=/trunk/mcs/; revision=100729

--- mcs/class/Managed.Windows.Forms/Assembly/AssemblyInfo.cs
+++ mcs/class/Managed.Windows.Forms/Assembly/AssemblyInfo.cs
@@ -17,42 +17,40 @@ using System.Diagnostics;
 
 // General Information about the System.Windows.Forms assembly
 
+[assembly: AssemblyTitle ("System.Windows.Forms.dll")]
+[assembly: AssemblyDescription ("System.Windows.Forms.dll")]
+[assembly: AssemblyDefaultAlias ("System.Windows.Forms.dll")]
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
 
-#if (ONLY_1_1)
-[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: TypeLibVersion (1, 10)]
-#endif
+[assembly: CLSCompliant (true)]
+[assembly: NeutralResourcesLanguage ("en-US")]
 
-#if ONLY_1_0 || ONLY_1_1
-[assembly: AssemblyConfiguration("Development version")]
-[assembly: AssemblyTrademark("")]
-#endif
+[assembly: ComVisible (false)]
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: AssemblyDelaySign (true)]
+[assembly: AssemblyKeyFile("../ecma.pub")]
 
 #if NET_2_0
-[assembly: AssemblyFileVersion("2.0.50727.42")]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-[assembly: CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
-[assembly: Dependency("System.Drawing,", LoadHint.Always)]
-[assembly: Dependency("System,", LoadHint.Always)]
-[assembly: StringFreezing]
-[assembly: ComCompatibleVersion(1, 0, 3300, 0)]
+	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+	[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+	[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+	[assembly: Dependency ("System.Drawing,", LoadHint.Always)]
+	[assembly: Dependency ("System,", LoadHint.Always)]
+	[assembly: StringFreezing]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+#elif NET_1_1
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: TypeLibVersion (1, 10)]
+#elif NET_1_0
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
 #endif
-
-[assembly: AssemblyTitle("System.Windows.Forms.dll")]
-[assembly: AssemblyDescription("System.Windows.Forms.dll")]
-[assembly: AssemblyCompany("MONO development team")]
-[assembly: AssemblyProduct("MONO CLI")]
-[assembly: AssemblyCopyright("(c) 2003 Various Authors")]
-
-[assembly: CLSCompliant(true)]
-[assembly: ComVisible(false)]
-[assembly: AssemblyDefaultAlias("System.Windows.Forms.dll")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: AllowPartiallyTrustedCallers()]
-
-[assembly: AssemblyDelaySign(true)]
-[assembly: AssemblyKeyFile("../ecma.pub")]
