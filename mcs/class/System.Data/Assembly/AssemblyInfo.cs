fec1176b53e1d624c36accd496afbd4a473c51d7
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-15 16:08:33 +0000

    2008-04-15  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=100739

--- mcs/class/System.Data/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Data/Assembly/AssemblyInfo.cs
@@ -34,38 +34,49 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Security;
+using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about the system assembly
+// General Information about the System.Data assembly
 
+[assembly: AssemblyTitle ("System.Data.dll")]
+[assembly: AssemblyDescription ("System.Data.dll")]
+[assembly: AssemblyDefaultAlias ("System.Data.dll")]
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
-
-#if (ONLY_1_1)
-[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: TypeLibVersion (1, 10)]
-#endif
-
-[assembly: AssemblyTitle("System.Data.dll")]
-[assembly: AssemblyDescription("System.Data.dll")]
-[assembly: AssemblyConfiguration("Development version")]
-[assembly: AssemblyCompany("MONO development team")]
-[assembly: AssemblyProduct("MONO CLI")]
-[assembly: AssemblyCopyright("(c) 2003 Various Authors")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
 
 #if !TARGET_JVM
-[assembly: CLSCompliant(true)]
+	[assembly: CLSCompliant (true)]
 #endif
-[assembly: AssemblyDefaultAlias("System.Data.dll")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: NeutralResourcesLanguage ("en-US")]
 
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 [assembly: AllowPartiallyTrustedCallers]
 
-[assembly: AssemblyDelaySign(true)]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: AssemblyDelaySign (true)]
 #if !TARGET_JVM
-[assembly: AssemblyKeyFile("../ecma.pub")]
+	[assembly: AssemblyKeyFile("../ecma.pub")]
+#endif
+
+#if NET_2_0
+	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: InternalsVisibleTo ("System.Data.Entity, PublicKey=00000000000000000400000000000000")]
+	[assembly: InternalsVisibleTo ("System.Data.DataSetExtensions, PublicKey=00000000000000000400000000000000")]
+	[assembly: InternalsVisibleTo ("SqlAccess, PublicKey=0024000004800000940000000602000000240000525341310004000001000100272736ad6e5f9586bac2d531eabc3acc666c2f8ec879fa94f8f7b0327d2ff2ed523448f83c3d5c5dd2dfc7bc99c5286b2c125117bf5cbe242b9d41750732b2bdffe649c6efb8e5526d526fdd130095ecdb7bf210809c6cdad8824faa9ac0310ac3cba2aa0523567b2dfa7fe250b30facbd62d4ec99b94ac47c7d3b28f1f6e4c8")]
+#elif NET_1_1
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: TypeLibVersion (1, 10)]
+#elif NET_1_0
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
 #endif
