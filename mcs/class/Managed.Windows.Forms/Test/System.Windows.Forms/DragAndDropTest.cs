889c88b4a8002f3aec27ac47e1c924dbcd376d0b
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2008-04-23 07:43:03 +0000

    2008-04-23  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* DragAndDropTest.cs: New dnd tests.
    
    
    svn path=/trunk/mcs/; revision=101509

---
+++ mcs/class/Managed.Windows.Forms/Test/System.Windows.Forms/DragAndDropTest.cs
@@ -0,0 +1,513 @@
+//
+// DragAndDropTest.cs: tests for general drag and drop operations.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Author:
+//   Carlos Alberto Cortez <ccortes@novell.com>
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
+// NOTE: Since this is an interactive set of tests, I didn't include it as part
+// of the MWF test suite build. You will need to build it by yourself and then
+// use nunit-console, since the nunit GUI has some problems with DND (basically a COM
+// issue).
+//
+
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+using NUnit.Framework;
+
+namespace MonoTests.System.Windows.Forms
+{
+	[TestFixture]
+	public class DragAndDropTest
+	{
+		[Test]
+		public void DropCancelledByEsc ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. While still pressing the mouse button, press ESC." + Environment.NewLine +
+				"4. Close the form.";
+			form.DragControl.DragData = "hello";
+			form.DragControl.AllowedEffects = DragDropEffects.All;
+			form.DropControl.DropEffect = DragDropEffects.Copy;
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.None, form.DragControl.PerformedEffect, "A1");
+			Assert.AreEqual (1, form.DropControl.EnterFiredCount, "A2");
+			Assert.AreEqual (1, form.DropControl.LeaveFiredCount, "A3");
+			Assert.AreEqual (0, form.DropControl.DropFiredCount, "A4");
+		}
+
+		[Test]
+		public void DropCancelledByQuery ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. The drop should be cancelled after some seconds." + Environment.NewLine +
+				"4. Close the form.";
+			form.DragControl.DragData = "hello";
+			form.DragControl.AllowedEffects = DragDropEffects.All;
+			form.DropControl.DropEffect = DragDropEffects.Copy;
+
+			query_counter = 0;
+			form.DragControl.QueryContinueDrag += new QueryContinueDragEventHandler (DragControl_QueryContinueDrag);
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.None, form.DragControl.PerformedEffect, "A1");
+			Assert.AreEqual (1, form.DropControl.EnterFiredCount, "A2");
+			Assert.AreEqual (1, form.DropControl.LeaveFiredCount, "A3");
+			Assert.AreEqual (0, form.DropControl.DropFiredCount, "A4");
+		}
+
+		int query_counter = 0;
+
+		void DragControl_QueryContinueDrag (object sender, QueryContinueDragEventArgs e)
+		{
+			DragControl c = (DragControl)sender;
+			DNDForm f = (DNDForm)c.Parent;
+			if (f.DropControl.EnterFiredCount == 0)
+				return;
+
+			// Cancel dnd operation AFTER we have reached the drop control
+			if (query_counter++ >= 32)
+				e.Action = DragAction.Cancel;
+		}
+
+		[Test]
+		public void EventsAfterDrop ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. Move the pointer around and then release the mouse button." + Environment.NewLine +
+				"4. Close the form.";
+			form.DragControl.DragData = "hello";
+			form.DragControl.AllowedEffects = DragDropEffects.All;
+			form.DropControl.DropEffect = DragDropEffects.Copy;
+
+			// These should clear the event counters AND
+			// thus the event counters should remain at 0 after drop occurred
+			// AND we should get any MouseUp event.
+			mouse_up_counter = 0;
+			form.DropControl.DragDrop += new DragEventHandler (DropControl_DragDrop);
+			form.DropControl.MouseUp += new MouseEventHandler (DropControl_MouseUp);
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.Copy, form.DragControl.PerformedEffect, "A1");
+			Assert.AreEqual (0, form.DropControl.EnterFiredCount, "A2");
+			Assert.AreEqual (0, form.DropControl.LeaveFiredCount, "A3");
+			Assert.AreEqual (0, form.DropControl.DropFiredCount, "A4");
+			Assert.AreEqual (0, form.DropControl.DragOverFiredCount, "A5");
+			Assert.AreEqual (0, mouse_up_counter, "A6");
+		}
+
+		void DropControl_DragDrop (object sender, DragEventArgs e)
+		{
+			DropControl c = (DropControl)sender;
+			c.ResetEventCounters ();
+		}
+
+		int mouse_up_counter;
+
+		void DropControl_MouseUp (object sender, MouseEventArgs e)
+		{
+			mouse_up_counter++;
+		}
+
+		[Test]
+		public void SimpleDragDrop ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. Drop on the right control." + Environment.NewLine +
+				"4. Close the form.";
+			form.DragControl.DragData = "SimpleDragDropMessage";
+			form.DragControl.AllowedEffects = DragDropEffects.Copy;
+			form.DropControl.DropEffect = DragDropEffects.Copy;
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.Copy, form.DragControl.PerformedEffect, "A1");
+			Assert.IsTrue (form.DropControl.Data != null, "A2");
+			Assert.AreEqual (true, form.DropControl.Data.GetDataPresent (typeof (string)), "A3");
+			Assert.AreEqual ("SimpleDragDropMessage", form.DropControl.Data.GetData (typeof (string)), "A4");
+		}
+
+		[Test]
+		public void SimpleLeave ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. Move the mouse pointer outside the drop control." + Environment.NewLine +
+				"5. Release mouse button." + Environment.NewLine +
+				"6. Close the form.";
+			form.DragControl.DragData = "hello";
+			form.DragControl.AllowedEffects = DragDropEffects.All;
+			form.DropControl.DropEffect = DragDropEffects.Copy;
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.None, form.DragControl.PerformedEffect, "A1");
+			Assert.AreEqual (1, form.DropControl.EnterFiredCount, "A2");
+			Assert.AreEqual (1, form.DropControl.LeaveFiredCount, "A3");
+			Assert.AreEqual (0, form.DropControl.DropFiredCount, "A4");
+		}
+
+		[Test]
+		public void NotAllowedDropEffect ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Move mouse pointer to the control in the right. " + Environment.NewLine +
+				"3. Drop on the right control." + Environment.NewLine +
+				"4. Close the form.";
+			form.DragControl.DragData = "SimpleDragDropMessage";
+			form.DragControl.AllowedEffects = DragDropEffects.Copy;
+			form.DropControl.DropEffect = DragDropEffects.Move;
+
+			Application.Run (form);
+
+			Assert.AreEqual (DragDropEffects.None, form.DragControl.PerformedEffect, "A1");
+			Assert.AreEqual (1, form.DropControl.EnterFiredCount, "A2");
+			Assert.AreEqual (0, form.DropControl.DropFiredCount, "A3");
+			Assert.AreEqual (1, form.DropControl.LeaveFiredCount, "A4");
+		}
+
+		// This test should probably include a 'log' check
+		[Test]
+		public void SequentialOperations ()
+		{
+			DNDForm form = new DNDForm ();
+			form.Text = MethodBase.GetCurrentMethod ().Name;
+			form.InstructionsText =
+				"Instructions:" + Environment.NewLine + Environment.NewLine +
+				"1. Click with left button on the control on the left, holding it." + Environment.NewLine +
+				"2. Drag on the control on the right. " + Environment.NewLine + Environment.NewLine +
+				"3. Click with left button on the control on the left again, holding it." + Environment.NewLine +
+				"4. Move the mouse pointer to the control in the right." + Environment.NewLine +
+				"5. Move the mouse pointer outside the drop control." + Environment.NewLine +
+				"6. Release mouse button." + Environment.NewLine + Environment.NewLine +
+				"7. Click with left button on the control on the left again, holding it." + Environment.NewLine +
+				"8. Drag on the control on the right. " + Environment.NewLine + Environment.NewLine +
+				"9. Close the form.";
+			form.DragControl.DragData = "SimpleDragDropMessage";
+			form.DragControl.AllowedEffects = DragDropEffects.Move;
+			form.DropControl.DropEffect = DragDropEffects.Move;
+
+			Application.Run (form);
+
+			Assert.AreEqual (2, form.DropControl.DropFiredCount, "A1");
+			Assert.AreEqual (1, form.DropControl.LeaveFiredCount, "A2");
+			Assert.AreEqual (DragDropEffects.Move, form.DragControl.PerformedEffect, "A3");
+		}
+	}
+
+	public class DNDForm : Form
+	{
+		DragControl drag_control;
+		DropControl drop_control;
+		TextBox instructions_tb;
+		Label test_name;
+
+		public DNDForm ()
+		{
+			test_name = new Label ();
+			test_name.Location = new Point (5, 5);
+			test_name.AutoSize = true;
+			//test_name.Font += new Font (Font, FontStyle.Bold | Font.Style);
+			test_name.Text = Text;
+
+			instructions_tb = new TextBox ();
+			instructions_tb.Multiline = true;
+			instructions_tb.Location = new Point (5, test_name.Bottom + 5);
+			instructions_tb.Size = new Size (460, 180);
+
+			drag_control = new DragControl ();
+			drag_control.Location = new Point (5, instructions_tb.Bottom + 10);
+			drag_control.Size = new Size (220, 180);
+
+			drop_control = new DropControl ();
+			drop_control.Location = new Point (drag_control.Right + 20, instructions_tb.Bottom + 10);
+			drop_control.Size = new Size (220, 180);
+
+			Controls.AddRange (new Control [] { test_name, instructions_tb, drag_control, drop_control });
+
+			Size = new Size (480, 440);
+		}
+
+		public string InstructionsText {
+			get {
+				return instructions_tb.Text;
+			}
+			set {
+				instructions_tb.Text = value;
+			}
+		}
+
+		public DragControl DragControl {
+			get {
+				return drag_control;
+			}
+		}
+
+		public DropControl DropControl {
+			get {
+				return drop_control;
+			}
+		}
+
+		protected override void OnTextChanged (EventArgs args)
+		{
+			base.OnTextChanged (args);
+			test_name.Text = Text;
+		}
+	}
+
+	public class DragControl : Control
+	{
+		Rectangle drag_rect;
+		object drag_data;
+		DragDropEffects allowed_effects;
+		DragDropEffects performed_effect;
+		Color drag_color;
+
+		public DragControl ()
+		{
+			drag_rect = new Rectangle (new Point (-1, -1), SystemInformation.DragSize);
+			BackColor = Color.LightYellow;
+			drag_color = Color.Yellow;
+		}
+
+		public object DragData
+		{
+			get {
+				return drag_data;
+			}
+			set {
+				drag_data = value;
+			}
+		}
+
+		public DragDropEffects AllowedEffects {
+			get {
+				return allowed_effects;
+			}
+			set {
+				allowed_effects = value;
+			}
+		}
+
+		public DragDropEffects PerformedEffect {
+			get {
+				return performed_effect;
+			}
+		}
+
+		protected override void OnMouseMove (MouseEventArgs e)
+		{
+			base.OnMouseMove (e);
+
+			if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Right) {
+
+				if (drag_rect.X == -1 && drag_rect.Y == -1)
+					drag_rect.Location = new Point (e.X, e.Y);
+				else {
+					if (!drag_rect.Contains (new Point (e.X, e.Y))) {
+						Color prev_color = BackColor;
+						BackColor = drag_color;
+
+						performed_effect = DoDragDrop (drag_data, allowed_effects);
+
+						drag_rect.Location = new Point (-1, -1);
+						BackColor = prev_color;
+					}
+				}
+
+			}
+		}
+
+		protected override void OnQueryContinueDrag (QueryContinueDragEventArgs qcdevent)
+		{
+			base.OnQueryContinueDrag (qcdevent);
+		}
+
+		protected override void OnPaint (PaintEventArgs e)
+		{
+			base.OnPaint (e);
+
+			Graphics g = e.Graphics;
+
+			StringFormat sf = new StringFormat ();
+			sf.Alignment = StringAlignment.Center;
+			sf.LineAlignment = StringAlignment.Center;
+
+			g.DrawString ("DragControl", SystemFonts.DefaultFont, SystemBrushes.ControlDark,
+				ClientRectangle, sf);
+
+			sf.Dispose ();
+		}
+	}
+
+	public class DropControl : Control
+	{
+		DragDropEffects effect;
+		IDataObject data;
+		Color drop_color;
+		Color prev_color;
+
+		int drop_fired_count;
+		int leave_fired_count;
+		int drag_over_fired_count;
+		int enter_fired_count;
+
+		public DropControl ()
+		{
+			AllowDrop = true;
+			BackColor = Color.LightGreen;
+			drop_color = Color.Green;
+		}
+
+		public DragDropEffects DropEffect {
+			get {
+				return effect;
+			}
+			set {
+				effect = value;
+			}
+		}
+
+		public IDataObject Data {
+			get {
+				return data;
+			}
+		}
+
+		public int DropFiredCount {
+			get {
+				return drop_fired_count;
+			}
+		}
+
+		public int EnterFiredCount {
+			get {
+				return enter_fired_count;
+			}
+		}
+
+		public int LeaveFiredCount {
+			get {
+				return leave_fired_count;
+			}
+		}
+
+		public int DragOverFiredCount {
+			get {
+				return drag_over_fired_count;
+			}
+		}
+
+		public void ResetEventCounters ()
+		{
+			drop_fired_count = enter_fired_count = leave_fired_count = drag_over_fired_count = 0;
+		}
+
+		protected override void OnDragEnter (DragEventArgs drgevent)
+		{
+			prev_color = BackColor;
+			BackColor = drop_color;
+			enter_fired_count++;
+
+			drgevent.Effect = effect;
+
+			base.OnDragEnter (drgevent);
+		}
+
+		protected override void OnDragOver (DragEventArgs drgevent)
+		{
+			base.OnDragOver (drgevent);
+		}
+
+		protected override void OnDragLeave (EventArgs e)
+		{
+			data = null;
+			BackColor = prev_color;
+			leave_fired_count++;
+
+			base.OnDragLeave (e);
+		}
+
+		protected override void OnDragDrop (DragEventArgs drgevent)
+		{
+			data = drgevent.Data;
+			BackColor = prev_color;
+			drop_fired_count++;
+
+			base.OnDragDrop (drgevent);
+		}
+
+		protected override void OnPaint (PaintEventArgs e)
+		{
+			base.OnPaint (e);
+
+			Graphics g = e.Graphics;
+
+			StringFormat sf = new StringFormat ();
+			sf.Alignment = StringAlignment.Center;
+			sf.LineAlignment = StringAlignment.Center;
+
+			g.DrawString ("DropControl", SystemFonts.DefaultFont, SystemBrushes.ControlDark,
+				ClientRectangle, sf);
+
+			sf.Dispose ();
+		}
+	}
+}
+
