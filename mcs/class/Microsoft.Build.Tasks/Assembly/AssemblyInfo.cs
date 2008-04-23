eef5836bb224e909050cfb089a21da4fdac3ddbe
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-23 18:58:30 +0000

    2008-04-23  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=101597

--- mcs/class/Microsoft.Build.Tasks/Assembly/AssemblyInfo.cs
+++ mcs/class/Microsoft.Build.Tasks/Assembly/AssemblyInfo.cs
@@ -26,33 +26,44 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+#if NET_2_0
+
 using System;
 using System.Reflection;
 using System.Resources;
 using System.Security;
+using System.Security.Permissions;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about the Microsoft.Build.Tasks assembly
+// General Information about the Microsoft.Build.Framework assembly
 
+[assembly: AssemblyTitle ("Microsoft.Build.Tasks.dll")]
+[assembly: AssemblyDescription ("Microsoft.Build.Tasks.dll")]
+[assembly: AssemblyDefaultAlias ("Microsoft.Build.Tasks.dll")]
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
 
-[assembly: AssemblyTitle("Microsoft.Build.Tasks.dll")]
-[assembly: AssemblyDescription("Microsoft.Build.Tasks.dll")]
-[assembly: AssemblyConfiguration("Development version")]
-[assembly: AssemblyCompany("Marek Sieradzki")]
-[assembly: AssemblyProduct("MONO CLI")]
-[assembly: AssemblyCopyright("(c) 2005 Marek Sieradzki")]
-[assembly: AssemblyTrademark("")]
+[assembly: NeutralResourcesLanguage ("en-US")]
 
-[assembly: CLSCompliant(true)]
-[assembly: AssemblyDefaultAlias("Microsoft.Build.Tasks.dll")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: ComVisible(false)]
-//[assembly: AllowPartiallyTrustedCallers]
-
-[assembly: AssemblyDelaySign(true)]
+[assembly: ComVisible (false)]
+[assembly: Guid ("D8A9BA71-4724-481d-9CA7-0DA23A1D615C")]
+[assembly: CLSCompliant (true)]
+[assembly: AssemblyDelaySign (true)]
 [assembly: AssemblyKeyFile("../msfinal.pub")]
+
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.Execution)]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: InternalsVisibleTo ("Microsoft.Build.Tasks.Unittest, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
+
+#endif
