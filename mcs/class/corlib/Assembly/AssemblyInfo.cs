4dc960ee23e893be4351696e360da26eb74af700
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-23 17:26:59 +0000

    2008-04-23  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=101581

--- mcs/class/corlib/Assembly/AssemblyInfo.cs
+++ mcs/class/corlib/Assembly/AssemblyInfo.cs
@@ -34,41 +34,55 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Security;
+using System.Diagnostics;
 using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about the system assembly
+// General Information about the mscorlib assembly
+
+#if NET_2_0
+	[assembly: AssemblyTitle ("mscorlib.dll")]
+	[assembly: AssemblyDescription ("mscorlib.dll")]
+	[assembly: AssemblyDefaultAlias ("mscorlib.dll")]
+
+	[assembly: AssemblyCompany (Consts.MonoCompany)]
+	[assembly: AssemblyProduct (Consts.MonoProduct)]
+	[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+
+	[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+#endif
 
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
 
-#if (ONLY_1_1)
+[assembly: NeutralResourcesLanguage ("en-US")]
+
+[assembly: CLSCompliant (true)]
 [assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: TypeLibVersion (1, 10)]
+[assembly: Guid ("BED7F4EA-1A96-11D2-8F08-00A0C9A6186D")]
+[assembly: AllowPartiallyTrustedCallers]
+
+#if !BOOTSTRAP_WITH_OLDLIB
+	[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+	[assembly: AssemblyDelaySign (true)]
+	[assembly: AssemblyKeyFile ("../ecma.pub")]
 #endif
 
-//[assembly: AssemblyTitle("mscorlib.dll")]
-[assembly: AssemblyDescription("Common Language Runtime Library")]
-//[assembly: AssemblyConfiguration("Development version")]
-//[assembly: AssemblyCompany("MONO development team")]
-//[assembly: AssemblyProduct("MONO CLI")]
-//[assembly: AssemblyCopyright("(c) 2003 Various Authors")]
-
-[assembly: CLSCompliant(true)]
-//[assembly: AssemblyDefaultAlias("mscorlib.dll")]
-//[assembly: AssemblyInformationalVersion("0.0.0.1")]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: Guid("BED7F4EA-1A96-11D2-8F08-00A0C9A6186D")]
-
-#if ! BOOTSTRAP_WITH_OLDLIB
-[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification=true)]
-[assembly: AssemblyDelaySign(true)]
-[assembly: AssemblyKeyFile("../ecma.pub")]
+#if NET_2_0
+	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+	[assembly: ComVisible (false)]
+	[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+	[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+	[assembly: TypeLibVersion (2, 0)]
+	[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
+	[assembly: DefaultDependency (LoadHint.Always)]
+	[assembly: StringFreezing]
+#elif NET_1_1 || NET_1_0
+	[assembly: AssemblyDescription("Common Language Runtime Library")]
+	[assembly: TypeLibVersion (1, 10)]
 #endif
 
 #if NET_2_1
-[assembly: InternalsVisibleTo ("Mono.Moonlight, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
+	[assembly: InternalsVisibleTo ("Mono.Moonlight, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
 #endif
