0b944535bd17a46854e46d940f3d7862b442b6d1
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-23 19:16:41 +0000

    2008-04-23  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=101601

--- mcs/class/System.Data.OracleClient/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Data.OracleClient/Assembly/AssemblyInfo.cs
@@ -11,49 +11,45 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Security;
+using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about the System.Data.OracleClient assembly
 
-[assembly: AssemblyVersion (Consts.FxVersion)]
-[assembly: SatelliteContractVersion (Consts.FxVersion)]
-
-#if (ONLY_1_1)
-[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: TypeLibVersion (1, 10)]
-#endif
-
 [assembly: AssemblyTitle ("System.Data.OracleClient.dll")]
 [assembly: AssemblyDescription ("System.Data.OracleClient.dll")]
-#if !NET_2_0
-[assembly: AssemblyConfiguration ("Development version")]
-#endif
-#if NET_2_0
-[assembly: AssemblyFileVersion("2.0.50727.42")]
-#endif
-[assembly: AssemblyCompany ("MONO development team")]
-[assembly: AssemblyProduct ("MONO CLI")]
-[assembly: AssemblyCopyright ("(c) 2002-2005 Novell, Inc and Various Authors")]
-#if !TARGET_JVM
-[assembly: CLSCompliant (true)]
-#endif
 [assembly: AssemblyDefaultAlias ("System.Data.OracleClient.dll")]
-#if !NET_2_0
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.1.4322.2032")]
-#endif
-#if NET_2_0
-[assembly: AssemblyInformationalVersion ("2.0.50727.42")]
-[assembly: Dependency ("System.Data", LoadHint.Always)]
-[assembly: ComCompatibleVersion(1,0,3300,0)]
-[assembly: AllowPartiallyTrustedCallers]
-#endif
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyVersion (Consts.FxVersion)]
+[assembly: SatelliteContractVersion (Consts.FxVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+
 [assembly: NeutralResourcesLanguage ("en-US")]
 
 [assembly: ComVisible (false)]
+[assembly: AllowPartiallyTrustedCallers]
 
-[assembly: AssemblyDelaySign (true)]
 #if !TARGET_JVM
-[assembly: AssemblyKeyFile ("../ecma.pub")]
+	[assembly: CLSCompliant (true)]
+	[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+	[assembly: AssemblyDelaySign (true)]
+	[assembly: AssemblyKeyFile ("../ecma.pub")]
+#endif
+
+#if NET_2_0
+	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: Dependency ("System.Data,", LoadHint.Always)]
+#elif NET_1_1
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: TypeLibVersion (1, 10)]
+#elif NET_1_0
+	[assembly: AssemblyTrademark ("")]
+	[assembly: AssemblyConfiguration ("")]
 #endif
