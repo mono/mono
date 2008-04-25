3c5b6820351aa3f4ee9b66d12892e35bae665e47
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-25 11:49:19 +0000

    2008-04-25  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=101794

--- mcs/class/System.Core/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Core/Assembly/AssemblyInfo.cs
@@ -28,31 +28,43 @@
 
 using System;
 using System.Reflection;
+using System.Resources;
 using System.Security;
 using System.Security.Permissions;
-using System.Resources;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("System.Core.dll")]
-[assembly: AssemblyDescription("System.Core.dll")]
-[assembly: AssemblyCompany("Mono Development Team")]
-[assembly: AssemblyProduct("System.Core")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+// General Information about the System.Core assembly
+// v3.5 Assembly
+
+[assembly: AssemblyTitle ("System.Core.dll")]
+[assembly: AssemblyDescription ("System.Core.dll")]
 [assembly: AssemblyDefaultAlias ("System.Core.dll")]
-[assembly: NeutralResourcesLanguage ("en-US")]
 
-[assembly: CLSCompliant(true)]
-[assembly: ComVisible (false)]
-
-[assembly: AssemblyVersion(Consts.FxVersion)]
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
-[assembly: AssemblyInformationalVersionAttribute (Consts.FxVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
 
+[assembly: NeutralResourcesLanguage ("en-US")]
+[assembly: CLSCompliant (true)]
 [assembly: AssemblyDelaySign (true)]
 [assembly: AssemblyKeyFile ("../ecma.pub")]
-[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification=true)]
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: SecurityCritical]
 
+[assembly: ComVisible (false)]
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
+// Extension attribute should be added by compiler
+
+[assembly: StringFreezing]
+[assembly: DefaultDependency (LoadHint.Always)]
+[assembly: SecurityCritical]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: InternalsVisibleTo ("Dlinq.Unittests, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
