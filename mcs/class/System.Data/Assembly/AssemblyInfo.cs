a491055bc6fcaf8d043979510b058486bf20881b
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-25 08:21:49 +0000

    2008-04-25  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Take into account the 3.5 changes. Hopefully corecompare is wrong predicting add+remove
    
    svn path=/trunk/mcs/; revision=101772

--- mcs/class/System.Data/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Data/Assembly/AssemblyInfo.cs
@@ -68,8 +68,6 @@ using System.Runtime.InteropServices;
 #if NET_2_0
 	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
 	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-	[assembly: InternalsVisibleTo ("System.Data.Entity, PublicKey=00000000000000000400000000000000")]
-	[assembly: InternalsVisibleTo ("System.Data.DataSetExtensions, PublicKey=00000000000000000400000000000000")]
 	[assembly: InternalsVisibleTo ("SqlAccess, PublicKey=0024000004800000940000000602000000240000525341310004000001000100272736ad6e5f9586bac2d531eabc3acc666c2f8ec879fa94f8f7b0327d2ff2ed523448f83c3d5c5dd2dfc7bc99c5286b2c125117bf5cbe242b9d41750732b2bdffe649c6efb8e5526d526fdd130095ecdb7bf210809c6cdad8824faa9ac0310ac3cba2aa0523567b2dfa7fe250b30facbd62d4ec99b94ac47c7d3b28f1f6e4c8")]
 #elif NET_1_1
 	[assembly: AssemblyTrademark ("")]
@@ -80,3 +78,8 @@ using System.Runtime.InteropServices;
 	[assembly: AssemblyTrademark ("")]
 	[assembly: AssemblyConfiguration ("")]
 #endif
+
+#if NET_3_5
+	[assembly: InternalsVisibleTo ("System.Data.Entity, PublicKey=00000000000000000400000000000000")]
+	[assembly: InternalsVisibleTo ("System.Data.DataSetExtensions, PublicKey=00000000000000000400000000000000")]
+endif
