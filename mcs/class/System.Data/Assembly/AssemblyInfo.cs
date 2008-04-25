4acf0c52b711a87ad5cd29ef52609177bb6c2e5e
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-25 09:06:58 +0000

    uups
    svn path=/trunk/mcs/; revision=101776

--- mcs/class/System.Data/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Data/Assembly/AssemblyInfo.cs
@@ -82,4 +82,4 @@ using System.Runtime.InteropServices;
 #if NET_3_5
 	[assembly: InternalsVisibleTo ("System.Data.Entity, PublicKey=00000000000000000400000000000000")]
 	[assembly: InternalsVisibleTo ("System.Data.DataSetExtensions, PublicKey=00000000000000000400000000000000")]
-endif
+#endif
