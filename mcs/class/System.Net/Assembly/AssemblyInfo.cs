a6a0c64a557e2f1191c58ca6a8c24919856ead08
Andreas N <andreas@mono-cvs.ximian.com>
2008-04-25 11:31:56 +0000

    2008-04-25  Andreas Nahr <ClassDevelopment@A-SoftTech.com>
    	* AssemblyInfo.cs: Added missing attributes
    
    svn path=/trunk/mcs/; revision=101791

--- mcs/class/System.Net/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Net/Assembly/AssemblyInfo.cs
@@ -31,12 +31,47 @@
 using System;
 using System.Reflection;
 using System.Resources;
+using System.Diagnostics;
 using System.Security;
 using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about the system assembly
+// General Information about the System.Net assembly
+// v3.5 Assembly
+
+[assembly: AssemblyTitle ("System.Net.dll")]
+[assembly: AssemblyDescription ("System.Net.dll")]
+[assembly: AssemblyDefaultAlias ("System.Net.dll")]
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyVersion (Consts.FxVersion)]
+[assembly: SatelliteContractVersion (Consts.FxVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+
+[assembly: NeutralResourcesLanguage ("en-US")]
+[assembly: CLSCompliant (true)]
+[assembly: AssemblyDelaySign (true)]
+[assembly: AssemblyKeyFile ("../ecma.pub")]
+
+[assembly: ComVisible (false)]
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.DisableOptimizations | 
+                       DebuggableAttribute.DebuggingModes.EnableEditAndContinue | 
+                       DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
+[assembly: SecurityCritical]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, Execution = true)]
+[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+
+
+
+
 
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
