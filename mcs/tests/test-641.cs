bdc31622ece4232e2f18a0c4cf72093032988604
Marek Safar <marek.safar@gmail.com>
2008-05-01 08:57:01 +0000

    New test.
    svn path=/trunk/mcs/; revision=102263

---
+++ mcs/tests/test-641.cs
@@ -0,0 +1,46 @@
+using System;
+
+public class Identifier
+{
+	public Identifier () { }
+
+	public static bool operator == (Identifier id1, Identifier id2)
+	{
+		return true;
+	}
+	public static bool operator != (Identifier id1, Identifier id2)
+	{
+		return true;
+	}
+
+	public static implicit operator Identifier (string identifier)
+	{
+		return null;
+	}
+	
+	public static implicit operator String (Identifier id)
+	{
+		return null;
+	}
+	
+	public static implicit operator decimal (Identifier id)
+	{
+		return -1;
+	}	
+
+	static int Main ()
+	{
+		Identifier a = null;
+		string b = "a";
+
+		if (!(a == b))
+			return 1;
+
+		decimal d = 5;
+		if (a == d)
+			return 2;
+		
+		return 0;
+	}
+}
+
