e81e477dfcf85a598f20e990a03623f9788c7bb6
Marek Safar <marek.safar@gmail.com>
2008-05-02 18:24:59 +0000

    New test.
    svn path=/trunk/mcs/; revision=102362

---
+++ mcs/tests/test-643.cs
@@ -0,0 +1,55 @@
+// Compiler options: -unsafe
+
+using System;
+
+class PointerArithmeticTest
+{
+	unsafe static int Main()
+	{
+		try {
+			return CheckAdd((byte*)(-1), -1);
+		} catch (System.OverflowException) {}
+		
+		try {
+			return CheckSub((short*)(-1), int.MaxValue);
+		} catch (System.OverflowException) {}
+		
+		CheckSub2((short*)(-1), int.MaxValue);
+			
+		if ((long)Conversions (long.MaxValue) != uint.MaxValue)
+			return 5;
+		
+		Console.WriteLine ("OK");
+		return 0;
+	}
+	
+	unsafe static int* Conversions (long b)
+	{
+		return (int*)b;
+	}
+	
+	unsafe static int CheckAdd(byte* ptr, int offset)
+	{
+		if (checked(ptr + offset < ptr))
+			return 1;
+		
+		return 101;
+	}
+	
+	unsafe static int CheckSub(short* ptr, int offset)
+	{
+		if (checked(ptr - offset < ptr))
+			return 2;
+
+		return 102;
+	}
+
+	unsafe static int CheckSub2(short* ptr, int offset)
+	{
+		short* b = ptr + offset;
+		if (checked(ptr - b < 0))
+			return 3;
+
+		return 0;
+	}
+}
