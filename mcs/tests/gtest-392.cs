e9db03a85be6a84f0841d679d3d10c0ddc5cfbd6
Marek Safar <marek.safar@gmail.com>
2008-05-05 17:45:56 +0000

    New test, fixed too agressive optimization
    svn path=/trunk/mcs/; revision=102554

---
+++ mcs/tests/gtest-392.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class DieSubrangeType
+{
+	public int? UpperBound
+	{
+		get;
+		private set;
+	}
+
+	public DieSubrangeType ()
+	{
+		UpperBound = 1;
+	}
+}
+
+class X
+{
+	static int Main ()
+	{
+		DieSubrangeType subrange = new DieSubrangeType ();
+		Console.WriteLine (subrange.UpperBound != null);
+		Console.WriteLine ((int) subrange.UpperBound);
+		return (int) subrange.UpperBound - 1;
+	}
+}
