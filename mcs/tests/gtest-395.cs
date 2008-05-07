7ee3c4413c79bc123ab2e2c2bcec54138c7ec394
C.J. Adams-Collier <cjac@colliertech.org>
2008-05-07 20:07:13 +0000

    2008-05-06T1808  C.J. Adams-Collier  <cjac@colliertech.org>
    	* known-issues-gmcs: added entry for gtest-395.cs
    	* known-issues-smcs: added entry for gtest-395.cs
    	* gtest-395.cs: added test from https://bugzilla.novell.com/show_bug.cgi?id=387040
    
    
    svn path=/trunk/mcs/; revision=102770

---
+++ mcs/tests/gtest-395.cs
@@ -0,0 +1,17 @@
+public class RuleBuilder<T> where T : class {}
+
+public interface IDynamicObject {
+    RuleBuilder<T> GetRule<T>() where T : class;
+}
+
+public class RubyMethod : IDynamicObject {
+    RuleBuilder<T> IDynamicObject.GetRule<T>() /* where T : class */ {
+        return new RuleBuilder<T>();
+    }
+}
+
+public class T {
+	static void Main ()
+	{
+	}
+}
