e9a0b8ef7fc687e64b5994373feda3f8031e048e
Roei Erez <roeie@mono-cvs.ximian.com>
2008-05-07 07:46:48 +0000

    isolate unsupported flow using TARGET_JVM compilation flag.
    svn path=/trunk/mcs/; revision=102701

--- mcs/class/System.Core/System.Linq.Expressions/Expression.cs
+++ mcs/class/System.Core/System.Linq.Expressions/Expression.cs
@@ -2086,9 +2086,11 @@ namespace System.Linq.Expressions {
 
 		internal static bool IsUnsigned (Type t)
 		{
+#if !TARGET_JVM
 			if (t.IsPointer)
 				return IsUnsigned (t.GetElementType ());
 
+#endif
 			return t == typeof (ushort) ||
 				t == typeof (uint) ||
 				t == typeof (ulong) ||
