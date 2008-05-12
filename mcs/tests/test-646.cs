792afb3798f753a8b5044660e8105c3044bee870
Marek Safar <marek.safar@gmail.com>
2008-05-12 08:22:38 +0000

    New test.
    svn path=/trunk/mcs/; revision=102983

---
+++ mcs/tests/test-646.cs
@@ -0,0 +1,32 @@
+using System;
+
+// Undocumented IntPtr and UIntPtr conversion exceptions
+
+class Program
+{
+	static long CastUIntPtrToInt64 (UIntPtr ptr)
+	{
+		return (long) ptr;
+	}
+	
+	static uint CastIntPtrToUInt32 (IntPtr ptr)
+	{
+		return (uint) ptr;
+	}
+	
+	static int Main ()
+	{
+		if (IntPtr.Size < 8) {
+			if (CastUIntPtrToInt64 (new UIntPtr (uint.MaxValue)) != uint.MaxValue)
+				return 1;
+			if (CastIntPtrToUInt32 (new IntPtr (int.MaxValue)) != int.MaxValue)
+				return 2;
+		} else {
+			throw new ApplicationException (CastUIntPtrToInt64 (new UIntPtr (ulong.MaxValue)));
+//			if (CastIntPtrToUInt32 (new IntPtr (long.MaxValue)) != long.MaxValue)
+//				return 4;
+		}
+		
+		return 0;
+	}
+}
