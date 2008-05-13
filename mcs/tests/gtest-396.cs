f1ccc373855fdbae3bee223f85c2ea858a7c93e9
Marek Safar <marek.safar@gmail.com>
2008-05-13 14:27:35 +0000

    New test.
    svn path=/trunk/mcs/; revision=103083

---
+++ mcs/tests/gtest-396.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Test
+{
+	public static void Invoke<A, TR>(Func<A, Func<TR>> callee, A arg1, TR result)
+	{
+	}
+
+	static Func<int> Method (string arg)
+	{
+		return null;
+	}
+
+	static void Main()
+	{
+		Invoke(Method, "one", 1);
+	}
+}
+
