9b990c3faf6e67b694b7c658f572a89f38144b67
Atsushi Eno <atsushieno@gmail.com>
2008-05-20 04:47:06 +0000

    new stub checkin.
    svn path=/trunk/mcs/; revision=103545

---
+++ mcs/class/System.Data.Linq/System.Data.Linq/Binary.cs
@@ -0,0 +1,101 @@
+//
+// Binary.cs
+//
+// Author:
+//   Atsushi Enomoto  <atsushi@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc.
+//
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Linq;
+
+namespace System.Data.Linq
+{
+	[SerializableAttribute]
+#if WCF_ENABLED
+	[DataContractAttribute]
+#endif
+	public sealed class Binary : IEquatable<Binary>
+	{
+		[MonoTODO]
+		public Binary (byte [] value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static bool operator == (Binary binary1, Binary binary2)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public static bool operator != (Binary binary1, Binary binary2)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static implicit operator Binary (byte [] value)
+		{
+			return new Binary (value);
+		}
+
+		[MonoTODO]
+		public int Length {
+			get { throw new NotImplementedException (); }
+		}
+
+		[MonoTODO]
+		public bool Equals (Binary other)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override bool Equals (object obj)
+		{
+			Binary other = obj as Binary;
+			return other != null && Equals (other);
+		}
+
+		[MonoTODO]
+		public override int GetHashCode ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public byte [] ToArray ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public override string ToString ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
