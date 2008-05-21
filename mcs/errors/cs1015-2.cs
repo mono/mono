884a126746955474213a62dbbab6342c59f745a7
Marek Safar <marek.safar@gmail.com>
2008-05-21 09:25:36 +0000

    New tests.
    svn path=/trunk/mcs/; revision=103647

---
+++ mcs/errors/cs1015-2.cs
@@ -0,0 +1,12 @@
+// CS1015: Only `object', `string', and types that derive from `System.Exception' can be passed into a catch block
+// Line: 9
+
+class Test
+{
+    public static void Main ()
+    {
+    	try {}
+    	catch (int[]) {}
+    }
+}
+
