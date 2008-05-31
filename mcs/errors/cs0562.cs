c223ea815453437b4bf2166bf3d30d739fba9db4
Marek Safar <marek.safar@gmail.com>
2008-05-31 18:49:44 +0000

    A message update
    svn path=/trunk/mcs/; revision=104605

--- mcs/errors/cs0562.cs
+++ mcs/errors/cs0562.cs
@@ -1,8 +1,9 @@
-// cs0562.cs: The parameter of a unary operator must be the containing type
-// Line: 5
+// CS0562: The parameter type of a unary operator must be the containing type
+// Line: 7
 
 class SampleClass {
-        public static SampleClass operator - (int value) {
-                return new SampleClass();
-        }
+	public static SampleClass operator - (int value)
+	{
+		return new SampleClass();
+	}
 }
