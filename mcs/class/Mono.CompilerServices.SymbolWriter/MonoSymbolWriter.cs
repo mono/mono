7cbacdfefcdf2d78153e9532d8deaa405ed9a657
Martin Baulig <martin@novell.com>
2008-06-10 13:26:02 +0000

    2008-06-10  Martin Baulig  <martin@ximian.com>
    	* MonoSymbolWriter.cs
    	(MonoSymbolWriter.CloseMethod): Quick-fix; still a lot of broken
    	code out there.
    
    
    svn path=/trunk/mcs/; revision=105419

--- mcs/class/Mono.CompilerServices.SymbolWriter/MonoSymbolWriter.cs
+++ mcs/class/Mono.CompilerServices.SymbolWriter/MonoSymbolWriter.cs
@@ -187,6 +187,12 @@ namespace Mono.CompilerServices.SymbolWriter
 
 		public void CloseMethod ()
 		{
+			if (current_method == null) {
+				Console.WriteLine ("Some clown is calling CloseMethod() while " +
+						   "current_method == null: {0}", Environment.StackTrace);
+				return;
+			}
+						
 			current_method.SetLineNumbers (
 				current_method_lines, current_method_lines_pos);
 			current_method_lines_pos = 0;
