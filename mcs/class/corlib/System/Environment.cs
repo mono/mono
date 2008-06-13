c063bc37a38b48252c6d9a457bc2575101e3b2ac
Zoltan Varga <vargaz@gmail.com>
2008-06-13 22:45:48 +0000

    2008-06-14  Zoltan Varga  <vargaz@gmail.com>
    	* Environment.cs: Bump corlib version.
    
    svn path=/trunk/mcs/; revision=105812

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -63,7 +63,7 @@ namespace System {
 		 * Changes which are already detected at runtime, like the addition
 		 * of icalls, do not require an increment.
 		 */
-		private const int mono_corlib_version = 67;
+		private const int mono_corlib_version = 68;
 		
 		public enum SpecialFolder
 		{	// TODO: Determine if these windoze style folder identifiers 
