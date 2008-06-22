1bb520c60668f24e7b454b9d866d0968aa6e3543
Marek Safar <marek.safar@gmail.com>
2008-06-22 09:48:50 +0000

    2008-06-22  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #394347
    
    
    svn path=/trunk/mcs/; revision=106367

---
+++ mcs/tests/test-anon-76.cs
@@ -0,0 +1,39 @@
+using System;
+
+delegate object FactoryDelegate ();
+
+public class C
+{
+	FactoryDelegate var;
+	int counter;
+
+	FactoryDelegate this [string s]
+	{
+		set { var = value; }
+		get { return var; }
+	}
+
+	public void X ()
+	{
+		this ["ABC"] = delegate () {
+			++counter;
+			Console.WriteLine ("A");
+			return "Return";
+		};
+	}
+
+	static int Main ()
+	{
+		C o = new C ();
+		o.X ();
+
+		Console.WriteLine ("B");
+		Console.WriteLine (o ["ABC"] ());
+		
+				Console.WriteLine (o.counter);
+		if (o.counter != 1)
+			return 1;
+
+		return 0;
+	}
+}
