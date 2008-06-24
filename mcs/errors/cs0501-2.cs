8befa396257cb799bf3a3a6e05222fe3b8cea4d1
Marek Safar <marek.safar@gmail.com>
2008-06-24 12:26:55 +0000

    New test.
    svn path=/trunk/mcs/; revision=106495

---
+++ mcs/errors/cs0501-2.cs
@@ -0,0 +1,7 @@
+// CS0501: `Test.this[string].get' must have a body because it is not marked abstract, extern, or partial
+// Line: 6
+
+public abstract class Test
+{
+	public string this [string key] { get; set; }
+}
