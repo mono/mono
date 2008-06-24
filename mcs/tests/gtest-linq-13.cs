1ed7f5bd0cf620907b49ea7ac27cb77ccfd79452
Marek Safar <marek.safar@gmail.com>
2008-06-24 00:08:25 +0000

    New test.
    svn path=/trunk/mcs/; revision=106478

---
+++ mcs/tests/gtest-linq-13.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+
+	static void Main ()
+	{
+	}
+
+	static void Foo (TypeDefinition type)
+	{
+		var res = from MethodDefinition meth in type.Methods
+					select meth;
+	}
+}
+
+interface IFoo
+{
+}
+
+static class Extension
+{
+	public static IEnumerable<T> Cast<T> (this IFoo i)
+	{
+		return null;
+	}
+}
+
+public class MethodDefinition
+{
+}
+
+public class TypeDefinition
+{
+	public MethodDefinitionCollection Methods { get { return null; } set {} }
+}
+
+public class MethodDefinitionCollection : CollectionBase 
+{
+}
