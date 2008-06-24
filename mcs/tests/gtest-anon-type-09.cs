1a753d987a023cc846b29e19823f1b909836cfeb
Marek Safar <marek.safar@gmail.com>
2008-06-24 23:29:28 +0000

    2008-06-24  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #394826
    
    
    svn path=/trunk/mcs/; revision=106553

---
+++ mcs/tests/gtest-anon-type-09.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace My.System
+{
+	static class Test
+	{
+    	public static void Main ()
+	    {
+    	    var a = new { X = 1 };
+	        Console.WriteLine(a);
+	    }
+	}
+}
