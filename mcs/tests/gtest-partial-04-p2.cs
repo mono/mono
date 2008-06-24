47df064e7bbeee3a0b8647827364f72c8a1428d9
Marek Safar <marek.safar@gmail.com>
2008-06-24 12:47:33 +0000

    2008-06-24  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #401012
    
    
    svn path=/trunk/mcs/; revision=106498

---
+++ mcs/tests/gtest-partial-04-p2.cs
@@ -0,0 +1,4 @@
+[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Explicit)] 
+public partial struct Bug
+{
+}
---
+++ mcs/tests/gtest-partial-04.cs
@@ -0,0 +1,14 @@
+// Compiler options: gtest-partial-04-p2.cs
+
+public partial struct Bug
+{
+	[System.Runtime.InteropServices.FieldOffset(0)]               
+	public int Integer;
+}
+
+class C
+{
+	public static void Main ()
+	{
+	}
+}
