bd52a339f0c2dfa80b5a9e7a48f38b1d30001352
Marek Safar <marek.safar@gmail.com>
2008-07-03 15:19:57 +0000

    2008-07-03  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #321615
    
    
    svn path=/trunk/mcs/; revision=107156

---
+++ mcs/tests/test-654.cs
@@ -0,0 +1,23 @@
+// Compiler options: -unsafe
+
+using System;
+
+class Program
+{
+	static unsafe int Main ()
+	{
+		return Test ((sbyte*) (-1));
+	}
+
+	static unsafe int Test (sbyte* x)
+	{
+		if ((x + 1) < x) {
+			Console.WriteLine ("OK");
+			return 0;
+		} else {
+			Console.WriteLine ("BAD");
+			return 1;
+		}
+	}
+}
+
