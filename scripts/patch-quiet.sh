af8b0632805c1d8ccdcb13ce84f6a4ab890eae5e
Zoltan Varga <vargaz@gmail.com>
2008-07-06 22:35:40 +0000

    2008-07-07  Zoltan Varga  <vargaz@gmail.com>
    	* scripts/patch-quiet.sh: Make this more precise.
    
    svn path=/trunk/mono/; revision=107355

--- scripts/patch-quiet.sh
+++ scripts/patch-quiet.sh
@@ -12,16 +12,16 @@ fi
 src=$1
 
 # compile
-sed -e 's/\t$(COMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(COMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
-sed -e 's/\t$(LTCOMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(LTCOMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(COMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(COMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(LTCOMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(LTCOMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # link
-sed -e 's/= $(LIBTOOL)/= \t$(if $(V),,@echo -e "LD\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/LINK = $(LIBTOOL)/LINK = \t$(if $(V),,@echo -e "LD\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # CC
-sed -e 's/\t$(CC)/\t$(if $(V),,@echo -e "CC \t$@";) $(CC)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(CC)/\t$(if $(V),,@echo -e "CC \t$@";) $(CC)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # mv
 sed -e 's/\tmv -f/\t$(if $(V),,@)mv -f/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # libtool messages
-sed -e 's/doltlibtool/doltlibtool --quiet/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/\$(LIBTOOL)/$(LIBTOOL) --quiet/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 
 # FIXME: libtool message which is not silenced by --quiet:
 # $echo "copying selected object files to avoid basename conflicts..."
