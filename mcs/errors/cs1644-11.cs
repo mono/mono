ef3cf4533c8a205e43d0d0f84d117aa233afa34d
Marek Safar <marek.safar@gmail.com>
2008-07-07 12:06:23 +0000

    New test.
    svn path=/trunk/mcs/; revision=107375

---
+++ mcs/errors/cs1644-11.cs
@@ -0,0 +1,11 @@
+// CS1644: Feature `nullable types' cannot be used because it is not part of the C# 1.0 language specification
+// Line: 9
+// Compiler options: -langversion:ISO-1
+
+public class Test
+{
+	static void Main ()
+	{
+		bool? testbool;
+	}
+}
