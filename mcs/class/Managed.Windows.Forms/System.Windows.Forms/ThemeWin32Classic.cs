e538c3c8a1c5c50005edd6ad7fd41e618b43ed61
Ivan Zlatev <ivan@ivanz.com>
2008-07-08 16:26:21 +0000

    2008-07-08  Ivan N. Zlatev  <contact@i-nz.net>
    	* ThemeWin32Classic.cs: (ManagedWindowBorderWidth): width 3 is only 
    	for fixed toolwindows.
    
    
    svn path=/trunk/mcs/; revision=107477

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
@@ -6073,7 +6073,7 @@ namespace System.Windows.Forms
 
 		public override int ManagedWindowBorderWidth (InternalWindowManager wm)
 		{
-			if (wm.IsToolWindow)
+			if (wm.IsToolWindow && wm.form.FormBorderStyle == FormBorderStyle.FixedToolWindow)
 				return 3;
 			else
 				return 4;
