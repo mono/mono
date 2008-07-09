fdc1cfb1a3956ef1337748d1e5be48776d7b3469
Marek Safar <marek.safar@gmail.com>
2008-07-09 10:33:58 +0000

    New test.
    svn path=/trunk/mcs/; revision=107529

---
+++ mcs/tests/gtest-400.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Gen<T> where T : class
+{
+	public static bool Foo (T t)
+	{
+		return t is Program;
+	}
+}
+
+class Program
+{
+	static bool Foo<T> ()
+	{
+		object o = 1;
+		return o is T;
+	}
+	
+	static int Main ()
+	{
+		if (Foo<bool> ())
+			return 1;
+			
+		if (!Foo<int> ())
+			return 2;
+			
+		if (Gen<object>.Foo (null))
+			return 3;
+
+		if (!Gen<Program>.Foo (new Program ()))
+			return 4;
+
+		Console.WriteLine ("ok");		
+		return 0;
+	}
+}
+
