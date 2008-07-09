9baa8148b776343c23a92748f61fc24e4dba6767
Zoltan Varga <vargaz@gmail.com>
2008-07-09 20:22:43 +0000

    2008-07-09  Zoltan Varga  <vargaz@gmail.com>
    	* scripts/patch-quiet.sh: Use \\t to encode \t.
    
    svn path=/trunk/mono/; revision=107594

--- scripts/patch-quiet.sh
+++ scripts/patch-quiet.sh
@@ -12,14 +12,14 @@ fi
 src=$1
 
 # compile
-sed -e 's/^\t$(COMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(COMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
-sed -e 's/^\t$(LTCOMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(LTCOMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(COMPILE)/\t$(if $(V),,@echo -e "CC\\t$@";) $(COMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(LTCOMPILE)/\t$(if $(V),,@echo -e "CC\\t$@";) $(LTCOMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # link
 # automake defines multiple symbols ending with LINK
-#sed -e 's/\t$(LINK)/\t$(if $(V),,@echo -e "LD\t$@";) $(LINK)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
-sed -e 's/LINK = $(LIBTOOL)/LINK = $(if $(V),,@echo -e "LD\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+#sed -e 's/\t$(LINK)/\t$(if $(V),,@echo -e "LD\\t$@";) $(LINK)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/LINK = $(LIBTOOL)/LINK = $(if $(V),,@echo -e "LD\\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # CC
-sed -e 's/^\t$(CC)/\t$(if $(V),,@echo -e "CC \t$@";) $(CC)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/^\t$(CC)/\t$(if $(V),,@echo -e "CC\\t$@";) $(CC)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # mv
 sed -e 's/\tmv -f/\t$(if $(V),,@)mv -f/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # libtool messages
