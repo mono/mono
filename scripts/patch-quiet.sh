88810f5b87ee2b260360c5fd27726ae7e9dd3815
Zoltan Varga <vargaz@gmail.com>
2008-07-09 19:57:29 +0000

    2008-07-09  Zoltan Varga  <vargaz@gmail.com>
    	* scripts/patch-quiet.sh: Remove a stray \t.
    
    svn path=/trunk/mono/; revision=107590

--- scripts/patch-quiet.sh
+++ scripts/patch-quiet.sh
@@ -15,7 +15,9 @@ src=$1
 sed -e 's/^\t$(COMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(COMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 sed -e 's/^\t$(LTCOMPILE)/\t$(if $(V),,@echo -e "CC\t$@";) $(LTCOMPILE)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # link
-sed -e 's/LINK = $(LIBTOOL)/LINK = \t$(if $(V),,@echo -e "LD\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+# automake defines multiple symbols ending with LINK
+#sed -e 's/\t$(LINK)/\t$(if $(V),,@echo -e "LD\t$@";) $(LINK)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
+sed -e 's/LINK = $(LIBTOOL)/LINK = $(if $(V),,@echo -e "LD\t$@";) $(LIBTOOL)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # CC
 sed -e 's/^\t$(CC)/\t$(if $(V),,@echo -e "CC \t$@";) $(CC)/g' < $src > $src.tmp && cp $src.tmp $src && rm -f $src.tmp
 # mv
