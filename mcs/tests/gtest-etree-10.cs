1f3d58e8773ec28e7e6e4350d877e69fdf4b703c
Marek Safar <marek.safar@gmail.com>
2008-07-09 09:36:29 +0000

    New tests.
    svn path=/trunk/mcs/; revision=107523

---
+++ mcs/tests/gtest-etree-10.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+class Foo<T>
+{
+	public bool ContainsAll<U> (IEnumerable<U> items) where U : T
+	{
+		foreach (U item in items) {
+			Expression<Func<bool>> e = () => !Contains (item);
+			if (!e.Compile () ())
+				return false;
+		}
+
+		return true;
+	}
+
+	public bool Contains (T t)
+	{
+		return false;
+	}
+}
+
+class Program
+{
+	static int Main ()
+	{
+		var x = new Foo<int> ();
+		return x.ContainsAll (new [] { 4, 6, 78 }) ? 0 : 1;
+	}
+}
+
