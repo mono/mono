c0236cec1a108f034980b3527a6cab5be12bf966
Marek Safar <marek.safar@gmail.com>
2008-07-18 22:56:48 +0000

    New tests.
    svn path=/trunk/mcs/; revision=108283

---
+++ mcs/tests/gtest-anon-39.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Test
+{
+	delegate void D ();
+	
+	public static void Main ()
+	{
+		Test t = new Test ();
+		t.Test_1 (t);
+		t.Test_2<int> (1);
+	}
+
+	public void Test_1<T> (T t) where T : Test
+	{
+		D d = delegate () {
+			Action<T> d2 = new Action<T> (t.Test_1);
+		};
+		d ();
+	}
+	
+	public void Test_2<T> (T? t) where T : struct
+	{
+		bool b;
+		T t2;
+		D d = delegate () {
+			b = t == null;
+			t2 = (T)t;
+			t2 = t ?? default (T);
+		};
+		
+		d ();
+	}
+}
