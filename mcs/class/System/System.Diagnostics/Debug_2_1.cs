7fb5fe021f311c4d5c030c62b04964f588c0cf62
Sebastien Pouliot <sebastien@ximian.com>
2008-08-06 23:52:01 +0000

    2008-08-06  Sebastien Pouliot  <sebastien@ximian.com>
    	* Debug_2_1.cs: Add missing [Conditional] attributes.
    
    
    svn path=/trunk/mcs/; revision=109803

--- mcs/class/System/System.Diagnostics/Debug_2_1.cs
+++ mcs/class/System/System.Diagnostics/Debug_2_1.cs
@@ -36,32 +36,39 @@ namespace System.Diagnostics {
 
 	public static class Debug {
 
+		[Conditional ("DEBUG")]
 		public static void Assert (bool condition)
 		{
 		}
 
+		[Conditional ("DEBUG")]
 		public static void Assert (bool condition, string message)
 		{
 		}
 
+		[Conditional ("DEBUG")]
 		public static void Assert (bool condition, string message, string details)
 		{
 		}
 
+		[Conditional ("DEBUG")]
 		public static void Assert (bool condition, string message, string details, object [] args)
 		{
 		}
 
+		[Conditional ("DEBUG")]
 		public static void WriteLine (object obj)
 		{
 			Console.WriteLine (obj);
 		}
 
+		[Conditional ("DEBUG")]
 		public static void WriteLine (string message)
 		{
 			Console.WriteLine (message);
 		}
 
+		[Conditional ("DEBUG")]
 		public static void WriteLine (string format, params object [] args)
 		{
 			Console.WriteLine (format, args);
