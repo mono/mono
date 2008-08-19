c7e792c61b9ab60a45f4bbb4bdb1b836388b2762
Jonathan Pobst <monkey@jpobst.com>
2008-08-19 21:09:09 +0000

    2008-08-19  Jonathan Pobst  <monkey@jpobst.com>
    	* ToolStripProfessionalRenderer.cs: Use Window color for the 
    	background of dropdowns to match .Net when the user is not
    	using the default white.  [Fixes bug #418108]
    
    svn path=/trunk/mcs/; revision=110994

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ToolStripProfessionalRenderer.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ToolStripProfessionalRenderer.cs
@@ -365,7 +365,7 @@ namespace System.Windows.Forms
 			base.OnRenderToolStripBackground (e);
 
 			if (e.ToolStrip is ToolStripDropDown) {
-				e.Graphics.Clear (this.ColorTable.ToolStripDropDownBackground);
+				e.Graphics.Clear (SystemColors.Window);
 				return;
 			}
 			
