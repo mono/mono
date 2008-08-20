0383f6fbdbd3af746db6398849b7a74bd2fa86c5
Jb Evain <jbevain@gmail.com>
2008-08-20 21:24:07 +0000

    move to from olive to mcs
    svn path=/trunk/mcs/; revision=111186

---
+++ mcs/class/System.IdentityModel.Selectors/System.IdentityModel.Selectors/CardSpacePolicyElement.cs
@@ -0,0 +1,87 @@
+//
+// CardSpacePolicyElement.cs
+//
+// Author:
+//	Atsushi Enomoto <atsushi@ximian.com>
+//
+// Copyright (C) 2006-2007 Novell, Inc.  http://www.novell.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+using System.Collections.ObjectModel;
+using System.Xml;
+
+namespace System.IdentityModel.Selectors
+{
+	public class CardSpacePolicyElement
+	{
+		public CardSpacePolicyElement (
+			XmlElement target, XmlElement issuer,
+			Collection<XmlElement> parameters,
+			Uri policyNoticeLink,
+			int policyNoticeVersion,
+			bool isManagedIssuer)
+		{
+			this.target = target;
+			this.issuer = issuer;
+			this.parameters = parameters ?? new Collection<XmlElement> ();
+			this.policy_link = policyNoticeLink;
+			policy_ver = policyNoticeVersion;
+			is_managed = isManagedIssuer;
+		}
+
+		XmlElement target;
+		XmlElement issuer;
+		Collection<XmlElement> parameters;
+		Uri policy_link;
+		int policy_ver;
+		bool is_managed;
+
+		public bool IsManagedIssuer {
+			get { return is_managed; }
+			set { is_managed = value; }
+		}
+
+		public XmlElement Issuer {
+			get { return issuer; }
+			set { issuer = value; }
+		}
+
+		public Collection<XmlElement> Parameters {
+			get { return parameters; }
+		}
+
+		public Uri PolicyNoticeLink {
+			get { return policy_link; }
+			set { policy_link = value; }
+		}
+
+		public int PolicyNoticeVersion {
+			get { return policy_ver; }
+			set { policy_ver = value; }
+		}
+
+		public XmlElement Target {
+			get { return target; }
+			set { target = value; }
+		}
+	}
+}
---
+++ mcs/class/System.IdentityModel.Selectors/System.IdentityModel.Selectors/CardSpaceSelector.cs
@@ -0,0 +1,92 @@
+//
+// CardSpaceSelector.cs
+//
+// Author:
+//	Atsushi Enomoto <atsushi@ximian.com>
+//
+// Copyright (C) 2005-2007 Novell, Inc.  http://www.novell.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens;
+using System.Reflection;
+using System.Xml;
+
+namespace System.IdentityModel.Selectors
+{
+	public static class CardSpaceSelector
+	{
+		static readonly Type impl_type;
+		static readonly object impl;
+		static readonly MethodInfo get_token, import, manage;
+
+		static CardSpaceSelector ()
+		{
+			string implName;
+			switch (Environment.GetEnvironmentVariable ("MONO_IDENTITY_SELECTOR_TYPE")) {
+			default:
+				implName = "Mono.ServiceModel.IdentitySelectors.Win32.CardSelectorClientWin32, Mono.ServiceModel.IdentitySelectors,  Version=3.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756";
+				break;
+			}
+			impl_type = Type.GetType (implName);
+			impl = Activator.CreateInstance (impl_type, new object [0]);
+			get_token = impl_type.GetMethod ("GetToken", new Type [] {
+				typeof (CardSpacePolicyElement []),
+				typeof (SecurityTokenSerializer)});
+			import = impl_type.GetMethod ("Import", new Type [] {
+				typeof (string)});
+			manage = impl_type.GetMethod ("Manage", new Type [0]);
+		}
+
+		[MonoTODO]
+		public static GenericXmlSecurityToken GetToken (
+			CardSpacePolicyElement [] policyChain,
+			SecurityTokenSerializer serializer)
+		{
+			return (GenericXmlSecurityToken) get_token.Invoke (impl, new object [] {policyChain, serializer});
+		}
+
+		public static GenericXmlSecurityToken GetToken (
+			XmlElement endpoint,
+			IEnumerable<XmlElement> policy,
+			XmlElement requiredRemoteTokenIssuer,
+			SecurityTokenSerializer serializer)
+		{
+			CardSpacePolicyElement pe = new CardSpacePolicyElement (endpoint, requiredRemoteTokenIssuer, new Collection<XmlElement> (new List<XmlElement> (policy)), null, 0, requiredRemoteTokenIssuer != null);
+			return GetToken (new CardSpacePolicyElement [] {pe}, serializer);
+		}
+
+		[MonoTODO]
+		public static void Import (string fileName)
+		{
+			import.Invoke (impl, new object [] {fileName});
+		}
+
+		[MonoTODO]
+		public static void Manage ()
+		{
+			manage.Invoke (impl, new object [0]);
+		}
+	}
+}
