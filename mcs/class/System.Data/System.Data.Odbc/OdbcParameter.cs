060406f25ebc80d25750c68085b553be588f4ccf
Veerapuram Varadhan <v.varadhan@gmail.com>
2008-08-26 19:23:11 +0000

    Handle binary type along with VarBinary and Image
    svn path=/branches/mono-2-0/mcs/; revision=111645

--- mcs/class/System.Data/System.Data.Odbc/OdbcParameter.cs
+++ mcs/class/System.Data/System.Data.Odbc/OdbcParameter.cs
@@ -428,8 +428,6 @@ namespace System.Data.Odbc
 			byte [] nativeBytes, buffer;
 
 			switch (_typeMap.OdbcType) {
-			case OdbcType.Binary:
-				throw new NotImplementedException ();
 			case OdbcType.Bit:
 				Marshal.WriteByte (_nativeBuffer, Convert.ToByte (Value));
 				return;
@@ -488,6 +486,7 @@ namespace System.Data.Odbc
 				return;
 			case OdbcType.VarBinary:
 			case OdbcType.Image:
+			case OdbcType.Binary:
 				if (Value.GetType ().IsArray &&
 				    Value.GetType ().GetElementType () == typeof (byte)) {
 					Marshal.Copy ( (byte []) Value, 0, _nativeBuffer, ((byte []) Value).Length);
