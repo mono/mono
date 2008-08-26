dc60cf7ea6f8a5ccd26bc7f5a6f29422cad49a36
Veerapuram Varadhan <v.varadhan@gmail.com>
2008-08-26 19:21:49 +0000

    Test case for testing binary type
    svn path=/branches/mono-2-0/mcs/; revision=111644

--- mcs/class/System.Data/Test/System.Data.Odbc/OdbcParameterTest.cs
+++ mcs/class/System.Data/Test/System.Data.Odbc/OdbcParameterTest.cs
@@ -60,6 +60,10 @@ namespace MonoTests.System.Data.Odbc
 			param = new OdbcParameter ("test", OdbcType.NText);
 			Assert.AreEqual (null, param.Value, "#6");
 			Assert.AreEqual (OdbcType.NText, param.OdbcType, "#7");
+
+			param = new OdbcParameter ("test", OdbcType.Binary);
+			Assert.AreEqual (null, param.Value, "#8");
+			Assert.AreEqual (OdbcType.Binary, param.OdbcType, "#9");
                 }
 
         }
