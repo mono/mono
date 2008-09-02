26ccfe97a37c071b4895baae3f74791d71bb8d4a
Jb Evain <jbevain@gmail.com>
2008-09-02 16:06:49 +0000

    2008-09-02  Jb Evain  <jbevain@novell.com>
    	* Mono.Cecil/GenericParameter.cs: fix Has*Constraint properties.
    
    
    svn path=/trunk/mcs/; revision=112097

--- mcs/class/Mono.Cecil/Mono.Cecil/GenericParameter.cs
+++ mcs/class/Mono.Cecil/Mono.Cecil/GenericParameter.cs
@@ -121,35 +121,32 @@ namespace Mono.Cecil {
 		}
 
 		public bool HasReferenceTypeConstraint {
-			get { return (m_attributes & GenericParameterAttributes.SpecialConstraintMask) == GenericParameterAttributes.ReferenceTypeConstraint; }
+			get { return (m_attributes & GenericParameterAttributes.ReferenceTypeConstraint) != 0; }
 			set {
 				if (value) {
-					m_attributes &= ~GenericParameterAttributes.SpecialConstraintMask;
 					m_attributes |= GenericParameterAttributes.ReferenceTypeConstraint;
 				} else
-					m_attributes &= ~(GenericParameterAttributes.SpecialConstraintMask & GenericParameterAttributes.ReferenceTypeConstraint);
+					m_attributes &= ~GenericParameterAttributes.ReferenceTypeConstraint;
 			}
 		}
 
 		public bool HasNotNullableValueTypeConstraint {
-			get { return (m_attributes & GenericParameterAttributes.SpecialConstraintMask) == GenericParameterAttributes.NotNullableValueTypeConstraint; }
+			get { return (m_attributes & GenericParameterAttributes.NotNullableValueTypeConstraint) != 0; }
 			set {
 				if (value) {
-					m_attributes &= ~GenericParameterAttributes.SpecialConstraintMask;
 					m_attributes |= GenericParameterAttributes.NotNullableValueTypeConstraint;
 				} else
-					m_attributes &= ~(GenericParameterAttributes.SpecialConstraintMask & GenericParameterAttributes.NotNullableValueTypeConstraint);
+					m_attributes &= ~GenericParameterAttributes.NotNullableValueTypeConstraint;
 			}
 		}
 
 		public bool HasDefaultConstructorConstraint {
-			get { return (m_attributes & GenericParameterAttributes.SpecialConstraintMask) == GenericParameterAttributes.DefaultConstructorConstraint; }
+			get { return (m_attributes & GenericParameterAttributes.DefaultConstructorConstraint) != 0; }
 			set {
 				if (value) {
-					m_attributes &= ~GenericParameterAttributes.SpecialConstraintMask;
 					m_attributes |= GenericParameterAttributes.DefaultConstructorConstraint;
 				} else
-					m_attributes &= ~(GenericParameterAttributes.SpecialConstraintMask & GenericParameterAttributes.DefaultConstructorConstraint);
+					m_attributes &= ~GenericParameterAttributes.DefaultConstructorConstraint;
 			}
 		}
 
