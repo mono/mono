7d01ce7e4f9040b332f42f99f4d73b69d61926b1
Marek Safar <marek.safar@gmail.com>
2008-09-02 14:36:07 +0000

    New test.
    svn path=/trunk/mcs/; revision=112088

---
+++ mcs/tests/gtest-exmethod-22-lib.cs
@@ -0,0 +1,8 @@
+// Compiler options: -t:library
+
+static public class A
+{
+	public static void Foo (this int i)
+	{
+	}
+}
---
+++ mcs/tests/gtest-exmethod-22.cs
@@ -0,0 +1,12 @@
+// Compiler options: -noconfig -r:gtest-exmethod-22-lib.dll
+
+using System;
+
+public class Program
+{
+	public static int Main ()
+	{
+		1.Foo ();
+		return 0;
+	}
+}
