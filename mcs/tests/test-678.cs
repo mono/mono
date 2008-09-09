435be9842cf1a108bb6346b266055bc53e2aab35
Marek Safar <marek.safar@gmail.com>
2008-09-09 10:11:20 +0000

    2008-09-09  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #423981
    
    
    svn path=/trunk/mcs/; revision=112573

---
+++ mcs/tests/test-678.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class NaNTest
+{
+	public NaNTest (double width, double height)
+	{
+		if (width < 0 || height < 0)
+			throw new ArgumentException ("fails");
+		
+		if (width <= 0 || height <= 0)
+			throw new ArgumentException ("fails 2");
+		
+		if (width > 0 || height > 0)
+			throw new ArgumentException ("fails 3");
+		
+		if (width >= 0 || height >= 0)
+			throw new ArgumentException ("fails 4");
+	}
+
+	static int Main ()
+	{
+		if (Double.NaN < 0 || Double.NaN < 0)
+			throw new ArgumentException ("passes");
+
+		new NaNTest (Double.NaN, Double.NaN);
+		
+		return 0;
+	} 
+}
