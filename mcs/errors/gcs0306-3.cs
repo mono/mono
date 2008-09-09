01226f2294cf1774ac7a52dabb6dd06218aa29ef
Marek Safar <marek.safar@gmail.com>
2008-09-09 15:49:11 +0000

    New tests.
    svn path=/trunk/mcs/; revision=112601

---
+++ mcs/errors/gcs0306-3.cs
@@ -0,0 +1,8 @@
+// CS0306: The type `System.ArgIterator' may not be used as a type argument
+// Line: 6
+
+class G<T> {}
+
+class C : G<System.ArgIterator>
+{
+}
---
+++ mcs/errors/gcs0313.cs
@@ -0,0 +1,18 @@
+// CS0313: The type `S?' cannot be used as type parameter `T' in the generic type or method `S.Foo<T>(T)'. The nullable type `S?' never satisfies interface constraint of type `I'
+// Line: 16
+
+interface I
+{
+}
+
+struct S : I
+{
+	static void Foo<T> (T t) where T : I
+	{
+	}
+
+	static void Test (S? s)
+	{
+		Foo (s);
+	}
+}
