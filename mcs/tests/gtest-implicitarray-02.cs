8e7317eb22a97debc08e737988eab4bf15ad843f
Marek Safar <marek.safar@gmail.com>
2008-09-17 09:29:35 +0000

    New test.
    svn path=/trunk/mcs/; revision=113264

---
+++ mcs/tests/gtest-implicitarray-02.cs
@@ -0,0 +1,18 @@
+class MyString
+{
+	public static implicit operator string (MyString s)
+	{
+		return "ggtt";
+	}
+}
+
+public class Test
+{
+	static int Main ()
+	{
+		var v = new [] { new MyString (), "a" };
+		if (v [0] != "ggtt")
+			return 1;
+		return 0;
+	}
+}
