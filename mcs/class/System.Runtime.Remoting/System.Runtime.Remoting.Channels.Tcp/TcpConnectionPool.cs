081c1c9c506ee0b88a868f0a2e5a859e022a6d37
Robert Jordan <robertj@gmx.net>
2008-09-18 08:32:17 +0000

    2008-09-18  Robert Jordan  <robertj@gmx.net>
    	* TcpConnectionPool.cs: Set NoDelay on both profiles.
    
    
    svn path=/trunk/mcs/; revision=113395

--- mcs/class/System.Runtime.Remoting/System.Runtime.Remoting.Channels.Tcp/TcpConnectionPool.cs
+++ mcs/class/System.Runtime.Remoting/System.Runtime.Remoting.Channels.Tcp/TcpConnectionPool.cs
@@ -124,9 +124,14 @@ namespace System.Runtime.Remoting.Channels.Tcp
 	{
 		public ReusableTcpClient (string host, int port): base (host, port)
 		{
+			// Avoid excessive waiting for data by the tcp stack in linux.
+			// We can't safely use SetSocketOption for both runtimes because
+			// it would break 2.0 TcpClient's property cache.
 #if NET_2_0
-			// Avoid excessive waiting for data by the tcp stack in linux
 			Client.NoDelay = true;
+#else
+			Client.SetSocketOption (SocketOptionLevel.Tcp,
+						SocketOptionName.NoDelay, 1);
 #endif
 		}
 		
