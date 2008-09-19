f16bca16db4d1017351b3e24a679c80dad9a8c18
Marek Safar <marek.safar@gmail.com>
2008-09-19 15:22:30 +0000

    New tests.
    svn path=/trunk/mcs/; revision=113535

---
+++ mcs/tests/gtest-411.cs
@@ -0,0 +1,30 @@
+using System;
+
+static class Maybe
+{
+	public static Maybe<T> C<T> (T value)
+	{
+		return null;
+	}
+}
+
+sealed class Maybe<T>
+{
+	public Maybe (T value)
+	{
+	}
+}
+
+static class Extensions {
+	public static R Match<T,R>(this T self, params Func<T,Maybe<R>>[] matchers)
+	{
+		return default (R);
+	}
+}
+
+class Test {
+	public static void Main ()
+	{
+		Extensions.Match ("a", s => Maybe.C(s));
+	}
+}
---
+++ mcs/tests/gtest-412.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+
+class Program
+{
+	static int Main ()
+	{
+		Type type = typeof (Foo<>);
+		Type [] gargs = type.GetGenericArguments ();
+		if (gargs == null || gargs.Length != 1) {
+			Console.WriteLine ("#1");
+			return 1;
+		}
+
+		Type garg = gargs [0];
+		Type [] csts = garg.GetGenericParameterConstraints ();
+
+		if (garg.Name != "T") {
+			Console.WriteLine ("#2: " + garg.Name);
+			return 2;
+		}
+		if (garg.GenericParameterAttributes !=
+			(GenericParameterAttributes.DefaultConstructorConstraint | GenericParameterAttributes.NotNullableValueTypeConstraint)) {
+			Console.WriteLine ("#3: " + garg.GenericParameterAttributes);
+			return 3;
+		}
+		if (csts == null || csts.Length != 1) {
+			Console.WriteLine ("#4");
+			return 4;
+		}
+		if (csts [0] != typeof (ValueType)) {
+			Console.WriteLine ("#5: " + csts [0].FullName);
+			return 5;
+		}
+
+		return 0;
+	}
+}
+
+struct Foo<T> where T : struct
+{
+}
