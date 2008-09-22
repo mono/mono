998009021c7ef75fb43913ae7a9f7d669bdc8f9d
Marek Safar <marek.safar@gmail.com>
2008-09-22 16:06:08 +0000

    New tests.
    svn path=/trunk/mcs/; revision=113745

---
+++ mcs/tests/test-591.cs
@@ -0,0 +1,23 @@
+// It's actually C# specification and csc bug
+// https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=310363
+
+class C1
+{
+	static void Foo (string val)
+	{
+		const object obj = null;
+		switch (val) {
+			case (string) obj:
+				return;
+		}
+	}
+}
+
+class C2
+{
+	public static void Main ()
+	{
+		const object o = null;
+		const string s = (string) o;
+	}
+}
---
+++ mcs/tests/test-684.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace mono_bug
+{
+	class Program
+	{
+		static void Main ()
+		{
+
+			// initialise so no null errors
+			double [,] [] foo = new double [1, 1] [];
+			foo [0, 0] = new double [2];
+
+			double [,] [] bar;
+
+			bar = (double [,] []) foo.Clone ();
+
+			bar = (double [,] []) ReturnArray ();
+
+			// compiles & works correctly
+			bar = ReturnArray ();
+
+			Console.WriteLine (bar [0, 0] [1].ToString ());
+		}
+
+		private static double [,] [] ReturnArray ()
+		{
+			// just creates a "useless", multi-dimensional jagged array
+			double [,] [] zoo = new double [1, 1] [];
+			zoo [0, 0] = new double [2];
+			zoo [0, 0] [0] = 1;
+			zoo [0, 0] [1] = 2;
+			return zoo;
+		}
+	}
+}
