0d05dd0a800719930834c2b5df33e4109588e0c9
Miguel de Icaza <miguel@gnome.org>
2008-09-27 21:16:23 +0000

    2008-09-27  Miguel de Icaza  <miguel@novell.com>
    	* Abdicate: renamed csharplib.dll into Mono.CSharp.dll
    
    svn path=/trunk/mcs/; revision=114301

--- mcs/tools/csharplib/sign.cs
+++ mcs/tools/csharplib/sign.cs
@@ -1,7 +1,17 @@
 //
-// used to sign csharplib.dll
+// used to sign Mono.CSharp.dll
 //
 using System.Reflection;
 
 [assembly: AssemblyDelaySign (true)]
 [assembly: AssemblyKeyFile ("../../class/mono.pub")]
+
+[assembly: AssemblyTitle ("Embedded C# Compiler and Library")]
+[assembly: AssemblyProduct ("Mono")]
+[assembly: AssemblyCopyright ("2001 - 2008 Novell, Inc.")]
+[assembly: AssemblyCompany ("Novell, Inc.")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyConfiguration ("")]
+
+[assembly: AssemblyVersion ("2.1.0.0")]
+
