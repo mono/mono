e749dc5d1082035dc8f966e00c31808317d56793
Jb Evain <jbevain@gmail.com>
2008-10-01 14:42:44 +0000

    add cil-strip tool
    svn path=/trunk/mcs/; revision=114564

---
+++ mcs/tools/cil-strip/cilstrip.cs
@@ -0,0 +1,63 @@
+//
+// mono-cil-strip
+//
+// Author(s):
+//   Jb Evain (jbevain@novell.com)
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.IO;
+
+using Mono.Cecil;
+
+namespace Mono.CilStripper {
+
+	class Program {
+
+		static void Main (string [] args)
+		{
+			Header ();
+
+			if (args.Length == 0)
+				Usage ();
+
+			string file = args [0];
+			string output = args.Length > 1 ? args [1] : file;
+
+			try {
+				AssemblyDefinition assembly = AssemblyFactory.GetAssembly (file);
+				StripAssembly (assembly, output);
+
+				if (file != output)
+					Console.WriteLine ("Assembly {0} stripped out into {1}", file, output);
+				else
+					Console.WriteLine ("Assembly {0} stripped", file);
+			} catch (Exception e) {
+				Console.WriteLine ("Error: {0}", e);
+			}
+		}
+
+		static void StripAssembly (AssemblyDefinition assembly, string output)
+		{
+			Type stripper = typeof (AssemblyDefinition).Assembly.GetType ("Mono.Cecil.AssemblyStripper");
+			if (stripper == null)
+				throw new NotSupportedException ("Cecil doesn't have support for mono-cil-strip");
+
+			stripper.GetMethod ("StripAssembly").Invoke (null, new object [] { assembly, output });
+		}
+
+		static void Header ()
+		{
+			Console.WriteLine ("Mono CIL Stripper");
+			Console.WriteLine ();
+		}
+
+		static void Usage ()
+		{
+			Console.WriteLine ("Usage: mono-cil-strip file [output]");
+			Environment.Exit (1);
+		}
+	}
+}
