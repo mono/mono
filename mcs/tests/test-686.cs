38ede7dcbaf16e14b913aab2605ccd3cc01cbbbc
Marek Safar <marek.safar@gmail.com>
2008-10-02 18:01:46 +0000

    New test.
    svn path=/trunk/mcs/; revision=114691

---
+++ mcs/tests/test-686.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Test
+{
+	static void Main ()
+	{
+		string s = "test me";
+		foreach (char c in s)
+			Console.WriteLine (c);
+
+		Foo ();
+	}
+
+	static void Foo ()
+	{
+		string [,] s = new string [,] { { "a", "b" }, { "c", "d" } };
+		foreach (string c in s)
+			Console.WriteLine (c [0]);
+	}
+}
