5936fd231f78a10cf48bad8fb339b2ebfe65a991
Marek Safar <marek.safar@gmail.com>
2008-10-03 17:27:36 +0000

    New test.
    svn path=/trunk/mcs/; revision=114765

---
+++ mcs/tests/gtest-iter-10.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+class Test
+{
+	static IEnumerable<int> FromTo (int from, int to)
+	{
+		while (from <= to) yield return from++;
+	}
+
+	static int Main ()
+	{
+		IEnumerable<int> e = FromTo (1, 10);
+
+		int i = 0;
+		foreach (int x in e) {
+			foreach (int y in e) {
+				i += y;
+				Console.Write ("{0,3} ", x * y);
+			}
+			Console.WriteLine ();
+		}
+		
+		Console.WriteLine (i);
+		if (i != 550)
+			return i;
+		
+		return 0;
+	}
+}
