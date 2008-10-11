8da93899d951b323f037a52eb1fee2b325a962dc
Zoltan Varga <vargaz@gmail.com>
2008-10-11 15:20:52 +0000

    2008-10-11  Zoltan Varga  <vargaz@gmail.com>
    	Backport of r113969.
    	
    	* object.c (mono_object_get_virtual_method): Fix an assertion introduced
    	by the previous change.
    
    	Backport of r113166.
    	
    	* object.c (mono_object_get_virtual_method): Handle the case method->slot
    	is not set. Fixes #426309.
    
    svn path=/branches/mono-2-0/mono/; revision=115528

--- mono/metadata/object.c
+++ mono/metadata/object.c
@@ -2056,21 +2056,26 @@ mono_object_get_virtual_method (MonoObject *obj, MonoMethod *method)
 	mono_class_setup_vtable (klass);
 	vtable = klass->vtable;
 
-	/* check method->slot is a valid index: perform isinstance? */
-	if (method->klass->flags & TYPE_ATTRIBUTE_INTERFACE) {
-		if (!is_proxy)
-		       res = vtable [mono_class_interface_offset (klass, method->klass) + method->slot];
-	} else {
-		if (method->slot != -1) {
-			res = vtable [method->slot];
+	if (method->slot == -1) {
+		/* method->slot might not be set for instances of generic methods */
+		if (method->is_inflated) {
+			g_assert (((MonoMethodInflated*)method)->declaring->slot != -1);
+			method->slot = ((MonoMethodInflated*)method)->declaring->slot; 
 		} else {
-			/* method->slot might not be set for instances of generic methods in the AOT case */
-			if (method->is_inflated) {
-				g_assert (((MonoMethodInflated*)method)->declaring->slot != -1);
-				res = vtable [((MonoMethodInflated*)method)->declaring->slot];
-			}
+			if (!is_proxy)
+				g_assert_not_reached ();
 		}
 	}
+
+	/* check method->slot is a valid index: perform isinstance? */
+	if (method->slot != -1) {
+		if (method->klass->flags & TYPE_ATTRIBUTE_INTERFACE) {
+			if (!is_proxy)
+				res = vtable [mono_class_interface_offset (klass, method->klass) + method->slot];
+		} else {
+			res = vtable [method->slot];
+		}
+    }
 
 	if (is_proxy) {
 		/* It may be an interface, abstract class method or generic method */
