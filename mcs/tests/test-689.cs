ad8cae428f80ae253fe7af06fb1c9719f64f121f
Marek Safar <marek.safar@gmail.com>
2008-10-13 17:05:30 +0000

    New test.
    svn path=/trunk/mcs/; revision=115672

---
+++ mcs/tests/test-689.cs
@@ -0,0 +1,22 @@
+// Compiler options: -unsafe
+
+public struct Sample
+{
+	public static unsafe void Foo (byte* a, byte* b)
+	{
+		int* p = (int*)0;
+		long* s = (long*) ++p;
+		s = (long*) --p;
+		
+		int v = 0;
+		s = (long*) -v;
+		s = (long*) +v;
+
+		byte c = (byte)(*a++ * *b++);
+	}
+	
+	public static void Main ()
+	{
+	}
+}
+
