1c223cb9b3cf1939235c1f935d9fe2f15b1df06c
Marek Safar <marek.safar@gmail.com>
2008-10-14 12:33:06 +0000

    New test.
    svn path=/trunk/mcs/; revision=115770

---
+++ mcs/tests/gtest-etree-14.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+class Person
+{
+	public int Age { get; set; }
+}
+
+class Repro
+{
+	static int Main ()
+	{
+		var persons = GetPersons (new [] { new Person { Age = 25 }, new Person { Age = 21 } }, 25);
+		return persons.Count () - 1;
+	}
+
+	static IEnumerable<T> GetPersons<T> (IEnumerable<T> persons, int age) where T : Person
+	{
+		foreach (var person in persons)
+			if (Test (person, p => p.Age == age))
+				yield return person;
+	}
+
+	static bool Test<T> (T t, Expression<Func<T, bool>> predicate)
+	{
+		return predicate.Compile () (t);
+	}
+}
