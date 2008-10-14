3047748cd1b95ba5ae36d114064b98255117634b
Marek Safar <marek.safar@gmail.com>
2008-10-14 11:25:35 +0000

    New test.
    svn path=/trunk/mcs/; revision=115765

---
+++ mcs/tests/gtest-etree-13.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+class Program
+{
+	static int Main ()
+	{
+		foreach (int o in Test<bool> (1)) {
+		}
+
+		return 0;
+	}
+
+	static IEnumerable<int> Test<T> (int i)
+	{
+		Expression<Func<int>> e = () => i;
+		yield return 1;
+	}
+}
