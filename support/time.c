a44ad6af8493b7a01b72288bfa2312d93563fb5a
Jonathan Pryor <jpryor@novell.com>
2008-10-16 00:52:09 +0000

    * time.c: Add Mono_Posix_Syscall_nanosleep().
    svn path=/trunk/mono/; revision=115944

--- support/time.c
+++ support/time.c
@@ -17,6 +17,37 @@
 
 G_BEGIN_DECLS
 
+#if defined(HAVE_STRUCT_TIMESPEC) && _POSIX_C_SOURCE >= 199309L
+int
+Mono_Posix_Syscall_nanosleep (struct Mono_Posix_Timespec *req,
+		struct Mono_Posix_Timespec *rem)
+{
+	struct timespec _req, _rem, *prem;
+	int r;
+
+	if (req == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	if (Mono_Posix_FromTimespec (req, &_req) == -1)
+		return -1;
+
+	if (rem) {
+		if (Mono_Posix_FromTimespec (rem, &_rem) == -1)
+			return -1;
+		prem = &_rem;
+	}
+
+	r = nanosleep (&_req, prem);
+
+	if (rem && Mono_Posix_ToTimespec (prem, rem) == -1)
+		return -1;
+
+	return r;
+}
+#endif
+
 #ifdef HAVE_STIME
 gint32
 Mono_Posix_Syscall_stime (mph_time_t *t)
