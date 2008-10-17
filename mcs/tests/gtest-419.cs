6cfacb83764e09a239692a5de2588d97b162428f
Marek Safar <marek.safar@gmail.com>
2008-10-17 11:35:45 +0000

    New test.
    svn path=/trunk/mcs/; revision=116181

---
+++ mcs/tests/gtest-419.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+	static int Main ()
+	{
+		int? i = ((int?) -4);
+		int? i2 = (int?) +4;
+		object o = (object) (int?) 42;
+
+		return (bool) Test ("True") == true ? 0 : 1;
+	}
+
+	static object Test (string s)
+	{
+		return (!string.IsNullOrEmpty (s)) ? (bool?) bool.Parse (s) : null;
+	}
+}
