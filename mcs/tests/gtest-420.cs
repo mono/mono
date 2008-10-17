9a02a105f50a6c504a7c439cf4488d2f6cfeb95f
Marek Safar <marek.safar@gmail.com>
2008-10-17 15:19:17 +0000

    New test.
    svn path=/trunk/mcs/; revision=116240

---
+++ mcs/tests/gtest-420.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+class C
+{
+}
+
+class TestClass
+{
+	static int Test (object a, object b, params object[] args)
+	{
+		return 0;
+	}
+	
+	static int Test (object a, params object[] args)
+	{
+		return 1;
+	}
+	
+	static int Main ()
+	{
+		C c = new C ();
+		Test (c, c, new object [0]);
+		
+		var v = new Func<C, C, object[], int>(Test);
+		
+		return v (null, null, null);
+	}
+}
