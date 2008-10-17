a53adefec92482e23887e0a95e9f7fc9badca84c
Marek Safar <marek.safar@gmail.com>
2008-10-17 11:34:52 +0000

    New tests.
    svn path=/trunk/mcs/; revision=116180

---
+++ mcs/errors/cs0119-8.cs
@@ -0,0 +1,10 @@
+// CS0119: Expression denotes a `type', where a `variable', `value' or `method group' was expected
+// Line: 8
+
+class C
+{
+	public static void Main ()
+	{
+		int i = (C) +1;
+	}
+}
---
+++ mcs/errors/gcs1525-3.cs
@@ -0,0 +1,10 @@
+// CS1525: Unexpected symbol `)', expecting `identifier'
+// Line: 8
+
+class C
+{
+	public static void Main ()
+	{
+		object o = (int) => null;
+	}
+}
