f47118c64d9c7d703a5b96586295ce73e45dc5cf
Rodrigo Kumpera <kumpera@gmail.com>
2008-10-28 00:06:39 +0000

    2008-10-27 Rodrigo Kumpera <rkumpera@novell.com>
    	* Vector2d.cs: New vector type.
    
    2008-10-27  Rodrigo Kumpera  <rkumpera@novell.com>
    
    	* Mono.Simd.dll.sources: Added Vector2d.cs.
    
    svn path=/trunk/mcs/; revision=117223

---
+++ mcs/class/Mono.Simd/Mono.Simd/Vector2d.cs
@@ -0,0 +1,283 @@
+// Vector2d.cs
+//
+// Author:
+//   Rodrigo Kumpera (rkumpera@novell.com)
+//
+// (C) 2008 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mono.Simd
+{
+
+	[StructLayout(LayoutKind.Sequential, Pack = 0, Size = 16)]
+	public struct Vector2d
+	{
+		private double x;
+		private double y;
+
+		public double X { get { return x; } set { x = value; } }
+		public double Y { get { return y; } set { y = value; } }
+
+		public Vector2d (double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public static unsafe Vector2d operator & (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			int *a = (int*)&v1;
+			int *b = (int*)&v2;
+			int *c = (int*)&res;
+			*c++ = *a++ & *b++;
+			*c++ = *a++ & *b++;
+			*c++ = *a++ & *b++;
+			*c = *a & *b;
+			return res;
+		}
+
+		public static unsafe Vector2d operator | (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			int *a = (int*)&v1;
+			int *b = (int*)&v2;
+			int *c = (int*)&res;
+			*c++ = *a++ | *b++;
+			*c++ = *a++ | *b++;
+			*c++ = *a++ | *b++;
+			*c = *a | *b;
+			return res;
+		}
+
+		public static unsafe Vector2d operator ^ (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			int *a = (int*)&v1;
+			int *b = (int*)&v2;
+			int *c = (int*)&res;
+			*c++ = *a++ ^ *b++;
+			*c++ = *a++ ^ *b++;
+			*c++ = *a++ ^ *b++;
+			*c = *a ^ *b;
+			return res;
+		}
+
+		public static Vector2d operator + (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x + v2.x, v1.y + v2.y);
+		}
+
+		public static Vector2d operator - (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x - v2.x, v1.y - v2.y);
+		}
+
+		public static Vector2d operator * (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x * v2.x, v1.y * v2.y);
+		}
+
+		public static Vector2d operator / (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x / v2.x, v1.y / v2.y);
+		}
+
+		public static unsafe Vector2d AndNot (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			int *a = (int*)&v1;
+			int *b = (int*)&v2;
+			int *c = (int*)&res;
+			*c++ = ~*a++ & *b++;
+			*c++ = ~*a++ & *b++;
+			*c++ = ~*a++ & *b++;
+			*c = ~*a & *b;
+			return res;
+		}
+
+		public static Vector2d Max (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (System.Math.Max (v1.x, v2.x),
+								System.Math.Max (v1.y, v2.y));
+		}
+
+		public static Vector2d Min (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (System.Math.Min (v1.x, v2.x),
+								System.Math.Min (v1.y, v2.y));
+		}
+
+		public static Vector2d HorizontalAdd (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x + v1.y, v2.x + v2.y);
+		}
+
+		public static Vector2d AddSub (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x - v2.x, v1.y + v2.y);
+		}
+
+		public static Vector2d HorizontalSub (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x - v1.y, v2.x - v2.y);
+		}
+
+		public static Vector2d InterleaveHigh (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.y, v2.y);
+		}
+
+		public static Vector2d InterleaveLow (Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x, v2.x);
+		}
+
+		/*Same as a == b. */
+		public unsafe static Vector2d CompareEqual (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x == v2.x ? -1 : 0;
+			*c = v1.y == v2.y ? -1 : 0;
+			return res;		}
+
+		/*Same as a < b. */
+		public unsafe static Vector2d CompareLessThan (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x < v2.x ? -1 : 0;
+			*c = v1.y < v2.y ? -1 : 0;
+			return res;
+		}
+
+		/*Same as a <= b. */
+		public unsafe static Vector2d CompareLessEqual (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x <= v2.x ? -1 : 0;
+			*c = v1.y <= v2.y ? -1 : 0;
+			return res;		}
+
+		/*Same double.IsNaN (a) || double.IsNaN (b). */
+		public unsafe static Vector2d CompareUnordered (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = double.IsNaN (v1.x) || double.IsNaN (v2.x) ? -1 : 0;
+			*c = double.IsNaN (v1.y) || double.IsNaN (v2.y) ? -1 : 0;
+			return res;		}
+
+		/*Same as a != b. */
+		public unsafe static Vector2d CompareNotEqual (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x != v2.x ? -1 : 0;
+			*c = v1.y != v2.y ? -1 : 0;
+			return res;
+		}
+
+		/*Same as !(a < b). */
+		public unsafe static Vector2d CompareNotLessThan (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x < v2.x ? 0 : -1;
+			*c = v1.y < v2.y ? 0 : -1;
+			return res;
+		}
+
+		/*Same as !(a <= b). */
+		public unsafe static Vector2d CompareNotLessEqual (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x <= v2.x ? 0 : -1;
+			*c = v1.y <= v2.y ? 0 : -1;
+			return res;
+		}
+
+		/*Same !double.IsNaN (a) && !double.IsNaN (b). */
+		public unsafe static Vector2d CompareOrdered (Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = !double.IsNaN (v1.x) && !double.IsNaN (v2.x) ? -1 : 0;
+			*c = !double.IsNaN (v1.y) && !double.IsNaN (v2.y) ? -1 : 0;
+			return res;		}
+
+		public static Vector2d Duplicate (Vector2d v1)
+		{
+			return new Vector2d (v1.x, v1.x);
+		}
+
+		[CLSCompliant(false)]
+		public static unsafe explicit operator Vector4ui(Vector2d v)
+		{
+			Vector4ui* p = (Vector4ui*)&v;
+			return *p;
+		}
+
+		[CLSCompliant(false)]
+		public static unsafe explicit operator Vector8us(Vector2d v)
+		{
+			Vector8us* p = (Vector8us*)&v;
+			return *p;
+		}
+
+		[CLSCompliant(false)]
+		public static unsafe explicit operator Vector16b(Vector2d v)
+		{
+			Vector16b* p = (Vector16b*)&v;
+			return *p;
+		}
+
+		public static Vector2d LoadAligned (ref Vector2d v)
+		{
+			return v;
+		}
+
+		public static void StoreAligned (ref Vector2d res, Vector2d val)
+		{
+			res = val;
+		}
+
+		[CLSCompliant(false)]
+		public static unsafe Vector2d LoadAligned (Vector2d *v)
+		{
+			return *v;
+		}
+
+		[CLSCompliant(false)]
+		public static unsafe void StoreAligned (Vector2d *res, Vector2d val)
+		{
+			*res = val;
+		}
+
+	}
+}
