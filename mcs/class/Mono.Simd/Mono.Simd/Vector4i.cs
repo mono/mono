229760dce045ab651000a1c814da945d362d3d32
Rodrigo Kumpera <kumpera@gmail.com>
2008-10-29 18:36:05 +0000

    2008-10-29 Rodrigo Kumpera <rkumpera@novell.com>
    	* Vector4i.cs: Add Acceleration attribute to all methods.
    
    svn path=/trunk/mcs/; revision=117404

--- mcs/class/Mono.Simd/Mono.Simd/Vector4i.cs
+++ mcs/class/Mono.Simd/Mono.Simd/Vector4i.cs
@@ -50,59 +50,67 @@ namespace Mono.Simd
 			this.w = w;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i operator + (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i operator - (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w);
 		}
 
-		/*
-		 * Requires SSE 4.1
-		 */
+		[Acceleration (AccelMode.SSE41)]
 		public static Vector4i operator * (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x * v2.x, v1.y * v2.y, v1.z * v2.z, v1.w * v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector4i operator << (Vector4i v1, int amount)
 		{
 			return new Vector4i (v1.x << amount, v1.y << amount, v1.z << amount, v1.w << amount);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector4i operator >> (Vector4i v1, int amount)
 		{
 			return new Vector4i (v1.x >> amount, v1.y >> amount, v1.z >> amount, v1.w >> amount);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i operator & (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x & v2.x, v1.y & v2.y, v1.z & v2.z, v1.w & v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i operator | (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x | v2.x, v1.y | v2.y, v1.z | v2.z, v1.w | v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i operator ^ (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x ^ v2.x, v1.y ^ v2.y, v1.z ^ v2.z, v1.w ^ v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i UnpackLow (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.x, v2.x, v1.y, v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i UnpackHigh (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (v1.z, v2.z, v1.w, v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector4i ShiftRightLogic (Vector4i v1, int amount)
 		{
 			Vector4i res = new Vector4i ();
@@ -113,18 +121,19 @@ namespace Mono.Simd
 			return res;
 		}
 
-		/*Requires SSE 4.1*/
+		[Acceleration (AccelMode.SSE41)]
 		public static Vector4i Max (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (System.Math.Max (v1.x, v2.x), System.Math.Max (v1.y, v2.y), System.Math.Max (v1.z, v2.z), System.Math.Max (v1.w, v2.w));
 		}
 
-		/*Requires SSE 4.1*/
+		[Acceleration (AccelMode.SSE41)]
 		public static Vector4i Min (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i (System.Math.Min (v1.x, v2.x), System.Math.Min (v1.y, v2.y), System.Math.Min (v1.z, v2.z), System.Math.Min (v1.w, v2.w));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe int ExtractByteMask (Vector4i va) {
 			int res = 0;
 			byte *a = (byte*)&va;
@@ -133,6 +142,7 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector4i Shuffle (Vector4i v1, ShuffleSel sel)
 		{
 			int *ptr = (int*)&v1;
@@ -140,18 +150,20 @@ namespace Mono.Simd
 			return new Vector4i (*(ptr + ((idx >> 0) & 0x3)),*(ptr + ((idx >> 2) & 0x3)),*(ptr + ((idx >> 4) & 0x3)),*(ptr + ((idx >> 6) & 0x3)));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i CompareEqual (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i ((int)(v1.x ==  v2.x ? -1 : 0), (int)(v1.y ==  v2.y ? -1 : 0), (int)(v1.z ==  v2.z ? -1 : 0), (int)(v1.w ==  v2.w ? -1 : 0));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector4i CompareGreaterThan (Vector4i v1, Vector4i v2)
 		{
 			return new Vector4i ((int)(v1.x > v2.x ? -1 : 0), (int)(v1.y >  v2.y ? -1 : 0), (int)(v1.z >  v2.z ? -1 : 0), (int)(v1.w >  v2.w ? -1 : 0));
 		}
 
-		/* Requires SSE 4.1 */
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE41)]
 		public static unsafe Vector8us PackWithUnsignedSaturation (Vector4i va, Vector4i vb) {
 			Vector8us res = new Vector8us ();
 			int *a = (int*)&va;
@@ -164,6 +176,7 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector8s PackWithSignedSaturation (Vector4i va, Vector4i vb) {
 			Vector8s res = new Vector8s ();
 			int *a = (int*)&va;
@@ -176,6 +189,7 @@ namespace Mono.Simd
 			return res;
 		}
 
+ 		[Acceleration (AccelMode.SSE1)]
   		public static unsafe explicit operator Vector4f (Vector4i v1)
 		{
 			Vector4f* p = (Vector4f*)&v1;
@@ -183,6 +197,7 @@ namespace Mono.Simd
 		}
 
  		[CLSCompliant(false)]
+ 		[Acceleration (AccelMode.SSE1)]
  		public static unsafe explicit operator Vector8us (Vector4i v1)
 		{
 			Vector8us* p = (Vector8us*)&v1;
@@ -190,30 +205,34 @@ namespace Mono.Simd
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
   		public static unsafe explicit operator Vector16b (Vector4i v1)
 		{
 			Vector16b* p = (Vector16b*)&v1;
 			return *p;
 		}
 
-
+		[Acceleration (AccelMode.SSE1)]
 		public static Vector4i LoadAligned (ref Vector4i v)
 		{
 			return v;
 		}
 
+		[Acceleration (AccelMode.SSE1)]
 		public static void StoreAligned (ref Vector4i res, Vector4i val)
 		{
 			res = val;
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe Vector4i LoadAligned (Vector4i *v)
 		{
 			return *v;
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe void StoreAligned (Vector4i *res, Vector4i val)
 		{
 			*res = val;
