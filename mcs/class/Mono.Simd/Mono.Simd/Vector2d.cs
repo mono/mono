26bb57573aebc35ec2166f6e4b882ca545157c4a
Rodrigo Kumpera <kumpera@gmail.com>
2008-10-29 18:35:54 +0000

    2008-10-29 Rodrigo Kumpera <rkumpera@novell.com>
    	* Vector2d.cs: Add Acceleration attribute to all methods.
    
    svn path=/trunk/mcs/; revision=117396

--- mcs/class/Mono.Simd/Mono.Simd/Vector2d.cs
+++ mcs/class/Mono.Simd/Mono.Simd/Vector2d.cs
@@ -46,6 +46,7 @@ namespace Mono.Simd
 			this.y = y;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector2d operator & (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -59,6 +60,7 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector2d operator | (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -72,6 +74,7 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector2d operator ^ (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -85,26 +88,31 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d operator + (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x + v2.x, v1.y + v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d operator - (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x - v2.x, v1.y - v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d operator * (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x * v2.x, v1.y * v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d operator / (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x / v2.x, v1.y / v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static unsafe Vector2d AndNot (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -118,53 +126,63 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d Max (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (System.Math.Max (v1.x, v2.x),
 								System.Math.Max (v1.y, v2.y));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d Min (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (System.Math.Min (v1.x, v2.x),
 								System.Math.Min (v1.y, v2.y));
 		}
 
+		[Acceleration (AccelMode.SSE3)]
 		public static Vector2d HorizontalAdd (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x + v1.y, v2.x + v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE3)]
 		public static Vector2d AddSub (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x - v2.x, v1.y + v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE3)]
 		public static Vector2d HorizontalSub (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x - v1.y, v2.x - v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d InterleaveHigh (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.y, v2.y);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
 		public static Vector2d InterleaveLow (Vector2d v1, Vector2d v2)
 		{
 			return new Vector2d (v1.x, v2.x);
 		}
 
 		/*Same as a == b. */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareEqual (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
 			long *c = (long*)&res;
 			*c++ = v1.x == v2.x ? -1 : 0;
 			*c = v1.y == v2.y ? -1 : 0;
-			return res;		}
+			return res;
+		}
 
 		/*Same as a < b. */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareLessThan (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -175,24 +193,29 @@ namespace Mono.Simd
 		}
 
 		/*Same as a <= b. */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareLessEqual (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
 			long *c = (long*)&res;
 			*c++ = v1.x <= v2.x ? -1 : 0;
 			*c = v1.y <= v2.y ? -1 : 0;
-			return res;		}
+			return res;
+		}
 
 		/*Same double.IsNaN (a) || double.IsNaN (b). */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareUnordered (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
 			long *c = (long*)&res;
 			*c++ = double.IsNaN (v1.x) || double.IsNaN (v2.x) ? -1 : 0;
 			*c = double.IsNaN (v1.y) || double.IsNaN (v2.y) ? -1 : 0;
-			return res;		}
+			return res;
+		}
 
 		/*Same as a != b. */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareNotEqual (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -203,6 +226,7 @@ namespace Mono.Simd
 		}
 
 		/*Same as !(a < b). */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareNotLessThan (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -213,6 +237,7 @@ namespace Mono.Simd
 		}
 
 		/*Same as !(a <= b). */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareNotLessEqual (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
@@ -223,20 +248,24 @@ namespace Mono.Simd
 		}
 
 		/*Same !double.IsNaN (a) && !double.IsNaN (b). */
+		[Acceleration (AccelMode.SSE2)]
 		public unsafe static Vector2d CompareOrdered (Vector2d v1, Vector2d v2)
 		{
 			Vector2d res = new Vector2d ();
 			long *c = (long*)&res;
 			*c++ = !double.IsNaN (v1.x) && !double.IsNaN (v2.x) ? -1 : 0;
 			*c = !double.IsNaN (v1.y) && !double.IsNaN (v2.y) ? -1 : 0;
-			return res;		}
+			return res;
+		}
 
+		[Acceleration (AccelMode.SSE3)]
 		public static Vector2d Duplicate (Vector2d v1)
 		{
 			return new Vector2d (v1.x, v1.x);
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe explicit operator Vector4ui(Vector2d v)
 		{
 			Vector4ui* p = (Vector4ui*)&v;
@@ -244,6 +273,7 @@ namespace Mono.Simd
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe explicit operator Vector8us(Vector2d v)
 		{
 			Vector8us* p = (Vector8us*)&v;
@@ -251,29 +281,34 @@ namespace Mono.Simd
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe explicit operator Vector16b(Vector2d v)
 		{
 			Vector16b* p = (Vector16b*)&v;
 			return *p;
 		}
 
+		[Acceleration (AccelMode.SSE1)]
 		public static Vector2d LoadAligned (ref Vector2d v)
 		{
 			return v;
 		}
 
+		[Acceleration (AccelMode.SSE1)]
 		public static void StoreAligned (ref Vector2d res, Vector2d val)
 		{
 			res = val;
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe Vector2d LoadAligned (Vector2d *v)
 		{
 			return *v;
 		}
 
 		[CLSCompliant(false)]
+		[Acceleration (AccelMode.SSE1)]
 		public static unsafe void StoreAligned (Vector2d *res, Vector2d val)
 		{
 			*res = val;
