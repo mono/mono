899b4184dc5545a92bb2ff84fa75fd2c32e275bf
Sebastien Pouliot <sebastien@ximian.com>
2008-11-02 00:22:45 +0000

    2008-11-01  Sebastien Pouliot  <sebastien@ximian.com>
    	* CmsRecipientCollection.cs: Add comment about compatibility-NRE
    	[Found using Gendarme CheckParametersNullityInVisibleMethodsRule]
    
    
    svn path=/trunk/mcs/; revision=117621

--- mcs/class/System.Security/System.Security.Cryptography.Pkcs/CmsRecipientCollection.cs
+++ mcs/class/System.Security/System.Security.Cryptography.Pkcs/CmsRecipientCollection.cs
@@ -5,7 +5,7 @@
 //	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // (C) 2003 Motus Technologies Inc. (http://www.motus.com)
-// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2004-2005, 2008 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -45,13 +45,14 @@ namespace System.Security.Cryptography.Pkcs {
 			_list = new ArrayList ();
 		}
 
-		public CmsRecipientCollection (CmsRecipient recipient) : base () 
+		public CmsRecipientCollection (CmsRecipient recipient)
 		{
 			_list.Add (recipient);
 		}
 
-		public CmsRecipientCollection (SubjectIdentifierType recipientIdentifierType, X509Certificate2Collection certificates) : base () 
+		public CmsRecipientCollection (SubjectIdentifierType recipientIdentifierType, X509Certificate2Collection certificates)
 		{
+			// no null check, MS throws a NullReferenceException here
 			foreach (X509Certificate2 x509 in certificates) {
 				CmsRecipient p7r = new CmsRecipient (recipientIdentifierType, x509);
 				_list.Add (p7r);
