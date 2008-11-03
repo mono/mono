d18c3793a2a73900196e28ca609517dbd0934e07
Marek Safar <marek.safar@gmail.com>
2008-11-03 17:08:48 +0000

    2008-11-03  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #440774
    
    
    svn path=/trunk/mcs/; revision=117780

---
+++ mcs/tests/test-691.cs
@@ -0,0 +1,31 @@
+// Compiler options: -unsafe
+
+using System;
+
+namespace ConsoleApplication6
+{
+	unsafe class Program
+	{
+		static int a;
+		static int* the_ptr = (int*) 0xdeadbeaf;
+		static int** the_pptr = (int**) 0xdeadbeaf;
+
+		static void Main ()
+		{
+			Console.WriteLine ("TEST: {0:x}", new IntPtr (the_pptr).ToInt64 ());
+
+			fixed (int* a_ptr = &a) {
+				Console.WriteLine (new IntPtr (a_ptr));
+
+				int*[] array = { the_ptr };
+				int*[] array2 = { a_ptr };
+				int* ptr = the_ptr;
+				int** pptr = the_pptr;
+
+				fixed (int** pptr2 = &the_ptr) {
+					Console.WriteLine (new IntPtr (pptr));
+				}
+			}
+		}
+	}
+}
