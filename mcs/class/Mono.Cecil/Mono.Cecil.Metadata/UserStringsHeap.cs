2dfcec5660394ae4aefd193f44a7090bdf072838
Jb Evain <jbevain@gmail.com>
2008-11-05 20:53:33 +0000

    protect against some oddness
    svn path=/trunk/mcs/; revision=118033

--- mcs/class/Mono.Cecil/Mono.Cecil.Metadata/UserStringsHeap.cs
+++ mcs/class/Mono.Cecil/Mono.Cecil.Metadata/UserStringsHeap.cs
@@ -61,6 +61,9 @@ namespace Mono.Cecil.Metadata {
 			if (length < 1)
 				return string.Empty;
 
+			if ((length & 1) != 0)
+				length--;
+
 			char [] chars = new char [length / 2];
 
 			for (int i = offset, j = 0; i < offset + length; i += 2)
