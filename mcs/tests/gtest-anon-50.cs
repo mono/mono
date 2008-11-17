3a3b8bec969232a7f8cb193375fc35c15cdbdff1
Marek Safar <marek.safar@gmail.com>
2008-11-17 13:18:40 +0000

    2008-11-17  Marek Safar  <marek.safar@gmail.com>
    	A test for bug #444673
    	
    
    svn path=/trunk/mcs/; revision=119015

---
+++ mcs/tests/gtest-anon-50.cs
@@ -0,0 +1,29 @@
+class M
+{
+	public static int Main ()
+	{
+		new SomeGenericClass<int>().FailsToCompile ();
+		return 0;
+	}
+}
+
+class SomeGenericClass<SomeType>
+{
+	object someValue;
+	delegate void SomeHandlerType ();
+
+	void Invoke (SomeHandlerType h)
+	{
+	}
+
+	public void FailsToCompile ()
+	{
+		Invoke (delegate {
+			object someObject = 1;
+			Invoke (delegate {
+				someValue = someObject;
+			});
+		});
+	}
+}
+
