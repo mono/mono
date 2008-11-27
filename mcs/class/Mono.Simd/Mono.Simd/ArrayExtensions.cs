08cc81ee34e3c6e55ec59d25dfc69ce7d9c5254e
Rodrigo Kumpera <kumpera@gmail.com>
2008-11-27 19:59:49 +0000

    2008-11-27 Rodrigo Kumpera <rkumpera@novell.com>
    	* ArrayExtensions.cs: Add Get/SetVector variants for
    	all vector types.
    
    	* ArrayExtensions.cs: Add IsAligned query method to check
    	is an index of a given array is property aligned for simd.
    
    svn path=/trunk/mcs/; revision=120175

--- mcs/class/Mono.Simd/Mono.Simd/ArrayExtensions.cs
+++ mcs/class/Mono.Simd/Mono.Simd/ArrayExtensions.cs
@@ -33,6 +33,41 @@ namespace Mono.Simd
 	{
 		[Acceleration (AccelMode.SSE1)]
 		[CLSCompliant(false)]
+		public static Vector2d GetVector (this double[] array, int offset)
+		{
+			return new Vector2d (array [offset], array [offset + 1]);
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector2l GetVector (this long[] array, int offset)
+		{
+			return new Vector2l (array [offset], array [offset + 1]);
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector2ul GetVector (this ulong[] array, int offset)
+		{
+			return new Vector2ul (array [offset], array [offset + 1]);
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector4f GetVector (this float[] array, int offset)
+		{
+			return new Vector4f (array [offset], array [offset + 1], array [offset + 2], array [offset + 3]);
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector4i GetVector (this int[] array, int offset)
+		{
+			return new Vector4i (array [offset], array [offset + 1], array [offset + 2], array [offset + 3]);
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
 		public static Vector4ui GetVector (this uint[] array, int offset)
 		{
 			return new Vector4ui (array [offset], array [offset + 1], array [offset + 2], array [offset + 3]);
@@ -40,12 +75,157 @@ namespace Mono.Simd
 
 		[Acceleration (AccelMode.SSE1)]
 		[CLSCompliant(false)]
-		public static void SetValue (this uint[] array, Vector4ui val, int offset)
+		public static Vector8s GetVector (this short[] array, int offset)
+		{
+			if (offset < 0 || offset > array.Length - 8)
+				throw new IndexOutOfRangeException ();
+			unsafe {
+				fixed (void *ptr = &array[0]) {
+					return *(Vector8s*)ptr;
+				}
+			}
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector8us GetVector (this ushort[] array, int offset)
+		{
+			if (offset < 0 || offset > array.Length - 8)
+				throw new IndexOutOfRangeException ();
+			unsafe {
+				fixed (void *ptr = &array[0]) {
+					return *(Vector8us*)ptr;
+				}
+			}
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector16sb GetVector (this sbyte[] array, int offset)
+		{
+			if (offset < 0 || offset > array.Length - 16)
+				throw new IndexOutOfRangeException ();
+			unsafe {
+				fixed (void *ptr = &array[0]) {
+					return *(Vector16sb*)ptr;
+				}
+			}
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static Vector16b GetVector (this byte[] array, int offset)
+		{
+			if (offset < 0 || offset > array.Length - 16)
+				throw new IndexOutOfRangeException ();
+			unsafe {
+				fixed (void *ptr = &array[0]) {
+					return *(Vector16b*)ptr;
+				}
+			}
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this double[] array, Vector2d val, int offset)
+		{
+			array [offset + 0] = val.X;
+			array [offset + 1] = val.Y;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this long[] array, Vector2l val, int offset)
+		{
+			array [offset + 0] = val.X;
+			array [offset + 1] = val.Y;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this ulong[] array, Vector2ul val, int offset)
+		{
+			array [offset + 0] = val.X;
+			array [offset + 1] = val.Y;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this float[] array, Vector4f val, int offset)
 		{
 			array [offset + 0] = val.X;
 			array [offset + 1] = val.Y;
 			array [offset + 2] = val.Z;
 			array [offset + 3] = val.W;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this int[] array, Vector4i val, int offset)
+		{
+			array [offset + 0] = val.X;
+			array [offset + 1] = val.Y;
+			array [offset + 2] = val.Z;
+			array [offset + 3] = val.W;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this uint[] array, Vector4ui val, int offset)
+		{
+			array [offset + 0] = val.X;
+			array [offset + 1] = val.Y;
+			array [offset + 2] = val.Z;
+			array [offset + 3] = val.W;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this short[] array, Vector8s val, int offset)
+		{
+			array [offset + 0] = val.V0;
+			array [offset + 1] = val.V1;
+			array [offset + 2] = val.V2;
+			array [offset + 3] = val.V3;
+			array [offset + 4] = val.V4;
+			array [offset + 5] = val.V5;
+			array [offset + 6] = val.V6;
+			array [offset + 7] = val.V7;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this ushort[] array, Vector8us val, int offset)
+		{
+			array [offset + 0] = val.V0;
+			array [offset + 1] = val.V1;
+			array [offset + 2] = val.V2;
+			array [offset + 3] = val.V3;
+			array [offset + 4] = val.V4;
+			array [offset + 5] = val.V5;
+			array [offset + 6] = val.V6;
+			array [offset + 7] = val.V7;
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this sbyte[] array, Vector16sb val, int offset)
+		{
+			for (int i = 0; i < 16; ++i)
+				array [offset + i] = val [i];
+		}
+
+		[Acceleration (AccelMode.SSE1)]
+		[CLSCompliant(false)]
+		public static void SetVector (this byte[] array, Vector16b val, int offset)
+		{
+			for (int i = 0; i < 16; ++i)
+				array [offset + i] = val [i];
+		}
+
+		public static bool IsAligned<T> (this T[] vect, int index) where T : struct
+		{
+			return false;
 		}
 	}
 }
