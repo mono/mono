9c6083df226e05e0e490baadafb2f8e53573f9a8
Marek Safar <marek.safar@gmail.com>
2008-11-28 15:55:34 +0000

    New test.
    svn path=/trunk/mcs/; revision=120242

---
+++ mcs/tests/gtest-430.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Tmp
+{
+	public int stuff;
+}
+public class Driver
+{
+	Tmp tmp;
+
+	public int? Prop {
+		get { return tmp != null ? tmp.stuff : (int?)null; }
+	}
+
+	static int Main ()
+	{
+		int? r = new Driver().Prop;
+		Console.WriteLine (r);
+		return r.HasValue ? 1 : 0;
+	}
+}
