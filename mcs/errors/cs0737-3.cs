4b309832eb3f51b7acc5f0baaf75afb72fd66d5d
Marek Safar <marek.safar@gmail.com>
2008-12-02 17:59:28 +0000

    New test.
    svn path=/trunk/mcs/; revision=120511

---
+++ mcs/errors/cs0737-3.cs
@@ -0,0 +1,22 @@
+// CS0737: `MySubClass' does not implement interface member `I.Foo.set' and the best implementing candidate `MyTest.Foo.set' in not public
+// Line: 6
+
+using System;
+
+interface I
+{
+	int Foo { get; set; }
+}
+
+public class MySubClass : MyTest, I
+{
+}
+
+public class MyTest
+{
+	public int Foo
+	{
+		get { return 1; }
+		protected set { }
+	}
+}
