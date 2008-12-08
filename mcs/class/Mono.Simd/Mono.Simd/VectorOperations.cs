c50f40f70c3255f7b5a118fa9ec2a7792e63ce85
Rodrigo Kumpera <kumpera@gmail.com>
2008-12-08 22:22:39 +0000

    2008-12-08 Rodrigo Kumpera <rkumpera@novell.com>
    	* VectorOperations.cs: Move methods from Vector2d.
    
    	* Vector2d.cs: Same.
    
    svn path=/trunk/mcs/; revision=121046

--- mcs/class/Mono.Simd/Mono.Simd/VectorOperations.cs
+++ mcs/class/Mono.Simd/Mono.Simd/VectorOperations.cs
@@ -46,6 +46,19 @@ namespace Mono.Simd
 			return res;
 		}
 
+		[Acceleration (AccelMode.SSE2)]
+		public static unsafe Vector2d AndNot (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			int *a = (int*)&v1;
+			int *b = (int*)&v2;
+			int *c = (int*)&res;
+			*c++ = ~*a++ & *b++;
+			*c++ = ~*a++ & *b++;
+			*c++ = ~*a++ & *b++;
+			*c = ~*a & *b;
+			return res;
+		}
 
 		/* ==== Math operations ==== */
 
@@ -82,6 +95,13 @@ namespace Mono.Simd
 								System.Math.Max (v1.w, v2.w));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
+		public static Vector2d Max (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (System.Math.Max (v1.x, v2.x),
+								System.Math.Max (v1.y, v2.y));
+		}
+
 		[Acceleration (AccelMode.SSE1)]
 		public static Vector4f Min (this Vector4f v1, Vector4f v2)
 		{
@@ -91,6 +111,12 @@ namespace Mono.Simd
 								System.Math.Min (v1.w, v2.w));
 		}
 
+		[Acceleration (AccelMode.SSE2)]
+		public static Vector2d Min (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (System.Math.Min (v1.x, v2.x),
+								System.Math.Min (v1.y, v2.y));
+		}
 
 		/* ==== Horizontal operations ==== */
 
@@ -98,6 +124,23 @@ namespace Mono.Simd
 		public static Vector4f HorizontalAdd (this Vector4f v1, Vector4f v2)
 		{
 			return new Vector4f (v1.x + v1.y, v1.z + v1.w, v2.x + v2.y, v2.z + v2.w);
+		}
+		[Acceleration (AccelMode.SSE3)]
+		public static Vector2d HorizontalAdd (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x + v1.y, v2.x + v2.y);
+		}
+
+		[Acceleration (AccelMode.SSE3)]
+		public static Vector4f HorizontalSub (this Vector4f v1, Vector4f v2)
+		{
+			return new Vector4f (v1.x - v1.y, v1.z - v1.w, v2.x - v2.y, v2.z - v2.w);
+		}
+
+		[Acceleration (AccelMode.SSE3)]
+		public static Vector2d HorizontalSub (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x - v1.y, v2.x - v2.y);
 		}
 
 		[Acceleration (AccelMode.SSE3)]
@@ -107,11 +150,10 @@ namespace Mono.Simd
 		}
 
 		[Acceleration (AccelMode.SSE3)]
-		public static Vector4f HorizontalSub (this Vector4f v1, Vector4f v2)
+		public static Vector2d AddSub (this Vector2d v1, Vector2d v2)
 		{
-			return new Vector4f (v1.x - v1.y, v1.z - v1.w, v2.x - v2.y, v2.z - v2.w);
+			return new Vector2d (v1.x - v2.x, v1.y + v2.y);
 		}
-
 
 		/* ==== Compare methods ==== */
 
@@ -128,6 +170,17 @@ namespace Mono.Simd
 			return res;
 		}
 
+		/*Same as a == b. */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareEqual (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x == v2.x ? -1 : 0;
+			*c = v1.y == v2.y ? -1 : 0;
+			return res;
+		}
+
 		/*Same as a < b. */
 		[Acceleration (AccelMode.SSE1)]
 		public unsafe static Vector4f CompareLessThan (this Vector4f v1, Vector4f v2)
@@ -138,6 +191,17 @@ namespace Mono.Simd
 			*c++ = v1.y < v2.y ? -1 : 0;
 			*c++ = v1.z < v2.z ? -1 : 0;
 			*c = v1.w < v2.w ? -1 : 0;
+			return res;
+		}
+
+		/*Same as a < b. */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareLessThan (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x < v2.x ? -1 : 0;
+			*c = v1.y < v2.y ? -1 : 0;
 			return res;
 		}
 
@@ -154,6 +218,17 @@ namespace Mono.Simd
 			return res;
 		}
 
+		/*Same as a <= b. */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareLessEqual (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x <= v2.x ? -1 : 0;
+			*c = v1.y <= v2.y ? -1 : 0;
+			return res;
+		}
+
 		/*Same float.IsNaN (a) || float.IsNaN (b). */
 		[Acceleration (AccelMode.SSE1)]
 		public unsafe static Vector4f CompareUnordered (this Vector4f v1, Vector4f v2)
@@ -164,6 +239,17 @@ namespace Mono.Simd
 			*c++ = float.IsNaN (v1.y) || float.IsNaN (v2.y) ? -1 : 0;
 			*c++ = float.IsNaN (v1.z) || float.IsNaN (v2.z) ? -1 : 0;
 			*c = float.IsNaN (v1.w) || float.IsNaN (v2.w) ? -1 : 0;
+			return res;
+		}
+
+		/*Same double.IsNaN (a) || double.IsNaN (b). */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareUnordered (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = double.IsNaN (v1.x) || double.IsNaN (v2.x) ? -1 : 0;
+			*c = double.IsNaN (v1.y) || double.IsNaN (v2.y) ? -1 : 0;
 			return res;
 		}
 
@@ -180,6 +266,17 @@ namespace Mono.Simd
 			return res;
 		}
 
+		/*Same as a != b. */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareNotEqual (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x != v2.x ? -1 : 0;
+			*c = v1.y != v2.y ? -1 : 0;
+			return res;
+		}
+
 		/*Same as !(a < b). */
 		[Acceleration (AccelMode.SSE1)]
 		public unsafe static Vector4f CompareNotLessThan (this Vector4f v1, Vector4f v2)
@@ -190,6 +287,17 @@ namespace Mono.Simd
 			*c++ = v1.y < v2.y ? 0 : -1;
 			*c++ = v1.z < v2.z ? 0 : -1;
 			*c = v1.w < v2.w ? 0 : -1;
+			return res;
+		}
+
+		/*Same as !(a < b). */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareNotLessThan (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x < v2.x ? 0 : -1;
+			*c = v1.y < v2.y ? 0 : -1;
 			return res;
 		}
 
@@ -206,6 +314,17 @@ namespace Mono.Simd
 			return res;
 		}
 
+		/*Same as !(a <= b). */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareNotLessEqual (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = v1.x <= v2.x ? 0 : -1;
+			*c = v1.y <= v2.y ? 0 : -1;
+			return res;
+		}
+
 		/*Same !float.IsNaN (a) && !float.IsNaN (b). */
 		[Acceleration (AccelMode.SSE1)]
 		public unsafe static Vector4f CompareOrdered (this Vector4f v1, Vector4f v2)
@@ -219,6 +338,17 @@ namespace Mono.Simd
 			return res;
 		}
 
+		/*Same !double.IsNaN (a) && !double.IsNaN (b). */
+		[Acceleration (AccelMode.SSE2)]
+		public unsafe static Vector2d CompareOrdered (this Vector2d v1, Vector2d v2)
+		{
+			Vector2d res = new Vector2d ();
+			long *c = (long*)&res;
+			*c++ = !double.IsNaN (v1.x) && !double.IsNaN (v2.x) ? -1 : 0;
+			*c = !double.IsNaN (v1.y) && !double.IsNaN (v2.y) ? -1 : 0;
+			return res;
+		}
+
 
 		/* ==== Data shuffling ==== */
 
@@ -228,10 +358,28 @@ namespace Mono.Simd
 			return new Vector4f (v1.z, v2.z, v1.w, v2.w);
 		}
 
+		[Acceleration (AccelMode.SSE2)]
+		public static Vector2d InterleaveHigh (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.y, v2.y);
+		}
+
 		[Acceleration (AccelMode.SSE1)]
 		public static Vector4f InterleaveLow (this Vector4f v1, Vector4f v2)
 		{
 			return new Vector4f (v1.x, v2.x, v1.y, v2.y);
+		}
+
+		[Acceleration (AccelMode.SSE2)]
+		public static Vector2d InterleaveLow (this Vector2d v1, Vector2d v2)
+		{
+			return new Vector2d (v1.x, v2.x);
+		}
+
+		[Acceleration (AccelMode.SSE3)]
+		public static Vector2d Duplicate (this Vector2d v1)
+		{
+			return new Vector2d (v1.x, v1.x);
 		}
 
 		[Acceleration (AccelMode.SSE3)]
