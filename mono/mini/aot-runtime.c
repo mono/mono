dcdfe8cc08b08ed8efc7d2a328ba01af41bf2a20
Zoltan Varga <vargaz@gmail.com>
2008-12-09 20:16:05 +0000

    2008-12-09  Zoltan Varga  <vargaz@gmail.com>
    	Backport of r121150.
    	
    	* aot-runtime.c (load_method): Avoid calling decode_exception_debug_info
    	while holding the aot lock.
    
    svn path=/branches/mono-2-0/mono/; revision=121153

--- mono/mini/aot-runtime.c
+++ mono/mini/aot-runtime.c
@@ -1063,6 +1063,9 @@ mono_aot_get_class_from_name (MonoImage *image, const char *name_space, const ch
 	return TRUE;
 }
 
+/*
+ * LOCKING: Acquires the domain lock.
+ */
 static MonoJitInfo*
 decode_exception_debug_info (MonoAotModule *aot_module, MonoDomain *domain, 
 							 MonoMethod *method, guint8* ex_info, guint8 *code)
@@ -1756,10 +1759,6 @@ mono_aot_load_method (MonoDomain *domain, MonoAotModule *aot_module, MonoMethod 
 			mono_mempool_destroy (mp);
 	}
 
-	mono_aot_lock ();
-
-	mono_jit_stats.methods_aot++;
-
 	if (mono_trace_is_traced (G_LOG_LEVEL_DEBUG, MONO_TRACE_AOT)) {
 		char *full_name = mono_method_full_name (method, TRUE);
 
@@ -1771,6 +1770,10 @@ mono_aot_load_method (MonoDomain *domain, MonoAotModule *aot_module, MonoMethod 
 		mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_AOT, "AOT FOUND AOT compiled code for %s %p - %p %p\n", full_name, code, code + jinfo->code_size, info);
 		g_free (full_name);
 	}
+
+	mono_aot_lock ();
+
+	mono_jit_stats.methods_aot++;
 
 	aot_module->methods_loaded [method_index / 32] |= 1 << (method_index % 32);
 
