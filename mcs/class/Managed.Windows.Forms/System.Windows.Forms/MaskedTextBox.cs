0b8e5472358831fc34b25d2fa0fd30a67e172ba3
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2008-12-11 11:37:57 +0000

    2008-12-11  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* MaskedTextBox.cs: MaskedTextService.Replace doesn't expect the
    	length of the text, but the end position, so we need to substract 1 to
    	have a valid value. Also, in the same InputText method, don't use
    	SelectionLength as the length of the text, since the selected text
    	could actually be empty, but we need to set the value anyway.
    	Fixes #457370.
    
    
    svn path=/trunk/mcs/; revision=121285

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/MaskedTextBox.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/MaskedTextBox.cs
@@ -853,14 +853,14 @@ namespace System.Windows.Forms
 			
 			if (RejectInputOnFirstFailure) {
 				if (overwrite) {
-					provider.Replace (input, SelectionStart, SelectionStart + SelectionLength, out testPosition, out resultHint);
+					provider.Replace (input, SelectionStart, SelectionStart + input.Length - 1, out testPosition, out resultHint);
 				} else {
 					provider.InsertAt (input, SelectionStart);
 				}
 			} else {
 				while (!result && input.Length > 0) {
 					if (overwrite) {
-						result = provider.Replace (input, SelectionStart, SelectionStart + input.Length, out testPosition, out resultHint);
+						result = provider.Replace (input, SelectionStart, SelectionStart + input.Length - 1, out testPosition, out resultHint);
 					} else {
 						result = provider.InsertAt (input, SelectionStart, out testPosition, out resultHint);
 					}
