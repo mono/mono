5c0e7ce3ded5564ccb8b83b5b81002a314a25053
Zoltan Varga <vargaz@gmail.com>
2008-12-12 19:55:32 +0000

    2008-12-12  Zoltan Varga  <vargaz@gmail.com>
    	Backport of r121457.
    	
    	* mini-arm.c: Fix calls to mono_arch_flush_icache.
    
    svn path=/branches/mono-2-0/mono/; revision=121459

--- mono/mini/mini-arm.c
+++ mono/mini/mini-arm.c
@@ -373,7 +373,7 @@ mono_arch_get_delegate_invoke_impl (MonoMethodSignature *sig, gboolean has_targe
 
 		g_assert ((code - start) <= 12);
 
-		mono_arch_flush_icache (code, 12);
+		mono_arch_flush_icache (start, 12);
 		cached = start;
 		mono_mini_arch_unlock ();
 		return cached;
@@ -406,7 +406,7 @@ mono_arch_get_delegate_invoke_impl (MonoMethodSignature *sig, gboolean has_targe
 
 		g_assert ((code - start) <= size);
 
-		mono_arch_flush_icache (code, size);
+		mono_arch_flush_icache (start, size);
 		cache [sig->param_count] = start;
 		mono_mini_arch_unlock ();
 		return start;
