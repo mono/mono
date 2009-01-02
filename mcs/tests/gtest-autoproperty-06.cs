0c5a2b9db77955a42de5f166a8b92e05173fb617
Marek Safar <marek.safar@gmail.com>
2009-01-02 15:45:24 +0000

    New test.
    svn path=/trunk/mcs/; revision=122338

---
+++ mcs/tests/gtest-autoproperty-06.cs
@@ -0,0 +1,21 @@
+class A { }
+class B { }
+
+interface I<T>
+{
+	T Prop { get; set; }
+}
+
+class C : I<A>, I<B>
+{
+	B I<B>.Prop { get; set; }
+	A I<A>.Prop { get; set; }
+}
+
+class Program
+{
+	static void Main (string[] args)
+	{
+		C c = new C ();
+	}
+}
