54c57a1163b3b6286778c1e89f2bd4df4e4b5600
Marek Safar <marek.safar@gmail.com>
2009-01-06 16:17:36 +0000

    New test.
    svn path=/trunk/mcs/; revision=122559

---
+++ mcs/tests/test-701.cs
@@ -0,0 +1,20 @@
+// Compiler options: -warnaserror -warn:4
+
+using System;
+
+[assembly: CLSCompliant (true)]
+
+public class Foo
+{
+#pragma warning disable 3019
+	[CLSCompliant (false)]
+#pragma warning restore 3019
+	internal ushort ToUint16 ()
+	{
+		return ushort.MaxValue;
+	}
+
+	static void Main ()
+	{
+	}
+}
