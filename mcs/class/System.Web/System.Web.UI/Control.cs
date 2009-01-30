0c0fa5ec89311474de42afc1c8d891828f648c49
Marek Habersack <grendel@twistedcode.net>
2009-01-30 16:53:05 +0000

    Backport of r125111.
    svn path=/branches/mono-2-4/mcs/; revision=125112

--- mcs/class/System.Web/System.Web.UI/Control.cs
+++ mcs/class/System.Web/System.Web.UI/Control.cs
@@ -1622,7 +1622,10 @@ namespace System.Web.UI
 			if (Adapter != null)
 				thisAdapterViewState = Adapter.SaveAdapterViewState ();
 #endif
-			object thisState = SaveViewState ();
+			object thisState = null;
+
+			if (EnableViewState)
+				thisState = SaveViewState ();
 
 			if (thisState == null && controlList == null && controlStates == null) {
 				if (trace != null) {
