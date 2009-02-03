a6a296be2c0b8811ee5f62110b77846e379068e9
Marek Safar <marek.safar@gmail.com>
2009-02-03 12:08:17 +0000

    New test, update.
    svn path=/trunk/mcs/; revision=125509

---
+++ mcs/tests/gtest-iter-11.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Foo {}
+	
+class Bar : Foo {
+	public string Name { get; set; }
+}
+
+class Collection<T> : IEnumerable<T> where T : Foo {
+	List<T> list = new List<T> ();
+
+	public void Add (T t)
+	{
+		list.Add (t);
+	}
+
+	public IEnumerator<T> GetEnumerator ()
+	{
+		foreach (T t in list)
+			yield return t;
+	}
+
+	IEnumerator IEnumerable.GetEnumerator ()
+	{
+		return GetEnumerator ();
+	}
+}
+
+class BarCollection : Collection<Bar> {}
+
+class Program {
+
+	static int Main ()
+	{
+		var collection = new BarCollection () {
+			new Bar { Name = "a" },
+			new Bar { Name = "b" },
+			new Bar { Name = "c" },
+		};
+
+		foreach (var bar in collection)
+			Console.WriteLine (bar.Name);
+		
+		return 0;
+	}
+}
