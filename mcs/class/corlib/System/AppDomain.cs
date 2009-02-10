42d8948ea3347bafa516a247f23b8bdb8f8cc749
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2009-02-10 07:37:33 +0000

    2009-02-10 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* AppDomain.cs: this fixes the remoting tests.
    	Zoltan found out that there was a domain improperly using shadow
    	copy.
    
    
    svn path=/branches/mono-2-4/mcs/; revision=126431

--- mcs/class/corlib/System/AppDomain.cs
+++ mcs/class/corlib/System/AppDomain.cs
@@ -966,7 +966,11 @@ namespace System {
 			if (shadowCopyFiles)
 				info.ShadowCopyFiles = "true";
 			else
+#if NET_2_0
 				info.ShadowCopyFiles = "false";
+#else
+				info.ShadowCopyFiles = null;
+#endif
 
 			return info;
 		}
