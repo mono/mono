e74faa5cd0bb193c5f3fb5c2fce64c8ab51307e0
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2009-02-12 07:43:30 +0000

    2009-02-12 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* MonoIO.cs:
    	* MonoIOError.cs: enable ERROR_NOT_SAME_DEVICE.
    
    
    svn path=/trunk/mcs/; revision=126688

--- mcs/class/corlib/System.IO/MonoIO.cs
+++ mcs/class/corlib/System.IO/MonoIO.cs
@@ -152,6 +152,10 @@ namespace System.IO
 				message = String.Format ("Path {0} is a directory", path);
 				return new IOException (message, unchecked((int)0x80070000) | (int)error);
 				
+			case MonoIOError.ERROR_NOT_SAME_DEVICE:
+				message = "Source and destination are not on the same device";
+				return new IOException (message, unchecked((int)0x80070000) | (int)error);
+				
 			default:
 				message = String.Format ("Win32 IO returned {0}. Path: {1}", error, path);
 				return new IOException (message, unchecked((int)0x80070000) | (int)error);
