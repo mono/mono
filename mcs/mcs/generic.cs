eceec54500f59a3aeff1ec44131863444ce21284
Marek Safar <marek.safar@gmail.com>
2009-02-16 15:33:42 +0000

    2009-02-16  Marek Safar  <marek.safar@gmail.com>
    	A fix for bug #475965
    	* generics.cs: Check generic parameter type after extracting from
    	Expression<T>.
    
    
    svn path=/branches/mono-2-4/mcs/; revision=127041

--- mcs/mcs/generic.cs
+++ mcs/mcs/generic.cs
@@ -2454,8 +2454,6 @@ namespace Mono.CSharp {
 			ArrayList types_to_fix = new ArrayList (unfixed_types);
 			for (int i = 0; i < methodParameters.Length; ++i) {
 				Type t = methodParameters[i];
-				if (t.IsGenericParameter)
-					continue;
 
 				if (!TypeManager.IsDelegateType (t)) {
 					if (TypeManager.DropGenericTypeArguments (t) != TypeManager.expression_type)
@@ -2463,6 +2461,9 @@ namespace Mono.CSharp {
 
 					t = t.GetGenericArguments () [0];
 				}
+
+				if (t.IsGenericParameter)
+					continue;
 
 				MethodInfo invoke = Delegate.GetInvokeMethod (t, t);
 				Type rtype = invoke.ReturnType;
