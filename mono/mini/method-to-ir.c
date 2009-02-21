53cc1f8d6dc74e2e00a24f358d6ade7b08422031
Zoltan Varga <vargaz@gmail.com>
2009-02-21 21:24:16 +0000

    2009-02-21  Zoltan Varga  <vargaz@gmail.com>
    	* method-to-ir.c (mono_method_to_ir): Don't assert if string ctors
    	are called from runtime invoke wrappers.
    
    svn path=/branches/mono-2-4/mono/; revision=127636

--- mono/mini/method-to-ir.c
+++ mono/mini/method-to-ir.c
@@ -5923,7 +5923,7 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 					CHECK_CFG_EXCEPTION;
 				}
 
-				if (cmethod->string_ctor)
+				if (cmethod->string_ctor && method->wrapper_type != MONO_WRAPPER_RUNTIME_INVOKE)
 					g_assert_not_reached ();
 			}
 
