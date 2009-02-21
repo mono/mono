8c8dd7942b852cdae3f50adb6de0ab11d49b768c
Ankit Jain <radical@corewars.org>
2009-02-21 13:07:52 +0000

    Fix bug #458916. In class/Microsoft.Build.Tasks:
    	* Microsoft.Build.Tasks_test.dll.sources: Added CopyTest.cs
    
    In class/Microsoft.Build.Tasks/Microsoft.Build.Tasks:
    	Fix bug #458916.
    	* Copy.cs: CopiedFiles and DestinationFiles should have target paths
    	instead of source. Also, the metadata should get retained from source
    	items. Based on a patch by Jeffrey Richardson <themann@indyfantasysports.net>
    	Refactor a bit to remove code duplication.
    
    In class/Microsoft.Build.Tasks/Test/Microsoft.Build.Tasks:
    
    	* CopyTest.cs: New.
    
    svn path=/trunk/mcs/; revision=127626

--- mcs/class/Microsoft.Build.Tasks/Microsoft.Build.Tasks/Copy.cs
+++ mcs/class/Microsoft.Build.Tasks/Microsoft.Build.Tasks/Copy.cs
@@ -64,49 +64,36 @@ namespace Microsoft.Build.Tasks {
 						string sourceFile = sourceItem.GetMetadata ("FullPath");
 						string destinationFile = destinationItem.GetMetadata ("FullPath");
 
-						if (skipUnchangedFiles == true) {
-							FileInfo sourceInfo = new FileInfo (sourceFile);
-							FileInfo destinationInfo = new FileInfo (destinationFile);
-							if (sourceInfo.Length == destinationInfo.Length && File.GetLastWriteTime(sourceFile) <=
-								File.GetLastWriteTime (destinationFile))
-								continue;
-						}
+						if (!skipUnchangedFiles || HasFileChanged (sourceFile, destinationFile))
+							CopyFile (sourceFile, destinationFile, true);
 
-						string dest_dir = Path.GetDirectoryName (destinationFile);
-						if (!Directory.Exists (dest_dir)) {
-							Log.LogMessage ("Creating directory '{0}'", dest_dir);
-							Directory.CreateDirectory (dest_dir);
-						}
-						Log.LogMessage ("Copying file from '{0}' to '{1}'", sourceFile, destinationFile);
-						File.Copy (sourceFile, destinationFile, true);
-						temporaryCopiedFiles.Add (sourceItem);
+						sourceItem.CopyMetadataTo (destinationItem);
+						temporaryCopiedFiles.Add (destinationItem);
 					}
 					
 				} else if (destinationFolder != null) {
-					bool directoryCreated = false;
+					List<ITaskItem> temporaryDestinationFiles = new List<ITaskItem> ();
 					string destinationDirectory = destinationFolder.GetMetadata ("FullPath");
-					if (Directory.Exists (destinationDirectory) == false) {
-						Log.LogMessage ("Creating directory '{0}'", destinationDirectory);
-						Directory.CreateDirectory (destinationDirectory);
-						directoryCreated = true;
-					}
+					bool directoryCreated = CreateDirectoryIfRequired (destinationDirectory);
 					
 					foreach (ITaskItem sourceItem in sourceFiles) {
 						string sourceFile = sourceItem.GetMetadata ("FullPath");
 						string filename = sourceItem.GetMetadata ("Filename") + sourceItem.GetMetadata ("Extension");
 						string destinationFile = Path.Combine (destinationDirectory,filename);
 
-						if (skipUnchangedFiles == true && directoryCreated == false) {
-							FileInfo sourceInfo = new FileInfo (sourceFile);
-							FileInfo destinationInfo = new FileInfo (destinationFile);
-							if (sourceInfo.Length == destinationInfo.Length && File.GetLastWriteTime(sourceFile) <=
-								File.GetLastWriteTime (destinationFile))
-								continue;
-						}
-						Log.LogMessage ("Copying file from '{0}' to '{1}'", sourceFile, destinationFile);
-						File.Copy (sourceFile, destinationFile, true);
-						temporaryCopiedFiles.Add (sourceItem);
+						if (!skipUnchangedFiles || directoryCreated ||
+							HasFileChanged (sourceFile, destinationFile))
+							CopyFile (sourceFile, destinationFile, false);
+
+						temporaryCopiedFiles.Add (new TaskItem (
+								Path.Combine (destinationFolder.GetMetadata ("Identity"), filename),
+								sourceItem.CloneCustomMetadata ()));
+
+						temporaryDestinationFiles.Add (new TaskItem (
+								Path.Combine (destinationFolder.GetMetadata ("Identity"), filename),
+								sourceItem.CloneCustomMetadata ()));
 					}
+					destinationFiles = temporaryDestinationFiles.ToArray ();
 				} else {
 					throw new Exception ("You must specify DestinationFolder or DestinationFiles attribute.");
 				}
@@ -164,6 +151,37 @@ namespace Microsoft.Build.Tasks {
 			set {
 				sourceFiles = value;
 			}
+		}
+
+		// returns whether directory was created or not
+		bool CreateDirectoryIfRequired (string name)
+		{
+			if (Directory.Exists (name))
+				return false;
+
+			Log.LogMessage ("Creating directory '{0}'", name);
+			Directory.CreateDirectory (name);
+			return true;
+		}
+
+		void CopyFile (string source, string dest, bool create_dir)
+		{
+			if (create_dir)
+				CreateDirectoryIfRequired (Path.GetDirectoryName (dest));
+			Log.LogMessage ("Copying file from '{0}' to '{1}'", source, dest);
+			File.Copy (source, dest, true);
+		}
+
+		bool HasFileChanged (string source, string dest)
+		{
+			if (!File.Exists (dest))
+				return true;
+
+			FileInfo sourceInfo = new FileInfo (source);
+			FileInfo destinationInfo = new FileInfo (dest);
+
+			return !(sourceInfo.Length == destinationInfo.Length &&
+					File.GetLastWriteTime(source) <= File.GetLastWriteTime (dest));
 		}
 
 	}
