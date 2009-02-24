40e7740221a5ef63898998f6683aa665220e9f0c
Marek Safar <marek.safar@gmail.com>
2009-02-24 16:58:37 +0000

    New test.
    svn path=/trunk/mcs/; revision=127868

---
+++ mcs/tests/gtest-etree-18.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq.Expressions;
+
+public class FooBase { }
+public class Foo : FooBase { }
+
+public interface IHelper
+{
+	void DoIt (FooBase foo);
+}
+
+public class Program
+{
+	static int Main ()
+	{
+		Expression<Action<IHelper>> e = (helper => helper.DoIt (new Foo ()));
+		var mce = e.Body as MethodCallExpression;
+		var et = mce.Arguments[0].NodeType;
+
+		Console.WriteLine (et);
+		if (et != ExpressionType.New)
+			return 1;
+
+		return 0;
+	}
+}
