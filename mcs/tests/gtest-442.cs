ef4823e2cce9620fb9454953bcd4ed244df05fdd
Marek Safar <marek.safar@gmail.com>
2009-02-25 11:40:21 +0000

    New test.
    svn path=/trunk/mcs/; revision=127991

---
+++ mcs/tests/gtest-442.cs
@@ -0,0 +1,20 @@
+public abstract class NonGenericBase
+{
+	public abstract int this[int i] { get; }
+}
+
+public abstract class GenericBase<T> : NonGenericBase
+	where T : GenericBase<T>
+{
+	T Instance { get { return default (T); } }
+
+	public void Foo ()
+	{
+		int i = Instance[10];
+	}
+}
+
+public class EntryPoint
+{
+	static void Main () { }
+}
