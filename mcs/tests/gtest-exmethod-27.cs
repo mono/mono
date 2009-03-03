1c3dd589e5c369ab80ad0d34120e2d63e6e9ea7e
Marek Safar <marek.safar@gmail.com>
2009-03-03 12:58:19 +0000

    New test.
    svn path=/trunk/mcs/; revision=128435

---
+++ mcs/tests/gtest-exmethod-27.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Foo
+{
+
+	static partial class Extensions
+	{
+	}
+
+	static partial class Extensions
+	{
+
+		public static string AsString (this IList<byte> bytes)
+		{
+			return "42";
+		}
+	}
+}
+
+namespace Bar
+{
+
+	using Foo;
+
+	class Program
+	{
+
+		static void Main ()
+		{
+			Console.WriteLine (Pan (new byte[0]));
+		}
+
+		internal static string Pan (byte[] bytes)
+		{
+			return bytes.AsString ();
+		}
+	}
+}
