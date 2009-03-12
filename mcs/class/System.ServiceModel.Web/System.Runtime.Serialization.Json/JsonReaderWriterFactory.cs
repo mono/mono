4302acae88d6cc637bfe7dd9ddbafaf4ddd0606f
Andreia Gaita <avidigal@novell.com>
2009-03-12 23:40:03 +0000

    2009-03-12  Andreia Gaita  <avidigal@novell.com>
    	* JsonReaderWriterFactory.cs: try to auto-detect encoding for streams
    	without BOM
    
    svn path=/trunk/mcs/; revision=129226

--- mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/JsonReaderWriterFactory.cs
+++ mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/JsonReaderWriterFactory.cs
@@ -43,7 +43,7 @@ namespace System.Runtime.Serialization.Json
 
 		public static XmlDictionaryReader CreateJsonReader (byte [] source, int offset, int length, XmlDictionaryReaderQuotas quotas)
 		{
-			return CreateJsonReader (source, offset, length, Encoding.UTF8, quotas, null);
+			return CreateJsonReader (source, offset, length, Detect (source), quotas, null);
 		}
 
 		public static XmlDictionaryReader CreateJsonReader (byte [] source, int offset, int length, Encoding encoding, XmlDictionaryReaderQuotas quotas, OnXmlDictionaryReaderClose readerClose)
@@ -53,7 +53,7 @@ namespace System.Runtime.Serialization.Json
 
 		public static XmlDictionaryReader CreateJsonReader (Stream source, XmlDictionaryReaderQuotas quotas)
 		{
-			return CreateJsonReader (source, Encoding.UTF8, quotas, null);
+			return CreateJsonReader (source, Detect (source), quotas, null);
 		}
 
 		public static XmlDictionaryReader CreateJsonReader (Stream source, Encoding encoding, XmlDictionaryReaderQuotas quotas, OnXmlDictionaryReaderClose readerClose)
@@ -74,6 +74,34 @@ namespace System.Runtime.Serialization.Json
 		public static XmlDictionaryWriter CreateJsonWriter (Stream stream, Encoding encoding, bool closeOutput)
 		{
 			return new JsonWriter (stream, encoding, closeOutput);
+		}
+
+		static Encoding Detect (int b1, int b2)
+		{
+			if (b1 != -1 && b2 != -1) {
+				if (b1 != 0 && b2 == 0)
+					return new UnicodeEncoding (false, false, true);
+				else if (b1 == 0 && b2 != 0)
+					return new UnicodeEncoding (true, false, true);
+			}
+			return new UTF8Encoding (false, true);
+		}
+
+		static Encoding Detect (Stream source)
+		{
+			Stream stream = source;
+			if (!stream.CanSeek)
+				stream = new BufferedStream (source);
+			Encoding e = Detect (stream.ReadByte(), stream.ReadByte());
+			stream.Position = 0;
+			return e;
+		}
+
+		static Encoding Detect (byte[] bytes)
+		{
+			if (bytes.Length < 2)
+				return new UTF8Encoding (false, true);
+			return Detect (bytes[0], bytes[1]);
 		}
 	}
 }
