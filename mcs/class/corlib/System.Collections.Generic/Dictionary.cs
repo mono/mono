b9a774091eb9d07b0ac7adb2b87b44bd50113366
Jb Evain <jbevain@gmail.com>
2009-04-02 17:45:23 +0000

    2009-04-02  Jb Evain  <jbevain@novell.com>
    	backport of r130884 and r130886.
    
    	* Dictionary.cs (ValueCollection.CopyTo): fix for copying to
    	an object array. Based on a patch by Tobias Grimm <mono@e-tobi.net>.
    	Fixes bug #328036.
    
    
    svn path=/branches/mono-2-4/mcs/; revision=130887

--- mcs/class/corlib/System.Collections.Generic/Dictionary.cs
+++ mcs/class/corlib/System.Collections.Generic/Dictionary.cs
@@ -960,8 +960,15 @@ namespace System.Collections.Generic {
 
 			void ICollection.CopyTo (Array array, int index)
 			{
+				var target = array as TKey [];
+				if (target != null) {
+					CopyTo (target, index);
+					return;
+				}
+
 				IList list = array;
 				CopyToCheck (list, index);
+
 				for (int i = 0; i < dictionary.touchedSlots; i++) {
 					if ((dictionary.linkSlots [i].HashCode & HASH_FLAG) != 0)
 						list [index++] = dictionary.keySlots [i];
@@ -1035,17 +1042,22 @@ namespace System.Collections.Generic {
 				this.dictionary = dictionary;
 			}
 
-			public void CopyTo (TValue [] array, int index)
+			void CopyToCheck (IList array, int index)
 			{
 				if (array == null)
 					throw new ArgumentNullException ("array");
 				if (index < 0)
 					throw new ArgumentOutOfRangeException ("index");
 				// we want no exception for index==array.Length && dictionary.Count == 0
-				if (index > array.Length)
+				if (index > array.Count)
 					throw new ArgumentException ("index larger than largest valid index of array");
-				if (array.Length - index < dictionary.Count)
+				if (array.Count - index < dictionary.Count)
 					throw new ArgumentException ("Destination array cannot hold the requested elements!");
+			}
+
+			public void CopyTo (TValue [] array, int index)
+			{
+				CopyToCheck (array, index);
 
 				for (int i = 0; i < dictionary.touchedSlots; i++) {
 					if ((dictionary.linkSlots [i].HashCode & HASH_FLAG) != 0)
@@ -1085,7 +1097,19 @@ namespace System.Collections.Generic {
 
 			void ICollection.CopyTo (Array array, int index)
 			{
-				CopyTo ((TValue []) array, index);
+				var target = array as TValue [];
+				if (target != null) {
+					CopyTo (target, index);
+					return;
+				}
+
+				IList list = array;
+				CopyToCheck (list, index);
+
+				for (int i = 0; i < dictionary.touchedSlots; i++) {
+					if ((dictionary.linkSlots [i].HashCode & HASH_FLAG) != 0)
+						list [index++] = dictionary.valueSlots [i];
+				}
 			}
 
 			IEnumerator IEnumerable.GetEnumerator ()
