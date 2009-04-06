bdd0072aee015ef4ede39fda13a8a114595ea8aa
Marek Safar <marek.safar@gmail.com>
2009-04-06 10:35:45 +0000

    2009-04-06  Marek Safar  <marek.safar@gmail.com>
    	A fix for bug #492329
    	* expression.cs (New): Load variable when assigning type parameter
    	to ref variable.
    
    
    svn path=/trunk/mcs/; revision=131102

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -5526,7 +5526,7 @@ namespace Mono.CSharp {
 			temp.AddressOf (ec, AddressOp.Store);
 			ig.Emit (OpCodes.Initobj, type);
 			temp.Emit (ec);
-			ig.Emit (OpCodes.Br, label_end);
+			ig.Emit (OpCodes.Br_S, label_end);
 
 			ig.MarkLabel (label_activator);
 
@@ -5567,9 +5567,6 @@ namespace Mono.CSharp {
 		//
 		public virtual bool Emit (EmitContext ec, IMemoryLocation target)
 		{
-			if (is_type_parameter)
-				return DoEmitTypeParameter (ec);
-
 			bool is_value_type = TypeManager.IsValueType (type);
 			ILGenerator ig = ec.ig;
 			VariableReference vr = target as VariableReference;
@@ -5579,6 +5576,9 @@ namespace Mono.CSharp {
 			} else if (vr != null && vr.IsRef) {
 				vr.EmitLoad (ec);
 			}
+			
+			if (is_type_parameter)
+				return DoEmitTypeParameter (ec);
 
 			if (method != null)
 				method.EmitArguments (ec, Arguments);
