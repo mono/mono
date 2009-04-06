68cb16acfcab13cbaaac2b06108da49a97eb22d8
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2009-04-06 19:42:07 +0000

    2009-04-06  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* ThemeWin32Classic.cs: Include the previous check for ListView in a
    	2.0 define as needed.
    
    
    svn path=/trunk/mcs/; revision=131148

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
@@ -2683,7 +2683,11 @@ namespace System.Windows.Forms
 #endif
 			
 			// draw the gridlines
+#if NET_2_0
 			if (details && control.GridLines && !control.UsingGroups) {
+#else
+			if (details && control.GridLines) {
+#endif
 				Size control_size = control.ClientSize;
 				int top = (control.HeaderStyle == ColumnHeaderStyle.None) ?
 					0 : control.header_control.Height;
