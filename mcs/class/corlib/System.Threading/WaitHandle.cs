6ea7633b75f9ced0b86b36de20e933fdbdcb5776
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2009-04-12 14:11:18 +0000

    2009-04-12 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* System.Threading/WaitHandle.cs: implemented 2 missing WaitAny overloads.
    	* corlib.dll.sources: add GCNotificationStatus.
    	* System/GCNotificationStatus.cs: new enum.
    
    
    svn path=/trunk/mcs/; revision=131542

--- mcs/class/corlib/System.Threading/WaitHandle.cs
+++ mcs/class/corlib/System.Threading/WaitHandle.cs
@@ -171,6 +171,19 @@ namespace System.Threading
 
 #if NET_2_0
 		[ReliabilityContract (Consistency.WillNotCorruptState, Cer.MayFail)]
+		public static int WaitAny(WaitHandle[] waitHandles, TimeSpan timeout)
+		{
+			return WaitAny (waitHandles, timeout, false);
+		}
+
+		[ReliabilityContract (Consistency.WillNotCorruptState, Cer.MayFail)]
+		public static int WaitAny(WaitHandle[] waitHandles, int millisecondsTimeout)
+		{
+			return WaitAny (waitHandles, millisecondsTimeout, false);
+		}
+#endif
+#if NET_2_0
+		[ReliabilityContract (Consistency.WillNotCorruptState, Cer.MayFail)]
 #endif
 		public static int WaitAny(WaitHandle[] waitHandles,
 					  TimeSpan timeout, bool exitContext)
@@ -190,7 +203,6 @@ namespace System.Threading
 			}
 		}
 
-		[MonoTODO]
 #if NET_2_0
 		protected
 #else
