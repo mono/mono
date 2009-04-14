e0a612c6bcb026a939e2d22ca294163e75500646
Marek Safar <marek.safar@gmail.com>
2009-04-14 09:01:16 +0000

    New test.
    svn path=/trunk/mcs/; revision=131652

---
+++ mcs/errors/gcs1928-2.cs
@@ -0,0 +1,22 @@
+// CS1928: Type `Test.mFoo' does not contain a member `Frobnicate' and the best extension method overload `Extensions.Frobnicate<Test>(this Test)' has some invalid arguments
+// Line: 20
+
+using System;
+using System.Collections;
+
+static class Extensions
+{
+	public static void Frobnicate<T> (this T foo) where T : IList
+	{
+	}
+}
+
+public class Test
+{
+	IList mFoo;
+
+	void Frobnicate ()
+	{
+		mFoo.Frobnicate<Test> ();
+	}
+}
