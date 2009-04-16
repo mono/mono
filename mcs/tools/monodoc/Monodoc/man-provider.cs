fd8dba7d2f7be4a215b6c83922003d68a1c55f75
Jonathan Pryor <jpryor@novell.com>
2009-04-16 20:09:40 +0000

    * Monodoc/man-provider.cs: NEVER return a non-null string from 	  HelpSource.GetText() unless *useful* content is being returned, as 	  when non-null is returned RootTree.RenderUrl() assumes that the url 	  was handled and won't try any other HelpSources.  In this case, we 	  can (and do) have multiple ManHelpSources present, but since 	  ManHelpSource.GetText() was returning "<html>url was null</html>", 	  RootTree thought it was handled...when it wasn't.  Fix.
    svn path=/trunk/mcs/; revision=131934

--- mcs/tools/monodoc/Monodoc/man-provider.cs
+++ mcs/tools/monodoc/Monodoc/man-provider.cs
@@ -110,7 +110,7 @@ public class ManHelpSource : HelpSource {
 		if (url == null || url.Length == 0)
 		{
 			Message (TraceLevel.Warning, "Warning, NULL url!");
-			return "<html>url was null</html>";
+			return null;
 		}
 
 		Stream stream = GetHelpStream (url);
@@ -120,7 +120,7 @@ public class ManHelpSource : HelpSource {
 	public static string GetTextFromStream (Stream stream)
 	{
 		if (stream == null)
-			return "<html>url was null</html>";
+			return null;
 		StreamReader file = new StreamReader(stream);
 
 		string line;
