6d40ddb441fadcc64a2917b8e2d914af55a4e1a7
Sebastien Pouliot <sebastien@ximian.com>
2009-04-21 14:09:37 +0000

    2009-04-21  Sebastien Pouliot  <sebastien@ximian.com>
    	* AssemblyInfo.cs: Remove extra attributes for 2.1. Open up the 
    	internals to System.ServiceModel, e.g. to call the ctor of
    	System.Xml.XmlDictionaryReaderQuotas
    
    
    svn path=/trunk/mcs/; revision=132261

--- mcs/class/System.Runtime.Serialization/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Runtime.Serialization/Assembly/AssemblyInfo.cs
@@ -57,16 +57,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDelaySign (true)]
 #if NET_2_1
 [assembly: AssemblyKeyFile ("../silverlight.pub")]
+[assembly: InternalsVisibleTo ("System.ServiceModel, PublicKey=0024000004800000940000000602000000240000525341310004000001000100B5FC90E7027F67871E773A8FDE8938C81DD402BA65B9201D60593E96C492651E889CC13F1415EBB53FAC1131AE0BD333C5EE6021672D9718EA31A8AEBD0DA0072F25D87DBA6FC90FFD598ED4DA35E44C398C454307E8E33B8426143DAEC9F596836F97C8F74750E5975C64E2189F45DEF46B2A2B1247ADC3652BF5C308055DA9")]
 #else
 [assembly: AssemblyKeyFile ("../ecma.pub")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: SecurityCritical (SecurityCriticalScope.Explicit)]
 #endif
 
 [assembly: ComVisible (false)]
-[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
-[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 [assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
-[assembly: SecurityCritical (SecurityCriticalScope.Explicit)]
 [assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
