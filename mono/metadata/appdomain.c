29035caddf699c100fa807072d94d20cd6b5dae1
Martin Baulig <martin@novell.com>
2009-04-28 21:11:23 +0000

    **** Backport of r130659 **** 2009-03-31  Martin Baulig  <martin@ximian.com>
    	* mono-debug-debugger.h (MonoDebuggerEvent): Add
    	`MONO_DEBUGGER_EVENT_CREATE_APPDOMAIN' and
    	`MONO_DEBUGGER_EVENT_UNLOAD_APPDOMAIN'.
    	(mono_debugger_event_create_appdomain): New function.
    	(mono_debugger_event_unload_appdomain): New function.
    
    	* appdomain.c (mono_domain_create_appdomain_internal): Call
    	mono_debugger_event_create_appdomain().
    
    svn path=/branches/mono-2-4/mono/; revision=132892

--- mono/metadata/appdomain.c
+++ mono/metadata/appdomain.c
@@ -41,6 +41,7 @@
 #include <mono/metadata/monitor.h>
 #include <mono/metadata/threadpool.h>
 #include <mono/metadata/mono-debug.h>
+#include <mono/metadata/mono-debug-debugger.h>
 #include <mono/metadata/attach.h>
 #include <mono/metadata/file-io.h>
 #include <mono/metadata/console-io.h>
@@ -110,6 +111,9 @@ add_assemblies_to_domain (MonoDomain *domain, MonoAssembly *ass, GHashTable *has
 
 static MonoAppDomain *
 mono_domain_create_appdomain_internal (char *friendly_name, MonoAppDomainSetup *setup);
+
+static char *
+get_shadow_assembly_location_base (MonoDomain *domain);
 
 static MonoLoadFunc load_function = NULL;
 
@@ -413,6 +417,8 @@ mono_domain_create_appdomain_internal (char *friendly_name, MonoAppDomainSetup *
 	mono_set_private_bin_path_from_config (data);
 	
 	add_assemblies_to_domain (data, mono_defaults.corlib->assembly, NULL);
+
+	mono_debugger_event_create_appdomain (data, get_shadow_assembly_location_base (data));
 
 	return ad;
 }
@@ -2031,6 +2037,8 @@ mono_domain_unload (MonoDomain *domain)
 			g_assert_not_reached ();
 		}
 	}
+
+	mono_debugger_event_unload_appdomain (domain);
 
 	mono_domain_set (domain, FALSE);
 	/* Notify OnDomainUnload listeners */
