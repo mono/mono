6f1e7fccdae8b7b66d309c322b449352ff0b2ba2
Marek Safar <marek.safar@gmail.com>
2009-05-08 13:52:17 +0000

    New test.
    svn path=/trunk/mcs/; revision=133768

---
+++ mcs/tests/gtest-initialize-10.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Foo
+{
+	public int P { get; set; }
+}
+
+class Y
+{
+	static int Main ()
+	{
+		Foo foo = new Foo ();
+		foo.P = 1;
+
+		if (!Do (foo))
+			return 1;
+
+		Console.WriteLine ("OK");
+		return 0;
+	}
+
+	static bool Do (Foo f)
+	{
+		f = new Foo () {
+			P = f.P
+		};
+
+		if (f.P != 1)
+			return false;
+
+		Foo f2 = new Foo ();
+		f2.P = 9;
+		f2 = new Foo () {
+			P = f2.P
+		};
+
+		if (f2.P != 9)
+			return false;
+
+		return true;
+	}
+}
