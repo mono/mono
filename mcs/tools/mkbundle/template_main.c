888a527041f0a995cddf2d98ba025202c38fd729
Tor Lillqvist <tml@mono-cvs.ximian.com>
2009-05-11 19:21:00 +0000

    2009-05-11  Tor Lillqvist  <tml@novell.com>
    	* template_main.c: Enable passing options to the bundled Mono
    	interpreter through a MONO_BUNDLED_OPTIONS environment
    	variable. Requested in bug #495858.
    
    	Enable non-ASCII (arbitraty Unicode in fact) command-line
    	parameters to a mkbundled executable on Windows. Use
    	GetCommandLineW(), CommandLineToArgvW(), and then
    	g_utf16_to_utf8(). From bug #446172.
    
    
    svn path=/trunk/mcs/; revision=133915

--- mcs/tools/mkbundle/template_main.c
+++ mcs/tools/mkbundle/template_main.c
@@ -1,22 +1,86 @@
 int mono_main (int argc, char* argv[]);
 
 #include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <shellapi.h>
+#endif
+
+static char **mono_options = NULL;
+
+static int count_mono_options_args (void)
+{
+	const char *e = getenv ("MONO_BUNDLED_OPTIONS");
+	const char *p, *q;
+	int i, n;
+
+	if (e == NULL)
+		return 0;
+
+	/* Don't bother with any quoting here. It is unlikely one would
+	 * want to pass options containing spaces anyway.
+	 */
+
+	p = e;
+	n = 1;
+	while ((q = strchr (p, ' ')) != NULL) {
+		n++;
+		p = q + 1;
+	}
+
+	mono_options = malloc (sizeof (char *) * (n + 1));
+
+	p = e;
+	i = 0;
+	while ((q = strchr (p, ' ')) != NULL) {
+		mono_options[i] = malloc ((q - p) + 1);
+		memcpy (mono_options[i], p, q - p);
+		mono_options[i][q - p] = '\0';
+		i++;
+		p = q + 1;
+	}
+	mono_options[i++] = strdup (p);
+	mono_options[i] = NULL;
+
+	return n;
+}
+
 
 int main (int argc, char* argv[])
 {
-	char **newargs = (char **) malloc (sizeof (char *) * (argc + 2));
-	int i;
+	char **newargs = (char **) malloc (sizeof (char *) * (argc + 2) + count_mono_options_args ());
+	int i, k = 0;
 
-	newargs [0] = argv [0];
-	newargs [1] = image_name;
-	for (i = 1; i < argc; i++)
-		newargs [i+1] = argv [i];
-	newargs [++i] = NULL;
+#ifdef _WIN32
+	int wargc;
+	wchar_t **wargv = CommandLineToArgvW (GetCommandLineW (), &wargc);
+
+	newargs [k++] = g_utf16_to_utf8 (wargv [0], -1, NULL, NULL, NULL);
+#else
+	newargs [k++] = argv [0];
+#endif
+
+	if (mono_options != NULL) {
+		i = 0;
+		while (mono_options[i] != NULL)
+			newargs[k++] = mono_options[i++];
+	}
+
+	newargs [k++] = image_name;
+
+	for (i = 1; i < argc; i++) {
+#ifdef _WIN32
+		newargs [k++] = g_utf16_to_utf8 (wargv [i], -1, NULL, NULL, NULL);
+#else
+		newargs [k++] = argv [i];
+#endif
+	}
+	newargs [k] = NULL;
 	
 	if (config_dir != NULL && getenv ("MONO_CFG_DIR") == NULL)
 		mono_set_dirs (getenv ("MONO_PATH"), config_dir);
 	
 	mono_mkbundle_init();
 
-	return mono_main (argc+1, newargs);
+	return mono_main (k, newargs);
 }
