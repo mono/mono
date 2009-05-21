75560da8819a4724aac2a37d6eca58228eb53605
Neville Gao <ngao@mono-cvs.ximian.com>
2009-05-21 06:39:35 +0000

    Cleaned up UIA properties.
    svn path=/trunk/mcs/; revision=134508

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ContextMenu.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ContextMenu.cs
@@ -118,28 +118,13 @@ namespace System.Windows.Forms
 
 			src_control = control;
 
-#if NET_2_0
-			visible = true;
-#endif
 			OnPopup (EventArgs.Empty);
 			pos = control.PointToScreen (pos);
 			MenuTracker.TrackPopupMenu (this, pos);
 #if NET_2_0
-			visible = false;
 			OnCollapse (EventArgs.Empty);
 #endif
 		}
-
-		#region UIA Framework Properties
-#if NET_2_0
-		private bool visible;
-
-		public bool UIAVisible {
-			get { return visible; }
-			set { visible = value; }
-		}
-#endif
-		#endregion
 
 #if NET_2_0
 		public void Show (Control control, Point pos, LeftRightAlignment alignment)
