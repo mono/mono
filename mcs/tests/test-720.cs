b87de8b6f1c716236159c80ab1cbbf3efe8174a0
Marek Safar <marek.safar@gmail.com>
2009-05-21 11:59:56 +0000

    New test.
    svn path=/trunk/mcs/; revision=134527

---
+++ mcs/tests/test-720.cs
@@ -0,0 +1,30 @@
+// Compiler options: -warn:4 -warnaserror
+
+using System;
+
+namespace N
+{
+	class Program
+	{
+		static void Main ()
+		{
+			Parent pr = new Child();
+			((Child)pr).OnExample();
+		}
+	}
+
+	public abstract class Parent
+	{
+		public delegate void ExampleHandler();
+		public abstract event ExampleHandler Example;
+	}
+
+	public class Child : Parent
+	{
+		public override event ExampleHandler Example;
+		public void OnExample()
+		{
+			if (Example != null) Example();
+		}
+	}
+}
