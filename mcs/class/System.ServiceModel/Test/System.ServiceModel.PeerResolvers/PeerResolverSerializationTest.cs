d1f95e4190757f56d342a7233bffb46fcf6dd702
Atsushi Eno <atsushieno@gmail.com>
2009-05-25 08:17:11 +0000

    2009-05-25  Atsushi Enomoto  <atsushi@ximian.com>
    	* PeerResolverSerializationTest.cs : new test for general
    	  serialization tests.
    
    	* System.ServiceModel.dll_test.sources:
    	  added PeerResolverSerializationTest.cs.
    
    
    svn path=/trunk/mcs/; revision=134672

---
+++ mcs/class/System.ServiceModel/Test/System.ServiceModel.PeerResolvers/PeerResolverSerializationTest.cs
@@ -0,0 +1,82 @@
+//
+// PeerResolversSerializationTest.cs
+//
+// Author:
+//	Atsushi Enomoto <atsushi@ximian.com>
+//
+// Copyright (C) 2009 Novell, Inc.  http://www.novell.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.PeerResolvers;
+using System.Xml;
+
+public class PeerResolverSerializationTest
+{
+	public void ResolveResponseInfo ()
+	{
+		var ser = new DataContractSerializer (typeof (ResolveResponseInfo));
+		var rri = new ResolveResponseInfo ();
+		var pna = new PeerNodeAddress (
+			new EndpointAddress ("http://localhost:8080"),
+			new ReadOnlyCollection<IPAddress> (new IPAddress [0]));
+		rri.Addresses = new List<PeerNodeAddress> ();
+		rri.Addresses.Add (pna);
+		var sw = new StringWriter ();
+		using (var xw = XmlWriter.Create (sw))
+			ser.WriteObject (xw, rri);
+		rri = (ResolveResponseInfo) ser.ReadObject (XmlReader.Create (new StringReader (sw.ToString ())));
+	}
+}
+
+/*
+[DataContract]
+public class ResolveResponseInfo
+{
+	public ResolveResponseInfo ()
+	{
+		Addresses = new List<PeerNodeAddress> ();
+	}
+
+	[DataMember]
+	public IList<PeerNodeAddress> Addresses { get; set; }
+}
+
+public class PeerNodeAddress
+{
+	public PeerNodeAddress (EndpointAddress ea, IList<IPAddress> al)
+	{
+		Endpoint = ea;
+		Addresses = al;
+	}
+
+	public EndpointAddress Endpoint { get; set; }
+	public IList<IPAddress> Addresses { get; set; }
+}
+*/
