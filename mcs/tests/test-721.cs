e2b31c9f8ee71fd4e5dc2044dff3880f9273bc4d
Marek Safar <marek.safar@gmail.com>
2009-05-26 16:37:15 +0000

    New test.
    svn path=/trunk/mcs/; revision=134777

---
+++ mcs/tests/test-721.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+class Program
+{
+	static int Main ()
+	{
+		Type t = typeof (Control);
+		MethodInfo m = t.GetMethod ("set_Foo");
+
+		if (m.GetParameters ()[0].Name != "value")
+			return 1;
+
+		return 0;
+	}
+}
+
+class Control
+{
+	public virtual int Foo
+	{
+		[param: MarshalAs (UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof (int))]
+		set
+		{
+		}
+	}
+}
