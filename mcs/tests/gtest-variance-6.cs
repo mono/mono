8e2c0158473d1b82d3247296454a086642c05bb6
Marek Safar <marek.safar@gmail.com>
2009-05-29 19:44:21 +0000

    New tests.
    svn path=/trunk/mcs/; revision=135080

---
+++ mcs/tests/gtest-variance-6.cs
@@ -0,0 +1,32 @@
+// Compiler options: -langversion:future
+
+interface ICovariant<out T> { }
+interface IContravariant<in T> { }
+
+delegate ICovariant<U> Cov1<out U> (IContravariant<U> x);
+delegate ICovariant<U> Cov2<out U> (IContravariant<ICovariant<U>> x);
+delegate ICovariant<IContravariant<IContravariant<U>>> Cov3<out U> (IContravariant<IContravariant<IContravariant<ICovariant<U>>>> x);
+delegate ICovariant<int> Cov4<out U> ();
+delegate IContravariant<int> Cov5<out U> ();
+
+delegate IContravariant<U[]> Contra5<in U> (U u, ICovariant<U> x);
+delegate IContravariant<U[]> Contra6<in U> ();
+delegate IContravariant<U> Contra7<in U> (U u, ICovariant<U> x);
+delegate IContravariant<ICovariant<U>> Contra8<in U> (U u, ICovariant<U> x);
+
+interface ITest_1<out T>
+{
+	ICovariant<T> CovariantHandler (IContravariant<T> x);
+}
+
+interface ITest_2<in T>
+{
+	IContravariant<T> CovariantHandler (ICovariant<T> x);
+}
+
+class Program
+{
+	static void Main ()
+	{
+	}
+}
---
+++ mcs/tests/gtest-variance-7.cs
@@ -0,0 +1,26 @@
+// Compiler options: -langversion:future
+
+delegate T Covariant<out T> ();
+delegate void Contra<in T> (T t);
+delegate TR CoContra<out TR, in T> (T t);
+delegate void None<T> (T t);
+
+delegate Covariant<Contra<Contra<Covariant<Covariant<Covariant<Covariant<Covariant<U>>>>>>>> Test<out U> ();
+delegate Contra<Covariant<Contra<Contra<Covariant<Covariant<Covariant<Covariant<Covariant<U>>>>>>>>> Test2<in U> ();
+delegate Contra<Contra<Covariant<Covariant<Covariant<Covariant<Contra<Contra<U>>>>>>>> Test3<out U> ();
+delegate Contra<Contra<Covariant<Covariant<Contra<Contra<Contra<Contra<U>>>>>>>> Test4<out U> ();
+delegate Contra<Contra<Covariant<Covariant<Contra<Contra<Contra<U>>>>>>> Test5<in U> ();
+delegate void Test6<in U> (Covariant<Contra<Contra<Covariant<Covariant<Covariant<Covariant<Covariant<U>>>>>>>> t);
+
+delegate void Both<in U, out V> (CoContra<U, V> p);
+delegate void Both2<in U, out V> (CoContra<U, Contra<U>> p);
+delegate void Both3<in U, out V> (CoContra<U, Contra<int>> p);
+delegate void Both4<in U, out V> (Both<V, U> b);
+delegate void Both5<in U, out V> (Both<V, int> b);
+
+class C
+{
+	public static void Main ()
+	{
+	}
+}
