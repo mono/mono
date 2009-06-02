d2bae0a896ca1849ee0508b95199c6d7d8229ef5
Marek Safar <marek.safar@gmail.com>
2009-06-02 12:42:56 +0000

    New test.
    svn path=/trunk/mcs/; revision=135197

---
+++ mcs/errors/cs0182-6.cs
@@ -0,0 +1,6 @@
+// CS0182: An attribute argument must be a constant expression, typeof expression or array creation expression
+// Line: 6
+
+using System.Reflection;
+
+[assembly: AssemblyKeyFile(string.Empty)]
