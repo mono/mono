80d2ae0f805dfb774da5673ed168f3543f73415f
Marek Safar <marek.safar@gmail.com>
2009-06-03 17:32:14 +0000

    2009-06-03  Marek Safar  <marek.safar@gmail.com>
    	* Func.cs: Add 4.0 forwarders.
    
    
    svn path=/trunk/mcs/; revision=135323

--- mcs/class/System.Core/System/Funcs.cs
+++ mcs/class/System.Core/System/Funcs.cs
@@ -25,10 +25,25 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //
 
-namespace System {
+#if NET_4_0
+using System.Runtime.CompilerServices;
+
+[assembly:TypeForwardedTo (typeof(Func<>))]
+[assembly:TypeForwardedTo (typeof(Func<,>))]
+[assembly:TypeForwardedTo (typeof(Func<,,>))]
+[assembly:TypeForwardedTo (typeof(Func<,,,>))]
+[assembly:TypeForwardedTo (typeof(Func<,,,,>))]
+
+#else
+
+namespace System
+{
 	public delegate TResult Func<TResult> ();
 	public delegate TResult Func<T, TResult> (T arg1);
 	public delegate TResult Func<T1, T2, TResult> (T1 arg1, T2 arg2);
 	public delegate TResult Func<T1, T2, T3, TResult> (T1 arg1, T2 arg2, T3 arg3);
 	public delegate TResult Func<T1, T2, T3, T4, TResult> (T1 arg1, T2 arg2, T3 arg3, T4 arg4);
-}
+}	
+
+#endif	
+
