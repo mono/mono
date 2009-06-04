05e9d0cd4c6b6faf7bfd07dc15e5e362d73520d1
Marek Safar <marek.safar@gmail.com>
2009-06-04 15:37:09 +0000

    Removed enabled test.
    svn path=/trunk/mcs/; revision=135444

--- mcs/errors/gcs0733.cs
+++
@@ -1,8 +0,0 @@
-// gcs0733.cs: Cannot forward generic type `C<int>'
-// Line: 8
-// Compiler options: -r:GCS0733-lib.dll
-
-using System;
-using System.Runtime.CompilerServices;
-
-[assembly: TypeForwardedTo(typeof (C<int>))]
