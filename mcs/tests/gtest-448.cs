2ef2561a5a0f6ee6706ff4853e101fa04a1c245f
Marek Safar <marek.safar@gmail.com>
2009-06-11 17:01:39 +0000

    New test.
    svn path=/trunk/mcs/; revision=135939

---
+++ mcs/tests/gtest-448.cs
@@ -0,0 +1,60 @@
+// Compiler options: -langversion:future
+
+using System;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+public class C
+{
+	public static void TestA ([Optional][DefaultParameterValue (1)] int u)
+	{
+	}
+
+	public static void TestB (long u = 12)
+	{
+	}
+	
+	public static void TestC (decimal d = decimal.MaxValue)
+	{
+	}
+
+	public static int Main ()
+	{
+		ParameterInfo[] info = typeof (C).GetMethod ("TestA").GetParameters ();
+
+		if (info[0].DefaultValue.GetType () != typeof (int))
+			return 1;
+
+		if ((int) info[0].DefaultValue != 1)
+			return 2;
+
+		if (!info[0].IsOptional)
+			return 3;
+
+		info = typeof (C).GetMethod ("TestB").GetParameters ();
+
+		if (info[0].DefaultValue.GetType () != typeof (int))
+			return 11;
+
+		if ((int) info[0].DefaultValue != 12)
+			return 12;
+
+		if (!info[0].IsOptional)
+			return 13;
+
+		info = typeof (C).GetMethod ("TestC").GetParameters ();
+
+/* RUNTIME BUG #512330
+		if (info[0].DefaultValue.GetType () != typeof (decimal))
+			return 21;
+
+		if ((int) info[0].DefaultValue != decimal.MaxValue)
+			return 22;
+*/
+		if (!info[0].IsOptional)
+			return 23;
+
+		Console.WriteLine ("ok");
+		return 0;
+	}
+}
