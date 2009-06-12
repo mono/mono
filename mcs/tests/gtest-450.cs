514337e0fa88e693b088798dfb2ff7c6f1ef50a7
Jb Evain <jbevain@gmail.com>
2009-06-12 12:41:42 +0000

    2009-06-12  Jb Evain  <jbevain@novell.com>
    	* gtest-449.cs
    	* gtest-450.cs:
    		new tests for the /platform support.
    
    
    svn path=/trunk/mcs/; revision=135991

---
+++ mcs/tests/gtest-450.cs
@@ -0,0 +1,26 @@
+// Compiler options: -platform:x86
+
+using System;
+using System.Reflection;
+
+class Program {
+
+	static int Main ()
+	{
+		PortableExecutableKinds pekind;
+		ImageFileMachine machine;
+
+		typeof (Program).Module.GetPEKind (out pekind, out machine);
+
+		if ((pekind & PortableExecutableKinds.ILOnly) != 0)
+			return 1;
+
+		if ((pekind & PortableExecutableKinds.Required32Bit) == 0)
+			return 2;
+
+		if (machine != ImageFileMachine.I386)
+			return 3;
+
+		return 0;
+	}
+}
