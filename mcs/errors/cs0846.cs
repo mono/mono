d127b293394ad7815b8c7cf9b4d21de8359b2707
Marek Safar <marek.safar@gmail.com>
2009-06-17 18:15:19 +0000

    New tests.
    svn path=/trunk/mcs/; revision=136336

---
+++ mcs/errors/cs0846.cs
@@ -0,0 +1,10 @@
+// CS0846: A nested array initializer was expected
+// Line: 8
+
+class M
+{
+	public static void Main ()
+	{
+		int[,] i = new int[1, 1] { 2 };
+	}
+}
---
+++ mcs/errors/cs0847.cs
@@ -0,0 +1,10 @@
+// CS0847: An array initializer of length `1' was expected
+// Line: 8
+
+class M
+{
+	public static void Main ()
+	{
+		int[] i = new int [1] { 2, 3 };
+	}
+}
