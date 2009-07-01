37601b3db7bae15a62a7adbe140d02b643be5ec1
Marek Safar <marek.safar@gmail.com>
2009-07-01 19:03:59 +0000

    New test.
    svn path=/trunk/mcs/; revision=137213

---
+++ mcs/tests/gtest-variance-12.cs
@@ -0,0 +1,26 @@
+// Compiler options: -langversion:future
+
+using System;
+
+delegate void D<in T> ();
+
+interface I<out T>
+{
+	event D<T> field;
+}
+
+class D : I<string>
+{
+	public event D<string> field;
+	
+	public static int Main ()
+	{
+		D<object> dd = () => {};
+		
+		D d = new D ();
+		d.field += dd;
+		d.field ();
+		
+		return 0;
+	}
+}
