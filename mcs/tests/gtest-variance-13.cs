b83bbc70e54241818c329ffaca1316a403806982
Marek Safar <marek.safar@gmail.com>
2009-07-02 12:31:00 +0000

    New test.
    svn path=/trunk/mcs/; revision=137285

---
+++ mcs/tests/gtest-variance-13.cs
@@ -0,0 +1,40 @@
+// Compiler options: -langversion:future
+
+using System;
+using System.Collections.Generic;
+
+interface I<in T>
+{
+}
+
+class A
+{
+	static void Foo<T> (T a, IList<T> c)
+	{
+	}
+	
+	public static void Test ()
+	{
+		Foo ("aaaa", new object[0]);
+	}
+}
+
+class B
+{
+	static void Foo<T> (T a, I<T> c)
+	{
+	}
+
+	static void Test<U> (U u, I<U> x)
+	{
+		Foo (u, x);
+	}
+}
+
+class M
+{
+	public static int Main ()
+	{
+		return 0;
+	}
+}
