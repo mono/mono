7f8a5c92755187b5944c0a4132145fd50aae1c61
Marek Safar <marek.safar@gmail.com>
2009-07-08 09:34:22 +0000

    New tests.
    svn path=/trunk/mcs/; revision=137556

---
+++ mcs/errors/cs0246-18.cs
@@ -0,0 +1,11 @@
+// CS0246: The type or namespace name `dynamic' could not be found. Are you missing a using directive or an assembly reference?
+// Line: 9
+// Compiler options: -langversion:ISO-2
+
+class C
+{
+	public static void Main ()
+	{
+		dynamic d = 9;
+	}
+}
---
+++ mcs/errors/cs1644-14.cs
@@ -0,0 +1,11 @@
+// CS1644: Feature `implicitly typed local variable' cannot be used because it is not part of the C# 2.0 language specification
+// Line:  9
+// Compiler options: -langversion:ISO-2
+
+class M
+{
+	public static void Main ()
+	{
+		var a = 1;
+	}
+}
