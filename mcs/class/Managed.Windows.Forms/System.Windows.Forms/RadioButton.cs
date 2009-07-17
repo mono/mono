ae62cfc9930956f88e5315593550913950d20d98
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2009-07-17 18:24:44 +0000

    2009-07-17  Carlos Alberto Cortez <calberto.cortez@gmail.com>
    	* RadioButton.cs: Setting Checked when none of the sibligs is checked
    	should happen *not* in GotFocus, but in the Enter event. This is
    	specially important since we should get that Checked behaviour even if
    	no handle is created yet - and thus, no actual GotFocus call happens at
    	the time.
    	Fixes #520764.
    
    
    svn path=/trunk/mcs/; revision=138145

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/RadioButton.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/RadioButton.cs
@@ -114,16 +114,14 @@ namespace System.Windows.Forms {
 			radiobutton_alignment = ContentAlignment.MiddleLeft;
 			TextAlign = ContentAlignment.MiddleLeft;
 			TabStop = false;
-
-			GotFocus += new EventHandler (GotFocusHandler);
 		}
 		#endregion	// Public Constructors
 
 		#region Private Methods
 
-		// if we get focus, and no RadioButton in the parent control is
-		// checked, mark Checked as true for us
-		private void GotFocusHandler (object o, EventArgs args)
+		// When getting OnEnter we need to set Checked as true in case none of the sibling radio
+		// buttons is checked.
+		private void PerformDefaultCheck ()
 		{
 			// if we are already checked, no need to check the other controls
 			if (!auto_check || Checked)
@@ -353,6 +351,7 @@ namespace System.Windows.Forms {
 		}
 
 		protected override void OnEnter(EventArgs e) {
+			PerformDefaultCheck ();
 			base.OnEnter(e);
 		}
 
