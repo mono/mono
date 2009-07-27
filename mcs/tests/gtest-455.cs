9c81481874050c19d56ef2bbc92d5d757381cb34
Marek Safar <marek.safar@gmail.com>
2009-07-27 16:30:53 +0000

    New test.
    svn path=/trunk/mcs/; revision=138748

---
+++ mcs/tests/gtest-455.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main ()
+		{
+			object o = new object ();
+			Inner<object>.Compare (o, o);
+		}
+	}
+
+	public class Inner<T> where T : class
+	{
+		public static void Compare (object obj, T value)
+		{
+			if (obj != value) { }
+		}
+	}
+}
