9852bdc0963cb68cbcd808e6e4fdc61c72c04323
Marek Safar <marek.safar@gmail.com>
2009-08-03 10:11:27 +0000

    New test.
    svn path=/trunk/mcs/; revision=139273

---
+++ mcs/errors/cs0738-2.cs
@@ -0,0 +1,25 @@
+// CS0738: `C' does not implement interface member `I2.Key.get' and the best implementing candidate `C.Key' return type `IB' does not match interface member return type `IA'
+// Line: 22
+
+public interface I1
+{
+	IB Key { get; }
+}
+
+public interface I2
+{
+	IA Key { get; }
+}
+
+public interface IB : IA
+{
+}
+
+public interface IA
+{
+}
+
+public class C : I1, I2
+{
+	public IB Key { get { return null; } }
+}
