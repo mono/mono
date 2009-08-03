f10bcc50718a43c389489de10c7655d63309286a
Miguel de Icaza <miguel@gnome.org>
2009-08-03 04:45:00 +0000

    2009-08-03  Miguel de Icaza  <miguel@novell.com>
    	* Extensions.cs: Add a null guard and return null on failure,
    	instead of crashing.
    
    svn path=/trunk/mcs/; revision=139263

--- mcs/class/System.Xml.Linq/System.Xml.XPath/Extensions.cs
+++ mcs/class/System.Xml.Linq/System.Xml.XPath/Extensions.cs
@@ -63,6 +63,8 @@ namespace System.Xml.XPath
 		public static XElement XPathSelectElement (this XNode node, string xpath, IXmlNamespaceResolver nsResolver)
 		{
 			XPathNavigator nav = CreateNavigator (node).SelectSingleNode (xpath, nsResolver);
+			if (nav == null)
+				return null;
 			return nav.UnderlyingObject as XElement;
 		}
 
