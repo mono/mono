b3810185ba2462594beacdbee7b868725cac2deb
Marek Safar <marek.safar@gmail.com>
2009-08-05 17:17:40 +0000

    Updated.
    svn path=/trunk/mcs/; revision=139414

---
+++ mcs/errors/cs1962.cs
@@ -0,0 +1,10 @@
+// CS1962: The typeof operator cannot be used on the dynamic type
+// Line: 8
+
+class C
+{
+	public static void Main ()
+	{
+		object t = typeof (dynamic);
+	}
+}
--- mcs/errors/cs1965.cs
+++ mcs/errors/cs1965.cs
@@ -1,4 +1,4 @@
-// CS1965: Class `C' cannot derive from dynamic type
+// CS1965: Class `C' cannot derive from the dynamic type
 // Line: 4
 
 class C : dynamic
