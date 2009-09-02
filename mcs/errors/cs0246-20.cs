1f073e845f078579a648f6baabd96290984847a3
Marek Safar <marek.safar@gmail.com>
2009-09-02 10:53:05 +0000

    New test.
    svn path=/trunk/mcs/; revision=141116

---
+++ mcs/errors/cs0246-20.cs
@@ -0,0 +1,10 @@
+// CS0246: The type or namespace name `C' could not be found. Are you missing a using directive or an assembly reference?
+// Line: 4
+
+using BigInteger = C.BigInteger;
+using C = Cks.Runtime.Data;
+
+namespace Cks.Runtime.Data
+{
+	public class BigInteger {}
+}
