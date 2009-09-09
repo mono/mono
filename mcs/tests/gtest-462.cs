2f111012af27c6066a7e2213225eed5f2a16d477
Marek Safar <marek.safar@gmail.com>
2009-09-09 15:48:28 +0000

    New test.
    svn path=/trunk/mcs/; revision=141620

---
+++ mcs/tests/gtest-462.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+	static int Main ()
+	{
+		Tester<int> t = new Tester<int> ();
+		int r = t.Get (333);
+		Console.WriteLine (r);
+		if (r != 333)
+			return 1;
+
+		r = t.Get (222.12);
+		Console.WriteLine (r);
+		if (r != 0)
+			return 2;
+
+		return 0;
+	}
+
+	class Tester<T> where T : struct, IConvertible
+	{
+		public T Get (object data)
+		{
+			var val = data;
+			if (val is T)
+				return (T) val;
+			return default (T);
+		}
+	}
+}
