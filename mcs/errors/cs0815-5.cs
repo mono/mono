e45ed74d9994285a34370d758768b815179322af
Marek Safar <marek.safar@gmail.com>
2009-09-17 13:02:06 +0000

    New tests.
    svn path=/trunk/mcs/; revision=142108

---
+++ mcs/errors/cs0815-5.cs
@@ -0,0 +1,10 @@
+// CS0815: An implicitly typed local variable declaration cannot be initialized with `Tests.Main()'
+// Line: 8
+
+public class Tests
+{
+	public static void Main ()
+	{
+		var d = Main;
+	}
+}
