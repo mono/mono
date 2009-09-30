e0a7feaf6e87070afae667ed61381c620e2a2349
Marek Safar <marek.safar@gmail.com>
2009-09-30 09:35:10 +0000

    New test.
    svn path=/trunk/mcs/; revision=142977

---
+++ mcs/tests/test-730.cs
@@ -0,0 +1,28 @@
+class Demo
+{
+	public void Test (object arg)
+	{
+	}
+
+	static int Test (int i)
+	{
+		return i;
+	}
+
+	delegate int D (int t);
+
+	int GetPhones ()
+	{
+		D d = Test;
+		return d (55);
+	}
+
+	static int Main ()
+	{
+		int r = new Demo ().GetPhones ();
+		if (r != 55)
+			return 1;
+
+		return 0;
+	}
+}
