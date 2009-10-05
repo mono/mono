5cc5c1ed8b01fe7d945aa4d95bd0deb0b1868bdf
Marek Safar <marek.safar@gmail.com>
2009-10-05 17:11:04 +0000

    New test.
    svn path=/trunk/mcs/; revision=143426

---
+++ mcs/tests/gtest-466.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+	bool Test<T> (T t) where T : class
+	{
+		return t == this;
+	}
+	
+	static int Main ()
+	{
+		var p = new Program ();
+
+		if (p.Test ("a"))
+			return 1;
+		
+		if (!p.Test (p))
+			return 2;
+
+		return 0;
+	}
+}
