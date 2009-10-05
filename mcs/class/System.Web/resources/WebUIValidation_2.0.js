17ebd3efa017ae09bc48ade546b61e2d4a535122
Marek Habersack <grendel@twistedcode.net>
2009-10-05 11:15:22 +0000

    Backport of r143388
    svn path=/branches/mono-2-6/mcs/; revision=143389

--- mcs/class/System.Web/resources/WebUIValidation_2.0.js
+++ mcs/class/System.Web/resources/WebUIValidation_2.0.js
@@ -4,7 +4,7 @@
  * Authors:
  *   Chris Toshok (toshok@ximian.com)
  *
- * (c) 2005 Novell, Inc. (http://www.novell.com)
+ * (c) 2005-2009 Novell, Inc. (http://www.novell.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -43,23 +43,25 @@ webForm.ValidatorOnLoad = function  ()
 		webForm.have_validation_summaries = true;
 	}
 
-	for (var v = 0; v < webForm.Page_Validators.length; v++) {
-		var vo = webForm.Page_Validators [v];
+	if (typeof (webForm.Page_Validators) != 'undefined' || webForm.Page_Validators != null) {
+		for (var v = 0; v < webForm.Page_Validators.length; v++) {
+			var vo = webForm.Page_Validators [v];
 
-		if (typeof(vo.isvalid) == "string" && vo.isvalid == "False")
-			vo._isvalid = false;
-		else
-			vo._isvalid = true;
+			if (typeof(vo.isvalid) == "string" && vo.isvalid == "False")
+				vo._isvalid = false;
+			else
+				vo._isvalid = true;
 
-		if (typeof(vo.enabled) == "string" && vo.enabled == "False")
-			vo._enabled = false;
-		else
-			vo._enabled = true;
+			if (typeof(vo.enabled) == "string" && vo.enabled == "False")
+				vo._enabled = false;
+			else
+				vo._enabled = true;
 			
-		if (typeof(vo.evaluationfunction) == "string")
-			vo.evaluationfunction = webForm [vo.evaluationfunction];
+			if (typeof(vo.evaluationfunction) == "string")
+				vo.evaluationfunction = webForm [vo.evaluationfunction];
+		}
 	}
-
+	
 	webForm.Page_ValidationActive = true;
 }
 
@@ -206,29 +208,32 @@ webForm.Page_ClientValidate = function (group)
 	}
 	
 	var invalidControlHasBeenFocused = false;
-	for (var v = 0; v < webForm.Page_Validators.length; v++) {
-		var vo = webForm.Page_Validators [v];
-		var evalfunc = vo.evaluationfunction;
-		var result = false;
+	if (typeof (webForm.Page_Validators) != 'undefined' || webForm.Page_Validators != null) {
+		for (var v = 0; v < webForm.Page_Validators.length; v++) {
+			var vo = webForm.Page_Validators [v];
+			var evalfunc = vo.evaluationfunction;
+			var result = false;
 
-		if (!vo._enabled || !webForm.IsValidationGroupMatch(vo, group)) {
-			result = true;
-			webForm.ValidatorSucceeded (vo);
-		}
-		else {
-			result = evalfunc.call (this, vo);
-		}
-
-		if (!result) {
-			webForm.validation_result = false;
-			if (!invalidControlHasBeenFocused && typeof(vo.focusOnError) == "string" && vo.focusOnError == "t") {
-				invalidControlHasBeenFocused = webForm.ValidatorSetFocus(vo);
+			if (!vo._enabled || !webForm.IsValidationGroupMatch(vo, group)) {
+				result = true;
+				webForm.ValidatorSucceeded (vo);
 			}
-		}
+			else {
+				result = evalfunc.call (this, vo);
+			}
+
+			if (!result) {
+				webForm.validation_result = false;
+				if (!invalidControlHasBeenFocused && typeof(vo.focusOnError) == "string" && vo.focusOnError == "t") {
+					invalidControlHasBeenFocused = webForm.ValidatorSetFocus(vo);
+				}
+			}
 		
-		vo._isvalid = result;
+			vo._isvalid = result;
+		}
+		webForm.ValidationSummaryOnSubmit(group);
 	}
-    webForm.ValidationSummaryOnSubmit(group);
+	
 	return webForm.validation_result;
 }
 
