52fdb32f839463bfea5fff84676803a7ca97fec1
Zoltan Varga <vargaz@gmail.com>
2009-10-26 17:08:23 +0000

    2009-05-26  Zoltan Varga  <vargaz@gmail.com>
    	* shared.c (_wapi_shm_file): Avoid creating a .wapi directory here, create
    	it in _wapi_shm_file_open (), since platforms using posix shm don't need it.
    
    svn path=/branches/mono-2-4/mono/; revision=144858

--- mono/io-layer/shared.c
+++ mono/io-layer/shared.c
@@ -129,7 +129,7 @@ _wapi_shm_open (const char *filename, int size)
 static gchar *_wapi_shm_file (_wapi_shm_t type)
 {
 	static gchar file[_POSIX_PATH_MAX];
-	gchar *name = NULL, *filename, *dir, *wapi_dir;
+	gchar *name = NULL, *filename, *wapi_dir;
 
 	name = _wapi_shm_base_name (type);
 
@@ -148,14 +148,6 @@ static gchar *_wapi_shm_file (_wapi_shm_t type)
 
 	g_snprintf (file, _POSIX_PATH_MAX, "%s", filename);
 	g_free (filename);
-		
-	/* No need to check if the dir already exists or check
-	 * mkdir() errors, because on any error the open() call will
-	 * report the problem.
-	 */
-	dir = g_path_get_dirname (file);
-	mkdir (dir, 0755);
-	g_free (dir);
 	
 	return(file);
 }
@@ -167,6 +159,15 @@ static int _wapi_shm_file_open (const gchar *filename, guint32 wanted_size)
 	int ret, tries = 0;
 	gboolean created = FALSE;
 	mode_t oldmask;
+	gchar *dir;
+		
+	/* No need to check if the dir already exists or check
+	 * mkdir() errors, because on any error the open() call will
+	 * report the problem.
+	 */
+	dir = g_path_get_dirname (filename);
+	mkdir (dir, 0755);
+	g_free (dir);
 
 try_again:
 	if (tries++ > 10) {
