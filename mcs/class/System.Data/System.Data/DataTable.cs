4f7d0c5f6e8488a00f7dedf6a9444a4c19125a18
Veerapuram Varadhan <v.varadhan@gmail.com>
2009-10-29 12:39:30 +0000

    2009-10-29  Veerapuram Varadhan  <vvaradhan@novell.com>
    	** Fixes #550960 - Patch by Stefano <stefano.sapienti@apsystems.it>
    	* DataTable.cs (BinarySerialize): Use datatype from DataColumn
    	instead of value of the column.
    
    svn path=/trunk/mcs/; revision=145011

--- mcs/class/System.Data/System.Data/DataTable.cs
+++ mcs/class/System.Data/System.Data/DataTable.cs
@@ -2757,8 +2757,8 @@ namespace System.Data {
 				if (rowsCount == 0)
 					continue;
 				BitArray nullBits = new BitArray (rowsCount);
-				Array recordArray = Array.CreateInstance (Rows[0][j].GetType (), recordsCount);
 				DataColumn column = Columns [j];
+				Array recordArray = Array.CreateInstance (column.DataType, recordsCount);
 				for (int k = 0, l = 0; k < Rows.Count; k++, l++) {
 					DataRowVersion version;
 					DataRow dr = Rows[k];
