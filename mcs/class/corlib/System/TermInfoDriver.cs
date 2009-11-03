6a3b240965b7bfe5bd9352a56c758910a7201b0f
Miguel de Icaza <miguel@gnome.org>
2009-11-03 05:08:39 +0000

    2009-11-03  Miguel de Icaza  <miguel@novell.com>
    	* TermInfoDriver.cs (CheckWindowDimensions) Avoid extra checks, as
    	the condition was always checked previously.
    
    	(WriteSpecialKey): Actually write the tab character.
    
    svn path=/trunk/mcs/; revision=145264

--- mcs/class/corlib/System/TermInfoDriver.cs
+++ mcs/class/corlib/System/TermInfoDriver.cs
@@ -28,7 +28,11 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 #if !NET_2_1
+
+//
+// Defining this writes the output to console.log
 //#define DEBUG
+
 using System.Collections;
 using System.IO;
 using System.Text;
@@ -359,8 +363,10 @@ namespace System {
 				break;
 			case ConsoleKey.Tab:
 				int n = 8 - (cursorLeft % 8);
-				for (int i = 0; i < n; i++)
+				for (int i = 0; i < n; i++){
 					IncrementX ();
+				}
+				WriteConsole ("\t");
 				break;
 			case ConsoleKey.Clear:
 				WriteConsole (clear);
@@ -713,11 +719,11 @@ namespace System {
 			}
 		}
 
+		//
+		// Requries that caller calls Init () if not !inited.
+		//
 		unsafe void CheckWindowDimensions ()
 		{
-			if (!inited)
-				Init ();
-
 			if (native_terminal_size == null || terminal_size == *native_terminal_size)
 				return;
 
