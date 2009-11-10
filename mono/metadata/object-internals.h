a206154c1858380ad57a9de0bd0ca3f39ecd9a35
Rodrigo Kumpera <kumpera@gmail.com>
2009-11-10 13:38:30 +0000

    In .: 2009-11-10  Rodrigo Kumpera  <rkumpera@novell.com>
    	* object-internals.h: Add mono_string_to_utf8_checked.
    
    	* object.c: Implement mono_string_to_utf8_checked.
    
    	Backport of r145539.
    
    
    svn path=/branches/mono-2-6/mono/; revision=145830

--- mono/metadata/object-internals.h
+++ mono/metadata/object-internals.h
@@ -1441,6 +1441,9 @@ mono_method_clear_object (MonoDomain *domain, MonoMethod *method) MONO_INTERNAL;
 void
 mono_class_compute_gc_descriptor (MonoClass *class) MONO_INTERNAL;
 
+char *
+mono_string_to_utf8_checked (MonoString *s, MonoError *error) MONO_INTERNAL;
+
 #endif /* __MONO_OBJECT_INTERNALS_H__ */
 
 
