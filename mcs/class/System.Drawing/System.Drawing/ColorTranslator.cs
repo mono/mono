073ba34f2b5f654155f657a6f3099313334dfc53
Marek Habersack <grendel@twistedcode.net>
2009-11-13 14:02:55 +0000

    1.1 build fix
    svn path=/branches/mono-2-4/mcs/; revision=146139

--- mcs/class/System.Drawing/System.Drawing/ColorTranslator.cs
+++ mcs/class/System.Drawing/System.Drawing/ColorTranslator.cs
@@ -45,7 +45,11 @@ namespace System.Drawing {
 			if ((htmlColor == null) || (htmlColor.Length == 0))
 				return Color.Empty;
 
+#if NET_2_0
 			switch (htmlColor.ToLowerInvariant ()) {
+#else
+			switch (htmlColor.ToLower ()) {
+#endif
 			case "buttonface":
 			case "threedface":		
 				return SystemColors.Control;
