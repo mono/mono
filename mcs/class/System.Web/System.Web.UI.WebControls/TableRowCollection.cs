f31512897b02c66737ae80166d2df6bc5879c3b9
Marek Habersack <grendel@twistedcode.net>
2009-11-13 13:26:20 +0000

    1.1 build fix
    svn path=/branches/mono-2-6/mcs/; revision=146134

--- mcs/class/System.Web/System.Web.UI.WebControls/TableRowCollection.cs
+++ mcs/class/System.Web/System.Web.UI.WebControls/TableRowCollection.cs
@@ -155,15 +155,20 @@ namespace System.Web.UI.WebControls {
 		
 		public void Remove (TableRow row)
 		{
-			row.Container = null;
+#if NET_2_0
+			if (row != null)
+				row.Container = null;
+#endif
 			cc.Remove (row);
 		}
 
 		public void RemoveAt (int index)
 		{
+#if NET_2_0
 			TableRow row = this [index] as TableRow;
 			if (row != null)
 				row.Container = null;
+#endif
 			
 			cc.RemoveAt (index);
 		}
