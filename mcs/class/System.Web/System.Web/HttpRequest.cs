e5ce3fc558d8b465734642cb627a5a6103caeb44
Marek Habersack <grendel@twistedcode.net>
2009-11-23 16:24:04 +0000

    2009-11-23  Marek Habersack  <mhabersack@novell.com>
    	* HttpRequest.cs: PathInfo returns an empty string when worker
    	request is absent or its GetPathInfo method returns a null. Patch
    	from Tiaan <tagdev@gmail.com>, thanks!
    
    2009-11-23  Marek Habersack  <mhabersack@novell.com>
    
    	* Route.cs: GetRouteData throws NotImplementedException only when
    	pathInfo for the current request is a non-empty string. Patch from
    	Tiaan <tagdev@gmail.com>, thanks!
    
    svn path=/trunk/mcs/; revision=146732

--- mcs/class/System.Web/System.Web/HttpRequest.cs
+++ mcs/class/System.Web/System.Web/HttpRequest.cs
@@ -942,7 +942,7 @@ namespace System.Web
 				if (path_info == null) {
 					if (worker_request == null)
 						return String.Empty;
-					path_info = worker_request.GetPathInfo ();
+					path_info = worker_request.GetPathInfo () ?? String.Empty;
 				}
 
 				return path_info;
