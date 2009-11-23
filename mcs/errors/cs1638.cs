4cb5e7dd669e6a67e0c976d7520fbd9e288ba5d0
Marek Safar <marek.safar@gmail.com>
2009-11-23 08:57:23 +0000

    Unused
    svn path=/trunk/mcs/; revision=146711

--- mcs/errors/cs1638.cs
+++
@@ -1,7 +0,0 @@
-// CS1638: `C__C': Any identifier with double underscores cannot be used when ISO language version mode is specified
-// Line: 5
-// Compiler options: -langversion:ISO-1
-
-class C__C
-{
-}
