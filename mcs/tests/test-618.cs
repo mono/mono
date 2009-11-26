28e4bd1bd98509f0b57799d7cc81f0c2d7aaa3b4
Marek Safar <marek.safar@gmail.com>
2009-11-26 14:30:41 +0000

    More tests, update
    svn path=/trunk/mcs/; revision=146986

--- mcs/tests/test-618.cs
+++ mcs/tests/test-618.cs
@@ -5,18 +5,23 @@ class C
 	//
 	// All the operations should be reduced
 	//
-	public void ZeroBasedReductions ()
+	public static void ZeroBasedReductions ()
 	{
 		int a = 1;
 		
 		a = a + 0;
 		a = a - 0;
+		a = a | 0;
+		a = 0 + a;
+		a = 0 - a;
+		a = 0 | a;
 		
 		a = a >> 0x40;
 	}
 	
 	public static void Main ()
 	{
+		ZeroBasedReductions ();
 	}
 }
 
