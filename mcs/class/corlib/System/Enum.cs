1d79382f9c8a768a66a2c6e0fcf754a25de91ff1
Jb Evain <jbevain@gmail.com>
2009-12-02 18:38:22 +0000

    2009-12-02  Jb Evain  <jbevain@novell.com>
    	* Enum.cs (IConvertible.ToType): properly convert enum values
    	to strings instead of their numeric value.
    
    
    svn path=/trunk/mcs/; revision=147482

--- mcs/class/corlib/System/Enum.cs
+++ mcs/class/corlib/System/Enum.cs
@@ -283,6 +283,8 @@ namespace System
 		{
 			if (targetType == null)
 				throw new ArgumentNullException ("targetType");
+			if (targetType == typeof (string))
+				return ToString (provider);
 			return Convert.ToType (Value, targetType, provider, false);
 		}
 
