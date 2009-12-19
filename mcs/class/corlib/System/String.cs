3b438d056df77be0d307b38d3cbebafec00d3447
Zoltan Varga <vargaz@gmail.com>
2009-12-19 21:36:51 +0000

    In System:
    2009-12-19  Zoltan Varga  <vargaz@gmail.com>
    
    	* String.cs: Fix the various IndexOf and LastIndexOf methods so they handle
    	"" and a non-zero startIndex correctly. Fixes #566106.
    
    In Test/System:
    
    2009-12-19  Zoltan Varga  <vargaz@gmail.com>
    
    	* StringTest.cs: Add tests for IndexOf ("", startIndex, count).
    
    svn path=/branches/mono-2-4/mcs/; revision=148791

--- mcs/class/corlib/System/String.cs
+++ mcs/class/corlib/System/String.cs
@@ -930,7 +930,7 @@ namespace System
 			if (valueLen <= 1) {
 				if (valueLen == 1)
 					return IndexOfUnchecked (value[0], startIndex, count);
-				return 0;
+				return startIndex;
 			}
 
 			fixed (char* thisptr = this, valueptr = value) {
@@ -958,7 +958,7 @@ namespace System
 				return -1;
 
 			if (valueLen == 0)
-				return 0;
+				return startIndex;
 
 			fixed (char* thisptr = this, valueptr = value) {
 				char* ap = thisptr + startIndex;
@@ -1036,7 +1036,7 @@ namespace System
 			if (valueLen <= 1) {
 				if (valueLen == 1)
 					return LastIndexOfUnchecked (value[0], startIndex, count);
-				return 0;
+				return startIndex;
 			}
 
 			fixed (char* thisptr = this, valueptr = value) {
@@ -1064,7 +1064,7 @@ namespace System
 				return -1;
 
 			if (valueLen == 0)
-				return 0;
+				return startIndex;
 
 			fixed (char* thisptr = this, valueptr = value) {
 				char* ap = thisptr + startIndex - valueLen + 1;
