d5342c563b73276dedc38bc4c1c5f9d3d8368f9b
Marek Safar <marek.safar@gmail.com>
2010-01-12 17:05:32 +0000

    Removed duplicate usings and 1.1 code
    svn path=/trunk/mcs/; revision=149418

--- mcs/class/System.DirectoryServices/Assembly/AssemblyInfo.cs
+++ mcs/class/System.DirectoryServices/Assembly/AssemblyInfo.cs
@@ -16,16 +16,6 @@ using System.Runtime.InteropServices;
 
 // General Information about the System.DirectoryServices assembly
 
-using System;
-using System.Reflection;
-using System.Resources;
-using System.Security;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about the system assembly
-
 [assembly: AssemblyTitle ("System.DirectoryServices.dll")]
 [assembly: AssemblyDescription ("System.DirectoryServices.dll")]
 [assembly: AssemblyDefaultAlias ("System.DirectoryServices.dll")]
@@ -47,16 +37,5 @@ using System.Runtime.InteropServices;
 	[assembly: AssemblyKeyFile ("../msfinal.pub")]
 #endif
 
-#if NET_2_0
-	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
-	[assembly: AllowPartiallyTrustedCallers]
-	[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
-	[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
-	[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-#elif NET_1_1
-	[assembly: AssemblyTrademark ("")]
-	[assembly: AssemblyConfiguration ("")]
-#elif NET_1_0
-	[assembly: AssemblyTrademark ("")]
-	[assembly: AssemblyConfiguration ("")]
-#endif
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: AllowPartiallyTrustedCallers]
