f76fb4d9af208b83f4108ea68f4c69ab4e1211e7
Sebastien Pouliot <sebastien@ximian.com>
2010-01-12 13:09:49 +0000

    In Test/System.Net: 2010-01-12  Sebastien Pouliot  <sebastien@ximian.com>
    	* NetworkCredentialTest.cs: New. Unit tests.
    	[Backport r149401]
    
    In System.Net:
    2010-01-12  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* NetworkCredential.cs: Fix properties to never return null.
    
    In .:
    2010-01-12  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* System_test.dll.sources: Add NetworkCredentialTest.cs to the tests
    	[Backport r149401]
    
    
    svn path=/branches/mono-2-6/mcs/; revision=149402

--- mcs/class/System/System.Net/NetworkCredential.cs
+++ mcs/class/System/System.Net/NetworkCredential.cs
@@ -4,8 +4,7 @@
 // Author: Duncan Mak (duncan@ximian.com)
 //
 // (C) Ximian, Inc.
-//
-
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -41,7 +40,6 @@ namespace System.Net
 		
 		// Constructors
 		public NetworkCredential ()
-			: base ()
 		{
 		}
 
@@ -60,21 +58,18 @@ namespace System.Net
 
 		// Properties
 
-		public string Domain
-		{
-			get { return domain; }
+		public string Domain {
+			get { return domain == null ? String.Empty : domain; }
 			set { domain = value; }
 		}
 
-		public string UserName
-		{
-			get { return userName; }
+		public string UserName {
+			get { return userName == null ? String.Empty : userName; }
 			set { userName = value; }			
 		}
 
-		public string Password
-		{
-			get { return password; }
+		public string Password {
+			get { return password == null ? String.Empty : password; }
 			set { password = value; }
 		}
 
