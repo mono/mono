62763da9fcf164ef07b13c3559d9f9ef8c164898
Marek Safar <marek.safar@gmail.com>
2010-01-12 10:44:01 +0000

    Forget to commit this one.
    svn path=/trunk/mcs/; revision=149394

--- mcs/errors/cs0543.cs
+++ mcs/errors/cs0543.cs
@@ -1,14 +1,10 @@
-// cs0543.cs: The enumerator value `Blah.MyEnum.Bar' is too large to fit in its type `byte'
-// Line : 9
+// CS0543: The enumerator value `Blah.MyEnum.Bar' is outside the range of enumerator underlying type `byte'
+// Line : 8
 
-public class Blah {
-
+public class Blah
+{
 	public enum MyEnum : byte {
 		Foo = 255,
 		Bar
-	}
-
-	public static void Main ()
-	{
 	}
 }
