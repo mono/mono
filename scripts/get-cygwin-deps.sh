4ec055da0c79e79b6c3a2310f4da7dbcee0f5547
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-01-15 00:06:19 +0000

    add 3 more zip files needed
    svn path=/trunk/mono/; revision=149585

--- scripts/get-cygwin-deps.sh
+++ scripts/get-cygwin-deps.sh
@@ -13,5 +13,9 @@ rm -rf cygwin-deps
 mkdir -p cygwin-deps
 wget -P cygwin-deps ftp://ftp.gtk.org/pub/gtk/v2.6/win32/glib-2.6.6.zip || exit 1
 wget -P cygwin-deps ftp://ftp.gtk.org/pub/gtk/v2.6/win32/glib-dev-2.6.6.zip || exit 1
+wget -P cygwin-deps http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/gettext-0.14.5.zip || exit 1
+wget -P cygwin-deps http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/gettext-dev-0.14.5.zip || exit 1
+wget -P cygwin-deps http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/libiconv-1.9.1.bin.woe32.zip || exit 1
 
-cd cygwin-deps && unzip glib-2.6.6.zip && unzip glib-dev-2.6.6.zip
+cd cygwin-deps && unzip glib-2.6.6.zip && unzip glib-dev-2.6.6.zip && unzip gettext-0.14.5.zip && unzip gettext-dev-0.14.5.zip && \
+	unzip libiconv-1.9.1.bin.woe32.zip
