8ce6a8ec16b7d13358fa69ad55e4e53ac2241f5a
Jb Evain <jbevain@gmail.com>
2010-01-25 15:46:31 +0000

    2010-01-25  Jb Evain  <jbevain@novell.com>
    	* QueryableTransformer.cs (ReplaceQueryableMethod): fix
    	exception message.
    
    
    svn path=/trunk/mcs/; revision=150132

--- mcs/class/System.Core/System.Linq/QueryableTransformer.cs
+++ mcs/class/System.Core/System.Linq/QueryableTransformer.cs
@@ -111,7 +111,8 @@ namespace System.Linq {
 
 		static MethodInfo ReplaceQueryableMethod (MethodInfo method)
 		{
-			var result = GetMatchingMethod (method, GetTargetDeclaringType (method));
+			var target_type = GetTargetDeclaringType (method);
+			var result = GetMatchingMethod (method, target_type);
 
 			if (result != null)
 				return result;
@@ -120,7 +121,7 @@ namespace System.Linq {
 				string.Format (
 					"There is no method {0} on type {1} that matches the specified arguments",
 					method.Name,
-					method.DeclaringType.FullName));
+					target_type.FullName));
 		}
 
 		static MethodInfo GetMatchingMethod (MethodInfo method, Type declaring)
