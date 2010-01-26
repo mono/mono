6a3017631e8304ced7df74c1596c10eb31e6f91a
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-01-26 21:12:01 +0000

    fix T8 method
    svn path=/trunk/mcs/; revision=150242

--- mcs/class/corlib/System/Tuple.cs
+++ mcs/class/corlib/System/Tuple.cs
@@ -34,7 +34,7 @@ namespace System
 {
 	public static class Tuple
 	{
-		public static Tuple<T1, T2, T3, T4, T5, T6, T7, T8> Create<T1, T2, T3, T4, T5, T6, T7, T8>
+		public static Tuple<T1, T2, T3, T4, T5, T6, T7, Tuple<T8>> Create<T1, T2, T3, T4, T5, T6, T7, T8>
 			(
 			 T1 item1,
 			 T2 item2,
@@ -43,8 +43,8 @@ namespace System
 			 T5 item5,
 			 T6 item6,
 			 T7 item7,
-			 T8 item8) {
-			return new Tuple<T1, T2, T3, T4, T5, T6, T7, T8> (item1, item2, item3, item4, item5, item6, item7, item8);
+			 Tuple<T8> item8) {
+			return new Tuple<T1, T2, T3, T4, T5, T6, T7, Tuple<T8>> (item1, item2, item3, item4, item5, item6, item7, item8);
 		}
 
 		public static Tuple<T1, T2, T3, T4, T5, T6, T7> Create<T1, T2, T3, T4, T5, T6, T7>
