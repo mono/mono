dc5a2680bc65325e4d9c52b2a4249194e019ccdf
Marek Habersack <grendel@twistedcode.net>
2010-02-12 02:04:34 +0000

    2010-02-12  Marek Habersack  <mhabersack@novell.com>
    	* AssemblyInfo.cs: added 4.0 attributes
    
    svn path=/trunk/mcs/; revision=151586

--- mcs/class/System.Web.Abstractions/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Web.Abstractions/Assembly/AssemblyInfo.cs
@@ -34,8 +34,11 @@ using System.Resources;
 using System.Security;
 using System.Security.Permissions;
 using System.Diagnostics;
+using System.Runtime;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Web;
 
 // General Information about the System.Web.Abstractions assembly
 // v3.5 Assembly
@@ -58,7 +61,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyKeyFile ("../winfx.pub")]
 
 [assembly: ComVisible (false)]
+#if !NET_4_0
 [assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+#endif
 [assembly: AllowPartiallyTrustedCallers]
 
 [assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
@@ -69,3 +74,32 @@ using System.Runtime.InteropServices;
 
 // FIXME: We get collisions with this internalsVisibleTo because of Consts.cs and MonoTodo
 //[assembly: InternalsVisibleTo ("System.ServiceModel.Web, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
+
+#if NET_4_0
+[assembly: AssemblyTargetedPatchBand ("1.0.21-0")]
+[assembly: Dependency ("System.Web,", LoadHint.Always)]
+[assembly: TargetFramework (".NETFramework,Version=v4.0")]
+[assembly: SecurityRules (SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpStaticObjectsCollectionBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpStaticObjectsCollectionWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpContextWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpContextBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpFileCollectionBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpPostedFileBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpFileCollectionWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpCachePolicyWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpApplicationStateWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpApplicationStateBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpBrowserCapabilitiesBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpCachePolicyBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpBrowserCapabilitiesWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpPostedFileWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpSessionStateWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpServerUtilityBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpSessionStateBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpServerUtilityWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpRequestBase))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpRequestWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpResponseWrapper))]
+[assembly: TypeForwardedTo (typeof (System.Web.HttpResponseBase))]
+#endif
