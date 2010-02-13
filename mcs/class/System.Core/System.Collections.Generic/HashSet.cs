dabb8965138a66e01fec0830a53d46d301892d86
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-02-13 17:49:23 +0000

    2010-02-13 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* Test/System.Collections.Generic/HashSetTest.cs:
    	* System.Collections.Generic/HashSet.cs: fix and test for bug #579791.
    	Patch from Tiaan Geldenhuys.
    
    
    svn path=/branches/mono-2-6/mcs/; revision=151706

--- mcs/class/System.Core/System.Collections.Generic/HashSet.cs
+++ mcs/class/System.Core/System.Collections.Generic/HashSet.cs
@@ -157,7 +157,7 @@ namespace System.Collections.Generic {
 			int current = table [index] - 1;
 			while (current != NO_SLOT) {
 				Link link = links [current];
-				if (link.HashCode == hash && comparer.Equals (item, slots [current]))
+				if (link.HashCode == hash && ((hash == HASH_FLAG && (item == null || null == slots [current])) ? (item == null && null == slots [current]) : comparer.Equals (item, slots [current])))
 					return true;
 
 				current = link.Next;
@@ -230,6 +230,8 @@ namespace System.Collections.Generic {
 
 		int GetItemHashCode (T item)
 		{
+			if (item == null)
+				return HASH_FLAG;
 			return comparer.GetHashCode (item) | HASH_FLAG;
 		}
 
@@ -311,7 +313,7 @@ namespace System.Collections.Generic {
 			int prev = NO_SLOT;
 			do {
 				Link link = links [current];
-				if (link.HashCode == hashCode && comparer.Equals (slots [current], item))
+				if (link.HashCode == hashCode && ((hashCode == HASH_FLAG && (item == null || null == slots [current])) ? (item == null && null == slots [current]) : comparer.Equals (slots [current], item)))
 					break;
 
 				prev = current;
