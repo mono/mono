e208b54d2d4634464db82c75e18d7795fd85aaf4
Rodrigo Kumpera <kumpera@gmail.com>
2010-02-18 22:58:05 +0000

    2010-02-18  Rodrigo Kumpera  <rkumpera@novell.com>
    	* reflection.c (mono_reflection_method_on_tb_inst_get_handle):
    	* Handle non
    	generic methods.
    
    	* reflection.c (mono_reflection_get_custom_attrs_info): Handle
    	* compiler context
    	cases for ParameterInfo.
    
    	Fixes #579493.
    
    	Backport of r152024.
    
    svn path=/branches/mono-2-6/mono/; revision=152034

--- mono/metadata/reflection.c
+++ mono/metadata/reflection.c
@@ -181,12 +181,16 @@ static MonoReflectionType* mono_reflection_type_resolve_user_types (MonoReflecti
 static gboolean is_sre_array (MonoClass *class);
 static gboolean is_sre_byref (MonoClass *class);
 static gboolean is_sre_pointer (MonoClass *class);
+static gboolean is_sre_method_builder (MonoClass *class);
+static gboolean is_sre_ctor_builder (MonoClass *class);
 static gboolean is_sr_mono_method (MonoClass *class);
 static gboolean is_sr_mono_cmethod (MonoClass *class);
 static gboolean is_sr_mono_generic_method (MonoClass *class);
 static gboolean is_sr_mono_generic_cmethod (MonoClass *class);
 static gboolean is_sr_mono_field (MonoClass *class);
 static gboolean is_sr_mono_property (MonoClass *class);
+static gboolean is_sre_method_on_tb_inst (MonoClass *class);
+static gboolean is_sre_ctor_on_tb_inst (MonoClass *class);
 
 static guint32 mono_image_get_methodspec_token (MonoDynamicImage *assembly, MonoMethod *method);
 static guint32 mono_image_get_inflated_method_token (MonoDynamicImage *assembly, MonoMethod *m);
@@ -2726,6 +2730,9 @@ mono_reflection_method_on_tb_inst_get_handle (MonoReflectionMethodOnTypeBuilderI
 	method = inflate_method (m->inst, (MonoObject*)m->mb);
 
 	klass = method->klass;
+
+	if (m->method_args == NULL)
+		return method;
 
 	if (method->is_inflated)
 		method = ((MonoMethodInflated *) method)->declaring;
@@ -8582,6 +8589,20 @@ mono_reflection_get_custom_attrs_info (MonoObject *obj)
 			g_assert (method);
 
 			cinfo = mono_custom_attrs_from_param (method, param->PositionImpl + 1);
+		} else if (is_sre_method_on_tb_inst (member_class)) {/*XXX This is a workaround for Compiler Context*/
+			MonoMethod *method = mono_reflection_method_on_tb_inst_get_handle ((MonoReflectionMethodOnTypeBuilderInst*)param->MemberImpl);
+			cinfo = mono_custom_attrs_from_param (method, param->PositionImpl + 1);
+		} else if (is_sre_ctor_on_tb_inst (member_class)) { /*XX This is a workaround for Compiler Context*/
+		MonoReflectionCtorOnTypeBuilderInst *c = (MonoReflectionCtorOnTypeBuilderInst*)param->MemberImpl;
+			MonoMethod *method = NULL;
+			if (is_sre_ctor_builder (mono_object_class (c->cb)))
+				method = ((MonoReflectionCtorBuilder *)c->cb)->mhandle;
+			else if (is_sr_mono_cmethod (mono_object_class (c->cb)))
+				method = ((MonoReflectionMethod *)c->cb)->method;
+			else
+				g_error ("mono_reflection_get_custom_attrs_info:: can't handle a CTBI with base_method of type %s", mono_type_get_full_name (member_class));
+
+			cinfo = mono_custom_attrs_from_param (method, param->PositionImpl + 1);
 		} else {
 			char *type_name = mono_type_get_full_name (member_class);
 			char *msg = g_strdup_printf ("Custom attributes on a ParamInfo with member %s are not supported", type_name);
@@ -8751,6 +8772,17 @@ is_sre_generic_instance (MonoClass *class)
 	check_corlib_type_cached (class, "System.Reflection", "MonoGenericClass");
 }
 
+static gboolean
+is_sre_method_builder (MonoClass *class)
+{
+	check_corlib_type_cached (class, "System.Reflection.Emit", "MethodBuilder");
+}
+
+static gboolean
+is_sre_ctor_builder (MonoClass *class)
+{
+	check_corlib_type_cached (class, "System.Reflection.Emit", "ConstructorBuilder");
+}
 
 static gboolean
 is_sr_mono_method (MonoClass *class)
@@ -8780,6 +8812,18 @@ static gboolean
 is_sr_mono_property (MonoClass *class)
 {
 	check_corlib_type_cached (class, "System.Reflection", "MonoProperty");
+}
+
+static gboolean
+is_sre_method_on_tb_inst (MonoClass *class)
+{
+	check_corlib_type_cached (class, "System.Reflection.Emit", "MethodOnTypeBuilderInst");
+}
+
+static gboolean
+is_sre_ctor_on_tb_inst (MonoClass *class)
+{
+	check_corlib_type_cached (class, "System.Reflection.Emit", "ConstructorOnTypeBuilderInst");
 }
 
 gboolean
