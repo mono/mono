0f73f9304c7a09226dd019d45e3f38632d55ea9f
Marek Habersack <grendel@twistedcode.net>
2010-02-19 03:03:16 +0000

    Backport of r152045
    svn path=/branches/mono-2-6/mcs/; revision=152046

--- mcs/class/System.Web/Test/System.Web.UI.WebControls/ButtonTest.cs
+++ mcs/class/System.Web/Test/System.Web.UI.WebControls/ButtonTest.cs
@@ -85,7 +85,26 @@ namespace MonoTests.System.Web.UI.WebControls
 
 
 	[TestFixture]
-	public class ButtonTest {
+	public class ButtonTest
+	{
+		[TestFixtureSetUp]
+		public void SetUp ()
+		{
+			WebTest.CopyResource (GetType (), "ButtonColor_Bug325489.aspx", "ButtonColor_Bug325489.aspx");
+		}
+
+#if NET_2_0
+		[Test]
+		public void ButtonColor_Bug325489 ()
+		{
+			WebTest t = new WebTest ("ButtonColor_Bug325489.aspx");
+			string origHtml = @"<input type=""submit"" name=""button1"" value="""" id=""button1"" style=""background-color:#316AC5;"" />";
+			string html = t.Run ();
+			string renderedHtml = HtmlDiff.GetControlFromPageHtml (html);
+
+			HtmlDiff.AssertAreEqual (origHtml, renderedHtml, "#A1");
+		}
+#endif
 		
 		[Test]
 		public void Button_DefaultValues ()
