923e44a3cf28b6e65e8224cef0c6fd9a6b1fa1df
Marek Safar <marek.safar@gmail.com>
2010-02-23 10:43:41 +0000

    2010-02-23  Marek Safar  <marek.safar@gmail.com>
    	* Enumerable.cs: Implement Zip.
    
    
    svn path=/trunk/mcs/; revision=152260

--- mcs/class/System.Core/System.Linq/Enumerable.cs
+++ mcs/class/System.Core/System.Linq/Enumerable.cs
@@ -2259,6 +2259,33 @@ namespace System.Linq
 		}
 
 		#endregion
+		
+#if NET_4_0		
+		#region Zip
+		
+		public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult> (this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
+		{
+			Check.FirstAndSecond (first, second);
+			if (resultSelector == null)
+				throw new ArgumentNullException ("resultSelector");
+				
+			return CreateZipIterator (first, second, resultSelector);
+		}
+		
+		static IEnumerable<TResult> CreateZipIterator<TFirst, TSecond, TResult> (IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> selector)
+		{
+			using (IEnumerator<TFirst> first_enumerator = first.GetEnumerator ()) {
+				using (IEnumerator<TSecond> second_enumerator = second.GetEnumerator ()) {
+
+					while (first_enumerator.MoveNext () && second_enumerator.MoveNext ()) {
+						yield return selector (first_enumerator.Current, second_enumerator.Current);
+					}
+				}
+			}
+		}
+		
+		#endregion
+#endif		
 
 		#region Where
 
