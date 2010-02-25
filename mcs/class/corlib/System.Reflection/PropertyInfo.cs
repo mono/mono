a98dc1dc5d192659b0781762a76acb2499f7720b
Rodrigo Kumpera <kumpera@gmail.com>
2010-02-25 01:48:46 +0000

    2010-02-24 Rodrigo Kumpera  <rkumpera@novell.com>
    	* MethodBody.cs: Add v4 bits.
    
    svn path=/trunk/mcs/; revision=152428

--- mcs/class/corlib/System.Reflection/PropertyInfo.cs
+++ mcs/class/corlib/System.Reflection/PropertyInfo.cs
@@ -118,6 +118,36 @@ namespace System.Reflection {
 			throw new NotImplementedException ();
 		}
 
+#if NET_4_0
+		public override bool Equals (object obj)
+		{
+			return obj == this;
+		}
+
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+
+		public static bool operator == (PropertyInfo left, PropertyInfo right)
+		{
+			if ((object)left == (object)right)
+				return true;
+			if ((object)left == null ^ (object)right == null)
+				return false;
+			return left.Equals (right);
+		}
+
+		public static bool operator != (PropertyInfo left, PropertyInfo right)
+		{
+			if ((object)left == (object)right)
+				return false;
+			if ((object)left == null ^ (object)right == null)
+				return true;
+			return !left.Equals (right);
+		}
+#endif
+
 		void _PropertyInfo.GetIDsOfNames ([In] ref Guid riid, IntPtr rgszNames, uint cNames, uint lcid, IntPtr rgDispId)
 		{
 			throw new NotImplementedException ();
