f8b0471cf14f20be4f52dc42ebd98a581db729e5
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-03-03 22:40:44 +0000

    2010-03-03  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* gensources.sh: Reverted to plain bash, cygwin/make seems to end up confused
    	with path separators with the managed version.
    
    svn path=/trunk/mcs/; revision=152970

--- mcs/tools/gensources.sh
+++ mcs/tools/gensources.sh
@@ -1,89 +1,68 @@
 #!/usr/bin/env bash
 
-cat >$0.cs <<EOF
+includefile=$1
+excludefile=$2
 
-using System;
-using System.Collections.Generic;
-using System.IO;
+## input variables:
+## 	$filelist:
+##		A colon (':') separated list of files already read.
+##		Must be initialized to ":".
+##	$excludelist:
+##		A newline separated of element (support Shell Patterns) to exclude.
+##	$separator:
+##		The separator used in list for the output
+##	
+## output variables:
+## 	$list:
+##		A list of elements separated by the separator given in $separator.
+##		The new elements will be appended to the list.
+readlist () {
+	local onelist
+	local onelistcontent
+	onelist=$1
 
-class GenSource {
-	static List<string> filelist;
-	static List<string> list;
-	static List<string> excludelist;
+	if [ ":$onelist:" = "::" ] ; then return ; fi
+	if [ ! -f $onelist ] ; then return ; fi
+	if [ ":${filelist##*:$onelist:*}:" = "::" ]  ; then return ; fi
+	filelist=":$onelist$filelist"
 
-	static int Main (string [] args)
-	{
-		string includefile = null;
-		string excludefile = null;
+	onelistcontent=`cat $onelist | sed "s=[ \t]*$==g" | while read line ; do echo -n $line ; echo -n ":" ; done`
 
-		if (args.Length == 0) {
-			Console.Error.WriteLine ("No arguments");
-			return 1;
-		}
-
-		if (args.Length > 0)
-			includefile = args [0];
-
-		if (args.Length > 1)
-			excludefile = args [1];
-
-		list = new List<string> ();
-		filelist = new List<string> ();
-		Parse (excludefile);
-		if (list.Count != 0) {
-			excludelist = list;
-			list = new List<string> ();
-		}
-
-		Parse (includefile);
-		foreach (string line in list)
-			Console.WriteLine (line);
-		return 0;
-	}
-
-	static void Parse (string onelist)
-	{
-		if (string.IsNullOrEmpty (onelist))
-			return;
-
-		if (!File.Exists (onelist))
-			return;
-
-		if (filelist.Contains (onelist))
-			return;
-
-		filelist.Add (onelist);
-
-		foreach (string l in File.ReadAllLines (onelist)) {
-			string line = l.Replace (" ", "").Replace ("\t", "").Replace ('/', Path.DirectorySeparatorChar);
-			if (!line.StartsWith ("#")) {
-				if (excludelist != null && excludelist.Contains (line))
-					continue;
-
-				if (string.IsNullOrEmpty (line))
-					continue;
-				
-				string dir = Path.GetDirectoryName (line);
-				string name = Path.GetFileName (line);
-				string [] expanded = Directory.GetFiles (Path.Combine (Environment.CurrentDirectory, dir), name);
-				foreach (string file in expanded) {
-					if (!list.Contains (file)) {
-						list.Add (file);
-					}
-				}
-			} else {
-				Parse (line.Substring ("#include".Length));
-			}
-		}
-	}
+	OFS="$IFS"
+	IFS=":"
+	for line in $onelistcontent ; do
+		line2=${line##\#}
+		if [ ":$line:" = ":$line2:" ] ; then
+			for linex in $excludelist ; do
+				if [ ":${line##$linex}:" = "::" ] ; then line="" ; fi
+			done
+			if [ ":$line:" != "::" ] ; then
+				if [ ":$list:" = "::" ] ; then
+					list="$line"
+				else
+					list="$list$separator$line"
+				fi
+			fi
+		else
+			line3=${line2##include }
+			if [ ":$line3:" != ":$line2:" -a ":$line3:" != "::" ] ; then
+				readlist "$line3"
+			fi
+		fi
+	done
+	IFS="$OFS"
 }
 
-EOF
+list=""
+filelist=":"
+excludelist=""
+separator=":"
+readlist "$excludefile"
 
-# check if $0.exe exists, or if $0 is newer than $0.exe
-if test ! -e $0.exe -o $0 -nt $0.exe; then
-	# echo compiling...
-	gmcs $0.cs -target:exe -out:$0.exe -debug+
-fi
-mono --debug $0.exe "$@" | sort
-# leave exe for next time the build requires us to avoid recompiling
+excludelist="$list"
+list=""
+filelist=":"
+separator="
+"
+readlist "$includefile"
+echo "$list" | sort | uniq
