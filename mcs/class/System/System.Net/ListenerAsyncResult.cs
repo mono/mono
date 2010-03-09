41ebfcc7e628786bb4bc5f6b3a3cde0bbbefd07d
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-03-09 06:34:44 +0000

    add realm from HttpListener
    svn path=/trunk/mcs/; revision=153302

--- mcs/class/System/System.Net/ListenerAsyncResult.cs
+++ mcs/class/System/System.Net/ListenerAsyncResult.cs
@@ -94,7 +94,7 @@ namespace System.Net {
 				AuthenticationSchemes schemes = context.Listener.SelectAuthenticationScheme (context);
 				if ((schemes == AuthenticationSchemes.Basic || context.Listener.AuthenticationSchemes == AuthenticationSchemes.Negotiate) && context.Request.Headers ["Authorization"] == null) {
 					context.Response.StatusCode = 401;
-					context.Response.Headers ["WWW-Authenticate"] = schemes + " realm=\"\"";
+					context.Response.Headers ["WWW-Authenticate"] = schemes + " realm=\"" + context.Listener.Realm + "\"";
 					context.Response.OutputStream.Close ();
 					IAsyncResult ares = context.Listener.BeginGetContext (cb, state);
 					this.forward = (ListenerAsyncResult) ares;
