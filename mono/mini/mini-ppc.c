6a50faab8d663b9eb67285be25539f1444c451f1
Zoltan Varga <vargaz@gmail.com>
2010-03-21 12:46:26 +0000

    2010-03-21  Zoltan Varga  <vargaz@gmail.com>
    	* mini-ppc.c (mono_arch_emit_exceptions): Avoid an assert in ppc_patch () for
    	some large methods with lots of exception handlers. Fixes #440924.
    
    svn path=/branches/mono-2-4/mono/; revision=153955

--- mono/mini/mini-ppc.c
+++ mono/mini/mini-ppc.c
@@ -4995,7 +4995,7 @@ mono_arch_emit_exceptions (MonoCompile *cfg)
 		case MONO_PATCH_INFO_EXC: {
 			unsigned char *ip = patch_info->ip.i + cfg->native_code;
 			i = exception_id_by_name (patch_info->data.target);
-			if (exc_throw_pos [i]) {
+			if (exc_throw_pos [i] && !(ip > exc_throw_pos [i] && ip - exc_throw_pos [i] > 50000)) {
 				ppc_patch (ip, exc_throw_pos [i]);
 				patch_info->type = MONO_PATCH_INFO_NONE;
 				break;
