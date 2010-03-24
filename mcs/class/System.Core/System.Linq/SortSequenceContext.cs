mono/mono

commit 200e7a4377ab10bf923a5d63d245b61cb7aebd03

2010-03-24  Jb Evain  <jbevain@novell.com>
	* SortSequenceContext.cs: Fix OrderByDescending stability.
	Based on a patch by Richard Kiene  <richard.kiene@logos.com>.

	backport of r154154.


svn path=/branches/mono-2-6/mcs/; revision=154156

mcs/class/System.Core/System.Linq/SortSequenceContext.cs
@@ -62,8 +62,10 @@ namespace System.Linq {
 			if (comparison == 0) {
 				if (child_context != null)
 					return child_context.Compare (first_index, second_index);
-				else
-					comparison = first_index - second_index;
+
+				comparison = direction === SortDirection.Descending
+					? second_index - first_index
+					: first_index - second_index;
 			}
 
 			return direction == SortDirection.Descending ? -comparison : comparison;

commit d2ed53e64d7e3d33dca03e4c73976bf2f8729c4a

2010-03-24  Andrés G. Aragoneses  <knocte@gmail.com>
	* SortSequenceContext.cs: Fix build, broken in r154156 by a
	typo in the last backport.


svn path=/branches/mono-2-6/mcs/; revision=154181

mcs/class/System.Core/System.Linq/SortSequenceContext.cs
@@ -63,7 +63,7 @@ namespace System.Linq {
 				if (child_context != null)
 					return child_context.Compare (first_index, second_index);
 
-				comparison = direction === SortDirection.Descending
+				comparison = direction == SortDirection.Descending
 					? second_index - first_index
 					: first_index - second_index;
 			}