4ddfad2f3be8068e1b2069abbfdadf97790f5f37
Jb Evain <jbevain@gmail.com>
2010-04-07 13:50:35 +0000

    2010-04-07  Jb Evain  <jbevain@novell.com>
    	* EnumerableQuery_T.cs: implement.
    
    
    svn path=/trunk/mcs/; revision=154956

--- mcs/class/System.Core/System.Linq/EnumerableQuery_T.cs
+++ mcs/class/System.Core/System.Linq/EnumerableQuery_T.cs
@@ -3,6 +3,7 @@
 //
 // Authors:
 //  Marek Safar  <marek.safar@gmail.com>
+//  Jb Evain  <jbevain@novell.com>
 //
 // Copyright (C) 2009 Novell, Inc (http://www.novell.com)
 //
@@ -28,19 +29,71 @@
 
 #if NET_4_0
 
-using System.Linq.Expressions;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace System.Linq
 {
-	public class EnumerableQuery<T> : EnumerableQuery
+	public class EnumerableQuery<T> : EnumerableQuery, IOrderedQueryable<T>, IQueryable<T>, IQueryProvider
 	{
+		QueryableEnumerable<T> queryable;
+
+		public Type ElementType {
+			get { return queryable.ElementType; }
+		}
+
+		public Expression Expression {
+			get { return queryable.Expression; }
+		}
+
+		public IQueryProvider Provider {
+			get { return queryable; }
+		}
+
 		public EnumerableQuery (Expression expression)
 		{
+			queryable = new QueryableEnumerable<T> (expression);
 		}
-		
-		public EnumerableQuery (IEnumerable<T> expression)
+
+		public EnumerableQuery (IEnumerable<T> enumerable)
 		{
+			queryable = new QueryableEnumerable<T> (enumerable);
+		}
+
+		public IEnumerable GetEnumerable ()
+		{
+			return queryable.GetEnumerable ();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return queryable.GetEnumerator ();
+		}
+
+		public IEnumerator<T> GetEnumerator ()
+		{
+			return queryable.GetEnumerator ();
+		}
+
+		public IQueryable CreateQuery (Expression expression)
+		{
+			return queryable.CreateQuery (expression);
+		}
+
+		public object Execute (Expression expression)
+		{
+			return queryable.Execute (expression);
+		}
+
+		public IQueryable<TElem> CreateQuery<TElem> (Expression expression)
+		{
+			return new EnumerableQuery<TElem> (expression);
+		}
+
+		public TResult Execute<TResult> (Expression expression)
+		{
+			return queryable.Execute<TResult> (expression);
 		}
 	}
 }
