316a2bff1e6dc7a609fc948dfe8478c4ff8a2db6
Zoltan Varga <vargaz@gmail.com>
2010-04-13 21:38:10 +0000

    2010-04-13  Zoltan Varga  <vargaz@gmail.com>
    	* Convert.cs (ToInt32): Do a checked cast. Fixes #596339.
    
    svn path=/trunk/mcs/; revision=155333

--- mcs/class/corlib/System/Convert.cs
+++ mcs/class/corlib/System/Convert.cs
@@ -1207,7 +1207,9 @@ namespace System {
 					"Value is greater than Int32.MaxValue or less than Int32.MinValue"));
 	  
 			// Returned Even-Rounded
-			return (int)(Math.Round (value));	  
+			checked {
+				return (int)(Math.Round (value));
+			}
 		}
  
 		public static int ToInt32 (float value) 
@@ -1218,7 +1220,9 @@ namespace System {
 	  
 			// Returned Even-Rounded, pass as a double, could just call
 			// Convert.ToInt32 ( (double)value);
-			return (int)(Math.Round ( (double)value));
+			checked {
+				return (int)(Math.Round ( (double)value));
+			}
 		}
 
 		public static int ToInt32 (int value) 
