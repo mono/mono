6635351ed8a4d4eb34eac71c3bdb954c96c250ee
Jb Evain <jbevain@gmail.com>
2010-04-15 16:12:45 +0000

    remove unused using
    svn path=/trunk/mcs/; revision=155514

--- mcs/class/System/System.Collections.Generic/SortedSet.cs
+++ mcs/class/System/System.Collections.Generic/SortedSet.cs
@@ -29,7 +29,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.InteropServices;
 using System.Security;
