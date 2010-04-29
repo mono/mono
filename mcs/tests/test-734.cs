7e50a4f70eb81fcd7487277c479bf42924262733
Marek Safar <marek.safar@gmail.com>
2010-04-29 11:12:47 +0000

    Test improved.
    svn path=/trunk/mcs/; revision=156415

--- mcs/tests/test-734.cs
+++ mcs/tests/test-734.cs
@@ -1,6 +1,7 @@
 // Compiler options: -r:test-734-lib.dll
 
 using System;
+using System.Reflection;
 
 class M : C
 {
@@ -13,6 +14,17 @@ class M : C
 	public static int Main ()
 	{
 		new M ().Run ();
+		
+		var body = typeof (M).GetMethod ("Run").GetMethodBody ();
+
+		// Check for volatile. (0xFE1E)
+		var array = body.GetILAsByteArray ();
+		if (array[2] != 0xFE)
+			return 1;
+
+		if (array[3] != 0x13)
+			return 1;
+		
 		return 0;
 	}
 }
