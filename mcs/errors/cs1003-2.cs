cb373a006e180e503ca4076d61b9dc6cff039f81
Miguel de Icaza <miguel@gnome.org>
2010-05-05 22:32:05 +0000

    2010-05-05  Miguel de Icaza  <miguel@novell.com>
    	* cs-parser.jay: Productions to catch common mistakes when other
    	punctuation operators are used instead of comma.   Fixes 571702	
    
    
    svn path=/trunk/mcs/; revision=156757

---
+++ mcs/errors/cs1003-2.cs
@@ -0,0 +1,12 @@
+// CS1003: Syntax error, `.' expected
+// Line: 9
+class X {
+    public int Field;
+
+    static void Main ()
+    {
+        var x = new X () {
+            Field = 1;
+        };
+    }
+}
