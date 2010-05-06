97b1ad63be130b4d2b63fa69fefe473402de4c76
Marek Safar <marek.safar@gmail.com>
2010-05-06 17:28:23 +0000

    New test.
    svn path=/trunk/mcs/; revision=156830

---
+++ mcs/tests/gtest-anon-61.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class TestCase
+{
+	static void Main ()
+	{
+		Test (new IList<int> [] { new int[] { 1, 2, 3 } });
+	}
+	
+	public static void Test<T> (IList<IList<T>> l)
+	{
+		Action action = delegate {
+			var temp = l;
+			Func<IList<IList<T>>, int> f = a => 1;
+			f (temp);
+		};
+		
+		action ();
+	}
+}
