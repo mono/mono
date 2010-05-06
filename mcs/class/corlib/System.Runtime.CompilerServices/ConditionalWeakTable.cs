be346a8bf5c34aa8444ae2cc39a383440cb5098a
Rodrigo Kumpera <kumpera@gmail.com>
2010-05-06 20:37:56 +0000

    2010-05-06 Rodrigo Kumpera  <rkumpera@novell.com>
    	* ConditionalWeakTable.cs: Discard old broken version. New
    	version user proper runtime support. Currently it performs
    	linear search over the keys, which is dog slow but it's a
    	good start.
    
    svn path=/trunk/mcs/; revision=156851

--- mcs/class/corlib/System.Runtime.CompilerServices/ConditionalWeakTable.cs
+++ mcs/class/corlib/System.Runtime.CompilerServices/ConditionalWeakTable.cs
@@ -1,128 +1,144 @@
-﻿// -----------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// -----------------------------------------------------------------------
 //
-// This code comes from the Managed Extension Frameworks:
-//  http://mef.codeplex.com
+// ConditionalWeakTable.cs
 //
-// And is licensed under the MS-PL license
+// Author:
+//   Rodrigo Kumpera (rkumpera@novell.com)
 //
-	// Glenn said on IRC:
-	//   "I think our table is weak, but does not do proper compacting"
-	//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #if NET_4_0 || BOOTSTRAP_NET_4_0 || MOONLIGHT
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace System.Runtime.CompilerServices
 {
+	internal struct Ephemeron
+	{
+		internal object key;
+		internal object value;
+	}
+
+	public sealed class ConditionalWeakTable<TKey, TValue> 
+		where TKey : class
+		where TValue : class
+	{
+		Ephemeron[] data;
+		object _lock = new object ();
+		int size;
+
+		public delegate TValue CreateValueCallback (TKey key);
+
+		public ConditionalWeakTable ()
+		{
+			data = new Ephemeron[16];
+			GC.register_ephemeron_array (data);
+		}
+
+		public void Add (TKey key, TValue value)
+		{
+			TValue tmp;
+			if (key == default (TKey))
+				throw new ArgumentNullException ("Null key", "key");
+
+			lock (_lock) {
+				if (TryGetValue (key, out tmp))
+					throw new ArgumentException ("Key already in the list", "key");
+			
+				if (size >= data.Length) {
+					Array.Resize (ref data, size * 2);
+					//Console.WriteLine ("--registering");
+					GC.register_ephemeron_array (data);
+				}
 	
-    // This is a broken implementation of ConditionalWeakTable that allows us
-    // to compile and work on versions of .Net eariler then 4.0. This class is
-    // broken when there are circular dependencies between keys and values, which
-    // can only be fixed by using some specific CLR 4.0 features.
-    // For code samples of the broken behavior see ConditionalWeakTableTests.cs.
-    public class ConditionalWeakTable<TKey, TValue> 
-        where TKey : class
-        where TValue : class
-    {
-        private readonly Dictionary<object, TValue> _table;
-        private int _capacity = 4;
+				data [size].key = key;
+				data [size].value = value;
+				++size;
+			}
+		}
 
-        public ConditionalWeakTable()
-        {
-            this._table = new Dictionary<object, TValue>();
-        }
+		public bool Remove (TKey key)
+		{
+			if (key == default (TKey))
+				throw new ArgumentNullException ("Null key", "key");
 
-        public void Add(TKey key, TValue value)
-        {
-            CleanupDeadReferences();
-            this._table.Add(CreateWeakKey(key), value);
-        }
+			lock (_lock) {
+				for (int i = 0; i < size; ++i) {
+					if (data [i].key == key) {
+						size--;
+						data [i].key = data [size].key;
+						data [i].value = data [size].value;
+	
+						data [size].key = data [size].value = null;
+						return true; 
+					}
+				}
+			}
+			return false;
+		}
 
-        public bool Remove(TKey key)
-        {
-            return this._table.Remove(key);
-        }
+		public bool TryGetValue (TKey key, out TValue value)
+		{
+			if (key == default (TKey))
+				throw new ArgumentNullException ("Null key", "key");
 
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            return this._table.TryGetValue(key, out value);
-        }
+			value = default (TValue);
+			lock (_lock) {
+				for (int i = 0; i < size; ++i) {
+					var k = data [i].key;
+					var v = data [i].value;
+					
+					if (key == k) {
+						value = (TValue)v;
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 
-        private void CleanupDeadReferences()
-        {
-            if (this._table.Count < _capacity)
-            {
-                return;
-            }
+		public TValue GetOrCreateValue (TKey key)
+		{
+			return GetValue (key, k => Activator.CreateInstance<TValue> ());
+		}
 
-	    ArrayList deadKeys = new ArrayList ();
-	    foreach (var weakRef in _table.Keys){
-		    if (!((EquivalentWeakReference)weakRef).IsAlive)
-			    deadKeys.Add (weakRef);
-	    }
+		public TValue GetValue (TKey key, CreateValueCallback createValueCallback)
+		{
+			if (key == default (TKey))
+				throw new ArgumentNullException ("Null key", "key");
+			if (createValueCallback == null)
+				throw new ArgumentNullException ("Null create delegate", "createValueCallback");
 
-            foreach (var deadKey in deadKeys)
-            {
-                this._table.Remove(deadKey);
-            }
+			TValue res;
 
-            if (this._table.Count >= _capacity)
-            {
-                _capacity *= 2;
-            }
-        }
+			lock (_lock) {
+				if (TryGetValue (key, out res))
+					return res;
+	
+				res = createValueCallback (key);
+				Add (key, res);
+			}
 
-        private static object CreateWeakKey(TKey key)
-        {
-            return new EquivalentWeakReference(key);
-        }
-
-        private class EquivalentWeakReference
-        {
-            private readonly WeakReference _weakReference;
-            private readonly int _hashCode;
-
-            public EquivalentWeakReference(object obj)
-            {
-                this._hashCode = obj.GetHashCode();
-                this._weakReference = new WeakReference(obj);
-            }
-
-            public bool IsAlive
-            {
-                get
-                {
-                    return this._weakReference.IsAlive;
-                }
-            }
-
-            public override bool Equals(object obj)
-            {
-                EquivalentWeakReference weakRef = obj as EquivalentWeakReference;
-
-                if (weakRef != null)
-                {
-                    obj = weakRef._weakReference.Target;
-                }
-
-                if (obj == null)
-                {
-                    return base.Equals(weakRef);
-                }
-                
-                return object.Equals(this._weakReference.Target, obj);
-            }
-
-            public override int GetHashCode()
-            {
-                return this._hashCode;
-            }
-        }
-    }
+			return res;
+		}
+	}
 }
 #endif
