3c7fd89d43ca0118c6104aec91c02becab6edd3d
Marek Habersack <grendel@twistedcode.net>
2010-05-07 23:03:49 +0000

    2010-05-06  Marek Habersack  <mhabersack@novell.com>
    	* PreApplicationStartMethodAttribute.cs: added. New 4.0 type.
    
    svn path=/trunk/mcs/; revision=156939

---
+++ mcs/class/System.Web/System.Web/PreApplicationStartMethodAttribute.cs
@@ -0,0 +1,45 @@
+//
+// PreApplicationStartMethodAttribute
+// 
+// Authors:
+//      Marek Habersack <mhabersack@novell.com>
+//
+//
+// Copyright (C) 2010 Novell, Inc (http://novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+
+namespace System.Web
+{
+	[AttributeUsage (AttributeTargets.Assembly, AllowMultiple = false)]
+	public sealed class PreApplicationStartMethodAttribute : Attribute
+	{
+		public string MethodName { get; private set; }
+		public Type Type { get; private set; }
+		
+		public PreApplicationStartMethodAttribute (Type type, string methodName)
+		{
+			this.MethodName = methodName;
+			this.Type = type;
+		}
+	}
+}
