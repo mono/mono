797d32aa61d9046b567206575153112fe0993e0c
Rodrigo Kumpera <kumpera@gmail.com>
2010-05-20 23:44:05 +0000

    In metadata: 2010-05-19 Rodrigo Kumpera  <rkumpera@novell.com>
    	* marshal.c (mono_marshal_init): Register mono_gchandle_get_target
    	icall.
    
    	* marshal.c (mono_delegate_to_ftnptr): Use a gchandle instead of
    	gc tracked memory.
    
    	* marshal.c (mono_marshal_emit_managed_wrapper): Retrieve the first
    	argument from a gchandle.
    
    	* marshal.c: Get rid of the delegate_target_locations hash table.
    
    	Fixes #605295.
    
    	Backport of r157589.
    
    
    svn path=/branches/mono-2-6/mono/; revision=157655

--- mono/metadata/marshal.h
+++ mono/metadata/marshal.h
@@ -203,7 +203,7 @@ MonoMethodSignature*
 mono_marshal_get_string_ctor_signature (MonoMethod *method) MONO_INTERNAL;
 
 MonoMethod *
-mono_marshal_get_managed_wrapper (MonoMethod *method, MonoClass *delegate_klass, MonoObject **this_loc) MONO_INTERNAL;
+mono_marshal_get_managed_wrapper (MonoMethod *method, MonoClass *delegate_klass, guint32 this_loc) MONO_INTERNAL;
 
 gpointer
 mono_marshal_get_vtfixup_ftnptr (MonoImage *image, guint32 token, guint16 type) MONO_INTERNAL;
@@ -468,7 +468,7 @@ void
 mono_marshal_emit_native_wrapper (MonoImage *image, MonoMethodBuilder *mb, MonoMethodSignature *sig, MonoMethodPInvoke *piinfo, MonoMarshalSpec **mspecs, gpointer func, gboolean aot, gboolean check_exceptions) MONO_INTERNAL;
 
 void
-mono_marshal_emit_managed_wrapper (MonoMethodBuilder *mb, MonoMethodSignature *invoke_sig, MonoMarshalSpec **mspecs, EmitMarshalContext* m, MonoMethod *method, MonoObject** this_loc) MONO_INTERNAL;
+mono_marshal_emit_managed_wrapper (MonoMethodBuilder *mb, MonoMethodSignature *invoke_sig, MonoMarshalSpec **mspecs, EmitMarshalContext* m, MonoMethod *method, guint32 target_handle) MONO_INTERNAL;
 
 GHashTable*
 mono_marshal_get_cache (GHashTable **var, GHashFunc hash_func, GCompareFunc equal_func) MONO_INTERNAL;
