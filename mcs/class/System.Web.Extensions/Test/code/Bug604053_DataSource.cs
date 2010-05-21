373971dc249362d7f314172d7f1ccaf352fb1a08
Atsushi Eno <atsushieno@gmail.com>
2010-05-21 07:27:00 +0000

    fix make dist.
    svn path=/branches/mono-2-6/mcs/; revision=157668

---
+++ mcs/class/System.Web.Extensions/Test/code/Bug604053_DataSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Bug604053.Prueba {
+	public class Data {
+		public int M1 { get; set; }
+		public string M2 { get; set; }
+		public Data(int m1, string m2) {
+			M1 = m1;
+			M2 = m2;
+		}
+	}
+	[DataObject(true)]
+	public class DataSource {
+		public Data[] Retrieve() {
+			Data[] data = new Data[10];
+			for(int i = 0; i < 10; i++) {
+				data[i] = new Data(i, i.ToString());
+			}
+			return data;
+		}
+	}
+}
