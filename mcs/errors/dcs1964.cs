ba4d3069b28cbffe12ae4324764f1814ebe31add
Marek Safar <marek.safar@gmail.com>
2010-06-18 14:16:34 +0000

    Updated.
    svn path=/trunk/mcs/; revision=159136

--- mcs/errors/dcs1964.cs
+++ mcs/errors/dcs1964.cs
@@ -1,4 +1,4 @@
-// CS1964: User-defined operator `C.implicit operator C(dynamic)' cannot convert to or from the dynamic type
+// CS1964: User-defined conversion `C.implicit operator C(dynamic)' cannot convert to or from the dynamic type
 // Line: 6
 
 class C
