6af4be168d3f9ab1c8544f61acb11a1bea32acc5
Jb Evain <jbevain@gmail.com>
2010-06-21 12:33:47 +0000

    2010-06-21  Jb Evain  <jbevain@novell.com>
    	* TypeAccessException.cs: TypeAccessException is part of moonlight 4.
    
    
    svn path=/trunk/mcs/; revision=159253

--- mcs/class/corlib/System/TypeAccessException.cs
+++ mcs/class/corlib/System/TypeAccessException.cs
@@ -25,7 +25,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-#if NET_4_0
+#if NET_4_0 || MOONLIGHT
 using System.Runtime.Serialization;
 using System.Runtime.InteropServices;
 
