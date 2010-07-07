e26ac7423dda96b144e1b94acf25102243ef4326
Veerapuram Varadhan <v.varadhan@gmail.com>
2010-07-07 07:07:30 +0000

    Ugh.. ugh.. final fix for handling MinValue for DateTime.
    svn path=/trunk/mcs/; revision=159988

--- mcs/class/Mono.Data.Tds/Mono.Data.Tds.Protocol/TdsComm.cs
+++ mcs/class/Mono.Data.Tds/Mono.Data.Tds.Protocol/TdsComm.cs
@@ -278,9 +278,9 @@ namespace Mono.Data.Tds.Protocol {
 			msecs = span.Milliseconds;
 			
 			if (epoch > t) {
-				// If t.Hour is > 0, days points to the next day and hence, 
+				// If t.Hour/Min/Sec/MSec is > 0, days points to the next day and hence, 
 				// we move it back by a day - otherwise, no change
-				days = t.Hour > 0 ? days-1: days;
+				days = (t.Hour > 0 || t.Minute > 0 || t.Second > 0 || t.Millisecond > 0) ? days-1: days;
 				hours = t.Hour;
 				minutes = t.Minute;
 				secs = t.Second;
