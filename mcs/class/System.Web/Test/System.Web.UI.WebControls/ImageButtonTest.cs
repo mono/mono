aa965a9bf36228b2cbf31695e09c79a04cb94ad5
Marek Habersack <grendel@twistedcode.net>
2010-07-08 13:09:56 +0000

    Another 4.0 test update
    svn path=/trunk/mcs/; revision=160052

--- mcs/class/System.Web/Test/System.Web.UI.WebControls/ImageButtonTest.cs
+++ mcs/class/System.Web/Test/System.Web.UI.WebControls/ImageButtonTest.cs
@@ -244,10 +244,15 @@ namespace MonoTests.System.Web.UI.WebControls
 		[Test]
 		public void Text ()
 		{
+#if NET_4_0
+			string origHtml = "<input type=\"image\" src=\"\" alt=\"MyText\" />";
+#else
+			string origHtml = "<input type=\"image\" src=\"\" alt=\"MyText\" style=\"border-width:0px;\" />";
+#endif
 			PokerImageButton b = new PokerImageButton ();
 			b.Text = "MyText";
 			string html = b.Render ();
-			HtmlDiff.AssertAreEqual ("<input type=\"image\" src=\"\" alt=\"MyText\" style=\"border-width:0px;\" />", html, "Text#1");
+			HtmlDiff.AssertAreEqual (origHtml, html, "Text#1");
 		}
 
 		[Test]
