5476e948bdb461212fae0452cf2b845188d7122d
Jb Evain <jbevain@gmail.com>
2010-07-08 10:35:27 +0000

    2010-07-08  Jb Evain  <jbevain@novell.com>
    	* Complex.cs: implement the different ToString overrides.
    
    
    svn path=/trunk/mcs/; revision=160043

--- mcs/class/System.Numerics/System.Numerics/Complex.cs
+++ mcs/class/System.Numerics/System.Numerics/Complex.cs
@@ -8,10 +8,25 @@
 //
 // Copyright 2009, 2010 Novell, Inc.
 //
-// TODO:
-// string ToString (string format, IFormatProvider)
-// string ToString (string format)
-// string ToString (IFormatProvider)
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
 
@@ -197,11 +212,6 @@ namespace System.Numerics {
 			return new Complex ((double) value, 0);
 		}
 
-		public override string ToString ()
-		{
-			return String.Format ("({0:G}, {1:G})", real, imaginary);
-		}
-
 		public static double Abs (Complex value)
 		{
 			return Math.Sqrt (value.imaginary * value.imaginary + value.real * value.real);
@@ -288,7 +298,7 @@ namespace System.Numerics {
 
 		public static Complex Log (Complex value, double baseValue)
 		{
-			return Log (value)/ Log (new Complex (baseValue, 0));
+			return Log (value) / Log (new Complex (baseValue, 0));
 		}
 
 		public static Complex Log10 (Complex value)
@@ -314,6 +324,26 @@ namespace System.Numerics {
 		public override int GetHashCode ()
 		{
 			return real.GetHashCode () ^ imaginary.GetHashCode ();
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("({0}, {1})", real, imaginary);
+		}
+
+		public string ToString (IFormatProvider provider)
+		{
+			return string.Format (provider, "({0}, {1})", real, imaginary);
+		}
+
+		public string ToString (string format)
+		{
+			return string.Format ("({0}, {1})", string.Format (format, real), string.Format (format, imaginary));
+		}
+
+		public string ToString (string format, IFormatProvider provider)
+		{
+			return string.Format ("({0}, {1})", string.Format (provider, format, real), string.Format (provider, format, imaginary));
 		}
 	}
 }
