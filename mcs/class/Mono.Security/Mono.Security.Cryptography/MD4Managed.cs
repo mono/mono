6be3c20a61d4c9937d4b9f1e95783a6203f9f9c7
Sebastien Pouliot <sebastien@ximian.com>
2010-07-16 15:41:59 +0000

    2010-07-16  Sebastien Pouliot  <sebastien@ximian.com>
    	* MD2Managed.cs:
    	* MD4Managed.cs:
    		Increment ibStart in HashCore
    
    
    svn path=/trunk/mcs/; revision=160490

--- mcs/class/Mono.Security/Mono.Security.Cryptography/MD4Managed.cs
+++ mcs/class/Mono.Security/Mono.Security.Cryptography/MD4Managed.cs
@@ -5,7 +5,7 @@
 //	Sebastien Pouliot (sebastien@ximian.com)
 //
 // (C) 2003 Motus Technologies Inc. (http://www.motus.com)
-// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2004-2005,2010 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -105,7 +105,7 @@ namespace Mono.Security.Cryptography {
 
 				for (i = partLen; i + 63 < cbSize; i += 64) {
 					// MD4Transform (context->state, &input[i]);
-					MD4Transform (state, array, i);
+					MD4Transform (state, array, ibStart + i);
 				}
 
 				index = 0;
