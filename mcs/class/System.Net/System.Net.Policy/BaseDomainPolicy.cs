755030134a34d25ce286b9edf02cd8092124d426
Sebastien Pouliot <sebastien@ximian.com>
2010-07-24 02:01:39 +0800

    Fix check order and throw NSE on invalid headers

--- mcs/class/System.Net/System.Net.Policy/BaseDomainPolicy.cs
+++ mcs/class/System.Net/System.Net.Policy/BaseDomainPolicy.cs
@@ -131,7 +131,7 @@ namespace System.Net.Policy {
 		abstract public bool IsAllowed (WebRequest request);
 
 		public Exception Exception {
-			get { return null; }
+			get; internal set;
 		}
 	}
 }
