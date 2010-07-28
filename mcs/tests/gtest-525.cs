491b93da3e94b99c26a67c45db8f7591a1019761
Marek Safar <marek.safar@gmail.com>
2010-07-28 09:44:23 +0100

    New tests

---
+++ mcs/tests/gtest-525.cs
@@ -0,0 +1,29 @@
+using System;
+
+class A
+{
+	static void MA<T> (string s)
+	{
+	}
+
+	static void F ()
+	{
+	}
+
+	class C
+	{
+		Func<int> MA;
+		int F;
+		
+		void Foo ()
+		{
+			F ();
+			MA<int> ("");
+		}
+		
+		public static void Main ()
+		{
+			new C ().Foo ();
+		}
+	}
+}
