bb57c7a7aba75d8a46d149efc0059a4c8d49fbca
Marek Safar <marek.safar@gmail.com>
2010-08-01 00:14:11 +0100

    Add my old sync script

---
+++ mcs/class/dlr/sync.sh
@@ -0,0 +1,2 @@
+rsync -r /home/marek/svn/dlr/svn/DLR_Main/Src/Runtime/Microsoft.Dynamic/ Runtime/Microsoft.Dynamic --exclude=".*/" --exclude="*.snk"
+rsync -r /home/marek/svn/dlr/svn/DLR_Main/Src/Runtime/Microsoft.Scripting.Core/ Runtime/Microsoft.Scripting.Core --exclude=".*/" --exclude="*.snk"
