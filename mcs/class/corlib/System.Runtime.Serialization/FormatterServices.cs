c606b22cd803f1ed898844e4e5dcc68baf9f5090
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2010-08-20 15:45:26 +0200

    Add 4.0 FormatterServices.IGetSurrogateForCyclicalReference.
    	* FormatterServices.cs: Add 4.0 GetSurrogateForCyclicalReference
    	method.

--- mcs/class/corlib/System.Runtime.Serialization/FormatterServices.cs
+++ mcs/class/corlib/System.Runtime.Serialization/FormatterServices.cs
@@ -33,6 +33,7 @@ using System;
 using System.Collections;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters;
 using System.Globalization;
 
@@ -206,5 +207,16 @@ namespace System.Runtime.Serialization
 			
 			return GetUninitializedObject (type);
 		}
+
+#if NET_4_0
+		// This method was introduced in .Net due to a bug serializing objects with circular references
+		// which we don't appear to have, so we just return the same object.
+		// See http://support.microsoft.com/kb/927495/en-us/ in case of doubt.
+		[ComVisible (false)]
+		public static ISerializationSurrogate GetSurrogateForCyclicalReference (ISerializationSurrogate innerSurrogate)
+		{
+			return innerSurrogate;
+		}
+#endif
 	}
 }
