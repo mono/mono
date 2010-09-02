4ea70039155b0a8e9b06adfa93cdbc3be792b93b
Jonathan Pryor <jonpryor@vt.edu>
2010-09-02 17:34:19 -0400

    [mdoc-update-ecma-xml] Assembly [CLSCompliant] shouldn't replace Type version.
    If a type has e.g. [CLSCompliant(false)], then the Assembly's
    [CLSCompliant(true)] shouldn't replace it.

--- mcs/tools/mdoc/Mono.Documentation/ecmadoc.cs
+++ mcs/tools/mdoc/Mono.Documentation/ecmadoc.cs
@@ -260,13 +260,15 @@ namespace Mono.Documentation
 			// TODO: assembly attributes?
 			// The problem is that .NET mscorlib.dll v4.0 has ~26 attributes, and
 			// importing these for every time seems like some serious bloat...
-			var clsAttr = assembly.Elements ("Attributes").Elements ("Attribute")
+			var clsDefAttr = assembly.Elements ("Attributes").Elements ("Attribute")
 				.FirstOrDefault (a => a.Element ("AttributeName").Value.StartsWith ("System.CLSCompliant"));
-			if (clsAttr != null) {
+			if (clsDefAttr != null &&
+					ai.Elements ("Attributes").Elements ("Attribute")
+					.FirstOrDefault (a => a.Element ("AttributeName").Value.StartsWith ("System.CLSCompliant")) == null) {
 				var dest = ai.Element ("Attributes");
 				if (dest == null)
 					ai.Add (dest = new XElement ("Attributes"));
-				dest.Add (clsAttr);
+				dest.Add (clsDefAttr);
 			}
 
 			return type;
