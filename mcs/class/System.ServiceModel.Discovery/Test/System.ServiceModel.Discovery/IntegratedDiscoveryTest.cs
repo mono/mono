270ab972e8cafc89b57fbb4e48db1edf1e2f0e0d
Atsushi Eno <atsushi@ximian.com>
2010-09-02 18:24:51 +0900

    Close AnnouncementService in the test cases. Assert that relevant ports become available.

--- mcs/class/System.ServiceModel.Discovery/Test/System.ServiceModel.Discovery/IntegratedDiscoveryTest.cs
+++ mcs/class/System.ServiceModel.Discovery/Test/System.ServiceModel.Discovery/IntegratedDiscoveryTest.cs
@@ -26,6 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Net.Sockets;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
@@ -39,10 +40,24 @@ namespace MonoTests.System.ServiceModel.Discovery
 	[TestFixture]
 	public class IntegratedDiscoveryTest
 	{
+		void AssertTcpPortOpen (int port)
+		{
+			try {
+				var l = new TcpListener (port);
+				l.Start ();
+				l.Stop ();
+			} catch (Exception ex) {
+				Assert.Fail (String.Format ("TCP port {0} is not freed", port));
+			}
+		}
+
 		[Test]
 		public void UseCase1 ()
 		{
-			RunCodeUnderDiscoveryHost (new Uri ("http://localhost:37564"), new Uri ("http://localhost:4949"), new Uri ("http://localhost:4989"), UseCase1Core);
+			RunCodeUnderDiscoveryHost1 (new Uri ("http://localhost:37564"), new Uri ("http://localhost:4949"), new Uri ("http://localhost:4989"), UseCase1Core);
+			AssertTcpPortOpen (4949);
+			AssertTcpPortOpen (4989);
+			AssertTcpPortOpen (37564);
 		}
 
 		void UseCase1Core (Uri serviceUri, AnnouncementEndpoint aEndpoint, DiscoveryEndpoint dEndpoint)
@@ -60,33 +75,19 @@ namespace MonoTests.System.ServiceModel.Discovery
 				TextWriter.Null.WriteLine ("Published Endpoint: " + edm.Address);
 			try {
 				// actual client, with DiscoveryClientBindingElement
-				var be = new DiscoveryClientBindingElement () { DiscoveryEndpointProvider = new ManagedDiscoveryEndpointProvider (dEndpoint) };
+				var be = new DiscoveryClientBindingElement () { DiscoveryEndpointProvider = new SimpleDiscoveryEndpointProvider (dEndpoint) };
 				var clientBinding = new CustomBinding (new BasicHttpBinding ());
 				clientBinding.Elements.Insert (0, be);
 				var cf = new ChannelFactory<ITestService> (clientBinding, DiscoveryClientBindingElement.DiscoveryEndpointAddress);
 				var ch = cf.CreateChannel ();
 				Assert.AreEqual ("TEST", ch.Echo ("TEST"), "#1");
+				cf.Close ();
 			} finally {
 				host.Close ();
 			}
 		}
 
-		class ManagedDiscoveryEndpointProvider : DiscoveryEndpointProvider
-		{
-			public ManagedDiscoveryEndpointProvider (DiscoveryEndpoint endpoint)
-			{
-				this.endpoint = endpoint;
-			}
-			
-			DiscoveryEndpoint endpoint;
-			
-			public override DiscoveryEndpoint GetDiscoveryEndpoint ()
-			{
-				return endpoint;
-			}
-		}
-
-		void RunCodeUnderDiscoveryHost (Uri serviceUri, Uri dHostUri, Uri aHostUri, Action<Uri,AnnouncementEndpoint,DiscoveryEndpoint> action)
+		void RunCodeUnderDiscoveryHost1 (Uri serviceUri, Uri dHostUri, Uri aHostUri, Action<Uri,AnnouncementEndpoint,DiscoveryEndpoint> action)
 		{
 			// announcement service
 			var abinding = new CustomBinding (new HttpTransportBindingElement ());
@@ -101,13 +102,89 @@ namespace MonoTests.System.ServiceModel.Discovery
 			dEndpoint.IsSystemEndpoint = false;
 
 			// it internally hosts an AnnouncementService
-			var host = new ServiceHost (new AnnouncementBoundDiscoveryService (aEndpoint));
-			host.AddServiceEndpoint (dEndpoint);
+			using (var inst = new AnnouncementBoundDiscoveryService (aEndpoint)) {
+				var host = new ServiceHost (inst);
+				host.AddServiceEndpoint (dEndpoint);
+				try {
+					host.Open ();
+					action (serviceUri, aEndpoint, dEndpoint);
+				} finally {
+					host.Close ();
+				}
+			}
+		}
+
+		[Test] // Announcement: UDP, Discovery: HTTP
+		public void UseCase2 ()
+		{
+			RunCodeUnderDiscoveryHost2 (new Uri ("http://localhost:37564"), new Uri ("http://localhost:4949"), UseCase2Core);
+			AssertTcpPortOpen (4949);
+			AssertTcpPortOpen (37564);
+		}
+
+		void UseCase2Core (Uri serviceUri, AnnouncementEndpoint aEndpoint, DiscoveryEndpoint dEndpoint)
+		{
+			// actual service, announcing to UDP
+			var host = new ServiceHost (typeof (TestService));
+			var sdb = new ServiceDiscoveryBehavior ();
+			sdb.AnnouncementEndpoints.Add (aEndpoint);
+			host.Description.Behaviors.Add (sdb);
+			host.AddServiceEndpoint (typeof (ITestService), new BasicHttpBinding (), serviceUri);
+			host.Open ();
+			// It does not start announcement very soon, so wait for a while.
+			Thread.Sleep (1000);
+			foreach (var edm in host.Extensions.Find<DiscoveryServiceExtension> ().PublishedEndpoints)
+				TextWriter.Null.WriteLine ("Published Endpoint: " + edm.Address);
 			try {
-				host.Open ();
-				action (serviceUri, aEndpoint, dEndpoint);
+				// actual client, with DiscoveryClientBindingElement
+				var be = new DiscoveryClientBindingElement () { DiscoveryEndpointProvider = new SimpleDiscoveryEndpointProvider (dEndpoint) };
+				var clientBinding = new CustomBinding (new BasicHttpBinding ());
+				clientBinding.Elements.Insert (0, be);
+				var cf = new ChannelFactory<ITestService> (clientBinding, DiscoveryClientBindingElement.DiscoveryEndpointAddress);
+				var ch = cf.CreateChannel ();
+				Assert.AreEqual ("TEST", ch.Echo ("TEST"), "#1");
 			} finally {
 				host.Close ();
+			}
+		}
+
+		void RunCodeUnderDiscoveryHost2 (Uri serviceUri, Uri dHostUri, Action<Uri,AnnouncementEndpoint,DiscoveryEndpoint> action)
+		{
+			// announcement service
+			var aEndpoint = new UdpAnnouncementEndpoint (new Uri ("soap.udp://239.255.255.250:3802/"));
+			
+			// discovery service
+			var dbinding = new CustomBinding (new HttpTransportBindingElement ());
+			var dAddress = new EndpointAddress (dHostUri);
+			var dEndpoint = new DiscoveryEndpoint (dbinding, dAddress);
+			// Without this, .NET rejects the host as if it had no service.
+			dEndpoint.IsSystemEndpoint = false;
+
+			// it internally hosts an AnnouncementService
+			using (var inst = new AnnouncementBoundDiscoveryService (aEndpoint)) {
+				var host = new ServiceHost (inst);
+				host.AddServiceEndpoint (dEndpoint);
+				try {
+					host.Open ();
+					action (serviceUri, aEndpoint, dEndpoint);
+				} finally {
+					host.Close ();
+				}
+			}
+		}
+
+		class SimpleDiscoveryEndpointProvider : DiscoveryEndpointProvider
+		{
+			public SimpleDiscoveryEndpointProvider (DiscoveryEndpoint endpoint)
+			{
+				this.endpoint = endpoint;
+			}
+			
+			DiscoveryEndpoint endpoint;
+			
+			public override DiscoveryEndpoint GetDiscoveryEndpoint ()
+			{
+				return endpoint;
 			}
 		}
 	}
