mono/mono

commit 2ec0ab59fe4aecf5507a81846a66de1a658bbfd2

Delegate.CreateDelegate: add support for delegates closed over null for instance methods

mcs/class/corlib/System/Delegate.cs
@@ -184,13 +184,17 @@ namespace System
 					// target is passed as the first argument to the static method
 					argLengthMatch = (args.Length == delargs.Length + 1);
 			} else {
-				if (!method.IsStatic)
+				if (!method.IsStatic) {
 					//
 					// Net 2.0 feature. The first argument of the delegate is passed
 					// as the 'this' argument to the method.
 					//
 					argLengthMatch = (args.Length + 1 == delargs.Length);
-				else {
+
+					if (!argLengthMatch)
+						// closed over a null reference
+						argLengthMatch = (args.Length == delargs.Length);
+				} else {
 					argLengthMatch = (args.Length == delargs.Length);
 
 					if (!argLengthMatch)
@@ -217,10 +221,17 @@ namespace System
 				}
 			} else {
 				if (!method.IsStatic) {
-					// The first argument should match this
-					argsMatch = arg_type_match (delargs [0].ParameterType, method.DeclaringType);
-					for (int i = 0; i < args.Length; i++)
-						argsMatch &= arg_type_match (delargs [i + 1].ParameterType, args [i].ParameterType);
+					if (args.Length + 1 == delargs.Length) {
+						// The first argument should match this
+						argsMatch = arg_type_match (delargs [0].ParameterType, method.DeclaringType);
+						for (int i = 0; i < args.Length; i++)
+							argsMatch &= arg_type_match (delargs [i + 1].ParameterType, args [i].ParameterType);
+					} else {
+						// closed over a null reference
+						argsMatch = true;
+						for (int i = 0; i < args.Length; i++)
+							argsMatch &= arg_type_match (delargs [i].ParameterType, args [i].ParameterType);
+					}
 				} else {
 					if (delargs.Length + 1 == args.Length) {
 						// closed over a null reference

commit 85c0976c3d913ba16a13d44159f8e7591732df56

[Fix] fix regression in Delegate.CreateDelegate
2ec0ab59fe4aecf5507a81846a66de1a658bbfd2 caused a regression in
Create.Delegate where the overload which doesn't take a firstArgument
could be used to create closed over null delegates while this
overload doesn't allow it.

mcs/class/corlib/System/Delegate.cs
@@ -150,6 +150,11 @@ namespace System
 
 		public static Delegate CreateDelegate (Type type, object firstArgument, MethodInfo method, bool throwOnBindFailure)
 		{
+			return CreateDelegate (type, firstArgument, method, throwOnBindFailure, true);
+		}
+
+		public static Delegate CreateDelegate (Type type, object firstArgument, MethodInfo method, bool throwOnBindFailure, bool allowClosed)
+		{
 			// The name of the parameter changed in 2.0
 			object target = firstArgument;
 
@@ -228,7 +233,7 @@ namespace System
 							argsMatch &= arg_type_match (delargs [i + 1].ParameterType, args [i].ParameterType);
 					} else {
 						// closed over a null reference
-						argsMatch = true;
+						argsMatch = allowClosed;
 						for (int i = 0; i < args.Length; i++)
 							argsMatch &= arg_type_match (delargs [i].ParameterType, args [i].ParameterType);
 					}
@@ -258,22 +263,24 @@ namespace System
 			return d;
 		}
 
-		public static Delegate CreateDelegate (Type type, object firstArgument, MethodInfo method) {
-			return CreateDelegate (type, firstArgument, method, true);
+		public static Delegate CreateDelegate (Type type, object firstArgument, MethodInfo method)
+		{
+			return CreateDelegate (type, firstArgument, method, true, true);
 		}
 
 		public static Delegate CreateDelegate (Type type, MethodInfo method, bool throwOnBindFailure)
 		{
-			return CreateDelegate (type, null, method, throwOnBindFailure);
+			return CreateDelegate (type, null, method, throwOnBindFailure, false);
 		}
 
-		public static Delegate CreateDelegate (Type type, MethodInfo method) {
+		public static Delegate CreateDelegate (Type type, MethodInfo method)
+		{
 			return CreateDelegate (type, method, true);
 		}
 
 		public static Delegate CreateDelegate (Type type, object target, string method)
 		{
-			return CreateDelegate(type, target, method, false);
+			return CreateDelegate (type, target, method, false);
 		}
 
 		static MethodInfo GetCandidateMethod (Type type, Type target, string method, BindingFlags bflags, bool ignoreCase, bool throwOnBindFailure)