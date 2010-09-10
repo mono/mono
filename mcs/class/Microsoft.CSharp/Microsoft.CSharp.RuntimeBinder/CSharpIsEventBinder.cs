0e419dbf3509887d9a21f11abd276acff44b111a
Marek Safar <marek.safar@gmail.com>
2010-09-10 16:43:20 +0100

    Update to the latest dmcs

--- mcs/class/Microsoft.CSharp/Microsoft.CSharp.RuntimeBinder/CSharpIsEventBinder.cs
+++ mcs/class/Microsoft.CSharp/Microsoft.CSharp.RuntimeBinder/CSharpIsEventBinder.cs
@@ -52,7 +52,8 @@ namespace Microsoft.CSharp.RuntimeBinder
 			var queried_type = ctx.ImportType (target.LimitType);
 			var rc = new Compiler.ResolveContext (new RuntimeBinderContext (ctx, context_type), 0);
 
-			var expr = Compiler.Expression.MemberLookup (rc, context_type, queried_type, name, 0, false, Compiler.Location.Null);
+			var expr = Compiler.Expression.MemberLookup (rc, context_type, queried_type,
+				name, 0, Compiler.Expression.MemberLookupRestrictions.ExactArity, Compiler.Location.Null);
 
 			var binder = new CSharpBinder (
 				this, new Compiler.BoolConstant (expr is Compiler.EventExpr, Compiler.Location.Null), null);
