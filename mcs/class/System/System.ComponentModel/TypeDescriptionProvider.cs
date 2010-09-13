b45d1da49df85793585afa8b48fbc9ac4e8e0af5
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2010-09-13 12:43:52 +0200

    Remove the NET_2_0 directive from TypeDescriptionProvider.cs
    	* TypeDescriptionProvider.cs: Remove the NET_2_0 directive.

--- mcs/class/System/System.ComponentModel/TypeDescriptionProvider.cs
+++ mcs/class/System/System.ComponentModel/TypeDescriptionProvider.cs
@@ -27,7 +27,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if NET_2_0
 
 using System;
 using System.Collections;
@@ -147,4 +146,3 @@ namespace System.ComponentModel
 
 }
 
-#endif
