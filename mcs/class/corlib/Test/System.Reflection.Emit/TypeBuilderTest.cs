8f1e86cc79e89ac7c62ed59c9f9bb9723ac909b9
Rodrigo Kumpera <kumpera@gmail.com>
2010-10-22 17:57:00 -0200

    Checks the constructor declaring type.
    	* TypeBuilder.cs (GetConstructor): Check if the ctor
    	declaring type is the right one and the same as type.
    
    	* TypeBuilderTest.cs: Add regression test.
    
    	Fixes #648391

--- mcs/class/corlib/Test/System.Reflection.Emit/TypeBuilderTest.cs
+++ mcs/class/corlib/Test/System.Reflection.Emit/TypeBuilderTest.cs
@@ -10911,5 +10911,21 @@ namespace MonoTests.System.Reflection.Emit
 			
 			Activator.CreateInstance(tb2.CreateType());
 		}
+
+		[Test] //Test for #648391
+		public void GetConstructorCheckCtorDeclaringType ()
+		{
+			TypeBuilder myType = module.DefineType ("Sample", TypeAttributes.Public);
+			string[] typeParamNames = { "TFirst" };
+			GenericTypeParameterBuilder[] typeParams = myType.DefineGenericParameters (typeParamNames);
+			var ctor = myType.DefineDefaultConstructor (MethodAttributes.Public);
+			var ctori = TypeBuilder.GetConstructor (myType.MakeGenericType (typeof (int)), ctor);
+			try {
+				TypeBuilder.GetConstructor (myType.MakeGenericType (typeof (bool)), ctori);
+				Assert.Fail ("#1");
+			} catch (ArgumentException) {
+				//OK
+			}
+		}
 	}
 }
