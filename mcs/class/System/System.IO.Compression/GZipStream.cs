f4f515fd0e9ac7f06f4835e1d410c023614cdbb2
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-10-24 18:44:33 -0400

    Throw exceptions here instead of in DeflateStream
    	To get the same stack trace as in bug #647948, we need to set the
    	underlying DeflateStream to null and throw the ObjectDisposedException
    	from this class. Same applies to the NotSupportedException.

--- mcs/class/System/System.IO.Compression/GZipStream.cs
+++ mcs/class/System/System.IO.Compression/GZipStream.cs
@@ -4,8 +4,9 @@
 //
 // Authors:
 //	Christopher James Lahey <clahey@ximian.com>
+//	Gonzalo Paniagua Javier <gonzalo@novell.com>
 //
-// Copyright (C) 2004-2005 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2004-2010 Novell, Inc (http://www.novell.com)
 //
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -50,89 +51,129 @@ namespace System.IO.Compression {
 
 		protected override void Dispose (bool disposing)
 		{
-			if (disposing)
-				deflateStream.Dispose ();
+			if (disposing) {
+				if (deflateStream != null) {
+					deflateStream.Dispose ();
+					deflateStream = null;
+				}
+			}
 			base.Dispose (disposing);
 		}
 
 		public override int Read (byte[] dest, int dest_offset, int count)
 		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			return deflateStream.Read(dest, dest_offset, count);
 		}
 
 
 		public override void Write (byte[] src, int src_offset, int count)
 		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			deflateStream.Write (src, src_offset, count);
 		}
 
-		public override void Flush() {
+		public override void Flush()
+		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			deflateStream.Flush();
 		}
 
-		public override long Seek (long offset, SeekOrigin origin) {
-			return deflateStream.Seek (offset, origin);
+		public override long Seek (long offset, SeekOrigin origin)
+		{
+			throw new NotSupportedException();
 		}
 
-		public override void SetLength (long value) {
-			deflateStream.SetLength (value);
+		public override void SetLength (long value)
+		{
+			throw new NotSupportedException();
 		}
 
 		public override IAsyncResult BeginRead (byte [] buffer, int offset, int count,
 							AsyncCallback cback, object state)
 		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			return deflateStream.BeginRead (buffer, offset, count, cback, state);
 		}
 
 		public override IAsyncResult BeginWrite (byte [] buffer, int offset, int count,
 							AsyncCallback cback, object state)
 		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			return deflateStream.BeginWrite (buffer, offset, count, cback, state);
 		}
 
 		public override int EndRead(IAsyncResult async_result) {
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			return deflateStream.EndRead (async_result);
 		}
 
 		public override void EndWrite (IAsyncResult async_result)
 		{
+			if (deflateStream == null)
+				throw new ObjectDisposedException (GetType ().FullName);
+
 			deflateStream.EndWrite (async_result);
 		}
 
 		public Stream BaseStream {
 			get {
+				if (deflateStream == null)
+					return null;
+
 				return deflateStream.BaseStream;
 			}
 		}
+
 		public override bool CanRead {
 			get {
+				if (deflateStream == null)
+					return false;
+
 				return deflateStream.CanRead;
 			}
 		}
+
 		public override bool CanSeek {
 			get {
+				if (deflateStream == null)
+					return false;
+
 				return deflateStream.CanSeek;
 			}
 		}
+
 		public override bool CanWrite {
 			get {
+				if (deflateStream == null)
+					return false;
+
 				return deflateStream.CanWrite;
 			}
 		}
+
 		public override long Length {
-			get {
-				return deflateStream.Length;
-			}
+			get { throw new NotSupportedException(); }
 		}
+
 		public override long Position {
-			get {
-				return deflateStream.Position;
-			}
-			set {
-				deflateStream.Position = value;
-			}
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
 		}
 	}
 }
 
 #endif
+
