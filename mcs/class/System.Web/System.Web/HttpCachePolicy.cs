mono/mono

commit ea730c5de2e555fb8ee5c5d906ad41a602f55848

[asp.net] Fix for bug #648407. HttpCachePolicy must use the value passed to AppendCacheExtension when generating the Cache-Control header

mcs/class/System.Web/System.Web/HttpCachePolicy.cs
@@ -353,7 +353,12 @@ namespace System.Web
 				cc = String.Concat (cc, ", no-store");
 			if (set_no_transform)
 				cc = String.Concat (cc, ", no-transform");
-
+			if (cache_extension != null && cache_extension.Length > 0) {
+				if (!Strng.IsNullOrEmpty (cc))
+					cc = String.Concat (cc, ", ");
+				cc = String.Concat (cc, cache_extension.ToString ());
+			}
+			
 			headers.Add ("Cache-Control", cc);
 
 			if (last_modified_from_file_dependencies || etag_from_file_dependencies)

commit 4b528bc0ed196f1285ec0891559a0c53587bf7ca

[asp.net] Fix a build-breaking typo introduced in ea730c5de2e555fb8ee5c5d906ad41a602f55848

mcs/class/System.Web/System.Web/HttpCachePolicy.cs
@@ -354,7 +354,7 @@ namespace System.Web
 			if (set_no_transform)
 				cc = String.Concat (cc, ", no-transform");
 			if (cache_extension != null && cache_extension.Length > 0) {
-				if (!Strng.IsNullOrEmpty (cc))
+				if (!String.IsNullOrEmpty (cc))
 					cc = String.Concat (cc, ", ");
 				cc = String.Concat (cc, cache_extension.ToString ());
 			}