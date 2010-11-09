1bd648c6156f4f99f1e5bf250b3c06d0b0b88f27
Jérémie Laval <jeremie.laval@gmail.com>
2010-11-09 13:41:53 +0000

    Use ulong to represent element hashes internally allowing full width 32bits hashes. Replace SpinLock+ManualResetEvent based bucket resize with a simpler reader-writer lock. Revert from two layers storage to a bare array storage for buckets temporarily as it causes a weird concurrency bug.

--- mcs/class/corlib/System.Collections.Concurrent/SplitOrderedList.cs
+++ mcs/class/corlib/System.Collections.Concurrent/SplitOrderedList.cs
@@ -34,63 +34,50 @@ namespace System.Collections.Concurrent
 {
 	internal class SplitOrderedList<T>
 	{
-		static readonly byte[] reverseTable = {
-			0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48, 176, 112, 240, 8, 136, 72, 200, 40, 168, 104, 232, 24, 152, 88, 216, 56, 184, 120, 248, 4, 132, 68, 196, 36, 164, 100, 228, 20, 148, 84, 212, 52, 180, 116, 244, 12, 140, 76, 204, 44, 172, 108, 236, 28, 156, 92, 220, 60, 188, 124, 252, 2, 130, 66, 194, 34, 162, 98, 226, 18, 146, 82, 210, 50, 178, 114, 242, 10, 138, 74, 202, 42, 170, 106, 234, 26, 154, 90, 218, 58, 186, 122, 250, 6, 134, 70, 198, 38, 166, 102, 230, 22, 150, 86, 214, 54, 182, 118, 246, 14, 142, 78, 206, 46, 174, 110, 238, 30, 158, 94, 222, 62, 190, 126, 254, 1, 129, 65, 193, 33, 161, 97, 225, 17, 145, 81, 209, 49, 177, 113, 241, 9, 137, 73, 201, 41, 169, 105, 233, 25, 153, 89, 217, 57, 185, 121, 249, 5, 133, 69, 197, 37, 165, 101, 229, 21, 149, 85, 213, 53, 181, 117, 245, 13, 141, 77, 205, 45, 173, 109, 237, 29, 157, 93, 221, 61, 189, 125, 253, 3, 131, 67, 195, 35, 163, 99, 227, 19, 147, 83, 211, 51, 179, 115, 243, 11, 139, 75, 203, 43, 171, 107, 235, 27, 155, 91, 219, 59, 187, 123, 251, 7, 135, 71, 199, 39, 167, 103, 231, 23, 151, 87, 215, 55, 183, 119, 247, 15, 143, 79, 207, 47, 175, 111, 239, 31, 159, 95, 223, 63, 191, 127, 255
-		};
-
-		static readonly byte[] logTable = {
-			0xFF, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
-		};
-
-		class Node 
+		class Node
 		{
 			public readonly bool Marked;
-			public uint Key;
+			public readonly ulong Key;
 			public T Data;
 			
 			public Node Next;
 			
-			public Node (uint key, T data)
-				: this (false)
+			public Node (ulong key, T data)
 			{
 				this.Key = key;
 				this.Data = data;
 			}
 
-			protected Node (bool marked)
+			// Used to create marked node
+			public Node (Node wrapped)
 			{
-				this.Marked = marked;
-			}
-		}
-		
-		class MarkedNode : Node
-		{
-			public MarkedNode (Node wrapped) : base (true)
-			{
-				Next = wrapped;
+				this.Marked = true;
+				this.Next = wrapped;
 			}
 		}
 
 		const int MaxLoad = 5;
-		const int SegmentSize = 50;
+		/*const uint SegmentSize = 1u << SegmentShift;
+		const int SegmentShift = 5;*/
+		const uint BucketSize = 512;
 
-		[ThreadStatic]
-		Node[] segmentCache;
+		/*[ThreadStatic]
+		Node[] segmentCache;*/
 
 		Node head;
 		Node tail;
-		
-		Node[][] buckets = new Node[10][];
-		int count;
-		int size = 1;
 
-		ManualResetEventSlim mres = new ManualResetEventSlim (true);
-		SpinLock mresLock = new SpinLock ();
+		//Node[][] buckets = new Node[BucketSize][];
+		Node[] buckets = new Node [BucketSize];
+		int count;
+		int size = 2;
+
+		SimpleRwLock slim = new SimpleRwLock ();
 
 		public SplitOrderedList ()
 		{
 			head = new Node (0, default (T));
-			tail = new Node (uint.MaxValue, default (T));
+			tail = new Node (ulong.MaxValue, default (T));
 			head.Next = tail;
 			SetBucket (0, head);
 		}
@@ -139,15 +126,18 @@ namespace System.Collections.Concurrent
 		bool InsertInternal (uint key, T data, Func<T> dataCreator, out Node current)
 		{
 			Node node = new Node (ComputeRegularKey (key), data);
+
 			uint b = key % (uint)size;
-			
-			if (GetBucket (b) == null)
-				InitializeBucket (b);
-			if (!ListInsert (node, GetBucket (b), out current, dataCreator))
+			Node bucket;
+
+			if ((bucket = GetBucket (b)) == null)
+				bucket = InitializeBucket (b);
+
+			if (!ListInsert (node, bucket, out current, dataCreator))
 				return false;
 
 			int csize = size;
-			if (Interlocked.Increment (ref count) / csize > MaxLoad)
+			if (Interlocked.Increment (ref count) / csize > MaxLoad && (csize & 0x40000000) == 0)
 				Interlocked.CompareExchange (ref size, 2 * csize, csize);
 
 			current = node;
@@ -160,11 +150,12 @@ namespace System.Collections.Concurrent
 			Node node;
 			uint b = key % (uint)size;
 			data = default (T);
+			Node bucket;
 
-			if (GetBucket (b) == null)
-				InitializeBucket (b);
+			if ((bucket = GetBucket (b)) == null)
+				bucket = InitializeBucket (b);
 
-			if (!ListFind (ComputeRegularKey (key), GetBucket (b), out node))
+			if (!ListFind (ComputeRegularKey (key), bucket, out node))
 				return false;
 
 			data = node.Data;
@@ -176,11 +167,12 @@ namespace System.Collections.Concurrent
 		{
 			Node node;
 			uint b = key % (uint)size;
+			Node bucket;
 
-			if (GetBucket (b) == null)
-				InitializeBucket (b);
+			if ((bucket = GetBucket (b)) == null)
+				bucket = InitializeBucket (b);
 
-			if (!ListFind (ComputeRegularKey (key), GetBucket (b), out node))
+			if (!ListFind (ComputeRegularKey (key), bucket, out node))
 				return false;
 
 			if (!check (node.Data))
@@ -194,10 +186,12 @@ namespace System.Collections.Concurrent
 		public bool Delete (uint key, out T data)
 		{
 			uint b = key % (uint)size;
-			if (GetBucket (b) == null)
-				InitializeBucket (b);
+			Node bucket;
 
-			if (!ListDelete (GetBucket (b), ComputeRegularKey (key), out data))
+			if ((bucket = GetBucket (b)) == null)
+				bucket = InitializeBucket (b);
+
+			if (!ListDelete (bucket, ComputeRegularKey (key), out data))
 				return false;
 
 			Interlocked.Decrement (ref count);
@@ -219,22 +213,24 @@ namespace System.Collections.Concurrent
 			}
 		}
 
-		void InitializeBucket (uint b)
+		Node InitializeBucket (uint b)
 		{
 			Node current;
 			uint parent = GetParent (b);
-			if (GetBucket (parent) == null)
-				InitializeBucket ((uint)parent);
+			Node bucket;
+
+			if ((bucket = GetBucket (parent)) == null)
+				bucket = InitializeBucket (parent);
 
 			Node dummy = new Node (ComputeDummyKey (b), default (T));
-			if (!ListInsert (dummy, GetBucket (parent), out current, null))
-				dummy = current;
+			if (!ListInsert (dummy, bucket, out current, null))
+				return current;
 
-			SetBucket (b, dummy);
+			return SetBucket (b, dummy);
 		}
 		
 		// Turn v's MSB off
-		uint GetParent (uint v)
+		static uint GetParent (uint v)
 		{
 			uint t, tt;
 			
@@ -247,84 +243,86 @@ namespace System.Collections.Concurrent
 		}
 
 		// Reverse integer bits and make sure LSB is set
-		uint ComputeRegularKey (uint key)
+		static ulong ComputeRegularKey (uint key)
 		{
-			return ComputeDummyKey (key | 0x80000000);
+			return ComputeDummyKey (key) | 1;
 		}
 		
 		// Reverse integer bits
-		uint ComputeDummyKey (uint key)
+		static ulong ComputeDummyKey (uint key)
 		{
-			return ((uint)reverseTable[key & 0xff] << 24) | 
-				((uint)reverseTable[(key >> 8) & 0xff] << 16) | 
-				((uint)reverseTable[(key >> 16) & 0xff] << 8) |
-				((uint)reverseTable[(key >> 24) & 0xff]);
+			return ((ulong)(((uint)reverseTable[key & 0xff] << 24) |
+			                ((uint)reverseTable[(key >> 8) & 0xff] << 16) |
+			                ((uint)reverseTable[(key >> 16) & 0xff] << 8) |
+			                ((uint)reverseTable[(key >> 24) & 0xff]))) << 1;
 		}
 
 		// Bucket storage is abstracted in a simple two-layer tree to avoid too much memory resize
 		Node GetBucket (uint index)
 		{
-			int segment = (int)(index / SegmentSize);
-			CheckSegment (segment);
+			/*uint segment = index >> SegmentShift;
+
 			if (buckets[segment] == null)
 				return null;
 
-			return buckets[segment][index % SegmentSize];
+			return buckets[segment][index & (SegmentSize - 1)];*/
+			if (index >= buckets.Length)
+				return null;
+			return buckets[index];
 		}
 
-		void SetBucket (uint index, Node node)
+		Node SetBucket (uint index, Node node)
 		{
-			int segment = (int)(index / SegmentSize);
-			CheckSegment (segment);
-			if (buckets[segment] == null) {
-				// Cache segment creation in case CAS fails
-				Node[] newSegment = segmentCache == null ? new Node[SegmentSize] : segmentCache;
-				segmentCache = Interlocked.CompareExchange (ref buckets[segment], newSegment, null) == null ? null : newSegment;
+			//uint segment = index >> SegmentShift;
+			try {
+				slim.EnterReadLock ();
+				CheckSegment (index, true);
+
+				/*if (buckets[segment] == null) {
+					// Cache segment creation in case CAS fails
+					Node[] newSegment = segmentCache == null ? new Node[SegmentSize] : segmentCache;
+					segmentCache = Interlocked.CompareExchange (ref buckets[segment], newSegment, null) == null ? null : newSegment;
+				}
+				return buckets[segment][index & (SegmentSize - 1)] = node;*/
+
+				Interlocked.CompareExchange (ref buckets[index], node, null);
+				return buckets[index];
+			} finally {
+				slim.ExitReadLock ();
 			}
-			buckets[segment][index % SegmentSize] = node;
 		}
 
 		// When we run out of space for bucket storage, we use a lock-based array resize
-		void CheckSegment (int segment)
+		void CheckSegment (uint segment, bool readLockTaken)
 		{
-			while (segment >= buckets.Length) {
-				bool shouldResize = false;
-				bool taken = false;
-				try {
-					mresLock.Enter (ref taken);
-					if (mres.IsSet) {
-						shouldResize = true;
-						mres.Reset ();
-					}
-				} finally {
-					if (taken)
-						mresLock.Exit ();
-				}
+			if (segment < buckets.Length)
+				return;
 
-				if (shouldResize) {
+			if (readLockTaken)
+				slim.ExitReadLock ();
+			try {
+				slim.EnterWriteLock ();
+				while (segment >= buckets.Length)
 					Array.Resize (ref buckets, buckets.Length * 2);
-					mres.Set ();
-				} else {
-					mres.Wait ();
-				}
+			} finally {
+				slim.ExitWriteLock ();
 			}
+			if (readLockTaken)
+				slim.EnterReadLock ();
 		}
-		
-		Node ListSearch (uint key, ref Node left, Node h)
+
+		Node ListSearch (ulong key, ref Node left, Node h)
 		{
 			Node leftNodeNext = null, rightNode = null;
-			
-		search_again:
+
 			do {
 				Node t = h;
-				Node tNext = h.Next;
-				
+				Node tNext = t.Next;
 				do {
 					if (!tNext.Marked) {
 						left = t;
 						leftNodeNext = tNext;
 					}
-					
 					t = tNext.Marked ? tNext.Next : tNext;
 					if (t == tail)
 						break;
@@ -336,21 +334,21 @@ namespace System.Collections.Concurrent
 				
 				if (leftNodeNext == rightNode) {
 					if (rightNode != tail && rightNode.Next.Marked)
-						goto search_again;
+						continue;
 					else 
 						return rightNode;
 				}
 				
 				if (Interlocked.CompareExchange (ref left.Next, rightNode, leftNodeNext) == leftNodeNext) {
 					if (rightNode != tail && rightNode.Next.Marked)
-						goto search_again;
+						continue;
 					else
 						return rightNode;
 				}
 			} while (true);
 		}
 
-		bool ListDelete (Node startPoint, uint key, out T data)
+		bool ListDelete (Node startPoint, ulong key, out T data)
 		{
 			Node rightNode = null, rightNodeNext = null, leftNode = null;
 			data = default (T);
@@ -364,7 +362,7 @@ namespace System.Collections.Concurrent
 				
 				rightNodeNext = rightNode.Next;
 				if (!rightNodeNext.Marked)
-					if (Interlocked.CompareExchange (ref rightNode.Next, new MarkedNode (rightNodeNext), rightNodeNext) == rightNodeNext)
+					if (Interlocked.CompareExchange (ref rightNode.Next, new Node (rightNodeNext), rightNodeNext) == rightNodeNext)
 						break;
 			} while (true);
 			
@@ -376,7 +374,7 @@ namespace System.Collections.Concurrent
 		
 		bool ListInsert (Node newNode, Node startPoint, out Node current, Func<T> dataCreator)
 		{
-			uint key = newNode.Key;
+			ulong key = newNode.Key;
 			Node rightNode = null, leftNode = null;
 			
 			do {
@@ -392,7 +390,7 @@ namespace System.Collections.Concurrent
 			} while (true);
 		}
 		
-		bool ListFind (uint key, Node startPoint, out Node data)
+		bool ListFind (ulong key, Node startPoint, out Node data)
 		{
 			Node rightNode = null, leftNode = null;
 			data = null;
@@ -401,6 +399,66 @@ namespace System.Collections.Concurrent
 			data = rightNode;
 			
 			return rightNode != tail && rightNode.Key == key;
+		}
+
+		static readonly byte[] reverseTable = {
+			0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48, 176, 112, 240, 8, 136, 72, 200, 40, 168, 104, 232, 24, 152, 88, 216, 56, 184, 120, 248, 4, 132, 68, 196, 36, 164, 100, 228, 20, 148, 84, 212, 52, 180, 116, 244, 12, 140, 76, 204, 44, 172, 108, 236, 28, 156, 92, 220, 60, 188, 124, 252, 2, 130, 66, 194, 34, 162, 98, 226, 18, 146, 82, 210, 50, 178, 114, 242, 10, 138, 74, 202, 42, 170, 106, 234, 26, 154, 90, 218, 58, 186, 122, 250, 6, 134, 70, 198, 38, 166, 102, 230, 22, 150, 86, 214, 54, 182, 118, 246, 14, 142, 78, 206, 46, 174, 110, 238, 30, 158, 94, 222, 62, 190, 126, 254, 1, 129, 65, 193, 33, 161, 97, 225, 17, 145, 81, 209, 49, 177, 113, 241, 9, 137, 73, 201, 41, 169, 105, 233, 25, 153, 89, 217, 57, 185, 121, 249, 5, 133, 69, 197, 37, 165, 101, 229, 21, 149, 85, 213, 53, 181, 117, 245, 13, 141, 77, 205, 45, 173, 109, 237, 29, 157, 93, 221, 61, 189, 125, 253, 3, 131, 67, 195, 35, 163, 99, 227, 19, 147, 83, 211, 51, 179, 115, 243, 11, 139, 75, 203, 43, 171, 107, 235, 27, 155, 91, 219, 59, 187, 123, 251, 7, 135, 71, 199, 39, 167, 103, 231, 23, 151, 87, 215, 55, 183, 119, 247, 15, 143, 79, 207, 47, 175, 111, 239, 31, 159, 95, 223, 63, 191, 127, 255
+		};
+
+		static readonly byte[] logTable = {
+			0xFF, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
+		};
+
+		struct SimpleRwLock
+		{
+			const int RwWait = 1;
+			const int RwWrite = 2;
+			const int RwRead = 4;
+
+			int rwlock;
+
+			public void EnterReadLock ()
+			{
+				SpinWait sw = new SpinWait ();
+				do {
+					while ((rwlock & (RwWrite | RwWait)) > 0)
+						sw.SpinOnce ();
+
+					if ((Interlocked.Add (ref rwlock, RwRead) & (RwWait | RwWait)) == 0)
+						return;
+
+					Interlocked.Add (ref rwlock, -RwRead);
+				} while (true);
+			}
+
+			public void ExitReadLock ()
+			{
+				Interlocked.Add (ref rwlock, -RwRead);
+			}
+
+			public void EnterWriteLock ()
+			{
+				SpinWait sw = new SpinWait ();
+				do {
+					int state = rwlock;
+					if (state < RwWrite) {
+						if (Interlocked.CompareExchange (ref rwlock, RwWrite, state) == state)
+							return;
+						state = rwlock;
+					}
+					// We register our interest in taking the Write lock (if upgradeable it's already done)
+					while ((state & RwWait) == 0 && Interlocked.CompareExchange (ref rwlock, state | RwWait, state) != state)
+						state = rwlock;
+					// Before falling to sleep
+					while (rwlock > RwWait)
+						sw.SpinOnce ();
+				} while (true);
+			}
+
+			public void ExitWriteLock ()
+			{
+				Interlocked.Add (ref rwlock, -RwWrite);
+			}
 		}
 	}
 }
