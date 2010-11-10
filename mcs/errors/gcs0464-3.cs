e152071d2a0061d7aa6d7f40b220b78f43132aa2
Marek Safar <marek.safar@gmail.com>
2010-11-10 17:52:43 +0000

    New test.

---
+++ mcs/errors/gcs0464-3.cs
@@ -0,0 +1,11 @@
+// CS0464: The result of comparing type `int?' with null is always `false'
+// Line: 9
+// Compiler options: -warnaserror -warn:2
+
+public class X
+{
+	public static bool Compute ()
+	{
+		return null >= null;
+	}
+}
