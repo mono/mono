37a947e74ed65d58d14ab37a52ba9ec04465ee79
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-11-20 16:46:07 -0500

    [HttpListener] Use RWL instead of lock()
    	Most of the time we just want to read the prefixes instead of
    	adding/removing one.

--- mcs/class/System/System.Net/EndPointListener.cs
+++ mcs/class/System/System.Net/EndPointListener.cs
@@ -33,6 +33,7 @@ using System.Net.Sockets;
 using System.Collections;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using Mono.Security.Authenticode;
 
 namespace System.Net {
@@ -40,6 +41,7 @@ namespace System.Net {
 	{
 		IPEndPoint endpoint;
 		Socket sock;
+		ReaderWriterLock plock;
 		Hashtable prefixes;  // Dictionary <ListenerPrefix, HttpListener>
 		ArrayList unhandled; // List<ListenerPrefix> unhandled; host = '*'
 		ArrayList all;       // List<ListenerPrefix> all;  host = '+'
@@ -63,6 +65,7 @@ namespace System.Net {
 			args.Completed += OnAccept;
 			sock.AcceptAsync (args);
 			prefixes = new Hashtable ();
+			plock = new ReaderWriterLock ();
 		}
 
 		void LoadCertificateAndKey (IPAddress addr, int port)
@@ -159,7 +162,8 @@ namespace System.Net {
 			HttpListener best_match = null;
 			int best_length = -1;
 
-			lock (prefixes) {
+			try {
+				plock.AcquireReaderLock (-1);
 				if (host != null && host != "") {
 					foreach (ListenerPrefix p in prefixes.Keys) {
 						string ppath = p.Path;
@@ -183,6 +187,10 @@ namespace System.Net {
 				best_match = MatchFromList (host, path, all, out prefix);
 				if (best_match != null)
 					return best_match;
+			} finally {
+				try {
+					plock.ReleaseReaderLock ();
+				} catch {}
 			}
 			return null;
 		}
@@ -216,12 +224,19 @@ namespace System.Net {
 			if (coll == null)
 				return;
 
-			foreach (ListenerPrefix p in coll) {
-				if (p.Path == prefix.Path) //TODO: code
-					throw new HttpListenerException (400, "Prefix already in use.");
+			try {
+				plock.AcquireReaderLock (-1);
+				foreach (ListenerPrefix p in coll) {
+					if (p.Path == prefix.Path) //TODO: code
+						throw new HttpListenerException (400, "Prefix already in use.");
+				}
+				plock.UpgradeToWriterLock (-1);
+				coll.Add (prefix);
+			} finally {
+				try {
+					plock.ReleaseReaderLock (); // This releases the writer lock if held.
+				} catch { }
 			}
-
-			coll.Add (prefix);
 		}
 
 		void RemoveSpecial (ArrayList coll, ListenerPrefix prefix)
@@ -229,17 +244,26 @@ namespace System.Net {
 			if (coll == null)
 				return;
 
-			int c = coll.Count;
-			for (int i = 0; i < c; i++) {
-				ListenerPrefix p = (ListenerPrefix) coll [i];
-				if (p.Path == prefix.Path) {
-					coll.RemoveAt (i);
-					CheckIfRemove ();
-					return;
+			try {
+				plock.AcquireReaderLock (-1);
+				int c = coll.Count;
+				for (int i = 0; i < c; i++) {
+					ListenerPrefix p = (ListenerPrefix) coll [i];
+					if (p.Path == prefix.Path) {
+						plock.UpgradeToWriterLock (-1);
+						coll.RemoveAt (i);
+						CheckIfRemove ();
+						return;
+					}
 				}
+			} finally {
+				try {
+					plock.ReleaseReaderLock (); // Releases the writer lock if held
+				} catch {}
 			}
 		}
 
+		// Writer lock held when calling (could use just reader)
 		void CheckIfRemove ()
 		{
 			if (prefixes.Count > 0)
@@ -261,52 +285,63 @@ namespace System.Net {
 
 		public void AddPrefix (ListenerPrefix prefix, HttpListener listener)
 		{
-			lock (prefixes) {
-				if (prefix.Host == "*") {
-					if (unhandled == null)
-						unhandled = new ArrayList ();
+			if (prefix.Host == "*") {
+				if (unhandled == null)
+					unhandled = new ArrayList ();
 
-					prefix.Listener = listener;
-					AddSpecial (unhandled, prefix);
-					return;
-				}
+				prefix.Listener = listener;
+				AddSpecial (unhandled, prefix);
+				return;
+			}
 
-				if (prefix.Host == "+") {
-					if (all == null)
-						all = new ArrayList ();
-					prefix.Listener = listener;
-					AddSpecial (all, prefix);
-					return;
-				}
+			if (prefix.Host == "+") {
+				if (all == null)
+					all = new ArrayList ();
+				prefix.Listener = listener;
+				AddSpecial (all, prefix);
+				return;
+			}
 
+			try { 
+				plock.AcquireReaderLock (-1);
 				if (prefixes.ContainsKey (prefix)) {
 					HttpListener other = (HttpListener) prefixes [prefix];
 					if (other != listener) // TODO: code.
 						throw new HttpListenerException (400, "There's another listener for " + prefix);
 					return;
 				}
-
+				plock.AcquireWriterLock (-1);
 				prefixes [prefix] = listener;
+			} finally {
+				try {
+					plock.ReleaseReaderLock ();
+				} catch {}
 			}
 		}
 
 		public void RemovePrefix (ListenerPrefix prefix, HttpListener listener)
 		{
-			lock (prefixes) {
-				if (prefix.Host == "*") {
-					RemoveSpecial (unhandled, prefix);
-					return;
-				}
+			if (prefix.Host == "*") {
+				RemoveSpecial (unhandled, prefix);
+				return;
+			}
 
-				if (prefix.Host == "+") {
-					RemoveSpecial (all, prefix);
-					return;
-				}
+			if (prefix.Host == "+") {
+				RemoveSpecial (all, prefix);
+				return;
+			}
 
+			try {
+				plock.AcquireReaderLock (-1);
 				if (prefixes.ContainsKey (prefix)) {
+					plock.UpgradeToWriterLock (-1);
 					prefixes.Remove (prefix);
 					CheckIfRemove ();
 				}
+			} finally {
+				try {
+					plock.ReleaseReaderLock ();
+				} catch {}
 			}
 		}
 	}
