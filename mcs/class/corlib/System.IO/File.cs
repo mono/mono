4cb8b2e6f326aa7a43a2ec1cff6daf7e99177ba3
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-11-21 15:12:45 -0500

    [File.ReadLines] Dispose the StreamReader in the right place.
    	The StreamReader was being disposed immediately after the call to
    	ReadLines() causing the enumerator to fails.
    
    	Fixes bug #649464.

--- mcs/class/corlib/System.IO/File.cs
+++ mcs/class/corlib/System.IO/File.cs
@@ -612,24 +612,23 @@ namespace System.IO
 #if MOONLIGHT || NET_4_0
 		public static IEnumerable<string> ReadLines (string path)
 		{
-			using (StreamReader reader = File.OpenText (path)) {
-				return ReadLines (reader);
-			}
+			return ReadLines (File.OpenText (path));
 		}
 
 		public static IEnumerable<string> ReadLines (string path, Encoding encoding)
 		{
-			using (StreamReader reader = new StreamReader (path, encoding)) {
-				return ReadLines (reader);
-			}
+			return ReadLines (new StreamReader (path, encoding));
 		}
 
 		// refactored in order to avoid compiler-generated names for Moonlight tools
 		static IEnumerable<string> ReadLines (StreamReader reader)
 		{
-			string s;
-			while ((s = reader.ReadLine ()) != null)
-				yield return s;
+			using (reader) {
+				string s;
+				while ((s = reader.ReadLine ()) != null) {
+					yield return s;
+				}
+			}
 		}
 
 		public static void AppendAllLines (string path, IEnumerable<string> contents)
