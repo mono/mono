01e525697bfa0228b0e071c6bf49758a73174fec
Marek Safar <marek.safar@gmail.com>
2010-11-24 20:25:34 +0000

    Recover from broken for statement

---
+++ mcs/errors/cs1525-29.cs
@@ -0,0 +1,13 @@
+// CS1525: Unexpected symbol `}'
+// Line: 10
+
+namespace TestProject
+{
+    class MainClass
+    {
+        public static void Main ()
+        {
+            for (int i = 0;
+        }
+    }
+} 
