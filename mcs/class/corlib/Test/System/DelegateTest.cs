5ca6fa9e2230fbeb10d350b53a7152cf03df28c4
Rodrigo Kumpera <kumpera@gmail.com>
2010-11-25 20:22:56 -0200

    Add regression test for #655741

--- mcs/class/corlib/Test/System/DelegateTest.cs
+++ mcs/class/corlib/Test/System/DelegateTest.cs
@@ -1142,6 +1142,50 @@ namespace MonoTests.System
 			Assert.AreEqual (hc1, hc2);
 		}
 
+		public interface CreateDelegateIFoo {
+			int Test2 ();
+		}
+		
+		public abstract class CreateDelegateFoo {
+			public abstract int Test ();
+		}
+		
+		public class CreateDelegateMid : CreateDelegateFoo {
+			public override int Test () {
+				return 1;
+			}
+		}
+		
+		public class CreateDelegateBar : CreateDelegateMid, CreateDelegateIFoo {
+			public override int Test () {
+				return 2;
+			}
+		
+			public int Test2 () {
+				return 3;
+			}
+		}
+	
+		delegate int IntNoArgs ();
+
+		[Test]
+		public void CreateDelegateWithAbstractMethods ()
+		{
+			var f = new CreateDelegateBar ();
+			var m = typeof (CreateDelegateFoo).GetMethod ("Test");
+			var m2 = typeof (CreateDelegateMid).GetMethod ("Test");
+			var m3 = typeof (CreateDelegateIFoo).GetMethod ("Test2");
+	
+			IntNoArgs a1 = (IntNoArgs)Delegate.CreateDelegate (typeof (IntNoArgs), f, m);
+			IntNoArgs a2 = (IntNoArgs)Delegate.CreateDelegate (typeof (IntNoArgs), f, m2);
+			IntNoArgs a3 = (IntNoArgs)Delegate.CreateDelegate (typeof (IntNoArgs), f, m3);
+
+			Assert.AreEqual (2, a1 (), "#1");
+			Assert.AreEqual (2, a2 (), "#2");
+			Assert.AreEqual (3, a3 (), "#3");
+		}
+		
+
 		delegate string FooDelegate (Iface iface, string s);
 
 		delegate string FooDelegate2 (B b, string s);
