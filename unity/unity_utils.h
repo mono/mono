a532d53e132b5e603d427a3687a3810620cd3e1a
Levi Bard <levi@unity3d.com>
2010-12-09 16:20:58 +0100

    Silence compiler warnings.

--- unity/unity_utils.h
+++ unity/unity_utils.h
@@ -17,9 +17,9 @@ void unity_mono_redirect_output( const char *fout, const char *ferr );
 /**
  *	Closes redirected output files.
  */
-void unity_mono_close_output();
+void unity_mono_close_output(void);
 
-extern MonoString* mono_unity_get_embeddinghostname();
+extern MonoString* mono_unity_get_embeddinghostname(void);
 
 void mono_unity_write_to_unity_log(MonoString* str);
 
@@ -27,7 +27,7 @@ void mono_unity_write_to_unity_log(MonoString* str);
 FILE* unity_fopen( const char *name, const char *mode );
 #endif
 
-extern gboolean mono_unity_socket_security_enabled_get ();
+extern gboolean mono_unity_socket_security_enabled_get (void);
 extern void mono_unity_socket_security_enabled_set (gboolean enabled);
 
 #endif
