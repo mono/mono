45f1c6f8722ded71e14682b9af3fd6f4c517d143
Marek Safar <marek.safar@gmail.com>
2010-12-10 10:40:10 +0000

    Removed compiler generated attributes

--- mcs/class/corlib/Assembly/AssemblyInfo.cs
+++ mcs/class/corlib/Assembly/AssemblyInfo.cs
@@ -41,15 +41,15 @@ using System.Runtime.InteropServices;
 
 // General Information about the mscorlib assembly
 
-	[assembly: AssemblyTitle ("mscorlib.dll")]
-	[assembly: AssemblyDescription ("mscorlib.dll")]
-	[assembly: AssemblyDefaultAlias ("mscorlib.dll")]
+[assembly: AssemblyTitle ("mscorlib.dll")]
+[assembly: AssemblyDescription ("mscorlib.dll")]
+[assembly: AssemblyDefaultAlias ("mscorlib.dll")]
 
-	[assembly: AssemblyCompany (Consts.MonoCompany)]
-	[assembly: AssemblyProduct (Consts.MonoProduct)]
-	[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
 
-	[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
 
 [assembly: AssemblyVersion (Consts.FxVersion)]
 [assembly: SatelliteContractVersion (Consts.FxVersion)]
@@ -57,27 +57,23 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage ("en-US")]
 
 [assembly: CLSCompliant (true)]
-#if !NET_2_1
-[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-[assembly: AllowPartiallyTrustedCallers]
-#endif
 
-[assembly: SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
 [assembly: AssemblyDelaySign (true)]
 #if NET_2_1
 	[assembly: AssemblyKeyFile ("../silverlight.pub")]
 #else
 	[assembly: AssemblyKeyFile ("../ecma.pub")]
+	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+	[assembly: AllowPartiallyTrustedCallers]
 #endif
 
-	[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
-	[assembly: ComVisible (false)]
-	[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
-	[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-	[assembly: TypeLibVersion (2, 0)]
-	[assembly: RuntimeCompatibility (WrapNonExceptionThrows = true)]
-	[assembly: DefaultDependency (LoadHint.Always)]
-	[assembly: StringFreezing]
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: ComVisible (false)]
+[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: TypeLibVersion (2, 0)]
+[assembly: DefaultDependency (LoadHint.Always)]
+[assembly: StringFreezing]
 
 #if MOONLIGHT
 	[assembly: InternalsVisibleTo ("System, PublicKey=00240000048000009400000006020000002400005253413100040000010001008D56C76F9E8649383049F383C44BE0EC204181822A6C31CF5EB7EF486944D032188EA1D3920763712CCB12D75FB77E9811149E6148E5D32FBAAB37611C1878DDC19E20EF135D0CB2CFF2BFEC3D115810C3D9069638FE4BE215DBF795861920E5AB6F7DB2E2CEEF136AC23D5DD2BF031700AEC232F6C6B1C785B4305C123B37AB")]
