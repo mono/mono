eabd497033f7f51358af5261b9e08da225342a8d
Atsushi Eno <atsushi@ximian.com>
2010-12-14 13:50:43 +0900

    add missing method in IAmbientProvider.

--- mcs/class/System.Xaml/System.Xaml/IAmbientProvider.cs
+++ mcs/class/System.Xaml/System.Xaml/IAmbientProvider.cs
@@ -30,6 +30,7 @@ namespace System.Xaml
 	{
 		IEnumerable<Object> GetAllAmbientValues (params XamlType[] types);
 		IEnumerable<AmbientPropertyValue> GetAllAmbientValues (IEnumerable<XamlType> ceilingTypes, params XamlMember [] properties);
+		IEnumerable<AmbientPropertyValue> GetAllAmbientValues (IEnumerable<XamlType> ceilingTypes, bool searchLiveStackOnly, IEnumerable<XamlType> types, params XamlMember[] properties);
 		Object GetFirstAmbientValue (params XamlType [] types);
 		AmbientPropertyValue GetFirstAmbientValue (IEnumerable<XamlType> ceilingTypes, params XamlMember [] properties);
 	}
