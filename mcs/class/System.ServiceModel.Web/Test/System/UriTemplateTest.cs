4beb7ab7059551b7dd0bf17c81cf11b06e67c38e
Marek Habersack <grendel@twistedcode.net>
2011-01-05 15:04:36 +0100

    [wcf] Remove two tests which are invalid for 2.8

--- mcs/class/System.ServiceModel.Web/Test/System/UriTemplateTest.cs
+++ mcs/class/System.ServiceModel.Web/Test/System/UriTemplateTest.cs
@@ -216,28 +216,6 @@ namespace MonoTests.System
 			var u = t.BindByName (new Uri ("http://localhost/"), n);
 			Assert.AreEqual ("http://localhost/value2/value1/", u.ToString ());
 		}
-
-		[Test]
-		public void BindByNameManySlashes ()
-		{
-			var t = new UriTemplate ("////{foo}/{bar}/");
-			var n = new NameValueCollection ();
-			n.Add ("Bar", "value1"); // case insensitive
-			n.Add ("FOO", "value2"); // case insensitive
-			var u = t.BindByName (new Uri ("http://localhost/"), n);
-			Assert.AreEqual ("http://localhost////value2/value1/", u.ToString ());
-		}
-
-		[Test]
-		public void BindByNameManySlashes2 ()
-		{
-			var t = new UriTemplate ("////{foo}/{bar}/");
-			var n = new NameValueCollection ();
-			n.Add ("Bar", "value1"); // case insensitive
-			n.Add ("FOO", "value2"); // case insensitive
-			var u = t.BindByName (new Uri ("http://localhost//"), n);
-			Assert.AreEqual ("http://localhost/////value2/value1/", u.ToString ());
-		}
 		
 		[Test]
 		public void BindByNameWithDefaults ()
