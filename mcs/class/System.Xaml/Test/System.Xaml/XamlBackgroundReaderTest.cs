2e2337e80a8db4bc045900b8bc18cf79754cddf6
Atsushi Eno <atsushi@ximian.com>
2011-01-12 16:49:38 +0900

    Implement XamlBackgroundReader.

---
+++ mcs/class/System.Xaml/Test/System.Xaml/XamlBackgroundReaderTest.cs
@@ -0,0 +1,110 @@
+//
+// Copyright (C) 2010 Novell Inc. http://novell.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Markup;
+using System.Xaml;
+using System.Xaml.Schema;
+using System.Xml;
+using NUnit.Framework;
+
+using CategoryAttribute = NUnit.Framework.CategoryAttribute;
+
+namespace MonoTests.System.Xaml
+{
+	[TestFixture]
+	public partial class XamlBackgroundReaderTest : XamlReaderTestBase
+	{
+		XamlReader GetReader (string filename)
+		{
+			return new XamlXmlReader (XmlReader.Create (Path.Combine ("Test/XmlFiles", filename), new XmlReaderSettings () { CloseInput =true }));
+		}
+
+		void ReadTest (string filename)
+		{
+			var r = new XamlBackgroundReader (GetReader (filename));
+			r.StartThread ();
+			while (!r.IsEof)
+				r.Read ();
+		}
+
+		[Test]
+		public void Read_Int32 ()
+		{
+			ReadTest ("Int32.xml");
+		}
+
+		[Test]
+		public void Read_DateTime ()
+		{
+			ReadTest ("DateTime.xml");
+		}
+
+		[Test]
+		public void Read_TimeSpan ()
+		{
+			ReadTest ("TimeSpan.xml");
+		}
+
+		[Test]
+		public void Read_ArrayInt32 ()
+		{
+			ReadTest ("Array_Int32.xml");
+		}
+
+		[Test]
+		public void Read_DictionaryInt32String ()
+		{
+			ReadTest ("Dictionary_Int32_String.xml");
+		}
+
+		[Test]
+		public void Read_DictionaryStringType ()
+		{
+			ReadTest ("Dictionary_String_Type.xml");
+		}
+
+		[Test]
+		public void Read_SilverlightApp1 ()
+		{
+			ReadTest ("SilverlightApp1.xaml");
+		}
+
+		[Test]
+		public void Read_Guid ()
+		{
+			ReadTest ("Guid.xml");
+		}
+
+		[Test]
+		public void Read_GuidFactoryMethod ()
+		{
+			ReadTest ("GuidFactoryMethod.xml");
+		}
+	}
+}
