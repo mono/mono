327d01dc9756568dfac373b54b86688b99d11534
Marek Safar <marek.safar@gmail.com>
2011-01-12 15:26:15 +0000

    Test update

--- mcs/errors/cs0012-11.cs
+++ mcs/errors/cs0012-11.cs
@@ -1,6 +1,6 @@
-// CS0012: The type `I1' is defined in an assembly that is not referenced. Consider adding a reference to assembly `CS0012-lib-missing, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'
-// Line: 10
-// Compiler options: -r:CS0012-5-lib.dll
+// CS0012: The type `A.Nested' is defined in an assembly that is not referenced. Consider adding a reference to assembly `CS0012-lib-missing, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'
+// Line: 9
+// Compiler options: -r:CS0012-11-lib.dll
 
 class Test
 {
