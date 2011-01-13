61f6558eae0fabda211339839419f22562980b02
Marek Habersack <grendel@twistedcode.net>
2011-01-13 22:44:57 +0100

    [asp.net] Added the Microsoft.Web.Infrastructure assembly

---
+++ mcs/class/Microsoft.Web.Infrastructure/Assembly/AssemblyInfo.cs
@@ -0,0 +1,54 @@
+﻿// 
+// AssemblyInfo.cs
+//  
+// Author:
+//       Marek Habersack <grendel@twistedcode.net>
+// 
+// Copyright (c) 2011 Novell, Inc (http://novell.com/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+
+[assembly: AssemblyProduct ("ASP.Net WebPages")]
+[assembly: AssemblyCompany ("Novell, Inc")]
+[assembly: AssemblyCopyright ("© Novell, Inc. All rights reserved.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: RuntimeCompatibility (WrapNonExceptionThrows=true)]
+[assembly: AssemblyTrademark ("")]
+[assembly: NeutralResourcesLanguage ("en-US")]
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: CLSCompliant (true)]
+[assembly: ComVisible (false)]
+[assembly: AssemblyFileVersion ("1.0.20105.407")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: TargetFramework (".NETFramework,Version=v4.0", FrameworkDisplayName=".NET Framework 4")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: CompilationRelaxations (8)]
+[assembly: AssemblyTitle ("Microsoft.Web.Infrastructure")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDelaySign (true)]
+[assembly: AssemblyKeyFile("../winfx.pub")]
---
+++ mcs/class/Microsoft.Web.Infrastructure/Microsoft.Web.Infrastructure/HttpContextHelper.cs
@@ -0,0 +1,44 @@
+﻿// 
+// HttpContextHelper.cs
+//  
+// Author:
+//       Marek Habersack <grendel@twistedcode.net>
+// 
+// Copyright (c) 2011 Novell, Inc (http://novell.com/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Microsoft.Web.Infrastructure;
+using System;
+using System.ComponentModel;
+using System.Security;
+using System.Web;
+
+namespace Microsoft.Web.Infrastructure
+{
+	[EditorBrowsable (EditorBrowsableState.Never)]
+	public static class HttpContextHelper
+	{
+		[SecuritySafeCritical]
+		public static void ExecuteInNullContext (Action action)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
