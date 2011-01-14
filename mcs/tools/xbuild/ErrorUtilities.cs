2fb5ad4bba4b60364e38868d3b2fff958796c3ff
Ankit Jain <radical@corewars.org>
2011-01-14 14:50:51 +0530

    [xbuild] Update Novell copyright.

--- mcs/tools/xbuild/ErrorUtilities.cs
+++ mcs/tools/xbuild/ErrorUtilities.cs
@@ -35,7 +35,7 @@ namespace Mono.XBuild.CommandLine {
 		static string[] version = {
 			String.Format ("XBuild Engine Version {0}", Consts.MonoVersion),
 			String.Format ("Mono, Version {0}", Consts.MonoVersion),
-			"Copyright (C) Marek Sieradzki 2005-2008, Novell 2008-2009.",
+			"Copyright (C) Marek Sieradzki 2005-2008, Novell 2008-2011.",
 		};
 
 		
