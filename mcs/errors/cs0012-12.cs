01ca29caf8676dac37fe24016ef24debb2c19cef
Marek Safar <marek.safar@gmail.com>
2011-01-18 19:24:48 +0000

    Update mcs error to test against mcs compiler

--- mcs/errors/cs0012-12.cs
+++ mcs/errors/cs0012-12.cs
@@ -1,4 +1,4 @@
-// CS0012: The type `Lib2.Class1`1<Lib2.Class2>' is defined in an assembly that is not referenced. Consider adding a reference to assembly `cs0012-12-missing, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'
+// CS0012: The type `Lib2.Class1`1<Lib2.Class2>' is defined in an assembly that is not referenced. Consider adding a reference to assembly `CS0012-lib-missing, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'
 // Line: 5
 // Compiler options: -r:CS0012-12-lib.dll
 
