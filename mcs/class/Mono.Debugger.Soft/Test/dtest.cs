626711d3ac62a1b8f451ede724c312d033645f4c
Zoltan Varga <vargaz@gmail.com>
2011-01-20 02:13:35 +0100

    Add a TID field to ThreadMirror which returns the system thread id.

--- mcs/class/Mono.Debugger.Soft/Test/dtest.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -1999,6 +1999,8 @@ public class DebuggerTests
 
 		Assert.IsTrue (e.Thread.ThreadId > 0);
 
+		Assert.AreEqual (e.Thread.TID, e.Thread.TID);
+
 		vm.EnableEvents (EventType.ThreadStart, EventType.ThreadDeath);
 
 		vm.Resume ();
