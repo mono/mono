788535030b0372c375ccf38c091c2b473154dbaa
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2011-01-20 21:27:30 -0500

    [System.Web] Razor works now (modulo DB)

--- mcs/class/System.Web/System.Web.Compilation/BuildManager.cs
+++ mcs/class/System.Web/System.Web.Compilation/BuildManager.cs
@@ -628,8 +628,8 @@ namespace System.Web.Compilation
 		public static Type GetGlobalAsaxType ()
 		{
 			Type ret = HttpApplicationFactory.AppType;
-			if (!preStartMethodsRunning)
-				throw new InvalidOperationException ("This method cannot be called during the application's pre-start initialization stage.");
+			if (ret == null)
+				return typeof (HttpApplication);
 			
 			return ret;
 		}
