8c3c6c4b543fba61d58dc065be43ef34ab49ea05
Geoff Norton <grompf@sublimeintervention.com>
2011-01-25 14:16:11 -0500

    Fix the mobile build

--- mcs/class/System.Core/System/Actions.cs
+++ mcs/class/System.Core/System/Actions.cs
@@ -29,7 +29,7 @@
 using System;
 using System.Runtime.CompilerServices;
 
-#if NET_4_0
+#if NET_4_0 || MOBILE
 [assembly:TypeForwardedTo (typeof(Action<>))]
 [assembly:TypeForwardedTo (typeof(Action<,>))]
 [assembly:TypeForwardedTo (typeof(Action<,,>))]
@@ -41,7 +41,7 @@ using System.Runtime.CompilerServices;
 
 namespace System
 {
-#if NET_4_0
+#if NET_4_0 || MOBILE
 	public delegate void Action<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9> (
 		T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9);
 
