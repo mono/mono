efae6c06019bc6f17656a75637791fb2a703b0c2
Lucas Meijer <lucas@unity3d.com>
2011-02-01 19:51:50 +0100

    Implement signal chaining for windows. Useful for letting custom crash reporters be able to deal with native crashes, without mono eating the native exception while checking to see if it was caused by a managed null dereference in jitted code or not.

--- mono/mini/mini-windows.c
+++ mono/mini/mini-windows.c
@@ -50,6 +50,10 @@
 
 #include "jit-icalls.h"
 
+extern LPTOP_LEVEL_EXCEPTION_FILTER old_win_toplevel_exception_filter;
+extern guint64 win_chained_exception_filter_result;
+extern gboolean win_chained_exception_filter_didrun;
+
 void
 mono_runtime_install_handlers (void)
 {
@@ -71,9 +75,24 @@ mono_runtime_cleanup_handlers (void)
 #endif
 }
 
+
+/* mono_chain_signal:
+ *
+ *   Call the original signal handler for the signal given by the arguments, which
+ * should be the same as for a signal handler. Returns TRUE if the original handler
+ * was called, false otherwise.
+ */
 gboolean
 SIG_HANDLER_SIGNATURE (mono_chain_signal)
 {
+	int signal = _dummy;
+	GET_CONTEXT;
+
+	if (old_win_toplevel_exception_filter) {
+		win_chained_exception_filter_didrun = TRUE;
+		win_chained_exception_filter_result = (*old_win_toplevel_exception_filter)(info);
+		return TRUE;
+	}
 	return FALSE;
 }
 
