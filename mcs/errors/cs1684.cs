049a51aa06dba22ff295fd47f4f2cc86e769ad10
Marek Safar <marek.safar@gmail.com>
2011-02-02 12:38:44 +0000

    Update error tests

--- mcs/errors/cs1684.cs
+++ mcs/errors/cs1684.cs
@@ -1,4 +1,4 @@
-// CS1684: Reference to type `TYWSF$' claims it is defined assembly `mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089', but it could not be found
+// CS1684: Reference to type `TYWSF$' claims it is defined assembly `mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089', but it could not be found
 // Line: 5
 // Compiler options: -r:CS1684-lib.dll
 
