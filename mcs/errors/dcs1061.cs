00641d503d39716627f4d6be974179c7710de554
Marek Safar <marek.safar@gmail.com>
2011-02-02 11:07:42 +0000

    New test

---
+++ mcs/errors/dcs1061.cs
@@ -0,0 +1,15 @@
+// CS1061: Type `object' does not contain a definition for `Foo' and no extension method `Foo' of type `object' could be found (are you missing a using directive or an assembly reference?)
+// Line: 12
+
+using System.Collections.Generic;
+
+public class C
+{
+	void M (IEnumerable<KeyValuePair<string, dynamic>> arg)
+	{
+		foreach (KeyValuePair<string, object> o in arg)
+		{
+			o.Value.Foo ();
+		}
+	}
+}
