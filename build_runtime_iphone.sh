1a775615321c1ba551100694450191b329fb62ac
Mantas Puida <mantas@unity3d.com>
2011-02-03 20:10:04 +0200

    Fixes for iOS soft debugger support

--- build_runtime_iphone.sh
+++ build_runtime_iphone.sh
@@ -66,7 +66,7 @@ build_arm_mono ()
 	./autogen.sh --prefix=$PRFX --disable-mcs-build --host=arm-apple-darwin9 --disable-shared-handles --with-tls=pthread --with-sigaltstack=no --with-glib=embedded --enable-minimal=jit,profiler,com --disable-nls || exit 1
 	perl -pi -e 's/MONO_SIZEOF_SUNPATH 0/MONO_SIZEOF_SUNPATH 104/' config.h
 	perl -pi -e 's/#define HAVE_FINITE 1//' config.h
-	perl -pi -e 's/#define HAVE_MMAP 1//' config.h
+	#perl -pi -e 's/#define HAVE_MMAP 1//' config.h
 	perl -pi -e 's/#define HAVE_CURSES_H 1//' config.h
 	make
 
