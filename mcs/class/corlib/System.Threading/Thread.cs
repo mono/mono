1066c2c1b5c547ac2dd8999c058db5ebc9cc8306
Rodrigo Kumpera <kumpera@gmail.com>
2011-02-11 14:51:59 +0100

    Make maxStackSize behavior compatible with MS.
    	* Thread.cs (.ctor): Check if maxStackSize is negative
    	and raise and AOORE. Silently use a saner minimum size
    	of 128k as mono can't really run with much less than that.

--- mcs/class/corlib/System.Threading/Thread.cs
+++ mcs/class/corlib/System.Threading/Thread.cs
@@ -823,8 +823,10 @@ namespace System.Threading {
 		{
 			if (start == null)
 				throw new ArgumentNullException ("start");
-			if (maxStackSize < 131072)
-				throw new ArgumentException ("< 128 kb", "maxStackSize");
+			if (maxStackSize < 0)
+				throw new ArgumentOutOfRangeException ("less than zero", "maxStackSize");
+			if (maxStackSize < 131072) //make sure stack is at least 128k big
+				maxStackSize = 131072;
 
 			threadstart = start;
 			Internal.stack_size = maxStackSize;
@@ -842,8 +844,10 @@ namespace System.Threading {
 		{
 			if (start == null)
 				throw new ArgumentNullException ("start");
-			if (maxStackSize < 131072)
-				throw new ArgumentException ("< 128 kb", "maxStackSize");
+			if (maxStackSize < 0)
+				throw new ArgumentOutOfRangeException ("less than zero", "maxStackSize");
+			if (maxStackSize < 131072) //make sure stack is at least 128k big
+				maxStackSize = 131072;
 
 			threadstart = start;
 			Internal.stack_size = maxStackSize;
