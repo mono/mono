2fa2ca356592a6d719fc89ffc8f15179ed559859
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2011-02-13 15:01:29 -0500

    [HttpWebRequest] Use the timeout in GetResponse()
    	The timeout was not being used in GetResponse.
    	Fixes bug #603794.

--- mcs/class/System/System.Net/HttpWebRequest.cs
+++ mcs/class/System/System.Net/HttpWebRequest.cs
@@ -811,7 +811,7 @@ namespace System.Net
 				asyncWrite = (WebAsyncResult) asyncResult;
 			}
 
-			if (!asyncResult.AsyncWaitHandle.WaitOne (timeout, false)) {
+			if (!asyncResult.IsCompleted && !asyncResult.AsyncWaitHandle.WaitOne (timeout, false)) {
 				Abort ();
 				throw new WebException ("The request timed out", WebExceptionStatus.Timeout);
 			}
@@ -907,6 +907,11 @@ namespace System.Net
 		public override WebResponse GetResponse()
 		{
 			WebAsyncResult result = (WebAsyncResult) BeginGetResponse (null, null);
+			if (!result.IsCompleted && !result.AsyncWaitHandle.WaitOne (timeout, false)) {
+				Abort ();
+				throw new WebException ("The request timed out", WebExceptionStatus.Timeout);
+			}
+
 			return EndGetResponse (result);
 		}
 		
