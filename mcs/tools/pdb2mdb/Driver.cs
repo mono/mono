2c9f304f50cd3559a311215eb148ed0c0db182d7
Martin Baulig <martin@novell.com>
2011-02-16 00:39:16 +0100

    Make pdb2mdb work with files in other directories.
    Also added a public static method, so we can also use this as a library.

--- mcs/tools/pdb2mdb/Driver.cs
+++ mcs/tools/pdb2mdb/Driver.cs
@@ -21,17 +21,30 @@ using Mono.CompilerServices.SymbolWriter;
 
 namespace Pdb2Mdb {
 
-	class Converter {
+	public class Converter {
 
 		MonoSymbolWriter mdb;
 		Dictionary<string, SourceFile> files = new Dictionary<string, SourceFile> ();
 
-		public Converter (MonoSymbolWriter mdb)
+		public static void Convert (string filename)
+		{
+			var asm = AssemblyDefinition.ReadAssembly (filename);
+
+			var pdb = asm.Name.Name + ".pdb";
+			pdb = Path.Combine (Path.GetDirectoryName (filename), pdb);
+
+			using (var stream = File.OpenRead (pdb)) {
+				var funcs = PdbFile.LoadFunctions (stream, true);
+				Converter.Convert (asm, funcs, new MonoSymbolWriter (filename));
+			}
+		}
+
+		internal Converter (MonoSymbolWriter mdb)
 		{
 			this.mdb = mdb;
 		}
 
-		public static void Convert (AssemblyDefinition assembly, IEnumerable<PdbFunction> functions, MonoSymbolWriter mdb)
+		internal static void Convert (AssemblyDefinition assembly, IEnumerable<PdbFunction> functions, MonoSymbolWriter mdb)
 		{
 			var converter = new Converter (mdb);
 
@@ -149,6 +162,7 @@ namespace Pdb2Mdb {
 			var assembly = AssemblyDefinition.ReadAssembly (asm);
 
 			var pdb = assembly.Name.Name + ".pdb";
+			pdb = Path.Combine (Path.GetDirectoryName (asm), pdb);
 
 			if (!File.Exists (pdb))
 				Usage ();
