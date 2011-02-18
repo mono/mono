3fc918496ac8d94eab3c8ef194fca6aabc4ff5c4
Jérémie Laval <jeremie.laval@gmail.com>
2011-02-18 14:06:12 +0000

    Lock-free work-stealing queue for the Thread pool

--- mono/metadata/mono-wsq.c
+++ mono/metadata/mono-wsq.c
@@ -11,17 +11,18 @@
 #include <mono/metadata/object.h>
 #include <mono/metadata/mono-wsq.h>
 #include <mono/utils/mono-semaphore.h>
+#include <mono/utils/mono-time.h>
+#include <glib.h>
 
 #define INITIAL_LENGTH	32
 #define WSQ_DEBUG(...)
 //#define WSQ_DEBUG(...) g_message(__VA_ARGS__)
 
 struct _MonoWSQ {
-	volatile gint head;
-	volatile gint tail;
+	volatile gint32 top;
+	volatile gint32 bottom;
+	gint32 upper_bound;
 	MonoArray *queue;
-	gint32 mask;
-	MonoSemType lock;
 };
 
 #define NO_KEY ((guint32) -1)
@@ -52,11 +53,9 @@ mono_wsq_create ()
 		return NULL;
 
 	wsq = g_new0 (MonoWSQ, 1);
-	wsq->mask = INITIAL_LENGTH - 1;
 	MONO_GC_REGISTER_ROOT_SINGLE (wsq->queue);
 	root = mono_get_root_domain ();
 	wsq->queue = mono_array_new_cached (root, mono_defaults.object_class, INITIAL_LENGTH);
-	MONO_SEM_INIT (&wsq->lock, 1);
 	if (!TlsSetValue (wsq_tlskey, wsq)) {
 		mono_wsq_destroy (wsq);
 		wsq = NULL;
@@ -72,7 +71,6 @@ mono_wsq_destroy (MonoWSQ *wsq)
 
 	g_assert (mono_wsq_count (wsq) == 0);
 	MONO_GC_UNREGISTER_ROOT (wsq->queue);
-	MONO_SEM_DESTROY (&wsq->lock);
 	memset (wsq, 0, sizeof (MonoWSQ));
 	if (wsq_tlskey != NO_KEY && TlsGetValue (wsq_tlskey) == wsq)
 		TlsSetValue (wsq_tlskey, NULL);
@@ -84,16 +82,16 @@ mono_wsq_count (MonoWSQ *wsq)
 {
 	if (!wsq)
 		return 0;
-	return ((wsq->tail - wsq->head) & wsq->mask);
+	return wsq->bottom - wsq->top;
 }
 
 gboolean
 mono_wsq_local_push (void *obj)
 {
-	int tail;
-	int head;
-	int count;
 	MonoWSQ *wsq;
+	gint32 b;
+	MonoArray* a;
+	gint size;
 
 	if (obj == NULL || wsq_tlskey == NO_KEY)
 		return FALSE;
@@ -104,46 +102,40 @@ mono_wsq_local_push (void *obj)
 		return FALSE;
 	}
 
-	tail = wsq->tail;
-	if (tail < wsq->head + wsq->mask) {
-		mono_array_setref (wsq->queue, tail & wsq->mask, (MonoObject *) obj);
-		wsq->tail = tail + 1;
-		WSQ_DEBUG ("local_push: OK %p %p\n", wsq, obj);
-		return TRUE;
-	}
+	b = wsq->bottom;
+	a = wsq->queue;
+	size = mono_array_length (a);
 
-	MONO_SEM_WAIT (&wsq->lock);
-	head = wsq->head;
-	count = wsq->tail - wsq->head;
-	if (count >= wsq->mask) {
+	if (b - wsq->upper_bound >= size - 1) {
 		MonoArray *new_array;
-		int length;
+		gint new_size;
 		int i;
 
-		length = mono_array_length (wsq->queue);
-		new_array = mono_array_new_cached (mono_get_root_domain (), mono_defaults.object_class, length * 2);
-		for (i = 0; i < length; i++)
-			mono_array_setref (new_array, i, mono_array_get (wsq->queue, MonoObject*, (i + head) & wsq->mask));
+		wsq->upper_bound = wsq->top;
+		new_size = size * 2;
 
-		memset (mono_array_addr (wsq->queue, MonoObject *, 0), 0, sizeof (MonoObject*) * length);
+		new_array = mono_array_new_cached (mono_get_root_domain (), mono_defaults.object_class, new_size);
+		for (i = wsq->upper_bound; i < b; ++i)
+			mono_array_setref (new_array, i, mono_array_get (a, void*, i % size));
+
 		wsq->queue = new_array;
-		wsq->head = 0;
-		wsq->tail = tail = count;
-		wsq->mask = (wsq->mask << 1) | 1;
 	}
-	mono_array_setref (wsq->queue, tail & wsq->mask, obj);
-	wsq->tail = tail + 1;
-	MONO_SEM_POST (&wsq->lock);
-	WSQ_DEBUG ("local_push: LOCK %p  %p\n", wsq, obj);
+
+	mono_array_setref (wsq->queue, b % size, obj);
+	InterlockedIncrement (&wsq->bottom);
+
+	WSQ_DEBUG ("local_push: push successfull\n");
+
 	return TRUE;
 }
 
 gboolean
 mono_wsq_local_pop (void **ptr)
 {
-	int tail;
-	gboolean res;
 	MonoWSQ *wsq;
+	gint32 b, t;
+	gint size;
+	MonoArray* a;
 
 	if (ptr == NULL || wsq_tlskey == NO_KEY)
 		return FALSE;
@@ -154,56 +146,56 @@ mono_wsq_local_pop (void **ptr)
 		return FALSE;
 	}
 
-	tail = wsq->tail;
-	if (wsq->head >= tail) {
-		WSQ_DEBUG ("local_pop: empty\n");
+	b = InterlockedDecrement (&wsq->bottom);
+	a = wsq->queue;
+	t = wsq->top;
+	size = b - t;
+
+	if (size < 0) {
+		InterlockedExchangeAdd (&wsq->bottom, t - b);
 		return FALSE;
 	}
-	tail--;
-	InterlockedExchange (&wsq->tail, tail);
-	if (wsq->head <= tail) {
-		*ptr = mono_array_get (wsq->queue, void *, tail & wsq->mask);
-		mono_array_set (wsq->queue, void *, tail & wsq->mask, NULL);
-		WSQ_DEBUG ("local_pop: GOT ONE %p %p\n", wsq, *ptr);
-		return TRUE;
-	}
 
-	MONO_SEM_WAIT (&wsq->lock);
-	if (wsq->head <= tail) {
-		*ptr = mono_array_get (wsq->queue, void *, tail & wsq->mask);
-		mono_array_set (wsq->queue, void *, tail & wsq->mask, NULL);
-		res = TRUE;
-	} else {
-		wsq->tail = tail + 1;
-		res = FALSE;
-	}
-	MONO_SEM_POST (&wsq->lock);
-	WSQ_DEBUG ("local_pop: LOCK %d %p %p\n", res, wsq, *ptr);
-	return res;
+	*ptr = mono_array_get (a, void*, b % mono_array_length (a));
+	if (size > 0)
+		return TRUE;
+
+	InterlockedExchangeAdd (&wsq->bottom, t + 1 - b);
+	if (InterlockedCompareExchange (&wsq->top, t + 1, t) != t)
+		return FALSE;
+
+	return TRUE;
 }
 
 void
 mono_wsq_try_steal (MonoWSQ *wsq, void **ptr, guint32 ms_timeout)
 {
+	gint32 t, b;
+	gint size;
+	MonoArray* a;
+	guint32 start_ticks = mono_msec_ticks ();
+
 	if (wsq == NULL || ptr == NULL || *ptr != NULL || wsq_tlskey == NO_KEY)
 		return;
 
 	if (TlsGetValue (wsq_tlskey) == wsq)
 		return;
 
-	if (mono_sem_timedwait (&wsq->lock, ms_timeout, FALSE) == 0) {
-		int head;
+	do {
+		t = wsq->top;
+		b = wsq->bottom;
 
-		head = wsq->head;
-		InterlockedExchange (&wsq->head, head + 1);
-		if (head < wsq->tail) {
-			*ptr = mono_array_get (wsq->queue, void *, head & wsq->mask);
-			mono_array_set (wsq->queue, void *, head & wsq->mask, NULL);
-			WSQ_DEBUG ("STEAL %p %p\n", wsq, *ptr);
-		} else {
-			wsq->head = head;
-		}
-		MONO_SEM_POST (&wsq->lock);
-	}
+		if (b - t <= 0)
+			return;
+
+		if (InterlockedCompareExchange (&wsq->top, t + 1, t) == t)
+			break;
+
+		ves_icall_System_Threading_Thread_Yield ();
+	} while (mono_msec_ticks () - start_ticks < ms_timeout);
+
+	a = wsq->queue;
+	size = mono_array_length (a);
+	*ptr = mono_array_get (a, void*, t % size);
 }
 
