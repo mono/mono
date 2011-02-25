450b0f1c4a8a3863a2c1e132d2b9a050a203d711
Steve Bjorg <steve.bjorg@gmail.com>
2011-02-24 21:37:46 -0800

    added units tests for TextReader ReadLine() and ReadToEnd()

--- mcs/class/corlib/Test/System.IO/TextReaderTest.cs
+++ mcs/class/corlib/Test/System.IO/TextReaderTest.cs
@@ -8,6 +8,7 @@
 
 using System;
 using System.IO;
+using System.Text;
 using NUnit.Framework;
 
 namespace MonoTests.System.IO
@@ -31,6 +32,53 @@ namespace MonoTests.System.IO
 
 			int count = TextReader.Null.Read ();
 			Assert.AreEqual (-1, count);
+		}
+		
+		[Test]
+		public void TestTextReaderReadLine() {
+			using(var reader = new TestTextReader("abc\n\ndef\r\nghi")) {
+				var line = reader.ReadLine();
+				Assert.AreEqual("abc", line, "first line (\\n)");
+				
+				line = reader.ReadLine();
+				Assert.AreEqual("", line, "second line (empty)");
+				
+				line = reader.ReadLine();
+				Assert.AreEqual("def", line, "third line (\\r\\n)");
+				
+				line = reader.ReadLine();
+				Assert.AreEqual("ghi", line, "fourth line (last)");
+				
+				line = reader.ReadLine();
+				Assert.AreEqual(null, line, "eof");
+			}
+		}
+		
+		[Test]
+		public void TestTextReaderReadToEnd() {
+			using(var reader = new TestTextReader("abc\n\ndef\r\nghi")) {
+				var contents = reader.ReadToEnd();
+				Assert.AreEqual("abc\n\ndef\r\nghi", contents);
+			}
+		}
+		
+		private class TestTextReader : TextReader {
+			TextReader reader;
+			
+			public TestTextReader(string text)
+			{
+				this.reader = new StringReader(text);
+			}
+		
+			public override int Peek()
+			{
+				return reader.Peek();
+			}
+			
+			public override int Read()
+			{
+				return reader.Read();
+			}
 		}
 	}
 }
