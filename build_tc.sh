fdb146c4d63f0f51923d546a3bf05ddccba40e1c
jechter <jonas@jonasechterhoff.com>
2011-03-03 15:31:25 +0100

    Add x86_64 build.

--- build_tc.sh
+++ build_tc.sh
@@ -11,13 +11,18 @@ export NACL_NATIVE_CLIENT="$PWD/../native_client_sdk/builds/"
 
 ./autogen.sh
 make distclean
+
 cd nacl
 ./nacl-runtime-mono.sh
-
+export TARGET_BIT_SIZE=64
+./nacl-runtime-mono.sh
 cd ..
 
 mkdir -p builds/embedruntimes/nacl
 cp nacl/runtime/lib/libmono-2.0.a builds/embedruntimes/nacl/libmono.a
+
+mkdir -p builds/embedruntimes/nacl64
+cp nacl/runtime64/lib/libmono-2.0.a builds/embedruntimes/nacl64/libmono.a
 
 mkdir -p builds/monodistribution/lib/mono/2.0
 cp nacl/runtime/lib/mono/2.0/mscorlib.dll* builds/monodistribution/lib/mono/2.0
