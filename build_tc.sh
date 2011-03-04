c07533f1ebea8928f9b2554813c4cae6fe9785d6
jechter <jonas@jonasechterhoff.com>
2011-03-04 08:19:41 +0100

    cleanup.

--- build_tc.sh
+++ build_tc.sh
@@ -1,11 +1,5 @@
 rm -r builds
 
-if [ "${UNITY_THISISABUILDMACHINE}" = "1" ]; then
-	svn --username unitybuild --password uni10666 co svn://svn.hq.unity3d.com/home/svn/external-tools/native_client_sdk ../native_client_sdk
-else
-	yes p | svn co --username je_script --password raidUvWarHi https://intra.unity3d.com:3680/home/svn/external-tools/native_client_sdk/ ../native_client_sdk
-fi
-
 perl ../native_client_sdk/prepare.pl
 export NACL_NATIVE_CLIENT="$PWD/../native_client_sdk/builds/"
 
