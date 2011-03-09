1cc350f886347ed3f451355c2285f57d349bcc90
Atsushi Eno <atsushi@ximian.com>
2011-03-09 10:18:42 +0900

    Temporarily disable InternalsVisibleTo(System.ServiceModel).
    until mcs (in 2-10 or later release) fixes the issue.

--- mcs/class/System.Runtime.Serialization/Assembly/AssemblyInfo.cs
+++ mcs/class/System.Runtime.Serialization/Assembly/AssemblyInfo.cs
@@ -67,7 +67,8 @@ using System.Runtime.InteropServices;
 [assembly: SecurityCritical (SecurityCriticalScope.Explicit)]
 #if NET_4_0
 // for SyndicationElementExtension
-[assembly: InternalsVisibleTo ("System.ServiceModel, PublicKey=00000000000000000400000000000000")]
+// FIXME: mcs in 2-10 branch breaks System.ServiceModel build on resolving this. So, disabling it so far.
+// [assembly: InternalsVisibleTo ("System.ServiceModel, PublicKey=00000000000000000400000000000000")]
 #else
 // for SyndicationElementExtension
 [assembly: InternalsVisibleTo ("System.ServiceModel.Web, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
