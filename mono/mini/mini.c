34519d21795e1df7d7735adae976d90b8cdaa921
Zoltan Varga <vargaz@gmail.com>
2011-03-10 01:03:33 +0100

    Fix the previous change.

--- mono/mini/mini.c
+++ mono/mini/mini.c
@@ -2277,12 +2277,12 @@ mono_jit_thread_attach (MonoDomain *domain)
 	if (!mono_lmf_addr) {
 		mono_thread_attach (domain);
 		// #678164
-		mono_thread_set_state (mono_thread_internal_current (), ThreadState_Background);
+		mono_thread_set_state (mono_thread_current (), ThreadState_Background);
 	}
 #else
 	if (!TlsGetValue (mono_jit_tls_id)) {
 		mono_thread_attach (domain);
-		mono_thread_set_state (mono_thread_internal_current (), ThreadState_Background);
+		mono_thread_set_state (mono_thread_current (), ThreadState_Background);
 	}
 #endif
 	if (mono_domain_get () != domain)
