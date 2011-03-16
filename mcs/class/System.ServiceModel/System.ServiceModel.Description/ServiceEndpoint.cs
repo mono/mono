59df21bbdb22b45f298cdf24e0430511afc03dbd
Atsushi Eno <atsushi@ximian.com>
2011-03-16 20:41:53 +0900

    Enable more behaviors use in NET_2_1 land, toward WebHttpBinding.

--- mcs/class/System.ServiceModel/System.ServiceModel.Description/ServiceEndpoint.cs
+++ mcs/class/System.ServiceModel/System.ServiceModel.Description/ServiceEndpoint.cs
@@ -116,8 +116,8 @@ namespace System.ServiceModel.Description
 			set { name = value; }
 		}
 
-		internal void Validate () {
-#if !MOONLIGHT
+		internal void Validate ()
+		{
 			foreach (IContractBehavior b in Contract.Behaviors)
 				b.Validate (Contract, this);
 			foreach (IEndpointBehavior b in Behaviors)
@@ -126,7 +126,6 @@ namespace System.ServiceModel.Description
 				foreach (IOperationBehavior b in operation.Behaviors)
 					b.Validate (operation);
 			}
-#endif
 		}
 
 
@@ -136,12 +135,11 @@ namespace System.ServiceModel.Description
 
 			var proxy = se.Contract.CreateClientRuntime (callbackDispatchRuntime);
 
-#if !NET_2_1
 			foreach (IEndpointBehavior b in se.Behaviors)
 				b.ApplyClientBehavior (se, proxy);
 			foreach (IContractBehavior b in se.Contract.Behaviors)
 				b.ApplyClientBehavior (se.Contract, se, proxy);
-#endif
+
 			return proxy;
 		}
 	}
