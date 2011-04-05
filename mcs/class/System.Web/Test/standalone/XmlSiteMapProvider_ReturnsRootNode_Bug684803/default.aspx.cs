98cf1c90d4a48fc98de4661760f0756fba83d498
Marek Habersack <grendel@twistedcode.net>
2011-04-05 14:34:17 +0200

    [asp.net] Fix for bug #684803. XmlSiteMapProvider must take its root node into account when matching an URL.

---
+++ mcs/class/System.Web/Test/standalone/XmlSiteMapProvider_ReturnsRootNode_Bug684803/default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+	    SiteMapNode node = SiteMap.Provider.FindSiteMapNode ("~/");
+	    log.InnerHtml = String.Format ("node is {0}null and it is {1}the root node", node == null ? String.Empty : "<strong>not</strong> ",
+					    node == null || node != SiteMap.Provider.RootNode ? "<strong>not</strong> " : String.Empty);
+    }
+}
