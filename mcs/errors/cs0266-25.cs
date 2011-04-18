ae7d230c7fb0a5b1b928dfc8949c18eac2b92425
Marek Safar <marek.safar@gmail.com>
2011-04-18 14:48:25 +0100

    Merge error files from 4.0 profile

--- mcs/errors/cs0266-25.cs
+++ mcs/errors/cs0266-25.cs
@@ -1,6 +1,6 @@
 // CS0266: Cannot implicitly convert type `System.Collections.Generic.IList<dynamic>' to `string[]'. An explicit conversion exists (are you missing a cast?)
 // Line: 10
-// Compiler options: -r:DCS0266-lib.dll -noconfig
+// Compiler options: -r:CS0266-25-lib.dll -noconfig
 
 public class C
 {
