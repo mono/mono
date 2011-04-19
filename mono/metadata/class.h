217223d0d6823744ee11aee58b65d0c2b624eea2
Levi Bard <levi@unity3d.com>
2011-04-19 15:21:39 +0200

    Expose mono_class_is_generic and mono_class_is_inflated.
     * class.c:
     * class.h: Expose mono_class_is_generic and mono_class_is_inflated.
    
    License: MIT/X11

--- mono/metadata/class.h
+++ mono/metadata/class.h
@@ -107,6 +107,12 @@ mono_class_is_subclass_of (MonoClass *klass, MonoClass *klassc,
 mono_bool
 mono_class_is_assignable_from (MonoClass *klass, MonoClass *oklass);
 
+mono_bool
+mono_class_is_generic (MonoClass *klass);
+
+mono_bool
+mono_class_is_inflated (MonoClass *klass);
+
 void*
 mono_ldtoken               (MonoImage *image, uint32_t token, MonoClass **retclass, MonoGenericContext *context);
 
