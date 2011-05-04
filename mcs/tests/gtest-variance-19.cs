a4a61d8c4109a656d41a211c5ec699e3979cfdc9
Marek Safar <marek.safar@gmail.com>
2011-05-04 12:18:15 +0100

    [691531] Variance conversion requires type parameters to be reference types

---
+++ mcs/tests/gtest-variance-19.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+	static void Bla<T, U> () where T : class, U
+	{
+		T[] ta = new T[0];
+		IEnumerable<T> ita = ta;
+		IEnumerable<U> itu = ita;
+	}
+
+	static void Main ()
+	{
+		Bla<string, object> ();
+	}
+}
