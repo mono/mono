f88fba3cbd17bad93fe9e6ee0fd1a900d43559f6
Marek Habersack <grendel@twistedcode.net>
2011-05-24 11:12:43 +0200

    [asp.net] Don't throw an exception if a session item being released does not have a lock.
    It can happen when the item is being released after its associated session has ended.

--- mcs/class/System.Web/System.Web.SessionState_2.0/SessionInProcHandler.cs
+++ mcs/class/System.Web/System.Web.SessionState_2.0/SessionInProcHandler.cs
@@ -380,7 +380,7 @@ namespace System.Web.SessionState
 				itemLock = inProcItem.rwlock;
 				if (itemLock != null && itemLock.TryEnterWriteLock (lockAcquireTimeout))
 					locked = true;
-				else
+				else if (itemLock != null)
 					throw new ApplicationException ("Failed to acquire lock");
 				inProcItem.locked = false;
 				inProcItem.items = itemItems;
