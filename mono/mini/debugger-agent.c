7e2eff0be732d7e55675d8318d8edbbe8f0999ec
Zoltan Varga <vargaz@gmail.com>
2011-08-14 22:30:54 +0200

    Don't pass MONO_UNWIND_ACTUAL_METHOD to the stack walker in a signal handler, its not signal safe. Fixes bxc #263.

--- mono/mini/debugger-agent.c
+++ mono/mini/debugger-agent.c
@@ -2064,7 +2064,12 @@ mono_debugger_agent_thread_interrupt (void *sigctx, MonoJitInfo *ji)
 			data.last_frame_set = FALSE;
 			if (sigctx) {
 				mono_arch_sigctx_to_monoctx (sigctx, &ctx);
-				mono_walk_stack (get_last_frame, mono_domain_get (), &ctx, MONO_UNWIND_DEFAULT, tls->thread, mono_get_lmf (), &data);
+				/* 
+				 * Don't pass MONO_UNWIND_ACTUAL_METHOD, its not signal safe, and
+				 * get_last_frame () doesn't need it, the last frame cannot be a ginst
+				 * since we are not in a JITted method.
+				 */
+				mono_walk_stack (get_last_frame, mono_domain_get (), &ctx, MONO_UNWIND_DENONE, tls->thread, mono_get_lmf (), &data);
 			}
 			if (data.last_frame_set) {
 				memcpy (&tls->async_last_frame, &data.last_frame, sizeof (StackFrameInfo));
