20d8e0162b6cdb1c5f5e6005bbf8f270cf670be7
Jeffrey Stedfast <jeff@xamarin.com>
2011-08-15 15:15:32 -0400

    Fix for bug #445855
    In this day and age of mobile personal computing devices,
    we need to periodically check the underlying system
    timezone because the device may have crossed a timezone
    boundary.

--- mcs/class/corlib/System/TimeZone.cs
+++ mcs/class/corlib/System/TimeZone.cs
@@ -56,6 +56,9 @@ namespace System
 		// Fields
 		static TimeZone currentTimeZone;
 
+		[NonSerialized]
+		static long timezone_check;
+
 		// Constructor
 		protected TimeZone ()
 		{
@@ -64,8 +67,13 @@ namespace System
 		// Properties
 		public static TimeZone CurrentTimeZone {
 			get {
-				if (currentTimeZone == null)
-					currentTimeZone = new CurrentSystemTimeZone (DateTime.GetNow ());
+				long now = DateTime.GetNow ();
+				
+				if (currentTimeZone == null || (now - timezone_check) > TimeSpan.TicksPerMinute) {
+					currentTimeZone = new CurrentSystemTimeZone (now);
+					timezone_check = now;
+				}
+				
 				return currentTimeZone;
 			}
 		}
