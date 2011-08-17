57bfe6463f3e640246b84ded9d60b179a3a26e98
Jérémie Laval <jeremie.laval@gmail.com>
2011-08-17 20:00:05 +0200

    Fix coding style

--- mcs/class/corlib/System.Threading.Tasks/Task.cs
+++ mcs/class/corlib/System.Threading.Tasks/Task.cs
@@ -70,7 +70,7 @@ namespace System.Threading.Tasks
 
 		CancellationToken token;
 
-		const TaskCreationOptions maxTaskCreationOptions =
+		const TaskCreationOptions MaxTaskCreationOptions =
 			TaskCreationOptions.PreferFairness | TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent;
 
 		public Task (Action action) : this (action, TaskCreationOptions.None)
@@ -93,7 +93,7 @@ namespace System.Threading.Tasks
 		{
 			if (action == null)
 				throw new ArgumentNullException ("action");
-			if (creationOptions > maxTaskCreationOptions || creationOptions < TaskCreationOptions.None)
+			if (creationOptions > MaxTaskCreationOptions || creationOptions < TaskCreationOptions.None)
 				throw new ArgumentOutOfRangeException ("creationOptions");
 			this.simpleAction = action;
 		}
@@ -117,7 +117,7 @@ namespace System.Threading.Tasks
 		{
 			if (action == null)
 				throw new ArgumentNullException ("action");
-			if (creationOptions > maxTaskCreationOptions || creationOptions < TaskCreationOptions.None)
+			if (creationOptions > MaxTaskCreationOptions || creationOptions < TaskCreationOptions.None)
 				throw new ArgumentOutOfRangeException ("creationOptions");
 		}
 
