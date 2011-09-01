5dac43143b66859f0cc5cd0b00e83e4747ed7a6b
Spencer Hakim <shakim@d3led.com>
2011-09-01 13:59:05 -0400

    Added test case for "Source file '{interactive}' could not be found" errors

---
+++ mcs/class/Mono.CSharp/Test/Evaluator/InteractiveSourceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using Mono.CSharp;
+
+namespace MonoTests.EvaluatorTest
+{
+	public class CatchInteractiveSourceFileErrorReportPrinter : ReportPrinter
+	{
+		public override void Print (AbstractMessage msg)
+		{
+			Assert.False (msg.Text == "Source file `{interactive}' could not be found");
+		}
+	}
+
+	[TestFixture]
+	public class ReportTest
+	{
+		CompilerSettings settings;
+		Report report;
+		Evaluator eval;
+
+		[SetUp]
+		public void Setup ()
+		{
+			settings = new CompilerSettings ();
+			report = new Report (new CatchInteractiveSourceFileErrorReportPrinter ());
+			eval = new Evaluator (settings, report);
+		}
+
+		[Test]
+		public void CreateInteractiveSourceFileError ()
+		{
+			//creating a second Evaluator using the same CompilerSettings object creates a second '{interactive}' CompilationSourceFile
+			//object in CompilerSettings.SourceFiles
+			Evaluator secondEval = new Evaluator (settings, report);
+			secondEval.Run ("int i = 0;");
+		}
+	}
+}
