37ac9968823a1c5a3c811095f6ad47e7ebee6cc2
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2011-09-21 07:37:59 -0400

    Return the right empty collection in ParseQueryString.
    	Fixes Xamarin bug #970.

--- mcs/class/System.Web/System.Web/HttpUtility.cs
+++ mcs/class/System.Web/System.Web/HttpUtility.cs
@@ -654,7 +654,7 @@ namespace System.Web {
 			if (encoding == null)
 				throw new ArgumentNullException ("encoding");
 			if (query.Length == 0 || (query.Length == 1 && query[0] == '?'))
-				return new NameValueCollection ();
+				return new HttpQSCollection ();
 			if (query[0] == '?')
 				query = query.Substring (1);
 				
