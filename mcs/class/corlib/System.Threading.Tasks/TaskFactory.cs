675aacc20f406bd32c49d53a378ad9418582542d
Marek Safar <marek.safar@gmail.com>
2011-10-12 12:29:17 +0100

    Fix wrong conditional

--- mcs/class/corlib/System.Threading.Tasks/TaskFactory.cs
+++ mcs/class/corlib/System.Threading.Tasks/TaskFactory.cs
@@ -26,7 +26,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#if NET_4_0 || MOBILE || true
+#if NET_4_0 || MOBILE
 
 using System;
 using System.Threading;
