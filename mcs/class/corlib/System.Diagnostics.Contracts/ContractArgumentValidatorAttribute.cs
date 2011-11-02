f2ba7a6e3995b20721e7a35e43d2a502fce37bac
Marek Safar <marek.safar@gmail.com>
2011-11-02 11:05:00 +0000

    Add few 4.5 types to compile MEF2

--- mcs/class/corlib/System.Diagnostics.Contracts/ContractArgumentValidatorAttribute.cs
+++ mcs/class/corlib/System.Diagnostics.Contracts/ContractArgumentValidatorAttribute.cs
@@ -1,3 +1,11 @@
+//
+// ContractArgumentValidatorAttribute.cs
+//
+// Authors:
+//	Marek Safar  <marek.safar@gmail.com>
+//
+// Copyright 2011 Xamarin Inc.
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -15,17 +23,17 @@
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-//
-// Authors:
-//        Alejandro Serrano "Serras" (trupill@yahoo.es)
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if !NET_4_5
+#if NET_4_5
 
-namespace System.Runtime.CompilerServices {
-
-	[AttributeUsage (AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
-	public sealed class ExtensionAttribute : Attribute {
+namespace System.Diagnostics.Contracts
+{
+	[ConditionalAttribute ("CONTRACTS_FULL")]
+	[AttributeUsageAttribute (AttributeTargets.Method)]
+	public sealed class ContractArgumentValidatorAttribute : Attribute
+	{
 	}
 }
 
