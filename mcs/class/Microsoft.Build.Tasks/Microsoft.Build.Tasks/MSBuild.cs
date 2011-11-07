f56700caa889dd3087af0cd77cb183208148f1c9
Miguel de Icaza <miguel@gnome.org>
2011-11-07 10:26:34 -0500

    update copyright

--- mcs/class/Microsoft.Build.Tasks/Microsoft.Build.Tasks/MSBuild.cs
+++ mcs/class/Microsoft.Build.Tasks/Microsoft.Build.Tasks/MSBuild.cs
@@ -5,6 +5,7 @@
 //   Marek Sieradzki (marek.sieradzki@gmail.com)
 //
 // (C) 2005 Marek Sieradzki
+// Copyright 2011 Xamarin Inc
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
