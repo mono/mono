mono/mono

commit 354778218590e906ea82154fff9ea960f9d03e2a

Fix a warning

mono/metadata/sgen-gc.h
@@ -267,7 +267,7 @@ extern long long stat_objects_copied_major;
 #define HEAVY_STAT(x)
 #endif
 
-#define DEBUG(level,a) do {if (G_UNLIKELY ((level) <= SGEN_MAX_DEBUG_LEVEL && (level) <= gc_debug_level)) a;} while (0)
+#define DEBUG(level,a) do {if (G_UNLIKELY ((level) <= SGEN_MAX_DEBUG_LEVEL && (level) <= gc_debug_level)) a; fflush (gc_debug_file); } while (0)
 
 extern int gc_debug_level;
 extern FILE* gc_debug_file;

commit 1f57742081719b03c4f203f6a051d75d9c4467b7

Fix a bug introduced by 354778218590e906ea82154fff9ea960f9d03e2a which killed sgen performance on HEAD.

mono/metadata/sgen-gc.c
@@ -337,8 +337,6 @@ static long long time_major_los_sweep = 0;
 static long long time_major_sweep = 0;
 static long long time_major_fragment_creation = 0;
 
-#define DEBUG(level,a) do {if (G_UNLIKELY ((level) <= SGEN_MAX_DEBUG_LEVEL && (level) <= gc_debug_level)) a; fflush (gc_debug_file); } while (0)
-
 int gc_debug_level = 0;
 FILE* gc_debug_file;
 static gboolean debug_print_allowance = FALSE;

mono/metadata/sgen-gc.h
@@ -267,7 +267,7 @@ extern long long stat_objects_copied_major;
 #define HEAVY_STAT(x)
 #endif
 
-#define DEBUG(level,a) do {if (G_UNLIKELY ((level) <= SGEN_MAX_DEBUG_LEVEL && (level) <= gc_debug_level)) a; fflush (gc_debug_file); } while (0)
+#define DEBUG(level,a) do {if (G_UNLIKELY ((level) <= SGEN_MAX_DEBUG_LEVEL && (level) <= gc_debug_level)) { a; fflush (gc_debug_file); } } while (0)
 
 extern int gc_debug_level;
 extern FILE* gc_debug_file;