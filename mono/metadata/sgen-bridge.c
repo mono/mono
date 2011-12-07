636f9ec0b9b5cb02242d8a4ef3f6e88cc83be815
Rodrigo Kumpera <kumpera@gmail.com>
2011-12-07 13:58:19 -0200

    Remove some dead code

--- mono/metadata/sgen-bridge.c
+++ mono/metadata/sgen-bridge.c
@@ -168,16 +168,6 @@ dyn_array_int_contains (DynArray *da, int x)
 	return FALSE;
 }
 
-static void
-dyn_array_append (DynArray *dst, DynArray *src)
-{
-	g_assert (dst->elem_size == src->elem_size);
-
-	dyn_array_ensure_capacity (dst, dst->size + src->size);
-	memcpy (DYN_ARRAY_REF (dst, dst->size), DYN_ARRAY_REF (src, 0), src->size * src->elem_size);
-	dst->size += src->size;
-}
-
 static DynArray merge_array;
 
 static void
