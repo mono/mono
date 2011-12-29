bf25b5061d38676de8821968e46eb55837acd409
Jonathan Chambers <joncham@gmail.com>
2011-12-29 14:34:55 -0500

    Add script for running classlib tests.

---
+++ run_classlib_tests.pl
@@ -0,0 +1,28 @@
+use Cwd;
+use Getopt::Long;
+
+system("source","~/.profile");
+print "My Path: $ENV{PATH}\n";
+
+my $root = getcwd();
+my $teamcity = 0;
+
+if ($ENV{UNITY_THISISABUILDMACHINE}) {
+	$teamcity = 1;
+}
+
+#do build
+chdir("$root/mcs/class/corlib") eq 1 or die("failed to chdir corlib");
+
+my $result = 0;
+if($^O eq 'MSWin32') {
+	$result = system("msbuild build.proj /t:Test");
+} else {
+	$result = system("make run-test-local");
+}
+
+if ($teamcity) {
+	print("##teamcity[importData type='nunit' path='TestResult-net_2_0.xml']\n");
+}
+
+$result eq 0 or die ("Failed running mono classlib tests");
