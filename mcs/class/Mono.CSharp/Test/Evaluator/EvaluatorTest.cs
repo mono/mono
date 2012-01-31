448a1977718c64098bf0fe6865e35532e63bf7f8
Marek Safar <marek.safar@gmail.com>
2012-01-31 10:19:03 +0000

    New test

---
+++ mcs/class/Mono.CSharp/Test/Evaluator/EvaluatorTest.cs
@@ -0,0 +1,44 @@
+//
+// EvaluatorTest.cs
+//
+// Authors:
+//	Marek Safar  <marek.safar@gmail.com>
+//
+// Copyright (C) 2012 Xamarin Inc (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using NUnit.Framework;
+using Mono.CSharp;
+
+namespace MonoTests.EvaluatorTest
+ {
+ 	public class EvaluatorTest : EvaluatorFixture
+	{
+		[Test]
+ 		public void SameSettings ()
+ 		{
+			var ctx = new CompilerContext (settings, new AssertReportPrinter ());
+			var evaluator2 = new Evaluator (ctx);
+			evaluator2.Run ("int i = 0;");
+		}
+ 	}
+ }
