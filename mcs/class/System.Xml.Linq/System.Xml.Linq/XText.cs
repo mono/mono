b68627cdb8970347eb1431686ad0d627d61ed28f
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-02-07 15:24:03 +0900

    Do not call WriteString() for whitespace XText.

--- mcs/class/System.Xml.Linq/System.Xml.Linq/XText.cs
+++ mcs/class/System.Xml.Linq/System.Xml.Linq/XText.cs
@@ -26,6 +26,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
 
@@ -60,7 +61,10 @@ namespace System.Xml.Linq
 
 		public override void WriteTo (XmlWriter w)
 		{
-			w.WriteString (value);
+			if (Value.Length > 0 && Value.All (c => c == ' ' || c == '\t' || c == '\r' || c == '\n'))
+				w.WriteWhitespace (value);
+			else
+				w.WriteString (value);
 		}
 	}
 }
