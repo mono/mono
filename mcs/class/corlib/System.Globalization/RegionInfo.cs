670fa9a727268190e23acff354209b3d508d0e8b
Sebastien Pouliot <sebastien@xamarin.com>
2012-02-15 16:55:12 -0500

    Ensure match between native/managed fields representation so we retrieve the right values

--- mcs/class/corlib/System.Globalization/RegionInfo.cs
+++ mcs/class/corlib/System.Globalization/RegionInfo.cs
@@ -56,8 +56,6 @@ namespace System.Globalization
 			}
 		}
 
-		int lcid; // it is used only for Equals() (not even used in GetHashCode()).
-
 #pragma warning disable 649
 		int regionId;
 		string iso2Name;
@@ -69,6 +67,8 @@ namespace System.Globalization
 		string currencyEnglishName;
 #pragma warning restore 649
 
+		int lcid; // it is used only for Equals() (not even used in GetHashCode()).
+		
 		public RegionInfo (int culture)
 		{
 			if (!GetByTerritory (CultureInfo.GetCultureInfo (culture)))
