998a69a6b11cbc130de5ae124c2482748cd0ebdd
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-03-28 21:12:54 +0900

    Add new System.Json assembly sources based on http://aspnetwebstack.codeplex.com/

---
+++ mcs/class/System.Json-new/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("System.Json")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("6fd72360-ebfc-4097-96fa-2ee418c04f7b")]
---
+++ mcs/class/System.Json-new/RS.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+
+internal static class RS
+{
+    public static string Format(string format, params object[] args)
+    {
+        return String.Format(CultureInfo.CurrentCulture, format, args);
+    }
+}
