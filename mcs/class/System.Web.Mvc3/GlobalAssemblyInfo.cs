a037b4f8fc61657e6dd48d98f06e6bf1e7aeff3b
Marek Habersack <grendel@twistedcode.net>
2012-03-29 19:30:48 +0200

    Version mvc3 properly

---
+++ mcs/class/System.Web.Mvc3/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft® .NET Framework")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.20107.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
