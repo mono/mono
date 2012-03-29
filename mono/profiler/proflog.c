13f5dc80f4b627cafb4cb02093e14c44fccc925e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-03-29 19:13:10 +0200

    Log profiler: don't ignore return values from select, in particular not EINTR.

--- mono/profiler/proflog.c
+++ mono/profiler/proflog.c
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <glib.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -1857,6 +1858,15 @@ helper_thread (void* arg)
 		tv.tv_sec = 1;
 		tv.tv_usec = 0;
 		len = select (max_fd + 1, &rfds, NULL, NULL, &tv);
+		
+		if (len < 0) {
+			if (errno == EINTR)
+				continue;
+			
+			g_warning ("Error in proflog server: %s", strerror (errno));
+			return NULL;
+		}
+		
 		if (FD_ISSET (prof->pipes [0], &rfds)) {
 			char c;
 			int r = read (prof->pipes [0], &c, 1);
