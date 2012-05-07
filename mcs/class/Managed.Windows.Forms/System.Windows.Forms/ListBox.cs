759d6501a5e2785497e034125f1508e5deea8e07
Eberhard Beilharz <eb1@sil.org>
2012-05-07 17:53:22 +0200

    Xamarin-4921: Fix Listbox.SelectedItem after adding sorted item
    When we add an item to a sorted listbox the indices of the selected
    items change and need to be updated.
    
    Change-Id: Ia32849fe10480354835dcd48b37d326d612af04f

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/ListBox.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/ListBox.cs
@@ -2531,13 +2531,26 @@ namespace System.Windows.Forms
 			public int Add (object item)
 			{
 				int idx;
+				object[] selectedItems = null;
+
+				// we need to remember the original selected items so that we can update the indices
+				if (owner.sorted) {
+					selectedItems = new object[owner.SelectedItems.Count];
+					owner.SelectedItems.CopyTo (selectedItems, 0);
+				}
 
 				idx = AddItem (item);
 				owner.CollectionChanged ();
 				
 				// If we are sorted, the item probably moved indexes, get the real one
-				if (owner.sorted)
+				if (owner.sorted) {
+					// update indices of selected items
+					owner.SelectedIndices.Clear ();
+					for (int i = 0; i < selectedItems.Length; i++) {
+						owner.SelectedIndex = this.IndexOf (selectedItems [i]);
+					}
 					return this.IndexOf (item);
+				}
 					
 				return idx;
 			}
