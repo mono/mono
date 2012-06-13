6c3c219ad3ad435ec010d9b2c365a8d678d9c336
eb1@sil.org <eb1@sil.org>
2012-06-13 21:13:14 +0200

    Make it so disposing of a Menu causes its MenuItems to get disposed too. Fixes Xamarin bug 3418.

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/Menu.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/Menu.cs
@@ -188,11 +188,17 @@ namespace System.Windows.Forms
 
 		protected override void Dispose (bool disposing)
 		{		
-			if (disposing) {
-				if (menu_handle != IntPtr.Zero) {
-					menu_handle = IntPtr.Zero;
+ 			if (disposing) {
+				if (menu_items != null) {
+					// MenuItem.Dispose removes the item from the list
+					while (menu_items.Count > 0) {
+						menu_items [0].Dispose ();
+					}
 				}
-			}
+ 				if (menu_handle != IntPtr.Zero) {
+ 					menu_handle = IntPtr.Zero;
+ 				}
+ 			}
 		}
 
 		// From Microsoft documentation is impossible to guess that 
