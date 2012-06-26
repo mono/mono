c14e6390bd226a8f8d29e5acd97f87b81d4f454c
Marek Safar <marek.safar@gmail.com>
2012-06-26 22:58:26 +0100

    Don't register namespace of predefined (optinal) types

---
+++ mcs/errors/cs0234.cs
@@ -0,0 +1,12 @@
+// CS0234: The type or namespace name `Linq' does not exist in the namespace `System'. Are you missing an assembly reference?
+// Line: 3
+// Compiler options: -noconfig
+
+using System.Linq;
+
+class C
+{
+	public static void Main ()
+	{
+	}
+}
