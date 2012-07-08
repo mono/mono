b58c6c1a790bf7473fc5b8aad304532688eeed5a
James Bellinger <jfb@zer7.com>
2012-07-08 07:46:43 -0400

    Implement SemaphoreSecurity.

--- mcs/class/System/System.Threading/Semaphore.cs
+++ mcs/class/System/System.Threading/Semaphore.cs
@@ -80,7 +80,7 @@ namespace System.Threading {
 		{
 		}
 
-		[MonoTODO ("Does not support access control, semaphoreSecurity is ignored")]
+		[MonoTODO ("CreateSemaphore_internal does not support access control, semaphoreSecurity is ignored")]
 		public Semaphore (int initialCount, int maximumCount, string name, out bool createdNew, 
 			SemaphoreSecurity semaphoreSecurity)
 		{
@@ -96,10 +96,12 @@ namespace System.Threading {
 							   out createdNew);
 		}
 
-		[MonoTODO]
 		public SemaphoreSecurity GetAccessControl ()
 		{
-			throw new NotImplementedException ();
+			return new SemaphoreSecurity (SafeWaitHandle,
+						      AccessControlSections.Owner |
+						      AccessControlSections.Group |
+						      AccessControlSections.Access);
 		}
 
 		[PrePrepareMethod]
@@ -128,13 +130,12 @@ namespace System.Threading {
 			return (ret);
 		}
 
-		[MonoTODO]
 		public void SetAccessControl (SemaphoreSecurity semaphoreSecurity)
 		{
 			if (semaphoreSecurity == null)
 				throw new ArgumentNullException ("semaphoreSecurity");
-
-			throw new NotImplementedException ();
+				
+			semaphoreSecurity.PersistModifications (SafeWaitHandle);
 		}
 
 		// static methods
