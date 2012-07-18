d3c8479b4e4b7ba27cf2c432f090b993f7a1f567
Martin Potter <martin.potter@logos.com>
2012-07-17 17:54:33 -0700

    Uri.ShemeDelimiter should not be used when the host is empty.

--- mcs/class/System/System/UriBuilder.cs
+++ mcs/class/System/System/UriBuilder.cs
@@ -263,7 +263,12 @@ namespace System
 
 			builder.Append (scheme);
 			// note: mailto and news use ':', not "://", as their delimiter
-			builder.Append (Uri.GetSchemeDelimiter (scheme));
+			if (UriParser.IsKnownScheme(scheme)) {
+				builder.Append (Uri.GetSchemeDelimiter (scheme));
+			}
+			else {
+				builder.Append (host.Length > 0 ? Uri.SchemeDelimiter : ":");
+			}
 
 			if (username != String.Empty) {
 				builder.Append (username);
@@ -280,7 +285,8 @@ namespace System
 
 			if (path != String.Empty &&
 			    builder [builder.Length - 1] != '/' &&
-			    path.Length > 0 && path [0] != '/')
+			    path.Length > 0 && path [0] != '/' &&
+				host.Length > 0)
 				builder.Append ('/');
 			builder.Append (path);
 			builder.Append (query);
