d279ba43001705d31eb16c682a1aaaecf124a82c
Petr Onderka <gsvick@gmail.com>
2012-07-23 19:52:09 +0200

    Made block interfaces variant

--- mcs/class/System.Threading.Tasks.Dataflow/System.Threading.Tasks.Dataflow/ISourceBlock.cs
+++ mcs/class/System.Threading.Tasks.Dataflow/System.Threading.Tasks.Dataflow/ISourceBlock.cs
@@ -22,7 +22,7 @@
 // THE SOFTWARE.
 
 namespace System.Threading.Tasks.Dataflow {
-	public interface ISourceBlock<TOutput> : IDataflowBlock {
+	public interface ISourceBlock<out TOutput> : IDataflowBlock {
 		TOutput ConsumeMessage (DataflowMessageHeader messageHeader, ITargetBlock<TOutput> target, out bool messageConsumed);
 		IDisposable LinkTo (ITargetBlock<TOutput> target, DataflowLinkOptions linkOptions);
 		void ReleaseReservation (DataflowMessageHeader messageHeader, ITargetBlock<TOutput> target);
--- mcs/class/System.Threading.Tasks.Dataflow/System.Threading.Tasks.Dataflow/ITargetBlock.cs
+++ mcs/class/System.Threading.Tasks.Dataflow/System.Threading.Tasks.Dataflow/ITargetBlock.cs
@@ -1,6 +1,7 @@
 // ITargetBlock.cs
 //
 // Copyright (c) 2011 Jérémie "garuma" Laval
+// Copyright (c) 2012 Petr Onderka
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,21 +20,11 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-//
-//
 
-
-using System;
-using System.Threading.Tasks;
-
-namespace System.Threading.Tasks.Dataflow
-{
-	public interface ITargetBlock<TInput> : IDataflowBlock
-	{
-		DataflowMessageStatus OfferMessage (DataflowMessageHeader messageHeader,
-		                                    TInput messageValue,
-		                                    ISourceBlock<TInput> source,
-		                                    bool consumeToAccept);
+namespace System.Threading.Tasks.Dataflow {
+	public interface ITargetBlock<in TInput> : IDataflowBlock {
+		DataflowMessageStatus OfferMessage (
+			DataflowMessageHeader messageHeader, TInput messageValue,
+			ISourceBlock<TInput> source, bool consumeToAccept);
 	}
 }
-
