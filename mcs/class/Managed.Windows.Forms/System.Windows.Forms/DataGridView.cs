ecef298bcaf571a3d50e022318b20117f9d0388c
eb1@sil.org <eb1@sil.org>
2012-07-28 12:44:48 +0200

    Ensure DataGridView disposes of its columns and rows. Fixes Novell bug 660986.

--- mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridView.cs
+++ mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridView.cs
@@ -1368,6 +1368,7 @@ namespace System.Windows.Forms {
 					} else {
 						Controls.Remove (editingControl);
 					}
+					editingControl.Dispose();
 				}
 				
 				
@@ -3386,6 +3387,17 @@ namespace System.Windows.Forms {
 		}
 
 		protected override void Dispose (bool disposing) {
+			if (disposing) {
+				foreach (DataGridViewColumn column in Columns)
+					column.Dispose();
+				Columns.Clear();
+				foreach (DataGridViewRow row in Rows)
+					row.Dispose();
+				Rows.Clear();
+			}
+			editingControl = null;
+
+			base.Dispose(disposing);
 		}
 
 		protected override AccessibleObject GetAccessibilityObjectById (int objectId)
