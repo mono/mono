b0e367c025e8c65ee4aca3b85dbabb97405f71bd
Paolo Molaro <lupus@oddwiz.org>
2012-08-02 18:10:23 +0200

    Fixes xambug#2965: enqueue the correct worker instance (change from Tammo Hinrichs).

--- mcs/class/System/System.Net.Sockets/Socket.cs
+++ mcs/class/System/System.Net.Sockets/Socket.cs
@@ -1643,10 +1643,9 @@ namespace System.Net.Sockets
 			res.Size = e.Count;
 			res.EndPoint = e.RemoteEndPoint;
 			res.SockFlags = e.SocketFlags;
-			Worker worker = new Worker (e);
 			int count;
 			lock (readQ) {
-				readQ.Enqueue (worker);
+				readQ.Enqueue (e.Worker);
 				count = readQ.Count;
 			}
 			if (count == 1)
@@ -1981,10 +1980,9 @@ namespace System.Net.Sockets
 			res.Size = e.Count;
 			res.SockFlags = e.SocketFlags;
 			res.EndPoint = e.RemoteEndPoint;
-			Worker worker = new Worker (e);
 			int count;
 			lock (writeQ) {
-				writeQ.Enqueue (worker);
+				writeQ.Enqueue (e.Worker);
 				count = writeQ.Count;
 			}
 			if (count == 1)
