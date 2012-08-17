60555cbde2f89061c1d4a9abcf1f84d46cf19f81
Martin Potter <martin.potter@logos.com>
2012-08-16 19:24:31 -0700

    Regex fixes for substring parsing.

--- mcs/class/System/System.Text.RegularExpressions/Regex.cs
+++ mcs/class/System/System.Text.RegularExpressions/Regex.cs
@@ -458,7 +458,7 @@ namespace System.Text.RegularExpressions {
 				throw new ArgumentNullException ("input");
 			if (startat < 0 || startat > input.Length)
 				throw new ArgumentOutOfRangeException ("startat");
-			return CreateMachine ().Scan (this, input, startat, input.Length);
+			return CreateMachine ().Scan (this, input, startat, input.Length, false);
 		}
 
 		public Match Match (string input, int beginning, int length)
@@ -469,7 +469,7 @@ namespace System.Text.RegularExpressions {
 				throw new ArgumentOutOfRangeException ("beginning");
 			if (length < 0 || length > input.Length - beginning)
 				throw new ArgumentOutOfRangeException ("length");
-			return CreateMachine ().Scan (this, input, beginning, beginning + length);
+			return CreateMachine ().Scan (this, input, beginning, beginning + length, true);
 		}
 
 		public MatchCollection Matches (string input)
