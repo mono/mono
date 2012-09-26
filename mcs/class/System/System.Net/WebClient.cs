mono/mono

commit 512fc1943e6c230fc0f26f99f9ebb138ba5507f8

Fix #5899: WebClient.*Async() now correctly builds the query string.
I also added 'async_thread.IsBackground = true'.

These methods could also use some more love; I filed a new bug #6904
about that.

Conflicts:

	mcs/class/System/System.Net/WebClient.cs

mcs/class/System/System.Net/WebClient.cs
@@ -1054,7 +1054,8 @@ namespace System.Net
 							new DownloadDataCompletedEventArgs (null, e, canceled, args [1]));
 					}
 				});
-				object [] cb_args = new object [] {address, userToken};
+				object [] cb_args = new object [] { CreateUri (address), userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1090,7 +1091,8 @@ namespace System.Net
 						OnDownloadFileCompleted (
 							new AsyncCompletedEventArgs (e, false, args [2]));
 					}});
-				object [] cb_args = new object [] {address, fileName, userToken};
+				object [] cb_args = new object [] { CreateUri (address), fileName, userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1125,7 +1127,8 @@ namespace System.Net
 						OnDownloadStringCompleted (
 							new DownloadStringCompletedEventArgs (null, e, canceled, args [1]));
 					}});
-				object [] cb_args = new object [] {address, userToken};
+				object [] cb_args = new object [] { CreateUri (address), userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1163,7 +1166,8 @@ namespace System.Net
 					} catch (Exception e){
 						OnOpenReadCompleted (new OpenReadCompletedEventArgs (null, e, false, args [1]));
 					} });
-				object [] cb_args = new object [] {address, userToken};
+				object [] cb_args = new object [] { CreateUri (address), userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1206,7 +1210,8 @@ namespace System.Net
 						OnOpenWriteCompleted (
 							new OpenWriteCompletedEventArgs (null, e, false, args [2]));
 					}});
-				object [] cb_args = new object [] {address, method, userToken};
+				object [] cb_args = new object [] { CreateUri (address), method, userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1250,7 +1255,8 @@ namespace System.Net
 						OnUploadDataCompleted (
 							new UploadDataCompletedEventArgs (null, e, false, args [3]));
 					}});
-				object [] cb_args = new object [] {address, method, data,  userToken};
+				object [] cb_args = new object [] { CreateUri (address), method, data,  userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1293,7 +1299,8 @@ namespace System.Net
 						OnUploadFileCompleted (
 							new UploadFileCompletedEventArgs (null, e, false, args [3]));
 					}});
-				object [] cb_args = new object [] {address, method, fileName,  userToken};
+				object [] cb_args = new object [] { CreateUri (address), method, fileName,  userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1335,7 +1342,8 @@ namespace System.Net
 						OnUploadStringCompleted (
 							new UploadStringCompletedEventArgs (null, e, false, args [3]));
 					}});
-				object [] cb_args = new object [] {address, method, data, userToken};
+				object [] cb_args = new object [] { CreateUri (address), method, data, userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}
@@ -1376,7 +1384,8 @@ namespace System.Net
 						OnUploadValuesCompleted (
 							new UploadValuesCompletedEventArgs (null, e, false, args [3]));
 					}});
-				object [] cb_args = new object [] {address, method, values,  userToken};
+				object [] cb_args = new object [] { CreateUri (address), method, data,  userToken };
+				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}
 		}

commit d775e2b2c416c35b8c9aaa72755e261f2110962d

Fix my 512fc194, which broke the build.

mcs/class/System/System.Net/WebClient.cs
@@ -1384,7 +1384,7 @@ namespace System.Net
 						OnUploadValuesCompleted (
 							new UploadValuesCompletedEventArgs (null, e, false, args [3]));
 					}});
-				object [] cb_args = new object [] { CreateUri (address), method, data,  userToken };
+				object [] cb_args = new object [] { CreateUri (address), method, values,  userToken };
 				async_thread.IsBackground = true;
 				async_thread.Start (cb_args);
 			}