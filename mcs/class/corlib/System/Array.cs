a55c6e0bc73beae3d7f4e33475b3394776b6718c
Rodrigo Kumpera <kumpera@gmail.com>
2012-10-01 13:02:34 -0400

    Implement Array.UnsafeLoad and Array.UnsafeStore. Both functions ignores bounds check when possible.
    	* Array.cs: Add UnsafeLoad and UnsafeStore functions that don't perform bounds check.
    	The JIT will generate, when possible, direct a load/store. Right now the only case we can't
    	handle are stores to reference arrays.
    
    	Reference arrays are covariant on their element type which means that we must perform a runtime
    	type check on stores to preserve type safety. Since the type check is usually must more expensive
    	than the bounds check we simply ignore this for now.
    
    	The reasoning behind this decision was that given our initial target are corlib's collections and
    	they are too covariant to their element types they do need too a runtime type check on stores to ensure
    	correctness.

--- mcs/class/corlib/System/Array.cs
+++ mcs/class/corlib/System/Array.cs
@@ -3077,5 +3077,13 @@ namespace System
 		{
 			Copy (sourceArray, sourceIndex, destinationArray, destinationIndex, length);
 		}
+
+		internal static T UnsafeLoad<T> (T[] array, int index) {
+			return array [index];
+		}
+
+		internal static void UnsafeStore<T> (T[] array, int index, T value) {
+			array [index] = value;
+		}
 	}
 }
