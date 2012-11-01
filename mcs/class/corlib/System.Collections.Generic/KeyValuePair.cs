d6396791cebc5b7eff726e796eac96ca9573a8f1
Alan McGovern <alan@xamarin.com>
2012-11-01 15:47:18 +0000

    [corlib] Remove the DebuggerDisplay attribute from KeyValuePair and DictionaryEntry
    This attribute gives us a horrible debugging experience when the
    'Value' of the KeyValuePair is null. By using this attribute, the
    user is told that the KeyValuePair itself is null when they inspect
    something like 'new KeyValuePair<string, string> (null, null)'.
    This is very confusing.
    
    We should just let the ToString method be invoked, which returns a
    nicely formatted string. This is also the behaviour observed when
    debugging using Visual Studio.

--- mcs/class/corlib/System.Collections.Generic/KeyValuePair.cs
+++ mcs/class/corlib/System.Collections.Generic/KeyValuePair.cs
@@ -35,7 +35,6 @@ using System.Diagnostics;
 
 namespace System.Collections.Generic {
 	[Serializable]
-	[DebuggerDisplay ("{value}", Name="[{key}]")]	
 	public struct KeyValuePair<TKey,TValue> {
 		private TKey key;
 		private TValue value;
