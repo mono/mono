7bec3945c1e0ad6aef1aa72f36aaa1a64cca1632
Zoltan Varga <vargaz@gmail.com>
2013-01-09 13:16:42 +0100

    Fix the check for autoheader in eglib/autogen.sh and libgc/autogen.sh too.

--- libgc/autogen.sh
+++ libgc/autogen.sh
@@ -98,7 +98,7 @@ aclocal $ACLOCAL_FLAGS || {
   exit 1
 }
 
-if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
+if grep "^AC_CONFIG_HEADERS" configure.in >/dev/null; then
   echo "Running autoheader..."
   autoheader || { echo "**Error**: autoheader failed."; exit 1; }
 fi
