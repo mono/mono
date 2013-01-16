2e2a5167cad51af56c5b22bcf01175401e3ab10e
Rodrigo Kumpera <kumpera@gmail.com>
2013-01-16 14:58:56 -0500

    Add long links support to Boehm.
    	* gc.h: Added parts of boehm 7.3a long link API.
    
    	* finalize.c (GC_finalize): Implemented long link support by adapting
    	code from boehm 7.3a.
    	The idea is to have two table of links, one for regular and another
    	for the long links. Regular links are processed before finalization
    	and the long ones right after. The bulk of the changes are abstracting
    	the disappearing link hashtable into a data structure so we can have
    	two of them.
    
    	Boehm code came from github.com/ivmai/bdwgc.git. Authors of the
    	relevant changes are Zach Saw and Ivan Maidanski.

--- libgc/include/gc.h
+++ libgc/include/gc.h
@@ -600,6 +600,8 @@ GC_API GC_PTR GC_debug_realloc_replacement
 #   define GC_END_STUBBORN_CHANGE(p) GC_debug_end_stubborn_change(p)
 #   define GC_GENERAL_REGISTER_DISAPPEARING_LINK(link, obj) \
 	GC_general_register_disappearing_link(link, GC_base(obj))
+#   define GC_REGISTER_LONG_LINK(link, obj) \
+	GC_register_long_link(link, GC_base(obj))
 #   define GC_REGISTER_DISPLACEMENT(n) GC_debug_register_displacement(n)
 # else
 #   define GC_MALLOC(sz) GC_malloc(sz)
@@ -622,6 +624,8 @@ GC_API GC_PTR GC_debug_realloc_replacement
 #   define GC_END_STUBBORN_CHANGE(p) GC_end_stubborn_change(p)
 #   define GC_GENERAL_REGISTER_DISAPPEARING_LINK(link, obj) \
 	GC_general_register_disappearing_link(link, obj)
+#   define GC_REGISTER_LONG_LINK(link, obj) \
+	GC_register_long_link(link, obj)
 #   define GC_REGISTER_DISPLACEMENT(n) GC_register_displacement(n)
 # endif
 /* The following are included because they are often convenient, and	*/
@@ -760,6 +764,9 @@ GC_API int GC_unregister_disappearing_link GC_PROTO((GC_PTR * /* link */));
 	/* Returns 0 if link was not actually registered.	*/
 	/* Undoes a registration by either of the above two	*/
 	/* routines.						*/
+
+GC_API int GC_register_long_link GC_PROTO((GC_PTR * /* link */, GC_PTR obj));
+GC_API int GC_unregister_long_link GC_PROTO((GC_PTR * /* link */));
 
 /* Returns !=0  if GC_invoke_finalizers has something to do. 		*/
 GC_API int GC_should_invoke_finalizers GC_PROTO((void));
