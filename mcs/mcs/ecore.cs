82bfae8a22686756e692dc97a0b7977a485ba01f
Marek Safar <marek.safar@gmail.com>
2013-01-30 15:54:42 +0100

    Better error message for missing using clause in alias scope. Fixes #9813

--- mcs/mcs/ecore.cs
+++ mcs/mcs/ecore.cs
@@ -2323,10 +2323,16 @@ namespace Mono.CSharp {
 
 			var ns_candidates = ctx.Module.GlobalRootNamespace.FindTypeNamespaces (ctx, Name, Arity);
 			if (ns_candidates != null) {
-				string usings = string.Join ("' or `", ns_candidates.ToArray ());
-				report.Error (246, loc,
-					"The type or namespace name `{0}' could not be found. Are you missing `{1}' using directive?",
-					Name, usings);
+				if (ctx is UsingAliasNamespace.AliasContext) {
+					report.Error (246, loc,
+						"The type or namespace name `{1}' could not be found. Consider using fully qualified name `{0}.{1}'",
+						ns_candidates[0], Name);
+				} else {
+					string usings = string.Join ("' or `", ns_candidates.ToArray ());
+					report.Error (246, loc,
+						"The type or namespace name `{0}' could not be found. Are you missing `{1}' using directive?",
+						Name, usings);
+				}
 			} else {
 				report.Error (246, loc,
 					"The type or namespace name `{0}' could not be found. Are you missing an assembly reference?",
