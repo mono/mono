3bc8c421833b44d3350a4e3d375478f6dfd45f08
Jérémie Laval <jeremie.laval@gmail.com>
2013-02-24 20:34:08 +0100

    [System] Add stub for System.Net.WebSockets

---
+++ mcs/class/System/System.Net.WebSockets/WebSocketReceiveResult.cs
@@ -0,0 +1,82 @@
+//
+// WebSocketReceiveResult.cs
+//
+// Authors:
+//    Jérémie Laval <jeremie dot laval at xamarin dot com>
+//
+// Copyright 2013 Xamarin Inc (http://www.xamarin.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+#if NET_4_5
+
+using System;
+using System.Security.Principal;
+using System.Runtime.CompilerServices;
+
+namespace System.Net.WebSockets
+{
+	public class WebSocketReceiveResult
+	{
+		[MonoTODO]
+		public WebSocketReceiveResult (int count, WebSocketMessageType messageType, bool endOfMessage)
+		{
+			throw new NotImplementedException ();
+		}
+
+		[MonoTODO]
+		public WebSocketReceiveResult (int count,
+		                               WebSocketMessageType messageType,
+		                               bool endOfMessage,
+		                               WebSocketCloseStatus? closeStatus,
+		                               string closeStatusDescription)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public WebSocketCloseStatus? CloseStatus {
+			get;
+			private set;
+		}
+
+		public string CloseStatusDescription {
+			get;
+			private set;
+		}
+
+		public int Count {
+			get;
+			private set;
+		}
+
+		public bool EndOfMessage {
+			get;
+			private set;
+		}
+
+		public WebSocketMessageType MessageType {
+			get;
+			private set;
+		}
+	}
+}
+
+#endif
