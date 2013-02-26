cb454b481719b4879e2cc1e551f48b806a0226e2
Duncan Mak <duncan.mak@xamarin.com>
2013-02-26 17:24:30 -0500

    Remove trailing whitespace.

--- mcs/class/corlib/Mono/Runtime.cs
+++ mcs/class/corlib/Mono/Runtime.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -36,10 +36,10 @@ namespace Mono {
 #endif
 	static class Runtime
 	{
-		
+
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
 		private static extern void mono_runtime_install_handlers ();
-		
+
 		static internal void InstallSignalHandlers ()
 		{
 			mono_runtime_install_handlers ();
@@ -57,5 +57,4 @@ namespace Mono {
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
 		public static extern bool SetGCAllowSynchronousMajor (bool flag);
 	}
-	
 }
