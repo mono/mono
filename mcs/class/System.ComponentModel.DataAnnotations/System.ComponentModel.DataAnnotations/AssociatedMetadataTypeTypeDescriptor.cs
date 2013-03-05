f86e2a1025451ab437db44f4889ae30bc5005e8b
Martin Baulig <martin.baulig@xamarin.com>
2013-03-05 16:33:32 -0500

    Add System.ComponentModel.DataAnnotations to the mobile build.

--- mcs/class/System.ComponentModel.DataAnnotations/System.ComponentModel.DataAnnotations/AssociatedMetadataTypeTypeDescriptor.cs
+++ mcs/class/System.ComponentModel.DataAnnotations/System.ComponentModel.DataAnnotations/AssociatedMetadataTypeTypeDescriptor.cs
@@ -27,6 +27,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+#if !MOBILE
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -166,3 +167,4 @@ namespace System.ComponentModel.DataAnnotations
 		}
 	}
 }
+#endif
