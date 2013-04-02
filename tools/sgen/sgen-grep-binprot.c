359f977e79ed0d5b32e9e8d386949c25828d8dd0
Mark Probst <mark.probst@gmail.com>
2013-04-02 10:11:36 -0700

    [sgen] Binary protocol entry for user-forced GCs.

--- tools/sgen/sgen-grep-binprot.c
+++ tools/sgen/sgen-grep-binprot.c
@@ -19,6 +19,7 @@ read_entry (FILE *in, void **data)
 	if (fread (&type, 1, 1, in) != 1)
 		return SGEN_PROTOCOL_EOF;
 	switch (type) {
+	case SGEN_PROTOCOL_COLLECTION_FORCE: size = sizeof (SGenProtocolCollectionForce); break;
 	case SGEN_PROTOCOL_COLLECTION_BEGIN: size = sizeof (SGenProtocolCollection); break;
 	case SGEN_PROTOCOL_COLLECTION_END: size = sizeof (SGenProtocolCollection); break;
 	case SGEN_PROTOCOL_ALLOC: size = sizeof (SGenProtocolAlloc); break;
@@ -61,6 +62,11 @@ static void
 print_entry (int type, void *data)
 {
 	switch (type) {
+	case SGEN_PROTOCOL_COLLECTION_FORCE: {
+		SGenProtocolCollectionForce *entry = data;
+		printf ("collection force generation %d\n", entry->generation);
+		break;
+	}
 	case SGEN_PROTOCOL_COLLECTION_BEGIN: {
 		SGenProtocolCollection *entry = data;
 		printf ("collection begin %d generation %d\n", entry->index, entry->generation);
@@ -201,6 +207,7 @@ static gboolean
 is_match (gpointer ptr, int type, void *data)
 {
 	switch (type) {
+	case SGEN_PROTOCOL_COLLECTION_FORCE:
 	case SGEN_PROTOCOL_COLLECTION_BEGIN:
 	case SGEN_PROTOCOL_COLLECTION_END:
 	case SGEN_PROTOCOL_THREAD_SUSPEND:
