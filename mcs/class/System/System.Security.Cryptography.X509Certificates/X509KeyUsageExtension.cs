3b058f06808d3caa8507a7e4cfff38b058642a8e
Marek Safar <marek.safar@gmail.com>
2013-04-05 11:14:30 +0200

    Redirect more duplicate security types to Mono.Security

--- mcs/class/System/System.Security.Cryptography.X509Certificates/X509KeyUsageExtension.cs
+++ mcs/class/System/System.Security.Cryptography.X509Certificates/X509KeyUsageExtension.cs
@@ -30,9 +30,11 @@
 
 #if SECURITY_DEP
 
+extern alias MonoSecurity;
+
 using System.Text;
 
-using Mono.Security;
+using MonoSecurity::Mono.Security;
 
 namespace System.Security.Cryptography.X509Certificates {
 
