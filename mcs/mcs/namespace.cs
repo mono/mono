52ba2fff6fae70840ef04fd9f201377c76863640
Marek Safar <marek.safar@gmail.com>
2013-05-09 12:37:45 +0200

    Nested dotted namespace can access its parent dotted namespace directly. Fixes #12165

--- mcs/mcs/namespace.cs
+++ mcs/mcs/namespace.cs
@@ -276,16 +276,7 @@ namespace Mono.CSharp {
 
 		public Namespace AddNamespace (MemberName name)
 		{
-			Namespace ns_parent;
-			if (name.Left != null) {
-				if (parent != null)
-					ns_parent = parent.AddNamespace (name.Left);
-				else
-					ns_parent = AddNamespace (name.Left);
-			} else {
-				ns_parent = this;
-			}
-
+			var ns_parent = name.Left == null ? this : AddNamespace (name.Left);
 			return ns_parent.TryAddNamespace (name.Basename);
 		}
 
