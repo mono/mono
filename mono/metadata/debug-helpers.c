mono/mono

commit 3f3092b4776a554abcf13f698a488adf749bba57

Add support for separating the class and method names by :: in mono_method_desc_new (). Fixes #12113.

mono/metadata/debug-helpers.c
@@ -326,10 +326,10 @@ mono_method_desc_new (const char *name, gboolean include_namespace)
 		g_free (class_nspace);
 		return NULL;
 	}
-	*method_name++ = 0;
 	/* allow two :: to separate the method name */
-	if (*method_name == ':')
-		method_name++;
+	if (method_name [-1] == ':')
+		method_name [-1] = 0;
+	*method_name++ = 0;
 	class_name = strrchr (class_nspace, '.');
 	if (class_name) {
 		*class_name++ = 0;

commit 437c3af12ea9edf7a8027066c61c84f13d3082a8

Fix a reading of invalid memory error introduced by 3f3092b4776a554abcf13f698a488adf749bba57.

mono/metadata/debug-helpers.c
@@ -327,7 +327,7 @@ mono_method_desc_new (const char *name, gboolean include_namespace)
 		return NULL;
 	}
 	/* allow two :: to separate the method name */
-	if (method_name [-1] == ':')
+	if (method_name != class_nspace && method_name [-1] == ':')
 		method_name [-1] = 0;
 	*method_name++ = 0;
 	class_name = strrchr (class_nspace, '.');