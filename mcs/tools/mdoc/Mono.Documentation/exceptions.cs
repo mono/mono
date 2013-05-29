574dbc8ec4a5975731dc938003b6e83ac4134667
Jonathan Pryor <jonpryor@vt.edu>
2013-05-29 11:53:22 -0400

    [mdoc] Fix unit tests.
    3d3fe6af (among other commits...) "broke" the `mdoc` unit tests, as it
    did two things:
    
     1. It added a new codepath calling string.Format(), thus introducing
        a new set of exceptions.
     2. Due to an `mdoc` bug, (1) resulted in a stack overflow on OS X.
    
    (1) demonstrates that the mdoc unit tests might be too brittle.
    
    (2) was fixed in 04bf9ad.
    
    The problem with 04bf9ad, though, is that it resulted in a gigantic
    ~212MB diff, because while the recursion was "fixed", we could still
    process the same method multiple times, resulting in _lots_ of
    duplication, e.g. System.String.get_Chars() would be repeated hundreds
    of times for a given exception.
    
    Improve the source reporting mechanism so that instead of a
    List<MemberReference> we use a HashSet<MemberReference>, listing each
    source location _once_ instead of repeating them.
    
    The _formatting_ of the <exception/> element is also changed so that
    each source location is on its own line, making it easier to read and
    review the locations.

--- mcs/tools/mdoc/Mono.Documentation/exceptions.cs
+++ mcs/tools/mdoc/Mono.Documentation/exceptions.cs
@@ -47,13 +47,11 @@ namespace Mono.Documentation {
 		internal ExceptionSources (TypeReference exception)
 		{
 			Exception = exception;
-			SourcesList = new List<MemberReference> ();
-			Sources = new ReadOnlyCollection<MemberReference> (SourcesList);
+			Sources   = new HashSet<MemberReference> ();
 		}
 
 		public TypeReference Exception { get; private set; }
-		public ReadOnlyCollection<MemberReference> Sources { get; private set; }
-		internal List<MemberReference> SourcesList;
+		internal  HashSet<MemberReference>  Sources;
 	}
 
 
@@ -93,13 +91,13 @@ namespace Mono.Documentation {
 				Dictionary<string, ExceptionSources> e;
 				if (!db.TryGetValue (memberDecl, out e)) {
 					e = new Dictionary<string, ExceptionSources> ();
+					db.Add (memberDecl, e);
 					var bodies = GetMethodBodies (member);
 					foreach (var body in bodies) {
 						if (body == null)
 							continue;
 						FillExceptions (body, e);
 					}
-					db.Add (memberDecl, e);
 				}
 				return e.Values;
 			}
@@ -146,9 +144,6 @@ namespace Mono.Documentation {
 								((locations & ExceptionLocations.DependentAssemblies) != 0 && 
 									body.Method.DeclaringType.Scope.Name != memberRef.DeclaringType.Scope.Name)) {
 
-							if (memberRef.Resolve () == body.Method)
-								break;
-
 							IEnumerable<ExceptionSources> memberExceptions = this [memberRef];
 							AddExceptions (body, instruction, 
 									memberExceptions.Select (es => es.Exception),
@@ -185,7 +180,8 @@ namespace Mono.Documentation {
 						s = new ExceptionSources (ex);
 						exceptions.Add (eName, s);
 					}
-					s.SourcesList.AddRange (sources);
+					foreach (var m in sources)
+						s.Sources.Add (m);
 				}
 			}
 		}
