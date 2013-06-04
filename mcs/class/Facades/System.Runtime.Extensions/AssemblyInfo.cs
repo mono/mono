25dbd0829effeff0b2aa1db5d7cf67a8e7641eaa
Martin Baulig <martin.baulig@xamarin.com>
2013-06-04 18:14:53 +0200

    [PCL]: Set Facade Assemblies' version number to "4.0.0.0".

--- mcs/class/Facades/System.Runtime.Extensions/AssemblyInfo.cs
+++ mcs/class/Facades/System.Runtime.Extensions/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany (Consts.MonoCompany)]
 [assembly: AssemblyProduct (Consts.MonoProduct)]
 [assembly: AssemblyCopyright (Consts.MonoCopyright)]
-[assembly: AssemblyVersion (Consts.FxVersion)]
-[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
-[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: AssemblyVersion ("4.0.0.0")]
+[assembly: AssemblyInformationalVersion ("4.0.0.0")]
+[assembly: AssemblyFileVersion ("4.0.0.0")]
 [assembly: AssemblyDelaySign (true)]
 [assembly: AssemblyKeyFile ("../../msfinal.pub")]
 
