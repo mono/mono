8a10b06610c67267d5f0940bdec8f0b64f478443
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-08-05 15:36:24 +0900

    Fix bug #13485: retrieve private fields and properties in serialization.

--- mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/TypeMap.cs
+++ mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/TypeMap.cs
@@ -81,10 +81,9 @@ namespace System.Runtime.Serialization.Json
 		static TypeMap CreateDefaultTypeMap (Type type)
 		{
 			var l = new List<TypeMapMember> ();
-			foreach (var fi in type.GetFields ())
-				if (!fi.IsStatic)
-					l.Add (new TypeMapField (fi, null));
-			foreach (var pi in type.GetProperties ())
+			foreach (var fi in type.GetFields (binding_flags))
+				l.Add (new TypeMapField (fi, null));
+			foreach (var pi in type.GetProperties (binding_flags))
 				if (pi.CanRead && pi.CanWrite && !pi.GetGetMethod ().IsStatic && pi.GetIndexParameters ().Length == 0)
 					l.Add (new TypeMapProperty (pi, null));
 			l.Sort ((x, y) => x.Order != y.Order ? x.Order - y.Order : String.Compare (x.Name, y.Name, StringComparison.Ordinal));
@@ -116,7 +115,7 @@ namespace System.Runtime.Serialization.Json
 
 			List<TypeMapMember> members = new List<TypeMapMember> ();
 
-			foreach (FieldInfo fi in type.GetFields (BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)) {
+			foreach (FieldInfo fi in type.GetFields (binding_flags)) {
 				if (dca != null) {
 					object [] atts = fi.GetCustomAttributes (typeof (DataMemberAttribute), true);
 					if (atts.Length == 0)
@@ -131,7 +130,7 @@ namespace System.Runtime.Serialization.Json
 			}
 
 			if (dca != null) {
-				foreach (PropertyInfo pi in type.GetProperties (BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)) {
+				foreach (PropertyInfo pi in type.GetProperties (binding_flags)) {
 					object [] atts = pi.GetCustomAttributes (typeof (DataMemberAttribute), true);
 					if (atts.Length == 0)
 						continue;
