41298f44b0450a20fb0bd495629cde52f8911a86
Francis Fisher <frankie@terrorise.me.uk>
2013-08-06 09:50:22 +0100

    Licenced my work under MIT per coding standards

--- mcs/class/System.Windows.Forms.DataVisualization/System.Windows.Forms.DataVisualization.Charting/Annotation.cs
+++ mcs/class/System.Windows.Forms.DataVisualization/System.Windows.Forms.DataVisualization.Charting/Annotation.cs
@@ -1,6 +1,26 @@
 // Authors:
 // Francis Fisher (frankie@terrorise.me.uk)
 //
+// (C) Francis Fisher 2013
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE./
 
 using System;
 using System.Drawing;
@@ -28,7 +48,7 @@ namespace System.Windows.Forms.DataVisualization.Charting
 		public virtual double AnchorX { get; set; }
 		public virtual double AnchorY { get; set; }
 		public AnnotationGroup AnnotationGroup { get; private set; }
-		public abstract string AnnotationType { get; } //FIXME: doesn't have setter according to docs
+		public abstract string AnnotationType { get; } 
 		public virtual Axis AxisX { get; set; }
 		public virtual string AxisXName { get; set; }
 		public virtual Axis AxisY { get; set; }
