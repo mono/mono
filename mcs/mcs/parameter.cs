155df94118af1c39a52d0080aaca4687d4caae14
Marek Safar <marek.safar@gmail.com>
2013-08-07 15:16:18 +0200

    Inflate default parameter expression without type checks. Fixes #13793

--- mcs/mcs/parameter.cs
+++ mcs/mcs/parameter.cs
@@ -972,10 +972,18 @@ namespace Mono.CSharp {
 					if (inflated_types[i] == expr.Type)
 						continue;
 
-					if (expr is DefaultValueExpression)
+					var c = expr as Constant;
+					if (c != null) {
+						//
+						// It may fail we are inflating before type validation is done
+						//
+						c = Constant.ExtractConstantFromValue (inflated_types[i], c.GetValue (), expr.Location);
+						if (c == null)
+							expr = new DefaultValueExpression (new TypeExpression (inflated_types[i], expr.Location), expr.Location);
+						else
+							expr = c;
+					} else if (expr is DefaultValueExpression)
 						expr = new DefaultValueExpression (new TypeExpression (inflated_types[i], expr.Location), expr.Location);
-					else if (expr is Constant)
-						expr = Constant.CreateConstantFromValue (inflated_types[i], ((Constant) expr).GetValue (), expr.Location);
 
 					clone.FixedParameters[i] = new ParameterData (fp.Name, fp.ModFlags, expr);
 				}
