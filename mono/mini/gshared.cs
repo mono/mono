mono/mono

commit d84a165c8e799d8734df5f51a6dcf12b1f2a3695

Fix enum->int unbox casts in gsharedvt code. Fixes desk #42463.

mono/mini/gshared.cs
@@ -212,6 +212,12 @@ public class Tests
 		return 0;
 	}
 
+	public static int test_0_unbox_any_enum () {
+		IFaceUnbox iface = new ClassUnbox ();
+		AnEnum res = iface.Unbox<AnEnum, int> (AnEnum.One, 0, 1);
+		return res == AnEnum.Two ? 0 : 1;
+	}
+
 	[MethodImplAttribute (MethodImplOptions.NoInlining)]
 	static void ldfld_nongeneric<T> (GFoo<T>[] foo, int[] arr) {
 		arr [0] = foo [0].i;
@@ -914,7 +920,8 @@ public class Tests
 	}
 
 	enum AnEnum {
-		One
+		One,
+		Two
 	};
 
 	public static int test_0_constrained_tostring () {

mono/mini/jit-icalls.c
@@ -1052,7 +1052,7 @@ mono_create_corlib_exception_2 (guint32 token, MonoString *arg1, MonoString *arg
 }
 
 MonoObject*
-mono_object_castclass (MonoObject *obj, MonoClass *klass)
+mono_object_castclass_unbox (MonoObject *obj, MonoClass *klass)
 {
 	MonoJitTlsData *jit_tls = NULL;
 
@@ -1064,8 +1064,12 @@ mono_object_castclass (MonoObject *obj, MonoClass *klass)
 	if (!obj)
 		return NULL;
 
-	if (mono_object_isinst (obj, klass))
+	if (klass->enumtype) {
+		if (obj->vtable->klass == klass->element_class)
+			return obj;
+	} else if (mono_object_isinst (obj, klass)) {
 		return obj;
+	}
 
 	if (mini_get_debug_options ()->better_cast_details) {
 		jit_tls->class_cast_from = obj->vtable->klass;

mono/mini/jit-icalls.h
@@ -165,7 +165,7 @@ MonoException *mono_create_corlib_exception_1 (guint32 token, MonoString *arg) M
 
 MonoException *mono_create_corlib_exception_2 (guint32 token, MonoString *arg1, MonoString *arg2) MONO_INTERNAL;
 
-MonoObject* mono_object_castclass (MonoObject *obj, MonoClass *klass) MONO_INTERNAL;
+MonoObject* mono_object_castclass_unbox (MonoObject *obj, MonoClass *klass) MONO_INTERNAL;
 
 gpointer mono_get_native_calli_wrapper (MonoImage *image, MonoMethodSignature *sig, gpointer func) MONO_INTERNAL;
 

mono/mini/method-to-ir.c
@@ -3533,7 +3533,7 @@ handle_unbox_gsharedvt (MonoCompile *cfg, int context_used, MonoClass *klass, Mo
 	args [1] = klass_inst;
 
 	/* CASTCLASS */
-	obj = mono_emit_jit_icall (cfg, mono_object_castclass, args);
+	obj = mono_emit_jit_icall (cfg, mono_object_castclass_unbox, args);
 
 	NEW_BBLOCK (cfg, is_ref_bb);
 	NEW_BBLOCK (cfg, is_nullable_bb);

mono/mini/mini.c
@@ -7255,7 +7255,7 @@ mini_init (const char *filename, const char *runtime_version)
 	register_icall (mono_helper_ldstr_mscorlib, "mono_helper_ldstr_mscorlib", "object int", FALSE);
 	register_icall (mono_helper_newobj_mscorlib, "mono_helper_newobj_mscorlib", "object int", FALSE);
 	register_icall (mono_value_copy, "mono_value_copy", "void ptr ptr ptr", FALSE);
-	register_icall (mono_object_castclass, "mono_object_castclass", "object object ptr", FALSE);
+	register_icall (mono_object_castclass_unbox, "mono_object_castclass_unbox", "object object ptr", FALSE);
 	register_icall (mono_break, "mono_break", NULL, TRUE);
 	register_icall (mono_create_corlib_exception_0, "mono_create_corlib_exception_0", "object int", TRUE);
 	register_icall (mono_create_corlib_exception_1, "mono_create_corlib_exception_1", "object int object", TRUE);

commit 74fc821cf6e1831a98365d781900ebc8d218e8ec

Fix a regression introduced by d84a165c8e799d8734df5f51a6dcf12b1f2a3695. Unbox casts were failing for enums. Fixes #14026.

mono/mini/gshared.cs
@@ -215,7 +215,12 @@ public class Tests
 	public static int test_0_unbox_any_enum () {
 		IFaceUnbox iface = new ClassUnbox ();
 		AnEnum res = iface.Unbox<AnEnum, int> (AnEnum.One, 0, 1);
-		return res == AnEnum.Two ? 0 : 1;
+		if (res != AnEnum.Two)
+			return 1;
+		res = iface.Unbox<AnEnum, int> (AnEnum.One, 0, AnEnum.Two);
+		if (res != AnEnum.Two)
+			return 2;
+		return 0;
 	}
 
 	[MethodImplAttribute (MethodImplOptions.NoInlining)]

mono/mini/jit-icalls.c
@@ -1071,12 +1071,10 @@ mono_object_castclass_unbox (MonoObject *obj, MonoClass *klass)
 	if (!obj)
 		return NULL;
 
-	if (klass->enumtype) {
-		if (obj->vtable->klass == klass->element_class)
-			return obj;
-	} else if (mono_object_isinst (obj, klass)) {
+	if (klass->enumtype && obj->vtable->klass == klass->element_class)
 		return obj;
-	}
+	if (mono_object_isinst (obj, klass))
+		return obj;
 
 	if (mini_get_debug_options ()->better_cast_details) {
 		jit_tls->class_cast_from = obj->vtable->klass;