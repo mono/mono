8bd66dbbeb710db965df602a7c4a77a24a0dfe27
Marek Safar <marek.safar@gmail.com>
2013-09-10 10:13:49 +0200

    Add System.ServiceModel.Activation assembly. Fixes #14544

--- mcs/class/System.ServiceModel/Assembly/AssemblyInfo.cs
+++ mcs/class/System.ServiceModel/Assembly/AssemblyInfo.cs
@@ -38,7 +38,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about the System.ServiceModel assembly
-// v3.0 Assembly
 
 [assembly: AssemblyTitle ("System.ServiceModel.dll")]
 [assembly: AssemblyDescription ("System.ServiceModel.dll")]
@@ -75,4 +74,12 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo ("System.ServiceModel.Routing, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
 [assembly: InternalsVisibleTo ("System.ServiceModel.Discovery, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")] // AnnouncementChannelEndpointElementCollection requires it.
 #endif
+
+#if NET_4_0 && HAS_ACTIVATION
+
+[assembly: TypeForwardedTo (typeof (System.ServiceModel.ServiceHostingEnvironment))]
+[assembly: TypeForwardedTo (typeof (System.ServiceModel.Activation.ServiceHostFactory))]
+
+#endif
+
 
