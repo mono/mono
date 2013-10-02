7e01f047fa03d2860abedf18a3bdcb03aebb8cfb
Marek Safar <marek.safar@gmail.com>
2013-10-02 17:29:12 -0400

    Emit better debugger attribute for stepping over state machine initialization

--- mcs/mcs/attribute.cs
+++ mcs/mcs/attribute.cs
@@ -1650,6 +1650,7 @@ namespace Mono.CSharp {
 		public readonly PredefinedAttribute UnsafeValueType;
 		public readonly PredefinedAttribute UnmanagedFunctionPointer;
 		public readonly PredefinedDebuggerBrowsableAttribute DebuggerBrowsable;
+		public readonly PredefinedAttribute DebuggerStepThrough;
 
 		// New in .NET 3.5
 		public readonly PredefinedAttribute Extension;
@@ -1716,6 +1717,7 @@ namespace Mono.CSharp {
 			UnsafeValueType = new PredefinedAttribute (module, "System.Runtime.CompilerServices", "UnsafeValueTypeAttribute");
 			UnmanagedFunctionPointer = new PredefinedAttribute (module, "System.Runtime.InteropServices", "UnmanagedFunctionPointerAttribute");
 			DebuggerBrowsable = new PredefinedDebuggerBrowsableAttribute (module, "System.Diagnostics", "DebuggerBrowsableAttribute");
+			DebuggerStepThrough = new PredefinedAttribute (module, "System.Diagnostics", "DebuggerStepThroughAttribute");
 
 			Extension = new PredefinedAttribute (module, "System.Runtime.CompilerServices", "ExtensionAttribute");
 
