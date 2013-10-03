mono/mono

commit 3912066fe1134ba665770723a24c8ea9b4f1aea3

Anonymous method with this access inside an iterator needs to initialize captured this field from iterator's copy. Fixes #14926

mcs/mcs/anonymous.cs
@@ -191,15 +191,32 @@ namespace Mono.CSharp {
 		sealed class ThisInitializer : Statement
 		{
 			readonly HoistedThis hoisted_this;
+			readonly AnonymousMethodStorey parent;
 
-			public ThisInitializer (HoistedThis hoisted_this)
+			public ThisInitializer (HoistedThis hoisted_this, AnonymousMethodStorey parent)
 			{
 				this.hoisted_this = hoisted_this;
+				this.parent = parent;
+			}
+
+			public override bool Resolve (BlockContext bc)
+			{
+				return base.Resolve (bc);
 			}
 
 			protected override void DoEmit (EmitContext ec)
 			{
-				hoisted_this.EmitAssign (ec, new CompilerGeneratedThis (ec.CurrentType, loc), false, false);
+				Expression source = EmptyExpression.Null;
+
+				if (parent == null)
+					source = new CompilerGeneratedThis (ec.CurrentType, loc);
+				else {
+					source = new FieldExpr (parent.HoistedThis.Field, Location.Null) {
+						InstanceExpression = new CompilerGeneratedThis (ec.CurrentType, Location.Null)
+					};
+				}
+
+				hoisted_this.EmitAssign (ec, source, false, false);
 			}
 
 			protected override void CloneTo (CloneContext clonectx, Statement target)
@@ -229,6 +246,7 @@ namespace Mono.CSharp {
 		public Expression Instance;
 
 		bool initialize_hoisted_this;
+		AnonymousMethodStorey hoisted_this_parent;
 
 		public AnonymousMethodStorey (ExplicitBlock block, TypeDefinition parent, MemberBase host, TypeParameters tparams, string name, MemberKind kind)
 			: base (parent, MakeMemberName (host, name, parent.Module.CounterAnonymousContainers, tparams, block.StartLocation),
@@ -238,13 +256,14 @@ namespace Mono.CSharp {
 			ID = parent.Module.CounterAnonymousContainers++;
 		}
 
-		public void AddCapturedThisField (EmitContext ec)
+		public void AddCapturedThisField (EmitContext ec, AnonymousMethodStorey parent)
 		{
 			TypeExpr type_expr = new TypeExpression (ec.CurrentType, Location);
 			Field f = AddCompilerGeneratedField ("$this", type_expr);
 			hoisted_this = new HoistedThis (this, f);
 
 			initialize_hoisted_this = true;
+			hoisted_this_parent = parent;
 		}
 
 		public Field AddCapturedVariable (string name, TypeSpec type)
@@ -553,7 +572,7 @@ namespace Mono.CSharp {
 			// referenced indirectly
 			//
 			if (initialize_hoisted_this) {
-				rc.CurrentBlock.AddScopeStatement (new ThisInitializer (hoisted_this));
+				rc.CurrentBlock.AddScopeStatement (new ThisInitializer (hoisted_this, hoisted_this_parent));
 			}
 
 			//
@@ -1680,13 +1699,13 @@ namespace Mono.CSharp {
 							//
 							parent = sm_parent.Parent.PartialContainer;
 						}
+					} else {
+						//
+						// For iterators we can host everything in one class
+						//
+						if (sm is IteratorStorey)
+							parent = storey = sm;
 					}
-
-					//
-					// For iterators we can host everything in one class
-					//
-					if (sm is IteratorStorey)
-						parent = storey = sm;
 				}
 
 				modifiers = storey != null ? Modifiers.INTERNAL : Modifiers.PRIVATE;

mcs/tests/ver-il-net_4_5.xml
@@ -24422,6 +24422,61 @@
       </method>
     </type>
   </test>
+  <test name="gtest-iter-30.cs">
+    <type name="Program">
+      <method name="Void Main()" attrs="150">
+        <size>68</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+    </type>
+    <type name="M">
+      <method name="System.Collections.Generic.IEnumerable`1[System.Int32] Test()" attrs="134">
+        <size>30</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+    </type>
+    <type name="M+&lt;Test&gt;c__Iterator0">
+      <method name="Int32 System.Collections.Generic.IEnumerator&lt;int&gt;.get_Current()" attrs="2529">
+        <size>14</size>
+      </method>
+      <method name="System.Object System.Collections.IEnumerator.get_Current()" attrs="2529">
+        <size>19</size>
+      </method>
+      <method name="Boolean MoveNext()" attrs="486">
+        <size>101</size>
+      </method>
+      <method name="Void Dispose()" attrs="486">
+        <size>15</size>
+      </method>
+      <method name="Void Reset()" attrs="486">
+        <size>6</size>
+      </method>
+      <method name="IEnumerator System.Collections.IEnumerable.GetEnumerator()" attrs="481">
+        <size>14</size>
+      </method>
+      <method name="System.Collections.Generic.IEnumerator`1[System.Int32] System.Collections.Generic.IEnumerable&lt;int&gt;.GetEnumerator()" attrs="481">
+        <size>40</size>
+      </method>
+      <method name="Void &lt;Test&gt;m__0()" attrs="129">
+        <size>52</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+    </type>
+    <type name="M+&lt;Test&gt;c__AnonStorey1">
+      <method name="Void &lt;&gt;m__1()" attrs="131">
+        <size>24</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+    </type>
+  </test>
   <test name="gtest-lambda-01.cs">
     <type name="IntFunc">
       <method name="Int32 Invoke(Int32)" attrs="454">

mcs/mcs/statement.cs
@@ -2609,7 +2609,7 @@ namespace Mono.CSharp {
 							continue;
 
 						if (storey.HoistedThis == null) {
-							storey.AddCapturedThisField (ec);
+							storey.AddCapturedThisField (ec, null);
 						}
 
 						for (ExplicitBlock b = ref_block; b.AnonymousMethodStorey != storey; b = b.Parent.Explicit) {
@@ -2628,8 +2628,10 @@ namespace Mono.CSharp {
 											break;
 									}
 
+									// Needs to be in sync with AnonymousMethodBody::DoCreateMethodHost
 									if (s == null) {
-										b.AnonymousMethodStorey.AddCapturedThisField (ec);
+										var parent = storey == null || storey.Kind == MemberKind.Struct ? null : storey;
+										b.AnonymousMethodStorey.AddCapturedThisField (ec, parent);
 										break;
 									}
 								}
@@ -2664,7 +2666,7 @@ namespace Mono.CSharp {
 								}
 
 								if (parent_storey_block.AnonymousMethodStorey == null) {
-									pb.StateMachine.AddCapturedThisField (ec);
+									pb.StateMachine.AddCapturedThisField (ec, null);
 									b.HasCapturedThis = true;
 									continue;
 								}

mcs/tests/gtest-iter-30.cs
@@ -1,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main ()
+	{
+		foreach (var x in new M ().Test ()) {
+			Console.WriteLine (x);
+		}
+	}
+}
+
+class M
+{
+	public IEnumerable<int> Test ()
+	{
+		Action a = delegate {
+			int k = 0;
+			Action x = delegate {
+				Console.WriteLine (this);
+				Console.WriteLine (k);
+			};
+
+			x ();
+			Console.WriteLine (this);
+		};
+
+		a ();
+		
+		yield return 1;
+	}
+}

commit a769cce0b77f0fbd057b379b623f2f874f48a935

Fixes regression caused by 3912066fe1134ba665770723a24c8ea9b4f1aea3 change

mcs/mcs/anonymous.cs
@@ -1698,13 +1698,12 @@ namespace Mono.CSharp {
 							// use ldftn on non-boxed instances either to share mutated state
 							//
 							parent = sm_parent.Parent.PartialContainer;
-						}
-					} else {
-						//
-						// For iterators we can host everything in one class
-						//
-						if (sm is IteratorStorey)
+						} else if (sm is IteratorStorey) {
+							//
+							// For iterators we can host everything in one class
+							//
 							parent = storey = sm;
+						}
 					}
 				}
 

mcs/tests/gtest-iter-31.cs
@@ -1,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+
+class B
+{
+	public object Foo (object obj)
+	{
+		return null;
+	}
+}
+
+class C
+{
+	B ctx = new B ();
+
+	public static void Main ()
+	{
+		foreach (var c in new C ().Test ()) {			
+		}
+	}
+
+	IEnumerable<ushort> Test ()
+	{
+		string[] s = new[] { "a", "b", "c" };
+
+		var m = s.Select (l => ctx.Foo (l)).ToArray ();
+
+		yield break;
+	}
+}
+

mcs/tests/ver-il-net_4_5.xml
@@ -24461,16 +24461,66 @@
       <method name="System.Collections.Generic.IEnumerator`1[System.Int32] System.Collections.Generic.IEnumerable&lt;int&gt;.GetEnumerator()" attrs="481">
         <size>40</size>
       </method>
-      <method name="Void &lt;Test&gt;m__0()" attrs="129">
-        <size>52</size>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+      <method name="Void &lt;&gt;m__0()" attrs="131">
+        <size>57</size>
+      </method>
+    </type>
+    <type name="M+&lt;Test&gt;c__Iterator0+&lt;Test&gt;c__AnonStorey1">
+      <method name="Void &lt;&gt;m__1()" attrs="131">
+        <size>24</size>
       </method>
       <method name="Void .ctor()" attrs="6278">
         <size>7</size>
       </method>
     </type>
-    <type name="M+&lt;Test&gt;c__AnonStorey1">
-      <method name="Void &lt;&gt;m__1()" attrs="131">
-        <size>24</size>
+  </test>
+  <test name="gtest-iter-31.cs">
+    <type name="B">
+      <method name="System.Object Foo(System.Object)" attrs="134">
+        <size>10</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>7</size>
+      </method>
+    </type>
+    <type name="C">
+      <method name="Void Main()" attrs="150">
+        <size>62</size>
+      </method>
+      <method name="System.Collections.Generic.IEnumerable`1[System.UInt16] Test()" attrs="129">
+        <size>30</size>
+      </method>
+      <method name="Void .ctor()" attrs="6278">
+        <size>18</size>
+      </method>
+    </type>
+    <type name="C+&lt;Test&gt;c__Iterator0">
+      <method name="UInt16 System.Collections.Generic.IEnumerator&lt;ushort&gt;.get_Current()" attrs="2529">
+        <size>14</size>
+      </method>
+      <method name="System.Object System.Collections.IEnumerator.get_Current()" attrs="2529">
+        <size>19</size>
+      </method>
+      <method name="Boolean MoveNext()" attrs="486">
+        <size>81</size>
+      </method>
+      <method name="Void Dispose()" attrs="486">
+        <size>1</size>
+      </method>
+      <method name="Void Reset()" attrs="486">
+        <size>6</size>
+      </method>
+      <method name="IEnumerator System.Collections.IEnumerable.GetEnumerator()" attrs="481">
+        <size>14</size>
+      </method>
+      <method name="System.Collections.Generic.IEnumerator`1[System.UInt16] System.Collections.Generic.IEnumerable&lt;ushort&gt;.GetEnumerator()" attrs="481">
+        <size>40</size>
+      </method>
+      <method name="System.Object &lt;&gt;m__0(System.String)" attrs="131">
+        <size>25</size>
       </method>
       <method name="Void .ctor()" attrs="6278">
         <size>7</size>