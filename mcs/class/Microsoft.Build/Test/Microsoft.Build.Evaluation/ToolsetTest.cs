49fe80ab14ec278e05343659e1a891ceab2903d4
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-10-11 21:20:18 +0900

    Add ToolsetTest and fix Toolset construction.

---
+++ mcs/class/Microsoft.Build/Test/Microsoft.Build.Evaluation/ToolsetTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Build.Evaluation;
+using NUnit.Framework;
+
+namespace MonoTests.Microsoft.Build.Evaluation
+{
+	[TestFixture]
+	public class ToolsetTest
+	{
+		[Test]
+		public void Constructor ()
+		{
+			var ts = new Toolset ("4.3", "c:\\", ProjectCollection.GlobalProjectCollection, null);
+			Assert.IsNotNull (ts.Properties, "#1");
+			Assert.AreEqual (0, ts.Properties.Count, "#2");
+			Assert.IsNull (ts.DefaultSubToolsetVersion, "#3");
+			Assert.IsNotNull (ts.SubToolsets, "#4");
+			Assert.AreEqual (0, ts.SubToolsets.Count, "#5");
+			Assert.AreEqual ("c:\\", ts.ToolsPath, "#6");
+			Assert.AreEqual ("4.3", ts.ToolsVersion, "#7");
+		}
+	}
+}
+
