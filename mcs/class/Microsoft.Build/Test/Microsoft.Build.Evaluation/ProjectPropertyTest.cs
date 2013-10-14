5cce2ee7181cf11b1ea6318af3adc405fc7ddf15
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-10-14 19:31:35 +0900

    implement several ProjectProperty types.

--- mcs/class/Microsoft.Build/Test/Microsoft.Build.Evaluation/ProjectPropertyTest.cs
+++ mcs/class/Microsoft.Build/Test/Microsoft.Build.Evaluation/ProjectPropertyTest.cs
@@ -6,6 +6,7 @@ using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MonoTests.Microsoft.Build.Evaluation
 {
@@ -21,7 +22,6 @@ namespace MonoTests.Microsoft.Build.Evaluation
     <Item/>
   </PropertyGroup>
 </Project>";
-			var path = "file://localhost/foo.xml";
 			var xml = XmlReader.Create (new StringReader (project_xml));
 			var root = ProjectRootElement.Create (xml);
 			var pe = root.Properties.First ();
@@ -31,6 +31,32 @@ namespace MonoTests.Microsoft.Build.Evaluation
 			Assert.AreEqual ("Bar", prop.UnevaluatedValue, "#2");
 			prop.UnevaluatedValue = "x";
 			Assert.AreEqual ("x", pe.Value, "#3");
+		}
+		
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void UpdateGlobalPropertyValue ()
+		{
+			string project_xml = @"<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003' />";
+			var xml = XmlReader.Create (new StringReader (project_xml));
+			var props = new Dictionary<string, string> ();
+			props.Add ("GP", "GV");
+			var root = ProjectRootElement.Create (xml);
+			var proj = new Project (root, props, null);
+			var pe = proj.Properties.First (p => p.IsGlobalProperty);
+			pe.UnevaluatedValue = "UPDATED";
+		}
+		
+		[Test]
+		[ExpectedException (typeof (InvalidOperationException))]
+		public void UpdateEnvironmentPropertyValue ()
+		{
+			string project_xml = @"<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003' />";
+			var xml = XmlReader.Create (new StringReader (project_xml));
+			var root = ProjectRootElement.Create (xml);
+			var proj = new Project (root);
+			var pe = proj.Properties.First (p => p.IsEnvironmentProperty);
+			pe.UnevaluatedValue = "UPDATED";
 		}
 	}
 }
