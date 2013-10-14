mono/mono

commit 66d4b82a871667b3317d4e43b855d1fc1db22108

Ensure that CodeDom generated temporary directory is always created. Fixes #12202

mcs/class/System/System.CodeDom.Compiler/TempFileCollection.cs
@@ -73,33 +73,36 @@ namespace System.CodeDom.Compiler {
 
 					// note: this property *cannot* change TempDir property
 					string temp = tempdir;
-					if (temp.Length == 0)
-						temp = GetOwnTempDir ();
+					if (temp.Length == 0) {
+						if (ownTempDir != null) {
+							temp = ownTempDir;
+							Directory.CreateDirectory (temp);
+						} else {
+							temp = CreateOwnTempDir ();
+						}
+					}
 
 					// Create a temporary file at the target directory. This ensures
 					// that the generated file name is unique.
-					FileStream f = null;
-					do {
+					int test_counter = 1000;
+					while (true) {
 						int num = rnd.Next ();
 						num++;
 						basepath = Path.Combine (temp, num.ToString("x"));
 						string path = basepath + ".tmp";
 
 						try {
-							f = new FileStream (path, FileMode.CreateNew);
-						}
-						catch (System.IO.IOException) {
-							f = null;
-							continue;
-						}
-						catch {
-							// avoid endless loop
+							using (var f = new FileStream (path, FileMode.CreateNew)) {
+								break;
+							}
+						} catch (IOException) {
+							if (test_counter-- > 0)
+								continue;
+
 							throw;
 						}
-					} while (f == null);
-					
-					f.Close ();
-					
+					}
+
 					// and you must have discovery access to the combined path
 					// note: the cache behaviour is tested in the CAS tests
 					if (SecurityManager.SecurityEnabled) {
@@ -110,12 +113,9 @@ namespace System.CodeDom.Compiler {
 				return(basepath);
 			}
 		}
-		
-		string GetOwnTempDir ()
-		{
-			if (ownTempDir != null)
-				return ownTempDir;
 
+		string CreateOwnTempDir ()
+		{
 			// this call ensure the Environment permissions check
 			string basedir = Path.GetTempPath ();
 			
@@ -247,7 +247,6 @@ namespace System.CodeDom.Compiler {
 			}
 			if (allDeleted && ownTempDir != null) {
 				Directory.Delete (ownTempDir, true);
-				ownTempDir = null;
 			}
 		}
 

mcs/class/System/Test/Microsoft.CSharp/CSharpCodeProviderTest.cs
@@ -359,6 +359,32 @@ namespace MonoTests.Microsoft.CSharp
 			Assert.AreEqual (tempFile, tempFiles[0], "#5");
 		}
 
+
+		[Test]
+		public void CompileFromSource_InMemory_Twice ()
+		{
+			CompilerParameters options = new CompilerParameters ();
+			options.GenerateExecutable = false;
+			options.GenerateInMemory = true;
+
+			ICodeCompiler compiler = _codeProvider.CreateCompiler ();
+
+			var src_1 = "class X { ";
+
+			CompilerResults results_1 = compiler.CompileAssemblyFromSource (options, src_1);
+			var output_1 = options.OutputAssembly;
+
+			var src_2 = "class X { }";
+
+			CompilerResults results_2 = compiler.CompileAssemblyFromSource (options, src_2);
+			var output_2 = options.OutputAssembly;
+
+			// verify compilation was successful
+			AssertCompileResults (results_2, true);
+
+			Assert.AreEqual (output_1, output_2, "#1");
+		}
+
 		[Test]
 		public void CompileFromSourceBatch_InMemory ()
 		{

commit 3a7e67b4c5802a352d3c9578fecfa9848b4e370b

Fixes CodeDom regression introduced by 66d4b82a871667b3317d4e43b855d1fc1db22108

mcs/class/System/System.CodeDom.Compiler/TempFileCollection.cs
@@ -245,7 +245,7 @@ namespace System.CodeDom.Compiler {
 				File.Delete (tmpFile);
 				basepath = null;
 			}
-			if (allDeleted && ownTempDir != null) {
+			if (allDeleted && ownTempDir != null && filenames.Length > 0) {
 				Directory.Delete (ownTempDir, true);
 			}
 		}

mcs/class/System/Test/Microsoft.CSharp/CSharpCodeProviderTest.cs
@@ -359,7 +359,6 @@ namespace MonoTests.Microsoft.CSharp
 			Assert.AreEqual (tempFile, tempFiles[0], "#5");
 		}
 
-
 		[Test]
 		public void CompileFromSource_InMemory_Twice ()
 		{
@@ -383,6 +382,24 @@ namespace MonoTests.Microsoft.CSharp
 			AssertCompileResults (results_2, true);
 
 			Assert.AreEqual (output_1, output_2, "#1");
+		}
+
+
+		[Test]
+		public void CompileFromSource_InMemory_With_Extra_Delete ()
+		{
+			CompilerParameters options = new CompilerParameters ();
+			options.GenerateExecutable = false;
+			options.GenerateInMemory = true;
+
+			ICodeCompiler compiler = _codeProvider.CreateCompiler ();
+
+			var src_1 = "class X { ";
+
+			compiler.CompileAssemblyFromSource (options, src_1);
+
+			options.TempFiles.Delete ();
+			options.TempFiles.Delete ();
 		}
 
 		[Test]