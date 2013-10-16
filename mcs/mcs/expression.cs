4387da15242255491f2f46099a6410b43e3487df
Marek Safar <marek.safar@gmail.com>
2013-10-16 20:21:18 +0200

    Better check for open generic types in probing expression. Fixes #15422

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -1562,7 +1562,7 @@ namespace Mono.CSharp
 					//
 					// open generic type
 					//
-					if (d is InflatedTypeSpec && InflatedTypeSpec.ContainsTypeParameter (d))
+					if ((d is InflatedTypeSpec || d.IsArray) && InflatedTypeSpec.ContainsTypeParameter (d))
 						return this;
 				}
 			}
