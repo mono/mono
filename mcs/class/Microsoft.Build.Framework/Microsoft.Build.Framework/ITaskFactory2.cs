f0c157933fbfbf8c9ebf5d54b5aee6047aa56403
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-11-20 05:01:17 +0900

    ITaskFactory2 is public.

--- mcs/class/Microsoft.Build.Framework/Microsoft.Build.Framework/ITaskFactory2.cs
+++ mcs/class/Microsoft.Build.Framework/Microsoft.Build.Framework/ITaskFactory2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Microsoft.Build.Framework
 {
-	interface ITaskFactory2 : ITaskFactory
+	public interface ITaskFactory2 : ITaskFactory
 	{
 		ITask CreateTask (IBuildEngine taskFactoryLoggingHost, IDictionary<string, string> taskIdentityParameters);
 		bool Initialize (string taskName, IDictionary<string, string> factoryIdentityParameters, IDictionary<string, TaskPropertyInfo> parameterGroup, string taskBody, IBuildEngine taskFactoryLoggingHost);
