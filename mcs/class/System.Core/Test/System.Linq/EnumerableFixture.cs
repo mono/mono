2ef208b1a6f94d8b8d91e4e868c731ac3a5481b8
Sebastien Pouliot <sebastien@xamarin.com>
2013-12-05 17:52:53 -0500

    [Test] Fix build of System.Core unit tests

--- mcs/class/System.Core/Test/System.Linq/EnumerableFixture.cs
+++ mcs/class/System.Core/Test/System.Linq/EnumerableFixture.cs
@@ -28,6 +28,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Text;
 using System.Threading;
