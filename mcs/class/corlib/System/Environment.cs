c8d91bc505446f3d017f238250e6de608b603110
Marek Safar <marek.safar@gmail.com>
2013-12-06 09:50:21 +0100

    Bump mscorlib version for monolite to work correctly

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -56,7 +56,7 @@ namespace System {
 		 * of icalls, do not require an increment.
 		 */
 #pragma warning disable 169
-		private const int mono_corlib_version = 110;
+		private const int mono_corlib_version = 111;
 #pragma warning restore 169
 
 		[ComVisible (true)]
