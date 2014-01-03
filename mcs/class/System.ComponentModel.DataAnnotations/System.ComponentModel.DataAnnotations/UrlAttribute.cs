ebf0fcf3d17bbf098ce767d7e09b1941e7d275e7
Matthias Dittrich <matthi.d@googlemail.com>
2014-01-03 01:04:32 +0100

    [API] Added some Attributes to System.ComponentModel.DataAnnotation
    added BindableTypeAttribute and UrlAttribute.

---
+++ mcs/class/System.ComponentModel.DataAnnotations/System.ComponentModel.DataAnnotations/UrlAttribute.cs
@@ -0,0 +1,50 @@
+﻿//
+// UrlAttribute.cs
+//
+// Authors:
+//	Matthias Dittrich <matthi.d@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+
+namespace System.ComponentModel.DataAnnotations {
+	[AttributeUsage (AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+	public sealed class UrlAttribute : DataTypeAttribute {
+		
+		public UrlAttribute ()
+			: base (DataType.Url)
+		{
+			this.ErrorMessage = "The given Url is invalid!";
+		}
+
+		public override bool IsValid (object value)
+		{
+			if (value == null)
+				return true;
+			string urldata = value as string;
+			Uri uri;
+			return
+				urldata != null &&
+				Uri.TryCreate (urldata, UriKind.Absolute, out uri) &&
+				(uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps || uri.Scheme == Uri.UriSchemeFtp);
+		}
+	}
+}
