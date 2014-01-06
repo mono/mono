b95d8a65b968c527f10eb89a04fc135a73cc3243
Marek Safar <marek.safar@gmail.com>
2014-01-06 17:42:05 +0100

    [corlib] Mark all promise-style task exceptions observed. Fixes #17015

--- mcs/class/corlib/System.Runtime.CompilerServices/AsyncTaskMethodBuilder.cs
+++ mcs/class/corlib/System.Runtime.CompilerServices/AsyncTaskMethodBuilder.cs
@@ -76,7 +76,7 @@ namespace System.Runtime.CompilerServices
 		
 		public void SetException (Exception exception)
 		{
-			if (Task.TrySetException (new AggregateException (exception), exception is OperationCanceledException))
+			if (Task.TrySetException (new AggregateException (exception), exception is OperationCanceledException, true))
 				return;
 
 			throw new InvalidOperationException ("The task has already completed");
