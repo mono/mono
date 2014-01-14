mono/mono

commit a0afa38296b8a3b0382bf34ce777357d2553c0f0

Move the mono_thread_create () function into utils/mono-threads.h/c, change/simplify its signature a bit.

mono/utils/mono-threads.h
@@ -26,6 +26,8 @@ typedef HANDLE MonoNativeThreadHandle; /* unused */
 
 typedef DWORD mono_native_thread_return_t;
 
+#define MONO_NATIVE_THREAD_ID_TO_UINT(tid) (tid)
+
 #else
 
 #include <pthread.h>
@@ -46,6 +48,8 @@ typedef pid_t MonoNativeThreadHandle;
 typedef pthread_t MonoNativeThreadId;
 
 typedef void* mono_native_thread_return_t;
+
+#define MONO_NATIVE_THREAD_ID_TO_UINT(tid) GPOINTER_TO_UINT((tid))
 
 #endif /* #ifdef HOST_WIN32 */
 
@@ -250,6 +254,9 @@ mono_thread_info_set_is_async_context (gboolean async_context) MONO_INTERNAL;
 
 gboolean
 mono_thread_info_is_async_context (void) MONO_INTERNAL;
+
+HANDLE
+mono_threads_create_thread (LPTHREAD_START_ROUTINE start, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid);
 
 #if !defined(HOST_WIN32)
 

mono/metadata/appdomain.c
@@ -62,6 +62,7 @@
 #include <mono/utils/mono-error-internals.h>
 #include <mono/utils/atomic.h>
 #include <mono/utils/mono-memory-model.h>
+#include <mono/utils/mono-threads.h>
 #ifdef HOST_WIN32
 #include <direct.h>
 #endif
@@ -2383,7 +2384,6 @@ void
 mono_domain_try_unload (MonoDomain *domain, MonoObject **exc)
 {
 	HANDLE thread_handle;
-	gsize tid;
 	MonoAppDomainState prev_state;
 	MonoMethod *method;
 	unload_data *thread_data;
@@ -2442,9 +2442,9 @@ mono_domain_try_unload (MonoDomain *domain, MonoObject **exc)
 	 * http://bugzilla.ximian.com/show_bug.cgi?id=27663
 	 */ 
 #if 0
-	thread_handle = mono_create_thread (NULL, 0, unload_thread_main, &thread_data, 0, &tid);
+	thread_handle = mono_threads_create_thread (unload_thread_main, thread_data, 0, 0, NULL);
 #else
-	thread_handle = mono_create_thread (NULL, 0, (LPTHREAD_START_ROUTINE)unload_thread_main, thread_data, CREATE_SUSPENDED, &tid);
+	thread_handle = mono_threads_create_thread ((LPTHREAD_START_ROUTINE)unload_thread_main, thread_data, 0, CREATE_SUSPENDED, NULL);
 	if (thread_handle == NULL) {
 		return;
 	}

mono/metadata/attach.c
@@ -31,13 +31,13 @@
 #include <netdb.h>
 #include <unistd.h>
 
-
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/metadata.h>
 #include <mono/metadata/class-internals.h>
 #include <mono/metadata/object-internals.h>
 #include <mono/metadata/threads-types.h>
 #include <mono/metadata/gc-internal.h>
+#include <mono/utils/mono-threads.h>
 #include "attach.h"
 
 /*
@@ -473,14 +473,12 @@ transport_send (int fd, guint8 *data, int len)
 static void
 transport_start_receive (void)
 {
-	gsize tid;
-
 	transport_connect ();
 
 	if (!listen_fd)
 		return;
 
-	receiver_thread_handle = mono_create_thread (NULL, 0, receiver_thread, NULL, 0, &tid);
+	receiver_thread_handle = mono_threads_create_thread (receiver_thread, NULL, 0, 0, NULL);
 	g_assert (receiver_thread_handle);
 }
 

mono/metadata/threads-types.h
@@ -54,10 +54,6 @@ typedef struct _MonoInternalThread MonoInternalThread;
 
 typedef void (*MonoThreadCleanupFunc) (MonoInternalThread* thread);
 
-gpointer mono_create_thread (WapiSecurityAttributes *security,
-							 guint32 stacksize, WapiThreadStart start,
-							 gpointer param, guint32 create, gsize *tid) MONO_INTERNAL;
-
 MonoInternalThread* mono_thread_create_internal (MonoDomain *domain, gpointer func, gpointer arg, gboolean threadpool_thread, gboolean no_detach, guint32 stack_size) MONO_INTERNAL;
 
 void mono_threads_install_cleanup (MonoThreadCleanupFunc func) MONO_INTERNAL;

mono/metadata/threads.c
@@ -673,7 +673,7 @@ create_thread (MonoThread *thread, MonoInternalThread *internal, StartInfo *star
 			   gboolean throw_on_failure)
 {
 	HANDLE thread_handle;
-	gsize tid;
+	MonoNativeThreadId tid;
 	guint32 create_flags;
 
 	mono_threads_lock ();
@@ -721,8 +721,8 @@ create_thread (MonoThread *thread, MonoInternalThread *internal, StartInfo *star
 	if (no_detach)
 		create_flags |= CREATE_NO_DETACH;
 #endif
-	thread_handle = mono_create_thread (NULL, stack_size, (LPTHREAD_START_ROUTINE)start_wrapper, start_info,
-				     create_flags, &tid);
+	thread_handle = mono_threads_create_thread ((LPTHREAD_START_ROUTINE)start_wrapper, start_info,
+												stack_size, create_flags, &tid);
 	if (thread_handle == NULL) {
 		/* The thread couldn't be created, so throw an exception */
 		mono_threads_lock ();
@@ -738,7 +738,7 @@ create_thread (MonoThread *thread, MonoInternalThread *internal, StartInfo *star
 	THREAD_DEBUG (g_message ("%s: Started thread ID %"G_GSIZE_FORMAT" (handle %p)", __func__, tid, thread_handle));
 
 	internal->handle = thread_handle;
-	internal->tid = tid;
+	internal->tid = MONO_NATIVE_THREAD_ID_TO_UINT (tid);
 
 	internal->threadpool_thread = threadpool_thread;
 	if (threadpool_thread)
@@ -789,31 +789,6 @@ void mono_threads_set_default_stacksize (guint32 stacksize)
 guint32 mono_threads_get_default_stacksize (void)
 {
 	return default_stacksize;
-}
-
-/*
- * mono_create_thread:
- *
- *   This is a wrapper around CreateThread which handles differences in the type of
- * the the 'tid' argument.
- */
-gpointer mono_create_thread (WapiSecurityAttributes *security,
-							 guint32 stacksize, WapiThreadStart start,
-							 gpointer param, guint32 create, gsize *tid)
-{
-	gpointer res;
-
-#ifdef HOST_WIN32
-	DWORD real_tid;
-
-	res = mono_threads_CreateThread (security, stacksize, start, param, create, &real_tid);
-	if (tid)
-		*tid = real_tid;
-#else
-	res = CreateThread (security, stacksize, start, param, create, tid);
-#endif
-
-	return res;
 }
 
 /*

mono/mini/aot-compiler.c
@@ -8386,7 +8386,7 @@ compile_methods (MonoAotCompile *acfg)
 			user_data [1] = acfg;
 			user_data [2] = frag;
 			
-			handle = mono_create_thread (NULL, 0, (gpointer)compile_thread_main, user_data, 0, NULL);
+			handle = mono_threads_create_thread ((gpointer)compile_thread_main, user_data, 0, 0, NULL);
 			g_ptr_array_add (threads, handle);
 		}
 		g_free (methods);

mono/mini/debugger-agent.c
@@ -1576,9 +1576,7 @@ stop_debugger_thread (void)
 static void
 start_debugger_thread (void)
 {
-	gsize tid;
-
-	debugger_thread_handle = mono_create_thread (NULL, 0, debugger_thread, NULL, 0, &tid);
+	debugger_thread_handle = mono_threads_create_thread (debugger_thread, NULL, 0, 0, NULL);
 	g_assert (debugger_thread_handle);
 }
 

mono/utils/mono-threads.c
@@ -690,3 +690,31 @@ mono_thread_info_is_async_context (void)
 		return FALSE;
 }
 
+/*
+ * mono_threads_create_thread:
+ *
+ *   Create a new thread executing START with argument ARG. Store its id into OUT_TID.
+ * Returns: a windows or io-layer handle for the thread.
+ */
+HANDLE
+mono_threads_create_thread (LPTHREAD_START_ROUTINE start, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid)
+{
+	HANDLE res;
+
+#ifdef HOST_WIN32
+	DWORD real_tid;
+
+	res = mono_threads_CreateThread (NULL, stack_size, start, arg, creation_flags, &real_tid);
+	if (out_tid)
+		*out_tid = real_tid;
+#else
+	gsize real_tid;
+
+	res = CreateThread (NULL, stack_size, start, arg, creation_flags, &real_tid);
+	if (out_tid)
+		*out_tid = (gpointer)real_tid;
+#endif
+
+	return res;
+}
+

commit 285f815fc5faa59d3be1f70b7427f91e4eaa461e

[runtime] Fix a regression caused by a0afa38296b8a3b0382bf34ce777357d2553c0f0.

mono/utils/mono-threads.h
@@ -49,7 +49,7 @@ typedef pthread_t MonoNativeThreadId;
 
 typedef void* mono_native_thread_return_t;
 
-#define MONO_NATIVE_THREAD_ID_TO_UINT(tid) GPOINTER_TO_UINT((tid))
+#define MONO_NATIVE_THREAD_ID_TO_UINT(tid) (gsize)(tid)
 
 #endif /* #ifdef HOST_WIN32 */
 