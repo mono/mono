mono/mono

commit 9e5b23eee3a38cf03059542666c6762cc4885697

[runtime] Centralize low-level thread creation in utils/mono-threads.c. Get rid of the CreateThread () function in io-layer.

mono/io-layer/wthreads.c
@@ -214,6 +214,12 @@ static void thread_exit (guint32 exitstatus, gpointer handle)
 	mono_gc_pthread_exit (NULL);
 }
 
+void
+wapi_thread_set_exit_code (guint32 exitstatus, gpointer handle)
+{
+	_wapi_thread_set_termination_details (handle, exitstatus);
+}
+
 static void thread_attached_exit (gpointer handle)
 {
 	/* Drop the extra reference we take in thread_attach, now this
@@ -248,214 +254,64 @@ static void _wapi_thread_resume (struct _WapiHandle_thread *thread)
 	MONO_SEM_POST (&thread->suspend_sem);
 }
 
-static void *thread_start_routine (gpointer args) G_GNUC_NORETURN;
-static void *thread_start_routine (gpointer args)
+void
+wapi_thread_suspend (gpointer handle)
 {
-	struct _WapiHandle_thread *thread = (struct _WapiHandle_thread *)args;
-	int thr_ret;
+	struct _WapiHandle_thread *thread;
+	int res;
 
-	if (!(thread->create_flags & CREATE_NO_DETACH)) {
-		thr_ret = mono_gc_pthread_detach (pthread_self ());
-		g_assert (thr_ret == 0);
-	}
+	res = _wapi_lookup_handle (handle, WAPI_HANDLE_THREAD,
+							   (gpointer *)&thread);
+	g_assert (res);
 
-	thr_ret = pthread_setspecific (thread_hash_key,
-				       (void *)thread->handle);
-	if (thr_ret != 0) {
-		/* This is only supposed to happen when Mono is
-		   shutting down.  We cannot assert on it, though,
-		   because we must not depend on metadata, which is
-		   where the shutdown code is.
-
-		   This is a race condition which arises because
-		   pthreads don't allow creation of suspended threads.
-		   Once Mono is set to shut down no new thread is
-		   allowed to start, even though threads may still be
-		   created.  We emulate suspended threads in this
-		   function by calling _wapi_thread_suspend() below.
-
-		   So it can happen that even though Mono is already
-		   shutting down we still end up here, and at this
-		   point the thread_hash_key might already be
-		   destroyed. */
-		mono_gc_pthread_exit (NULL);
-	}
-
-	DEBUG ("%s: started thread id %ld", __func__, thread->id);
-
-	/* We set it again here since passing &thread->id to pthread_create is racy
-	   as the thread can start running before the value is set.*/
-	thread->id = pthread_self ();
-
-	if (thread->create_flags & CREATE_SUSPENDED) {
-		_wapi_thread_suspend (thread);
-	}
-	
-	thread_exit (thread->start_routine (thread->start_arg),
-		     thread->handle);
-
-#ifndef __GNUC__
-	/* Even though we tell gcc that this function doesn't return,
-	 * other compilers won't see that.
-	 */
-	return(NULL);
-#endif
+	_wapi_thread_suspend (thread);
 }
 
-/**
- * CreateThread:
- * @security: Ignored for now.
- * @stacksize: the size in bytes of the new thread's stack. Use 0 to
- * default to the normal stack size. (Ignored for now).
- * @start: The function that the new thread should start with
- * @param: The parameter to give to @start.
- * @create: If 0, the new thread is ready to run immediately.  If
- * %CREATE_SUSPENDED, the new thread will be in the suspended state,
- * requiring a ResumeThread() call to continue running.
- * @tid: If non-NULL, the ID of the new thread is stored here.  NB
- * this is defined as a DWORD (ie 32bit) in the MS API, but we need to
- * cope with 64 bit IDs for s390x and amd64.
+/*
+ * wapi_create_thread_handle:
  *
- * Creates a new threading handle.
- *
- * Return value: a new handle, or NULL
+ *   Create a thread handle for the current thread.
  */
-gpointer CreateThread(WapiSecurityAttributes *security G_GNUC_UNUSED, guint32 stacksize,
-		      WapiThreadStart start, gpointer param, guint32 create,
-		      gsize *tid) 
+gpointer
+wapi_create_thread_handle (void)
 {
-	struct _WapiHandle_thread thread_handle = {0}, *thread_handle_p;
-	pthread_attr_t attr;
+	struct _WapiHandle_thread thread_handle = {0}, *thread;
 	gpointer handle;
-	gboolean ok;
-	int ret;
-	int thr_ret;
-	int i, unrefs = 0;
-	gpointer ct_ret = NULL;
-	
+	int res;
+
 	mono_once (&thread_hash_once, thread_hash_init);
 	mono_once (&thread_ops_once, thread_ops_init);
-	
-	if (start == NULL) {
-		return(NULL);
-	}
 
 	thread_handle.state = THREAD_STATE_START;
 	thread_handle.owned_mutexes = g_ptr_array_new ();
-	thread_handle.create_flags = create;
-	thread_handle.start_routine = start;
-	thread_handle.start_arg = param;
-	
+
 	handle = _wapi_handle_new (WAPI_HANDLE_THREAD, &thread_handle);
 	if (handle == _WAPI_HANDLE_INVALID) {
 		g_warning ("%s: error creating thread handle", __func__);
 		SetLastError (ERROR_GEN_FAILURE);
 		
-		return (NULL);
+		return NULL;
 	}
 
-	pthread_cleanup_push ((void(*)(void *))_wapi_handle_unlock_handle,
-			      handle);
-	thr_ret = _wapi_handle_lock_handle (handle);
-	g_assert (thr_ret == 0);
-	
-	ok = _wapi_lookup_handle (handle, WAPI_HANDLE_THREAD,
-				  (gpointer *)&thread_handle_p);
-	if (ok == FALSE) {
-		g_warning ("%s: error looking up thread handle %p", __func__,
-			   handle);
-		SetLastError (ERROR_GEN_FAILURE);
-		
-		goto cleanup;
-	}
+	res = _wapi_lookup_handle (handle, WAPI_HANDLE_THREAD,
+							   (gpointer *)&thread);
+	g_assert (res);
 
-	/* Hold a reference while the thread is active, because we use
+	res = pthread_setspecific (thread_hash_key, handle);
+	if (res)
+		mono_gc_pthread_exit (NULL);
+
+	thread->id = pthread_self ();
+
+	/*
+	 * Hold a reference while the thread is active, because we use
 	 * the handle to store thread exit information
 	 */
 	_wapi_handle_ref (handle);
-	
-	/* Set a 2M stack size.  This is the default on Linux, but BSD
-	 * needs it.  (The original bug report from Martin Dvorak <md@9ll.cz>
-	 * set the size to 2M-4k.  I don't know why it's short by 4k, so
-	 * I'm leaving it as 2M until I'm told differently.)
-	 */
-	thr_ret = pthread_attr_init(&attr);
-	g_assert (thr_ret == 0);
-	
-	/* defaults of 2Mb for 32bits and 4Mb for 64bits */
-	/* temporarily changed to use 1 MB: this allows more threads
-	 * to be used, as well as using less virtual memory and so
-	 * more is available for the GC heap.
-	 */
-	if (stacksize == 0){
-#if HAVE_VALGRIND_MEMCHECK_H
-		if (RUNNING_ON_VALGRIND) {
-			stacksize = 1 << 20;
-		} else {
-			stacksize = (SIZEOF_VOID_P / 4) * 1024 * 1024;
-		}
-#else
-		stacksize = (SIZEOF_VOID_P / 4) * 1024 * 1024;
-#endif
-	}
 
-#ifdef PTHREAD_STACK_MIN
-	if (stacksize < PTHREAD_STACK_MIN)
-		stacksize = PTHREAD_STACK_MIN;
-#endif
-
-#ifdef HAVE_PTHREAD_ATTR_SETSTACKSIZE
-	thr_ret = pthread_attr_setstacksize(&attr, stacksize);
-	g_assert (thr_ret == 0);
-#endif
-
-	MONO_SEM_INIT (&thread_handle_p->suspend_sem, 0);
-	thread_handle_p->handle = handle;
+	DEBUG ("%s: started thread id %ld", __func__, thread->id);
 	
-
-	ret = mono_threads_pthread_create (&thread_handle_p->id, &attr,
-									   thread_start_routine, (void *)thread_handle_p);
-
-	if (ret != 0) {
-		g_warning ("%s: Error creating native thread handle %s (%d)", __func__,
-			   strerror (ret), ret);
-		SetLastError (ERROR_GEN_FAILURE);
-
-		/* Two, because of the reference we took above */
-		unrefs = 2;
-		
-		goto cleanup;
-	}
-	ct_ret = handle;
-	
-	DEBUG("%s: Started thread handle %p ID %ld", __func__, handle,
-		  thread_handle_p->id);
-	
-	if (tid != NULL) {
-#ifdef PTHREAD_POINTER_ID
-		/* Don't use GPOINTER_TO_UINT here, it can't cope with
-		 * sizeof(void *) > sizeof(uint) when a cast to uint
-		 * would overflow
-		 */
-		*tid = (gsize)(thread_handle_p->id);
-#else
-		*tid = thread_handle_p->id;
-#endif
-	}
-
-cleanup:
-	thr_ret = _wapi_handle_unlock_handle (handle);
-	g_assert (thr_ret == 0);
-	pthread_cleanup_pop (0);
-	
-	/* Must not call _wapi_handle_unref() with the shared handles
-	 * already locked
-	 */
-	for (i = 0; i < unrefs; i++) {
-		_wapi_handle_unref (handle);
-	}
-	
-	return(ct_ret);
+	return handle;
 }
 
 /* The only time this function is called when tid != pthread_self ()

mono/utils/mono-threads-posix.c
@@ -13,6 +13,7 @@
 #include <mono/utils/mono-semaphore.h>
 #include <mono/utils/mono-threads.h>
 #include <mono/utils/mono-tls.h>
+#include <mono/utils/gc_wrapper.h>
 #include <mono/metadata/threads-types.h>
 
 #include <errno.h>
@@ -24,58 +25,129 @@ extern int tkill (pid_t tid, int signal);
 #if defined(_POSIX_VERSION) || defined(__native_client__)
 #include <signal.h>
 
+#if defined(__native_client__)
+void nacl_shutdown_gc_thread(void);
+#endif
+
 typedef struct {
 	void *(*start_routine)(void*);
 	void *arg;
 	int flags;
 	MonoSemType registered;
-} ThreadStartInfo;
-
+	HANDLE handle;
+} StartInfo;
 
 static void*
 inner_start_thread (void *arg)
 {
-	ThreadStartInfo *start_info = arg;
+	StartInfo *start_info = arg;
 	void *t_arg = start_info->arg;
-	int post_result;
+	int res;
 	void *(*start_func)(void*) = start_info->start_routine;
+	guint32 flags = start_info->flags;
 	void *result;
+	HANDLE handle;
+	MonoThreadInfo *info;
 
-	mono_thread_info_attach (&result)->runtime_thread = TRUE;
+	/* Register the thread with the io-layer */
+	handle = wapi_create_thread_handle ();
+	if (!handle) {
+		res = MONO_SEM_POST (&(start_info->registered));
+		g_assert (!res);
+		return NULL;
+	}
+	start_info->handle = handle;
 
-	post_result = MONO_SEM_POST (&(start_info->registered));
-	g_assert (!post_result);
+	if (!(flags & CREATE_NO_DETACH)) {
+		res = mono_gc_pthread_detach (pthread_self ());
+		g_assert (!res);
+	}
 
+	info = mono_thread_info_attach (&result);
+	info->runtime_thread = TRUE;
+
+	/* start_info is not valid after this */
+	res = MONO_SEM_POST (&(start_info->registered));
+	g_assert (!res);
+	start_info = NULL;
+
+	if (flags & CREATE_SUSPENDED)
+		wapi_thread_suspend (handle);
+
+	/* Run the actual main function of the thread */
 	result = start_func (t_arg);
-	g_assert (!mono_domain_get ());
 
+	g_assert (!mono_domain_get ());
 	mono_thread_info_dettach ();
 
+#if defined(__native_client__)
+	nacl_shutdown_gc_thread();
+#endif
+
+	wapi_thread_set_exit_code (GPOINTER_TO_UINT (result), handle);
+
+	// FIXME: Why is this needed ?
+	mono_gc_pthread_exit (NULL);
+
+	g_assert_not_reached ();
 	return result;
 }
 
-int
-mono_threads_pthread_create (pthread_t *new_thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg)
+HANDLE
+mono_threads_core_create_thread (LPTHREAD_START_ROUTINE start_routine, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid)
 {
-	ThreadStartInfo *start_info;
-	int result;
+	pthread_attr_t attr;
+	int res;
+	pthread_t thread;
+	StartInfo start_info;
 
-	start_info = g_malloc0 (sizeof (ThreadStartInfo));
-	if (!start_info)
-		return ENOMEM;
-	MONO_SEM_INIT (&(start_info->registered), 0);
-	start_info->arg = arg;
-	start_info->start_routine = start_routine;
+	res = pthread_attr_init (&attr);
+	g_assert (!res);
 
-	result = mono_threads_get_callbacks ()->mono_gc_pthread_create (new_thread, attr, inner_start_thread, start_info);
-	if (result == 0) {
-		while (MONO_SEM_WAIT (&(start_info->registered)) != 0) {
-			/*if (EINTR != errno) ABORT("sem_wait failed"); */
-		}
+	if (stack_size == 0) {
+#if HAVE_VALGRIND_MEMCHECK_H
+		if (RUNNING_ON_VALGRIND)
+			stack_size = 1 << 20;
+		else
+			stack_size = (SIZEOF_VOID_P / 4) * 1024 * 1024;
+#else
+		stack_size = (SIZEOF_VOID_P / 4) * 1024 * 1024;
+#endif
 	}
-	MONO_SEM_DESTROY (&(start_info->registered));
-	g_free (start_info);
-	return result;
+
+#ifdef PTHREAD_STACK_MIN
+	if (stack_size < PTHREAD_STACK_MIN)
+		stack_size = PTHREAD_STACK_MIN;
+#endif
+
+#ifdef HAVE_PTHREAD_ATTR_SETSTACKSIZE
+	res = pthread_attr_setstacksize (&attr, stack_size);
+	g_assert (!res);
+#endif
+
+	memset (&start_info, 0, sizeof (StartInfo));
+	start_info.start_routine = (gpointer)start_routine;
+	start_info.arg = arg;
+	start_info.flags = creation_flags;
+	MONO_SEM_INIT (&(start_info.registered), 0);
+
+	/* Actually start the thread */
+	res = mono_threads_get_callbacks ()->mono_gc_pthread_create (&thread, &attr, inner_start_thread, &start_info);
+	if (res) {
+		// FIXME:
+		g_assert_not_reached ();
+	}
+
+	/* Wait until the thread register itself in various places */
+	while (MONO_SEM_WAIT (&(start_info.registered)) != 0) {
+		/*if (EINTR != errno) ABORT("sem_wait failed"); */
+	}
+	MONO_SEM_DESTROY (&(start_info.registered));
+
+	if (out_tid)
+		*out_tid = thread;
+
+	return start_info.handle;
 }
 
 #if !defined (__MACH__)

mono/io-layer/thread-private.h
@@ -34,7 +34,6 @@ struct _WapiHandle_thread
 	WapiThreadState state : 2;
 	guint joined : 1;
 	guint has_apc : 1;
-	guint32 create_flags;
 	/* Fields below this point are only valid for the owning process */
 	pthread_t id;
 	GPtrArray *owned_mutexes;

mono/io-layer/threads.h
@@ -40,14 +40,6 @@ G_BEGIN_DECLS
 typedef guint32 (*WapiThreadStart)(gpointer);
 typedef guint32 (*WapiApcProc)(gpointer);
 
-/* 
- * The 'tid' argument has a different type than in win32, which breaks on win64.
- * Runtime code shouldn't use this, use the mono_thread_create () function in
- * metadata instead.
- */
-extern gpointer CreateThread(WapiSecurityAttributes *security,
-			     guint32 stacksize, WapiThreadStart start,
-			     gpointer param, guint32 create, gsize *tid); /* NB tid is 32bit in MS API */
 extern gpointer OpenThread (guint32 access, gboolean inherit, gsize tid); /* NB tid is 32bit in MS API */
 extern void ExitThread(guint32 exitcode) G_GNUC_NORETURN;
 extern gboolean GetExitCodeThread(gpointer handle, guint32 *exitcode);
@@ -76,6 +68,10 @@ void wapi_finish_interrupt_thread (gpointer wait_handle);
 
 
 char* wapi_current_thread_desc (void);
+
+gpointer wapi_create_thread_handle (void);
+void wapi_thread_suspend (gpointer handle);
+void wapi_thread_set_exit_code (guint32 exitstatus, gpointer handle);
 
 G_END_DECLS
 #endif /* _WAPI_THREADS_H_ */

mono/utils/mono-threads-windows.c
@@ -104,11 +104,11 @@ inner_start_thread (LPVOID arg)
 }
 
 HANDLE
-mono_threads_CreateThread (LPSECURITY_ATTRIBUTES attributes, SIZE_T stack_size, LPTHREAD_START_ROUTINE start_routine,
-		LPVOID arg, DWORD creation_flags, LPDWORD thread_id)
+mono_threads_core_create_thread (LPTHREAD_START_ROUTINE start_routine, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid)
 {
 	ThreadStartInfo *start_info;
 	HANDLE result;
+	DWORD thread_id;
 
 	start_info = g_malloc0 (sizeof (ThreadStartInfo));
 	if (!start_info)
@@ -118,27 +118,26 @@ mono_threads_CreateThread (LPSECURITY_ATTRIBUTES attributes, SIZE_T stack_size, 
 	start_info->start_routine = start_routine;
 	start_info->suspend = creation_flags & CREATE_SUSPENDED;
 	creation_flags &= ~CREATE_SUSPENDED;
-	if (start_info->suspend)
-	{
+	if (start_info->suspend) {
 		start_info->suspend_event = CreateEvent (NULL, TRUE, FALSE, NULL);
 		if (!start_info->suspend_event)
 			return NULL;
 	}
 
-	result = CreateThread (attributes, stack_size, inner_start_thread, start_info, creation_flags, thread_id);
-
+	result = CreateThread (attributes, stack_size, inner_start_thread, start_info, creation_flags, &thread_id);
 	if (result) {
 		while (MONO_SEM_WAIT (&(start_info->registered)) != 0) {
 			/*if (EINTR != errno) ABORT("sem_wait failed"); */
 		}
-		if (start_info->suspend)
-		{
+		if (start_info->suspend) {
 			g_assert (SuspendThread (result) != (DWORD)-1);
 			SetEvent (start_info->suspend_event);
 		}
-	}
-	else if (start_info->suspend)
+	} else if (start_info->suspend) {
 		CloseHandle (start_info->suspend_event);
+	}
+	if (out_tid)
+		*out_tid = thread_id;
 	MONO_SEM_DESTROY (&(start_info->registered));
 	g_free (start_info);
 	return result;

mono/utils/mono-threads.c
@@ -699,22 +699,5 @@ mono_thread_info_is_async_context (void)
 HANDLE
 mono_threads_create_thread (LPTHREAD_START_ROUTINE start, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid)
 {
-	HANDLE res;
-
-#ifdef HOST_WIN32
-	DWORD real_tid;
-
-	res = mono_threads_CreateThread (NULL, stack_size, start, arg, creation_flags, &real_tid);
-	if (out_tid)
-		*out_tid = real_tid;
-#else
-	gsize real_tid;
-
-	res = CreateThread (NULL, stack_size, start, arg, creation_flags, &real_tid);
-	if (out_tid)
-		*out_tid = (gpointer)real_tid;
-#endif
-
-	return res;
+	return mono_threads_core_create_thread (start, arg, stack_size, creation_flags, out_tid);
 }
-

mono/utils/mono-threads.h
@@ -260,9 +260,6 @@ mono_threads_create_thread (LPTHREAD_START_ROUTINE start, gpointer arg, guint32 
 
 #if !defined(HOST_WIN32)
 
-int
-mono_threads_pthread_create (pthread_t *new_thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) MONO_INTERNAL;
-
 #if !defined(__MACH__)
 /*Use this instead of pthread_kill */
 int
@@ -286,6 +283,7 @@ void mono_threads_platform_free (THREAD_INFO_TYPE *info) MONO_INTERNAL;
 void mono_threads_core_interrupt (THREAD_INFO_TYPE *info) MONO_INTERNAL;
 void mono_threads_core_abort_syscall (THREAD_INFO_TYPE *info) MONO_INTERNAL;
 gboolean mono_threads_core_needs_abort_syscall (void) MONO_INTERNAL;
+HANDLE mono_threads_core_create_thread (LPTHREAD_START_ROUTINE start, gpointer arg, guint32 stack_size, guint32 creation_flags, MonoNativeThreadId *out_tid) MONO_INTERNAL;
 
 MonoNativeThreadId mono_native_thread_id_get (void) MONO_INTERNAL;
 

commit 1da09a9d2085150aaf397835237724e919790286

[runtime] Fix regressions caused by 9e5b23eee3a38cf03059542666c6762cc4885697.

mono/io-layer/wthreads.c
@@ -297,6 +297,9 @@ wapi_create_thread_handle (void)
 							   (gpointer *)&thread);
 	g_assert (res);
 
+	MONO_SEM_INIT (&thread->suspend_sem, 0);
+	thread->handle = handle;
+
 	res = pthread_setspecific (thread_hash_key, handle);
 	if (res)
 		mono_gc_pthread_exit (NULL);

mono/utils/mono-threads-posix.c
@@ -77,8 +77,10 @@ inner_start_thread (void *arg)
 	/* Run the actual main function of the thread */
 	result = start_func (t_arg);
 
+	/*
 	g_assert (!mono_domain_get ());
 	mono_thread_info_dettach ();
+	*/
 
 #if defined(__native_client__)
 	nacl_shutdown_gc_thread();