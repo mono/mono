mono/mono

commit 28374f3161d7bd2521b26b4ce23515ec27eb2174

[aot] Add the rgctx trampolines required by array helper wrappers in the normal place instead of in mono_aot_get_method () so they won't interfere with gsharedvt. Fixes #17284.

mono/mini/mini-trampolines.c
@@ -206,10 +206,7 @@ __attribute__ ((noinline))
 			WrapperInfo *info = mono_marshal_get_wrapper_info (impl);
 
 			if (info && info->subtype == WRAPPER_SUBTYPE_GENERIC_ARRAY_HELPER) {
-				// FIXME: This needs a gsharedvt-out trampoline, since the caller uses the gsharedvt calling conv, but the
-				// wrapper is a normal non-generic method.
 				*need_rgctx_tramp = TRUE;
-				//g_assert_not_reached ();
 			}
 		}
 
@@ -290,10 +287,10 @@ mini_add_method_trampoline (MonoMethod *orig_method, MonoMethod *m, gpointer com
 	gpointer addr = compiled_method;
 	gboolean callee_gsharedvt, callee_array_helper;
 	MonoMethod *jmethod = NULL;
-	MonoJitInfo *ji = 
-		mini_jit_info_table_find (mono_domain_get (), mono_get_addr_from_ftnptr (compiled_method), NULL);
+	MonoJitInfo *ji;
 
-	// FIXME: This loads information from AOT
+	// FIXME: This loads information from AOT (perf problem)
+	ji = mini_jit_info_table_find (mono_domain_get (), mono_get_addr_from_ftnptr (compiled_method), NULL);
 	callee_gsharedvt = mini_jit_info_is_gsharedvt (ji);
 
 	callee_array_helper = FALSE;
@@ -357,7 +354,16 @@ mini_add_method_trampoline (MonoMethod *orig_method, MonoMethod *m, gpointer com
 		//printf ("IN: %s\n", mono_method_full_name (m, TRUE));
 	}
 
-	if (add_static_rgctx_tramp && !callee_array_helper)
+	if (callee_array_helper) {
+		add_static_rgctx_tramp = FALSE;
+		if (ji && ji->from_aot) {
+			/* In AOT mode, compiled_method points to one of the InternalArray methods in Array. */
+			if (mono_method_needs_static_rgctx_invoke (jinfo_get_method (ji), TRUE))
+				add_static_rgctx_tramp = TRUE;
+		}
+	}
+
+	if (add_static_rgctx_tramp)
 		addr = mono_create_static_rgctx_trampoline (m, addr);
 
 	return addr;

mono/mini/aot-runtime.c
@@ -3641,15 +3641,8 @@ mono_aot_get_method (MonoDomain *domain, MonoMethod *method)
 			MonoMethod *m = mono_aot_get_array_helper_from_wrapper (method);
 
 			code = mono_aot_get_method (domain, m);
-			if (code) {
-				if (mono_method_needs_static_rgctx_invoke (m, FALSE)) {
-					code = mono_create_static_rgctx_trampoline (m, mono_create_ftnptr (domain, code));
-					/* The call above returns an ftnptr */
-					code = mono_get_addr_from_ftnptr (code);
-				}
-
+			if (code)
 				return code;
-			}
 		}
 
 		/*

mono/mini/gshared.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -1436,6 +1437,12 @@ public class Tests
 	public static int test_0_async_call_from_generic () {
 		call_async<string> (1, 2);
 		return 0;
+	}
+
+	public static int test_0_array_helper_gsharedvt () {
+		var arr = new AnEnum [16];
+		var c = new ReadOnlyCollection<AnEnum> (arr);
+		return c.Contains (AnEnum.Two) == false ? 0 : 1;
 	}
 }
 

commit 6c9a1d91c858d1861724648711e4a0d072e789cd

[jit] Fix a regression caused by 28374f3161d7bd2521b26b4ce23515ec27eb2174. Fixes #17392.

mono/mini/mini-trampolines.c
@@ -356,7 +356,8 @@ mini_add_method_trampoline (MonoMethod *orig_method, MonoMethod *m, gpointer com
 
 	if (callee_array_helper) {
 		add_static_rgctx_tramp = FALSE;
-		if (ji && ji->from_aot) {
+		/* FIXME: ji->from_aot is not set for llvm methods */
+		if (ji && (ji->from_aot || mono_aot_only)) {
 			/* In AOT mode, compiled_method points to one of the InternalArray methods in Array. */
 			if (mono_method_needs_static_rgctx_invoke (jinfo_get_method (ji), TRUE))
 				add_static_rgctx_tramp = TRUE;