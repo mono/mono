df528313ab61cd58fda10ab6f5468d431dedd34a
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-02-11 16:44:09 -0500

    [xbuild] Make ToolLocationHelper work better for 2.0/3.5
    Mono keeps unversioned tools in the 4.5 directory, so look there
    for 2.0. Additionally, most 3.5 tools are in the 2.0 directory.

--- mcs/class/Microsoft.Build.Utilities/Microsoft.Build.Utilities/ToolLocationHelper.cs
+++ mcs/class/Microsoft.Build.Utilities/Microsoft.Build.Utilities/ToolLocationHelper.cs
@@ -128,6 +128,19 @@ namespace Microsoft.Build.Utilities
 			string file = Path.Combine (dir, fileName);
 			if (File.Exists (file))
 				return file;
+
+			//Mono doesn't ship multiple versions of tools that are backwards/forwards compatible
+			if (!runningOnDotNet) {
+#if NET_3_5
+				//most of the 3.5 tools are in the 2.0 directory
+				if (version == TargetDotNetFrameworkVersion.Version35)
+					return GetPathToDotNetFrameworkFile (fileName, TargetDotNetFrameworkVersion.Version20);
+#endif
+				//unversioned tools are in the 4.5 directory
+				if (version == TargetDotNetFrameworkVersion.Version20)
+					return GetPathToDotNetFrameworkFile (fileName, (TargetDotNetFrameworkVersion)5);
+			}
+
 			return null;
 		}
 
