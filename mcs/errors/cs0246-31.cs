706426c3a2a37045a620e3b5789446094201b04b
Marek Safar <marek.safar@gmail.com>
2014-02-24 23:07:26 +0100

    [mcs] Don't report internal error for unresolved catch type. Fixes #17975

---
+++ mcs/errors/cs0246-31.cs
@@ -0,0 +1,13 @@
+// CS0246: The type or namespace name `Etp' could not be found. Are you missing an assembly reference?
+// Line: 8
+
+class Test
+{
+	static void Main ()
+	{
+		try {
+		} catch (Etp e) {
+		} catch {
+		}
+	}
+}
