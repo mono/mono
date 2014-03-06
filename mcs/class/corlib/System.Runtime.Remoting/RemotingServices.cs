4b4ddb66b315759d030a10153f47d5b17dc03af8
Marek Safar <marek.safar@gmail.com>
2014-03-06 19:50:52 +0100

    [corlib] Improve flow of ExecutionContext (few tests taken from https://github.com/mono/mono/pull/864). Fixes #18121 and similar.

--- mcs/class/corlib/System.Runtime.Remoting/RemotingServices.cs
+++ mcs/class/corlib/System.Runtime.Remoting/RemotingServices.cs
@@ -169,7 +169,7 @@ namespace System.Runtime.Remoting
 						returnArgs [n++] = null; 
 				}
 				
-				result = new ReturnMessage (rval, returnArgs, n, CallContext.CreateLogicalCallContext (true), reqMsg);
+				result = new ReturnMessage (rval, returnArgs, n, ExecutionContext.CreateLogicalCallContext (true), reqMsg);
 			} 
 			catch (Exception e) 
 			{
@@ -770,7 +770,7 @@ namespace System.Runtime.Remoting
 		[SecurityPermission (SecurityAction.Assert, SerializationFormatter = true)] // FIXME: to be reviewed
 		internal static byte[] SerializeCallData (object obj)
 		{
-			LogicalCallContext ctx = CallContext.CreateLogicalCallContext (false);
+			LogicalCallContext ctx = ExecutionContext.CreateLogicalCallContext (false);
 			if (ctx != null) {
 				CACD cad = new CACD ();
 				cad.d = obj;
