61fec7514766088cb58ca074cb7b94efcfb3b73c
Marek Safar <marek.safar@gmail.com>
2014-03-18 18:38:59 +0100

    [mcs] Flow analysis of nested binary expressions inside anonymous methods was missing on left/right reset. Fixes #18417

--- mcs/mcs/anonymous.cs
+++ mcs/mcs/anonymous.cs
@@ -1599,6 +1599,7 @@ namespace Mono.CSharp {
 			var da_ontrue = fc.DefiniteAssignmentOnTrue;
 			var da_onfalse = fc.DefiniteAssignmentOnFalse;
 
+			fc.DefiniteAssignmentOnTrue = fc.DefiniteAssignmentOnFalse = null;
 			block.FlowAnalysis (fc);
 
 			fc.ParametersBlock = prev_pb;
