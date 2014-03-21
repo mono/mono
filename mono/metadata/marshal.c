mono/mono

commit a7d26d482273950d8435810b571603f1ae300082

[jit] Add a 'check-pinvoke-callconv' MONO_DEBUG option to check for cdecl/stdcall mismatches.

mono/metadata/marshal.c
@@ -9044,7 +9044,7 @@ mono_marshal_get_native_wrapper (MonoMethod *method, gboolean check_exceptions, 
 									mb, csig, csig->param_count + 16);
 	mono_mb_free (mb);
 
-	info = mono_wrapper_info_create (res, WRAPPER_SUBTYPE_NONE);
+	info = mono_wrapper_info_create (res, WRAPPER_SUBTYPE_PINVOKE);
 	info->d.managed_to_native.method = method;
 	mono_marshal_set_wrapper_info (res, info);
 

man/mono.1
@@ -1534,6 +1534,12 @@ after a SIGSEGV or SIGABRT in unmanaged code.
 This option will suspend the program when a native SIGSEGV is received.
 This is useful for debugging crashes which do not happen under gdb,
 since a live process contains more information than a core file.
+.TP
+\fBcheck-pinvoke-callconv\fR
+This option causes the runtime to check for calling convention
+mismatches when using pinvoke, i.e. mixing cdecl/stdcall. It only
+works on windows. If a mismatch is detected, an
+ExecutionEngineException is thrown.
 .ne
 .RE
 .TP

mono/metadata/marshal.h
@@ -104,6 +104,7 @@ typedef enum {
 	/* Subtypes of MONO_WRAPPER_MANAGED_TO_NATIVE */
 	WRAPPER_SUBTYPE_ICALL_WRAPPER,
 	WRAPPER_SUBTYPE_NATIVE_FUNC_AOT,
+	WRAPPER_SUBTYPE_PINVOKE,
 	/* Subtypes of MONO_WRAPPER_UNKNOWN */
 	WRAPPER_SUBTYPE_SYNCHRONIZED_INNER,
 	WRAPPER_SUBTYPE_GSHAREDVT_IN,

mono/mini/aot-compiler.c
@@ -2655,7 +2655,7 @@ encode_method_ref (MonoAotCompile *acfg, MonoMethod *method, guint8 *buf, guint8
 			} else if (info->subtype == WRAPPER_SUBTYPE_NATIVE_FUNC_AOT) {
 				encode_method_ref (acfg, info->d.managed_to_native.method, p, &p);
 			} else {
-				g_assert (info->subtype == WRAPPER_SUBTYPE_NONE);
+				g_assert (info->subtype == WRAPPER_SUBTYPE_NONE || info->subtype == WRAPPER_SUBTYPE_PINVOKE);
 				encode_method_ref (acfg, info->d.managed_to_native.method, p, &p);
 			}
 			break;

mono/mini/method-to-ir.c
@@ -2571,11 +2571,29 @@ inline static MonoInst*
 mono_emit_calli (MonoCompile *cfg, MonoMethodSignature *sig, MonoInst **args, MonoInst *addr, MonoInst *imt_arg, MonoInst *rgctx_arg)
 {
 	MonoCallInst *call;
+	MonoInst *ins;
 	int rgctx_reg = -1;
+	gboolean check_sp = FALSE;
+
+	if (cfg->check_pinvoke_callconv && cfg->method->wrapper_type == MONO_WRAPPER_MANAGED_TO_NATIVE) {
+		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
+
+		if (info && info->subtype == WRAPPER_SUBTYPE_PINVOKE)
+			check_sp = TRUE;
+	}
 
 	if (rgctx_arg) {
 		rgctx_reg = mono_alloc_preg (cfg);
 		MONO_EMIT_NEW_UNALU (cfg, OP_MOVE, rgctx_reg, rgctx_arg->dreg);
+	}
+
+	if (check_sp) {
+		if (!cfg->stack_inbalance_var)
+			cfg->stack_inbalance_var = mono_compile_create_var (cfg, &mono_defaults.int_class->byval_arg, OP_LOCAL);
+
+		MONO_INST_NEW (cfg, ins, OP_GET_SP);
+		ins->dreg = cfg->stack_inbalance_var->dreg;
+		MONO_ADD_INS (cfg->cbb, ins);
 	}
 
 	call = mono_emit_call_args (cfg, sig, args, TRUE, FALSE, FALSE, rgctx_arg ? TRUE : FALSE, FALSE);
@@ -2586,6 +2604,24 @@ mono_emit_calli (MonoCompile *cfg, MonoMethodSignature *sig, MonoInst **args, Mo
 		emit_imt_argument (cfg, call, NULL, imt_arg);
 
 	MONO_ADD_INS (cfg->cbb, (MonoInst*)call);
+
+	if (check_sp) {
+		int sp_reg;
+
+		sp_reg = mono_alloc_preg (cfg);
+
+		MONO_INST_NEW (cfg, ins, OP_GET_SP);
+		ins->dreg = sp_reg;
+		MONO_ADD_INS (cfg->cbb, ins);
+
+		/* Restore the stack so we don't crash when throwing the exception */
+		MONO_INST_NEW (cfg, ins, OP_SET_SP);
+		ins->sreg1 = cfg->stack_inbalance_var->dreg;
+		MONO_ADD_INS (cfg->cbb, ins);
+
+		MONO_EMIT_NEW_BIALU (cfg, OP_COMPARE, -1, cfg->stack_inbalance_var->dreg, sp_reg);
+		MONO_EMIT_NEW_COND_EXC (cfg, NE_UN, "ExecutionEngineException");
+	}
 
 	if (rgctx_arg)
 		set_rgctx_arg (cfg, call, rgctx_reg, rgctx_arg);

mono/mini/mini-ops.h
@@ -1245,3 +1245,6 @@ MINI_OP(OP_LLVM_OUTARG_VT,	"llvm_outarg_vt", IREG, VREG, NONE)
 
 MINI_OP(OP_OBJC_GET_SELECTOR, "objc_get_selector", IREG, NONE, NONE)
 
+MINI_OP(OP_GET_SP, "get_sp", IREG, NONE, NONE)
+MINI_OP(OP_SET_SP, "set_sp", NONE, IREG, NONE)
+

mono/mini/mini-x86.c
@@ -5039,6 +5039,12 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			ins->backend.pc_offset = code - cfg->native_code;
 			bb->spill_slot_defs = g_slist_prepend_mempool (cfg->mempool, bb->spill_slot_defs, ins);
 			break;
+		case OP_GET_SP:
+			x86_mov_reg_reg (code, ins->dreg, X86_ESP, sizeof (mgreg_t));
+			break;
+		case OP_SET_SP:
+			x86_mov_reg_reg (code, X86_ESP, ins->sreg1, sizeof (mgreg_t));
+			break;
 		default:
 			g_warning ("unknown opcode %s\n", mono_inst_name (ins->opcode));
 			g_assert_not_reached ();

mono/mini/mini.c
@@ -4990,6 +4990,7 @@ mini_method_compile (MonoMethod *method, guint32 opts, MonoDomain *domain, JitFl
 	cfg->gen_seq_points = debug_options.gen_seq_points;
 	cfg->explicit_null_checks = debug_options.explicit_null_checks;
 	cfg->soft_breakpoints = debug_options.soft_breakpoints;
+	cfg->check_pinvoke_callconv = debug_options.check_pinvoke_callconv;
 	if (try_generic_shared)
 		cfg->generic_sharing_context = (MonoGenericSharingContext*)&cfg->gsctx;
 	cfg->compile_llvm = try_llvm;
@@ -6985,9 +6986,11 @@ mini_parse_debug_options (void)
 			debug_options.better_cast_details = TRUE;
 		else if (!strcmp (arg, "soft-breakpoints"))
 			debug_options.soft_breakpoints = TRUE;
+		else if (!strcmp (arg, "check-pinvoke-callconv"))
+			debug_options.check_pinvoke_callconv = TRUE;
 		else {
 			fprintf (stderr, "Invalid option for the MONO_DEBUG env variable: %s\n", arg);
-			fprintf (stderr, "Available options: 'handle-sigint', 'keep-delegates', 'reverse-pinvoke-exceptions', 'collect-pagefault-stats', 'break-on-unverified', 'no-gdb-backtrace', 'dont-free-domains', 'suspend-on-sigsegv', 'suspend-on-exception', 'suspend-on-unhandled', 'dyn-runtime-invoke', 'gdb', 'explicit-null-checks', 'init-stacks'\n");
+			fprintf (stderr, "Available options: 'handle-sigint', 'keep-delegates', 'reverse-pinvoke-exceptions', 'collect-pagefault-stats', 'break-on-unverified', 'no-gdb-backtrace', 'dont-free-domains', 'suspend-on-sigsegv', 'suspend-on-exception', 'suspend-on-unhandled', 'dyn-runtime-invoke', 'gdb', 'explicit-null-checks', 'init-stacks', 'check-pinvoke-callconv'\n");
 			exit (1);
 		}
 	}

commit 9ccbd6a0963177ab00106d433ac7825cd5012b80

[runtime] Fix a regression caused by a7d26d482273950d8435810b571603f1ae300082.

mono/metadata/marshal.c
@@ -2606,7 +2606,7 @@ mono_marshal_method_from_wrapper (MonoMethod *wrapper)
 			return res;
 	case MONO_WRAPPER_MANAGED_TO_NATIVE:
 		info = mono_marshal_get_wrapper_info (wrapper);
-		if (info && (info->subtype == WRAPPER_SUBTYPE_NONE || info->subtype == WRAPPER_SUBTYPE_NATIVE_FUNC_AOT))
+		if (info && (info->subtype == WRAPPER_SUBTYPE_NONE || info->subtype == WRAPPER_SUBTYPE_NATIVE_FUNC_AOT || info->subtype == WRAPPER_SUBTYPE_PINVOKE))
 			return info->d.managed_to_native.method;
 		else
 			return NULL;