a2784e664e72b95af6d935437435e32b7d632563
Eduardo Burgos <eburgos@gmail.com>
2014-03-22 11:47:51 -0400

    Added spaces between function name and parenthesis Added method to test case to trigger both ImplicitDelegateCreation and NewDelegate classes Forgot to commit modifications to tests/ver-il-net_4_5.xml

--- mcs/tests/gtest-607.cs
+++ mcs/tests/gtest-607.cs
@@ -2,19 +2,23 @@ using System;
 
 public class C
 {
-	public C(out string b)
+	public C (out string b)
 	{
 		b = "";
 	}
-	public string D() {
+	public string D () {
 		return "";
 	}
 }
 public class A
 {
-	public Func<String> E(out string b)
+	public Func<String> E (out string b)
 	{
-		return new C(out b).D;
+		return new C (out b).D;
+	}
+	public Func<String> F (out string b)
+	{
+		return new Func<String>(new C (out b).D);
 	}
 	static void Main() {
 	}
