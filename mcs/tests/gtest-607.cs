e64d495ddf2a326b14b25fbfaf32784e7c1021bd
Eduardo Burgos <eburgos@gmail.com>
2014-03-22 10:37:09 -0400

    Adding tests/gtest-607.cs covering the case where instantiating a DelegateCreation didn't pass FlowAnalysis through it's method group, allowing a wrong compilation error.

---
+++ mcs/tests/gtest-607.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class C
+{
+	public C(out string b)
+	{
+		b = "";
+	}
+	public string D() {
+		return "";
+	}
+}
+public class A
+{
+	public Func<String> E(out string b)
+	{
+		return new C(out b).D;
+	}
+	static void Main() {
+	}
+}
