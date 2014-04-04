mono/mono

commit 837773af14c334427dcb17b57c5cc40c678ac943

[jit] Implement support for atomic intrinsics on arm.

mono/mini/mini.c
@@ -7945,6 +7945,16 @@ mono_arch_get_gsharedvt_trampoline (MonoTrampInfo **info, gboolean aot)
 
 #endif
 
+#ifndef MONO_ARCH_HAVE_OPCODE_SUPPORTED
+
+gboolean
+mono_arch_opcode_supported (int opcode)
+{
+	return FALSE;
+}
+
+#endif
+
 #if defined(MONO_ARCH_GSHAREDVT_SUPPORTED) && !defined(ENABLE_GSHAREDVT)
 
 gboolean

mono/arch/arm/arm-codegen.h
@@ -1107,6 +1107,18 @@ typedef union {
 #define ARM_UDIV_COND(p, rd, rn, rm, cond) ARM_EMIT (p, (((cond) << 28) | (0xe << 23) | (0x3 << 20) | ((rd) << 16) | (0xf << 12) | ((rm) << 8) | (0x0 << 5) | (0x1 << 4) | ((rn) << 0)))
 #define ARM_UDIV(p, rd, rn, rm) ARM_UDIV_COND ((p), (rd), (rn), (rm), ARMCOND_AL)
 
+/* ARMv7 */
+
+typedef enum {
+	ARM_DMB_SY = 0xf,
+} ArmDmbFlags;
+
+#define ARM_DMB(p, option) ARM_EMIT ((p), ((0xf << 28) | (0x57 << 20) | (0xf << 16) | (0xf << 12) | (0x0 << 8) | (0x5 << 4) | ((option) << 0)))
+
+#define ARM_LDREX_REG(p, rt, rn) ARM_EMIT ((p), ((ARMCOND_AL << 28) | (0xc << 21) | (0x1 << 20) | ((rn) << 16) | ((rt) << 12)) | (0xf << 8) | (0x9 << 4) | 0xf << 0)
+
+#define ARM_STREX_REG(p, rd, rt, rn) ARM_EMIT ((p), ((ARMCOND_AL << 28) | (0xc << 21) | (0x0 << 20) | ((rn) << 16) | ((rd) << 12)) | (0xf << 8) | (0x9 << 4) | ((rt) << 0))
+
 #ifdef __cplusplus
 }
 #endif

mono/mini/method-to-ir.c
@@ -5478,6 +5478,9 @@ mini_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSign
 			else if (fsig->params [0]->type == MONO_TYPE_I8)
 				opcode = OP_ATOMIC_ADD_NEW_I8;
 #endif
+			if (opcode && !mono_arch_opcode_supported (opcode))
+				return NULL;
+
 			if (opcode) {
 				MONO_INST_NEW (cfg, ins_iconst, OP_ICONST);
 				ins_iconst->inst_c0 = 1;
@@ -5504,6 +5507,9 @@ mini_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSign
 			else if (fsig->params [0]->type == MONO_TYPE_I8)
 				opcode = OP_ATOMIC_ADD_NEW_I8;
 #endif
+			if (opcode && !mono_arch_opcode_supported (opcode))
+				return NULL;
+
 			if (opcode) {
 				MONO_INST_NEW (cfg, ins_iconst, OP_ICONST);
 				ins_iconst->inst_c0 = -1;
@@ -5564,6 +5570,9 @@ mini_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSign
 			else
 				return NULL;
 
+			if (!mono_arch_opcode_supported (opcode))
+				return NULL;
+
 			MONO_INST_NEW (cfg, ins, opcode);
 			ins->dreg = is_ref ? mono_alloc_ireg_ref (cfg) : mono_alloc_ireg (cfg);
 			ins->inst_basereg = args [0]->dreg;
@@ -5609,6 +5618,7 @@ mini_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSign
 				ins->sreg3 = args [2]->dreg;
 				ins->type = STACK_I4;
 				MONO_ADD_INS (cfg->cbb, ins);
+				cfg->has_atomic_cas_i4 = TRUE;
 			} else if (size == 8) {
 				MONO_INST_NEW (cfg, ins, OP_ATOMIC_CAS_I8);
 				ins->dreg = is_ref ? alloc_ireg_ref (cfg) : alloc_ireg (cfg);

mono/mini/mini-arm.c
@@ -1971,6 +1971,20 @@ mono_arch_allocate_vars (MonoCompile *cfg)
 		offset += size;
 	}
 
+	if (cfg->has_atomic_exchange_i4 || cfg->has_atomic_cas_i4 || cfg->has_atomic_add_new_i4) {
+		/* Allocate a temporary used by the atomic ops */
+		size = 4;
+		align = 4;
+
+		/* Allocate a local slot to hold the sig cookie address */
+		offset += align - 1;
+		offset &= ~(align - 1);
+		cfg->arch.atomic_tmp_offset = offset;
+		offset += size;
+	} else {
+		cfg->arch.atomic_tmp_offset = -1;
+	}
+
 	cfg->locals_min_stack_offset = offset;
 
 	curinst = cfg->locals_start;
@@ -4122,6 +4136,73 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			g_assert_not_reached ();
 #endif
 			break;
+		case OP_ATOMIC_EXCHANGE_I4:
+		case OP_ATOMIC_CAS_I4:
+		case OP_ATOMIC_ADD_NEW_I4: {
+			int tmpreg;
+			guint8 *buf [16];
+
+			g_assert (v7_supported);
+
+			/* Free up a reg */
+			if (ins->sreg1 != ARMREG_IP && ins->sreg2 != ARMREG_IP && ins->sreg3 != ARMREG_IP)
+				tmpreg = ARMREG_IP;
+			else if (ins->sreg1 != ARMREG_R0 && ins->sreg2 != ARMREG_R0 && ins->sreg3 != ARMREG_R0)
+				tmpreg = ARMREG_R0;
+			else if (ins->sreg1 != ARMREG_R1 && ins->sreg2 != ARMREG_R1 && ins->sreg3 != ARMREG_R1)
+				tmpreg = ARMREG_R1;
+			else
+				tmpreg = ARMREG_R2;
+			g_assert (cfg->arch.atomic_tmp_offset != -1);
+			ARM_STR_IMM (code, tmpreg, cfg->frame_reg, cfg->arch.atomic_tmp_offset);
+
+			switch (ins->opcode) {
+			case OP_ATOMIC_EXCHANGE_I4:
+				buf [0] = code;
+				ARM_DMB (code, ARM_DMB_SY);
+				ARM_LDREX_REG (code, ARMREG_LR, ins->sreg1);
+				ARM_STREX_REG (code, tmpreg, ins->sreg2, ins->sreg1);
+				ARM_CMP_REG_IMM (code, tmpreg, 0, 0);
+				buf [1] = code;
+				ARM_B_COND (code, ARMCOND_NE, 0);
+				arm_patch (buf [1], buf [0]);
+				break;
+			case OP_ATOMIC_CAS_I4:
+				ARM_DMB (code, ARM_DMB_SY);
+				buf [0] = code;
+				ARM_LDREX_REG (code, ARMREG_LR, ins->sreg1);
+				ARM_CMP_REG_REG (code, ARMREG_LR, ins->sreg3);
+				buf [1] = code;
+				ARM_B_COND (code, ARMCOND_NE, 0);
+				ARM_STREX_REG (code, tmpreg, ins->sreg2, ins->sreg1);
+				ARM_CMP_REG_IMM (code, tmpreg, 0, 0);
+				buf [2] = code;
+				ARM_B_COND (code, ARMCOND_NE, 0);
+				arm_patch (buf [2], buf [1]);
+				arm_patch (buf [1], code);
+				break;
+			case OP_ATOMIC_ADD_NEW_I4:
+				buf [0] = code;
+				ARM_DMB (code, ARM_DMB_SY);
+				ARM_LDREX_REG (code, ARMREG_LR, ins->sreg1);
+				ARM_ADD_REG_REG (code, ARMREG_LR, ARMREG_LR, ins->sreg2);
+				ARM_STREX_REG (code, tmpreg, ARMREG_LR, ins->sreg1);
+				ARM_CMP_REG_IMM (code, tmpreg, 0, 0);
+				buf [1] = code;
+				ARM_B_COND (code, ARMCOND_NE, 0);
+				arm_patch (buf [1], buf [0]);
+				break;
+			default:
+				g_assert_not_reached ();
+			}
+
+			ARM_DMB (code, ARM_DMB_SY);
+			if (tmpreg != ins->dreg)
+				ARM_LDR_IMM (code, tmpreg, cfg->frame_reg, cfg->arch.atomic_tmp_offset);
+			ARM_MOV_REG_REG (code, ins->dreg, ARMREG_LR);
+			break;
+		}
+
 		/*case OP_BIGMUL:
 			ppc_mullw (code, ppc_r4, ins->sreg1, ins->sreg2);
 			ppc_mulhw (code, ppc_r3, ins->sreg1, ins->sreg2);
@@ -7057,6 +7138,19 @@ mono_arch_set_target (char *mtriple)
 	}
 	if (strstr (mtriple, "gnueabi"))
 		eabi_supported = TRUE;
+}
+
+gboolean
+mono_arch_opcode_supported (int opcode)
+{
+	switch (opcode) {
+	case OP_ATOMIC_EXCHANGE_I4:
+	case OP_ATOMIC_CAS_I4:
+	case OP_ATOMIC_ADD_NEW_I4:
+		return v7_supported;
+	default:
+		return FALSE;
+	}
 }
 
 #if defined(ENABLE_GSHAREDVT)

mono/mini/mini-arm.h
@@ -203,6 +203,7 @@ typedef struct MonoCompileArch {
 	gboolean omit_fp, omit_fp_computed;
 	gpointer cinfo;
 	gpointer *vfp_scratch_slots [2];
+	int atomic_tmp_offset;
 } MonoCompileArch;
 
 #define MONO_ARCH_EMULATE_FCONV_TO_I8 1
@@ -262,6 +263,10 @@ typedef struct MonoCompileArch {
 #define MONO_ARCH_HAVE_OP_TAIL_CALL 1
 #endif
 #define MONO_ARCH_HAVE_DUMMY_INIT 1
+#define MONO_ARCH_HAVE_OPCODE_SUPPORTED 1
+#define MONO_ARCH_HAVE_ATOMIC_EXCHANGE 1
+#define MONO_ARCH_HAVE_ATOMIC_CAS 1
+#define MONO_ARCH_HAVE_ATOMIC_ADD 1
 
 #if defined(__native_client__)
 #undef MONO_ARCH_SOFT_DEBUG_SUPPORTED

mono/mini/mini.h
@@ -1488,6 +1488,7 @@ typedef struct {
 	guint            has_indirection : 1;
 	guint            has_atomic_add_new_i4 : 1;
 	guint            has_atomic_exchange_i4 : 1;
+	guint            has_atomic_cas_i4 : 1;
 	guint            check_pinvoke_callconv : 1;
 	gpointer         debug_info;
 	guint32          lmf_offset;
@@ -2336,6 +2337,7 @@ gpointer  mono_arch_get_gsharedvt_call_info     (gpointer addr, MonoMethodSignat
 gboolean  mono_arch_opcode_needs_emulation      (MonoCompile *cfg, int opcode) MONO_INTERNAL;
 gboolean  mono_arch_tail_call_supported         (MonoCompile *cfg, MonoMethodSignature *caller_sig, MonoMethodSignature *callee_sig) MONO_INTERNAL;
 int       mono_arch_translate_tls_offset        (int offset) MONO_INTERNAL;
+gboolean  mono_arch_opcode_supported            (int opcode) MONO_INTERNAL;
 
 #ifdef MONO_ARCH_SOFT_FLOAT_FALLBACK
 gboolean  mono_arch_is_soft_float               (void) MONO_INTERNAL;

commit edce99bf2e057373cb4b70413b4dbb3d82f4492c

[jit] Fix a regression caused by 837773af14c334427dcb17b57c5cc40c678ac943.

mono/mini/mini.c
@@ -7958,7 +7958,7 @@ mono_arch_get_gsharedvt_trampoline (MonoTrampInfo **info, gboolean aot)
 gboolean
 mono_arch_opcode_supported (int opcode)
 {
-	return FALSE;
+	return TRUE;
 }
 
 #endif