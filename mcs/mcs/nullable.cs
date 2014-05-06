7e389f425d529190af317313f29eab7992f48748
Marek Safar <marek.safar@gmail.com>
2014-05-06 17:19:15 +0200

    [mcs] Report error for invalid unary nullable predefined operators

--- mcs/mcs/nullable.cs
+++ mcs/mcs/nullable.cs
@@ -529,6 +529,11 @@ namespace Mono.CSharp.Nullable
 				return null;
 
 			Expression res = base.ResolveOperator (ec, unwrap);
+			if (res == null) {
+				Error_OperatorCannotBeApplied (ec, loc, OperName (Oper), Expr.Type);
+				return null;
+			}
+
 			if (res != this) {
 				if (user_operator == null)
 					return res;
