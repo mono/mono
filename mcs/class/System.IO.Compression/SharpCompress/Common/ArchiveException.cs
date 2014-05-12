d4c8e67b63fd1718f72a65ed8cffb3ea42d8ecd7
Joao Matos <joao.matos@xamarin.com>
2014-05-12 19:09:37 +0100

    Added SharpCompress Zip archive implementation.
    This copy is based on our own fork at mono/sharpcompress.

---
+++ mcs/class/System.IO.Compression/SharpCompress/Common/ArchiveException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpCompress.Common
+{
+    internal class ArchiveException : Exception
+    {
+        public ArchiveException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ mcs/class/System.IO.Compression/SharpCompress/Common/ArchiveExtractionEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpCompress.Common
+{
+    internal class ArchiveExtractionEventArgs<T> : EventArgs
+    {
+        internal ArchiveExtractionEventArgs(T entry)
+        {
+            Item = entry;
+        }
+
+        public T Item { get; private set; }
+    }
+}
