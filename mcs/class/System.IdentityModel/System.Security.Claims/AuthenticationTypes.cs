2aed7bc797e59665a097843469bb2552ac7a3d2c
Miguel de Icaza <miguel@gnome.org>
2014-05-15 11:40:08 -0400

    [System.Security.Claims] Add basic support for Claims

---
+++ mcs/class/System.IdentityModel/System.Security.Claims/AuthenticationTypes.cs
@@ -0,0 +1,13 @@
+namespace System.Security.Claims {
+
+	public static class AuthenticationTypes {
+		public const string Basic = "Basic";
+		public const string Federation = "Federation";
+		public const string Kerberos = "Kerberos";
+		public const string Negotiate = "Negotiate";
+		public const string Password = "Password";
+		public const string Signature = "Signature";
+		public const string Windows = "Windows";
+		public const string X509 = "X509";
+	}
+}
