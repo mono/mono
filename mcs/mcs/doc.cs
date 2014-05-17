9543a375a776a7f134ee64ab5767f207165508d0
Marek Safar <marek.safar@gmail.com>
2014-05-17 18:17:27 +0200

    [mcs] Add handling for special case cref types/members

--- mcs/mcs/doc.cs
+++ mcs/mcs/doc.cs
@@ -438,7 +438,10 @@ namespace Mono.CSharp
 
 					int parameters_match = -1;
 					do {
-						var members = MemberCache.FindMembers (type, member_name, true);
+						//
+						// Include base type search only for interface types
+						//
+						var members = MemberCache.FindMembers (type, member_name, !(type.IsInterface && ParsedName.Left == null));
 						if (members != null) {
 							foreach (var m in members) {
 								if (ParsedName != null && m.Arity != ParsedName.Arity)
