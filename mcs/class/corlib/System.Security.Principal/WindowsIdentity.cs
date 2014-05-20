c1028cf91286489ef075a33c91d29d1efea8d200
Marek Safar <marek.safar@gmail.com>
2014-05-20 19:35:00 +0200

    [corlib] Fix public api

--- mcs/class/corlib/System.Security.Principal/WindowsIdentity.cs
+++ mcs/class/corlib/System.Security.Principal/WindowsIdentity.cs
@@ -172,7 +172,9 @@ namespace System.Security.Principal {
 		}
 
 		// properties
-
+#if NET_4_5
+		sealed override
+#endif
 		public string AuthenticationType {
 			get { return _type; }
 		}
@@ -182,7 +184,12 @@ namespace System.Security.Principal {
 			get { return (_account == WindowsAccountType.Anonymous); }
 		}
 
-		public virtual bool IsAuthenticated
+#if NET_4_5
+		override
+#else
+		virtual
+#endif
+		public bool IsAuthenticated
 		{
 			get { return _authenticated; }
 		}
@@ -197,7 +204,12 @@ namespace System.Security.Principal {
 			get { return (_account == WindowsAccountType.System); }
 		}
 
-		public virtual string Name
+#if NET_4_5
+		override
+#else
+		virtual
+#endif
+		public string Name
 		{
 			get {
 				if (_name == null) {
