1eac449e6912bd64764d7e255188b81c7d1443b5
Marek Safar <marek.safar@gmail.com>
2014-05-23 21:11:42 +0200

    [system] Add SendMailAsync methods

--- mcs/class/System/System.Net.Mail/SmtpClient.cs
+++ mcs/class/System/System.Net.Mail/SmtpClient.cs
@@ -55,6 +55,9 @@ using System.Net.Configuration;
 using System.Configuration;
 using System.Net.Security;
 using System.Security.Authentication;
+#if NET_4_5
+using System.Threading.Tasks;
+#endif
 
 namespace System.Net.Mail {
 	public class SmtpClient
@@ -740,6 +743,43 @@ namespace System.Net.Mail {
 		{
 			Send (new MailMessage (from, to, subject, body));
 		}
+
+#if NET_4_5
+		public Task SendMailAsync (MailMessage message)
+		{
+			var tcs = new TaskCompletionSource<object> ();
+			SendCompletedEventHandler handler = null;
+			handler = (s, e) => SendMailAsyncCompletedHandler (tcs, e, handler, this);
+			SendCompleted += handler;
+			SendAsync (message, tcs);
+			return tcs.Task;
+		}
+
+		public Task SendMailAsync (string from, string recipients, string subject, string body)
+		{
+			return SendMailAsync (new MailMessage (from, recipients, subject, body));
+		}
+
+		static void SendMailAsyncCompletedHandler (TaskCompletionSource<object> source, AsyncCompletedEventArgs e, SendCompletedEventHandler handler, SmtpClient client)
+		{
+			if (handler != e.UserState)
+				return;
+
+			client.SendCompleted -= handler;
+
+			if (e.Error != null) {
+				source.SetException (e.Error);
+				return;
+			}
+
+			if (e.Cancelled) {
+				source.SetCanceled ();
+				return;
+			}
+
+			source.SetResult (null);
+		}
+#endif
 
 		private void SendDot()
 		{
