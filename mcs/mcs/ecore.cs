122a67e4c88064d4a67434e59fd9191260bfcd91
Marek Safar <marek.safar@gmail.com>
2014-05-26 18:50:05 +0200

    [mcs] Don't release field address copy before it's used. Fixes #20040

--- mcs/mcs/ecore.cs
+++ mcs/mcs/ecore.cs
@@ -6259,7 +6259,6 @@ namespace Mono.CSharp {
 				var temp = ec.GetTemporaryLocal (type);
 				ec.Emit (OpCodes.Stloc, temp);
 				ec.Emit (OpCodes.Ldloca, temp);
-				ec.FreeTemporaryLocal (temp, type);
 				return;
 			}
 
