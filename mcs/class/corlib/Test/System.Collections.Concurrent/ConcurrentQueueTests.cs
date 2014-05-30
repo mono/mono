02aacdb39e8303db5dc23690257514895325c1a9
marcos henrich <marcoshenrich@gmail.com>
2014-05-30 20:22:21 +0100

    Added stress test to ConcurrentQueue.TryPeek. The test checks that in some race condition the queue does not return null.

--- mcs/class/corlib/Test/System.Collections.Concurrent/ConcurrentQueueTests.cs
+++ mcs/class/corlib/Test/System.Collections.Concurrent/ConcurrentQueueTests.cs
@@ -30,6 +30,7 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 
 using NUnit.Framework;
+using MonoTests.System.Threading.Tasks;
 
 namespace MonoTests.System.Collections.Concurrent
 {
@@ -113,6 +114,34 @@ namespace MonoTests.System.Collections.Concurrent
 			});*/
 			
 			CollectionStressTestHelper.RemoveStressTest (new ConcurrentQueue<int> (), CheckOrderingType.InOrder);
+		}
+		
+		[Test]
+		public void StressTryPeekTestCase ()
+		{
+			ParallelTestHelper.Repeat (delegate {
+				var queue = new ConcurrentQueue<object> ();
+				queue.Enqueue (new object());
+				
+				const int threads = 10;
+				int threadCounter = 0;
+				bool success = true;
+				
+				ParallelTestHelper.ParallelStressTest (queue, (q) => {
+					int threadId = Interlocked.Increment (ref threadCounter);
+					object temp;
+					if (threadId < threads)
+					{
+						while (queue.TryPeek (out temp))
+							if (temp == null)
+								success = false;
+					} else {
+						queue.TryDequeue (out temp);
+					}
+				}, threads);
+				
+				Assert.IsTrue (success, "TryPeek returned unexpected null value.");
+			}, 10);
 		}
 		
 		[Test]
