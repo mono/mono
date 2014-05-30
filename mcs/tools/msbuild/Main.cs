001edf133e28d191463e466fb62100e00e82b4bd
Atsushi Eno <atsushieno@gmail.com>
2014-05-30 20:17:05 +0900

    [msbuild] build the projects via MSBuild task with appropriate configuration, not the entire projects in the solution.

--- mcs/tools/msbuild/Main.cs
+++ mcs/tools/msbuild/Main.cs
@@ -160,9 +160,9 @@ namespace Mono.XBuild.CommandLine {
 				if (string.Equals (Path.GetExtension (projectFile), ".sln", StringComparison.OrdinalIgnoreCase)) {
 					var parser = new SolutionParser ();
 					var root = ProjectRootElement.Create (project_collection);
+					root.FullPath = projectFile;
 					parser.ParseSolution (projectFile, project_collection, root, LogWarning);
-					foreach (var p in project_collection.LoadedProjects)
-						projectInstances.Add (p.CreateProjectInstance ());
+					projectInstances.Add (new Project (root, parameters.Properties, parameters.ToolsVersion, project_collection).CreateProjectInstance ());
 				} else {
 					project = ProjectRootElement.Create (XmlReader.Create (projectFile, settings), project_collection);
 					project.FullPath = projectFile;
@@ -170,7 +170,7 @@ namespace Mono.XBuild.CommandLine {
 					projectInstances.Add (pi);
 				}
 				foreach (var projectInstance in projectInstances) {
-					var targets = parameters.Targets.Length == 0 ? projectInstance.DefaultTargets.ToArray () : parameters.Targets;
+					var targets = parameters.Targets.Length > 0 ? parameters.Targets : projectInstance.DefaultTargets.ToArray ();
 					result = projectInstance.Build (targets, parameters.Loggers.Count > 0 ? parameters.Loggers : project_collection.Loggers);
 					if (!result)
 						break;
