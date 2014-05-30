014d517bfb04121ebafcbc7ad35a199acfc7239f
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2014-05-30 17:11:55 +0200

    Mono.Cairo: Add Context.SetSourceColor(Color) convenience method
    The Color property was obsoleted, but the suggested replacement
    SetSourceRGBA has 4 double parameters, which can be unconvenient.
    
    Add a SetSourceColor that takes a Cairo.Color parameter, make the Color
    property use it, and update the message on the Obsolete attribute.

--- mcs/class/Mono.Cairo/Mono.Cairo/Context.cs
+++ mcs/class/Mono.Cairo/Mono.Cairo/Context.cs
@@ -161,10 +161,10 @@ namespace Cairo {
 			}
 		}
 
-		[Obsolete ("Use SetSourceRGBA method")]
+		[Obsolete ("Use SetSourceColor method")]
 		public Color Color {
 			set {
-				NativeMethods.cairo_set_source_rgba (handle, value.R, value.G, value.B, value.A);
+				SetSourceColor (value);
 			}
 		}
 
@@ -335,6 +335,11 @@ namespace Cairo {
 
 		public uint ReferenceCount {
 			get { return NativeMethods.cairo_get_reference_count (handle); }
+		}
+
+		public void SetSourceColor (Color color)
+		{
+			NativeMethods.cairo_set_source_rgba (handle, color.R, color.G, color.B, color.A);
 		}
 
 		public void SetSourceRGB (double r, double g, double b)
