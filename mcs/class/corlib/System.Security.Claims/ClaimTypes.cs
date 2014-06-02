c1a76023943fbb32286ea22d69579e57e02fcec7
Tom Rathbone <tom.rathbone@gmail.com>
2014-06-02 23:12:54 +0100

    Add new missing ClaimTypes entries
    Added 2008 and 2009 definitions and fixed Email definition

--- mcs/class/corlib/System.Security.Claims/ClaimTypes.cs
+++ mcs/class/corlib/System.Security.Claims/ClaimTypes.cs
@@ -32,29 +32,57 @@ namespace System.Security.Claims
 {
 	public static class ClaimTypes
 	{
+		public const string Actor = "http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor";
+
 		public const string Anonymous = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/anonymous";
 
 		public const string Authentication = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/authentication";
 
+		public const string AuthenticationInstant = "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationinstant";
+
+		public const string AuthenticationMethod = "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationmethod";
+
 		public const string AuthorizationDecision = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/authorizationdecision";
+
+		public const string ClaimsType2005Namespace = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims";
+
+		public const string ClaimsType2009Namespace = "http://schemas.xmlsoap.org/ws/2009/09/identity/claims";
+
+		public const string ClaimsTypeNamespace = "http://schemas.microsoft.com/ws/2008/06/identity/claims";
+
+		public const string CookiePath = "http://schemas.microsoft.com/ws/2008/06/identity/claims/cookiepath";
 
 		public const string Country = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/country";
 
 		public const string DateOfBirth = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/dateofbirth";
 
+		public const string DenyOnlyPrimaryGroup = "http://schemas.microsoft.com/ws/2008/06/identity/claims/denyonlyprimarygroup";
+
+		public const string DenyOnlyPrimarySid = "http://schemas.microsoft.com/ws/2008/06/identity/claims/denyonlyprimarysid";
+
 		public const string DenyOnlySid = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/denyonlysid";
 
 		public const string Dns = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/dns";
 
-		public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+		public const string Dsa = "http://schemas.microsoft.com/ws/2008/06/identity/claims/dsa";
+
+		public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email";
+
+		public const string Expiration = "http://schemas.microsoft.com/ws/2008/06/identity/claims/expiration";
+
+		public const string Expired = "http://schemas.microsoft.com/ws/2008/06/identity/claims/expired";
 
 		public const string Gender = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/gender";
 
 		public const string GivenName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname";
 
+		public const string GroupSid = "http://schemas.microsoft.com/ws/2008/06/identity/claims/groupsid";
+
 		public const string Hash = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/hash";
 
 		public const string HomePhone = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/homephone";
+
+		public const string IsPersistent = "http://schemas.microsoft.com/ws/2008/06/identity/claims/ispersistent";
 
 		public const string Locality = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/locality";
 
@@ -70,7 +98,15 @@ namespace System.Security.Claims
 
 		public const string PPID = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/privatepersonalidentifier";
 
+		public const string PrimaryGroupSid = "http://schemas.microsoft.com/ws/2008/06/identity/claims/primarygroupsid";
+
+		public const string PrimarySid = "http://schemas.microsoft.com/ws/2008/06/identity/claims/primarysid";
+
+		public const string Role = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+
 		public const string Rsa = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/rsa";
+
+		public const string SerialNumber = "http://schemas.microsoft.com/ws/2008/06/identity/claims/serialnumber";
 
 		public const string Sid = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid";
 
@@ -90,7 +126,13 @@ namespace System.Security.Claims
 
 		public const string Uri = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri";
 
+		public const string UserData = "http://schemas.microsoft.com/ws/2008/06/identity/claims/userdata";
+
+		public const string Version = "http://schemas.microsoft.com/ws/2008/06/identity/claims/version";
+
 		public const string Webpage = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/webpage";
+
+		public const string WindowsAccountName = "http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname";
 
 		public const string X500DistinguishedName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/x500distinguishedname";
 	}
