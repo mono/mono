1ad233157ab1c63df6b30cb84849444edde35f32
Tak <levi@unity3d.com>
2014-06-03 11:53:06 +0200

    Fix android build.

--- external/buildscripts/build_runtime_android.sh
+++ external/buildscripts/build_runtime_android.sh
@@ -127,7 +127,7 @@ clean_build "$CCFLAGS_ARMv6_VFP" "$LDFLAGS_ARMv5" "$OUTDIR/armv6_vfp"
 clean_build "$CCFLAGS_ARMv7_VFP" "$LDFLAGS_ARMv7" "$OUTDIR/armv7a"
 
 # works only with ndk-r6b and later
-source build_runtime_android_x86.sh dontclean
+source ${BUILDSCRIPTSDIR}/build_runtime_android_x86.sh dontclean
 
 NUM_LIBS_BUILT=`ls -AlR $OUTDIR | grep libmono | wc -l`
 if [ $NUM_LIBS_BUILT -eq 8 ]; then
