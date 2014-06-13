b811cf8821ecb8b4d794c57c450810861095efe5
Ludovic Henry <ludovic.henry@xamarin.com>
2014-06-13 18:04:38 -0400

    [delegate] Add cache to mono_arch_get_delegate_virtual_invoke_impl

--- mono/mini/mini-amd64.c
+++ mono/mini/mini-amd64.c
@@ -8083,8 +8083,10 @@ gpointer
 mono_arch_get_delegate_virtual_invoke_impl (MonoMethodSignature *sig, MonoMethod *method)
 {
 	guint8 *code, *start;
-	int size = 20, offset;
+	int size = 20, offset, cache_idx;
 	gboolean is_virtual_generic = FALSE;
+
+	static guint8 *cache[4] = { NULL, NULL, NULL, NULL };
 
 	/* FIXME Support more cases */
 	if (MONO_TYPE_ISSTRUCT (sig->ret))
@@ -8092,6 +8094,12 @@ mono_arch_get_delegate_virtual_invoke_impl (MonoMethodSignature *sig, MonoMethod
 
 	if (method->is_inflated)
 		is_virtual_generic = mono_method_get_declaring_generic_method (method)->is_generic;
+
+	cache_idx  = is_virtual_generic ? 1 : 0;
+	cache_idx |= (method->klass->flags & TYPE_ATTRIBUTE_INTERFACE ? 1 : 0) << 2;
+
+	if (cache [cache_idx])
+		return cache [cache_idx];
 
 	/*
 	 * The stack contains:
@@ -8120,6 +8128,10 @@ mono_arch_get_delegate_virtual_invoke_impl (MonoMethodSignature *sig, MonoMethod
 	amd64_mov_reg_membase (code, AMD64_RAX, AMD64_ARG_REG1, G_STRUCT_OFFSET (MonoObject, vtable), 8);
 	amd64_jump_membase (code, AMD64_RAX, offset);
 
+	mono_memory_barrier ();
+
+	cache [cache_idx] = start;
+	
 	return start;
 }
 
