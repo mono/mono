mono/mono

commit a791cfc1640bec442757f78fe71e428d643e90ed

[winforms] revert patch, it regresses the test suite: https://github.com/mono/mono/commit/8334fe3a56c978ffe7f8cb199536f9b3234afdbe

mcs/class/Managed.Windows.Forms/System.Windows.Forms/CurrencyManager.cs
@@ -390,7 +390,7 @@ namespace System.Windows.Forms {
 				else if (e.NewIndex <= listposition) {
 					/* the deleted row was either the current one, or one earlier in the list.
 					   Update the index and emit PositionChanged, CurrentChanged, and ItemChanged. */
-					ChangeRecordState (e.NewIndex,
+					ChangeRecordState (listposition+1,
 							   false, false, e.NewIndex != listposition, false);
 				}
 				else {

commit 4b50211fb7f1dd2621673edd100f30eb39098d7d

[Managed.Windows.Forms] Revert a revert commit that accidentally changed the wrong code
When the commit 8334fe3a56c978ffe7f8cb199536f9b3234afdbe was reverted in a791cfc1640bec442757f78fe71e428d643e90ed, accidentally the wrong chunk of code was changed.

This commit reverts the changes correctly.

mcs/class/Managed.Windows.Forms/System.Windows.Forms/CurrencyManager.cs
@@ -390,7 +390,8 @@ namespace System.Windows.Forms {
 				else if (e.NewIndex <= listposition) {
 					/* the deleted row was either the current one, or one earlier in the list.
 					   Update the index and emit PositionChanged, CurrentChanged, and ItemChanged. */
-					ChangeRecordState (listposition+1,
+					// FIXME: this looks wrong, shouldn't it be (listposition - 1) instead of e.NewIndex ?
+					ChangeRecordState (e.NewIndex,
 							   false, false, e.NewIndex != listposition, false);
 				}
 				else {
@@ -412,7 +413,7 @@ namespace System.Windows.Forms {
 				}
 				else {
 					if (e.NewIndex <= listposition) {
-						ChangeRecordState (e.NewIndex,
+						ChangeRecordState (listposition + 1,
 								   false, false, false, false);
 						OnItemChanged (new ItemChangedEventArgs (-1));
 						OnListChanged (e);