3325f97e15360ef08a28a999db70de90b8e99ad7
Marcos Henrich <marcos.henrich@xamarin.com>
2014-07-16 13:45:04 +0100

    Added UriKind and String  error to UriParseComponent.ParserState. UriKind was added so Relative Uri can be parsed by UriParseComponent.ParseComponents. String error was added so a error can be returned when an invalid Uri is provided.

--- mcs/class/System/System/UriParser.cs
+++ mcs/class/System/System/UriParser.cs
@@ -55,7 +55,7 @@ namespace System {
 
 		internal string GetComponentsHelper (Uri uri, UriComponents components, UriFormat format)
 		{
-			UriElements elements = UriParseComponents.ParseComponents (uri.OriginalString.Trim ());
+			UriElements elements = UriParseComponents.ParseComponents (uri.OriginalString.Trim (), UriKind.Absolute);
 
 			string scheme = scheme_name;
 			int dp = default_port;
