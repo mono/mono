31628f1e87755ae89740170b9ec427fcec01c966
Joel Martinez <joelmartinez@gmail.com>
2014-08-02 19:47:36 -0400

    [mdoc] Changes to support new-style iOS API:
    - the dropns flag now lets you define a namespace that is dropped in a new version of the assembly during update.
    - the ntypes flag lets you tell the update command that certain methods are now using native types, and as such will attempt to match with old types in order to preserve documentation.
    - the preserve flag will avoid removing members. This is to avoid dropping documentation for members that changed from one version of the framework to another new-style version (ie. iOS).

---
+++ mcs/tools/mdoc/Mono.Documentation/preserver.cs
@@ -0,0 +1,34 @@
+﻿// Command to preserve member documentation for types that are changing in a subsequent version
+// By Joel Martinez <joel.martinez@xamarin.com
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Options;
+
+namespace Mono.Documentation
+{
+	public class MDocPreserve : MDocCommand
+	{
+		MDocUpdater updater;
+
+		public MDocPreserve ()
+		{
+			updater = new MDocUpdater ();
+		}
+
+		public override void Run (IEnumerable<string> args)
+		{
+			string preserveName = string.Empty;
+			var p = new OptionSet () { { "name=",
+					"Root {DIRECTORY} to generate/update documentation.",
+					v => preserveName = v
+				}
+			};
+
+			updater.PreserveTag = preserveName;
+
+			updater.Run (args);
+		}
+	}
+}
+
