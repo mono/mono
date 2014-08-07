5f4b8c2cf4295e82196b0a93908638de9fce2992
Alexander Köplinger <alex.koeplinger@outlook.com>
2014-08-07 21:09:54 +0200

    [Cleanup] Removed JavaEE csproj and sln files
    They were once part of a special Java target profile, but this is no longer used today.

--- mcs/class/System.XML/Test/System.Xml.Xsl/standalone_tests/run-test.j2ee.bat
+++
@@ -1,3 +0,0 @@
-set JGAC_PATH=c:\Program Files\Mainsoft\Visual MainWin for J2EE\java_refs\framework\
-java -cp "%JGAC_PATH%mscorlib.jar;%JGAC_PATH%System.jar;%JGAC_PATH%System.Xml.jar;%JGAC_PATH%nunit.core.jar;%JGAC_PATH%nunit.framework.jar;%JGAC_PATH%nunit.util.jar;%JGAC_PATH%J2SE.Helpers.jar;xslt.jar;nunit-console.jar" NUnit.Console.ConsoleUi xslt.jar /fixture:MonoTests.oasis_xslt.SuiteBuilder /xml=TestResult.xml /include=KnownFailures
-
