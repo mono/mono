1f1c2f8d1e4879367e034fc358c7e9ba45f47bd4
Marek Safar <marek.safar@gmail.com>
2014-08-13 20:20:06 +0200

    [mcs] Check for duplicate destructors. Fixes #21983

---
+++ mcs/errors/cs0111-10.cs
@@ -0,0 +1,16 @@
+// CS0111: A member `Test.EmptyClass.~EmptyClass()' is already defined. Rename this member or use different parameter types
+// Line: 12
+
+namespace Test
+{
+	public class EmptyClass
+	{
+		~EmptyClass ()
+		{
+		}
+
+		~EmptyClass ()
+		{
+		}
+	}
+}
