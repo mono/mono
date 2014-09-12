139ab680566406199d06e0e3a535c85a283de827
Marc-Andre Ferland <madrang@gmail.com>
2014-09-12 19:11:32 -0400

    Added test to check that state is enforced properly.

--- mcs/class/corlib/Test/System.Security.Cryptography/HashAlgorithmTest.cs
+++ mcs/class/corlib/Test/System.Security.Cryptography/HashAlgorithmTest.cs
@@ -210,6 +210,22 @@ public class HashAlgorithmTest {
 		byte[] array = new byte [1];
 		hash.ComputeHash (array, 1, Int32.MaxValue);
 	}
+	
+	[Test]
+	[ExpectedException (typeof (CryptographicUnexpectedOperationException))]
+	public void GetHash_StateExeception ()
+	{
+		hash.Initialize ();
+		byte[] input = new byte [8];
+		byte[] output = new byte [8];
+		hash.TransformBlock (input, 0, input.Length, output, 0);
+		
+		//Should fail with CryptographicUnexpectedOperationException as TransformFinalBlock was not called.
+		byte[] result = hash.Hash;
+		
+		//Use var so no warning is shown when compiling.
+		result.GetType ();
+	}
 
 	[Test]
 // not checked in Fx 1.1
