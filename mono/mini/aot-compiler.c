mono/mono

commit 82c15fa592b23cf466b0683cc3f02f30d7ea8828

[aot] Add an 'llvm' aot option which does the same as --llvm. This allows running the AOT compiler with LLVM enabled in aot cache mode.

mono/mini/aot-compiler.c
@@ -136,6 +136,7 @@ typedef struct MonoAotOptions {
 	gboolean use_trampolines_page;
 	gboolean no_instances;
 	gboolean gnu_asm;
+	gboolean llvm;
 	int nthreads;
 	int ntrampolines;
 	int nrgctx_trampolines;
@@ -6392,6 +6393,8 @@ mono_aot_parse_options (const char *aot_options, MonoAotOptions *opts)
 			opts->instances_logfile_path = g_strdup (arg + strlen ("log-instances="));
 		} else if (str_begins_with (arg, "log-instances")) {
 			opts->log_instances = TRUE;
+		} else if (str_begins_with (arg, "llvm")) {
+			opts->llvm = TRUE;
 		} else if (str_begins_with (arg, "internal-logfile=")) {
 			opts->logfile = g_strdup (arg + strlen ("internal-logfile="));
 		} else if (str_begins_with (arg, "mtriple=")) {
@@ -8867,7 +8870,7 @@ mono_compile_assembly (MonoAssembly *ass, guint32 opts, const char *aot_options)
 		acfg->flags |= MONO_AOT_FILE_FLAG_DEBUG;
 	}
 
-	if (mono_use_llvm) {
+	if (mono_use_llvm || acfg->aot_opts.llvm) {
 		acfg->llvm = TRUE;
 		acfg->aot_opts.asm_writer = TRUE;
 		acfg->flags |= MONO_AOT_FILE_FLAG_WITH_LLVM;

commit 19619d925ebab08f7a59e859bf073ae084e80d57

[aot] Fix a regression introduced by 82c15fa592b23cf466b0683cc3f02f30d7ea8828.

mono/mini/aot-compiler.c
@@ -6393,14 +6393,14 @@ mono_aot_parse_options (const char *aot_options, MonoAotOptions *opts)
 			opts->instances_logfile_path = g_strdup (arg + strlen ("log-instances="));
 		} else if (str_begins_with (arg, "log-instances")) {
 			opts->log_instances = TRUE;
-		} else if (str_begins_with (arg, "llvm")) {
-			opts->llvm = TRUE;
 		} else if (str_begins_with (arg, "internal-logfile=")) {
 			opts->logfile = g_strdup (arg + strlen ("internal-logfile="));
 		} else if (str_begins_with (arg, "mtriple=")) {
 			opts->mtriple = g_strdup (arg + strlen ("mtriple="));
 		} else if (str_begins_with (arg, "llvm-path=")) {
 			opts->llvm_path = g_strdup (arg + strlen ("llvm-path="));
+		} else if (!strcmp (arg, "llvm")) {
+			opts->llvm = TRUE;
 		} else if (str_begins_with (arg, "readonly-value=")) {
 			add_readonly_value (opts, arg + strlen ("readonly-value="));
 		} else if (str_begins_with (arg, "info")) {