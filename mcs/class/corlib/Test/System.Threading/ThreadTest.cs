2b5078c30637b87e30a26919e49c819d4e7a7e32
Zoltan Varga <vargaz@gmail.com>
2014-09-23 18:40:34 -0400

    [bcl] Fix the RenameTpThread test, renaming a tp thread should not fail.

--- mcs/class/corlib/Test/System.Threading/ThreadTest.cs
+++ mcs/class/corlib/Test/System.Threading/ThreadTest.cs
@@ -545,16 +545,16 @@ namespace MonoTests.System.Threading
 				if (!rename_finished)
 					Monitor.Wait (monitor);
 			}
-			Assert.IsTrue (rename_failed);
+			Assert.IsTrue (!rename_failed);
 		}
 
 		void Rename_callback (object o) {
 			Thread.CurrentThread.Name = "a";
 			try {
 				Thread.CurrentThread.Name = "b";
-				Console.WriteLine ("Thread name is: {0}", Thread.CurrentThread.Name);
+				//Console.WriteLine ("Thread name is: {0}", Thread.CurrentThread.Name);
 			} catch (Exception e) {
-				Console.Error.WriteLine (e);
+				//Console.Error.WriteLine (e);
 				rename_failed = true;
 			}
 			object monitor = o;
