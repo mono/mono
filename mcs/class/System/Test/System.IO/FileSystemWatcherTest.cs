cf00808811997eaf19829e24d65a524e410c534c
Cody Russell <cody@jhu.edu>
2014-09-26 17:48:53 -0500

    Move directory creation/cleanup into fixture setup/teardown so that if there are errors they don't break the next run of the test suite.

--- mcs/class/System/Test/System.IO/FileSystemWatcherTest.cs
+++ mcs/class/System/Test/System.IO/FileSystemWatcherTest.cs
@@ -19,8 +19,18 @@ namespace MonoTests.System.IO
 	[TestFixture]
 	public class FileSystemWatcherTest
 	{
-		static string base_path = Path.Combine (Path.GetTempPath (), "FSWTest");
-		static string path_a = Path.Combine (base_path, "a.txt");
+		static string createPath = Path.Combine (Path.GetTempPath (), "FSWTest");
+		static string fileToCreate = Path.Combine (createPath, "a.txt");
+
+		static string deletePath = Path.Combine (Path.GetTempPath (), "FSWTestDelete");
+		static string fileToDelete = Path.Combine (deletePath, "deleteMe.txt");
+
+		static string modifyPath = Path.Combine (Path.GetTempPath (), "FSWTestModify");
+		static string fileToModify = Path.Combine (modifyPath, "modifyMe.txt");
+
+		static string renamePath = Path.Combine (Path.GetTempPath (), "FSWTestRename");
+		static string sourceFile = Path.Combine (renamePath, "renameMe-src.txt");
+		static string destFile = Path.Combine (renamePath, "renameMe-dest.txt");
 
 		AutoResetEvent eventFired = new AutoResetEvent (false);
 		WatcherChangeTypes lastChangeType;
@@ -29,15 +39,39 @@ namespace MonoTests.System.IO
 		[TestFixtureSetUp]
 		public void FixtureSetup ()
 		{
-			if (!Directory.Exists (base_path))
-			   Directory.CreateDirectory (base_path);
+			if (!Directory.Exists (createPath))
+				Directory.CreateDirectory (createPath);
+
+			if (!Directory.Exists (deletePath))
+				Directory.CreateDirectory (deletePath);
+
+			if (!Directory.Exists (modifyPath))
+				Directory.CreateDirectory (modifyPath);
+
+			if (!Directory.Exists (renamePath))
+				Directory.CreateDirectory (renamePath);
 		}
 
 		[TestFixtureTearDown]
 		public void FixtureTearDown ()
 		{
-			if (Directory.Exists (base_path))
-			   Directory.Delete (base_path, true);
+			if (Directory.Exists (createPath))
+				Directory.Delete (createPath, true);
+
+			if (Directory.Exists (deletePath))
+				Directory.Delete (deletePath);
+
+			if (File.Exists (fileToModify))
+				File.Delete (fileToModify);
+
+			if (Directory.Exists (modifyPath))
+				Directory.Delete (modifyPath);
+
+			if (File.Exists (destFile))
+				File.Delete (destFile);
+
+			if (Directory.Exists (renamePath))
+				Directory.Delete (renamePath);
 		}
 
 		[Test]
@@ -116,18 +150,18 @@ namespace MonoTests.System.IO
 				lastChangeType = WatcherChangeTypes.Created;
 			};
 
-			var fsw = new FileSystemWatcher (base_path);
+			var fsw = new FileSystemWatcher (createPath);
 			fsw.IncludeSubdirectories = true;
 			fsw.Created += createdDelegate;
 			fsw.EnableRaisingEvents = true;
 			Thread.Sleep (1000);
 
-			Assert.IsFalse (File.Exists (path_a));
+			Assert.IsFalse (File.Exists (fileToCreate));
 
-			File.WriteAllText (path_a, "this should create the file");
+			File.WriteAllText (fileToCreate, "this should create the file");
 			bool gotEvent = eventFired.WaitOne (20000, true);
 
-			Assert.IsTrue (File.Exists (path_a));
+			Assert.IsTrue (File.Exists (fileToCreate));
 
 			Assert.IsTrue (gotEvent);
 			Assert.IsTrue ((lastChangeType == WatcherChangeTypes.Created));
@@ -140,12 +174,6 @@ namespace MonoTests.System.IO
 		[Test]
 		public void TestWatchPathForFileDelete ()
 		{
-			var basePath = Path.Combine (Path.GetTempPath (), "FSWTestDelete");
-			var fileToDelete = Path.Combine (basePath, "deleteMe.txt");
-
-			if (!Directory.Exists (basePath))
-			   Directory.CreateDirectory (basePath);
-
 			File.WriteAllText (fileToDelete, "this file will be deleted");
 
 			FileSystemEventHandler deletedDelegate = delegate (object o, FileSystemEventArgs e) {
@@ -153,7 +181,7 @@ namespace MonoTests.System.IO
 				lastChangeType = WatcherChangeTypes.Deleted;
 			};
 
-			var fsw = new FileSystemWatcher (basePath);
+			var fsw = new FileSystemWatcher (deletePath);
 			fsw.IncludeSubdirectories = true;
 			fsw.Deleted += deletedDelegate;
 			fsw.EnableRaisingEvents = true;
@@ -170,19 +198,11 @@ namespace MonoTests.System.IO
 			fsw.EnableRaisingEvents = false;
 			fsw.Deleted -= deletedDelegate;
 			fsw.Dispose ();
-
-			Directory.Delete (basePath);
 		}
 
 		[Test]
 		public void TestWatchPathForFileModify ()
 		{
-			var basePath = Path.Combine (Path.GetTempPath (), "FSWTestModify");
-			var fileToModify = Path.Combine (basePath, "modifyMe.txt");
-
-			if (!Directory.Exists (basePath))
-				Directory.CreateDirectory (basePath);
-
 			File.WriteAllText (fileToModify, "this file will be changed");
 
 			FileSystemEventHandler changedDelegate = delegate (object o, FileSystemEventArgs e) {
@@ -190,7 +210,7 @@ namespace MonoTests.System.IO
 				lastChangeType = WatcherChangeTypes.Changed;
 			};
 
-			var fsw = new FileSystemWatcher (basePath);
+			var fsw = new FileSystemWatcher (modifyPath);
 			fsw.Changed += changedDelegate;
 			fsw.IncludeSubdirectories = true;
 			fsw.EnableRaisingEvents = true;
@@ -223,21 +243,11 @@ namespace MonoTests.System.IO
 			fsw.EnableRaisingEvents = false;
 			fsw.Changed -= changedDelegate;
 			fsw.Dispose ();
-
-			File.Delete (fileToModify);
-			Directory.Delete (basePath);
 		}
 
 		[Test]
 		public void TestWatchPathForFileRename ()
 		{
-			var basePath = Path.Combine (Path.GetTempPath (), "FSWTestRename");
-			var sourceFile = Path.Combine (basePath, "renameMe-src.txt");
-			var destFile = Path.Combine (basePath, "renameMe-dest.txt");
-
-			if (!Directory.Exists (basePath))
-				Directory.CreateDirectory (basePath);
-
 			File.WriteAllText (sourceFile, "this file will be renamed");
 
 			RenamedEventHandler renamedDelegate = delegate (object o, RenamedEventArgs e) {
@@ -245,7 +255,7 @@ namespace MonoTests.System.IO
 				lastChangeType = WatcherChangeTypes.Renamed;
 			};
 
-			var fsw = new FileSystemWatcher (basePath);
+			var fsw = new FileSystemWatcher (renamePath);
 			fsw.Renamed += renamedDelegate;
 			fsw.IncludeSubdirectories = true;
 			fsw.EnableRaisingEvents = true;
@@ -268,9 +278,6 @@ namespace MonoTests.System.IO
 			fsw.EnableRaisingEvents = false;
 			fsw.Renamed -= renamedDelegate;
 			fsw.Dispose ();
-
-			File.Delete (destFile);
-			Directory.Delete (basePath);
 		}
 	}
 }
