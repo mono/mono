84110040aac8c8999a9c510e418ec8cec1234a81
Alexis Christoforides <alexis@thenull.net>
2014-10-05 22:21:15 -0700

    [runtime] Convert String.InternalSetLength to an icall. Needed for canary support. The change does not have a negative impact on StringBuilder performance; under variable-string-length microbenchmarks it seems to perform ~15-20% faster than the managed implementation.

--- mcs/class/corlib/System/String.cs
+++ mcs/class/corlib/System/String.cs
@@ -2688,24 +2688,8 @@ namespace System
 			}
 		}
 
-		internal unsafe void InternalSetLength (int newLength)
-		{
-			if (newLength > length)
-				throw new ArgumentOutOfRangeException ("newLength", "newLength as to be <= length");
-
-			// zero terminate, we can pass string objects directly via pinvoke
-			// we also zero the rest of the string, since the new GC needs to be
-			// able to handle the changing size (it will skip the 0 bytes).
-			fixed (char * pStr = &start_char) {
-				char *p = pStr + newLength;
-				char *end = pStr + length;
-				while (p < end) {
-					p [0] = '\0';
-					p++;
-				}
-			}
-			length = newLength;
-		}
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		internal extern void InternalSetLength (int newLength);
 
 		[ReliabilityContractAttribute (Consistency.WillNotCorruptState, Cer.MayFail)]
 		// When modifying it, GetCaseInsensitiveHashCode() should be modified as well.
