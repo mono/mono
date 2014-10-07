mono/mono

commit 5564dad15084e33861a5fac30ca4bee7d531c277

Fall back to calling Java.Util.TimeZone.Default.ID if the persist.sys.timezone system property is not present on the system or its value is an empty string. This ensures that we obtain valid timezone info on systems which do not define the above property.
Part of fix for https://bugzilla.xamarin.com/show_bug.cgi?id=4902

mcs/class/System.Core/System/TimeZoneInfo.Android.cs
@@ -398,6 +398,12 @@ namespace System {
 
 			FileStream GetTimeZoneData (string name, out int start, out int length)
 			{
+				if (name == null) { // Just in case, to avoid NREX as in xambug #4902
+					start = 0;
+					length = 0;
+					return null;
+				}
+				
 				var f = new FileInfo (Path.Combine (zoneRoot, name));
 				if (f.Exists) {
 					start   = 0;
@@ -536,20 +542,34 @@ namespace System {
 					}
 				}
 			}
+			
+			[DllImport ("__Internal")]
+			static extern int monodroid_get_system_property (string name, ref IntPtr value);
 
-			// <sys/system_properties.h>
-			[DllImport ("/system/lib/libc.so")]
-			static extern int __system_property_get (string name, StringBuilder value);
-
-			const int MaxPropertyNameLength   = 32; // <sys/system_properties.h>
-			const int MaxPropertyValueLength  = 92; // <sys/system_properties.h>
-
+			[DllImport ("__Internal")]
+			static extern void monodroid_free (IntPtr ptr);
+			
 			static string GetDefaultTimeZoneName ()
 			{
-				var buf = new StringBuilder (MaxPropertyValueLength + 1);
-				int n = __system_property_get ("persist.sys.timezone", buf);
-				if (n > 0)
-					return buf.ToString ();
+				IntPtr value = IntPtr.Zero;
+				int n = 0;
+				string defaultTimeZone;
+
+				// Used by the tests
+				if (Environment.GetEnvironmentVariable ("__XA_USE_JAVA_DEFAULT_TIMEZONE_ID__") == null)
+					n = monodroid_get_system_property ("persist.sys.timezone", ref value);
+				
+				if (n > 0 && value != IntPtr.Zero) {
+					defaultTimeZone = (Marshal.PtrToStringAnsi (value) ?? String.Empty).Trim ();
+					monodroid_free (value);
+					if (!String.IsNullOrEmpty (defaultTimeZone))
+						return defaultTimeZone;
+				}
+				
+				defaultTimeZone = (AndroidPlatform.GetDefaultTimeZone () ?? String.Empty).Trim ();
+				if (!String.IsNullOrEmpty (defaultTimeZone))
+					return defaultTimeZone;
+
 				return null;
 			}
 

mcs/class/corlib/System/AndroidPlatform.cs
@@ -34,6 +34,7 @@ namespace System {
 	internal class AndroidPlatform {
 
 		static readonly Func<SynchronizationContext> getDefaultSyncContext;
+		static readonly Func<string> getDefaultTimeZone;
 
 		static AndroidPlatform ()
 		{
@@ -42,11 +43,22 @@ namespace System {
 						Type.GetType ("Android.Runtime.AndroidEnvironment, Mono.Android", true)
 						.GetMethod ("GetDefaultSyncContext", 
 							System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic));
+
+			getDefaultTimeZone = (Func<string>)
+				Delegate.CreateDelegate (typeof(Func<string>), 
+						Type.GetType ("Android.Runtime.AndroidEnvironment, Mono.Android", true)
+						.GetMethod ("GetDefaultTimeZone", 
+							System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic));
 		}
 
 		internal static SynchronizationContext GetDefaultSyncContext ()
 		{
 			return getDefaultSyncContext ();
+		}
+
+		internal static string GetDefaultTimeZone ()
+		{
+			return getDefaultTimeZone ();
 		}
 	}
 }

commit 8b90653ff3048951167da9e6c7cc074a90815c5b

[System.Core/Android] Fix standalone unit tests.
Commit 5564dad1 broke the self-test (for testing on the developer
machine, not an Android target). Fixup the comment so that appropriate
data is grabbed and used for testing, and improve
AndroidTimeZones.GetDefaultTimeZoneName() so that the default timezone
ID can be specified via the __XA_OVERRIDE_TIMEZONE_ID__ environment
variable for testing.

Finally, demonstrate TimeZoneInfo.GetUtcOffset(DateTime) use so that I
can get better insight into the timezone data stored within the
database:

	$ __XA_OVERRIDE_TIMEZONE_ID__=America/New_York ANDROID_ROOT=`pwd` ANDROID_DATA=`pwd` mono --debug tzi.exe --offset=1969-01-01
	Using DateTime Offset: 1/1/1969 12:00:00 AM
	DB type: System.AndroidTzData
	name=Africa/Abidjan                           Africa/Abidjan                          From Offset: 00:00:00
	name=Africa/Accra                             Africa/Accra                            From Offset: 00:00:00
	name=Africa/Addis_Ababa                       Africa/Addis_Ababa                      From Offset: 03:00:00
	...

mcs/class/System.Core/System/TimeZoneInfo.Android.cs
@@ -553,7 +553,10 @@ namespace System {
 			{
 				IntPtr value = IntPtr.Zero;
 				int n = 0;
-				string defaultTimeZone;
+				string defaultTimeZone  = Environment.GetEnvironmentVariable ("__XA_OVERRIDE_TIMEZONE_ID__");
+
+				if (!string.IsNullOrEmpty (defaultTimeZone))
+					return defaultTimeZone;
 
 				// Used by the tests
 				if (Environment.GetEnvironmentVariable ("__XA_USE_JAVA_DEFAULT_TIMEZONE_ID__") == null)
@@ -576,16 +579,20 @@ namespace System {
 #if SELF_TEST
 			/*
 			 * Compile:
-			 *    mcs  /out:tzi.exe /unsafe "/d:INSIDE_CORLIB;MONODROID;NET_4_0;LIBC;SELF_TEST" System/TimeZone*.cs ../../build/common/Consts.cs ../Mono.Options/Mono.Options/Options.cs
+			 *    mcs /debug+ /out:tzi.exe /unsafe "/d:INSIDE_CORLIB;MONODROID;NET_4_0;LIBC;SELF_TEST" ../corlib/System/AndroidPlatform.cs System/TimeZone*.cs ../../build/common/Consts.cs ../Mono.Options/Mono.Options/Options.cs
 			 * Prep:
 			 *    mkdir -p usr/share/zoneinfo
+			 *    mkdir -p misc/zoneinfo/zoneinfo
 			 *    android_root=`adb shell echo '$ANDROID_ROOT' | tr -d "\r"`
+			 *    android_data=`adb shell echo '$ANDROID_DATA' | tr -d "\r"`
 			 *    adb pull $android_root/usr/share/zoneinfo usr/share/zoneinfo
+			 *    adb pull $android_data/misc/zoneinfo/tzdata misc/zoneinfo
 			 * Run:
-			 *    ANDROID_ROOT=`pwd` mono tzi.exe
+			 *    __XA_OVERRIDE_TIMEZONE_ID__=America/New_York ANDROID_ROOT=`pwd` ANDROID_DATA=`pwd` mono --debug tzi.exe --offset=1969-01-01
 			 */
 			static void Main (string[] args)
 			{
+				DateTime? offset           = null;
 				Func<IAndroidTimeZoneDB> c = () => GetDefaultTimeZoneDB ();
 				Mono.Options.OptionSet p = null;
 				p = new Mono.Options.OptionSet () {
@@ -594,6 +601,10 @@ namespace System {
 					} },
 					{ "Z=", "Create ZoneInfoDB from {DIR}.", v => {
 							c = () => new ZoneInfoDB (v);
+					} },
+					{ "offset=", "Show timezone info offset for DateTime {OFFSET}.", v => {
+						offset = DateTime.Parse (v);
+						Console.WriteLine ("Using DateTime Offset: {0}", offset);
 					} },
 					{ "help", "Show this message and exit", v => {
 							p.WriteOptionDescriptions (Console.Out);
@@ -607,8 +618,12 @@ namespace System {
 					Console.Write ("name={0,-40}", id);
 					try {
 						TimeZoneInfo zone = _GetTimeZone (id);
-						if (zone != null)
-							Console.Write (" {0}", zone);
+						if (zone != null) {
+							Console.Write (" {0,-40}", zone);
+							if (offset.HasValue) {
+								Console.Write ("From Offset: {0}", zone.GetUtcOffset (offset.Value));
+							}
+						}
 						else {
 							Console.Write (" ERROR:null");
 						}