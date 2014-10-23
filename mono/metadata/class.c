mono/mono

commit 8d9f05ea9a943ff1a36bb0ef3dd0521853d3a489

[runtime] Add checks for overlapping reference and non-reference fields when using explicit layout. Fixes #23954.

mono/metadata/class.c
@@ -1943,7 +1943,10 @@ mono_class_layout_fields (MonoClass *class)
 			}
 		}
 		break;
-	case TYPE_ATTRIBUTE_EXPLICIT_LAYOUT:
+	case TYPE_ATTRIBUTE_EXPLICIT_LAYOUT: {
+		guint8 *ref_bitmap;
+
+		ref_bitmap = g_new0 (guint8, class->instance_size / sizeof (gpointer));
 		real_size = 0;
 		for (i = 0; i < top; i++) {
 			gint32 align;
@@ -1956,7 +1959,6 @@ mono_class_layout_fields (MonoClass *class)
 			 * There must be info about all the fields in a type if it
 			 * uses explicit layout.
 			 */
-
 			if (mono_field_is_deleted (field))
 				continue;
 			if (field->type->attrs & FIELD_ATTRIBUTE_STATIC)
@@ -1979,6 +1981,7 @@ mono_class_layout_fields (MonoClass *class)
 				if (field->offset % sizeof (gpointer)) {
 					mono_class_set_failure (class, MONO_EXCEPTION_TYPE_LOAD, NULL);
 				}
+				ref_bitmap [field->offset / sizeof (gpointer)] = 1;
 			}
 
 			/*
@@ -1986,12 +1989,30 @@ mono_class_layout_fields (MonoClass *class)
 			 */
 			real_size = MAX (real_size, size + field->offset);
 		}
+
+		/* Check for overlapping reference and non-reference fields */
+		for (i = 0; i < top; i++) {
+			field = &class->fields [i];
+
+			if (mono_field_is_deleted (field))
+				continue;
+			if (field->type->attrs & FIELD_ATTRIBUTE_STATIC)
+				continue;
+
+			if (!MONO_TYPE_IS_REFERENCE (field->type) && ref_bitmap [field->offset / sizeof (gpointer)]) {
+				char *err_msg = g_strdup_printf ("Could not load type '%s' because it contains an object field at offset %d that is incorrectly aligned or overlapped by a non-object field.", class->name, field->offset);
+				mono_class_set_failure (class, MONO_EXCEPTION_TYPE_LOAD, err_msg);
+			}
+		}
+		g_free (ref_bitmap);
+
 		class->instance_size = MAX (real_size, class->instance_size);
 		if (class->instance_size & (class->min_align - 1)) {
 			class->instance_size += class->min_align - 1;
 			class->instance_size &= ~(class->min_align - 1);
 		}
 		break;
+	}
 	}
 
 	if (layout != TYPE_ATTRIBUTE_EXPLICIT_LAYOUT) {

commit 2d3dd5a40af0d687cb5bbbd018fe4d10803ff8c3

[runtime] Fix a regression introduced by 8d9f05ea9a943ff1a36bb0ef3dd0521853d3a489. Only do the check for overlapping fields when the size of the type is known.

mono/metadata/class.c
@@ -1946,7 +1946,6 @@ mono_class_layout_fields (MonoClass *class)
 	case TYPE_ATTRIBUTE_EXPLICIT_LAYOUT: {
 		guint8 *ref_bitmap;
 
-		ref_bitmap = g_new0 (guint8, class->instance_size / sizeof (gpointer));
 		real_size = 0;
 		for (i = 0; i < top; i++) {
 			gint32 align;
@@ -1981,7 +1980,6 @@ mono_class_layout_fields (MonoClass *class)
 				if (field->offset % sizeof (gpointer)) {
 					mono_class_set_failure (class, MONO_EXCEPTION_TYPE_LOAD, NULL);
 				}
-				ref_bitmap [field->offset / sizeof (gpointer)] = 1;
 			}
 
 			/*
@@ -1990,21 +1988,38 @@ mono_class_layout_fields (MonoClass *class)
 			real_size = MAX (real_size, size + field->offset);
 		}
 
-		/* Check for overlapping reference and non-reference fields */
-		for (i = 0; i < top; i++) {
-			field = &class->fields [i];
+		if (class->has_references) {
+			ref_bitmap = g_new0 (guint8, real_size / sizeof (gpointer));
 
-			if (mono_field_is_deleted (field))
-				continue;
-			if (field->type->attrs & FIELD_ATTRIBUTE_STATIC)
-				continue;
+			/* Check for overlapping reference and non-reference fields */
+			for (i = 0; i < top; i++) {
+				MonoType *ftype;
 
-			if (!MONO_TYPE_IS_REFERENCE (field->type) && ref_bitmap [field->offset / sizeof (gpointer)]) {
-				char *err_msg = g_strdup_printf ("Could not load type '%s' because it contains an object field at offset %d that is incorrectly aligned or overlapped by a non-object field.", class->name, field->offset);
-				mono_class_set_failure (class, MONO_EXCEPTION_TYPE_LOAD, err_msg);
+				field = &class->fields [i];
+
+				if (mono_field_is_deleted (field))
+					continue;
+				if (field->type->attrs & FIELD_ATTRIBUTE_STATIC)
+					continue;
+				ftype = mono_type_get_underlying_type (field->type);
+				if (MONO_TYPE_IS_REFERENCE (ftype))
+					ref_bitmap [field->offset / sizeof (gpointer)] = 1;
 			}
+			for (i = 0; i < top; i++) {
+				field = &class->fields [i];
+
+				if (mono_field_is_deleted (field))
+					continue;
+				if (field->type->attrs & FIELD_ATTRIBUTE_STATIC)
+					continue;
+
+				if (!MONO_TYPE_IS_REFERENCE (field->type) && ref_bitmap [field->offset / sizeof (gpointer)]) {
+					char *err_msg = g_strdup_printf ("Could not load type '%s' because it contains an object field at offset %d that is incorrectly aligned or overlapped by a non-object field.", class->name, field->offset);
+					mono_class_set_failure (class, MONO_EXCEPTION_TYPE_LOAD, err_msg);
+				}
+			}
+			g_free (ref_bitmap);
 		}
-		g_free (ref_bitmap);
 
 		class->instance_size = MAX (real_size, class->instance_size);
 		if (class->instance_size & (class->min_align - 1)) {