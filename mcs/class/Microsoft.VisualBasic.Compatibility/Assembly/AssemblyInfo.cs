b5fcc729a32800501d1e5a112942c828d90beb74
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2014-10-29 09:21:35 +1100

    Add Microsoft.VisualBasic.Compatibility Assembly
    This change is released under the MIT license.

---
+++ mcs/class/Microsoft.VisualBasic.Compatibility/Assembly/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+//
+// AssemblyInfo.cs
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about the Microsoft.VisualBasic.Compatibility assembly
+
+[assembly: AssemblyTitle ("Microsoft.VisualBasic.Compatibility.dll")]
+[assembly: AssemblyDescription ("Microsoft.VisualBasic.Compatibility.dll")]
+[assembly: AssemblyDefaultAlias ("Microsoft.VisualBasic.Compatibility.dll")]
+
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyVersion ("10.0.0.0")]
+[assembly: SatelliteContractVersion ("10.0.0.0")]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
+
+[assembly: NeutralResourcesLanguage ("en-US")]
+
+[assembly: ComVisible (false)]
+
+	[assembly: CLSCompliant (true)]
+	[assembly: AssemblyDelaySign (true)]
+	[assembly: AssemblyKeyFile ("../msfinal.pub")]
+
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: AllowPartiallyTrustedCallers]
