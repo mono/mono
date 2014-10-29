mono/mono

commit 6ec1ebf99078a996ca16fd0c56cbd4e96e32ed41

[llvm] Rewrite the way direct calls are made between llvm methods. Previously all calls were made to plt entries and the plt entry symbols were aliased to point to the corresponding method if the method was directly callable using the .set assembler directive. This doesn't seem to work with newer LLVM/CLANG versions, so do this completely on the LLVM side using Value::replaceAllUsesWith (). Fixes #23976.

mono/mini/mini-llvm.c
@@ -10,6 +10,7 @@
 #include <mono/metadata/mempool-internals.h>
 #include <mono/utils/mono-tls.h>
 #include <mono/utils/mono-dl.h>
+#include <mono/utils/mono-time.h>
 
 #ifndef __STDC_LIMIT_MACROS
 #define __STDC_LIMIT_MACROS
@@ -35,6 +36,8 @@ typedef struct {
 	LLVMValueRef got_var;
 	const char *got_symbol;
 	GHashTable *plt_entries;
+	GHashTable *plt_entries_ji;
+	GHashTable *method_to_lmethod;
 	char **bb_names;
 	int bb_names_len;
 	GPtrArray *used;
@@ -1272,6 +1275,7 @@ get_plt_entry (EmitContext *ctx, LLVMTypeRef llvm_sig, MonoJumpInfoType type, gc
 {
 	char *callee_name = mono_aot_get_plt_symbol (type, data);
 	LLVMValueRef callee;
+	MonoJumpInfo *ji = NULL;
 
 	if (!callee_name)
 		return NULL;
@@ -1288,6 +1292,14 @@ get_plt_entry (EmitContext *ctx, LLVMTypeRef llvm_sig, MonoJumpInfoType type, gc
 		LLVMSetVisibility (callee, LLVMHiddenVisibility);
 
 		g_hash_table_insert (ctx->lmodule->plt_entries, (char*)callee_name, callee);
+	}
+
+	if (ctx->cfg->compile_aot) {
+		ji = g_new0 (MonoJumpInfo, 1);
+		ji->type = type;
+		ji->data.target = data;
+
+		g_hash_table_insert (ctx->lmodule->plt_entries_ji, ji, callee);
 	}
 
 	return callee;
@@ -4675,6 +4687,9 @@ mono_llvm_emit_method (MonoCompile *cfg)
 		/* FIXME: Free the LLVM IL for the function */
 	}
 
+	if (ctx->lmodule->method_to_lmethod)
+		g_hash_table_insert (ctx->lmodule->method_to_lmethod, cfg->method, method);
+
 	goto CLEANUP;
 
  FAILURE:
@@ -5331,6 +5346,8 @@ mono_llvm_create_aot_module (const char *got_symbol)
 
 	aot_module.llvm_types = g_hash_table_new (NULL, NULL);
 	aot_module.plt_entries = g_hash_table_new (g_str_hash, g_str_equal);
+	aot_module.plt_entries_ji = g_hash_table_new (NULL, NULL);
+	aot_module.method_to_lmethod = g_hash_table_new (NULL, NULL);
 }
 
 /*
@@ -5341,6 +5358,7 @@ mono_llvm_emit_aot_module (const char *filename, int got_size)
 {
 	LLVMTypeRef got_type;
 	LLVMValueRef real_got;
+	MonoLLVMModule *module = &aot_module;
 
 	/* 
 	 * Create the real got variable and replace all uses of the dummy variable with
@@ -5359,6 +5377,26 @@ mono_llvm_emit_aot_module (const char *filename, int got_size)
 	LLVMDeleteGlobal (aot_module.got_var);
 
 	emit_llvm_used (&aot_module);
+
+	/* Replace PLT entries for directly callable methods with the methods themselves */
+	{
+		GHashTableIter iter;
+		MonoJumpInfo *ji;
+		LLVMValueRef callee;
+
+		g_hash_table_iter_init (&iter, aot_module.plt_entries_ji);
+		while (g_hash_table_iter_next (&iter, (void**)&ji, (void**)&callee)) {
+			if (mono_aot_is_direct_callable (ji)) {
+				LLVMValueRef lmethod;
+
+				lmethod = g_hash_table_lookup (module->method_to_lmethod, ji->data.method);
+				if (lmethod) {
+					mono_llvm_replace_uses_of (callee, lmethod);
+					mono_aot_mark_unused_llvm_plt_entry (ji);
+				}
+			}
+		}
+	}
 
 #if 0
 	{

mono/mini/aot-compiler.c
@@ -5776,39 +5776,14 @@ emit_plt (MonoAotCompile *acfg)
 		plt_entry = g_hash_table_lookup (acfg->plt_offset_to_entry, GUINT_TO_POINTER (i));
 		ji = plt_entry->ji;
 
-		if (acfg->llvm) {
-			/*
-			 * If the target is directly callable, alias the plt symbol to point to
-			 * the method code.
-			 * FIXME: Use this to simplify emit_and_reloc_code ().
-			 * FIXME: Avoid the got slot.
-			 * FIXME: Add support to the binary writer.
-			 */
-			if (ji && is_direct_callable (acfg, NULL, ji) && !acfg->use_bin_writer) {
-				MonoCompile *callee_cfg = g_hash_table_lookup (acfg->method_to_cfg, ji->data.method);
-
-				if (callee_cfg) {
-					if (acfg->thumb_mixed && !callee_cfg->compile_llvm) {
-						/* LLVM calls the PLT entries using bl, so emit a stub */
-						emit_set_thumb_mode (acfg);
-						fprintf (acfg->fp, "\n.thumb_func\n");
-						emit_label (acfg, plt_entry->llvm_symbol);
-						fprintf (acfg->fp, "bx pc\n");
-						fprintf (acfg->fp, "nop\n");
-						emit_set_arm_mode (acfg);
-						fprintf (acfg->fp, "b %s\n", callee_cfg->asm_symbol);
-					} else {
-						fprintf (acfg->fp, "\n.set %s, %s\n", plt_entry->llvm_symbol, callee_cfg->asm_symbol);
-					}
-					continue;
-				}
-			}
-		}
-
 		debug_sym = plt_entry->debug_sym;
 
 		if (acfg->thumb_mixed && !plt_entry->jit_used)
 			/* Emit only a thumb version */
+			continue;
+
+		/* Skip plt entries not actually called */
+		if (!plt_entry->jit_used && !plt_entry->llvm_used)
 			continue;
 
 		if (acfg->llvm && !acfg->thumb_mixed)
@@ -5849,9 +5824,6 @@ emit_plt (MonoAotCompile *acfg)
 
 			plt_entry = g_hash_table_lookup (acfg->plt_offset_to_entry, GUINT_TO_POINTER (i));
 			ji = plt_entry->ji;
-
-			if (ji && is_direct_callable (acfg, NULL, ji) && !acfg->use_bin_writer)
-				continue;
 
 			/* Skip plt entries not actually called by LLVM code */
 			if (!plt_entry->llvm_used)
@@ -7025,6 +6997,21 @@ mono_aot_get_method_name (MonoCompile *cfg)
 		return g_strdup_printf ("%s_%s", llvm_acfg->assembly_name_sym, get_debug_sym (cfg->orig_method, "", llvm_acfg->method_label_hash));
 	else
 		return get_debug_sym (cfg->orig_method, "", llvm_acfg->method_label_hash);
+}
+
+gboolean
+mono_aot_is_direct_callable (MonoJumpInfo *patch_info)
+{
+	return is_direct_callable (llvm_acfg, NULL, patch_info);
+}
+
+void
+mono_aot_mark_unused_llvm_plt_entry (MonoJumpInfo *patch_info)
+{
+	MonoPltEntry *plt_entry;
+
+	plt_entry = get_plt_entry (llvm_acfg, patch_info);
+	plt_entry->llvm_used = FALSE;
 }
 
 char*

mono/mini/mini.h
@@ -2174,6 +2174,8 @@ guint32  mono_aot_method_hash               (MonoMethod *method) MONO_INTERNAL;
 MonoMethod* mono_aot_get_array_helper_from_wrapper (MonoMethod *method) MONO_INTERNAL;
 guint32  mono_aot_get_got_offset            (MonoJumpInfo *ji) MONO_LLVM_INTERNAL;
 char*    mono_aot_get_method_name           (MonoCompile *cfg) MONO_LLVM_INTERNAL;
+gboolean mono_aot_is_direct_callable        (MonoJumpInfo *patch_info) MONO_LLVM_INTERNAL;
+void     mono_aot_mark_unused_llvm_plt_entry(MonoJumpInfo *patch_info) MONO_LLVM_INTERNAL;
 char*    mono_aot_get_plt_symbol            (MonoJumpInfoType type, gconstpointer data) MONO_LLVM_INTERNAL;
 int      mono_aot_get_method_index          (MonoMethod *method) MONO_LLVM_INTERNAL;
 MonoJumpInfo* mono_aot_patch_info_dup       (MonoJumpInfo* ji) MONO_LLVM_INTERNAL;

commit 2e8f21d4f2402516b81ed42aacf0f1d43103621e

[llvm] Fix a regression caused by 6ec1ebf99078a996ca16fd0c56cbd4e96e32ed41. Do the direct call rewrite only if the caller and callee signature matches.

mono/mini/mini-llvm.c
@@ -5390,7 +5390,8 @@ mono_llvm_emit_aot_module (const char *filename, int got_size)
 				LLVMValueRef lmethod;
 
 				lmethod = g_hash_table_lookup (module->method_to_lmethod, ji->data.method);
-				if (lmethod) {
+				/* The types might not match because the caller might pass an rgctx */
+				if (lmethod && LLVMTypeOf (callee) == LLVMTypeOf (lmethod)) {
 					mono_llvm_replace_uses_of (callee, lmethod);
 					mono_aot_mark_unused_llvm_plt_entry (ji);
 				}