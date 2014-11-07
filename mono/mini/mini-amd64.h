mono/mono

commit e19f539121e0b78b778aa8cf5711ccc1876eb443

[amd64] Make xmm6:xmm15 callee saved on winx64 as required by the ABI.

mono/mini/mini-amd64.h
@@ -123,8 +123,14 @@ struct sigcontext {
 #define MONO_ARCH_FP_RETURN_REG AMD64_XMM0
 
 /* xmm15 is reserved for use by some opcodes */
+#ifdef TARGET_WIN32
+#define MONO_ARCH_CALLEE_FREGS 0x3f
+/* xmm6:xmm15 */
+#define MONO_ARCH_CALLEE_SAVED_FREGS (0x7fff - 0x3f)
+#else
 #define MONO_ARCH_CALLEE_FREGS 0x7fff
 #define MONO_ARCH_CALLEE_SAVED_FREGS 0
+#endif
 
 #define MONO_MAX_XREGS MONO_MAX_FREGS
 

commit 7d319221f1d2863aca40bc491d7c94bef82853fb

Fix a problem introduced by e19f539121e0b78b778aa8cf5711ccc1876eb443.

mono/mini/mini-amd64.h
@@ -134,8 +134,8 @@ struct sigcontext {
 
 #define MONO_MAX_XREGS MONO_MAX_FREGS
 
-#define MONO_ARCH_CALLEE_XREGS 0x7fff
-#define MONO_ARCH_CALLEE_SAVED_XREGS 0
+#define MONO_ARCH_CALLEE_XREGS MONO_ARCH_CALLEE_FREGS
+#define MONO_ARCH_CALLEE_SAVED_XREGS MONO_ARCH_CALLEE_SAVED_FREGS
 
 
 #define MONO_ARCH_CALLEE_REGS AMD64_CALLEE_REGS