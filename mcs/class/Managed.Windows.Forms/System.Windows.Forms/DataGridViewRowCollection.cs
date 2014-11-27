mono/mono

commit 7d1b878fbf13462a282860c6d1dd493a48d7b1eb

[MWF] DataGridViewRowCollection Clear() speed up
When issuing a Clear() to remove all rows, a ReIndex() was being called for
each row that was being removed.  This is extremely inefficient, instead
do a single ReIndex() after all rows have been removed.

Before doing a Clear() of 30k rows was taking 5.8 seconds.  With this patch
it now takes 670ms.

mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridViewRowCollection.cs
@@ -268,9 +268,9 @@ namespace System.Windows.Forms
 
 				row.SetDataGridView (null);
 				list.Remove (row);
-				ReIndex ();
 			}
 
+			ReIndex ();
 			DataGridView.OnRowsPostRemovedInternal (new DataGridViewRowsRemovedEventArgs (0, total));
 			OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Refresh, null));
 		}

commit fb7332bec128c08c7a84a4fb14a97dc846b96d59

[MWF] Fix bug in DataGridViewRowCollection Clear() introduced by recent commit
7d1b878fbf13462a282860c6d1dd493a48d7b1eb altered the behavior of removing rows, breaking
the DataGridViewRowCollectionTest.ClearRows () test

Before the change, this is what happened when the list was cleared
(the number in brackets is the value of the Index property of the DataGridViewRow):

[0]  Remove   [1]  ReIndex  [0] Remove  [1] ReIndex  [0]
[1] --------> [2] --------> [1] ------> [2] -------> [1]  ...
[2]           [3]           [2]
[3]

After the offending commit, ReIndex is only called after the list is cleared, which means
the Index property of the rows is never updated (as there are no items in the list).

The fix is to explicitly set the Index of each row to 0.
This was the end result before the commit anyway, so we can omit the ReIndex call entirely.

mcs/class/Managed.Windows.Forms/System.Windows.Forms/DataGridViewRowCollection.cs
@@ -261,7 +261,8 @@ namespace System.Windows.Forms
 
 			for (int i = 0; i < total; i++) {
 				DataGridViewRow row = (DataGridViewRow)list[0];
-				
+				row.SetIndex(0);
+
 				// We can exit because the NewRow is always last
 				if (row.IsNewRow)
 					break;
@@ -270,7 +271,6 @@ namespace System.Windows.Forms
 				list.Remove (row);
 			}
 
-			ReIndex ();
 			DataGridView.OnRowsPostRemovedInternal (new DataGridViewRowsRemovedEventArgs (0, total));
 			OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Refresh, null));
 		}