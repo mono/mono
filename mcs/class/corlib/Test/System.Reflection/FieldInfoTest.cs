46b15a2a6b800506ab5d52693f3c68ff82aeb354
Zoltan Varga <vargaz@gmail.com>
2015-01-02 18:36:02 -0500

    [bcl] Fix crashes when FieldInfo.GetValue () is called on a pointer field with a null value. Fixes #25664.

--- mcs/class/corlib/Test/System.Reflection/FieldInfoTest.cs
+++ mcs/class/corlib/Test/System.Reflection/FieldInfoTest.cs
@@ -519,6 +519,10 @@ namespace MonoTests.System.Reflection
 		[Test]
 		public unsafe void GetSetValuePointers ()
 		{
+			Pointer p0 = (Pointer)typeof (FieldInfoTest).GetField ("ip").GetValue (null);
+			int *p0i = (int*)Pointer.Unbox (p0);
+			Assert.AreEqual (IntPtr.Zero, new IntPtr (p0i));
+
 			int i = 5;
 			void *p = &i;
 			typeof (FieldInfoTest).GetField ("ip").SetValue (null, (IntPtr)p);
