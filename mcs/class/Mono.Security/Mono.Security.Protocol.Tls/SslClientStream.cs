ba6b10b69ce3f01e120aad26e1af9617b58d0e1b
Martin Baulig <martin.baulig@xamarin.com>
2015-01-15 11:55:13 +0100

    [Mono.Security]: Improve error reporting when sending TLS Alerts fail.
    Conflicts:
    	mcs/class/Mono.Security/Mono.Security.Protocol.Tls/SslClientStream.cs

--- mcs/class/Mono.Security/Mono.Security.Protocol.Tls/SslClientStream.cs
+++ mcs/class/Mono.Security/Mono.Security.Protocol.Tls/SslClientStream.cs
@@ -586,10 +586,28 @@ namespace Mono.Security.Protocol.Tls
 					break;
 				}
 			}
+			catch (TlsException ex)
+			{
+				AggregateException aggregate;
+				try {
+					// FIXME: should the send alert also be done asynchronously here and below?
+					this.protocol.SendAlert(ex.Alert);
+					aggregate = new AggregateException ("The authentication or decryption has failed.", ex);
+				} catch (Exception alertEx) {
+					aggregate = new AggregateException ("The authentication or decryption has failed (error while sending Alert).", ex, alertEx);
+				}
+				negotiate.SetComplete (aggregate);
+			}
 			catch (Exception ex)
 			{
-				this.protocol.SendAlert(ref ex);
-				negotiate.SetComplete(new IOException("The authentication or decryption has failed.", ex));
+				AggregateException aggregate;
+				try {
+					this.protocol.SendAlert(AlertDescription.InternalError);
+					aggregate = new AggregateException ("The authentication or decryption has failed.", ex);
+				} catch (Exception alertEx) {
+					aggregate = new AggregateException ("The authentication or decryption has failed (error while sending Alert).", ex, alertEx);
+				}
+				negotiate.SetComplete (aggregate);
 			}
 		}
 
