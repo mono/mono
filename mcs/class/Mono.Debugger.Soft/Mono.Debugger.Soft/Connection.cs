mono/mono

commit 52b168e3366344fa426fabb121df79a156a424c9

[Mono.Debugger.Soft] Caching VirtualMachine threads, caching frames in ThreadMirror and fetching StackFrames in bulk for all threads

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/Connection.cs
@@ -1432,12 +1432,12 @@ namespace Mono.Debugger.Soft
 		//
 		public void StartBuffering () {
 			buffer_packets = true;
-			if (Version.AtLeast (3, 34))
+			if (Version.AtLeast (2, 34))
 				VM_StartBuffering ();
 		}
 
 		public void StopBuffering () {
-			if (Version.AtLeast (3, 34))
+			if (Version.AtLeast (2, 34))
 				VM_StopBuffering ();
 			buffer_packets = false;
 
@@ -1983,21 +1983,20 @@ namespace Mono.Debugger.Soft
 			return SendReceive (CommandSet.THREAD, (int)CmdThread.GET_NAME, new PacketWriter ().WriteId (id)).ReadString ();
 		}
 
-		internal FrameInfo[] Thread_GetFrameInfo (long id, int start_frame, int length) {
-			var res = SendReceive (CommandSet.THREAD, (int)CmdThread.GET_FRAME_INFO, new PacketWriter ().WriteId (id).WriteInt (start_frame).WriteInt (length));
-			int count = res.ReadInt ();
-
-			var frames = new FrameInfo [count];
-			for (int i = 0; i < count; ++i) {
-				var f = new FrameInfo ();
-				f.id = res.ReadInt ();
-				f.method = res.ReadId ();
-				f.il_offset = res.ReadInt ();
-				f.flags = (StackFrameFlags)res.ReadByte ();
-				frames [i] = f;
-			}
-
-			return frames;
+		internal void Thread_GetFrameInfo (long id, int start_frame, int length, Action<FrameInfo[]> resultCallaback) {
+			Send (CommandSet.THREAD, (int)CmdThread.GET_FRAME_INFO, new PacketWriter ().WriteId (id).WriteInt (start_frame).WriteInt (length), (res) => {
+				int count = res.ReadInt ();
+				var frames = new FrameInfo[count];
+				for (int i = 0; i < count; ++i) {
+					var f = new FrameInfo ();
+					f.id = res.ReadInt ();
+					f.method = res.ReadId ();
+					f.il_offset = res.ReadInt ();
+					f.flags = (StackFrameFlags)res.ReadByte ();
+					frames [i] = f;
+				}
+				resultCallaback (frames);
+			}, 1);
 		}
 
 		internal int Thread_GetState (long id) {

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/VirtualMachine.cs
@@ -121,6 +121,7 @@ namespace Mono.Debugger.Soft
 
 		public void Resume () {
 			try {
+				InvalidateThreadsAndFramesCache ();
 				conn.VM_Resume ();
 			} catch (CommandException ex) {
 				if (ex.ErrorCode == ErrorCode.NOT_SUSPENDED)
@@ -151,12 +152,50 @@ namespace Mono.Debugger.Soft
 			conn.ForceDisconnect ();
 		}
 
+		HashSet<ThreadMirror> threadsToInvalidate = new HashSet<ThreadMirror> ();
+		ThreadMirror[] threadsCache;
+		object threadCacheLocker=new object();
+
+		internal void InvalidateThreadsAndFramesCache () {
+			lock (threadsToInvalidate) {
+				foreach (var thread in threadsToInvalidate)
+					thread.InvalidateFrames ();
+				threadsToInvalidate.Clear ();
+			}
+			lock (threadCacheLocker) {
+				threadsCache = null;
+			}
+		}
+
+		internal void InvalidateThreadsCache () {
+			lock (threadCacheLocker) {
+				threadsCache = null;
+			}
+		}
+
+		internal void AddThreadToInvalidateList (ThreadMirror threadMirror)
+		{
+			lock (threadsToInvalidate) {
+				threadsToInvalidate.Add (threadMirror);
+			}
+		}
+
 		public IList<ThreadMirror> GetThreads () {
-			long[] ids = vm.conn.VM_GetThreads ();
-			ThreadMirror[] res = new ThreadMirror [ids.Length];
-			for (int i = 0; i < ids.Length; ++i)
-				res [i] = GetThread (ids [i]);
-			return res;
+			lock (threadCacheLocker) {
+				if (threadsCache == null) {
+					long[] ids = vm.conn.VM_GetThreads ();
+					threadsCache = new ThreadMirror [ids.Length];
+					for (int i = 0; i < ids.Length; ++i)
+						threadsCache [i] = GetThread (ids [i]);
+					if (Version.AtLeast (2, 34)) {
+						conn.StartBuffering ();
+						foreach (var thread in threadsCache)
+							thread.FetchFrames ();
+						conn.StopBuffering ();
+					}
+				}
+				return threadsCache;
+			}
 		}
 
 		// Same as the mirrorOf methods in JDI
@@ -675,9 +714,11 @@ namespace Mono.Debugger.Soft
 					vm.notify_vm_event (EventType.VMDeath, suspend_policy, req_id, thread_id, null, ei.ExitCode);
 					break;
 				case EventType.ThreadStart:
+					vm.InvalidateThreadsCache ();
 					l.Add (new ThreadStartEvent (vm, req_id, id));
 					break;
 				case EventType.ThreadDeath:
+					vm.InvalidateThreadsCache ();
 					l.Add (new ThreadDeathEvent (vm, req_id, id));
 					break;
 				case EventType.AssemblyLoad:

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/ObjectMirror.cs
@@ -262,7 +262,7 @@ namespace Mono.Debugger.Soft
 				f |= InvokeFlags.SINGLE_THREADED;
 
 			InvokeAsyncResult r = new InvokeAsyncResult { AsyncState = state, AsyncWaitHandle = new ManualResetEvent (false), VM = vm, Thread = thread, Callback = callback };
-
+			thread.InvalidateFrames ();
 			r.ID = vm.conn.VM_BeginInvokeMethod (thread.Id, method.Id, this_obj != null ? vm.EncodeValue (this_obj) : vm.EncodeValue (vm.CreateValue (null)), vm.EncodeValues (arguments), f, InvokeCB, r);
 
 			return r;
@@ -370,6 +370,7 @@ namespace Mono.Debugger.Soft
 			var args = new List<ValueImpl[]> ();
 			for (int i = 0; i < methods.Length; ++i)
 				args.Add (vm.EncodeValues (arguments [i]));
+			thread.InvalidateFrames ();
 			r.ID = vm.conn.VM_BeginInvokeMethods (thread.Id, mids, this_obj != null ? vm.EncodeValue (this_obj) : vm.EncodeValue (vm.CreateValue (null)), args, f, InvokeMultipleCB, r);
 
 			return r;

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/ThreadMirror.cs
@@ -7,7 +7,12 @@ namespace Mono.Debugger.Soft
 	public class ThreadMirror : ObjectMirror
 	{
 		string name;
+		bool framesCacheIsInvalid = true;
+		bool fetchingInProgress;
+		object fetchingLocker = new object ();
+		ManualResetEvent fetchingEvent = new ManualResetEvent (false);
 		ThreadInfo info;
+		StackFrame[] frames;
 
 		internal ThreadMirror (VirtualMachine vm, long id) : base (vm, id) {
 		}
@@ -15,22 +20,47 @@ namespace Mono.Debugger.Soft
 		internal ThreadMirror (VirtualMachine vm, long id, TypeMirror type, AppDomainMirror domain) : base (vm, id, type, domain) {
 		}
 
-		// FIXME: Cache, invalidate when the thread/runtime is resumed
 		public StackFrame[] GetFrames () {
-			FrameInfo[] frame_info = vm.conn.Thread_GetFrameInfo (id, 0, -1);
+			FetchFrames (true);
+			fetchingEvent.WaitOne ();
+			return frames;
+		}
 
-			var frames = new List<StackFrame> ();
+		internal void InvalidateFrames () {
+			framesCacheIsInvalid = true;
+		}
 
-			for (int i = 0; i < frame_info.Length; ++i) {
-				FrameInfo info = (FrameInfo)frame_info [i];
-				MethodMirror method = vm.GetMethod (info.method);
-				var f = new StackFrame (vm, info.id, this, method, info.il_offset, info.flags);
-				if (!(f.IsNativeTransition && !NativeTransitions))
-					frames.Add (f);
+		internal void FetchFrames (bool mustFetch = false) {
+			lock (fetchingLocker) {
+				if (fetchingInProgress || !framesCacheIsInvalid)
+					return;
+				framesCacheIsInvalid = false;
+				fetchingInProgress = true;
+				fetchingEvent.Reset ();
 			}
-
-			return frames.ToArray ();
-	    }
+			vm.conn.Thread_GetFrameInfo (id, 0, -1, (frame_info) => {
+				var framesList = new List<StackFrame> ();
+				for (int i = 0; i < frame_info.Length; ++i) {
+					var frameInfo = (FrameInfo)frame_info [i];
+					var method = vm.GetMethod (frameInfo.method);
+					var f = new StackFrame (vm, frameInfo.id, this, method, frameInfo.il_offset, frameInfo.flags);
+					if (!(f.IsNativeTransition && !NativeTransitions))
+						framesList.Add (f);
+				}
+				lock (fetchingLocker) {
+					vm.AddThreadToInvalidateList (this);
+					fetchingInProgress = false;
+					//In case it was invalidated during waiting for response from
+					//runtime and mustFetch was set refetch
+					if (framesCacheIsInvalid && mustFetch) {
+						FetchFrames (mustFetch);
+						return;
+					}
+					frames = framesList.ToArray ();
+					fetchingEvent.Set ();
+				}
+			});
+		}
 
 		public string Name {
 			get {
@@ -38,7 +68,7 @@ namespace Mono.Debugger.Soft
 					name = vm.conn.Thread_GetName (id);
 				return name;
 			}
-	    }
+		}
 
 		public new long Id {
 			get {

commit 948689360ba96ab29b109ae353e678ef633e2675

[sdb] Fixed GetThreads deadlock if thread was started or stopped during GetThreads call.
Regression introduced in 52b168e3366344fa426fabb121df79a156a424c9

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/Connection.cs
@@ -1638,13 +1638,14 @@ namespace Mono.Debugger.Soft
 			SendReceive (CommandSet.VM, (int)CmdVM.SET_PROTOCOL_VERSION, new PacketWriter ().WriteInt (major).WriteInt (minor));
 		}
 
-		internal long[] VM_GetThreads () {
-			var res = SendReceive (CommandSet.VM, (int)CmdVM.ALL_THREADS, null);
-			int len = res.ReadInt ();
-			long[] arr = new long [len];
-			for (int i = 0; i < len; ++i)
-				arr [i] = res.ReadId ();
-			return arr;
+		internal void VM_GetThreads (Action<long[]> resultCallaback) {
+			Send (CommandSet.VM, (int)CmdVM.ALL_THREADS, null, (res) => {
+				int len = res.ReadInt ();
+				long[] arr = new long [len];
+				for (int i = 0; i < len; ++i)
+					arr [i] = res.ReadId ();
+				resultCallaback(arr);
+			}, 1);
 		}
 
 		internal void VM_Suspend () {

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/VirtualMachine.cs
@@ -162,15 +162,11 @@ namespace Mono.Debugger.Soft
 					thread.InvalidateFrames ();
 				threadsToInvalidate.Clear ();
 			}
-			lock (threadCacheLocker) {
-				threadCache = null;
-			}
+			threadCache = null;
 		}
 
 		internal void InvalidateThreadCache () {
-			lock (threadCacheLocker) {
-				threadCache = null;
-			}
+			threadCache = null;
 		}
 
 		internal void AddThreadToInvalidateList (ThreadMirror threadMirror)
@@ -181,14 +177,30 @@ namespace Mono.Debugger.Soft
 		}
 
 		public IList<ThreadMirror> GetThreads () {
-			lock (threadCacheLocker) {
-				if (threadCache == null) {
-					long[] ids = vm.conn.VM_GetThreads ();
-					threadCache = new ThreadMirror [ids.Length];
-					for (int i = 0; i < ids.Length; ++i)
-						threadCache [i] = GetThread (ids [i]);
+			var threads = threadCache;
+			if (threads == null) {
+				long[] ids = null;
+				var fetchingEvent = new ManualResetEvent (false);
+				vm.conn.VM_GetThreads ((threadsIds) => {
+					ids = threadsIds;
+					threadCache = threads = new ThreadMirror [threadsIds.Length];
+					fetchingEvent.Set ();
+				});
+				if (WaitHandle.WaitAny (new []{ vm.conn.DisconnectedEvent, fetchingEvent }) == 0) {
+					throw new VMDisconnectedException ();
 				}
-				return threadCache;
+				for (int i = 0; i < ids.Length; ++i)
+					threads [i] = GetThread (ids [i]);
+				//Uncomment lines below if you want to re-fetch threads if new threads were started/stopped while
+				//featching threads... This is probably more correct but might cause deadlock of this method if runtime
+				//is starting/stopping threads nonstop, need way to prevent this(counting number of recursions?)
+				//possiblity before uncommenting
+				//if (threadCache != threads) {//While fetching threads threadCache was invalidated(thread was created/destoyed)
+				//	return GetThreads ();
+				//}
+				return threads;
+			} else {
+				return threads;
 			}
 		}
 