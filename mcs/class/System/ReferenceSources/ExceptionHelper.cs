f0abad374853a184443d4d7c0644cfa7b2c71aa9
Martin Baulig <martin.baulig@xamarin.com>
2015-01-22 19:51:10 +0100

    Bring Internal.cs, Logging.cs and _LoggingObject.cs.

--- mcs/class/System/ReferenceSources/ExceptionHelper.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.Permissions;
-
-namespace System.Net {
-	internal static class ExceptionHelper
-    {
-        internal static readonly WebPermission WebPermissionUnrestricted = new WebPermission(NetworkAccess.Connect);
-    }
-}
