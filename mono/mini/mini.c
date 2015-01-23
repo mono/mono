mono/mono

commit adca49cdd28e7c7b0cc65073167c143cc5f52fdd

[jit] First pass of portability fixes for the JIT.

mono/mini/mini.c
@@ -12,7 +12,6 @@
 
 #define MONO_LLVM_IN_MINI 1
 #include <config.h>
-#include <signal.h>
 #ifdef HAVE_ALLOCA_H
 #include <alloca.h>
 #endif

mono/mini/driver.c
@@ -10,7 +10,9 @@
  */
 
 #include <config.h>
+#ifdef HAVE_SIGNAL_H
 #include <signal.h>
+#endif
 #if HAVE_SCHED_SETAFFINITY
 #include <sched.h>
 #endif

mono/mini/exceptions-amd64.c
@@ -11,9 +11,11 @@
 #include <config.h>
 
 #include <glib.h>
-#include <signal.h>
 #include <string.h>
 
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
 #ifdef HAVE_UCONTEXT_H
 #include <ucontext.h>
 #endif

mono/mini/mini-amd64.c
@@ -8130,6 +8130,8 @@ mono_arch_get_cie_program (void)
 	return l;
 }
 
+#ifndef DISABLE_JIT
+
 MonoInst*
 mono_arch_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsig, MonoInst **args)
 {
@@ -8204,6 +8206,7 @@ mono_arch_emit_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMetho
 
 	return ins;
 }
+#endif
 
 gboolean
 mono_arch_print_tree (MonoInst *tree, int arity)

mono/mini/mini-amd64.h
@@ -270,7 +270,7 @@ typedef struct {
  */
 #define MONO_ARCH_VARARG_ICALLS 1
 
-#if !defined( HOST_WIN32 ) && !defined(__native_client__) && !defined(__native_client_codegen__)
+#if (!defined( HOST_WIN32 ) && !defined(__native_client__) && !defined(__native_client_codegen__)) && defined (HAVE_SIGACTION)
 
 #define MONO_ARCH_USE_SIGACTION 1
 

mono/mini/mini-exceptions.c
@@ -12,8 +12,11 @@
 
 #include <config.h>
 #include <glib.h>
-#include <signal.h>
 #include <string.h>
+
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
 
 #ifdef HAVE_EXECINFO_H
 #include <execinfo.h>

mono/mini/mini.h
@@ -8,7 +8,9 @@
 
 #include "config.h"
 #include <glib.h>
+#ifdef HAVE_SIGNAL_H
 #include <signal.h>
+#endif
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif

mono/mini/trace.c
@@ -10,7 +10,6 @@
  */
 
 #include <config.h>
-#include <signal.h>
 #ifdef HAVE_ALLOCA_H
 #include <alloca.h>
 #endif

commit 6d17bd56118016a0d269c3de5fb2e3f8eb3c369f

[mini] Include signal.h in mini.c.
This fixes Monodroid cross build, which broke with https://github.com/mono/mono/commit/adca49cdd28e7c7b0cc65073167c143cc5f52fdd.

Error:

```
/Users/joao/Dev/droid/mono/mono/mini/mini.c:6645:31: note: each undeclared identifier is reported only once for each function it appears in
/Users/joao/Dev/droid/mono/mono/mini/mini.c: In function 'mono_sigsegv_signal_handler':
/Users/joao/Dev/droid/mono/mono/mini/mini.c:6704:31: error: 'SIGSEGV' undeclared (first use in this function)
   mono_handle_native_sigsegv (SIGSEGV, ctx, info);
```

mono/mini/mini.c
@@ -22,6 +22,9 @@
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #endif
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
 
 #include <mono/utils/memcheck.h>
 