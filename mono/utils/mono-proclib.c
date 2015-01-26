mono/mono

commit 2f1b464beb14a1b6b6099a03fc332493293bef35

[runtime] Avoid failures in Process.GetProcesses () when the number of processes is above 400. Fixes #26382.

mono/utils/mono-proclib.c
@@ -61,17 +61,19 @@ mono_process_list (int *size)
 	struct kinfo_proc2 *processes = malloc (data_len);
 #else
 	int mib [4];
-	size_t data_len = sizeof (struct kinfo_proc) * 400;
-	struct kinfo_proc *processes = malloc (data_len);
+	size_t data_len = sizeof (struct kinfo_proc) * 16;
+	struct kinfo_proc *processes;
+	int limit = 8;
 #endif /* KERN_PROC2 */
 	void **buf = NULL;
 
 	if (size)
 		*size = 0;
+
+#ifdef KERN_PROC2
 	if (!processes)
 		return NULL;
 
-#ifdef KERN_PROC2
 	mib [0] = CTL_KERN;
 	mib [1] = KERN_PROC2;
 	mib [2] = KERN_PROC_ALL;
@@ -80,19 +82,34 @@ mono_process_list (int *size)
 	mib [5] = 400; /* XXX */
 
 	res = sysctl (mib, 6, processes, &data_len, NULL, 0);
-#else
-	mib [0] = CTL_KERN;
-	mib [1] = KERN_PROC;
-	mib [2] = KERN_PROC_ALL;
-	mib [3] = 0;
-	
-	res = sysctl (mib, 4, processes, &data_len, NULL, 0);
-#endif /* KERN_PROC2 */
-
 	if (res < 0) {
 		free (processes);
 		return NULL;
 	}
+#else
+	processes = NULL;
+	while (limit) {
+		mib [0] = CTL_KERN;
+		mib [1] = KERN_PROC;
+		mib [2] = KERN_PROC_ALL;
+		mib [3] = 0;
+
+		res = sysctl (mib, 4, NULL, &data_len, NULL, 0);
+		if (res)
+			return NULL;
+		processes = malloc (data_len);
+		res = sysctl (mib, 4, NULL, &data_len, NULL, 0);
+		if (res < 0) {
+			free (processes);
+			if (errno != ENOMEM)
+				return NULL;
+			limit --;
+		} else {
+			break;
+		}
+	}
+#endif /* KERN_PROC2 */
+
 #ifdef KERN_PROC2
 	res = data_len/sizeof (struct kinfo_proc2);
 #else

commit 20eb41f97a6dcfeb4b738e0efdc9ffc1b9239c05

[runtime] Fixed the iOS build.
Failure caused by https://github.com/mono/mono/commit/2f1b464beb14a1b6b6099a03fc332493293bef35.

```
../../../../../mono/mono/utils/mono-proclib.c:104:8: error: use of undeclared identifier 'errno'
                        if (errno != ENOMEM)
                            ^
../../../../../mono/mono/utils/mono-proclib.c:104:17: error: use of undeclared identifier 'ENOMEM'
                        if (errno != ENOMEM)
```

mono/utils/mono-proclib.c
@@ -20,6 +20,7 @@
 #endif
 
 #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#include <sys/errno.h>
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>