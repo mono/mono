mono/mono

commit 417fc5e7897fab0c675b37930853e861e54f6ee1

[runtime] Cleanup networking init/shutdown code.

mono/io-layer/socket-wrappers.h
@@ -85,5 +85,6 @@ extern int _wapi_FD_ISSET(guint32 handle, fd_set *set);
 extern void _wapi_FD_SET(guint32 handle, fd_set *set);
 #endif
 
+extern void _wapi_cleanup_networking (void);
 #endif /* HOST_WIN32 */
 

mono/utils/networking-posix.c
@@ -325,3 +325,15 @@ mono_networking_addr_to_str (MonoAddress *address, char *buffer, socklen_t bufle
 }
 
 #endif
+
+void
+mono_networking_init (void)
+{
+	//nothing really
+}
+
+void
+mono_networking_shutdown (void)
+{
+	//nothing really
+}

mono/utils/networking-windows.c
@@ -8,12 +8,30 @@
 #include <mono/utils/networking.h>
 
 #if defined(HOST_WIN32)
+#include 
 
 void *
 mono_get_local_interfaces (int family, int *interface_count)
 {
 	*interface_count = 0;
 	return NULL;
+}
+
+void
+mono_networking_init (void)
+{
+	WSADATA wsadata;
+	int err;
+
+	err = WSAStartup (MAKEWORD (2,0), &wsadata);
+	if(err)
+		g_error ("%s: Couldn't initialise networking", __func__);
+}
+
+void
+mono_networking_shutdown (void)
+{
+	WSACleanup ();
 }
 
 #endif /* defined(HOST_WIN32) */

mono/io-layer/sockets.c
@@ -59,7 +59,6 @@
 #define DEBUG(...)
 #endif
 
-static guint32 startup_count=0;
 static guint32 in_cleanup = 0;
 
 static void socket_close (gpointer handle, gpointer data);
@@ -87,11 +86,6 @@ static void socket_close (gpointer handle, gpointer data)
 
 	DEBUG ("%s: closing socket handle %p", __func__, handle);
 
-	if (startup_count == 0 && !in_cleanup) {
-		WSASetLastError (WSANOTINITIALISED);
-		return;
-	}
-
 	/* Shutdown the socket for reading, to interrupt any potential
 	 * receives that may be blocking for data.  See bug 75705.
 	 */
@@ -114,33 +108,6 @@ static void socket_close (gpointer handle, gpointer data)
 		socket_handle->saved_error = 0;
 }
 
-int WSAStartup(guint32 requested, WapiWSAData *data)
-{
-	if (data == NULL) {
-		return(WSAEFAULT);
-	}
-
-	/* Insist on v2.0+ */
-	if (requested < MAKEWORD(2,0)) {
-		return(WSAVERNOTSUPPORTED);
-	}
-
-	startup_count++;
-
-	/* I've no idea what is the minor version of the spec I read */
-	data->wHighVersion = MAKEWORD(2,2);
-	
-	data->wVersion = requested < data->wHighVersion? requested:
-		data->wHighVersion;
-
-	DEBUG ("%s: high version 0x%x", __func__, data->wHighVersion);
-	
-	strncpy (data->szDescription, "WAPI", WSADESCRIPTION_LEN);
-	strncpy (data->szSystemStatus, "groovy", WSASYS_STATUS_LEN);
-	
-	return(0);
-}
-
 static gboolean
 cleanup_close (gpointer handle, gpointer data)
 {
@@ -148,19 +115,13 @@ cleanup_close (gpointer handle, gpointer data)
 	return TRUE;
 }
 
-int WSACleanup(void)
+void _wapi_cleanup_networking(void)
 {
 	DEBUG ("%s: cleaning up", __func__);
-
-	if (--startup_count) {
-		/* Do nothing */
-		return(0);
-	}
 
 	in_cleanup = 1;
 	_wapi_handle_foreach (WAPI_HANDLE_SOCKET, cleanup_close, NULL);
 	in_cleanup = 0;
-	return(0);
 }
 
 void WSASetLastError(int error)
@@ -194,11 +155,6 @@ guint32 _wapi_accept(guint32 fd, struct sockaddr *addr, socklen_t *addrlen)
 	struct _WapiHandle_socket new_socket_handle = {0};
 	gboolean ok;
 	int new_fd;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(INVALID_SOCKET);
-	}
 
 	if (addr != NULL && *addrlen < sizeof(struct sockaddr)) {
 		WSASetLastError (WSAEFAULT);
@@ -268,11 +224,6 @@ int _wapi_bind(guint32 fd, struct sockaddr *my_addr, socklen_t addrlen)
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -297,11 +248,6 @@ int _wapi_connect(guint32 fd, const struct sockaddr *serv_addr,
 	struct _WapiHandle_socket *socket_handle;
 	gboolean ok;
 	gint errnum;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -402,11 +348,6 @@ int _wapi_getpeername(guint32 fd, struct sockaddr *name, socklen_t *namelen)
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -431,11 +372,6 @@ int _wapi_getsockname(guint32 fd, struct sockaddr *name, socklen_t *namelen)
 {
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -466,11 +402,6 @@ int _wapi_getsockopt(guint32 fd, int level, int optname, void *optval,
 	void *tmp_val;
 	struct _WapiHandle_socket *socket_handle;
 	gboolean ok;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -531,11 +462,6 @@ int _wapi_listen(guint32 fd, int backlog)
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -567,11 +493,6 @@ int _wapi_recvfrom(guint32 fd, void *buf, size_t len, int recv_flags,
 	struct _WapiHandle_socket *socket_handle;
 	gboolean ok;
 	int ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -630,11 +551,6 @@ _wapi_recvmsg(guint32 fd, struct msghdr *msg, int recv_flags)
 	gboolean ok;
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -671,11 +587,6 @@ int _wapi_send(guint32 fd, const void *msg, size_t len, int send_flags)
 {
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -714,11 +625,6 @@ int _wapi_sendto(guint32 fd, const void *msg, size_t len, int send_flags,
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -746,11 +652,6 @@ _wapi_sendmsg(guint32 fd,  const struct msghdr *msg, int send_flags)
 {
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -785,11 +686,6 @@ int _wapi_setsockopt(guint32 fd, int level, int optname,
 	int bufsize = 0;
 #endif
 	struct timeval tv;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -852,12 +748,7 @@ int _wapi_shutdown(guint32 fd, int how)
 	gboolean ok;
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
+
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -1127,12 +1018,7 @@ TransmitFile (guint32 socket, gpointer file, guint32 bytes_to_write, guint32 byt
 {
 	gpointer sock = GUINT_TO_POINTER (socket);
 	gint ret;
-	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return FALSE;
-	}
-	
+
 	if (_wapi_handle_type (sock) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return FALSE;
@@ -1181,11 +1067,6 @@ WSAIoctl (guint32 fd, gint32 command,
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	gchar *buffer = NULL;
-
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
 
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
@@ -1325,11 +1206,6 @@ int ioctlsocket(guint32 fd, gint32 command, gpointer arg)
 	gpointer handle = GUINT_TO_POINTER (fd);
 	int ret;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-	
 	if (_wapi_handle_type (handle) != WAPI_HANDLE_SOCKET) {
 		WSASetLastError (WSAENOTSOCK);
 		return(SOCKET_ERROR);
@@ -1402,11 +1278,6 @@ int _wapi_select(int nfds G_GNUC_UNUSED, fd_set *readfds, fd_set *writefds,
 {
 	int ret, maxfd;
 	
-	if (startup_count == 0) {
-		WSASetLastError (WSANOTINITIALISED);
-		return(SOCKET_ERROR);
-	}
-
 	for (maxfd = FD_SETSIZE-1; maxfd >= 0; maxfd--) {
 		if ((readfds && FD_ISSET (maxfd, readfds)) ||
 		    (writefds && FD_ISSET (maxfd, writefds)) ||

mono/io-layer/sockets.h
@@ -78,9 +78,6 @@ typedef gboolean (*WapiTransmitFileFn)(guint32, gpointer, guint32, guint32,
 					WapiTransmitFileBuffers *,
 					WapiTransmitFileFlags);
 
-
-extern int WSAStartup(guint32 requested, WapiWSAData *data);
-extern int WSACleanup(void);
 extern void WSASetLastError(int error);
 extern int WSAGetLastError(void);
 extern int closesocket(guint32 handle);

mono/io-layer/wapi-remap.h
@@ -81,8 +81,6 @@
 #define CreateSemaphore wapi_CreateSemaphore
 #define ReleaseSemaphore wapi_ReleaseSemaphore
 #define OpenSemaphore wapi_OpenSemaphore 
-#define WSAStartup wapi_WSAStartup
-#define WSACleanup wapi_WSACleanup
 #define WSASetLastError wapi_WSASetLastError
 #define WSAGetLastError wapi_WSAGetLastError
 #define WSAIoctl wapi_WSAIoctl 

mono/metadata/socket-io.c
@@ -2528,22 +2528,13 @@ ves_icall_System_Net_Sockets_Socket_SendFile (SOCKET sock, MonoString *filename,
 
 void mono_network_init(void)
 {
-	WSADATA wsadata;
-	int err;
-	
-	err=WSAStartup(MAKEWORD(2,0), &wsadata);
-	if(err!=0) {
-		g_error("%s: Couldn't initialise networking", __func__);
-		exit(-1);
-	}
-
-	LOGDEBUG (g_message("%s: Using socket library: %s", __func__, wsadata.szDescription));
-	LOGDEBUG (g_message("%s: Socket system status: %s", __func__, wsadata.szSystemStatus));
+	mono_networking_init ();
 }
 
 void mono_network_cleanup(void)
 {
-	WSACleanup();
+	_wapi_cleanup_networking ();
+	mono_networking_shutdown ();
 }
 
 void

mono/utils/networking.h
@@ -99,5 +99,8 @@ int mono_networking_get_tcp_protocol (void) MONO_INTERNAL;
 int mono_networking_get_ip_protocol (void) MONO_INTERNAL;
 int mono_networking_get_ipv6_protocol (void) MONO_INTERNAL;
 
+void mono_networking_init (void) MONO_INTERNAL;
+void mono_networking_shutdown (void) MONO_INTERNAL;
+
 
 #endif

commit 5a453b160bf17baedf76b5eb84e819b2b0ba23e0

Fix Windows build
It was broken by 417fc5e7897fab0c675b37930853e861e54f6ee1

mono/io-layer/socket-wrappers.h
@@ -36,6 +36,7 @@
 #define _wapi_shutdown shutdown 
 #define _wapi_socket WSASocket 
 #define _wapi_select select 
+#define _wapi_cleanup_networking WSACleanup
 
 /* No need to wrap FD_ZERO because it doesnt involve file
  * descriptors

mono/utils/networking-posix.c
@@ -326,6 +326,8 @@ mono_networking_addr_to_str (MonoAddress *address, char *buffer, socklen_t bufle
 
 #endif
 
+#if HAVE_NET_IF_H
+
 void
 mono_networking_init (void)
 {
@@ -337,3 +339,5 @@ mono_networking_shutdown (void)
 {
 	//nothing really
 }
+
+#endif

mono/utils/networking-windows.c
@@ -8,7 +8,6 @@
 #include <mono/utils/networking.h>
 
 #if defined(HOST_WIN32)
-#include 
 
 void *
 mono_get_local_interfaces (int family, int *interface_count)