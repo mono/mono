b927fa8ee635c50c566c900aad6d0a894c95fc77
Marek Safar <marek.safar@gmail.com>
2015-02-06 18:36:45 +0100

    [corlib] Imports messages from coreclr project

--- mcs/class/corlib/ReferenceSources/Environment.cs
+++ mcs/class/corlib/ReferenceSources/Environment.cs
@@ -6,56 +6,17 @@ namespace System
 	{
 		internal static string GetResourceString (string key)
 		{
-			switch (key) {
-			case "AssertionFailed":
-				return "Assertion failed.";
-			case "AssumptionFailed":
-				return "Assumption failed.";
-			case "InvariantFailed":
-				return "Invariant failed.";
-			case "PostconditionFailed":
-				return "Postcondition failed.";
-			case "PostconditionOnExceptionFailed":
-				return "Postcondition failed after throwing an exception.";
-			case "PreconditionFailed":
-				return "Precondition failed.";
-			case "AggregateException_ToString":
-				return "AggregateException_ToString={0}{1}---> (Inner Exception #{2}) {3}{4}{5}";
-			case "AggregateException_ctor_DefaultMessage":
-				return "One or more errors occurred.";
-			}
-
-			return key;
+			return Messages.GetMessage (key);
 		}
 
 		internal static string GetResourceString (string key, CultureInfo culture)
 		{
-			return key;
+			return GetResourceString (key);
 		}
 
 		internal static string GetResourceString (string key, params object[] values)
 		{
-			switch (key) {
-			case "AssertionFailed_Cnd":
-				key = "Assertion failed: {0}";
-				break;
-			case "AssumptionFailed_Cnd":
-				key = "Assumption failed: {0}";
-				break;
-			case "InvariantFailed_Cnd":
-				key = "Invariant failed: {0}";
-				break;
-			case "PostconditionFailed_Cnd":
-				key = "Postcondition failed: {0}";
-				break;
-			case "PostconditionOnExceptionFailed_Cnd":
-				key = "Postcondition failed after throwing an exception: {0}";
-				break;
-			case "PreconditionFailed_Cnd":
-				key = "Precondition failed: {0}";
-				break;
-			}
-
+			key = Messages.GetMessage (key);
 			return string.Format (CultureInfo.InvariantCulture, key, values);
 		}
 	}
