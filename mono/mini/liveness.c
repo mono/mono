mono/mono

commit 00a28a7dfd44a10c3bc4c3b57449c93aa5124092

[jit] Avoid live range indexes overflowing for large basic blocks in the liveness pass. Fixes #26732.

mono/mini/liveness.c
@@ -20,6 +20,8 @@
 
 #define BITS_PER_CHUNK MONO_BITSET_BITS_PER_CHUNK
 
+#define BB_ID_SHIFT 18
+
 /* 
  * The liveness2 pass can't handle long vars on 32 bit platforms because the component
  * vars have the same 'idx'.
@@ -217,7 +219,7 @@ analyze_liveness_bb (MonoCompile *cfg, MonoBasicBlock *bb)
 	MonoInst *ins;
 	int sreg, inst_num;
 	MonoMethodVar *vars = cfg->vars;
-	guint32 abs_pos = (bb->dfn << 16);
+	guint32 abs_pos = (bb->dfn << BB_ID_SHIFT);
 	
 	/* Start inst_num from > 0, so last_use.abs_pos is only 0 for dead variables */
 	for (inst_num = 2, ins = bb->code; ins; ins = ins->next, inst_num += 2) {
@@ -492,7 +494,7 @@ mono_analyze_liveness (MonoCompile *cfg)
 	for (i = 0; i < cfg->num_bblocks; ++i) {
 		MonoBasicBlock *bb = cfg->bblocks [i];
 		guint32 max;
-		guint32 abs_pos = (bb->dfn << 16);
+		guint32 abs_pos = (bb->dfn << BB_ID_SHIFT);
 		MonoMethodVar *vars = cfg->vars;
 
 		if (!bb->live_out_set)
@@ -512,7 +514,7 @@ mono_analyze_liveness (MonoCompile *cfg)
 				if (bits_in & 1)
 					update_live_range (&vars [k], abs_pos + 0);
 				if (bits_out & 1)
-					update_live_range (&vars [k], abs_pos + 0xffff);
+					update_live_range (&vars [k], abs_pos + ((1 << BB_ID_SHIFT) - 1));
 				bits_in >>= 1;
 				bits_out >>= 1;
 				k ++;
@@ -859,6 +861,23 @@ mono_analyze_liveness2 (MonoCompile *cfg)
 	if (disabled)
 		return;
 
+	if (cfg->num_bblocks < (1 << (32 - BB_ID_SHIFT)))
+		/* Ranges would overflow */
+		return;
+
+	for (bnum = cfg->num_bblocks - 1; bnum >= 0; --bnum) {
+		MonoBasicBlock *bb = cfg->bblocks [bnum];
+		MonoInst *ins;
+
+		nins = 0;
+		for (nins = 0, ins = bb->code; ins; ins = ins->next, ++nins)
+			nins ++;
+
+		if (nins >= ((1 << BB_ID_SHIFT) - 1))
+			/* Ranges would overflow */
+			return;
+	}
+
 	LIVENESS_DEBUG (printf ("LIVENESS 2 %s\n", mono_method_full_name (cfg->method, TRUE)));
 
 	/*
@@ -886,12 +905,12 @@ mono_analyze_liveness2 (MonoCompile *cfg)
 		MonoBasicBlock *bb = cfg->bblocks [bnum];
 		MonoInst *ins;
 
-		block_from = (bb->dfn << 16) + 1; /* so pos > 0 */
+		block_from = (bb->dfn << BB_ID_SHIFT) + 1; /* so pos > 0 */
 		if (bnum < cfg->num_bblocks - 1)
 			/* Beginning of the next bblock */
-			block_to = (cfg->bblocks [bnum + 1]->dfn << 16) + 1;
+			block_to = (cfg->bblocks [bnum + 1]->dfn << BB_ID_SHIFT) + 1;
 		else
-			block_to = (bb->dfn << 16) + 0xffff;
+			block_to = (bb->dfn << BB_ID_SHIFT) + ((1 << BB_ID_SHIFT) - 1);
 
 		LIVENESS_DEBUG (printf ("LIVENESS BLOCK BB%d:\n", bb->block_num));
 

commit bcd70a77a7da753708f8603cef08d6aac9c963c6

[jit] Fix a regression introduced by 00a28a7dfd44a10c3bc4c3b57449c93aa5124092 which disabled liveness2 for most methods.

mono/mini/liveness.c
@@ -861,7 +861,7 @@ mono_analyze_liveness2 (MonoCompile *cfg)
 	if (disabled)
 		return;
 
-	if (cfg->num_bblocks < (1 << (32 - BB_ID_SHIFT)))
+	if (cfg->num_bblocks >= (1 << (32 - BB_ID_SHIFT)))
 		/* Ranges would overflow */
 		return;
 