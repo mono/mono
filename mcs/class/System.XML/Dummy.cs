0ff0e4a3ab7c55d95ff64a58c6e84c890172aee2
Atsushi Eno <atsushieno@gmail.com>
2015-02-21 00:32:29 +0800

    Add referencesource-based message strings to the build.
    It should be using RESOURCE_STRINGS, but mcs doesn't support its special
    option for string resources for multiple files.
    
    The resource .cs files are generated by genresstringcs.sh, but System.Xml.txt
    contains some bogus characters that prevents source generation, so after
    the tool has generated the sources, I manually fixed them.
    It won't matter once mcs supports the option for multiple files anyways.

--- mcs/class/System.XML/Dummy.cs
+++ mcs/class/System.XML/Dummy.cs
@@ -2,6 +2,28 @@
 using System.Runtime.CompilerServices;
 using System.Xml.Schema;
 
+namespace System.Xml
+{
+	static partial class Res
+	{
+		public static string GetString (string s, params object [] args)
+		{
+			return args == null ? s : string.Format (s, args);
+		}
+	}
+}
+
+namespace System.Xml.Utils
+{
+	static partial class Res
+	{
+		public static string GetString (string s, params object [] args)
+		{
+			return args == null || args.Length == 0 ? s : string.Format (s, args);
+		}
+	}
+}
+
 namespace System
 {
 	static class AssemblyRef
