0b67c3c09c6d86f9f4701f499bfaa2bb3f89bff2
Mark Probst <mark.probst@gmail.com>
2015-02-26 15:40:38 -0800

    [sgen] Separate concurrent M&S object operations into start, concurrent, finish.

--- mono/metadata/sgen-workers.c
+++ mono/metadata/sgen-workers.c
@@ -236,12 +236,11 @@ marker_idle_func (void *data_untyped)
 	if (workers_state != STATE_WORKING)
 		return FALSE;
 
+	SGEN_ASSERT (0, sgen_concurrent_collection_in_progress (), "The worker should only mark in concurrent collections.");
 	SGEN_ASSERT (0, sgen_get_current_collection_generation () != GENERATION_NURSERY, "Why are we doing work while there's a nursery collection happening?");
 
 	if (!sgen_gray_object_queue_is_empty (&data->private_gray_queue) || workers_get_work (data)) {
-		SgenObjectOperations *ops = sgen_concurrent_collection_in_progress ()
-			? &major->major_concurrent_ops
-			: &major->major_ops;
+		SgenObjectOperations *ops = &major->major_ops_concurrent;
 		ScanCopyContext ctx = CONTEXT_FROM_OBJECT_OPERATIONS (ops, &data->private_gray_queue);
 
 		SGEN_ASSERT (0, !sgen_gray_object_queue_is_empty (&data->private_gray_queue), "How is our gray queue empty if we just got work?");
