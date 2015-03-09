mono/mono

commit 71e152accc727089faffa6b4fd6bf58cb62636cf

[bcl] Implement KnownTypes for DataContractJsonSerializer. Fixes #23058 and polymorphism support. Also discarded old __type hint behavior in JsonSerializationReader that potentially allowed any loaded type to be instantiated.

mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/JsonSerializationReader.cs
@@ -182,8 +182,18 @@ namespace System.Runtime.Serialization.Json
 					if (ct != null) {
 						return DeserializeGenericCollection (type, ct, instance);
 					} else {
-						TypeMap map = GetTypeMap (type);
-						return map.Deserialize (this, instance);
+						string typeHint = reader.GetAttribute ("__type");
+						if (typeHint != null) {
+							// this might be a derived & known type. We allow it when it's both.
+							Type exactType = GetRuntimeType (typeHint, type);
+							if (exactType == null)
+								throw SerializationError (String.Format ("Cannot load type '{0}'", typeHint));
+							 TypeMap map = GetTypeMap (exactType);
+							 return map.Deserialize (this, instance);
+						} else { // no type hint
+							TypeMap map = GetTypeMap (type);
+							 return map.Deserialize (this, instance);
+						}
 					}
 				}
 				else
@@ -198,24 +208,22 @@ namespace System.Runtime.Serialization.Json
 		}
 		
 
-		Type GetRuntimeType (string name)
+		Type GetRuntimeType (string name, Type baseType)
 		{
-			name = ToRuntimeTypeName (name);
+			string properName = ToRuntimeTypeName (name);
+
+			if (baseType != null && baseType.FullName.Equals (properName))
+				return baseType;
+
 			if (serializer.KnownTypes != null)
 				foreach (Type t in serializer.KnownTypes)
-					if (t.FullName == name)
+					if (t.FullName.Equals (properName)) 
 						return t;
-			var ret = root_type.Assembly.GetType (name, false) ?? Type.GetType (name, false);
-			if (ret != null)
-				return ret;
 
-			// We probably have to iterate all the existing
-			// assemblies that are loaded in current domain.
-			foreach (var ass in AppDomain.CurrentDomain.GetAssemblies ()) {
-				ret = ass.GetType (name, false);
-				if (ret != null)
-					return ret;
-			}
+			if (baseType != null)
+				foreach (var attr in baseType.GetCustomAttributes (typeof(KnownTypeAttribute)))
+					if ((attr as KnownTypeAttribute).Type.FullName.Equals (properName))
+						return (attr as KnownTypeAttribute).Type;
 
 			return null;
 		}
@@ -230,7 +238,7 @@ namespace System.Runtime.Serialization.Json
 			case "object":
 				string runtimeType = reader.GetAttribute ("__type");
 				if (runtimeType != null) {
-					Type t = GetRuntimeType (runtimeType);
+					Type t = GetRuntimeType (runtimeType, null);
 					if (t == null)
 						throw SerializationError (String.Format ("Cannot load type '{0}'", runtimeType));
 					return ReadObject (t);

mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/DataContractJsonSerializer.cs
@@ -84,7 +84,17 @@ namespace System.Runtime.Serialization.Json
 				throw new ArgumentOutOfRangeException ("maxItemsInObjectGraph");
 
 			this.type = type;
-			known_types = new ReadOnlyCollection<Type> (knownTypes != null ? knownTypes.ToArray () : Type.EmptyTypes);
+
+			var knownTypesFromAttributes = new List<Type> ();
+
+			foreach (var attr in type.GetCustomAttributes (typeof(KnownTypeAttribute)))
+				knownTypesFromAttributes.Add ((attr as KnownTypeAttribute).Type);
+
+			if (knownTypes != null)
+				knownTypesFromAttributes.AddRange (knownTypes);
+
+			known_types = new ReadOnlyCollection<Type> (knownTypesFromAttributes);
+
 			root = rootName;
 			max_items = maxItemsInObjectGraph;
 			ignore_extension = ignoreExtensionDataObject;
@@ -134,8 +144,6 @@ namespace System.Runtime.Serialization.Json
 		public bool IgnoreExtensionDataObject {
 			get { return ignore_extension; }
 		}
-
-		[MonoTODO]
 		public ReadOnlyCollection<Type> KnownTypes {
 			get { return known_types; }
 		}

commit 22d5581e87ca4dc82412528cda5a77c53355973a

[DataContractJsonSerializer] Allow abstract base classes in lists. Fixes #26998
Abstract classes can still have subclasses with KnownType attributes, which are allowed in deserialization.

This used to work as the '__type' hint  was always honored no matter the type, and broke with https://github.com/mono/mono/commit/71e152accc727089faffa6b4fd6bf58cb62636cf which removed that behavior.

mcs/class/System.ServiceModel.Web/System.Runtime.Serialization.Json/JsonSerializationReader.cs
@@ -353,8 +353,7 @@ namespace System.Runtime.Serialization.Json
 				for (reader.MoveToContent (); reader.NodeType != XmlNodeType.EndElement; reader.MoveToContent ()) {
 					if (!reader.IsStartElement ("item"))
 						throw SerializationError (String.Format ("Expected element 'item', but found '{0}' in namespace '{1}'", reader.LocalName, reader.NamespaceURI));
-					Type et = elementType == typeof (object) || elementType.IsAbstract ? null : elementType;
-					object elem = ReadObject (et ?? typeof (object));
+					object elem = ReadObject (elementType);
 					c.Add (elem);
 				}
 #if NET_2_1