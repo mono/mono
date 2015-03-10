mono/mono

commit 15c813d8fda7998688d17b2996543f2e1476ec88

[runtime] Make the image->gsharedvt_types into an array.

mono/mini/mini-runtime.c
@@ -1678,9 +1678,11 @@ get_gsharedvt_type (MonoType *t)
 		image = par->owner->image;
 
 		mono_image_lock (image);
-		if (!image->gsharedvt_types)
-			image->gsharedvt_types = g_hash_table_new (NULL, NULL);
-		res = g_hash_table_lookup (image->gsharedvt_types, par);
+		if (!image->gshared_types) {
+			image->gshared_types_len = 1;
+			image->gshared_types [0] = g_hash_table_new (NULL, NULL);
+		}
+		res = g_hash_table_lookup (image->gshared_types [0], par);
 		mono_image_unlock (image);
 		if (res)
 			return res;
@@ -1699,7 +1701,7 @@ get_gsharedvt_type (MonoType *t)
 	if (par->owner) {
 		mono_image_lock (image);
 		/* Duplicates are ok */
-		g_hash_table_insert (image->gsharedvt_types, par, res);
+		g_hash_table_insert (image->gshared_types [0], par, res);
 		mono_image_unlock (image);
 	}
 

mono/metadata/image.c
@@ -1652,7 +1652,9 @@ mono_image_close_except_pools (MonoImage *image)
 	free_hash (image->native_wrapper_aot_cache);
 	free_hash (image->pinvoke_scopes);
 	free_hash (image->pinvoke_scope_filenames);
-	free_hash (image->gsharedvt_types);
+	for (i = 0; i < image->gshared_types_len; ++i)
+		free_hash (image->gshared_types [i]);
+	g_free (image->gshared_types);
 
 	/* The ownership of signatures is not well defined */
 	g_hash_table_destroy (image->memberref_signatures);

mono/metadata/metadata-internals.h
@@ -339,7 +339,9 @@ struct _MonoImage {
 	GHashTable *pinvoke_scope_filenames;
 
 	/* Indexed by MonoGenericParam pointers */
-	GHashTable *gsharedvt_types;
+	GHashTable **gshared_types;
+	/* The length of the above array */
+	int gshared_types_len;
 
 	/*
 	 * No other runtime locks must be taken while holding this lock.

commit 8e4f5b95706cc1750d8e1ac2fde7ae6651adbdfa

Fix a crash introduced by 15c813d8fda7998688d17b2996543f2e1476ec88.

mono/mini/mini-runtime.c
@@ -1680,6 +1680,7 @@ get_gsharedvt_type (MonoType *t)
 		mono_image_lock (image);
 		if (!image->gshared_types) {
 			image->gshared_types_len = 1;
+			image->gshared_types = g_new0 (GHashTable, image->gshared_types_len);
 			image->gshared_types [0] = g_hash_table_new (NULL, NULL);
 		}
 		res = g_hash_table_lookup (image->gshared_types [0], par);