mono/mono

commit 9ae88709dbd3d3ed77e1a6ed3d3493ec14f991fc

[System] Fixed SafeSocketHandle concurrency problem. When ReleaseHandle was called by RegisterForBlockingSyscall's DangerousRelease the current thread would be in blocking_threads but there would be no blocking system call to interrupt.

mcs/class/System/System.Net.Sockets/SafeSocketHandle.cs
@@ -17,6 +17,8 @@ namespace System.Net.Sockets {
 
 		List<Thread> blocking_threads;
 
+		const int SOCKET_CLOSED = 10004;
+
 		const int ABORT_RETRIES = 10;
 		static bool THROW_ON_ABORT_RETRIES = Environment.GetEnvironmentVariable("MONO_TESTS_IN_PROGRESS") == "yes";
 
@@ -67,6 +69,15 @@ namespace System.Net.Sockets {
 						break;
 					}
 
+					/*
+					 * This method can be called by the DangerousRelease inside RegisterForBlockingSyscall
+					 * When this happens blocking_threads contains the current thread.
+					 * We can safely close the socket and throw SocketException in RegisterForBlockingSyscall
+					 * before the blocking system call.
+					 */
+					if (blocking_threads.Count == 1 && blocking_threads[0] == Thread.CurrentThread)
+						break;
+
 					AbortRegisteredThreads ();
 					// Sleep so other threads can resume
 					Thread.Sleep (1);
@@ -93,6 +104,10 @@ namespace System.Net.Sockets {
 				}
 				if (release)
 					DangerousRelease ();
+
+				// Handle can be closed by DangerousRelease
+				if (IsClosed)
+					throw new SocketException (SOCKET_CLOSED);
 			}
 		}
 

commit c1bca019818bd3db8d45418f1a2aede88beb75ff

[System] Fix SafeSocketHandle race introduced in 9ae88709dbd3d3ed77e1a6ed3d3493ec14f991fc.
  at System.ThrowHelper.ThrowArgumentOutOfRangeException () [0x00014] in /Users/marek/git/mono/external/referencesource/mscorlib/system/throwhelper.cs:56
  at System.Collections.Generic.List`1[System.Threading.Thread].get_Item (Int32 index) [0x0000e] in /Users/marek/git/mono/external/referencesource/mscorlib/system/collections/generic/list.cs:199
  at System.Net.Sockets.SafeSocketHandle.ReleaseHandle () [0x00052] in /Users/marek/git/mono/mcs/class/System/System.Net.Sockets/SafeSocketHandle.cs:79
  at System.Runtime.InteropServices.SafeHandle.RunRelease () [0x00091] in /Users/marek/git/mono/mcs/class/corlib/System.Runtime.InteropServices/SafeHandle.cs:203
  at System.Runtime.InteropServices.SafeHandle.Dispose (Boolean disposing) [0x00019] in /Users/marek/git/mono/mcs/class/corlib/System.Runtime.InteropServices/SafeHandle.cs:171
  at System.Runtime.InteropServices.SafeHandle.Dispose () [0x00003] in /Users/marek/git/mono/mcs/class/corlib/System.Runtime.InteropServices/SafeHandle.cs:150
  at System.Net.Sockets.Socket.Dispose (Boolean disposing) [0x0004d] in /Users/marek/git/mono/mcs/class/System/System.Net.Sockets/Socket_2_1.cs:1168
  at System.Net.Sockets.Socket.Dispose () [0x00003] in /Users/marek/git/mono/mcs/class/System/System.Net.Sockets/Socket_2_1.cs:1174
  at System.Net.Sockets.Socket.Close () [0x00009] in /Users/marek/git/mono/mcs/class/System/System.Net.Sockets/Socket_2_1.cs:1185
  at System.Net.Sockets.TcpListener.Stop () [0x0000d] in /Users/marek/git/mono/mcs/class/System/System.Net.Sockets/TcpListener.cs:296
  at System.Runtime.Remoting.Channels.Tcp.TcpServerChannel.StopListening (System.Object data) [0x00022] in /Users/marek/git/mono/mcs/class/System.Runtime.Remoting/System.Runtime.Remoting.Channels.Tcp/TcpServerChannel.cs:257

mcs/class/System/System.Net.Sockets/SafeSocketHandle.cs
@@ -75,8 +75,10 @@ namespace System.Net.Sockets {
 					 * We can safely close the socket and throw SocketException in RegisterForBlockingSyscall
 					 * before the blocking system call.
 					 */
-					if (blocking_threads.Count == 1 && blocking_threads[0] == Thread.CurrentThread)
-						break;
+					lock (blocking_threads) {
+						if (blocking_threads.Count == 1 && blocking_threads[0] == Thread.CurrentThread)
+							break;
+					}
 
 					AbortRegisteredThreads ();
 					// Sleep so other threads can resume