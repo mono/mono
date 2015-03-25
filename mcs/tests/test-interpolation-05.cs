de86cb955b3d693063858461a0b9115b123daf0c
Marek Safar <marek.safar@gmail.com>
2015-03-25 17:17:23 +0100

    [mcs] Resize identifier buffer in all cases. Fixes #28292

--- mcs/tests/test-interpolation-05.cs
+++ mcs/tests/test-interpolation-05.cs
@@ -8,6 +8,11 @@ public class ParserTest
 		Console.WriteLine(b ? $"{1:0.00}" : $"bar");
 	}
 
+	void Test2 ()
+	{
+		Console.WriteLine($"This should work but the compiler explodes if the string is too long!");		
+	}
+
 	public static void Main()
 	{
 	}
