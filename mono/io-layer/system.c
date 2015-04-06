ad4718238e7b5f1ae6bfe4884c93a75cce5548b4
Alexander Kyte <alexander.kyte@xamarin.com>
2015-04-06 15:31:18 -0400

    [runtime] Use _SC_NPROCESSORS_CONF not _SC_NPROCESSORS_ONLN
    We had a failing arm test which exposed that our caching of the
    number of processors was leading to some inconsistencies when the number
    of processors increased. TODO: Use the actual number of online processors when
    rebalancing threadpool.

--- mono/io-layer/system.c
+++ mono/io-layer/system.c
@@ -20,8 +20,8 @@ void GetSystemInfo(WapiSystemInfo *info)
 	info->dwPageSize=getpagesize();
 
 	/* Fill in the rest of this junk. Maybe with libgtop */
-#ifdef _SC_NPROCESSORS_ONLN
-	info->dwNumberOfProcessors = sysconf (_SC_NPROCESSORS_ONLN);
+#ifdef _SC_NPROCESSORS_CONF
+	info->dwNumberOfProcessors = sysconf (_SC_NPROCESSORS_CONF);
 	if (info->dwNumberOfProcessors <= 0)
 		info->dwNumberOfProcessors = 1;
 #else
