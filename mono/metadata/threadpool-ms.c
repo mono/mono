mono/mono

commit 4de22ecb2148f4faa3d2892c9835c0cf4b8024b6

[config] Move the complex.h check to autoconf instead of a PLATFORM_ANDROID check.

mono/metadata/threadpool-ms.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#ifdef PLATFORM_ANDROID
+#if !defined (HAVE_COMPLEX_H)
 #include <../../support/libm/complex.h>
 #else
 #include <complex.h>

configure.ac
@@ -432,7 +432,7 @@ AC_PROG_LD_GNU
 AM_ICONV()
 
 AC_CHECK_HEADERS(sys/filio.h sys/sockio.h netdb.h utime.h sys/utime.h semaphore.h sys/un.h linux/rtc.h sys/syscall.h sys/mkdev.h sys/uio.h sys/param.h sys/sysctl.h libproc.h)
-AC_CHECK_HEADERS(sys/param.h sys/socket.h sys/ipc.h sys/sem.h sys/utsname.h alloca.h ucontext.h pwd.h sys/select.h netinet/tcp.h netinet/in.h unistd.h sys/types.h link.h asm/sigcontext.h sys/inotify.h arpa/inet.h)
+AC_CHECK_HEADERS(sys/param.h sys/socket.h sys/ipc.h sys/sem.h sys/utsname.h alloca.h ucontext.h pwd.h sys/select.h netinet/tcp.h netinet/in.h unistd.h sys/types.h link.h asm/sigcontext.h sys/inotify.h arpa/inet.h complex.h)
 AC_CHECK_HEADERS([linux/netlink.h linux/rtnetlink.h],
                   [], [], [#include <stddef.h>
 		  #include <sys/socket.h>

commit a579b358296342bf233ee0f6b7d789ab21b88ee2

Fix build broken by 4de22ecb2148f4faa3d2892c9835c0cf4b8024b6
config.h needs to be included before HAVE_COMPLEX_H is used, or it won't work.

mono/metadata/threadpool-ms.c
@@ -21,15 +21,14 @@
 
 #include <stdlib.h>
 #include <math.h>
+#include <config.h>
+#include <glib.h>
 
 #if !defined (HAVE_COMPLEX_H)
 #include <../../support/libm/complex.h>
 #else
 #include <complex.h>
 #endif
-
-#include <config.h>
-#include <glib.h>
 
 #include <mono/metadata/class-internals.h>
 #include <mono/metadata/exception.h>