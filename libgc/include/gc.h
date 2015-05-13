d2986015a23569e02200c46a168f9dd11cdb9c82
Alex Rønne Petersen <alexrp@xamarin.com>
2015-05-13 11:05:38 +0200

    [runtime] Drop support for external libgc.
    This has not been supported for a while, so remove it.

--- libgc/include/gc.h
+++ libgc/include/gc.h
@@ -927,11 +927,7 @@ GC_API void (*GC_is_visible_print_proc)
 
 #if !defined(GC_USE_LD_WRAP) && \
     (defined(GC_PTHREADS) || defined(GC_SOLARIS_THREADS) || defined(GC_DARWIN_THREADS) || defined(GC_MACOSX_THREADS))
-#if defined(_IN_LIBGC) || defined(USE_INCLUDED_LIBGC)
 # include "gc_pthread_redirects.h"
-#else
-# include <gc/gc_pthread_redirects.h>
-#endif
 #endif
 
 # if defined(PCR) || defined(GC_SOLARIS_THREADS) || \
