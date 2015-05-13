27fa4cc8627de162731a002d8ed8d36c31a9287c
unixrt <brunolauze.bl@gmail.com>
2015-05-13 12:35:13 -0400

    dfadsfasdfads :q :wq

---
+++ mcs/class/System.Core/Microsoft.Win32/SafeLibraryHandle.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Win32
+{
+    using Microsoft.Win32.SafeHandles;
+    using System;
+    using System.Security;
+    using System.Security.Permissions;
+
+    //[SecurityCritical(SecurityCriticalScope.Everything), HostProtection(SecurityAction.LinkDemand, MayLeakOnAbort=true)]
+	[SecurityCritical, HostProtection(SecurityAction.LinkDemand, MayLeakOnAbort=true)]
+    internal sealed class SafeLibraryHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        internal SafeLibraryHandle() : base(true)
+        {
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            return Microsoft.Win32.UnsafeNativeMethods.FreeLibrary(base.handle);
+        }
+    }
+}
+
