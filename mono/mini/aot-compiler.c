mono/mono

commit b41ed5db82ad82f0540eef70d821318836fba107

[arm] Use gcc --shared for linking aot images.

mono/mini/aot-compiler.c
@@ -8821,6 +8821,8 @@ compile_asm (MonoAotCompile *acfg)
 #define LD_NAME "gcc -shared --dll"
 #elif defined(TARGET_X86) && defined(TARGET_MACH) && !defined(__native_client_codegen__)
 #define LD_NAME "clang -m32 -dynamiclib"
+#elif defined(TARGET_ARM)
+#define LD_NAME "gcc --shared"
 #endif
 
 	if (acfg->aot_opts.asm_only) {

commit 75175b2f261262b9369b1e1c505a7b5be193a331

[aot] Fixed linker invocation when cross-compiling to Android.
Fixes an Android AOT regression introduced by https://github.com/mono/mono/commit/b41ed5db82ad82f0540eef70d821318836fba107.

mono/mini/aot-compiler.c
@@ -8844,7 +8844,7 @@ compile_asm (MonoAotCompile *acfg)
 #define LD_NAME "gcc -shared --dll"
 #elif defined(TARGET_X86) && defined(TARGET_MACH) && !defined(__native_client_codegen__)
 #define LD_NAME "clang -m32 -dynamiclib"
-#elif defined(TARGET_ARM)
+#elif defined(TARGET_ARM) && !defined(TARGET_ANDROID)
 #define LD_NAME "gcc --shared"
 #endif
 