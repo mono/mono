cb90d1a7503726cd3e7201c69ee0642ca8ac5c36
Marcos Henrich <marcos.henrich@xamarin.com>
2015-05-18 18:18:40 +0100

    [corlib] Fixed TimeZoneInfo.GetDaylightChanges.
    Changed GetDaylightChanges to match .NET behaviour.
    
    GetDaylightChanges now only returns DaylightTime.Start and
    DaylightTime.End in the given year.
    
    DaylightTime.Start and DaylightTime.End may not belong to the same DST
    period.

--- mcs/class/corlib/System/TimeZoneInfo.cs
+++ mcs/class/corlib/System/TimeZoneInfo.cs
@@ -892,24 +892,17 @@ namespace System
 					DateTime ttime = pair.Key;
 					TimeType ttype = pair.Value;
 
+					if (ttime.Year > year)
+						continue;
+					if (ttime.Year < year)
+						break;
+
 					if (ttype.IsDst) {
 						// DaylightTime.Delta is relative to the current BaseUtcOffset.
-						var d =  new TimeSpan (0, 0, ttype.Offset) - BaseUtcOffset;
-						// Handle DST gradients
-						if (start != DateTime.MinValue && delta != d)
-							end = start;
-
+						delta =  new TimeSpan (0, 0, ttype.Offset) - BaseUtcOffset;
 						start = ttime;
-						delta = d;
-
-						if (ttime.Year <= year)
-							break;
 					} else {
-						if (ttime.Year < year)
-							break;
-
 						end = ttime;
-						start = DateTime.MinValue;
 					}
 				}
 
@@ -918,22 +911,26 @@ namespace System
 					start += BaseUtcOffset;
 
 				// DaylightTime.End is relative to the DST time.
-				if (end != DateTime.MaxValue)
+				if (end != DateTime.MinValue)
 					end += BaseUtcOffset + delta;
 			} else {
-				AdjustmentRule rule = null;
-				foreach (var r in GetAdjustmentRules ()) {
-					if (r.DateEnd.Year < year)
+				AdjustmentRule first = null, last = null;
+
+				foreach (var rule in GetAdjustmentRules ()) {
+					if (rule.DateStart.Year != year && rule.DateEnd.Year != year)
 						continue;
-					if (r.DateStart.Year > year)
-						break;
-					rule = r;
+					if (rule.DateStart.Year == year)
+						first = rule;
+					if (rule.DateEnd.Year == year)
+						last = rule;
 				}
-				if (rule != null) {
-					start = TransitionPoint (rule.DaylightTransitionStart, year);
-					end = TransitionPoint (rule.DaylightTransitionEnd, year);
-					delta = rule.DaylightDelta;
-				}
+
+				if (first == null || last == null)
+					return new DaylightTime (new DateTime (), new DateTime (), new TimeSpan ());
+
+				start = TransitionPoint (first.DaylightTransitionStart, year);
+				end = TransitionPoint (last.DaylightTransitionEnd, year);
+				delta = first.DaylightDelta;
 			}
 
 			if (start == DateTime.MinValue || end == DateTime.MinValue)
