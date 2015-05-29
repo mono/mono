f0bea6966af0d1f18b8467a06b3f8ee01574fd81
Marek Safar <marek.safar@gmail.com>
2015-05-29 08:36:52 +0200

    [corlib] Add timeout to NestedLeakingSynchronizationContext

--- mcs/class/corlib/Test/System.Runtime.CompilerServices/TaskAwaiterTest.cs
+++ mcs/class/corlib/Test/System.Runtime.CompilerServices/TaskAwaiterTest.cs
@@ -368,7 +368,7 @@ namespace MonoTests.System.Runtime.CompilerServices
 		public void NestedLeakingSynchronizationContext ()
 		{
 			var sc = SynchronizationContext.Current;
-			NestedLeakingSynchronizationContext_MainAsync (sc).GetAwaiter ().GetResult ();
+			Assert.IsTrue (NestedLeakingSynchronizationContext_MainAsync (sc).GetAwaiter ().Wait (5000), "#1");
 		}
 
 		static async Task NestedLeakingSynchronizationContext_MainAsync (SynchronizationContext sc)
