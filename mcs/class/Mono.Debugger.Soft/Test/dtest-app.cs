158783843eca12f4de1d5e4905317450435f4bf0
Zoltan Varga <vargaz@gmail.com>
2015-06-03 13:52:46 -0400

    [sdb] Fix interface/virtual calls to valuetype methods.

--- mcs/class/Mono.Debugger.Soft/Test/dtest-app.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest-app.cs
@@ -80,7 +80,7 @@ public class Tests2 {
 	}
 }
 
-public struct AStruct {
+public struct AStruct : ITest2 {
 	public int i;
 	public string s;
 	public byte k;
@@ -115,6 +115,14 @@ public struct AStruct {
 	[MethodImplAttribute (MethodImplOptions.NoInlining)]
 	public void invoke_mutate () {
 		l = 5;
+	}
+
+	public int invoke_iface () {
+		return i;
+	}
+
+	public override string ToString () {
+		return i.ToString ();
 	}
 }
 
