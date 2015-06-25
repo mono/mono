mono/mono

commit 3c47caca34e3c96aa43dc1009e285b7a79ee366c

[system] Don't dispose exposed redirected streams. Fixes #30868

mcs/class/System/System.Diagnostics/Process.cs
@@ -1603,20 +1603,9 @@ namespace System.Diagnostics {
 						if (async_error != null)
 							async_error.Close ();
 
-						if (input_stream != null) {
-							input_stream.Close();
-							input_stream = null;
-						}
-
-						if (output_stream != null) {
-							output_stream.Close();
-							output_stream = null;
-						}
-
-						if (error_stream != null) {
-							error_stream.Close();
-							error_stream = null;
-						}
+						input_stream = null;
+						output_stream = null;
+						error_stream = null;
 					}
 				}
 				

mcs/class/System/Test/System.Diagnostics/ProcessTest.cs
@@ -909,5 +909,19 @@ namespace MonoTests.System.Diagnostics
 		public void HasExitedCurrent () {
 			Assert.IsFalse (Process.GetCurrentProcess ().HasExited);
 		}
+
+		[Test]
+		public void DisposeWithDisposedStreams ()
+		{
+			var psi = GetCrossPlatformStartInfo ();
+			psi.RedirectStandardInput = true;
+			psi.RedirectStandardOutput = true;
+			psi.UseShellExecute = false;
+
+			var p = Process.Start (psi);
+			p.StandardInput.BaseStream.Dispose ();
+			p.StandardOutput.BaseStream.Dispose ();
+			p.Dispose ();
+		}
 	}
 }

commit 2a9e0ea28f506a8a832ea317ec169d56f61b436f

Fix the leaking of process streams when using RedirectStandardOutput etc., introduced by 3c47caca34e3c96aa43dc1009e285b7a79ee366c.

mcs/class/System/System.Diagnostics/Process.cs
@@ -616,7 +616,8 @@ namespace System.Diagnostics {
 		}
 
 		private StreamReader error_stream=null;
-		
+		bool error_stream_exposed;
+
 		[DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden), Browsable (false)]
 		[MonitoringDescription ("The standard error stream of this process.")]
 		public StreamReader StandardError {
@@ -629,11 +630,13 @@ namespace System.Diagnostics {
 
 				async_mode |= AsyncModes.SyncError;
 
+				error_stream_exposed = true;
 				return(error_stream);
 			}
 		}
 
 		private StreamWriter input_stream=null;
+		bool input_stream_exposed;
 		
 		[DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden), Browsable (false)]
 		[MonitoringDescription ("The standard input stream of this process.")]
@@ -642,11 +645,13 @@ namespace System.Diagnostics {
 				if (input_stream == null)
 					throw new InvalidOperationException("Standard input has not been redirected");
 
+				input_stream_exposed = true;
 				return(input_stream);
 			}
 		}
 
 		private StreamReader output_stream=null;
+		bool output_stream_exposed;
 		
 		[DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden), Browsable (false)]
 		[MonitoringDescription ("The standard output stream of this process.")]
@@ -660,6 +665,7 @@ namespace System.Diagnostics {
 
 				async_mode |= AsyncModes.SyncOutput;
 
+				output_stream_exposed = true;
 				return(output_stream);
 			}
 		}
@@ -1603,9 +1609,21 @@ namespace System.Diagnostics {
 						if (async_error != null)
 							async_error.Close ();
 
-						input_stream = null;
-						output_stream = null;
-						error_stream = null;
+						if (input_stream != null) {
+							if (!input_stream_exposed)
+								input_stream.Close ();
+							input_stream = null;
+						}
+						if (output_stream != null) {
+							if (!output_stream_exposed)
+								output_stream.Close ();
+							output_stream = null;
+						}
+						if (error_stream != null) {
+							if (!error_stream_exposed)
+								error_stream.Close ();
+							error_stream = null;
+						}
 					}
 				}
 				