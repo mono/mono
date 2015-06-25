c6f0e4b6804b2d0d9ff5c29b3f27526c9c1b9d77
Miguel de Icaza <miguel@gnome.org>
2015-06-25 13:56:54 -0400

    Attempts to locate the C# and VB compilers based on the location of mscorlib, does not assume they are on the path.   Fixes #31369

--- mcs/class/System/Microsoft.CSharp/CSharpCodeCompiler.cs
+++ mcs/class/System/Microsoft.CSharp/CSharpCodeCompiler.cs
@@ -50,6 +50,7 @@ namespace Mono.CSharp
 	{
 		static string windowsMcsPath;
 		static string windowsMonoPath;
+		static string unixMcsCommand;
 
 		Mutex mcsOutMutex;
 		StringCollection mcsOutput;
@@ -85,6 +86,13 @@ namespace Mono.CSharp
 				
 				if (!File.Exists (windowsMcsPath))
 					throw new FileNotFoundException ("Windows mcs path not found: " + windowsMcsPath);
+			} else {
+				var mscorlibPath = new Uri (typeof (object).Assembly.CodeBase).LocalPath;
+				var unixMcsPath = Path.GetFullPath (Path.Combine (mscorlibPath, "..", "..", "..", "..", "bin", "mcs"));
+				if (File.Exists (unixMcsPath))
+					unixMcsCommand = unixMcsPath;
+				else
+					unixMcsCommand = "mcs";
 			}
 		}
 
@@ -173,7 +181,7 @@ namespace Mono.CSharp
 				mcs.StartInfo.Arguments = "\"" + windowsMcsPath + "\" " +
 					BuildArgs (options, fileNames, ProviderOptions);
 			} else {
-				mcs.StartInfo.FileName="mcs";
+				mcs.StartInfo.FileName=unixMcsCommand;
 				mcs.StartInfo.Arguments=BuildArgs(options, fileNames, ProviderOptions);
 			}
 
