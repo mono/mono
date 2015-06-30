a5badc4588255f776c45df6f39b139c41c85b226
Marek Safar <marek.safar@gmail.com>
2015-06-30 11:49:53 +0200

    [mcs] Fixes name collision check for generic types within user namespaces. Fixes #30904

---
+++ mcs/errors/CS0101-9-2.cs
@@ -0,0 +1,4 @@
+namespace N
+{
+	class Test<T> {}
+}
---
+++ mcs/errors/cs0101-9.cs
@@ -0,0 +1,8 @@
+// CS0101: The namespace `N' already contains a definition for `Test<T>'
+// Line: 7
+// Compiler options: CS0101-9-2.cs
+
+namespace N
+{
+	class Test<T> {}
+}
