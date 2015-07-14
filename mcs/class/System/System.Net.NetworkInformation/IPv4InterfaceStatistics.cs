4ad996b1e1301f9c20b3518e073a4f5c15b29fe2
Marek Safar <marek.safar@gmail.com>
2015-07-14 20:07:40 +0200

    [system] Add missing APIs

--- mcs/class/System/System.Net.NetworkInformation/IPv4InterfaceStatistics.cs
+++ mcs/class/System/System.Net.NetworkInformation/IPv4InterfaceStatistics.cs
@@ -28,27 +28,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 namespace System.Net.NetworkInformation {
-	public abstract class IPv4InterfaceStatistics {
-		protected IPv4InterfaceStatistics ()
-		{
-		}
-		
-		public abstract long BytesReceived { get; }
-		public abstract long BytesSent { get; }
-		public abstract long IncomingPacketsDiscarded { get; }
-		public abstract long IncomingPacketsWithErrors { get; }
-		public abstract long IncomingUnknownProtocolPackets { get; }
-		public abstract long NonUnicastPacketsReceived { get; }
-		public abstract long NonUnicastPacketsSent { get; }
-		public abstract long OutgoingPacketsDiscarded { get; }
-		public abstract long OutgoingPacketsWithErrors { get; }
-
-		[MonoTODO("Not implemented for Linux")]
-		public abstract long OutputQueueLength { get; }
-		public abstract long UnicastPacketsReceived { get; }
-		public abstract long UnicastPacketsSent { get; }
-	}
-
 	class Win32IPv4InterfaceStatistics : IPv4InterfaceStatistics
 	{
 		Win32_MIB_IFROW info;
