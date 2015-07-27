833137618276d3c5f190df2736485da09aa846d0
Marek Safar <marek.safar@gmail.com>
2015-07-27 14:10:13 +0200

    [mcs] Reset doc reader state after expression bodied property. Fixes #32456

---
+++ mcs/tests/test-xml-072.cs
@@ -0,0 +1,17 @@
+// Compiler options: -doc:xml-072.xml
+
+class X
+{
+	/// <summary>
+	/// Top-class
+	/// </summary>
+	public sealed class C
+	{
+		public string Severity => "foo";
+
+		/// <summary>
+		/// foo
+		/// </summary>
+		public static void Main() {}
+	}
+}
