72b39fdc680f64682153fb95264289d529ae164f
Rodrigo Kumpera <kumpera@gmail.com>
2015-08-08 10:17:58 -0400

    [coop] Switch from thread_state_init_from_sigctx to thread_state_init.
    Suspend needs to work in the intermediate state where the mono-thread
    is attached but not the managed thread. This happens in the startup
    and cleanup paths and leads to crashes there.
    
    The suspend initiator is now responsible to check if the thread is
    attached or not. Which is easy, just check lmf/jit_tls.

--- mono/utils/mono-threads-coop.c
+++ mono/utils/mono-threads-coop.c
@@ -35,7 +35,7 @@ mono_threads_state_poll (void)
 	if (!(info->thread_state & (STATE_ASYNC_SUSPEND_REQUESTED | STATE_SELF_SUSPEND_REQUESTED)))
 		return;
 
-	g_assert (mono_threads_get_runtime_callbacks ()->thread_state_init_from_sigctx (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX], NULL));
+	mono_threads_get_runtime_callbacks ()->thread_state_init (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX]);
 
 	/* commit the saved state and notify others if needed */
 	switch (mono_threads_transition_state_poll (info)) {
@@ -64,11 +64,7 @@ mono_threads_prepare_blocking (void)
 	}
 
 retry:
-	/*The JIT might not be able to save*/
-	if (!mono_threads_get_runtime_callbacks ()->thread_state_init_from_sigctx (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX], NULL)) {
-		THREADS_SUSPEND_DEBUG ("PREPARE-BLOCKING failed %p to save thread state\n", mono_thread_info_get_tid (info));
-		return NULL;
-	}
+	mono_threads_get_runtime_callbacks ()->thread_state_init (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX]);
 
 	switch (mono_threads_transition_do_blocking (info)) {
 	case DoBlockingContinue:
@@ -161,15 +157,12 @@ mono_threads_try_prepare_blocking (void)
 	/* If the thread is not attached, it doesn't make sense prepare for suspend. */
 	if (!info || !mono_thread_info_is_live (info) || mono_thread_info_current_state (info) == STATE_BLOCKING) {
 		THREADS_SUSPEND_DEBUG ("PREPARE-TRY-BLOCKING failed %p\n", mono_thread_info_get_tid (info));
+		g_error ("FUUU");
 		return NULL;
 	}
 
 retry:
-	/*The JIT might not be able to save*/
-	if (!mono_threads_get_runtime_callbacks ()->thread_state_init_from_sigctx (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX], NULL)) {
-		THREADS_SUSPEND_DEBUG ("PREPARE-TRY-BLOCKING failed %p to save thread state\n", mono_thread_info_get_tid (info));
-		return NULL;
-	}
+	mono_threads_get_runtime_callbacks ()->thread_state_init (&info->thread_saved_state [SELF_SUSPEND_STATE_INDEX]);
 
 	switch (mono_threads_transition_do_blocking (info)) {
 	case DoBlockingContinue:
