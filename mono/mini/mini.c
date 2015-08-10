mono/mono

commit edb4d8f3faef5f5d0ad3fc639d58b4d490c82c94

[coop] Don't emit safepoints in the wrapper of the polling function.
Otherwise during a collection the wrapper would call itself resulting
in a stack overflow.

mono/mini/mini.c
@@ -3098,6 +3098,23 @@ static void
 mono_insert_safepoints (MonoCompile *cfg)
 {
 	MonoBasicBlock *bb;
+	if (cfg->method->wrapper_type == MONO_WRAPPER_MANAGED_TO_NATIVE) {
+		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
+#if defined(__native_client__) || defined(__native_client_codegen__)
+		gpointer poll_func = &mono_nacl_gc;
+#elif defined(USE_COOP_GC)
+		gpointer poll_func = &mono_threads_state_poll;
+#else
+		gpointer poll_func = NULL;
+#endif
+
+		if (info->subtype == WRAPPER_SUBTYPE_ICALL_WRAPPER && info->d.icall.func == poll_func) {
+			if (cfg->verbose_level > 1)
+				printf ("SKIPPING SAFEPOINTS for the polling function icall\n");
+			return;
+		}
+	}
+
 
 	if (cfg->verbose_level > 1)
 		printf ("INSERTING SAFEPOINTS\n");

commit 80a69d2d61cece1d3514f006679543265cd2b128

[coop] Fix a crash introduced by edb4d8f3faef5f5d0ad3fc639d58b4d490c82c94, some wrappers don't have wrapper infos.

mono/mini/mini.c
@@ -3108,7 +3108,7 @@ mono_insert_safepoints (MonoCompile *cfg)
 		gpointer poll_func = NULL;
 #endif
 
-		if (info->subtype == WRAPPER_SUBTYPE_ICALL_WRAPPER && info->d.icall.func == poll_func) {
+		if (info && info->subtype == WRAPPER_SUBTYPE_ICALL_WRAPPER && info->d.icall.func == poll_func) {
 			if (cfg->verbose_level > 1)
 				printf ("SKIPPING SAFEPOINTS for the polling function icall\n");
 			return;