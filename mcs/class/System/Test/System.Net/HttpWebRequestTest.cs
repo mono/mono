4fa77a26b662f04624e3129404c9cd75143ba4a8
BogdanovKirill <bogdanov@macroscop.com>
2015-08-18 14:32:25 +0500

    [Processes] Memory leak fix in EnumProcessModules function
    The main point is that get_process_name_from_procs's result string is
    never released.
    
    This change is released under the MIT license.

--- mcs/class/System/Test/System.Net/HttpWebRequestTest.cs
+++ mcs/class/System/Test/System.Net/HttpWebRequestTest.cs
@@ -25,7 +25,6 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
-using System.Reflection;
 using Mono.Security.Authenticode;
 #if !MOBILE
 using Mono.Security.Protocol.Tls;
@@ -2307,7 +2306,7 @@ namespace MonoTests.System.Net
 		[Test]
 		public void TestLargeDataReading ()
 		{
-			int near2GBStartPosition = rand.Next (int.MaxValue - 500, int.MaxValue);
+			const int internalBufferSize = 16 * 1024 * 1024;
 			AutoResetEvent readyGetLastPortionEvent = new AutoResetEvent (false);
 			AssertionException testException = null;
 
@@ -2321,22 +2320,28 @@ namespace MonoTests.System.Net
 					request.Timeout = timeoutMs;
 					request.ReadWriteTimeout = timeoutMs;
 
+					if (Type.GetType ("Mono.Runtime") == null)
+						//significantly increases speed of test on MS .NET, because default value	
+						//of receive buffer is about 8192 bytes. doesn't implemented now on Mono.
+						request.ServicePoint.ReceiveBufferSize = internalBufferSize;
+
 					WebResponse webResponse = request.GetResponse ();
 					Stream webResponseStream = webResponse.GetResponseStream ();
 					Assert.IsNotNull (webResponseStream, null, "#1");
-					
-					Type webConnectionStreamType = webResponseStream.GetType ();
-					FieldInfo totalReadField = webConnectionStreamType.GetField ("totalRead", BindingFlags.NonPublic | BindingFlags.Instance);
-					Assert.IsNotNull (totalReadField, "#2");
-					totalReadField.SetValue (webResponseStream, near2GBStartPosition);
-					
-					byte[] readBuffer = new byte[int.MaxValue - near2GBStartPosition];
-					Assert.AreEqual (webResponseStream.Read (readBuffer, 0, readBuffer.Length), readBuffer.Length, "#3");
+
+					int totalRead = 0;
+					byte[] readBuffer = new byte[internalBufferSize];
+
+					while (totalRead < int.MaxValue) {
+						int read = webResponseStream.Read (readBuffer, 0, readBuffer.Length);
+						Assert.Greater (read, 0, "#2");
+						totalRead += read;
+						Assert.Greater (totalRead, 0, "#3");
+					}
+
+					Assert.AreEqual (totalRead, int.MaxValue, "#4");
 					readyGetLastPortionEvent.Set ();
-					Assert.Greater (webResponseStream.Read (readBuffer, 0, readBuffer.Length), 0, "#4");
-					readyGetLastPortionEvent.Set ();
-					
-					webResponse.Close();
+					Assert.Greater (webResponseStream.Read (readBuffer, 0, readBuffer.Length), 0, "#5");
 				}
 				catch (AssertionException e)
 				{
@@ -2350,19 +2355,31 @@ namespace MonoTests.System.Net
 			processor =>
 			{
 				processor.Request.InputStream.Close ();
-				
+
 				HttpListenerResponse response = processor.Response;
 				response.SendChunked = true;
-				
+
 				Stream outputStream = response.OutputStream;
-				var writeBuffer = new byte[int.MaxValue - near2GBStartPosition];
-				outputStream.Write (writeBuffer, 0, writeBuffer.Length);
+
+				long totalWritten = 0;
+				byte[] writeBuffer = new byte[internalBufferSize];
+
+				while (totalWritten < int.MaxValue) {
+					int size;
+
+					if (totalWritten + writeBuffer.Length < int.MaxValue)
+						size = writeBuffer.Length;
+					else
+						size = (int) (int.MaxValue - totalWritten);
+
+					outputStream.Write (writeBuffer, 0, size);
+					totalWritten += size;
+				}
+
 				readyGetLastPortionEvent.WaitOne ();
 				outputStream.Write (writeBuffer, 0, writeBuffer.Length);
-				readyGetLastPortionEvent.WaitOne ();
-				
-				response.Close();
-			});
+				response.Close ();
+			}, 60 * 1000);
 
 			if (testException != null)
 				throw testException;
@@ -2382,7 +2399,7 @@ namespace MonoTests.System.Net
 				Assert.Fail ("Test hung");
 		}
 
-		void DoRequest (Action<HttpWebRequest, EventWaitHandle> request, Action<HttpListenerContext> processor)
+		void DoRequest (Action<HttpWebRequest, EventWaitHandle> request, Action<HttpListenerContext> processor, int timeoutMs = 10000)
 		{
 			int port = NetworkHelpers.FindFreePort ();
 
@@ -2397,7 +2414,7 @@ namespace MonoTests.System.Net
 
 				ThreadPool.QueueUserWorkItem ((o) => request (client, completed [1]));
 
-				if (!WaitHandle.WaitAll (completed, 10000))
+				if (!WaitHandle.WaitAll (completed, timeoutMs))
 					Assert.Fail ("Test hung.");
 			}
 		}
