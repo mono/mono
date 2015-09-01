625c305ed15fd8b8501f4a8ab0e7bac029635bfb
Timotheus Pokorra <timotheus.pokorra@gmail.com>
2015-09-01 09:56:28 +0200

    fixing typo appropiate in WebHeaderCollection.cs
    as mentioned by FastCode in IRC

--- mcs/class/System/System.Net/WebHeaderCollection.cs
+++ mcs/class/System/System.Net/WebHeaderCollection.cs
@@ -677,7 +677,7 @@ namespace System.Net
 				return;
 
 			if ((info & headerRestriction.Value) != 0)
-				throw new ArgumentException ("This header must be modified with the appropiate property.");
+				throw new ArgumentException ("This header must be modified with the appropriate property.");
 		}
 
 		void CheckHeaderConsistency (HeaderInfo value)
