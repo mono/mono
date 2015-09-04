a4a1679333dc939e9e9b9c362c07e6d5a0c75e0a
triton <joao@tritao.eu>
2015-09-04 15:17:17 +0100

    [System.IO.Compression] Fixed handling of empty Zip archives in Update mode. Fixes bug #32725.
    https://bugzilla.xamarin.com/show_bug.cgi?id=32725

--- mcs/class/System.IO.Compression/Test/System.IO.Compression/ZipTest.cs
+++ mcs/class/System.IO.Compression/Test/System.IO.Compression/ZipTest.cs
@@ -251,5 +251,16 @@ namespace MonoTests.System.IO.Compression
 
 			File.Delete ("test.zip");
 		}
+
+		[Test]
+		public void ZipUpdateEmptyArchive()
+		{
+			File.WriteAllText("empty.zip", string.Empty);
+			using (var archive = new ZipArchive(File.Open("empty.zip", FileMode.Open),
+				ZipArchiveMode.Update))
+			{
+			}
+			File.Delete ("empty.zip");
+		}
 	}
 }
