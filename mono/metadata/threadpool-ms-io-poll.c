90c59feb751679265ea972b04371853037c28d56
Ludovic Henry <ludovic@xamarin.com>
2015-10-15 20:55:07 +0100

    [threadpool-io] Move IOSelector to manage

--- mono/metadata/threadpool-ms-io-poll.c
+++ mono/metadata/threadpool-ms-io-poll.c
@@ -1,9 +1,11 @@
 
 #include "utils/mono-poll.h"
 
-static mono_pollfd *poll_fds;
-static guint poll_fds_capacity;
-static guint poll_fds_size;
+typedef struct {
+	mono_pollfd *fds;
+	gint fds_capacity;
+	gint fds_size;
+} ThreadPoolIOBackendPoll;
 
 static inline void
 POLL_INIT_FD (mono_pollfd *poll_fd, gint fd, gint events)
@@ -13,100 +15,135 @@ POLL_INIT_FD (mono_pollfd *poll_fd, gint fd, gint events)
 	poll_fd->revents = 0;
 }
 
-static gboolean
-poll_init (gint wakeup_pipe_fd)
+static gpointer
+poll_init (gpointer wakeup_pipe_handle)
 {
+	ThreadPoolIOBackendPoll *backend_poll;
+	gint wakeup_pipe_fd;
+
+	backend_poll = g_new0 (ThreadPoolIOBackendPoll, 1);
+	g_assert (backend_poll);
+
+	mono_trace (G_LOG_LEVEL_INFO, MONO_TRACE_IO_THREADPOOL,
+		"[%p][poll] init", backend_poll);
+
+	backend_poll->fds_size = 1;
+	backend_poll->fds_capacity = 64;
+
+	backend_poll->fds = g_new0 (mono_pollfd, backend_poll->fds_capacity);
+	g_assert (backend_poll->fds);
+
+	wakeup_pipe_fd = GPOINTER_TO_INT (wakeup_pipe_handle);
 	g_assert (wakeup_pipe_fd >= 0);
 
-	poll_fds_size = 1;
-	poll_fds_capacity = 64;
+	POLL_INIT_FD (&backend_poll->fds [0], wakeup_pipe_fd, MONO_POLLIN);
 
-	poll_fds = g_new0 (mono_pollfd, poll_fds_capacity);
-
-	POLL_INIT_FD (&poll_fds [0], wakeup_pipe_fd, MONO_POLLIN);
-
-	return TRUE;
+	return backend_poll;
 }
 
 static void
-poll_cleanup (void)
+poll_cleanup (gpointer backend)
 {
-	g_free (poll_fds);
+	ThreadPoolIOBackendPoll *backend_poll;
+
+	backend_poll = (ThreadPoolIOBackendPoll*) backend;
+	g_assert (backend_poll);
+
+	g_free (backend_poll->fds);
+	g_free (backend_poll);
 }
 
 static void
-poll_register_fd (gint fd, gint events, gboolean is_new)
+poll_add_handle (gpointer backend, gpointer handle, gint16 operations, gboolean is_new)
 {
+	ThreadPoolIOBackendPoll *backend_poll;
+	gint fd;
 	gint i;
 	gint poll_event;
 
-	g_assert (fd >= 0);
-	g_assert (poll_fds_size <= poll_fds_capacity);
+	backend_poll = (ThreadPoolIOBackendPoll*) backend;
+	g_assert (backend_poll);
 
-	g_assert ((events & ~(EVENT_IN | EVENT_OUT)) == 0);
+	fd = GPOINTER_TO_INT (handle);
+	g_assert (fd >= 0);
+
+	g_assert ((operations & ~(OPERATION_READ | OPERATION_WRITE)) == 0);
+
+	g_assert (backend_poll->fds_size <= backend_poll->fds_capacity);
 
 	poll_event = 0;
-	if (events & EVENT_IN)
+	if (operations & OPERATION_READ)
 		poll_event |= MONO_POLLIN;
-	if (events & EVENT_OUT)
+	if (operations & OPERATION_WRITE)
 		poll_event |= MONO_POLLOUT;
 
-	for (i = 0; i < poll_fds_size; ++i) {
-		if (poll_fds [i].fd == fd) {
-			g_assert (!is_new);
-			POLL_INIT_FD (&poll_fds [i], fd, poll_event);
+	for (i = 0; i < backend_poll->fds_size; ++i) {
+		if (backend_poll->fds [i].fd == fd) {
+			if (is_new)
+				g_error ("[%p][poll] add: handle %d already present", backend_poll, fd);
+			POLL_INIT_FD (&backend_poll->fds [i], fd, poll_event);
 			return;
 		}
 	}
 
-	g_assert (is_new);
+	if (!is_new)
+		g_error ("[%p][poll] add: could not find existing handle %d", backend_poll, fd);
 
-	for (i = 0; i < poll_fds_size; ++i) {
-		if (poll_fds [i].fd == -1) {
-			POLL_INIT_FD (&poll_fds [i], fd, poll_event);
+	for (i = 0; i < backend_poll->fds_size; ++i) {
+		if (backend_poll->fds [i].fd == -1) {
+			POLL_INIT_FD (&backend_poll->fds [i], fd, poll_event);
 			return;
 		}
 	}
 
-	poll_fds_size += 1;
+	backend_poll->fds_size += 1;
 
-	if (poll_fds_size > poll_fds_capacity) {
-		poll_fds_capacity *= 2;
-		g_assert (poll_fds_size <= poll_fds_capacity);
+	if (backend_poll->fds_size > backend_poll->fds_capacity) {
+		backend_poll->fds_capacity *= 2;
+		g_assert (backend_poll->fds_size <= backend_poll->fds_capacity);
 
-		poll_fds = g_renew (mono_pollfd, poll_fds, poll_fds_capacity);
+		backend_poll->fds = g_renew (mono_pollfd, backend_poll->fds, backend_poll->fds_capacity);
 	}
 
-	POLL_INIT_FD (&poll_fds [poll_fds_size - 1], fd, poll_event);
+	POLL_INIT_FD (&backend_poll->fds [backend_poll->fds_size - 1], fd, poll_event);
 }
 
 static void
-poll_remove_fd (gint fd)
+poll_remove_handle (gpointer backend, gpointer handle)
 {
+	ThreadPoolIOBackendPoll *backend_poll;
+	gint fd;
 	gint i;
 
+	backend_poll = (ThreadPoolIOBackendPoll*) backend;
+	g_assert (backend_poll);
+
+	fd = GPOINTER_TO_INT (handle);
 	g_assert (fd >= 0);
 
-	for (i = 0; i < poll_fds_size; ++i) {
-		if (poll_fds [i].fd == fd) {
-			POLL_INIT_FD (&poll_fds [i], -1, 0);
+	for (i = 0; i < backend_poll->fds_size; ++i) {
+		if (backend_poll->fds [i].fd == fd) {
+			POLL_INIT_FD (&backend_poll->fds [i], -1, 0);
 			break;
 		}
 	}
 
 	/* if we don't find the fd in poll_fds,
 	 * it means we try to delete it twice */
-	g_assert (i < poll_fds_size);
+	if (i == backend_poll->fds_size)
+		g_error ("[%p][poll] remove: trying to remove handle %d twice", backend_poll, fd);
 
 	/* if we find it again, it means we added
 	 * it twice */
-	for (; i < poll_fds_size; ++i)
-		g_assert (poll_fds [i].fd != fd);
+	for (; i < backend_poll->fds_size; ++i) {
+		if (backend_poll->fds [i].fd == fd)
+			g_error ("[%p][poll] remove: handle %d is present twice", backend_poll, fd);
+	}
 
 	/* reduce the value of poll_fds_size so we
 	 * do not keep it too big */
-	while (poll_fds_size > 1 && poll_fds [poll_fds_size - 1].fd == -1)
-		poll_fds_size -= 1;
+	while (backend_poll->fds_size > 1 && backend_poll->fds [backend_poll->fds_size - 1].fd == -1)
+		backend_poll->fds_size -= 1;
 }
 
 static inline gint
@@ -139,19 +176,22 @@ poll_mark_bad_fds (mono_pollfd *poll_fds, gint poll_fds_size)
 	return ready;
 }
 
-static gint
-poll_event_wait (void (*callback) (gint fd, gint events, gpointer user_data), gpointer user_data)
+static void
+poll_poll (gpointer backend, ThreadPoolIOBackendEvent *events, gint nevents)
 {
-	gint i, ready;
+	ThreadPoolIOBackendPoll *backend_poll;
+	gint i, j, ready;
 
-	for (i = 0; i < poll_fds_size; ++i)
-		poll_fds [i].revents = 0;
+	backend_poll = (ThreadPoolIOBackendPoll*) backend;
+	g_assert (backend_poll);
 
-	mono_gc_set_skip_thread (TRUE);
+	g_assert (events);
+	g_assert (nevents > 0);
 
-	ready = mono_poll (poll_fds, poll_fds_size, -1);
+	for (i = 0; i < backend_poll->fds_size; ++i)
+		backend_poll->fds [i].revents = 0;
 
-	mono_gc_set_skip_thread (FALSE);
+	ready = mono_poll (backend_poll->fds, backend_poll->fds_size, -1);
 
 	if (ready == -1) {
 		/*
@@ -189,55 +229,54 @@ poll_event_wait (void (*callback) (gint fd, gint events, gpointer user_data), gp
 		case WSAEBADF:
 #endif
 		{
-			ready = poll_mark_bad_fds (poll_fds, poll_fds_size);
+			ready = poll_mark_bad_fds (backend_poll->fds, backend_poll->fds_size);
 			break;
 		}
 		default:
 #if !defined(HOST_WIN32)
-			g_error ("poll_event_wait: mono_poll () failed, error (%d) %s", errno, g_strerror (errno));
+			g_error ("[%p][poll] poll: mono_poll () failed, error (%d) %s", backend_poll, errno, g_strerror (errno));
 #else
-			g_error ("poll_event_wait: mono_poll () failed, error (%d)\n", WSAGetLastError ());
+			g_error ("[%p][poll] poll: mono_poll () failed, error (%d)\n", backend_poll, WSAGetLastError ());
 #endif
 			break;
 		}
 	}
 
-	if (ready == -1)
-		return -1;
 	if (ready == 0)
-		return 0;
+		return;
 
 	g_assert (ready > 0);
 
-	for (i = 0; i < poll_fds_size; ++i) {
-		gint fd, events = 0;
+	for (i = 0, j = 0; i < backend_poll->fds_size; ++i) {
+		mono_pollfd e = backend_poll->fds [i];
 
-		if (poll_fds [i].fd == -1)
+		if (e.fd == -1)
 			continue;
-		if (poll_fds [i].revents == 0)
+		if (e.revents == 0)
 			continue;
 
-		fd = poll_fds [i].fd;
-		if (poll_fds [i].revents & (MONO_POLLIN | MONO_POLLERR | MONO_POLLHUP | MONO_POLLNVAL))
-			events |= EVENT_IN;
-		if (poll_fds [i].revents & (MONO_POLLOUT | MONO_POLLERR | MONO_POLLHUP | MONO_POLLNVAL))
-			events |= EVENT_OUT;
-		if (poll_fds [i].revents & (MONO_POLLERR | MONO_POLLHUP | MONO_POLLNVAL))
-			events |= EVENT_ERR;
+		events [j].handle = GINT_TO_POINTER (e.fd);
+		if (e.revents & (MONO_POLLERR | MONO_POLLHUP | MONO_POLLNVAL)) {
+			events [j].events = EVENT_IN | EVENT_OUT | EVENT_ERROR;
+		} else {
+			events [j].events = 0;
+			if (e.revents & MONO_POLLIN)
+				events [j].events |= EVENT_IN;
+			if (e.revents & MONO_POLLOUT)
+				events [j].events |= EVENT_OUT;
+		}
 
-		callback (fd, events, user_data);
-
+		if (++j == nevents)
+			break;
 		if (--ready == 0)
 			break;
 	}
-
-	return 0;
 }
 
 static ThreadPoolIOBackend backend_poll = {
 	.init = poll_init,
 	.cleanup = poll_cleanup,
-	.register_fd = poll_register_fd,
-	.remove_fd = poll_remove_fd,
-	.event_wait = poll_event_wait,
+	.add_handle = poll_add_handle,
+	.remove_handle = poll_remove_handle,
+	.poll = poll_poll,
 };
