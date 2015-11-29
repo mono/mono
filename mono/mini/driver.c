mono/mono

commit 8593c4dddc28a83bb137e2ad502382efc0d53a0d

[runtime] Move the MONO_ENV_OPTIONS parsing code to driver.c

mono/mini/driver.c
@@ -2286,3 +2286,81 @@ mono_set_crash_chaining (gboolean chain_crashes)
 {
 	mono_do_crash_chaining = chain_crashes;
 }
+
+void
+mono_parse_env_options (void)
+{
+	const char *env_options = g_getenv ("MONO_ENV_OPTIONS");
+	if (env_options != NULL){
+		GPtrArray *array = g_ptr_array_new ();
+		GString *buffer = g_string_new ("");
+		const char *p;
+		unsigned i;
+		gboolean in_quotes = FALSE;
+		char quote_char = '\0';
+
+		for (p = env_options; *p; p++){
+			switch (*p){
+			case ' ': case '\t':
+				if (!in_quotes) {
+					if (buffer->len != 0){
+						g_ptr_array_add (array, g_strdup (buffer->str));
+						g_string_truncate (buffer, 0);
+					}
+				} else {
+					g_string_append_c (buffer, *p);
+				}
+				break;
+			case '\\':
+				if (p [1]){
+					g_string_append_c (buffer, p [1]);
+					p++;
+				}
+				break;
+			case '\'':
+			case '"':
+				if (in_quotes) {
+					if (quote_char == *p)
+						in_quotes = FALSE;
+					else
+						g_string_append_c (buffer, *p);
+				} else {
+					in_quotes = TRUE;
+					quote_char = *p;
+				}
+				break;
+			default:
+				g_string_append_c (buffer, *p);
+				break;
+			}
+		}
+		if (in_quotes) {
+			fprintf (stderr, "Unmatched quotes in value of MONO_ENV_OPTIONS: [%s]\n", env_options);
+			exit (1);
+		}
+			
+		if (buffer->len != 0)
+			g_ptr_array_add (array, g_strdup (buffer->str));
+		g_string_free (buffer, TRUE);
+
+		if (array->len > 0){
+			int new_argc = array->len + argc;
+			char **new_argv = g_new (char *, new_argc + 1);
+			int j;
+
+			new_argv [0] = argv [0];
+			
+			/* First the environment variable settings, to allow the command line options to override */
+			for (i = 0; i < array->len; i++)
+				new_argv [i+1] = g_ptr_array_index (array, i);
+			i++;
+			for (j = 1; j < argc; j++)
+				new_argv [i++] = argv [j];
+			new_argv [i] = NULL;
+
+			argc = new_argc;
+			argv = new_argv;
+		}
+		g_ptr_array_free (array, TRUE);
+	}
+}

mono/mini/main.c
@@ -1,5 +1,6 @@
 #include <config.h>
 #include "mini.h"
+
 #ifndef HOST_WIN32
 #ifndef BUILDVER_INCLUDED
 #include "buildver-boehm.h"
@@ -14,80 +15,6 @@
 static int
 mono_main_with_options (int argc, char *argv [])
 {
-	const char *env_options = g_getenv ("MONO_ENV_OPTIONS");
-	if (env_options != NULL){
-		GPtrArray *array = g_ptr_array_new ();
-		GString *buffer = g_string_new ("");
-		const char *p;
-		unsigned i;
-		gboolean in_quotes = FALSE;
-		char quote_char = '\0';
-
-		for (p = env_options; *p; p++){
-			switch (*p){
-			case ' ': case '\t':
-				if (!in_quotes) {
-					if (buffer->len != 0){
-						g_ptr_array_add (array, g_strdup (buffer->str));
-						g_string_truncate (buffer, 0);
-					}
-				} else {
-					g_string_append_c (buffer, *p);
-				}
-				break;
-			case '\\':
-				if (p [1]){
-					g_string_append_c (buffer, p [1]);
-					p++;
-				}
-				break;
-			case '\'':
-			case '"':
-				if (in_quotes) {
-					if (quote_char == *p)
-						in_quotes = FALSE;
-					else
-						g_string_append_c (buffer, *p);
-				} else {
-					in_quotes = TRUE;
-					quote_char = *p;
-				}
-				break;
-			default:
-				g_string_append_c (buffer, *p);
-				break;
-			}
-		}
-		if (in_quotes) {
-			fprintf (stderr, "Unmatched quotes in value of MONO_ENV_OPTIONS: [%s]\n", env_options);
-			exit (1);
-		}
-			
-		if (buffer->len != 0)
-			g_ptr_array_add (array, g_strdup (buffer->str));
-		g_string_free (buffer, TRUE);
-
-		if (array->len > 0){
-			int new_argc = array->len + argc;
-			char **new_argv = g_new (char *, new_argc + 1);
-			int j;
-
-			new_argv [0] = argv [0];
-			
-			/* First the environment variable settings, to allow the command line options to override */
-			for (i = 0; i < array->len; i++)
-				new_argv [i+1] = g_ptr_array_index (array, i);
-			i++;
-			for (j = 1; j < argc; j++)
-				new_argv [i++] = argv [j];
-			new_argv [i] = NULL;
-
-			argc = new_argc;
-			argv = new_argv;
-		}
-		g_ptr_array_free (array, TRUE);
-	}
-
 	return mono_main (argc, argv);
 }
 

mono/mini/mini.h
@@ -2151,6 +2151,7 @@ mono_bb_last_inst (MonoBasicBlock *bb, int filter)
 /* main function */
 MONO_API int         mono_main                      (int argc, char* argv[]);
 MONO_API void        mono_set_defaults              (int verbose_level, guint32 opts);
+void        mono_parse_env_options         (void);
 MonoDomain* mini_init                      (const char *filename, const char *runtime_version);
 void        mini_cleanup                   (MonoDomain *domain);
 MONO_API MonoDebugOptions *mini_get_debug_options   (void);

commit 7e4eb026205d01fa0ac6e69cf9765b0e780a1248

[runtime] Fix support for MONO_ENV_OPTIONS which got broken by 8593c4dddc28a83bb137e2ad502382efc0d53a0d.

mono/mini/driver.c
@@ -2290,8 +2290,11 @@ mono_set_crash_chaining (gboolean chain_crashes)
 }
 
 void
-mono_parse_env_options (int argc, char *argv [])
+mono_parse_env_options (int *ref_argc, char **ref_argv [])
 {
+	int argc = *ref_argc;
+	char **argv = *ref_argv;
+
 	const char *env_options = g_getenv ("MONO_ENV_OPTIONS");
 	if (env_options != NULL){
 		GPtrArray *array = g_ptr_array_new ();
@@ -2360,8 +2363,8 @@ mono_parse_env_options (int argc, char *argv [])
 				new_argv [i++] = argv [j];
 			new_argv [i] = NULL;
 
-			argc = new_argc;
-			argv = new_argv;
+			*ref_argc = new_argc;
+			*ref_argv = new_argv;
 		}
 		g_ptr_array_free (array, TRUE);
 	}

mono/mini/main.c
@@ -15,7 +15,7 @@
 static int
 mono_main_with_options (int argc, char *argv [])
 {
-	mono_parse_env_options (argc, argv);
+	mono_parse_env_options (&argc, &argv);
 
 	return mono_main (argc, argv);
 }

mono/mini/mini.h
@@ -2173,7 +2173,7 @@ mono_bb_last_inst (MonoBasicBlock *bb, int filter)
 /* main function */
 MONO_API int         mono_main                      (int argc, char* argv[]);
 MONO_API void        mono_set_defaults              (int verbose_level, guint32 opts);
-MONO_API void        mono_parse_env_options         (int argc, char *argv []);
+MONO_API void        mono_parse_env_options         (int *ref_argc, char **ref_argv []);
 MonoDomain* mini_init                      (const char *filename, const char *runtime_version);
 void        mini_cleanup                   (MonoDomain *domain);
 MONO_API MonoDebugOptions *mini_get_debug_options   (void);