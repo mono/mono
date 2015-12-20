mono/mono

commit 42c0a9444519f730479129f1dd20fac352b35b16

[mcs] Handle commas inside quotes correctly
OpenLiveWriter's build files break due to incorrectly parsing (".",",") as three arguments: a . and two " instead of two: a . and a ,

This is the error:

OpenLiveWriter/writer.build.targets: error : Error executing task WriteLinesToFile: Error converting Property named 'Lines' with value '#define FILE_VERSION $(BuildVersion.Replace(".",","));#define PRODUCT_VERSION $(BuildVersion)' to type Microsoft.Build.Framework.ITaskItem[]: Method 'Replace(., , )' arguments cannot be evaluated'

This patch ignores , as a separator character if it is inside ""

mcs/class/Microsoft.Build.Engine/Microsoft.Build.BuildEngine/Expression.cs
@@ -464,6 +464,7 @@ namespace Microsoft.Build.BuildEngine {
 			List<string> args = new List<string> ();
 			int parens = 0;
 			bool backticks = false;
+			bool inquotes = false;
 			int start = pos;
 			for (; pos < text.Length; ++pos) {
 				var ch = text [pos];
@@ -475,6 +476,11 @@ namespace Microsoft.Build.BuildEngine {
 
 				if (backticks)
 					continue;
+
+				if (ch == '\"') {
+					inquotes = !inquotes;
+					continue;
+				}
 
 				if (ch == '(') {
 					++parens;
@@ -498,7 +504,7 @@ namespace Microsoft.Build.BuildEngine {
 				if (parens != 0)
 					continue;
 
-				if (ch == ',') {
+				if (ch == ',' && !inquotes) {
 					args.Add (text.Substring (start, pos - start));
 					start = pos + 1;
 					continue;

mcs/class/Microsoft.Build.Engine/Test/Microsoft.Build.BuildEngine/ProjectTest.cs
@@ -67,7 +67,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		void Message (object sender, BuildMessageEventArgs args)
 		{
 		}
-		
+
 		void Warning (object sender, BuildWarningEventArgs args)
 		{
 		}
@@ -97,7 +97,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 			Project project;
 			string documentString =
 				"<Project></Project>";
-			
+
 			engine = new Engine (Consts.BinPath);
 
 			DateTime time = DateTime.Now;
@@ -127,7 +127,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 			Project project;
 			string documentString =
 				"<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\"></Project>";
-			
+
 			engine = new Engine (Consts.BinPath);
 			DateTime time = DateTime.Now;
 			project = engine.CreateNewProject ();
@@ -310,7 +310,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
-					<Target 
+					<Target
 						Name='Main'
 						Inputs='a;b;c'
 						Outputs='d;e;f'
@@ -345,7 +345,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 			Engine engine;
 			Project project;
 			IDictionary hashtable = new Hashtable ();
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 					<Target Name='Main'>
@@ -353,7 +353,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 					</Target>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -769,12 +769,12 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -787,15 +787,15 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			engine.GlobalProperties.SetProperty ("Property", "Value");
-			
+
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
 
@@ -815,12 +815,12 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -834,7 +834,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 					<PropertyGroup>
@@ -842,7 +842,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 					</PropertyGroup>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -861,7 +861,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 					<PropertyGroup>
@@ -869,7 +869,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 					</PropertyGroup>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -914,7 +914,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 		{
 			Engine engine;
 			Project project;
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 
@@ -1076,18 +1076,18 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 			Assert.AreEqual (3, tl.TargetStartedEvents, "A1");
 			Assert.AreEqual (3, tl.TargetFinishedEvents, "A1");
 		}
-		
+
 		[Test]
 		public void TestSchemaFile ()
 		{
 			Engine engine;
 			Project project;
-			
+
 			string documentString = @"
 				<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
 				</Project>
 			";
-			
+
 			engine = new Engine (Consts.BinPath);
 			project = engine.CreateNewProject ();
 			project.LoadXml (documentString);
@@ -2092,7 +2092,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
                         <MeTAdata1>md3</MeTAdata1>
                         <Metadata2>md4</Metadata2>
                 </Abc>
-        </ItemGroup> 
+        </ItemGroup>
         <PropertyGroup><ProP1>ValueProp</ProP1></PropertyGroup>
 	<Target Name=""Main"">
 		<MesSAGE Text=""Full item: @(ABC)""/>
@@ -2159,7 +2159,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 					Path.Combine ("fr-FR", "Lib2.resources.dll"),
 					"Lib4.dll", "Lib4" + debug_extn
 				},
-				
+
 				// lib1
 				new string [] {
 					// lib1 files
@@ -2216,7 +2216,7 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 
 			Project project = engine.CreateNewProject ();
 			project.Load (Path.Combine (basepath, "Project01.sln.proj"));
-			
+
 			bool result = project.Build ();
 			if (!result) {
 				logger.DumpMessages ();
@@ -2372,6 +2372,25 @@ namespace MonoTests.Microsoft.Build.BuildEngine {
 			}
 
 			return project;
+		}
+
+		[Test]
+		public void CheckCommaInQuotes ()
+		{
+			string testString = "Test.string.with.Commas";
+			string documentString = string.Format (@"
+			<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+				<PropertyGroup>
+					<TestString>{0}</TestString>
+					<TestString2>$(TestString.Replace(&quot;.&quot;,&quot;,&quot;))</TestString2>
+				</PropertyGroup>
+			</Project>", testString);
+
+			Project project = new Project ();
+      project.LoadXml (documentString);
+
+			string result = project.EvaluatedProperties ["TestString2"].FinalValue;
+      Assert.AreEqual (testString.Replace (".", ","), result, "A1");
 		}
 	}
 }

commit 49f2a66d257558686cc00bfeab305ddcdb91c7dd

[XBuild] Fixed method invocation with string argument containing ",` or ' Before this commit string value starting or ending with this char was malformed because Trim removed also part of string value Also fixed recent regression from 42c0a94445 which failed to split arguments if doubleQuote was inside singleQuoute

mcs/class/Microsoft.Build.Engine/Microsoft.Build.BuildEngine/Expression.cs
@@ -464,7 +464,8 @@ namespace Microsoft.Build.BuildEngine {
 			List<string> args = new List<string> ();
 			int parens = 0;
 			bool backticks = false;
-			bool inquotes = false;
+			bool inDoubleQuotes = false;
+			bool inSingleQuotes = false;
 			int start = pos;
 			for (; pos < text.Length; ++pos) {
 				var ch = text [pos];
@@ -477,8 +478,13 @@ namespace Microsoft.Build.BuildEngine {
 				if (backticks)
 					continue;
 
-				if (ch == '\"') {
-					inquotes = !inquotes;
+				if(ch == '\'' && !inDoubleQuotes) {
+					inSingleQuotes = !inSingleQuotes;
+					continue;
+				}
+
+				if (ch == '\"' && !inSingleQuotes) {
+					inDoubleQuotes = !inDoubleQuotes;
 					continue;
 				}
 
@@ -504,7 +510,7 @@ namespace Microsoft.Build.BuildEngine {
 				if (parens != 0)
 					continue;
 
-				if (ch == ',' && !inquotes) {
+				if (ch == ',' && !inDoubleQuotes && !inSingleQuotes) {
 					args.Add (text.Substring (start, pos - start));
 					start = pos + 1;
 					continue;

mcs/class/Microsoft.Build.Engine/Microsoft.Build.BuildEngine/MemberInvocationReference.cs
@@ -124,6 +124,14 @@ namespace Microsoft.Build.BuildEngine
 			return value;
 		}
 
+		static string TrimFirstAndLast(string unTrimmed)
+		{
+			if (unTrimmed.Length > 1 && Array.IndexOf (ArgumentTrimChars, unTrimmed [0]) != -1 && Array.IndexOf (ArgumentTrimChars, unTrimmed [unTrimmed.Length - 1]) != -1) {
+				return unTrimmed.Substring (1, unTrimmed.Length - 2);
+			}
+			return unTrimmed;
+		}
+
 		void ExpandArguments (Project project, ExpressionOptions options)
 		{
 			for (int i = 0; i < Arguments.Count; ++i) {
@@ -134,7 +142,7 @@ namespace Microsoft.Build.BuildEngine
 					arg = Expression.ParseAs<string> (arg, ParseOptions.None,
 						project, options);
 
-					arg = arg.Trim (ArgumentTrimChars);
+					arg = TrimFirstAndLast (arg);
 				}
 
 				Arguments [i] = arg;

mcs/class/Microsoft.Build.Engine/Test/various/Properties.cs
@@ -101,11 +101,17 @@ namespace MonoTests.Microsoft.Build.BuildEngine.Various {
 							<Config>debug</Config>
 							<NullValue>null</NullValue>
 							<TargetValue>  </TargetValue>
+							<StringWithQuotes>abc""def</StringWithQuotes>
 							<Prop1>$(Config.Substring(0,3)) </Prop1>
 							<Prop2>$(Config.Length )</Prop2>
 							<Prop3>$(Config.StartsWith ('DE', System.StringComparison.OrdinalIgnoreCase))</Prop3>
 							<Prop4>$(NullValue.StartsWith ('Te', StringComparison.OrdinalIgnoreCase))</Prop4>
 							<Prop5>$(TargetValue.Trim('\\'))</Prop5>
+							<Prop6>$(StringWithQuotes.Replace('""', ""'""))</Prop6>
+							<Prop7>$(StringWithQuotes.Replace('""', ''))</Prop7>
+							<Prop8>$(StringWithQuotes.Replace('""', """"))</Prop8>
+							<Prop9>$(StringWithQuotes.Replace('""', ``))</Prop9>
+							<Prop9>$(StringWithQuotes.Replace(`c""d`, `2""'3`))</Prop9>
 						</PropertyGroup>
 					</Project>
 				";
@@ -116,6 +122,10 @@ namespace MonoTests.Microsoft.Build.BuildEngine.Various {
 			Assert.AreEqual ("True", proj.GetEvaluatedProperty ("Prop3"), "#3");
 			Assert.AreEqual ("False", proj.GetEvaluatedProperty ("Prop4"), "#4");
 			Assert.AreEqual ("", proj.GetEvaluatedProperty ("Prop5"), "#5");
+			Assert.AreEqual ("abc'def", proj.GetEvaluatedProperty ("Prop6"), "#6");
+			Assert.AreEqual ("abcdef", proj.GetEvaluatedProperty ("Prop7"), "#7");
+			Assert.AreEqual ("abcdef", proj.GetEvaluatedProperty ("Prop8"), "#8");
+			Assert.AreEqual ("ab2\"'3ef", proj.GetEvaluatedProperty ("Prop9"), "#9");
 		}
 
 		[Test]