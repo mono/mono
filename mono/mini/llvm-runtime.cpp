460affcfa97334f0d76006ee3d0a63a4982871a3
Zoltan Varga <vargaz@gmail.com>
2016-01-05 09:29:27 -0500

    [runtime] Fix the loaded llvm build.

--- mono/mini/llvm-runtime.cpp
+++ mono/mini/llvm-runtime.cpp
@@ -8,10 +8,15 @@ extern "C" {
 void
 mono_llvm_cpp_throw_exception (void)
 {
+#ifdef MONO_LLVM_LOADED
+	// FIXME:
+	g_assert_not_reached ();
+#else
 	gint32 *ex = NULL;
 
 	/* The generated code catches an int32* */
 	throw ex;
+#endif
 }
 
 }
