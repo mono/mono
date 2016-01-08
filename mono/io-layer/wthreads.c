mono/mono

commit d5768a7f141e2a579cbca26f76c791c215f4aabf

Implement a SetPriority/GetPriority facility for real-time threads: if mono is executed using the SCHED_RR policy, we will map POSIX priorities to .NET priorities. Under this policy a thread running under this policy which uses the SetPriority API will have its priority remapped and a call to pthread_setschedparam() made to change its priority.
The following code fragment may be used to run a mono application
without requiring all root privileges other than the CAP_SYS_NICE
capability which enables priority manipulation:

void
runMonoRT(const char *const name, const int policy, char **argv)
{
    const pid_t         me = getpid();
    struct sched_param  param;

    param.sched_priority = sched_get_priority_min(policy);
    if (sched_setscheduler(me, policy, &param) == -1)
        fprintf(stderr, "sched_setscheduler(getpid(), %s, { %d }): %s.\n",
			name, param.sched_priority, strerror(errno));
    else
        execvp("mono", argv);
}

int
main(int argc, char **argv)
{
    uid_t       user;
    cap_value_t root_caps[2] = { CAP_SYS_NICE, CAP_SETUID };
    cap_value_t user_caps[1] = { CAP_SYS_NICE };
    cap_t       capabilities;

    /* Get real user ID. */
    user = getuid();

    /* Get full root privileges. Normally being effectively root
     * (see man 7 credentials, User and Group Identifiers, for explanation
     *  for effective versus real identity) is enough, but some security
     * modules restrict actions by processes that are only effectively root.
     * To make sure we don't hit those problems, we switch to root fully. */
    if (setresuid(0, 0, 0)) {
        fprintf(stderr, "Cannot switch to root: %s.\n", strerror(errno));
        return 1;
    }

    /* Create an empty set of capabilities. */
    capabilities = cap_init();

    /* Capabilities have three subsets:
     *      INHERITABLE:    Capabilities permitted after an execv()
     *      EFFECTIVE:      Currently effective capabilities

mono/io-layer/wthreads.c
@@ -55,6 +55,14 @@ struct _WapiHandleOps _wapi_thread_ops = {
 	NULL,				/* special_wait */
 	NULL				/* prewait */
 };
+ 
+typedef enum {
+	THREAD_PRIORITY_LOWEST = -2,
+	THREAD_PRIORITY_BELOW_NORMAL = -1,
+	THREAD_PRIORITY_NORMAL = 0,
+	THREAD_PRIORITY_ABOVE_NORMAL = 1,
+	THREAD_PRIORITY_HIGHEST = 2
+} WapiThreadPriority;
 
 static mono_once_t thread_ops_once = MONO_ONCE_INIT;
 
@@ -225,6 +233,201 @@ _wapi_thread_disown_mutex (gpointer mutex)
 	_wapi_handle_unref (mutex);
 	
 	g_ptr_array_remove (thread->owned_mutexes, mutex);
+}
+
+/**
+ * _wapi_thread_posix_priority_to_priority:
+ *
+ *   Convert a POSIX priority to a WapiThreadPriority.
+ * sched_priority is a POSIX priority,
+ * policy is the current scheduling policy
+ */
+static WapiThreadPriority 
+_wapi_thread_posix_priority_to_priority (int sched_priority, int policy)
+{
+/* Necessary to get valid priority range */
+#ifdef _POSIX_PRIORITY_SCHEDULING
+	int max,
+	    min,
+	    i,
+	    priority,
+	    chunk;
+	WapiThreadPriority priorities[] = {
+		THREAD_PRIORITY_LOWEST,
+		THREAD_PRIORITY_LOWEST,
+		THREAD_PRIORITY_BELOW_NORMAL,
+		THREAD_PRIORITY_NORMAL,
+		THREAD_PRIORITY_ABOVE_NORMAL,
+		THREAD_PRIORITY_HIGHEST,
+		THREAD_PRIORITY_HIGHEST
+	};
+	    
+	max = sched_get_priority_max (policy);
+	min = sched_get_priority_min (policy);
+	
+	/* Partition priority range linearly, 
+	   assign each partition a thread priority */
+	if (max != min && 0 <= max && 0 <= min) {
+		for (i=1, priority=min, chunk=(max-min)/7; 
+		     i<6 && sched_priority > priority;
+		     ++i) {
+			priority += chunk;
+		}
+		
+		if (max <= priority)
+		{
+			return (THREAD_PRIORITY_HIGHEST);
+		}
+		else
+		{
+			return (priorities[i-1]);
+		}
+	}
+#endif
+
+	return (THREAD_PRIORITY_NORMAL);
+}
+
+/**
+ * _wapi_thread_priority_to_posix_priority:
+ *
+ *   Convert a WapiThreadPriority to a POSIX priority.
+ * priority is a WapiThreadPriority,
+ * policy is the current scheduling policy
+ */
+static int 
+_wapi_thread_priority_to_posix_priority (WapiThreadPriority priority, int policy)
+{
+/* Necessary to get valid priority range */
+#ifdef _POSIX_PRIORITY_SCHEDULING
+	int max,
+	    min,
+	    posix_priority,
+	    i;
+	WapiThreadPriority priorities[] = {
+		THREAD_PRIORITY_LOWEST,
+		THREAD_PRIORITY_LOWEST,
+		THREAD_PRIORITY_BELOW_NORMAL,
+		THREAD_PRIORITY_NORMAL,
+		THREAD_PRIORITY_ABOVE_NORMAL,
+		THREAD_PRIORITY_HIGHEST,
+		THREAD_PRIORITY_HIGHEST
+	};
+	
+	max = sched_get_priority_max (policy);
+	min = sched_get_priority_min (policy);
+
+	/* Partition priority range linearly, 
+	   numerically approximate matching ThreadPriority */
+	if (max != min && 0 <= max && 0 <= min) {
+		for (i=0; i<7; ++i) {
+			if (priorities[i] == priority) {
+				posix_priority = min + ((max-min)/7) * i;
+				if (max < posix_priority)
+				{
+					return max;
+				}
+				else {
+					return posix_priority;
+				}
+			}
+		}
+	}
+#endif
+
+	switch (policy) {
+		case SCHED_FIFO:
+		case SCHED_RR:
+			return 50;
+		case SCHED_BATCH:
+		case SCHED_OTHER:
+			return 0;
+		default:
+			return -1;
+	}
+}
+
+/**
+ * GetThreadPriority:
+ * @param handle: The thread handle to query.
+ *
+ * Gets the priority of the given thread.
+ * @return: A MonoThreadPriority approximating the current POSIX 
+ * thread priority, or THREAD_PRIORITY_NORMAL on error.
+ */
+gint32 
+GetThreadPriority (gpointer handle)
+{
+	struct _WapiHandle_thread *thread_handle;
+	int policy;
+	struct sched_param param;
+	gboolean ok = _wapi_lookup_handle (handle, WAPI_HANDLE_THREAD,
+				  (gpointer *)&thread_handle);
+				  
+	if (ok == FALSE) {
+		return (THREAD_PRIORITY_NORMAL);
+	}
+	
+	switch (pthread_getschedparam (thread_handle->id, &policy, &param)) {
+		case 0:
+			return (_wapi_thread_posix_priority_to_priority (param.sched_priority, policy));
+		case ESRCH:
+			g_warning ("pthread_getschedparam: error looking up thread id %x", (gsize)thread_handle->id);
+	}
+	
+	return (THREAD_PRIORITY_NORMAL);
+}
+
+/**
+ * SetThreadPriority:
+ * @param handle: The thread handle to query.
+ * @param priority: The priority to give to the thread.
+ *
+ * Sets the priority of the given thread.
+ * @return: TRUE on success, FALSE on failure or error.
+ */
+gboolean 
+SetThreadPriority (gpointer handle, gint32 priority)
+{
+	struct _WapiHandle_thread *thread_handle;
+	int policy,
+	    posix_priority,
+	    rv;
+	struct sched_param param;
+	gboolean ok = _wapi_lookup_handle (handle, WAPI_HANDLE_THREAD,
+				  (gpointer *)&thread_handle);
+				  
+	if (ok == FALSE) {
+		return ok;
+	}
+	
+	rv = pthread_getschedparam (thread_handle->id, &policy, &param);
+	if (rv) {
+		if (ESRCH == rv)
+			g_warning ("pthread_getschedparam: error looking up thread id %x", (gsize)thread_handle->id);
+		return FALSE;
+	}
+	
+	posix_priority =  _wapi_thread_priority_to_posix_priority (priority, policy);
+	if (0 > posix_priority)
+		return FALSE;
+		
+	param.sched_priority = posix_priority;
+	switch (pthread_setschedparam (thread_handle->id, policy, &param)) {
+		case 0:
+			return TRUE;
+		case ESRCH:
+			g_warning ("pthread_setschedparam: error looking up thread id %x", (gsize)thread_handle->id);
+			break;
+		case ENOTSUP:
+			g_warning ("%s: priority %d not supported", __func__, priority);
+			break;
+		case EPERM:
+			g_warning ("%s: permission denied", __func__);
+			break;
+	}
+	
+	return FALSE;
 }
 
 char*

mono/io-layer/threads.h
@@ -33,5 +33,8 @@ gpointer wapi_get_current_thread_handle (void);
 
 char* wapi_current_thread_desc (void);
 
+extern gint32 GetThreadPriority (gpointer handle);
+extern gboolean SetThreadPriority (gpointer handle, gint32 priority);
+
 G_END_DECLS
 #endif /* _WAPI_THREADS_H_ */

mono/metadata/threads.c
@@ -208,7 +208,7 @@ static void abort_thread_internal (MonoInternalThread *thread, gboolean can_rais
 static void suspend_thread_internal (MonoInternalThread *thread, gboolean interrupt);
 static void self_suspend_internal (MonoInternalThread *thread);
 
-static MonoException* mono_thread_execute_interruption ();
+static MonoException* mono_thread_execute_interruption (void);
 static void ref_stack_destroy (gpointer rs);
 
 /* Spin lock for InterlockedXXX 64 bit functions */
@@ -1300,15 +1300,40 @@ ves_icall_System_Threading_Thread_SetName_internal (MonoInternalThread *this_obj
 	mono_thread_set_name_internal (this_obj, name, TRUE);
 }
 
+/*
+ * ves_icall_System_Threading_Thread_GetPriority_internal:
+ * @param this_obj: The MonoInternalThread on which to operate.
+ *
+ * Gets the priority of the given thread.
+ * @return: The priority of the given thread.
+ */
 int
 ves_icall_System_Threading_Thread_GetPriority (MonoThread *this_obj)
 {
-	return ThreadPriority_Lowest;
+	gint32 priority;
+	MonoInternalThread *internal = this_obj->internal_thread;
+
+	LOCK_THREAD (internal);
+	priority = GetThreadPriority (internal->handle) + 2;
+	UNLOCK_THREAD (internal);
+	return priority;
 }
 
+/* 
+ * ves_icall_System_Threading_Thread_SetPriority_internal:
+ * @param this_obj: The MonoInternalThread on which to operate.
+ * @param priority: The priority to set.
+ *
+ * Sets the priority of the given thread.
+ */
 void
 ves_icall_System_Threading_Thread_SetPriority (MonoThread *this_obj, int priority)
 {
+	MonoInternalThread *internal = this_obj->internal_thread;
+
+	LOCK_THREAD (internal);
+	SetThreadPriority (internal->handle, priority - 2);
+	UNLOCK_THREAD (internal);
 }
 
 /* If the array is already in the requested domain, we just return it,

commit 31f13881a483201933b16e75d47a6d103d959a64

[io-layer] Fix build on OSX
This got broken by d5768a7f141e2a579cbca26f76c791c215f4aabf, SCHED_BATCH isn't available on OSX or Linux before 2.6.16

configure.ac
@@ -1182,6 +1182,20 @@ if test x$host_win32 = xno; then
 		AC_MSG_RESULT(no)
 	])
 
+	dnl ****************************************************************
+	dnl *** Check for SCHED_BATCH                                    ***
+	dnl ****************************************************************
+	AC_MSG_CHECKING(for SCHED_BATCH)
+	AC_TRY_COMPILE([#include <sched.h>], [
+			int policy = SCHED_BATCH;
+			return 0;
+	], [
+		# Yes, we have it...
+		AC_MSG_RESULT(yes)
+		AC_DEFINE(HAVE_SCHED_BATCH, 1, [Have HAVE_SCHED_BATCH])
+	], [
+		AC_MSG_RESULT(no)
+	])
 
 	dnl ******************************************************************
 	dnl *** Check for large file support                               ***

mono/io-layer/wthreads.c
@@ -339,7 +339,9 @@ _wapi_thread_priority_to_posix_priority (WapiThreadPriority priority, int policy
 		case SCHED_FIFO:
 		case SCHED_RR:
 			return 50;
+#ifdef HAVE_SCHED_BATCH
 		case SCHED_BATCH:
+#endif
 		case SCHED_OTHER:
 			return 0;
 		default: