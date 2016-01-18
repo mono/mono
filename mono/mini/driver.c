efea2b47265325757aee2b581da9f48ec4c8e9b6
Marek Habersack <grendel@twistedcode.net>
2016-01-18 23:41:30 +0100

    Move mono_trace to header file
    This commit moves mono_trace to a header file so that all the
    calls are inlined, avoiding one function call at the small expense
    of binary size.

--- mono/mini/driver.c
+++ mono/mini/driver.c
@@ -50,6 +50,7 @@
 #include <mono/metadata/attach.h>
 #include "mono/utils/mono-counters.h"
 #include "mono/utils/mono-hwcap.h"
+#include "mono/utils/mono-logger-internals.h"
 
 #include "mini.h"
 #include "jit.h"
