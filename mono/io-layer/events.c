backend-devloper/runtime

Replace DEBUG messages with calls to mono_trace
The messages contain useful debugging information which is unavailable
unless the mono runtime is recompiled. The debugging information is
particularly useful on mobile platforms and in test runs on bots.

The I/O layer trace messages can be disabled on compile time by
defining the DISABLE_IO_LAYER_TRACE preprocessor symbol.


Commit migrated from https://github.com/mono/mono/commit/39393f118d6a532ccca30a9103a3dd0febdfbef6

src/mono/mono/io-layer/events.c
@@ -16,13 +16,9 @@
 #include <mono/io-layer/wapi-private.h>
 #include <mono/io-layer/handles-private.h>
 #include <mono/io-layer/event-private.h>
+#include <mono/io-layer/io-trace.h>
 #include <mono/utils/mono-once.h>
-
-#if 0
-#define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
+#include <mono/utils/mono-logger-internals.h>
 
 static void event_signal(gpointer handle);
 static gboolean event_own (gpointer handle);
@@ -111,7 +107,7 @@ static gboolean event_own (gpointer handle)
 		return (FALSE);
 	}
 	
-	DEBUG("%s: owning event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: owning event handle %p", __func__, handle);
 
 	if(event_handle->manual==FALSE) {
 		g_assert (event_handle->set_count > 0);
@@ -135,7 +131,7 @@ static gboolean namedevent_own (gpointer handle)
 	struct _WapiHandle_namedevent *namedevent_handle;
 	gboolean ok;
 	
-	DEBUG ("%s: owning named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: owning named event handle %p", __func__, handle);
 
 	ok = _wapi_lookup_handle (handle, WAPI_HANDLE_NAMEDEVENT,
 				  (gpointer *)&namedevent_handle);
@@ -168,7 +164,7 @@ static gpointer event_create (WapiSecurityAttributes *security G_GNUC_UNUSED,
 	 */
 	SetLastError (ERROR_SUCCESS);
 
-	DEBUG ("%s: Creating unnamed event", __func__);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Creating unnamed event", __func__);
 	
 	event_handle.manual = manual;
 	event_handle.set_count = 0;
@@ -193,7 +189,7 @@ static gpointer event_create (WapiSecurityAttributes *security G_GNUC_UNUSED,
 		_wapi_handle_set_signal_state (handle, TRUE, FALSE);
 	}
 	
-	DEBUG("%s: created new event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: created new event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_unlock_handle (handle);
 	g_assert (thr_ret == 0);
@@ -227,7 +223,7 @@ static gpointer namedevent_create (WapiSecurityAttributes *security G_GNUC_UNUSE
 	
 	utf8_name = g_utf16_to_utf8 (name, -1, NULL, NULL, NULL);
 	
-	DEBUG ("%s: Creating named event [%s]", __func__, utf8_name);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Creating named event [%s]", __func__, utf8_name);
 	
 	offset = _wapi_search_handle_namespace (WAPI_HANDLE_NAMEDEVENT,
 						utf8_name);
@@ -298,7 +294,7 @@ static gpointer namedevent_create (WapiSecurityAttributes *security G_GNUC_UNUSE
 		_wapi_handle_unlock_shared_handles ();
 	}
 	
-	DEBUG ("%s: returning event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: returning event handle %p", __func__, handle);
 
 cleanup:
 	g_free (utf8_name);
@@ -360,7 +356,7 @@ static gboolean event_pulse (gpointer handle)
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Pulsing event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Pulsing event handle %p", __func__, handle);
 
 	if (event_handle->manual == TRUE) {
 		_wapi_handle_set_signal_state (handle, TRUE, TRUE);
@@ -385,7 +381,7 @@ static gboolean event_pulse (gpointer handle)
 		 * have proceeded.  Currently we rely on broadcasting
 		 * a condition.
 		 */
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		thr_ret = _wapi_handle_lock_handle (handle);
@@ -417,7 +413,7 @@ static gboolean namedevent_pulse (gpointer handle)
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Pulsing named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Pulsing named event handle %p", __func__, handle);
 
 	if (namedevent_handle->manual == TRUE) {
 		_wapi_shared_handle_set_signal_state (handle, TRUE);
@@ -441,7 +437,7 @@ static gboolean namedevent_pulse (gpointer handle)
 		 * have proceeded.  Currently we rely on waiting for
 		 * twice the shared handle poll interval.
 		 */
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		thr_ret = _wapi_handle_lock_shared_handles ();
@@ -503,16 +499,16 @@ static gboolean event_reset (gpointer handle)
 		return(FALSE);
 	}
 
-	DEBUG ("%s: Resetting event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Resetting event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 	
 	if (_wapi_handle_issignalled (handle) == FALSE) {
-		DEBUG ("%s: No need to reset event handle %p", __func__,
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: No need to reset event handle %p", __func__,
 			   handle);
 	} else {
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		_wapi_handle_set_signal_state (handle, FALSE, FALSE);
@@ -540,16 +536,16 @@ static gboolean namedevent_reset (gpointer handle)
 		return(FALSE);
 	}
 
-	DEBUG ("%s: Resetting named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Resetting named event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 	
 	if (_wapi_handle_issignalled (handle) == FALSE) {
-		DEBUG ("%s: No need to reset named event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: No need to reset named event handle %p",
 			   __func__, handle);
 	} else {
-		DEBUG ("%s: Obtained write lock on named event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on named event handle %p",
 			   __func__, handle);
 
 		_wapi_shared_handle_set_signal_state (handle, FALSE);
@@ -607,7 +603,7 @@ static gboolean event_set (gpointer handle)
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Setting event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Setting event handle %p", __func__, handle);
 
 	if (event_handle->manual == TRUE) {
 		_wapi_handle_set_signal_state (handle, TRUE, TRUE);
@@ -639,7 +635,7 @@ static gboolean namedevent_set (gpointer handle)
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Setting named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Setting named event handle %p", __func__, handle);
 
 	if (namedevent_handle->manual == TRUE) {
 		_wapi_shared_handle_set_signal_state (handle, TRUE);
@@ -704,7 +700,7 @@ gpointer OpenEvent (guint32 access G_GNUC_UNUSED, gboolean inherit G_GNUC_UNUSED
 
 	utf8_name = g_utf16_to_utf8 (name, -1, NULL, NULL, NULL);
 	
-	DEBUG ("%s: Opening named event [%s]", __func__, utf8_name);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Opening named event [%s]", __func__, utf8_name);
 	
 	offset = _wapi_search_handle_namespace (WAPI_HANDLE_NAMEDEVENT,
 						utf8_name);
@@ -733,7 +729,7 @@ gpointer OpenEvent (guint32 access G_GNUC_UNUSED, gboolean inherit G_GNUC_UNUSED
 	}
 	ret = handle;
 
-	DEBUG ("%s: returning named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: returning named event handle %p", __func__, handle);
 
 cleanup:
 	g_free (utf8_name);

src/mono/mono/io-layer/locking.c
@@ -16,12 +16,8 @@
 #include <mono/io-layer/wapi-private.h>
 #include <mono/io-layer/handles-private.h>
 #include <mono/io-layer/io-private.h>
-
-#if 0
-// #define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
+#include <mono/io-layer/io-trace.h>
+#include <mono/utils/mono-logger-internals.h>
 
 gboolean
 _wapi_lock_file_region (int fd, off_t offset, off_t length)
@@ -48,7 +44,7 @@ _wapi_lock_file_region (int fd, off_t offset, off_t length)
 		ret = fcntl (fd, F_SETLK, &lock_data);
 	} while(ret == -1 && errno == EINTR);
 	
-	DEBUG ("%s: fcntl returns %d", __func__, ret);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: fcntl returns %d", __func__, ret);
 
 	if (ret == -1) {
 		/*
@@ -93,7 +89,7 @@ _wapi_unlock_file_region (int fd, off_t offset, off_t length)
 		ret = fcntl (fd, F_SETLK, &lock_data);
 	} while(ret == -1 && errno == EINTR);
 	
-	DEBUG ("%s: fcntl returns %d", __func__, ret);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: fcntl returns %d", __func__, ret);
 	
 	if (ret == -1) {
 		/*
@@ -140,7 +136,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	if (!(file_handle->fileaccess & GENERIC_READ) &&
 	    !(file_handle->fileaccess & GENERIC_WRITE) &&
 	    !(file_handle->fileaccess & GENERIC_ALL)) {
-		DEBUG ("%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
 		SetLastError (ERROR_ACCESS_DENIED);
 		return(FALSE);
 	}
@@ -149,7 +145,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	offset = ((gint64)offset_high << 32) | offset_low;
 	length = ((gint64)length_high << 32) | length_low;
 
-	DEBUG ("%s: Locking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Locking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
 #else
 	if (offset_high > 0 || length_high > 0) {
 		SetLastError (ERROR_INVALID_PARAMETER);
@@ -158,7 +154,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	offset = offset_low;
 	length = length_low;
 
-	DEBUG ("%s: Locking handle %p, offset %ld, length %ld", __func__,
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Locking handle %p, offset %ld, length %ld", __func__,
 		   handle, offset, length);
 #endif
 
@@ -187,7 +183,7 @@ UnlockFile (gpointer handle, guint32 offset_low,
 	if (!(file_handle->fileaccess & GENERIC_READ) &&
 	    !(file_handle->fileaccess & GENERIC_WRITE) &&
 	    !(file_handle->fileaccess & GENERIC_ALL)) {
-		DEBUG ("%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
 		SetLastError (ERROR_ACCESS_DENIED);
 		return(FALSE);
 	}
@@ -196,12 +192,12 @@ UnlockFile (gpointer handle, guint32 offset_low,
 	offset = ((gint64)offset_high << 32) | offset_low;
 	length = ((gint64)length_high << 32) | length_low;
 
-	DEBUG ("%s: Unlocking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Unlocking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
 #else
 	offset = offset_low;
 	length = length_low;
 
-	DEBUG ("%s: Unlocking handle %p, offset %ld, length %ld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Unlocking handle %p, offset %ld, length %ld", __func__, handle, offset, length);
 #endif
 
 	return(_wapi_unlock_file_region (fd, offset, length));

src/mono/mono/io-layer/wthreads.c
@@ -25,26 +25,16 @@
 #include <mono/io-layer/handles-private.h>
 #include <mono/io-layer/thread-private.h>
 #include <mono/io-layer/mutex-private.h>
+#include <mono/io-layer/io-trace.h>
 
 #include <mono/utils/mono-threads.h>
 #include <mono/utils/atomic.h>
 #include <mono/utils/mono-time.h>
 #include <mono/utils/mono-once.h>
+#include <mono/utils/mono-logger-internals.h>
 
 #ifdef HAVE_VALGRIND_MEMCHECK_H
 #include <valgrind/memcheck.h>
-#endif
-
-#if 0
-#define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
-
-#if 0
-#define WAIT_DEBUG(code) do { code } while (0)
-#else
-#define WAIT_DEBUG(code) do { } while (0)
 #endif
 
 struct _WapiHandleOps _wapi_thread_ops = {
@@ -126,11 +116,11 @@ wapi_thread_handle_set_exited (gpointer handle, guint32 exitstatus)
 		return;
 	}
 
-	DEBUG ("%s: Thread %p terminating", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Thread %p terminating", __func__, handle);
 
 	thread_handle = lookup_thread (handle);
 
-	DEBUG ("%s: Thread %p abandoning held mutexes", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Thread %p abandoning held mutexes", __func__, handle);
 
 	for (i = 0; i < thread_handle->owned_mutexes->len; i++) {
 		gpointer mutex = g_ptr_array_index (thread_handle->owned_mutexes, i);
@@ -148,7 +138,7 @@ wapi_thread_handle_set_exited (gpointer handle, guint32 exitstatus)
 	thr_ret = _wapi_handle_unlock_handle (handle);
 	g_assert (thr_ret == 0);
 	
-	DEBUG("%s: Recording thread handle %p id %ld status as %d",
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Recording thread handle %p id %ld status as %d",
 		  __func__, handle, thread_handle->id, exitstatus);
 	
 	/* The thread is no longer active, so unref it */
@@ -188,7 +178,7 @@ wapi_create_thread_handle (void)
 	 */
 	_wapi_handle_ref (handle);
 
-	DEBUG ("%s: started thread id %ld", __func__, thread->id);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: started thread id %ld", __func__, thread->id);
 	
 	return handle;
 }

mono/mono

mono/io-layer/events.c
@@ -18,13 +18,8 @@
 #include <mono/io-layer/misc-private.h>
 
 #include <mono/io-layer/event-private.h>
-
-#include <mono/utils/mono-mutex.h>
-#if 0
-#define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
+#include <mono/io-layer/io-trace.h>
+#include <mono/utils/mono-logger-internal.h>
 
 static void event_signal(gpointer handle);
 static gboolean event_own (gpointer handle);
@@ -115,7 +110,7 @@ static gboolean event_own (gpointer handle)
 		return (FALSE);
 	}
 	
-	DEBUG("%s: owning event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: owning event handle %p", __func__, handle);
 
 	if(event_handle->manual==FALSE) {
 		g_assert (event_handle->set_count > 0);
@@ -139,7 +134,7 @@ static gboolean namedevent_own (gpointer handle)
 	struct _WapiHandle_namedevent *namedevent_handle;
 	gboolean ok;
 	
-	DEBUG ("%s: owning named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: owning named event handle %p", __func__, handle);
 
 	ok = _wapi_lookup_handle (handle, WAPI_HANDLE_NAMEDEVENT,
 				  (gpointer *)&namedevent_handle);
@@ -172,7 +167,7 @@ static gpointer event_create (WapiSecurityAttributes *security G_GNUC_UNUSED,
 	 */
 	SetLastError (ERROR_SUCCESS);
 
-	DEBUG ("%s: Creating unnamed event", __func__);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Creating unnamed event", __func__);
 	
 	event_handle.manual = manual;
 	event_handle.set_count = 0;
@@ -197,7 +192,7 @@ static gpointer event_create (WapiSecurityAttributes *security G_GNUC_UNUSED,
 		_wapi_handle_set_signal_state (handle, TRUE, FALSE);
 	}
 	
-	DEBUG("%s: created new event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: created new event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_unlock_handle (handle);
 	g_assert (thr_ret == 0);
@@ -231,7 +226,7 @@ static gpointer namedevent_create (WapiSecurityAttributes *security G_GNUC_UNUSE
 	
 	utf8_name = g_utf16_to_utf8 (name, -1, NULL, NULL, NULL);
 	
-	DEBUG ("%s: Creating named event [%s]", __func__, utf8_name);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Creating named event [%s]", __func__, utf8_name);
 	
 	offset = _wapi_search_handle_namespace (WAPI_HANDLE_NAMEDEVENT,
 						utf8_name);
@@ -302,7 +297,7 @@ static gpointer namedevent_create (WapiSecurityAttributes *security G_GNUC_UNUSE
 		_wapi_handle_unlock_shared_handles ();
 	}
 	
-	DEBUG ("%s: returning event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: returning event handle %p", __func__, handle);
 
 cleanup:
 	g_free (utf8_name);
@@ -364,7 +359,7 @@ static gboolean event_pulse (gpointer handle)
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Pulsing event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Pulsing event handle %p", __func__, handle);
 
 	if (event_handle->manual == TRUE) {
 		_wapi_handle_set_signal_state (handle, TRUE, TRUE);
@@ -389,7 +384,7 @@ static gboolean event_pulse (gpointer handle)
 		 * have proceeded.  Currently we rely on broadcasting
 		 * a condition.
 		 */
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		thr_ret = _wapi_handle_lock_handle (handle);
@@ -421,7 +416,7 @@ static gboolean namedevent_pulse (gpointer handle)
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Pulsing named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Pulsing named event handle %p", __func__, handle);
 
 	if (namedevent_handle->manual == TRUE) {
 		_wapi_shared_handle_set_signal_state (handle, TRUE);
@@ -445,7 +440,7 @@ static gboolean namedevent_pulse (gpointer handle)
 		 * have proceeded.  Currently we rely on waiting for
 		 * twice the shared handle poll interval.
 		 */
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		thr_ret = _wapi_handle_lock_shared_handles ();
@@ -507,16 +502,16 @@ static gboolean event_reset (gpointer handle)
 		return(FALSE);
 	}
 
-	DEBUG ("%s: Resetting event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Resetting event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 	
 	if (_wapi_handle_issignalled (handle) == FALSE) {
-		DEBUG ("%s: No need to reset event handle %p", __func__,
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: No need to reset event handle %p", __func__,
 			   handle);
 	} else {
-		DEBUG ("%s: Obtained write lock on event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on event handle %p",
 			   __func__, handle);
 
 		_wapi_handle_set_signal_state (handle, FALSE, FALSE);
@@ -544,16 +539,16 @@ static gboolean namedevent_reset (gpointer handle)
 		return(FALSE);
 	}
 
-	DEBUG ("%s: Resetting named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Resetting named event handle %p", __func__, handle);
 
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 	
 	if (_wapi_handle_issignalled (handle) == FALSE) {
-		DEBUG ("%s: No need to reset named event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: No need to reset named event handle %p",
 			   __func__, handle);
 	} else {
-		DEBUG ("%s: Obtained write lock on named event handle %p",
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Obtained write lock on named event handle %p",
 			   __func__, handle);
 
 		_wapi_shared_handle_set_signal_state (handle, FALSE);
@@ -611,7 +606,7 @@ static gboolean event_set (gpointer handle)
 	thr_ret = _wapi_handle_lock_handle (handle);
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Setting event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Setting event handle %p", __func__, handle);
 
 	if (event_handle->manual == TRUE) {
 		_wapi_handle_set_signal_state (handle, TRUE, TRUE);
@@ -643,7 +638,7 @@ static gboolean namedevent_set (gpointer handle)
 	thr_ret = _wapi_handle_lock_shared_handles ();
 	g_assert (thr_ret == 0);
 
-	DEBUG ("%s: Setting named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Setting named event handle %p", __func__, handle);
 
 	if (namedevent_handle->manual == TRUE) {
 		_wapi_shared_handle_set_signal_state (handle, TRUE);
@@ -708,7 +703,7 @@ gpointer OpenEvent (guint32 access G_GNUC_UNUSED, gboolean inherit G_GNUC_UNUSED
 
 	utf8_name = g_utf16_to_utf8 (name, -1, NULL, NULL, NULL);
 	
-	DEBUG ("%s: Opening named event [%s]", __func__, utf8_name);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Opening named event [%s]", __func__, utf8_name);
 	
 	offset = _wapi_search_handle_namespace (WAPI_HANDLE_NAMEDEVENT,
 						utf8_name);
@@ -737,7 +732,7 @@ gpointer OpenEvent (guint32 access G_GNUC_UNUSED, gboolean inherit G_GNUC_UNUSED
 	}
 	ret = handle;
 
-	DEBUG ("%s: returning named event handle %p", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: returning named event handle %p", __func__, handle);
 
 cleanup:
 	g_free (utf8_name);

mono/io-layer/locking.c
@@ -16,12 +16,8 @@
 #include <mono/io-layer/wapi-private.h>
 #include <mono/io-layer/handles-private.h>
 #include <mono/io-layer/io-private.h>
-
-#if 0
-// #define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
+#include <mono/io-layer/io-trace.h>
+#include <mono/utils/mono-logger-internal.h>
 
 gboolean
 _wapi_lock_file_region (int fd, off_t offset, off_t length)
@@ -48,7 +44,7 @@ _wapi_lock_file_region (int fd, off_t offset, off_t length)
 		ret = fcntl (fd, F_SETLK, &lock_data);
 	} while(ret == -1 && errno == EINTR);
 	
-	DEBUG ("%s: fcntl returns %d", __func__, ret);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: fcntl returns %d", __func__, ret);
 
 	if (ret == -1) {
 		/*
@@ -93,7 +89,7 @@ _wapi_unlock_file_region (int fd, off_t offset, off_t length)
 		ret = fcntl (fd, F_SETLK, &lock_data);
 	} while(ret == -1 && errno == EINTR);
 	
-	DEBUG ("%s: fcntl returns %d", __func__, ret);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: fcntl returns %d", __func__, ret);
 	
 	if (ret == -1) {
 		/*
@@ -140,7 +136,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	if (!(file_handle->fileaccess & GENERIC_READ) &&
 	    !(file_handle->fileaccess & GENERIC_WRITE) &&
 	    !(file_handle->fileaccess & GENERIC_ALL)) {
-		DEBUG ("%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
 		SetLastError (ERROR_ACCESS_DENIED);
 		return(FALSE);
 	}
@@ -149,7 +145,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	offset = ((gint64)offset_high << 32) | offset_low;
 	length = ((gint64)length_high << 32) | length_low;
 
-	DEBUG ("%s: Locking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Locking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
 #else
 	if (offset_high > 0 || length_high > 0) {
 		SetLastError (ERROR_INVALID_PARAMETER);
@@ -158,7 +154,7 @@ LockFile (gpointer handle, guint32 offset_low, guint32 offset_high,
 	offset = offset_low;
 	length = length_low;
 
-	DEBUG ("%s: Locking handle %p, offset %ld, length %ld", __func__,
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Locking handle %p, offset %ld, length %ld", __func__,
 		   handle, offset, length);
 #endif
 
@@ -187,7 +183,7 @@ UnlockFile (gpointer handle, guint32 offset_low,
 	if (!(file_handle->fileaccess & GENERIC_READ) &&
 	    !(file_handle->fileaccess & GENERIC_WRITE) &&
 	    !(file_handle->fileaccess & GENERIC_ALL)) {
-		DEBUG ("%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
+		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: handle %p doesn't have GENERIC_READ or GENERIC_WRITE access: %u", __func__, handle, file_handle->fileaccess);
 		SetLastError (ERROR_ACCESS_DENIED);
 		return(FALSE);
 	}
@@ -196,12 +192,12 @@ UnlockFile (gpointer handle, guint32 offset_low,
 	offset = ((gint64)offset_high << 32) | offset_low;
 	length = ((gint64)length_high << 32) | length_low;
 
-	DEBUG ("%s: Unlocking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Unlocking handle %p, offset %lld, length %lld", __func__, handle, offset, length);
 #else
 	offset = offset_low;
 	length = length_low;
 
-	DEBUG ("%s: Unlocking handle %p, offset %ld, length %ld", __func__, handle, offset, length);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Unlocking handle %p, offset %ld, length %ld", __func__, handle, offset, length);
 #endif
 
 	return(_wapi_unlock_file_region (fd, offset, length));

mono/io-layer/wthreads.c
@@ -26,26 +26,16 @@
 #include <mono/io-layer/misc-private.h>
 #include <mono/io-layer/thread-private.h>
 #include <mono/io-layer/mutex-private.h>
+#include <mono/io-layer/io-trace.h>
 
 #include <mono/utils/mono-threads.h>
 #include <mono/utils/atomic.h>
 #include <mono/utils/mono-mutex.h>
 #include <mono/utils/mono-time.h>
+#include <mono/utils/mono-logger-internal.h>
 
 #ifdef HAVE_VALGRIND_MEMCHECK_H
 #include <valgrind/memcheck.h>
-#endif
-
-#if 0
-#define DEBUG(...) g_message(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
-
-#if 0
-#define WAIT_DEBUG(code) do { code } while (0)
-#else
-#define WAIT_DEBUG(code) do { } while (0)
 #endif
 
 struct _WapiHandleOps _wapi_thread_ops = {
@@ -119,11 +109,11 @@ wapi_thread_handle_set_exited (gpointer handle, guint32 exitstatus)
 		return;
 	}
 
-	DEBUG ("%s: Thread %p terminating", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Thread %p terminating", __func__, handle);
 
 	thread_handle = lookup_thread (handle);
 
-	DEBUG ("%s: Thread %p abandoning held mutexes", __func__, handle);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Thread %p abandoning held mutexes", __func__, handle);
 
 	for (i = 0; i < thread_handle->owned_mutexes->len; i++) {
 		gpointer mutex = g_ptr_array_index (thread_handle->owned_mutexes, i);
@@ -141,7 +131,7 @@ wapi_thread_handle_set_exited (gpointer handle, guint32 exitstatus)
 	thr_ret = _wapi_handle_unlock_handle (handle);
 	g_assert (thr_ret == 0);
 	
-	DEBUG("%s: Recording thread handle %p id %ld status as %d",
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: Recording thread handle %p id %ld status as %d",
 		  __func__, handle, thread_handle->id, exitstatus);
 	
 	/* The thread is no longer active, so unref it */
@@ -181,7 +171,7 @@ wapi_create_thread_handle (void)
 	 */
 	_wapi_handle_ref (handle);
 
-	DEBUG ("%s: started thread id %ld", __func__, thread->id);
+	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "%s: started thread id %ld", __func__, thread->id);
 	
 	return handle;
 }