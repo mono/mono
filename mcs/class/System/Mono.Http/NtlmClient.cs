mono/mono

commit d2325f1483cd642457a7b2efe402e3c1d414d69d

[System] Fixes leak in NtlmClient which never released cached WebRequest. Fixes #36596

mcs/class/System/Mono.Http/NtlmClient.cs
@@ -40,6 +40,7 @@ using MonoSecurity::Mono.Security.Protocol.Ntlm;
 using System;
 using System.Collections;
 using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace Mono.Http
 {
@@ -122,14 +123,8 @@ namespace Mono.Http
 
 	class NtlmClient : IAuthenticationModule
 	{
-		static Hashtable cache;
-
-		static NtlmClient () 
-		{
-			cache = new Hashtable ();
-		}
-	
-		public NtlmClient () {}
+		static readonly ConditionalWeakTable<HttpWebRequest, NtlmSession> cache =
+			new ConditionalWeakTable<HttpWebRequest, NtlmSession> ();
 	
 		public Authorization Authenticate (string challenge, WebRequest webRequest, ICredentials credentials) 
 		{
@@ -153,12 +148,7 @@ namespace Mono.Http
 				return null;
 
 			lock (cache) {
-				NtlmSession ds = (NtlmSession) cache [request];
-				if (ds == null) {
-					ds = new NtlmSession ();
-					cache.Add (request, ds);
-				}
-
+				var ds = cache.GetOrCreateValue (request);
 				return ds.Authenticate (header, webRequest, credentials);
 			}
 		}

commit b9015716470568e39b56cb4384d8799ae80f98c5

[system] Avoid reflection use to create NtlmSession. Fixes #37732
commit d2325f1483cd642457a7b2efe402e3c1d414d69d (only since 4.3.2, not
4.3.1) caused the failure when an application is linked.

The use of `GetOrCreateValue` uses reflection to create an `NtlmSession`
instance so the linker won't see it as used anymore (and removes it).

Fixed by avoiding reflection (which will also perform better).

https://bugzilla.xamarin.com/show_bug.cgi?id=37732

mcs/class/System/Mono.Http/NtlmClient.cs
@@ -148,7 +148,7 @@ namespace Mono.Http
 				return null;
 
 			lock (cache) {
-				var ds = cache.GetOrCreateValue (request);
+				var ds = cache.GetValue (request, x => new NtlmSession ());
 				return ds.Authenticate (header, webRequest, credentials);
 			}
 		}