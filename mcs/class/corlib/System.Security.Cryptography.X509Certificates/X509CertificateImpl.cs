a9fc9c628e6cee8a81b42100d9a5765b0e19749d
Martin Baulig <martin.baulig@xamarin.com>
2016-03-15 19:33:11 -0400

    [corlib]: Cleanup the internal 'X509CertificateImpl' API.

--- mcs/class/corlib/System.Security.Cryptography.X509Certificates/X509CertificateImpl.cs
+++ mcs/class/corlib/System.Security.Cryptography.X509Certificates/X509CertificateImpl.cs
@@ -45,17 +45,15 @@ namespace System.Security.Cryptography.X509Certificates
 
 		public abstract X509CertificateImpl Clone ();
 
-		public abstract string GetSubjectSummary ();
-
 		public abstract string GetIssuerName (bool legacyV1Mode);
 
 		public abstract string GetSubjectName (bool legacyV1Mode);
 
 		public abstract byte[] GetRawCertData ();
 
-		public abstract DateTime GetEffectiveDateString ();
+		public abstract DateTime GetValidFrom ();
 
-		public abstract DateTime GetExpirationDateString ();
+		public abstract DateTime GetValidUntil ();
 
 		byte[] cachedCertificateHash;
 
