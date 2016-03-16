583567dc94b5c8337eb6b07beb05206874eb7458
Alexander Köplinger <alex.koeplinger@outlook.com>
2016-03-16 22:15:15 +0100

    [acceptance-test] Ensure the exit code of the main test script is used
    Due to our usage of chained commands it'd use the exit code of the last command,
    which is not what we want. We use chaining because e.g. in the ms-test-suite
    case we still want to run the systemruntimebringup suite even if the conformance
    suite had a failing test.

--- acceptance-tests/roslyn.mk
+++ acceptance-tests/roslyn.mk
@@ -19,5 +19,6 @@ check-roslyn:
 	cd $(ROSLYN_PATH); \
 	sed -i -e 'N; s/bootstrapArg=".*\n.*"/bootstrapArg=""/g' cibuild.sh; \
 	sed -i -e 's#-xml Binaries/\$$BUILD_CONFIGURATION/xUnitResults/#-nunit $(abs_top_builddir)/acceptance-tests/TestResult-#g' cibuild.sh; \
-	./cibuild.sh --mono-path $$PREFIX/bin; \
-	sed -i -e 's/\\4.5"/\\4.5-api"/g' $$PREFIX/lib/mono/xbuild-frameworks/.NETFramework/v4.5/RedistList/FrameworkList.xml;
+	./cibuild.sh --mono-path $$PREFIX/bin || EXIT_CODE=1; \
+	sed -i -e 's/\\4.5"/\\4.5-api"/g' $$PREFIX/lib/mono/xbuild-frameworks/.NETFramework/v4.5/RedistList/FrameworkList.xml; \
+	exit $$EXIT_CODE
