680321339db47c8458c5e4697309b17d972b19ec
Marek Safar <marek.safar@gmail.com>
2016-04-04 16:40:22 +0200

    [System.Runtime.Serialization.dll] Update referencesource strings

--- mcs/class/System.Runtime.Serialization/ReferenceSources/SR.missing.cs
+++ mcs/class/System.Runtime.Serialization/ReferenceSources/SR.missing.cs
@@ -1,7 +1,28 @@
+using System.Globalization;
+
 namespace System.Runtime.Serialization
 {
 	static partial class SR
 	{
+		internal static string GetString(string name, params object[] args)
+		{
+			return GetString (CultureInfo.InvariantCulture, name, args);
+		}
+
+		internal static string GetString(CultureInfo culture, string name, params object[] args)
+		{
+			return string.Format (culture, name, args);
+		}
+
+		internal static string GetString(string name)
+		{
+			return name;
+		}
+
+		internal static string GetString(CultureInfo culture, string name)
+		{
+			return name;
+		}
 
 #region MissingInStrings.txt
 
