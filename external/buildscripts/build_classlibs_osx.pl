121a015373a0b3502559cf36eedeaac916a9eebe
Mike Voorhees <michaelv@unity3d.com>
2016-05-02 09:33:24 -0400

    Port building of autoconf,automake,libtool

--- external/buildscripts/build_classlibs_osx.pl
+++ external/buildscripts/build_classlibs_osx.pl
@@ -17,6 +17,7 @@ my $monoprefix = "$root/tmp/monoprefix";
 my $xcodePath = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform';
 my $macversion = '10.5';
 my $sdkversion = '10.6';
+my $externalBuildDeps = "$root/external/mono-build-deps";
 
 my $dependencyBranchToUse = "unity3.0";
 
@@ -78,6 +79,67 @@ if (not $skipbuild)
 		chdir("$root/eglib") eq 1 or die ("Failed chdir 1");
 		system("make","clean");
 	}
+
+	if ($externalBuildDeps ne "")
+	{
+		print "\n";
+		print ">>> Building autoconf, automake, and libtool if needed...\n";
+		my $autoconfVersion = "2.69";
+		my $automakeVersion = "1.15";
+		my $libtoolVersion = "2.4.6";
+		my $autoconfDir = "$externalBuildDeps/autoconf-$autoconfVersion";
+		my $automakeDir = "$externalBuildDeps/automake-$automakeVersion";
+		my $libtoolDir = "$externalBuildDeps/libtool-$libtoolVersion";
+
+		if (!(-d "$autoconfDir"))
+		{
+			chdir("$externalBuildDeps") eq 1 or die ("failed to chdir to external directory\n");
+			system("tar xzf autoconf-$autoconfVersion.tar.gz") eq 0  or die ("failed to extract autoconf\n");
+
+			chdir("$autoconfDir") eq 1 or die ("failed to chdir to autoconf directory\n");
+			system("./configure --prefix=$autoconfDir/tmp") eq 0 or die ("failed to configure autoconf\n");
+			system("make") eq 0 or die ("failed to make autoconf\n");
+			system("make install") eq 0 or die ("failed to make install autoconf\n");
+
+			chdir("$root") eq 1 or die ("failed to chdir to $root\n");
+		}
+
+		$ENV{PATH} = "$autoconfDir/tmp/bin:$ENV{PATH}";
+
+		if (!(-d "$automakeDir"))
+		{
+			chdir("$externalBuildDeps") eq 1 or die ("failed to chdir to external directory\n");
+			system("tar xzf automake-$automakeVersion.tar.gz") eq 0  or die ("failed to extract automake\n");
+
+			chdir("$automakeDir") eq 1 or die ("failed to chdir to automake directory\n");
+			system("./configure --prefix=$automakeDir/tmp") eq 0 or die ("failed to configure automake\n");
+			system("make") eq 0 or die ("failed to make automake\n");
+			system("make install") eq 0 or die ("failed to make install automake\n");
+
+			chdir("$root") eq 1 or die ("failed to chdir to $root\n");
+
+		}
+
+		if (!(-d "$libtoolDir"))
+		{
+			chdir("$externalBuildDeps") eq 1 or die ("failed to chdir to external directory\n");
+			system("tar xzf libtool-$libtoolVersion.tar.gz") eq 0  or die ("failed to extract libtool\n");
+		
+			chdir("$libtoolDir") eq 1 or die ("failed to chdir to libtool directory\n");
+			system("./configure --prefix=$libtoolDir/tmp") eq 0 or die ("failed to configure libtool\n");
+			system("make") eq 0 or die ("failed to make libtool\n");
+			system("make install") eq 0 or die ("failed to make install libtool\n");
+
+			chdir("$root") eq 1 or die ("failed to chdir to $root\n");
+		}
+
+		$ENV{PATH} = "$automakeDir/tmp/bin:$ENV{PATH}";
+		$ENV{PATH} = "$libtoolDir/tmp/bin:$ENV{PATH}";
+		$ENV{'LIBTOOLIZE'} = "$libtoolDir/tmp/bin/libtoolize";
+		$ENV{'LIBTOOL'} = "$libtoolDir/tmp/bin/libtool";
+	}
+
+
 	chdir("$root") eq 1 or die ("failed to chdir 2");
 	if ($cleanbuild)
 	{
