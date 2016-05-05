8b8d688fefcd5ad26cacacf23809f8b230244298
Martin Baulig <martin.baulig@xamarin.com>
2016-05-05 17:19:20 -0400

    [System]: Obsolete the Mono-specific cipher suite callbacks in ServicePointManager.
    (cherry picked from commit fa75386bb88de96d58487673885b969c9d784f30)

--- mcs/class/System/System.Net/ServicePointManager.extra.cs
+++ mcs/class/System/System.Net/ServicePointManager.extra.cs
@@ -12,12 +12,15 @@ using System.Collections.Generic;
 
 namespace System.Net {
 
+	[Obsolete]
 	public delegate IEnumerable<string> CipherSuitesCallback (SecurityProtocolType protocol, IEnumerable<string> allCiphers);
 
 	public partial class ServicePointManager {
 
+		[Obsolete ("", true)]
 		public static CipherSuitesCallback ClientCipherSuitesCallback { get; set; }
 
+		[Obsolete ("", true)]
 		public static CipherSuitesCallback ServerCipherSuitesCallback { get; set; }
 	}
 }
