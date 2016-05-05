932d9b31158521dadcfee2f4cf502e8095cbc910
Vlad Brezae <brezaevlad@gmail.com>
2016-05-06 00:51:48 +0300

    [sgen] Split protocol entry grep from argument parsing

---
+++ tools/sgen/sgen-grep-binprot.h
@@ -0,0 +1,3 @@
+void
+sgen_binary_protocol_grep_entries (EntryStream *stream, int num_nums, long nums [], int num_vtables, long vtables [],
+                        gboolean dump_all, gboolean pause_times, gboolean color_output, unsigned long long first_entry_to_consider);
