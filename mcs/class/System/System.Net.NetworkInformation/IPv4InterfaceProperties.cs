d3b0701f62b23b23ce05e5115d66deea14c69869
Marek Safar <marek.safar@gmail.com>
2016-05-17 12:00:24 +0200

    [System] Common NetworkInformation code from referencesource

--- mcs/class/System/System.Net.NetworkInformation/IPv4InterfaceProperties.cs
+++ mcs/class/System/System.Net.NetworkInformation/IPv4InterfaceProperties.cs
@@ -31,20 +31,6 @@ using System.IO;
 using System.Runtime.InteropServices;
 
 namespace System.Net.NetworkInformation {
-	public abstract class IPv4InterfaceProperties {
-		protected IPv4InterfaceProperties ()
-		{
-		}
-
-		public abstract int Index { get; }
-		public abstract bool IsAutomaticPrivateAddressingActive { get; }
-		public abstract bool IsAutomaticPrivateAddressingEnabled { get; }
-		public abstract bool IsDhcpEnabled { get; }
-		public abstract bool IsForwardingEnabled { get; }
-		public abstract int Mtu { get; }
-		public abstract bool UsesWins { get; }
-	}
-
 	abstract class UnixIPv4InterfaceProperties : IPv4InterfaceProperties
 	{
 		protected UnixNetworkInterface iface;
