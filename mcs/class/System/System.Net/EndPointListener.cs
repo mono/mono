mono/mono

commit 123aaa5db07fe464d12ea178ecc3f27990cc1a72

handles AwaitAsync finishing synchronously
A connect flood will break the async loop and take down the entire HttpListener instance (will stop accepting connections).

To reproduce. Take a HttpListener run say 2 wrk test instances against it (1000 connections each works here). After the run the HttpListener has broken its async loop and will no longer accept any connections after. The reason for this is that it was not previously handling the case that AcceptAsync can return synchronously. In this case event will not be called with args.

Patch handles this case and the above issue is not reproducible with.

mcs/class/System/System.Net/EndPointListener.cs
@@ -73,7 +73,7 @@ namespace System.Net {
 			SocketAsyncEventArgs args = new SocketAsyncEventArgs ();
 			args.UserToken = this;
 			args.Completed += OnAccept;
-			sock.AcceptAsync (args);
+			Accept (sock, args);
 			prefixes = new Hashtable ();
 			unregistered = new Dictionary<HttpConnection, HttpConnection> ();
 		}
@@ -82,28 +82,25 @@ namespace System.Net {
 			get { return listener; }
 		}
 
-		static void OnAccept (object sender, EventArgs e)
-		{
-			SocketAsyncEventArgs args = (SocketAsyncEventArgs) e;
-			EndPointListener epl = (EndPointListener) args.UserToken;
-			Socket accepted = null;
-			if (args.SocketError == SocketError.Success) {
-				accepted = args.AcceptSocket;
-				args.AcceptSocket = null;
+		static void Accept (Socket socket, SocketAsyncEventArgs e) {
+			e.AcceptSocket = null;
+			var asyn = socket.AcceptAsync(e);
+			if (!asyn) {
+				ProcessAccept(e);
 			}
+		}
 
-			try {
-				if (epl.sock != null)
-					epl.sock.AcceptAsync (args);
-			} catch {
-				if (accepted != null) {
-					try {
-						accepted.Close ();
-					} catch {}
-					accepted = null;
-				}
-			} 
 
+		static void ProcessAccept (SocketAsyncEventArgs args) 
+		{
+			Socket accepted = null;
+			if (args.SocketError == SocketError.Success)
+				accepted = args.AcceptSocket;
+
+			EndPointListener epl = (EndPointListener) args.UserToken;
+
+
+			Accept (epl.sock, args);
 			if (accepted == null)
 				return;
 
@@ -118,7 +115,12 @@ namespace System.Net {
 			conn.BeginReadRequest ();
 		}
 
-		internal void RemoveConnection (HttpConnection conn)
+		static void OnAccept (object sender, SocketAsyncEventArgs e) 
+		{
+			ProcessAccept (e);
+		}
+
+		internal void RemoveConnection (HttpConnection conn) 
 		{
 			lock (unregistered) {
 				unregistered.Remove (conn);

commit d9fc8f866ac1c2637350458f12e910f21fd8c4ca

[bcl] Avoid ObjectDisposedException in EndPointListener.
This fixes a regression introduced in 123aaa5db07fe464d12ea178ecc3f27990cc1a72.
We would sometimes see an ObjectDisposedException when calling AcceptAsync ()
because that patch removed the try-catch around it.

mcs/class/System/System.Net/EndPointListener.cs
@@ -73,7 +73,8 @@ namespace System.Net {
 			SocketAsyncEventArgs args = new SocketAsyncEventArgs ();
 			args.UserToken = this;
 			args.Completed += OnAccept;
-			Accept (sock, args);
+			Socket dummy = null;
+			Accept (sock, args, ref dummy);
 			prefixes = new Hashtable ();
 			unregistered = new Dictionary<HttpConnection, HttpConnection> ();
 		}
@@ -82,9 +83,21 @@ namespace System.Net {
 			get { return listener; }
 		}
 
-		static void Accept (Socket socket, SocketAsyncEventArgs e) {
+		static void Accept (Socket socket, SocketAsyncEventArgs e, ref Socket accepted) {
 			e.AcceptSocket = null;
-			var asyn = socket.AcceptAsync(e);
+			bool asyn;
+			try {
+				asyn = socket.AcceptAsync(e);
+			} catch {
+				if (accepted != null) {
+					try {
+						accepted.Close ();
+					} catch {
+					}
+					accepted = null;
+				}
+				return;
+			}
 			if (!asyn) {
 				ProcessAccept(e);
 			}
@@ -100,7 +113,7 @@ namespace System.Net {
 			EndPointListener epl = (EndPointListener) args.UserToken;
 
 
-			Accept (epl.sock, args);
+			Accept (epl.sock, args, ref accepted);
 			if (accepted == null)
 				return;
 