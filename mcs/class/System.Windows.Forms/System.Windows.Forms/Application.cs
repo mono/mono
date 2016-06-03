mono/mono

commit 5d54803af7af4c78cfeac020e0e2046b1282af91

SWF - Fixed exiting modal runloop. (#2980)
Fixes a bug in exiting modal runloop that caused not really exiting it
(not calling XplatUI.SetModal(..., false).

It caused issues with menu, for example, if you first ran a modal
dialog (a wizard), and then you started the application.

mcs/class/System.Windows.Forms/System.Windows.Forms/Application.cs
@@ -969,8 +969,8 @@ namespace System.Windows.Forms
 
 				EnableFormsForModalLoop (toplevels, context);
 				
-				if (context.MainForm != null && context.MainForm.IsHandleCreated) {
-					XplatUI.SetModal (context.MainForm.Handle, false);
+				if (old.MainForm != null && old.IsHandleCreated) {
+					XplatUI.SetModal (old.Handle, false);
 				}
 				#if DebugRunLoop
 					Console.WriteLine ("   Done with the SetModal");

commit 627709367794a53694497aea57aef66a8e088bcd

[WinForms] Fix build broken by 5d54803af7af4c78cfeac020e0e2046b1282af91

mcs/class/System.Windows.Forms/System.Windows.Forms/Application.cs
@@ -969,7 +969,7 @@ namespace System.Windows.Forms
 
 				EnableFormsForModalLoop (toplevels, context);
 				
-				if (old.MainForm != null && old.IsHandleCreated) {
+				if (old != null && old.IsHandleCreated) {
 					XplatUI.SetModal (old.Handle, false);
 				}
 				#if DebugRunLoop