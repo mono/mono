277aab686344a092176b4e083e27e7395d802f3e
Rodrigo Kumpera <kumpera@gmail.com>
2016-06-10 13:31:10 -0700

    [corlib] Move Assembly::GetName to be implemented in managed.
    This involves a few steps.
    
    First define MonoAssemblyName in RuntimeStructs.cs
    
    Then add some helped to decode blob arrays and handle hex digits.
    
    Finally, move the actual implementation from native to managed.

--- mcs/class/corlib/Mono/RuntimeStructs.cs
+++ mcs/class/corlib/Mono/RuntimeStructs.cs
@@ -45,5 +45,20 @@ namespace Mono {
 		}
 	}
 
+	//Maps to metadata-internals.h:: MonoAssemblyName
+	internal unsafe struct MonoAssemblyName
+	{
+		const int MONO_PUBLIC_KEY_TOKEN_LENGTH = 17;
+
+		internal IntPtr name;
+		internal IntPtr culture;
+		internal IntPtr hash_value;
+		internal IntPtr public_key;
+		internal fixed byte public_key_token [MONO_PUBLIC_KEY_TOKEN_LENGTH];
+		internal uint hash_alg;
+		internal uint hash_len;
+		internal uint flags;
+		internal ushort major, minor, build, revision;
+		internal ushort arch;
+	}
 }
-	
