5bf6df828c59ab373700f17dd19d17a359630c40
Alexander Köplinger <alex.koeplinger@outlook.com>
2016-06-20 13:00:54 +0200

    [ci] Add ms-test-suite step to default test runs

--- scripts/ci/run-test-default.sh
+++ scripts/ci/run-test-default.sh
@@ -79,4 +79,5 @@ ${TESTCMD} --label=Microsoft.Build.Engine-14 --timeout=60m make -w -C mcs/class/
 ${TESTCMD} --label=Microsoft.Build.Framework-14 --timeout=60m make -w -C mcs/class/Microsoft.Build.Framework run-test PROFILE=xbuild_14
 ${TESTCMD} --label=Microsoft.Build.Tasks-14 --timeout=60m make -w -C mcs/class/Microsoft.Build.Tasks run-test PROFILE=xbuild_14
 ${TESTCMD} --label=Microsoft.Build.Utilities-14 --timeout=60m make -w -C mcs/class/Microsoft.Build.Utilities run-test PROFILE=xbuild_14
+${TESTCMD} --label=ms-test-suite --timeout=15m make -w -C acceptance-tests check-ms-test-suite
 rm -fr /tmp/jenkins-temp-aspnet*
