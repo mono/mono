900fb81eeda298e7ff9d1d7225bf8c0af5426992
Marcos Henrich <marcos.henrich@xamarin.com>
2016-08-18 18:58:27 +0100

    [runtime] test_op_il_seq_point check indeterministic JIT compilations
    When mismatches are found between compilation with and without
    OP_IL_SEQ_POINTS, now the script checks it is not due to indeterministic
    JIT compilations.

--- mono/mini/test_op_il_seq_point.sh
+++ mono/mini/test_op_il_seq_point.sh
@@ -49,6 +49,20 @@ TMP_FILE2=$(tmp_file)
 echo "$(MONO_DEBUG=no-compact-seq-points,single-imm-size get_compile_dump $MONO_PATH $RUNTIME $TEST_FILE $USE_AOT)" >$TMP_FILE1
 echo "$(MONO_DEBUG=single-imm-size get_compile_dump $MONO_PATH $RUNTIME $TEST_FILE $USE_AOT)" >$TMP_FILE2
 
+LINE_DIFF_COUNT=$(diff -y --suppress-common-lines $TMP_FILE1 $TMP_FILE2 | grep '^' | wc -l)
+LINE_DIFF_COUNT_REF=0
+
+# Check if differences are due to JIT compiler beeing undeterminisc
+if [ $LINE_DIFF_COUNT != 0 ]; then
+	TMP_FILE1_REF=$(tmp_file)
+	echo "$(MONO_DEBUG=no-compact-seq-points,single-imm-size get_compile_dump $MONO_PATH $RUNTIME $TEST_FILE $USE_AOT)" >$TMP_FILE1_REF
+	LINE_DIFF_COUNT_REF=$(diff -y --suppress-common-lines $TMP_FILE1 $TMP_FILE1_REF| grep '^' | wc -l)
+	if [ $LINE_DIFF_COUNT_REF != 0 ]
+	then
+		$TMP_FILE2=$TMP_FILE1_REF
+	fi
+fi
+
 TMP_FILE=$(tmp_file)
 TMP_METHOD_DIFF_BAD=$(tmp_file)
 
@@ -76,39 +90,40 @@ awk \
 			break;
 		}
 	}
-}" $TMP_FILE > $TMP_METHOD_DIFF_BAD || (echo "awk failed" && echo "$(cat $TMP_METHOD_DIFF_BAD)" && exit 1)
+}" $TMP_FILE > $TMP_METHOD_DIFF_BAD || FAIL_MESSAGE_FILE=$TMP_METHOD_DIFF_BAD
+
+if [ $LINE_DIFF_COUNT_REF != 0 ]; then
+	FAIL_MESSAGE_FILE=$(tmp_file)
+	echo "Detected indeterministic JIT compilation on $TEST_FILE" >> $FAIL_MESSAGE_FILE
+	echo "  $LINE_DIFF_COUNT lines differ between compilations." >> $FAIL_MESSAGE_FILE
+	echo '  This is probably caused by a runtime optimization that is not deterministic' >> $FAIL_MESSAGE_FILE
+	echo '' >> $FAIL_MESSAGE_FILE
+	echo "$(cat $TMP_METHOD_DIFF_BAD)" >> $FAIL_MESSAGE_FILE
+elif [ $LINE_DIFF_COUNT != 0 ]; then
+	FAIL_MESSAGE_FILE=$(tmp_file)
+	echo "Detected OP_IL_SEQ_POINT incompatibility on $TEST_FILE" >> $FAIL_MESSAGE_FILE
+	echo "  $LINE_DIFF_COUNT lines differ when sequence points are enabled." >> $FAIL_MESSAGE_FILE
+	echo '  This is probably caused by a runtime optimization that is not handling OP_IL_SEQ_POINT' >> $FAIL_MESSAGE_FILE
+	echo '' >> $FAIL_MESSAGE_FILE
+	echo "Without IL_OP_SEQ_POINT                                                         With IL_OP_SEQ_POINT" >> $FAIL_MESSAGE_FILE
+	echo "$(cat $TMP_METHOD_DIFF_BAD)" >> $FAIL_MESSAGE_FILE
+fi
 
 TESTRESULT_FILE=TestResult-op_il_seq_point.tmp
-
 echo -n "              <test-case name=\"MonoTests.op_il_seq_point.${TEST_FILE}${USE_AOT}\" executed=\"True\" time=\"0\" asserts=\"0\" success=\"" >> $TESTRESULT_FILE
 
-LINE_DIFF_COUNT=$(diff -y --suppress-common-lines $TMP_FILE1 $TMP_FILE2 | grep '^' | wc -l)
-if [ $LINE_DIFF_COUNT != 0 ]
-then
+if [ $FAIL_MESSAGE_FILE ]; then
 	echo "False\">" >> $TESTRESULT_FILE
 	echo "                <failure>" >> $TESTRESULT_FILE
 	echo -n "                  <message><![CDATA[" >> $TESTRESULT_FILE
-	echo "Detected OP_IL_SEQ_POINT incompatibility on $TEST_FILE" >> $TESTRESULT_FILE
-	echo "  $LINE_DIFF_COUNT lines differ when sequence points are enabled." >> $TESTRESULT_FILE
-	echo '  This is probably caused by a runtime optimization that is not handling OP_IL_SEQ_POINT' >> $TESTRESULT_FILE
-	echo '' >> $TESTRESULT_FILE
-	echo "Without IL_OP_SEQ_POINT                                                         With IL_OP_SEQ_POINT" >> $TESTRESULT_FILE
-	echo "$(cat $TMP_METHOD_DIFF_BAD)" >> $TESTRESULT_FILE
+	echo "$(cat $FAIL_MESSAGE_FILE)" >> $TESTRESULT_FILE
 	echo "]]></message>" >> $TESTRESULT_FILE
 	echo "                  <stack-trace>" >> $TESTRESULT_FILE
 	echo "                  </stack-trace>" >> $TESTRESULT_FILE
 	echo "                </failure>" >> $TESTRESULT_FILE
 	echo "              </test-case>" >> $TESTRESULT_FILE
 
-	echo ''
-	echo "Detected OP_IL_SEQ_POINT incompatibility on $TEST_FILE"
-	echo "  $LINE_DIFF_COUNT lines differ when sequence points are enabled."
-	echo '  This is probably caused by a runtime optimization that is not handling OP_IL_SEQ_POINT'
-
-	echo ''
-	echo "Without IL_OP_SEQ_POINT                                                         With IL_OP_SEQ_POINT"
-	echo "$(cat $TMP_METHOD_DIFF_BAD)"
-
+	echo "$(cat $FAIL_MESSAGE_FILE)"
 	exit 1
 else
 	echo "True\" />" >> $TESTRESULT_FILE
