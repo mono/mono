06615fb9070dc240d9d6f9e8898ac74ada0ffe5b
Josh Peterson <petersonjm1@gmail.com>
2016-09-01 14:59:41 -0400

    Try to use an IPv4 address first for TCP remoting
    * Provide a better fix for case 826526, based on the .NET reference source.
    * In the .NET implementation of TcpServerChannel, if IPv4 is supported, the address for the host will use the first IPv4 address.
    * Even if the first address is an IPv6 address, it should be skipped, as the server side will expect and IPv4 address.
    * Change this implementation to look for an IPv4 address first, then fall back to whatever the first address is (IPv4 or IPv6) if things don't work out.

--- mcs/class/System.Runtime.Remoting/System.Runtime.Remoting.Channels.Tcp/TcpServerChannel.cs
+++ mcs/class/System.Runtime.Remoting/System.Runtime.Remoting.Channels.Tcp/TcpServerChannel.cs
@@ -74,18 +74,16 @@ namespace System.Runtime.Remoting.Channels.Tcp
 					else {
 						IPHostEntry he = Dns.Resolve (Dns.GetHostName());
 						if (he.AddressList.Length == 0) throw new RemotingException ("IP address could not be determined for this host");
-						host = he.AddressList [0].ToString ();
+						AddressFamily addressFamily = (Socket.SupportsIPv4) ? AddressFamily.InterNetwork : AddressFamily.InterNetworkV6;
+						IPAddress addr = GetMachineAddress (he, addressFamily);
+						if (addr != null)
+							host = addr.ToString ();
+						else
+							host = he.AddressList [0].ToString ();
 					}
 				}
 				else
 					host = Dns.GetHostByName(Dns.GetHostName()).HostName;
-			}
-
-			IPAddress hostAddress;
-			if (IPAddress.TryParse(host, out hostAddress))
-			{
-				if (hostAddress.AddressFamily == AddressFamily.InterNetworkV6)
-					host = "[" + host + "]";
 			}
 			
 			// Gets channel data from the chain of channel providers
@@ -282,6 +280,22 @@ namespace System.Runtime.Remoting.Channels.Tcp
 			threadPool.Free ();
 			server_thread.Join ();
 			server_thread = null;			
+		}
+
+		private static IPAddress GetMachineAddress (IPHostEntry host, AddressFamily addressFamily)
+		{
+			IPAddress result = null;
+			if (host != null) {
+				IPAddress[] addressList = host.AddressList;
+				for (int i = 0; i < addressList.Length; i++) {
+					if (addressList[i].AddressFamily == addressFamily) {
+						result = addressList[i];
+						break;
+					}
+				}
+			}
+
+			return result;
 		}
 	}
 
