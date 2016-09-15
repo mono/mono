28194f79feb88b3b282ad9ec640e7fd5b046f9aa
Marek Safar <marek.safar@gmail.com>
2016-09-15 13:36:35 +0200

    [mcs] Improve member lookup rules when type parameter has both non-object effective base class and interface constraint(s)

---
+++ mcs/errors/cs1070-3.cs
@@ -0,0 +1,11 @@
+// CS1070: The type `C' has been forwarded to an assembly that is not referenced. Consider adding a reference to assembly `CS1070-lib-missing, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'
+// Line: 5
+// Compiler options: -r:CS1070-lib.dll
+
+public class D
+{
+	static void Main ()
+	{
+		new C ();
+	}
+}
