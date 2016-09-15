5e245bed38a937ea836c3dc69ec6d95b34f72e7a
Marek Safar <marek.safar@gmail.com>
2016-09-15 17:22:23 +0200

    [corlib] Use simple BinaryCompatibility as anywhere else

---
+++ mcs/class/corlib/ReferenceSources/BinaryCompatibility.cs
@@ -0,0 +1,8 @@
+namespace System.Runtime.Versioning
+{
+	static class BinaryCompatibility
+	{
+		public static readonly bool TargetsAtLeast_Desktop_V4_5 = true;
+		public static readonly bool TargetsAtLeast_Desktop_V4_5_1 = true;
+	}
+}
