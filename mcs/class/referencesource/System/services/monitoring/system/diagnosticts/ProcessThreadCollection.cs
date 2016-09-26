1306fa1237b3e799c4d407410fdceb915b59c5d9
Marek Safar <marek.safar@gmail.com>
2016-09-26 16:56:19 +0200

    [System] Revert copyright change in 8e4a52a37499af9383dfdec5a67c81775d260899

--- mcs/class/referencesource/System/services/monitoring/system/diagnosticts/ProcessThreadCollection.cs
+++ mcs/class/referencesource/System/services/monitoring/system/diagnosticts/ProcessThreadCollection.cs
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// <copyright file="ProcessThread.cs" company="Microsoft">
+// <copyright file="ProcessThreadCollection.cs" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>                                                                
 //------------------------------------------------------------------------------
