bbc18d934f6930ce1e3b442359c7a7ded42ee5cb
Martin Baulig <martin.baulig@xamarin.com>
2016-09-29 23:13:44 +0200

    Remove all 'HAVE_BTLS' conditionals from managed code.

--- mcs/class/System/Mono.Net.Security/MonoTlsProviderFactory.Droid.cs
+++ mcs/class/System/Mono.Net.Security/MonoTlsProviderFactory.Droid.cs
@@ -2,9 +2,7 @@
 #if SECURITY_DEP
 using System;
 using MSI = Mono.Security.Interface;
-#if HAVE_BTLS
 using Mono.Btls;
-#endif
 
 namespace Mono.Net.Security
 {
@@ -20,13 +18,9 @@ namespace Mono.Net.Security
 			case "legacy":
 				return new LegacyTlsProvider ();
 			case "btls":
-#if HAVE_BTLS
 				if (!MonoBtlsProvider.IsSupported ())
 					throw new NotSupportedException ("BTLS in not supported!");
 				return new MonoBtlsProvider ();
-#else
-				throw new NotSupportedException ("BTLS in not supported!");
-#endif
 			default:
 				throw new NotSupportedException (string.Format ("Invalid TLS Provider: `{0}'.", provider));
 			}
