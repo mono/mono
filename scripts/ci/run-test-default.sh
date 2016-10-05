960ae16ebc00568cd585e9d5f3ff6777794afdc2
Alexander Köplinger <alex.koeplinger@outlook.com>
2016-10-05 13:13:10 +0200

    [ci] Correctly set the MONO_TLS_PROVIDER env var

--- scripts/ci/run-test-default.sh
+++ scripts/ci/run-test-default.sh
@@ -9,10 +9,12 @@ ${TESTCMD} --label=verify --timeout=15m make -w -C runtime mcs-compileall
 ${TESTCMD} --label=profiler --timeout=30m make -w -C mono/profiler -k check
 ${TESTCMD} --label=compiler --timeout=30m make -w -C mcs/tests run-test
 ${TESTCMD} --label=compiler-errors --timeout=30m make -w -C mcs/errors run-test
-${TESTCMD} --label=System --timeout=10m MONO_TLS_PROVIDER=legacy make -w -C mcs/class/System run-test
+export MONO_TLS_PROVIDER=legacy
+${TESTCMD} --label=System --timeout=10m make -w -C mcs/class/System run-test
 if [[ ${label} == osx-* ]]
-then ${TESTCMD} --label=System-btls --timeout=10m MONO_TLS_PROVIDER=btls make -w -C mcs/class/System run-test
+then export MONO_TLS_PROVIDER=btls && ${TESTCMD} --label=System-btls --timeout=10m make -w -C mcs/class/System run-test
 fi
+unset MONO_TLS_PROVIDER
 ${TESTCMD} --label=System.XML --timeout=5m make -w -C mcs/class/System.XML run-test
 ${TESTCMD} --label=Mono.Security --timeout=5m make -w -C mcs/class/Mono.Security run-test
 ${TESTCMD} --label=System.Security --timeout=5m make -w -C mcs/class/System.Security run-test;
