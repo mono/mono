00a8ed722d3620601f02f9c2b19f2cac716779c8
Marek Safar <marek.safar@gmail.com>
2016-11-10 18:41:15 +0100

    [System.Xml] Add XElement array handling to serialization interpreter. Fixes #12571

--- mcs/class/System.Xml.Linq/Test/System.Xml.Linq/XElementTest.cs
+++ mcs/class/System.Xml.Linq/Test/System.Xml.Linq/XElementTest.cs
@@ -2099,6 +2099,27 @@ namespace MonoTests.System.Xml.Linq
 			Assert.AreEqual (xe.Content.ToString (), "<Data />", "#3");
 		}
 
+		[XmlType ("Root")]
+		public class DeserializeXElementArray_Data
+		{
+			[XmlAnyElement]
+			public XElement[] Content;
+		}
+
+		[Test]
+		public void DeserializeXElementArray ()
+		{
+			var xmlString = "<Root><Data /></Root>";
+
+			var serializer = new XmlSerializer (typeof (DeserializeXElementArray_Data));
+			var res = serializer.Deserialize (new StringReader (xmlString));
+
+			Assert.IsNotNull (res, "#1");
+			Assert.AreEqual (typeof (DeserializeXElementArray_Data), res.GetType (), "#2");
+			var xe = (DeserializeXElementArray_Data)res;
+			Assert.AreEqual (xe.Content [0].ToString (), "<Data />", "#3");
+		}
+
 		[Test] // Bug #20151
 		public void XElementFromArrayWithNullValuesAsObject ()
 		{
