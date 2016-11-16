f4ecb2dd0622034b5412112c5e4d6344104c589f
Marek Safar <marek.safar@gmail.com>
2016-11-16 15:22:44 +0100

    [acceptance-tests] Specify nunitlite path

--- acceptance-tests/ms-test-suite.mk
+++ acceptance-tests/ms-test-suite.mk
@@ -1,6 +1,6 @@
 check-ms-test-suite:
 	@if $(MAKE) validate-ms-test-suite RESET_VERSIONS=1; then \
-		$(MAKE) -C $(MSTESTSUITE_PATH)/conformance build MCS="$(MCS) -debug -t:library -warn:1 -r:nunitlite.dll" && \
+		$(MAKE) -C $(MSTESTSUITE_PATH)/conformance build MCS="$(MCS) -debug -t:library -warn:1 -r:$(CLASS)/nunitlite.dll" && \
 		$(MAKE) -C $(MSTESTSUITE_PATH)/conformance run NUNIT-CONSOLE="$(RUNTIME) $(CLASS)/nunit-lite-console.exe -exclude=MonoBug,BadTest -format:nunit2" NUNIT_XML_RESULT="-result:$(abs_top_builddir)/acceptance-tests/TestResult-ms-test-suite-conformance.xml" || EXIT_CODE=1; \
 		$(MAKE) -C $(MSTESTSUITE_PATH)/systemruntimebringup build MCS="$(MCS) -debug -warn:1" && \
 		$(MAKE) -C $(MSTESTSUITE_PATH)/systemruntimebringup run MONO="$(RUNTIME)" || EXIT_CODE=1; \
