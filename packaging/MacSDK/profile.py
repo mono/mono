264e6c4b7cb0c42ff5b807cccf8a614593e888e2
Alexis Christoforides <alexis@thenull.net>
2016-11-21 17:40:00 -0500

    [MacSDK] Fix typo

--- packaging/MacSDK/profile.py
+++ packaging/MacSDK/profile.py
@@ -201,7 +201,7 @@ class MonoReleaseProfile(DarwinProfile):
         print "Setting up temporary package directory:", tmpdir
 
         # setup metadata
-        self.packaging_dir = os.path.join(self.resource_path, "packaging")
+        self.packaging_dir = os.path.join(self.path, "packaging")
         run_shell('rsync -aPq %s/* %s' % (self.packaging_dir, tmpdir), False)
 
         packages_list = string.join(
