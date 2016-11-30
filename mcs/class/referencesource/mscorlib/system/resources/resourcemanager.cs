mono/mono

commit 47a6f56439c9deab287be770c902d41e91480799

[bcl] Clean up some warnings

mcs/class/referencesource/mscorlib/system/resources/resourcemanager.cs
@@ -501,7 +501,9 @@ namespace System.Resources {
         [System.Security.SecuritySafeCritical]
         private void CommonAssemblyInit()
         {
+#if FEATURE_APPX            
             if (_bUsingModernResourceManagement == false)
+#endif
             {
                 UseManifest = true;
         
@@ -516,7 +518,7 @@ namespace System.Resources {
 
             _neutralResourcesCulture = ManifestBasedResourceGroveler.GetNeutralResourcesLanguage(MainAssembly, ref _fallbackLoc);
 
-#if !FEATURE_CORECLR   // PAL doesn't support eventing, and we don't compile event providers for coreclr
+#if !FEATURE_CORECLR && FEATURE_APPX  // PAL doesn't support eventing, and we don't compile event providers for coreclr
             if (_bUsingModernResourceManagement == false)
             {
 #if !MONO
@@ -1020,12 +1022,10 @@ namespace System.Resources {
             Type WinRTResourceManagerType = Type.GetType("System.Resources.WindowsRuntimeResourceManager, " + AssemblyRef.SystemRuntimeWindowsRuntime, true);
             return (WindowsRuntimeResourceManagerBase)Activator.CreateInstance(WinRTResourceManagerType, true);
         }
-#endif
 
         [NonSerialized]
         private bool _bUsingModernResourceManagement; // Written only by SetAppXConfiguration
 
-#if FEATURE_APPX
         [NonSerialized]
         [SecurityCritical]
         private WindowsRuntimeResourceManagerBase _WinRTResourceManager; // Written only by SetAppXConfiguration
@@ -1094,8 +1094,8 @@ namespace System.Resources {
 
         private void SetAppXConfiguration()
         {
+#if FEATURE_APPX            
             Contract.Assert(_bUsingModernResourceManagement == false); // Only this function writes to this member
-#if FEATURE_APPX
             Contract.Assert(_WinRTResourceManager == null); // Only this function writes to this member
             Contract.Assert(_PRIonAppXInitialized == false); // Only this function writes to this member
             Contract.Assert(_PRIExceptionInfo == null); // Only this function writes to this member

commit 1037af0e60efd0bd3be4edcce39e9ae725ff7e7f

[referencesource] Fix a regression from 47a6f56439c9deab287be770c902d41e91480799
ResourceSets needs to be initialized on Mono.

mcs/class/referencesource/mscorlib/system/resources/resourcemanager.cs
@@ -521,7 +521,6 @@ namespace System.Resources {
 #if !FEATURE_CORECLR && FEATURE_APPX  // PAL doesn't support eventing, and we don't compile event providers for coreclr
             if (_bUsingModernResourceManagement == false)
             {
-#if !MONO
                 if (FrameworkEventSource.IsInitialized && FrameworkEventSource.Log.IsEnabled()) {
                     CultureInfo culture = CultureInfo.InvariantCulture;
                     String defaultResName = GetResourceFileName(culture);
@@ -536,11 +535,13 @@ namespace System.Resources {
                         FrameworkEventSource.Log.ResourceManagerNeutralResourcesNotFound(BaseNameField, MainAssembly, outputResName);
                     }
                 }
-#endif
 #pragma warning disable 618
                 ResourceSets = new Hashtable(); // for backward compatibility
 #pragma warning restore 618
             }
+#endif
+#if MONO
+            ResourceSets = new Hashtable(); // for backward compatibility
 #endif
         }
 