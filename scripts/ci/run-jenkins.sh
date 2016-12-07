01eadce0984f5981f427ff96916a760d1478880f
Ludovic Henry <ludovic@xamarin.com>
2016-12-07 16:35:54 -0500

    [jenkins] Add `bitcode` CI tag and rename `bitcode_aot_only` runtime preset to `bitcode`

--- scripts/ci/run-jenkins.sh
+++ scripts/ci/run-jenkins.sh
@@ -40,6 +40,9 @@ elif [[ ${CI_TAGS} == *'aot_llvm'* ]];
 elif [[ ${CI_TAGS} == *'aot'* ]];
     then
     EXTRA_CONF_FLAGS="${EXTRA_CONF_FLAGS} --with-runtime_preset=aot";
+elif [[ ${CI_TAGS} == *'bitcode'* ]];
+    then
+    EXTRA_CONF_FLAGS="${EXTRA_CONF_FLAGS} --with-runtime_preset=bitcode";
 elif [[ ${CI_TAGS} == *'acceptance-tests'* ]];
     then
     EXTRA_CONF_FLAGS="${EXTRA_CONF_FLAGS} --prefix=${WORKSPACE}/tmp/mono-acceptance-tests --with-sgen-default-concurrent=yes";
