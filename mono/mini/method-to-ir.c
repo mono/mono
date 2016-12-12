dotnet/runtime

[runtime] Make sure Monitor.Enter () is always inlined since it consists only of a call to an icall.
Commit migrated from https://github.com/mono/mono/commit/534bb6eaa00ad79f361197f15b6b3d2feccf8c06

src/mono/mono/mini/method-to-ir.c
@@ -6916,7 +6916,7 @@ inline_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsig,
 	cfg->disable_inline = prev_disable_inline;
 	cfg->inline_depth --;
 
-	if ((costs >= 0 && costs < 60) || inline_always) {
+	if ((costs >= 0 && costs < 60) || inline_always || (costs >= 0 && (cmethod->iflags & METHOD_IMPL_ATTRIBUTE_AGGRESSIVE_INLINING))) {
 		if (cfg->verbose_level > 2)
 			printf ("INLINE END %s -> %s\n", mono_method_full_name (cfg->method, TRUE), mono_method_full_name (cmethod, TRUE));
 		

mono/mono

(cherry picked from commit 534bb6eaa00ad79f361197f15b6b3d2feccf8c06)
# Conflicts:
#	mcs/class/corlib/System.Threading/Monitor.cs
#	mono/mini/method-to-ir.c

mono/mini/method-to-ir.c
@@ -4451,7 +4451,7 @@ inline_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsig,
 	cfg->ret_var_set = prev_ret_var_set;
 	cfg->inline_depth --;
 
-	if ((costs >= 0 && costs < 60) || inline_allways) {
+	if ((costs >= 0 && costs < 60) || inline_allways || (costs >= 0 && (cmethod->iflags & METHOD_IMPL_ATTRIBUTE_AGGRESSIVE_INLINING))) {
 		if (cfg->verbose_level > 2)
 			printf ("INLINE END %s -> %s\n", mono_method_full_name (cfg->method, TRUE), mono_method_full_name (cmethod, TRUE));
 		