d2b161ab7bbfd60423912bb219a04b40a8d04375
Marek Safar <marek.safar@gmail.com>
2016-12-20 10:18:56 +0100

    [Facades] Removes duplicate signing info

--- mcs/class/Facades/netstandard/AssemblyInfo.cs
+++ mcs/class/Facades/netstandard/AssemblyInfo.cs
@@ -33,5 +33,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion ("2.0.0.0")]
 [assembly: AssemblyInformationalVersion ("2.0.0.0")]
 [assembly: AssemblyFileVersion ("2.0.0.0")]
-[assembly: AssemblyDelaySign (true)]
-[assembly: AssemblyKeyFile ("../../msfinal.pub")]
