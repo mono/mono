a85ce46c6b1cb77e80ffe087cc28527d9c6638b6
Marek Safar <marek.safar@gmail.com>
2016-12-20 18:49:55 +0100

    [System.Core] LINQ from corefx

--- mcs/class/System.Core/ReferenceSources/SR.missing.cs
+++ mcs/class/System.Core/ReferenceSources/SR.missing.cs
@@ -28,30 +28,6 @@
 
 partial class SR
 {
-	public const string MoreThanOneMatch = "MoreThanOneMatch";
-	public const string NoElements = "NoElements";
-	public const string PLINQ_CommonEnumerator_Current_NotStarted = "PLINQ_CommonEnumerator_Current_NotStarted";
-	public const string PLINQ_DisposeRequested = "PLINQ_DisposeRequested";
-	public const string PLINQ_EnumerationPreviouslyFailed = "PLINQ_EnumerationPreviouslyFailed";
-	public const string PLINQ_ExternalCancellationRequested = "PLINQ_ExternalCancellationRequested";
-	public const string ParallelEnumerable_BinaryOpMustUseAsParallel = "ParallelEnumerable_BinaryOpMustUseAsParallel";
-	public const string ParallelEnumerable_WithCancellation_TokenSourceDisposed = "ParallelEnumerable_WithCancellation_TokenSourceDisposed";
-	public const string ParallelEnumerable_WithMergeOptions_InvalidOptions = "ParallelEnumerable_WithMergeOptions_InvalidOptions";
-	public const string ParallelEnumerable_WithQueryExecutionMode_InvalidMode = "ParallelEnumerable_WithQueryExecutionMode_InvalidMode";
-	public const string ParallelPartitionable_IncorretElementCount = "ParallelPartitionable_IncorretElementCount";
-	public const string ParallelPartitionable_NullElement = "ParallelPartitionable_NullElement";
-	public const string ParallelPartitionable_NullReturn = "ParallelPartitionable_NullReturn";
-	public const string ParallelQuery_DuplicateDOP = "ParallelQuery_DuplicateDOP";
-	public const string ParallelQuery_DuplicateExecutionMode = "The WithExecutionMode operator may be used at most once in a query.";
-	public const string ParallelQuery_DuplicateMergeOptions = "ParallelQuery_DuplicateMergeOptions";
-	public const string ParallelQuery_DuplicateTaskScheduler = "ParallelQuery_DuplicateTaskScheduler";
-	public const string ParallelQuery_DuplicateWithCancellation = "ParallelQuery_DuplicateWithCancellation";
-	public const string ParallelQuery_InvalidAsOrderedCall = "AsOrdered may only be called on the result of AsParallel, ParallelEnumerable.Range, or ParallelEnumerable.Repeat.";
-	public const string ParallelQuery_InvalidNonGenericAsOrderedCall = "ParallelQuery_InvalidNonGenericAsOrderedCall";
-	public const string ParallelQuery_PartitionerNotOrderable = "ParallelQuery_PartitionerNotOrderable";
-	public const string PartitionerQueryOperator_NullPartition = "PartitionerQueryOperator_NullPartition";
-	public const string PartitionerQueryOperator_NullPartitionList = "PartitionerQueryOperator_NullPartitionList";
-	public const string PartitionerQueryOperator_WrongNumberOfPartitions = "PartitionerQueryOperator_WrongNumberOfPartitions";
 	public const string Cryptography_HashAlgorithmNameNullOrEmpty = "Cryptography_HashAlgorithmNameNullOrEmpty";
 	public const string NotSupported_SubclassOverride = "NotSupported_SubclassOverride";
 }
