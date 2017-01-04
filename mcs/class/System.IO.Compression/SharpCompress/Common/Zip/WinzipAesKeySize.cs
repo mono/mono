3f9b2dfd5fd9a3efaf492558a7df71647f15ab42
Marek Safar <marek.safar@gmail.com>
2017-01-04 15:32:53 +0100

    [System.IO.Compression] Replaced by corefx implementation

--- mcs/class/System.IO.Compression/SharpCompress/Common/Zip/WinzipAesKeySize.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharpCompress.Common.Zip
-{
-    internal enum WinzipAesKeySize
-    {
-        KeySize128 = 1,
-        KeySize192 = 2,
-        KeySize256 = 3,
-    }
-}
--- mcs/class/System.IO.Compression/SharpCompress/IO/StreamingMode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharpCompress.IO
-{
-    internal enum StreamingMode
-    {
-        Streaming,
-        Seekable,
-    }
-}
