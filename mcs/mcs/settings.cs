5c4f9d75282aa1bc696a772afe1fdb917c37bacb
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-01-04 23:45:27 +0100

    [mcs] Accept and ignore /debug:portable option
    We'll just generate a normal .mdb instead.
    
    Decided not to update the help text since
    technically we don't support generating a ppdb.

--- mcs/mcs/settings.cs
+++ mcs/mcs/settings.cs
@@ -934,7 +934,7 @@ namespace Mono.CSharp {
 				return ParseResult.Success;
 
 			case "/debug":
-				if (value.Equals ("full", StringComparison.OrdinalIgnoreCase) || value.Equals ("pdbonly", StringComparison.OrdinalIgnoreCase) || idx < 0) {
+				if (value.Equals ("full", StringComparison.OrdinalIgnoreCase) || value.Equals ("pdbonly", StringComparison.OrdinalIgnoreCase) || value.Equals ("portable", StringComparison.OrdinalIgnoreCase) || idx < 0) {
 					settings.GenerateDebugInfo = true;
 					return ParseResult.Success;
 				}
