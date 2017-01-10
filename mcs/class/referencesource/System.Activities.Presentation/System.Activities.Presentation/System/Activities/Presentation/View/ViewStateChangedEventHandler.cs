ca5104094c0ee0be6524e1a0b880b84da1180ffd
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-01-10 16:29:59 +0100

    [bcl] Remove System.Activities.Presentation folder from referencesource
    We won't import it into Mono anytime soon and the long paths
    cause issues when checking out Mono on Windows.

--- mcs/class/referencesource/System.Activities.Presentation/System.Activities.Presentation/System/Activities/Presentation/View/ViewStateChangedEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿//------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//------------------------------------------------------------
-
-namespace System.Activities.Presentation.View  
-{
-    public delegate void ViewStateChangedEventHandler(object sender, ViewStateChangedEventArgs e);
-}
