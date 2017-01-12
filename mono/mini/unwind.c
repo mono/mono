mono/mono

commit 3baad19c6c34891fa04c59d9673cbbd7c91e5ac2

[unwind] reduce frame size of mono_unwind_frame ()
on arm32 it goes down from 5120 bytes to 676 bytes in terms of frame
size.

mono/mini/unwind.c
@@ -46,19 +46,19 @@ static int unwind_info_size;
 
 #ifdef TARGET_AMD64
 static int map_hw_reg_to_dwarf_reg [] = { 0, 2, 1, 3, 7, 6, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-#define NUM_REGS AMD64_NREG
+#define NUM_DWARF_REGS AMD64_NREG
 #define DWARF_DATA_ALIGN (-8)
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (AMD64_RIP))
 #elif defined(TARGET_ARM)
 // http://infocenter.arm.com/help/topic/com.arm.doc.ihi0040a/IHI0040A_aadwarf.pdf
 /* Assign d8..d15 to hregs 16..24 (dwarf regs 264..271) */
 static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 264, 265, 266, 267, 268, 269, 270, 271 };
-#define NUM_REGS 272
+#define NUM_DWARF_REGS 272
 #define DWARF_DATA_ALIGN (-4)
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (ARMREG_LR))
 #define IS_DOUBLE_REG(dwarf_reg) (((dwarf_reg) >= 264) && ((dwarf_reg) <= 271))
 #elif defined(TARGET_ARM64)
-#define NUM_REGS 96
+#define NUM_DWARF_REGS 96
 #define DWARF_DATA_ALIGN (-8)
 /* LR */
 #define DWARF_PC_REG 30
@@ -75,7 +75,7 @@ static int map_hw_reg_to_dwarf_reg [] = {
  */
 static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 5, 4, 6, 7, 8 };
 /* + 1 is for IP */
-#define NUM_REGS X86_NREG + 1
+#define NUM_DWARF_REGS (X86_NREG + 1)
 #define DWARF_DATA_ALIGN (-4)
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (X86_NREG))
 #elif defined (TARGET_POWERPC)
@@ -84,12 +84,12 @@ static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8,
 										  9, 10, 11, 12, 13, 14, 15, 16,
 										  17, 18, 19, 20, 21, 22, 23, 24,
 										  25, 26, 27, 28, 29, 30, 31 };
-#define NUM_REGS 110
+#define NUM_DWARF_REGS 110
 #define DWARF_DATA_ALIGN (-(gint32)sizeof (mgreg_t))
 #define DWARF_PC_REG 108
 #elif defined (TARGET_S390X)
 static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-#define NUM_REGS 16
+#define NUM_DWARF_REGS 16
 #define DWARF_DATA_ALIGN (-8)
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (14))
 #elif defined (TARGET_MIPS)
@@ -100,15 +100,17 @@ static int map_hw_reg_to_dwarf_reg [32] = {
 	16, 17, 18, 19, 20, 21, 22, 23,
 	24, 25, 26, 27, 28, 29, 30, 31
 };
-#define NUM_REGS 32
+#define NUM_DWARF_REGS 32
 #define DWARF_DATA_ALIGN (-(gint32)sizeof (mgreg_t))
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (mips_ra))
 #else
 static int map_hw_reg_to_dwarf_reg [16];
-#define NUM_REGS 16
+#define NUM_DWARF_REGS 16
 #define DWARF_DATA_ALIGN 0
 #define DWARF_PC_REG -1
 #endif
+
+#define NUM_HW_REGS (sizeof (map_hw_reg_to_dwarf_reg) / sizeof (int))
 
 #ifndef IS_DOUBLE_REG
 #define IS_DOUBLE_REG(dwarf_reg) 0
@@ -116,7 +118,7 @@ static int map_hw_reg_to_dwarf_reg [16];
 
 static gboolean dwarf_reg_to_hw_reg_inited;
 
-static int map_dwarf_reg_to_hw_reg [NUM_REGS];
+static int map_dwarf_reg_to_hw_reg [NUM_DWARF_REGS];
 
 /*
  * mono_hw_reg_to_dwarf_reg:
@@ -130,10 +132,10 @@ mono_hw_reg_to_dwarf_reg (int reg)
 	if (reg == ppc_lr)
 		return 108;
 	else
-		g_assert (reg < NUM_REGS);
+		g_assert (reg < NUM_HW_REGS);
 #endif
 
-	if (NUM_REGS == 0) {
+	if (NUM_HW_REGS == 0) {
 		g_assert_not_reached ();
 		return -1;
 	} else {
@@ -146,8 +148,8 @@ init_reg_map (void)
 {
 	int i;
 
-	g_assert (NUM_REGS > 0);
-	for (i = 0; i < sizeof (map_hw_reg_to_dwarf_reg) / sizeof (int); ++i) {
+	g_assert (NUM_HW_REGS > 0);
+	for (i = 0; i < NUM_HW_REGS; ++i) {
 		map_dwarf_reg_to_hw_reg [mono_hw_reg_to_dwarf_reg (i)] = i;
 	}
 
@@ -479,8 +481,8 @@ print_dwarf_state (int cfa_reg, int cfa_offset, int ip, int nregs, Loc *location
 }
 
 typedef struct {
-	Loc locations [NUM_REGS];
-	guint8 reg_saved [NUM_REGS];
+	Loc locations [NUM_HW_REGS];
+	guint8 reg_saved [NUM_HW_REGS];
 	int cfa_reg, cfa_offset;
 } UnwindState;
 
@@ -501,9 +503,9 @@ mono_unwind_frame (guint8 *unwind_info, guint32 unwind_info_len,
 				   mgreg_t **save_locations, int save_locations_len,
 				   guint8 **out_cfa)
 {
-	Loc locations [NUM_REGS];
-	guint8 reg_saved [NUM_REGS];
-	int i, pos, reg, cfa_reg = -1, cfa_offset = 0, offset;
+	Loc locations [NUM_HW_REGS];
+	guint8 reg_saved [NUM_HW_REGS];
+	int pos, reg, hwreg, cfa_reg = -1, cfa_offset = 0, offset;
 	guint8 *p;
 	guint8 *cfa_val;
 	UnwindState state_stack [1];
@@ -528,11 +530,11 @@ mono_unwind_frame (guint8 *unwind_info, guint32 unwind_info_len,
 			p ++;
 			break;
 		case DW_CFA_offset:
-			reg = *p & 0x3f;
+			hwreg = mono_dwarf_reg_to_hw_reg (*p & 0x3f);
 			p ++;
-			reg_saved [reg] = TRUE;
-			locations [reg].loc_type = LOC_OFFSET;
-			locations [reg].offset = decode_uleb128 (p, &p) * DWARF_DATA_ALIGN;
+			reg_saved [hwreg] = TRUE;
+			locations [hwreg].loc_type = LOC_OFFSET;
+			locations [hwreg].offset = decode_uleb128 (p, &p) * DWARF_DATA_ALIGN;
 			break;
 		case 0: {
 			int ext_op = *p;
@@ -550,23 +552,25 @@ mono_unwind_frame (guint8 *unwind_info, guint32 unwind_info_len,
 				break;
 			case DW_CFA_offset_extended_sf:
 				reg = decode_uleb128 (p, &p);
+				hwreg = mono_dwarf_reg_to_hw_reg (reg);
 				offset = decode_sleb128 (p, &p);
-				g_assert (reg < NUM_REGS);
-				reg_saved [reg] = TRUE;
-				locations [reg].loc_type = LOC_OFFSET;
-				locations [reg].offset = offset * DWARF_DATA_ALIGN;
+				g_assert (reg < NUM_DWARF_REGS);
+				reg_saved [hwreg] = TRUE;
+				locations [hwreg].loc_type = LOC_OFFSET;
+				locations [hwreg].offset = offset * DWARF_DATA_ALIGN;
 				break;
 			case DW_CFA_offset_extended:
 				reg = decode_uleb128 (p, &p);
+				hwreg = mono_dwarf_reg_to_hw_reg (reg);
 				offset = decode_uleb128 (p, &p);
-				g_assert (reg < NUM_REGS);
-				reg_saved [reg] = TRUE;
-				locations [reg].loc_type = LOC_OFFSET;
-				locations [reg].offset = offset * DWARF_DATA_ALIGN;
+				g_assert (reg < NUM_DWARF_REGS);
+				reg_saved [hwreg] = TRUE;
+				locations [hwreg].loc_type = LOC_OFFSET;
+				locations [hwreg].offset = offset * DWARF_DATA_ALIGN;
 				break;
 			case DW_CFA_same_value:
-				reg = decode_uleb128 (p, &p);
-				locations [reg].loc_type = LOC_SAME;
+				hwreg = mono_dwarf_reg_to_hw_reg (decode_uleb128 (p, &p));
+				locations [hwreg].loc_type = LOC_SAME;
 				break;
 			case DW_CFA_advance_loc1:
 				pos += *p;
@@ -615,16 +619,16 @@ mono_unwind_frame (guint8 *unwind_info, guint32 unwind_info_len,
 
 	g_assert (cfa_reg != -1);
 	cfa_val = (guint8*)regs [mono_dwarf_reg_to_hw_reg (cfa_reg)] + cfa_offset;
-	for (i = 0; i < NUM_REGS; ++i) {
-		if (reg_saved [i] && locations [i].loc_type == LOC_OFFSET) {
-			int hreg = mono_dwarf_reg_to_hw_reg (i);
-			g_assert (hreg < nregs);
-			if (IS_DOUBLE_REG (i))
-				regs [hreg] = *(guint64*)(cfa_val + locations [i].offset);
+	for (hwreg = 0; hwreg < NUM_HW_REGS; ++hwreg) {
+		if (reg_saved [hwreg] && locations [hwreg].loc_type == LOC_OFFSET) {
+			int dwarfreg = mono_hw_reg_to_dwarf_reg (hwreg);
+			g_assert (hwreg < nregs);
+			if (IS_DOUBLE_REG (dwarfreg))
+				regs [hwreg] = *(guint64*)(cfa_val + locations [hwreg].offset);
 			else
-				regs [hreg] = *(mgreg_t*)(cfa_val + locations [i].offset);
-			if (save_locations && hreg < save_locations_len)
-				save_locations [hreg] = (mgreg_t*)(cfa_val + locations [i].offset);
+				regs [hwreg] = *(mgreg_t*)(cfa_val + locations [hwreg].offset);
+			if (save_locations && hwreg < save_locations_len)
+				save_locations [hwreg] = (mgreg_t*)(cfa_val + locations [hwreg].offset);
 		}
 	}
 

commit 109b68badd87a63b60c54fe4fcc3d31512d23cc0

[unwind] respect link register on ppc and use right dwarf register for PC depending on ELF spec version
fixes a regression introduced by 3baad19c6c34891fa04c59d9673cbbd7c91e5ac2

mono/mini/unwind.c
@@ -80,13 +80,17 @@ static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 5, 4, 6, 7, 8 };
 #define DWARF_PC_REG (mono_hw_reg_to_dwarf_reg (X86_NREG))
 #elif defined (TARGET_POWERPC)
 // http://refspecs.linuxfoundation.org/ELF/ppc64/PPC-elf64abi-1.9.html
-static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 
+static int map_hw_reg_to_dwarf_reg [ppc_lr + 1] = { 0, 1, 2, 3, 4, 5, 6, 7, 8,
 										  9, 10, 11, 12, 13, 14, 15, 16,
 										  17, 18, 19, 20, 21, 22, 23, 24,
 										  25, 26, 27, 28, 29, 30, 31 };
-#define NUM_DWARF_REGS 110
 #define DWARF_DATA_ALIGN (-(gint32)sizeof (mgreg_t))
+#if _CALL_ELF == 2
+#define DWARF_PC_REG 65
+#else
 #define DWARF_PC_REG 108
+#endif
+#define NUM_DWARF_REGS (DWARF_PC_REG + 1)
 #elif defined (TARGET_S390X)
 static int map_hw_reg_to_dwarf_reg [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 #define NUM_DWARF_REGS 16
@@ -117,8 +121,19 @@ static int map_hw_reg_to_dwarf_reg [16];
 #endif
 
 static gboolean dwarf_reg_to_hw_reg_inited;
+static gboolean hw_reg_to_dwarf_reg_inited;
 
 static int map_dwarf_reg_to_hw_reg [NUM_DWARF_REGS];
+
+static void
+init_hw_reg_map (void)
+{
+#ifdef TARGET_POWERPC
+	map_hw_reg_to_dwarf_reg [ppc_lr] = DWARF_PC_REG;
+#endif
+	mono_memory_barrier ();
+	hw_reg_to_dwarf_reg_inited = TRUE;
+}
 
 /*
  * mono_hw_reg_to_dwarf_reg:
@@ -128,12 +143,8 @@ static int map_dwarf_reg_to_hw_reg [NUM_DWARF_REGS];
 int
 mono_hw_reg_to_dwarf_reg (int reg)
 {
-#ifdef TARGET_POWERPC
-	if (reg == ppc_lr)
-		return 108;
-	else
-		g_assert (reg < NUM_HW_REGS);
-#endif
+	if (!hw_reg_to_dwarf_reg_inited)
+		init_hw_reg_map ();
 
 	if (NUM_HW_REGS == 0) {
 		g_assert_not_reached ();
@@ -144,7 +155,7 @@ mono_hw_reg_to_dwarf_reg (int reg)
 }
 
 static void
-init_reg_map (void)
+init_dwarf_reg_map (void)
 {
 	int i;
 
@@ -152,10 +163,6 @@ init_reg_map (void)
 	for (i = 0; i < NUM_HW_REGS; ++i) {
 		map_dwarf_reg_to_hw_reg [mono_hw_reg_to_dwarf_reg (i)] = i;
 	}
-
-#ifdef TARGET_POWERPC
-	map_dwarf_reg_to_hw_reg [DWARF_PC_REG] = ppc_lr;
-#endif
 
 	mono_memory_barrier ();
 	dwarf_reg_to_hw_reg_inited = TRUE;
@@ -165,7 +172,7 @@ int
 mono_dwarf_reg_to_hw_reg (int reg)
 {
 	if (!dwarf_reg_to_hw_reg_inited)
-		init_reg_map ();
+		init_dwarf_reg_map ();
 
 	return map_dwarf_reg_to_hw_reg [reg];
 }