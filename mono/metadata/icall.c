0de3c27f0ed38979615f07e9d5fd0d1265ab083e
Rodrigo Kumpera <kumpera@gmail.com>
2017-01-18 17:54:32 -0800

    [runtime] A pair of small fixes for the ignore assembly work.
    image.c (do_mono_image_load): Ignore refonly images when checking for ignorable assemblies.
    
    icall.c (ves_icall_System_Reflection_Assembly_InternalGetAssemblyName): Load the image in ref only mode.

--- mono/metadata/icall.c
+++ mono/metadata/icall.c
@@ -5423,7 +5423,7 @@ ves_icall_System_Reflection_Assembly_InternalGetAssemblyName (MonoString *fname,
 	replace_shadow_path (mono_domain_get (), dirname, &filename);
 	g_free (dirname);
 
-	image = mono_image_open (filename, &status);
+	image = mono_image_open_full (filename, &status, TRUE);
 
 	if (!image){
 		MonoException *exc;
