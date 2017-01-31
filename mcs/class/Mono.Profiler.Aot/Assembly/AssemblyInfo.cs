7d27f1bb4b946eeb60798d832f14d2bc10e903d7
Zoltan Varga <vargaz@gmail.com>
2017-01-31 11:37:48 -0500

    [aot] Add new Mono.Profiler.Aot assembly to read .aotprofile files.

---
+++ mcs/class/Mono.Profiler.Aot/Assembly/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion (Consts.FxVersion)]
+
+[assembly: AssemblyTitle("Mono.Profiler.Aot.dll")]
+[assembly: AssemblyDescription("Mono AOT profiler library")]
+
---
+++ mcs/class/Mono.Profiler.Aot/Mono.Profiler.Aot/ProfileData.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+
+namespace Mono.Profiler.Aot
+{
+	//
+	// Represents the contents of an .aotprofile file created by the
+	// AOT profiler
+	//
+public class ProfileRecord {
+
+	public ProfileRecord (int id) {
+		Id = id;
+	}
+
+	public int Id {
+		get; set;
+	}
+}
+
+public class ModuleRecord : ProfileRecord
+{
+	public ModuleRecord (int id, string name, string mvid) : base (id) {
+		Name = name;
+		Mvid = mvid;
+	}
+
+	public string Name {
+		get; set;
+	}
+
+	public string Mvid {
+		get; set;
+	}
+}
+
+public class GenericInstRecord : ProfileRecord
+{
+	public GenericInstRecord (int id, TypeRecord[] types) : base (id) {
+		Types = types;
+	}
+
+	public TypeRecord[] Types {
+		get; set;
+	}
+}
+
+public class TypeRecord : ProfileRecord
+{
+	public TypeRecord (int id, ModuleRecord module, string name, GenericInstRecord ginst) : base (id) {
+		Module = module;
+		Name = name;
+		GenericInst = ginst;
+	}
+
+	public ModuleRecord Module {
+		get; set;
+	}
+
+	public string Name {
+		get; set;
+	}
+
+	public GenericInstRecord GenericInst {
+		get; set;
+	}
+}
+
+public class MethodRecord : ProfileRecord
+{
+	public MethodRecord (int id, TypeRecord type, GenericInstRecord ginst, string name, string sig, int param_count) : base (id) {
+		Type = type;
+		GenericInst = ginst;
+		Name = name;
+		Signature = sig;
+		ParamCount = param_count;
+	}
+
+	public TypeRecord Type {
+		get; set;
+	}
+
+	public GenericInstRecord GenericInst {
+		get; set;
+	}
+
+	public string Name {
+		get; set;
+	}
+
+	public string Signature {
+		get; set;
+	}
+
+	public int ParamCount {
+		get; set;
+	}
+}
+
+public class ProfileData
+{
+	List<ModuleRecord> modules;
+	List<TypeRecord> types;
+	List<MethodRecord> methods;
+
+	internal ProfileData () {
+		modules = new List<ModuleRecord> ();
+		types = new List<TypeRecord> ();
+		methods = new List<MethodRecord> ();
+	}
+
+	public List<ModuleRecord> Modules {
+		get {
+			return modules;
+		}
+	}
+
+	public List<TypeRecord> Types {
+		get {
+			return types;
+		}
+	}
+
+	public List<MethodRecord> Methods {
+		get {
+			return methods;
+		}
+	}
+}
+}
+
