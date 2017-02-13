ac10f4e562ac5bf9768b4aa33b4745b3d6db4a86
Alexis Christoforides <alexis@thenull.net>
2017-02-13 07:14:09 -0500

    [MacSDK] Update F#

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -3,8 +3,8 @@ class FsharpPackage(GitHubTarballPackage):
     def __init__(self):
         GitHubTarballPackage.__init__(self,
                                       'fsharp', 'fsharp',
-                                      '4.0.1.9',
-                                      '0a6c66a8f18eb8a5c4d0bfac61d883b6994a918a',
+                                      '4.0.1.20',
+                                      '9bd7c2420e06c1597ef5a37b6cb6e0f8d2911b10',
                                       configure='./configure --prefix="%{package_prefix}"')
 
         self.extra_stage_files = [
