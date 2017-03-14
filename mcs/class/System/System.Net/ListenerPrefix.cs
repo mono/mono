0014f68478f6e08fbccc315c23f42218acf93370
Adrian Cheng <adrian@pixis.com.tw>
2017-03-14 19:08:48 +0800

    Fix Bug #40969

--- mcs/class/System/System.Net/ListenerPrefix.cs
+++ mcs/class/System/System.Net/ListenerPrefix.cs
@@ -86,76 +86,91 @@ namespace System.Net {
 			return original.GetHashCode ();
 		}
 
-		void Parse (string uri)
-		{
-			ushort default_port = 80;
-			if (uri.StartsWith ("https://")) {
-				default_port = 443;
-				secure = true;
-			}
+        void Parse(string uri)
+        {
+            ushort default_port = 80;
+            if (uri.StartsWith("https://"))
+            {
+                default_port = 443;
+                secure = true;
+            }
 
-			int length = uri.Length;
-			int start_host = uri.IndexOf (':') + 3;
-			if (start_host >= length)
-				throw new ArgumentException ("No host specified.");
+            int length = uri.Length;
+            int start_host = uri.IndexOf(':') + 3;
+            if (start_host >= length)
+                throw new ArgumentException("No host specified.");
 
-			int colon = uri.IndexOf (':', start_host, length - start_host);
-			int root;
-			if (colon > 0) {
-				host = uri.Substring (start_host, colon - start_host);
-				root = uri.IndexOf ('/', colon, length - colon);
-				port = (ushort) Int32.Parse (uri.Substring (colon + 1, root - colon - 1));
-				path = uri.Substring (root);
-			} else {
-				root = uri.IndexOf ('/', start_host, length - start_host);
-				host = uri.Substring (start_host, root - start_host);
-				port = default_port;
-				path = uri.Substring (root);
-			}
-			if (path.Length != 1)
-				path = path.Substring (0, path.Length - 1);
-		}
+            int startPort = uri.IndexOf(':', start_host, length - start_host);
+            if (uri[start_host] == '[')
+            {
+                startPort = uri.IndexOf("]:") + 1;
+            }
+            if (start_host == startPort)
+                throw new ArgumentException("No host specified.");
 
-		public static void CheckUri (string uri)
-		{
-			if (uri == null)
-				throw new ArgumentNullException ("uriPrefix");
+            int root = uri.IndexOf('/', start_host, length - start_host);
+            if (root == -1)
+                throw new ArgumentException("No path specified.");
 
-			if(!uri.StartsWith ("http://") && !uri.StartsWith ("https://"))
-				throw new ArgumentException ("Only 'http' and 'https' schemes are supported.");
+            if (startPort > 0)
+            {
+                host = uri.Substring(start_host, startPort - start_host).Trim('[', ']');
+                port = UInt16.Parse(uri.Substring(startPort + 1, root - startPort - 1));
+            }
+            else
+            {
+                host = uri.Substring(start_host, root - start_host).Trim('[', ']');
+                port = default_port;
+            }
+            path = uri.Substring(root);
 
-			int length = uri.Length;
-			int start_host = uri.IndexOf (':') + 3;
-			if (start_host >= length)
-				throw new ArgumentException ("No host specified.");
+            if (path.Length != 1)
+                path = path.Substring(0, path.Length - 1);
+        }
 
-			int colon = uri.IndexOf (':', start_host, length - start_host);
-			if (start_host == colon)
-				throw new ArgumentException ("No host specified.");
+        public static void CheckUri(string uri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException("uriPrefix");
 
-			int root;
-			if (colon > 0) {
-				root = uri.IndexOf ('/', colon, length - colon);
-				if (root == -1)
-					throw new ArgumentException ("No path specified.");
+            if (!uri.StartsWith("http://") && !uri.StartsWith("https://"))
+                throw new ArgumentException("Only 'http' and 'https' schemes are supported.");
 
-				try {
-					int p = Int32.Parse (uri.Substring (colon + 1, root - colon - 1));
-					if (p <= 0 || p >= 65536)
-						throw new Exception ();
-				} catch {
-					throw new ArgumentException ("Invalid port.");
-				}
-			} else {
-				root = uri.IndexOf ('/', start_host, length - start_host);
-				if (root == -1)
-					throw new ArgumentException ("No path specified.");
-			}
+            int length = uri.Length;
+            int start_host = uri.IndexOf(':') + 3;
+            if (start_host >= length)
+                throw new ArgumentException("No host specified.");
 
-			if (uri [uri.Length - 1] != '/')
-				throw new ArgumentException ("The prefix must end with '/'");
-		}
-	}
+
+            int startPort = uri.IndexOf(':', start_host, length - start_host);
+            if (uri[start_host] == '[')
+            {
+                startPort = uri.IndexOf("]:") + 1;
+            }
+            if (start_host == startPort)
+                throw new ArgumentException("No host specified.");
+            int root = uri.IndexOf('/', start_host, length - start_host);
+            if (root == -1)
+                throw new ArgumentException("No path specified.");
+
+            if (startPort > 0)
+            {
+                try
+                {
+                    int p = Int32.Parse(uri.Substring(startPort + 1, root - startPort - 1));
+                    if (p <= 0 || p >= 65536)
+                        throw new Exception();
+                }
+                catch
+                {
+                    throw new ArgumentException("Invalid port.");
+                }
+            }
+
+            if (uri[uri.Length - 1] != '/')
+                throw new ArgumentException("The prefix must end with '/'");
+        }
+    }
 }
 #endif
 
