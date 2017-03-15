fc662e3ffeb9c4d150a69507f1909fb5b906d651
Rodrigo Kumpera <kumpera@gmail.com>
2017-03-15 16:30:34 -0700

    [Mono.Posix] Add file that was on the xamarin-android repo so it can be built from mono.

--- mcs/class/Mono.Posix/Mono.Unix.Native/NativeConvert.cs
+++ mcs/class/Mono.Posix/Mono.Unix.Native/NativeConvert.cs
@@ -8,6 +8,9 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using Mono.Unix.Native;
+#if MONODROID
+using Mono.Unix.Android;
+#endif
 
 namespace Mono.Unix.Native {
 
@@ -17,7 +20,17 @@ namespace Mono.Unix.Native {
 		//
 		// Non-generated exports
 		//
-#if !MONODROID
+#if MONODROID
+		[DllImport (LIB, EntryPoint="Mono_Posix_FromRealTimeSignum")]
+		static extern int HelperFromRealTimeSignum (Int32 offset, out Int32 rval);
+
+		static int FromRealTimeSignum (Int32 offset, out Int32 rval)
+		{
+			if (!AndroidUtils.AreRealTimeSignalsSafe ())
+				throw new PlatformNotSupportedException ("Real-time signals are not supported on this Android architecture");
+			return HelperFromRealTimeSignum (offset, out rval);
+		}
+#else
 		[DllImport (LIB, EntryPoint="Mono_Posix_FromRealTimeSignum")]
 		private static extern int FromRealTimeSignum (Int32 offset, out Int32 rval);
 #endif
