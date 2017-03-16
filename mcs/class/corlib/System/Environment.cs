6cb0887d9e9e772c480d975168c72366bb626b6e
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-03-16 21:15:32 +0100

    Include Mono version in corlib version
    This makes it possible to version the runtime<->corlib interface
    for each version branch independently and fixes the problem
    of monolite getting overwritten by changes in different branches
    when corlib version is still the same.
    
    We're moving definition of the corlib version into configure.ac
    so we can more easily bake the Mono version into it.

--- mcs/class/corlib/System/Environment.cs
+++ mcs/class/corlib/System/Environment.cs
@@ -48,16 +48,11 @@ namespace System {
 	public static partial class Environment {
 
 		/*
-		 * This is the version number of the corlib-runtime interface. When
-		 * making changes to this interface (by changing the layout
-		 * of classes the runtime knows about, changing icall signature or
-		 * semantics etc), increment this variable. Also increment the
-		 * pair of this variable in the runtime in metadata/appdomain.c.
-		 * Changes which are already detected at runtime, like the addition
-		 * of icalls, do not require an increment.
+		 * This is the version number of the corlib-runtime interface.
+		 * It is defined in configure.ac.
 		 */
 #pragma warning disable 169
-		private const int mono_corlib_version = 164;
+		private const int mono_corlib_version = Consts.MonoCorlibVersion;
 #pragma warning restore 169
 
 		[ComVisible (true)]
