mono/mono

commit 399f6e2425d5fdf73c4f956e9e906613b55968d0

[runtime] Switch getenv to use heap memory
Getenv doesn't use heap memory, it uses static memory.
Mono's usage is consistent with a view of getenv memory contents
that are durable and can be stored and manipulated.

In a multithreaded context, getenv calls can race and corrupt
string buffers as they are read. In a single threaded context,
subsequent calls to getenv will lead to overwriting the memory
that pointers to previous calls point to.

In order to bring memory safety back, we duplicate memory and
manage it at each call-site.

Note: this changes the API semantics of g_getenv. This is necessary
because the locking around the getenv buffer is safest when confined to
the function in eglib.

mono/mini/mini.c
@@ -3113,7 +3113,7 @@ mini_method_compile (MonoMethod *method, guint32 opts, MonoDomain *domain, JitFl
 	gboolean llvm = (flags & JIT_FLAG_LLVM) ? 1 : 0;
 #endif
 	static gboolean verbose_method_inited;
-	static const char *verbose_method_name;
+	static char *verbose_method_name;
 
 	InterlockedIncrement (&mono_jit_stats.methods_compiled);
 	if (mono_profiler_get_events () & MONO_PROFILE_JIT_COMPILATION)
@@ -3405,6 +3405,7 @@ mini_method_compile (MonoMethod *method, guint32 opts, MonoDomain *domain, JitFl
 			if (strcmp (cfg->method->name, name) == 0)
 				cfg->verbose_level = 4;
 		}
+		g_free (verbose_method_name);
 	}
 
 	cfg->intvars = (guint16 *)mono_mempool_alloc0 (cfg->mempool, sizeof (guint16) * STACK_MAX * header->max_stack);

eglib/src/glib.h
@@ -160,7 +160,8 @@ typedef struct _GMemChunk GMemChunk;
  * Misc.
  */
 
-const gchar *    g_getenv(const gchar *variable);
+gboolean         g_hasenv(const gchar *variable);
+gchar *          g_getenv(const gchar *variable);
 gboolean         g_setenv(const gchar *variable, const gchar *value, gboolean overwrite);
 void             g_unsetenv(const gchar *variable);
 

eglib/src/gmisc-unix.c
@@ -39,23 +39,69 @@
 #include <unistd.h>
 #endif
 
+static pthread_mutex_t env_lock = PTHREAD_MUTEX_INITIALIZER;
 
-const gchar *
-g_getenv(const gchar *variable)
+/* MONO Comment
+ * 
+ * As per the UNIX spec, 
+ * "The return value from getenv() may point to static data which may be overwritten by subsequent calls to getenv(), setenv(), or unsetenv()."
+ * Source: Unix Manual Pages for getenv, IEEE Std 1003.1
+ *
+ * This means that using pointers returned from getenv may (and does) lead to many
+ * pointers which refer to the same piece of memory. When one is freed, all will be freed.
+ *
+ * This is unsafe and an ergonomics risk to fix in the callers. While the caller could lock,
+ * this introduces the risk for looping or exiting while inside of a lock. For this reason,
+ * g_getenv does not mimic the behavior of POSIX getenv anymore.
+ *
+ * The memory address returned will be unique to the invocaton, and must be freed.
+ * */ 
+gchar *
+g_getenv (const gchar *variable)
 {
-	return getenv(variable);
+	gchar *ret = NULL;
+	pthread_mutex_lock (&env_lock);
+	gchar *res = getenv(variable);
+	if (res)
+		ret = g_strdup(res);
+	pthread_mutex_unlock (&env_lock);
+
+	return ret;
+}
+
+/*
+ * This function checks if the given variable is non-NULL
+ * in the environment. It's useful because it removes memory
+ * freeing requirements.
+ *
+ */
+gboolean
+g_hasenv (const gchar *variable)
+{
+	pthread_mutex_lock (&env_lock);
+	gchar *res = getenv(variable);
+	gboolean not_null = (res != NULL);
+	pthread_mutex_unlock (&env_lock);
+
+	return not_null;
 }
 
 gboolean
 g_setenv(const gchar *variable, const gchar *value, gboolean overwrite)
 {
-	return setenv(variable, value, overwrite) == 0;
+	gboolean res;
+	pthread_mutex_lock (&env_lock);
+	res = (setenv(variable, value, overwrite) == 0);
+	pthread_mutex_unlock (&env_lock);
+	return res;
 }
 
 void
 g_unsetenv(const gchar *variable)
 {
+	pthread_mutex_lock (&env_lock);
 	unsetenv(variable);
+	pthread_mutex_unlock (&env_lock);
 }
 
 gchar*

eglib/src/gmisc-win32.c
@@ -39,6 +39,12 @@
 #include <io.h>
 #include <assert.h>
 
+gboolean
+g_hasenv (const gchar *variable)
+{
+	return g_getenv (variable) != NULL;
+}
+
 const gchar *
 g_getenv(const gchar *variable)
 {

mono/arch/ia64/codegen.c
@@ -52,11 +52,12 @@ mono_disassemble_code (guint8 *code, int size, char *id)
 	system (cmd); 
 	g_free (cmd);
 	if (!objdump_args)
-		objdump_args = "";
+		objdump_args = strdup("");
 	
 	cmd = g_strdup_printf (DIS_CMD " %s %s", objdump_args, o_file);
 	system (cmd);
 	g_free (cmd);
+	g_free (objdump_args);
 	
 	g_free (o_file);
 	g_free (as_file);

mono/metadata/appdomain.c
@@ -2210,8 +2210,9 @@ ves_icall_System_AppDomain_InternalUnload (gint32 domain_id, MonoError *error)
 	 * Unloading seems to cause problems when running NUnit/NAnt, hence
 	 * this workaround.
 	 */
-	if (g_getenv ("MONO_NO_UNLOAD"))
+	if (g_hasenv ("MONO_NO_UNLOAD"))
 		return;
+
 #ifdef __native_client__
 	return;
 #endif

mono/metadata/assembly.c
@@ -294,7 +294,7 @@ mono_set_assemblies_path (const char* path)
 	}
 	*dest = *splitted;
 
-	if (g_getenv ("MONO_DEBUG") == NULL)
+	if (g_hasenv ("MONO_DEBUG"))
 		return;
 
 	splitted = assemblies_path;
@@ -315,21 +315,25 @@ char* nacl_mono_path = NULL;
 static void
 check_path_env (void)
 {
-	const char* path;
-	path = g_getenv ("MONO_PATH");
+	if (assemblies_path != NULL)
+		return;
+
+	char* path = g_getenv ("MONO_PATH");
 #ifdef __native_client__
 	if (!path)
-		path = nacl_mono_path;
+		path = strdup (nacl_mono_path);
 #endif
-	if (!path || assemblies_path != NULL)
+	if (!path)
 		return;
 
 	mono_set_assemblies_path(path);
+	g_free (path);
 }
 
 static void
-check_extra_gac_path_env (void) {
-	const char *path;
+check_extra_gac_path_env (void) 
+{
+	char *path;
 	char **splitted, **dest;
 	
 	path = g_getenv ("MONO_GAC_PREFIX");
@@ -337,6 +341,8 @@ check_extra_gac_path_env (void) {
 		return;
 
 	splitted = g_strsplit (path, G_SEARCHPATH_SEPARATOR_S, 1000);
+	g_free (path);
+
 	if (extra_gac_paths)
 		g_strfreev (extra_gac_paths);
 	extra_gac_paths = dest = splitted;
@@ -347,7 +353,7 @@ check_extra_gac_path_env (void) {
 	}
 	*dest = *splitted;
 	
-	if (g_getenv ("MONO_DEBUG") == NULL)
+	if (!g_hasenv ("MONO_DEBUG"))
 		return;
 
 	while (*splitted) {

mono/metadata/boehm-gc.c
@@ -198,6 +198,7 @@ mono_gc_base_init (void)
 					log_finalizers = 1;
 				}
 			}
+			g_free (env);
 		}
 	}
 
@@ -241,6 +242,7 @@ mono_gc_base_init (void)
 				*/
 			}
 		}
+		g_free (env);
 		g_strfreev (opts);
 	}
 
@@ -1319,7 +1321,7 @@ mono_gc_is_moving (void)
 gboolean
 mono_gc_is_disabled (void)
 {
-	if (GC_dont_gc || g_getenv ("GC_DONT_GC"))
+	if (GC_dont_gc || g_hasenv ("GC_DONT_GC"))
 		return TRUE;
 	else
 		return FALSE;

commit 786f289e00c17f6fb9485e547505f76e8633089c

[jit] Fix a regression caused by 399f6e2425d5fdf73c4f956e9e906613b55968d0. Avoid a crash if MONO_VERBOS_METHOD is used.

mono/mini/mini.c
@@ -3406,7 +3406,6 @@ mini_method_compile (MonoMethod *method, guint32 opts, MonoDomain *domain, JitFl
 			if (strcmp (cfg->method->name, name) == 0)
 				cfg->verbose_level = 4;
 		}
-		g_free (verbose_method_name);
 	}
 
 	cfg->intvars = (guint16 *)mono_mempool_alloc0 (cfg->mempool, sizeof (guint16) * STACK_MAX * header->max_stack);