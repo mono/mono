3360f50a89933903ca80552ecf29d4ab717a29aa
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-04-18 18:28:15 -0400

    [System] Test for broken Socket.*EndPoint after BeginConnect
    https://bugzilla.xamarin.com/show_bug.cgi?id=42738

--- mcs/class/System/Test/System.Net.Sockets/SocketTest.cs
+++ mcs/class/System/Test/System.Net.Sockets/SocketTest.cs
@@ -4622,6 +4622,8 @@ namespace MonoTests.System.Net.Sockets
 				client.DualMode = true;
 				var ar1 = client.BeginConnect (ep, BCCallback, client);
 				Assert.IsTrue (BCCalledBack.WaitOne (10000), "#1");
+				Assert.AreEqual(server.AddressFamily, client.RemoteEndPoint.AddressFamily, "#2");
+				Assert.AreEqual(server.AddressFamily, client.LocalEndPoint.AddressFamily, "#3");
 				client.Disconnect (false);
 				client.Close ();
 
@@ -4629,7 +4631,9 @@ namespace MonoTests.System.Net.Sockets
 				client = new Socket (AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
 				client.DualMode = true;
 				var ar2 = client.BeginConnect (IPAddress.Loopback, ep.Port, BCCallback, client);
-				Assert.IsTrue (BCCalledBack.WaitOne (10000), "#2");
+				Assert.IsTrue (BCCalledBack.WaitOne (10000), "#4");
+				Assert.AreEqual(server.AddressFamily, client.RemoteEndPoint.AddressFamily, "#5");
+				Assert.AreEqual(server.AddressFamily, client.LocalEndPoint.AddressFamily, "#6");
 				client.Disconnect (false);
 				client.Close ();
 
@@ -4637,7 +4641,9 @@ namespace MonoTests.System.Net.Sockets
 				client = new Socket (AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
 				client.DualMode = true;
 				var ar3 = client.BeginConnect (new [] {IPAddress.Loopback}, ep.Port, BCCallback, client);
-				Assert.IsTrue (BCCalledBack.WaitOne (10000), "#2");
+				Assert.IsTrue (BCCalledBack.WaitOne (10000), "#7");
+				Assert.AreEqual(server.AddressFamily, client.RemoteEndPoint.AddressFamily, "#8");
+				Assert.AreEqual(server.AddressFamily, client.LocalEndPoint.AddressFamily, "#9");
 				client.Disconnect (false);
 				client.Close();
 			}
