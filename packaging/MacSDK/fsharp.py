06657dc387bc0c77a6fb2338b144ae2eb4b6a158
Alexis Christoforides <alexis@thenull.net>
2017-04-27 11:32:24 -0400

    [MacSDK] Update F# -> 4.1.8

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -1,14 +1,24 @@
 class FsharpPackage(GitHubTarballPackage):
 
     def __init__(self):
-        GitHubTarballPackage.__init__(self,
-                                      'fsharp', 'fsharp',
-                                      '4.0.1.20',
-                                      '9bd7c2420e06c1597ef5a37b6cb6e0f8d2911b10',
-                                      configure='./configure --prefix="%{package_prefix}"')
+        GitHubTarballPackage.__init__(
+            self,
+            'fsharp',
+            'fsharp',
+            '4.1.8',
+            '991186f6c95b30a80f217b9319354b32c86212de',
+            configure='./configure --prefix="%{package_prefix}"',
+            override_properties={
+                'make': 'make'})
 
         self.extra_stage_files = [
             'lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
+        self.sources.extend(
+            [
+                'patches/fsharp-enable-jit-tracking-for-portable-pdb.patch',
+                'patches/fsharp-fix-mdb-support.patch',
+                'patches/fsharp-Fix-mono-gac-location.patch',
+                'patches/fsharp-fix-xbuild-check.patch'])
 
     def prep(self):
         Package.prep(self)
