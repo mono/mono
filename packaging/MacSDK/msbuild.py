e523f243d6642dec1ebb6f67f1a1b782d271f4eb
Alexis Christoforides <alexis@thenull.net>
2017-05-03 15:56:47 -0400

    [MacSDK] Build msbuild for Release config

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -1,16 +1,14 @@
 import fileinput
 
-
 class MSBuild (GitHubPackage):
+	def __init__ (self):
+		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.0',
+			git_branch = 'xplat-master')
 
-    def __init__(self):
-        GitHubPackage.__init__(self, 'mono', 'msbuild', '15.0',
-                               git_branch='xplat-master')
+	def build (self):
+		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
 
-    def build(self):
-        self.sh('./cibuild.sh --scope Compile --target Mono --host Mono')
+	def install (self):
+                self.sh ('./install-mono-prefix.sh %s' % self.staged_prefix)
 
-    def install(self):
-        self.sh('./install-mono-prefix.sh %s' % self.staged_prefix)
-
-MSBuild()
+MSBuild ()
