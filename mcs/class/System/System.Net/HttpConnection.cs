2ce82742e89376874c8b6802760e25d3e41591ec
Martin Baulig <mabaul@microsoft.com>
2017-05-08 15:05:33 -0400

    Revert "[System]: Fix bug #52508 in HttpListener when doing multiple Https request."
    This reverts commit 81da99eb76a5ecc1567058811c80b4ae289bdcd6.

--- mcs/class/System/System.Net/HttpConnection.cs
+++ mcs/class/System/System.Net/HttpConnection.cs
@@ -97,8 +97,6 @@ namespace System.Net {
 				stream = ssl_stream.AuthenticatedStream;
 			}
 			timer = new Timer (OnTimeout, null, Timeout.Infinite, Timeout.Infinite);
-			if (ssl_stream != null)
-				ssl_stream.AuthenticateAsServer (cert, true, (SslProtocols)ServicePointManager.SecurityProtocol, false);
 			Init ();
 		}
 
@@ -112,6 +110,10 @@ namespace System.Net {
 
 		void Init ()
 		{
+			if (ssl_stream != null) {
+				ssl_stream.AuthenticateAsServer (cert, true, (SslProtocols)ServicePointManager.SecurityProtocol, false);
+			}
+
 			context_bound = false;
 			i_stream = null;
 			o_stream = null;
