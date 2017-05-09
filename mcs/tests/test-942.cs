f8c62963040b7d356d24b6460b506fcdf0814a74
Marek Safar <marek.safar@gmail.com>
2017-05-09 19:02:48 +0200

    [mcs] Conditional check on attribute type needs to check its base types too

---
+++ mcs/tests/test-942.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace ConditionalAttributeTesting
+{
+	class MainClass
+	{
+		public static int Main ()
+		{
+			return HelloWorld ();
+		}
+
+		[Some ("Test")]
+		public static int HelloWorld ()
+		{
+			var methodInfo = MethodBase.GetCurrentMethod ();
+			SomeAttribute someAttribute = Attribute.GetCustomAttribute (methodInfo, typeof (SomeAttribute)) as SomeAttribute;
+			if (someAttribute != null) {
+				return 1;
+			}
+
+			return 0;
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.All)]
+	[Conditional ("NOT_DEFINED")]
+	public abstract class BaseAttribute : Attribute
+	{
+	}
+
+	public class SomeAttribute : BaseAttribute
+	{
+		public SomeAttribute (string someText)
+		{
+		}
+	}
+}
