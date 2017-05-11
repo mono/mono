42618a674ab09bc4341518da5ffc757b31580500
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-05-12 01:36:05 +0200

    [Facades] Update with new typeforwarders/APIs (#4838)
    (cherry picked from commit 2324773fbb7ffa6e86fafb39c11ac724cc1ccd50)

--- mcs/class/Facades/System.ServiceModel.Primitives/AssemblyInfo.cs
+++ mcs/class/Facades/System.ServiceModel.Primitives/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany ("Xamarin, Inc.")]
 [assembly: AssemblyProduct ("Mono Common Language Infrastructure")]
 [assembly: AssemblyCopyright ("Copyright (c) 2013 Xamarin Inc. (http://www.xamarin.com)")]
-[assembly: AssemblyVersion ("4.1.1.0")]
+[assembly: AssemblyVersion ("4.2.0.0")]
 [assembly: AssemblyInformationalVersion ("4.0.0.0")]
 [assembly: AssemblyFileVersion ("4.0.0.0")]
