6d5f8d67a0a1190c8796f1acaeedf30b4610e8f5
Michael Oborne <mich146@hotmail.com>
2017-05-11 10:42:02 +0800

    SystemDrawingSection: create

---
+++ mcs/class/System.Drawing/System.Drawing.Configuration/SystemDrawingSection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+
+namespace System.Drawing.Configuration
+{
+    public sealed class SystemDrawingSection : ConfigurationSection
+    {
+        private const string BitmapSuffixSectionName = "bitmapSuffix";
+
+        private static readonly ConfigurationPropertyCollection properties;
+
+        private static readonly ConfigurationProperty bitmapSuffix;
+
+        [ConfigurationProperty("bitmapSuffix")]
+        public string BitmapSuffix
+        {
+            get
+            {
+                return (string)base[SystemDrawingSection.bitmapSuffix];
+            }
+            set
+            {
+                base[SystemDrawingSection.bitmapSuffix] = value;
+            }
+        }
+
+        protected override ConfigurationPropertyCollection Properties
+        {
+            get
+            {
+                return SystemDrawingSection.properties;
+            }
+        }
+
+        static SystemDrawingSection()
+        {
+            SystemDrawingSection.properties = new ConfigurationPropertyCollection();
+            SystemDrawingSection.bitmapSuffix = new ConfigurationProperty("bitmapSuffix", typeof(string), null, ConfigurationPropertyOptions.None);
+            SystemDrawingSection.properties.Add(SystemDrawingSection.bitmapSuffix);
+        }
+    }
+}
