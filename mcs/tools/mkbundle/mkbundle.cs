8ec530a2d55230bea203ec9b23afc52734901e02
Miguel de Icaza <miguel@gnome.org>
2017-05-24 09:34:09 -0400

    Improve error messages to help users diagnose problems when using the custom mode

--- mcs/tools/mkbundle/mkbundle.cs
+++ mcs/tools/mkbundle/mkbundle.cs
@@ -1110,6 +1110,14 @@ void          mono_register_config_for_assembly (const char* assembly_name, cons
 		}
 
 		if (error != null) {
+			Console.Error.WriteLine ("Failure to load i18n assemblies, the following diretories were searched for the assemblies:");
+			foreach (var path in link_paths){
+				Console.Error.WriteLine ("   Path: " + path);
+			}
+			if (custom_mode){
+				Console.WriteLine ("In Custom mode, you need to provide the directory to lookup assemblies from using -L");
+			}
+			
 			Error ("Couldn't load one or more of the i18n assemblies: " + error);
 			Environment.Exit (1);
 		}
@@ -1183,7 +1191,12 @@ void          mono_register_config_for_assembly (const char* assembly_name, cons
 		Assembly a = null;
 		
 		try {
+			if (!quiet)
+				Console.WriteLine ("Attempting to load assembly: {0}", assembly);
 			a = universe.LoadFile (assembly);
+			if (!quiet)
+				Console.WriteLine ("Assembly {0} loaded successfully.", assembly);
+			
 		} catch (FileNotFoundException){
 			Error ($"Cannot find assembly `{assembly}'");
 		} catch (IKVM.Reflection.BadImageFormatException f) {
@@ -1206,6 +1219,8 @@ void          mono_register_config_for_assembly (const char* assembly_name, cons
 		string total_log = "";
 		foreach (string dir in link_paths){
 			string full_path = Path.Combine (dir, assembly);
+			if (!quiet)
+				Console.WriteLine ("Attempting to load assembly from: " + full_path);
 			if (!assembly.EndsWith (".dll") && !assembly.EndsWith (".exe"))
 				full_path += ".dll";
 			
