c9f799086358244fbfaab001063fac0d68ba8cb6
Haryono <hharyono@culturedsw.com>
2017-05-30 09:51:49 +0700

    Fixing on WriteContentType when extension is empty.

--- mcs/class/WindowsBase/System.IO.Packaging/ZipPackage.cs
+++ mcs/class/WindowsBase/System.IO.Packaging/ZipPackage.cs
@@ -226,9 +226,14 @@ namespace System.IO.Packaging {
 				var extension = Path.GetExtension (part.Uri.OriginalString);
 				if (extension.Length > 0)
 					extension = extension.Substring (1);
-				
-				if (!mimes.TryGetValue (extension, out existingMimeType)) {
-					node = doc.CreateNode (XmlNodeType.Element, "Default", ContentNamespace);
+
+                if (extension.Length > 0 &&
+                    !mimes.TryGetValue(extension, out existingMimeType))
+                {
+                    // If extension is empty string (no extension), can possible goes here,
+                    // So to avoid that, we give a condition, only having extension will go here.
+
+                    node = doc.CreateNode (XmlNodeType.Element, "Default", ContentNamespace);
 					
 					XmlAttribute ext = doc.CreateAttribute ("Extension");
 					ext.Value = extension;
