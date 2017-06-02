a560146b7ce6fe9ac36c69c7818c288befe8f113
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-06-02 03:42:01 +0200

    [MacSDKRelease] Pin revision of mono-extensions
    With the new bockbuild in place we can finally remove the
    implicit dependency on a point-in-time state of mono-extensions
    and instead explicitly pin the desired revision.

--- packaging/MacSDKRelease/mono-extensions.py
+++ packaging/MacSDKRelease/mono-extensions.py
@@ -7,7 +7,8 @@ class MonoExtensionsPackage(Package):
         Package.__init__(self, 'mono-extensions', None,
                          sources=['git@github.com:xamarin/mono-extensions.git'],
                          git_branch=self.profile.release_packages[
-                             'mono'].git_branch
+                             'mono'].git_branch,
+                         revision='07ad37d63e0e9dcf7c879a72bc14c5d6c794f7b6'
                          )
         self.source_dir_name = 'mono-extensions'
 
