fb54066c96251ea93db09f662843bbe7b0cc89b4
Josh Peterson <petersonjm1@gmail.com>
2017-06-15 13:27:39 -0400

    Teach winsetup.bat to accept optional arguments
    This helps on non-windows platforms where we want to provide hard-coded
    config.h files like we do on Windows.

--- msvc/winsetup.bat
+++ msvc/winsetup.bat
@@ -1,20 +1,43 @@
 @ECHO off
 
+SETLOCAL
+
+IF not "%1"=="" (
+    IF not "%2"=="" (
+        SET WIN_CONFIG_H=%1
+        SET EGLIB_WIN_CONFIG_H=%2
+        SET ARGS_USED=1
+    )
+)
+
+IF "%ARGS_USED%"=="" (
+    SET WIN_CONFIG_H=..\winconfig.h
+    SET EGLIB_WIN_CONFIG_H=..\eglib\winconfig.h
+)
+
 SET CONFIG_H=..\config.h
 SET EGLIB_CONFIG_H=..\eglib\config.h
 SET CYG_CONFIG_H=..\cygconfig.h
 SET EGLIB_CYG_CONFIG_H=..\eglib\cygconfig.h
-SET WIN_CONFIG_H=..\winconfig.h
-SET EGLIB_WIN_CONFIG_H=..\eglib\winconfig.h
 SET CONFIGURE_AC=..\configure.ac
 SET VERSION_H=..\mono\mini\version.h
 
 
 ECHO Setting up Mono configuration headers...
 
-REM Backup existing config.h into cygconfig.h if its not already replaced.
-%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -NonInteractive -File backup-config-files.ps1 %CONFIG_H% %CYG_CONFIG_H% 2>&1
-%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -NonInteractive -File backup-config-files.ps1 %EGLIB_CONFIG_H% %EGLIB_CYG_CONFIG_H% 2>&1
+IF EXIST %CONFIG_H% (
+	IF NOT EXIST %CYG_CONFIG_H% (
+		ECHO copy %CONFIG_H% %CYG_CONFIG_H%
+		copy %CONFIG_H% %CYG_CONFIG_H%
+	)
+)
+
+IF EXIST %EGLIB_CONFIG_H% (
+	IF NOT EXIST %EGLIB_CYG_CONFIG_H% (
+		ECHO copy %EGLIB_CONFIG_H% %EGLIB_CYG_CONFIG_H%
+		copy %EGLIB_CONFIG_H% %EGLIB_CYG_CONFIG_H%
+	)
+)
 
 %windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -NonInteractive -File compare-config-files.ps1 %WIN_CONFIG_H% %CONFIG_H% %CONFIGURE_AC% 2>&1
 
@@ -22,7 +45,6 @@ IF NOT %ERRORLEVEL% == 0 (
 	ECHO copy %WIN_CONFIG_H% %CONFIG_H%
 	copy %WIN_CONFIG_H% %CONFIG_H%
 	%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -NonInteractive -Command "(Get-Content %CONFIG_H%) -replace '#MONO_VERSION#', (Select-String -path %CONFIGURE_AC% -pattern 'AC_INIT\(mono, \[(.*)\]').Matches[0].Groups[1].Value | Set-Content %CONFIG_H%" 2>&1
-	%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -NonInteractive -Command "$mono_version=[int[]](Select-String -path %CONFIGURE_AC% -pattern 'AC_INIT\(mono, \[(.*)\]').Matches[0].Groups[1].Value.Split('.'); $corlib_counter=[int](Select-String -path %CONFIGURE_AC% -pattern 'MONO_CORLIB_COUNTER=(.*)').Matches[0].Groups[1].Value; (Get-Content %CONFIG_H%) -replace '#MONO_CORLIB_VERSION#',('1{0:00}{1:00}{2:00}{3:000}' -f $mono_version[0],$mono_version[1],$mono_version[2],$corlib_counter) | Set-Content %CONFIG_H%" 2>&1
 )
 
 %windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -NonInteractive -File compare-config-files.ps1 %EGLIB_WIN_CONFIG_H% %EGLIB_CONFIG_H% 2>&1
