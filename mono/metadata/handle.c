b05d0aa90e6c663c5552f4de5ed881184c6b8022
cherusker <prince.cherusker@gmail.com>
2017-06-17 20:46:44 +0200

    Fix compiler warnings (misc - metadata)
    - specify the arguments (void) to avoid -Wstrict-prototypes warnings

--- mono/metadata/handle.c
+++ mono/metadata/handle.c
@@ -71,7 +71,7 @@ Combine: MonoDefaults, GENERATE_GET_CLASS_WITH_CACHE, TYPED_HANDLE_DECL and frie
 
 #ifdef HAVE_BOEHM_GC
 static HandleStack*
-new_handle_stack ()
+new_handle_stack (void)
 {
 	return (HandleStack *)mono_gc_alloc_fixed (sizeof (HandleStack), MONO_GC_DESCRIPTOR_NULL, MONO_ROOT_SOURCE_HANDLE, "Thread Handle Stack");
 }
@@ -83,7 +83,7 @@ free_handle_stack (HandleStack *stack)
 }
 
 static HandleChunk*
-new_handle_chunk ()
+new_handle_chunk (void)
 {
 	return (HandleChunk *)GC_MALLOC (sizeof (HandleChunk));
 }
@@ -94,7 +94,7 @@ free_handle_chunk (HandleChunk *chunk)
 }
 #else
 static HandleStack*
-new_handle_stack ()
+new_handle_stack (void)
 {
 	return g_new (HandleStack, 1);
 }
@@ -106,7 +106,7 @@ free_handle_stack (HandleStack *stack)
 }
 
 static HandleChunk*
-new_handle_chunk ()
+new_handle_chunk (void)
 {
 	return g_new (HandleChunk, 1);
 }
