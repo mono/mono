961f84ad9d07adca7092af92ec8c63fc09697813
Jonathan Chambers <joncham@gmail.com>
2017-06-17 08:30:01 -0400

    Fix merge issue from cherry-pick. Adjust as boehm_thread_detach already exists in 2017-02 branch

--- mono/metadata/boehm-gc.c
+++ mono/metadata/boehm-gc.c
@@ -58,8 +58,6 @@ static mono_mutex_t mono_gc_lock;
 static void*
 boehm_thread_register (MonoThreadInfo* info, void *baseptr);
 static void
-boehm_thread_detach (MonoThreadInfo *info);
-static void
 boehm_thread_unregister (MonoThreadInfo *p);
 static void
 boehm_thread_detach (MonoThreadInfo *p);
@@ -254,7 +252,6 @@ mono_gc_base_init (void)
 
 	memset (&cb, 0, sizeof (cb));
 	cb.thread_register = boehm_thread_register;
-	cb.thread_detach = boehm_thread_detach;
 	cb.thread_unregister = boehm_thread_unregister;
 	cb.thread_detach = boehm_thread_detach;
 	cb.mono_method_is_critical = (gboolean (*)(void *))mono_runtime_is_critical_method;
@@ -416,17 +413,6 @@ boehm_thread_register (MonoThreadInfo* info, void *baseptr)
 }
 
 static void
-boehm_thread_detach (MonoThreadInfo *info)
-{
-	/* Free in detach rather than unregister since 
-	 * Boehm handle stack uses GC memory and acquires
-	 * GC lock to free it. The detach callback 
-	 * does not hold any locks while unregister does.
-	 */
-	mono_handle_stack_free (info->handle_stack);
-}
-
-static void
 boehm_thread_unregister (MonoThreadInfo *p)
 {
 	MonoNativeThreadId tid;
@@ -446,6 +432,13 @@ boehm_thread_detach (MonoThreadInfo *p)
 {
 	if (mono_thread_internal_current_is_attached ())
 		mono_thread_detach_internal (mono_thread_internal_current ());
+
+	/* Free in detach rather than unregister since 
+	 * Boehm handle stack uses GC memory and acquires
+	 * GC lock to free it. The detach callback 
+	 * does not hold any locks while unregister does.
+	 */
+	mono_handle_stack_free (p->handle_stack);
 }
 
 gboolean
