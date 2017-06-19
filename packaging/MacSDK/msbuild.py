f98b92ea6c84e06fb87640fbbd8f718cef5f2064
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-06-19 15:15:08 +0200

    [MacSDK] Bump msbuild

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,6 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
+			revision = '622cece0cf47460bf83f2931c5788134bac4c48d',
 			git_branch = 'xplat-master')
 
 	def build (self):
