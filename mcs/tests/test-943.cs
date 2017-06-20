mono/mono

commit a0fc3f4dcf571edc997f53066c09671650e96670

[mcs] When setting struct empty layout consider compiler generated fields. Fixes #55604

mcs/mcs/class.cs
@@ -639,6 +639,15 @@ namespace Mono.CSharp
 			}
 		}
 
+		public bool HasInstanceField {
+			get {
+				return (caching_flags & Flags.HasInstanceField) != 0;
+			}
+			set {
+				caching_flags |= Flags.HasInstanceField;
+			}
+		}
+
 		// Indicated whether container has StructLayout attribute set Explicit
 		public bool HasExplicitLayout {
 			get { return (caching_flags & Flags.HasExplicitLayout) != 0; }
@@ -848,18 +857,22 @@ namespace Mono.CSharp
 			if ((field.ModFlags & Modifiers.STATIC) != 0)
 				return true;
 
-			var first_field = PartialContainer.first_nonstatic_field;
-			if (first_field == null) {
+			if (!PartialContainer.HasInstanceField) {
+				PartialContainer.HasInstanceField = true;
 				PartialContainer.first_nonstatic_field = field;
 				return true;
 			}
 
-			if (Kind == MemberKind.Struct && first_field.Parent != field.Parent) {
-				Report.SymbolRelatedToPreviousError (first_field.Parent);
-				Report.Warning (282, 3, field.Location,
-					"struct instance field `{0}' found in different declaration from instance field `{1}'",
-					field.GetSignatureForError (), first_field.GetSignatureForError ());
+			if (Kind == MemberKind.Struct) {
+				var first_field = PartialContainer.first_nonstatic_field;
+				if (first_field.Parent != field.Parent) {
+					Report.SymbolRelatedToPreviousError (first_field.Parent);
+					Report.Warning (282, 3, field.Location,
+						"struct instance field `{0}' found in different declaration from instance field `{1}'",
+						field.GetSignatureForError (), first_field.GetSignatureForError ());
+				}
 			}
+
 			return true;
 		}
 
@@ -1299,7 +1312,7 @@ namespace Mono.CSharp
 			//
 			// Sets .size to 1 for structs with no instance fields
 			//
-			int type_size = Kind == MemberKind.Struct && first_nonstatic_field == null && !(this is StateMachine) ? 1 : 0;
+			int type_size = Kind == MemberKind.Struct && !HasInstanceField && !(this is StateMachine) ? 1 : 0;
 
 			var parent_def = Parent as TypeDefinition;
 			if (parent_def == null) {
@@ -2214,6 +2227,10 @@ namespace Mono.CSharp
 				Module.PredefinedAttributes.CompilerGenerated.EmitAttribute (TypeBuilder);
 
 #if STATIC
+			if (Kind == MemberKind.Struct && HasInstanceField) {
+				TypeBuilder.__SetLayout (0, 0);
+			}
+
 			if ((TypeBuilder.Attributes & TypeAttributes.StringFormatMask) == 0 && Module.HasDefaultCharSet)
 				TypeBuilder.__SetAttributes (TypeBuilder.Attributes | Module.DefaultCharSetType);
 #endif
@@ -3146,7 +3163,7 @@ namespace Mono.CSharp
 				return false;
 			}
 
-			if (first_nonstatic_field != null) {
+			if (HasInstanceField) {
 				requires_delayed_unmanagedtype_check = true;
 
 				foreach (var member in Members) {

mcs/tests/test-943.cs
@@ -1,0 +1,19 @@
+using System;
+
+public struct MyStruct
+{
+	public int X { get; set; }
+}
+
+class X
+{
+	public static int Main ()
+	{
+		var s = typeof (MyStruct);
+
+		if (s.StructLayoutAttribute.Size != 0)
+			return 1;
+
+		return 0;
+	}
+}

mcs/errors/cs0208-19.cs
@@ -1,0 +1,20 @@
+// CS0208: Cannot take the address of, get the size of, or declare a pointer to a managed type `CS208.Foo'
+// Line: 17
+// Compiler options: -unsafe
+
+namespace CS208
+{
+	public struct Foo
+	{
+		public string PP { get; set; }
+	}
+
+	public class Bar
+	{
+		unsafe static void Main ()
+		{			
+			Foo f = new Foo ();
+			void *s = &f;
+		}
+	}
+}

mcs/mcs/decl.cs
@@ -290,7 +290,8 @@ namespace Mono.CSharp {
 			HasInstanceConstructor = 1 << 16,
 			HasUserOperators = 1 << 17,
 			CanBeReused = 1 << 18,
-			InterfacesExpanded = 1 << 19
+			InterfacesExpanded = 1 << 19,
+			HasInstanceField = 1 << 20
 		}
 
 		/// <summary>

mcs/mcs/property.cs
@@ -820,8 +820,10 @@ namespace Mono.CSharp
 			Parent.PartialContainer.Members.Add (BackingField);
 
 			FieldExpr fe = new FieldExpr (BackingField, Location);
-			if ((BackingField.ModFlags & Modifiers.STATIC) == 0)
+			if ((BackingField.ModFlags & Modifiers.STATIC) == 0) {
 				fe.InstanceExpression = new CompilerGeneratedThis (Parent.CurrentType, Location);
+				Parent.PartialContainer.HasInstanceField = true;
+			}
 
 			//
 			// Create get block but we careful with location to

commit d2b64bb5cf3b1fcc878196f2b2a76b6ce2c6c4e3

[mcs] Fixes regression introduced by a0fc3f4dcf571edc997f53066c09671650e96670 to consider user set values. Fixes #57242

mcs/mcs/class.cs
@@ -2227,7 +2227,7 @@ namespace Mono.CSharp
 				Module.PredefinedAttributes.CompilerGenerated.EmitAttribute (TypeBuilder);
 
 #if STATIC
-			if (Kind == MemberKind.Struct && HasInstanceField) {
+			if (Kind == MemberKind.Struct && !HasStructLayout && HasInstanceField) {
 				TypeBuilder.__SetLayout (0, 0);
 			}
 

mcs/tests/test-943.cs
@@ -1,8 +1,16 @@
 using System;
+using System.Runtime.InteropServices;
 
 public struct MyStruct
 {
 	public int X { get; set; }
+}
+
+[StructLayout (LayoutKind.Sequential, Pack = 1)]
+public struct MyStruct2
+{
+    public IntPtr handle;
+    public uint type_reference;
 }
 
 class X
@@ -13,6 +21,14 @@ class X
 
 		if (s.StructLayoutAttribute.Size != 0)
 			return 1;
+
+		var s2 = typeof (MyStruct2);
+
+		if (s2.StructLayoutAttribute.Size != 0)
+			return 2;
+
+		if (s2.StructLayoutAttribute.Pack != 1)
+			return 3;
 
 		return 0;
 	}

mcs/tests/ver-il-net_4_x.xml
@@ -52719,7 +52719,7 @@
     </type>
     <type name="X">
       <method name="Int32 Main()" attrs="150">
-        <size>44</size>
+        <size>102</size>
       </method>
       <method name="Void .ctor()" attrs="6278">
         <size>7</size>