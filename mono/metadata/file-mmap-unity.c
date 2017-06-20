ea4575efc40d9e154c7ee198fd2f8dc1c6eb6775
Doug Piconi <dougp@unity3d.com>
2017-06-20 10:09:09 -0400

    code review cleanup of comments

--- mono/metadata/file-mmap-unity.c
+++ mono/metadata/file-mmap-unity.c
@@ -39,39 +39,39 @@ typedef struct {
 
 void mono_mmap_close (void *mmap_handle)
 {
-	/* Not Supported in libil2cpp */
+	/* Not Supported in UnityPAL */
 	g_assert_not_reached();
 }
 
 void mono_mmap_configure_inheritability (void *mmap_handle, gboolean inheritability)
 {
-	/* Not Supported in libil2cpp */
+	/* Not Supported in UnityPAL */
 	g_assert_not_reached();
 }
 
 void mono_mmap_flush (void *mmap_handle)
 {
-	/* Not Supported in libil2cpp */
+	/* Not Supported in UnityPAL */
 	g_assert_not_reached();
 }
 
 void *mono_mmap_open_file (MonoString *string, int mode, MonoString *mapName, gint64 *capacity, int access, int options, int *error)
 {
-	/* Not Supported in libil2cpp */
+	/* Not Supported in UnityPAL */
 	g_assert_not_reached();
 	return NULL;
 }
 
 void *mono_mmap_open_handle (void *handle, MonoString *mapName, gint64 *capacity, int access, int options, int *error)
 {
-	/* Not Supported in libil2cpp */
+	/* Not Supported in UnityPAL */
 	g_assert_not_reached();
 	return NULL;
 }
 
 int mono_mmap_map (void *handle, gint64 offset, gint64 *size, int access, void **mmap_handle, void **base_address)
 {
-    /* We are dropping access parameter, libil2cpp does not support */
+	/* We are dropping access parameter, UnityPAL does not support */
 	g_assert (handle);
 
 	MmapInstance *h = g_malloc0 (sizeof (MmapInstance));
@@ -108,7 +108,7 @@ mono_mmap_unmap (void *mmap_handle)
 
 	g_free (h);
 
-	/* libil2cpp does not give any indication of success or failure of an unmap, forced
+	/* UnityPAL does not give any indication of success or failure of an unmap, forced
        to always return true */
 
 	return TRUE;
