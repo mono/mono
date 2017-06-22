105482274a535887e0e75d39daf6acfe7193dc3e
Lukasz Paczkowski <lukasz@unity3d.com>
2017-06-22 08:06:40 +0200

    Add stub implementation of mono_unity_capture_memory_snapshot and mono_unity_free_captured_memory_snapshot.

---
+++ mono/metadata/unity-memory-info.c
@@ -0,0 +1,13 @@
+#include "unity-memory-info.h"
+
+MonoManagedMemorySnapshot* mono_unity_capture_memory_snapshot()
+{
+	MonoManagedMemorySnapshot* snapshot;
+	snapshot = g_new0(MonoManagedMemorySnapshot, 1);
+	return snapshot;
+}
+
+void mono_unity_free_captured_memory_snapshot(MonoManagedMemorySnapshot* snapshot)
+{
+	g_free(snapshot);
+}
