019a83f88fc581b7ffc041032afe88ca5c97858f
Ankit Jain <ankit.jain@xamarin.com>
2017-06-22 21:31:11 -0400

    [packaging/msbuild] Update commit hash to point to xplat-master HEAD
    cherry-pick 4f7b15520d09843c1c0668f9fa4924f3cccb7398 from master

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision = '622cece0cf47460bf83f2931c5788134bac4c48d',
+			revision = '416ff926678717d73cc42f89656392fb92da967e',
 			git_branch = 'xplat-master')
 
 	def build (self):
