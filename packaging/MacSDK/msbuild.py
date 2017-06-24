80165d21175e05d502b968701b41a0d66608cc99
Ankit Jain <radical@gmail.com>
2017-06-23 23:35:53 -0400

    [packaging/msbuild] Bump to get new changes on xplat-master (#5113) (#5114)

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision = 'bce6977da8fdb05f30d80bfc5649d9edd543b026',
+			revision = '483c14cc7ae36b2314f87591f2806be53ab7322b',
 			git_branch = 'xplat-master')
 
 	def build (self):
