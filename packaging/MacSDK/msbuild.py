13aeb8836a5c036a7b8918a2e53f58a10475069e
Ankit Jain <ankit.jain@xamarin.com>
2017-07-03 21:47:26 -0400

    Revert "[packaging/msbuild] Bump to get new changes on xplat-master (#5145)"
    This reverts commit 94dcde72917979d9b25b3786e214f08cbc4d2299.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision = '5666044e2a28dd632b2dfb9e4387882945e50b6f')
+			revision = 'f7dcc3900c808775adad970f047202b4de34e986')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
