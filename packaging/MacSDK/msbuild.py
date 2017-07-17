38af2673cb2cfc83046280aa8bf793a6daf199c4
Ankit Jain <ankit.jain@xamarin.com>
2017-07-17 12:56:43 -0400

    [msbuild] Update packaging/msbuild.py to use branch HEAD
    .. and bump version from 15.3 to 15.5, which is only really used by
    bockbuild itself.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -2,8 +2,7 @@ import fileinput
 
 class MSBuild (GitHubPackage):
 	def __init__ (self):
-		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision = 'f7dcc3900c808775adad970f047202b4de34e986')
+		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.5', git_branch = 'xplat-master')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
