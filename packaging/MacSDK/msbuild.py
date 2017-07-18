b4ef279dd767faf5bc881a17eb2dae07f07c0026
Ankit Jain <ankit.jain@xamarin.com>
2017-07-17 20:19:10 -0400

    [msbuild] Bump msbuild, fix build breakage

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision = '81687aa7dd4ad9b76c52c1efa841b4527e0f45f9')
+			revision = '9fcd43ddc7c2f9b250b0d7e823ae01862246b35a')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
