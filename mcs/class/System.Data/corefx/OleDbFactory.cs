201bc2f6286855b8dc0154f953b995543eadf4d8
EgorBo <egorbo@gmail.com>
2017-08-03 17:22:21 +0300

    OleDbPermission should be serializable (fixing tests)

--- mcs/class/System.Data/corefx/OleDbFactory.cs
+++ mcs/class/System.Data/corefx/OleDbFactory.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Data;
 using System.Data.Common;
+using System.Runtime.Serialization;
 using System.Security;
 using System.Security.Permissions;
 
@@ -14,8 +15,12 @@ namespace System.Data.OleDb
     public sealed class OleDbFactory : DbProviderFactory { }
 
     [MonoTODO] // OLEDB is not implemented
+    [Serializable] 
     public class OleDbPermission : DBDataPermission 
     {
+        [Obsolete("OleDbPermission() has been deprecated.  Use the OleDbPermission(PermissionState.None) constructor.  http://go.microsoft.com/fwlink/?linkid=14202", true) ] // MDAC 86034
+        public OleDbPermission() : this(PermissionState.None) { }
+
         public OleDbPermission(PermissionState state) : base(state) { }
     }
 }
