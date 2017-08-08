4b24679c395717f07006c43b44a10b8b00cfb8a9
Ankit Jain <ankit.jain@xamarin.com>
2017-08-08 16:09:41 -0400

    [msbuild] Bump to include deny asm PR

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.4',
-			revision = '7dd27d93ae66f75cf8132f4490d90442cd1b62f6')
+			revision = '484b64c744ca17e543f215ba1419d08f702bf2b8')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
