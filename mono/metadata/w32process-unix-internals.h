6ab74ad86379308d28e18559b31c946afc2087cb
Alex Rønne Petersen <alpeters@microsoft.com>
2017-08-09 13:13:32 +0200

    [w32process] Fix some warnings by marking some static functions G_GNUC_UNUSED.

--- mono/metadata/w32process-unix-internals.h
+++ mono/metadata/w32process-unix-internals.h
@@ -40,7 +40,7 @@ mono_w32process_get_name (pid_t pid);
 GSList*
 mono_w32process_get_modules (pid_t pid);
 
-static void
+static G_GNUC_UNUSED void
 mono_w32process_module_free (MonoW32ProcessModule *module)
 {
 	g_free (module->perms);
@@ -51,7 +51,7 @@ mono_w32process_module_free (MonoW32ProcessModule *module)
 /*
  * Used to look through the GSList* returned by mono_w32process_get_modules
  */
-static gint
+static G_GNUC_UNUSED gint
 mono_w32process_module_equals (gconstpointer a, gconstpointer b)
 {
 	MonoW32ProcessModule *want = (MonoW32ProcessModule *)a;
