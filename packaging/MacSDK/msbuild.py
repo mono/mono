e9f63bbd97721cc5d472d02bb8b1374853a9d755
Ankit Jain <ankit.jain@xamarin.com>
2017-08-09 21:08:55 -0400

    [msbuild] Bump to xplat-master branch HEAD

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.4',
-			revision = '7dd27d93ae66f75cf8132f4490d90442cd1b62f6')
+			revision = '0f250921e0b91ca1bda6494df49ae8ad703aeb5c')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
