2071615b874905bd85a3503affd261fe33cd1221
Ankit Jain <ankit.jain@xamarin.com>
2017-08-16 14:45:40 -0400

    [msbuild] Bump to d15.4 branch HEAD

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.4',
-			revision = '7dd27d93ae66f75cf8132f4490d90442cd1b62f6')
+			revision = '5633e38dd86d5ca588a7a3b9c8e961b5f7bdcf62')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
