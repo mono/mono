mono/mono

commit a48bc439850869e565833d7fe6330c289beffe40

[bcl] Fix Delegate.GetHashCode () so it takes into account the target too. Fixes #58399. (#5296)

mcs/class/corlib/System/Delegate.cs
@@ -495,8 +495,11 @@ namespace System
 
 		public override int GetHashCode ()
 		{
-			/* same implementation as CoreCLR */
-			return GetType ().GetHashCode ();
+			MethodInfo m;
+
+			m = Method;
+
+			return (m != null ? m.GetHashCode () : GetType ().GetHashCode ()) ^ (m_target != null ? m_target.GetHashCode () : 0);
 		}
 
 		protected virtual MethodInfo GetMethodImpl ()

commit 6e23f46c7107dae28d2060a3b3891298ee9c74ab

[bcl] Use RuntimeHelpers.GetHashCode () for getting the hash code of the delegate target to avoid calling user defined hash code implementations which can throw exceptions. Fixes a regression introduced by a48bc439850869e565833d7fe6330c289beffe40.

mcs/class/corlib/System/Delegate.cs
@@ -499,7 +499,7 @@ namespace System
 
 			m = Method;
 
-			return (m != null ? m.GetHashCode () : GetType ().GetHashCode ()) ^ (m_target != null ? m_target.GetHashCode () : 0);
+			return (m != null ? m.GetHashCode () : GetType ().GetHashCode ()) ^ (m_target != null ? RuntimeHelpers.GetHashCode (m_target) : 0);
 		}
 
 		protected virtual MethodInfo GetMethodImpl ()