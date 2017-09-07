7d68dc8e71623ba76b16c5c5aa597a2fc7783f16
Miguel de Icaza <miguel@gnome.org>
2017-09-07 15:13:39 -0400

    Toggle the turbo button

--- mcs/mcs/settings.cs
+++ mcs/mcs/settings.cs
@@ -346,7 +346,7 @@ namespace Mono.CSharp {
 		void About ()
 		{
 			output.WriteLine (
-				"The Mono C# compiler is Copyright 2001-2011, Novell, Inc. 2011-2016 Xamarin Inc, 2016-2017 Microsoft Corp\n\n" +
+				"The Mono Turbo C# compiler is Copyright 2001-2011, Novell, Inc. 2011-2016 Xamarin Inc, 2016-2017 Microsoft Corp\n\n" +
 				"The compiler source code is released under the terms of the \n" +
 				"MIT X11 or GNU GPL licenses\n\n" +
 
@@ -1603,7 +1603,7 @@ namespace Mono.CSharp {
 		void Usage ()
 		{
 			output.WriteLine (
-				"Mono C# compiler, Copyright 2001-2011 Novell, Inc., 2011-2016 Xamarin, Inc, 2016-2017 Microsoft Corp\n" +
+				"Mono Turbo C# compiler, Copyright 2001-2011 Novell, Inc., 2011-2016 Xamarin, Inc, 2016-2017 Microsoft Corp\n" +
 				"mcs [options] source-files\n" +
 				"   --about              About the Mono C# compiler\n" +
 				"   -addmodule:M1[,Mn]   Adds the module to the generated assembly\n" +
