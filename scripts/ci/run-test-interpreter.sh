b29ef216ace1e57dd83ed37e37474a1658071c60
Bernhard Urban <bernhard.urban@xamarin.com>
2017-10-17 18:10:38 +0200

    [mcs/tests] add interp target

--- scripts/ci/run-test-interpreter.sh
+++ scripts/ci/run-test-interpreter.sh
@@ -6,3 +6,4 @@ ${TESTCMD} --label=interpreter-regression --timeout=10m make -C mono/mini richec
 ${TESTCMD} --label=mixedmode-regression --timeout=10m make -C mono/mini mixedcheck
 ${TESTCMD} --label=compile-runtime-tests --timeout=40m make -w -C mono/tests -j4 tests
 ${TESTCMD} --label=runtime-interp --timeout=160m make -w -C mono/tests -k testinterp V=1
+${TESTCMD} --label=mcs-tests --timeout=160m make -w -C mcs/tests -k check-interp V=1
