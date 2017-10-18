156f9c56557064f79863d65afe9d41d2f5e2ddc2
Marek Safar <marek.safar@gmail.com>
2017-10-18 13:04:54 +0200

    [bcl] Update PlatformDetection helpers

--- mcs/class/test-helpers/PlatformDetection.cs
+++ mcs/class/test-helpers/PlatformDetection.cs
@@ -3,5 +3,9 @@ namespace System
 	static partial class PlatformDetection
 	{
 		public static readonly bool IsNetNative = false;
+
+		public static readonly bool IsFullFramework = true;
+
+		public static bool IsNonZeroLowerBoundArraySupported => true;
 	}
 }
