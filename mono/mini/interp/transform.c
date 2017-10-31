2c35d1a52bfd396fd479460f82bebebc814c9b37
Bernhard Urban <bernhard.urban@xamarin.com>
2017-10-31 13:09:14 +0100

    [interp] use checked variant of method_get_header and throw exception if there is an error

--- mono/mini/interp/transform.c
+++ mono/mini/interp/transform.c
@@ -4363,8 +4363,11 @@ mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context, Mon
 		}
 	}
 
-	if (!header)
-		header = mono_method_get_header (method);
+	if (!header) {
+		header = mono_method_get_header_checked (method, &error);
+		if (!is_ok (&error))
+			return mono_error_convert_to_exception (&error);
+	}
 
 	g_assert ((signature->param_count + signature->hasthis) < 1000);
 	g_assert (header->max_stack < 10000);
