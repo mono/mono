4e0db3f68bb99df9c0cee5f601f34035022f9efe
Manuel de la Pena <mandel@microsoft.com>
2017-11-02 17:10:26 +0100

    [Tests] Fix flaky IO tests due to parrallel execution on Mac bots.
    Some of the IO tests do not create a diff temp file per test but per
    fixture. This is problematic when the test cases are ran in parallel.
    Moving the creation of the tmp dirs to the SetUp to ensure that each
    test case has its own path.

--- mcs/class/corlib/Test/System.IO/StreamReaderTest.cs
+++ mcs/class/corlib/Test/System.IO/StreamReaderTest.cs
@@ -18,15 +18,18 @@ namespace MonoTests.System.IO
 [TestFixture]
 public class StreamReaderTest
 {
-	static string TempFolder = Path.Combine (Path.GetTempPath (), "MonoTests.System.IO.Tests");
-	private string _codeFileName = TempFolder + Path.DirectorySeparatorChar + "AFile.txt";
+	private string _tmpFolder;
+	private string _codeFileName;
 	private const string TestString = "Hello World!";
 
 	[SetUp]
 	public void SetUp ()
 	{	
-		if (!Directory.Exists (TempFolder))
-			Directory.CreateDirectory (TempFolder);
+		_tmpFolder = Path.Combine (Path.GetTempPath (), "MonoTests.System.IO.Tests");
+		_codeFileName = _tmpFolder + Path.DirectorySeparatorChar + "AFile.txt";
+
+		if (!Directory.Exists (_tmpFolder))
+			Directory.CreateDirectory (_tmpFolder);
 		
 		if (!File.Exists (_codeFileName))
 			File.Create (_codeFileName).Close ();
@@ -35,8 +38,8 @@ public class StreamReaderTest
 	[TearDown]
 	public void TearDown ()
 	{
-		if (Directory.Exists (TempFolder))
-			Directory.Delete (TempFolder, true);
+		if (Directory.Exists (_tmpFolder))
+			Directory.Delete (_tmpFolder, true);
 	}
 
 
@@ -245,7 +248,7 @@ public class StreamReaderTest
 		{
 			bool errorThrown = false;
 			try {
-				new StreamReader(TempFolder + "/nonexistentfile", false);
+				new StreamReader(_tmpFolder + "/nonexistentfile", false);
 			} catch (FileNotFoundException) {
 				errorThrown = true;
 			} catch (Exception e) {
@@ -256,7 +259,7 @@ public class StreamReaderTest
 		{
 			bool errorThrown = false;
 			try {
-				new StreamReader(TempFolder + "/nonexistentdir/file", false);
+				new StreamReader(_tmpFolder + "/nonexistentdir/file", false);
 			} catch (DirectoryNotFoundException) {
 				errorThrown = true;
 			} catch (Exception e) {
@@ -310,7 +313,7 @@ public class StreamReaderTest
 		{
 			bool errorThrown = false;
 			try {
-				new StreamReader(TempFolder + "/nonexistentfile", true);
+				new StreamReader(_tmpFolder + "/nonexistentfile", true);
 			} catch (FileNotFoundException) {
 				errorThrown = true;
 			} catch (Exception e) {
@@ -321,7 +324,7 @@ public class StreamReaderTest
 		{
 			bool errorThrown = false;
 			try {
-				new StreamReader(TempFolder + "/nonexistentdir/file", true);
+				new StreamReader(_tmpFolder + "/nonexistentdir/file", true);
 			} catch (DirectoryNotFoundException) {
 				errorThrown = true;
 			} catch (Exception e) {
