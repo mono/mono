mono/mono

commit 1b5e0f73316ee7b83c8947bc738015443fabd7af

[System]: Correctly implement close and shutdown in SslStream. (#4969) (#5008)
* [System]: Correctly implement close and shutdown in SslStream.

* Cleanup.

* Cosmetic.

* More ReferenceSources/SR2.cs into common.sources to make it build.

(cherry picked from commit 21d5c9c346113d47455d96e545410733bfccf1fb)

mcs/class/System/Mono.Net.Security/AsyncProtocolRequest.cs
@@ -11,14 +11,14 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Security;
+using System.Security.Authentication;
 using SD = System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.ExceptionServices;
 
 namespace Mono.Net.Security
 {
-	delegate AsyncOperationStatus AsyncOperation (AsyncProtocolRequest asyncRequest, AsyncOperationStatus status);
-
 	class BufferOffsetSize
 	{
 		public byte[] Buffer;
@@ -37,6 +37,13 @@ namespace Mono.Net.Security
 
 		public BufferOffsetSize (byte[] buffer, int offset, int size)
 		{
+			if (buffer == null)
+				throw new ArgumentNullException (nameof (buffer));
+			if (offset < 0)
+				throw new ArgumentOutOfRangeException (nameof (offset));
+			if (size < 0 || offset + size > buffer.Length)
+				throw new ArgumentOutOfRangeException (nameof (size));
+
 			Buffer = buffer;
 			Offset = offset;
 			Size = size;
@@ -54,7 +61,7 @@ namespace Mono.Net.Security
 		public readonly int InitialSize;
 
 		public BufferOffsetSize2 (int size)
-			: base (new byte [size], 0, 0)
+			: base (new byte[size], 0, 0)
 		{
 			InitialSize = size;
 		}
@@ -63,7 +70,7 @@ namespace Mono.Net.Security
 		{
 			Offset = Size = 0;
 			TotalBytes = 0;
-			Buffer = new byte [InitialSize];
+			Buffer = new byte[InitialSize];
 			Complete = false;
 		}
 
@@ -74,11 +81,11 @@ namespace Mono.Net.Security
 
 			int missing = size - Remaining;
 			if (Offset == 0 && Size == 0) {
-				Buffer = new byte [size];
+				Buffer = new byte[size];
 				return;
 			}
 
-			var buffer = new byte [Buffer.Length + missing];
+			var buffer = new byte[Buffer.Length + missing];
 			Buffer.CopyTo (buffer, 0);
 			Buffer = buffer;
 		}
@@ -91,201 +98,296 @@ namespace Mono.Net.Security
 		}
 	}
 
-	enum AsyncOperationStatus {
-		NotStarted,
+	enum AsyncOperationStatus
+	{
 		Initialize,
 		Continue,
-		Running,
-		Complete,
-		WantRead,
-		WantWrite,
 		ReadDone,
-		FinishWrite
+		Complete
 	}
 
-	class AsyncProtocolRequest
+	class AsyncProtocolResult
 	{
-		public readonly MobileAuthenticatedStream Parent;
-		public readonly BufferOffsetSize UserBuffer;
-
-		int RequestedSize;
-		public int CurrentSize;
-		public int UserResult;
-
-		AsyncOperation Operation;
-		int Status;
-
-		public readonly int ID = ++next_id;
-		static int next_id;
-
-		public readonly LazyAsyncResult UserAsyncResult;
-
-		public AsyncProtocolRequest (MobileAuthenticatedStream parent, LazyAsyncResult lazyResult, BufferOffsetSize userBuffer = null)
-		{
-			Parent = parent;
-			UserAsyncResult = lazyResult;
-			UserBuffer = userBuffer;
+		public int UserResult {
+			get;
+		}
+		public ExceptionDispatchInfo Error {
+			get;
 		}
 
-		public bool CompleteWithError (Exception ex)
+		public AsyncProtocolResult (int result)
 		{
-			Status = (int)AsyncOperationStatus.Complete;
-			if (UserAsyncResult == null)
-				return true;
-			if (!UserAsyncResult.InternalPeekCompleted)
-				UserAsyncResult.InvokeCallback (ex);
-			return false;
+			UserResult = result;
+		}
+
+		public AsyncProtocolResult (ExceptionDispatchInfo error)
+		{
+			Error = error;
+		}
+	}
+
+	abstract class AsyncProtocolRequest
+	{
+		public MobileAuthenticatedStream Parent {
+			get;
+		}
+
+		public bool RunSynchronously {
+			get;
+		}
+
+		public int ID => ++next_id;
+
+		public string Name => GetType ().Name;
+
+		public int UserResult {
+			get;
+			protected set;
+		}
+
+		int Started;
+		int RequestedSize;
+		int WriteRequested;
+		readonly object locker = new object ();
+
+		static int next_id;
+
+		public AsyncProtocolRequest (MobileAuthenticatedStream parent, bool sync)
+		{
+			Parent = parent;
+			RunSynchronously = sync;
 		}
 
 		[SD.Conditional ("MARTIN_DEBUG")]
 		protected void Debug (string message, params object[] args)
 		{
-			Parent.Debug ("AsyncProtocolRequest({0}:{1}): {2}", Parent.ID, ID, string.Format (message, args));
+			Parent.Debug ("{0}({1}:{2}): {3}", Name, Parent.ID, ID, string.Format (message, args));
 		}
 
 		internal void RequestRead (int size)
 		{
-			var oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.WantRead, (int)AsyncOperationStatus.Running);
-			Debug ("RequestRead: {0} {1}", oldStatus, size);
-			if (oldStatus == AsyncOperationStatus.Running)
-				RequestedSize = size;
-			else if (oldStatus == AsyncOperationStatus.WantRead)
+			lock (locker) {
 				RequestedSize += size;
-			else if (oldStatus != AsyncOperationStatus.WantWrite)
-				throw new InvalidOperationException ();
-		}
-
-		internal void ResetRead ()
-		{
-			var oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.Complete, (int)AsyncOperationStatus.WantRead);
-			Debug ("ResetRead: {0} {1}", oldStatus, Status);
-		}
-
-		internal void ResetWrite ()
-		{
-			var oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.Complete, (int)AsyncOperationStatus.WantWrite);
-			Debug ("ResetWrite: {0} {1}", oldStatus, Status);
+				Debug ("RequestRead: {0}", size);
+			}
 		}
 
 		internal void RequestWrite ()
 		{
-			var oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.WantWrite, (int)AsyncOperationStatus.Running);
-			Debug ("RequestWrite: {0} {1}", oldStatus, Status);
-			if (oldStatus == AsyncOperationStatus.Running)
-				return;
-			else if (oldStatus != AsyncOperationStatus.WantRead && oldStatus != AsyncOperationStatus.WantWrite)
-				throw new InvalidOperationException ();
+			WriteRequested = 1;
 		}
 
-		internal void StartOperation (AsyncOperation operation)
+		internal async Task<AsyncProtocolResult> StartOperation (CancellationToken cancellationToken)
 		{
-			Debug ("Start Operation: {0} {1}", Status, operation);
-			if (Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.Initialize, (int)AsyncOperationStatus.NotStarted) != (int)AsyncOperationStatus.NotStarted)
+			Debug ("Start Operation: {0}", this);
+			if (Interlocked.CompareExchange (ref Started, 1, 0) != 0)
 				throw new InvalidOperationException ();
 
-			Operation = operation;
-
-			if (UserAsyncResult == null) {
-				StartOperation ();
-				return;
-			}
-
-			ThreadPool.QueueUserWorkItem (_ => StartOperation ());
-		}
-
-		void StartOperation ()
-		{
 			try {
-				ProcessOperation ();
-				if (UserAsyncResult != null && !UserAsyncResult.InternalPeekCompleted)
-					UserAsyncResult.InvokeCallback (UserResult);
+				await ProcessOperation (cancellationToken).ConfigureAwait (false);
+				return new AsyncProtocolResult (UserResult);
 			} catch (Exception ex) {
-				if (UserAsyncResult == null)
-					throw;
-				if (!UserAsyncResult.InternalPeekCompleted)
-					UserAsyncResult.InvokeCallback (ex);
+				var info = Parent.SetException (MobileAuthenticatedStream.GetSSPIException (ex));
+				return new AsyncProtocolResult (info);
 			}
 		}
 
-		void ProcessOperation ()
+		async Task ProcessOperation (CancellationToken cancellationToken)
 		{
-			AsyncOperationStatus status;
-			do {
-				status = (AsyncOperationStatus)Interlocked.Exchange (ref Status, (int)AsyncOperationStatus.Running);
-
+			var status = AsyncOperationStatus.Initialize;
+			while (status != AsyncOperationStatus.Complete) {
+				cancellationToken.ThrowIfCancellationRequested ();
 				Debug ("ProcessOperation: {0}", status);
 
-				status = ProcessOperation (status);
-
-				Debug ("ProcessOperation done: {0}", status);
-
-				AsyncOperationStatus oldStatus;
-				if (status == AsyncOperationStatus.Complete) {
-					oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)AsyncOperationStatus.FinishWrite, (int)AsyncOperationStatus.WantWrite);
-					if (oldStatus == AsyncOperationStatus.WantWrite) {
-						// We are done, but still need to flush the write queue.
-						status = AsyncOperationStatus.FinishWrite;
-						continue;
+				var ret = await InnerRead (cancellationToken).ConfigureAwait (false);
+				if (ret != null) {
+					if (ret == 0) {
+						// End-of-stream
+						Debug ("END OF STREAM!");
+						status = AsyncOperationStatus.ReadDone;
+					} else if (ret < 0) {
+						// remote prematurely closed connection.
+						throw new IOException ("Remote prematurely closed connection.");
 					}
 				}
 
-				oldStatus = (AsyncOperationStatus)Interlocked.CompareExchange (ref Status, (int)status, (int)AsyncOperationStatus.Running);
-				Debug ("ProcessOperation done: {0} -> {1}", oldStatus, status);
+				Debug ("ProcessOperation run: {0}", status);
 
-				if (oldStatus != AsyncOperationStatus.Running) {
-					if (status == oldStatus || status == AsyncOperationStatus.Continue || status == AsyncOperationStatus.Complete)
-						status = oldStatus;
-					else
-						throw new InvalidOperationException ();
+				AsyncOperationStatus newStatus;
+				switch (status) {
+				case AsyncOperationStatus.Initialize:
+				case AsyncOperationStatus.Continue:
+				case AsyncOperationStatus.ReadDone:
+					newStatus = Run (status);
+					break;
+				default:
+					throw new InvalidOperationException ();
 				}
-			} while (status != AsyncOperationStatus.Complete);
+
+				if (Interlocked.Exchange (ref WriteRequested, 0) != 0) {
+					// Flush the write queue.
+					await Parent.InnerWrite (RunSynchronously, cancellationToken);
+				}
+
+				Debug ("ProcessOperation done: {0} -> {1}", status, newStatus);
+
+				status = newStatus;
+			}
 		}
 
-		AsyncOperationStatus ProcessOperation (AsyncOperationStatus status)
+		async Task<int?> InnerRead (CancellationToken cancellationToken)
 		{
-			if (status == AsyncOperationStatus.WantRead) {
-				if (RequestedSize < 0)
+			int? totalRead = null;
+			var requestedSize = Interlocked.Exchange (ref RequestedSize, 0);
+			while (requestedSize > 0) {
+				Debug ("ProcessOperation - read inner: {0}", requestedSize);
+
+				var ret = await Parent.InnerRead (RunSynchronously, requestedSize, cancellationToken).ConfigureAwait (false);
+				Debug ("ProcessOperation - read inner done: {0} - {1}", requestedSize, ret);
+
+				if (ret <= 0)
+					return ret;
+				if (ret > requestedSize)
 					throw new InvalidOperationException ();
-				else if (RequestedSize == 0)
-					return AsyncOperationStatus.Continue;
 
-				Debug ("ProcessOperation - read inner: {0}", RequestedSize);
-				var ret = Parent.InnerRead (RequestedSize);
-				Debug ("ProcessOperation - read inner done: {0} - {1}", RequestedSize, ret);
+				totalRead += ret;
+				requestedSize -= ret;
+				var newRequestedSize = Interlocked.Exchange (ref RequestedSize, 0);
+				requestedSize += newRequestedSize;
+			}
 
-				if (ret < 0)
-					return AsyncOperationStatus.ReadDone;
+			return totalRead;
+		}
 
-				RequestedSize -= ret;
+		/*
+		 * This will operate on the internal buffers and never block.
+		 */
+		protected abstract AsyncOperationStatus Run (AsyncOperationStatus status);
 
-				if (ret == 0 || RequestedSize == 0)
-					return AsyncOperationStatus.Continue;
-				else
-					return AsyncOperationStatus.WantRead;
-			} else if (status == AsyncOperationStatus.WantWrite) {
-				Debug ("ProcessOperation - want write");
-				Parent.InnerWrite ();
-				Debug ("ProcessOperation - want write done");
-				return AsyncOperationStatus.Continue;
-			} else if (status == AsyncOperationStatus.Initialize || status == AsyncOperationStatus.Continue) {
-				Debug ("ProcessOperation - continue");
-				status = Operation (this, status);
-				Debug ("ProcessOperation - continue done: {0}", status);
-				return status;
-			} else if (status == AsyncOperationStatus.ReadDone) {
-				Debug ("ProcessOperation - read done");
-				status = Operation (this, status);
-				Debug ("ProcessOperation - read done: {0}", status);
-				return status;
-			} else if (status == AsyncOperationStatus.FinishWrite) {
-				Debug ("ProcessOperation - finish write");
-				Parent.InnerWrite ();
-				Debug ("ProcessOperation - finish write done");
+		public override string ToString ()
+		{
+			return string.Format ("[{0}]", Name);
+		}
+	}
+
+	class AsyncHandshakeRequest : AsyncProtocolRequest
+	{
+		public AsyncHandshakeRequest (MobileAuthenticatedStream parent, bool sync)
+			: base (parent, sync)
+		{
+		}
+
+		protected override AsyncOperationStatus Run (AsyncOperationStatus status)
+		{
+			return Parent.ProcessHandshake (status);
+		}
+	}
+
+	abstract class AsyncReadOrWriteRequest : AsyncProtocolRequest
+	{
+		protected BufferOffsetSize UserBuffer {
+			get;
+		}
+
+		protected int CurrentSize {
+			get; set;
+		}
+
+		public AsyncReadOrWriteRequest (MobileAuthenticatedStream parent, bool sync, byte[] buffer, int offset, int size)
+			: base (parent, sync)
+		{
+			UserBuffer = new BufferOffsetSize (buffer, offset, size);
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("[{0}: {1}]", Name, UserBuffer);
+		}
+	}
+
+	class AsyncReadRequest : AsyncReadOrWriteRequest
+	{
+		public AsyncReadRequest (MobileAuthenticatedStream parent, bool sync, byte[] buffer, int offset, int size)
+			: base (parent, sync, buffer, offset, size)
+		{
+		}
+
+		protected override AsyncOperationStatus Run (AsyncOperationStatus status)
+		{
+			Debug ("ProcessRead - read user: {0} {1}", this, status);
+
+			var (ret, wantMore) = Parent.ProcessRead (UserBuffer);
+
+			Debug ("ProcessRead - read user done: {0} - {1} {2}", this, ret, wantMore);
+
+			if (ret < 0) {
+				UserResult = -1;
 				return AsyncOperationStatus.Complete;
 			}
 
-			throw new InvalidOperationException ();
+			CurrentSize += ret;
+			UserBuffer.Offset += ret;
+			UserBuffer.Size -= ret;
+
+			Debug ("Process Read - read user done #1: {0} - {1} {2}", this, CurrentSize, wantMore);
+
+			if (wantMore && CurrentSize == 0)
+				return AsyncOperationStatus.Continue;
+
+			UserResult = CurrentSize;
+			return AsyncOperationStatus.Complete;
 		}
 	}
+
+	class AsyncWriteRequest : AsyncReadOrWriteRequest
+	{
+		public AsyncWriteRequest (MobileAuthenticatedStream parent, bool sync, byte[] buffer, int offset, int size)
+			: base (parent, sync, buffer, offset, size)
+		{
+		}
+
+		protected override AsyncOperationStatus Run (AsyncOperationStatus status)
+		{
+			Debug ("ProcessWrite - write user: {0} {1}", this, status);
+
+			if (UserBuffer.Size == 0) {
+				UserResult = CurrentSize;
+				return AsyncOperationStatus.Complete;
+			}
+
+			var (ret, wantMore) = Parent.ProcessWrite (UserBuffer);
+
+			Debug ("ProcessWrite - write user done: {0} - {1} {2}", this, ret, wantMore);
+
+			if (ret < 0) {
+				UserResult = -1;
+				return AsyncOperationStatus.Complete;
+			}
+
+			CurrentSize += ret;
+			UserBuffer.Offset += ret;
+			UserBuffer.Size -= ret;
+
+			if (wantMore)
+				return AsyncOperationStatus.Continue;
+
+			UserResult = CurrentSize;
+			return AsyncOperationStatus.Complete;
+		}
+	}
+
+	class AsyncShutdownRequest : AsyncProtocolRequest
+	{
+		public AsyncShutdownRequest (MobileAuthenticatedStream parent)
+			: base (parent, false)
+		{
+		}
+
+		protected override AsyncOperationStatus Run (AsyncOperationStatus status)
+		{
+			return Parent.ProcessShutdown (status);
+		}
+	}
+
 }
 #endif

mcs/class/Mono.Security/Mono.Security.Interface/IMonoSslStream.cs
@@ -86,6 +86,8 @@ namespace Mono.Security.Interface
 
 		void EndWrite (IAsyncResult asyncResult);
 
+		Task ShutdownAsync ();
+
 		TransportContext TransportContext {
 			get;
 		}

mcs/class/System/Mono.AppleTls/AppleTlsContext.cs
@@ -848,12 +848,12 @@ namespace Mono.AppleTls
 		[DllImport (SecurityLibrary)]
 		extern static /* OSStatus */ SslStatus SSLClose (/* SSLContextRef */ IntPtr context);
 
-		public override void Close ()
+		public override void Shutdown ()
 		{
 			if (Interlocked.Exchange (ref pendingIO, 1) == 1)
 				throw new InvalidOperationException ();
 
-			Debug ("Close");
+			Debug ("Shutdown");
 
 			lastException = null;
 
@@ -862,7 +862,7 @@ namespace Mono.AppleTls
 					return;
 
 				var status = SSLClose (Handle);
-				Debug ("Close done: {0}", status);
+				Debug ("Shutdown done: {0}", status);
 				CheckStatusAndThrow (status);
 			} finally {
 				closed = true;

mcs/class/System/Mono.AppleTls/AppleTlsStream.cs
@@ -38,12 +38,12 @@ namespace Mono.AppleTls
 		}
 
 		protected override MNS.MobileTlsContext CreateContext (
-			MNS.MobileAuthenticatedStream parent, bool serverMode, string targetHost,
-			SslProtocols enabledProtocols, X509Certificate serverCertificate,
-			X509CertificateCollection clientCertificates, bool askForClientCert)
+			bool serverMode, string targetHost, SslProtocols enabledProtocols,
+			X509Certificate serverCertificate, X509CertificateCollection clientCertificates,
+			bool askForClientCert)
 		{
 			return new AppleTlsContext (
-				parent, serverMode, targetHost,
+				this, serverMode, targetHost,
 				enabledProtocols, serverCertificate,
 				clientCertificates, askForClientCert);
 		}

mcs/class/System/Mono.Btls/MonoBtlsContext.cs
@@ -316,6 +316,9 @@ namespace Mono.Btls
 				if (status == MonoBtlsSslError.WantRead) {
 					wantMore = true;
 					return 0;
+				} else if (status == MonoBtlsSslError.ZeroReturn) {
+					wantMore = false;
+					return size;
 				} else if (status != MonoBtlsSslError.None) {
 					throw GetException (status);
 				}
@@ -358,26 +361,11 @@ namespace Mono.Btls
 			}
 		}
 
-		public override void Close ()
+		public override void Shutdown ()
 		{
-			Debug ("Close!");
-
-			if (ssl != null) {
-				ssl.Dispose ();
-				ssl = null;
-			}
-			if (ctx != null) {
-				ctx.Dispose ();
-				ctx = null;
-			}
-			if (bio != null) {
-				bio.Dispose ();
-				bio = null;
-			}
-			if (errbio != null) {
-				errbio.Dispose ();
-				errbio = null;
-			}
+			Debug ("Shutdown!");
+//			ssl.SetQuietShutdown ();
+			ssl.Shutdown ();
 		}
 
 		void Dispose<T> (ref T disposable)
@@ -397,12 +385,12 @@ namespace Mono.Btls
 		{
 			try {
 				if (disposing) {
+					Dispose (ref ssl);
+					Dispose (ref ctx);
 					Dispose (ref remoteCertificate);
 					Dispose (ref nativeServerCertificate);
 					Dispose (ref nativeClientCertificate);
 					Dispose (ref clientCertificate);
-					Dispose (ref ctx);
-					Dispose (ref ssl);
 					Dispose (ref bio);
 					Dispose (ref errbio);
 				}

mcs/class/System/Mono.Btls/MonoBtlsObject.cs
@@ -102,6 +102,20 @@ namespace Mono.Btls
 			CheckError (ret == 1, callerName);
 		}
 
+		protected internal void CheckLastError ([CallerMemberName] string callerName = null)
+		{
+			var error = Interlocked.Exchange (ref lastError, null);
+			if (error == null)
+				return;
+
+			string message;
+			if (callerName != null)
+				message = string.Format ("Caught unhandled exception in {0}.{1}.", GetType ().Name, callerName);
+			else
+				message = string.Format ("Caught unhandled exception.");
+			throw new MonoBtlsException (message, error);
+		}
+
 		[DllImport (BTLS_DYLIB)]
 		extern static void mono_btls_free (IntPtr data);
 

mcs/class/System/Mono.Btls/MonoBtlsSsl.cs
@@ -47,6 +47,7 @@ namespace Mono.Btls
 			protected override bool ReleaseHandle ()
 			{
 				mono_btls_ssl_destroy (handle);
+				handle = IntPtr.Zero;
 				return true;
 			}
 		}
@@ -77,6 +78,12 @@ namespace Mono.Btls
 
 		[DllImport (BTLS_DYLIB)]
 		extern static void mono_btls_ssl_close (IntPtr handle);
+
+		[DllImport (BTLS_DYLIB)]
+		extern static int mono_btls_ssl_shutdown (IntPtr handle);
+
+		[DllImport (BTLS_DYLIB)]
+		extern static void mono_btls_ssl_set_quiet_shutdown (IntPtr handle, int mode);
 
 		[DllImport (BTLS_DYLIB)]
 		extern static void mono_btls_ssl_set_bio (IntPtr handle, IntPtr bio);
@@ -131,6 +138,7 @@ namespace Mono.Btls
 			return new BoringSslHandle (handle);
 		}
 
+		MonoBtlsBio bio;
 		PrintErrorsCallbackFunc printErrorsFunc;
 		IntPtr printErrorsFuncPtr;
 
@@ -148,6 +156,7 @@ namespace Mono.Btls
 		public void SetBio (MonoBtlsBio bio)
 		{
 			CheckThrow ();
+			this.bio = bio;
 			mono_btls_ssl_set_bio (
 				Handle.DangerousGetHandle (),
 				bio.Handle.DangerousGetHandle ());
@@ -164,18 +173,17 @@ namespace Mono.Btls
 				errors = null;
 			}
 
-			if (errors != null) {
-				Console.Error.WriteLine ("ERROR: {0} failed: {1}", callerName, errors);
+			if (errors != null)
 				throw new MonoBtlsException ("{0} failed: {1}.", callerName, errors);
-			} else {
-				Console.Error.WriteLine ("ERROR: {0} failed.", callerName);
+			else
 				throw new MonoBtlsException ("{0} failed.", callerName);
-			}
 		}
 
 		MonoBtlsSslError GetError (int ret_code)
 		{
 			CheckThrow ();
+			bio.CheckLastError ();
+
 			var error = mono_btls_ssl_get_error (
 				Handle.DangerousGetHandle (), ret_code);
 			return (MonoBtlsSslError)error;
@@ -287,15 +295,20 @@ namespace Mono.Btls
 			var ret = mono_btls_ssl_read (
 				Handle.DangerousGetHandle (), data, dataSize);
 
-			if (ret >= 0) {
+			if (ret > 0) {
 				dataSize = ret;
 				return MonoBtlsSslError.None;
 			}
 
-			var error = mono_btls_ssl_get_error (
-				Handle.DangerousGetHandle (), ret);
+			var error = GetError (ret);
+			if (ret == 0 && error == MonoBtlsSslError.Syscall) {
+				// End-of-stream
+				dataSize = 0;
+				return MonoBtlsSslError.None;
+			}
+
 			dataSize = 0;
-			return (MonoBtlsSslError)error;
+			return error;
 		}
 
 		public MonoBtlsSslError Write (IntPtr data, ref int dataSize)
@@ -416,9 +429,24 @@ namespace Mono.Btls
 			return Marshal.PtrToStringAnsi (namePtr);
 		}
 
+		public void Shutdown ()
+		{
+			CheckThrow ();
+			var ret = mono_btls_ssl_shutdown (Handle.DangerousGetHandle ());
+			if (ret < 0)
+				throw ThrowError ();
+		}
+
+		public void SetQuietShutdown ()
+		{
+			CheckThrow ();
+			mono_btls_ssl_set_quiet_shutdown (Handle.DangerousGetHandle (), 1);
+		}
+
 		protected override void Close ()
 		{
-			mono_btls_ssl_close (Handle.DangerousGetHandle ());
+			if (!Handle.IsInvalid)
+				mono_btls_ssl_close (Handle.DangerousGetHandle ());
 		}
 	}
 }

mcs/class/System/Mono.Btls/MonoBtlsStream.cs
@@ -53,12 +53,12 @@ namespace Mono.Btls
 		}
 
 		protected override MNS.MobileTlsContext CreateContext (
-			MNS.MobileAuthenticatedStream parent, bool serverMode, string targetHost,
-			SslProtocols enabledProtocols, X509Certificate serverCertificate,
-			X509CertificateCollection clientCertificates, bool askForClientCert)
+			bool serverMode, string targetHost, SslProtocols enabledProtocols,
+			X509Certificate serverCertificate, X509CertificateCollection clientCertificates,
+			bool askForClientCert)
 		{
 			return new MonoBtlsContext (
-				parent, serverMode, targetHost,
+				this, serverMode, targetHost,
 				enabledProtocols, serverCertificate,
 				clientCertificates, askForClientCert);
 		}

commit 2140681c9570cd35a8b10bc2386abac143dfd0a7

[System] Add missing ConfigureAwait
This is a candidate fix for:
https://bugzilla.xamarin.com/show_bug.cgi?id=60317

The bug was introduced by 1b5e0f73316ee7b83c8947bc738015443fabd7af.

Preliminary verification
========================

If I make just this _one_ change to a "bad" commit of Mono from after
commit 1b5e0f7, rebuild System.dll in the `xammac` profile, and then
overwrite that file in the MonoBundle directory of a Xamarin.Mac test
app, then I restore the old "good" behavior.  (I started with
38da0b3b4996357a7472e3202c575c9111469721 as the "bad" version for my
tests.)

I believe this confirms that the cause of bug 60317 was the usual
deadlocking issue when awaiting Tasks on the UI thread.  As discussed on
https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html, if
any Tasks awaited anywhere up the call stack are missing
`ConfigureAwait (false)`, then you can run into trouble on the UI
thread.

Additional little checks
========================

1. Do the other `await` statements in commit
1b5e0f73316ee7b83c8947bc738015443fabd7af all look OK?  Yes, it looks
like this was the only `await` statement that was missing
`ConfigureAwait (false)`:

```
$ git show 1b5e0f73316ee7b83c8947bc738015443fabd7af | grep await
+				await ProcessOperation (cancellationToken).ConfigureAwait (false);
+				var ret = await InnerRead (cancellationToken).ConfigureAwait (false);
+					await Parent.InnerWrite (RunSynchronously, cancellationToken);
+				var ret = await Parent.InnerRead (RunSynchronously, requestedSize, cancellationToken).ConfigureAwait (false);
+					result = await asyncRequest.StartOperation (CancellationToken.None).ConfigureAwait (false);
+				result = await asyncRequest.StartOperation (cancellationToken).ConfigureAwait (false);
+			var ret = await task.ConfigureAwait (false);
+			await task.ConfigureAwait (false);
```

2. Is it a concern that mcs/class/System did not contain any calls to
`ConfigureAwait ()` until commit 1b5e0f7:

```

mcs/class/System/Mono.Net.Security/AsyncProtocolRequest.cs
@@ -226,7 +226,7 @@ namespace Mono.Net.Security
 
 				if (Interlocked.Exchange (ref WriteRequested, 0) != 0) {
 					// Flush the write queue.
-					await Parent.InnerWrite (RunSynchronously, cancellationToken);
+					await Parent.InnerWrite (RunSynchronously, cancellationToken).ConfigureAwait (false);
 				}
 
 				Debug ("ProcessOperation done: {0} -> {1}", status, newStatus);