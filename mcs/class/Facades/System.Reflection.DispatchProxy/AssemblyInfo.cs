a60b50417a8a5cd9f9f0ccdb6ecd279b610fb937
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-11-08 06:01:53 +0100

    [Facades] Use same assembly company/product/copyright attributes as other BCL assemblies (#5961)
    * [Facades] Use same assembly company/product/copyright attributes as other BCL assemblies
    
    We can't use Consts.cs since it'd bloat up the facades too much.
    
    Also remove ".dll" suffix from AssemblyTitle/Description to match
    what the Facades in the .NET Framework Reference Assemblies pack
    are using.
    
    * Bump API snapshot submodule

--- mcs/class/Facades/System.Reflection.DispatchProxy/AssemblyInfo.cs
+++ mcs/class/Facades/System.Reflection.DispatchProxy/AssemblyInfo.cs
@@ -24,12 +24,12 @@ using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle ("System.Reflection.DispatchProxy.dll")]
-[assembly: AssemblyDescription ("System.Reflection.DispatchProxy.dll")]
-[assembly: AssemblyDefaultAlias ("System.Reflection.DispatchProxy.dll")]
-[assembly: AssemblyCompany ("Xamarin, Inc.")]
+[assembly: AssemblyTitle ("System.Reflection.DispatchProxy")]
+[assembly: AssemblyDescription ("System.Reflection.DispatchProxy")]
+[assembly: AssemblyDefaultAlias ("System.Reflection.DispatchProxy")]
+[assembly: AssemblyCompany ("Mono development team")]
 [assembly: AssemblyProduct ("Mono Common Language Infrastructure")]
-[assembly: AssemblyCopyright ("Copyright (c) 2016 Xamarin Inc. (http://www.xamarin.com)")]
+[assembly: AssemblyCopyright ("(c) Various Mono authors")]
 [assembly: AssemblyVersion ("4.0.3.0")]
 [assembly: AssemblyInformationalVersion ("4.0.0.0")]
 [assembly: AssemblyFileVersion ("4.0.0.0")]
