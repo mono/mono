fbbde0f0e5283f58accfa43cf0c85f1a20c04bfa
Ankit Jain <radical@gmail.com>
2017-11-09 19:17:15 -0500

    [msbuild] Bump msbuild to mono-2017-10 branch which tracks vs15.5 (#5977)

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -2,8 +2,8 @@ import fileinput
 
 class MSBuild (GitHubPackage):
 	def __init__ (self):
-		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.4',
-			revision = '5bb588162eadfc68c6af8895397f4f65f8008b24')
+		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.5',
+			revision = '98281e4fffa3b63746c16044a6d0162804480927')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
