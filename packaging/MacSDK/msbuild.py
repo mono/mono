af818a17099103fc697e58e0fac9861b80000670
Ankit Jain <radical@gmail.com>
2017-11-10 11:48:30 -0500

    [msbuild] Change the version used in bockbuild to just '15', instead .. (#5986)
    .. of 15.x. It doesn't really affect anything and makes it simpler on
    wrench to specify directory from which to run the tests!
    
    (cherry picked from commit 3f3b106aca5e0ed3d72a2447a84d4729b3cdb36d)
    
    # Conflicts:
    #	packaging/MacSDK/msbuild.py

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -2,7 +2,7 @@ import fileinput
 
 class MSBuild (GitHubPackage):
 	def __init__ (self):
-		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.4',
+		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',
 			revision = '5bb588162eadfc68c6af8895397f4f65f8008b24')
 
 	def build (self):
