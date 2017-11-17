24fc118eefeb87eaa1fd632e4e3416f5e9c7b60e
Ludovic Henry <luhenry@microsoft.com>
2017-11-17 17:21:44 -0500

    [sdks] Cleanup wasm targets (#6025)
    * [sdks] Cleanup wasm targets
    
    * [sdks] Remove CUSTOM_BUILD_TARGETS variable

--- sdks/builds/wasm.mk
+++ sdks/builds/wasm.mk
@@ -16,37 +16,36 @@ WASM_INTERP_CONFIGURE_FLAGS = \
 	--enable-minimal=ssa,com,jit,reflection_emit_save,reflection_emit,portability,assembly_remapping,attach,verifier,full_messages,appdomains,security,sgen_remset,sgen_marksweep_par,sgen_marksweep_fixed,sgen_marksweep_fixed_par,sgen_copying,logging,remoting,shared_perfcounters \
 	--host=i386-apple-darwin10
 
-#toolchain code
-.stamp-wasm-toolchain:
+$(TOP)/sdks/builds/toolchains/emsdk:
 	git clone https://github.com/juj/emsdk.git $(TOP)/sdks/builds/toolchains/emsdk
-	cd $(TOP)/sdks/builds/toolchains/emsdk && ./emsdk install latest && ./emsdk activate --embedded latest
+
+.stamp-wasm-toolchain: | $(TOP)/sdks/builds/toolchains/emsdk
+	cd $(TOP)/sdks/builds/toolchains/emsdk && ./emsdk install latest
+	cd $(TOP)/sdks/builds/toolchains/emsdk && ./emsdk activate --embedded latest
 	touch $@
 
 .stamp-wasm-interp-toolchain: .stamp-wasm-toolchain
 	touch $@
 
-#configure step
 .stamp-wasm-interp-configure: $(TOP)/configure .stamp-wasm-interp-toolchain
 	mkdir -p $(TOP)/sdks/builds/wasm-interp
 	cd $(TOP)/sdks/builds/wasm-interp && source $(TOP)/sdks/builds/toolchains/emsdk/emsdk_env.sh && CFLAGS="-Os -g" emconfigure $(TOP)/configure $(WASM_INTERP_CONFIGURE_FLAGS)
 	touch $@
 
+build-custom-wasm-interp:
+	source $(TOP)/sdks/builds/toolchains/emsdk/emsdk_env.sh && make -C wasm-interp
+
+.PHONY: package-wasm-interp
 package-wasm-interp:
 	$(MAKE) -C $(TOP)/sdks/builds/wasm-interp/mono install
 
-
-#custom build rule
-CUSTOM_BUILD_TARGETS += build-wasm-interp
-build-wasm-interp: .stamp-wasm-interp-configure
-	source $(TOP)/sdks/builds/toolchains/emsdk/emsdk_env.sh && make -C wasm-interp
-
-
-
-.PHONY: clean-wasm-interp clean-wasm
+.PHONY: clean-wasm
 clean-wasm::
-	rm -rf .stamp-wasm-toolchain
+	rm -rf .stamp-wasm-toolchain $(TOP)/sdks/builds/toolchains/emsdk
+
+.PHONY: clean-wasm-interp
 clean-wasm-interp:: clean-wasm
-	rm -rf .stamp-wasm-configure $(TOP)/sdks/builds/toolchains/emcc $(TOP)/sdks/builds/wasm $$(TOP)/sdks/builds/wasm.config.cache
+	rm -rf .stamp-wasm-interp-toolchain .stamp-wasm-interp-configure $(TOP)/sdks/builds/wasm $(TOP)/sdks/builds/wasm.config.cache
 
 TARGETS += wasm-interp
 
