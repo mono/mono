009eee9f1956f1f996982e5c63ce358d2f7b1e2c
Rodrigo Kumpera <kumpera@gmail.com>
2017-11-16 16:48:46 -0800

    [sgen] Move sgen_is_object_alive to sgen-gc.h

--- mono/sgen/sgen-gc.h
+++ mono/sgen/sgen-gc.h
@@ -964,6 +964,23 @@ sgen_major_is_object_alive (GCObject *object)
 	return major_collector.is_object_live (object);
 }
 
+
+/*
+ * If the object has been forwarded it means it's still referenced from a root. 
+ * If it is pinned it's still alive as well.
+ * A LOS object is only alive if we have pinned it.
+ * Return TRUE if @obj is ready to be finalized.
+ */
+static inline gboolean
+sgen_is_object_alive (GCObject *object)
+{
+	if (sgen_ptr_in_nursery (object))
+		return sgen_nursery_is_object_alive (object);
+
+	return sgen_major_is_object_alive (object);
+}
+
+
 /*
  * This function returns true if @object is either alive or it belongs to the old gen
  * and we're currently doing a minor collection.
