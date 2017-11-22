cc985c46c929e53c9c49acb659a0d5a7cde9f1af
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-11-22 01:57:58 +0100

    [ci] Fix testing of msbuild in run-test-mac-sdk.sh
    It contained the old version number and failed on bots that didn't
    have leftover old build result.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -2,7 +2,7 @@ import fileinput
 
 class MSBuild (GitHubPackage):
 	def __init__ (self):
-		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',
+		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
 			revision = '5bb588162eadfc68c6af8895397f4f65f8008b24')
 
 	def build (self):
