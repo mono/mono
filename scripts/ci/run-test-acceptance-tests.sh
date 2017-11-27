41a3571272de1f31f4723fd91cb4e293f1d41ab3
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-11-27 21:49:53 +0100

    [acceptance-tests] Run shorter version of GCStress on PRs and update targets (#6106)
    In 22f0977132c09c17bfc88c6e64b27c9eb1c91c50 it was disabled
    but we can actually just run a shorter version in PRs.
    
    Also reenable the precompilation target for the managed CoreCLR tests.

--- scripts/ci/run-test-acceptance-tests.sh
+++ scripts/ci/run-test-acceptance-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# run the MS test suite
 LANG=en_US.UTF-8 ${TESTCMD} --label=check-ms-test-suite --timeout=10m make -C acceptance-tests check-ms-test-suite
 
 total_tests=$(find acceptance-tests/ -name TestResult*xml | xargs cat | grep -c "<test-case")
@@ -8,9 +9,13 @@ if [ "$total_tests" -lt "1600" ]
 	exit 1
 fi
 
+# run Roslyn tests
 ${TESTCMD} --label=check-roslyn --timeout=60m make -C acceptance-tests check-roslyn
 
-#${TESTCMD} --label=coreclr-compile-tests --timeout=80m --fatal make -C acceptance-tests coreclr-compile-tests
-${TESTCMD} --label=coreclr-runtest-basic --timeout=45m make -C acceptance-tests coreclr-runtest-basic
-${TESTCMD} --label=coreclr-runtest-coremanglib --timeout=90m make -C acceptance-tests coreclr-runtest-coremanglib
-#${TESTCMD} --label=coreclr-gcstress --timeout=1200m make -C acceptance-tests coreclr-gcstress
+# run CoreCLR managed tests, we precompile them in parallel so individual steps don't need to do it
+${TESTCMD} --label=coreclr-compile-tests --timeout=80m --fatal make -C acceptance-tests coreclr-compile-tests
+${TESTCMD} --label=coreclr-runtest-basic --timeout=10m make -C acceptance-tests coreclr-runtest-basic
+${TESTCMD} --label=coreclr-runtest-coremanglib --timeout=10m make -C acceptance-tests coreclr-runtest-coremanglib
+
+# run the GC stress tests (on PRs we only run a short version)
+${TESTCMD} --label=coreclr-gcstress --timeout=1200m make -C acceptance-tests coreclr-gcstress CI_PR=${ghprbPullId}
