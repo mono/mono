9224a1871af7991565c433d0691c37527049a915
Ludovic Henry <ludovic@xamarin.com>
2017-12-07 19:37:32 -0500

    [threads] Fix leak of gchandle to MonoInternalThread
    Fixes https://github.com/mono/mono/issues/6169

--- mono/metadata/threads.c
+++ mono/metadata/threads.c
@@ -741,10 +741,15 @@ mono_thread_attach_internal (MonoThread *thread, gboolean force_attach, gboolean
 static void
 mono_thread_detach_internal (MonoInternalThread *thread)
 {
+	MonoThreadInfo *info;
 	gboolean removed;
+	guint32 gchandle;
 
 	g_assert (thread != NULL);
 	SET_CURRENT_OBJECT (thread);
+
+	info = (MonoThreadInfo*) thread->thread_info;
+	g_assert (info);
 
 	THREAD_DEBUG (g_message ("%s: mono_thread_detach for %p (%"G_GSIZE_FORMAT")", __func__, thread, (gsize)thread->tid));
 
@@ -770,7 +775,7 @@ mono_thread_detach_internal (MonoInternalThread *thread)
 	* race with runtime shutdown.
 	*/
 #ifdef HOST_WIN32
-	mono_threads_add_joinable_runtime_thread (thread->thread_info);
+	mono_threads_add_joinable_runtime_thread (info);
 #endif
 
 	/*
@@ -882,7 +887,12 @@ done:
 	SET_CURRENT_OBJECT (NULL);
 	mono_domain_unset ();
 
-	mono_thread_info_unset_internal_thread_gchandle ((MonoThreadInfo*) thread->thread_info);
+	if (!mono_thread_info_try_get_internal_thread_gchandle (info, &gchandle))
+		g_error ("%s: failed to get gchandle, info = %p", __func__, info);
+
+	mono_gchandle_free (gchandle);
+
+	mono_thread_info_unset_internal_thread_gchandle (info);
 
 	MONO_PROFILER_RAISE (thread_exited, (thread->tid));
 
