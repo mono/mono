f77b0c50961346774dd5f54bcc7d35801ca9585c
andreasr <andreasr@unity3d.com>
2017-12-14 18:32:30 +0100

    [unitytls] Use of CertHelper in TlsProvider

--- mcs/class/System/Mono.UnityTls/UnityTlsProvider.cs
+++ mcs/class/System/Mono.UnityTls/UnityTlsProvider.cs
@@ -75,17 +75,11 @@ namespace Mono.Unity
 
 			// convert cert to native
 			UnityTls.unitytls_errorstate errorState = UnityTls.unitytls_errorstate_create();
-			UnityTls.unitytls_x509verify_result result;
-
 			UnityTls.unitytls_x509list* certificatesNative = UnityTls.unitytls_x509list_create(&errorState);
+			UnityTls.unitytls_x509verify_result result = UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_NOT_DONE;
 			try
 			{
-				foreach (X509Certificate certificate in certificates) {
-					byte[] certDer = certificate.GetRawCertData();
-					fixed(byte* certDerPtr = certDer) {
-						UnityTls.unitytls_x509list_append_der(certificatesNative, certDerPtr, certDer.Length, &errorState);
-					}
-				}
+				CertHelper.AddCertificatesToNativeChain(certificatesNative, certificates, &errorState);
 
 				// validate
 				UnityTls.unitytls_x509list_ref certificatesNativeRef = UnityTls.unitytls_x509list_get_ref(certificatesNative, &errorState);
