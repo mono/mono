98e940dd409782d01626c22c0e4af60ecaf80706
andreasr <andreasr@unity3d.com>
2017-12-14 16:36:45 +0100

    [unitytls] added checking method that evaluates unitytls_x509verify_result as well

--- mcs/class/System/Mono.UnityTLS/Debug.cs
+++ mcs/class/System/Mono.UnityTLS/Debug.cs
@@ -16,8 +16,50 @@ namespace Mono.Unity
 		public static void CheckAndThrow(UnityTls.unitytls_errorstate errorState, string context)
 		{
 			if (errorState.code != UnityTls.unitytls_error_code.UNITYTLS_SUCCESS) {
-				string message = string.Format("{0} - error code {1}", context, errorState.code);
+				string message = string.Format("{0} - error code: {1}", context, errorState.code);
 				throw new TlsException(AlertDescription.InternalError, message);
+			}
+		}
+
+		public static AlertDescription VerifyResultToAlertDescription(UnityTls.unitytls_x509verify_result verifyResult, AlertDescription defaultAlert = AlertDescription.InternalError)
+		{
+			if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_EXPIRED))
+				return AlertDescription.CertificateExpired;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_REVOKED))
+				return AlertDescription.CertificateRevoked;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_CN_MISMATCH))
+				return AlertDescription.UnknownCA;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_NOT_TRUSTED))
+				return AlertDescription.CertificateUnknown;
+
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR1))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR2))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR2))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR3))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR4))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR5))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR6))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR7))
+				return AlertDescription.UserCancelled;
+			else if (verifyResult.HasFlag(UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_FLAG_USER_ERROR8))
+				return AlertDescription.UserCancelled;
+
+			return defaultAlert;
+		}
+
+		public static void CheckAndThrow(UnityTls.unitytls_errorstate errorState, UnityTls.unitytls_x509verify_result verifyResult, string context, AlertDescription defaultAlert = AlertDescription.InternalError)
+		{
+			if (verifyResult != UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_SUCCESS) {
+				AlertDescription alert = VerifyResultToAlertDescription(verifyResult, defaultAlert);
+				string message = string.Format("{0} - error code: {1}, verify result: {2}", context, errorState.code, verifyResult);
+				throw new TlsException(alert, message);
 			}
 		}
 	}
