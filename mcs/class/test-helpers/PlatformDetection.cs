e9e17e069e6949600a382341e8888d7486553f21
Egor Bogatov <egorbo@gmail.com>
2017-12-19 11:37:51 +0100

    [System] System.Net.* tests from corefx (+RemoteExecutorTestBase) (#6136)

--- mcs/class/test-helpers/PlatformDetection.cs
+++ mcs/class/test-helpers/PlatformDetection.cs
@@ -5,9 +5,14 @@ namespace System
 		public static readonly bool IsNetNative = false;
 		public static readonly bool IsNotWinRT = true;
 		public static readonly bool IsWinRT = false;
-		public static readonly bool IsFullFramework = true;
 		public static readonly bool IsWindowsNanoServer = false;
+		public static bool IsFullFramework => true;
 		public static bool IsNonZeroLowerBoundArraySupported => true;
 		public static bool IsUap => false;
+
+		//TODO: check?
+		public static bool IsNotWindowsSubsystemForLinux => true;
+		public static bool IsWindowsSubsystemForLinux => false;
+		public static bool IsFedora => false;
 	}
 }
