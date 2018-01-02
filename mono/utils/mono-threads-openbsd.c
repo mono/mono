5813374445d526e69aaacd253750c589034c6209
Robert Nagy <robert@openbsd.org>
2018-01-02 16:43:58 +0100

    there is no need for pthread_attr_init/destroy anymore

--- mono/utils/mono-threads-openbsd.c
+++ mono/utils/mono-threads-openbsd.c
@@ -13,25 +13,14 @@
 void
 mono_threads_platform_get_stack_bounds (guint8 **staddr, size_t *stsize)
 {
-	/* TODO :   Determine if this code is actually still needed. It may already be covered by the case above. */
-	pthread_attr_t attr;
-	guint8 *current = (guint8*)&attr;
-
-	*staddr = NULL;
-	*stsize = (size_t)-1;
-
-	pthread_attr_init (&attr);
-
 	stack_t ss;
 	int rslt;
 
 	rslt = pthread_stackseg_np (pthread_self (), &ss);
 	g_assert (rslt == 0);
 
-	*staddr = (guint8*)((size_t)ss.ss_sp - ss.ss_size);
+	*staddr = (void*)((size_t)ss.ss_sp - ss.ss_size);
 	*stsize = ss.ss_size;
-
-	pthread_attr_destroy (&attr);
 }
 
 #endif
