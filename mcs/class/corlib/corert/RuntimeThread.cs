575d1f29849ebfb32677e914d3cb0ca0e6fa12e4
Marek Safar <marek.safar@gmail.com>
2018-01-05 15:11:41 +0100

    Bump corert

--- mcs/class/corlib/corert/RuntimeThread.cs
+++ mcs/class/corlib/corert/RuntimeThread.cs
@@ -1,9 +1,14 @@
 namespace Internal.Runtime.Augments
 {
-    class RuntimeThread
-    {
-    	public static void InitializeThreadPoolThread ()
-    	{
-    	}
-    }
+	class RuntimeThread
+	{
+		public static RuntimeThread InitializeThreadPoolThread ()
+		{
+			return default;
+		}
+
+		public void ResetThreadPoolThread ()
+		{
+		}
+	}
 }
