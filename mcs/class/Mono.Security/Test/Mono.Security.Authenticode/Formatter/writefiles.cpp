02b8c2fd1d732f110921e1ce8a8e35e6e98c40fe
Jay Krell <jaykrell@microsoft.com>
2018-01-12 13:08:10 -0800

    tweak

--- mcs/class/Mono.Security/Test/Mono.Security.Authenticode/Formatter/writefiles.cpp
+++ mcs/class/Mono.Security/Test/Mono.Security.Authenticode/Formatter/writefiles.cpp
@@ -10,7 +10,7 @@ typedef unsigned long long ULONGLONG;
 #define IMAGE_DOS_SIGNATURE 0x5A4D
 #define IMAGE_NT_SIGNATURE 0x00004550
 
-    typedef struct _IMAGE_DOS_HEADER {
+typedef struct _IMAGE_DOS_HEADER {
       WORD e_magic;
       WORD e_cblp;
       WORD e_cp;
@@ -30,10 +30,10 @@ typedef unsigned long long ULONGLONG;
       WORD e_oeminfo;
       WORD e_res2[10];
       LONG e_lfanew;
-    } IMAGE_DOS_HEADER,*PIMAGE_DOS_HEADER;
+} IMAGE_DOS_HEADER,*PIMAGE_DOS_HEADER;
 
 
-    typedef struct _IMAGE_FILE_HEADER {
+typedef struct _IMAGE_FILE_HEADER {
       WORD Machine;
       WORD NumberOfSections;
       DWORD TimeDateStamp;
@@ -41,7 +41,7 @@ typedef unsigned long long ULONGLONG;
       DWORD NumberOfSymbols;
       WORD SizeOfOptionalHeader;
       WORD Characteristics;
-    } IMAGE_FILE_HEADER,*PIMAGE_FILE_HEADER;
+} IMAGE_FILE_HEADER,*PIMAGE_FILE_HEADER;
 
 #define IMAGE_SIZEOF_FILE_HEADER 20
 
@@ -52,8 +52,7 @@ typedef unsigned long long ULONGLONG;
 
 #define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 16
 
-    typedef struct _IMAGE_OPTIONAL_HEADER {
-
+typedef struct _IMAGE_OPTIONAL_HEADER {
       WORD Magic;
       BYTE MajorLinkerVersion;
       BYTE MinorLinkerVersion;
@@ -85,9 +84,9 @@ typedef unsigned long long ULONGLONG;
       DWORD LoaderFlags;
       DWORD NumberOfRvaAndSizes;
       IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-    } IMAGE_OPTIONAL_HEADER32,*PIMAGE_OPTIONAL_HEADER32;
+} IMAGE_OPTIONAL_HEADER32,*PIMAGE_OPTIONAL_HEADER32;
 
-    typedef struct _IMAGE_ROM_OPTIONAL_HEADER {
+typedef struct _IMAGE_ROM_OPTIONAL_HEADER {
       WORD Magic;
       BYTE MajorLinkerVersion;
       BYTE MinorLinkerVersion;
@@ -101,9 +100,9 @@ typedef unsigned long long ULONGLONG;
       DWORD GprMask;
       DWORD CprMask[4];
       DWORD GpValue;
-    } IMAGE_ROM_OPTIONAL_HEADER,*PIMAGE_ROM_OPTIONAL_HEADER;
+} IMAGE_ROM_OPTIONAL_HEADER,*PIMAGE_ROM_OPTIONAL_HEADER;
 
-    typedef struct _IMAGE_OPTIONAL_HEADER64 {
+typedef struct _IMAGE_OPTIONAL_HEADER64 {
       WORD Magic;
       BYTE MajorLinkerVersion;
       BYTE MinorLinkerVersion;
@@ -134,7 +133,7 @@ typedef unsigned long long ULONGLONG;
       DWORD LoaderFlags;
       DWORD NumberOfRvaAndSizes;
       IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-    } IMAGE_OPTIONAL_HEADER64,*PIMAGE_OPTIONAL_HEADER64;
+} IMAGE_OPTIONAL_HEADER64,*PIMAGE_OPTIONAL_HEADER64;
 
 #define IMAGE_SIZEOF_ROM_OPTIONAL_HEADER 56
 #define IMAGE_SIZEOF_STD_OPTIONAL_HEADER 28
@@ -145,29 +144,28 @@ typedef unsigned long long ULONGLONG;
 #define IMAGE_NT_OPTIONAL_HDR64_MAGIC 0x20b
 #define IMAGE_ROM_OPTIONAL_HDR_MAGIC 0x107
 
-    typedef struct _IMAGE_NT_HEADERS64 {
+typedef struct _IMAGE_NT_HEADERS64 {
       DWORD Signature;
       IMAGE_FILE_HEADER FileHeader;
       IMAGE_OPTIONAL_HEADER64 OptionalHeader;
-    } IMAGE_NT_HEADERS64,*PIMAGE_NT_HEADERS64;
+} IMAGE_NT_HEADERS64,*PIMAGE_NT_HEADERS64;
 
-    typedef struct _IMAGE_NT_HEADERS {
+typedef struct _IMAGE_NT_HEADERS {
       DWORD Signature;
       IMAGE_FILE_HEADER FileHeader;
       IMAGE_OPTIONAL_HEADER32 OptionalHeader;
     } IMAGE_NT_HEADERS32,*PIMAGE_NT_HEADERS32;
 
-    typedef struct _IMAGE_ROM_HEADERS {
+typedef struct _IMAGE_ROM_HEADERS {
       IMAGE_FILE_HEADER FileHeader;
       IMAGE_ROM_OPTIONAL_HEADER OptionalHeader;
-    } IMAGE_ROM_HEADERS,*PIMAGE_ROM_HEADERS;
-
+} IMAGE_ROM_HEADERS,*PIMAGE_ROM_HEADERS;
 
 #define IMAGE_FIRST_SECTION(ntheader) ((PIMAGE_SECTION_HEADER) ((ULONG_PTR)ntheader + FIELD_OFFSET(IMAGE_NT_HEADERS,OptionalHeader) + ((PIMAGE_NT_HEADERS)(ntheader))->FileHeader.SizeOfOptionalHeader))
 
 #define IMAGE_SIZEOF_SHORT_NAME 8
 
-    typedef struct _IMAGE_SECTION_HEADER {
+typedef struct _IMAGE_SECTION_HEADER {
       BYTE Name[IMAGE_SIZEOF_SHORT_NAME];
       union {
 	DWORD PhysicalAddress;
@@ -181,7 +179,7 @@ typedef unsigned long long ULONGLONG;
       WORD NumberOfRelocations;
       WORD NumberOfLinenumbers;
       DWORD Characteristics;
-    } IMAGE_SECTION_HEADER,*PIMAGE_SECTION_HEADER;
+} IMAGE_SECTION_HEADER,*PIMAGE_SECTION_HEADER;
 
 #define IMAGE_SIZEOF_SECTION_HEADER 40
 
@@ -216,7 +214,7 @@ int main()
 	dos->e_lfanew = 0x1000 - 2;
 	fwrite(dos, sizeof(*dos), 1, f);
 	fclose(f);
-	
+
 	// 4k and PE signature out of range
 	f = open("pe-sig-of-range-4kfile.dll");
 	dos->e_lfanew = 0x1000;
@@ -228,7 +226,7 @@ int main()
 	dos->e_lfanew = 0x1000 - 2;
 	fwrite(buffer, 0x1000, 1, f);
 	fclose(f);
-	
+
 	// now carefully with each field out of range
 	// or less carefully and just do every value
 	for (int i = 0; i < sizeof (IMAGE_NT_HEADERS32); ++i)
@@ -237,7 +235,7 @@ int main()
 		PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)&buffer;
 		PIMAGE_NT_HEADERS32 nt = (PIMAGE_NT_HEADERS32)&buffer[0x1000 - i];
 		char name[100];
-	
+
 		dos->e_lfanew = 0x1000 - i;
 		nt->Signature = IMAGE_NT_SIGNATURE;
 		sprintf(name, "32-%d.dll", i);
