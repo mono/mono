d241a118b4643574f78d091dab44335d15073299
Jay Krell <jaykrell@microsoft.com>
2018-01-16 11:56:51 -0800

    fix C# indent

--- mcs/class/Mono.Security/Test/Mono.Security.Authenticode/Formatter/hashfile.cs
+++ mcs/class/Mono.Security/Test/Mono.Security.Authenticode/Formatter/hashfile.cs
@@ -2,17 +2,17 @@ using System.IO;
 
 public class hashfile
 {
-public static void Main(string[] args)
-{
-	string temp = args[0] + ".temp";
-	File.Delete(temp);
-	File.Copy(args[0], temp);
-	var a = new Mono.Security.Authenticode.AuthenticodeFormatter();
-	var b = a.Sign(temp);
-	System.Console.WriteLine($"hashfile {temp}{b}", temp, b);
+	public static void Main(string[] args)
+	{
+		string temp = args[0] + ".temp";
+		File.Delete(temp);
+		File.Copy(args[0], temp);
+		var a = new Mono.Security.Authenticode.AuthenticodeFormatter();
+		var b = a.Sign(temp);
+		System.Console.WriteLine($"hashfile {temp}{b}", temp, b);
 	
-	// TODO dump the signature
-	// TODO if hosting on windows, run signcode and compare
-	// For now, we are just going after failing cases anyway.
-}
+		// TODO dump the signature
+		// TODO if hosting on windows, run signcode and compare
+		// For now, we are just going after failing cases anyway.
+	}
 }
