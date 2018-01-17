cb1747a70c48786e719912b116d7f4d6e3987327
Manuel de la Pena <mandel@microsoft.com>
2018-01-17 01:00:42 +0100

    [Tests] Skip some of the System.ServiceModel tests when using MOBILE (#6539)
    Mobile does not support all clases found in System.ServiceModel, we skip
    those tests of the not supported classes yet run all the others.

--- mcs/class/System.ServiceModel/Test/System.ServiceModel.Security.Tokens/X509ListedCertificateValidator.cs
+++ mcs/class/System.ServiceModel/Test/System.ServiceModel.Security.Tokens/X509ListedCertificateValidator.cs
@@ -25,6 +25,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+#if !MOBILE
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -64,3 +65,4 @@ namespace MonoTests.System.ServiceModel.Security.Tokens
 		}
 	}
 }
+#endif
