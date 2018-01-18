06b9b48420c2dc4b5d4928217797efb34a56779b
Ankit Jain <radical@gmail.com>
2018-01-18 06:37:33 -0500

    Bump msbuild (#6569)

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'abbd02a3b0fa710c5a208b62bc0ba944b0a5badf')
+			revision = 'e7f1eadcea75f4594e141bfcf9f4d901f56c68ac')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
