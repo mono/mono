d07a00a7b3f2c38f9052ada3c601029867cc7123
Calvin <calvin@cmpct.info>
2018-01-21 20:55:41 -0500

    Work around missing definition on AIX/PASE

--- mono/utils/mono-mmap.c
+++ mono/utils/mono-mmap.c
@@ -351,7 +351,8 @@ mono_mprotect (void *addr, size_t length, int flags)
 			memset (addr, 0, length);
 #else
 		memset (addr, 0, length);
-#ifdef HAVE_MADVISE
+/* AIX doesn't have MADV_FREE */
+#if defined(HAVE_MADVISE) && !defined(_AIX)
 		madvise (addr, length, MADV_DONTNEED);
 		madvise (addr, length, MADV_FREE);
 #else
