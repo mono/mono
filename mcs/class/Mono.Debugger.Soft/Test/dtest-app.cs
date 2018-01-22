123d623cf84f4613cb93a22b494c7f8d5e28493b
Alexander Kyte <alexmkyte@gmail.com>
2018-01-22 15:55:12 -0500

    [runtime] Apply cctor filters to callees of cctors (#6562)

--- mcs/class/Mono.Debugger.Soft/Test/dtest-app.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest-app.cs
@@ -357,6 +357,7 @@ public class Tests : TestsBase, ITest2
 		if (args.Length > 0 && args [0] == "invoke-abort")
 			new Tests ().invoke_abort ();
 		new Tests ().evaluate_method ();
+		Bug59649 ();
 		return 3;
 	}
 
@@ -1720,6 +1721,11 @@ public class Tests : TestsBase, ITest2
 			attach_break ();
 		}
 	}
+
+	public static void Bug59649 ()
+	{
+		UninitializedClass.Call();//Breakpoint here and step in
+	}
 }
 
 public class SentinelClass : MarshalByRefObject {
@@ -1798,5 +1804,23 @@ public class LineNumbers
 	}
 }
 
+class UninitializedClass
+{
+	static string DummyCall()
+	{
+		//Should NOT step into this method
+		//if StepFilter.StaticCtor is set
+		//because this is part of static class initilization
+		return String.Empty;
+	}
+
+	static string staticField = DummyCall();
+
+	public static void Call()
+	{
+		//Should step into this method
+		//Console.WriteLine ("Call called");
+	}
+}
 
 
