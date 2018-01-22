09c124f6dba2a3e8fc26eddfac7e3e237cf59db4
Calvin <calvin@cmpct.info>
2018-01-22 00:55:05 -0500

    Fix lack of casting on sigcontext macros, fix errnos for AIX
    * AIX doesn't distinguish between ENOTEMPTY and EEXIST without a
      define. Handle this case with a conditional cpp directive..
    
    * When ctx is a void pointer, dereferencing as a struct will fail.
      Handle this case with a cast.

--- mono/metadata/w32error-unix.c
+++ mono/metadata/w32error-unix.c
@@ -54,7 +54,9 @@ mono_w32error_unix_to_win32 (guint32 error)
 	case ENOENT:
 	case ENOTDIR: return ERROR_FILE_NOT_FOUND;
 	case ENOSPC: return ERROR_HANDLE_DISK_FULL;
+#if !defined(_AIX) || (defined(_AIX) && defined(_LINUX_SOURCE_COMPAT))
 	case ENOTEMPTY: return ERROR_DIR_NOT_EMPTY;
+#endif
 	case ENOEXEC: return ERROR_BAD_FORMAT;
 	case ENAMETOOLONG: return ERROR_FILENAME_EXCED_RANGE;
 #ifdef EINPROGRESS
--- mono/utils/mono-sigcontext.h
+++ mono/utils/mono-sigcontext.h
@@ -370,10 +370,10 @@ typedef struct ucontext {
 #elif defined(_AIX)
 	typedef ucontext_t os_ucontext;
 
-	#define UCONTEXT_REG_Rn(ctx, n)   ((ctx)->uc_mcontext.jmp_context.gpr[(n)])
-	#define UCONTEXT_REG_FPRn(ctx, n) ((ctx)->uc_mcontext.jmp_context.fpr[(n)])
-	#define UCONTEXT_REG_NIP(ctx)     ((ctx)->uc_mcontext.jmp_context.iar)
-	#define UCONTEXT_REG_LNK(ctx)     ((ctx)->uc_mcontext.jmp_context.lr)
+	#define UCONTEXT_REG_Rn(ctx, n)   (((os_ucontext*)(ctx))->uc_mcontext.jmp_context.gpr[(n)])
+	#define UCONTEXT_REG_FPRn(ctx, n) (((os_ucontext*)(ctx))->uc_mcontext.jmp_context.fpr[(n)])
+	#define UCONTEXT_REG_NIP(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.jmp_context.iar)
+	#define UCONTEXT_REG_LNK(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.jmp_context.lr)
 #endif
 
 #elif defined(TARGET_ARM)
