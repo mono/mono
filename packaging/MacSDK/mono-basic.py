84c32514499fef4da5724b9a09e8c49d226e936e
Marek Safar <marek.safar@gmail.com>
2018-01-29 07:12:43 -0500

    Bump mono-basic to bring latest fixes

--- packaging/MacSDK/mono-basic.py
+++ packaging/MacSDK/mono-basic.py
@@ -2,7 +2,7 @@
 class MonoBasicPackage (GitHubTarballPackage):
 
     def __init__(self):
-        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.6', 'c93133db1d511f994918391f429fee29b9250004',
+        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.6', '0ebb1bf528dd77842d672ce95e185e7e27ba6db1',
                                       configure='./configure --prefix="%{staged_profile}"')
 
     def install(self):
