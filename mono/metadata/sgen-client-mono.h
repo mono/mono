mono/mono

commit d7ac79ccc0edc7de26f319447935f179f893bff6

[profiler] Rework GC roots reporting. (#6144)
This change re-implement GC roots reporting in a way that allows users to correctly track the source of a GC root.

The new design is based on emitting GC root information upfront and only report addresses during heap dumps.

When a GC root is registered we emit an event that is an address range, the root kind, a key and a text description.

A decoder can use this information to match a reported root address with all registration addresses to figure out what they mean.

A GC Root key is used to further its meaning. For example, if kind is thread, key is a tid, if kind is static variables, key is the vtable pointer and so on.

The build of the change is introducing this key argument across all root registration code and rework our root reporting code to encode their addresses.

Some roots use pseudo-addresses when they are not registrable. This is the case of thread stacks/registers and the finalizer queue.

Finally, root reporting was changed to happen only once per collection and at the end, leading to correct and useful data being produced.

mono/metadata/sgen-client-mono.h
@@ -288,28 +288,11 @@ sgen_client_binary_protocol_collection_requested (int generation, size_t request
 	MONO_GC_REQUESTED (generation, requested_size, force);
 }
 
-static void G_GNUC_UNUSED
-sgen_client_binary_protocol_collection_begin (int minor_gc_count, int generation)
-{
-	MONO_GC_BEGIN (generation);
+void
+sgen_client_binary_protocol_collection_begin (int minor_gc_count, int generation);
 
-	MONO_PROFILER_RAISE (gc_event, (MONO_GC_EVENT_START, generation));
-
-#ifndef DISABLE_PERFCOUNTERS
-	if (generation == GENERATION_NURSERY)
-		mono_atomic_inc_i32 (&mono_perfcounters->gc_collections0);
-	else
-		mono_atomic_inc_i32 (&mono_perfcounters->gc_collections1);
-#endif
-}
-
-static void G_GNUC_UNUSED
-sgen_client_binary_protocol_collection_end (int minor_gc_count, int generation, long long num_objects_scanned, long long num_unique_objects_scanned)
-{
-	MONO_GC_END (generation);
-
-	MONO_PROFILER_RAISE (gc_event, (MONO_GC_EVENT_END, generation));
-}
+void
+sgen_client_binary_protocol_collection_end (int minor_gc_count, int generation, long long num_objects_scanned, long long num_unique_objects_scanned);
 
 static void G_GNUC_UNUSED
 sgen_client_binary_protocol_concurrent_start (void)
@@ -684,6 +667,18 @@ sgen_client_binary_protocol_header (long long check, int version, int ptr_size, 
 static void G_GNUC_UNUSED
 sgen_client_binary_protocol_pin_stats (int objects_pinned_in_nursery, size_t bytes_pinned_in_nursery, int objects_pinned_in_major, size_t bytes_pinned_in_major)
 {
+}
+
+static void G_GNUC_UNUSED
+sgen_client_root_registered (char *start, size_t size, int source, void *key, const char *msg)
+{
+	MONO_PROFILER_RAISE (gc_root_register, ((const mono_byte *) start, size, source, key, msg));
+}
+
+static void G_GNUC_UNUSED
+sgen_client_root_deregistered (char *start)
+{
+	MONO_PROFILER_RAISE (gc_root_unregister, ((const mono_byte *) start));
 }
 
 static void G_GNUC_UNUSED

mono/metadata/sgen-mono.c
@@ -443,6 +443,13 @@ sgen_client_zero_array_fill_header (void *p, size_t size)
 	}
 }
 
+MonoVTable *
+mono_gc_get_vtable (MonoObject *obj)
+{
+	// See sgen/sgen-tagged-pointer.h.
+	return SGEN_LOAD_VTABLE (obj);
+}
+
 /*
  * Finalization
  */
@@ -979,13 +986,13 @@ mono_gc_alloc_mature (MonoVTable *vtable, size_t size)
  * mono_gc_alloc_fixed:
  */
 void*
-mono_gc_alloc_fixed (size_t size, MonoGCDescriptor descr, MonoGCRootSource source, const char *msg)
+mono_gc_alloc_fixed (size_t size, MonoGCDescriptor descr, MonoGCRootSource source, void *key, const char *msg)
 {
 	/* FIXME: do a single allocation */
 	void *res = g_calloc (1, size);
 	if (!res)
 		return NULL;
-	if (!mono_gc_register_root ((char *)res, size, descr, source, msg)) {
+	if (!mono_gc_register_root ((char *)res, size, descr, source, key, msg)) {
 		g_free (res);
 		res = NULL;
 	}
@@ -1875,11 +1882,13 @@ mono_gc_set_string_length (MonoString *str, gint32 new_length)
  */
 
 #define GC_ROOT_NUM 32
+#define SPECIAL_ADDRESS_FIN_QUEUE ((void*)1)
+#define SPECIAL_ADDRESS_CRIT_FIN_QUEUE ((void*)2)
+
 typedef struct {
 	int count;		/* must be the first field */
+	void *addresses [GC_ROOT_NUM];
 	void *objects [GC_ROOT_NUM];
-	int root_types [GC_ROOT_NUM];
-	uintptr_t extra_info [GC_ROOT_NUM];
 } GCRootReport;
 
 static void
@@ -1887,63 +1896,34 @@ notify_gc_roots (GCRootReport *report)
 {
 	if (!report->count)
 		return;
-	MONO_PROFILER_RAISE (gc_roots, ((MonoObject **) report->objects, (MonoProfilerGCRootType *) report->root_types, report->extra_info, report->count));
+	MONO_PROFILER_RAISE (gc_roots, (report->count, (const mono_byte *const *)report->addresses, (MonoObject *const *) report->objects));
 	report->count = 0;
 }
 
 static void
-add_profile_gc_root (GCRootReport *report, void *object, int rtype, uintptr_t extra_info)
+report_gc_root (GCRootReport *report, void *address, void *object)
 {
 	if (report->count == GC_ROOT_NUM)
 		notify_gc_roots (report);
+	report->addresses [report->count] = address;
 	report->objects [report->count] = object;
-	report->root_types [report->count] = rtype;
-	report->extra_info [report->count++] = (uintptr_t)SGEN_LOAD_VTABLE (object)->klass;
+	report->count++;
 }
-
-void
-sgen_client_nursery_objects_pinned (void **definitely_pinned, int count)
-{
-	if (MONO_PROFILER_ENABLED (gc_roots)) {
-		GCRootReport report;
-		int idx;
-		report.count = 0;
-		for (idx = 0; idx < count; ++idx)
-			add_profile_gc_root (&report, definitely_pinned [idx], MONO_PROFILER_GC_ROOT_PINNING | MONO_PROFILER_GC_ROOT_MISC, 0);
-		notify_gc_roots (&report);
-	}
-}
-
-static void
-report_finalizer_roots_from_queue (SgenPointerQueue *queue)
-{
-	GCRootReport report;
-	size_t i;
-
-	report.count = 0;
-	for (i = 0; i < queue->next_slot; ++i) {
-		void *obj = queue->data [i];
-		if (!obj)
-			continue;
-		add_profile_gc_root (&report, obj, MONO_PROFILER_GC_ROOT_FINALIZER, 0);
-	}
-	notify_gc_roots (&report);
-}
-
-static void
-report_finalizer_roots (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
-{
-	report_finalizer_roots_from_queue (fin_ready_queue);
-	report_finalizer_roots_from_queue (critical_fin_queue);
-}
-
-static GCRootReport *root_report;
 
 static void
 single_arg_report_root (MonoObject **obj, void *gc_data)
 {
+	GCRootReport *report = gc_data;
 	if (*obj)
-		add_profile_gc_root (root_report, *obj, MONO_PROFILER_GC_ROOT_OTHER, 0);
+		report_gc_root (report, obj, *obj);
+}
+
+static void
+two_args_report_root (void *address, MonoObject *obj, void *gc_data)
+{
+	GCRootReport *report = gc_data;
+	if (obj)
+		report_gc_root (report, address, obj);
 }
 
 static void
@@ -1953,9 +1933,8 @@ precisely_report_roots_from (GCRootReport *report, void** start_root, void** end
 	case ROOT_DESC_BITMAP:
 		desc >>= ROOT_DESC_TYPE_SHIFT;
 		while (desc) {
-			if ((desc & 1) && *start_root) {
-				add_profile_gc_root (report, *start_root, MONO_PROFILER_GC_ROOT_OTHER, 0);
-			}
+			if ((desc & 1) && *start_root)
+				report_gc_root (report, start_root, *start_root);
 			desc >>= 1;
 			start_root++;
 		}
@@ -1969,9 +1948,8 @@ precisely_report_roots_from (GCRootReport *report, void** start_root, void** end
 			gsize bmap = *bitmap_data++;
 			void **objptr = start_run;
 			while (bmap) {
-				if ((bmap & 1) && *objptr) {
-					add_profile_gc_root (report, *objptr, MONO_PROFILER_GC_ROOT_OTHER, 0);
-				}
+				if ((bmap & 1) && *objptr)
+					report_gc_root (report, objptr, *objptr);
 				bmap >>= 1;
 				++objptr;
 			}
@@ -1984,14 +1962,17 @@ precisely_report_roots_from (GCRootReport *report, void** start_root, void** end
 
 		for (p = start_root; p < end_root; p++) {
 			if (*p)
-				add_profile_gc_root (report, *p, MONO_PROFILER_GC_ROOT_OTHER, 0);
+				report_gc_root (report, p, *p);
 		}
 		break;
 	}
 	case ROOT_DESC_USER: {
 		MonoGCRootMarkFunc marker = (MonoGCRootMarkFunc)sgen_get_user_descriptor_func (desc);
-		root_report = report;
-		marker ((MonoObject**)start_root, single_arg_report_root, NULL);
+
+		if ((void*)marker == (void*)sgen_mark_normal_gc_handles)
+			sgen_gc_handles_report_roots (two_args_report_root, report);
+		else
+			marker ((MonoObject**)start_root, single_arg_report_root, report);
 		break;
 	}
 	case ROOT_DESC_RUN_LEN:
@@ -2002,15 +1983,194 @@ precisely_report_roots_from (GCRootReport *report, void** start_root, void** end
 }
 
 static void
-report_registered_roots_by_type (int root_type)
+report_pinning_roots (GCRootReport *report, void **start, void **end)
+{
+	while (start < end) {
+		mword addr = (mword)*start;
+		addr &= ~(SGEN_ALLOC_ALIGN - 1);
+		if (addr)
+			report_gc_root (report, start, (void*)addr);
+
+		start++;
+	}
+}
+
+static SgenPointerQueue pinned_objects = SGEN_POINTER_QUEUE_INIT (INTERNAL_MEM_MOVED_OBJECT);
+static mword lower_bound, upper_bound;
+
+static GCObject*
+find_pinned_obj (char *addr)
+{
+	size_t idx = sgen_pointer_queue_search (&pinned_objects, addr);
+
+	if (idx != pinned_objects.next_slot) {
+		if (pinned_objects.data [idx] == addr)
+			return pinned_objects.data [idx];
+		if (idx == 0)
+			return NULL;
+	}
+
+	GCObject *obj = pinned_objects.data [idx - 1];
+	if (addr > (char*)obj && addr < ((char*)obj + sgen_safe_object_get_size (obj)))
+		return obj;
+	return NULL;
+}
+
+
+/*
+ * We pass @root_report_address so register are properly accounted towards their thread
+*/
+static void
+report_conservative_roots (GCRootReport *report, char *root_report_address, void **start, void **end)
+{
+	while (start < end) {
+		mword addr = (mword)*start;
+		addr &= ~(SGEN_ALLOC_ALIGN - 1);
+
+		if (addr < lower_bound || addr > upper_bound) {
+			++start;
+			continue;
+		}
+
+		GCObject *obj = find_pinned_obj ((char*)addr);
+		if (obj)
+			report_gc_root (report, root_report_address, obj);
+		start++;
+	}
+}
+
+typedef struct {
+	gboolean precise;
+	GCRootReport *report;
+	SgenThreadInfo *info;
+} ReportHandleStackRoot;
+
+static void
+report_handle_stack_root (gpointer *ptr, gpointer user_data)
+{
+	ReportHandleStackRoot *ud = user_data;
+	GCRootReport *report = ud->report;
+	gpointer addr = ud->info->client_info.info.handle_stack;
+
+	// Note: We know that *ptr != NULL.
+	if (ud->precise)
+		report_gc_root (report, addr, *ptr);
+	else
+		report_conservative_roots (report, addr, ptr, ptr + 1);
+}
+
+static void
+report_handle_stack_roots (GCRootReport *report, SgenThreadInfo *info, gboolean precise)
+{
+	ReportHandleStackRoot ud = {
+		.precise = precise,
+		.report = report,
+		.info = info,
+	};
+
+	mono_handle_stack_scan ((HandleStack *) info->client_info.info.handle_stack, report_handle_stack_root, &ud, ud.precise, FALSE);
+}
+
+static void
+report_stack_roots (void)
+{
+	GCRootReport report = {0};
+	FOREACH_THREAD (info) {
+		void *aligned_stack_start;
+
+		if (info->client_info.skip) {
+			continue;
+		} else if (info->client_info.gc_disabled) {
+			continue;
+		} else if (!mono_thread_info_is_live (info)) {
+			continue;
+		} else if (!info->client_info.stack_start) {
+			continue;
+		}
+
+		g_assert (info->client_info.stack_start);
+		g_assert (info->client_info.info.stack_end);
+
+		aligned_stack_start = (void*)(mword) ALIGN_TO ((mword)info->client_info.stack_start, SIZEOF_VOID_P);
+#ifdef HOST_WIN32
+		/* Windows uses a guard page before the committed stack memory pages to detect when the
+		   stack needs to be grown. If we suspend a thread just after a function prolog has
+		   decremented the stack pointer to point into the guard page but before the thread has
+		   been able to read or write to that page, starting the stack scan at aligned_stack_start
+		   will raise a STATUS_GUARD_PAGE_VIOLATION and the process will crash. This code uses
+		   VirtualQuery() to determine whether stack_start points into the guard page and then
+		   updates aligned_stack_start to point at the next non-guard page. */
+		MEMORY_BASIC_INFORMATION mem_info;
+		SIZE_T result = VirtualQuery (info->client_info.stack_start, &mem_info, sizeof(mem_info));
+		g_assert (result != 0);
+		if (mem_info.Protect & PAGE_GUARD) {
+			aligned_stack_start = ((char*) mem_info.BaseAddress) + mem_info.RegionSize;
+		}
+#endif
+
+		g_assert (info->client_info.suspend_done);
+
+		report_conservative_roots (&report, aligned_stack_start, (void **)aligned_stack_start, (void **)info->client_info.info.stack_end);
+		report_conservative_roots (&report, aligned_stack_start, (void**)&info->client_info.ctx, (void**)(&info->client_info.ctx + 1));
+
+		report_handle_stack_roots (&report, info, FALSE);
+		report_handle_stack_roots (&report, info, TRUE);
+	} FOREACH_THREAD_END
+
+	notify_gc_roots (&report);
+}
+
+static void
+report_pin_queue (void)
+{
+	lower_bound = SIZE_MAX;
+	upper_bound = 0;
+
+	//sort the addresses
+	sgen_pointer_queue_sort_uniq (&pinned_objects);
+
+	for (int i = 0; i < pinned_objects.next_slot; ++i) {
+		GCObject *obj = pinned_objects.data [i];
+		ssize_t size = sgen_safe_object_get_size (obj);
+
+		ssize_t addr = (ssize_t)obj;
+		lower_bound = MIN (lower_bound, addr);
+		upper_bound = MAX (upper_bound, addr + size);
+	}
+
+	report_stack_roots ();
+	sgen_pointer_queue_clear (&pinned_objects);
+}
+
+static void
+report_finalizer_roots_from_queue (SgenPointerQueue *queue, void* queue_address)
 {
 	GCRootReport report;
+	size_t i;
+
+	report.count = 0;
+	for (i = 0; i < queue->next_slot; ++i) {
+		void *obj = queue->data [i];
+		if (!obj)
+			continue;
+		report_gc_root (&report, queue_address, obj);
+	}
+	notify_gc_roots (&report);
+}
+
+static void
+report_registered_roots_by_type (int root_type)
+{
+	GCRootReport report = { 0 };
 	void **start_root;
 	RootRecord *root;
 	report.count = 0;
 	SGEN_HASH_TABLE_FOREACH (&roots_hash [root_type], void **, start_root, RootRecord *, root) {
-		SGEN_LOG (6, "Precise root scan %p-%p (desc: %p)", start_root, root->end_root, (void*)root->root_desc);
-		precisely_report_roots_from (&report, start_root, (void**)root->end_root, root->root_desc);
+		SGEN_LOG (6, "Profiler root scan %p-%p (desc: %p)", start_root, root->end_root, (void*)root->root_desc);
+		if (root_type == ROOT_TYPE_PINNED)
+			report_pinning_roots (&report, start_root, (void**)root->end_root);
+		else
+			precisely_report_roots_from (&report, start_root, (void**)root->end_root, root->root_desc);
 	} SGEN_HASH_TABLE_FOREACH_END;
 	notify_gc_roots (&report);
 }
@@ -2018,52 +2178,85 @@ report_registered_roots_by_type (int root_type)
 static void
 report_registered_roots (void)
 {
-	report_registered_roots_by_type (ROOT_TYPE_NORMAL);
-	report_registered_roots_by_type (ROOT_TYPE_WBARRIER);
+	for (int i = 0; i < ROOT_TYPE_NUM; ++i)
+		report_registered_roots_by_type (i);
+}
+
+static void
+sgen_report_all_roots (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
+{
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
+
+	report_registered_roots ();
+	report_pin_queue ();
+	report_finalizer_roots_from_queue (fin_ready_queue, SPECIAL_ADDRESS_FIN_QUEUE);
+	report_finalizer_roots_from_queue (critical_fin_queue, SPECIAL_ADDRESS_CRIT_FIN_QUEUE);
 }
 
 void
-sgen_client_collecting_minor (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
+sgen_client_pinning_start (void)
 {
-	if (MONO_PROFILER_ENABLED (gc_roots))
-		report_registered_roots ();
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
 
-	if (MONO_PROFILER_ENABLED (gc_roots))
-		report_finalizer_roots (fin_ready_queue, critical_fin_queue);
+	sgen_pointer_queue_clear (&pinned_objects);
 }
 
-static GCRootReport major_root_report;
-static gboolean profile_roots;
+void
+sgen_client_pinning_end (void)
+{
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
+}
 
 void
-sgen_client_collecting_major_1 (void)
+sgen_client_nursery_objects_pinned (void **definitely_pinned, int count)
 {
-	profile_roots = MONO_PROFILER_ENABLED (gc_roots);
-	memset (&major_root_report, 0, sizeof (GCRootReport));
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
+
+	for (int i = 0; i < count; ++i)
+		sgen_pointer_queue_add (&pinned_objects, definitely_pinned [i]);
 }
 
 void
 sgen_client_pinned_los_object (GCObject *obj)
 {
-	if (profile_roots)
-		add_profile_gc_root (&major_root_report, (char*)obj, MONO_PROFILER_GC_ROOT_PINNING | MONO_PROFILER_GC_ROOT_MISC, 0);
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
+
+	sgen_pointer_queue_add (&pinned_objects, obj);
 }
 
 void
-sgen_client_collecting_major_2 (void)
+sgen_client_pinned_cemented_object (GCObject *obj)
 {
-	if (profile_roots)
-		notify_gc_roots (&major_root_report);
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
 
-	if (MONO_PROFILER_ENABLED (gc_roots))
-		report_registered_roots ();
+	// TODO: How do we report this in a way that makes sense?
 }
 
 void
-sgen_client_collecting_major_3 (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
+sgen_client_pinned_major_heap_object (GCObject *obj)
 {
-	if (MONO_PROFILER_ENABLED (gc_roots))
-		report_finalizer_roots (fin_ready_queue, critical_fin_queue);
+	if (!MONO_PROFILER_ENABLED (gc_roots))
+		return;
+
+	sgen_pointer_queue_add (&pinned_objects, obj);
+}
+
+void
+sgen_client_collecting_minor_report_roots (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
+{
+	sgen_report_all_roots (fin_ready_queue, critical_fin_queue);
+}
+
+void
+sgen_client_collecting_major_report_roots (SgenPointerQueue *fin_ready_queue, SgenPointerQueue *critical_fin_queue)
+{
+	sgen_report_all_roots (fin_ready_queue, critical_fin_queue);
 }
 
 #define MOVED_OBJECTS_NUM 64
@@ -2454,12 +2647,12 @@ sgen_client_scan_thread_data (void *start_nursery, void *end_nursery, gboolean p
 			  beginning of the object.
 			*/
 			if (precise)
-				mono_handle_stack_scan ((HandleStack*)info->client_info.info.handle_stack, (GcScanFunc)ctx.ops->copy_or_mark_object, ctx.queue, precise);
+				mono_handle_stack_scan ((HandleStack*)info->client_info.info.handle_stack, (GcScanFunc)ctx.ops->copy_or_mark_object, ctx.queue, precise, TRUE);
 			else {
 				PinHandleStackInteriorPtrData ud = { .start_nursery = start_nursery,
 								     .end_nursery = end_nursery,
 				};
-				mono_handle_stack_scan ((HandleStack*)info->client_info.info.handle_stack, pin_handle_stack_interior_ptrs, &ud, precise);
+				mono_handle_stack_scan ((HandleStack*)info->client_info.info.handle_stack, pin_handle_stack_interior_ptrs, &ud, precise, FALSE);
 			}
 		}
 	} FOREACH_THREAD_END
@@ -2490,15 +2683,15 @@ mono_gc_set_stack_end (void *stack_end)
  */
 
 int
-mono_gc_register_root (char *start, size_t size, MonoGCDescriptor descr, MonoGCRootSource source, const char *msg)
+mono_gc_register_root (char *start, size_t size, MonoGCDescriptor descr, MonoGCRootSource source, void *key, const char *msg)
 {
-	return sgen_register_root (start, size, descr, descr ? ROOT_TYPE_NORMAL : ROOT_TYPE_PINNED, source, msg);
+	return sgen_register_root (start, size, descr, descr ? ROOT_TYPE_NORMAL : ROOT_TYPE_PINNED, source, key, msg);
 }
 
 int
-mono_gc_register_root_wbarrier (char *start, size_t size, MonoGCDescriptor descr, MonoGCRootSource source, const char *msg)
+mono_gc_register_root_wbarrier (char *start, size_t size, MonoGCDescriptor descr, MonoGCRootSource source, void *key, const char *msg)
 {
-	return sgen_register_root (start, size, descr, ROOT_TYPE_WBARRIER, source, msg);
+	return sgen_register_root (start, size, descr, ROOT_TYPE_WBARRIER, source, key, msg);
 }
 
 void
@@ -2532,6 +2725,8 @@ mono_gc_pthread_create (pthread_t *new_thread, const pthread_attr_t *attr, void 
 void
 sgen_client_total_allocated_heap_changed (size_t allocated_heap)
 {
+	MONO_PROFILER_RAISE (gc_resize, (allocated_heap));
+
 	mono_runtime_resource_check_limit (MONO_RESOURCE_GC_HEAP, allocated_heap);
 }
 
@@ -3109,4 +3304,34 @@ sgen_client_get_weak_bitmap (MonoVTable *vt, int *nbits)
 	return mono_class_get_weak_bitmap (klass, nbits);
 }
 
+void
+sgen_client_binary_protocol_collection_begin (int minor_gc_count, int generation)
+{
+	static gboolean pseudo_roots_registered;
+
+	MONO_GC_BEGIN (generation);
+
+	MONO_PROFILER_RAISE (gc_event, (MONO_GC_EVENT_START, generation));
+
+	if (!pseudo_roots_registered) {
+		pseudo_roots_registered = TRUE;
+		MONO_PROFILER_RAISE (gc_root_register, (SPECIAL_ADDRESS_FIN_QUEUE, 1, MONO_ROOT_SOURCE_FINALIZER_QUEUE, NULL, "Finalizer Queue"));
+		MONO_PROFILER_RAISE (gc_root_register, (SPECIAL_ADDRESS_CRIT_FIN_QUEUE, 1, MONO_ROOT_SOURCE_FINALIZER_QUEUE, NULL, "Finalizer Queue (Critical)"));
+	}
+
+#ifndef DISABLE_PERFCOUNTERS
+	if (generation == GENERATION_NURSERY)
+		mono_atomic_inc_i32 (&mono_perfcounters->gc_collections0);
+	else
+		mono_atomic_inc_i32 (&mono_perfcounters->gc_collections1);
+#endif
+}
+
+void
+sgen_client_binary_protocol_collection_end (int minor_gc_count, int generation, long long num_objects_scanned, long long num_unique_objects_scanned)
+{
+	MONO_GC_END (generation);
+
+	MONO_PROFILER_RAISE (gc_event, (MONO_GC_EVENT_END, generation));
+}
 #endif

mcs/class/Mono.Profiler.Log/Makefile
@@ -6,7 +6,7 @@ LIBRARY_SNK = ../mono.snk
 
 LIB_REFS = System System.Core
 KEYFILE = $(LIBRARY_SNK)
-LIB_MCS_FLAGS = /unsafe /publicsign
+LIB_MCS_FLAGS = /unsafe /publicsign /nowarn:0618
 
 LIBRARY_WARN_AS_ERROR = yes
 

mcs/class/Mono.Profiler.Log/Mono.Profiler.Log/LogEnums.cs
@@ -54,6 +54,8 @@ namespace Mono.Profiler.Log {
 		HeapEnd = 1 << 4,
 		HeapObject = 2 << 4,
 		HeapRoots = 3 << 4,
+		HeapRootRegister = 4 << 4,
+		HeapRootUnregister = 5 << 4,
 
 		SampleHit = 0 << 4,
 		SampleUnmanagedSymbol = 1 << 4,
@@ -74,6 +76,7 @@ namespace Mono.Profiler.Log {
 		AppDomain = 4,
 		Thread = 5,
 		Context = 6,
+		VTable = 7,
 	}
 
 	// mono/utils/mono-counters.h : MONO_COUNTER_*
@@ -133,6 +136,7 @@ namespace Mono.Profiler.Log {
 
 	// mono/metadata/profiler.h : MonoProfilerGCRootType
 	[Flags]
+	[Obsolete ("The event field using this enum is no longer produced.")]
 	public enum LogHeapRootAttributes {
 		Pinning = 1 << 8,
 		WeakReference = 2 << 8,
@@ -145,6 +149,25 @@ namespace Mono.Profiler.Log {
 		Miscellaneous = 1 << 4,
 
 		TypeMask = 0xff,
+	}
+
+	// mono/metadata/mono-gc.h : MonoGCRootSource
+	public enum LogHeapRootSource {
+		External = 0,
+		Stack = 1,
+		FinalizerQueue = 2,
+		Static = 3,
+		ThreadStatic = 4,
+		ContextStatic = 5,
+		GCHandle = 6,
+		Jit = 7,
+		Threading = 8,
+		AppDomain = 9,
+		Reflection = 10,
+		Marshal = 11,
+		ThreadPool = 12,
+		Debugger = 13,
+		Handle = 14,
 	}
 
 	// mono/profiler/log.h : MonoProfilerMonitorEvent

mcs/class/Mono.Profiler.Log/Mono.Profiler.Log/LogEventVisitor.cs
@@ -66,6 +66,10 @@ namespace Mono.Profiler.Log {
 		{
 		}
 
+		public virtual void Visit (VTableLoadEvent ev)
+		{
+		}
+
 		public virtual void Visit (JitEvent ev)
 		{
 		}
@@ -91,6 +95,14 @@ namespace Mono.Profiler.Log {
 		}
 
 		public virtual void Visit (HeapRootsEvent ev)
+		{
+		}
+
+		public virtual void Visit (HeapRootRegisterEvent ev)
+		{
+		}
+
+		public virtual void Visit (HeapRootUnregisterEvent ev)
 		{
 		}
 

mcs/class/Mono.Profiler.Log/Mono.Profiler.Log/LogEvents.cs
@@ -161,6 +161,20 @@ namespace Mono.Profiler.Log {
 		}
 	}
 
+	public sealed class VTableLoadEvent : LogEvent {
+
+		public long VTablePointer { get; internal set; }
+
+		public long AppDomainId { get; internal set; }
+
+		public long ClassPointer { get; internal set; }
+
+		internal override void Accept (LogEventVisitor visitor)
+		{
+			visitor.Visit (this);
+		}
+	}
+
 	public sealed class JitEvent : LogEvent {
 
 		public long MethodPointer { get; internal set; }
@@ -195,7 +209,10 @@ namespace Mono.Profiler.Log {
 
 	public sealed class AllocationEvent : LogEvent {
 
+		[Obsolete ("This field is no longer produced.")]
 		public long ClassPointer { get; internal set; }
+
+		public long VTablePointer { get; internal set; }
 
 		public long ObjectPointer { get; internal set; }
 
@@ -236,7 +253,10 @@ namespace Mono.Profiler.Log {
 
 		public long ObjectPointer { get; internal set; }
 
+		[Obsolete ("This field is no longer produced.")]
 		public long ClassPointer { get; internal set; }
+
+		public long VTablePointer { get; internal set; }
 
 		public long ObjectSize { get; internal set; }
 
@@ -252,16 +272,49 @@ namespace Mono.Profiler.Log {
 
 		public struct HeapRoot {
 
+			public long AddressPointer { get; internal set; }
+
 			public long ObjectPointer { get; internal set; }
 
+			[Obsolete ("This field is no longer produced.")]
 			public LogHeapRootAttributes Attributes { get; internal set; }
 
+			[Obsolete ("This field is no longer produced.")]
 			public long ExtraInfo { get; internal set; }
 		}
 
+		[Obsolete ("This field is no longer produced.")]
 		public long MaxGenerationCollectionCount { get; internal set; }
 
 		public IReadOnlyList<HeapRoot> Roots { get; internal set; }
+
+		internal override void Accept (LogEventVisitor visitor)
+		{
+			visitor.Visit (this);
+		}
+	}
+
+	public sealed class HeapRootRegisterEvent : LogEvent {
+
+		public long RootPointer { get; internal set; }
+
+		public long RootSize { get; internal set; }
+
+		public LogHeapRootSource Source { get; internal set; }
+
+		public long Key { get; internal set; }
+
+		public string Name { get; internal set; }
+
+		internal override void Accept (LogEventVisitor visitor)
+		{
+			visitor.Visit (this);
+		}
+	}
+
+	public sealed class HeapRootUnregisterEvent : LogEvent {
+
+		public long RootPointer { get; internal set; }
 
 		internal override void Accept (LogEventVisitor visitor)
 		{

commit 6e596beb22e7ad78c998ad2ba0b9f58433ffc25d

[sgen] Don't report gc_resize profiler events from worker threads.
See 4acc3dfeedb4087a99179cd64b59419c931a6c3f for why this is problematic. The
bug was introduced in d7ac79ccc0edc7de26f319447935f179f893bff6.

This fixes an occasional deadlock in the profiler stress tests.

mono/metadata/sgen-client-mono.h
@@ -77,6 +77,7 @@ struct _SgenClientThreadInfo {
 
 extern void mono_sgen_register_moved_object (void *obj, void *destination);
 extern void mono_sgen_gc_event_moves (void);
+extern void mono_sgen_gc_event_resize (void);
 
 extern void mono_sgen_init_stw (void);
 

mono/metadata/sgen-mono.c
@@ -2756,12 +2756,30 @@ mono_gc_pthread_create (pthread_t *new_thread, const pthread_attr_t *attr, void 
  * Miscellaneous
  */
 
+static size_t last_heap_size = -1;
+static size_t worker_heap_size;
+
 void
 sgen_client_total_allocated_heap_changed (size_t allocated_heap)
 {
-	MONO_PROFILER_RAISE (gc_resize, (allocated_heap));
-
 	mono_runtime_resource_check_limit (MONO_RESOURCE_GC_HEAP, allocated_heap);
+
+	/*
+	 * This function can be called from SGen's worker threads. We want to try
+	 * and avoid exposing those threads to the profiler API, so save the heap
+	 * size value and report it later when the main GC thread calls
+	 * mono_sgen_gc_event_resize ().
+	 */
+	worker_heap_size = allocated_heap;
+}
+
+void
+mono_sgen_gc_event_resize (void)
+{
+	if (worker_heap_size != last_heap_size) {
+		last_heap_size = worker_heap_size;
+		MONO_PROFILER_RAISE (gc_resize, (last_heap_size));
+	}
 }
 
 gboolean

mono/metadata/sgen-stw.c
@@ -151,6 +151,9 @@ sgen_client_restart_world (int generation, gboolean serial_collection, gint64 *s
 	if (MONO_PROFILER_ENABLED (gc_moves))
 		mono_sgen_gc_event_moves ();
 
+	if (MONO_PROFILER_ENABLED (gc_resize))
+		mono_sgen_gc_event_resize ();
+
 	MONO_PROFILER_RAISE (gc_event, (MONO_GC_EVENT_PRE_START_WORLD, generation));
 	MONO_PROFILER_RAISE (gc_event2, (MONO_GC_EVENT_PRE_START_WORLD, generation, serial_collection));
 