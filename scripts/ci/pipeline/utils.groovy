f7df2f1633f9f8c6fc4e09f4524b02aea1afc22d
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-02-05 17:24:58 +0100

    [ci] Report pending/failed status on GitHub for pkg/msi

---
+++ scripts/ci/pipeline/utils.groovy
@@ -0,0 +1,11 @@
+def reportGitHubStatus(commitHash, context, backref, statusResult, statusResultMessage) {
+    step([
+        $class: 'GitHubCommitStatusSetter',
+        commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitHash],
+        contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: context],
+        statusBackrefSource: [$class: 'ManuallyEnteredBackrefSource', backref: backref],
+        statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', state: statusResult, message: statusResultMessage]]]
+    ])
+}
+
+return this
