b0898c628c5920459cd48f9c1e5b9f2a7ab6a958
Ludovic Henry <luhenry@microsoft.com>
2018-02-06 18:56:32 -0500

    [sdks] Bring back download of llvm for iOS (#6818)
    Revert part of https://github.com/mono/mono/commit/605cadfb09d6c79723763b8afd09480a5941eabf

---
+++ sdks/builds/download-llvm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -ex
+
+LLVM_REV=$2
+
+URL=http://xamjenkinsartifact.blob.core.windows.net/build-package-osx-llvm/llvm-osx64-$LLVM_REV.tar.gz
+
+wget -O tmp.tar.gz --show-progress $URL
+
+rm -rf llvm-tmp
+mkdir -p llvm-tmp
+tar -xC llvm-tmp -f tmp.tar.gz
+rm -rf ../out/ios-{llvm32,llvm64}
+mkdir -p ../out/ios-llvm32 ../out/ios-llvm64
+cp -r llvm-tmp/usr64/* ../out/ios-llvm64
+cp -r llvm-tmp/usr32/* ../out/ios-llvm32
+rm -rf llvm-tmp tmp.tar.gz
