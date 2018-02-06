20ada1bc3bf926f9ae43401a956eb45b4df7a182
monojenkins <jo.shields+jenkins@xamarin.com>
2018-02-06 13:13:44 -0500

    [2018-02] [runtime] Add a 'frame_addr' field to MonoStackFrameInfo, which is equal to either interp_frame or ctx->sp. (#6813)
    * Add a 'frame_addr' field to MonoStackFrameInfo, which is equal to either interp_frame or ctx->sp.
    
    Use this field to compare frames with each other or with stack addresses instead of setting ctx->sp to
    the interp frame address, which breaks mixed mode, since mixed mode assumes ctx->sp is the real
    sp of the last native interpreter frame.
    
    * Reenable mixed mode tests.

--- scripts/ci/run-test-interpreter.sh
+++ scripts/ci/run-test-interpreter.sh
@@ -4,7 +4,7 @@ export TESTCMD=`dirname "${BASH_SOURCE[0]}"`/run-step.sh
 export TEST_WITH_INTERPRETER=1
 
 ${TESTCMD} --label=interpreter-regression --timeout=10m make -C mono/mini richeck
-#${TESTCMD} --label=mixedmode-regression --timeout=10m make -C mono/mini mixedcheck
+${TESTCMD} --label=mixedmode-regression --timeout=10m make -C mono/mini mixedcheck
 ${TESTCMD} --label=compile-runtime-tests --timeout=40m make -w -C mono/tests -j4 tests
 ${TESTCMD} --label=runtime-interp --timeout=160m make -w -C mono/tests -k testinterp V=1 CI=1 CI_PR=${ghprbPullId}
 ${TESTCMD} --label=corlib --timeout=160m make -w -C mcs/class/corlib run-test V=1
