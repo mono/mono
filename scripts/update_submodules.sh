c944c358f1379e21fb5ba5e0f1b40ac73453c828
jaykrell <jay.krell@cornell.edu>
2018-02-11 20:15:25 -0800

    [scripts] Fix scripts/update_submodules.sh for use with git worktree

--- scripts/update_submodules.sh
+++ scripts/update_submodules.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SUBMODULE_ERROR='Could not recursively update all git submodules. You may experience compilation problems if some submodules are out of date'
 SUBMODULE_OK='Git submodules updated successfully'
-if test -d .git; then \
+if test -e .git; then \
 	   (git submodule update --init --recursive && echo $SUBMODULE_OK) \
 	|| (git submodule init && git submodule update --recursive && echo $SUBMODULE_OK) \
 	|| (git submodule init && git submodule update && echo $SUBMODULE_ERROR) \
