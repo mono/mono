0d51a8063beb25da3f4390a9ffddb231e3776a89
Ludovic Henry <luhenry@microsoft.com>
2018-02-16 13:01:17 -0500

    [sdks] Backport changes from master (#7084)
    This allows us to start integration to the products straight from the mono:2017-12 branch, without having to wait for mono:2018-02 to be merged.

--- sdks/builds/android.mk
+++ sdks/builds/android.mk
@@ -3,9 +3,8 @@
 # Parameters:
 #  $(1): target
 #  $(2): arch
-#  $(3): platform
-#  $(4): abi_name
-#  $(5): host_triple
+#  $(3): abi_name
+#  $(4): host_triple
 #
 # Flags:
 #  android_$(1)_CFLAGS
@@ -13,17 +12,17 @@
 #  android_$(1)_LDFLAGS
 define AndroidTargetTemplate
 
-_android_$(1)_AR=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-ar
-_android_$(1)_AS=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-as
-_android_$(1)_CC=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-clang
-_android_$(1)_CXX=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-clang++
-_android_$(1)_CPP=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-cpp -I$$(TOP)/sdks/builds/toolchains/android-$(1)/usr/include
-_android_$(1)_CXXCPP=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-cpp -I$$(TOP)/sdks/builds/toolchains/android-$(1)/usr/include
+_android_$(1)_AR=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-ar
+_android_$(1)_AS=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-as
+_android_$(1)_CC=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-clang
+_android_$(1)_CXX=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-clang++
+_android_$(1)_CPP=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-cpp -I$$(TOP)/sdks/builds/toolchains/android-$(1)/usr/include
+_android_$(1)_CXXCPP=$$(CCACHE) $$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-cpp -I$$(TOP)/sdks/builds/toolchains/android-$(1)/usr/include
 _android_$(1)_DLLTOOL=
-_android_$(1)_LD=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-ld
-_android_$(1)_OBJDUMP="$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-objdump"
-_android_$(1)_RANLIB=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-ranlib
-_android_$(1)_STRIP=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(4)-strip
+_android_$(1)_LD=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-ld
+_android_$(1)_OBJDUMP="$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-objdump"
+_android_$(1)_RANLIB=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-ranlib
+_android_$(1)_STRIP=$$(TOP)/sdks/builds/toolchains/android-$(1)/bin/$(3)-strip
 
 _android_$(1)_AC_VARS= \
 	mono_cv_uscore=yes \
@@ -45,8 +44,8 @@ _android_$(1)_CXXFLAGS= \
 _android_$(1)_LDFLAGS= \
 	-z now -z relro -z noexecstack \
 	-ldl -lm -llog -lc -lgcc \
-	-Wl,-rpath-link=$$(NDK_DIR)/platforms/android-$(3)/arch-$(2)/usr/lib,-dynamic-linker=/system/bin/linker \
-	-L$$(NDK_DIR)/platforms/android-$(3)/arch-$(2)/usr/lib \
+	-Wl,-rpath-link=$$(NDK_DIR)/platforms/android-$$(ANDROID_PLATFORM_VERSION_$(1))/arch-$(2)/usr/lib,-dynamic-linker=/system/bin/linker \
+	-L$$(NDK_DIR)/platforms/android-$$(ANDROID_PLATFORM_VERSION_$(1))/arch-$(2)/usr/lib \
 	$$(android_$(1)_LDFLAGS)
 
 _android_$(1)_CONFIGURE_ENVIRONMENT = \
@@ -66,7 +65,7 @@ _android_$(1)_CONFIGURE_ENVIRONMENT = \
 	RANLIB="$$(_android_$(1)_RANLIB)"
 
 _android_$(1)_CONFIGURE_FLAGS= \
-	--host=$(5) \
+	--host=$(4) \
 	--cache-file=$$(TOP)/sdks/builds/android-$(1)-$$(CONFIGURATION).config.cache \
 	--prefix=$$(TOP)/sdks/out/android-$(1)-$$(CONFIGURATION) \
 	--disable-boehm \
@@ -83,7 +82,7 @@ _android_$(1)_CONFIGURE_FLAGS= \
 	--without-ikvm-native
 
 .stamp-android-$(1)-toolchain:
-	python "$$(NDK_DIR)/build/tools/make_standalone_toolchain.py" --verbose --force --api=$(3) --arch=$(2) --install-dir=$$(TOP)/sdks/builds/toolchains/android-$(1)
+	python "$$(NDK_DIR)/build/tools/make_standalone_toolchain.py" --verbose --force --api=$$(ANDROID_PLATFORM_VERSION_$(1)) --arch=$(2) --install-dir=$$(TOP)/sdks/builds/toolchains/android-$(1)
 	touch $$@
 
 .stamp-android-$(1)-$$(CONFIGURATION)-configure: $$(TOP)/configure .stamp-android-$(1)-toolchain
@@ -127,23 +126,175 @@ endef
 android_armeabi_CFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -march=armv5te
 android_armeabi_CXXFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -march=armv5te
 android_armeabi_LDFLAGS=-Wl,--fix-cortex-a8
-$(eval $(call AndroidTargetTemplate,armeabi,arm,9,arm-linux-androideabi,armv5-linux-androideabi))
+$(eval $(call AndroidTargetTemplate,armeabi,arm,arm-linux-androideabi,armv5-linux-androideabi))
 
 ## android-armeabi-v7a
 android_armeabi-v7a_CFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -march=armv7-a -mtune=cortex-a8 -mfpu=vfp -mfloat-abi=softfp
 android_armeabi-v7a_CXXFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -march=armv7-a -mtune=cortex-a8 -mfpu=vfp -mfloat-abi=softfp
 android_armeabi-v7a_LDFLAGS=-Wl,--fix-cortex-a8
-$(eval $(call AndroidTargetTemplate,armeabi-v7a,arm,9,arm-linux-androideabi,armv5-linux-androideabi))
+$(eval $(call AndroidTargetTemplate,armeabi-v7a,arm,arm-linux-androideabi,armv5-linux-androideabi))
 
 ## android-arm64-v8a
 android_arm64-v8a_CFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -DL_cuserid=9 -DANDROID64
 android_arm64-v8a_CXXFLAGS=-D__POSIX_VISIBLE=201002 -DSK_RELEASE -DNDEBUG -UDEBUG -fpic -DL_cuserid=9 -DANDROID64
-$(eval $(call AndroidTargetTemplate,arm64-v8a,arm64,21,aarch64-linux-android,aarch64-linux-android))
+$(eval $(call AndroidTargetTemplate,arm64-v8a,arm64,aarch64-linux-android,aarch64-linux-android))
 
 ## android-x86
-$(eval $(call AndroidTargetTemplate,x86,x86,9,i686-linux-android,i686-linux-android))
+$(eval $(call AndroidTargetTemplate,x86,x86,i686-linux-android,i686-linux-android))
 
 ## android-x86_64
 android_x86_64_CFLAGS=-DL_cuserid=9
 android_x86_64_CXXFLAGS=-DL_cuserid=9
-$(eval $(call AndroidTargetTemplate,x86_64,x86_64,21,x86_64-linux-android,x86_64-linux-android))
+$(eval $(call AndroidTargetTemplate,x86_64,x86_64,x86_64-linux-android,x86_64-linux-android))
+
+##
+# Parameters
+#  $(1): target
+#
+# Flags:
+#  android_$(1)_CFLAGS
+#
+# Notes:
+#  XA doesn't seem to build differently for Darwin and Linux, seems like a bug on their end
+define AndroidHostTemplate
+
+_android_$(1)_AR=ar
+_android_$(1)_AS=as
+_android_$(1)_CC=cc
+_android_$(1)_CXX=c++
+_android_$(1)_CXXCPP=cpp
+_android_$(1)_LD=ld
+_android_$(1)_RANLIB=ranlib
+_android_$(1)_STRIP=strip
+
+_android_$(1)_CFLAGS= \
+	-ggdb3 -O0 -fno-omit-frame-pointer \
+	$$(android_$(1)_CFLAGS)
+
+_android_$(1)_CONFIGURE_ENVIRONMENT= \
+	AR="$$(_android_$(1)_AR)" \
+	AS="$$(_android_$(1)_AS)" \
+	CC="$$(_android_$(1)_CC)" \
+	CFLAGS="$$(_android_$(1)_CFLAGS)" \
+	CXX="$$(_android_$(1)_CXX)" \
+	CXXCPP="$$(_android_$(1)_CXXCPP)" \
+	LD="$$(_android_$(1)_LD)" \
+	RANLIB="$$(_android_$(1)_RANLIB)" \
+	STRIP="$$(_android_$(1)_STRIP)"
+
+_android_$(1)_CONFIGURE_FLAGS= \
+	--cache-file=$$(TOP)/sdks/builds/android-$(1).config.cache \
+	--prefix=$$(TOP)/sdks/out/android-$(1) \
+	--disable-boehm \
+	--disable-iconv \
+	--disable-mono-debugger \
+	--disable-nls \
+	--enable-dynamic-btls \
+	--enable-maintainer-mode \
+	--with-mcs-docs=no \
+	--with-monodroid \
+	--with-profile4_x=no \
+	--without-ikvm-native
+
+.stamp-android-$(1)-toolchain:
+	touch $$@
+
+.stamp-android-$(1)-configure: $$(TOP)/configure .stamp-android-$(1)-toolchain
+	mkdir -p $$(TOP)/sdks/builds/android-$(1)
+	cd $$(TOP)/sdks/builds/android-$(1) && $$< $$(_android_$(1)_CONFIGURE_ENVIRONMENT) $$(_android_$(1)_CONFIGURE_FLAGS)
+	touch $$@
+
+.PHONY: package-android-$(1)
+package-android-$(1):
+	$$(MAKE) -C $$(TOP)/sdks/builds/android-$(1)/mono install
+	$$(MAKE) -C $$(TOP)/sdks/builds/android-$(1)/support install
+
+.PHONY: clean-android-$(1)
+clean-android-$(1):
+	rm -rf .stamp-android-$(1)-toolchain .stamp-android-$(1)-configure $$(TOP)/sdks/builds/android-$(1) $$(TOP)/sdks/builds/android-$(1).config.cache $$(TOP)/sdks/out/android-$(1)
+
+TARGETS += android-$(1)
+
+endef
+
+android_host-Darwin_CFLAGS=-mmacosx-version-min=10.9
+$(eval $(call AndroidHostTemplate,host-Darwin))
+$(eval $(call AndroidHostTemplate,host-Linux))
+
+##
+# Parameters
+#  $(1): target
+#  $(2): arch
+#  $(3): mxe
+define AndroidHostMxeTemplate
+
+_android_$(1)_PATH=$$(TOP)/sdks/out/mxe-$(3)/bin
+
+_android_$(1)_AR=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ar
+_android_$(1)_AS=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-as
+_android_$(1)_CC=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-gcc
+_android_$(1)_CXX=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-g++
+_android_$(1)_DLLTOOL=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-dlltool
+_android_$(1)_LD=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ld
+_android_$(1)_OBJDUMP=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-objdump
+_android_$(1)_RANLIB=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ranlib
+_android_$(1)_STRIP=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-strip
+
+_android_$(1)_AC_VARS= \
+	ac_cv_header_zlib_h=no \
+	ac_cv_search_dlopen=no
+
+_android_$(1)_CFLAGS= \
+	-ggdb3 -O0 -fno-omit-frame-pointer \
+	-DXAMARIN_PRODUCT_VERSION=0
+
+_android_$(1)_CXXFLAGS= \
+	-ggdb3 -O0 -fno-omit-frame-pointer \
+	-DXAMARIN_PRODUCT_VERSION=0
+
+_android_$(1)_CONFIGURE_ENVIRONMENT= \
+	AR="$$(_android_$(1)_AR)" \
+	AS="$$(_android_$(1)_AS)" \
+	CC="$$(_android_$(1)_CC)" \
+	CFLAGS="$$(_android_$(1)_CFLAGS)" \
+	CXX="$$(_android_$(1)_CXX)" \
+	CXXFLAGS="$$(_android_$(1)_CXXFLAGS)" \
+	LD="$$(_android_$(1)_LD)" \
+	RANLIB="$$(_android_$(1)_RANLIB)" \
+	STRIP="$$(_android_$(1)_STRIP)"
+
+_android_$(1)_CONFIGURE_FLAGS= \
+	--host=$(2)-w64-mingw32.static \
+	--target=$(2)-w64-mingw32.static \
+	--cache-file=$$(TOP)/sdks/builds/android-$(1).config.cache \
+	--prefix=$$(TOP)/sdks/out/android-$(1) \
+	--disable-boehm \
+	--disable-llvm \
+	--disable-mcs-build \
+	--disable-nls \
+	--enable-maintainer-mode \
+	--with-monodroid
+
+.stamp-android-$(1)-toolchain:
+	touch $$@
+
+.stamp-android-$(1)-configure: $$(TOP)/configure .stamp-android-$(1)-toolchain | package-mxe-$(3)
+	mkdir -p $$(TOP)/sdks/builds/android-$(1)
+	cd $$(TOP)/sdks/builds/android-$(1) && PATH="$$$$PATH:$$(_android_$(1)_PATH)" $$< $$(_android_$(1)_AC_VARS) $$(_android_$(1)_CONFIGURE_ENVIRONMENT) $$(_android_$(1)_CONFIGURE_FLAGS)
+	touch $$@
+
+.PHONY: package-android-$(1)
+package-android-$(1):
+	$$(MAKE) -C $$(TOP)/sdks/builds/android-$(1)/mono install
+	$$(MAKE) -C $$(TOP)/sdks/builds/android-$(1)/support install
+
+.PHONY: clean-android-$(1)
+clean-android-$(1):
+	rm -rf .stamp-android-$(1)-toolchain .stamp-android-$(1)-configure $$(TOP)/sdks/builds/android-$(1) $$(TOP)/sdks/builds/android-$(1).config.cache $$(TOP)/sdks/out/android-$(1)
+
+TARGETS += android-$(1)
+
+endef
+
+$(eval $(call AndroidHostMxeTemplate,host-mxe-Win32,i686,Win32))
+$(eval $(call AndroidHostMxeTemplate,host-mxe-Win64,x86_64,Win64))
---
+++ sdks/builds/llvm.mk
@@ -0,0 +1,127 @@
+
+$(TOP)/sdks/builds/toolchains/llvm:
+	git clone -b master https://github.com/mono/llvm.git $@
+
+$(TOP)/sdks/builds/toolchains/llvm/configure: | $(TOP)/sdks/builds/toolchains/llvm
+
+##
+# Parameters
+#  $(1): target
+#  $(2): arch
+define LLVMTemplate
+
+_llvm_$(1)_CXXFLAGS= \
+	-stdlib=libc++ \
+	-mmacosx-version-min=10.9
+
+_llvm_$(1)_LDFLAGS= \
+	-mmacosx-version-min=10.9
+
+_llvm_$(1)_CONFIGURE_ENVIRONMENT= \
+	CXXFLAGS="$$(_llvm_$(1)_CXXFLAGS)" \
+	LDFLAGS="$$(_llvm_$(1)_LDFLAGS)"
+
+_llvm_$(1)_CONFIGURE_FLAGS= \
+	--host=$(2)-apple-darwin10 \
+	--cache-file=$$(TOP)/sdks/builds/llvm-$(1).config.cache \
+	--prefix=$$(TOP)/sdks/out/llvm-$(1) \
+	--enable-assertions=no \
+	--enable-optimized \
+	--enable-targets="arm,aarch64,x86" \
+	--enable-libcpp
+
+.stamp-llvm-$(1)-toolchain: | $$(TOP)/sdks/builds/toolchains/llvm
+	touch $$@
+
+.stamp-llvm-$(1)-configure: $$(TOP)/sdks/builds/toolchains/llvm/configure
+	mkdir -p $$(TOP)/sdks/builds/llvm-$(1)
+	cd $$(TOP)/sdks/builds/llvm-$(1) && $$< $$(_llvm_$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm_$(1)_CONFIGURE_FLAGS)
+	touch $$@
+
+.PHONY: package-llvm-$(1)
+package-llvm-$(1):
+	$$(MAKE) -C $$(TOP)/sdks/builds/llvm-$(1) install
+
+.PHONY: clean-llvm-$(1)
+clean-llvm-$(1):
+	rm -rf .stamp-llvm-$(1)-toolchain .stamp-llvm-$(1)-configure $$(TOP)/sdks/builds/llvm-$(1) $$(TOP)/sdks/builds/llvm-$(1).config.cache $$(TOP)/sdks/out/llvm-$(1)
+
+TARGETS += llvm-$(1)
+
+endef
+
+$(eval $(call LLVMTemplate,llvm32,i386))
+$(eval $(call LLVMTemplate,llvm64,x86_64))
+
+##
+# Parameters
+#  $(1): target
+#  $(2): arch
+#  $(3): mxe
+#
+# Flags
+#  llvm_$(1)_CONFIGURE_ENVIRONMENT
+define LLVMMxeTemplate
+
+_llvm_$(1)_PATH=$$(TOP)/sdks/out/mxe-$(3)/bin
+
+_llvm_$(1)_AR=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ar
+_llvm_$(1)_AS=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-as
+_llvm_$(1)_CC=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-gcc
+_llvm_$(1)_CXX=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-g++
+_llvm_$(1)_DLLTOOL=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-dlltool
+_llvm_$(1)_LD=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ld
+_llvm_$(1)_OBJDUMP=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-objdump
+_llvm_$(1)_RANLIB=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-ranlib
+_llvm_$(1)_STRIP=$$(TOP)/sdks/out/mxe-$(3)/bin/$(2)-w64-mingw32.static-strip
+
+_llvm_$(1)_CXXFLAGS=
+
+_llvm_$(1)_LDFLAGS=
+
+_llvm_$(1)_CONFIGURE_ENVIRONMENT = \
+	AR="$$(_llvm_$(1)_AR)" \
+	AS="$$(_llvm_$(1)_AS)" \
+	CC="$$(_llvm_$(1)_CC)" \
+	CXX="$$(_llvm_$(1)_CXX)" \
+	DLLTOOL="$$(_llvm_$(1)_DLLTOOL)" \
+	LD="$$(_llvm_$(1)_LD)" \
+	OBJDUMP="$$(_llvm_$(1)_OBJDUMP)" \
+	RANLIB="$$(_llvm_$(1)_RANLIB)" \
+	STRIP="$$(_llvm_$(1)_STRIP)" \
+	CXXFLAGS="$$(_llvm_$(1)_CXXFLAGS)" \
+	LDFLAGS="$$(_llvm_$(1)_LDFLAGS)"
+
+_llvm_$(1)_CONFIGURE_FLAGS = \
+	--host=$(2)-w64-mingw32.static \
+	--cache-file=$$(TOP)/sdks/builds/llvm-$(1).config.cache \
+	--prefix=$$(TOP)/sdks/out/llvm-$(1) \
+	--enable-assertions=no \
+	--enable-optimized \
+	--enable-targets="arm,aarch64,x86" \
+	--disable-pthreads \
+	--disable-zlib
+
+.stamp-llvm-$(1)-toolchain: | $$(TOP)/sdks/builds/toolchains/llvm
+	cd $$(TOP)/sdks/builds/toolchains/llvm && git checkout $(LLVM_HASH)
+	touch $$@
+
+.stamp-llvm-$(1)-configure: $$(TOP)/sdks/builds/toolchains/llvm/configure | package-mxe-$(3)
+	mkdir -p $$(TOP)/sdks/builds/llvm-$(1)
+	cd $$(TOP)/sdks/builds/llvm-$(1) && PATH="$$$$PATH:$$(_llvm_$(1)_PATH)" $$< $$(_llvm_$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm_$(1)_CONFIGURE_FLAGS)
+	touch $$@
+
+.PHONY: package-llvm-$(1)
+package-llvm-$(1):
+	$$(MAKE) -C $$(TOP)/sdks/builds/llvm-$(1) install
+
+.PHONY: clean-llvm-$(1)
+clean-llvm-$(1):
+	rm -rf .stamp-llvm-$(1)-toolchain .stamp-llvm-$(1)-configure $$(TOP)/sdks/builds/llvm-$(1) $$(TOP)/sdks/builds/llvm-$(1).config.cache $$(TOP)/sdks/out/llvm-$(1)
+
+TARGETS += llvm-$(1)
+
+endef
+
+$(eval $(call LLVMMxeTemplate,llvmwin32,i686,Win32))
+$(eval $(call LLVMMxeTemplate,llvmwin64,x86_64,Win64))
