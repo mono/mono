a19fad65f3f62372170eeabcc23553f6b73b81e1
Alexis Christoforides <alexis@thenull.net>
2018-02-20 21:15:56 -0500

    Fix copyright

--- mcs/class/corlib/Test/System.Runtime.Remoting/RemotingServicesTest.cs
+++ mcs/class/corlib/Test/System.Runtime.Remoting/RemotingServicesTest.cs
@@ -3,8 +3,8 @@
 //
 // Author: Alexis Christoforides (alchri@microsoft.com)
 //
-// 2018 (C) Copyright, Novell, Inc.
-//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using System.Reflection;
