a974057d929eaa15f73a13fa1bbddd6eb9ef2503
Ludovic Henry <ludovic@xamarin.com>
2018-02-23 11:12:14 -0500

    [sdks] Merge mxe-Win32 and mxe-Win64 targets since we are building both in XA anyway

--- sdks/builds/mxe.mk
+++ sdks/builds/mxe.mk
@@ -2,38 +2,31 @@
 $(TOP)/sdks/builds/toolchains/mxe:
 	git clone -b xamarin https://github.com/xamarin/mxe.git $@
 
-##
-# Parameters
-#  $(1): target
-#  $(2): arch
-define MxeTemplate
+.stamp-mxe-toolchain: | $(TOP)/sdks/builds/toolchains/mxe
+	cd $(TOP)/sdks/builds/toolchains/mxe && git checkout $(MXE_HASH)
+	touch $@
 
-.stamp-mxe-$(1)-toolchain: | $$(TOP)/sdks/builds/toolchains/mxe
-	cd $$(TOP)/sdks/builds/toolchains/mxe && git checkout $$(MXE_HASH)
-	touch $$@
+.stamp-mxe-configure:
+	touch $@
 
-.stamp-mxe-$(1)-configure:
-	touch $$@
+.PHONY: build-custom-mxe
+build-custom-mxe:
+	PATH="$$PATH:$(dir $(shell which autopoint))" $(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
+		MXE_TARGETS="i686-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe" OS_SHORT_NAME="disable-native-plugins"
+	PATH="$$PATH:$(dir $(shell which autopoint))" $(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
+		MXE_TARGETS="x86_64-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe" OS_SHORT_NAME="disable-native-plugins"
 
-.PHONY: build-custom-mxe-$(1)
-build-custom-mxe-$(1):
-	PATH="$$$$PATH:$$(dir $$(shell which autopoint))" $$(MAKE) -C $$(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
-		MXE_TARGETS="$(2)-w64-mingw32.static" PREFIX="$$(TOP)/sdks/out/mxe" OS_SHORT_NAME="disable-native-plugins"
+.PHONY: setup-custom-mxe
+setup-custom-mxe:
 
-.PHONY: setup-custom-mxe-$(1)
-setup-custom-mxe-$(1):
+.PHONY: package-mxe
+package-mxe:
 
-.PHONY: package-mxe-$(1)
-package-mxe-$(1):
+.PHONY: clean-mxe
+clean-mxe:
+	$(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe clean \
+		MXE_TARGETS="i686-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe"
+	$(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe clean \
+		MXE_TARGETS="x86_64-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe"
 
-.PHONY: clean-mxe-$(1)
-clean-mxe-$(1):
-	$$(MAKE) -C $$(TOP)/sdks/builds/toolchains/mxe clean \
-		MXE_TARGETS="$(2)-w64-mingw32.static" PREFIX="$$(TOP)/sdks/out/mxe-$(1)"
-
-TARGETS += mxe-$(1)
-
-endef
-
-$(eval $(call MxeTemplate,Win32,i686))
-$(eval $(call MxeTemplate,Win64,x86_64))
+TARGETS += mxe
