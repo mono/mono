f828d757ced5dd6115fa4ae48d38ae2d90645d83
Filip Navara <navara@emclient.com>
2018-02-23 18:01:00 +0100

    Don't create the Hwnd object in the XplatUIWin32 backend.

--- mcs/class/System.Windows.Forms/System.Windows.Forms/XplatUIWin32.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/XplatUIWin32.cs
@@ -64,7 +64,6 @@ namespace System.Windows.Forms {
 		private static Hashtable	wm_nc_registered;
 		private static RECT		clipped_cursor_rect;
 		private Hashtable		registered_classes;
-		private Hwnd HwndCreating; // the Hwnd we are currently creating (see CreateWindow)
 
 		#endregion	// Local Variables
 
@@ -1617,9 +1616,6 @@ namespace System.Windows.Forms {
 		internal override IntPtr CreateWindow(CreateParams cp) {
 			IntPtr	WindowHandle;
 			IntPtr	ParentHandle;
-			Hwnd	hwnd;
-
-			hwnd = new Hwnd();
 
 			ParentHandle=cp.Parent;
 
@@ -1642,7 +1638,6 @@ namespace System.Windows.Forms {
 			}
 
 			string class_name = RegisterWindowClass (cp.ClassStyle);
-			HwndCreating = hwnd;
 
 			// We cannot actually send the WS_EX_MDICHILD flag to Windows because we
 			// are faking MDI, not uses Windows' version.
@@ -1651,16 +1646,12 @@ namespace System.Windows.Forms {
 				
 			WindowHandle = Win32CreateWindow (cp.WindowExStyle, class_name, cp.Caption, cp.WindowStyle, location.X, location.Y, cp.Width, cp.Height, ParentHandle, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
 
-			HwndCreating = null;
-
 			if (WindowHandle==IntPtr.Zero) {
 				int error = Marshal.GetLastWin32Error ();
 
 				Win32MessageBox(IntPtr.Zero, "Error : " + error.ToString(), "Failed to create window, class '"+cp.ClassName+"'", 0);
 			}
 
-			hwnd.ClientWindow = WindowHandle;
-			hwnd.Mapped = true;
 			Win32SetWindowLong(WindowHandle, WindowLong.GWL_USERDATA, (uint)ThemeEngine.Current.DefaultControlBackColor.ToArgb());
 
 			return WindowHandle;
@@ -1685,12 +1676,7 @@ namespace System.Windows.Forms {
 		}
 
 		internal override void DestroyWindow(IntPtr handle) {
-			Hwnd	hwnd;
-
-			hwnd = Hwnd.ObjectFromHandle(handle);
 			Win32DestroyWindow(handle);
-			hwnd.Dispose();
-			return;
 		}
 
 		internal override void SetWindowMinMax(IntPtr handle, Rectangle maximized, Size min, Size max) {
@@ -1958,8 +1944,6 @@ namespace System.Windows.Forms {
 
 		private IntPtr InternalWndProc (IntPtr hWnd, Msg msg, IntPtr wParam, IntPtr lParam)
 		{
-			if (HwndCreating != null && HwndCreating.ClientWindow == IntPtr.Zero)
-				HwndCreating.ClientWindow = hWnd;
 			return NativeWindow.WndProc (hWnd, msg, wParam, lParam);
 		}
 
