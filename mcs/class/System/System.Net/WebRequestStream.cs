1e546e5870493d73d4248c3de0f7e6f15ddf6cfc
Martin Baulig <martin.baulig@xamarin.com>
2018-02-26 17:07:30 -0500

    [System]: Use new `WebCompletionSource` instead of `TaskCompletionSource<>`.
    This new helper class uses `TaskCompletionSource<>` internally, but instead
    of using TrySetException(), it sets a result containing an `ExceptionDispatchInfo`.
    
    The problem with using TrySetException() is that it wraps the exception
    object in an `AggregateException`, that would be unobserved if the caller
    throws the original exception.

--- mcs/class/System/System.Net/WebRequestStream.cs
+++ mcs/class/System/System.Net/WebRequestStream.cs
@@ -39,7 +39,7 @@ namespace System.Net
 		bool requestWritten;
 		bool allowBuffering;
 		bool sendChunked;
-		TaskCompletionSource<int> pendingWrite;
+		WebCompletionSource pendingWrite;
 		long totalWritten;
 		byte[] headers;
 		bool headersSent;
@@ -149,8 +149,8 @@ namespace System.Net
 			if (size < 0 || (length - offset) < size)
 				throw new ArgumentOutOfRangeException (nameof (size));
 
-			var myWriteTcs = new TaskCompletionSource<int> ();
-			if (Interlocked.CompareExchange (ref pendingWrite, myWriteTcs, null) != null)
+			var completion = new WebCompletionSource ();
+			if (Interlocked.CompareExchange (ref pendingWrite, completion, null) != null)
 				throw new InvalidOperationException (SR.GetString (SR.net_repcall));
 
 			try {
@@ -162,7 +162,7 @@ namespace System.Net
 					await FinishWriting (cancellationToken);
 
 				pendingWrite = null;
-				myWriteTcs.TrySetResult (0);
+				completion.SetCompleted ();
 			} catch (Exception ex) {
 				KillBuffer ();
 				closed = true;
@@ -175,7 +175,7 @@ namespace System.Net
 				Operation.CompleteRequestWritten (this, ex);
 
 				pendingWrite = null;
-				myWriteTcs.TrySetException (ex);
+				completion.SetException (ex);
 				throw;
 			}
 		}
@@ -364,11 +364,12 @@ namespace System.Net
 				cts.CancelAfter (WriteTimeout);
 				var timeoutTask = Task.Delay (WriteTimeout);
 				while (true) {
-					var myWriteTcs = new TaskCompletionSource<int> ();
-					var oldTcs = Interlocked.CompareExchange (ref pendingWrite, myWriteTcs, null);
-					if (oldTcs == null)
+					var completion = new WebCompletionSource ();
+					var oldCompletion = Interlocked.CompareExchange (ref pendingWrite, completion, null);
+					if (oldCompletion == null)
 						break;
-					var ret = await Task.WhenAny (timeoutTask, oldTcs.Task).ConfigureAwait (false);
+					var oldWriteTask = oldCompletion.WaitForCompletion (true);
+					var ret = await Task.WhenAny (timeoutTask, oldWriteTask).ConfigureAwait (false);
 					if (ret == timeoutTask)
 						throw new WebException ("The operation has timed out.", WebExceptionStatus.Timeout);
 				}
