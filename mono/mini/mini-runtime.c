980f4e097ae10730efe1fc2c0d12081ea4cffbd3
Rodrigo Kumpera <kumpera@gmail.com>
2018-03-05 14:17:56 -0800

    [mini] Don't wait for other threads to finish JITing if the current thread is running a cctor.
    This eliminates a possible source of deadlocks found during AOT runs of Roslyn.

--- mono/mini/mini-runtime.c
+++ mono/mini/mini-runtime.c
@@ -1915,9 +1915,12 @@ wait_or_register_method_to_compile (MonoMethod *method, MonoDomain *domain)
 
 		unlock_compilation_data ();
 		return FALSE;
-	} else if (jit_tls->active_jit_methods > 0) {
+	} else if (jit_tls->active_jit_methods > 0 || mono_threads_is_current_thread_in_protected_block ()) {
 		//We can't suspend the current thread if it's already JITing a method.
 		//Dependency management is too compilated and we want to get rid of this anyways.
+
+		//We can't suspend the current thread if it's running a protected block (such as a cctor)
+		//We can't relie only on JIT nesting as cctor's can be run from outside the JIT.
 		++entry->compilation_count;
 		++jit_methods_multiple;
 		++jit_tls->active_jit_methods;
