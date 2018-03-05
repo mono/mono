19e8b897d5d0289f1390e2a1bda3e7996fa1556f
Zoltan Varga <vargaz@gmail.com>
2018-03-05 14:27:59 -0500

    [sdks] Compile monotouch/tvos/watchos profiles for the bcl build.

--- sdks/builds/bcl.mk
+++ sdks/builds/bcl.mk
@@ -2,9 +2,24 @@
 .stamp-bcl-toolchain:
 	touch $@
 
+bcl_ios_CONFIGURE_FLAGS = \
+	--with-monotouch=yes \
+	--with-monotouch_tv=yes \
+	--with-xammac=yes \
+	--with-monotouch_watch=yes
+
+bcl_CONFIGURE_FLAGS = \
+	--with-profile4_x=no \
+	$(if $(DISABLE_ANDROID),,--with-monodroid=yes) \
+	$(if $(DISABLE_IOS),,$(bcl_ios_CONFIGURE_FLAGS)) \
+	$(if $(DISABLE_WASM),,--with-wasm=yes) \
+	--with-mcs-docs=no \
+	--disable-nls \
+	--disable-boehm
+
 .stamp-bcl-configure: $(TOP)/configure
 	mkdir -p $(TOP)/sdks/builds/bcl
-	cd $(TOP)/sdks/builds/bcl && $(TOP)/configure --with-profile4_x=no $(if $(DISABLE_ANDROID),,--with-monodroid=yes) $(if $(DISABLE_IOS),,--with-monotouch=yes) $(if $(DISABLE_WASM),,--with-wasm=yes) --with-mcs-docs=no --disable-nls --disable-boehm
+	cd $(TOP)/sdks/builds/bcl && $(TOP)/configure $(bcl_CONFIGURE_FLAGS)
 	touch $@
 
 $(TOP)/sdks/out/bcl/monodroid $(TOP)/sdks/out/bcl/monotouch $(TOP)/sdks/out/bcl/wasm:
