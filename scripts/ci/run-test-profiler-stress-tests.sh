197745501c642b251b15d734fafe5a77ad74f2df
Alex Rønne Petersen <alpeters@microsoft.com>
2018-03-09 19:43:24 +0100

    [ci] Run the regular profiler tests on the stress test jobs as well.

--- scripts/ci/run-test-profiler-stress-tests.sh
+++ scripts/ci/run-test-profiler-stress-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-export MONO_BABYSITTER_EXTRA_XML=${MONO_REPO_ROOT}/acceptance-tests/profiler-stress/TestResult-profiler-stress.xml
+${TESTCMD} --label=profiler --timeout=30m make -w -C mono/profiler -k check
 
+export MONO_BABYSITTER_EXTRA_XML=${MONO_REPO_ROOT}/acceptance-tests/profiler-stress/TestResult-profiler-stress.xml
 ${TESTCMD} --label=check-profiler-stress --timeout=24h make -C acceptance-tests check-profiler-stress
