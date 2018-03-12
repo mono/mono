60ddb406a787ad87f72e81097ad35b0148e7bbbf
Filip Navara <navara@emclient.com>
2018-03-12 11:18:20 +0100

    Fix TopLevelControl property to actually return top-level controls, not just Forms.

--- mcs/class/System.Windows.Forms/System.Windows.Forms/Control.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/Control.cs
@@ -3168,12 +3168,10 @@ namespace System.Windows.Forms
 		public Control TopLevelControl {
 			get {
 				Control	p = this;
-
-				while (p.parent != null) {
+				while (p != null && !p.GetTopLevel()) {
 					p = p.parent;
 				}
-
-				return p is Form ? p : null;
+				return p;
 			}
 		}
 
