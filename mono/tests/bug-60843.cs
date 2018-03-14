40760e412098ce4c3c9d5ebf4e47b492b1f4a70c
Katelyn Gadd <kg@luminance.org>
2018-03-13 19:44:55 -0700

    Fix bug #60843 and add test: If an enum was nested inside a generic class, using it as an argument to a custom attribute would cause a runtime error in the custom attribute decoder. Update the decoder to forward this special case through the normal primitive loading path.

---
+++ mono/tests/bug-60843.cs
@@ -0,0 +1,38 @@
+using System;
+
+class A : Attribute
+{
+    public object X;
+
+    public static void Main()
+    {
+        var x = (C<int>.E)AttributeTest(typeof(C<>.E));
+        Assert(C<int>.E.V == x);
+        var y = (C<int>.E2[])AttributeTest(typeof(C<>.E2));
+        Assert(y.Length == 2);
+        Assert(y[0] == C<int>.E2.A);
+        Assert(y[1] == C<int>.E2.B);
+    }
+
+    public static object AttributeTest (Type t) {
+        var cas = t.GetCustomAttributes(false);
+        Assert(cas.Length == 1);
+        Assert(cas[0] is A);
+        var a = (A)cas[0];
+        return a.X;
+    }
+
+    public static void Assert (bool b) {
+        if (!b)
+            throw new Exception("Assert failed");
+    }
+}
+ 
+public class C<T>
+{
+    [A(X = C<int>.E.V)]
+    public enum E { V }
+
+    [A(X = new [] { C<int>.E2.A, C<int>.E2.B })]
+    public enum E2 { A, B }
+}
