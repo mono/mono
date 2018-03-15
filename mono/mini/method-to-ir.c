mono/mono

commit b4ee86154fce71ba4eb96f4c591a5ce236d23b85

[Tailcall] tail.callvirt support for AMD64 JIT (#7426)
* [Tailcall] Fix the F# code; let rec functiona ()= printfn "%d" 1; functionb ()
	and functionb ()= functiona ()
	let ()= functiona ()

The test case and the fix are from https://bugzilla.xamarin.com/show_bug.cgi?id=25224.

Just bringing in change ca0dd5c0bb0f600331aac3c6b8d0579e37cdd0c7:
	commit ca0dd5c0bb0f600331aac3c6b8d0579e37cdd0c7
	Author: Zoltan Varga <vargaz@gmail.com>
	Date:   Thu Dec 11 15:37:29 2014 -0500
	[jit] Enable tail calls from gshared code. Fixes #25224.

Previously it was reverted due to performance regression, which has been fixed.
The problem was that use vs. patch of jmps was using inflated vs. non-inflated code.
Fixed here: https://github.com/mono/mono/commit/fe95e4b34b00caa8073dfa3b4473a43182abcbbb

The IL is:
  .method public static !!a  functiona<a>() cil managed
  {
.
.
.
    IL_001d:  tail.
    IL_001f:  call       !!0 '1'::functionb<!!0>()
    IL_0024:  ret
  }

  .method public static !!a  functionb<a>() cil managed
  {
    IL_0000:  tail.
    IL_0002:  call       !!0 '1'::functiona<!!0>()
    IL_0007:  ret
  }

* [Tailcall] Add tail.callvirt support to AMD64 JIT. Not yet
any other architecture (arm, x86, etc.) or scenario (AOT, LLVM,
interpreter, etc.)

This is based on F# that stack overflows to report failure:
    type A() =
        member this.f2 (a) = if a > 0 then this.f1(a - 1)
                             else 1
        member this.f1 (a) = if a > 0 then this.f2(a - 1)
                             else 2

    let a = A()
    printfn "%d" (a.f1(1000 * 1000 * 10))

and in turn C#, self checking instead of stack overflowing:

    using System;
    using System.Runtime.CompilerServices;
    using static System.Runtime.CompilerServices.MethodImplOptions;

    unsafe class A
    {
        static int check (long stack1, long stack2)
        {
            return (stack1 == stack2) ? 0 : 1;
        }

mono/mini/method-to-ir.c
@@ -2030,6 +2030,8 @@ static int
 callvirt_to_call (int opcode)
 {
 	switch (opcode) {
+	case OP_TAILCALL_MEMBASE:
+		return OP_TAILCALL;
 	case OP_CALL_MEMBASE:
 		return OP_CALL;
 	case OP_VOIDCALL_MEMBASE:
@@ -2173,8 +2175,7 @@ mono_emit_call_args (MonoCompile *cfg, MonoMethodSignature *sig,
 
 	if (tail) {
 		mini_profiler_emit_tail_call (cfg, target);
-
-		MONO_INST_NEW_CALL (cfg, call, OP_TAILCALL);
+		MONO_INST_NEW_CALL (cfg, call, virtual_ ? OP_TAILCALL_MEMBASE : OP_TAILCALL);
 	} else
 		MONO_INST_NEW_CALL (cfg, call, ret_type_to_call_opcode (cfg, sig->ret, calli, virtual_));
 
@@ -2437,7 +2438,7 @@ mono_emit_method_call_full (MonoCompile *cfg, MonoMethod *method, MonoMethodSign
 			For a test case look into #667921.
 
 			FIXME: a dummy use is not the best way to do it as the local register allocator
-			will put it on a caller save register and spil it around the call. 
+			will put it on a caller save register and spill it around the call.
 			Ideally, we would either put it on a callee save register or only do the store part.  
 			 */
 			EMIT_NEW_DUMMY_USE (cfg, dummy_use, args [0]);
@@ -7005,8 +7006,22 @@ is_supported_tail_call (MonoCompile *cfg, MonoMethod *method, MonoMethod *cmetho
 		supported_tail_call = FALSE;
 	if (cmethod->wrapper_type && cmethod->wrapper_type != MONO_WRAPPER_DYNAMIC_METHOD)
 		supported_tail_call = FALSE;
-	if (call_opcode != CEE_CALL)
+
+	switch (call_opcode) {
+	case CEE_CALL:
+		break;
+	case CEE_CALLI:
 		supported_tail_call = FALSE;
+		break;
+	case CEE_CALLVIRT:
+		if (!cfg->backend->have_op_tail_call_membase)
+			supported_tail_call = FALSE;
+		break;
+	default:
+		g_assertf (call_opcode == CEE_CALL || call_opcode == CEE_CALLVIRT || call_opcode == CEE_CALLI, "%s (%d)", mono_opcode_name (call_opcode), (int)call_opcode);
+		supported_tail_call = FALSE;
+		break;
+	}
 
 	/* Debugging support */
 #if 0
@@ -9027,14 +9042,30 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 			if (ins)
 				goto call_end;
 
+			gboolean inst_tailcall;
+			inst_tailcall = (ins_flag & MONO_INST_TAILCALL) != 0;
+
 			/* Tail prefix / tail call optimization */
 
-			/* FIXME: Enabling TAILC breaks some inlining/stack trace/etc tests */
+			/* FIXME: Enabling TAILC breaks some inlining/stack trace/etc tests.
+				  Inlining and stack traces are not guaranteed however. */
 			/* FIXME: runtime generic context pointer for jumps? */
 			/* FIXME: handle this for generic sharing eventually */
-			if ((ins_flag & MONO_INST_TAILCALL) &&
-				!vtable_arg && !cfg->gshared && is_supported_tail_call (cfg, method, cmethod, fsig, call_opcode))
+			if (inst_tailcall
+					&& (cfg->backend->have_op_tail_call || (!vtable_arg && !cfg->gshared))
+					&& is_supported_tail_call (cfg, method, cmethod, fsig, call_opcode))
 				supported_tail_call = TRUE;
+
+			// http://www.mono-project.com/docs/advanced/runtime/docs/generic-sharing/
+			// 1. Non-generic non-static methods of reference types have access to the
+			//    RGCTX via the “this” argument (this->vtable->rgctx).
+			// 2. a Non-generic static methods of reference types and b. non-generic methods
+			//    of value types need to be passed a pointer to the caller’s class’s VTable in the MONO_ARCH_RGCTX_REG register.
+			// 3. Generic methods need to be passed a pointer to the MRGCTX in the MONO_ARCH_RGCTX_REG register
+			if (inst_tailcall && cfg->verbose_level >= 2)
+				g_print ("tail.%s %s -> %s supported_tail_call:%d gshared:%d vtable_arg:%d\n",
+					mono_opcode_name (*ip), method->name, cmethod->name,
+					(int)supported_tail_call, (int)cfg->gshared, !!vtable_arg);
 
 			if (supported_tail_call) {
 				MonoCallInst *call;

mono/tests/Makefile.am
@@ -565,6 +565,10 @@ TESTS_IL_SRC=			\
 	tail1.il		\
 	taili1.il		\
 	vtail1.il		\
+	tailcall-virt.il 	\
+	tailcall-rgctxa.il 	\
+	tailcall-rgctxb.il 	\
+	tailcall-mrgctx.il 	\
 	field-access.il		\
 	method-access.il	\
 	ldftn-access.il		\
@@ -697,7 +701,15 @@ TESTS_GSHARED_SRC = \
 	bug-1147.cs	\
 	generic-type-builder.2.cs
 
-PLATFORM_DISABLED_TESTS=
+PLATFORM_DISABLED_TESTS=\
+	tailcall-rgctxb.exe
+
+# tailcall-virt only currently implemented on AMD64 (JIT, AOT, LLVM).
+# Not ARM, not interpreter, etc.
+
+if !AMD64
+PLATFORM_DISABLED_TESTS += tailcall-virt.exe
+endif
 
 if HOST_WIN32
 PLATFORM_DISABLED_TESTS += bug-58782-plain-throw.exe bug-58782-capture-and-throw.exe
@@ -763,7 +775,8 @@ PLATFORM_DISABLED_TESTS += \
 	sgen-bridge-xref.exe
 endif
 
-PROFILE_DISABLED_TESTS=
+PROFILE_DISABLED_TESTS= \
+	tailcall-rgctxb.exe
 
 if FULL_AOT_TESTS
 # Tests which rely on TypeLoadExceptions
@@ -980,6 +993,7 @@ LLVM = $(filter --llvm, $(MONO_ENV_OPTIONS))
 # appdomain-marshalbyref-assemblyload.exe: https://bugzilla.xamarin.com/show_bug.cgi?id=49308
 # threads-init.exe: runs out of system threads
 # dim-constrainedcall.exe: fails on dontnet as well (https://github.com/dotnet/coreclr/issues/15353)
+# tailcall-rgctxb.exe seems like it would but it does not, needs debugging
 KNOWN_FAILING_TESTS = \
 	delegate-async-exception.exe	\
 	bug-348522.2.exe	\
@@ -998,7 +1012,8 @@ KNOWN_FAILING_TESTS = \
 	stail1.exe \
 	tail1.exe \
 	taili1.exe \
-	vtail1.exe
+	vtail1.exe \
+	tailcall-rgctxb.exe
 
 DISABLED_TESTS = \
 	$(KNOWN_FAILING_TESTS) \
@@ -1009,7 +1024,14 @@ DISABLED_TESTS = \
 	$(if $(CI_PR),$(CI_PR_DISABLED_TESTS)) \
 	$(if $(LLVM),$(LLVM_DISABLED_TESTS))
 
-INTERP_DISABLED_TESTS = $(DISABLED_TESTS)
+# Interpreter does not currently implement tailcall.
+# Disable all tailcall tests that are not otherwise disabled.
+INTERP_DISABLED_TESTS = $(DISABLED_TESTS) \
+	itail1.exe \
+	sitail1.exe \
+	tailcall-mrgctx.exe \
+	tailcall-virt.exe   \
+	tailcall-rgctxa.exe
 
 # bug-48015.exe: be careful when re-enabling, it happens that it returns with exit code 0, but doesn't actually execute the test.
 # bug-60862.exe: missing support to map IP->method; only works on platforms with altstack support.
@@ -1049,7 +1071,6 @@ INTERP_DISABLED_TESTS += \
 	field-access.exe \
 	finalizer-exception.exe \
 	handleref.exe \
-	itail1.exe \
 	monitor-abort.exe \
 	nullable_boxing.2.exe \
 	pinvoke.exe \
@@ -1058,7 +1079,6 @@ INTERP_DISABLED_TESTS += \
 	remoting2.exe \
 	remoting3.exe \
 	safehandle.2.exe \
-	sitail1.exe \
 	stackframes-async.2.exe \
 	static-constructor.exe \
 	threadpool-exceptions2.exe \
@@ -1080,10 +1100,8 @@ INTERP_DISABLED_TESTS += \
 	calliGenericTest.exe \
 	delegate-with-null-target.exe \
 	dim-diamondshape.exe \
-	itail1.exe \
 	pinvoke2.exe \
-	pinvoke3.exe \
-	sitail1.exe
+	pinvoke3.exe
 endif
 
 if AMD64
@@ -1095,11 +1113,9 @@ INTERP_DISABLED_TESTS += \
 	cominterop.exe \
 	delegate-with-null-target.exe \
 	dim-diamondshape.exe \
-	itail1.exe \
 	pinvoke.exe \
 	pinvoke2.exe \
-	pinvoke3.exe \
-	sitail1.exe
+	pinvoke3.exe
 endif
 
 TESTS_CS=$(filter-out $(DISABLED_TESTS),$(TESTS_CS_SRC:.cs=.exe))

mono/mini/genmdesc.c
@@ -94,6 +94,9 @@ load_file (const char *name) {
 			g_string_append (comment, str);
 			continue;
 		}
+		p = strchr (str, '#');
+		if (p)
+			*p = 0;
 		p = strchr (str, ':');
 		if (!p)
 			g_error ("Invalid format at line %d in %s\n", line, name);

mono/mini/genmdesc.py
@@ -95,9 +95,17 @@ def parse_mini_ops (target_define):
     return opcodes
 
 def parse_input(infile, opcodes):
+
+    # Comments are pound sign to end of string.
+    remove_comments = re.compile ("#.*")
+
     for line in infile:
         line = line.strip ()
-        if line == "" or line.startswith ("#"):
+        # remove comments
+        line = re.sub (remove_comments, "", line)
+
+        # Ignore empty lines -- including it was just a comment.
+        if line == "":
             continue
         # Lines look like:
         # expand_i2: dest:x src1:i len:18

mono/mini/mini-amd64.c
@@ -4573,9 +4573,14 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 				amd64_sse_movsd_reg_reg (code, ins->dreg, ins->sreg1);
 			break;
 		}
-		case OP_TAILCALL: {
+		case OP_TAILCALL:
+		case OP_TAILCALL_MEMBASE: {
 			MonoCallInst *call = (MonoCallInst*)ins;
 			int i, save_area_offset;
+			gboolean membase = (ins->opcode == OP_TAILCALL_MEMBASE);
+
+			// FIXME make this two pass for an accurate size
+			// FIXME varargs
 
 			g_assert (!cfg->method->save_lmf);
 
@@ -4583,12 +4588,23 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			 * space in the code buffer here again */
 			max_len += AMD64_NREG * 4 + call->stack_usage * 15 + EXTRA_CODE_SPACE;
 
+			if (membase)
+				max_len += 3;
+
 			if (G_UNLIKELY (offset + max_len > cfg->code_size)) {
 				cfg->code_size *= 2;
 				cfg->native_code = (unsigned char *) mono_realloc_native_code(cfg);
 				code = cfg->native_code + offset;
 				cfg->stat_code_reallocs++;
 			}
+
+			// FIXME This is overly pessimistic.
+			// Rax can work, swap with below.
+			// R10 can work?
+			// Unused parameter registers are also ok.
+			// non-volatiles cannot work.
+			if (membase)
+				amd64_mov_reg_reg (code, AMD64_R11, ins->sreg1, 8);
 
 			/* Restore callee saved registers */
 			save_area_offset = cfg->arch.reg_save_area_offset;
@@ -4619,13 +4635,22 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 				amd64_leave (code);
 #endif
 			}
-
-			mono_add_patch_info (cfg, code - cfg->native_code, MONO_PATCH_INFO_METHOD_JUMP, call->method);
-			if (cfg->compile_aot)
-				amd64_mov_reg_membase (code, AMD64_R11, AMD64_RIP, 0, 8);
-			else
-				amd64_set_reg_template (code, AMD64_R11);
-			amd64_jump_reg (code, AMD64_R11);
+			if (membase) {
+				// rex opcode modrm 4bytes => 7 bytes
+				amd64_jump_membase (code, AMD64_R11, ins->inst_offset);
+			} else {
+				// FIXME move this earlier to fix ABI violation
+				mono_add_patch_info (cfg, code - cfg->native_code, MONO_PATCH_INFO_METHOD_JUMP, call->method);
+				if (cfg->compile_aot) {
+					// rex opcode modrm 4bytes => 7 bytes
+					amd64_mov_reg_membase (code, AMD64_R11, AMD64_RIP, 0, 8);
+				} else {
+					// rex opcode 8byte => 10 bytes
+					amd64_set_reg_template (code, AMD64_R11);
+				}
+				// rex + opcode => 2 bytes
+				amd64_jump_reg (code, AMD64_R11);
+			}
 			ins->flags |= MONO_INST_GC_CALLSITE;
 			ins->backend.pc_offset = code - cfg->native_code;
 			break;

mono/mini/mini-amd64.h
@@ -443,6 +443,7 @@ typedef struct {
 #define MONO_ARCH_HAVE_SETUP_ASYNC_CALLBACK 1
 #define MONO_ARCH_HAVE_CREATE_LLVM_NATIVE_THUNK 1
 #define MONO_ARCH_HAVE_OP_TAIL_CALL 1
+#define MONO_ARCH_HAVE_OP_TAIL_CALL_MEMBASE 1
 #define MONO_ARCH_HAVE_SDB_TRAMPOLINES 1
 #define MONO_ARCH_HAVE_PATCH_CODE_NEW 1
 #define MONO_ARCH_HAVE_OP_GENERIC_CLASS_INIT 1

mono/mini/mini-codegen.c
@@ -596,7 +596,8 @@ mono_print_ins_index_strbuf (int i, MonoInst *ins)
 	case OP_VCALL2_MEMBASE:
 	case OP_VOIDCALL:
 	case OP_VOIDCALL_MEMBASE:
-	case OP_TAILCALL: {
+	case OP_TAILCALL:
+	case OP_TAILCALL_MEMBASE: {
 		MonoCallInst *call = (MonoCallInst*)ins;
 		GSList *list;
 

mono/mini/mini-llvm.c
@@ -3202,7 +3202,7 @@ process_call (EmitContext *ctx, MonoBasicBlock *bb, LLVMBuilderRef *builder_ref,
 	if (!ctx_ok (ctx))
 		return;
 
-	is_virtual = (ins->opcode == OP_VOIDCALL_MEMBASE || ins->opcode == OP_CALL_MEMBASE || ins->opcode == OP_VCALL_MEMBASE || ins->opcode == OP_LCALL_MEMBASE || ins->opcode == OP_FCALL_MEMBASE || ins->opcode == OP_RCALL_MEMBASE);
+	is_virtual = (ins->opcode == OP_VOIDCALL_MEMBASE || ins->opcode == OP_CALL_MEMBASE || ins->opcode == OP_VCALL_MEMBASE || ins->opcode == OP_LCALL_MEMBASE || ins->opcode == OP_FCALL_MEMBASE || ins->opcode == OP_RCALL_MEMBASE || ins->opcode == OP_TAILCALL_MEMBASE);
 	calli = !call->fptr_is_patch && (ins->opcode == OP_VOIDCALL_REG || ins->opcode == OP_CALL_REG || ins->opcode == OP_VCALL_REG || ins->opcode == OP_LCALL_REG || ins->opcode == OP_FCALL_REG || ins->opcode == OP_RCALL_REG);
 	/* Unused */
 	preserveall = FALSE;
@@ -3530,7 +3530,7 @@ process_call (EmitContext *ctx, MonoBasicBlock *bb, LLVMBuilderRef *builder_ref,
 
 	lcall = emit_call (ctx, bb, &builder, callee, args, LLVMCountParamTypes (llvm_sig));
 
-	if (ins->opcode != OP_TAILCALL && LLVMGetInstructionOpcode (lcall) == LLVMCall)
+	if (ins->opcode != OP_TAILCALL && ins->opcode != OP_TAILCALL_MEMBASE && LLVMGetInstructionOpcode (lcall) == LLVMCall)
 		mono_llvm_set_call_notail (lcall);
 
 	/*

commit 8ceead93e3ef8e7ad9f81c27401bcf1fe852c6e8

[Tailcall] Quick fix for building F#. Do not tailcall any method with a vtable_arg. (#7609)
* [Tailcall] Quick fix for building F#.
Do not tailcall any thing with a vtable_arg.
This does often work, but not in F#, along with unbox in the call target.
The problem is being further reduced and debugged, but this will serve for now.

Regression is from change b4ee86154fce71ba4eb96f4c591a5ce236d23b85.

* [Tailcall] Temporarily disable tests that depend on tailcall.

mono/mini/method-to-ir.c
@@ -6997,6 +6997,7 @@ is_supported_tail_call (MonoCompile *cfg, MonoMethod *method, MonoMethod *cmetho
 		|| (cmethod->wrapper_type && cmethod->wrapper_type != MONO_WRAPPER_DYNAMIC_METHOD)
 		|| call_opcode == CEE_CALLI
 		|| ((virtual_ || call_opcode == CEE_CALLVIRT) && !cfg->backend->have_op_tail_call_membase)
+		|| vtable_arg // FIXME
 		|| ((vtable_arg || cfg->gshared) && !cfg->backend->have_op_tail_call)
 		|| !mono_arch_tail_call_supported (cfg, mono_method_signature (method), mono_method_signature (cmethod)))
 		return FALSE;
@@ -7010,7 +7011,6 @@ is_supported_tail_call (MonoCompile *cfg, MonoMethod *method, MonoMethod *cmetho
 #if 0
 	if (!mono_debug_count ())
 		return FALSE;
-	}
 #endif
 
 	return TRUE;

mono/tests/Makefile.am
@@ -998,6 +998,8 @@ LLVM = $(filter --llvm, $(MONO_ENV_OPTIONS))
 # threads-init.exe: runs out of system threads
 # dim-constrainedcall.exe: fails on dontnet as well (https://github.com/dotnet/coreclr/issues/15353)
 # tailcall-rgctxb.exe seems like it would but it does not, needs debugging
+# tailcall-mrgctx.exe   temporarily disabled
+# tailcall-rgctxa.exe   temporarily disabled
 KNOWN_FAILING_TESTS = \
 	delegate-async-exception.exe	\
 	bug-348522.2.exe	\
@@ -1017,6 +1019,8 @@ KNOWN_FAILING_TESTS = \
 	tail1.exe \
 	taili1.exe \
 	vtail1.exe \
+	tailcall-mrgctx.exe \
+	tailcall-rgctxa.exe \
 	tailcall-rgctxb.exe
 
 DISABLED_TESTS = \