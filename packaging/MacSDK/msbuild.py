7bc29fdb1713c7b13a88b8cdf8d7354c54aff310
Ankit Jain <radical@gmail.com>
2018-03-16 19:27:03 -0400

    [msbuild] Bump msbuild to pick up fix for issue #7184
    MSBuild always recompiles project if it has a resx with a windows path.
    https://github.com/mono/mono/issues/7184

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'f1690533927874a694261992b2074a329aab8edf')
+			revision = '8ee608e5990a82670cae5d8547128db5a0ba15d6')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
