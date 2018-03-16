6cd5a10844964e3499fa442313baafd6a2395769
Jay Krell <jaykrell@microsoft.com>
2018-03-16 04:52:32 -0700

    [Tailcall] Dial back untested architectures MIPS S390x PowerPC SPARC.

--- mono/mini/mini-ppc.h
+++ mono/mini/mini-ppc.h
@@ -257,7 +257,7 @@ typedef struct MonoCompileArch {
 
 // Does the ABI have a volatile non-parameter register, so tailcall
 // can pass context to generics or interfaces?
-#define MONO_ARCH_HAVE_VOLATILE_NON_PARAM_REGISTER 1
+#define MONO_ARCH_HAVE_VOLATILE_NON_PARAM_REGISTER 0 // FIXME?
 
 #if defined(_AIX)
 /*
