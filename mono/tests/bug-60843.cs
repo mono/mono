e17541fb9cadce4064f0ed5bef4f3513908c9756
Katelyn Gadd <kg@luminance.org>
2018-03-19 12:29:24 -0700

    Revert "Don't use caller info attributes since FullAOT does not have them"
    This reverts commit 316afb686363ef0e46c15d5858bbc1b85ffd5264.

--- mono/tests/bug-60843.cs
+++ mono/tests/bug-60843.cs
@@ -7,17 +7,17 @@ class A : Attribute
     public static void Main()
     {
         var x = (C<int>.E)AttributeTest(typeof(C<>.E));
-        Assert(C<int>.E.V == x, "x.X == E.V");
+        Assert(C<int>.E.V == x);
         var y = (C<int>.E2[])AttributeTest(typeof(C<>.E2));
-        Assert(y.Length == 2, "E2 argument is two enums");
-        Assert(y[0] == C<int>.E2.A, "y[0] == E2.A");
-        Assert(y[1] == C<int>.E2.B, "y[1] == E2.B");
+        Assert(y.Length == 2);
+        Assert(y[0] == C<int>.E2.A);
+        Assert(y[1] == C<int>.E2.B);
     }
 
     public static object AttributeTest (Type t) {
         var cas = t.GetCustomAttributes(false);
-        Assert(cas.Length == 1, "There must be exactly one attribute");
-        Assert(cas[0] is A, "Attribute type must be A");
+        Assert(cas.Length == 1);
+        Assert(cas[0] is A);
         var a = (A)cas[0];
         return a.X;
     }
@@ -26,12 +26,13 @@ class A : Attribute
 
     public static void Assert (
         bool b, 
-        string message
+        [CallerFilePath] string sourceFile = null, 
+        [CallerLineNumber] int lineNumber = 0
     ) {
         AssertCount++;
 
         if (!b) {
-            Console.Error.WriteLine($"Assert #{AssertCount} failed: {message}");
+            Console.Error.WriteLine($"Assert failed at {sourceFile}:{lineNumber}");
             Environment.Exit(AssertCount);
         }
     }
