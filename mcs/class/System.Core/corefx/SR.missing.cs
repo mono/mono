b3881a418f1391a33c1b709f1113574b74ae85ae
Marek Safar <marek.safar@gmail.com>
2018-03-20 10:02:33 +0100

    CoreFX update

--- mcs/class/System.Core/corefx/SR.missing.cs
+++ mcs/class/System.Core/corefx/SR.missing.cs
@@ -1,4 +1,5 @@
 partial class SR
 { 
 	public const string MethodBuilderDoesNotHaveTypeBuilder = "MethodBuilder does not have a valid TypeBuilder";
+	public const string InvalidOperation_ConcurrentOperationsNotSupported = "Operations that change non-concurrent collections must have exclusive access. A concurrent update was performed on this collection and corrupted its state. The collection's state is no longer correct.";
 }
