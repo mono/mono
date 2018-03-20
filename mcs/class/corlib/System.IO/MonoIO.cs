mono/mono

commit 85f4b5b16704262bda7eb745aa74d125afa5c4ea

[coop-handles] System.IO.MonoIO.ReplaceFile

mcs/class/corlib/System.IO/MonoIO.cs
@@ -245,11 +245,26 @@ namespace System.IO
 		}
 
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
-		public extern static bool ReplaceFile (string sourceFileName, 
-						       string destinationFileName, 
-						       string destinationBackupFileName, 
-						       bool ignoreMetadataErrors,
-						       out MonoIOError error);
+		private unsafe extern static bool ReplaceFile (char* sourceFileName,
+							       char* destinationFileName,
+							       char* destinationBackupFileName,
+							       bool ignoreMetadataErrors,
+							       out MonoIOError error);
+
+		public static bool ReplaceFile (string sourceFileName,
+						string destinationFileName,
+						string destinationBackupFileName,
+						bool ignoreMetadataErrors,
+						out MonoIOError error)
+		{
+			unsafe {
+				fixed (char* sourceFileNameChars = sourceFileName,
+				       destinationFileNameChars = destinationFileName,
+				       destinationBackupFileNameChars = destinationBackupFileName) {
+					return ReplaceFile (sourceFileNameChars, destinationFileNameChars, destinationBackupFileNameChars, ignoreMetadataErrors, out error);
+				}
+			}
+		}
 
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
 		private unsafe extern static FileAttributes GetFileAttributes (char* path, out MonoIOError error);

mono/metadata/icall-def.h
@@ -382,7 +382,7 @@ ICALL(MONOIO_16, "Open(char*,System.IO.FileMode,System.IO.FileAccess,System.IO.F
 ICALL(MONOIO_17, "Read(intptr,byte[],int,int,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_Read)
 #ifndef PLATFORM_RO_FS
 ICALL(MONOIO_18, "RemoveDirectory(char*,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_RemoveDirectory)
-ICALL(MONOIO_18M, "ReplaceFile(string,string,string,bool,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_ReplaceFile)
+ICALL(MONOIO_18M, "ReplaceFile(char*,char*,char*,bool,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_ReplaceFile)
 #endif /* !PLATFORM_RO_FS */
 ICALL(MONOIO_19, "Seek(intptr,long,System.IO.SeekOrigin,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_Seek)
 ICALL(MONOIO_20, "SetCurrentDirectory(string,System.IO.MonoIOError&)", ves_icall_System_IO_MonoIO_SetCurrentDirectory)

mono/metadata/w32file-unix.c
@@ -4714,11 +4714,11 @@ mono_w32file_copy (const gunichar2 *path, const gunichar2 *dest, gboolean overwr
 }
 
 gboolean
-mono_w32file_replace (gunichar2 *destinationFileName, gunichar2 *sourceFileName, gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error)
+mono_w32file_replace (const gunichar2 *destination_file_name, const gunichar2 *source_file_name, const gunichar2 *destination_backup_file_name, guint32 flags, gint32 *error)
 {
 	gboolean result;
 
-	result = ReplaceFile (destinationFileName, sourceFileName, destinationBackupFileName, flags, NULL, NULL);
+	result = ReplaceFile (destination_file_name, source_file_name, destination_backup_file_name, flags, NULL, NULL);
 	if (!result)
 		*error = mono_w32error_get_last ();
 	return result;

mono/metadata/w32file-win32-uwp.c
@@ -29,8 +29,8 @@ mono_w32file_move (const gunichar2 *path, const gunichar2 *dest, gint32 *error)
 }
 
 gboolean
-mono_w32file_replace (gunichar2 *destinationFileName, gunichar2 *sourceFileName,
-			   gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error)
+mono_w32file_replace (const gunichar2 *destinationFileName, const gunichar2 *sourceFileName,
+			   const gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error)
 {
 	gboolean result = FALSE;
 	MONO_ENTER_GC_SAFE;

mono/metadata/w32file-win32.c
@@ -404,7 +404,7 @@ mono_w32file_move (const gunichar2 *path, const gunichar2 *dest, gint32 *error)
 }
 
 gboolean
-mono_w32file_replace (gunichar2 *destinationFileName, gunichar2 *sourceFileName, gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error)
+mono_w32file_replace (const gunichar2 *destinationFileName, const gunichar2 *sourceFileName, const gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error)
 {
 	gboolean result;
 

mono/metadata/w32file.c
@@ -334,27 +334,19 @@ ves_icall_System_IO_MonoIO_MoveFile (const gunichar2 *path, const gunichar2 *des
 }
 
 MonoBoolean
-ves_icall_System_IO_MonoIO_ReplaceFile (MonoString *sourceFileName, MonoString *destinationFileName,
-					MonoString *destinationBackupFileName, MonoBoolean ignoreMetadataErrors,
+ves_icall_System_IO_MonoIO_ReplaceFile (const gunichar2 *source_file_name, const gunichar2 *destination_file_name,
+					const gunichar2 *destination_backup_file_name, MonoBoolean ignore_metadata_errors,
 					gint32 *error)
 {
-	gunichar2 *utf16_sourceFileName = NULL, *utf16_destinationFileName = NULL, *utf16_destinationBackupFileName = NULL;
-	guint32 replaceFlags = REPLACEFILE_WRITE_THROUGH;
-
-	if (sourceFileName)
-		utf16_sourceFileName = mono_string_chars (sourceFileName);
-	if (destinationFileName)
-		utf16_destinationFileName = mono_string_chars (destinationFileName);
-	if (destinationBackupFileName)
-		utf16_destinationBackupFileName = mono_string_chars (destinationBackupFileName);
+	guint32 replace_flags = REPLACEFILE_WRITE_THROUGH;
 
 	*error = ERROR_SUCCESS;
-	if (ignoreMetadataErrors)
-		replaceFlags |= REPLACEFILE_IGNORE_MERGE_ERRORS;
+	if (ignore_metadata_errors)
+		replace_flags |= REPLACEFILE_IGNORE_MERGE_ERRORS;
 
 	/* FIXME: source and destination file names must not be NULL, but apparently they might be! */
-	return mono_w32file_replace (utf16_destinationFileName, utf16_sourceFileName,
-					  utf16_destinationBackupFileName, replaceFlags, error);
+	return mono_w32file_replace (destination_file_name, source_file_name,
+					  destination_backup_file_name, replace_flags, error);
 }
 
 MonoBoolean

mono/metadata/w32file.h
@@ -241,8 +241,8 @@ extern void ves_icall_System_IO_MonoIO_Unlock (gpointer handle, gint64 position,
 					       gint64 length, gint32 *error);
 
 extern MonoBoolean
-ves_icall_System_IO_MonoIO_ReplaceFile (MonoString *sourceFileName, MonoString *destinationFileName,
-					MonoString *destinationBackupFileName, MonoBoolean ignoreMetadataErrors,
+ves_icall_System_IO_MonoIO_ReplaceFile (const gunichar2 *source_file_name, const gunichar2 *destination_file_name,
+					const gunichar2 *destination_backup_file_name, MonoBoolean ignore_metadata_errors,
 					gint32 *error);
 
 #if defined (TARGET_IOS) || defined (TARGET_ANDROID)
@@ -403,7 +403,7 @@ gboolean
 mono_w32file_lock (gpointer handle, gint64 position, gint64 length, gint32 *error);
 
 gboolean
-mono_w32file_replace (gunichar2 *destinationFileName, gunichar2 *sourceFileName, gunichar2 *destinationBackupFileName, guint32 flags, gint32 *error);
+mono_w32file_replace (const gunichar2 *destination_file_name, const gunichar2 *source_file_name, const gunichar2 *destination_backup_file_name, guint32 flags, gint32 *error);
 
 gboolean
 mono_w32file_unlock (gpointer handle, gint64 position, gint64 length, gint32 *error);

commit 65c4deb0b4670e63d2e77d1afb165893a5145cb4

[coop] fix regression in System.IO.MonoIO.ReplaceFile
introduced by 85f4b5b16704262bda7eb745aa74d125afa5c4ea

fixes crash that looks like this:
```
    frame #3: 0x000000010b91351a mono`mono_handle_native_crash(signal="SIGSEGV", ctx=0x0000000000000000, info=0x0000000000000000) at mini-exceptions.c:2652
    frame #4: 0x000000010ba059eb mono`altstack_handle_and_restore(ctx=0x00007ffee43ed3a0, obj=0x0000000000000000, stack_ovf=0) at exceptions-amd64.c:855
    frame #5: 0x000000010bcdcda7 mono`monoeg_g_utf16_to_utf8(str=0x0000000000000014, len=0, items_read=0x0000000000000000, items_written=0x0000000000000000, err=0x0000000000000000) at giconv.c:1036
    frame #6: 0x000000010bcc1a5b mono`mono_unicode_to_external(uni=0x0000000000000014) at strenc.c:183
    frame #7: 0x000000010ba77c3e mono`convert_arg_to_utf8(arg=0x0000000000000014, arg_name="backupFileName") at w32file-unix.c:2459
    frame #8: 0x000000010ba75d65 mono`ReplaceFile(replacedFileName=0x000000010c7b4ba4, replacementFileName=0x000000010c7b473c, backupFileName=0x0000000000000014, replaceFlags=1, exclude=0x0000000000000000, reserved=0x0000000000000000) at w32file-unix.c:2483
    frame #9: 0x000000010ba75c87 mono`mono_w32file_replace(destination_file_name=0x000000010c7b4ba4, source_file_name=0x000000010c7b473c, destination_backup_file_name=0x0000000000000014, flags=1, error=0x00007ffee43efbe8) at w32file-unix.c:4795
    frame #10: 0x000000010bad02e9 mono`ves_icall_System_IO_MonoIO_ReplaceFile(source_file_name=0x000000010c7b473c, destination_file_name=0x000000010c7b4ba4, destination_backup_file_name=0x0000000000000014, ignore_metadata_errors='\0', error=0x00007ffee43efbe8) at w32file.c:348
```

happens when running `FileInfoTest::Replace1_Backup_Null ()` of corlib
tests
(https://github.com/mono/mono/blob/7dea71ce64bb77cb96ff3dfb06bb9141fb73c3f0/mcs/class/corlib/Test/System.IO/FileInfoTest.cs#L937
) if it's compiled with `mcs`. Not sure why it doesn't trigger with
`roslyn`...

mcs/class/corlib/System.IO/MonoIO.cs
@@ -270,7 +270,10 @@ namespace System.IO
 				fixed (char* sourceFileNameChars = sourceFileName,
 				       destinationFileNameChars = destinationFileName,
 				       destinationBackupFileNameChars = destinationBackupFileName) {
-					return ReplaceFile (sourceFileNameChars, destinationFileNameChars, destinationBackupFileNameChars, ignoreMetadataErrors, out error);
+					return ReplaceFile (sourceFileName == null ? null : sourceFileNameChars,
+							destinationFileName == null ? null : destinationFileNameChars,
+							destinationBackupFileName == null ? null : destinationBackupFileNameChars,
+							ignoreMetadataErrors, out error);
 				}
 			}
 		}