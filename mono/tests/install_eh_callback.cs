a20760c3d2ddb43512e71c782cfbbfb8121175a3
Aleksey Kliger <alklig@microsoft.com>
2018-03-23 10:53:45 -0400

    [test] AOT-friendly test for mono_install_ftnptr_eh_callback

--- mono/tests/install_eh_callback.cs
+++ mono/tests/install_eh_callback.cs
@@ -1,5 +1,10 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+public class MonoPInvokeCallbackAttribute : Attribute {
+	public MonoPInvokeCallbackAttribute (Type delegateType) { }
+}
 
 public class Tests {
 
@@ -13,24 +18,39 @@ public class Tests {
 
 	public class SomeOtherExn : Exception {
 	}
+
+	[MethodImpl (MethodImplOptions.NoInlining)]
 	private static void callee (ref bool called) {
 		called = true;
 		throw new SpecialExn ();
 	}
 
-	public static int test_0_setjmp_exn_handler ()
-	{
-		IntPtr res;
-		bool called = false;
-		bool finally_called = false;
-		VoidVoidDelegate f = () => {
+	public class Caller {
+		public static bool called;
+		public static bool finally_called;
+		
+		public static void Setup () {
+			called = false;
+			finally_called = false;
+		}
+
+		[MonoPInvokeCallback (typeof (VoidVoidDelegate))]
+		public static void M () {
 			try {
 				callee (ref called);
 				throw new Exception ("unexpected return from callee");
+			} catch (SomeOtherExn) {
 			} finally {
 				finally_called = true;
 			}
-		};
+		}
+	}
+
+	public static int test_0_setjmp_exn_handler ()
+	{
+		IntPtr res;
+		Caller.Setup ();
+		VoidVoidDelegate f = new VoidVoidDelegate (Caller.M);
 			
 		try {
 			mono_test_setjmp_and_call (f, out res);
@@ -38,15 +58,15 @@ public class Tests {
 			Console.Error.WriteLine ("should not have caught a SpecialExn");
 			return 1;
 		}
-		if (!called) {
+		if (!Caller.called) {
 			Console.Error.WriteLine ("delegate not even called");
 			return 2;
 		}
-		if (!finally_called) {
+		if (!Caller.finally_called) {
 			Console.Error.WriteLine ("finally not reached");
 			return 3;
 		}
-		if (res == null) {
+		if (res == IntPtr.Zero) {
 			Console.Error.WriteLine ("res should be a GCHandle, was 0");
 			return 4;
 		}
