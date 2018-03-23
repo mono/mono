9851401e2fb6dadc2ab8b31f13cfc8a887abbe9e
Ludovic Henry <ludovic@xamarin.com>
2018-03-22 20:40:33 -0400

    Fix Android build for mxe

--- sdks/builds/mxe.mk
+++ sdks/builds/mxe.mk
@@ -1,4 +1,6 @@
 
+MXE_PREFIX?=$(TOP)/sdks/out/mxe
+
 $(TOP)/sdks/builds/toolchains/mxe:
 	git clone -b xamarin https://github.com/xamarin/mxe.git $@
 
@@ -11,10 +13,9 @@ $(TOP)/sdks/builds/toolchains/mxe:
 
 .PHONY: build-custom-mxe
 build-custom-mxe:
-	PATH="$$PATH:$(dir $(shell which autopoint))" $(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
-		MXE_TARGETS="i686-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe" OS_SHORT_NAME="disable-native-plugins"
-	PATH="$$PATH:$(dir $(shell which autopoint))" $(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
-		MXE_TARGETS="x86_64-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe" OS_SHORT_NAME="disable-native-plugins"
+	$(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe gcc cmake zlib pthreads dlfcn-win32 mman-win32 \
+		MXE_TARGETS="i686-w64-mingw32.static x86_64-w64-mingw32.static" PREFIX="$(MXE_PREFIX)"
+			OS_SHORT_NAME="disable-native-plugins" PATH="$$PATH:$(dir $(shell brew list gettext | grep autopoint$))"
 
 .PHONY: setup-custom-mxe
 setup-custom-mxe:
@@ -24,9 +25,6 @@ package-mxe:
 
 .PHONY: clean-mxe
 clean-mxe:
-	$(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe clean \
-		MXE_TARGETS="i686-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe"
-	$(MAKE) -C $(TOP)/sdks/builds/toolchains/mxe clean \
-		MXE_TARGETS="x86_64-w64-mingw32.static" PREFIX="$(TOP)/sdks/out/mxe"
+	rm -rf $(TOP)/sdks/builds/toolchains/mxe $(MXE_PREFIX)
 
 TARGETS += mxe
