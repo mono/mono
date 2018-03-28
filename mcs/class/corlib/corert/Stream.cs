3c76d261e16ee5fcc037af17fbf6e839e86cefcf
Marek Safar <marek.safar@gmail.com>
2018-03-28 23:11:50 +0200

    Hide new API which didn't make it to stable on time for dev15.7

--- mcs/class/corlib/corert/Stream.cs
+++ mcs/class/corlib/corert/Stream.cs
@@ -23,7 +23,7 @@ namespace System.IO
 			throw new NotImplementedException ();
 		}
 
-		public virtual Task WriteAsync (ReadOnlyMemory<byte> source, CancellationToken cancellationToken = default(CancellationToken))
+		internal virtual Task WriteAsync (ReadOnlyMemory<byte> source, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			throw new NotImplementedException ();
 		}
