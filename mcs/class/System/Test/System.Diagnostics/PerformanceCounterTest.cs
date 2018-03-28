f1743e80038ed4a660f253037ce38a30458c3fe6
Zoltan Varga <vargaz@gmail.com>
2018-03-28 13:32:48 -0400

    [bcl] Fix the compilation of System tests in the mobile profile.

--- mcs/class/System/Test/System.Diagnostics/PerformanceCounterTest.cs
+++ mcs/class/System/Test/System.Diagnostics/PerformanceCounterTest.cs
@@ -19,6 +19,8 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Specialized;
 
+#if !MOBILE
+
 namespace MonoTests.System.Diagnostics
 {
 	// The first part is from external/corefx.
@@ -326,3 +328,5 @@ namespace MonoTests.System.Diagnostics
 		}
 	}
 }
+
+#endif
