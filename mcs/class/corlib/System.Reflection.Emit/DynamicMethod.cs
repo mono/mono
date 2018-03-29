77b2db73aafee3c38f40d86ad63cc020d9040078
Austin Wise <AustinWise@gmail.com>
2018-03-29 05:18:27 -0700

    Add BindingFlags.DoNotWrapExceptions (#7863)
    The original design review is here: dotnet/corefx#22866 . To summarize, this adds a new flag to `BindingFlags` called `DoNotWrapExceptions`. When this flag is used to invoke a method using reflection, any exceptions thrown are not wrapped with a `TargetInvocationException`.
    
    It has already been implemented in some other version of .NET:
    - dotnet/corert#4437
    - dotnet/coreclr#13767
    
    I would be delighted if this handy feature was also available in Mono.
    
    I have in part based my changes on the CoreCLR implementation. There all already tests for this feature in corefx, so I have added those to the corlib_xtest.dll.sources file.
    
    I have a couple of concerns about my implementation:
    - I notice that `DynamicMethod.Invoke` was ignoring the BindingFlags and other arguments. I changed this method to pass along all the arguments. For what it's worth, it appears that [CoreCLR respects](https://github.com/dotnet/coreclr/blob/master/src/mscorlib/src/System/Reflection/Emit/DynamicMethod.cs#L488) these arguments.

--- mcs/class/corlib/System.Reflection.Emit/DynamicMethod.cs
+++ mcs/class/corlib/System.Reflection.Emit/DynamicMethod.cs
@@ -296,7 +296,7 @@ namespace System.Reflection.Emit {
 				if (method == null)
 					method = new MonoMethod (mhandle);
 
-				return method.Invoke (obj, parameters);
+				return method.Invoke (obj, invokeAttr, binder, parameters, culture);
 			}
 			catch (MethodAccessException mae) {
 				throw new TargetInvocationException ("Method cannot be invoked.", mae);
