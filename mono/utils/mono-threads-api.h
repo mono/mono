f9f032e94ecae94aa9ebee3f3e98cea04e0d5178
Jay Krell <jaykrell@microsoft.com>
2018-03-30 03:58:42 -0700

    [coop] Include function names in coop state change assertions
    i.e. better reporting here:
    
    https://jenkins.mono-project.com/job/test-mono-pull-request-coop/2298/parsed_console/log.html
    
    ***** MonoTests.System.Runtime.Remoting.RemotingServicesTest
    ***** MonoTests.System.Runtime.Remoting.RemotingServicesTest.Bug46473
    Assertion failure in thread 0x7f21a504b740 due to: Cannot transition thread 0x7f21a504b740 from STATE_BLOCKING with DO_BLOCKING
    Last 3 state transitions: (most recent first)
    [DO_BLOCKING] RUNNING -> STATE_BLOCKING (0) 0 at:
    	at /mnt/jenkins/workspace/test-mono-pull-request-coop/mono/mini/mono(+0x2ef31a) [0x560336a6f31a]
    	at /mnt/jenkins/workspace/test-mono-pull-request-coop/mono/mini/mono(+0x2eb477) [0x560336a6b477]
    	at /mnt/jenkins/workspace/test-mono-pull-request-coop/mono/mini/mono(+0x2ec7a2) [0x560336a6c7a2]
    	at /mnt/jenkins/workspace/test-mono-pull-request-coop/mono/mini/mono(+0x2ec7e6) [0x560336a6c7e6]
    
    This will provide one function instead of 0 or n, and could be expanded to include more.
    
    This bloats shipping binary some (under condfigure only) which can be addressed with
    an #if that condtionally turns the strings to null or empty. And slight code bloat.
    
    Note that products and test use all this stuff and are left compatible and unchanged.

--- mono/utils/mono-threads-api.h
+++ mono/utils/mono-threads-api.h
@@ -22,34 +22,62 @@ MONO_BEGIN_DECLS
 This API is experimental. It will eventually be required to properly use the rest of the raw-omp embedding API.
 */
 
+typedef struct _MonoStackData {
+	const char *function_name;
+} MonoStackData;
+// const here triggers a clang bug, auto is changed to static
+#define MONO_STACK_DATA(x) /*const*/ MonoStackData x = { __func__ };
+
 MONO_API gpointer
 mono_threads_enter_gc_unsafe_region (gpointer* stackdata);
+
+gpointer
+mono_threads_enter_gc_unsafe_region_internal (MonoStackData *stackdata);
 
 MONO_API void
 mono_threads_exit_gc_unsafe_region (gpointer cookie, gpointer* stackdata);
 
+void
+mono_threads_exit_gc_unsafe_region_internal (gpointer cookie, MonoStackData *stackdata);
+
 MONO_API gpointer
 mono_threads_enter_gc_unsafe_region_unbalanced (gpointer* stackdata);
+
+gpointer
+mono_threads_enter_gc_unsafe_region_unbalanced_internal (MonoStackData *stackdata);
 
 MONO_API void
 mono_threads_exit_gc_unsafe_region_unbalanced (gpointer cookie, gpointer* stackdata);
 
+void
+mono_threads_exit_gc_unsafe_region_unbalanced_internal (gpointer cookie, MonoStackData *stackdata);
+
 MONO_API void
 mono_threads_assert_gc_unsafe_region (void);
-
-
 
 MONO_API gpointer
 mono_threads_enter_gc_safe_region (gpointer *stackdata);
 
+gpointer
+mono_threads_enter_gc_safe_region_internal (MonoStackData *stackdata);
+
 MONO_API void
 mono_threads_exit_gc_safe_region (gpointer cookie, gpointer *stackdata);
+
+void
+mono_threads_exit_gc_safe_region_internal (gpointer cookie, MonoStackData *stackdata);
 
 MONO_API gpointer
 mono_threads_enter_gc_safe_region_unbalanced (gpointer *stackdata);
 
+gpointer
+mono_threads_enter_gc_safe_region_unbalanced_internal (MonoStackData *stackdata);
+
 MONO_API void
 mono_threads_exit_gc_safe_region_unbalanced (gpointer cookie, gpointer *stackdata);
+
+void
+mono_threads_exit_gc_safe_region_unbalanced_internal (gpointer cookie, MonoStackData *stackdata);
 
 MONO_API void
 mono_threads_assert_gc_safe_region (void);
@@ -63,38 +91,38 @@ http://www.mono-project.com/docs/advanced/runtime/docs/coop-suspend/#gc-unsafe-m
 */
 #define MONO_ENTER_GC_UNSAFE	\
 	do {	\
-		gpointer __gc_unsafe_dummy;	\
-		gpointer __gc_unsafe_cookie = mono_threads_enter_gc_unsafe_region (&__gc_unsafe_dummy)
+		MONO_STACK_DATA (__gc_unsafe_dummy); \
+		gpointer __gc_unsafe_cookie = mono_threads_enter_gc_unsafe_region_internal (&__gc_unsafe_dummy)
 
 #define MONO_EXIT_GC_UNSAFE	\
-		mono_threads_exit_gc_unsafe_region	(__gc_unsafe_cookie, &__gc_unsafe_dummy);	\
+		mono_threads_exit_gc_unsafe_region_internal (__gc_unsafe_cookie, &__gc_unsafe_dummy);	\
 	} while (0)
 
 #define MONO_ENTER_GC_UNSAFE_UNBALANCED	\
 	do {	\
-		gpointer __gc_unsafe_unbalanced_dummy;	\
-		gpointer __gc_unsafe_unbalanced_cookie = mono_threads_enter_gc_unsafe_region_unbalanced (&__gc_unsafe_unbalanced_dummy)
+		MONO_STACK_DATA (__gc_unsafe_unbalanced_dummy); \
+		gpointer __gc_unsafe_unbalanced_cookie = mono_threads_enter_gc_unsafe_region_unbalanced_internal (&__gc_unsafe_unbalanced_dummy)
 
 #define MONO_EXIT_GC_UNSAFE_UNBALANCED	\
-		mono_threads_exit_gc_unsafe_region_unbalanced	(__gc_unsafe_unbalanced_cookie, &__gc_unsafe_unbalanced_dummy);	\
+		mono_threads_exit_gc_unsafe_region_unbalanced_internal (__gc_unsafe_unbalanced_cookie, &__gc_unsafe_unbalanced_dummy);	\
 	} while (0)
 
 #define MONO_ENTER_GC_SAFE	\
 	do {	\
-		gpointer __gc_safe_dummy;	\
-		gpointer __gc_safe_cookie = mono_threads_enter_gc_safe_region (&__gc_safe_dummy)
+		MONO_STACK_DATA (__gc_safe_dummy); \
+		gpointer __gc_safe_cookie = mono_threads_enter_gc_safe_region_internal (&__gc_safe_dummy)
 
 #define MONO_EXIT_GC_SAFE	\
-		mono_threads_exit_gc_safe_region (__gc_safe_cookie, &__gc_safe_dummy);	\
+		mono_threads_exit_gc_safe_region_internal (__gc_safe_cookie, &__gc_safe_dummy);	\
 	} while (0)
 
 #define MONO_ENTER_GC_SAFE_UNBALANCED	\
 	do {	\
-		gpointer __gc_safe_unbalanced_dummy;	\
-		gpointer __gc_safe_unbalanced_cookie = mono_threads_enter_gc_safe_region_unbalanced (&__gc_safe_unbalanced_dummy)
+		MONO_STACK_DATA (__gc_safe_unbalanced_dummy); \
+		gpointer __gc_safe_unbalanced_cookie = mono_threads_enter_gc_safe_region_unbalanced_internal (&__gc_safe_unbalanced_dummy)
 
 #define MONO_EXIT_GC_SAFE_UNBALANCED	\
-		mono_threads_exit_gc_safe_region_unbalanced (__gc_safe_unbalanced_cookie, &__gc_safe_unbalanced_dummy);	\
+		mono_threads_exit_gc_safe_region_unbalanced_internal (__gc_safe_unbalanced_cookie, &__gc_safe_unbalanced_dummy);	\
 	} while (0)
 
 MONO_END_DECLS
