0bbbaac5eebeea34c2adc21f1de92f7c8b785cca
Marek Safar <marek.safar@gmail.com>
2018-04-05 17:15:05 +0200

    [mcs] Implements C# 7.2 stackalloc span conversion in conditional expression

--- mcs/tests/test-948.cs
+++ mcs/tests/test-948.cs
@@ -1,4 +1,4 @@
-// Compiler options: -langversion:7.2 -unsafe
+// Compiler options: -langversion:7.2 /unsafe
 
 using System;
 
@@ -7,10 +7,16 @@ class X
 	public static void Main ()
 	{
 		Span<int> stackSpan = stackalloc int[100];
+
+		bool b = false;
+
+		var r1 = !b ? stackalloc char[1] : throw null;
+		var r2 = b ? throw null : stackalloc char[1];
+		var r3 = b ? stackalloc char[1] : stackalloc char[2];
 	}
 
+	// Disables verifier
 	unsafe void Foo ()
 	{
-
 	}
 }
