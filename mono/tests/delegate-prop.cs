mono/mono

commit 77b4cd11d1d7c7b79c426ddc5a343b373a3b2cc3

Only null check target for delegate if method is not static. (#5833)
* Only null check target for delegate if method is not static. Fixes regression introduced by 1cfd52452a4af3fd6eee32f45a8067926af8479e.

* [interp] pass first stack arg to transform, so we get the correct delegate wrapper

the wrapper logic detects if the target method of a delegate is a static
method with an argument bound, and handles it properly in the generated
code.

fixes mono/tests/delegate14.cs

mono/mini/interp/transform.c
@@ -4224,7 +4224,7 @@ mono_interp_transform_init (void)
 }
 
 MonoException *
-mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context)
+mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context, MonoDelegate *del)
 {
 	MonoError error;
 	int i, align, size, offset;
@@ -4295,7 +4295,7 @@ mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context)
 					nm = mono_marshal_get_icall_wrapper (mi->sig, wrapper_name, mi->func, TRUE);
 					g_free (wrapper_name);
 				} else if (*name == 'I' && (strcmp (name, "Invoke") == 0)) {
-					nm = mono_marshal_get_delegate_invoke (method, NULL);
+					nm = mono_marshal_get_delegate_invoke (method, del);
 				} else if (*name == 'B' && (strcmp (name, "BeginInvoke") == 0)) {
 					nm = mono_marshal_get_delegate_begin_invoke (method);
 				} else if (*name == 'E' && (strcmp (name, "EndInvoke") == 0)) {

mono/mini/interp/interp-internals.h
@@ -149,7 +149,7 @@ extern int mono_interp_traceopt;
 extern GSList *jit_classes;
 
 MonoException *
-mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context);
+mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context, MonoDelegate *del);
 
 void
 mono_interp_transform_init (void);

mono/mini/interp/interp.c
@@ -1908,7 +1908,7 @@ do_transform_method (InterpFrame *frame, ThreadContext *context)
 	/* Use the parent frame as the current frame is not complete yet */
 	interp_push_lmf (&ext, frame->parent);
 
-	frame->ex = mono_interp_transform_method (frame->imethod, context);
+	frame->ex = mono_interp_transform_method (frame->imethod, context, (MonoDelegate *) frame->stack_args [0].data.p);
 
 	interp_pop_lmf (&ext);
 }
@@ -2391,7 +2391,7 @@ ves_exec_method_with_context (InterpFrame *frame, ThreadContext *context, unsign
 
 			if (!new_method->transformed) {
 				frame->ip = ip;
-				frame->ex = mono_interp_transform_method (new_method, context);
+				frame->ex = mono_interp_transform_method (new_method, context, NULL);
 				if (frame->ex)
 					goto exit_frame;
 			}

mono/mini/method-to-ir.c
@@ -3973,8 +3973,10 @@ handle_delegate_ctor (MonoCompile *cfg, MonoClass *klass, MonoInst *target, Mono
 	/* Set target field */
 	/* Optimize away setting of NULL target */
 	if (!MONO_INS_IS_PCONST_NULL (target)) {
-		MONO_EMIT_NEW_BIALU_IMM (cfg, OP_COMPARE_IMM, -1, target->dreg, 0);
-		MONO_EMIT_NEW_COND_EXC (cfg, EQ, "NullReferenceException");
+		if (!(method->flags & METHOD_ATTRIBUTE_STATIC)) {
+			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_COMPARE_IMM, -1, target->dreg, 0);
+			MONO_EMIT_NEW_COND_EXC (cfg, EQ, "NullReferenceException");
+		}
 		MONO_EMIT_NEW_STORE_MEMBASE (cfg, OP_STORE_MEMBASE_REG, obj->dreg, MONO_STRUCT_OFFSET (MonoDelegate, target), target->dreg);
 		if (cfg->gen_write_barriers) {
 			dreg = alloc_preg (cfg);

mono/tests/delegate14.cs
@@ -6,6 +6,12 @@ public static class Program
 {
 	public static int Main (string[] args)
 	{
+		// calling delegate on extension method with null target is allowed
+		Func<int> func = null;
+		if (CallFunc(func.CallFuncIfNotNull) != 0)
+			return 2;
+
+		// constructing delegate on instance method with null target should throw
 		ITest obj = null;
 		try
 		{
@@ -22,5 +28,21 @@ public static class Program
 	interface ITest
 	{
 		void Func ();
+	}
+
+	static int CallFunc(Func<int> func)
+	{
+		return func();
+	}
+}
+
+public static class FuncExtensions
+{
+	public static int CallFuncIfNotNull(this Func<int> func)
+	{
+		if (func != null)
+			return func();
+
+		return 0;
 	}
 }

commit a6d296d6f66dde4fe399b6d0a56608bfc5a044da

[interp] fix regression for virtual delegates
Regression introduced in 77b4cd11d1d7c7b79c426ddc5a343b373a3b2cc3. We
need to distinguish this case.

Fixes #7944

mono/mini/interp/transform.c
@@ -4637,7 +4637,10 @@ mono_interp_transform_method (InterpMethod *imethod, ThreadContext *context, Int
 					g_free (wrapper_name);
 				} else if (*name == 'I' && (strcmp (name, "Invoke") == 0)) {
 					MonoDelegate *del = frame->stack_args [0].data.p;
-					nm = mono_marshal_get_delegate_invoke (method, del);
+					if (del && del->method && del->method->flags & METHOD_ATTRIBUTE_STATIC)
+						nm = mono_marshal_get_delegate_invoke (method, del);
+					else
+						nm = mono_marshal_get_delegate_invoke (method, NULL);
 				} else if (*name == 'B' && (strcmp (name, "BeginInvoke") == 0)) {
 					nm = mono_marshal_get_delegate_begin_invoke (method);
 				} else if (*name == 'E' && (strcmp (name, "EndInvoke") == 0)) {

mono/tests/Makefile.am
@@ -565,6 +565,7 @@ TESTS_CS_SRC=		\
 	bug-685908.cs	\
 	sgen-long-vtype.cs	\
 	delegate-invoke.cs	\
+	delegate-prop.cs	\
 	bug-696593.cs	\
 	bug-705140.cs	\
 	bug-1147.cs	\

mono/tests/delegate-prop.cs
@@ -1,0 +1,38 @@
+using System;
+using System.Reflection;
+using System.Globalization;
+using System.Diagnostics;
+
+/* Regression test for https://github.com/mono/mono/issues/7944 */
+public class MyClass
+{
+	public string Prop1 { get; set; }
+	public string Prop2 { get; set; }
+
+	public void DoRepro()
+	{
+		var prop1Setter = this.GetType ().GetProperty (nameof (Prop1)).GetSetMethod ();
+		var prop2Setter = this.GetType ().GetProperty (nameof (Prop2)).GetSetMethod ();
+		var prop1Delegate = (Action <MyClass, string>) prop1Setter.CreateDelegate(typeof (Action <MyClass, string>));
+		var prop2Delegate = (Action <MyClass, string>) prop2Setter.CreateDelegate(typeof (Action <MyClass, string>));
+		prop1Delegate (this, "prop1Value");
+		prop2Delegate (this, "prop2Value");
+
+		// Console.WriteLine ($"prop1: {Prop1}");
+		// Console.WriteLine ($"prop2: {Prop2}");
+	}
+
+	public static int Main (string []args) {
+		var o = new MyClass ();
+		o.DoRepro ();
+
+		if (o.Prop1 != "prop1Value")
+			return 1;
+
+		if (o.Prop2 != "prop2Value")
+			return 2;
+
+		return 0;
+	}
+}
+