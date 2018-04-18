e107b6d9ba29f9201a9ec28d8f996c1ce1c10302
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-04-18 13:53:17 +0200

    Fix ZipPackage logic

--- mcs/class/WindowsBase/System.IO.Packaging/ZipPackage.cs
+++ mcs/class/WindowsBase/System.IO.Packaging/ZipPackage.cs
@@ -226,14 +226,9 @@ namespace System.IO.Packaging {
 				var extension = Path.GetExtension (part.Uri.OriginalString);
 				if (extension.Length > 0)
 					extension = extension.Substring (1);
-				
-				if (extension.Length == 0) {
-					// If extension is empty string (no extension), can possible goes here,
-					// So to avoid that, we give a condition, only having extension will go here.
-					continue;
-				}
 
-				if (!mimes.TryGetValue (extension, out existingMimeType)) {
+				if (!mimes.TryGetValue (extension, out existingMimeType) && extension.Length != 0) {
+					// we should only get here when we have an extension (non-empty string)
 					node = doc.CreateNode (XmlNodeType.Element, "Default", ContentNamespace);
 					
 					XmlAttribute ext = doc.CreateAttribute ("Extension");
