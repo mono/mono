b7e2b2b620205047a3a4680436ec18f1e060b92b
Vlad Brezae <brezaevlad@gmail.com>
2018-04-19 14:54:39 -0400

    [interp] Include lmf wrapper in aot image

--- mono/mini/aot-compiler.c
+++ mono/mini/aot-compiler.c
@@ -12660,9 +12660,12 @@ mono_compile_assembly (MonoAssembly *ass, guint32 opts, const char *aot_options,
 #endif
 
 	if (mono_aot_mode_is_interp (&acfg->aot_opts)) {
+		MonoMethod *wrapper = mini_get_interp_lmf_wrapper ();
+		add_method (acfg, wrapper);
+
 		for (int i = 0; i < sizeof (interp_in_static_sigs) / sizeof (const char *); i++) {
 			MonoMethodSignature *sig = mono_create_icall_signature (interp_in_static_sigs [i]);
-			MonoMethod *wrapper = mini_get_interp_in_wrapper (sig);
+			wrapper = mini_get_interp_in_wrapper (sig);
 			add_method (acfg, wrapper);
 		}
 	}
