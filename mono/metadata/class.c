928e2632e49d5655af4c4b9ac9840202e2a377b1
Zoltan Varga <vargaz@gmail.com>
2018-04-19 02:59:57 -0400

    [runtime] Use m_class_has_failure ().

--- mono/metadata/class.c
+++ mono/metadata/class.c
@@ -2603,7 +2603,7 @@ mono_type_get_checked (MonoImage *image, guint32 type_token, MonoGenericContext 
 	if ((type_token & 0xff000000) != MONO_TOKEN_TYPE_SPEC) {
 		MonoClass *klass = mono_class_get_checked (image, type_token, error);
 
-		if (!klass || klass->has_failure)
+		if (!klass || m_class_has_failure (klass))
 			return NULL;
 		return mono_class_get_type (klass);
 	}
