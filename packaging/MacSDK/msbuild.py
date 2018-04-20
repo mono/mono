6e59877f81b2cf15922f29e184bf046f7050e358
Ankit Jain <radical@gmail.com>
2018-04-20 18:12:54 -0400

    Bump msbuild to use the temporary msbuild-port branch

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '93531d65ebbbe33b59e741b31a0bd994e612f9f2')
+			revision = '1a1f5cc6e5314ad6c6e23ff7991b054c516086f8')
 
 	def build (self):
 		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
