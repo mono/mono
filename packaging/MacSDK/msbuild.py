07bc1bb535ac2573f54ffcd47310257c518b5ef7
Ankit Jain <radical@gmail.com>
2018-04-21 04:48:08 -0400

    Update msbuild.py
    Bump to HEAD of `tmp-full-port` and update build command.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,10 +3,10 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '1a1f5cc6e5314ad6c6e23ff7991b054c516086f8')
+			revision = '7f225d9838d57969df8ff93da4539374492e7a28')
 
 	def build (self):
-		self.sh ('./cibuild.sh --scope Compile --target Mono --host Mono --config Release')
+		self.sh ('./build.sh -host mono -configuration Release')
 
 	def install (self):
                 self.sh ('./install-mono-prefix.sh %s' % self.staged_prefix)
