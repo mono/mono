fa83dedf4ccec07207beba53926e9a7d7ada4773
Ankit Jain <radical@gmail.com>
2018-04-21 23:28:10 -0400

    Don't run tests for the build step

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -6,7 +6,7 @@ class MSBuild (GitHubPackage):
 			revision = 'a69c2a1ae7c06c3ac4593bf192500279a2e05463')
 
 	def build (self):
-		self.sh ('./build.sh -host mono -configuration Release')
+		self.sh ('./build.sh -host mono -configuration Release -skipTests')
 
 	def install (self):
                 self.sh ('./install-mono-prefix.sh %s' % self.staged_prefix)
