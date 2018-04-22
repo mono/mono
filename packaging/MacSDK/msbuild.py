fade19840d49d5fc7617a534a15cc5699b4243af
Ankit Jain <radical@gmail.com>
2018-04-21 21:07:06 -0400

    Bump again
    msbuild now has support for a bootstrap msbuild

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '7f225d9838d57969df8ff93da4539374492e7a28')
+			revision = 'a69c2a1ae7c06c3ac4593bf192500279a2e05463')
 
 	def build (self):
 		self.sh ('./build.sh -host mono -configuration Release')
