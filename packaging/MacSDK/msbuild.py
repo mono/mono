daa731ffe2832f7f7ad7454992500fdb37411777
Ankit Jain <radical@gmail.com>
2018-04-22 03:28:51 -0400

    Bump and explicitly don't ignore diff failures

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,13 +3,13 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'a69c2a1ae7c06c3ac4593bf192500279a2e05463')
+			revision = 'db0fb4f6c1210aae62b431deaa6673431eb678de')
 
 	def build (self):
 		self.sh ('./build.sh -host mono -configuration Release -skipTests')
 
 	def install (self):
 		# use the bootstrap msbuild as the system might not have one available!
-                self.sh ('./artifacts/msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO' % self.staged_prefix)
+                self.sh ('./artifacts/msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO /p:IgnoreDiffFailure=false' % self.staged_prefix)
 
 MSBuild ()
