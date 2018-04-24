e7cf396df73f6f5de858fcce93e63fa4d0811be4
Aleksey Kliger <alklig@microsoft.com>
2018-04-24 14:55:14 -0400

    [aot] Make load_image return the aot module assembly for LoadFile
    LoadFile allows loading more than one assembly with the same name.
    That means that just calling mono_assembly_load is unlikely to find the correct
    assembly (it'll just return the first one loaded).  But we shouldn't hardcode the
    full assembly filepath into the AOT image, so it's not obvious that we can call
    mono_assembly_open_predicate.
    
    Fortunately, an assembly opened with LoadFile is supposed to only refer to
    already-loaded assemblies (or to GAC & MONO_PATH) assemblies - so nothing new
    should be loading.  And for the LoadFile'd assembly itself, we can check if the
    name and guid of the current AOT module matches the wanted name and guid and
    just return the AOT module's assembly.

--- mono/mini/aot-runtime.c
+++ mono/mini/aot-runtime.c
@@ -272,6 +272,8 @@ load_image (MonoAotModule *amodule, int index, MonoError *error)
 
 	error_init (error);
 
+	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_AOT, "AOT: module %s wants to load image %d: %s", amodule->aot_name, index, amodule->image_names[index].name);
+
 	if (amodule->image_table [index])
 		return amodule->image_table [index];
 	if (amodule->out_of_date) {
@@ -279,7 +281,26 @@ load_image (MonoAotModule *amodule, int index, MonoError *error)
 		return NULL;
 	}
 
-	assembly = mono_assembly_load (&amodule->image_names [index], amodule->assembly->basedir, &status);
+	/*
+	 * LoadFile allows loading more than one assembly with the same name.
+	 * That means that just calling mono_assembly_load is unlikely to find
+	 * the correct assembly (it'll just return the first one loaded).  But
+	 * we shouldn't hardcode the full assembly filepath into the AOT image,
+	 * so it's not obvious that we can call mono_assembly_open_predicate.
+	 *
+	 * In the JIT, an assembly opened with LoadFile is supposed to only
+	 * refer to already-loaded assemblies (or to GAC & MONO_PATH)
+	 * assemblies - so nothing new should be loading.  And for the
+	 * LoadFile'd assembly itself, we can check if the name and guid of the
+	 * current AOT module matches the wanted name and guid and just return
+	 * the AOT module's assembly.
+	 */
+	if (mono_asmctx_get_kind (&amodule->assembly->context) == MONO_ASMCTX_INDIVIDUAL &&
+	    !strcmp (amodule->assembly->image->guid, amodule->image_guids [index]) &&
+	    mono_assembly_names_equal (&amodule->image_names [index], &amodule->assembly->aname))
+		assembly = amodule->assembly;
+	else
+		assembly = mono_assembly_load (&amodule->image_names [index], amodule->assembly->basedir, &status);
 	if (!assembly) {
 		mono_trace (G_LOG_LEVEL_INFO, MONO_TRACE_AOT, "AOT: module %s is unusable because dependency %s is not found.", amodule->aot_name, amodule->image_names [index].name);
 		mono_error_set_bad_image_by_name (error, amodule->aot_name, "module is unusable because dependency %s is not found (error %d).\n", amodule->image_names [index].name, status);
