dbb0417e2f9f8b429b4ec76db01869ac96a08c4c
EgorBo <egorbo@gmail.com>
2018-04-26 01:03:46 +0300

    corefx imports: InteropServices

---
+++ mcs/class/corlib/corefx/SR.missing.cs
@@ -0,0 +1,7 @@
+partial class SR
+{
+	public const string Arg_InvalidComObjectException = "Attempt has been made to use a COM object that does not have a backing class factory.";
+	public const string Arg_InvalidOleVariantTypeException = "Specified OLE variant was invalid.";
+	public const string Arg_SafeArrayRankMismatchException = "Specified array was not of the expected rank.";
+	public const string Arg_SafeArrayTypeMismatchException = "Specified array was not of the expected type.";
+}
