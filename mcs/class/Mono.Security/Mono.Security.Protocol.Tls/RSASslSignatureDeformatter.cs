6801680650fa06fc3e37f8b7e976eb4551f41c32
Martin Baulig <mabaul@microsoft.com>
2018-05-03 17:15:02 -0400

    Move MD5SHA1 from Mono.Security.Cryptography -> Mono.Security.Protocol.Tls.

--- mcs/class/Mono.Security/Mono.Security.Protocol.Tls/RSASslSignatureDeformatter.cs
+++ mcs/class/Mono.Security/Mono.Security.Protocol.Tls/RSASslSignatureDeformatter.cs
@@ -83,7 +83,7 @@ namespace Mono.Security.Protocol.Tls
 			switch (strName)
 			{
 				case "MD5SHA1":
-					this.hash = new Mono.Security.Cryptography.MD5SHA1();
+					this.hash = new MD5SHA1();
 					break;
 
 				default:
