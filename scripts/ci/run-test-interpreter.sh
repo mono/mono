a10f33067b7cf3604ef64040ae0db9700d32e955
Bernhard Urban <lewurm@gmail.com>
2018-05-06 16:28:02 +0200

    [interp] enable xunit corlib tests

--- scripts/ci/run-test-interpreter.sh
+++ scripts/ci/run-test-interpreter.sh
@@ -8,6 +8,7 @@ ${TESTCMD} --label=mixedmode-regression --timeout=10m make -C mono/mini mixedche
 ${TESTCMD} --label=compile-runtime-tests --timeout=40m make -w -C mono/tests -j4 tests
 ${TESTCMD} --label=runtime-interp --timeout=160m make -w -C mono/tests -k testinterp V=1 CI=1 CI_PR=$([[ ${CI_TAGS} == *'pull-request'* ]] && echo 1 || true)
 ${TESTCMD} --label=corlib --timeout=160m make -w -C mcs/class/corlib run-test V=1
+${TESTCMD} --label=corlib-xunit --timeout=10m make -w -C mcs/class/corlib run-xunit-test
 ${TESTCMD} --label=System --timeout=160m bash -c "export MONO_TLS_PROVIDER=legacy && make -w -C mcs/class/System run-test V=1"
 ${TESTCMD} --label=System.Core --timeout=160m make -w -C mcs/class/System.Core run-test V=1
 ${TESTCMD} --label=mcs-tests --timeout=160m make -w -C mcs/tests run-test V=1;
