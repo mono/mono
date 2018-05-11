9a853e998c04aa8a7aba797120fc47ba5a4a2dab
Michael DeRoy <michaelde@unity3d.com>
2018-05-11 09:53:29 -0400

    Use CharUnicodeInfo from corert to fix webgl failure

---
+++ mcs/class/corlib/System/Debug.cs
@@ -0,0 +1,13 @@
+//Stubs for Assert used by CharUnicodeInfo pulled in from corert
+namespace System.Globalization
+{
+	public static partial class CharUnicodeInfo
+	{
+		internal static class Debug
+		{
+			internal static void Assert(bool condition, string message)
+			{
+			}
+		}
+	}
+}
