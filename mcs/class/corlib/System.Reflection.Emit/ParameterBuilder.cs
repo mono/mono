1b47c51966f6c94e766cce7a24f2a07c4ce4f2e3
Aleksey Kliger <alklig@microsoft.com>
2018-05-15 19:56:20 -0400

    [sre] ParameterBuilder.SetConstant should work with Nullable<T>
    If a parameter is of type Nullable<T> then pb.SetConstant (null) should work.
    Also pb.SetConstant (value) should work when T is one of the supported
    types (primitives, enum, datetime, etc).
    
    Fixes https://github.com/mono/mono/issues/8597 and fixes https://github.com/mono/mono/issues/8504

--- mcs/class/corlib/System.Reflection.Emit/ParameterBuilder.cs
+++ mcs/class/corlib/System.Reflection.Emit/ParameterBuilder.cs
@@ -96,12 +96,24 @@ namespace System.Reflection.Emit {
 		{
 			if (position > 0) {
 				Type t = methodb.GetParameterType (position - 1);
-				if (defaultValue != null && t != defaultValue.GetType ()) {
-					if(!t.IsEnum || t.UnderlyingSystemType != defaultValue.GetType ())
-						throw new ArgumentException ("Constant does not match the defined type.");
+				if (defaultValue == null) {
+					if (!(t.IsGenericType && t.GetGenericTypeDefinition () == typeof(Nullable<>)))
+						throw new ArgumentException("Null is not a valid constant value for this type");
+				} else {
+					if (t.IsEnum) {
+						Type underlyingType = Enum.GetUnderlyingType (t);
+						if (defaultValue.GetType () != underlyingType && defaultValue.GetType () != t) {
+							throw new ArgumentException ("Constant does not match the defined type.");
+						}
+					} else {
+						if (!t.IsAssignableFrom (defaultValue.GetType ()))
+							throw new ArgumentException ("Constant does not match the defined type.");
+					}
+
+					if (t.IsValueType && !t.IsPrimitive && !t.IsEnum && t != typeof (DateTime) &&
+					    !(t.IsGenericType && t.GetGenericTypeDefinition () == typeof (Nullable<>)))
+						throw new ArgumentException ("" + t + " is not a supported constant type.");
 				}
-				if (t.IsValueType && !t.IsPrimitive && !t.IsEnum && t != typeof (DateTime))
-					throw new ArgumentException ("" + t + " is not a supported constant type.");
 			}
 
 			def_value = defaultValue;
