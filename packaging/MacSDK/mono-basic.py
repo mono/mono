bc80db0a17e1bea579499d47c24f0a97c5100b15
Marek Safar <marek.safar@gmail.com>
2018-05-15 17:04:56 +0200

    [packaging] Bump basic to fix build with master and add deprecation message

--- packaging/MacSDK/mono-basic.py
+++ packaging/MacSDK/mono-basic.py
@@ -2,11 +2,11 @@
 class MonoBasicPackage (GitHubTarballPackage):
 
     def __init__(self):
-        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.6', '0ebb1bf528dd77842d672ce95e185e7e27ba6db1',
+        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.6', '6c5b2e2b09fa91338fddd942ec32471c5227a545',
                                       configure='./configure --prefix="%{staged_profile}"')
 
     def install(self):
         self.sh('./configure --prefix="%{staged_prefix}"')
-        self.sh('make install')
+        self.sh('DEPRECATED=1 make install')
 
 MonoBasicPackage()
