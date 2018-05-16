83ff4f3cc69738006da371f0d8653d8a829d05c1
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-05-16 02:10:57 +0200

    Bump version to 4.8 to match github tag

--- packaging/MacSDK/mono-basic.py
+++ packaging/MacSDK/mono-basic.py
@@ -2,7 +2,7 @@
 class MonoBasicPackage (GitHubTarballPackage):
 
     def __init__(self):
-        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.6', '6c5b2e2b09fa91338fddd942ec32471c5227a545',
+        GitHubTarballPackage.__init__(self, 'mono', 'mono-basic', '4.8', 'e31cb702937a0adcc853250a0989c5f43565f9b8',
                                       configure='./configure --prefix="%{staged_profile}"')
 
     def install(self):
