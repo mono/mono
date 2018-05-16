b15191d4c29e5c7d9c013d0338dbbd22f21b4b85
Aleksey Kliger <alklig@microsoft.com>
2018-05-16 16:33:09 -0400

    [test] ParameterBuilder.SetConstant with a nullable type

--- mcs/class/corlib/Test/System.Reflection.Emit/ParameterBuilderTest.cs
+++ mcs/class/corlib/Test/System.Reflection.Emit/ParameterBuilderTest.cs
@@ -47,5 +47,48 @@ namespace MonoTests.System.Reflection.Emit
 			var builder = hello.DefineParameter(1, ParameterAttributes.In, "failParam");
 			builder.SetConstant(1);
 		}
+
+		public enum E : byte {
+			E1, E2
+		}
+
+		[Test]
+		public void SetConstantNullable ()
+		{
+			// SetConstant  for a Nullable<X> parameter for various X.
+			var aName = new AssemblyName ("TestSetConstantNullable");
+			var ab = AppDomain.CurrentDomain.DefineDynamicAssembly (aName, AssemblyBuilderAccess.Run);
+
+			var mb = ab.DefineDynamicModule (aName.Name);
+
+			var tb = mb.DefineType ("TestItf", TypeAttributes.Abstract | TypeAttributes.Interface);
+
+			var paramTypes = new[] { typeof (int?), typeof (int?),
+						 typeof (E?), typeof (E?) };
+
+			var methb = tb.DefineMethod ("Method", MethodAttributes.Public | MethodAttributes.Abstract | MethodAttributes.Virtual, typeof (void), paramTypes);
+			var pb = new ParameterBuilder [paramTypes.Length];
+			for (int idx = 0, pnum = 1; idx < paramTypes.Length; idx++, pnum++) {
+				pb [idx] = methb.DefineParameter (pnum, ParameterAttributes.Optional | ParameterAttributes.HasDefault, "arg" + idx);
+			}
+
+			pb [0].SetConstant (null);
+			pb [1].SetConstant (42);
+			pb [2].SetConstant (null);
+			pb [3].SetConstant (E.E2);
+
+			var t = tb.CreateType ();
+
+			var mi = t.GetMethod ("Method");
+
+			Assert.IsNotNull (mi);
+
+			var ps = mi.GetParameters ();
+
+			Assert.AreEqual (null, ps[0].DefaultValue);
+			Assert.AreEqual (42, ps[1].DefaultValue);
+			Assert.AreEqual (null, ps[2].DefaultValue);
+			Assert.AreEqual ((byte)E.E2, ps[3].DefaultValue);
+		}
 	}
 }
