0815b21027d84c3fca5b9641f08a10f64cffe6db
Jay Krell <jaykrell@microsoft.com>
2018-05-15 17:07:17 -0700

    No _Static_assert on msvc.

--- mono/mini/method-to-ir.c
+++ mono/mini/method-to-ir.c
@@ -6290,8 +6290,10 @@ typedef union _MonoOpcodeParameter {
 	guchar *branch_target;
 } MonoOpcodeParameter;
 
+#ifndef _MSC_VER
 _Static_assert (sizeof (float) == 4, "");
 _Static_assert (sizeof (double) == 8, "");
+#endif
 
 typedef struct _MonoOpcodeInfo {
 	guint constant : 4; // private
