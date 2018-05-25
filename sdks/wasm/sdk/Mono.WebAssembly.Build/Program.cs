2f95aa6e44358d1a2180a38aaf4015a6064495a2
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-05-25 14:25:32 -0400

    [Wasm] Stub the sdk projects

---
+++ sdks/wasm/sdk/Mono.WebAssembly.Build/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mono.WebAssembly.Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
