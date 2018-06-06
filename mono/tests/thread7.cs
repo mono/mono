mono/mono

commit 5f5c5e97a08f7086d7c18af37352c4a03dc4c0d1

[threadpool] Replace parked_threads condition variable by a semaphore (#6222)

mono/metadata/threadpool-worker-default.c
@@ -131,9 +131,8 @@ typedef struct {
 
 	ThreadPoolWorkerCounter counters;
 
-	MonoCoopMutex parked_threads_lock;
+	MonoCoopSem parked_threads_sem;
 	gint32 parked_threads_count;
-	MonoCoopCond parked_threads_cond;
 
 	volatile gint32 work_items_count;
 
@@ -216,8 +215,7 @@ rand_next (gpointer *handle, guint32 min, guint32 max)
 static void
 destroy (gpointer data)
 {
-	mono_coop_mutex_destroy (&worker.parked_threads_lock);
-	mono_coop_cond_destroy (&worker.parked_threads_cond);
+	mono_coop_sem_destroy (&worker.parked_threads_sem);
 
 	mono_coop_mutex_destroy (&worker.worker_creation_lock);
 
@@ -238,9 +236,8 @@ mono_threadpool_worker_init (MonoThreadPoolWorkerCallback callback)
 
 	worker.callback = callback;
 
-	mono_coop_mutex_init (&worker.parked_threads_lock);
+	mono_coop_sem_init (&worker.parked_threads_sem, 0);
 	worker.parked_threads_count = 0;
-	mono_coop_cond_init (&worker.parked_threads_cond);
 
 	worker.worker_creation_current_second = -1;
 	mono_coop_mutex_init (&worker.worker_creation_lock);
@@ -359,77 +356,66 @@ mono_threadpool_worker_request (void)
 	mono_refcount_dec (&worker);
 }
 
-static void
-worker_wait_interrupt (gpointer unused)
-{
-	/* If the runtime is not shutting down, we are not using this mechanism to wake up a unparked thread, and if the
-	 * runtime is shutting down, then we need to wake up ALL the threads.
-	 * It might be a bit wasteful, but I witnessed shutdown hang where the main thread would abort and then wait for all
-	 * background threads to exit (see mono_thread_manage). This would go wrong because not all threadpool threads would
-	 * be unparked. It would end up getting unstucked because of the timeout, but that would delay shutdown by 5-60s. */
-	if (!mono_runtime_is_shutting_down ())
-		return;
-
-	if (!mono_refcount_tryinc (&worker))
-		return;
-
-	mono_coop_mutex_lock (&worker.parked_threads_lock);
-	mono_coop_cond_broadcast (&worker.parked_threads_cond);
-	mono_coop_mutex_unlock (&worker.parked_threads_lock);
-
-	mono_refcount_dec (&worker);
-}
-
 /* return TRUE if timeout, FALSE otherwise (worker unpark or interrupt) */
 static gboolean
 worker_park (void)
 {
 	gboolean timeout = FALSE;
 	gboolean interrupted = FALSE;
+	gint32 old, new;
 
 	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_THREADPOOL, "[%p] worker parking",
 		GUINT_TO_POINTER (MONO_NATIVE_THREAD_ID_TO_UINT (mono_native_thread_id_get ())));
 
-	mono_coop_mutex_lock (&worker.parked_threads_lock);
-
 	if (!mono_runtime_is_shutting_down ()) {
 		static gpointer rand_handle = NULL;
-		MonoInternalThread *thread;
 		ThreadPoolWorkerCounter counter;
 
-		if (!rand_handle)
+		if (!rand_handle) {
 			rand_handle = rand_create ();
-		g_assert (rand_handle);
-
-		thread = mono_thread_internal_current ();
-		g_assert (thread);
+			g_assert (rand_handle);
+		}
 
 		COUNTER_ATOMIC (counter, {
 			counter._.working --;
 			counter._.parked ++;
 		});
 
-		worker.parked_threads_count += 1;
+		do {
+			old = mono_atomic_load_i32 (&worker.parked_threads_count);
+			g_assert (old >= G_MININT32);
 
-		mono_thread_info_install_interrupt (worker_wait_interrupt, NULL, &interrupted);
-		if (interrupted)
-			goto done;
+			new = old + 1;
+		} while (mono_atomic_cas_i32 (&worker.parked_threads_count, new, old) != old);
 
-		if (mono_coop_cond_timedwait (&worker.parked_threads_cond, &worker.parked_threads_lock, rand_next (&rand_handle, 5 * 1000, 60 * 1000)) != 0)
+		switch (mono_coop_sem_timedwait (&worker.parked_threads_sem, rand_next (&rand_handle, 5 * 1000, 60 * 1000), MONO_SEM_FLAGS_ALERTABLE)) {
+		case MONO_SEM_TIMEDWAIT_RET_SUCCESS:
+			break;
+		case MONO_SEM_TIMEDWAIT_RET_ALERTED:
+			interrupted = TRUE;
+			break;
+		case MONO_SEM_TIMEDWAIT_RET_TIMEDOUT:
 			timeout = TRUE;
+			break;
+		default:
+			g_assert_not_reached ();
+		}
 
-		mono_thread_info_uninstall_interrupt (&interrupted);
+		if (interrupted || timeout) {
+			/* If the semaphore was posted, then worker.parked_threads_count was decremented in worker_try_unpark */
+			do {
+				old = mono_atomic_load_i32 (&worker.parked_threads_count);
+				g_assert (old > G_MININT32);
 
-done:
-		worker.parked_threads_count -= 1;
+				new = old - 1;
+			} while (mono_atomic_cas_i32 (&worker.parked_threads_count, new, old) != old);
+		}
 
 		COUNTER_ATOMIC (counter, {
 			counter._.working ++;
 			counter._.parked --;
 		});
 	}
-
-	mono_coop_mutex_unlock (&worker.parked_threads_lock);
 
 	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_THREADPOOL, "[%p] worker unparking, timeout? %s interrupted? %s",
 		GUINT_TO_POINTER (MONO_NATIVE_THREAD_ID_TO_UINT (mono_native_thread_id_get ())), timeout ? "yes" : "no", interrupted ? "yes" : "no");
@@ -440,17 +426,26 @@ done:
 static gboolean
 worker_try_unpark (void)
 {
-	gboolean res = FALSE;
+	gboolean res = TRUE;
+	gint32 old, new;
 
 	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_THREADPOOL, "[%p] try unpark worker",
 		GUINT_TO_POINTER (MONO_NATIVE_THREAD_ID_TO_UINT (mono_native_thread_id_get ())));
 
-	mono_coop_mutex_lock (&worker.parked_threads_lock);
-	if (worker.parked_threads_count > 0) {
-		mono_coop_cond_signal (&worker.parked_threads_cond);
-		res = TRUE;
-	}
-	mono_coop_mutex_unlock (&worker.parked_threads_lock);
+	do {
+		old = mono_atomic_load_i32 (&worker.parked_threads_count);
+		g_assert (old > G_MININT32);
+
+		if (old <= 0) {
+			res = FALSE;
+			break;
+		}
+
+		new = old - 1;
+	} while (mono_atomic_cas_i32 (&worker.parked_threads_count, new, old) != old);
+
+	if (res)
+		mono_coop_sem_post (&worker.parked_threads_sem);
 
 	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_THREADPOOL, "[%p] try unpark worker, success? %s",
 		GUINT_TO_POINTER (MONO_NATIVE_THREAD_ID_TO_UINT (mono_native_thread_id_get ())), res ? "yes" : "no");

commit 4dcce6f35648320ab0d6dc9327cfb038091b0aaf

Don't wait 5-60 seconds for work in threadpool on aborted threads. This should fix or mitigate #7167 and #9000.
Aborting already aborted threads is skipped, but interrupts on aborted threads are not.
This regressed with change 5f5c5e9 [threadpool] Replace parked_threads condition variable by a semaphore (#6222)

The skipping of syscall abort in aborted threads is circa here:
```
request_thread_abort:
	if (thread->state & (ThreadState_AbortRequested | ThreadState_Stopped))
	{
		UNLOCK_THREAD (thread);
		return FALSE;
	}
```

Otherwise there is a wait for a random duration between 5 and 60 seconds in mono/tests/thread6.cs,
which aborts a threadpool worker thread -- probably an inappropriate thing to do,
threadpool threads being a shared resource, however with the right amount of synchronization,
one can probably be sure to only abort one's own work, and the threadpool will survive by
spinning up additional threads, or in this case, shutting down in the expected rapid way.

Possibly there should be a notion of canceling a threadpool work item or task, instead
of the entire thread. Even if it is safe, it is defeating the efficiency of a threadpool.

mono/metadata/threadpool-worker-default.c
@@ -367,7 +367,7 @@ worker_park (void)
 	mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_THREADPOOL, "[%p] worker parking",
 		GUINT_TO_POINTER (MONO_NATIVE_THREAD_ID_TO_UINT (mono_native_thread_id_get ())));
 
-	if (!mono_runtime_is_shutting_down ()) {
+	if (!mono_runtime_is_shutting_down () && !mono_thread_test_state (mono_thread_internal_current (), ThreadState_AbortRequested)) {
 		static gpointer rand_handle = NULL;
 		ThreadPoolWorkerCounter counter;
 

mono/tests/Makefile.am
@@ -646,6 +646,7 @@ TESTS_CS_SRC=		\
 	generic-unloading.2.cs	\
 	namedmutex-destroy-race.cs	\
 	thread6.cs	\
+	thread7.cs	\
 	appdomain-threadpool-unload.cs	\
 	process-unref-race.cs	\
 	bug-46661.cs	\

mono/tests/thread7.cs
@@ -1,0 +1,58 @@
+//
+// Subset of thread6.cs, just to watch for the hang at end.
+// Note this test does not indicate success or failure well,
+// it just runs quickly or slowly.
+//
+using System;
+using System.Threading;
+
+public class Tests {
+
+	public static int Main() {
+		return test_0_regress_4413();
+	}
+
+	public static int test_0_regress_4413 () {
+		// Check that thread abort exceptions originating in another thread are not automatically rethrown
+		object o = new object ();
+		Thread t = null;
+		bool waiting = false;
+		Action a = delegate () {
+			t = Thread.CurrentThread;
+			while (true) {
+				lock (o) {
+					if (waiting) {
+						Monitor.Pulse (o);
+						break;
+					}
+				}
+
+				Thread.Sleep (10);
+			}
+			while (true) {
+				Thread.Sleep (1000);
+			}
+		};
+		var ar = a.BeginInvoke (null, null);
+		lock (o) {
+			waiting = true;
+			Monitor.Wait (o);
+		}
+
+		t.Abort ();
+
+		try {
+			try {
+				a.EndInvoke (ar);
+			} catch (ThreadAbortException) {
+			}
+		} catch (ThreadAbortException) {
+			// This will fail
+			Thread.ResetAbort ();
+			return 1;
+		}
+
+		return 0;
+	}
+}
+