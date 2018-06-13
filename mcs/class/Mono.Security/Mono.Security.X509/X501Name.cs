272d70cfdc636b64f712f0ab0e6d4fad636469e4
Filip Navara <filip.navara@gmail.com>
2018-06-13 10:37:45 +0200

    Fix quoting of BMPString names in X501Name. (#9105)
    * Fix quoting of BMPString names in X501Name.
    
    * Fix the set of characters in X.501 name that need quoting.

--- mcs/class/Mono.Security/Mono.Security.X509/X501Name.cs
+++ mcs/class/Mono.Security/Mono.Security.X509/X501Name.cs
@@ -180,14 +180,17 @@ namespace Mono.Security.X509 {
 						sValue = Encoding.UTF7.GetString (s.Value);
 					else
 						sValue = Encoding.UTF8.GetString (s.Value);
-					// in some cases we must quote (") the value
-					// Note: this doesn't seems to conform to RFC2253
-					char[] specials = { ',', '+', '"', '\\', '<', '>', ';' };
-					if (quotes) {
-						if ((sValue.IndexOfAny (specials, 0, sValue.Length) > 0) ||
-						    sValue.StartsWith (" ") || (sValue.EndsWith (" ")))
-							sValue = "\"" + sValue + "\"";
-					}
+				}
+
+				// in some cases we must quote (") the value
+				// Note: this doesn't seems to conform to RFC2253
+				// Set of characters that need quoting is taken from s_quoteNeedingChars
+				// in corefx/src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Unix/X500NameEncoder.cs
+				char[] specials = { ',', '+', '"', '=', '<', '>', ';', '#', '\n' };
+				if (quotes) {
+					if ((sValue.IndexOfAny (specials, 0, sValue.Length) > 0) ||
+					    sValue.StartsWith (" ") || (sValue.EndsWith (" ")))
+						sValue = "\"" + sValue.Replace ("\"", "") + "\"";
 				}
 
 				sb.Append (sValue);
