6fc2e0576fdd1015d641fd1f76adebb35adf239c
Jonas Echterhoff <jonas@unity3d.com>
2018-06-14 09:45:55 +0200

    Match mono code conventions

--- mono/metadata/boehm-gc.c
+++ mono/metadata/boehm-gc.c
@@ -187,11 +187,11 @@ mono_gc_base_init (void)
 
 				opt = strchr (opt, '=') + 1;
 				if (*opt && mono_gc_parse_environment_string_extract_number (opt, &time_limit)) {
-					GC_enable_incremental();
+					GC_enable_incremental ();
 			#if HAVE_BDWGC_GC	
 					if (time_limit != 0)
 						// value is in milliseconds
-						GC_set_time_limit(time_limit);
+						GC_set_time_limit (time_limit);
 			#endif					
 				}
 				continue;
@@ -622,7 +622,7 @@ mono_gc_weak_link_add (void **link_addr, MonoObject *obj, gboolean track)
 {
 	/* libgc requires that we use HIDE_POINTER... */
 	*link_addr = (void*)HIDE_POINTER (obj);
-	GC_dirty(link_addr);
+	GC_dirty (link_addr);
 	if (track)
 		GC_REGISTER_LONG_LINK (link_addr, obj);
 	else
@@ -879,48 +879,48 @@ void
 mono_gc_wbarrier_set_field_internal (MonoObject *obj, gpointer field_ptr, MonoObject* value)
 {
 	*(void**)field_ptr = value;
-	GC_dirty(field_ptr);
+	GC_dirty (field_ptr);
 }
 
 void
 mono_gc_wbarrier_set_arrayref_internal (MonoArray *arr, gpointer slot_ptr, MonoObject* value)
 {
 	*(void**)slot_ptr = value;
-	GC_dirty(slot_ptr);
+	GC_dirty (slot_ptr);
 }
 
 void
 mono_gc_wbarrier_arrayref_copy_internal (gpointer dest_ptr, gconstpointer src_ptr, int count)
 {
 	mono_gc_memmove_aligned (dest_ptr, src_ptr, count * sizeof (gpointer));
-	GC_dirty(dest_ptr);
+	GC_dirty (dest_ptr);
 }
 
 void
 mono_gc_wbarrier_generic_store_internal (void volatile* ptr, MonoObject* value)
 {
 	*(void**)ptr = value;
-	GC_dirty(ptr);
+	GC_dirty (ptr);
 }
 
 void
 mono_gc_wbarrier_generic_store_atomic_internal (gpointer ptr, MonoObject *value)
 {
 	mono_atomic_store_ptr ((volatile gpointer *)ptr, value);
-	GC_dirty(ptr);
+	GC_dirty (ptr);
 }
 
 void
 mono_gc_wbarrier_generic_nostore_internal (gpointer ptr)
 {
-	GC_dirty(ptr);
+	GC_dirty (ptr);
 }
 
 void
 mono_gc_wbarrier_value_copy_internal (gpointer dest, gconstpointer src, int count, MonoClass *klass)
 {
 	mono_gc_memmove_atomic (dest, src, count * mono_class_value_size (klass, NULL));
-	GC_dirty(dest);
+	GC_dirty (dest);
 }
 
 void
@@ -929,7 +929,7 @@ mono_gc_wbarrier_object_copy_internal (MonoObject* obj, MonoObject *src)
 	/* do not copy the sync state */
 	mono_gc_memmove_aligned (mono_object_get_data (obj), (char*)src + MONO_ABI_SIZEOF (MonoObject),
 				m_class_get_instance_size (mono_object_class (obj)) - MONO_ABI_SIZEOF (MonoObject));
-	GC_dirty(obj);
+	GC_dirty (obj);
 }
 
 void
@@ -1073,7 +1073,7 @@ mono_gc_set_desktop_mode (void)
 gboolean
 mono_gc_is_moving (void)
 {
-	return GC_is_incremental_mode();
+	return GC_is_incremental_mode ();
 }
 
 gboolean
@@ -1088,8 +1088,8 @@ mono_gc_is_disabled (void)
 void
 mono_gc_wbarrier_range_copy (gpointer _dest, gconstpointer _src, int size)
 {
-	memcpy(_dest, _src, size);
-	GC_dirty(_dest);
+	memcpy (_dest, _src, size);
+	GC_dirty (_dest);
 }
 
 MonoRangeCopyFunction
@@ -1473,7 +1473,7 @@ handle_data_grow (HandleData *handles, gboolean track)
 		gpointer *entries;
 		entries = (void **)mono_gc_alloc_fixed (sizeof (*handles->entries) * new_size, NULL, MONO_ROOT_SOURCE_GC_HANDLE, NULL, "GC Handle Table (Boehm)");
 		mono_gc_memmove_aligned (entries, handles->entries, sizeof (*handles->entries) * handles->size);
-		GC_dirty(entries);
+		GC_dirty (entries);
 		mono_gc_free_fixed (handles->entries);
 		handles->entries = entries;
 	}
@@ -1506,7 +1506,7 @@ alloc_handle (HandleData *handles, MonoObject *obj, gboolean track)
 			mono_gc_weak_link_add (&(handles->entries [slot]), obj, track);
 	} else {
 		handles->entries [slot] = obj;
-		GC_dirty(handles->entries + slot);
+		GC_dirty (handles->entries + slot);
 	}
 
 #ifndef DISABLE_PERFCOUNTERS
@@ -1627,7 +1627,7 @@ mono_gchandle_set_target (MonoGCHandle gch, MonoObject *obj)
 			handles->domain_ids [slot] = (obj ? mono_object_get_domain_internal (obj) : mono_domain_get ())->domain_id;
 		} else {
 			handles->entries [slot] = obj;
-			GC_dirty(handles->entries + slot);
+			GC_dirty (handles->entries + slot);
 		}
 	} else {
 		/* print a warning? */
@@ -1711,7 +1711,7 @@ mono_gchandle_free_internal (MonoGCHandle gch)
 				mono_gc_weak_link_remove (&handles->entries [slot], handles->type == HANDLE_WEAK_TRACK);
 		} else {
 			handles->entries [slot] = NULL;
-			GC_dirty(handles->entries + slot);
+			GC_dirty (handles->entries + slot);
 		}
 		vacate_slot (handles, slot);
 	} else {
@@ -1754,7 +1754,7 @@ mono_gchandle_free_domain (MonoDomain *domain)
 				if (handles->entries [slot] && mono_object_domain (handles->entries [slot]) == domain) {
 					vacate_slot (handles, slot);
 					handles->entries [slot] = NULL;
-					GC_dirty(handles->entries + slot);
+					GC_dirty (handles->entries + slot);
 				}
 			}
 		}
--- mono/metadata/object.c
+++ mono/metadata/object.c
@@ -2224,7 +2224,7 @@ mono_class_create_runtime_vtable (MonoDomain *domain, MonoClass *klass, MonoErro
 				/* This is not needed by sgen, as it does not seem 
 +				to need write barriers for uncollectable objects (like the vtables storing static 
 +				fields), but it is needed for incremental boehm. */
-				mono_gc_wbarrier_generic_nostore(t);
+				mono_gc_wbarrier_generic_nostore (t);
 			}
 			continue;
 		}		
@@ -3471,7 +3471,7 @@ mono_field_static_set_value_internal (MonoVTable *vt, MonoClassField *field, voi
 	to need write barriers for uncollectable objects (like the vtables storing static 
 +	fields), but it is needed for incremental boehm. */
 	if (field->offset == -1)
-		mono_gc_wbarrier_generic_nostore(dest);
+		mono_gc_wbarrier_generic_nostore (dest);
 }
 
 /**
