d904e6b30decf783c6ec042c502b0ed20c9d20c9
monojenkins <jo.shields+jenkins@xamarin.com>
2018-06-16 09:15:48 -0400

    [SDKS,Android] Install cmake when provisioning Android SDK (#9141)
    Android NDK supports cmake in addition to the older make-based ndk-build as the
    build system and Xamarin.Android is in the process of switching the build of its
    native bits to cmake. The reason for this switch is that certain parts of
    Xamarin.Android (libmonodroid) are built for both the host system and
    the Android target. The host system build currently uses hand-crafted code
    directly in .csproj to build the native bits while the Android build uses
    ndk-build. This results in duplication of code, compiler parameters are
    specified in two locations - it is easy to introduce incompatibilities, bug
    fixes may be applied only in one scenario etc.
    
    Using cmake allows us to use a single build "script" for all the targets, thus
    making the build more reliable. Android SDK has a cmake package which contains
    full support for NDK builds and allows us to easily use cmake across all the
    supported platforms without having to install cmake on the host system.
    
    This commit adds the cmake package to the Android SDK provisiong process and,
    alas, at the same time adds a workaround to properly unpack the package. Unlike
    other Android SDK packages, cmake doesn't unpack to a single subdirectory of the
    zip root, instead all its files and directories are placed directly in the zip
    root. This requires yet another workaround to install the package properly.

--- sdks/builds/android.mk
+++ sdks/builds/android.mk
@@ -54,12 +54,14 @@ $(eval $(call AndroidSDKProvisioningTemplate,build-tools_r$(ANDROID_BUILD_TOOLS_
 $(eval $(call AndroidSDKProvisioningTemplate,platform-tools_r27.0.1-darwin,platform-tools))
 $(eval $(call AndroidSDKProvisioningTemplate,sdk-tools-darwin-4333796,tools))
 $(eval $(call AndroidSDKProvisioningTemplate,emulator-darwin-4266726,emulator))
+$(eval $(call AndroidSDKProvisioningTemplate,cmake-3.6.4111459-darwin-x86_64,cmake/3.6.4111459))
 else
 ifeq ($(UNAME),Linux)
 $(eval $(call AndroidSDKProvisioningTemplate,build-tools_r$(ANDROID_BUILD_TOOLS_VERSION)-linux,build-tools/$(or $(ANDROID_BUILD_TOOLS_DIR),$(ANDROID_BUILD_TOOLS_VERSION))))
 $(eval $(call AndroidSDKProvisioningTemplate,platform-tools_r27.0.1-linux,platform-tools))
 $(eval $(call AndroidSDKProvisioningTemplate,sdk-tools-linux-4333796,tools))
 $(eval $(call AndroidSDKProvisioningTemplate,emulator-linux-4266726,emulator))
+$(eval $(call AndroidSDKProvisioningTemplate,cmake-3.6.4111459-linux-x86_64,cmake/3.6.4111459))
 else
 $(error "Unknown UNAME=$(UNAME)")
 endif
