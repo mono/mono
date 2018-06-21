af9f004887f13cd3f315a1261b2670805fc4fa7c
Katelyn Gadd <kg@luminance.org>
2018-06-21 01:29:47 -0700

    Strip double slashes from paths to fix spurious csproj change

--- mcs/build/gensources.cs
+++ mcs/build/gensources.cs
@@ -146,7 +146,8 @@ public class ParseResult {
 
         var relativePath = Uri.UnescapeDataString (
             dirUri.MakeRelativeUri (pathUri).OriginalString
-        ).Replace ("/", SourcesParser.DirectorySeparator);
+        ).Replace ("/", SourcesParser.DirectorySeparator)
+         .Replace (SourcesParser.DirectorySeparator + SourcesParser.DirectorySeparator, SourcesParser.DirectorySeparator);
 
         if (SourcesParser.TraceLevel >= 4)
             Console.Error.WriteLine ($"// {fullPath} -> {relativePath}");
