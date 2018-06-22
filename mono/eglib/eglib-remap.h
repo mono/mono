b74ff526116dd97f64e5f71c4aba0c170c20ec91
Jay Krell <jaykrell@microsoft.com>
2018-06-22 00:38:21 -0400

    g_string_insert does not update string->len, and copies the wrong number of characters around, 	and does not allow for overlapping parameters, 	and is not used -- remove it.
    g_string_erase does not always update string->len,
    	and is not used -- remove it.
    
    g_string_prepend does not update string->len.
     Switch the only use to append in the reverse order, and fix as leak.

--- mono/eglib/eglib-remap.h
+++ mono/eglib/eglib-remap.h
@@ -94,7 +94,6 @@
 #define g_list_length monoeg_g_list_length
 #define g_list_nth monoeg_g_list_nth
 #define g_list_nth_data monoeg_g_list_nth_data
-#define g_list_prepend monoeg_g_list_prepend
 #define g_list_remove monoeg_g_list_remove
 #define g_list_remove_all monoeg_g_list_remove_all
 #define g_list_remove_link monoeg_g_list_remove_link
@@ -205,9 +204,7 @@
 #define g_string_append_unichar monoeg_g_string_append_unichar
 #define g_string_append_printf monoeg_g_string_append_printf
 #define g_string_append_vprintf monoeg_g_string_append_vprintf
-#define g_string_erase monoeg_g_string_erase
 #define g_string_free monoeg_g_string_free
-#define g_string_insert monoeg_g_string_insert
 #define g_string_new monoeg_g_string_new
 #define g_string_new_len monoeg_g_string_new_len
 #define g_string_prepend monoeg_g_string_prepend
