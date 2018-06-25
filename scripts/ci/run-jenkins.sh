ce443f6670db4b7b6c7fedb7d15b93ed953149f7
Alexander Kyte <alkyte@microsoft.com>
2018-06-25 16:02:10 -0400

    [runtime] Download LLVM during ios sdk CI build

--- scripts/ci/run-jenkins.sh
+++ scripts/ci/run-jenkins.sh
@@ -87,6 +87,7 @@ if [[ ${CI_TAGS} == *'product-sdks-ios'* ]];
    then
 	   echo "DISABLE_ANDROID=1" > sdks/Make.config
 	   echo "DISABLE_WASM=1" >> sdks/Make.config
+	   echo "IGNORE_PACKAGE_LLVM=1" >> sdks/Make.config
 	   export device_test_suites="Mono.Runtime.Tests System.Core"
 	   ${TESTCMD} --label=runtimes --timeout=60m --fatal make -j4 -C sdks/builds package-ios-sim64 package-ios-target64
 	   ${TESTCMD} --label=cross --timeout=60m --fatal make -j4 -C sdks/builds package-ios-cross64 package-ios-cross32
