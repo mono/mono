mono/mono

commit b26a41cc3ee5fbee01430b8e4f31e687ef90b7db

Reading of Metadata and pdb blobs via Soft debugger protocol (#9082)
* Support reading metadata and pdb from vm

* Add tests for reading metadata and pdb from vm

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/AssemblyMirror.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using Mono.Debugger;
 using Mono.Cecil;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Mono.Debugger.Soft
 {
@@ -15,8 +16,13 @@ namespace Mono.Debugger.Soft
 		AssemblyName aname;
 		AssemblyDefinition meta;
 		AppDomainMirror domain;
+		byte[] metadata_blob;
+		bool? isDynamic;
+		byte[] pdb_blob;
 		Dictionary<string, long> typeCacheIgnoreCase = new Dictionary<string, long> (StringComparer.InvariantCultureIgnoreCase);
 		Dictionary<string, long> typeCache = new Dictionary<string, long> ();
+		Dictionary<uint, long> tokenTypeCache = new Dictionary<uint, long> ();
+		Dictionary<uint, long> tokenMethodCache = new Dictionary<uint, long> ();
 
 		internal AssemblyMirror (VirtualMachine vm, long id) : base (vm, id) {
 		}
@@ -118,7 +124,14 @@ namespace Mono.Debugger.Soft
 		 */
 		public AssemblyDefinition Metadata {
 			get {
-				return meta;
+				if (meta != null)
+					return meta;
+
+				if (IsDynamic)
+					throw new NotSupportedException ();
+				
+				using (var ms = new MemoryStream (GetMetadataBlob ()))
+					return meta = AssemblyDefinition.ReadAssembly (ms);
 			}
 			set {
 				if (value.MainModule.Name != ManifestModule.Name)
@@ -127,6 +140,74 @@ namespace Mono.Debugger.Soft
 					throw new ArgumentException ("The supplied assembly's main module has guid '" + value.MainModule.Mvid + ", while the assembly in the debuggee has guid '" + ManifestModule.ModuleVersionId + "'.", "value");
 				meta = value;
 			}
+		}
+		
+		public byte[] GetMetadataBlob () {
+			if (metadata_blob != null)
+				return metadata_blob;
+			
+			vm.CheckProtocolVersion (2, 47);
+
+			return metadata_blob = vm.conn.Assembly_GetMetadataBlob (id);
+		}
+		
+		public bool IsDynamic {
+			get {
+				if (isDynamic.HasValue)
+					return isDynamic.Value;
+				
+				vm.CheckProtocolVersion (2, 47);
+
+				isDynamic = vm.conn.Assembly_IsDynamic (id);
+				return isDynamic.Value;
+			}
+		}	
+		
+		public bool HasPdb {
+			get {
+				return pdb_blob != null;
+			}
+		}
+
+		public bool HasFetchedPdb { get; private set; }
+		
+		public byte[] GetPdbBlob () {
+			if (HasFetchedPdb)
+				return pdb_blob;
+			
+			vm.CheckProtocolVersion (2, 47);
+			var blob = vm.conn.Assembly_GetPdbBlob (id);
+			if (blob != null && blob.Length > 0) {
+				pdb_blob = blob;
+			}
+			HasFetchedPdb = true;
+			return pdb_blob;
+		}
+
+		public TypeMirror GetType (uint token) {
+			vm.CheckProtocolVersion (2, 47);
+			if (IsDynamic)
+				throw new NotSupportedException ();
+			
+			long typeId;
+			if (!tokenTypeCache.TryGetValue (token, out typeId)) {
+				typeId = vm.conn.Assembly_GetType (id, token);
+				tokenTypeCache.Add (token, typeId);
+			}
+			return vm.GetType (typeId);
+		}
+
+		public MethodMirror GetMethod (uint token) {
+			vm.CheckProtocolVersion (2, 47);
+			if (IsDynamic)
+				throw new NotSupportedException ();
+			
+			long methodId;
+			if (!tokenMethodCache.TryGetValue (token, out methodId)) {
+				methodId = vm.conn.Assembly_GetMethod (id, token);
+				tokenMethodCache.Add (token, methodId);
+			}
+			return vm.GetMethod (methodId);
 		}
     }
 }

mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -431,6 +431,86 @@ public class DebuggerTests
 	}
 
 	[Test]
+	public void MetadataAndPdbTest () {
+		run_until ("bp1");
+		var assemblyMirrors = entry_point.DeclaringType.Assembly.Domain.GetAssemblies ();
+		Assert.IsTrue (assemblyMirrors.Length > 0);
+
+		foreach (var assemblyMirror in assemblyMirrors) {
+			var metadata = assemblyMirror.Metadata;
+			Assert.AreEqual (metadata.MainModule.Mvid, assemblyMirror.ManifestModule.ModuleVersionId);
+
+			var location = assemblyMirror.Location;
+			if (!File.Exists (location))
+				continue;
+
+			Assert.IsFalse (assemblyMirror.IsDynamic);
+
+			var rawDataFromMemory = assemblyMirror.GetMetadataBlob ();
+			var rawDataFromDisk = File.ReadAllBytes (location);
+			Assert.IsTrue (rawDataFromMemory.SequenceEqual (rawDataFromDisk));
+
+			string pdbPath = Path.ChangeExtension (location, "pdb");
+			Assert.IsFalse (assemblyMirror.HasPdb);
+			Assert.IsFalse (assemblyMirror.HasFetchedPdb);
+			
+			var pdbFromMemory = assemblyMirror.GetPdbBlob ();
+			if (!File.Exists (pdbPath)) {
+				Assert.IsFalse (assemblyMirror.HasPdb);
+				Assert.IsTrue (assemblyMirror.HasFetchedPdb);
+				continue;
+			}
+
+			Assert.IsTrue (pdbFromMemory != null && pdbFromMemory.Length > 0);
+			var pdbFromDisk = File.ReadAllBytes (pdbPath);
+			Assert.IsTrue (pdbFromMemory.SequenceEqual (pdbFromDisk));
+			
+			Assert.IsTrue (assemblyMirror.HasPdb);
+			Assert.IsTrue (assemblyMirror.HasFetchedPdb);
+
+			foreach (var type in metadata.Modules.SelectMany (x => x.GetTypes ())) {
+				var typeMirror = assemblyMirror.GetType (type.MetadataToken.ToUInt32 ());
+				Assert.AreEqual (type.Name, typeMirror.Name);
+				Assert.AreEqual (type.MetadataToken.ToInt32 (), typeMirror.MetadataToken);
+
+				int methodCount = 0;
+				foreach (var method in type.Methods) {
+					var methodMirror = assemblyMirror.GetMethod (method.MetadataToken.ToUInt32 ());
+					Assert.AreEqual (method.Name, methodMirror.Name);
+					Assert.AreEqual (method.MetadataToken.ToInt32 (), methodMirror.MetadataToken);
+					
+					if (methodCount++ > 10)
+						break;
+				}
+
+				if (type.IsNested)
+					break;
+			}
+		}
+	}
+	
+	[Test]
+	public void IsDynamicAssembly () {
+		vm.Detach ();
+
+		Start (new string[] {"dtest-app.exe", "ref-emit-test"});
+
+		run_until ("ref_emit_call");
+		var assemblyMirrors = entry_point.DeclaringType.Assembly.Domain.GetAssemblies ();
+		bool dynamicAssemblyFound = false;
+		foreach (var assemblyMirror in assemblyMirrors) {
+			if (assemblyMirror.GetName ().Name == "foo") {
+				dynamicAssemblyFound = true;
+				Assert.IsTrue (assemblyMirror.IsDynamic);
+			}
+			else
+				Assert.IsFalse (assemblyMirror.IsDynamic);
+		}
+
+		Assert.IsTrue (dynamicAssemblyFound);
+	}
+
+	[Test]
 	public void BreakpointAlreadyJITted () {
 		Event e = run_until ("bp1");
 

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/Connection.cs
@@ -420,7 +420,7 @@ namespace Mono.Debugger.Soft
 		 * with newer runtimes, and vice versa.
 		 */
 		internal const int MAJOR_VERSION = 2;
-		internal const int MINOR_VERSION = 46;
+		internal const int MINOR_VERSION = 47;
 
 		enum WPSuspendPolicy {
 			NONE = 0,
@@ -534,7 +534,12 @@ namespace Mono.Debugger.Soft
 			GET_OBJECT = 4,
 			GET_TYPE = 5,
 			GET_NAME = 6,
-			GET_DOMAIN = 7
+			GET_DOMAIN = 7,
+			GET_METADATA_BLOB = 8,
+			GET_IS_DYNAMIC = 9,
+			GET_PDB_BLOB = 10,
+			GET_TYPE_FROM_TOKEN = 11,
+			GET_METHOD_FROM_TOKEN = 12
 		}
 
 		enum CmdModule {
@@ -646,6 +651,17 @@ namespace Mono.Debugger.Soft
 
 		static int decode_byte (byte[] packet, ref int offset) {
 			return packet [offset++];
+		}
+
+		static byte[] decode_bytes (byte[] packet, ref int offset, int length)
+		{
+			if (length + offset > packet.Length)
+				throw new ArgumentOutOfRangeException ();
+
+			var bytes = new byte[length];
+			Array.Copy (packet, offset, bytes, 0, length);
+			offset += length;
+			return bytes;
 		}
 
 		static int decode_short (byte[] packet, ref int offset) {
@@ -894,6 +910,15 @@ namespace Mono.Debugger.Soft
 				for (int i = 0; i < n; ++i)
 					res [i] = ReadId ();
 				return res;
+			}
+			
+			public byte[] ReadByteArray () {
+				var length = ReadInt ();
+				return decode_bytes (packet, ref offset, length);
+			}
+
+			public bool ReadBool () {
+				return ReadByte () != 0;
 			}
 		}
 
@@ -2134,6 +2159,26 @@ namespace Mono.Debugger.Soft
 
 		internal long Assembly_GetIdDomain (long id) {
 			return SendReceive (CommandSet.ASSEMBLY, (int)CmdAssembly.GET_DOMAIN, new PacketWriter ().WriteId (id)).ReadId ();
+		}
+		
+		internal byte[] Assembly_GetMetadataBlob (long id) {
+			return SendReceive (CommandSet.ASSEMBLY, (int)CmdAssembly.GET_METADATA_BLOB, new PacketWriter ().WriteId (id)).ReadByteArray ();
+		}
+
+		internal bool Assembly_IsDynamic (long id) {
+			return SendReceive (CommandSet.ASSEMBLY, (int) CmdAssembly.GET_IS_DYNAMIC, new PacketWriter ().WriteId (id)).ReadBool ();
+		}
+		
+		internal byte[] Assembly_GetPdbBlob (long id) {
+			return SendReceive (CommandSet.ASSEMBLY, (int)CmdAssembly.GET_PDB_BLOB, new PacketWriter ().WriteId (id)).ReadByteArray ();
+		}
+
+		internal long Assembly_GetType (long id, uint token) {
+			return SendReceive (CommandSet.ASSEMBLY, (int)CmdAssembly.GET_TYPE_FROM_TOKEN, new PacketWriter ().WriteId (id).WriteInt ((int)token)).ReadId ();
+		}
+
+		internal long Assembly_GetMethod (long id, uint token) {
+			return SendReceive (CommandSet.ASSEMBLY, (int)CmdAssembly.GET_METHOD_FROM_TOKEN, new PacketWriter ().WriteId (id).WriteInt ((int)token)).ReadId ();
 		}
 
 		/*

mono/metadata/debug-mono-ppdb.c
@@ -376,6 +376,12 @@ mono_ppdb_lookup_location (MonoDebugMethodInfo *minfo, uint32_t offset)
 	return location;
 }
 
+MonoImage *
+mono_ppdb_get_image (MonoPPDBFile *ppdb)
+{
+    return  ppdb->image;
+}
+
 void
 mono_ppdb_get_seq_points (MonoDebugMethodInfo *minfo, char **source_file, GPtrArray **source_file_list, int **source_files, MonoSymSeqPoint **seq_points, int *n_seq_points)
 {

mono/metadata/debug-mono-ppdb.h
@@ -38,4 +38,7 @@ mono_ppdb_lookup_locals (MonoDebugMethodInfo *minfo);
 MonoDebugMethodAsyncInfo*
 mono_ppdb_lookup_method_async_debug_info (MonoDebugMethodInfo *minfo);
 
+MonoImage *
+mono_ppdb_get_image (MonoPPDBFile *ppdb);
+
 #endif

mono/metadata/mono-debug.c
@@ -248,6 +248,12 @@ mono_debug_close_image (MonoImage *image)
 	mono_debugger_unlock ();
 }
 
+MonoDebugHandle *
+mono_debug_get_handle (MonoImage *image)
+{
+    return mono_debug_open_image (image, NULL, 0);
+}
+
 static MonoDebugHandle *
 mono_debug_open_image (MonoImage *image, const guint8 *raw_contents, int size)
 {

mono/metadata/mono-debug.h
@@ -178,6 +178,9 @@ mono_debug_lookup_method_addresses (MonoMethod *method);
 MONO_API MonoDebugMethodJitInfo*
 mono_debug_find_method (MonoMethod *method, MonoDomain *domain);
 
+MONO_API MonoDebugHandle *
+mono_debug_get_handle (MonoImage *image);
+
 MONO_API void
 mono_debug_free_method_jit_info (MonoDebugMethodJitInfo *jit);
 

mono/mini/debugger-agent.c
@@ -80,6 +80,7 @@
 #include "mini-runtime.h"
 #include "interp/interp.h"
 #include "debugger-engine.h"
+#include "mono/metadata/debug-mono-ppdb.h"
 
 /*
  * On iOS we can't use System.Environment.Exit () as it will do the wrong
@@ -266,7 +267,7 @@ typedef struct {
 #define HEADER_LENGTH 11
 
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 46
+#define MINOR_VERSION 47
 
 typedef enum {
 	CMD_SET_VM = 1,
@@ -394,7 +395,12 @@ typedef enum {
 	CMD_ASSEMBLY_GET_OBJECT = 4,
 	CMD_ASSEMBLY_GET_TYPE = 5,
 	CMD_ASSEMBLY_GET_NAME = 6,
-	CMD_ASSEMBLY_GET_DOMAIN = 7
+	CMD_ASSEMBLY_GET_DOMAIN = 7,
+	CMD_ASSEMBLY_GET_METADATA_BLOB = 8,
+	CMD_ASSEMBLY_GET_IS_DYNAMIC = 9,
+	CMD_ASSEMBLY_GET_PDB_BLOB = 10,
+	CMD_ASSEMBLY_GET_TYPE_FROM_TOKEN = 11,
+	CMD_ASSEMBLY_GET_METHOD_FROM_TOKEN = 12
 } CmdAssembly;
 
 typedef enum {
@@ -1695,6 +1701,13 @@ buffer_add_string (Buffer *buf, const char *str)
 		buffer_add_int (buf, len);
 		buffer_add_data (buf, (guint8*)str, len);
 	}
+}
+
+static inline void
+buffer_add_byte_array (Buffer *buf, guint8 *bytes, guint32 arr_len)
+{
+    buffer_add_int (buf, arr_len);
+    buffer_add_data (buf, bytes, arr_len);
 }
 
 static inline void
@@ -7868,6 +7881,65 @@ assembly_commands (int command, guint8 *p, guint8 *end, Buffer *buf)
 		g_free (name);
 		break;
 	}
+    case CMD_ASSEMBLY_GET_METADATA_BLOB: {
+        MonoImage* image = ass->image;
+        if (ass->dynamic) {
+            return ERR_NOT_IMPLEMENTED;
+        }
+        buffer_add_byte_array (buf, (guint8*)image->raw_data, image->raw_data_len);
+        break;
+    }
+    case CMD_ASSEMBLY_GET_IS_DYNAMIC: {
+        buffer_add_byte (buf, ass->dynamic);
+        break;
+    }
+    case CMD_ASSEMBLY_GET_PDB_BLOB: {
+        MonoImage* image = ass->image;
+        MonoDebugHandle* handle = mono_debug_get_handle (image); 
+        if (!handle) {
+            return ERR_INVALID_ARGUMENT;
+        }
+        MonoPPDBFile* ppdb = handle->ppdb;
+        if (ppdb) {
+            image = mono_ppdb_get_image (ppdb);
+            buffer_add_byte_array (buf, (guint8*)image->raw_data, image->raw_data_len);
+        } else {
+            buffer_add_byte_array (buf, NULL, 0);
+        }
+        break;
+    }
+    case CMD_ASSEMBLY_GET_TYPE_FROM_TOKEN: {
+        if (ass->dynamic) {
+            return ERR_NOT_IMPLEMENTED;
+        }
+        guint32 token = decode_int (p, &p, end);
+        ERROR_DECL (error);
+        error_init (error);
+        MonoClass* mono_class = mono_class_get_checked (ass->image, token, error);
+        if (!is_ok (error)) {
+            mono_error_cleanup (error);
+            return ERR_INVALID_ARGUMENT;
+        }
+        buffer_add_typeid (buf, domain, mono_class);
+        mono_error_cleanup (error);
+        break;
+    }
+    case CMD_ASSEMBLY_GET_METHOD_FROM_TOKEN: {
+        if (ass->dynamic) {
+            return ERR_NOT_IMPLEMENTED;
+        }
+        guint32 token = decode_int (p, &p, end);
+        ERROR_DECL (error);
+        error_init (error);
+        MonoMethod* mono_method = mono_get_method_checked (ass->image, token, NULL, NULL, error);
+        if (!is_ok (error)) {
+            mono_error_cleanup (error);
+            return ERR_INVALID_ARGUMENT;
+        }
+        buffer_add_methodid (buf, domain, mono_method);
+        mono_error_cleanup (error);
+        break;
+    }
 	default:
 		return ERR_NOT_IMPLEMENTED;
 	}

commit 96b78b42b76c7cc74d3563ee33b138a96b05e0ff

[sdb] Add a AssemblyMirror.GetMetadata () method to read the metadata from the debuggee, make AssemblyMirror.Metadata return it, or null if its not set/read yet.
Fixes a regression introduced by b26a41cc3ee5fbee01430b8e4f31e687ef90b7db. Parts of Mono.Debugger.Soft would check
AssemblyMirror.Metadata against null, and that would throw an exception when the debuggee was using an older protocol version.

mcs/class/Mono.Debugger.Soft/Mono.Debugger.Soft/AssemblyMirror.cs
@@ -126,12 +126,7 @@ namespace Mono.Debugger.Soft
 			get {
 				if (meta != null)
 					return meta;
-
-				if (IsDynamic)
-					throw new NotSupportedException ();
-				
-				using (var ms = new MemoryStream (GetMetadataBlob ()))
-					return meta = AssemblyDefinition.ReadAssembly (ms);
+				return null;
 			}
 			set {
 				if (value.MainModule.Name != ManifestModule.Name)
@@ -142,6 +137,16 @@ namespace Mono.Debugger.Soft
 			}
 		}
 		
+		// Read assembly metadata from the debuggee
+		// Since protocol version 2.47
+		public AssemblyDefinition GetMetadata () {
+			if (IsDynamic)
+				throw new NotSupportedException ();
+				
+			using (var ms = new MemoryStream (GetMetadataBlob ()))
+				return meta = AssemblyDefinition.ReadAssembly (ms);
+		}
+
 		public byte[] GetMetadataBlob () {
 			if (metadata_blob != null)
 				return metadata_blob;

mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -437,6 +437,7 @@ public class DebuggerTests
 		Assert.IsTrue (assemblyMirrors.Length > 0);
 
 		foreach (var assemblyMirror in assemblyMirrors) {
+			assemblyMirror.GetMetadata ();
 			var metadata = assemblyMirror.Metadata;
 			Assert.AreEqual (metadata.MainModule.Mvid, assemblyMirror.ManifestModule.ModuleVersionId);
 