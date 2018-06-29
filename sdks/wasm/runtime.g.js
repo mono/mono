a3e3e628a4011d3d1452a2f2603a5bde2e446ea9
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2018-06-29 12:59:48 -0400

    [wasm] Reorganize and simplify the location of all debugging support code. It's all now on library_mono.js under the same object. (#9370)
    * [wasm] Add MonoRuntime.get_loaded_files
    
    * [wasm] Add missing dependency.
    
    * [wasm] Move bootstrap and debug interface to library_mono.js to help upstream.
    
    * Update library_mono.js

--- sdks/wasm/runtime.g.js
+++ sdks/wasm/runtime.g.js
@@ -1,52 +1,14 @@
-//FIXME move this to library_mono.js
-var mono_wasm_runtime_is_ready = false;
-function mono_wasm_runtime_ready () {
-	console.log (">>mono_wasm_runtime_ready");
-	mono_wasm_runtime_is_ready = true;
-	debugger;
-}
-
-
 
 var Module = { 
 	onRuntimeInitialized: function () {
-		var assemblies = [ @FILE_LIST@ ];
-		Module.FS_createPath ("/", "@VFS_PREFIX@", true, true);
-
-		var pending = 0;
-		assemblies.forEach (function(file_name) {
-			++pending;
-			fetch ("@DEPLOY_PREFIX@" + file_name, { credentials: 'same-origin' }).then (function (response) {
-				if (!response.ok)
-					throw "failed to load '" + file_name + "'";
-				return response['arrayBuffer']();
-			}).then (function (blob) {
-				var asm = new Uint8Array (blob);
-				Module.FS_createDataFile ("@VFS_PREFIX@/" + file_name, null, asm, true, true, true);
-				console.log ("Loaded: " + file_name);
-				--pending;
-				if (pending == 0)
-					Module.bclLoadingDone ();
+		MONO.mono_load_runtime_and_bcl (
+			"@VFS_PREFIX@",
+			"@DEPLOY_PREFIX@",
+			@ENABLE_DEBUGGING@,
+			[ @FILE_LIST@ ],
+			function () {
+				@BINDINGS_LOADING@
+				App.init ();
 			});
-		});
-	},
-	
-	bclLoadingDone: function () {
-		MonoRuntime.init ();
-	}
-};
-
-var MonoRuntime = {
-	init: function () {
-		this.load_runtime = Module.cwrap ('mono_wasm_load_runtime', null, ['string', 'number']);
-
-		//FIXME move this two to library_mono.js (better yet - library_mono_debugger.js)
-		this.mono_wasm_set_breakpoint = Module.cwrap ('mono_wasm_set_breakpoint', 'number', ['string', 'number', 'number']);
-		this.mono_wasm_clear_all_breakpoints = Module.cwrap ('mono_wasm_clear_all_breakpoints', 'void', [ ]);
-
-		this.load_runtime ("@VFS_PREFIX@", @ENABLE_DEBUGGING@);
-		mono_wasm_runtime_ready ();
-		@BINDINGS_LOADING@
-		App.init ();
 	},
 };
