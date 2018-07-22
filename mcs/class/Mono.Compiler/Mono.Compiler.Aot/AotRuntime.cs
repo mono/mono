8cf8750543ab0b2eb5ec26735722902a66e5bc71
Bernhard Urban <lewurm@gmail.com>
2018-07-22 22:58:03 +0200

    [Mono.Compiler] add csproj file

--- mcs/class/Mono.Compiler/Mono.Compiler.Aot/AotRuntime.cs
+++ mcs/class/Mono.Compiler/Mono.Compiler.Aot/AotRuntime.cs
@@ -7,11 +7,12 @@ namespace Mono.Compiler.Aot
 	{
 		public CompilationResult CompileMethod (ICompilerInformation compilerInfo, MethodInfo methodInfo, CompilationFlags flags, out byte nativeCode, out ulong nativeCodeSize)
 		{
+#if FALSE
 			if (!AppDomain.CurrentDomain.IsDefaultAppDomain) {
 				// Non shared AOT code can't be used in other appdomains
 				return CompilationResult.Skipped;
 			}
-
+#endif
 			
 			// Fetch and return nativeCode from pre-compiled .{so,dylib,dll} image corresponding to `methodInfo`'s assembly.
 			throw new NotImplementedException ();
