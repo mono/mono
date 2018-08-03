ece2cbde54c39014e332d887a1a753694a98e8dc
Zoltan Varga <vargaz@gmail.com>
2018-08-03 10:29:02 -0400

    [sdks] Fix 'Run the offset-tool in 64 bit mode.'

--- sdks/builds/ios.mk
+++ sdks/builds/ios.mk
@@ -370,7 +370,7 @@ _ios-$(1)_CONFIGURE_FLAGS= \
 
 $$(TOP)/sdks/builds/ios-$(1)-$$(CONFIGURATION)/$(4).h: .stamp-ios-$(1)-$$(CONFIGURATION)-configure $$(TOP)/tools/offsets-tool/MonoAotOffsetsDumper.exe
 	cd $$(TOP)/sdks/builds/ios-$(1)-$$(CONFIGURATION) && \
-		MONO_PATH=$(TOP)/tools/offsets-tool/CppSharp/osx \
+		MONO_PATH=$(TOP)/tools/offsets-tool/CppSharp/osx_64 \
 			mono --debug $$(TOP)/tools/offsets-tool/MonoAotOffsetsDumper.exe \
 				--gen-ios --abi $$(_ios-$(1)_OFFSET_TOOL_ABI) --outfile $$@ --mono $$(TOP) --targetdir $$(TOP)/sdks/builds/ios-$(1)-$$(CONFIGURATION)
 
