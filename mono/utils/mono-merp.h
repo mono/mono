a697beaa19b40f5445f0d1b3243acfd4e2054f81
Jay Krell <jaykrell@microsoft.com>
2018-08-04 18:22:30 -0700

    diff reduction

--- mono/utils/mono-merp.h
+++ mono/utils/mono-merp.h
@@ -15,8 +15,6 @@
 #include <glib.h>
 #include <mono/metadata/threads-types.h>
 
-
-
 #ifdef TARGET_OSX
 
 /**
@@ -53,7 +51,5 @@ mono_merp_invoke (const intptr_t crashed_pid, const char *signal, const char *du
 
 
 #endif // TARGET_OSX
-
-
 
 #endif // MONO_UTILS_MERP
