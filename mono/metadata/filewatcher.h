a75bfafdd8b4e872c2cde6b59f25056028da98d5
Jay Krell <jaykrell@microsoft.com>
2018-08-04 18:00:53 -0700

    temporary diff reduction

--- mono/metadata/filewatcher.h
+++ mono/metadata/filewatcher.h
@@ -20,6 +20,8 @@
 #include <unistd.h>
 #endif
 
+G_BEGIN_DECLS
+
 ICALL_EXPORT
 gint ves_icall_System_IO_FSW_SupportsFSW (void);
 
@@ -45,4 +47,7 @@ MONO_API char* SystemNative_RealPath(const char* path);
 MONO_API void SystemNative_Sync (void);
 #endif
 
+G_END_DECLS
+
 #endif
+
