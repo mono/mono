679f19711d32727ea1506b7cdac028cf002877cd
Zoltan Varga <vargaz@gmail.com>
2018-08-07 13:03:56 -0400

    [sdks] Install the llvm36 versions of opt/llc as well.

--- sdks/builds/download-llvm36.sh
+++ sdks/builds/download-llvm36.sh
@@ -15,6 +15,7 @@ tar -xC llvm-tmp36 -f tmp36.tar.gz
 rm -rf ../out/ios-llvm36-32
 mkdir -p ../out/llvm-llvm36-32
 cp -r llvm-tmp36/usr32/* ../out/llvm-llvm36-32
+cp -r llvm-tmp36/usr64/bin/{llc,opt} ../out/llvm-llvm36-32/bin/
 rm -rf llvm-tmp36 tmp36.tar.gz
 cd ../out
 ln -sf llvm-llvm36-32 ios-llvm36-32
