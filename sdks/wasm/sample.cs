4d45f25a385b121c8cc7c2f5c9f524e98a2b97cd
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2018-08-09 19:12:19 -0400

    [wasm] Call the correct single step trampoline. Fixes some step-into scenarios. (#9611)
    * [wasm] Call the correct single step trampoline. Fixes some step-into scenarios.
    
    * [wasm] Make the debugging sample a bit more complicated.
    
    * [wasm] Add debugger support for removing breakpoints.

--- sdks/wasm/sample.cs
+++ sdks/wasm/sample.cs
@@ -3,11 +3,16 @@ using System.Linq;
 
 public class Math {
 	public static int IntAdd (int a, int b) {
+		var cp = new Simple.Complex (10, "hello");
 		int c = a + b;
 		int d = c + b;
 		int e = d + a;
+
+		e += cp.DoStuff ();
+
 		return e;
 	}
+
 
 	public int First (int[] x) {
 		return x.FirstOrDefault ();
