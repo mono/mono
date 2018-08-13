8a27d040d53b8268468d895ffadb6e56055616c1
Alexander Kyte <alkyte@microsoft.com>
2018-08-13 15:37:39 -0400

    [runtime] Add initial support for Exc -> Telemetry
    In order to allow managed crash reporting using the existing telemetry
    format, I have added initial support for converting a MonoException into
    a json blob for telemetry.

--- mono/metadata/icall-def.h
+++ mono/metadata/icall-def.h
@@ -94,7 +94,8 @@ HANDLES(ICALL(TLS_PROVIDER_FACTORY_1, "IsBtlsSupported", ves_icall_Mono_TlsProvi
 ICALL_TYPE(RUNTIME, "Mono.Runtime", RUNTIME_1)
 HANDLES(ICALL(RUNTIME_1, "DisableMicrosoftTelemetry", ves_icall_Mono_Runtime_DisableMicrosoftTelemetry))
 HANDLES(ICALL(RUNTIME_2, "EnableMicrosoftTelemetry_internal", ves_icall_Mono_Runtime_EnableMicrosoftTelemetry))
-HANDLES(ICALL(RUNTIME_3, "GetDisplayName", ves_icall_Mono_Runtime_GetDisplayName))
+HANDLES(ICALL(RUNTIME_3, "ExceptionToState", ves_icall_Mono_Runtime_ExceptionToState))
+HANDLES(ICALL(RUNTIME_4, "GetDisplayName", ves_icall_Mono_Runtime_GetDisplayName))
 HANDLES(ICALL(RUNTIME_12, "GetNativeStackTrace", ves_icall_Mono_Runtime_GetNativeStackTrace))
 HANDLES(ICALL(RUNTIME_13, "SendMicrosoftTelemetry", ves_icall_Mono_Runtime_SendMicrosoftTelemetry))
 
--- mono/utils/mono-state.h
+++ mono/utils/mono-state.h
@@ -16,6 +16,7 @@
 #include <mono/utils/mono-publib.h>
 #include <mono/utils/mono-context.h>
 #include <mono/metadata/threads-types.h>
+#include <mono/utils/json.h>
 
 #define MONO_NATIVE_STATE_PROTOCOL_VERSION "0.0.1"
 
@@ -29,6 +30,15 @@ mono_summarize_native_state_end (void);
 
 void
 mono_summarize_native_state_add_thread (MonoThreadSummary *thread, MonoContext *ctx);
+
+void
+mono_native_state_init (JsonWriter *writer);
+
+char *
+mono_native_state_emit (JsonWriter *writer);
+
+char *
+mono_native_state_free (JsonWriter *writer, gboolean free_data);
 
 MONO_END_DECLS
 #endif // TARGET_OSX
