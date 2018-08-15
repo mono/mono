b3042974a6243beec0ee7410aab7fc34891fcf35
Jay Krell <jaykrell@microsoft.com>
2018-08-15 00:39:06 -0700

    [cxx] Cast function-pointer function-parameter to void* with macro (mono_mb_emit_icall).

--- mono/metadata/method-builder-ilgen.c
+++ mono/metadata/method-builder-ilgen.c
@@ -538,6 +538,8 @@ mono_mb_emit_native_call (MonoMethodBuilder *mb, MonoMethodSignature *sig, gpoin
 	mono_mb_emit_calli (mb, sig);
 }
 
+#undef mono_mb_emit_icall
+
 void
 mono_mb_emit_icall (MonoMethodBuilder *mb, gpointer func)
 {
