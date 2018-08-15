2a136250e812c2b7b7b4b1986fbb6590f3acc366
Jay Krell <jaykrell@microsoft.com>
2018-08-15 02:38:48 -0700

    [cxx] Remove most G_BEGIN_DECLS but add back to make things work. In particular, for monodis, pedump, tests, wasm, OS APIs, debugger/JIT interaction, and incorrect prototypes. Also add missing ICALL_EXPORT, in trace for G_BEGIN_DECLS. Incorrect prototypes related to MonoGCDescriptor and THREAD_INFO_TYPE being ifdefed. OS APIs include: 	sys_icache_invalidate 	__aeabi_read_tp 	misc. in mono-compiler.h

--- mono/metadata/gc-internals.h
+++ mono/metadata/gc-internals.h
@@ -160,7 +160,9 @@ gboolean mono_gc_user_markers_supported (void);
  * size bytes will be available from the returned address (ie, descr
  * must not be stored in the returned memory)
  */
+G_BEGIN_DECLS // FIXMEcxx? Probably because MonoGCDescriptor varies.
 MonoObject* mono_gc_alloc_fixed      (size_t size, MonoGCDescriptor descr, MonoGCRootSource source, void *key, const char *msg);
+G_END_DECLS // FIXMEcxx?
 void  mono_gc_free_fixed             (void* addr);
 
 /* make sure the gchandle was allocated for an object in domain */
@@ -437,11 +439,15 @@ void mono_gc_register_altstack (gpointer stack, gint32 stack_size, gpointer alts
 
 gboolean mono_gc_is_critical_method (MonoMethod *method);
 
+G_BEGIN_DECLS // FIXMEcxx THREAD_INFO_TYPE varies between declaration and definition
+
 gpointer mono_gc_thread_attach (THREAD_INFO_TYPE *info);
 
 void mono_gc_thread_detach_with_lock (THREAD_INFO_TYPE *info);
 
 gboolean mono_gc_thread_in_critical_region (THREAD_INFO_TYPE *info);
+
+G_END_DECLS // FIXMEcxx THREAD_INFO_TYPE varies between declaration and definition
 
 /* If set, print debugging messages around finalizers. */
 extern gboolean mono_log_finalizers;
