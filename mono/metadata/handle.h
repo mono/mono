47a9328a54274bf51c831bb33dbb0ec053bb27c5
Jay Krell <jaykrell@microsoft.com>
2018-08-14 23:55:09 -0700

    [cxx] Remove excess externC.

--- mono/metadata/handle.h
+++ mono/metadata/handle.h
@@ -52,8 +52,6 @@
 #define MONO_TYPE_SAFE_HANDLES 0 // PowerPC, S390X, SPARC, MIPS, Linux/x86, BSD/x86, etc.
 #endif
 
-G_BEGIN_DECLS
-
 /*
 Handle stack.
 
@@ -771,7 +769,5 @@ mono_gchandle_new_weakref_from_handle_track_resurrection (MonoObjectHandle handl
 {
 	return mono_gchandle_new_weakref (MONO_HANDLE_SUPPRESS (MONO_HANDLE_RAW (handle)), TRUE);
 }
-
-G_END_DECLS
 
 #endif /* __MONO_HANDLE_H__ */
