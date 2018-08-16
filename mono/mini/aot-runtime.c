32acf3e3073a457cbceca2569bb767e1c4dbb835
Jay Krell <jaykrell@microsoft.com>
2018-08-16 14:54:41 -0700

    [cxx] [mini]

--- mono/mini/aot-runtime.c
+++ mono/mini/aot-runtime.c
@@ -86,7 +86,14 @@ typedef struct {
 	MonoJitInfo *jinfo;
 } JitInfoMap;
 
-typedef struct MonoAotModule {
+// warning: redefinition of typedef 'MonoAotModule' is a C11 feature [-Wtypedef-redefinition]
+#ifndef MonoAotModule
+#define MonoAotModule MonoAotModule
+struct MonoAotModule;
+typedef struct MonoAotModule MonoAotModule;
+#endif
+
+struct MonoAotModule {
 	char *aot_name;
 	/* Pointer to the Global Offset Table */
 	gpointer *got;
@@ -157,7 +164,7 @@ typedef struct MonoAotModule {
 
 	JitInfoMap *async_jit_info_table;
 	mono_mutex_t mutex;
-} MonoAotModule;
+};
 
 typedef struct {
 	void *next;
@@ -4471,7 +4478,8 @@ init_method (MonoAotModule *amodule, guint32 method_index, MonoMethod *method, M
 	if (mini_get_debug_options ()->load_aot_jit_info_eagerly)
 		jinfo = mono_aot_find_jit_info (domain, amodule->assembly->image, code);
 
-	gboolean inited_ok = TRUE;
+	gboolean inited_ok;
+	inited_ok = TRUE;
 	if (init_class) {
 		MonoVTable *vt = mono_class_vtable_checked (domain, init_class, error);
 		if (!is_ok (error))
@@ -5188,6 +5196,7 @@ mono_create_ftnptr_malloc (guint8 *code)
  *   Register a JIT icall which is called by trampolines in full-aot mode. This should
  * be called from mono_arch_init () during startup.
  */
+#undef mono_aot_register_jit_icall
 void
 mono_aot_register_jit_icall (const char *name, gpointer addr)
 {
@@ -5535,8 +5544,8 @@ get_new_trampoline_from_page (int tramp_type)
 		page = (TrampolinePage*)addr;
 		page->next = trampoline_pages [tramp_type];
 		trampoline_pages [tramp_type] = page;
-		page->trampolines = (void*)(taddr + amodule->info.tramp_page_code_offsets [tramp_type]);
-		page->trampolines_end = (void*)(taddr + psize - 64);
+		page->trampolines = (guint8*)(taddr + amodule->info.tramp_page_code_offsets [tramp_type]);
+		page->trampolines_end = (guint8*)(taddr + psize - 64);
 		code = page->trampolines;
 		page->trampolines += specific_trampoline_size;
 		mono_aot_page_unlock ();
@@ -6254,6 +6263,7 @@ mono_aot_get_unwind_info (MonoJitInfo *ji, guint32 *unwind_info_len)
 	return NULL;
 }
 
+#undef mono_aot_register_jit_icall
 void
 mono_aot_register_jit_icall (const char *name, gpointer addr)
 {
