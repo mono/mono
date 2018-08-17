ddf5ab2f78be8add00cd28def9968ec72f9e5d95
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2018-08-17 13:11:42 -0400

    [wasm] Enough scafold to enable AOT work with the SDKs (#10136)
    * [wasm] add wasm-aot and wasm-aot-runtime
    
    * [runtime] Add MONO_STRUCT_SIZE that works similarly to MONO_STRUCT_OFFSET and handles mini types.
    
    * [wasm] Add enough scafold for the AOT compiler.
    
    * [wasm] Make some functions available for the interp to satisfy the linker.
    
    * [wasm] Add aot-sample and link-sample helper targets for AOT development.
    
    * [wasm/sdks] Update documentation.
    
    * Update README.md

--- sdks/builds/wasm.mk
+++ sdks/builds/wasm.mk
@@ -48,9 +48,86 @@ clean-wasm:
 
 .PHONY: clean-wasm-interp
 clean-wasm-interp: clean-wasm
-	rm -rf .stamp-wasm-interp-toolchain .stamp-wasm-interp-configure $(TOP)/sdks/builds/wasm $(TOP)/sdks/builds/wasm.config.cache $(TOP)/sdks/out/wasm-interp
+	rm -rf .stamp-wasm-interp-toolchain .stamp-wasm-interp-configure $(TOP)/sdks/builds/wasm-interp $(TOP)/sdks/builds/wasm.config.cache $(TOP)/sdks/out/wasm-interp
 
 TARGETS += wasm-interp
 
+UNAME := $(shell uname -s)
+ifeq ($(UNAME),Linux)
+	CROSS_HOST=i386-unknown-linux
+endif
+ifeq ($(UNAME),Darwin)
+	CROSS_HOST=i386-apple-darwin10
+endif
+
+WASM_AOT_CONFIGURE_FLAGS = \
+	--cache-file=$(TOP)/sdks/builds/wasm-aot.config.cache \
+	--prefix=$(TOP)/sdks/out/wasm-aot \
+	--host=$(CROSS_HOST)	\
+	--target=wasm32	\
+	--disable-mcs-build \
+	--disable-nls \
+	--disable-boehm \
+	--disable-btls \
+	--disable-support-build \
+	--enable-maintainer-mode	\
+	--enable-llvm	\
+	--enable-minimal=appdomains,com,remoting
+
+.stamp-wasm-aot-toolchain: .stamp-wasm-toolchain
+	touch $@
+
+.stamp-wasm-aot-configure: $(TOP)/configure
+	mkdir -p $(TOP)/sdks/builds/wasm-aot
+	cd $(TOP)/sdks/builds/wasm-aot && CFLAGS="-g" $(TOP)/configure $(WASM_AOT_CONFIGURE_FLAGS)
+	touch $@
+
+.PHONY: package-wasm-aot
+package-wasm-aot:
+	$(MAKE) -C $(TOP)/sdks/builds/wasm-aot/mono install
+
+.PHONY: clean-wasm-aot
+clean-wasm-aot: clean-wasm
+	rm -rf .stamp-wasm-aot-toolchain .stamp-wasm-aot-configure $(TOP)/sdks/builds/wasm-aot $(TOP)/sdks/builds/wasm.config.cache $(TOP)/sdks/out/wasm-aot
+
+TARGETS += wasm-aot
 
 
+WASM_AOT_RUNTIME_CONFIGURE_FLAGS = \
+	--cache-file=$(TOP)/sdks/builds/wasm-aot-runtime.config.cache \
+	--prefix=$(TOP)/sdks/out/wasm-aot-runtime \
+	--enable-llvm-runtime \
+	--with-bitcode=yes	\
+	--host=wasm32	\
+	--disable-mcs-build \
+	--disable-nls \
+	--disable-boehm \
+	--disable-btls \
+	--with-lazy-gc-thread-creation=yes \
+	--with-libgc=none \
+	--disable-executables \
+	--disable-support-build \
+	--disable-visibility-hidden \
+	--enable-maintainer-mode	\
+	--enable-minimal=ssa,com,jit,reflection_emit_save,reflection_emit,portability,assembly_remapping,attach,verifier,full_messages,appdomains,security,sgen_marksweep_conc,sgen_split_nursery,sgen_gc_bridge,logging,remoting,shared_perfcounters,sgen_debug_helpers,soft_debug
+
+.stamp-wasm-aot-runtime-toolchain: .stamp-wasm-toolchain
+	touch $@
+
+.stamp-wasm-aot-runtime-configure: $(TOP)/configure
+	mkdir -p $(TOP)/sdks/builds/wasm-aot-runtime
+	cd $(TOP)/sdks/builds/wasm-aot-runtime && source $(TOP)/sdks/builds/toolchains/emsdk/emsdk_env.sh && CFLAGS="-Os -g" emconfigure $(TOP)/configure $(WASM_AOT_RUNTIME_CONFIGURE_FLAGS)
+	touch $@
+
+build-custom-wasm-aot-runtime:
+	source $(TOP)/sdks/builds/toolchains/emsdk/emsdk_env.sh && make -C wasm-aot-runtime
+
+.PHONY: package-wasm-aot-runtime
+package-wasm-aot-runtime:
+	$(MAKE) -C $(TOP)/sdks/builds/wasm-aot-runtime/mono install
+
+.PHONY: clean-wasm-aot-runtime
+clean-wasm-aot-runtime: clean-wasm
+	rm -rf .stamp-wasm-aot-runtime-toolchain .stamp-wasm-aot-runtime-configure $(TOP)/sdks/builds/wasm-aot-runtime $(TOP)/sdks/builds/wasm.config.cache $(TOP)/sdks/out/wasm-aot-runtime
+
+TARGETS += wasm-aot-runtime
