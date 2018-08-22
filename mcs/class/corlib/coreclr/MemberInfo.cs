f769e729f4980717faa949732947207331dbe5d0
Maxim Lipnin <v-maxlip@microsoft.com>
2018-08-22 13:23:06 +0300

    Fix styles

--- mcs/class/corlib/coreclr/MemberInfo.cs
+++ mcs/class/corlib/coreclr/MemberInfo.cs
@@ -1,25 +1,26 @@
-namespace System.Reflection
-{
-    partial class MemberInfo
-    {
-        internal virtual bool CacheEquals(object o) { throw new NotImplementedException(); }
+namespace System.Reflection {
+	partial class MemberInfo {
+		internal virtual bool CacheEquals (object o) 
+		{ 
+			throw new NotImplementedException (); 
+		}
 
-        internal bool HasSameMetadataDefinitionAsCore<TOther>(MemberInfo other) where TOther : MemberInfo
-        {
-            if (other == null)
-                throw new ArgumentNullException(nameof(other));
+		internal bool HasSameMetadataDefinitionAsCore<TOther> (MemberInfo other) where TOther : MemberInfo
+		{
+			if (other == null)
+				throw new ArgumentNullException (nameof (other));
 
-            // Ensure that "other" is a runtime-implemented MemberInfo. Do this check before calling any methods on it!
-            if (!(other is TOther)) 
-                return false;
+			// Ensure that "other" is a runtime-implemented MemberInfo. Do this check before calling any methods on it!
+			if (!(other is TOther)) 
+				return false;
 
-            if (MetadataToken != other.MetadataToken)
-                return false;
+			if (MetadataToken != other.MetadataToken)
+				return false;
 
-            if (!(Module.Equals(other.Module)))
-                return false;
+			if (!(Module.Equals (other.Module)))
+				return false;
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
