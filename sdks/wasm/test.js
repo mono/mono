147dc0d0e981a08a58f4712d8cf3453617e3c96d
Rodrigo Kumpera <kumpera@gmail.com>
2018-08-23 11:50:52 -0400

    [wasm] Fix typo.

--- sdks/wasm/test.js
+++ sdks/wasm/test.js
@@ -96,13 +96,13 @@ var find_method = Module.cwrap ('mono_wasm_assembly_find_method', 'number', ['nu
 const IGNORE_PARAM_COUNT = -1;
 
 //test driver support code
-var bad_semd_msg_detected = false;
+var bad_send_msg_detected = false;
 function mono_send_msg (key, val) {
 	try {
 		return Module.mono_method_invoke (send_message, null, "ss", [key, val]);
 	} catch (e) {
 		print ("BAD SEND MSG: " + e);
-		bad_semd_msg_detected = true;
+		bad_send_msg_detected = true;
 		return null;
 	}
 }
@@ -159,5 +159,5 @@ print ("Test status " + status)
 if (status != "PASS")
 	fail_exec ("BAD TEST STATUS");
 
-if (bad_semd_msg_detected)
+if (bad_send_msg_detected)
 	fail_exec ("BAD MSG SEND DETECTED");
