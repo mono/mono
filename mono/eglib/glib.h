fbc21785fd1aa90fe8c375e2093d33c088ee8ef3
Jay Krell <jaykrell@microsoft.com>
2018-08-23 23:15:17 -0700

    [cxx] int vs. enum.
    There are at least three or so approaches to this.
    
    1. Sprinkle around casts.
    2. Overload operators.
    2a. Bit-only (| & ^ |= &= ^=)   This is this PR and seemingly the consensus.
    2b.  Also math (+ - ++ -- == !=)
    3. typedef to int instead (https://github.com/mono/mono/pull/10231)
    
    They all have tradeoffs.
    
    1. Casts: The most line damage. Not huge, but the most.
    
    2. Overloads: New idiom to some but an old idiom. Supported in this way by windows.h even.
      This is the most "C++y solution".
    
    3. int: Highest level of source compat and semantic compat. Least line damage (just
    change the declarations). Least debuggable and least typesafe.
    
    In terms of semantic compat, even in C the guarantees aren't particularly strong.
    Some enums will be int, some unsigned, depending on the values and the compiler.
    Perhaps even other types, but I doubt it.
    
    I observe C++ compilers don't tend toward unsigned the same way, so switching
    to C++ will on some platforms lose unsignedness. Given we didn't have it portably
    anyway, should be ok.
    
    A typesafe enum has the properties that enum A cannot be silently assigned to enum B (ok,
    there might be a warning), or from an integer.
    
    A typesafe enum, bitfield or otherwise, can/will be shown symbolically by a debugger.
     (lldb does not handle bitfields, only strict enums)
    
    A C enum might be debuggable, w/o typesafety.
    
    A C enum can be freely mixed with other enums and integers.
    
    A C++ enum is more restricted.
    
    Using typedef int, is a maybe surprising option, but does provide the closest meaning to C enum.
    
    Per guidance, this PR also changes one enum to #defines + int.

--- mono/eglib/glib.h
+++ mono/eglib/glib.h
@@ -107,6 +107,56 @@ public:
 
 #endif
 
+#ifdef __cplusplus
+
+// Provide for bit operations on enums, but not all integer operations.
+// This alleviates a fair number of casts in porting C to C++.
+
+// Forward declare template with no generic implementation.
+template <size_t> struct mono_size_to_int;
+
+// Template specializations.
+template <> struct mono_size_to_int<1> { typedef int8_t T; };
+template <> struct mono_size_to_int<2> { typedef int16_t T; };
+template <> struct mono_size_to_int<4> { typedef int32_t T; };
+template <> struct mono_size_to_int<8> { typedef int64_t T; };
+
+#define MONO_SIZE_TO_INT(x) mono_size_to_int <sizeof (x)>::T
+
+#define G_ENUM_BINOP(Enum, op, opeq) 		\
+inline Enum					\
+operator op (Enum a, Enum b)			\
+{						\
+	typedef MONO_SIZE_TO_INT (a) T;		\
+	return (Enum)((T)a op (T)b);		\
+}						\
+						\
+inline Enum&					\
+operator opeq (Enum& a, Enum b)			\
+{						\
+	typedef MONO_SIZE_TO_INT (a) T; 	\
+	return (Enum&)((T&)a opeq (T)b);	\
+}						\
+
+#define G_ENUM_FUNCTIONS(Enum)			\
+extern "C++" { /* in case within extern "C" */	\
+inline Enum					\
+operator~ (Enum a)				\
+{						\
+	typedef MONO_SIZE_TO_INT (a) T;		\
+	return (Enum)~(T)a;			\
+}						\
+						\
+G_ENUM_BINOP (Enum, |, |=) 			\
+G_ENUM_BINOP (Enum, &, &=) 			\
+G_ENUM_BINOP (Enum, ^, ^=) 			\
+						\
+} /* extern "C++" */
+
+#else
+#define G_ENUM_FUNCTIONS(Enum) /* nothing */
+#endif
+
 G_BEGIN_DECLS
 
 /*
@@ -652,6 +702,8 @@ typedef enum {
 	G_LOG_LEVEL_MASK              = ~(G_LOG_FLAG_RECURSION | G_LOG_FLAG_FATAL)
 } GLogLevelFlags;
 
+G_ENUM_FUNCTIONS (GLogLevelFlags)
+
 void           g_printv               (const gchar *format, va_list args);
 void           g_print                (const gchar *format, ...);
 void           g_printerr             (const gchar *format, ...);
@@ -984,6 +1036,7 @@ typedef enum {
 	G_FILE_TEST_EXISTS = 1 << 4
 } GFileTest;
 
+G_ENUM_FUNCTIONS (GFileTest)
 
 gboolean   g_file_set_contents (const gchar *filename, const gchar *contents, gssize length, GError **gerror);
 gboolean   g_file_get_contents (const gchar *filename, gchar **contents, gsize *length, GError **gerror);
