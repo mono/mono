0c952fb0f8507a856dba2540f4f4eb37dfe02e70
Jay Krell <jaykrell@microsoft.com>
2018-08-24 14:13:41 -0700

    PR: "mono" => "g" "T" => "type" static_cast where it works (not to reference) Use templatized typedef instead of macro, if it works on g++4.4. Add test. Provide to build eglib tests. Borrow CXXFLAGS_COMMON from https://github.com/mono/mono/pull/10271. Add some const to test harness.

--- mono/eglib/glib.h
+++ mono/eglib/glib.h
@@ -113,29 +113,30 @@ public:
 // This alleviates a fair number of casts in porting C to C++.
 
 // Forward declare template with no generic implementation.
-template <size_t> struct mono_size_to_int;
+template <size_t> struct g_size_to_int;
 
 // Template specializations.
-template <> struct mono_size_to_int<1> { typedef int8_t T; };
-template <> struct mono_size_to_int<2> { typedef int16_t T; };
-template <> struct mono_size_to_int<4> { typedef int32_t T; };
-template <> struct mono_size_to_int<8> { typedef int64_t T; };
+template <> struct g_size_to_int<1> { typedef int8_t type; };
+template <> struct g_size_to_int<2> { typedef int16_t type; };
+template <> struct g_size_to_int<4> { typedef int32_t type; };
+template <> struct g_size_to_int<8> { typedef int64_t type; };
 
-#define MONO_SIZE_TO_INT(x) mono_size_to_int <sizeof (x)>::T
+template <typename T>
+using g_size_to_int_t = typename g_size_to_int <sizeof (T)>::type;
 
 #define G_ENUM_BINOP(Enum, op, opeq) 		\
 inline Enum					\
 operator op (Enum a, Enum b)			\
 {						\
-	typedef MONO_SIZE_TO_INT (a) T;		\
-	return (Enum)((T)a op (T)b);		\
+	typedef g_size_to_int_t <Enum> type; 	\
+	return static_cast<Enum>(static_cast<type>(a) op static_cast<type>(b)); \
 }						\
 						\
 inline Enum&					\
 operator opeq (Enum& a, Enum b)			\
 {						\
-	typedef MONO_SIZE_TO_INT (a) T; 	\
-	return (Enum&)((T&)a opeq (T)b);	\
+	typedef g_size_to_int_t <Enum> type; 	\
+	return (Enum&)((type&)(a) opeq static_cast<type>(b)); \
 }						\
 
 #define G_ENUM_FUNCTIONS(Enum)			\
@@ -143,8 +144,8 @@ extern "C++" { /* in case within extern "C" */	\
 inline Enum					\
 operator~ (Enum a)				\
 {						\
-	typedef MONO_SIZE_TO_INT (a) T;		\
-	return (Enum)~(T)a;			\
+	typedef g_size_to_int_t <Enum> type; 	\
+	return static_cast<Enum>(~static_cast<type>(a)); \
 }						\
 						\
 G_ENUM_BINOP (Enum, |, |=) 			\
