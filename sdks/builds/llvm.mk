e2018ed1e7a8445ec00e31bbbe1c13927fbc9a10
Ludovic Henry <luhenry@microsoft.com>
2018-08-27 07:57:04 -0400

    [2018-04] [sdks] Fix llvm local variable names (#10300)
    * [sdks] Fix llvm local variable names
    
    * [sdks] Allow for overwriting llvm-{llvm32,llvm64} CC and CXX

--- sdks/builds/llvm.mk
+++ sdks/builds/llvm.mk
@@ -13,17 +13,19 @@ $(LLVM_SRC)/configure: | $(LLVM_SRC)
 #  $(2): arch
 define LLVMTemplate
 
-_llvm_$(1)_CXXFLAGS= \
+_llvm-$(1)_CXXFLAGS= \
 	$$(if $$(filter $$(UNAME),Darwin),-mmacosx-version-min=10.9 -stdlib=libc++)
 
-_llvm_$(1)_LDFLAGS= \
+_llvm-$(1)_LDFLAGS= \
 	$$(if $$(filter $$(UNAME),Darwin),-mmacosx-version-min=10.9)
 
-_llvm_$(1)_CONFIGURE_ENVIRONMENT= \
-	CXXFLAGS="$$(_llvm_$(1)_CXXFLAGS)" \
-	LDFLAGS="$$(_llvm_$(1)_LDFLAGS)"
+_llvm-$(1)_CONFIGURE_ENVIRONMENT= \
+	$$(if $$(llvm-$(1)_CC),CC="$$(llvm-$(1)_CC)") \
+	$$(if $$(llvm-$(1)_CXX),CC="$$(llvm-$(1)_CXX)") \
+	CXXFLAGS="$$(_llvm-$(1)_CXXFLAGS)" \
+	LDFLAGS="$$(_llvm-$(1)_LDFLAGS)"
 
-_llvm_$(1)_CONFIGURE_FLAGS= \
+_llvm-$(1)_CONFIGURE_FLAGS= \
 	--build=$$(if $$(filter $$(UNAME),Darwin),$(2)-apple-darwin10,$$(if $$(filter $$(UNAME),Linux),x86_64-linux-gnu,$$(error "Unknown UNAME='$$(UNAME)'"))) \
 	--cache-file=$$(TOP)/sdks/builds/llvm-$(1).config.cache \
 	--prefix=$$(TOP)/sdks/out/llvm-$(1) \
@@ -37,7 +39,7 @@ _llvm_$(1)_CONFIGURE_FLAGS= \
 
 .stamp-llvm-$(1)-configure: $$(LLVM_SRC)/configure
 	mkdir -p $$(TOP)/sdks/builds/llvm-$(1)
-	cd $$(TOP)/sdks/builds/llvm-$(1) && $$< $$(_llvm_$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm_$(1)_CONFIGURE_FLAGS)
+	cd $$(TOP)/sdks/builds/llvm-$(1) && $$< $$(_llvm-$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm-$(1)_CONFIGURE_FLAGS)
 	touch $$@
 
 .PHONY: package-llvm-$(1)
@@ -61,39 +63,39 @@ $(eval $(call LLVMTemplate,llvm64,x86_64))
 #  $(2): arch
 #
 # Flags
-#  llvm_$(1)_CONFIGURE_ENVIRONMENT
+#  llvm-$(1)_CONFIGURE_ENVIRONMENT
 define LLVMMxeTemplate
 
-_llvm_$(1)_PATH=$$(MXE_PREFIX)/bin
+_llvm-$(1)_PATH=$$(MXE_PREFIX)/bin
 
-_llvm_$(1)_AR=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ar
-_llvm_$(1)_AS=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-as
-_llvm_$(1)_CC=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-gcc
-_llvm_$(1)_CXX=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-g++
-_llvm_$(1)_DLLTOOL=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-dlltool
-_llvm_$(1)_LD=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ld
-_llvm_$(1)_OBJDUMP=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-objdump
-_llvm_$(1)_RANLIB=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ranlib
-_llvm_$(1)_STRIP=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-strip
+_llvm-$(1)_AR=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ar
+_llvm-$(1)_AS=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-as
+_llvm-$(1)_CC=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-gcc
+_llvm-$(1)_CXX=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-g++
+_llvm-$(1)_DLLTOOL=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-dlltool
+_llvm-$(1)_LD=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ld
+_llvm-$(1)_OBJDUMP=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-objdump
+_llvm-$(1)_RANLIB=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-ranlib
+_llvm-$(1)_STRIP=$$(MXE_PREFIX)/bin/$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static)-strip
 
-_llvm_$(1)_CXXFLAGS=
+_llvm-$(1)_CXXFLAGS=
 
-_llvm_$(1)_LDFLAGS=
+_llvm-$(1)_LDFLAGS=
 
-_llvm_$(1)_CONFIGURE_ENVIRONMENT = \
-	AR="$$(_llvm_$(1)_AR)" \
-	AS="$$(_llvm_$(1)_AS)" \
-	CC="$$(_llvm_$(1)_CC)" \
-	CXX="$$(_llvm_$(1)_CXX)" \
-	DLLTOOL="$$(_llvm_$(1)_DLLTOOL)" \
-	LD="$$(_llvm_$(1)_LD)" \
-	OBJDUMP="$$(_llvm_$(1)_OBJDUMP)" \
-	RANLIB="$$(_llvm_$(1)_RANLIB)" \
-	STRIP="$$(_llvm_$(1)_STRIP)" \
-	CXXFLAGS="$$(_llvm_$(1)_CXXFLAGS)" \
-	LDFLAGS="$$(_llvm_$(1)_LDFLAGS)"
+_llvm-$(1)_CONFIGURE_ENVIRONMENT = \
+	AR="$$(_llvm-$(1)_AR)" \
+	AS="$$(_llvm-$(1)_AS)" \
+	CC="$$(_llvm-$(1)_CC)" \
+	CXX="$$(_llvm-$(1)_CXX)" \
+	DLLTOOL="$$(_llvm-$(1)_DLLTOOL)" \
+	LD="$$(_llvm-$(1)_LD)" \
+	OBJDUMP="$$(_llvm-$(1)_OBJDUMP)" \
+	RANLIB="$$(_llvm-$(1)_RANLIB)" \
+	STRIP="$$(_llvm-$(1)_STRIP)" \
+	CXXFLAGS="$$(_llvm-$(1)_CXXFLAGS)" \
+	LDFLAGS="$$(_llvm-$(1)_LDFLAGS)"
 
-_llvm_$(1)_CONFIGURE_FLAGS = \
+_llvm-$(1)_CONFIGURE_FLAGS = \
 	--host=$(2)-w64-mingw32$$(if $$(filter $(UNAME),Darwin),.static) \
 	--cache-file=$$(TOP)/sdks/builds/llvm-$(1).config.cache \
 	--prefix=$$(TOP)/sdks/out/llvm-$(1) \
@@ -108,7 +110,7 @@ _llvm_$(1)_CONFIGURE_FLAGS = \
 
 .stamp-llvm-$(1)-configure: $$(LLVM_SRC)/configure | $(if $(IGNORE_PROVISION_MXE),,provision-mxe)
 	mkdir -p $$(TOP)/sdks/builds/llvm-$(1)
-	cd $$(TOP)/sdks/builds/llvm-$(1) && PATH="$$$$PATH:$$(_llvm_$(1)_PATH)" $$< $$(_llvm_$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm_$(1)_CONFIGURE_FLAGS)
+	cd $$(TOP)/sdks/builds/llvm-$(1) && PATH="$$$$PATH:$$(_llvm-$(1)_PATH)" $$< $$(_llvm-$(1)_CONFIGURE_ENVIRONMENT) $$(_llvm-$(1)_CONFIGURE_FLAGS)
 	touch $$@
 
 .PHONY: package-llvm-$(1)
