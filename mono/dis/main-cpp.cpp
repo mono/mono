45260d3965942e1c1a0ca85bdd02c8ff68989de2
Jay Krell <jaykrell@microsoft.com>
2018-08-28 02:13:16 -0700

    Rework pedump/monodis/monograph/unit-tests build to not link to libstdc++. This broke running pedump in Windows x64 CI.
    The new approach is:
     move "executable" (main) into a static library.
     Rename main. Make it extern "C".
     Use a small .c file with main that calls the renamed main.
     Link to the .o files that used to be in in the executable.
      This has higher build fidelity than linking to libraries.
    
    There is excess libtool usage here. Fix later.

--- mono/dis/main-cpp.cpp
+++
@@ -1,5 +0,0 @@
-// Currently we cannot compile the file containing main as C++,
-// using the -xc++ switch. The compiler applies the -xc++ switch
-// to .o/.a files and fails. But we can if we rename to change
-// the extension to .cpp. This is like a rename.
-#include "main.c"
