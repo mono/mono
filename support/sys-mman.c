a0647d883d229017592c5810c9b7092c19b5f599
Jay Krell <jaykrell@microsoft.com>
2018-08-28 22:38:17 -0700

    Make it work on Linux too.

--- support/sys-mman.c
+++ support/sys-mman.c
@@ -143,7 +143,12 @@ Mono_Posix_Syscall_mincore (void *start, mph_size_t length, unsigned char *vec)
 #else
 	mph_return_if_size_t_overflow (length);
 
-	return mincore (start, (size_t) length, (char*)vec);
+#ifdef __linux__
+	typedef unsigned char T;
+#else
+	typedef char T;
+#endif
+	return mincore (start, (size_t) length, (T*)vec);
 #endif
 }
 
