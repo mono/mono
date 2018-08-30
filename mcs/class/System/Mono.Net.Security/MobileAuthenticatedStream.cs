0dc7d2f8fd73a6342535596ff5cef3a007c0b91d
Martin Baulig <martin.baulig@xamarin.com>
2018-08-30 22:29:52 +0200

    [System]: Make `MobileAuthenticatedStream's read buffer large enough to hold a full TLS record.

--- mcs/class/System/Mono.Net.Security/MobileAuthenticatedStream.cs
+++ mcs/class/System/Mono.Net.Security/MobileAuthenticatedStream.cs
@@ -77,7 +77,7 @@ namespace Mono.Net.Security
 			Settings = settings;
 			Provider = provider;
 
-			readBuffer = new BufferOffsetSize2 (16384);
+			readBuffer = new BufferOffsetSize2 (16500);
 			writeBuffer = new BufferOffsetSize2 (16384);
 			operation = Operation.None;
 		}
