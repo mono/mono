d76f307a863228bfbbebc970e3756742d4cdf94d
Jonathan Chambers <joncham@gmail.com>
2018-09-04 21:37:43 -0400

    Handle ref returns in runtime invoke wrappers (case 1076527)
    Indirectly load ref return values in invoke wrappers and return by value.
    Don't share wrappers with ref returns since indirect load is used.
    Add soft debugger support for methods with ref returns.

--- mcs/class/Mono.Debugger.Soft/Test/dtest.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -4398,15 +4398,31 @@ public class DebuggerTests
 	}
 
 	[Test]
-    public void InvokeGenericMethod () {
-        Event e = run_until ("bp1");
-        StackFrame frame = e.Thread.GetFrames()[0];
-        TypeMirror t = frame.Method.DeclaringType;
-        MethodMirror m;
-        m = t.GetMethod ("generic_method");
-        AssertThrows<ArgumentException> (delegate {
-        	t.InvokeMethod (e.Thread, m, null);
-        	});      
-    }
+	public void InvokeGenericMethod () {
+		Event e = run_until ("bp1");
+		StackFrame frame = e.Thread.GetFrames()[0];
+		TypeMirror t = frame.Method.DeclaringType;
+		MethodMirror m;
+		m = t.GetMethod ("generic_method");
+		AssertThrows<ArgumentException> (delegate {
+				t.InvokeMethod (e.Thread, m, null);
+			});
+	}
+
+	[Test]
+	public void InvokeRefReturnMethod () {
+		Event e = run_until ("ref_return");
+		StackFrame frame = e.Thread.GetFrames()[0];
+		TypeMirror t = frame.Method.DeclaringType;
+		MethodMirror m;
+
+		m = t.GetMethod ("get_ref_int");
+		var v = t.InvokeMethod (e.Thread, m, null);
+		AssertValue (1, v);
+
+		m = t.GetMethod ("get_ref_string");
+		v = t.InvokeMethod (e.Thread, m, null);
+		AssertValue ("byref", v);
+	}
 } // class DebuggerTests
 } // namespace
