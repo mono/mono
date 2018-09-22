e257971d3c89ba4a054514d83ea84eb2bcb7ad69
Zoltan Varga <vargaz@gmail.com>
2018-09-22 02:58:44 +0200

    [llvm] Remove an incorrect assert.

--- mono/mini/mini-llvm.c
+++ mono/mini/mini-llvm.c
@@ -4527,7 +4527,6 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			if (ins->opcode == OP_LCOMPARE_IMM) {
 				lhs = convert (ctx, lhs, LLVMInt64Type ());
 				rhs = LLVMConstInt (LLVMInt64Type (), GET_LONG_IMM (ins), FALSE);
-				g_assert (GET_LONG_IMM (ins) == ins->inst_l);
 			}
 			if (ins->opcode == OP_LCOMPARE) {
 				lhs = convert (ctx, lhs, LLVMInt64Type ());
