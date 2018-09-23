aee100b3213295ae293dc5bc44b4b02f8246ffac
Zoltan Varga <vargaz@gmail.com>
2018-09-23 17:08:57 +0200

    [llvm] Avoid passing a dummy arg to string:memcpy/bzero.

--- mono/mini/mini-llvm.c
+++ mono/mini/mini-llvm.c
@@ -3238,6 +3238,10 @@ needs_extra_arg (EmitContext *ctx, MonoMethod *method)
 	}
 	if (method->string_ctor)
 		return FALSE;
+
+	/* These are called from gsharedvt code with an indirect call which doesn't pass an extra arg */
+	if (method->klass == mono_defaults.string_class && (strstr (method->name, "memcpy") || strstr (method->name, "bzero")))
+		return FALSE;
 	return TRUE;
 }
 
