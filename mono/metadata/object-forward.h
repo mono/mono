50f4a95fd1b0510c5c769c49c45d50d13bf4e459
Jay Krell <jaykrell@microsoft.com>
2018-09-26 11:22:56 -0700

    Fix merge.

--- mono/metadata/object-forward.h
+++ mono/metadata/object-forward.h
@@ -14,10 +14,10 @@ typedef struct _MonoClass MonoClass;
 typedef struct _MonoImage MonoImage;
 typedef struct _MonoMethod MonoMethod;
 
-typedef struct MonoObject MONO_RT_MANAGED_ATTR MonoObject;
-typedef struct MonoException MONO_RT_MANAGED_ATTR MonoException;
+typedef struct _MonoObject MONO_RT_MANAGED_ATTR MonoObject;
+typedef struct _MonoException MONO_RT_MANAGED_ATTR MonoException;
 typedef struct _MonoMList MONO_RT_MANAGED_ATTR MonoMList;
-typedef struct MonoReflectionAssembly MONO_RT_MANAGED_ATTR MonoReflectionAssembly;
-typedef struct MonoReflectionTypeBuilder MONO_RT_MANAGED_ATTR MonoReflectionTypeBuilder;
+typedef struct _MonoReflectionAssembly MONO_RT_MANAGED_ATTR MonoReflectionAssembly;
+typedef struct _MonoReflectionTypeBuilder MONO_RT_MANAGED_ATTR MonoReflectionTypeBuilder;
 
 #endif /* __MONO_OBJECT_FORWARD_H__ */
