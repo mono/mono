7faf8f2124801a8e6dd3366a784314639740ddb2
Ludovic Henry <luhenry@microsoft.com>
2018-09-28 19:36:41 -0400

    [sdks] Build BCL profiles without configure flags

--- sdks/builds/bcl.mk
+++ sdks/builds/bcl.mk
@@ -1,38 +1,51 @@
 
+# controls, essentially, which profiles get built (maybe more later)
+BCL_PROFILES=
+
+ifndef DISABLE_ANDROID
+BCL_PROFILES += monodroid monodroid_tools
+endif
+
+ifndef DISABLE_IOS
+BCL_PROFILES += monotouch monotouch_tv monotouch_watch monotouch_runtime monotouch_tv_runtime monotouch_watch_runtime xammac xammac_net_4_5
+endif
+
+ifndef DISABLE_DESKTOP
+BCL_PROFILES += net_4_x
+endif
+
+ifndef DISABLE_WASM
+BCL_PROFILES += wasm
+endif
+
 .stamp-bcl-toolchain:
 	touch $@
 
-bcl-ios_CONFIGURE_FLAGS = \
-       --with-monotouch=yes \
-       --with-monotouch_tv=yes \
-       --with-xammac=yes \
-       --with-monotouch_watch=yes
-
 bcl_CONFIGURE_FLAGS = \
-       $(if $(DISABLE_DESKTOP),--with-profile4_x=no,--with-profile4_x=yes) \
-       $(if $(DISABLE_ANDROID),,--with-monodroid=yes) \
-       $(if $(DISABLE_IOS),,$(bcl-ios_CONFIGURE_FLAGS)) \
-       $(if $(DISABLE_WASM),,--with-wasm=yes) \
-       --with-mcs-docs=no \
-       --disable-nls \
-       --disable-btls-lib \
-       --disable-support-build \
-       --disable-boehm
+	--with-mcs-docs=no \
+	--disable-nls \
+	--disable-btls-lib \
+	--disable-support-build \
+	--disable-boehm
 
 .stamp-bcl-configure: $(TOP)/configure
 	mkdir -p $(TOP)/sdks/builds/bcl
 	cd $(TOP)/sdks/builds/bcl && $(TOP)/configure $(bcl_CONFIGURE_FLAGS)
 	touch $@
 
-$(TOP)/sdks/out/bcl/monodroid $(TOP)/sdks/out/bcl/monotouch $(TOP)/sdks/out/bcl/wasm $(TOP)/sdks/out/bcl/net_4_x:
-	mkdir -p $@
+.PHONY: setup-custom-bcl
+setup-custom-bcl:
+	mkdir -p $(TOP)/sdks/out/bcl $(foreach profile,$(BCL_PROFILES),$(TOP)/sdks/out/bcl/$(profile))
+
+.PHONY: build-custom-bcl
+build-custom-bcl:
+	$(MAKE) -C bcl -C mono
+	$(MAKE) -C bcl -C runtime all-mcs build_profiles="$(BCL_PROFILES)"
 
 .PHONY: package-bcl
-package-bcl: | $(TOP)/sdks/out/bcl/net_4_x $(TOP)/sdks/out/bcl/monodroid $(TOP)/sdks/out/bcl/monotouch $(TOP)/sdks/out/bcl/wasm
-	if [ -d $(TOP)/mcs/class/lib/monodroid ]; then cp -R $(TOP)/mcs/class/lib/monodroid/* $(TOP)/sdks/out/bcl/monodroid; fi
-	if [ -d $(TOP)/mcs/class/lib/monotouch ]; then cp -R $(TOP)/mcs/class/lib/monotouch/* $(TOP)/sdks/out/bcl/monotouch; fi
-	if [ -d $(TOP)/mcs/class/lib/wasm ]; then cp -R $(TOP)/mcs/class/lib/wasm/* $(TOP)/sdks/out/bcl/wasm; fi
-	if [ -d $(TOP)/mcs/class/lib/net_4_x ]; then cp -R $(TOP)/mcs/class/lib/net_4_x/* $(TOP)/sdks/out/bcl/net_4_x; fi
+package-bcl:
+	$(foreach profile,$(BCL_PROFILES), \
+		cp -R $(TOP)/mcs/class/lib/$(profile)/* $(TOP)/sdks/out/bcl/$(profile);)
 
 .PHONY: clean-bcl
 clean-bcl:
