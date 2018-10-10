b3246bb91224dbc6ae5f264e7e0fb5161121a2d0
Martin Baulig <mabaul@microsoft.com>
2018-10-10 16:35:52 -0400

    Cleanup tests; include the source files to use the enum instead of magic integers.

--- mcs/class/corlib/Test/Mono/NativePlatformTest.cs
+++ mcs/class/corlib/Test/Mono/NativePlatformTest.cs
@@ -8,9 +8,6 @@ namespace MonoTests.Mono
 	[TestFixture]
 	public class NativePlatformTest
 	{
-		[DllImport ("System.Native")]
-		extern static int mono_native_get_platform_type ();
-
 		[TestFixtureSetUp]
 		public void SetUp ()
 		{
@@ -21,11 +18,11 @@ namespace MonoTests.Mono
 		[Test]
 		public void Test ()
 		{
-			var type = mono_native_get_platform_type ();
-			Assert.That (type, Is.GreaterThan (0), "platform type");
+			var type = MonoNativePlatform.GetPlatformType ();
+			Assert.That ((int)type, Is.GreaterThan (0), "platform type");
 
-			var usingCompat = (type & 16384) != 0; // MONO_NATIVE_PLATFORM_TYPE_COMPAT
-			Assert.AreEqual (usingCompat, MonoNativeConfig.UsingCompat, "using compatibility layer");
+			var usingCompat = (type & MonoNativePlatformType.MONO_NATIVE_PLATFORM_TYPE_COMPAT) != 0;
+			Assert.AreEqual (MonoNativeConfig.UsingCompat, usingCompat, "using compatibility layer");
 		}
 	}
 }
