2989a83f23bc46ce5d44486bc33e0dc6d627270d
Zoltan Varga <vargaz@gmail.com>
2018-10-15 04:50:54 +0200

    [sdks] Switch the invocation of the offsets too to 64 bit on osx. (#10903)

--- sdks/builds/runtime.mk
+++ sdks/builds/runtime.mk
@@ -139,8 +139,8 @@ _cross-runtime_$(1)_CONFIGURE_FLAGS= \
 
 $$(TOP)/sdks/builds/$(1)-$$(CONFIGURATION)/$(3).h: .stamp-$(1)-$$(CONFIGURATION)-configure $$(TOP)/tools/offsets-tool/MonoAotOffsetsDumper.exe | configure-$(4)
 	cd $$(TOP)/sdks/builds/$(1)-$$(CONFIGURATION) && \
-		MONO_PATH=$$(TOP)/tools/offsets-tool/CppSharp/$$(if $$(filter $$(UNAME),Darwin),osx_32,$$(if $$(filter $$(UNAME),Linux),linux_64,$$(error "Unknown UNAME='$$(UNAME)'"))) \
-			mono $$(if $$(filter $$(UNAME),Darwin),--arch=32) --debug "$$(TOP)/tools/offsets-tool/MonoAotOffsetsDumper.exe" \
+		MONO_PATH=$$(TOP)/tools/offsets-tool/CppSharp/$$(if $$(filter $$(UNAME),Darwin),osx_64,$$(if $$(filter $$(UNAME),Linux),linux_64,$$(error "Unknown UNAME='$$(UNAME)'"))) \
+			mono --debug "$$(TOP)/tools/offsets-tool/MonoAotOffsetsDumper.exe" \
 				--abi $(6) --outfile "$$@" --mono "$$(TOP)" --targetdir "$$(TOP)/sdks/builds/$(4)-$$(CONFIGURATION)" \
 					$$(_$(1)_OFFSETS_DUMPER_ARGS)
 
