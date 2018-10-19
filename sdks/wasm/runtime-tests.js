759154b449666e78e73ebf2a91670b1812d8c77f
Zoltan Varga <vargaz@gmail.com>
2018-10-19 21:36:01 +0200

    [wasm] Exit with an error code if the emscripten runtime hits an abort.

--- sdks/wasm/runtime-tests.js
+++ sdks/wasm/runtime-tests.js
@@ -85,6 +85,14 @@ var Module = {
 	print: function(x) { print ("WASM: " + x) },
 	printErr: function(x) { print ("WASM-ERR: " + x) },
 
+	onAbort: function(x) {
+		print ("ABORT: " + x);
+		var err = new Error();
+		print ("Stacktrace: \n");
+		print (err.stack);
+		wasm_exit (1);
+	},
+
 	onRuntimeInitialized: function () {
 		// Have to set env vars here to enable setting MONO_LOG_LEVEL etc.
 		var wasm_setenv = Module.cwrap ('mono_wasm_setenv', 'void', ['string', 'string']);
