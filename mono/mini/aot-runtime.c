mono/mono

commit 2b64076769354d449deb0b2a7a2b078d657baa83

[aot] Ensure shared got entries are initialized before loading methods (#11190)
Executing aot code that uses them would crash. Fixes random csc crashes.

mono/mini/aot-runtime.c
@@ -105,7 +105,7 @@ typedef struct MonoAotModule {
 	guint32 image_table_len;
 	gboolean out_of_date;
 	gboolean plt_inited;
-	gboolean got_initializing;
+	gboolean got_initialized;
 	guint8 *mem_begin;
 	guint8 *mem_end;
 	guint8 *jit_code_start;
@@ -1947,10 +1947,15 @@ init_amodule_got (MonoAotModule *amodule)
 	int i, npatches;
 
 	/* These can't be initialized in load_aot_module () */
-	if (amodule->shared_got [0] || amodule->got_initializing)
+	if (amodule->got_initialized)
 		return;
 
-	amodule->got_initializing = TRUE;
+	mono_loader_lock ();
+
+	if (amodule->got_initialized) {
+		mono_loader_unlock ();
+		return;
+	}
 
 	mp = mono_mempool_new ();
 	npatches = amodule->info.nshared_got_entries;
@@ -1996,6 +2001,10 @@ init_amodule_got (MonoAotModule *amodule)
 	}
 
 	mono_mempool_destroy (mp);
+
+	mono_memory_barrier ();
+	amodule->got_initialized = TRUE;
+	mono_loader_unlock ();
 }
 
 static void

commit c0fe724379084be07e0e0f66a6340167ebab8bcf

[aot] Reenable recursion checking when initing shared got entries (#11295)
[2018-06] [aot] Reenable recursion checking when initing shared got entries

Backport of #11258.

/cc @BrzVlad 

Description:
Broken in https://github.com/mono/mono/pull/11190
<!--
Thank you for your Pull Request!

If you are new to contributing to Mono, please try to do your best at conforming to our coding guidelines http://www.mono-project.com/community/contributing/coding-guidelines/ but don't worry if you get something wrong. One of the project members will help you to get things landed.

Does your pull request fix any of the existing issues? Please use the following format: Fixes #issue-number
-->

mono/mini/aot-runtime.c
@@ -86,6 +86,9 @@ typedef struct {
 	MonoJitInfo *jinfo;
 } JitInfoMap;
 
+#define GOT_INITIALIZING 1
+#define GOT_INITIALIZED  2
+
 typedef struct MonoAotModule {
 	char *aot_name;
 	/* Pointer to the Global Offset Table */
@@ -105,7 +108,7 @@ typedef struct MonoAotModule {
 	guint32 image_table_len;
 	gboolean out_of_date;
 	gboolean plt_inited;
-	gboolean got_initialized;
+	int got_initialized;
 	guint8 *mem_begin;
 	guint8 *mem_end;
 	guint8 *jit_code_start;
@@ -1942,15 +1945,22 @@ init_amodule_got (MonoAotModule *amodule)
 	int i, npatches;
 
 	/* These can't be initialized in load_aot_module () */
-	if (amodule->got_initialized)
+	if (amodule->got_initialized == GOT_INITIALIZED)
 		return;
 
 	mono_loader_lock ();
 
+	/*
+	 * If it is initialized some other thread did it in the meantime. If it is
+	 * initializing it means the current thread is initializing it since we are
+	 * holding the loader lock, skip it.
+	 */
 	if (amodule->got_initialized) {
 		mono_loader_unlock ();
 		return;
 	}
+
+	amodule->got_initialized = GOT_INITIALIZING;
 
 	mp = mono_mempool_new ();
 	npatches = amodule->info.nshared_got_entries;
@@ -1998,7 +2008,7 @@ init_amodule_got (MonoAotModule *amodule)
 	mono_mempool_destroy (mp);
 
 	mono_memory_barrier ();
-	amodule->got_initialized = TRUE;
+	amodule->got_initialized = GOT_INITIALIZED;
 	mono_loader_unlock ();
 }
 