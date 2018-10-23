fa51e8ce18ffff0ec224d568ecc6544b8aace10b
Alexander Kyte <alkyte@microsoft.com>
2018-10-23 17:21:35 -0400

    [runtime] Add microbenchmarks to CI script

---
+++ scripts/ci/run-test-microbench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+export TESTCMD=`dirname "${BASH_SOURCE[0]}"`/run-step.sh
+
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-BinaryTrees
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-Fannkuchredux
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-Fasta
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-NBody
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-Mandelbrot
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-RegexRedux
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-RevComp
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-SpectralNorm
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-KNucleotide
