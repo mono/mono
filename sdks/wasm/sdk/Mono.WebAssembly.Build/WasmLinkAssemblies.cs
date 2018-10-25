834d1083cfcfc2800210364a331b1fc1620c8c57
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-10-25 19:06:49 -0400

    [Wasm] Start fleshing out wasm linker task

--- sdks/wasm/sdk/Mono.WebAssembly.Build/WasmLinkAssemblies.cs
+++ sdks/wasm/sdk/Mono.WebAssembly.Build/WasmLinkAssemblies.cs
@@ -5,52 +5,107 @@
 using System;
 using System.IO;
 using System.Text;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace Mono.WebAssembly.Build
 {
-    class WasmLinkAssemblies : ToolTask
-    {
-        public TaskItem[] Assemblies { get; set; }
-        public string FrameworkDir { get; set; }
-        public string OutputDir { get; set; }
+	class WasmLinkAssemblies : ToolTask
+	{
+		/// <summary>
+		/// The application assemblies to be linked.
+		/// </summary>
+		[Required]
+		public ITaskItem[] Assemblies { get; set; }
 
-        protected override string ToolName => "monolinker";
+		/// <summary>
+		/// The directory containing the framework assemblies.
+		/// </summary>
+		[Required]
+		public string FrameworkDir { get; set; }
 
-        protected override string GenerateFullPathToTool()
-        {
-            var dir = Path.GetDirectoryName(GetType().Assembly.Location);
-            return Path.Combine(dir, "monolinker.exe");
-        }
+		/// <summary>
+		/// The directory into which to output the linked assemblies.
+		/// </summary>
+		[Required]
+		public string OutputDir { get; set; }
 
-        protected override bool ValidateParameters()
-        {
-            if (string.IsNullOrEmpty(OutputDir))
-            {
-                Log.LogError("OutputDir is required");
-                return false;
-            }
+		/// <summary>
+		/// Controls which kinds of assemblies are linked.
+		/// </summary>
+		public LinkMode LinkMode { get; set; }
 
-            if (string.IsNullOrEmpty(FrameworkDir))
-            {
-                Log.LogError("FrameworkDir is required");
-                return false;
-            }
+		/// <summary>
+		/// Semicolon separated list of assembly names that should not be linked.
+		/// </summary>
+		public string LinkSkip { get; set; }
 
-            return base.ValidateParameters();
-        }
+		/// <summary>
+		/// Whether to include debug information
+		/// </summary>
+		public bool Debug { get; set; }
 
-        protected override string GenerateCommandLineCommands()
-        {
-            var sb = new StringBuilder();
+		protected override string ToolName => "monolinker";
 
-            sb.Append(" -verbose");
-            sb.Append(" -c copyused");
-            sb.Append(" -u copy");
-            sb.AppendFormat(" -out \"{0}\"", OutputDir);
-            sb.AppendFormat(" -d \"{0}\"", FrameworkDir);
+		protected override string GenerateFullPathToTool ()
+		{
+			var dir = Path.GetDirectoryName (GetType ().Assembly.Location);
+			return Path.Combine (dir, "monolinker.exe");
+		}
 
-            return sb.ToString();
-        }
-    }
+		protected override bool ValidateParameters ()
+		{
+			if (string.IsNullOrEmpty (OutputDir)) {
+				Log.LogError ("OutputDir is required");
+				return false;
+			}
+
+			if (string.IsNullOrEmpty (FrameworkDir)) {
+				Log.LogError ("FrameworkDir is required");
+				return false;
+			}
+
+			return base.ValidateParameters ();
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			var sb = new StringBuilder ();
+
+			sb.Append (" -verbose");
+
+			switch (LinkMode) {
+			case LinkMode.None:
+				sb.Append (" -c copyused -u copy");
+				break;
+			case LinkMode.SdkOnly:
+				sb.Append (" -c link -u copy");
+				break;
+			case LinkMode.Full:
+				sb.Append (" -c link -u link");
+				break;
+			}
+
+			var skips = LinkSkip.Split (new[] { ';', ',', ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			foreach (var s in skips) {
+				sb.AppendFormat (" -p {0} copy", s);
+			}
+
+			sb.AppendFormat (" -out \"{0}\"", OutputDir);
+			sb.AppendFormat (" -d \"{0}\"", FrameworkDir);
+
+			if (Debug) {
+				sb.Append (" -b -v");
+			}
+
+			return sb.ToString ();
+		}
+	}
+
+	public enum LinkMode
+	{
+		None,
+		SdkOnly,
+		Full
+	}
 }
