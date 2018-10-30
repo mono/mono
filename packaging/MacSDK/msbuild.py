f038e7cd8a4edcfc9ebbfb8dfd0b595fdcaada15
Ankit Jain <radical@gmail.com>
2018-10-30 05:28:53 -0700

    Bump msbuild to track xplat-master (#11440)
    - pickup fix for https://github.com/mono/mono/issues/11214 .

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '105cb8a15778bf32efa730b5125d1503e5b44629')
+			revision = 'c25860499b19599171cf6fe7b74dc0119b1e5b40')
 
 	def build (self):
 		self.sh ('./build.sh -hostType mono -configuration Release -skipTests')
