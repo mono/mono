mono/mono

commit e8b037642104527bd9b9ba70d502210b9c12d2b8

Stack-based icall parameter handles and only one managed/native transition per icall. (#11294)
Handles for icall parameters allocated in managed (but not native), can just be pointers to locals or parameters.

Currently the managed stack is scanned conservatively.
When this changes, there will be metadata provided to find these, so this approach will just keep working and should still be ideal (aside from nohandles or staying in managed).

For ObjIn, use pointer to parameter.
For ObjOut/ObjInOut, the parameter might, though it is far fetched, point to a native frame, which isn't convincing. Therefore for those, use a pointer to a managed volatile local, closer to how handles looked like.
For ValueTypeRef, make the parameter volatile so it is sure to survive.

We use volatile instead of merely address-taken because 1. a hypothetical aggressive whole program compiler/optimizer could see through address-taken (runtime and wrappers and corlib compiled together).  2. ValuetypeRef is even easier to optimize/break because the address-taken is  dead.

FIXME: Tests that exercise every icall, coop and otherwise.
FIXME: Gcstress.

Collateral damage: The one rarely run assert that handles are not out of scope
cannot be so easily implemented so is removed. It might come back though, if we type tag the handles.

Furthermore, the handle stack setup/cleanup can be done in native instead of managed.
Furthermore, the error check can either be done in native instead of managed, or the fast path inlined in the JIT. Here we just move it to native (and inline the fast path in native).

Therefore coop icalls go from 3 + handle-count managed/native transitions, to just always one,  *and* at least some of their handle allocation is exceedingly fast and free of TLS.

The typed handle wrappers change from returning a handle to returning a raw pointer.
Because previously the handle would be allocated in the handle frame setup by the JIT.

mono/metadata/method-builder-ilgen.c
@@ -141,6 +141,10 @@ create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int 
 	header->code_size = mb->pos;
 	header->num_locals = mb->locals;
 	header->init_locals = mb->init_locals;
+	header->volatile_args = mb->volatile_args;
+	header->volatile_locals = mb->volatile_locals;
+	mb->volatile_args = NULL;
+	mb->volatile_locals = NULL;
 
 	header->num_clauses = mb->num_clauses;
 	header->clauses = mb->clauses;

mono/mini/mini.c
@@ -2010,6 +2010,12 @@ mono_add_var_location (MonoCompile *cfg, MonoInst *var, gboolean is_reg, int reg
 }
 
 static void
+mono_apply_volatile (MonoInst *inst, MonoBitSet *set, gsize index)
+{
+	inst->flags |= mono_bitset_test_safe (set, index) ? MONO_INST_VOLATILE : 0;
+}
+
+static void
 mono_compile_create_vars (MonoCompile *cfg)
 {
 	MonoMethodSignature *sig;
@@ -2031,12 +2037,16 @@ mono_compile_create_vars (MonoCompile *cfg)
 	cfg->args = (MonoInst **)mono_mempool_alloc0 (cfg->mempool, (sig->param_count + sig->hasthis) * sizeof (MonoInst*));
 
 	if (sig->hasthis) {
-		cfg->args [0] = mono_compile_create_var (cfg, m_class_get_this_arg (cfg->method->klass), OP_ARG);
-		cfg->this_arg = cfg->args [0];
+		MonoInst* arg = mono_compile_create_var (cfg, m_class_get_this_arg (cfg->method->klass), OP_ARG);
+		mono_apply_volatile (arg, header->volatile_args, 0);
+		cfg->args [0] = arg;
+		cfg->this_arg = arg;
 	}
 
 	for (i = 0; i < sig->param_count; ++i) {
-		cfg->args [i + sig->hasthis] = mono_compile_create_var (cfg, sig->params [i], OP_ARG);
+		MonoInst* arg = mono_compile_create_var (cfg, sig->params [i], OP_ARG);
+		mono_apply_volatile (arg, header->volatile_args, i + sig->hasthis);
+		cfg->args [i + sig->hasthis] = arg;
 	}
 
 	if (cfg->verbose_level > 2) {
@@ -2066,7 +2076,14 @@ mono_compile_create_vars (MonoCompile *cfg)
 		if (cfg->verbose_level > 2)
 			g_print ("\tlocal [%d]: ", i);
 		cfg->locals [i] = mono_compile_create_var (cfg, header->locals [i], OP_LOCAL);
+		mono_apply_volatile (cfg->locals [i], header->volatile_locals, i);
 	}
+
+	mono_bitset_free (header->volatile_locals);
+	header->volatile_locals = NULL;
+
+	mono_bitset_free (header->volatile_args);
+	header->volatile_args = NULL;
 
 	if (cfg->verbose_level > 2)
 		g_print ("locals done\n");

mcs/class/corlib/LinkerDescriptor/mscorlib.xml
@@ -808,9 +808,7 @@
 		-->
 		<type fullname="Mono.Runtime" />
 
-		<!-- marshal.c (mono_marshal_get_native_wrapper) -->
-		<type fullname="Mono.RuntimeStructs/HandleStackMark" />
-		<!-- marshal.c (mono_marshal_get_native_wrapper) -->
+		<!-- marshal-ilgen.c (emit_native_icall_wrapper_ilgen) -->
 		<type fullname="Mono.RuntimeStructs/MonoError" />
 
 		<type fullname="System.Threading.WasmRuntime"/>

mcs/class/corlib/Mono/RuntimeStructs.cs
@@ -49,12 +49,6 @@ namespace Mono {
 			internal int len;
 		}
 
-		// handle.h HandleStackMark
-		struct HandleStackMark {
-			int size, interior_size;
-			IntPtr chunk;
-		}
-
 		// mono-error.h MonoError
 		struct MonoError {
 			ushort error_code;

mono/eglib/glib.h
@@ -80,7 +80,7 @@ struct g_cast
 private:
 	void * const x;
 public:
-	explicit g_cast (void *y) : x(y) { }
+	explicit g_cast (void volatile *y) : x((void*)y) { }
 	// Lack of rvalue constructor inhibits ternary operator.
 	// Either don't use ternary, or cast each side.
 	// sa = (salen <= 128) ? g_alloca (salen) : g_malloc (salen);

mono/metadata/exception.c
@@ -1209,7 +1209,7 @@ mono_error_raise_exception_deprecated (MonoError *target_error)
 }
 
 /**
- * mono_error_set_pending_exception:
+ * mono_error_set_pending_exception_slow:
  * \param error The error
  * If \p error is set, convert it to an exception and set the pending exception for the current icall.
  * \returns TRUE if \p error was set, or FALSE otherwise, so that you can write:
@@ -1218,8 +1218,9 @@ mono_error_raise_exception_deprecated (MonoError *target_error)
  *      return;
  *    }
  */
+// For efficiency, call mono_error_set_pending_exception instead of mono_error_set_pending_exception_slow.
 gboolean
-mono_error_set_pending_exception (MonoError *error)
+mono_error_set_pending_exception_slow (MonoError *error)
 {
 	if (is_ok (error))
 		return FALSE;

mono/metadata/handle-decl.h
@@ -68,14 +68,14 @@ Handle macros/functions
 			MONO_ALWAYS_INLINE					\
 			TYPE * GetRaw () { return __raw ? *__raw : NULL; }	\
 		)								\
-		TYPE **__raw;							\
+		TYPE * volatile *__raw;						\
 	} TYPED_HANDLE_NAME (TYPE),						\
 	  TYPED_OUT_HANDLE_NAME (TYPE),						\
 	  TYPED_IN_OUT_HANDLE_NAME (TYPE);					\
 /* Do not call these functions directly. Use MONO_HANDLE_NEW and MONO_HANDLE_CAST. */ \
 /* Another way to do this involved casting mono_handle_new function to a different type. */ \
 static inline MONO_ALWAYS_INLINE TYPED_HANDLE_NAME (TYPE) 	\
-MONO_HANDLE_CAST_FOR (TYPE) (gpointer a)			\
+MONO_HANDLE_CAST_FOR (TYPE) (MonoRawHandle a)			\
 {								\
 	TYPED_HANDLE_NAME (TYPE) b = { (TYPE**)a };		\
 	return b;						\

mono/metadata/handle.c
@@ -104,36 +104,6 @@ chunk_element (HandleChunk *chunk, int idx)
 	return &chunk->elems[idx];
 }
 
-static HandleChunkElem*
-handle_to_chunk_element (MonoObjectHandle o)
-{
-	return (HandleChunkElem*)o.__raw;
-}
-
-/* Given a HandleChunkElem* search through the current handle stack to find its chunk and offset. */
-static HandleChunk*
-chunk_element_to_chunk_idx (HandleStack *stack, HandleChunkElem *elem, int *out_idx)
-{
-	HandleChunk *top = stack->top;
-	HandleChunk *cur = stack->bottom;
-
-	*out_idx = 0;
-
-	while (cur != NULL) {
-		HandleChunkElem *front = &cur->elems [0];
-		HandleChunkElem *back = &cur->elems [cur->size];
-
-		if (front <= elem && elem < back) {
-			*out_idx = (int)(elem - front);
-			return cur;
-		}
-
-		if (cur == top)
-			break; /* didn't find it. */
-		cur = cur->next;
-	}
-	return NULL;
-}
 
 #ifdef MONO_HANDLE_TRACK_OWNER
 #ifdef HAVE_BACKTRACE_SYMBOLS
@@ -240,54 +210,16 @@ retry:
 	goto retry;
 }
 
-gpointer
-#ifndef MONO_HANDLE_TRACK_OWNER
-mono_handle_new_interior (gpointer rawptr)
-#else
-mono_handle_new_interior (gpointer rawptr, const char *owner)
-#endif
-{
-	MonoThreadInfo *info = mono_thread_info_current ();
-	HandleStack *handles = info->handle_stack;
-	HandleChunk *top = handles->interior;
-#ifdef MONO_HANDLE_TRACK_SP
-	mono_handle_chunk_leak_check (handles);
-#endif
-
-	g_assert (top);
-
-	/*
-	 * Don't extend the chunk now, interior handles are
-	 * only used for icall arguments, they shouldn't
-	 * overflow.
-	 */
-	g_assert (top->size < OBJECTS_PER_HANDLES_CHUNK);
-	int idx = top->size;
-	gpointer *objslot = &top->elems [idx].o;
-	*objslot = NULL;
-	mono_memory_write_barrier ();
-	top->size++;
-	mono_memory_write_barrier ();
-	*objslot = rawptr;
-	SET_OWNER (top,idx);
-	SET_SP (handles, top, idx);
-	return objslot;
-}
-
 HandleStack*
 mono_handle_stack_alloc (void)
 {
 	HandleStack *stack = new_handle_stack ();
 	HandleChunk *chunk = new_handle_chunk ();
-	HandleChunk *interior = new_handle_chunk ();
 
 	chunk->prev = chunk->next = NULL;
 	chunk->size = 0;
-	interior->prev = interior->next = NULL;
-	interior->size = 0;
 	mono_memory_write_barrier ();
 	stack->top = stack->bottom = chunk;
-	stack->interior = interior;
 #ifdef MONO_HANDLE_TRACK_SP
 	stack->stackmark_sp = NULL;
 #endif
@@ -308,7 +240,6 @@ mono_handle_stack_free (HandleStack *stack)
 		c = next;
 	}
 	free_handle_chunk (c);
-	free_handle_chunk (stack->interior);
 	free_handle_stack (stack);
 }
 
@@ -340,10 +271,6 @@ mono_handle_stack_free_domain (HandleStack *stack, MonoDomain *domain)
 			break;
 		cur = cur->next;
 	}
-	/* We don't examine the interior pointers here because the GC treats
-	 * them conservatively and anyway we don't have enough information here to
-	 * find the object's vtable.
-	 */
 }
 
 static void
@@ -385,39 +312,29 @@ mono_handle_stack_scan (HandleStack *stack, GcScanFunc func, gpointer gc_data, g
 		check_handle_stack_monotonic (stack);
 
 	/*
-	  We're called twice - on the imprecise pass we call func to pin the
-	  objects where the handle points to its interior.  On the precise
-	  pass, we scan all the objects where the handles point to the start of
+	  We're called twice - on the imprecise pass we do nothing.
+	  Interior pointers are retained in managed frames.
+	  On the precise pass, we scan all the objects where the handles point to the start of
 	  the object.
 
 	  Note that if we're running, we know the world is stopped.
 	*/
-	if (precise) {
-		HandleChunk *cur = stack->bottom;
-		HandleChunk *last = stack->top;
+	if (!precise)
+		return;
 
-		while (cur) {
-			for (int i = 0; i < cur->size; ++i) {
-				HandleChunkElem* elem = chunk_element (cur, i);
-				gpointer* obj_slot = &elem->o;
-				if (*obj_slot != NULL)
-					func (obj_slot, gc_data);
-			}
-			if (cur == last)
-				break;
-			cur = cur->next;
-		}
-	} else {
-		HandleChunk *cur = stack->interior;
+	HandleChunk *cur = stack->bottom;
+	HandleChunk *last = stack->top;
 
-		if (!cur)
-			return;
+	while (cur) {
 		for (int i = 0; i < cur->size; ++i) {
 			HandleChunkElem* elem = chunk_element (cur, i);
-			gpointer* ptr_slot = &elem->o;
-			if (*ptr_slot != NULL)
-				func (ptr_slot, gc_data);
+			gpointer* obj_slot = &elem->o;
+			if (*obj_slot != NULL)
+				func (obj_slot, gc_data);
 		}
+		if (cur == last)
+			break;
+		cur = cur->next;
 	}
 }
 
@@ -486,15 +403,12 @@ mono_array_handle_length (MonoArrayHandle arr)
 uint32_t
 mono_gchandle_from_handle (MonoObjectHandle handle, mono_bool pinned)
 {
-	/* FIXME: chunk_element_to_chunk_idx does a linear search through the
-	 * chunks and we only need it for the assert */
-	MonoThreadInfo *info = mono_thread_info_current ();
-	HandleStack *stack = info->handle_stack;
-	HandleChunkElem* elem = handle_to_chunk_element (handle);
-	int elem_idx = 0;
-	HandleChunk *chunk = chunk_element_to_chunk_idx (stack, elem, &elem_idx);
-	/* gchandles cannot deal with interior pointers */
-	g_assert (chunk != NULL);
+	// FIXME This used to check for out of scope handles.
+	// Stack-based handles coming from icall wrappers do not
+	// work with that. This functionality can be largely restored
+	// by introducing a tag bit in handles -- 0 for managed stack-based,
+	// 1 for native TLS-based, having MONO_HANDLE_RAW clear it, and only
+	// doing the former checking for native TLS-based handles.
 	return mono_gchandle_new_internal (MONO_HANDLE_RAW (handle), pinned);
 }
 

commit 9e2106451332aa9f829b269a01785f71dd2df846

Copy method_builder->volatile_args and method_builder->volatile_locals to MonoMethodHeader in order to get its lifetime.
Alernative to https://github.com/mono/mono/pull/11490 though maybe
https://github.com/mono/mono/pull/11490 is simpler and faster.

Fixes https://github.com/mono/mono/issues/11488.
My regression from https://github.com/mono/mono/pull/11294.

mono/metadata/method-builder-ilgen.c
@@ -72,6 +72,25 @@ free_ilgen (MonoMethodBuilder *mb)
 	g_free (mb);
 }
 
+static gsize
+mono_bitset_duplicate_size (MonoBitSet *set)
+{
+	return set ? mono_bitset_alloc_size (mono_bitset_size (set), 0) : 0;
+}
+
+static void
+mono_bitset_move (MonoBitSet **dest, MonoBitSet **source, char **end)
+{
+	if (!*source)
+		return;
+	const gsize size = mono_bitset_duplicate_size (*source);
+	*dest = (MonoBitSet*)*end;
+	*end += size;
+	memcpy (*dest, *source, size);
+	mono_bitset_free (*source);
+	*source = NULL;
+}
+
 static MonoMethod *
 create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int max_stack)
 {
@@ -86,6 +105,11 @@ create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int 
 
 	image = m_class_get_image (mb->method->klass);
 
+	const gsize size_volatile_args = mono_bitset_duplicate_size (mb->volatile_args);
+	const gsize size_volatile_locals = mono_bitset_duplicate_size (mb->volatile_locals);
+	const gsize header_size1 = MONO_SIZEOF_METHOD_HEADER + mb->locals * sizeof (MonoType *);
+	const gsize header_size = header_size1 + size_volatile_args + size_volatile_locals;
+
 	if (mb->dynamic) {
 		method = mb->method;
 		mw = (MonoMethodWrapper*)method;
@@ -93,8 +117,7 @@ create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int 
 		method->name = mb->name;
 		method->dynamic = TRUE;
 
-		mw->header = header = (MonoMethodHeader *) 
-			g_malloc0 (MONO_SIZEOF_METHOD_HEADER + mb->locals * sizeof (MonoType *));
+		mw->header = header = (MonoMethodHeader *)g_malloc0 (header_size);
 
 		header->code = mb->code;
 
@@ -114,8 +137,7 @@ create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int 
 		else
 			method->name = mono_image_strdup (image, mb->name);
 
-		mw->header = header = (MonoMethodHeader *) 
-			mono_image_alloc0 (image, MONO_SIZEOF_METHOD_HEADER + mb->locals * sizeof (MonoType *));
+		mw->header = header = (MonoMethodHeader *)mono_image_alloc0 (image, header_size);
 
 		header->code = (const unsigned char *)mono_image_alloc (image, mb->pos);
 		memcpy ((char*)header->code, mb->code, mb->pos);
@@ -137,6 +159,10 @@ create_method_ilgen (MonoMethodBuilder *mb, MonoMethodSignature *signature, int 
 		max_stack = 8;
 
 	header->max_stack = max_stack;
+
+	char *end = ((char*)header) + header_size1;
+	mono_bitset_move (&header->volatile_args, &mb->volatile_args, &end);
+	mono_bitset_move (&header->volatile_locals, &mb->volatile_locals, &end);
 
 	header->code_size = mb->pos;
 	header->num_locals = mb->locals;

mono/mini/mini.c
@@ -2079,12 +2079,6 @@ mono_compile_create_vars (MonoCompile *cfg)
 		mono_apply_volatile (cfg->locals [i], header->volatile_locals, i);
 	}
 
-	mono_bitset_free (header->volatile_locals);
-	header->volatile_locals = NULL;
-
-	mono_bitset_free (header->volatile_args);
-	header->volatile_args = NULL;
-
 	if (cfg->verbose_level > 2)
 		g_print ("locals done\n");
 