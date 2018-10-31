cde0dab5ca997e798d8ac13de99011d5d4b14da1
Bernhard Urban <lewurm@gmail.com>
2018-10-31 15:57:26 +0100

    use mono_bitset_alloc_size to determine size

--- mono/metadata/marshal-ilgen.c
+++ mono/metadata/marshal-ilgen.c
@@ -6375,7 +6375,7 @@ emit_native_icall_wrapper_ilgen (MonoMethodBuilder *mb, MonoMethod *method, Mono
 					local = mono_mb_add_local (mb, mono_get_object_type ());
 
 					if (!mb->volatile_locals) {
-						char *mem = (char *)mono_image_alloc0 (get_method_image (method), csig->param_count + 1);
+						char *mem = (char *)mono_image_alloc0 (get_method_image (method), mono_bitset_alloc_size (csig->param_count + 1));
 						mb->volatile_locals = mono_bitset_mem_new (mem, csig->param_count + 1, 0);
 					}
 					mono_bitset_set (mb->volatile_locals, local);
@@ -6383,7 +6383,7 @@ emit_native_icall_wrapper_ilgen (MonoMethodBuilder *mb, MonoMethod *method, Mono
 				case ICALL_HANDLES_WRAP_VALUETYPE_REF:
 				case ICALL_HANDLES_WRAP_OBJ:
 					if (!mb->volatile_args) {
-						char *mem = (char *)mono_image_alloc0 (get_method_image (method), csig->param_count + 1);
+						char *mem = (char *)mono_image_alloc0 (get_method_image (method), mono_bitset_alloc_size (csig->param_count + 1));
 						mb->volatile_args = mono_bitset_mem_new (mem, csig->param_count + 1, 0);
 					}
 					mono_bitset_set (mb->volatile_args, i);
