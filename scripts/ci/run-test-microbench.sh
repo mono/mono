1191effa327f687883104b5b4823d4dbdf10a0ff
Alexander Kyte <alkyte@microsoft.com>
2018-11-13 17:36:46 -0500

    [bench] Don't penalize first benchmark with AOT time.

--- scripts/ci/run-test-microbench.sh
+++ scripts/ci/run-test-microbench.sh
@@ -2,6 +2,7 @@
 
 export TESTCMD=`dirname "${BASH_SOURCE[0]}"`/run-step.sh
 
+${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks DebianShootoutMono.stamp
 ${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-BinaryTrees
 ${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-Fannkuchredux
 ${TESTCMD} --label=microbenchmark --timeout=40m make -C mono/tests/microbenchmarks test-Fasta
