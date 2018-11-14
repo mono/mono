930fa40957a9156c12dd02e82923f9b4bc7b6443
Zoltan Varga <vargaz@gmail.com>
2018-11-14 00:25:39 -0500

    [wasm] Ongoing AOT work. (#11637)
    [wasm] Ongoing AOT work.

--- sdks/builds/wasm.mk
+++ sdks/builds/wasm.mk
@@ -114,6 +114,8 @@ wasm_TARGETS += wasm-$(1)-$$(CONFIGURATION) $(5)
 endef
 
 $(eval $(call WasmCrossTemplate,cross,i686,wasm32,wasm-runtime,llvm-llvm32,wasm32-unknown-unknown))
+# 64->32 bit cross compiler
+$(eval $(call WasmCrossTemplate,cross-64,x86_64,wasm32,wasm-runtime,llvm-llvm64,wasm32-unknown-unknown))
 
 ##
 # Parameters
