5f6a28ee65f196a0e3440a8171a551fa97a833e7
Alexander Kyte <alkyte@microsoft.com>
2018-11-15 18:42:57 -0500

    [bench] Add reporting logic for perf

--- acceptance-tests/microbench.mk
+++ acceptance-tests/microbench.mk
@@ -25,21 +25,52 @@ run-microbench-$(1):: DebianShootoutMono.stamp
 test-run-microbench:: run-microbench-$(1)
 
 if HOST_LINUX
-run-microbench-profiled-$(1):: DebianShootoutMono.stamp
+run-microbench-profiled-$(1):: microbench-results/$(1).perf.data 
+
+microbench-results/$(1).perf.data: DebianShootoutMono.stamp 
+	mkdir -p microbench-results
 	MONO_BENCH_EXECUTABLE="$(PERF_RUNTIME)" \
 	MONO_BENCH_AOT_RUN="$(AOT_RUN_FLAGS)"\
 	MONO_BENCH_AOT_BUILD="$(AOT_BUILD_FLAGS)"\
 	MONO_BENCH_PATH="$(abs_top_srcdir)/mcs/class/lib/$(TEST_PROFILE)" \
 	$(NET_4_X_RUNTIME) \
 	$(TEST_EXE_PATH)/DebianShootoutMono.exe $(1)
-	perf script > $(1).out.perf
-	cat $(1).out.perf | ./DebianShootoutMono/external/FlameGraph/stackcollapse-perf.pl > $(1).out.perf-folded
-	./external/DebianShootoutMono/external/FlameGraph/flamegraph.pl $(1).out.perf-folded > $(1).out.perf-flame.svg
+	mv perf.data microbench-results/$(1).perf.data
+
+microbench-results/$(1).tmp.perf: microbench-results/$(1).perf.data
+	perf script -i microbench-results/$(1).perf.data > microbench-results/$(1).tmp.perf
+
+microbench-results/$(1).perf-flame.svg: microbench-results/$(1).tmp.perf
+	cat microbench-results/$(1).tmp.perf | ./external/DebianShootoutMono/external/FlameGraph/stackcollapse-perf.pl > microbench-results/$(1).perf-folded
+	./external/DebianShootoutMono/external/FlameGraph/flamegraph.pl microbench-results/$(1).perf-folded > microbench-results/$(1).perf-flame.svg
+	rm microbench-results/$(1).tmp.perf
+	rm microbench-results/$(1).perf-folded
+
+MONO_PERF_FLAGS=--show-cpu-utilization -n --hierarchy -T $(MONO_PERF_ADDITIONAL_FLAGS)
+
+microbench-results/$(1).perf.report: microbench-results/$(1).perf.data
+	perf report -i microbench-results/$(1).perf.data $(MONO_PERF_FLAGS) > microbench-results/$(1).perf.report 
 
 test-run-microbench-profiled:: run-microbench-profiled-$(1)
+
+test-run-microbench-publish-collect:: microbench-results/$(1).perf.data microbench-results/$(1).perf.report microbench-results/$(1).perf-flame.svg
+
 endif
 
 endef
+
+if HOST_LINUX
+
+microbench-results/perf-data.zip:
+	zip microbench-results/perf-data.zip microbench-results/*.perf.data
+	rm microbench-results/*.perf.data
+
+perf-report: microbench-results/perf-data.zip
+
+perf-report-total: test-run-microbench-publish-collect
+	@$(MAKE) perf-report
+
+endif
 
 .PHONY: prepare-dlls
 
@@ -62,5 +93,4 @@ $(eval $(call BenchmarkDotNetTemplate,Fannkuchredux))
 $(eval $(call BenchmarkDotNetTemplate,Fasta))
 $(eval $(call BenchmarkDotNetTemplate,KNucleotide))
 $(eval $(call BenchmarkDotNetTemplate,RevComp))
-
 
