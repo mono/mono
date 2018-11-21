a0f714fb917c6ebf8f6edbd5b092d3ca7caee885
Alexander Kyte <alkyte@microsoft.com>
2018-11-20 23:19:33 -0500

    [bench] Fix jenkins labels

--- scripts/ci/run-test-microbench.sh
+++ scripts/ci/run-test-microbench.sh
@@ -5,34 +5,34 @@ export TESTCMD=`dirname "${BASH_SOURCE[0]}"`/run-step.sh
 ${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests DebianShootoutMono.stamp
 
 if [ -z "$MONO_BENCH_GIST_URL"]; then
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-BinaryTrees
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-Fannkuchredux
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-Fasta
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-NBody
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-Mandelbrot
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-RegexRedux
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-RevComp
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-SpectralNorm
-	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-KNucleotide
+	${TESTCMD} --label=microbenchmark-BinaryTrees --timeout=40m make -C acceptance-tests run-microbench-BinaryTrees
+	${TESTCMD} --label=microbenchmark-Fannkuchredux --timeout=40m make -C acceptance-tests run-microbench-Fannkuchredux
+	${TESTCMD} --label=microbenchmark-Fasta --timeout=40m make -C acceptance-tests run-microbench-Fasta
+	${TESTCMD} --label=microbenchmark-NBody --timeout=40m make -C acceptance-tests run-microbench-NBody
+	${TESTCMD} --label=microbenchmark-Mandelbrot --timeout=40m make -C acceptance-tests run-microbench-Mandelbrot
+	${TESTCMD} --label=microbenchmark-RegexRedux --timeout=40m make -C acceptance-tests run-microbench-RegexRedux
+	${TESTCMD} --label=microbenchmark-RevComp --timeout=40m make -C acceptance-tests run-microbench-RevComp
+	${TESTCMD} --label=microbenchmark-SpectralNorm --timeout=40m make -C acceptance-tests run-microbench-SpectralNorm
+	${TESTCMD} --label=microbenchmark-KNucleotide --timeout=40m make -C acceptance-tests run-microbench-KNucleotide
 else
 	${TESTCMD} --label=microbenchmark --timeout=40m make -C acceptance-tests run-microbench-GistBenchmark
 fi
 
 if [[ ${CI_TAGS} == *'linux-'* ]]; then
-	${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests test-run-microbench-perf-check
+	${TESTCMD} --label=microbench-profiler-check --timeout=40m make -C acceptance-tests test-run-microbench-perf-check
 
 	if [ -z "$MONO_BENCH_GIST_URL"]; then
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-BinaryTrees
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-Fannkuchredux
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-Fasta
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-NBody
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-Mandelbrot
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-RegexRedux
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-RevComp
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-SpectralNorm
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-KNucleotide
+		${TESTCMD} --label=microbench-profiled-BinaryTrees   --timeout=40m make -C acceptance-tests run-microbench-profiled-BinaryTrees
+		${TESTCMD} --label=microbench-profiled-Fannkuchredux --timeout=40m make -C acceptance-tests run-microbench-profiled-Fannkuchredux
+		${TESTCMD} --label=microbench-profiled-Fasta         --timeout=40m make -C acceptance-tests run-microbench-profiled-Fasta
+		${TESTCMD} --label=microbench-profiled-NBody         --timeout=40m make -C acceptance-tests run-microbench-profiled-NBody
+		${TESTCMD} --label=microbench-profiled-Mandelbrot    --timeout=40m make -C acceptance-tests run-microbench-profiled-Mandelbrot
+		${TESTCMD} --label=microbench-profiled-RegexRedux    --timeout=40m make -C acceptance-tests run-microbench-profiled-RegexRedux
+		${TESTCMD} --label=microbench-profiled-RevComp       --timeout=40m make -C acceptance-tests run-microbench-profiled-RevComp
+		${TESTCMD} --label=microbench-profiled-SpectralNorm  --timeout=40m make -C acceptance-tests run-microbench-profiled-SpectralNorm
+		${TESTCMD} --label=microbench-profiled-KNucleotide   --timeout=40m make -C acceptance-tests run-microbench-profiled-KNucleotide
 	else
-		${TESTCMD} --label=microbench-profiled --timeout=40m make -C acceptance-tests run-microbench-profiled-GistBenchmark
+		${TESTCMD} --label=microbench-profiled-Gist --timeout=40m make -C acceptance-tests run-microbench-profiled-GistBenchmark
 	fi
 
 	${TESTCMD} --label=microbench-report --timeout=40m make -C acceptance-tests perf-report
