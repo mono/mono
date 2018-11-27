mono/mono

commit e15b8c53ffc24d2c0cc1438498c23c6461d0c6e0

[runtime] Integrate BenchmarkDotNet for in-tree microbenchmarks (#9833)
Don't merge, using my fork of the BenchmarkDotNet repo so I can use a not-yet-merged hack to fix the build.

mcs/build/rules.make
@@ -175,6 +175,9 @@ ifneq ("$(wildcard $(topdir)/class/lib/$(PROFILE))","")
 ifdef ALWAYS_AOT_BCL
 AOT_PROFILE_ASSEMBLIES := $(sort $(patsubst .//%,%,$(filter-out %.dll.dll %.exe.dll %bare% %plaincore% %secxml% %Facades% %ilasm%,$(filter %.dll %.exe,$(wildcard $(topdir)/class/lib/$(PROFILE)/*)))))
 AOT_PROFILE_ASSEMBLIES_OUT := $(patsubst %,%$(PLATFORM_AOT_SUFFIX),$(AOT_PROFILE_ASSEMBLIES))
+
+AOT_PROFILE_FACADES := $(sort $(patsubst .//%,%,$(filter-out %.dll.dll %.exe.dll %bare% %plaincore% %secxml% %Facades% %ilasm%,$(filter %.dll %.exe,$(wildcard $(topdir)/class/lib/$(PROFILE)/Facades/*)))))
+AOT_PROFILE_FACADES_OUT := $(patsubst %,%$(PLATFORM_AOT_SUFFIX),$(AOT_PROFILE_FACADES))
 endif
 
 ifdef ALWAYS_AOT_TESTS
@@ -185,7 +188,7 @@ endif
 # This can run in parallel
 .PHONY: aot-all-profile
 ifdef AOT_BUILD_FLAGS
-aot-all-profile: $(AOT_PROFILE_ASSEMBLIES_OUT) $(AOT_PROFILE_TESTS_OUT)
+aot-all-profile: $(AOT_PROFILE_ASSEMBLIES_OUT) $(AOT_PROFILE_TESTS_OUT) $(AOT_PROFILE_FACADES_OUT)
 else
 aot-all-profile:
 	echo AOT_BUILD_FLAGS not set, skipping AOT.

acceptance-tests/Makefile.am
@@ -3,9 +3,10 @@ BENCHMARKER_PATH=$(ACCEPTANCE_TESTS_PATH)/benchmarker
 ROSLYN_PATH=$(ACCEPTANCE_TESTS_PATH)/roslyn
 CORECLR_PATH=$(ACCEPTANCE_TESTS_PATH)/coreclr
 MSTESTSUITE_PATH=$(ACCEPTANCE_TESTS_PATH)/ms-test-suite
+DEBIANSHOOTOUTMONO_PATH=$(ACCEPTANCE_TESTS_PATH)/DebianShootoutMono
 
 CLEANFILES = *.dll *.exe *.mdb
-EXTRA_DIST=README.md SUBMODULES.json versions.mk profiler-stress.mk roslyn.mk coreclr.mk ms-test-suite.mk
+EXTRA_DIST=README.md SUBMODULES.json microbench.mk versions.mk profiler-stress.mk roslyn.mk coreclr.mk ms-test-suite.mk
 
 CLASS=$(mcs_topdir)/class/lib/$(DEFAULT_PROFILE)
 TOOLS_CLASS=$(mcs_topdir)/class/lib/build
@@ -17,6 +18,7 @@ ILASM = $(TOOLS_RUNTIME) $(TOOLS_CLASS)/ilasm.exe
 XUNIT = $(RUNTIME) $(abs_top_builddir)/external/xunit-binaries/xunit.console.exe
 
 include versions.mk
+include microbench.mk
 include profiler-stress.mk
 include roslyn.mk
 include coreclr.mk

acceptance-tests/microbench-perf.sh.in
@@ -1,0 +1,17 @@
+#! /bin/sh
+r='@mono_build_root@'
+aotpattern="--aot="
+
+# if this is an aot invoke
+if [[ $@ =~ $aotpattern ]];
+then
+# just aot the code
+echo "$r/runtime/mono-wrapper $@"
+exec $r/runtime/mono-wrapper $@
+else
+# else run it under perf
+echo "$MONO_PERF_BINARY record -o $r/acceptance-tests/perf.data -v -s -g -- $r/mono/mini/mono-sgen $@"
+exec $MONO_PERF_BINARY record -o $r/acceptance-tests/perf.data -v -s -g $r/mono/mini/mono-sgen $@
+fi
+
+

acceptance-tests/microbench.mk
@@ -1,0 +1,119 @@
+check-microbench: DebianShootoutMono.stamp
+	@$(MAKE) test-run-microbench
+
+DebianShootoutMono.stamp: 
+	@$(MAKE) validate-DebianShootoutMono RESET_VERSIONS=1
+	@$(MAKE) prepare-dlls
+	@touch $@
+
+abs_top_srcdir = $(abspath $(top_srcdir))
+TEST_EXE_PATH=$(abs_top_srcdir)/acceptance-tests/external/DebianShootoutMono/release/
+NET_4_X_RUNTIME=MONO_PATH=$(TEST_EXE_PATH):$(abs_top_srcdir)/mcs/class/lib/net_4_x $(abs_top_srcdir)/runtime/mono-wrapper
+FULL_AOT_RUNTIME=MONO_PATH=$(abs_top_srcdir)/mcs/class/lib/testing_aot_full $(abs_top_srcdir)/runtime/mono-wrapper
+
+PERF_BINARY=$(if $(MONO_PERF_BINARY),$(MONO_PERF_BINARY),perf)
+PERF_RUNTIME=$(abs_top_srcdir)/acceptance-tests/microbench-perf.sh
+
+define BenchmarkDotNetTemplate
+
+run-microbench-$(1):: DebianShootoutMono.stamp
+	MONO_BENCH_AOT_RUN="$(AOT_RUN_FLAGS)" \
+	MONO_BENCH_AOT_BUILD="$(AOT_BUILD_FLAGS)" \
+	MONO_BENCH_EXECUTABLE="$(abs_top_srcdir)/runtime/mono-wrapper" \
+	MONO_BENCH_PATH="$(abs_top_srcdir)/mcs/class/lib/$(TEST_PROFILE)" \
+	MONO_BENCH_INPUT="$(2)" \
+	$(NET_4_X_RUNTIME) \
+	$(TEST_EXE_PATH)/DebianShootoutMono.exe $(1)
+
+test-run-microbench:: run-microbench-$(1)
+
+run-microbench-debug-$(1):: DebianShootoutMono.stamp
+	echo MONO_PATH="$(abs_top_srcdir)/mcs/class/lib/$(TEST_PROFILE)" $(abs_top_srcdir)/runtime/mono-wrapper $(AOT_BUILD_FLAGS) $(TEST_EXE_PATH)/DebianShootoutMono.exe
+	MONO_BENCH_INPUT="$(2)" \
+	MONO_PATH="$(abs_top_srcdir)/mcs/class/lib/$(TEST_PROFILE)" \
+	$(abs_top_srcdir)/runtime/mono-wrapper $(AOT_RUN_FLAGS) $(TEST_EXE_PATH)/DebianShootoutMono.exe Run $(1)
+
+test-run-microbench-debug:: run-microbench-debug-$(1)
+
+if HOST_LINUX
+run-microbench-profiled-$(1):: microbench-results/$(1).perf.data 
+
+microbench-results/$(1).perf.data: DebianShootoutMono.stamp 
+	mkdir -p microbench-results
+	MONO_PERF_BINARY="$(PERF_BINARY)" \
+	MONO_BENCH_EXECUTABLE="$(PERF_RUNTIME)" \
+	MONO_BENCH_AOT_RUN="$(AOT_RUN_FLAGS)"\
+	MONO_BENCH_AOT_BUILD="$(AOT_BUILD_FLAGS)"\
+	MONO_BENCH_PATH="$(abs_top_srcdir)/mcs/class/lib/$(TEST_PROFILE)" \
+	MONO_BENCH_INPUT="$(2)" \
+	$(NET_4_X_RUNTIME) \
+	$(TEST_EXE_PATH)/DebianShootoutMono.exe $(1) $(2)
+	mv perf.data microbench-results/$(1).perf.data
+
+microbench-results/$(1).tmp.perf: microbench-results/$(1).perf.data
+	$(PERF_BINARY) script -i microbench-results/$(1).perf.data > microbench-results/$(1).tmp.perf
+
+microbench-results/$(1).perf-flame.svg: microbench-results/$(1).tmp.perf
+	cat microbench-results/$(1).tmp.perf | ./external/DebianShootoutMono/FlameGraph/stackcollapse-perf.pl > microbench-results/$(1).perf-folded
+	./external/DebianShootoutMono/FlameGraph/flamegraph.pl microbench-results/$(1).perf-folded > microbench-results/$(1).perf-flame.svg
+	rm microbench-results/$(1).tmp.perf
+	rm microbench-results/$(1).perf-folded
+
+MONO_PERF_FLAGS=--show-cpu-utilization -n --hierarchy -T $(MONO_PERF_ADDITIONAL_FLAGS)
+
+microbench-results/$(1).perf.report: microbench-results/$(1).perf.data
+	$(PERF_BINARY) report -i microbench-results/$(1).perf.data $(MONO_PERF_FLAGS) > microbench-results/$(1).perf.report 
+
+test-run-microbench-profiled:: run-microbench-profiled-$(1)
+
+test-run-microbench-publish-collect:: microbench-results/$(1).perf.data microbench-results/$(1).perf.report microbench-results/$(1).perf-flame.svg
+
+endif
+
+endef
+
+if HOST_LINUX
+
+test-run-microbench-perf-check:
+	$(PERF_BINARY) record -a -o perf.data -- echo "testing"
+	rm perf.data
+
+microbench-results/perf-data.zip:
+	zip microbench-results/perf-data.zip microbench-results/*.perf.data
+	rm microbench-results/*.perf.data
+
+perf-report: microbench-results/perf-data.zip
+
+perf-report-total: test-run-microbench-publish-collect
+	@$(MAKE) perf-report
+
+endif
+
+.PHONY: prepare-dlls
+
+if FULL_AOT_TESTS
+prepare-dlls: 
+	$(FULL_AOT_RUNTIME) $(AOT_BUILD_FLAGS) $(TEST_EXE_PATH)/*.{dll,exe}
+
+else
+
+prepare-dlls:
+
+endif
+
+FIXTURE_DIR=$(abs_top_srcdir)/acceptance-tests/external/DebianShootoutMono/fixtures
+
+$(eval $(call BenchmarkDotNetTemplate,Mandelbrot,))
+$(eval $(call BenchmarkDotNetTemplate,RegexRedux,$(FIXTURE_DIR)/regexredux-input.txt))
+$(eval $(call BenchmarkDotNetTemplate,KNucleotide,$(FIXTURE_DIR)/knucleotide-input.txt))
+
+$(eval $(call BenchmarkDotNetTemplate,BinaryTrees,))
+$(eval $(call BenchmarkDotNetTemplate,NBodyTest,))
+$(eval $(call BenchmarkDotNetTemplate,SpectralNorm,))
+$(eval $(call BenchmarkDotNetTemplate,Fannkuchredux,))
+$(eval $(call BenchmarkDotNetTemplate,Fasta,))
+
+$(eval $(call BenchmarkDotNetTemplate,RevComp,$(FIXTURE_DIR)/revcomp-input.txt)) 
+
+#$(eval $(call BenchmarkDotNetTemplate,GistBenchmark,$(MONO_BENCH_GIST_URL))) broken in BDN, bug filed
+

acceptance-tests/versions.mk
@@ -7,6 +7,7 @@ $(eval $(call ValidateVersionTemplate,benchmarker,BENCHMARKER))
 $(eval $(call ValidateVersionTemplate,roslyn,ROSLYN))
 $(eval $(call ValidateVersionTemplate,coreclr,CORECLR))
 $(eval $(call ValidateVersionTemplate,ms-test-suite,MSTESTSUITE))
+$(eval $(call ValidateVersionTemplate,DebianShootoutMono,DEBIANSHOOTOUTMONO))
 
 # Bump the given submodule to the revision given by the REV make variable
 # If COMMIT is 1, commit the change
@@ -14,6 +15,7 @@ bump-benchmarker: __bump-benchmarker
 bump-roslyn: __bump-version-roslyn
 bump-coreclr: __bump-version-coreclr
 bump-ms-test-suite: __bump-version-ms-test-suite
+bump-DebianShootoutMono: __bump-version-DebianShootoutMono
 
 # Bump the given submodule to the branch given by the BRANCH/REMOTE_BRANCH make variables
 # If COMMIT is 1, commit the change
@@ -21,6 +23,7 @@ bump-branch-benchmarker: __bump-branch-benchmarker
 bump-branch-roslyn: __bump-branch-roslyn
 bump-branch-coreclr: __bump-branch-coreclr
 bump-branch-ms-test-suite: __bump-branch-ms-test-suite
+bump-branch-DebianShootoutMono: __bump-branch-DebianShootoutMono
 
 # Bump the given submodule to its current GIT version
 # If COMMIT is 1, commit the change
@@ -28,6 +31,7 @@ bump-current-benchmarker: __bump-current-benchmarker
 bump-current-roslyn: __bump-current-version-roslyn
 bump-current-coreclr: __bump-current-version-coreclr
 bump-current-ms-test-suite: __bump-current-version-ms-test-suite
+bump-current-DebianShootoutMono: __bump-current-version-DebianShootoutMono
 
 commit-bump-benchmarker:
 	$(MAKE) bump-benchmarker COMMIT=1
@@ -41,6 +45,9 @@ commit-bump-coreclr:
 commit-bump-ms-test-suite:
 	$(MAKE) bump-ms-test-suite COMMIT=1
 
+commit-bump-DebianShootoutMono:
+	$(MAKE) bump-DebianShootoutMono COMMIT=1
+
 commit-bump-current-benchmarker:
 	$(MAKE) bump-current-benchmarker COMMIT=1
 
@@ -52,3 +59,6 @@ commit-bump-current-coreclr:
 
 commit-bump-current-ms-test-suite:
 	$(MAKE) bump-current-ms-test-suite COMMIT=1
+
+commit-bump-current-DebianShootoutMono:
+	$(MAKE) bump-current-DebianShootoutMono COMMIT=1

configure.ac
@@ -1174,6 +1174,7 @@ if test x$cross_compiling = xyes -o x$enable_mcs_build = xno; then
    DISABLE_MCS_DOCS_default=yes
 elif test x$with_runtime_preset = xnet_4_x; then
    with_profile4_x_default=yes
+   TEST_PROFILE=net_4_x
 elif test x$with_runtime_preset = xall; then
    with_profile4_x_default=yes
    with_monodroid_default=yes
@@ -1188,6 +1189,7 @@ elif test x$with_runtime_preset = xall; then
    with_testing_aot_full_interp_default=yes
    with_testing_aot_hybrid_default=yes
    with_testing_aot_full_default=yes
+   TEST_PROFILE=net_4_x
 elif test x$with_runtime_preset = xbitcode; then
    DISABLE_MCS_DOCS_default=yes
    with_testing_aot_full_default=yes
@@ -1323,6 +1325,7 @@ elif test x$with_runtime_preset = xwasm; then
    AOT_BUILD_FLAGS="--runtime=mobile --aot=full,$INVARIANT_AOT_OPTIONS"
    AOT_RUN_FLAGS="--runtime=mobile --full-aot"
 else
+   TEST_PROFILE=net_4_x
    with_profile4_x_default=yes
 fi
 
@@ -1407,15 +1410,6 @@ fi
 if test -z "$INSTALL_XAMMAC_TRUE"; then :
    default_profile=xammac
 fi
-if test -z "$INSTALL_TESTING_AOT_FULL_INTERP_TRUE"; then :
-   default_profile=testing_aot_full_interp
-fi
-if test -z "$INSTALL_TESTING_AOT_HYBRID_TRUE"; then :
-   default_profile=testing_aot_hybrid
-fi
-if test -z "$INSTALL_TESTING_AOT_FULL_TRUE"; then :
-   default_profile=testing_aot_full
-fi
 if test -z "$INSTALL_WINAOT_TRUE"; then :
    default_profile=winaot
 fi
@@ -1430,6 +1424,15 @@ if test -z "$INSTALL_WASM_TRUE"; then :
 fi
 if test -z "$INSTALL_4_x_TRUE"; then :
    default_profile=net_4_x
+fi
+if test -z "$INSTALL_TESTING_AOT_FULL_INTERP_TRUE"; then :
+   default_profile=testing_aot_full_interp
+fi
+if test -z "$INSTALL_TESTING_AOT_HYBRID_TRUE"; then :
+   default_profile=testing_aot_hybrid
+fi
+if test -z "$INSTALL_TESTING_AOT_FULL_TRUE"; then :
+   default_profile=testing_aot_full
 fi
 DEFAULT_PROFILE=$default_profile
 AC_SUBST(DEFAULT_PROFILE)
@@ -5557,6 +5560,7 @@ AC_SUBST(CSC)
 
 AC_CONFIG_FILES([po/mcs/Makefile.in])
 
+AC_CONFIG_FILES([acceptance-tests/microbench-perf.sh],[chmod +x acceptance-tests/microbench-perf.sh])
 AC_CONFIG_FILES([runtime/mono-wrapper],[chmod +x runtime/mono-wrapper])
 AC_CONFIG_FILES([runtime/monodis-wrapper],[chmod +x runtime/monodis-wrapper])
 

mcs/class/Facades/System.Drawing.Common/Makefile
@@ -26,7 +26,7 @@ EMBEDDED_DRAWING_DEP := $(filter xammac_net_4_5 testing_aot_full testing_aot_hyb
 ifndef EMBEDDED_DRAWING_DEP
 
 # profiles which build a System.Drawing.dll in the repo
-REPO_DRAWING_DEP := $(filter net_4_x orbis winaot unreal wasm, $(PROFILE))
+REPO_DRAWING_DEP := $(filter net_4_x orbis winaot unreal testing_aot_full wasm, $(PROFILE))
 
 ifdef REPO_DRAWING_DEP
 LIB_REFS += System.Drawing

mcs/class/Facades/System.Drawing.Primitives/Makefile
@@ -36,7 +36,7 @@ API_BIN_REFS := Xamarin.Mac
 endif
 
 # profiles which build a System.Drawing.dll in the repo
-REPO_DRAWING_DEP := $(filter net_4_x orbis winaot unreal wasm, $(PROFILE))
+REPO_DRAWING_DEP := $(filter net_4_x orbis testing_aot_full winaot unreal wasm, $(PROFILE))
 
 ifdef REPO_DRAWING_DEP
 LIB_REFS += System.Drawing

commit c1dce239d65bac97090e8c93c12277ed074d2fb9

[build] Fixes documentation build broken by #9833

docs/Makefile.am
@@ -52,7 +52,8 @@ EXTRA_DIST = \
 
 -include $(mcs_topdir)/build/config.make
 
-CLASS=$(mcs_topdir)/class/lib/$(DEFAULT_PROFILE)
+DOC_PROFILE:=net_4_x
+CLASS=$(mcs_topdir)/class/lib/$(DOC_PROFILE)
 
 dist-hook:
 	$(mkdir_p)  $(distdir)/sources
@@ -68,7 +69,7 @@ clean-local:
 	-rm -f deploy/* convert.exe* AgilityPack.dll*
 	-rm -f $(ASSEMBLED_DOCS)
 
-TOOL_MAKE=$(MAKE) -f $(srcdir)/docs.make topdir=$(srcdir)/../mcs srcdir=$(srcdir)
+TOOL_MAKE=$(MAKE) -f $(srcdir)/docs.make topdir=$(srcdir)/../mcs srcdir=$(srcdir) PROFILE=$(DOC_PROFILE)
 
 monoapi.zip: monoapi.tree
 

mcs/build/rules.make
@@ -339,7 +339,7 @@ dist-default:
 ## Documentation stuff
 
 Q_MDOC =$(if $(V),,@echo "MDOC    [$(PROFILE_DIRECTORY)] $(notdir $(@))";)
-MDOC   =$(Q_MDOC) MONO_PATH="$(topdir)/class/lib/$(DEFAULT_PROFILE)$(PLATFORM_PATH_SEPARATOR)$$MONO_PATH" $(RUNTIME) $(topdir)/class/lib/$(DEFAULT_PROFILE)/mdoc.exe
+MDOC   =$(Q_MDOC) MONO_PATH="$(topdir)/class/lib/$(PROFILE_DIRECTORY)$(PLATFORM_PATH_SEPARATOR)$$MONO_PATH" $(RUNTIME) $(topdir)/class/lib/$(PROFILE_DIRECTORY)/mdoc.exe
 
 Q_MDOC_UP=$(if $(V),,@echo "MDOC-UP [$(PROFILE_DIRECTORY)] $(notdir $(@))";)
-MDOC_UP  =$(Q_MDOC_UP) MONO_PATH="$(topdir)/class/lib/$(DEFAULT_PROFILE)$(PLATFORM_PATH_SEPARATOR)$$MONO_PATH" $(RUNTIME) $(topdir)/class/lib/$(DEFAULT_PROFILE)/mdoc.exe update --delete -o Documentation/en
+MDOC_UP  =$(Q_MDOC_UP) MONO_PATH="$(topdir)/class/lib/$(PROFILE_DIRECTORY)$(PLATFORM_PATH_SEPARATOR)$$MONO_PATH" $(RUNTIME) $(topdir)/class/lib/$(PROFILE_DIRECTORY)/mdoc.exe update --delete -o Documentation/en