mono/mono

commit 18b6846a3a2218525b7335a1f0515de93a1e44ca

[Coop] Cominterop and marshal conversion. (#9503)
* [Coop] Convert ves_icall_System_Runtime_InteropServices_Marshal_IsComObject.

* [Coop] cominterop and marshalling partial conversion and cleanup.

* Some refinement/correction/commenting to mono_string_to_utf8str_handle.

* Fix index out of bounds.

* Fix error handling.

* Remove some mono_error_set_pending_exception.

* Fix icall interface -- string vs. ptr/len.

* tweaks

* Restore the corlib interface, and maybe address the apidiff gate.

* cleanup

* Fix PtrToStructure GC assertion failure tests/marshal2 interpreter.

* Fix merge.

* Revert "Restore the corlib interface, and maybe address the apidiff gate."

This reverts commit ca8456a05f75f0fc05f6559e2f075e18a455a362.

* Try harder to support fixed in array copying.

* Allow for overlap.

* Put mono_ in front of type and fix Windows.

* Fix small problems on Windows build.

* Fix Windows and support larger data.

* [arm64] Make OP_CHECK_THIS read only a byte, to prevent unaligned access errors.

* Bump corlib higher.

* Merge with https://github.com/mono/mono/pull/9430/files.

* Bump API snapshot submodule

mono/metadata/marshal-windows.c
@@ -31,7 +31,6 @@ void
 mono_marshal_free_hglobal (gpointer ptr)
 {
 	GlobalFree (ptr);
-	return;
 }
 #endif /* G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT) */
 
@@ -45,7 +44,6 @@ void
 mono_marshal_free_co_task_mem (void *ptr)
 {
 	CoTaskMemFree (ptr);
-	return;
 }
 
 gpointer
@@ -54,73 +52,89 @@ mono_marshal_realloc_co_task_mem (gpointer ptr, size_t size)
 	return CoTaskMemRealloc (ptr, size);
 }
 
-gpointer
-ves_icall_System_Runtime_InteropServices_Marshal_StringToHGlobalAnsi (MonoString *string)
+char*
+ves_icall_System_Runtime_InteropServices_Marshal_StringToHGlobalAnsi (const gunichar2 *s, int length, MonoError *error)
 {
-	ERROR_DECL (error);
-	char* tres, *ret;
-	size_t len;
-	tres = mono_string_to_utf8_checked (string, error);
-	if (mono_error_set_pending_exception (error))
-		return NULL;
+	// FIXME pass mono_utf16_to_utf8 an allocator to avoid double alloc/copy.
+
+	char* tres = mono_utf16_to_utf8 (s, length, error);
+	return_val_if_nok (error, NULL);
 	if (!tres)
 		return tres;
 
 	/*
-	 * mono_string_to_utf8_checked() returns a memory area at least as large as the size of the
-	 * MonoString, even if it contains NULL characters. The copy we allocate here has to be equally
+	 * mono_utf16_to_utf8() returns a memory area at least as large as length,
+	 * even if it contains NULL characters. The copy we allocate here has to be equally
 	 * large.
 	 */
-	len = MAX (strlen (tres) + 1, string->length);
-	ret = ves_icall_System_Runtime_InteropServices_Marshal_AllocHGlobal ((gpointer)len);
-	memcpy (ret, tres, len);
+	size_t len = MAX (strlen (tres) + 1, length);
+	char* ret = ves_icall_System_Runtime_InteropServices_Marshal_AllocHGlobal (len, error);
+	if (ret)
+		memcpy (ret, tres, len);
 	g_free (tres);
 	return ret;
 }
 
-gpointer
-ves_icall_System_Runtime_InteropServices_Marshal_StringToHGlobalUni (MonoString *string)
+gunichar2*
+ves_icall_System_Runtime_InteropServices_Marshal_StringToHGlobalUni (const gunichar2 *s, int length, MonoError *error)
 {
-	if (string == NULL)
+	if (!s)
 		return NULL;
-	else {
-		size_t len = ((mono_string_length (string) + 1) * 2);
-		gunichar2 *res = ves_icall_System_Runtime_InteropServices_Marshal_AllocHGlobal ((gpointer)len);
 
-		memcpy (res, mono_string_chars (string), mono_string_length (string) * 2);
-		res [mono_string_length (string)] = 0;
-		return res;
+	gsize const len = ((gsize)length + 1) * 2;
+	gunichar2 *res = ves_icall_System_Runtime_InteropServices_Marshal_AllocHGlobal (len, error);
+	if (res) {
+		memcpy (res, s, length * 2);
+		res [length] = 0;
 	}
+	return res;
 }
 
 gpointer
-mono_string_to_utf8str (MonoString *s)
+mono_string_to_utf8str_handle (MonoStringHandle s, MonoError *error)
 {
 	char *as, *tmp;
 	glong len;
 	GError *gerror = NULL;
 
-	if (s == NULL)
+	if (MONO_HANDLE_IS_NULL (s))
 		return NULL;
 
-	if (!s->length) {
+	if (!mono_string_handle_length (s)) {
 		as = CoTaskMemAlloc (1);
+		g_assert (as);
 		as [0] = '\0';
 		return as;
 	}
 
-	tmp = g_utf16_to_utf8 (mono_string_chars (s), s->length, NULL, &len, &gerror);
+	// FIXME pass g_utf16_to_utf8 an allocator to avoid double alloc/copy.
+
+	uint32_t gchandle = 0;
+	tmp = g_utf16_to_utf8 (mono_string_handle_pin_chars (s, &gchandle), mono_string_handle_length (s), NULL, &len, &gerror);
+	mono_gchandle_free (gchandle);
 	if (gerror) {
-		MonoException *exc = mono_get_exception_argument ("string", gerror->message);
+		mono_error_set_argument (error, "string", gerror->message);
 		g_error_free (gerror);
-		mono_set_pending_exception (exc);
 		return NULL;
 	} else {
 		as = CoTaskMemAlloc (len + 1);
+		g_assert (as);
 		memcpy (as, tmp, len + 1);
 		g_free (tmp);
 		return as;
 	}
+}
+
+/* This is a JIT icall, it sets the pending exception and returns NULL on error. */
+gpointer
+mono_string_to_utf8str (MonoString *s_raw)
+{
+	HANDLE_FUNCTION_ENTER ();
+	ERROR_DECL (error);
+	MONO_HANDLE_DCL (MonoString, s);
+	gpointer result = mono_string_to_utf8str_handle (s, error);
+	mono_error_set_pending_exception (error);
+	HANDLE_FUNCTION_RETURN_VAL (result);
 }
 
 #endif /* HOST_WIN32 */

configure.ac
@@ -40,7 +40,7 @@ MONO_VERSION_BUILD=`echo $VERSION | cut -d . -f 3`
 # This can be reset to 0 when Mono's version number is bumped
 # since it's part of the corlib version (the prefix '1' in the full
 # version number is to ensure the number isn't treated as octal in C)
-MONO_CORLIB_COUNTER=17
+MONO_CORLIB_COUNTER=21
 MONO_CORLIB_VERSION=`printf "1%02d%02d%02d%03d" $MONO_VERSION_MAJOR $MONO_VERSION_MINOR 0 $MONO_CORLIB_COUNTER`
 
 AC_DEFINE_UNQUOTED(MONO_CORLIB_VERSION,$MONO_CORLIB_VERSION,[Version of the corlib-runtime interface])

mcs/class/corlib/System.Runtime.InteropServices/Marshal.cs
@@ -110,92 +110,174 @@ namespace System.Runtime.InteropServices
 			throw new NotImplementedException ();
 		}
 
+		unsafe internal static void copy_to_unmanaged (Array source, int startIndex,
+		       IntPtr destination, int length)
+		{
+			copy_to_unmanaged_fixed (source, startIndex, destination, length, null);
+		}
+
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		internal extern static void copy_to_unmanaged (Array source, int startIndex,
-							       IntPtr destination, int length);
+		unsafe private extern static void copy_to_unmanaged_fixed (Array source, int startIndex,
+		       IntPtr destination, int length, void* fixed_source_element);
+
+		static private bool skip_fixed (System.Array array, int startIndex)
+		{
+			// In particular, we see length == 0 && startIndex == array.Length, and fixed fails.
+			return startIndex < 0 || startIndex >= array.Length;
+		}
+
+		unsafe internal static void copy_to_unmanaged (byte[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		unsafe internal static void copy_to_unmanaged (char[] source, int startIndex,
+		       IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (byte[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (char[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (short[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (int[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (long[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (float[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (double[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		public unsafe static void Copy (IntPtr[] source, int startIndex, IntPtr destination, int length)
+		{
+			if (skip_fixed (source, startIndex))
+				copy_to_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_source = &source [startIndex])
+				copy_to_unmanaged_fixed (source, startIndex, destination, length, fixed_source);
+		}
+
+		unsafe internal static void copy_from_unmanaged (IntPtr source, int startIndex, Array destination, int length)
+		{
+			copy_from_unmanaged_fixed (source, startIndex, destination, length, null);
+		}
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		internal extern static void copy_from_unmanaged (IntPtr source, int startIndex,
-								 Array destination, int length);
+		unsafe private extern static void copy_from_unmanaged_fixed (IntPtr source, int startIndex,
+			Array destination, int length, void* fixed_destination_element);
 
-		public static void Copy (byte[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, byte[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (char[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, char[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (short[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, short[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (int[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, int[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (long[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, long[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (float[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, float[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (double[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, double[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
-		public static void Copy (IntPtr[] source, int startIndex, IntPtr destination, int length)
+		public unsafe static void Copy (IntPtr source, IntPtr[] destination, int startIndex, int length)
 		{
-			copy_to_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, byte[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, char[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, short[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, int[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, long[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, float[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, double[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
-		}
-
-		public static void Copy (IntPtr source, IntPtr[] destination, int startIndex, int length)
-		{
-			copy_from_unmanaged (source, startIndex, destination, length);
+			if (skip_fixed (destination, startIndex))
+				copy_from_unmanaged (source, startIndex, destination, length);
+			else fixed (void* fixed_destination = &destination [startIndex])
+				copy_from_unmanaged_fixed (source, startIndex, destination, length, fixed_destination);
 		}
 
 		public static IntPtr CreateAggregatedObject (IntPtr pOuter,
@@ -1070,8 +1152,13 @@ namespace System.Runtime.InteropServices
 			return (size + 3) & (~((uint)3));
 		}
 
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		public extern static IntPtr StringToBSTR (string s);
+		public unsafe static IntPtr StringToBSTR (string s)
+		{
+			if (s == null)
+				return IntPtr.Zero;
+			fixed (char* fixed_s = s)
+				return BufferToBSTR (fixed_s, s.Length);
+		}
 
 		public static IntPtr StringToCoTaskMemAnsi (string s)
 		{
@@ -1098,7 +1185,13 @@ namespace System.Runtime.InteropServices
 		}
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		public extern static IntPtr StringToHGlobalAnsi (string s);
+		unsafe extern static IntPtr StringToHGlobalAnsi (char* s, int length);
+
+		public unsafe static IntPtr StringToHGlobalAnsi (string s)
+		{
+			fixed (char* fixed_s = s)
+				return StringToHGlobalAnsi (fixed_s, (s != null) ? s.Length : 0);
+		}
 
 		unsafe public static IntPtr StringToAllocatedMemoryUTF8(String s)
 		{
@@ -1134,9 +1227,15 @@ namespace System.Runtime.InteropServices
 		}
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		public extern static IntPtr StringToHGlobalUni (string s);
+		unsafe extern static IntPtr StringToHGlobalUni (char* s, int length);
 
-		public static IntPtr SecureStringToBSTR (SecureString s)
+		public unsafe static IntPtr StringToHGlobalUni (string s)
+		{
+			fixed (char* fixed_s = s)
+				return StringToHGlobalUni (fixed_s, (s != null) ? s.Length : 0);
+		}
+
+		public unsafe static IntPtr SecureStringToBSTR (SecureString s)
 		{
 			if (s == null)
 				throw new ArgumentNullException ("s");
@@ -1153,8 +1252,9 @@ namespace System.Runtime.InteropServices
 					buffer[i + 1] = b;
 				}
 			}
-			return BufferToBSTR (buffer, len);
-        }
+			fixed (byte* fixed_buffer = buffer)
+				return BufferToBSTR ((char*)fixed_buffer, len);
+		}
 
 		public static IntPtr SecureStringToCoTaskMemAnsi (SecureString s)
 		{
@@ -1243,7 +1343,7 @@ namespace System.Runtime.InteropServices
 
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		extern static IntPtr BufferToBSTR (Array ptr, int slen);
+		extern unsafe static IntPtr BufferToBSTR (char* ptr, int slen);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public extern static IntPtr UnsafeAddrOfPinnedArrayElement (Array arr, int index);

mcs/class/corlib/System.Runtime.InteropServices/SafeBuffer.cs
@@ -145,7 +145,7 @@ namespace System.Runtime.InteropServices
 
 		[CLSCompliant (false)]
 		[ReliabilityContract (Consistency.WillNotCorruptState, Cer.MayFail)]
-		public void WriteArray<T> (ulong byteOffset, T[] array, int index, int count) where T : struct
+		public unsafe void WriteArray<T> (ulong byteOffset, T[] array, int index, int count) where T : struct
 		{
 			if (!inited)
 				throw new InvalidOperationException ();
@@ -155,10 +155,9 @@ namespace System.Runtime.InteropServices
 				int size = Marshal.SizeOf (typeof (T)) * count;
 				if (target >= last_byte || target + size > last_byte)
 					throw new ArgumentException ("would overrite");
-				
+
 				Marshal.copy_to_unmanaged (array, index, (IntPtr) target, count);
 			}
 		}
 	}
 }
-

mono/metadata/cominterop.h
@@ -57,16 +57,13 @@ mono_cominterop_emit_marshal_safearray (EmitMarshalContext *m, int argnum,
 
 MONO_RT_EXTERNAL_ONLY
 MONO_API MonoString * 
-mono_string_from_bstr (gpointer bstr);
+mono_string_from_bstr (/*mono_bstr*/gpointer bstr);
 
 MonoString *
-mono_string_from_bstr_icall (gpointer bstr);
-
-MonoString *
-mono_string_from_bstr_checked (const gunichar2* bstr, MonoError *error);
+mono_string_from_bstr_icall (mono_bstr_const bstr);
 
 MONO_API void 
-mono_free_bstr (gpointer bstr);
+mono_free_bstr (/*mono_bstr_const*/gpointer bstr);
 
 MonoClass*
 mono_class_try_get_com_object_class (void);

mono/metadata/handle.c
@@ -508,6 +508,12 @@ mono_gchandle_get_target_handle (uint32_t gchandle)
 	return MONO_HANDLE_NEW (MonoObject, mono_gchandle_get_target (gchandle));
 }
 
+gboolean
+mono_gchandle_target_equal (uint32_t gchandle, MonoObjectHandle equal)
+{
+	return mono_gchandle_get_target (gchandle) == MONO_HANDLE_RAW (equal);
+}
+
 gpointer
 mono_array_handle_pin_with_size (MonoArrayHandle handle, int size, uintptr_t idx, uint32_t *gchandle)
 {

commit fc04c8248ebe924d58e30970c46d6ab37853ed68

Fix https://github.com/mono/mono/issues/11898 This regressed six months ago in https://github.com/mono/mono/pull/9503.
There are two identical sounding functions:

mono_string_handle_to_utf8:
  What we usually use. Sounds reasonable. Usually is reasonable.
  Allocates with malloc.

mono_string_to_utf8str_handle:
  What should be used here. Allocates with CoTaskMemAlloc.
  So it can be freed with marshal_free => CoTaskMemFree.

i.e. The allocator and the freeer need to agree.

mono/metadata/marshal-internals.h
@@ -14,7 +14,7 @@ MonoObjectHandle
 mono_marshal_xdomain_copy_value_handle (MonoObjectHandle val, MonoError *error);
 
 // On Windows platform implementation of bellow methods are hosted in separate source file
-// masrshal-windows.c or marshal-windows-*.c. On other platforms the implementation is still keept
+// marshal-windows.c or marshal-windows-*.c. On other platforms the implementation is still keept
 // in marshal.c still declared as static and in some places even inlined.
 #ifdef HOST_WIN32
 void*
@@ -35,9 +35,22 @@ mono_marshal_realloc_hglobal (gpointer ptr, size_t size);
 void
 mono_marshal_free_hglobal (void *ptr);
 
+// Allocates with CoTaskMemAlloc. Free with mono_marshal_free (CoTaskMemFree).
+gpointer
+mono_string_to_utf8str_handle (MonoStringHandle s, MonoError *error);
+
+#else
+
+// Allocates with g_malloc. Free with mono_marshal_free (g_free).
+#define mono_string_to_utf8str_handle mono_string_handle_to_utf8
+
+#endif // HOST_WIN32
+
+// Windows: Allocates with CoTaskMemAlloc.
+// Unix: Allocates with g_malloc.
+// Either way: Free with mono_marshal_free (Windows:CoTaskMemFree, Unix:g_free).
 gpointer
 mono_string_to_utf8str (MonoString *s);
-#endif  /* HOST_WIN32 */
 
 typedef enum {
 	TYPECHECK_OBJECT_ARG_POS = 0,

mono/metadata/marshal-windows.c
@@ -113,16 +113,4 @@ mono_string_to_utf8str_handle (MonoStringHandle s, MonoError *error)
 	}
 }
 
-/* This is a JIT icall, it sets the pending exception and returns NULL on error. */
-gpointer
-mono_string_to_utf8str (MonoString *s_raw)
-{
-	HANDLE_FUNCTION_ENTER ();
-	ERROR_DECL (error);
-	MONO_HANDLE_DCL (MonoString, s);
-	gpointer result = mono_string_to_utf8str_handle (s, error);
-	mono_error_set_pending_exception (error);
-	HANDLE_FUNCTION_RETURN_VAL (result);
-}
-
 #endif /* HOST_WIN32 */

mono/metadata/marshal.c
@@ -1039,8 +1039,6 @@ mono_string_builder_to_utf16 (MonoStringBuilder *sb)
 	return str;
 }
 
-#ifndef HOST_WIN32
-
 /* This is a JIT icall, it sets the pending exception and returns NULL on error. */
 gpointer
 mono_string_to_utf8str (MonoString *s_raw)
@@ -1048,17 +1046,10 @@ mono_string_to_utf8str (MonoString *s_raw)
 	HANDLE_FUNCTION_ENTER ();
 	ERROR_DECL (error);
 	MONO_HANDLE_DCL (MonoString, s);
-	gpointer result = mono_string_handle_to_utf8 (s, error);
+	gpointer result = mono_string_to_utf8str_handle (s, error);
 	mono_error_set_pending_exception (error);
 	HANDLE_FUNCTION_RETURN_VAL (result);
 }
-
-
-#else
-
-// Win32 version uses CoTaskMemAlloc.
-
-#endif
 
 gpointer
 mono_string_to_ansibstr (MonoString *string_obj)
@@ -5878,7 +5869,7 @@ mono_marshal_asany_handle (MonoObjectHandle o, MonoMarshalNative string_encoding
 		case MONO_NATIVE_LPSTR:
 		case MONO_NATIVE_UTF8STR:
 			// Same code path, because in Mono, we treated strings as Utf8
-			return mono_string_handle_to_utf8 (MONO_HANDLE_CAST (MonoString, o), error);
+			return mono_string_to_utf8str_handle (MONO_HANDLE_CAST (MonoString, o), error);
 		default:
 			g_warning ("marshaling conversion %d not implemented", string_encoding);
 			g_assert_not_reached ();