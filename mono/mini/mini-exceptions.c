e94c6368e80b594cc6a651b2dbc890337f797462
Alexander Kyte <alkyte@microsoft.com>
2018-12-12 17:35:34 -0500

    [runtime] Remove duplicate merp invoke line
    Backport of #12036

--- mono/mini/mini-exceptions.c
+++ mono/mini/mini-exceptions.c
@@ -3322,7 +3322,6 @@ mono_handle_native_crash (const char *signal, void *ctx, MONO_SIG_HANDLER_INFO_T
 				if (!ctx) {
 					mono_runtime_printf_err ("\nMust always pass non-null context when using merp.\n");
 				} else if (output) {
-					mono_merp_invoke (crashed_pid, signal, output, &hashes);
 					gboolean merp_upload_success = mono_merp_invoke (crashed_pid, signal, output, &hashes);
 
 					g_assert (merp_upload_success);
