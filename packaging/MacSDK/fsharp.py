2303ec57e61959da4eef3ca987f1440b45c27b5b
Don Syme <dsyme@microsoft.com>
2018-12-14 13:33:20 +0000

    Revert "attempt F# update"
    This reverts commit 5c0194a9b4472f316984969df05d400b52e0c024.

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -2,15 +2,17 @@ class FsharpPackage(GitHubTarballPackage):
     def __init__(self):
         GitHubTarballPackage.__init__(self,
             'fsharp', 'fsharp',
-            '4.5.0',
-            '3de387432de8d11a89f99d1af87aa9ce194fe21b',
-            configure='',
-            override_properties={ 'make': 'make all install' })
+            '4.1.34',
+            '662492595a63dffff8fac84939614743fd6d34f9',
+            configure='./configure --prefix="%{package_prefix}"',
+            override_properties={ 'make': 'make' })
 
         self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
-        self.sources.extend(['patches/fsharp-IsPathRooted-type-inference.patch',
-                             'patches/fsharp-portable-pdb.patch',
-                             'patches/fsharp-noinstall.patch',
+        self.sources.extend(['patches/fsharp-portable-pdb.patch',
+                             'patches/fsharp-string-switchName.patch',
+                             'patches/fsharp-path-overloads.patch',
+                             'patches/fsharp-debug-pinvoke-fix.patch',
+                             'patches/fsharp-IsPathRooted-type-inference.patch',
                              'patches/fsharp-msbuild-16-0.patch'])
 
     def prep(self):
@@ -20,6 +22,8 @@ class FsharpPackage(GitHubTarballPackage):
             self.sh('patch -p1 < "%{local_sources[' + str(p) + ']}"')
 
     def build(self):
+        self.sh('autoreconf') 
+        Package.configure(self)
         Package.make(self)
 
 FsharpPackage()
