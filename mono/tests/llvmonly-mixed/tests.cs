571ce97e1adf91a323a21d45381b636076d13ba9
Zoltan Varga <vargaz@gmail.com>
2019-01-02 22:43:40 -0500

    [llvmonly] Add mixed mode tests.

---
+++ mono/tests/llvmonly-mixed/tests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+
+public class BitcodeMixedTests
+{
+	public static int Main (String[] args) {
+		return TestDriver.RunTests (typeof (BitcodeMixedTests), args);
+	}
+
+	public static int test_2_entry_simple () {
+		return InterpOnly.entry_1 (1);
+	}
+
+	public static int test_0_corlib_call () {
+		var alist2 = new ArrayList (10);
+		var alist = InterpOnly.corlib_call ();
+		return alist.Capacity == 5 ? 0 : 1;
+	}
+	/*
+	public static int test_2_entry_delegate () {
+		Func<int, int> func = InterpOnly.entry_1;
+
+		return func (1);
+	}
+	*/
+}
