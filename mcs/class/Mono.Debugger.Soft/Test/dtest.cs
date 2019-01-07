f7036f792badd6c6b69f11466c3e8b5043d88b65
Thays Grazia <thaystg@gmail.com>
2019-01-07 15:48:24 -0200

    Changing the test behavior as discussed with Ludovic, because we already have knowledge about the unreliability of the crash reporter and the test is not about it. So if in 10 times at least one of them works for now it's good. Fixes #11385

--- mcs/class/Mono.Debugger.Soft/Test/dtest.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -2457,34 +2457,43 @@ public class DebuggerTests
 
 	[Test]
 	[Category("NotOnWindows")]
-	[Ignore("https://github.com/mono/mono/issues/11385")]
 	public void Crash () {
 		bool success = false;
-
-		try {
-			vm.Detach ();
-			Start (new string [] { dtest_app_path, "crash-vm" });
-			Event e = run_until ("crash");
-			while (!success) {
-				vm.Resume ();
-				e = GetNextEvent ();
-				var crash = e as CrashEvent;
-				if (crash == null)
-					continue;
-
-				success = true;
-				Assert.AreNotEqual (0, crash.Dump.Length);
-
-				break;
-			}
-		} finally {
+		for (int i = 0 ; i < 10; i++) {
 			try {
 				vm.Detach ();
-			} finally {
-				vm = null;
-			}
-		}
+				Start (new string [] { dtest_app_path, "crash-vm" });
+				Event e = run_until ("crash");
+				while (!success) {
+					vm.Resume ();
+					e = GetNextEvent ();
+					var crash = e as CrashEvent;
+					if (crash == null)
+						continue;
 
+					success = true;
+					Assert.AreNotEqual (0, crash.Dump.Length);
+
+					break;
+				}
+			} catch (VMDisconnectedException vmDisconnect) { //expected behavior because of unreliability of the crash reporter.
+						success = false;
+			} finally {
+				try {
+					vm.Detach ();
+				} catch (VMDisconnectedException vmDisconnect) { //expected behavior because of unreliability of the crash reporter.
+						success = false;
+				} finally {
+					vm = null;
+				}
+			}
+			if (success) 
+				break;
+			System.Console.WriteLine("tentando");
+			//try again because of unreliability of the crash reporter.
+			TearDown();
+			SetUp();
+		}
 		if (!success)
 			Assert.Fail ("Didn't get crash event");
 	}
