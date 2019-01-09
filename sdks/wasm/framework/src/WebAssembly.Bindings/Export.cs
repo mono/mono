4b8b0e91c44495fccf82ee58a588f68ef6d6d66b
Kenneth Pouncey <kjpou@pt.lu>
2019-01-09 07:45:17 +0100

    Cleanup Framework Bindings and HttpMessageHandler code.
    - move to framework folder to match package structure.  Simplifies packager code finding the framework libraries.
    - create solution and project files
    - split up the `bindings.cs`, now removed, into multiple files and reformat code
    - Clean up xml documentation
    - generate xml file documentation on build.
    - modify make file to build the solution and modify tests to use framework output.
    - modify `packager` to use the correct directory structure.
    - modify `sdk` build targets.
    - modify `package` target to package the correct assemblies along with source projects and solution.
    
    Tested:
    
    - JavaScript shell tests which run in jenkens
    - Browser Tests
    - AOT build and tests

---
+++ sdks/wasm/framework/src/WebAssembly.Bindings/Export.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace WebAssembly {
+	public enum ConvertEnum {
+		Default,
+		ToLower,
+		ToUpper,
+		Numeric
+	}
+
+	[AttributeUsage (AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Field,
+			AllowMultiple = true, Inherited = false)]
+	public class ExportAttribute : Attribute {
+		public ExportAttribute () : this (null, null)
+		{
+		}
+
+		public ExportAttribute (Type contractType) : this (null, contractType)
+		{
+		}
+
+		public ExportAttribute (string contractName) : this (contractName, null)
+		{
+		}
+
+		public ExportAttribute (string contractName, Type contractType)
+		{
+			ContractName = contractName;
+			ContractType = contractType;
+		}
+
+		public string ContractName { get; }
+
+		public Type ContractType { get; }
+		public ConvertEnum EnumValue { get; set; }
+	}
+}
