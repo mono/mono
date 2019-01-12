13a46dcba5f83aee2dc0a27b70a2b1aa8e6165bd
Katelyn Gadd <kg@luminance.org>
2019-01-11 22:06:03 -0800

    Enable roslyn log for compiler server

--- mcs/build/start-compiler-server.sh
+++ mcs/build/start-compiler-server.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -x "$(command -v mono)" ] ; then
     if test -f $1; then
-        mono --debug $1 -pipename:$2 &
+        RoslynCommandLineLogFile=/tmp/vbcs-log.txt mono --debug $1 -pipename:$2 &
         serverpid=$!
         echo Compiler server started with PID $serverpid.
     else
