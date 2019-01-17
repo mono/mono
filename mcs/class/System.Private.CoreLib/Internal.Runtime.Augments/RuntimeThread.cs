6c13551f9c2bd3eccbe27038ad10b2444d744fbf
Marek Safar <marek.safar@gmail.com>
2019-01-17 14:13:01 +0100

    Formatting changes

--- mcs/class/System.Private.CoreLib/Internal.Runtime.Augments/RuntimeThread.cs
+++ mcs/class/System.Private.CoreLib/Internal.Runtime.Augments/RuntimeThread.cs
@@ -12,39 +12,12 @@ namespace Internal.Runtime.Augments
 
 		readonly Thread thread;
 
-		RuntimeThread (Thread t) { thread = t; }
-
-		private Thread AsThread()
+		RuntimeThread (Thread t)
 		{
-			throw new NotImplementedException ();
+			thread = t;
 		}
 
-		public static RuntimeThread Create(ThreadStart start) => throw new NotImplementedException ();
-		public static RuntimeThread Create(ThreadStart start, int maxStackSize) => throw new NotImplementedException ();
-		public static RuntimeThread Create(ParameterizedThreadStart start) => throw new NotImplementedException ();
-		public static RuntimeThread Create(ParameterizedThreadStart start, int maxStackSize) => throw new NotImplementedException ();
-
-		public static int GetCurrentProcessorId ()
-		{
-			// TODO: Implement correctly
-			return 1;
-		}
-
-		public static bool SpinWait (int iterations)
-		{
-			Thread.SpinWait (iterations);
-			return true;
-		}
-
-		public static void Sleep(int millisecondsTimeout) => Thread.Sleep (millisecondsTimeout);
-
-		public void Start() => throw new NotImplementedException ();
-		public void Start(object parameter) => throw new NotImplementedException ();
-
-		public static bool Yield () => Thread.Yield ();
-
-		public bool IsBackground
-		{
+		public bool IsBackground {
 			get { throw new NotImplementedException (); }
 			set { }
 		}
@@ -59,41 +32,79 @@ namespace Internal.Runtime.Augments
 
 		public extern bool IsThreadPoolThread
 		{
+			[MethodImpl (MethodImplOptions.InternalCall)]
+			get;
+		}
+
+		public int ManagedThreadId => AsThread ().ManagedThreadId;
+
+		public string Name {
+			get {
+				return AsThread ().Name;
+			}
+			set {
+				AsThread ().Name = value;
+			}
+		}
+
+		public ThreadPriority Priority {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+			}
+		}
+
+		public ThreadState ThreadState {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public extern bool IsAlive {
 			[MethodImpl(MethodImplOptions.InternalCall)]
 			get;
 		}
 
-		public int ManagedThreadId => AsThread().ManagedThreadId;
-
-		public string Name { get { return AsThread().Name; } set { AsThread().Name = value; } }
-
-		public ThreadPriority Priority
-		{
-			get { throw new NotImplementedException (); }
-			set { }
-		}
-
-		public ThreadState ThreadState
-		{
-			get { throw new NotImplementedException (); }
-		}
+		public static bool Yield () => Thread.Yield ();
 
 		public void Interrupt ()
 		{
 			throw new NotImplementedException ();
 		}
 
-		public extern bool IsAlive
+		private Thread AsThread ()
 		{
-			[MethodImpl(MethodImplOptions.InternalCall)]
-			get;
+			throw new NotImplementedException ();
 		}
 
-		public void Join() => JoinInternal(Timeout.Infinite);
+		public static RuntimeThread Create (ThreadStart start) => throw new NotImplementedException ();
+		public static RuntimeThread Create (ThreadStart start, int maxStackSize) => throw new NotImplementedException ();
+		public static RuntimeThread Create (ParameterizedThreadStart start) => throw new NotImplementedException ();
+		public static RuntimeThread Create (ParameterizedThreadStart start, int maxStackSize) => throw new NotImplementedException ();
 
-		public bool Join(int millisecondsTimeout) => JoinInternal(millisecondsTimeout);
+		public static int GetCurrentProcessorId ()
+		{
+			// TODO: Implement correctly
+			return 1;
+		}
 
-		private bool JoinInternal(int millisecondsTimeout)
+		public static bool SpinWait (int iterations)
+		{
+			Thread.SpinWait (iterations);
+			return true;
+		}
+
+		public static void Sleep (int millisecondsTimeout) => Thread.Sleep (millisecondsTimeout);
+
+		public void Start () => throw new NotImplementedException ();
+		public void Start (object parameter) => throw new NotImplementedException ();
+
+		public void Join () => JoinInternal (Timeout.Infinite);
+
+		public bool Join (int millisecondsTimeout) => JoinInternal (millisecondsTimeout);
+
+		private bool JoinInternal (int millisecondsTimeout)
 		{
 			throw new NotImplementedException ();
 		}
