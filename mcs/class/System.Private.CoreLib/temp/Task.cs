96822a37b5137753a885033921b27a17a2dc32ae
Marek Safar <marek.safar@gmail.com>
2019-01-20 22:21:38 +0100

    Fix Mono env parsing for netcore and reduce Task dependencies

--- mcs/class/System.Private.CoreLib/temp/Task.cs
+++ mcs/class/System.Private.CoreLib/temp/Task.cs
@@ -212,7 +212,7 @@ namespace System.Threading.Tasks
 
         // This dictonary relates the task id, from an operation id located in the Async Causality log to the actual
         // task. This is to be used by the debugger ONLY. Task in this dictionary represent current active tasks.
-        private static readonly Dictionary<int, Task> s_currentActiveTasks = new Dictionary<int, Task>();
+        private static Dictionary<int, Task> s_currentActiveTasks;
         private static readonly object s_activeTasksLock = new object();
 
         // These methods are a way to access the dictionary both from this class and for other classes that also
@@ -222,6 +222,8 @@ namespace System.Threading.Tasks
             Debug.Assert(task != null, "Null Task objects can't be added to the ActiveTasks collection");
             lock (s_activeTasksLock)
             {
+                if (s_currentActiveTasks == null)
+                    s_currentActiveTasks = new Dictionary<int, Task>();
                 s_currentActiveTasks[task.Id] = task;
             }
             //always return true to keep signature as bool for backwards compatibility
@@ -232,7 +234,7 @@ namespace System.Threading.Tasks
         {
             lock (s_activeTasksLock)
             {
-                s_currentActiveTasks.Remove(taskId);
+                s_currentActiveTasks?.Remove(taskId);
             }
         }
 
