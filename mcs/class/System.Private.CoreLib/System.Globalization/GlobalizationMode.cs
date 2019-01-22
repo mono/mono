69356fa7b3ed46716c39226b9d429f5dbcb53203
Zoltan Varga <vargaz@gmail.com>
2019-01-22 10:16:35 -0500

    Implement GlobalizationMode.

--- mcs/class/System.Private.CoreLib/System.Globalization/GlobalizationMode.cs
+++ mcs/class/System.Private.CoreLib/System.Globalization/GlobalizationMode.cs
@@ -6,6 +6,19 @@ namespace System.Globalization
 {
     internal static partial class GlobalizationMode
     {
-        internal static bool Invariant => false;
+		internal static bool Invariant { get; } = GetGlobalizationInvariantMode ();
+
+		static bool GetGlobalizationInvariantMode () {
+			var val = Environment.GetEnvironmentVariable ("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT");
+			if (val != null)
+				return Boolean.IsTrueStringIgnoreCase (val) || val.Equals ("1");
+			int res = Interop.Globalization.LoadICU ();
+			if (res == 0) {
+				string message = "Couldn't find a valid ICU package installed on the system. " +
+					"Set the configuration flag System.Globalization.Invariant to true if you want to run with no globalization support.";
+				Environment.FailFast (message);
+			}
+            return false;
+        }
     }
 }
