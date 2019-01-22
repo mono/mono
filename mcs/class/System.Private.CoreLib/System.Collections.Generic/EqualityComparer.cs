fb56e612e2fae3dbdfe782348d91038c1c65cd9b
Zoltan Varga <vargaz@gmail.com>
2019-01-22 09:55:35 -0500

    Fix indentation.

--- mcs/class/System.Private.CoreLib/System.Collections.Generic/EqualityComparer.cs
+++ mcs/class/System.Private.CoreLib/System.Collections.Generic/EqualityComparer.cs
@@ -4,49 +4,49 @@ namespace System.Collections.Generic
 {
 	partial class EqualityComparer<T>
 	{
-        static volatile EqualityComparer<T> defaultComparer;
+		static volatile EqualityComparer<T> defaultComparer;
 
-        public static EqualityComparer<T> Default {
+		public static EqualityComparer<T> Default {
 			[MethodImplAttribute (MethodImplOptions.AggressiveInlining)]
-            get {
-                EqualityComparer<T> comparer = defaultComparer;
-                if (comparer == null) {
-                    comparer = CreateComparer();
-                    defaultComparer = comparer;
-                }
-                return comparer;
-            }
-        }
+			get {
+				EqualityComparer<T> comparer = defaultComparer;
+				if (comparer == null) {
+					comparer = CreateComparer();
+					defaultComparer = comparer;
+				}
+				return comparer;
+			}
+		}
 
-        static EqualityComparer<T> CreateComparer ()
-        {
-            RuntimeType t = (RuntimeType)typeof(T);
-            if (t == typeof(byte)) {
-                return (EqualityComparer<T>)(object)(new ByteEqualityComparer());
-            }
+		static EqualityComparer<T> CreateComparer ()
+		{
+			RuntimeType t = (RuntimeType)typeof(T);
+			if (t == typeof(byte)) {
+				return (EqualityComparer<T>)(object)(new ByteEqualityComparer());
+			}
 
 			/////////////////////////////////////////////////
 			// KEEP THIS IN SYNC WITH THE DEVIRT CODE
 			// IN METHOD-TO-IR.C
 			/////////////////////////////////////////////////
 
-            if (typeof(IEquatable<T>).IsAssignableFrom(t)) {
-                return (EqualityComparer<T>)RuntimeType.CreateInstanceForAnotherGenericParameter (typeof(GenericEqualityComparer<>), t);
-            }
+			if (typeof(IEquatable<T>).IsAssignableFrom(t)) {
+				return (EqualityComparer<T>)RuntimeType.CreateInstanceForAnotherGenericParameter (typeof(GenericEqualityComparer<>), t);
+			}
 
-            if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>)) {
-                RuntimeType u = (RuntimeType)t.GetGenericArguments()[0];
-                if (typeof(IEquatable<>).MakeGenericType(u).IsAssignableFrom(u)) {
-                    return (EqualityComparer<T>)RuntimeType.CreateInstanceForAnotherGenericParameter (typeof(NullableEqualityComparer<>), u);
-                }
-            }
+			if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>)) {
+				RuntimeType u = (RuntimeType)t.GetGenericArguments()[0];
+				if (typeof(IEquatable<>).MakeGenericType(u).IsAssignableFrom(u)) {
+					return (EqualityComparer<T>)RuntimeType.CreateInstanceForAnotherGenericParameter (typeof(NullableEqualityComparer<>), u);
+				}
+			}
 
-            if (t.IsEnum) {
-                TypeCode underlyingTypeCode = Type.GetTypeCode(Enum.GetUnderlyingType(t));
+			if (t.IsEnum) {
+				TypeCode underlyingTypeCode = Type.GetTypeCode(Enum.GetUnderlyingType(t));
 
 				return (EqualityComparer<T>)RuntimeType.CreateInstanceForAnotherGenericParameter (typeof(EnumEqualityComparer<>), t);
-            }
-            return new ObjectEqualityComparer<T>();
+			}
+			return new ObjectEqualityComparer<T>();
 		}
 	}
 
