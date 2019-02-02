e9c6c86915307d2312c821d9c9dc84ea5d9e536d
Zoltan Varga <vargaz@gmail.com>
2019-02-02 14:40:56 -0500

    [netcore] Fix the layout of Assembly/MonoDomain classes. Disable contexts.

--- mcs/class/System.Private.CoreLib/Mono/MonoDomain.cs
+++ mcs/class/System.Private.CoreLib/Mono/MonoDomain.cs
@@ -4,9 +4,8 @@ using System.Runtime.InteropServices;
 
 namespace Mono
 {
-
 	[StructLayout (LayoutKind.Sequential)]
-	internal sealed partial class MonoDomain : MarshalByRefObject {
+	internal sealed partial class MonoDomain {
         #pragma warning disable 169
         #region Sync with object-internals.h
 		IntPtr _mono_app_domain;
