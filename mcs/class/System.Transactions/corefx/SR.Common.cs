63fe75c981b3b71e0cbcf78ee27a606c98c6bbc4
Maxim <hardhub@users.noreply.github.com>
2019-02-07 10:49:37 +0300

    Use external files from CoreFX for System.Transactions (#6972).

---
+++ mcs/class/System.Transactions/corefx/SR.Common.cs
@@ -0,0 +1,59 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+partial class SR
+{
+    // This method is used to decide if we need to append the exception message parameters to the message when calling SR.Format.
+    // by default it returns false.
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool UsingResourceKeys()
+    {
+        return false;
+    }
+
+    internal static string Format(string resourceFormat, params object[] args)
+    {
+        if (args != null)
+        {
+            if (UsingResourceKeys())
+            {
+                return resourceFormat + string.Join(", ", args);
+            }
+
+            return string.Format(resourceFormat, args);
+        }
+
+        return resourceFormat;
+    }
+    internal static string Format(string resourceFormat, object p1)
+    {
+        if (UsingResourceKeys())
+        {
+            return string.Join(", ", resourceFormat, p1);
+        }
+
+        return string.Format(resourceFormat, p1);
+    }
+    internal static string Format(string resourceFormat, object p1, object p2)
+    {
+        if (UsingResourceKeys())
+        {
+            return string.Join(", ", resourceFormat, p1, p2);
+        }
+
+        return string.Format(resourceFormat, p1, p2);
+    }
+    internal static string Format(string resourceFormat, object p1, object p2, object p3)
+    {
+        if (UsingResourceKeys())
+        {
+            return string.Join(", ", resourceFormat, p1, p2, p3);
+        }
+
+        return string.Format(resourceFormat, p1, p2, p3);
+    }
+}
