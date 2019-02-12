ddd250b80f70c6fb4c1fd1ea09047b1dc6f95aa3
Thays Grazia <thaystg@gmail.com>
2019-02-11 23:36:21 -0200

    Changing {

--- mcs/class/Mono.Debugger.Soft/Test/dtest.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -4829,8 +4829,7 @@ public class DebuggerTests
 
 	}
 	[Test]
-	public void IfPropertyStepping ()
-	{
+	public void IfPropertyStepping () {
 		Event e = run_until ("if_property_stepping");
 		var req = create_step (e);
 		req.Enable ();
