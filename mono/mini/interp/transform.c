3de427283e8962115211c02709b4e64d521adeb6
Vlad Brezae <brezaevlad@gmail.com>
2019-02-15 16:02:30 +0200

    [interp] Properly init locals
    When we have locals that need to be initialized.

--- mono/mini/interp/transform.c
+++ mono/mini/interp/transform.c
@@ -2461,13 +2461,8 @@ generate_code (TransformData *td, MonoMethod *method, MonoMethodHeader *header, 
 
 		td->body_start_offset = td->new_ip - td->new_code;
 
-		for (i = 0; i < header->num_locals; i++) {
-			int mt = mint_type(header->locals [i]);
-			if (mt == MINT_TYPE_VT || mt == MINT_TYPE_O || mt == MINT_TYPE_P) {
-				ADD_CODE(td, MINT_INITLOCALS);
-				break;
-			}
-		}
+		if (header->num_locals && header->init_locals)
+			ADD_CODE(td, MINT_INITLOCALS);
 
 		if (rtm->prof_flags & MONO_PROFILER_CALL_INSTRUMENTATION_ENTER)
 			ADD_CODE (td, MINT_PROF_ENTER);
