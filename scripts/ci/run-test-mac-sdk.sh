67dd16331ffc5e4a74bc5294ef8d0bc4f6fa9727
Ankit Jain <radical@gmail.com>
2019-02-18 15:25:56 -0500

    [msbuild] Bump to update-msbuild-16 branch HEAD
    .. and track the build command line changes.

--- scripts/ci/run-test-mac-sdk.sh
+++ scripts/ci/run-test-mac-sdk.sh
@@ -7,8 +7,8 @@ export PATH=${MONO_REPO_ROOT}/external/bockbuild/stage/bin:$PATH
 
 # Bundled MSBuild
 cd ${MONO_REPO_ROOT}/external/bockbuild/builds/msbuild-15/
-${TESTCMD} --label="msbuild-tests" --timeout=180m ./build.sh -hostType mono -configuration Release
-zip ${MONO_REPO_ROOT}/msbuild-test-results.zip artifacts/2/Release-MONO/TestResults/*
+${TESTCMD} --label="msbuild-tests" --timeout=180m ./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release
+zip ${MONO_REPO_ROOT}/msbuild-test-results.zip artifacts/2/TestResults/Release-MONO/* artifacts/2/log/Release-MONO/*.log
 
 # Bundled LLVM
 cd ${MONO_REPO_ROOT}/external/bockbuild/builds/mono
