6686f12d8c57c1f861e258d5e9e5e1367e1420a0
Ankit Jain <radical@gmail.com>
2019-02-18 15:25:56 -0500

    [msbuild] Bump to update-msbuild-16 branch HEAD
    .. and track the build command line changes.
    
    (cherry picked from commit 7100368ddb5821851e6ef19d262c64b0520fdf5d)

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -6,8 +6,8 @@ class MSBuild (GitHubPackage):
 			revision = '8b3a694c2c419582108588a7bd1133630a1b8077')
 
 	def build (self):
-		self.sh ('./build.sh -hostType mono -configuration Release -skipTests')
-		self.sh ('zip msbuild-bin-logs.zip artifacts/Release-MONO/log/*')
+		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
+		self.sh ('find artifacts -wholename \'*/log/*\' -type f -exec zip msbuild-bin-logs.zip {} \+')
 
 	def install (self):
 		# use the bootstrap msbuild as the system might not have one available!
