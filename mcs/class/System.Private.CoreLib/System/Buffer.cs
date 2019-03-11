164509cc89b9742cb4cf19e996056822bbeb03ac
Zoltan Varga <vargaz@gmail.com>
2019-03-11 05:35:50 -0400

    [netcore] Fix build.

--- mcs/class/System.Private.CoreLib/System/Buffer.cs
+++ mcs/class/System.Private.CoreLib/System/Buffer.cs
@@ -52,7 +52,7 @@ namespace System
 
 		static bool IsPrimitiveTypeArray (Array array)
 		{
-			throw new NotImplementedException ();
+			return array.GetType ().GetElementType ().IsPrimitive;
 		}
 
 		internal static unsafe void Memcpy (byte* dest, byte* src, int len) => Memmove (dest, src, (nuint) len);
