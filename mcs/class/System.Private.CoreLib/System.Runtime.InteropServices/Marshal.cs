cdfad074a17853caa0445dffdc2c594eecfa3abc
Egor Bogatov <egorbo@gmail.com>
2019-03-24 21:59:59 +0200

    Update Marshal.cs

--- mcs/class/System.Private.CoreLib/System.Runtime.InteropServices/Marshal.cs
+++ mcs/class/System.Private.CoreLib/System.Runtime.InteropServices/Marshal.cs
@@ -49,7 +49,7 @@ namespace System.Runtime.InteropServices
 
 		internal static bool IsPinnable (object obj)
 		{
-			return (obj == null) || RuntimeTypeHandle.HasReferences (obj.GetType () as RuntimeType));
+			return (obj == null) || RuntimeTypeHandle.HasReferences (obj.GetType () as RuntimeType);
 		}
 
 		// TODO: Should be called from Windows only code
