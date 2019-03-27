bff188abc469852c82b6e4e02c184373b8ff1a31
Zoltan Varga <vargaz@gmail.com>
2019-03-27 18:46:43 -0400

    [runtime] Revert daf92b9e62a847d521ae314c320d3502fefe217e. (#13696)
    Fixes https://github.com/mono/mono/issues/13607.
    
    (cherry picked from commit 5433bd030ea2d6645c333845408fb5e1b1bd5d5b)

--- mono/metadata/class.c
+++ mono/metadata/class.c
@@ -620,6 +620,7 @@ can_inflate_gparam_with (MonoGenericParam *gparam, MonoType *type)
 {
 	if (!mono_type_is_valid_generic_argument (type))
 		return FALSE;
+#if FALSE
 	/* Avoid inflating gparams with valuetype constraints with ref types during gsharing */
 	MonoGenericParamInfo *info = mono_generic_param_info (gparam);
 	if (info && (info->flags & GENERIC_PARAMETER_ATTRIBUTE_VALUE_TYPE_CONSTRAINT)) {
@@ -629,6 +630,7 @@ can_inflate_gparam_with (MonoGenericParam *gparam, MonoType *type)
 				return FALSE;
 		}
 	}
+#endif
 	return TRUE;
 }
 
