151e18a33262c3622005d68a43cfe4a6b3766313
Jay Krell <jaykrell@ntdev.microsoft.com>
2019-03-28 14:43:48 -0700

    PR: Restore case insensitivity testing on Windows. Tweak a little bit.

--- mono/unit-tests/test-path.c
+++ mono/unit-tests/test-path.c
@@ -11,14 +11,17 @@
 static char*
 make_path (const char *a, int itrail, int slash, int upcase)
 {
-	char trail [3] = {'/', '/'};
+	// Append 0, 1, or 2 trailing slashes to a.
+	g_assert (itrail >= 0 && itrail <= 2);
+	char trail [] = "//";
 	trail [itrail] = 0;
 	char *b = g_strdup_printf ("%s%s", a, trail);
+
 #ifdef HOST_WIN32
 	if (slash)
 		g_strdelimit (b, '/', '\\');
 	if (upcase)
-		g_strdelimit (b, 'f', 'F');
+		g_strdelimit (b, 'a', 'A');
 #endif
 	return b;
 }
@@ -26,8 +29,9 @@ make_path (const char *a, int itrail, int slash, int upcase)
 int
 main (void)
 {
-	static const char * const bases [2] = {"/", "/1"};
-	static const char * const files [6] = {"/1", "/1/2", "/1/2/3", "/12", "/2", "/2/2/"};
+	// Use letters not numbers in this data to exercise case insensitivity.
+	static const char * const bases [2] = {"/", "/a"};
+	static const char * const files [6] = {"/a", "/a/b", "/a/b/c", "/ab", "/b", "/b/b/"};
 
 	static const gboolean result [2][6] = {
 		{ TRUE, FALSE, FALSE, TRUE, TRUE, FALSE },
@@ -43,6 +47,7 @@ main (void)
 	const int win32 = 0;
 #endif
 
+	// Iterate a cross product.
 	for (int upcase_file = 0; upcase_file <= win32; ++upcase_file) {
 		for (int upcase_base = 0; upcase_base <= win32; ++upcase_base) {
 			for (int itrail_base = 0; itrail_base <= 2; ++itrail_base) {
@@ -51,6 +56,7 @@ main (void)
 						for (int ifile = 0; ifile < G_N_ELEMENTS (files); ++ifile) {
 							for (int islash_base = 0; islash_base <= win32; ++islash_base) {
 								for (int islash_file = 0; islash_file <= win32; ++islash_file) {
+
 									char *base = make_path (bases [ibase], itrail_base, islash_base, upcase_base);
 									char *file = make_path (files [ifile], itrail_file, islash_file, upcase_file);
 									//verbose && printf ("mono_path_filename_in_basedir (%s, %s)\n", file, base);
