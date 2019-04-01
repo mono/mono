mono/mono

commit 9ccfd778e34aa12cf718968381968a46451f6468

[System.Private.CoreLib/mscorlib] Implement Interlocked.MemoryBarrierProcessWide (#13712)
* Implement Interlocked.MemoryBarrierProcessWide

* Bump MONO_CORLIB_VERSION

* Bump API snapshot submodule

mono/utils/mono-membar.h
@@ -30,6 +30,10 @@ static inline void mono_memory_write_barrier (void)
 {
 }
 
+static inline void mono_memory_barrier_process_wide (void)
+{
+}
+
 #elif _MSC_VER
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
@@ -61,6 +65,8 @@ static inline void mono_memory_write_barrier (void)
 	_WriteBarrier ();
 	MemoryBarrier ();
 }
+
+void mono_memory_barrier_process_wide (void);
 #elif defined(USE_GCC_ATOMIC_OPS)
 static inline void mono_memory_barrier (void)
 {
@@ -76,6 +82,8 @@ static inline void mono_memory_write_barrier (void)
 {
 	mono_memory_barrier ();
 }
+
+void mono_memory_barrier_process_wide (void);
 #else
 #error "Don't know how to do memory barriers!"
 #endif

configure.ac
@@ -59,7 +59,7 @@ MONO_VERSION_BUILD=`echo $VERSION | cut -d . -f 3`
 # This line is parsed by tools besides autoconf, such as msvc/mono.winconfig.targets.
 # It should remain in the format they expect.
 #
-MONO_CORLIB_VERSION=92351299-EECB-4E61-A590-39382BA4F7A0
+MONO_CORLIB_VERSION=48B64EB1-FC8C-4707-A363-84A1FE488DCA
 
 #
 # Put a quoted #define in config.h.

mcs/class/System.Private.CoreLib/System.Threading/Interlocked.cs
@@ -69,6 +69,7 @@ namespace System.Threading
 
 		public static void MemoryBarrier () => Thread.MemoryBarrier ();
 
-		public static void MemoryBarrierProcessWide () => throw new NotImplementedException ();
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		public extern static void MemoryBarrierProcessWide ();
 	}
 }

mcs/class/corlib/System.Threading/Interlocked.cs
@@ -126,6 +126,7 @@ namespace System.Threading
 			Thread.MemoryBarrier ();
 		}
 
-		public static void MemoryBarrierProcessWide () => throw new PlatformNotSupportedException (); 
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		public extern static void MemoryBarrierProcessWide ();
 	}
 }

mono/metadata/icall-def.h
@@ -1064,7 +1064,8 @@ ICALL(ILOCK_18, "Exchange(object&,object)", ves_icall_System_Threading_Interlock
 NOHANDLES(ICALL(ILOCK_19, "Exchange(single&,single)", ves_icall_System_Threading_Interlocked_Exchange_Single))
 NOHANDLES(ICALL(ILOCK_20, "Increment(int&)", ves_icall_System_Threading_Interlocked_Increment_Int))
 NOHANDLES(ICALL(ILOCK_21, "Increment(long&)", ves_icall_System_Threading_Interlocked_Increment_Long))
-NOHANDLES(ICALL(ILOCK_22, "Read(long&)", ves_icall_System_Threading_Interlocked_Read_Long))
+NOHANDLES(ICALL(ILOCK_22, "MemoryBarrierProcessWide", ves_icall_System_Threading_Interlocked_MemoryBarrierProcessWide))
+NOHANDLES(ICALL(ILOCK_23, "Read(long&)", ves_icall_System_Threading_Interlocked_Read_Long))
 
 #ifndef ENABLE_NETCORE
 ICALL_TYPE(ITHREAD, "System.Threading.InternalThread", ITHREAD_1)

mono/metadata/threads-types.h
@@ -216,6 +216,9 @@ ICALL_EXPORT
 gint64 ves_icall_System_Threading_Interlocked_Decrement_Long(gint64 * location);
 
 ICALL_EXPORT
+void ves_icall_System_Threading_Interlocked_MemoryBarrierProcessWide (void);
+
+ICALL_EXPORT
 gint8 ves_icall_System_Threading_Thread_VolatileRead1 (void *ptr);
 
 ICALL_EXPORT

mono/metadata/threads.c
@@ -2534,6 +2534,12 @@ ves_icall_System_Threading_Interlocked_Read_Long (gint64 *location)
 }
 
 void
+ves_icall_System_Threading_Interlocked_MemoryBarrierProcessWide (void)
+{
+	mono_memory_barrier_process_wide ();
+}
+
+void
 ves_icall_System_Threading_Thread_MemoryBarrier (void)
 {
 	mono_memory_barrier ();

mono/utils/mono-threads-posix.c
@@ -43,9 +43,18 @@ extern int tkill (pid_t tid, int signal);
 
 #include <pthread.h>
 
+#include <sys/mman.h>
+#include <limits.h>    /* for PAGESIZE */
+#ifndef PAGESIZE
+#define PAGESIZE 4096
+#endif
+
 #ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #endif
+
+static pthread_mutex_t memory_barrier_process_wide_mutex = PTHREAD_MUTEX_INITIALIZER;
+static void *memory_barrier_process_wide_helper_page;
 
 gboolean
 mono_thread_platform_create_thread (MonoThreadStart thread_fn, gpointer thread_data, gsize* const stack_size, MonoNativeThreadId *tid)
@@ -298,6 +307,36 @@ mono_native_thread_join (MonoNativeThreadId tid)
 	void *res;
 
 	return !pthread_join (tid, &res);
+}
+
+void
+mono_memory_barrier_process_wide (void)
+{
+	int status;
+
+	status = pthread_mutex_lock (&memory_barrier_process_wide_mutex);
+	g_assert (status == 0);
+
+	if (memory_barrier_process_wide_helper_page == NULL) {
+		status = posix_memalign(&memory_barrier_process_wide_helper_page, PAGESIZE, PAGESIZE);
+		g_assert (status == 0);
+	}
+
+	// Changing a helper memory page protection from read / write to no access
+	// causes the OS to issue IPI to flush TLBs on all processors. This also
+	// results in flushing the processor buffers.
+	status = mprotect (memory_barrier_process_wide_helper_page, PAGESIZE, PROT_READ | PROT_WRITE);
+	g_assert (status == 0);
+
+	// Ensure that the page is dirty before we change the protection so that
+	// we prevent the OS from skipping the global TLB flush.
+	__sync_add_and_fetch ((size_t*)memory_barrier_process_wide_helper_page, 1);
+
+	status = mprotect (memory_barrier_process_wide_helper_page, PAGESIZE, PROT_NONE);
+	g_assert (status == 0);
+
+	status = pthread_mutex_unlock (&memory_barrier_process_wide_mutex);
+	g_assert (status == 0);
 }
 
 #endif /* defined(_POSIX_VERSION) */

commit acb8dd1183acb4ed4b9b967004a0499cf44de45c

Fix WASM build (#13763)
Broken by https://github.com/mono/mono/pull/13712.

mono/utils/mono-membar.h
@@ -30,10 +30,6 @@ static inline void mono_memory_write_barrier (void)
 {
 }
 
-static inline void mono_memory_barrier_process_wide (void)
-{
-}
-
 #elif _MSC_VER
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
@@ -65,8 +61,6 @@ static inline void mono_memory_write_barrier (void)
 	_WriteBarrier ();
 	MemoryBarrier ();
 }
-
-void mono_memory_barrier_process_wide (void);
 #elif defined(USE_GCC_ATOMIC_OPS)
 static inline void mono_memory_barrier (void)
 {
@@ -82,10 +76,10 @@ static inline void mono_memory_write_barrier (void)
 {
 	mono_memory_barrier ();
 }
-
-void mono_memory_barrier_process_wide (void);
 #else
 #error "Don't know how to do memory barriers!"
 #endif
+
+void mono_memory_barrier_process_wide (void);
 
 #endif	/* _MONO_UTILS_MONO_MEMBAR_H_ */

mono/utils/mono-threads-wasm.c
@@ -187,4 +187,9 @@ mono_background_exec (void)
 	g_slist_free (j);
 }
 
+void
+mono_memory_barrier_process_wide (void)
+{
+}
+
 #endif