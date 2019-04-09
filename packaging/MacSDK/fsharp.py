dacacde49dc506c7cd022b02d8b82c57072d9cd2
Alexis Christoforides <alexis@thenull.net>
2019-04-09 19:05:50 -0400

    Use a custom prefix instead of tryiing to infer it, which doesn't work under bockbuild.
    The following line is problematic in the scope of building the Mac SDK package (using bockbuild):
    
    https://github.com/fsharp/fsharp/blob/ccfccbacfdb16b2ab143cf83f5ece20f738fd864/mono/config.make#L5
    
    It's problematic because where `mono` lives in this case would be something like `/macsdk-staging-dir/bin/mono` which is not the actual prefix (prefix here being the final destination, `/Library/Frameworks/Mono.framework/...`) but a staging area that bockbuild uses (this is how we build on system prefixes without installing on CI)
    
    For the F# build, bockbuild expects things to end up  in `/fsharp-staging-dir/$(prefix)` (because that's what most `make install`s do when given `DESTDIR=/fsharp-staging-dir/`). If things end up in _any other_ directory in `/fsharp-staging-dir/`  (like `/fsharp-staging-dir/macsdk-staging-dir` in our case) that would cause us to miss packaging those files (this has happened before, with packages that have broken or different use of `DESTDIR`). So there is an added write-protection for anywhere in `/fsharp-staging-dir` that *isn't*  `/fsharp-staging-dir/$(prefix)` , because a write-protection error, while obtuse, is much preferable to missing the fact that some files are not packaged (as would have happened here)
    
    So the fix is to stop trying to infer the prefix, and return to providing it explicitly to the build system (this used to be the case via the `configure` step)

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -5,14 +5,15 @@ class FsharpPackage(GitHubTarballPackage):
             '4.5.0',
             '3de387432de8d11a89f99d1af87aa9ce194fe21b',
             configure='',
-            override_properties={ 'make': 'make all install' })
+            override_properties={ 'make': 'make all install PREFIX="%{package_prefix}"' })
 
         self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
         self.sources.extend(['patches/fsharp-IsPathRooted-type-inference.patch',
                              'patches/fsharp-portable-pdb.patch',
                              'patches/fsharp-noinstall.patch',
                              'patches/fsharp-GetFileNameWithoutExtension-type-inference.patch',
-                             'patches/fsharp-msbuild-16-0.patch'])
+                             'patches/fsharp-msbuild-16-0.patch',
+                             'patches/fsharp-custom-prefix.patch'])
 
     def prep(self):
         Package.prep(self)
