3756f4a33098e34efe7db53e26cc486e2e1b419a
Alexis Christoforides <alexis@thenull.net>
2019-04-10 00:37:22 -0400

    Also, if we are doing an override of the 'make' command, we need to take care of passing DESTDIR ourselves too

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -5,7 +5,7 @@ class FsharpPackage(GitHubTarballPackage):
             '4.5.0',
             '3de387432de8d11a89f99d1af87aa9ce194fe21b',
             configure='',
-            override_properties={ 'make': 'make all install PREFIX="%{package_prefix}"' })
+            override_properties={ 'make': 'make all install PREFIX="%{package_prefix}" DESTDIR=%{stage_root}' })
 
         self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
         self.sources.extend(['patches/fsharp-IsPathRooted-type-inference.patch',
