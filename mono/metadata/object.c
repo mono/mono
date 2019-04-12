19e8d3bdee60bae2c7a2b01f52d2c81c43c8f6a4
Thays Grazia <thaystg@gmail.com>
2019-04-12 18:15:20 -0300

    Trying to box a null value crashes inside mono_gc_wbarrier_value_copy_internal. As described by the customer using version 5.18.0.240 the crash doesn't happen. As I could debug using this version doesn't crash only because is ignoring the SIGSEGV, when I attach to LLDB I can see the same crash in both versions. I protected to only add data to res_handle if there is a valid value. Fixes #13969

--- mono/metadata/object.c
+++ mono/metadata/object.c
@@ -6946,37 +6946,38 @@ mono_value_box_handle (MonoDomain *domain, MonoClass *klass, gpointer value, Mon
 	return_val_if_nok (error, NULL_HANDLE);
 
 	size -= MONO_ABI_SIZEOF (MonoObject);
-
-	if (mono_gc_is_moving ()) {
-		g_assert (size == mono_class_value_size (klass, NULL));
-		MONO_ENTER_NO_SAFEPOINTS;
-		gpointer data = mono_handle_get_data_unsafe (res_handle);
-		mono_gc_wbarrier_value_copy_internal (data, value, 1, klass);
-		MONO_EXIT_NO_SAFEPOINTS;
-	} else {
-		MONO_ENTER_NO_SAFEPOINTS;
-		gpointer data = mono_handle_get_data_unsafe (res_handle);
-#if NO_UNALIGNED_ACCESS
-		mono_gc_memmove_atomic (data, value, size);
-#else
-		switch (size) {
-		case 1:
-			*(guint8*)data = *(guint8 *) value;
-			break;
-		case 2:
-			*(guint16 *)(data) = *(guint16 *) value;
-			break;
-		case 4:
-			*(guint32 *)(data) = *(guint32 *) value;
-			break;
-		case 8:
-			*(guint64 *)(data) = *(guint64 *) value;
-			break;
-		default:
+	if (value != NULL) { 
+		if (mono_gc_is_moving ()) {
+			g_assert (size == mono_class_value_size (klass, NULL));
+			MONO_ENTER_NO_SAFEPOINTS;
+			gpointer data = mono_handle_get_data_unsafe (res_handle);
+			mono_gc_wbarrier_value_copy_internal (data, value, 1, klass);
+			MONO_EXIT_NO_SAFEPOINTS;
+		} else {
+			MONO_ENTER_NO_SAFEPOINTS;
+			gpointer data = mono_handle_get_data_unsafe (res_handle);
+	#if NO_UNALIGNED_ACCESS
 			mono_gc_memmove_atomic (data, value, size);
+	#else
+			switch (size) {
+			case 1:
+				*(guint8*)data = *(guint8 *) value;
+				break;
+			case 2:
+				*(guint16 *)(data) = *(guint16 *) value;
+				break;
+			case 4:
+				*(guint32 *)(data) = *(guint32 *) value;
+				break;
+			case 8:
+				*(guint64 *)(data) = *(guint64 *) value;
+				break;
+			default:
+				mono_gc_memmove_atomic (data, value, size);
+			}
+	#endif
+			MONO_EXIT_NO_SAFEPOINTS;
 		}
-#endif
-		MONO_EXIT_NO_SAFEPOINTS;
 	}
 	if (m_class_has_finalize (klass))
 		mono_object_register_finalizer_handle (res_handle);
