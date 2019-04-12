592c75cf1f76a21ac655f9d57f5021cdffdf9589
Ankit Jain <radical@gmail.com>
2019-04-12 02:09:15 -0400

    Bump msbuild to track mono-2018-08-test (#13927)
    Bump to get the msbuild 16 update
    <!--
    Thank you for your Pull Request!
    
    If you are new to contributing to Mono, please try to do your best at conforming to our coding guidelines http://www.mono-project.com/community/contributing/coding-guidelines/ but don't worry if you get something wrong. One of the project members will help you to get things landed.
    
    Does your pull request fix any of the existing issues? Please use the following format: Fixes #issue-number
    -->

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,12 +3,11 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '804bde742bdf9d65c7ceb672a3d5400c0c22e628')
+			revision = '8b3a694c2c419582108588a7bd1133630a1b8077')
 
 	def build (self):
-		self.sh ('mono --version')
-		self.sh ('./build.sh -hostType mono -configuration Release -skipTests')
-		self.sh ('zip msbuild-bin-logs.zip artifacts/Release-MONO/log/*')
+		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
+		self.sh ('find artifacts -wholename \'*/log/*\' -type f -exec zip msbuild-bin-logs.zip {} \+')
 
 	def install (self):
 		# use the bootstrap msbuild as the system might not have one available!
--- tools/nuget-hash-extractor/download.sh
+++ tools/nuget-hash-extractor/download.sh
@@ -61,6 +61,11 @@ for ver in $MS_EXTN_VERSIONS; do
 	curl -L https://dotnet.myget.org/F/dotnet-core/api/v2/package/Microsoft.NET.Build.Extensions/${ver} -o nugets/microsoft.net.build.extensions.${ver}.nupkg
 done
 
-curl https://dotnetfeed.blob.core.windows.net/dotnet-core/flatcontainer/microsoft.net.build.extensions/2.1.300-preview3-62804-06/microsoft.net.build.extensions.2.1.300-preview3-62804-06.nupkg -o nugets//microsoft.net.build.extensions.2.1.300-preview3-62804-06.nupkg
+MS_EXTN_VERSIONS_DOTNETFEED="2.1.300-preview3-62804-06" 			    # https://github.com/dotnet/cli/blob/8c937a0db08e56660aca456ac088f2d0e70735ab/build/DependencyVersions.props
+MS_EXTN_VERSIONS_DOTNETFEED="$MS_EXTN_VERSIONS_DOTNETFEED 2.1.600-preview-63821-02" # https://github.com/dotnet/cli/blob/4b9d6502f8061db4a56b730d3d4e65262c72ad5c/build/DependencyVersions.props
+
+for ver in $MS_EXTN_VERSIONS_DOTNETFEED; do
+	curl https://dotnetfeed.blob.core.windows.net/dotnet-core/flatcontainer/microsoft.net.build.extensions/${ver}/microsoft.net.build.extensions.${ver}.nupkg -o nugets//microsoft.net.build.extensions.${ver}.nupkg
+done
 
 touch .download_stamp_file
