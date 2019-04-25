0fe5e8cd506454657acdbbff508cdfcf877b8b1c
Egor Bogatov <egorbo@gmail.com>
2019-04-25 18:57:03 +0300

    replace mvc with web sample

--- netcore/sample/AspNetCore/Program.cs
+++ netcore/sample/AspNetCore/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AspNetMvc
+namespace AspNetCore
 {
     public class Program
     {
