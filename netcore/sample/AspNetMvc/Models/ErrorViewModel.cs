9c899ddffd07e734582744b89ad3cc19a48a61fa
Egor Bogatov <egorbo@gmail.com>
2019-04-25 14:02:23 +0300

    ASP.NET Core sample

---
+++ netcore/sample/AspNetMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
