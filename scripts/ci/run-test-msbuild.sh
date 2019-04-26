c452b246e8ad6c14a8540653b1083f16bc5ee72b
Katelyn Gadd <kg@luminance.org>
2019-04-26 16:13:22 -0700

    Fix cert-sync args and re-enable autogen

--- scripts/ci/run-test-msbuild.sh
+++ scripts/ci/run-test-msbuild.sh
@@ -3,14 +3,14 @@
 OLD_CWD=`pwd`
 TESTCMD=`realpath ${TESTCMD}`
 
-# ${TESTCMD} --label=remove-mono-temp-dir --timeout=1m rm -rf /tmp/xplat-master
-# ${TESTCMD} --label=autogen --timeout=15m ./autogen.sh --prefix=/tmp/mono-from-source
+${TESTCMD} --label=remove-mono-temp-dir --timeout=1m rm -rf /tmp/xplat-master
+${TESTCMD} --label=autogen --timeout=15m ./autogen.sh --prefix=/tmp/mono-from-source
 ${TESTCMD} --label=make --timeout=30m make PROFILE=net_4_x -j 2
 ${TESTCMD} --label=make-install --timeout=15m make install
 
 OLD_PATH=${PATH}
 export PATH=/tmp/mono-from-source/bin/:${OLD_PATH}
-${TESTCMD} --label=cert-sync --timeout=15m cert-sync
+${TESTCMD} --label=cert-sync --timeout=15m cert-sync /etc/ssl/certs/ca-certificates.crt
 
 ${TESTCMD} --label=remove-msbuild-temp-dir --timeout=1m rm -rf /tmp/xplat-master
 ${TESTCMD} --label=clone-msbuild --timeout=5m git clone https://github.com/mono/msbuild.git /tmp/xplat-master
