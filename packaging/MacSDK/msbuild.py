b24062d57744bd58d4fe80bd8af73aa71d7625ad
Ankit Jain <radical@gmail.com>
2019-04-29 17:10:36 -0400

    Bump msbuild to track mono-2019-02

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '8b3a694c2c419582108588a7bd1133630a1b8077')
+			revision = '5504cdefce9a8e7a590134c11be3bdce1329dcba')
 
 	def build (self):
 		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
