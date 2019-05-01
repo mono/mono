805ea2b59a26aa1eed490c42da06ed901f0472d2
Katelyn Gadd <kg@luminance.org>
2019-05-01 16:24:53 -0700

    Checkpoint

--- scripts/ci/run-test-msbuild.sh
+++ scripts/ci/run-test-msbuild.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -u
+set -e
+set -x
 
 OLD_CWD=`pwd`
 TESTCMD=`realpath ${TESTCMD}`
@@ -12,7 +16,7 @@ OLD_PATH=${PATH}
 export PATH=/tmp/mono-from-source/bin/:${OLD_PATH}
 ${TESTCMD} --label=cert-sync --timeout=15m cert-sync /etc/ssl/certs/ca-certificates.crt
 
-${TESTCMD} --label=remove-msbuild-temp-dir --timeout=1m rm -rf /tmp/xplat-master
+rm -rf /tmp/xplat-master || true
 
 MSBUILD_REVISION=`grep -Po "(?<=revision \= \')[0-9a-f]+" packaging/MacSDK/msbuild.py`
 echo Detected msbuild hash ${MSBUILD_REVISION} in MacSDK/msbuild.py
@@ -23,10 +27,14 @@ ${TESTCMD} --label=checkout-xplat-master --timeout=5m git checkout ${MSBUILD_REV
 
 ${TESTCMD} --label=compile-msbuild --timeout=15m ./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release /p:DisableNerdbankVersioning=true /p:CreateBootstrap=true "/p:Projects=/tmp/xplat-master/src/MSBuild.Bootstrap/MSBuild.Bootstrap.csproj" /p:AssemblyVersion=15.1.0.0
 
-${TESTCMD} --label=check-for-dll --timeout=1m test -s /tmp/xplat-master/artifacts/bin/MSBuild/*-MONO/net472/MSBuild.dll
+MONO=/tmp/mono-from-source/bin/mono
+DLL_PATH=`realpath /tmp/xplat-master/artifacts/2/bin/MSBuild.Bootstrap/*-MONO/net472/MSBuild.dll`
+EXE_PATH=mcs/class/lib/net_4_x-*/culevel.exe
 
-DLL_PATH=`/tmp/xplat-master/artifacts/bin/MSBuild/*-MONO/net472/MSBuild.dll`
+${TESTCMD} --label=check-for-dll --timeout=1m test -s ${DLL_PATH}
 
-${TESTCMD} --label=remove-culevel --timeout=1m rm mcs/class/lib/net_4_x-*/culevel.exe
+rm -f ${EXE_PATH} || true
 
-${TESTCMD} --label=try-to-use-built-msbuild-to-build-culevel --timeout=1m mono ${DLL_PATH} ${OLD_CWD}/mcs/class/tools/culevel/culevel.csproj
+${TESTCMD} --label=try-to-use-built-msbuild-to-build-culevel --timeout=1m ${MONO} ${DLL_PATH} ${OLD_CWD}/mcs/tools/culevel/culevel.csproj /p:BuildProjectReferences=false
+
+${TESTCMD} --label=check-for-culevel-exe --timeout=1m test -s ${EXE_PATH}
