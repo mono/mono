f8428761f4abac4be5d2451374f77ab718a74d81
Kenneth Pouncey <kjpou@pt.lu>
2019-05-07 11:40:08 +0200

    Formatting

--- sdks/wasm/tests/browser/ZipTestSuite.cs
+++ sdks/wasm/tests/browser/ZipTestSuite.cs
@@ -8,54 +8,48 @@ using System.Threading;
 using System.IO;
 using System.IO.Compression;
 
-namespace TestSuite
-{
-    public class Program
-    {
+namespace TestSuite {
+	public class Program {
 
-		public static async Task<object> ZipGetEntryReadMode(string zipEntry)
+		public static async Task<object> ZipGetEntryReadMode (string zipEntry)
 		{
-            var requestTcs = new TaskCompletionSource<object>();
+			var requestTcs = new TaskCompletionSource<object> ();
 
-            using (HttpClient client = CreateHttpClient())
-            using (Stream stream = await client.GetStreamAsync("base/publish/archive.zip"))
-            using (var memoryStream = new MemoryStream())
-            {
-                await stream.CopyToAsync(memoryStream);
-                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
-                {
-                    var entry = archive.GetEntry(zipEntry);
-                    requestTcs.SetResult(entry);
-                }
-            }
-            return requestTcs.Task;
+			using (HttpClient client = CreateHttpClient ())
+			using (Stream stream = await client.GetStreamAsync ("base/publish/archive.zip"))
+			using (var memoryStream = new MemoryStream ()) {
+				await stream.CopyToAsync (memoryStream);
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Read)) {
+					var entry = archive.GetEntry (zipEntry);
+					requestTcs.SetResult (entry);
+				}
+			}
+			return requestTcs.Task;
 
 		}
-        public static async Task<object> GetStreamAsync_ReadZeroBytes_Success()
-        {
-            var requestTcs = new TaskCompletionSource<object>();
+		public static async Task<object> GetStreamAsync_ReadZeroBytes_Success ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
 
-            using (HttpClient client = CreateHttpClient())
-            using (Stream stream = await client.GetStreamAsync("base/publish/NowIsTheTime.txt"))
-            {
-                requestTcs.SetResult(await stream.ReadAsync(new byte[1], 0, 0));
-            }
+			using (HttpClient client = CreateHttpClient ())
+			using (Stream stream = await client.GetStreamAsync ("base/publish/NowIsTheTime.txt")) {
+				requestTcs.SetResult (await stream.ReadAsync (new byte [1], 0, 0));
+			}
 
-            return requestTcs.Task;
-        }
+			return requestTcs.Task;
+		}
 
-        static HttpClient CreateHttpClient()
-        {
-            //Console.WriteLine("Create  HttpClient");
-            string BaseApiUrl = string.Empty;
-            var window = (JSObject)WebAssembly.Runtime.GetGlobalObject("window");
-            using (var location = (JSObject)window.GetObjectProperty("location"))
-            {
-                BaseApiUrl = (string)location.GetObjectProperty("origin");
-            }
-            WasmHttpMessageHandler.StreamingEnabled = true;
-            return new HttpClient() { BaseAddress = new Uri(BaseApiUrl) };
-        }
-    }
-   
+		static HttpClient CreateHttpClient ()
+		{
+			//Console.WriteLine("Create  HttpClient");
+			string BaseApiUrl = string.Empty;
+			var window = (JSObject)WebAssembly.Runtime.GetGlobalObject ("window");
+			using (var location = (JSObject)window.GetObjectProperty ("location")) {
+				BaseApiUrl = (string)location.GetObjectProperty ("origin");
+			}
+			WasmHttpMessageHandler.StreamingEnabled = true;
+			return new HttpClient () { BaseAddress = new Uri (BaseApiUrl) };
+		}
+	}
+
 }
