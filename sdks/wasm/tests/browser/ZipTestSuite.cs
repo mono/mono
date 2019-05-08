e29cf895ee1ab8fd3ed3b3cb8fc81744592b4ccb
Kenneth Pouncey <kjpou@pt.lu>
2019-05-08 12:07:46 +0200

    Add more Zip tests

--- sdks/wasm/tests/browser/ZipTestSuite.cs
+++ sdks/wasm/tests/browser/ZipTestSuite.cs
@@ -7,6 +7,7 @@ using WebAssembly;
 using System.Threading;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace TestSuite {
 	public class Program {
@@ -91,12 +92,184 @@ namespace TestSuite {
 
 		}
 
+		public static async Task<object> ZipOpenCloseAndReopenEntry ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+
+			using (var memoryStream = await GetArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Update)) {
+					var entry = archive.GetEntry ("foo.txt");
+					var stream = entry.Open ();
+					stream.Dispose ();
+					stream = entry.Open ();
+					requestTcs.SetResult (true);
+				}
+			}
+			return requestTcs.Task;
+
+		}
+		public static async Task<object> ZipDeleteEntryCheckEntries ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+
+			using (var memoryStream = await GetArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Update)) {
+					var entry = archive.GetEntry ("foo.txt");
+					entry.Delete ();
+					requestTcs.SetResult (archive.Entries.FirstOrDefault (e => e == entry));
+				}
+			}
+			return requestTcs.Task;
+
+		}
+		public static async Task<object> ZipGetEntryDeleteUpdateMode ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+
+			using (var memoryStream = await GetArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Update, true)) {
+					var entry = archive.GetEntry ("foo.txt");
+					entry.Delete ();
+				}
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Read)) {
+					var entry = archive.GetEntry ("foo.txt");
+					requestTcs.SetResult (entry);
+				}
+
+			}
+			return requestTcs.Task;
+
+		}
+
+		public static Task<object> ZipCreateArchive ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+
+			using (var memoryStream = new MemoryStream ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Create, true)) {
+					var dir = archive.CreateEntry ("foobar/");
+
+					var entry = archive.CreateEntry ("foo.txt");
+					using (var stream = entry.Open ()) {
+						using (var streamWriter = new StreamWriter (stream))
+							streamWriter.Write ("foo");
+					}
+				}
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Read)) {
+					var returnArray = new WebAssembly.Core.Array ();
+					returnArray.Push (archive.GetEntry ("foobar/"));
+
+					var entry = archive.GetEntry ("foo.txt");
+					returnArray.Push (entry);
+
+					var streamReader = new StreamReader (entry.Open ());
+					var text = streamReader.ReadToEnd ();
+
+					returnArray.Push (text);
+
+					requestTcs.SetResult (returnArray);
+				}
+
+			}
+			return requestTcs.Task;
+
+		}
+		public static async Task<object> ZipEnumerateEntriesModifiedTime ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+			var date = DateTimeOffset.Now;
+			using (var memoryStream = await GetArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Update, true)) {
+					var entry = archive.GetEntry ("foo.txt");
+					entry.LastWriteTime = date;
+				}
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Read)) {
+					var returnArray = new WebAssembly.Core.Array ();
+					var entry = archive.GetEntry ("foo.txt");
+					returnArray.Push (entry.LastWriteTime.Year);
+					returnArray.Push (entry.LastWriteTime.Month);
+					returnArray.Push (entry.LastWriteTime.Day);
+
+					requestTcs.SetResult (returnArray);
+				}
+
+			}
+			return requestTcs.Task;
+
+		}
+		public static async Task<object> ZipEnumerateArchiveDefaultLastWriteTime ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+			using (var memoryStream = await GetPackageArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, ZipArchiveMode.Read)) {
+					var entry = archive.GetEntry ("_rels/.rels");
+					requestTcs.SetResult (entry.LastWriteTime.Ticks);
+				}
+
+			}
+			return requestTcs.Task;
+
+		}
+
+		public static async Task<WebAssembly.Core.Array> ZipGetArchiveEntryStreamLengthPosition (ZipArchiveMode mode)
+		{
+			var requestTcs = new TaskCompletionSource<WebAssembly.Core.Array> ();
+
+			using (var memoryStream = await GetPackageArchiveStreamAsync ()) {
+				using (var archive = new ZipArchive (memoryStream, mode)) {
+					var resultsArray = new WebAssembly.Core.Array ();
+					var entry = archive.GetEntry ("_rels/.rels");
+
+					using (var stream = entry.Open ()) {
+						resultsArray.Push ((double)stream.Position);
+						resultsArray.Push ((double)stream.Length);
+					}
+
+					var entry2 = archive.GetEntry ("modernhttpclient.nuspec");
+					using (var stream = entry2.Open ()) {
+						// .NET does not support these in Read mode
+						if (mode == ZipArchiveMode.Update) {
+							resultsArray.Push ((double)stream.Length);
+							resultsArray.Push ((double)stream.Position);
+						}
+					}
+					requestTcs.SetResult (resultsArray);
+				}
+			}
+
+			return await requestTcs.Task;
+		}
+		public static async Task<object> ZipGetArchiveEntryStreamLengthPositionReadMode ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+			var resultsArray = await ZipGetArchiveEntryStreamLengthPosition (ZipArchiveMode.Read);
+			requestTcs.SetResult (resultsArray);
+			return requestTcs.Task;
+
+		}
+		public static async Task<object> ZipGetArchiveEntryStreamLengthPositionUpdateMode ()
+		{
+			var requestTcs = new TaskCompletionSource<object> ();
+			var resultsArray = await ZipGetArchiveEntryStreamLengthPosition (ZipArchiveMode.Update);
+			requestTcs.SetResult (resultsArray);
+			return requestTcs.Task;
+
+		}
+
 		static async Task<MemoryStream> GetArchiveStreamAsync ()
 		{
-			MemoryStream archiveStream;
+			MemoryStream archiveStream = new MemoryStream ();
 			using (HttpClient client = CreateHttpClient ())
 			using (Stream stream = await client.GetStreamAsync ("base/publish/archive.zip")) {
-				archiveStream = new MemoryStream ();
+				await stream.CopyToAsync (archiveStream);
+			}
+			return archiveStream;
+		}
+		static async Task<MemoryStream> GetPackageArchiveStreamAsync ()
+		{
+			MemoryStream archiveStream = new MemoryStream ();
+			using (HttpClient client = CreateHttpClient ())
+			using (Stream stream = await client.GetStreamAsync ("base/publish/test.nupkg")) {
 				await stream.CopyToAsync (archiveStream);
 			}
 			return archiveStream;
@@ -105,11 +278,10 @@ namespace TestSuite {
 		static HttpClient CreateHttpClient ()
 		{
 			string BaseApiUrl = string.Empty;
-			var window = (JSObject)WebAssembly.Runtime.GetGlobalObject ("window");
+			using (var window = (JSObject)WebAssembly.Runtime.GetGlobalObject ("window"))
 			using (var location = (JSObject)window.GetObjectProperty ("location")) {
 				BaseApiUrl = (string)location.GetObjectProperty ("origin");
 			}
-			WasmHttpMessageHandler.StreamingEnabled = true;
 			return new HttpClient () { BaseAddress = new Uri (BaseApiUrl) };
 		}
 	}
--- sdks/wasm/tests/browser/zip-spec.js
+++ sdks/wasm/tests/browser/zip-spec.js
@@ -174,6 +174,185 @@ describe("The WebAssembly Zip Test Suite",function(){
       
     }, DEFAULT_TIMEOUT);
 
+    it('ZipOpenCloseAndReopenEntry: opening, closing and then reopening should not fail.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipOpenCloseAndReopenEntry", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.isTrue(result, "result should be true the opened entry.");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
 
+      );
+      
+    }, DEFAULT_TIMEOUT);
+
+    it('ZipDeleteEntryCheckEntries: should delete entry.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipDeleteEntryCheckEntries", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.isUndefined(result, "result should be undefined.");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);
+
+    it('ZipGetEntryDeleteUpdateMode: should delete entry in update mode.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipGetEntryDeleteUpdateMode", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.isUndefined(result, "result should be undefined.");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
+
+    it('ZipCreateArchive: should create archive.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipCreateArchive", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.equal(result.length, 3, "result length doesn't match 3");
+              assert.isDefined(result[0], "result[0] should be an object");
+              assert.isDefined(result[1], "result[1] should be an object");
+              assert.equal(result[2], "foo", "result[2] doesn't match text foo");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
+
+    it('ZipEnumerateEntriesModifiedTime: should enumerate entry modified time.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipEnumerateEntriesModifiedTime", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            var resultNow = new Date();
+            try {
+              assert.equal(result.length, 3, "result length doesn't match 3");
+              assert.equal(result[0], resultNow.getFullYear() , "result[0] doesn't match current year");
+              // Month is a zero-based value (where zero indicates the first month of the year)
+              assert.equal(result[1], resultNow.getMonth() + 1, "result[1] doesn't match current month");
+              assert.equal(result[2], resultNow.getDate(), "result[2] doesn't match current day");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
+
+    it('ZipEnumerateArchiveDefaultLastWriteTime: should return archive last write time.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipEnumerateArchiveDefaultLastWriteTime", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.equal(result, 0, "result does not match .NET ticks");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
+
+    it('ZipGetArchiveEntryStreamLengthPositionReadMode: should return archive entry length values.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipGetArchiveEntryStreamLengthPositionReadMode", []).then(
+        (result) => 
+        {
+            //console.log("we are here: " + result);
+            try {
+              assert.equal(result.length, 2, "result does not match expected result values");
+              assert.equal(result[0], 0, "result[0] does not match expected result position: 0");
+              assert.equal(result[1], 425, "result[0] does not match expected result length: 425");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
+
+    it('ZipGetArchiveEntryStreamLengthPositionUpdateMode: should return archive entry length values.', (done) => {
+      //karmaHTML.zipspec.document gives the access to the Document object of 'http-spec.html' file
+      var _document = karmaHTML.zipspec.document;
+      
+      _document.Module.BINDING.call_static_method("[ZipTestSuite]TestSuite.Program:ZipGetArchiveEntryStreamLengthPositionUpdateMode", []).then(
+        (result) => 
+        {
+            try {
+              assert.equal(result.length, 4, "result does not match expected result values");
+              assert.equal(result[0], 0, "result[0] does not match expected result position: 0");
+              assert.equal(result[1], 425, "result[0] does not match expected result length: 425");
+              assert.equal(result[2], 857, "result[0] does not match expected result length.");
+              assert.equal(result[3], 0, "result[0] does not match expected result position.");
+              done()
+            } catch (e) {
+              done.fail(e);
+            }
+        },
+        (error) => done.fail(error)
+
+      );
+      
+    }, DEFAULT_TIMEOUT);    
     
   });
