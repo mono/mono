6961b064c7413f517014e8776e923d3d55a6abf4
Martin Baulig <mabaul@microsoft.com>
2019-05-16 15:26:30 -0700

    X

--- mcs/class/corlib/corefx/GlobalizationMode.cs
+++ mcs/class/corlib/corefx/GlobalizationMode.cs
@@ -1,7 +1,13 @@
 namespace System.Globalization
 {
-    partial class GlobalizationMode
-    {
-        private static bool GetGlobalizationInvariantMode () => false;
-    }
+	partial class GlobalizationMode
+	{
+#if WASM
+		internal const bool Invariant = true;
+#else
+		internal static bool Invariant { get; } = GetGlobalizationInvariantMode();
+
+		static bool GetGlobalizationInvariantMode () => false;
+#endif
+	}
 }
