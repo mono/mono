a6c2c984347283fe43b93eab5b6ad71e0681429a
Egor Bogatov <egorbo@gmail.com>
2019-05-28 13:18:52 +0300

    cleanup

--- mcs/class/corlib/corefx/RandomNumberGenerator.cs
+++ mcs/class/corlib/corefx/RandomNumberGenerator.cs
@@ -51,49 +51,49 @@ namespace System.Security.Cryptography
 		}
 
 		public static int GetInt32(int fromInclusive, int toExclusive)
-        {
-            if (fromInclusive >= toExclusive)
-                throw new ArgumentException(SR.Argument_InvalidRandomRange);
+		{
+			if (fromInclusive >= toExclusive)
+				throw new ArgumentException(SR.Argument_InvalidRandomRange);
 
-            // The total possible range is [0, 4,294,967,295).
-            // Subtract one to account for zero being an actual possibility.
-            uint range = (uint)toExclusive - (uint)fromInclusive - 1;
+			// The total possible range is [0, 4,294,967,295).
+			// Subtract one to account for zero being an actual possibility.
+			uint range = (uint)toExclusive - (uint)fromInclusive - 1;
 
-            // If there is only one possible choice, nothing random will actually happen, so return
-            // the only possibility.
-            if (range == 0)
-            {
-                return fromInclusive;
-            }
+			// If there is only one possible choice, nothing random will actually happen, so return
+			// the only possibility.
+			if (range == 0)
+			{
+				return fromInclusive;
+			}
 
-            // Create a mask for the bits that we care about for the range. The other bits will be
-            // masked away.
-            uint mask = range;
-            mask |= mask >> 1;
-            mask |= mask >> 2;
-            mask |= mask >> 4;
-            mask |= mask >> 8;
-            mask |= mask >> 16;
+			// Create a mask for the bits that we care about for the range. The other bits will be
+			// masked away.
+			uint mask = range;
+			mask |= mask >> 1;
+			mask |= mask >> 2;
+			mask |= mask >> 4;
+			mask |= mask >> 8;
+			mask |= mask >> 16;
 
-            Span<uint> resultSpan = stackalloc uint[1];
-            uint result;
+			Span<uint> resultSpan = stackalloc uint[1];
+			uint result;
 
-            do
-            {
-                FillSpan(MemoryMarshal.AsBytes(resultSpan));
-                result = mask & resultSpan[0];
-            }
-            while (result > range);
+			do
+			{
+				FillSpan(MemoryMarshal.AsBytes(resultSpan));
+				result = mask & resultSpan[0];
+			}
+			while (result > range);
 
-            return (int)result + fromInclusive;
-        }
+			return (int)result + fromInclusive;
+		}
 
-        public static int GetInt32(int toExclusive)
-        {
-            if (toExclusive <= 0)
-                throw new ArgumentOutOfRangeException(nameof(toExclusive), SR.ArgumentOutOfRange_NeedPosNum);
+		public static int GetInt32(int toExclusive)
+		{
+			if (toExclusive <= 0)
+				throw new ArgumentOutOfRangeException(nameof(toExclusive), SR.ArgumentOutOfRange_NeedPosNum);
 
-            return GetInt32(0, toExclusive);
-        }
+			return GetInt32(0, toExclusive);
+		}
 	}
 }
