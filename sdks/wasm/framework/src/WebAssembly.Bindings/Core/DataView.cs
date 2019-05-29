98092c280b886b29b9250a9ce92afd7464138b2a
Kenneth Pouncey <kjpou@pt.lu>
2019-05-29 10:02:57 +0200

    Add a private routine UnBoxValue to fix cast exceptions.

--- sdks/wasm/framework/src/WebAssembly.Bindings/Core/DataView.cs
+++ sdks/wasm/framework/src/WebAssembly.Bindings/Core/DataView.cs
@@ -81,7 +81,7 @@ namespace WebAssembly.Core {
 		/// <returns>A signed 32-bit float number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 32-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public float GetFloat32 (int byteOffset, bool littleEndian = false) => (float)Invoke ("getFloat32", byteOffset, littleEndian);
+		public float GetFloat32 (int byteOffset, bool littleEndian = false) => UnBoxValue<float>(Invoke ("getFloat32", byteOffset, littleEndian));
 
 		/// <summary>
 		/// Gets the signed 64-bit double (double) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
@@ -89,49 +89,49 @@ namespace WebAssembly.Core {
 		/// <returns>A signed 64-bit coulbe number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 64-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public double GetFloat64 (int byteOffset, bool littleEndian = false) => (double)Invoke ("getFloat64", byteOffset, littleEndian);
+		public double GetFloat64 (int byteOffset, bool littleEndian = false) => UnBoxValue<double>(Invoke ("getFloat64", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the signed 16-bit integer (short) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A signed 16-bit ineger (short) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 16-bit integer (short) is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public short GetInt16 (int byteOffset, bool littleEndian = false) => (short)Invoke ("getInt16", byteOffset, littleEndian);
+		public short GetInt16 (int byteOffset, bool littleEndian = false) => UnBoxValue<short>(Invoke ("getInt16", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the signed 32-bit integer (int) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A signed 32-bit ineger (int) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 32-bit integer (int) is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public int GetInt32 (int byteOffset, bool littleEndian = false) => (int)Invoke ("getInt32", byteOffset, littleEndian);
+		public int GetInt32 (int byteOffset, bool littleEndian = false) => UnBoxValue<int>(Invoke ("getInt32", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the signed 8-bit byte (sbyte) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A signed 8-bit byte (sbyte) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 8-bit byte is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public sbyte GetInt8 (int byteOffset, bool littleEndian = false) => (sbyte)Invoke ("getInt8", byteOffset, littleEndian);
+		public sbyte GetInt8 (int byteOffset, bool littleEndian = false) => UnBoxValue<sbyte>(Invoke ("getInt8", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the unsigned 16-bit integer (short) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A unsigned 16-bit integer (ushort) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the unsigned 16-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public ushort GetUint16 (int byteOffset, bool littleEndian = false) => (ushort)Invoke ("getUint16", byteOffset, littleEndian);
+		public ushort GetUint16 (int byteOffset, bool littleEndian = false) => UnBoxValue<ushort>(Invoke ("getUint16", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the usigned 32-bit integer (uint) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A usigned 32-bit ineger (uint) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 32-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public uint GetUint32 (int byteOffset, bool littleEndian = false) => (uint)Invoke ("getUint32", byteOffset, littleEndian);
+		public uint GetUint32 (int byteOffset, bool littleEndian = false) => UnBoxValue<uint>(Invoke ("getUint32", byteOffset, littleEndian));
 		/// <summary>
 		/// Gets the unsigned 8-bit byte (byte) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
 		/// <returns>A unsigned 8-bit byte (byte) number.</returns>
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 32-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
-		public byte GetUint8 (int byteOffset, bool littleEndian = false) => (byte)Invoke ("getUint8", byteOffset, littleEndian);
+		public byte GetUint8 (int byteOffset, bool littleEndian = false) => UnBoxValue<byte>(Invoke ("getUint8", byteOffset, littleEndian));
 		/// <summary>
 		/// Sets the signed 32-bit float (float) at the specified byte offset from the start of the <see cref="T:WebAssembly.Core.DataView"/>.
 		/// </summary>
@@ -189,6 +189,20 @@ namespace WebAssembly.Core {
 		/// <param name="byteOffset">Byte offset.</param>
 		/// <param name="littleEndian">Indicates whether the 32-bit float is stored in little- or big-endian format. If <c>false</c>, a big-endian value is read.</param>
 		public void SetUint8 (int byteOffset, byte value, bool littleEndian = false) => Invoke ("setUint8", byteOffset, value, littleEndian);
+
+		private U UnBoxValue<U> (object jsValue) where U : struct
+		{
+			if (jsValue == null) {
+				throw new InvalidCastException ($"Unable to cast null to type {typeof (U)}.");
+			}
+
+			var type = jsValue.GetType ();
+			if (type.IsPrimitive) {
+				return (U)Convert.ChangeType (jsValue, typeof (U));
+			} else {
+				throw new InvalidCastException ($"Unable to cast object of type {type} to type {typeof (U)}.");
+			}
+		}
 
 	}
 }
