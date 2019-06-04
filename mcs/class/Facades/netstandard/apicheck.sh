274d6d55dfd92891bcaaedaf64f21028e18ef652
Egor Bogatov <egorbo@gmail.com>
2019-06-04 15:01:55 +0300

    Fix wasm & monotouch profiles

--- mcs/class/Facades/netstandard/apicheck.sh
+++ mcs/class/Facades/netstandard/apicheck.sh
@@ -13,7 +13,10 @@ NSAPI=$STANDARD_REPO/artifacts/bin/ref/netstandard/Debug/netstandard.dll
 # mono $APICOMPAT $NSAPI --impl-dirs "../../lib/net_4_x/Facades/,../../lib/net_4_x/" --exclude-non-browsable > ns21_diff_net_4_x.txt
 
 # run ApiCompat for all profiles (runtime should be built with `--runtime-preset=all` flag)
-for profile in ../../lib/*; do
-    echo "checking $(basename $profile) ..."
-    mono $APICOMPAT $NSAPI --impl-dirs "$profile/Facades/,$profile/" --exclude-non-browsable > ns21_diff_$(basename $profile).txt
+for profile in net_4_x monodroid monotouch monotouch_watch testing_aot_full_interp testing_aot_full testing_aot_hybrid unreal wasm winaot xammac xammac_net_4_5; do
+    profile_path=../../lib/$profile
+    if [ -d "$profile_path" ]; then
+        echo "checking $(basename $profile_path) ..."
+        mono $APICOMPAT $NSAPI --impl-dirs "$profile_path/Facades/,$profile_path/" --exclude-non-browsable > ns21_diff_$(basename $profile_path).txt
+    fi
 done
