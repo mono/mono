274118e6b827a91241da6e739612164fdb20eef1
naricc@microsoft.com <naricc@microsoft.com>
2019-06-11 15:16:11 -0400

    Added test for gc alloc count to corelib build.

--- mcs/class/corlib/Test/System.GC/GCTest.cs
+++ mcs/class/corlib/Test/System.GC/GCTest.cs
@@ -15,24 +15,81 @@ using System;
 
 namespace MonoTests.System.GC {
 
+	public interface SizedObject 
+	{
+		int ExpectedSize();
+	}
+
+	public class NoPointer : SizedObject {
+		public int ExpectedSize() {
+			return 2 * IntPtr.Size;
+		}
+	}
+
+	public class TwoPointer : SizedObject {
+		public object field1;
+		public object field2;
+
+		public int ExpectedSize() 
+		{
+			return 4*IntPtr.Size;
+		}
+
+	}
+
+	public class FourPointer : SizedObject {
+		public object field1;
+		public object field2;
+		public object field3;
+		public object field4;
+
+		public int ExpectedSize() {
+			return 6*IntPtr.Size();
+		}
+	}
+
+	public class EightPointer : SizedObject {
+		public object field1;
+		public object field2;
+		public object fiedl3;
+		public object field4;
+		public object field5;
+		public object field6;
+		public object field7;
+		public object field8;
+
+		public int ExpectedSize() {
+			10*IntPtr.Size();
+		}
+		
+	}
+
 	[TestFixture]
 	public class ThreadAllocationCounterTest
 	{
-		private static final sizeOfObject = 16; // Bytes
+		private static SizedObject makeRandomSizedObject;
 
 		[Test]
-		static void TestGetBytesAllocatedForCurrentThread()
+		public static void TestGetBytesAllocatedForCurrentThread()
 		{
-			long bytesBeforeObjectAlloc = GC.GetAllocatedBytesForCurrentThread();
+			Action<SizedObject>[] objectAllocators = {
+				() => new TwoPointerObject(),
+				() => new FourPointerObject(),
+				() => new EightPointerObject()
+			};
 
-			int numberOfObjectsToAllocate = 100000;
-			for (int i = 0; i < objectsToAllocate; i++) {
-                        Object o = new object();
-            }
+			Random r = new  Random();
 
-			bytesAfter100kAlloc = GC.GetAllocatedBytesForCurrentThread();
+			long expectedSize = 0;
+			long bytesBeforeAlloc = GC.GetAllocatedBytesForCurrentThread();
 
-			Assert.AreEqual(bytesAfter100kAlloc - bytesBeforeObjectAlloc, numberOfObjectsToAllocate * sizeOfObject);
+			for (int i = 0; i < 10000000; i++) {
+				expectedSize += objectAllocators[r.next() %  objectAlloctors.Size ]().ExpectedSize();
+			}
+
+			bytesAfterAlloc = GC.GetAllocatedBytesForCurrentThread();
+
+			Assert.AreEqual(bytesAfterAlloc - bytesBeforeObjectAlloc, expectedSize);
 		}
 
 	}
