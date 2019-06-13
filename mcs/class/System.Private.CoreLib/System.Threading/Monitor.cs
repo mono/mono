42fa169ef3bcad1c6ddc68d5915ab273490f57d6
Marek Safar <marek.safar@gmail.com>
2019-06-13 09:35:01 +0200

    [System.Private.CoreLib] Add licence headers

--- mcs/class/System.Private.CoreLib/System.Threading/Monitor.cs
+++ mcs/class/System.Private.CoreLib/System.Threading/Monitor.cs
@@ -1,8 +1,12 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Runtime.CompilerServices;
 
 namespace System.Threading
 {
-	public static partial class Monitor
+	public static class Monitor
 	{
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
 		public static extern void Enter (object obj);
