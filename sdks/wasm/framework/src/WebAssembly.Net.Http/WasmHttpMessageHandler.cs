a524ba163bf4ca9da6546a5818229ebaf55f2e82
Kenneth Pouncey <kjpou@pt.lu>
2019-06-17 08:01:43 +0200

    Remove unused `using`

--- sdks/wasm/framework/src/WebAssembly.Net.Http/WasmHttpMessageHandler.cs
+++ sdks/wasm/framework/src/WebAssembly.Net.Http/WasmHttpMessageHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using WebAssembly.Core;
