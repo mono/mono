658340bd96f211115e408b0343e0ea5e9bc4d2e9
Thays Grazia <thaystg@gmail.com>
2019-06-19 11:33:38 -0300

    I've tested the MonoTests.DebuggerTests.UnhandledException_2 test on VsWin using .net Framework and I think the test is wrong because the first break is in throw and the next break will be on breakpoint. In our implementation we were ignoring the unhandled exception and stopping only on breakpoint.

--- mcs/class/Mono.Debugger.Soft/Test/dtest.cs
+++ mcs/class/Mono.Debugger.Soft/Test/dtest.cs
@@ -4211,6 +4211,8 @@ public class DebuggerTests
 		var e = run_until ("unhandled_exception_endinvoke");
 		vm.Resume ();
 
+		var e1 = GetNextEvent (); //this should be the exception
+		vm.Resume ();
 		var e2 = GetNextEvent ();
 		Assert.IsFalse (e2 is ExceptionEvent);
 
