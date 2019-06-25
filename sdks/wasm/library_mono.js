e9993af0aa82f44eb780dabde4c9951a78fc4584
Thays Grazia <thaystg@gmail.com>
2019-06-25 12:45:28 -0300

    Fixing side effect of #15357.

--- sdks/wasm/library_mono.js
+++ sdks/wasm/library_mono.js
@@ -41,14 +41,12 @@ var MonoSupportLib = {
 				this.mono_wasm_get_var_info = Module.cwrap ("mono_wasm_get_var_info", 'void', [ 'number', 'number', 'number']);
 
 			this.var_info = [];
-			var typedArray = new Int32Array(var_list.length);
-			for (let i=0; i<var_list.length; i++) {
-				typedArray[i] = var_list[i]
-			}
-			var numBytes = typedArray.length * typedArray.BYTES_PER_ELEMENT;
+			var numBytes = var_list.length * Int32Array.BYTES_PER_ELEMENT;
 			var ptr = Module._malloc(numBytes);
-			var heapBytes = new Uint8Array(Module.HEAPU8.buffer, ptr, numBytes);
-			heapBytes.set(new Uint8Array(typedArray.buffer));
+			var heapBytes = new Int32Array(Module.HEAP32.buffer, ptr, numBytes);
+			for (let i=0; i<var_list.length; i++) {
+				heapBytes[i] = var_list[i]
+			}
 			this.mono_wasm_get_var_info (scope, heapBytes.byteOffset, var_list.length);
 			Module._free(heapBytes.byteOffset);
 			var res = this.var_info;
