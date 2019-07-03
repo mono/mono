972d548394f519d2ce3f2c4bf91ddc9b46e490bf
Ankit Jain <radical@gmail.com>
2019-07-03 04:57:13 -0400

    [2019-06] Bump msbuild to track mono-2019-06 (#15525)
    * Bump msbuild to track mono-2019-06
    
    * Bump nuget.exe to 5.1.0 to match what msbuild has.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '8f608e49833cc1e2f6c9c070dacbd8fdcb41a20e')
+			revision = '5ffd8b1ca7a3025a147807f8e7c0d964870dd84e')
 
 	def build (self):
 		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
