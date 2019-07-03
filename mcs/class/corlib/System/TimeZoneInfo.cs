23ab3b7dbc71340870029e8fbaa6e24ea3da206c
Maxim Lipnin <v-maxlip@microsoft.com>
2019-07-03 20:41:22 +0300

    Fix ambiguous time at the end of DST period on Windows

--- mcs/class/corlib/System/TimeZoneInfo.cs
+++ mcs/class/corlib/System/TimeZoneInfo.cs
@@ -919,8 +919,9 @@ namespace System
 			DateTime DST_end = TransitionPoint (rule.DaylightTransitionEnd, year + ((rule.DaylightTransitionStart.Month < rule.DaylightTransitionEnd.Month) ? 0 : 1));
 			if (dateTime.Kind == DateTimeKind.Utc) {
 				DST_start -= BaseUtcOffset;
-				DST_end -= (BaseUtcOffset + rule.DaylightDelta);
+				DST_end -= BaseUtcOffset;
 			}
+			DST_end -= rule.DaylightDelta;
 			return (dateTime >= DST_start && dateTime < DST_end);
 		}
 		
