1002a4de2b0d95c4bb2acb9439b218e86b17c6f9
Micah N Gorrell <minego@users.noreply.github.com>
2019-07-11 11:09:33 -0600

    [Mono.Security] Allow importing certificates from authenticode assemblies (#14210)
    This change allows a number of games, such as DARK, to start successfully. The games use X509Certificate.CreateFromSignedFile() to load the certificate from their assemblies in order to verify them.
    
    * Added support for creating an AuthenticodeDeformatter for an assembly
    that has been loaded into memory
    
    * Allow importing a certificate from an assembly signed with authenticode
    when using BTLS
    
    * Allow importing a certificate from an assembly signed with authenticode
    when not using BTLS
    
    * Added a test for loading certificates from assemblies signed with
    authenticode

--- mcs/class/Mono.Security/Mono.Security.Authenticode/AuthenticodeDeformatter.cs
+++ mcs/class/Mono.Security/Mono.Security.Authenticode/AuthenticodeDeformatter.cs
@@ -49,6 +49,7 @@ namespace Mono.Security.Authenticode {
 	class AuthenticodeDeformatter : AuthenticodeBase {
 
 		private string filename;
+		private byte[] rawdata;
 		private byte[] hash;
 		private X509CertificateCollection coll;
 		private ASN1 signedHash;
@@ -74,17 +75,36 @@ namespace Mono.Security.Authenticode {
 			FileName = fileName;
 		}
 
+		public AuthenticodeDeformatter (byte[] rawData) : this ()
+		{
+			RawData = rawData;
+		}
+
 		public string FileName {
 			get { return filename; }
 			set { 
 				Reset ();
+				filename = value;
 				try {
-					CheckSignature (value); 
-				}
-				catch (SecurityException) {
+					CheckSignature ();
+				} catch (SecurityException) {
 					throw;
+				} catch {
+					reason = 1;
 				}
-				catch (Exception) {
+			}
+		}
+
+		public byte[] RawData {
+			get { return rawdata; }
+			set {
+				Reset ();
+				rawdata = value;
+				try {
+					CheckSignature ();
+				} catch (SecurityException) {
+					throw;
+				} catch {
 					reason = 1;
 				}
 			}
@@ -166,10 +186,13 @@ namespace Mono.Security.Authenticode {
 			get { return signingCertificate; }
 		}
 
-		private bool CheckSignature (string fileName) 
+		private bool CheckSignature ()
 		{
-			filename = fileName;
-			Open (filename);
+			if (filename != null) {
+				Open (filename);
+			} else {
+				Open (rawdata);
+			}
 			entry = GetSecurityEntry ();
 			if (entry == null) {
 				// no signature is present
@@ -462,6 +485,7 @@ namespace Mono.Security.Authenticode {
 		private void Reset ()
 		{
 			filename = null;
+			rawdata = null;
 			entry = null;
 			hash = null;
 			signedHash = null;
