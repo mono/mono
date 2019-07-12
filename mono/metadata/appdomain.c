2a96bcf9a50d976bed0cfc4c8c7cc8d279b9da1e
Aleksey Kliger <alklig@microsoft.com>
2019-07-12 13:35:15 -0400

    Move ALC icall to assembly-load-context.c

--- mono/metadata/appdomain.c
+++ mono/metadata/appdomain.c
@@ -3242,27 +3242,3 @@ mono_runtime_install_appctx_properties (void)
 
 #endif
 
-#ifdef ENABLE_NETCORE
-gpointer
-ves_icall_System_Runtime_Loader_AssemblyLoadContext_InternalInitializeNativeALC (gpointer this_gchandle_ptr, MonoBoolean is_default_alc, MonoBoolean collectible, MonoError *error)
-{
-	/* If the ALC is collectible, this_gchandle is weak, otherwise it's strong. */
-	uint32_t this_gchandle = (uint32_t)GPOINTER_TO_UINT (this_gchandle_ptr);
-	if (collectible) {
-		mono_error_set_execution_engine (error, "Collectible AssemblyLoadContexts are not yet supported by MonoVM");
-		return NULL;
-	}
-
-	MonoDomain *domain = mono_domain_get ();
-	MonoAssemblyLoadContext *alc = NULL;
-
-	if (is_default_alc) {
-		alc = mono_domain_default_alc (domain);
-		g_assert (alc);
-	} else {
-		/* create it */
-		alc = mono_domain_create_individual_alc (domain, this_gchandle, collectible, error);
-	}
-	return alc;
-}
-#endif
