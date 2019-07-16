ee2b1089ff92d5ec5aa89d9b7d647f902c4ec7e4
Larry Ewing <lewing@microsoft.com>
2019-07-16 10:40:10 -0400

    Bump msbuild to track xplat-master

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'a003aaa604f1fde7767927f7201996f76422e6c0')
+			revision = '4ab9414c628679ded1fb7623268b8d664f222d51')
 
 	def build (self):
 		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
