8965314a2274fd27ad3859ccc8c51a3880538e25
Larry Ewing <lewing@microsoft.com>
2019-07-16 12:59:29 -0400

    Bump msbuild to msbuild:mono-2019-06 to stop disabling compiler server

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '5ffd8b1ca7a3025a147807f8e7c0d964870dd84e')
+			revision = 'e80320f2b43d8569ddd8443c20d8066ec97b3f52')
 
 	def build (self):
 		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
