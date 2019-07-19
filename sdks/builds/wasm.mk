f82d2e4ac4f855372d983547d9729a54bd958141
Bernhard Urban <lewurm@gmail.com>
2019-07-18 22:52:50 -0700

    [wasm] fix libclang path

--- sdks/builds/wasm.mk
+++ sdks/builds/wasm.mk
@@ -151,7 +151,7 @@ endif
 #  $(6): offsets dumper abi
 define WasmCrossTemplate
 
-_wasm-$(1)_OFFSETS_DUMPER_ARGS=--emscripten-sdk="$$(EMSCRIPTEN_SDK_DIR)/upstream/emscripten" --libclang-path /home/lewurm/work/mono/sdks/builds/toolchains/emsdk/upstream/lib/
+_wasm-$(1)_OFFSETS_DUMPER_ARGS=--emscripten-sdk="$$(EMSCRIPTEN_SDK_DIR)/upstream/emscripten" --libclang-path $(EMSCRIPTEN_LOCAL_SDK_DIR)/upstream/lib/
 
 _wasm-$(1)_CONFIGURE_FLAGS= \
 	--disable-boehm \
