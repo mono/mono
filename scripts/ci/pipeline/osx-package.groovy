471421023c27752f02ce4728778d6ff814ce362f
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-07-31 13:32:06 +0200

    [ci] Handle master build purging in a separate cleanup job

--- scripts/ci/pipeline/osx-package.groovy
+++ scripts/ci/pipeline/osx-package.groovy
@@ -11,8 +11,7 @@ utils = null
 
 if (monoBranch == 'master') {
     properties([ /* compressBuildLog() */  // compression is incompatible with JEP-210 right now
-                pipelineTriggers([cron('0 3 * * *')]),
-                buildDiscarder(logRotator(numToKeepStr: '1'))  // delete skipped builds
+                pipelineTriggers([cron('0 3 * * *')])
     ])
 
     // multi-branch pipelines still get triggered for each commit, skip these builds on master by checking whether this build was timer-triggered
@@ -20,9 +19,6 @@ if (monoBranch == 'master') {
         echo "Skipping per-commit build on master."
         return
     }
-
-    // make sure this build isn't deleted by logRotator
-    currentBuild.setKeepLog(true)
 }
 
 try {
