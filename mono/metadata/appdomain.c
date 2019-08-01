e2f588bb74ba6ff21f1e0ff908e1e8597c6770bd
Ryan Lucia <rylucia@microsoft.com>
2019-08-01 16:55:55 -0400

    Remove usage of loader lock
    Originally added in https://github.com/mono/mono/commit/c4cbd3b700eea52c84c68c9450d80357b3be681d but no longer relevant

--- mono/metadata/appdomain.c
+++ mono/metadata/appdomain.c
@@ -3035,11 +3035,8 @@ unload_thread_main (void *arg)
 	}
 
 	/* Clear references to our vtables in class->runtime_info.
-	 * We also hold the loader lock because we're going to change
-	 * class->runtime_info.
 	 */
 
-	mono_loader_lock (); //FIXME why do we need the loader lock here?
 	mono_domain_lock (domain);
 	/*
 	 * We need to make sure that we don't have any remsets
@@ -3060,7 +3057,6 @@ unload_thread_main (void *arg)
 	mono_assembly_cleanup_domain_bindings (domain->domain_id);
 
 	mono_domain_unlock (domain);
-	mono_loader_unlock ();
 
 	domain->state = MONO_APPDOMAIN_UNLOADED;
 
