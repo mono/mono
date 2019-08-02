83dd74d4f2d96ca06bb1303f4372eb8fc1a6c630
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-08-02 20:50:40 +0200

    Update libgdiplus.py

--- packaging/MacSDK/libgdiplus.py
+++ packaging/MacSDK/libgdiplus.py
@@ -2,7 +2,7 @@ GitHubTarballPackage(
     'mono',
     'libgdiplus',
     'release/6.0',
-    '93f43204a94ac75f468c58334ad07726958f4f26',
+    '8ad7eafc0cbe891e784188681fc6fb6373ff394e',
     configure='CFLAGS="%{gcc_flags} %{local_gcc_flags} -I/opt/X11/include" ./autogen.sh --prefix="%{package_prefix}"',
     override_properties={
         'make': 'C_INCLUDE_PATH="" make'})
