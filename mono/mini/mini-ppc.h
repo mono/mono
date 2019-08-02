mono/mono

commit 3c92cd8c94c027b7c9215a12044345106851262b

[jit] Align float/double Nan/Inf to uint conversion behavior with CoreCLR/LLVM/Roslyn (#15930)
* [jit] Align float/double Nan/Inf to uint conversion behavior with CoreCLR/LLVM

* Fix float/double -> ulong conversion for edge cases

* Fix mono_fconv_u8 / mono_rconv_u8 to also return 0 for PositiveInfinity

* Enable support for platform specific implementations of OP_RCONV_U8/OP_FCONV_U8/OP_FCONV_U4

* Restore OP emulation on WASM target

* Update tests

* Disable failing tests for now

* Adjust tests

* Adjust tests

* Fix float -> uint conversion on WASM

* Make interpreter consistent with compiled code

* Blind attempt to fix WASM test leg

* Implement remaining conversions in LLVM

* Fix ARM32

* Workaround for Windows x64 FullAOT+LLVM

* Bump AOT version

mono/mini/mini-ppc.h
@@ -103,6 +103,7 @@ typedef struct MonoCompileArch {
 #define MONO_ARCH_EMULATE_LCONV_TO_R4 1
 #endif
 
+#define MONO_ARCH_EMULATE_FCONV_TO_U8 1
 #define MONO_ARCH_EMULATE_LCONV_TO_R8_UN 1
 #define MONO_ARCH_EMULATE_FREM 1
 #define MONO_ARCH_GC_MAPS_SUPPORTED 1

mono/metadata/icall-signatures.h
@@ -49,6 +49,7 @@
 // mono_icall_sig_ptr_ptr
 // mono_icall_sig_uint16_double
 // mono_icall_sig_uint32_double
+// mono_icall_sig_uint32_float
 // mono_icall_sig_uint8_double
 // mono_icall_sig_ulong_double
 // mono_icall_sig_ulong_float
@@ -173,6 +174,7 @@ ICALL_SIG (2, (ptr, object))			\
 ICALL_SIG (2, (ptr, ptr))			\
 ICALL_SIG (2, (uint16, double))			\
 ICALL_SIG (2, (uint32, double))			\
+ICALL_SIG (2, (uint32, float))			\
 ICALL_SIG (2, (uint8, double))			\
 ICALL_SIG (2, (ulong, double))			\
 ICALL_SIG (2, (ulong, float))			\

mono/metadata/jit-icall-reg.h
@@ -116,6 +116,7 @@ MONO_JIT_ICALL (__emul_rconv_to_i8) \
 MONO_JIT_ICALL (__emul_rconv_to_ovf_i8) \
 MONO_JIT_ICALL (__emul_rconv_to_ovf_u8) \
 MONO_JIT_ICALL (__emul_rconv_to_ovf_u8_un) \
+MONO_JIT_ICALL (__emul_rconv_to_u4)	\
 MONO_JIT_ICALL (__emul_rconv_to_u8) \
 MONO_JIT_ICALL (__emul_rrem) \
 MONO_JIT_ICALL (cominterop_get_ccw) \

mono/mini/aot-runtime.h
@@ -11,7 +11,7 @@
 #include "mini.h"
 
 /* Version number of the AOT file format */
-#define MONO_AOT_FILE_VERSION 168
+#define MONO_AOT_FILE_VERSION 169
 
 #define MONO_AOT_TRAMP_PAGE_SIZE 16384
 

mono/mini/basic-float.cs
@@ -80,27 +80,80 @@ class Tests
 		sbyte sb = (sbyte)a;
 		if (sb != 2)
 			return 6;
-		/* MS.NET special cases these */
+		return 0;
+	}
+
+	public static int test_0_fconv_u8 () {
 		double d = Double.NaN;
+		ulong ui;
+		ui = (ulong)d;
+		if (ui != 0)
+			return 1;
+		d = Double.PositiveInfinity;
+		ui = (ulong)d;
+		/* x64: 0, ARM64: ulong.MaxValue (verified against CoreCLR) */
+		if (ui != 0 && ui != ulong.MaxValue)
+			return 2;
+		d = Double.NegativeInfinity;
+		ui = (ulong)d;
+		/* x64: long.MaxValue + 1 (inconsistent with conversion to u4 but matches CLR) */
+		if (ui != 0 && ui != (ulong)long.MaxValue + 1)
+			return 3;
+		return 0;
+	}
+
+	public static int test_0_fconv_u4 () {
+		double d = Double.NaN;
+		uint ui;
 		ui = (uint)d;
 		if (ui != 0)
-			return 7;
+			return 1;
 		d = Double.PositiveInfinity;
 		ui = (uint)d;
-		if (ui != 0)
-			return 8;
+		/* x64: 0, ARM64: uint.MaxValue (verified against CoreCLR) */
+		if (ui != 0 && ui != uint.MaxValue)
+			return 2;
 		d = Double.NegativeInfinity;
 		ui = (uint)d;
 		if (ui != 0)
-			return 9;
-		/* FIXME: This fails with llvm and with gcc -O2 on osx/linux */
-		/*
-		d = Double.MaxValue;
-		i = (int)d;
-		if (i != -2147483648)
-			return 10;
-		*/
+			return 3;
+		return 0;
+	}
 
+	public static int test_0_rconv_u8 () {
+		float d = float.NaN;
+		ulong ui;
+		ui = (ulong)d;
+		if (ui != 0)
+			return 1;
+		d = float.PositiveInfinity;
+		ui = (ulong)d;
+		/* x64: 0, ARM64: ulong.MaxValue (verified against CoreCLR) */
+		if (ui != 0 && ui != ulong.MaxValue)
+			return 2;
+		d = float.NegativeInfinity;
+		ui = (ulong)d;
+		/* x64: long.MaxValue + 1 (inconsistent with conversion to u4 but matches CLR) */
+		if (ui != 0 && ui != (ulong)long.MaxValue + 1)
+			return 3;
+		return 0;
+	}
+
+	public static int test_0_rconv_u4 () {
+		float d = float.NaN;
+		uint ui;
+		ui = (uint)d;
+		if (ui != 0)
+			return 1;
+		d = float.PositiveInfinity;
+		ui = (uint)d;
+		/* x64: 0, ARM64: uint.MaxValue (verified against CoreCLR) */
+		if (ui != 0 && ui != uint.MaxValue)
+			return 2;
+		d = float.NegativeInfinity;
+		ui = (uint)d;
+		if (ui != 0)
+			return 3;
 		return 0;
 	}
 

mono/mini/interp/interp.c
@@ -4245,25 +4245,19 @@ interp_exec_method_full (InterpFrame *frame, ThreadContext *context, FrameClause
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CONV_U4_R4)
-			/* needed on arm64 */
-			if (isinf (sp [-1].data.f_r4))
-				sp [-1].data.i = 0;
-			/* needed by wasm */
-			else if (isnan (sp [-1].data.f_r4))
-				sp [-1].data.i = 0;
-			else
-				sp [-1].data.i = (guint32) sp [-1].data.f_r4;
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U4
+			sp [-1].data.i = mono_rconv_u4 (sp [-1].data.f_r4);
+#else
+			sp [-1].data.i = (guint32) sp [-1].data.f_r4;
+#endif
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CONV_U4_R8)
-			/* needed on arm64 */
-			if (mono_isinf (sp [-1].data.f))
-				sp [-1].data.i = 0;
-			/* needed by wasm */
-			else if (isnan (sp [-1].data.f))
-				sp [-1].data.i = 0;
-			else
-				sp [-1].data.i = (guint32)sp [-1].data.f;
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U4
+			sp [-1].data.i = mono_fconv_u4_2 (sp [-1].data.f);
+#else
+			sp [-1].data.i = (guint32) sp [-1].data.f;
+#endif
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CONV_I8_I4)
@@ -4319,11 +4313,19 @@ interp_exec_method_full (InterpFrame *frame, ThreadContext *context, FrameClause
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CONV_U8_R4)
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U8
+			sp [-1].data.l = mono_rconv_u8 (sp [-1].data.f_r4);
+#else
 			sp [-1].data.l = (guint64) sp [-1].data.f_r4;
+#endif
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CONV_U8_R8)
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U8
+			sp [-1].data.l = mono_fconv_u8_2 (sp [-1].data.f);
+#else
 			sp [-1].data.l = (guint64)sp [-1].data.f;
+#endif
 			++ip;
 			MINT_IN_BREAK;
 		MINT_IN_CASE(MINT_CPOBJ) {

mono/mini/jit-icalls.c
@@ -852,9 +852,21 @@ mono_ldtoken_wrapper_generic_shared (MonoImage *image, int token, MonoMethod *me
 guint64
 mono_fconv_u8 (double v)
 {
+#if defined(TARGET_X86) || defined(TARGET_AMD64)
+	const double two63 = 2147483648.0 * 4294967296.0;
+	if (v < two63) {
+		return (gint64)v;
+	} else {
+		return (gint64)(v - two63) + ((guint64)1 << 63);
+	}
+#else
+	if (mono_isinf (v) || mono_isnan (v))
+		return 0;
 	return (guint64)v;
+#endif
 }
 
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U8
 guint64
 mono_fconv_u8_2 (double v)
 {
@@ -869,8 +881,20 @@ mono_fconv_u8_2 (double v)
 guint64
 mono_rconv_u8 (float v)
 {
+#if defined(TARGET_X86) || defined(TARGET_AMD64)
+	const float two63 = 2147483648.0 * 4294967296.0;
+	if (v < two63) {
+		return (gint64)v;
+	} else {
+		return (gint64)(v - two63) + ((guint64)1 << 63);
+	}
+#else
+	if (mono_isinf (v) || mono_isnan (v))
+		return 0;
 	return (guint64)v;
+#endif
 }
+#endif
 
 #ifdef MONO_ARCH_EMULATE_FCONV_TO_I8
 gint64
@@ -889,6 +913,7 @@ mono_fconv_u4 (double v)
 	return (guint32)v;
 }
 
+#ifdef MONO_ARCH_EMULATE_FCONV_TO_U4
 guint32
 mono_fconv_u4_2 (double v)
 {
@@ -899,6 +924,15 @@ mono_fconv_u4_2 (double v)
 	// wrappers are only produced for one of them, breaking FullAOT.
 	return mono_fconv_u4 (v);
 }
+
+gint32
+mono_rconv_u4 (float v)
+{
+	if (mono_isinf (v) || mono_isnan (v))
+		return 0;
+	return (gint32)v;
+}
+#endif
 
 gint64
 mono_fconv_ovf_i8 (double v)

mono/mini/jit-icalls.h
@@ -80,6 +80,8 @@ G_EXTERN_C gint64 mono_fconv_i8 (double v);
 G_EXTERN_C guint32 mono_fconv_u4 (double v);
 G_EXTERN_C guint32 mono_fconv_u4_2 (double v);
 
+G_EXTERN_C gint32 mono_rconv_u4 (float v);
+
 G_EXTERN_C gint64 mono_fconv_ovf_i8 (double v);
 
 G_EXTERN_C guint64 mono_fconv_ovf_u8 (double v);

commit a466337063ff108fc3062348a125c0dedf5e131e

Enable MONO_ARCH_EMULATE_FCONV_TO_U4 on PPC
Fixes regression related to floating point introduced in #15930.

This does mean we have to take a slower path instead of using the
PPC instruction, but it provides consistent results. Other CPUs
have to do this too, so it's not like PPC is impacted unfairly.
(i.e, NaN gives an inconsistent result, at least with POWER7 and
some amd64 CPUs tested)

Fixes #15990.

mono/mini/mini-ppc.h
@@ -103,6 +103,7 @@ typedef struct MonoCompileArch {
 #define MONO_ARCH_EMULATE_LCONV_TO_R4 1
 #endif
 
+#define MONO_ARCH_EMULATE_FCONV_TO_U4 1
 #define MONO_ARCH_EMULATE_FCONV_TO_U8 1
 #define MONO_ARCH_EMULATE_LCONV_TO_R8_UN 1
 #define MONO_ARCH_EMULATE_FREM 1