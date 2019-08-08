5d9caa237340433e48e958f44fa89626c1fb9957
Ankit Jain <radical@gmail.com>
2019-08-08 14:07:00 -0400

    [msbuild] Bump to track mono-2019-06 and update roslyn (#16095)
    Prompted by mono/msbuild#123 :
    
    This includes:
    
    Update to upstream vs16.3 branch
    SDK update to track dotnet release/3.0.100-preview8
    Roslyn updated to 3.3.0-beta2-19381-14
    
    * Work around remoting1.exe test issue with new Roslyn

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,14 +3,16 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '095258fa3977e056c15bcc9a2d6920eb4793eafa')
+			revision = '4d92cbc5100f4d956cc31583f48fa149ea46c7d1')
 
 	def build (self):
-		self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
-		self.sh ('find artifacts -wholename \'*/log/*\' -type f -exec zip msbuild-bin-logs.zip {} \+')
+		try:       
+			self.sh ('./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
+		finally:
+			self.sh ('find artifacts stage1 -wholename \'*/log/*\' -type f -exec zip msbuild-bin-logs.zip {} \+')
 
 	def install (self):
 		# use the bootstrap msbuild as the system might not have one available!
-		self.sh ('./artifacts/mono-msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO /p:IgnoreDiffFailure=true' % self.staged_prefix)
+		self.sh ('./stage1/mono-msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO /p:IgnoreDiffFailure=true' % self.staged_prefix)
 
 MSBuild ()
