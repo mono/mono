4988ad56ad09f01baf903ff87fef8e3cb7d12494
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-08-13 12:12:22 +0200

    [pkg] Bump libgdiplus to 6.0.2
    (cherry picked from commit b60aaea7912839e5d343ad2bd3fde0a5c35704c1)

--- packaging/MacSDK/libgdiplus.py
+++ packaging/MacSDK/libgdiplus.py
@@ -1,8 +1,8 @@
 GitHubTarballPackage(
     'mono',
     'libgdiplus',
-    '6.0.1',
-    '5bc2f73373cb879e1c78c91fe4bf63f3893f8714',
+    '6.0.2',
+    '35ae5341c2326c44e26058ca90b12303bb9b1da0',
     configure='CFLAGS="%{gcc_flags} %{local_gcc_flags} -I/opt/X11/include" ./autogen.sh --prefix="%{package_prefix}"',
     override_properties={
         'make': 'C_INCLUDE_PATH="" make'})
