00f3144b1070c05240ce0e2058a0103a1622c16d
Kenneth Pouncey <kjpou@pt.lu>
2019-08-14 10:23:21 +0200

    Remove the FreeObject from source as it is not needed to free delegates.
    (cherry picked from commit 3d5459b6d1f36bd3bb9fc0837a525fa118822b4f)

--- sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
+++ sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
@@ -264,7 +264,6 @@ namespace WebAssembly.Net.WebSockets {
 												using (var binResult = (ArrayBuffer)target.GetObjectProperty ("result")) {
 													var mess = new ReceivePayload (binResult, WebSocketMessageType.Binary);
 													receiveMessageQueue.BufferPayload (mess);
-													Runtime.FreeObject (loadend);
 												}
 											}
 										}
@@ -327,19 +326,15 @@ namespace WebAssembly.Net.WebSockets {
 			// are possible leading to crashes.
 			if (onClose != null) {
 				innerWebSocket.SetObjectProperty ("onclose", "");
-				Runtime.FreeObject (onClose);
 			}
 			if (onError != null) {
 				innerWebSocket.SetObjectProperty ("onerror", "");
-				Runtime.FreeObject (onError);
 			}
 			if (onOpen != null) {
 				innerWebSocket.SetObjectProperty ("onopen", "");
-				Runtime.FreeObject (onOpen);
 			}
 			if (onMessage != null) {
 				innerWebSocket.SetObjectProperty ("onmessage", "");
-				Runtime.FreeObject (onMessage);
 			}
 			innerWebSocket?.Dispose ();
 		}
