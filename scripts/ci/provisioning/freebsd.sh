27cc3059b33818aba9efb0b7156cc666a814b024
Phil Jaenke <prj@rootwyrm.com>
2019-08-19 17:09:44 -0400

    Many updates to make it better
    Too much to list by many measures. This makes sure appropriate kernel modules, file systems, build packages, supplemental packages, and directories are installed or created as appropriate. This also addresses a known failure condition in System.Security-xunit due to a missing directory resulting in ownership problems.

--- scripts/ci/provisioning/freebsd.sh
+++ scripts/ci/provisioning/freebsd.sh
@@ -1,9 +1,70 @@
 #!/bin/sh -e
 
-pkg install -y bash openjdk8 git gmake autoconf automake libtool cmake gettext libgdiplus unixODBC sqlite
 mkdir -p /mnt/jenkins/pbuilder /mnt/scratch /mnt/jenkins/buildplace
 chown builder /mnt/jenkins /mnt/jenkins/pbuilder /mnt/scratch
 
+## Set up our kernel modules
+for km in fdescfs procfs linprocfs; do
+  kldstat | grep $km > /dev/null
+  if [ $? -ne 0 ]; then
+    ## Attempt loading.
+    kldload $km
+  fi
+done
+## Set up the appropriate mounts prior to pkg installation. It's easier this way.
+if [ ! -e /dev/fd ]; then
+  echo "fdesc /dev/fd fdescfs rw 0 0" >> /etc/fstab
+  mount /dev/fd
+fi
+## Key off 0 (kernel) which must always be present
+if [ ! -f /proc/0/status ]; then
+  echo "proc /proc procfs rw 0 0" >> /etc/fstab
+  mount /proc
+fi
+## Linux compatibility shim here; this may be required for some unit tests and applications
+if [ ! -d /compat/linux/proc ]; then
+  mkdir -p /compat/linux/proc
+  echo "linprocfs /compat/linux/proc linprocfs rw 0 0" >> /etc/fstab
+  mount /compat/linux/proc
+elif [ -e /compat/linux/proc ]; then
+  echo "linprocfs /compat/linux/proc linprocfs rw 0 0" >> /etc/fstab
+  mount /compat/linux/proc
+fi
+
+## Validate that pkg is working.
+pkg info > /dev/null 2&>1
+if [ $? -ne 0 ]; then
+  ## We can't possibly continue.
+  echo "[FATAL] pkgng is non-functional; build impossible."
+  exit 1
+fi
+
+## These packages are MUST have.
+pkg install -y bash git gmake autoconf automake cmake libtool python27 python36 ca_root_nss
+## Kill the attempt quickly if we definitely cannot build.
+if [ $? -ne 0 ]; then
+	echo "[FATAL] Error installing critical packages. Aborting because building is impossible."
+	exit 1
+fi
+
+## These packages are NICE to have, so be more graceful.
+for pn in openjdk8 libgdiplus unixODBC sqlite3 xorgproto pango libinotify; do
+  pkg install -y $pn
+  if [ $? -ne 0 ]; then
+    ## This is non-fatal, but advise that test failures should be expected.
+    echo "[NOTICE] Could not install supplementary package $x - some tests may fail!"
+  fi
+done
+
 # for compatibility with the mono build scripts, ideally shouldn't be necessary
 ln -s /usr/local/bin/bash /bin/bash
-rm -f /bin/make && ln -s /usr/local/bin/gmake /bin/make
+## Do not remove, instead rename; otherwise it's impossible to support ports infrastructure testing
+mv /bin/make /bin/bsdmake && ln -s /usr/local/bin/gmake /sbin/make
+
+## XXX: System.Security-xunit failures are addressed here
+if [ ! -d /usr/share/.mono ]; then
+  mkdir /usr/share/.mono
+  chown builder /usr/share/.mono
+else
+ chown builder /usr/share/.mono
+fi
