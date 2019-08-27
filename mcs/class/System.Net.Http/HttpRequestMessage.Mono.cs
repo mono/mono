44e1854943e51d09f596ee41313e7a807de1b523
Steve Pfister <steveisok@users.noreply.github.com>
2019-08-27 13:54:52 -0400

    Allow file:// based Uri's to be used in HttpClient when the BaseAddress is set
    Fixes https://devdiv.visualstudio.com/DefaultCollection/DevDiv/_workitems/edit/967582
    
    Bumps the HttpClient part in mono/corefx at: https://github.com/mono/corefx/commit/ee3acec16b37bec5016da129e1e3840097ca629b
    
    The test that would have caught this failure was disabled previously via:
    https://github.com/mono/mono/commit/2f411750acfd2132f9b4970ef8cf11d9a0f62df9
    
    (cherry picked from commit 580defd967aa3c0bbdc985ca001bad28c6fdec80)

--- mcs/class/System.Net.Http/HttpRequestMessage.Mono.cs
+++ mcs/class/System.Net.Http/HttpRequestMessage.Mono.cs
@@ -12,6 +12,10 @@ namespace System.Net.Http
 				return true;
 #endif
 
+			// Mono URI handling which does not distinguish between file and url absolute paths without scheme
+			if (uri.Scheme == Uri.UriSchemeFile && uri.OriginalString.StartsWith ("/", StringComparison.Ordinal))
+				return true;
+
 			return HttpUtilities.IsHttpUri (uri);
 		}
 	}
