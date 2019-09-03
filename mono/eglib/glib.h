c9ab7a5f1b25c358bcfa727f465e34796096be11
Jay Krell <jay.krell@cornell.edu>
2019-09-03 15:45:36 -0700

    Take stars out of types where they make more sense.
    Co-Authored-By: Larry Ewing <lewing@microsoft.com>

--- mono/eglib/glib.h
+++ mono/eglib/glib.h
@@ -1400,7 +1400,7 @@ mono_get_module_filename_ex (gpointer process, gpointer mod, gunichar2 **pstr, g
 // g_free the result
 // No MAX_PATH limit.
 gboolean
-mono_get_module_basename (gpointer process, gpointer mod, gunichar2** pstr, guint32* plength);
+mono_get_module_basename (gpointer process, gpointer mod, gunichar2 **pstr, guint32 *plength);
 
 // g_free the result
 // No MAX_PATH limit.
