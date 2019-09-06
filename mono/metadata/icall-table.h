45b5d02ad7edd11a2ffd79a35cf72facf98c008e
Jay Krell <jaykrell@microsoft.com>
2019-09-05 20:24:03 -0700

    Revert "[Coop] ves_icall_InternalInvoke. (#16625)"
    This reverts commit 651eb7696e7887597df0eb149cd7eb222ea807b4.
    
    Seeing if it fixes Windows x64 CI.

--- mono/metadata/icall-table.h
+++ mono/metadata/icall-table.h
@@ -93,18 +93,16 @@ typedef MonoW32ProcessInfo *MonoW32ProcessInfo_ref;
 // Maybe do this in TYPED_HANDLE_DECL.
 typedef MonoArray MonoArrayOut;
 typedef MonoArray MonoArrayInOut;
-typedef MonoArrayHandle MonoArrayOutHandle;
-typedef MonoArrayHandle MonoArrayInOutHandle;
-typedef MonoException MonoExceptionOut;
-typedef MonoExceptionHandle MonoExceptionOutHandle;
 typedef MonoObject MonoObjectOut;
 typedef MonoObject MonoObjectInOut;
 typedef MonoObjectHandle MonoObjectOutHandle;
 typedef MonoObjectHandle MonoObjectInOutHandle;
-typedef MonoReflectionModule MonoReflectionModuleOut;
-typedef MonoReflectionModuleHandle MonoReflectionModuleOutHandle;
+typedef MonoArrayHandle MonoArrayOutHandle;
+typedef MonoArrayHandle MonoArrayInOutHandle;
 typedef MonoString MonoStringOut;
 typedef MonoStringHandle MonoStringOutHandle;
+typedef MonoReflectionModule MonoReflectionModuleOut;
+typedef MonoReflectionModuleHandle MonoReflectionModuleOutHandle;
 
 // How the arguments and return value of an icall should be wrapped.
 // The names and meanings are from marshal-ilgen.c.
@@ -233,7 +231,6 @@ typedef MonoStringHandle MonoStringOutHandle;
 #define MONO_HANDLE_TYPE_WRAP_MonoTransparentProxy			ICALL_HANDLES_WRAP_OBJ
 #define MONO_HANDLE_TYPE_WRAP_MonoW32ProcessStartInfo		ICALL_HANDLES_WRAP_OBJ
 
-#define MONO_HANDLE_TYPE_WRAP_MonoExceptionOut		ICALL_HANDLES_WRAP_OBJ_OUT
 #define MONO_HANDLE_TYPE_WRAP_MonoObjectOut		ICALL_HANDLES_WRAP_OBJ_OUT
 #define MONO_HANDLE_TYPE_WRAP_MonoStringOut		ICALL_HANDLES_WRAP_OBJ_OUT
 #define MONO_HANDLE_TYPE_WRAP_MonoArrayOut		ICALL_HANDLES_WRAP_OBJ_OUT
