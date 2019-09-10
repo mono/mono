ee4b25712e1e221daf5bd9b364ec84416bb09303
Larry Ewing <lewing@microsoft.com>
2019-09-09 20:01:07 -0500

    [msbuild] pick up p4 versions

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'da00a8b482617ed0cf64c7929be8c82c254f0d52')
+			revision = '63cdc9079266c170dc4a3f14704f3fc07be81bc7')
 
 	def build (self):
 		try:       
