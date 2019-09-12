5e1b0399247a67b218ae68664b4df58848a8dd1f
Steve Pfister <steve.pfister@microsoft.com>
2019-09-11 21:59:10 -0400

    Round 2... Go

--- scripts/ci/run-jenkins.sh
+++ scripts/ci/run-jenkins.sh
@@ -339,9 +339,8 @@ if [[ ${CI_TAGS} == *'webassembly'* ]] || [[ ${CI_TAGS} == *'wasm'* ]];
             ${TESTCMD} --label=build-runtime     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm runtime
             ${TESTCMD} --label=build-bcl     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm bcl
             ${TESTCMD} --label=build-cross     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm cross
-
-            ${TESTCMD} --label=build-cross     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm cross
-            ${TESTCMD} --label=build-wasm     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm             
+            
+            #${TESTCMD} --label=build-wasm     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/wasm             
         else        
             ${TESTCMD} --label=configure --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/builds configure-wasm NINJA=
             ${TESTCMD} --label=build     --timeout=180m --fatal $gnumake -j ${CI_CPU_COUNT} --output-sync=recurse --trace -C sdks/builds build-wasm     NINJA=
