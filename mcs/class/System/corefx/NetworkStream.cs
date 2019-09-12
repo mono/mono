7ecf1f007ef20d4addcc6f62ff1e0ab2d10333e5
Martin Baulig <mabaul@microsoft.com>
2019-09-12 17:56:29 +0200

    X

---
+++ mcs/class/System/corefx/NetworkStream.cs
@@ -0,0 +1,15 @@
+namespace System.Net.Sockets
+{
+	partial class NetworkStream
+	{
+		internal Socket InternalSocket {
+			get {
+				Socket chkSocket = _streamSocket;
+				if (_cleanedUp || chkSocket == null)
+					throw new ObjectDisposedException (GetType ().FullName);
+
+				return chkSocket;
+			}
+		}
+	}
+}
