172bf2377aafd2703119bc915e58b49367939745
Bernhard Urban <lewurm@gmail.com>
2019-09-16 19:18:20 +0200

    [arm64_32] make trampolines bitwidth aware
    * replace pointer loads/stores with bitwidth-dependent macro
    * reflect `specific` trampolines so that only two instructions are used for ILP32, since the related pair in the data page only needs 2 * 32bits too
    * replace some `sizeof (target_mgreg_t)` with `TARGET_SIZEOF_VOID_P` (former is hardware register size, latter is pointer size)

--- mono/mini/image-writer.c
+++ mono/mini/image-writer.c
@@ -1866,7 +1866,7 @@ static void
 asm_writer_emit_pointer (MonoImageWriter *acfg, const char *target)
 {
 	asm_writer_emit_unset_mode (acfg);
-	asm_writer_emit_alignment (acfg, sizeof (target_mgreg_t));
+	asm_writer_emit_alignment (acfg, TARGET_SIZEOF_VOID_P);
 	asm_writer_emit_pointer_unaligned (acfg, target);
 }
 
