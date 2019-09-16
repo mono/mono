c8310ce7e1c1aab5aa6cac21e299f77de5ff7707
Jay Krell <jaykrell@microsoft.com>
2019-09-15 22:49:14 -0700

    [Coop] InterlockedExchange (object and T), InterlockedCompareExchange (object and T).
     - Usually intrinsified.
     - Always?
     - Avoid coop handles by manually always using ref to object.
     - For efficiency on input, and efficiency wrt MonoError.
     - For correctness on output (due to aliasing in the wrappers).
     - To probably avoid assertion failure in runtime around icall marshaling of generics.
     - Unclear on location.

--- mono/mini/aot-compiler.c
+++ mono/mini/aot-compiler.c
@@ -5684,7 +5684,8 @@ add_generic_instances (MonoAotCompile *acfg)
 			gpointer iter = NULL;
 
 			while ((m = mono_class_get_methods (interlocked_klass, &iter))) {
-				if ((!strcmp (m->name, "CompareExchange") || !strcmp (m->name, "Exchange")) && m->is_generic) {
+				if ((m->iflags & METHOD_IMPL_ATTRIBUTE_INTERNAL_CALL) && m->is_generic &&
+					(!strcmp (m->name, "CompareExchange") || !strcmp (m->name, "Exchange"))) {
 					ERROR_DECL (error);
 					memset (&ctx, 0, sizeof (ctx));
 					args [0] = object_type;
