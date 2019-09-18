fe64a4765e6d1dbb41d5c86708fcb02aa519247a
Larry Ewing <lewing@microsoft.com>
2019-09-18 12:13:51 -0500

    [2019-06] Bump msbuild and sdk versions to 3.0.1xx latest (#16870)
    * [2019-06] Bump msbuild and sdk versions to 3.0.1xx latest
    
    * Bump msbuild again to pull in the build updates
    
    * pull in roslyn-binaries fix
    
    * One more bump
    
    * Fix the roslyn hash
    
    * [msbuild] Bump to pick up a build fix from https://github.com/mono/msbuild/pull/138
    
    * Pull in final update from msbuild of nuget and sdk
    NuGet 5.3.0-rtm.6251
    Sdk 3.0.100-rc2.19467.3
    
    * Pick up packaging fix in https://github.com/mono/msbuild/pull/141

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '63cdc9079266c170dc4a3f14704f3fc07be81bc7')
+			revision = '560b1caa659dea7c4d51cf566b50ac9735bfb05b')
 
 	def build (self):
 		try:       
