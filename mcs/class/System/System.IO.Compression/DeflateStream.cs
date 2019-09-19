10a24f3ea1db9c776fa59d69050691659464608d
EgorBo <egorbo@gmail.com>
2019-09-19 22:56:42 +0300

    Implement WriteCore and ReadCore in DeflateStream

--- mcs/class/System/System.IO.Compression/DeflateStream.cs
+++ mcs/class/System/System.IO.Compression/DeflateStream.cs
@@ -145,7 +145,10 @@ namespace System.IO.Compression
 
 		internal int ReadCore (Span<byte> destination)
 		{
-			throw new NotImplementedException ();
+			var buffer = new byte [destination.Length];
+			int count = Read(buffer, 0, buffer.Length);
+			buffer.AsSpan(0, count).CopyTo(destination);
+			return count;
 		}
 
 		public override int Read (byte[] array, int offset, int count)
@@ -185,7 +188,7 @@ namespace System.IO.Compression
 
 		internal void WriteCore (ReadOnlySpan<byte> source)
 		{
-			throw new NotImplementedException ();
+			Write (source.ToArray (), 0, source.Length);
 		}
 
 		public override void Write (byte[] array, int offset, int count)
