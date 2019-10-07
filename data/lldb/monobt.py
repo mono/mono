d46c25e2946f1d671db38c1df5a2d3318237772d
Bernhard Urban <lewurm@gmail.com>
2019-10-07 08:33:01 +0200

    [lldb] make it python3 safe (required for xcode11)

--- data/lldb/monobt.py
+++ data/lldb/monobt.py
@@ -2,7 +2,7 @@ import lldb
 
 def print_frames(thread, num_frames, current_thread):
     # TODO: Make output header similar to bt.
-    print '%c thread #%i' % ('*' if current_thread else ' ', thread.idx)
+    print('%c thread #%i' % ('*' if current_thread else ' ', thread.idx))
 
     if current_thread:
         selected_frame = thread.GetSelectedFrame()
@@ -23,7 +23,7 @@ def print_frames(thread, num_frames, current_thread):
                     insn = "\"" + frame.EvaluateExpression('mono_interp_opname [*ip]').summary[1:-1] + "\""
                 var = '%s @ %d %s || %s' % (methoddesc, ipoffset, insn, frame)
             except Exception as e:
-                print "DBG: execfail:" + str(e)
+                print("DBG: execfail:" + str(e))
         elif function_name == "mono_interp_transform_method":
             try:
                 s = 'runtime_method->method'
@@ -31,7 +31,7 @@ def print_frames(thread, num_frames, current_thread):
                 methodname = frame.EvaluateExpression('(char*) ' + s + '->name').summary[1:-1]
                 var = 'transforming %s::%s || %s' % (klassname, methodname, frame)
             except Exception as e:
-                print "DBG: transformfail:" + str(e)
+                print("DBG: transformfail:" + str(e))
         elif pc[0] == '0':
             try:
                 framestr = frame.EvaluateExpression('(char*)mono_pmip((void*)%s)' % pc).summary[1:-1]
@@ -39,7 +39,7 @@ def print_frames(thread, num_frames, current_thread):
             except:
                 pass
 
-        print '  %c %s' % ('*' if current_thread and frame.idx == selected_frame.idx else ' ', var)
+        print('  %c %s' % ('*' if current_thread and frame.idx == selected_frame.idx else ' ', var))
 
 def monobt(debugger, command, result, dict):
     opts = {'all_bt': False, 'num_frames': None}
@@ -49,20 +49,20 @@ def monobt(debugger, command, result, dict):
     elif command.isdigit():
         opts['num_frames'] = int(command)
     elif command != '':
-        print 'error: monobt [<number>|all]'
+        print('error: monobt [<number>|all]')
         return
 
     target = debugger.GetSelectedTarget()
     process = target.process
 
     if not process.IsValid():
-        print 'error: invalid process'
+        print('error: invalid process')
         return
 
     if opts['all_bt']:
         for thread in process.threads:
             print_frames(thread, len(thread), process.selected_thread == thread)
-            print ''
+            print('')
     else:
         thread = process.selected_thread
         num_frames = len(thread) if opts['num_frames'] is None else opts['num_frames']
@@ -75,4 +75,4 @@ def __lldb_init_module (debugger, dict):
     # Add any commands contained in this module to LLDB
     debugger.HandleCommand('command script add -f monobt.monobt monobt')
     debugger.HandleCommand('command alias mbt monobt')
-    print '"monobt" command installed'
+    print('"monobt" command installed')
