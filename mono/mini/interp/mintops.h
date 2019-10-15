dd66e3c51da6cab6bf76be2083285558026c049c
Vlad Brezae <brezaevlad@gmail.com>
2019-10-15 17:41:44 +0300

    [interp] Move instructions together
    For easy check if an instruction is unary or binary operation. We consider conversion instructions to be unary operators and compare instructions to be binary operators.

--- mono/mini/interp/mintops.h
+++ mono/mini/interp/mintops.h
@@ -62,6 +62,8 @@ typedef enum {
 #define MINT_IS_CALL(op) ((op) >= MINT_CALL && (op) <= MINT_JIT_CALL)
 #define MINT_IS_NEWOBJ(op) ((op) >= MINT_NEWOBJ && (op) <= MINT_NEWOBJ_MAGIC)
 #define MINT_IS_LDC_I4(op) ((op) >= MINT_LDC_I4_M1 && (op) <= MINT_LDC_I4)
+#define MINT_IS_UNOP(op) ((op) >= MINT_ADD1_I4 && (op) <= MINT_CEQ0_I4)
+#define MINT_IS_BINOP(op) ((op) >= MINT_ADD_I4 && (op) <= MINT_CLT_UN_R8)
 
 #define MINT_POP_ALL	-2
 #define MINT_VAR_PUSH	-1
