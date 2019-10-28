4847b8e7a721a488c228080a932124337966e5a8
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2019-10-28 17:16:42 -0400

    [2019-10] Embedding API GC Unsafe transition fixes (#17586)
    * [runtime] Make mono_thread_manage external only
    
    runtime should use mono_thread_manage_internal
    
    * [runtime] Do an unbalanced GC Unsafe transition in mono_jit_cleanup
    
    After mini_cleanup, we don't have GC thread states anymore because all that
    stuff was cleaned up, so MONO_EXIT_GC_UNSAFE would assert
    
    * [runtime] Mark mono_runtime_quit external only.
    
    Runtime should use mono_runtime_quit_internal.
    
    After we call the quit_function (aka mini_cleanup) we don't have any GC thread
    states anymore because all that stuff got cleaned up.  So MONO_EXIT_GC_UNSAFE
    can't work.

--- mono/metadata/threads.c
+++ mono/metadata/threads.c
@@ -3662,11 +3662,13 @@ mono_threads_set_shutting_down (void)
 }
 
 /**
- * mono_thread_manage:
+ * mono_thread_manage_internal:
  */
 void
-mono_thread_manage (void)
+mono_thread_manage_internal (void)
 {
+	MONO_REQ_GC_UNSAFE_MODE;
+
 	struct wait_data wait_data;
 	struct wait_data *wait = &wait_data;
 
