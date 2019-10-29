e4b9771a41afab8f86b42f346f87e7e729297f08
Nathan Ricci <naricc@microsoft.com>
2019-10-29 14:41:26 -0400

    Rolled back unneeded change.

--- mono/sgen/sgen-copy-object.h
+++ mono/sgen/sgen-copy-object.h
@@ -68,7 +68,6 @@ copy_object_no_checks (GCObject *obj, SgenGrayQueue *queue)
 	GCVTable vt = SGEN_LOAD_VTABLE_UNCHECKED (obj);
 	gboolean has_references = SGEN_VTABLE_HAS_REFERENCES (vt);
 	mword objsize = SGEN_ALIGN_UP (sgen_client_par_object_get_size (vt, obj));
-
 	void *destination = COLLECTOR_SERIAL_ALLOC_FOR_PROMOTION (vt, obj, objsize, has_references);
 
 	if (G_UNLIKELY (!destination)) {
@@ -109,7 +108,6 @@ copy_object_no_checks_par (GCObject *obj, SgenGrayQueue *queue)
 		 */
 		gboolean has_references = SGEN_VTABLE_HAS_REFERENCES (vt);
 		mword objsize = SGEN_ALIGN_UP (sgen_client_par_object_get_size (vt, obj));
-		
 		destination = COLLECTOR_PARALLEL_ALLOC_FOR_PROMOTION (vt, obj, objsize, has_references);
 
 		par_copy_object_no_checks ((char*)destination, vt, obj, objsize);
