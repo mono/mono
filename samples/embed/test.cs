d160682d54b0627a9212d140cec6e22a3a7036c1
Thays Grazia <thaystg@gmail.com>
2019-11-06 15:09:11 -0300

    Update samples/embed/test.cs
    Co-Authored-By: Aleksey Kliger (λgeek) <akliger@gmail.com>

--- samples/embed/test.cs
+++ samples/embed/test.cs
@@ -5,7 +5,8 @@ class MonoEmbed {
 	[MethodImplAttribute(MethodImplOptions.InternalCall)]
 	extern static string gimme();
 
-	static int Main() {
+	static int Main ()
+	{
 		System.Console.WriteLine(gimme ());
 		if (gimme ().Equals("All your monos are belong to us!"))
 			return 0;
