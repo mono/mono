44826291e7a0779dd66af67ac8a825b4b0d1fca6
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-11-07 02:19:50 +0100

    [sdks] Add xunit to iOS test runner and add results reporting
    - Allows the on-device runner to work with the xunit framework in preparation for netcore.
    
    - Added a simple UI to show the test run progress
    
    - Report the test result XML back to the host so we can process it on Jenkins/AzDO

--- sdks/ios/runtime/ViewController.m
+++ sdks/ios/runtime/ViewController.m
@@ -16,15 +16,47 @@
 
 @implementation ViewController
 
+UILabel *lblResults;
+int passed = 0, skipped = 0, failed = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+	UILabel *lblHeader = [[UILabel alloc] init];
+    lblHeader.frame = CGRectMake(100, 100, 200, 200);
+	lblHeader.backgroundColor = [UIColor clearColor];
+	lblHeader.textColor = [UIColor redColor];
+	lblHeader.font = [UIFont boldSystemFontOfSize: 20];
+	lblHeader.numberOfLines = 2;
+	lblHeader.text = @"Mono iOS SDK\nRunning tests...";
+	[self.view addSubview:lblHeader];
+
+	lblResults = [[UILabel alloc] init];
+    lblResults.frame = CGRectMake(100, 200, 200, 200);
+	lblResults.backgroundColor = [UIColor clearColor];
+	lblResults.textColor = [UIColor redColor];
+	lblResults.font = [UIFont boldSystemFontOfSize: 20];
+	lblResults.numberOfLines = 3;
+	lblResults.text = @"Passed: 0\nSkipped: 0\nFailed: 0";
+	[self.view addSubview:lblResults];
+
+	NSTimer* timer = [NSTimer timerWithTimeInterval:0.5f target:self selector:@selector(updateTestResults) userInfo:nil repeats:YES];
+	[[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+
 	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-			[self startRuntime];
-		});
+		[self startRuntime];
+	});
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)updateTestResults {
+	dispatch_async(dispatch_get_main_queue(), ^{
+		lblResults.text = [NSString stringWithFormat: @"Passed: %i\nSkipped: %i\nFailed: %i", passed, skipped, failed];
+	});
+}
+
 - (void)startRuntime {
+	mono_sdks_ui_register_testcase_result_fields (&passed, &skipped, &failed);
 	mono_ios_runtime_init ();
 }
 
