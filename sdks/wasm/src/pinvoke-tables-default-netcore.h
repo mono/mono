79ce0d30cc6d3716de71c07211b2f3dc27832108
Zoltan Varga <vargaz@gmail.com>
2019-11-15 00:41:47 +0100

    Add more netcore pinvokes.

--- sdks/wasm/src/pinvoke-tables-default-netcore.h
+++ sdks/wasm/src/pinvoke-tables-default-netcore.h
@@ -76,6 +76,34 @@ int SystemNative_ReadStdin (int,int);
 void SystemNative_InitializeConsoleBeforeRead (int,int);
 void SystemNative_UninitializeConsoleAfterRead ();
 int SystemNative_StdinReady ();
+int SystemNative_ConvertErrorPlatformToPal (int);
+int SystemNative_ConvertErrorPalToPlatform (int);
+int SystemNative_StrErrorR (int,int,int);
+int SystemNative_GetEUid ();
+int SystemNative_GetAllMountPoints (int);
+int SystemNative_OpenDir (int);
+int SystemNative_GetReadDirRBufferSize ();
+int SystemNative_ReadDirR (int,int,int,int);
+int SystemNative_CloseDir (int);
+int SystemNative_Stat (int,int);
+int SystemNative_LStat (int,int);
+int SystemNative_Unlink (int);
+int SystemNative_ChMod (int,int);
+int SystemNative_CopyFile (int,int,int,int);
+int SystemNative_GetEGid ();
+int SystemNative_LChflags (int,int);
+int SystemNative_LChflagsCanSetHiddenFlag ();
+int SystemNative_Link (int,int);
+int SystemNative_MkDir (int,int);
+int SystemNative_Rename (int,int);
+int SystemNative_RmDir (int);
+int SystemNative_Stat (int,int);
+int SystemNative_LStat (int,int);
+int SystemNative_UTimensat (int,int);
+int SystemNative_GetUnixName ();
+int SystemNative_GetUnixVersion (int,int);
+int SystemNative_GetOSArchitecture ();
+int SystemNative_GetProcessArchitecture ();
 static PinvokeImport System_Native_imports [] = {
 {"SystemNative_ConvertErrorPlatformToPal", SystemNative_ConvertErrorPlatformToPal},
 {"SystemNative_ConvertErrorPalToPlatform", SystemNative_ConvertErrorPalToPlatform},
@@ -149,6 +177,34 @@ static PinvokeImport System_Native_imports [] = {
 {"SystemNative_InitializeConsoleBeforeRead", SystemNative_InitializeConsoleBeforeRead},
 {"SystemNative_UninitializeConsoleAfterRead", SystemNative_UninitializeConsoleAfterRead},
 {"SystemNative_StdinReady", SystemNative_StdinReady},
+{"SystemNative_ConvertErrorPlatformToPal", SystemNative_ConvertErrorPlatformToPal},
+{"SystemNative_ConvertErrorPalToPlatform", SystemNative_ConvertErrorPalToPlatform},
+{"SystemNative_StrErrorR", SystemNative_StrErrorR},
+{"SystemNative_GetEUid", SystemNative_GetEUid},
+{"SystemNative_GetAllMountPoints", SystemNative_GetAllMountPoints},
+{"SystemNative_OpenDir", SystemNative_OpenDir},
+{"SystemNative_GetReadDirRBufferSize", SystemNative_GetReadDirRBufferSize},
+{"SystemNative_ReadDirR", SystemNative_ReadDirR},
+{"SystemNative_CloseDir", SystemNative_CloseDir},
+{"SystemNative_Stat", SystemNative_Stat},
+{"SystemNative_LStat", SystemNative_LStat},
+{"SystemNative_Unlink", SystemNative_Unlink},
+{"SystemNative_ChMod", SystemNative_ChMod},
+{"SystemNative_CopyFile", SystemNative_CopyFile},
+{"SystemNative_GetEGid", SystemNative_GetEGid},
+{"SystemNative_LChflags", SystemNative_LChflags},
+{"SystemNative_LChflagsCanSetHiddenFlag", SystemNative_LChflagsCanSetHiddenFlag},
+{"SystemNative_Link", SystemNative_Link},
+{"SystemNative_MkDir", SystemNative_MkDir},
+{"SystemNative_Rename", SystemNative_Rename},
+{"SystemNative_RmDir", SystemNative_RmDir},
+{"SystemNative_Stat", SystemNative_Stat},
+{"SystemNative_LStat", SystemNative_LStat},
+{"SystemNative_UTimensat", SystemNative_UTimensat},
+{"SystemNative_GetUnixName", SystemNative_GetUnixName},
+{"SystemNative_GetUnixVersion", SystemNative_GetUnixVersion},
+{"SystemNative_GetOSArchitecture", SystemNative_GetOSArchitecture},
+{"SystemNative_GetProcessArchitecture", SystemNative_GetProcessArchitecture},
 {NULL, NULL}
 };
 static void *pinvoke_tables[] = { System_Native_imports,};
