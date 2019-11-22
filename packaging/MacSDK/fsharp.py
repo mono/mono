39d7919c044c374333bde9ecc63e288c2133bd85
Alex Corrado <alexc@xamarin.com>
2019-11-22 18:35:53 -0500

    [fsharp] Remove failing patch

--- packaging/MacSDK/fsharp.py
+++ packaging/MacSDK/fsharp.py
@@ -8,8 +8,7 @@ class FsharpPackage(GitHubTarballPackage):
             override_properties={ 'make': 'make all install PREFIX="%{package_prefix}" DESTDIR=%{stage_root}' })
 
         self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
-        self.sources.extend(['patches/fsharp-IsPathRooted-type-inference.patch',
-                             'patches/fsharp-portable-pdb.patch',
+        self.sources.extend(['patches/fsharp-portable-pdb.patch',
                              'patches/fsharp-noinstall.patch',
                              'patches/fsharp-GetFileNameWithoutExtension-type-inference.patch',
                              'patches/fsharp-msbuild-16-0.patch',
