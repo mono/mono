9524d1a487073a7e2e4e8b4e108932efdefc6740
Maxim Lipnin <v-maxlip@microsoft.com>
2019-11-26 10:07:20 +0300

    [wasm][nunit tests] Remove PNSE expectation in MonoTests.System.Net.HttpListenerTest nunit tests

--- mcs/class/System/Test/System.Net/HttpListenerTest.cs
+++ mcs/class/System/Test/System.Net/HttpListenerTest.cs
@@ -39,8 +39,7 @@ namespace MonoTests.System.Net {
 	[TestFixture]
 	public class HttpListenerTest {
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void DefaultProperties ()
@@ -56,8 +55,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void Start1 ()
@@ -67,8 +65,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void Stop1 ()
@@ -78,8 +75,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (InvalidOperationException))]
@@ -92,8 +88,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (InvalidOperationException))]
@@ -107,8 +102,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (InvalidOperationException))]
@@ -121,8 +115,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void BeginGetContext2 ()
@@ -234,8 +227,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void CloseBeforeStart ()
@@ -279,8 +271,7 @@ namespace MonoTests.System.Net {
 		}
 		
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void AbortBeforeStart ()
@@ -301,8 +292,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void PropertiesWhenClosed1 ()
@@ -318,8 +308,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -332,8 +321,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -346,8 +334,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -361,7 +348,7 @@ namespace MonoTests.System.Net {
 
 		[Test]
 		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -374,8 +361,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -388,8 +374,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #else
 		[ExpectedException (typeof (ObjectDisposedException))]
@@ -402,8 +387,7 @@ namespace MonoTests.System.Net {
 		}
 
 		[Test]
-		[Category ("NotWasm")]
-#if FEATURE_NO_BSD_SOCKETS
+#if FEATURE_NO_BSD_SOCKETS && !WASM
 		[ExpectedException (typeof (PlatformNotSupportedException))]
 #endif
 		public void PropertiesWhenClosed3 ()
