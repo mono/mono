mono/mono

commit 43d87f5b691b374211f4af35dbe51d1a2775ccee

Break out of outter loop once file is found. (case 1118285)

mono/mini/debugger-agent.c
@@ -3824,7 +3824,7 @@ create_event_list (EventKind event, GPtrArray *reqs, MonoJitInfo *ji, DebuggerEv
 							break;
 					}
 #else
-					while ((method = mono_class_get_methods (ei->klass, &iter))) {
+					while (!found && (method = mono_class_get_methods (ei->klass, &iter))) {
 						MonoDebugMethodInfo *minfo = mono_debug_lookup_method (method);
 
 						if (minfo) {

commit 97d5edc2f1ea6555522fcca81d050f02d13a4e25

Reintroduce fix for case 1118285.
Original commit https://github.com/Unity-Technologies/mono/commit/43d87f5b691b374211f4af35dbe51d1a2775ccee

Accidentally reverted https://github.com/Unity-Technologies/mono/commit/8d86df978f3907ccf038294b2730297c2b22fe20

mono/mini/debugger-agent.c
@@ -3904,7 +3904,7 @@ create_event_list (EventKind event, GPtrArray *reqs, MonoJitInfo *ji, DebuggerEv
 							break;
 					}
 #else
-					while ((method = mono_class_get_methods (ei->klass, &iter))) {
+					while (!found && (method = mono_class_get_methods (ei->klass, &iter))) {
 						MonoDebugMethodInfo *minfo = mono_debug_lookup_method (method);
 
 						if (minfo) {