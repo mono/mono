17a925ca258658bc539eb649258f0053416d1d4f
Ankit Jain <radical@gmail.com>
2019-12-10 06:39:51 -0500

    Bump msbuild+roslyn to track mono-2019-10 (#18092)
    * Bump msbuild+roslyn to track mono-2019-10
    
    - bumps roslyn to 3.5.0-beta1-19606-04
    
    * roslyn: Delete reference to the now removed Microsoft.Managed.EditorConfig.targets

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '3495481fd0db507969134989f138bf89d6f56873')
+			revision = '5310081448152f227bec72971e8f242f42ae84b4')
 
 	def build (self):
 		try:
