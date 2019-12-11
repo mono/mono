58806bfdd42971ce6d722dbca77f0345f9865cd0
Ankit Jain <radical@gmail.com>
2019-12-11 10:20:39 -0500

    Bump msbuild to track mono-2019-10
    - msbuild doesn't have a separate branch for mono-2019-12 right now

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '5310081448152f227bec72971e8f242f42ae84b4')
+			revision = 'efd2efd2b88d9e747f40a9fb4489b8f7ac3e8299')
 
 	def build (self):
 		try:
