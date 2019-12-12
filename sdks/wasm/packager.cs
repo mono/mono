d6b5c24032bda662e20f9c5804b323bc1ebe766d
Zoltan Varga <vargaz@gmail.com>
2019-12-12 08:12:11 +0100

    [wasm] Add linker substitutions in netcore mode.

--- sdks/wasm/packager.cs
+++ sdks/wasm/packager.cs
@@ -943,7 +943,10 @@ class Driver {
 		}
 		if (enable_aot)
 			ninja.WriteLine ("build $builddir/aot-in: mkdir");
-
+		if (is_netcore) {
+			var source_file = Path.GetFullPath (Path.Combine (tool_prefix, "src", "linker-subs.xml"));
+			ninja.WriteLine ($"build $builddir/linker-subs.xml: cpifdiff {source_file}");
+		}
 		var ofiles = "";
 		var bc_files = "";
 		string linker_infiles = "";
@@ -1085,6 +1088,10 @@ class Driver {
 			}
 
 			string linker_args = "";
+			if (is_netcore) {
+				linker_args += "--substitutions linker-subs.xml ";
+				linker_infiles += "| linker-subs.xml";
+			}
 			if (!string.IsNullOrEmpty (linkDescriptor)) {
 				linker_args += $"-x {linkDescriptor} ";
 				foreach (var assembly in root_assemblies) {
