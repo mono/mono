2aca5b4055651ae066fb5c57e178be79cb59d200
Zoltan Varga <vargaz@gmail.com>
2019-12-16 20:27:24 +0100

    [wasm] Disable linker substitutions for now, it generates invalid IL when running make build-aot-hello-netcore.

--- sdks/wasm/packager.cs
+++ sdks/wasm/packager.cs
@@ -1088,7 +1088,7 @@ class Driver {
 			}
 
 			string linker_args = "";
-			linker_args += "--substitutions linker-subs.xml ";
+			//linker_args += "--substitutions linker-subs.xml ";
 			linker_infiles += "| linker-subs.xml";
 			if (!string.IsNullOrEmpty (linkDescriptor)) {
 				linker_args += $"-x {linkDescriptor} ";
