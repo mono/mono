5f43260052eeb6a1c6d54e7e5d505510c1d71599
Katelyn Gadd <kg@luminance.org>
2019-12-30 16:43:33 -0800

    Tabify

--- mcs/tools/exception-filter-rewriter/Program.cs
+++ mcs/tools/exception-filter-rewriter/Program.cs
@@ -6,104 +6,104 @@ using Mono.Cecil;
 using Mono.Cecil.Cil;
 
 namespace ExceptionRewriter {
-    class Program {
-        public static int Main (string[] _args) {
-            try {
-                var options = new RewriteOptions();
+	class Program {
+		public static int Main (string[] _args) {
+			try {
+				var options = new RewriteOptions();
 
-                foreach (var arg in _args)
-                    ParseArgument(arg, options);
+				foreach (var arg in _args)
+					ParseArgument(arg, options);
 
-                var argv = _args.Where(arg => !arg.StartsWith("-")).ToArray();
+				var argv = _args.Where(arg => !arg.StartsWith("-")).ToArray();
 
-                var step = options.Overwrite ? 1 : 2;
-                if (argv.Length < step) {
-                    Usage();
-                    return 1;
-                }
+				var step = options.Overwrite ? 1 : 2;
+				if (argv.Length < step) {
+					Usage();
+					return 1;
+				}
 
-                for (int i = 0; i < argv.Length; i += step) {
-                    var src = argv[i];
-                    var dst = options.Overwrite ? src : argv[i + 1];
+				for (int i = 0; i < argv.Length; i += step) {
+					var src = argv[i];
+					var dst = options.Overwrite ? src : argv[i + 1];
 
-                    if (options.Verbose)
-                        Console.WriteLine($"{Path.GetFileName(src)} -> {Path.GetFullPath(dst)}...{Environment.NewLine}====");
-                    else
-                        Console.WriteLine($"{Path.GetFileName(src)} -> {Path.GetFullPath(dst)}");
+					if (options.Verbose)
+						Console.WriteLine($"{Path.GetFileName(src)} -> {Path.GetFullPath(dst)}...{Environment.NewLine}====");
+					else
+						Console.WriteLine($"{Path.GetFileName(src)} -> {Path.GetFullPath(dst)}");
 
-                    var assemblyResolver = new DefaultAssemblyResolver();
-                    assemblyResolver.AddSearchDirectory(Path.GetDirectoryName(src));
+					var assemblyResolver = new DefaultAssemblyResolver();
+					assemblyResolver.AddSearchDirectory(Path.GetDirectoryName(src));
 
-                    using (var def = AssemblyDefinition.ReadAssembly(src, new ReaderParameters {
-                        ReadWrite = false,
-                        ReadingMode = ReadingMode.Immediate,
-                        AssemblyResolver = assemblyResolver,
-                        ReadSymbols = options.EnableSymbols,
-                        SymbolReaderProvider = new DefaultSymbolReaderProvider(throwIfNoSymbol: false)
-                    })) {
-                        var arw = new AssemblyRewriter(def, options);
-                        arw.Rewrite();
+					using (var def = AssemblyDefinition.ReadAssembly(src, new ReaderParameters {
+						ReadWrite = false,
+						ReadingMode = ReadingMode.Immediate,
+						AssemblyResolver = assemblyResolver,
+						ReadSymbols = options.EnableSymbols,
+						SymbolReaderProvider = new DefaultSymbolReaderProvider(throwIfNoSymbol: false)
+					})) {
+						var arw = new AssemblyRewriter(def, options);
+						arw.Rewrite();
 
-                        var shouldWriteSymbols = options.EnableSymbols && def.MainModule.SymbolReader != null;
+						var shouldWriteSymbols = options.EnableSymbols && def.MainModule.SymbolReader != null;
 
-                        def.Write(dst + ".tmp", new WriterParameters {
-                            WriteSymbols = shouldWriteSymbols
-                        });
-                    }
+						def.Write(dst + ".tmp", new WriterParameters {
+							WriteSymbols = shouldWriteSymbols
+						});
+					}
 
-                    File.Copy(dst + ".tmp", dst, true);
-                    if (File.Exists(dst + ".pdb")) {
-                        File.Copy(dst + ".pdb", dst.Replace(".exe", ".pdb"), true);
-                        File.Delete(dst + ".pdb");
-                    }
-                    File.Delete(dst + ".tmp");
-                }
+					File.Copy(dst + ".tmp", dst, true);
+					if (File.Exists(dst + ".pdb")) {
+						File.Copy(dst + ".pdb", dst.Replace(".exe", ".pdb"), true);
+						File.Delete(dst + ".pdb");
+					}
+					File.Delete(dst + ".tmp");
+				}
 
-                return 0;
-            } finally {
-                if (Debugger.IsAttached) {
-                    Console.WriteLine("Press enter to exit");
-                    Console.ReadLine();
-                }
-            }
-        }
+				return 0;
+			} finally {
+				if (Debugger.IsAttached) {
+					Console.WriteLine("Press enter to exit");
+					Console.ReadLine();
+				}
+			}
+		}
 
-        static void ParseArgument (string arg, RewriteOptions options) {
-            if (!arg.StartsWith("-"))
-                return;
+		static void ParseArgument (string arg, RewriteOptions options) {
+			if (!arg.StartsWith("-"))
+				return;
 
-            switch (arg) {
-                case "--overwrite":
-                    options.Overwrite = true;
-                    break;
-                case "--abort":
-                    options.ThrowOnError = true;
-                    break;
-                case "--warn":
-                    options.ThrowOnError = false;
-                    break;
-                case "--generics":
-                    options.EnableGenerics = true;
-                    break;
-                case "--no-generics":
-                    options.EnableGenerics = false;
-                    break;
-                case "--verbose":
-                    options.Verbose = true;
-                    break;
-                case "--quiet":
-                    options.Verbose = false;
-                    break;
-                case "--symbols":
-                    options.EnableSymbols = true;
-                    break;
-                default:
-                    throw new Exception("Unsupported argument: " + arg);
-            }
-        }
+			switch (arg) {
+				case "--overwrite":
+					options.Overwrite = true;
+					break;
+				case "--abort":
+					options.ThrowOnError = true;
+					break;
+				case "--warn":
+					options.ThrowOnError = false;
+					break;
+				case "--generics":
+					options.EnableGenerics = true;
+					break;
+				case "--no-generics":
+					options.EnableGenerics = false;
+					break;
+				case "--verbose":
+					options.Verbose = true;
+					break;
+				case "--quiet":
+					options.Verbose = false;
+					break;
+				case "--symbols":
+					options.EnableSymbols = true;
+					break;
+				default:
+					throw new Exception("Unsupported argument: " + arg);
+			}
+		}
 
-        static void Usage () {
-            Console.WriteLine(@"Expected: exception-filter-rewriter [options] input output [input2 output2] ...
+		static void Usage () {
+			Console.WriteLine(@"Expected: exception-filter-rewriter [options] input output [input2 output2] ...
 or        exception-filter-rewriter [options] --overwrite file1 [file2] ...
 --overwrite   Overwrite source file with rewritten output (input is a list of filenames instead of in/out pairs)
 --abort       Abort on error (default)
@@ -114,6 +114,6 @@ or        exception-filter-rewriter [options] --overwrite file1 [file2] ...
 --verbose     Output name of every rewritten method (default)
 --quiet       Do not output anything");
 
-        }
-    }
+		}
+	}
 }
