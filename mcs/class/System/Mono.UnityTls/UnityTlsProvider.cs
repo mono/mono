18d60a264a39d94653dcda1036d30f1887878560
andreasr <andreasr@unity3d.com>
2020-01-03 17:05:40 +0100

    [unitytls] Fixed/cleaned validation behavior for missing certificates

--- mcs/class/System/Mono.UnityTls/UnityTlsProvider.cs
+++ mcs/class/System/Mono.UnityTls/UnityTlsProvider.cs
@@ -53,19 +53,26 @@ namespace Mono.Unity
 			X509CertificateCollection certificates, bool wantsChain, ref X509Chain chain,
 			ref MonoSslPolicyErrors errors, ref int status11)
 		{
-			bool hasUnityTlsChain = (chain != null && chain.Impl is X509ChainImplUnityTls);
+			var errorState = UnityTls.NativeInterface.unitytls_errorstate_create ();
 
-			if (certificates == null && !hasUnityTlsChain) {
-				errors |= MonoSslPolicyErrors.RemoteCertificateNotAvailable;
-				return false;
+			var unityTlsChainImpl = chain.Impl as X509ChainImplUnityTls;
+			if (unityTlsChainImpl == null)
+			{
+				if (certificates == null || certificates.Count == 0) {
+					errors |= MonoSslPolicyErrors.RemoteCertificateNotAvailable;
+					return false;
+				}
+
+				if (wantsChain)
+					chain = MNS.SystemCertificateValidator.CreateX509Chain (certificates);
 			}
-
-			if (wantsChain && !hasUnityTlsChain)
-				chain = MNS.SystemCertificateValidator.CreateX509Chain (certificates);
-
-			if (certificates == null || certificates.Count == 0) {
-				errors |= MonoSslPolicyErrors.RemoteCertificateNotAvailable;
-				return false;
+			else
+			{
+				var cert = UnityTls.NativeInterface.unitytls_x509list_get_x509 (unityTlsChainImpl.NativeCertificateChain, (size_t)0, &errorState);
+				if (cert.handle == UnityTls.NativeInterface.UNITYTLS_INVALID_HANDLE) {
+					errors |= MonoSslPolicyErrors.RemoteCertificateNotAvailable;
+					return false;
+				}
 			}
 
 			// fixup targetHost name by removing port
@@ -76,7 +83,6 @@ namespace Mono.Unity
 			}
 
 			// convert cert to native
-			var errorState = UnityTls.NativeInterface.unitytls_errorstate_create ();
 			var result = UnityTls.unitytls_x509verify_result.UNITYTLS_X509VERIFY_NOT_DONE;
 			UnityTls.unitytls_x509list* certificatesNative = null;
 			try
@@ -88,14 +94,14 @@ namespace Mono.Unity
 				//validator.Settings.CertificateSearchPaths				// currently only used by MonoBtlsProvider
 
 				UnityTls.unitytls_x509list_ref certificatesNativeRef;
-				if (!hasUnityTlsChain)
+				if (unityTlsChainImpl == null)
 				{
 					certificatesNative = UnityTls.NativeInterface.unitytls_x509list_create (&errorState);
 					CertHelper.AddCertificatesToNativeChain (certificatesNative, certificates, &errorState);
 					certificatesNativeRef = UnityTls.NativeInterface.unitytls_x509list_get_ref (certificatesNative, &errorState);
 				}
 				else
-					certificatesNativeRef = ((X509ChainImplUnityTls)chain.Impl).NativeCertificateChain;
+					certificatesNativeRef = unityTlsChainImpl.NativeCertificateChain;
 				
 				var targetHostUtf8 = Encoding.UTF8.GetBytes (targetHost);
 
