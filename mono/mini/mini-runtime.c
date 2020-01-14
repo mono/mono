1bc59119993ef704dff415adb79ec68b9cf3c6f2
Rene Garcia <rgarc@mit.edu>
2020-01-14 17:08:48 -0500

    Avoid always printing stats at shutdown.

--- mono/mini/mini-runtime.c
+++ mono/mini/mini-runtime.c
@@ -4749,7 +4749,8 @@ print_jit_stats (void)
 void
 mini_cleanup (MonoDomain *domain)
 {
-	g_printf ("Printing stats at shutdown\n");
+	if (mono_jit_stats.enabled)
+		g_printf ("Printing stats at shutdown\n");
 	print_jit_stats ();
 	mono_counters_dump (MONO_COUNTER_SECTION_MASK | MONO_COUNTER_MONOTONIC, stdout);
 }
@@ -4757,7 +4758,8 @@ mini_cleanup (MonoDomain *domain)
 void
 mini_cleanup (MonoDomain *domain)
 {
-	g_printf ("Printing stats at shutdown\n");
+	if (mono_jit_stats.enabled)
+		g_printf ("Printing stats at shutdown\n");
 	if (mono_profiler_sampling_enabled ())
 		mono_runtime_shutdown_stat_profiler ();
 
