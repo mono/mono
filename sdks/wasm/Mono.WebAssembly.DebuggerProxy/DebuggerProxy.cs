c24dbcad5222e4005a734949223b1d4fc231c988
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-25 11:38:49 +0000

    Move ownership of logger factory to calling code

--- sdks/wasm/Mono.WebAssembly.DebuggerProxy/DebuggerProxy.cs
+++ sdks/wasm/Mono.WebAssembly.DebuggerProxy/DebuggerProxy.cs
@@ -8,22 +8,15 @@ namespace WebAssembly.Net.Debugging {
 	// This type is the public entrypoint that allows external code to attach the debugger proxy
 	// to a given websocket listener. Everything else in this package can be internal.
 
-	public class DebuggerProxy : IDisposable {
-		private readonly ILoggerFactory loggerFactory;
+	public class DebuggerProxy {
 		private readonly MonoProxy proxy;
 
-		public DebuggerProxy () {
-			loggerFactory = LoggerFactory.Create(
-				builder => builder.AddConsole().AddFilter(null, LogLevel.Trace));
+		public DebuggerProxy (ILoggerFactory loggerFactory) {
 			proxy = new MonoProxy(loggerFactory);
 		}
 
 		public Task Run (Uri browserUri, WebSocket ideSocket) {
 			return proxy.Run (browserUri, ideSocket);
-		}
-
-		void IDisposable.Dispose() {
-			loggerFactory.Dispose();
 		}
 	}
 }
