691665884b7e53600c6d382373f72e930cc1b02a
Larry Ewing <lewing@microsoft.com>
2020-03-11 15:20:33 -0500

    [wasm][benchmarks] Add --wasm-package to bench runner to consume package builds (#19190)
    This adds an option the standalone-benchmark runner to extract the package zips created with the package target and available from jenkins for commits to master.

--- sdks/wasm/standalone-benchmarks/run-benchmark-wasm.sh
+++ sdks/wasm/standalone-benchmarks/run-benchmark-wasm.sh
@@ -4,13 +4,15 @@ set -x
 set -u
 function realpath { echo $(cd $(dirname ${1}); pwd)/$(basename ${1}); }
 
-
-RUNTIME_DIR="../builds"
 BENCHMARK=""
 while (( "$#" )); do
   case "$1" in
     --wasm-runtime-path=*)
       RUNTIME_DIR="${1#*=}"
+      shift
+      ;;
+    --wasm-package=*)
+      MONO_PACKAGE="${1#*=}"
       shift
       ;;
     -*|--*=)
@@ -23,6 +25,14 @@ while (( "$#" )); do
       ;;
   esac
 done
+
+
+if [ ! -z ${MONO_PACKAGE+x} ]; then
+    ZIP_DIR=${RUNTIME_DIR:=$(basename ${MONO_PACKAGE%.*})}
+    unzip -u -d $ZIP_DIR $MONO_PACKAGE "builds/**"
+    RUNTIME_DIR=$ZIP_DIR/builds
+fi
+RUNTIME_DIR=${RUNTIME_DIR:="../builds"}
 
 PACKAGER=$(realpath ../packager.exe)
 SRC_DIR=$(realpath $BENCHMARK/bin/Debug/netcoreapp2.1)
