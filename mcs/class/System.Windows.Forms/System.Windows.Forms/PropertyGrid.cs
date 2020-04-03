53992f16361a3ce207c791d5123298165dce4195
sancheolz <sancheolz@gmail.com>
2020-04-03 10:16:29 -0700

    [WinForms] Show ToolTips for PropertyTab`s buttons for custom tabs.

--- mcs/class/System.Windows.Forms/System.Windows.Forms/PropertyGrid.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/PropertyGrid.cs
@@ -939,6 +939,7 @@ namespace System.Windows.Forms
 			if (tabs != null && tabs.Count > 0) {
 				foreach (PropertyTab tab in tabs) {
 					PropertyToolBarButton button = new PropertyToolBarButton (tab);
+					button.ToolTipText = Locale.GetText(tab.TabName);
 					button.Click += new EventHandler (toolbarbutton_clicked);
 					toolbar.Items.Add (button);
 					if (tab.Bitmap != null) {
