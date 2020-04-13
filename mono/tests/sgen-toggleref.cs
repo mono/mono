8818a2707c58d999a272f0d537472af2cad02b50
Thays Grazia <thaystg@gmail.com>
2020-04-13 15:59:45 -0300

    Fixing old test that doesn't run on coreclr either.

--- mono/tests/sgen-toggleref.cs
+++ mono/tests/sgen-toggleref.cs
@@ -19,16 +19,45 @@ public class Toggleref {
 	}
 }
 
-[StructLayout (LayoutKind.Explicit)]
+
 public struct Helper {
+
+	private class ObjectWrapper
+	{
+		public object Object;
+	}
+
+	private class IntPtrWrapper
+	{
+		public IntPtr Value;
+	}
+
+	[StructLayout(LayoutKind.Explicit)]
+	private struct ObjectReinterpreter
+	{
+		[FieldOffset(0)] public ObjectWrapper AsObject;
+		[FieldOffset(0)] public IntPtrWrapper AsIntPtr;
+	}
+
+	private static object mutualObject;
+    private static ObjectReinterpreter reinterpreter;
+	
+	static Helper()
+    {
+        Helper.mutualObject = new object();
+        Helper.reinterpreter = new ObjectReinterpreter();
+        Helper.reinterpreter.AsObject = new ObjectWrapper();
+    }
+
 	public static IntPtr ObjToPtr (object obj)
 	{
-		unsafe 
-		{
-			TypedReference tr = __makeref(obj);
-			IntPtr ptr = **(IntPtr**)(&tr);
-			return ptr;
-		}
+		lock (Helper.mutualObject)
+        {
+            Helper.reinterpreter.AsObject.Object = obj;
+            IntPtr address = Helper.reinterpreter.AsIntPtr.Value;
+            Helper.reinterpreter.AsObject.Object = null;
+            return address;
+        }
 	}
 }
 
