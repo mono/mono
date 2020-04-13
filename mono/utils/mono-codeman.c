6d6f25c4c7f078799997299aadd16eb1d3c37e22
Geoffroy Aubey <geoffroy.aubey@apple.com>
2020-04-13 16:18:20 -0700

    Do not use MONO_MMAP_32BIT on Apple platforms MMAP_32BITS used to be undefined on darwin but was added recently which leads to unexpected behavior
    Signed-off-by: Tautvydas Žilys <tautvydas.zilys@gmail.com>

--- mono/utils/mono-codeman.c
+++ mono/utils/mono-codeman.c
@@ -57,7 +57,7 @@ static MonoCodeManagerCallbacks code_manager_callbacks;
 #define MAX_WASTAGE 32
 #define MIN_BSIZE 32
 
-#ifdef __x86_64__
+#if defined(__x86_64__) && !defined(__APPLE__)
 #define ARCH_MAP_FLAGS MONO_MMAP_32BIT
 #else
 #define ARCH_MAP_FLAGS 0
