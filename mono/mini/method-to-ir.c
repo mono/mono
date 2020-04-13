a8725b08dc6593de504524e2394b2154f5d1348a
Thays Grazia <thaystg@gmail.com>
2020-04-13 17:35:27 -0300

    Initialize klass value before call mono_field_from_token_checked.

--- mono/mini/method-to-ir.c
+++ mono/mini/method-to-ir.c
@@ -9284,9 +9284,10 @@ calli_end:
 				klass = field->parent;
 			}
 			else {
+				klass = NULL;
 				field = mono_field_from_token_checked (image, token, &klass, generic_context, cfg->error);
-				if (!field && mono_class_has_failure (klass))
-					TYPE_LOAD_ERROR (klass);
+				if (!field)
+					CHECK_TYPELOAD (klass);
 				CHECK_CFG_ERROR;
 			}
 			if (!dont_verify && !cfg->skip_visibility && !mono_method_can_access_field (method, field))
