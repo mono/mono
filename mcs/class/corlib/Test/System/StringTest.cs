c4ee08a75e75ad44838f09089e7a8de6ec2bb14f
Zoltan Varga <vargaz@gmail.com>
2020-04-16 21:05:23 -0400

    [corlib] Fix String.Replace (..., StringComparison.InvariantCultureIgnoreCase).
    Fixes https://github.com/mono/mono/issues/18978.

--- mcs/class/corlib/Test/System/StringTest.cs
+++ mcs/class/corlib/Test/System/StringTest.cs
@@ -3221,6 +3221,8 @@ public class StringTest
 
 		// Test replacing null characters (bug #67395)
 		Assert.AreEqual ("is this ok ?", "is \0 ok ?".Replace ("\0", "this"), "should not strip content after nullchar");
+
+		Assert.AreEqual ("CBC", "ABC".Replace ("a", "C", StringComparison.InvariantCultureIgnoreCase));
 	}
 
 	[Test]
