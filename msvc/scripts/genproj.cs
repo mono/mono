b1263bcf1192f294ee2e263956edc1d1cf0768aa
Katelyn Gadd <kg@luminance.org>
2020-04-28 06:45:16 -0700

    Fix whitespace damage in genproj

--- msvc/scripts/genproj.cs
+++ msvc/scripts/genproj.cs
@@ -47,11 +47,11 @@ public static class KnownProject {
 			Path = @"$(SolutionDir)\mcs\tools\culevel\culevel.csproj",
 			Guid = "{E8E246BD-CD0C-4734-A3C2-7F44796EC47B}"
 		},
-        Exceptionrewriter = new KnownProjectInfo {
-            Name = "exception-filter-rewriter",
-            Path = @"$(SolutionDir)\mcs\tools\exception-filter-rewriter\exception-filter-rewriter.csproj",
-            Guid = "{D7459320-5DC6-4B6A-97F4-0EC56AC80FA5}"
-        };		
+		Exceptionrewriter = new KnownProjectInfo {
+			Name = "exception-filter-rewriter",
+			Path = @"$(SolutionDir)\mcs\tools\exception-filter-rewriter\exception-filter-rewriter.csproj",
+			Guid = "{D7459320-5DC6-4B6A-97F4-0EC56AC80FA5}"
+		};		
 }
 
 public class KnownProjectInfo {
@@ -254,7 +254,7 @@ public class MsbuildGenerator {
 
 	public static readonly (string, string)[] fixed_guids = new [] {
 		("tools/culevel/culevel.csproj", KnownProject.Culevel.Guid),
-        ("tools/exception-filter-rewriter/exception-filter-rewriter.csproj", KnownProject.Exceptionrewriter.Guid)
+		("tools/exception-filter-rewriter/exception-filter-rewriter.csproj", KnownProject.Exceptionrewriter.Guid)
 	};
 
 	static void Usage ()
