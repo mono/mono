e4acd81888007b73481eb2945b7d3497986ac1c6
tqiu8 <tammy.qiu@yahoo.com>
2020-05-08 12:00:47 -0400

    incorporate PR feedback & reference issue number for disabled locales

--- sdks/wasm/DebuggerTestSuite/DateTimeTests.cs
+++ sdks/wasm/DebuggerTestSuite/DateTimeTests.cs
@@ -1,42 +1,21 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
-
-using System.Net.WebSockets;
-using System.Threading;
-using System.IO;
-using System.Text;
-using System.Collections.Generic;
-
-using Newtonsoft.Json.Linq;
 using Xunit;
-using WebAssembly.Net.Debugging;
 using System.Globalization;
 
 namespace DebuggerTests
 {
 	public class DateTimeList : DebuggerTestBase {
-		public DateTimeList() : base ("debugger-driver.html") {}
-
-		[Fact]
-		public async Task CheckThatAllLocaleSourcesAreSent () {
-			var insp = new Inspector ();
-			//Collect events
-			var scripts = SubscribeToScripts(insp);
-
-			await Ready();
-			//all sources are sent before runtime ready is sent, nothing to check
-			await insp.Ready ();
-			Assert.Contains ("dotnet://debugger-test.dll/debugger-datetime-test.cs", scripts.Values);
-		}
 
 		[Theory]
 		[InlineData ("en-US")]
-		// [InlineData ("es-ES")]
 		[InlineData ("de-DE")]
-		// [InlineData ("ja-JP")]
 		[InlineData ("ka-GE")]
 		[InlineData ("hu-HU")]
+        
+        // Currently not passing tests. Issue #19743
+        // [InlineData ("ja-JP")]
+        // [InlineData ("es-ES")]
 		public async Task CheckDateTimeLocale (string locale) {
 			var insp = new Inspector ();
 			var scripts = SubscribeToScripts(insp);
@@ -53,11 +32,11 @@ namespace DebuggerTests
 					+ $"'{locale}'); }}, 1);",
 					debugger_test_loc, 20, 3, "LocaleTest",
 					locals_fn: (locals) => {
+                        DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo(locale).DateTimeFormat;
 						CultureInfo.CurrentCulture = new CultureInfo (locale, false);
 						DateTime dt = new DateTime (2020, 1, 2, 3, 4, 5);
 						string dt_str = dt.ToString();
 
-						DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo(locale).DateTimeFormat;
 						var fdtp = dtfi.FullDateTimePattern;
 						var ldp = dtfi.LongDatePattern;
 						var ltp = dtfi.LongTimePattern;
