98320ecbde70b29db4c2591f9a8ae82ee60a2b52
Ankit Jain <radical@gmail.com>
2020-05-18 15:02:38 -0400

    [wasm][debugger][tests] Allow overriding which browser to use (#19769)
    * [wasm][debugger] Reduce debugger-tests timeout to 40m
    
    - And run that before the system tests since the debugger is more likely
    to fail at this point.
    
    Based on @lewing's suggestion
    
    * [wasm][tests] Allow overriding which browser to use
    
    .. via the envvar `WASM_TESTS_ChromePath`.
    
    This can also be set in an `appsettings.json` file next to the test
    binaries, like:
    ```
    { "ChromePath": "</path/to/chrome/binary>" }
    ```
    
    Also, prefer stable Chrome over Canary.
    
    Co-authored by @lewing

--- scripts/ci/run-jenkins.sh
+++ scripts/ci/run-jenkins.sh
@@ -351,11 +351,11 @@ if [[ ${CI_TAGS} == *'webassembly'* ]] || [[ ${CI_TAGS} == *'wasm'* ]];
         if [[ ${CI_TAGS} != *'no-tests'* ]]; then
             ${TESTCMD} --label=mini --timeout=20m $gnumake -C sdks/wasm run-all-mini
             ${TESTCMD} --label=v8-corlib --timeout=20m $gnu$gnumake -C sdks/wasm run-v8-corlib
+            ${TESTCMD} --label=debugger --timeout=40m $gnumake -C sdks/wasm run-debugger-tests
             ${TESTCMD} --label=mini-system --timeout=60m $gnu$gnumake -C sdks/wasm run-all-System
             ${TESTCMD} --label=system-core --timeout=60m $gnumake -C sdks/wasm run-all-System.Core
             for suite in ${xunit_test_suites}; do ${TESTCMD} --label=xunit-${suite} --timeout=30m $gnumake -C sdks/wasm run-${suite}-xunit; done
             # disable for now until https://github.com/mono/mono/pull/13622 goes in
-            ${TESTCMD} --label=debugger --timeout=60m $gnumake -C sdks/wasm run-debugger-tests
             ${TESTCMD} --label=browser --timeout=20m $gnumake -C sdks/wasm run-browser-tests
             ${TESTCMD} --label=browser-threads --timeout=20m $gnumake -C sdks/wasm run-browser-threads-tests
             #${TESTCMD} --label=browser-dynamic --timeout=20m $gnumake -C sdks/wasm run-browser-dynamic-tests
