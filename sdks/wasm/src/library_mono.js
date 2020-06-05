3ad244fab08aeb8c90fd320ca3146582b3375737
Zoltan Varga <vargaz@gmail.com>
2020-06-05 04:56:52 -0400

    [wasm] Print out the real stack trace when load_runtime () fails.

--- sdks/wasm/src/library_mono.js
+++ sdks/wasm/src/library_mono.js
@@ -678,9 +678,8 @@ var MonoSupportLib = {
 								load_runtime (vfs_prefix, enable_debugging);
 							} catch (ex) {
 								print ("MONO_WASM: load_runtime () failed: " + ex);
-								var err = new Error();
 								print ("MONO_WASM: Stacktrace: \n");
-								print (err.stack);
+								print (ex.stack);
 
 								var wasm_exit = Module.cwrap ('mono_wasm_exit', null, ['number']);
 								wasm_exit (1);
