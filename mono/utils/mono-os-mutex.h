mono/mono

commit 3e8108ea6576b07de2a64528be18674683879189

threads: set mutex PRIO_INHERIT in attribute (#13333)
threads: set mutex PRIO_INHERIT in attribute

When running multiple threads on a linux-rt kernel, with different priorities, the lack of priority-inheritance
makes the kernel make wrong scheduling choices.
This commit changes the system-wide mutex initializers.
This change protects our system against unexpected latencies in higher priority threads.

<!--
Thank you for your Pull Request!

If you are new to contributing to Mono, please try to do your best at conforming to our coding guidelines http://www.mono-project.com/community/contributing/coding-guidelines/ but don't worry if you get something wrong. One of the project members will help you to get things landed.

Does your pull request fix any of the existing issues? Please use the following format: Fixes #issue-number
-->

mono/utils/mono-os-mutex.h
@@ -44,17 +44,7 @@ typedef pthread_mutex_t mono_mutex_t;
 typedef pthread_cond_t mono_cond_t;
 
 static inline void
-mono_os_mutex_init (mono_mutex_t *mutex)
-{
-	int res;
-
-	res = pthread_mutex_init (mutex, NULL);
-	if (G_UNLIKELY (res != 0))
-		g_error ("%s: pthread_mutex_init failed with \"%s\" (%d)", __func__, g_strerror (res), res);
-}
-
-static inline void
-mono_os_mutex_init_recursive (mono_mutex_t *mutex)
+mono_os_mutex_init_type (mono_mutex_t *mutex, int type)
 {
 	int res;
 	pthread_mutexattr_t attr;
@@ -63,9 +53,16 @@ mono_os_mutex_init_recursive (mono_mutex_t *mutex)
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: pthread_mutexattr_init failed with \"%s\" (%d)", __func__, g_strerror (res), res);
 
-	res = pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_RECURSIVE);
+	res = pthread_mutexattr_settype (&attr, type);
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: pthread_mutexattr_settype failed with \"%s\" (%d)", __func__, g_strerror (res), res);
+
+#ifdef PTHREAD_PRIO_INHERIT
+	/* use PTHREAD_PRIO_INHERIT if possible */
+	res = pthread_mutexattr_setprotocol (&attr, PTHREAD_PRIO_INHERIT);
+	if (G_UNLIKELY (res != 0))
+		g_error ("%s: pthread_mutexattr_setprotocol failed with \"%s\" (%d)", __func__, g_strerror (res), res);
+#endif
 
 	res = pthread_mutex_init (mutex, &attr);
 	if (G_UNLIKELY (res != 0))
@@ -74,6 +71,18 @@ mono_os_mutex_init_recursive (mono_mutex_t *mutex)
 	res = pthread_mutexattr_destroy (&attr);
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: pthread_mutexattr_destroy failed with \"%s\" (%d)", __func__, g_strerror (res), res);
+}
+
+static inline void
+mono_os_mutex_init (mono_mutex_t *mutex)
+{
+	mono_os_mutex_init_type(mutex, PTHREAD_MUTEX_DEFAULT);
+}
+
+static inline void
+mono_os_mutex_init_recursive (mono_mutex_t *mutex)
+{
+	mono_os_mutex_init_type(mutex, PTHREAD_MUTEX_RECURSIVE);
 }
 
 static inline void

commit df2f9b01d29dcc6d24d3ca52894566ac439eb9db

Avoid setting PTHREAD_PRIO_INHERIT on Alpine, since this causes deadlocks (#19914)
Fixes #7167

The PR 3e8108ea6576b07de2a64528be18674683879189 introduced a deadlock on Alpine, making it impossible to even build mono. This PR reverts the setting of PTHREAD_PRIO_INHERIT when on Alpine.
Since GCC on Alpine doesn't provide a builtin macro to detect Alpine, I also added a #define in configure.ac.

configure.ac
@@ -320,6 +320,11 @@ case "$host" in
                 	# available during cross-compilation
                 	mono_cv_uscore=no
                 fi
+                case "$host" in
+                *-musl)
+                        AC_DEFINE(MUSL, 1, [musl libc])
+                        ;;
+                esac
 		case "$host" in
 		*-tizen-linux-*)
 			platform_tizen=yes

mono/utils/mono-os-mutex.h
@@ -59,7 +59,7 @@ mono_os_mutex_init_type (mono_mutex_t *mutex, int type)
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: pthread_mutexattr_settype failed with \"%s\" (%d)", __func__, g_strerror (res), res);
 
-#if !defined(__HAIKU__) && defined (PTHREAD_PRIO_INHERIT) && HAVE_DECL_PTHREAD_MUTEXATTR_SETPROTOCOL
+#if !defined(__HAIKU__) && !defined(MUSL) && defined (PTHREAD_PRIO_INHERIT) && HAVE_DECL_PTHREAD_MUTEXATTR_SETPROTOCOL
 	/* use PTHREAD_PRIO_INHERIT if possible */
 	res = pthread_mutexattr_setprotocol (&attr, PTHREAD_PRIO_INHERIT);
 	if (G_UNLIKELY (res != 0 && res != ENOTSUP))