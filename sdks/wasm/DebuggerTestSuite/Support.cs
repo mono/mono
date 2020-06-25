b437d111e2dd6605424efb22a9f464307c3cc735
Martin Baulig <mabaul@microsoft.com>
2020-06-25 16:42:57 +0200

    Squashed version of Ankit's https://github.com/mono/mono/pull/19940

--- sdks/wasm/DebuggerTestSuite/Support.cs
+++ sdks/wasm/DebuggerTestSuite/Support.cs
@@ -24,6 +24,13 @@ namespace DebuggerTests
 		public const string PAUSE = "pause";
 		public const string READY = "ready";
 
+		static string[] FailOnMethods = new string[] {
+			"Runtime.exceptionThrown",
+			"Inspector.targetCrashed",
+			"Inspector.targetReloadedAfterCrash",
+			"Inspector.detached"
+		};
+
 		public Task<JObject> WaitFor(string what) {
 			if (notifications.ContainsKey (what))
 				throw new Exception ($"Invalid internal state, waiting for {what} while another wait is already setup");
@@ -43,10 +50,10 @@ namespace DebuggerTests
 			eventListeners[evtName] = cb;
 		}
 
-		void FailAllWaitersWithException (JObject exception)
+		void FailAllWaitersWithException (string method, JObject exception)
 		{
 			foreach (var tcs in notifications.Values)
-				tcs.SetException (new ArgumentException (exception.ToString ()));
+				tcs.SetException (new ArgumentException ($"{method}: {exception}"));
 		}
 
 		async Task OnMessage(string method, JObject args, CancellationToken token)
@@ -65,8 +72,8 @@ namespace DebuggerTests
 			}
 			if (eventListeners.ContainsKey (method))
 				await eventListeners[method](args, token);
-			else if (String.Compare (method, "Runtime.exceptionThrown") == 0)
-				FailAllWaitersWithException (args);
+			else if (FailOnMethods.Any (m => string.Compare (m, method) == 0))
+				FailAllWaitersWithException (method, args);
 		}
 
 		public async Task Ready (Func<InspectorClient, CancellationToken, Task> cb = null, TimeSpan? span = null) {
