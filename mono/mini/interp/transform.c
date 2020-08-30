ecde6ed4a61c37094d73dfc9963d8c7a24504399
Vlad Brezae <brezaevlad@gmail.com>
2020-08-30 12:38:27 +0300

    [interp] Simplify computation of clause ranges
    The per instruction IL offset tracking was complicated and prone to bugs. We use it only for debug features now.

--- mono/mini/interp/transform.c
+++ mono/mini/interp/transform.c
@@ -6521,27 +6521,6 @@ exit:
 	goto exit_ret;
 }
 
-// Find the offset of the first interp instruction generated starting il_offset
-// This is needed to find the end of clauses.
-static int
-find_in_offset (TransformData *td, int il_offset)
-{
-	int i = il_offset;
-	while (!td->in_offsets [i])
-		i++;
-	return td->in_offsets [i] - 1;
-}
-
-// We store in the in_offset array the native_offset + 1, so 0 can mean only that the il
-// offset is uninitialized. Otherwise 0 is valid value for first interp instruction.
-static int
-get_in_offset (TransformData *td, int il_offset)
-{
-	int target_offset = td->in_offsets [il_offset];
-	g_assert (target_offset);
-	return target_offset - 1;
-}
-
 static void
 handle_relocations (TransformData *td)
 {
@@ -7796,6 +7775,18 @@ interp_fix_localloc_ret (TransformData *td)
 	}
 }
 
+static int
+get_native_offset (TransformData *td, int il_offset)
+{
+	if (il_offset < td->header->code_size) {
+		InterpBasicBlock *bb = td->offset_to_bb [il_offset];
+		g_assert (bb);
+		return bb->native_offset;
+	} else {
+		return td->new_code_end - td->new_code;
+	}
+}
+
 static void
 generate (MonoMethod *method, MonoMethodHeader *header, InterpMethod *rtm, MonoGenericContext *generic_context, MonoError *error)
 {
@@ -7900,15 +7891,15 @@ generate (MonoMethod *method, MonoMethodHeader *header, InterpMethod *rtm, MonoG
 	for (i = 0; i < header->num_clauses; i++) {
 		MonoExceptionClause *c = rtm->clauses + i;
 		int end_off = c->try_offset + c->try_len;
-		c->try_offset = get_in_offset (td, c->try_offset);
-		c->try_len = find_in_offset (td, end_off) - c->try_offset;
-		g_assert ((c->try_offset + c->try_len) < code_len_u16);
+		c->try_offset = get_native_offset (td, c->try_offset);
+		c->try_len = get_native_offset (td, end_off) - c->try_offset;
+		g_assert ((c->try_offset + c->try_len) <= code_len_u16);
 		end_off = c->handler_offset + c->handler_len;
-		c->handler_offset = get_in_offset (td, c->handler_offset);
-		c->handler_len = find_in_offset (td, end_off) - c->handler_offset;
+		c->handler_offset = get_native_offset (td, c->handler_offset);
+		c->handler_len = get_native_offset (td, end_off) - c->handler_offset;
 		g_assert (c->handler_len >= 0 && (c->handler_offset + c->handler_len) <= code_len_u16);
 		if (c->flags & MONO_EXCEPTION_CLAUSE_FILTER)
-			c->data.filter_offset = get_in_offset (td, c->data.filter_offset);
+			c->data.filter_offset = get_native_offset (td, c->data.filter_offset);
 	}
 	rtm->stack_size = (sizeof (stackval)) * (td->max_stack_height + 2); /* + 1 for returns of called functions  + 1 for 0-ing in trace*/
 	rtm->stack_size = ALIGN_TO (rtm->stack_size, MINT_VT_ALIGNMENT);
