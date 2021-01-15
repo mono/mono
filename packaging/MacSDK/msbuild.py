1d405057b7273e522aabff9e8e721cf68d14dfbd
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-01-15 12:44:09 +0100

    Bump msbuild to fix build break

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = 'f048f95f8c67b4316d72f362ff2fa3dac67f58d4')
+			revision = 'cad85cefabdaa001fb4bdbea2f5bf1d1cdb83c9b')
 
 	def build (self):
 		try:
