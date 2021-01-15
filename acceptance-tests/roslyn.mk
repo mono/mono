364c87b1a7a9d6a01cd8b15c0a6d5b4022000aa7
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-01-15 17:50:51 +0100

    Disable acceptance-tests/roslyn.mk
    The commit we're using doesn't build anymore due to removed myget feeds.
    
    (cherry picked from commit 2688ce59b2fe2e97c76a48fa36576fdfa4f7d8d4)

--- acceptance-tests/roslyn.mk
+++ acceptance-tests/roslyn.mk
@@ -1,7 +1,9 @@
 check-roslyn:
-	@echo "Runnning roslyn tests using mono from PATH:"
-	mono --version
-	@$(MAKE) validate-roslyn RESET_VERSIONS=1
-	cd $(ROSLYN_PATH); \
-	./build.sh --restore --build --test --mono || exit; \
-	echo "done"
+
+# Disabled because it doesn't build anymore due to removed myget feeds
+#	@echo "Runnning roslyn tests using mono from PATH:"
+#	mono --version
+#	@$(MAKE) validate-roslyn RESET_VERSIONS=1
+#	cd $(ROSLYN_PATH); \
+#	./build.sh --restore --build --test --mono || exit; \
+#	echo "done"
