a4cd67e31f7e02e1cce0d51508ef71f467f4baa7
Mathieu Bourgeois <mathieu.bourgeois@gameloft.com>
2021-02-03 17:46:07 -0500

    Add support for Apple Silicon on Xamarin.Mac SDK

--- sdks/builds/runtime.mk
+++ sdks/builds/runtime.mk
@@ -221,8 +221,8 @@ _cross-runtime_$(1)-$(2)_CONFIGURE_FLAGS= \
 
 .stamp-$(1)-$(2)-$$(CONFIGURATION)-configure: | $$(if $$(IGNORE_PROVISION_LLVM),,provision-$(6))
 
-$$(TOP)/sdks/builds/$(1)-$(2)-$$(CONFIGURATION)/$(4).h: .stamp-$(1)-$(2)-$$(CONFIGURATION)-configure | configure-$(1)-$(5)
-	python3 $(TOP)/mono/tools/offsets-tool/offsets-tool.py --targetdir="$$(TOP)/sdks/builds/$(1)-$(5)-$$(CONFIGURATION)" --abi=$(7) --monodir="$$(TOP)" --outfile="$$@" $$(_$(1)-$(2)_OFFSETS_DUMPER_ARGS)
+$$(TOP)/sdks/builds/$(1)-$(2)-$$(CONFIGURATION)/$(4).h: .stamp-$(1)-$(2)-$$(CONFIGURATION)-configure | configure-$(1)-$(2)
+	python3 $(TOP)/mono/tools/offsets-tool/offsets-tool.py --targetdir="$$(TOP)/sdks/builds/$(1)-$(2)-$$(CONFIGURATION)" --abi=$(7) --monodir="$$(TOP)" --outfile="$$@" $$(_$(1)-$(2)_OFFSETS_DUMPER_ARGS)
 
 build-$(1)-$(2): $$(TOP)/sdks/builds/$(1)-$(2)-$$(CONFIGURATION)/$(4).h
 
