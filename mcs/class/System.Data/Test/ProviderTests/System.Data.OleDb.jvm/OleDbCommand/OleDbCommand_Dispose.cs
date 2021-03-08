67e0ffec40887a77942f2d7ff48c81e9f3b6bc7b
Sam Patel <46026722+SamMonoRT@users.noreply.github.com>
2021-03-08 17:15:25 -0500

    CredScan issue - remove hardcoded password
    Remove commented out lines of code which had a hardcoded password flagged by the CredScan tool (line 86)

--- mcs/class/System.Data/Test/ProviderTests/System.Data.OleDb.jvm/OleDbCommand/OleDbCommand_Dispose.cs
+++ mcs/class/System.Data/Test/ProviderTests/System.Data.OleDb.jvm/OleDbCommand/OleDbCommand_Dispose.cs
@@ -55,57 +55,6 @@ public class OleDbCommand_Dispose : GHTBase
 		}
 	}
 
-	//problems from Synergy
-
-	//Ofer, 
-
-	//Take a look at #1. Test this to see if we have a problem in dispose. If we do, add a test for this to make sure we catch it in next rounds. 
-
-	//
-
-	//-----Original Message-----
-	//From: David Teplitchi [mailto:davidt@mainsoft.com] 
-	//Sent: Sunday, March 21, 2004 9:31 AM
-	//To: //Oved Yavine//
-	//Subject: 2 problems from Synergy
-
-
-	//Please check those 2 problems reported by Synergy and tell me what do you think.
-
-	//1) The following code works in dotnet but doesn//t work in j2ee.
-	//// Oracle Drivers
-	//OleDbConnection Connect;
-	//OleDbDataReader DbReader;
-	//OleDbCommand DbCommand;
-	//IDataReader DR;
-	//string sSQL;
-	//int iField;
-	//bool bFound;
-	//try
-	//{
-	//Connect = new OleDbConnection("Provider=\"MSDAORA.1\";User
-	//ID=COM;Password=SQL;Data Source=LLO1;HostName=WS10359;Port=1521");
-	//Connect.Open();
-	//sSQL = "SELECT * FROM PRODUCT WHERE PRO_SKU = //SKU_208//";
-	//DbCommand = new OleDbCommand(sSQL, Connect);
-	//DbReader = DbCommand.ExecuteReader(CommandBehavior.SingleRow);
-	//DR = DbReader;
-	//DbCommand.Dispose(); // comment out
-	////DbCommand = null;
-	//bFound = DbReader.HasRows;
-	//DR.Read();
-	//// Get Column Ordinal
-	//iField = DR.GetOrdinal("PRO_DESCRIPTION");
-
-	//string sDesc = DR.GetString(iField);
-	//}
-	//catch(Exception e)
-	//{
-	//this.WriteErrorLog(e.Message);
-	//}
-	//i have identified the problem as being "DbCommand.Dispose()". If you comment this line out or make it 
-	//"DbCommand = null", then its okay. So this instruction doesnt work the same in both in .net and j2ee.
-
 
 	[Test]
 	public void run()
