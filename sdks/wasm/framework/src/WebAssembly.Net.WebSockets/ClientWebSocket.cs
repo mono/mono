966e1e5df51117404184155beb995723d0eaa22e
rootflood <50250836+rootflood@users.noreply.github.com>
2021-03-15 00:29:21 +0330

    Fix SendAsync method
    Fix SendAsync method
    socket should wait while sending completed

--- sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
+++ sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
@@ -393,11 +393,17 @@ namespace WebAssembly.Net.WebSockets {
 					if (messageType == WebSocketMessageType.Binary) {
 						using (var uint8Buffer = Uint8Array.From(buffer)){
 							innerWebSocket.Invoke ("send", uint8Buffer);
+							await Task.Delay(1);
+							while ((int)innerWebSocket.GetObjectProperty("bufferedAmount") != 0)
+								await Task.WhenAny(tcsSend.Task, Task.Delay(200));
 							tcsSend.SetResult (true);
 						}
 					} else if (messageType == WebSocketMessageType.Text) {
 						var strBuffer = Encoding.UTF8.GetString (buffer.Array, buffer.Offset, buffer.Count);
 						innerWebSocket.Invoke ("send", strBuffer);
+						await Task.Delay(1);
+						while ((int)innerWebSocket.GetObjectProperty("bufferedAmount") != 0)
+							await Task.WhenAny(tcsSend.Task, Task.Delay(200));
 						tcsSend.SetResult (true);
 					}
 				} catch (Exception excb) {
