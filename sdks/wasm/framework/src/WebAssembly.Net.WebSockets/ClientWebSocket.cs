6d4996c007c7da22f6f6057b3b2ddc02b3576631
rootflood <50250836+rootflood@users.noreply.github.com>
2021-03-18 17:29:25 +0330

    Fix CloseAsync method
    because the "CloseAsync" method is async and the state may changed before of call this method we should ignore the "CloseAsync" method if state is "closed"
    for example in flowing codes if server closed the connection before of client close then I have an error and this is even when I trying to check the "state"
    ```c#
                protected override async Task Inner_Disconnect()
                {
                    if (Socket.State != WebSocketState.Open)
                        return;
                    await Socket.CloseAsync(WebSocketCloseStatus.NormalClosure,"", CancellationToken.None);
                }
    ```

--- sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
+++ sdks/wasm/framework/src/WebAssembly.Net.WebSockets/ClientWebSocket.cs
@@ -474,13 +474,13 @@ namespace WebAssembly.Net.WebSockets {
 		public override async Task CloseAsync (WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken)
 		{
 			writeBuffer = null;
-			ThrowIfNotConnected ();
-
 			await CloseAsyncCore (closeStatus, statusDescription, cancellationToken);
 		}
 
 		private async Task CloseAsyncCore (WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken)
 		{
+			if (State == WebSocketState.Closed)
+				return;
 			ThrowOnInvalidState (State,
 			    WebSocketState.Open, WebSocketState.CloseReceived, WebSocketState.CloseSent);
 
