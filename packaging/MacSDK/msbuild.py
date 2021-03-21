40153f6604dbe9fd226ffdcaf64aede31fd6bca4
Matt Ward <matt.ward@microsoft.com>
2021-03-21 12:48:34 +0000

    Update to latest vs16.9 branch
    Includes a fix for NuGet restore failures on a clean machine.

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mrward', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '43e8f1e508eaa9168709890c41a75ff7687b4776')
+			revision = '75bc8d30cdaf7cfef7a365e362bb016268f3ede9')
 
 	def build (self):
 		try:
