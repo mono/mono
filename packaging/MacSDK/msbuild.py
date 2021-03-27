8d03fbb81a4dc6b696cea460d88bc06de97db572
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-03-27 13:06:51 +0100

    Update msbuild.py

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-			revision = '9acaaeee43e3750d2a1beeed1331108f7fd51980')
+			revision = 'd094b49f5b5095efe15685431275b9c38fa6d845')
 
 	def build (self):
 		try:
