83c7d9f8ca8a343de84c6c5aabc7936287344467
Josh Peterson <joshuap@unity3d.com>
2021-04-06 15:33:36 -0400

    Indicate that System.Reflection.Emit is not supported in the AOT profile

--- mcs/class/corlib/corefx/RuntimeFeature.cs
+++ mcs/class/corlib/corefx/RuntimeFeature.cs
@@ -2,8 +2,13 @@ namespace System.Runtime.CompilerServices
 {
 	partial class RuntimeFeature
 	{
+#if UNITY_AOT
+		public static bool IsDynamicCodeSupported => false;
+		public static bool IsDynamicCodeCompiled => false;
+#else
 		// https://github.com/dotnet/coreclr/blob/397aaccb5104844998c3bcf6e9245cc81127e1e2/src/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeFeature.CoreCLR.cs#L9-L10
 		public static bool IsDynamicCodeSupported => true;
 		public static bool IsDynamicCodeCompiled => true;
+#endif
 	}
 }
