bb4af41b62743cbdc5d751b91e95ea9352996c19
Jo Shields <joshield@microsoft.com>
2021-05-04 05:23:26 -0400

    Azure Pipelines Windows CI (#20879)
    * Disable EventLog tests on Windows if we're not running as admin
    
    * Disable a socket test that doesn't work on Win10
    
    * Fix MetadataResolverTest on Windows in AzDO
    
    There's something running on localhost already that makes the test fail, use a different hostname instead.
    
    * Force ASCII encoding for console-output.cs
    
    * Use different way to compare output of test-console-output
    
    * Fix issue with symlink on PublishTestResults
    
    * Use Unicode encoding in Win32EventLog p/invokes, fixes eventlog tests on Win10
    
    Co-authored-by: Alexander Köplinger <alex.koeplinger@outlook.com>

--- mono/tests/console-output.cs
+++ mono/tests/console-output.cs
@@ -5,6 +5,7 @@ class Driver
 {
 	public static void Main ()
 	{
+		Console.OutputEncoding = System.Text.Encoding.ASCII;
 		Console.Out.WriteLine ("Testing Console.Out");
 		Console.Error.WriteLine ("Testing Console.Error");
 	}
