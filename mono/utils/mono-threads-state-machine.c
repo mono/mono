a563ccadac94aa5b1f67233eb6601943d9892970
Alfonso Gregory <gfunni234@gmail.com>
2021-07-14 20:08:35 -0400

    Update mono/utils/mono-threads-state-machine.c
    Co-authored-by: Aleksey Kliger (λgeek) <akliger@gmail.com>

--- mono/utils/mono-threads-state-machine.c
+++ mono/utils/mono-threads-state-machine.c
@@ -856,7 +856,7 @@ retry_state_change:
 		trace_state_change_sigsafe ("ABORT_BLOCKING", info, raw_state, STATE_RUNNING, FALSE, 0, func);
 		return AbortBlockingOk;
 	case STATE_BLOCKING_SUSPEND_REQUESTED:
-		if (suspend_count <= 0)
+		if (!(suspend_count > 0))
 			mono_fatal_with_history ("suspend_count = %d, but should be > 0", suspend_count);
 		if (no_safepoints)
 			mono_fatal_with_history ("no_safepoints = TRUE, but should be FALSE");
