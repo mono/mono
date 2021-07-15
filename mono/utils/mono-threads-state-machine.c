44cc8e82b33a1cca3ceb9977237ce9e0bdccb369
Alfonso Gregory <gfunni234@gmail.com>
2021-07-14 20:08:44 -0400

    Update mono/utils/mono-threads-state-machine.c
    Co-authored-by: Aleksey Kliger (λgeek) <akliger@gmail.com>

--- mono/utils/mono-threads-state-machine.c
+++ mono/utils/mono-threads-state-machine.c
@@ -767,7 +767,7 @@ retry_state_change:
 	UNWRAP_THREAD_STATE (raw_state, cur_state, suspend_count, no_safepoints, info);
 	switch (cur_state) {
 	case STATE_BLOCKING:
-		if (suspend_count != 0)
+		if (!(suspend_count == 0))
 			mono_fatal_with_history ("%s suspend_count = %d, but should be == 0", func, suspend_count);
 		if (no_safepoints)
 			mono_fatal_with_history ("no_safepoints = TRUE, but should be FALSE");
