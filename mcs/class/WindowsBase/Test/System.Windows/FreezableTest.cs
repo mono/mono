1f9b895d123558dce068ec9393e5e916daccf3c3
Fox2Code <fox2code@gmail.com>
2021-10-07 13:42:54 +0200

    [WindowsBase] Implement System.Windows.Freezable
    This change is released under the MIT license.

--- mcs/class/WindowsBase/Test/System.Windows/FreezableTest.cs
+++ mcs/class/WindowsBase/Test/System.Windows/FreezableTest.cs
@@ -24,6 +24,7 @@
 // Authors:
 //	Iain McCoy (iain@mccoy.id.au)
 //	Chris Toshok (toshok@ximian.com)
+//	Loïc Rebmeister (fox2code@gmail.com)
 //
 
 using System;
@@ -33,10 +34,97 @@ using NUnit.Framework;
 
 namespace MonoTests.System.Windows {
 
-	class FreezablePoker {
+	class FreezablePoker : Freezable {
+		public bool changedCalled = false;
+		public bool onChangedCalled = false;
+
+		public FreezablePoker ()
+		{
+			changed += new EventHandler (changedHandler);
+		}
+
+		private void changedHandler (object obj, EventArgs args)
+		{
+			changedCalled = true;
+		}
+
+		protected override void OnChanged ()
+		{
+			base.OnChanged ();
+			onChangedCalled = true;
+		}
+
+		protected override Freezable CreateInstanceCore () {
+			return new FreezablePoker ();
+		}
 	};
+
+	class Unfreezable : Freezable {
+
+		protected override bool FreezeCore (bool isChecking) {
+			return false;
+		}
+
+		protected override Freezable CreateInstanceCore () {
+			return new Unfreezable ();
+		}
+	}
 
 	[TestFixture]
 	public class FreezableTest {
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached ("Value", typeof (string), typeof (FreezableTest));
+
+		// Test that the Frezzable.Freeze () and that a cloned object with Clone () is not frozen even if the source is frozen
+		[Test]
+		public void TestFreezeClone ()
+		{
+			var freezable = new FreezablePoker ();
+			freezable.SetValue (ValueProperty, "test");
+			freezable.Freeze ();
+			Assert.IsTrue (freezable.IsFrozen, "Freezable is not frozen!");
+			var freezableClone = freezable.Clone ();
+			Assert.IsFalse (freezableClone.IsFrozen, "FreezableClone is frozen!");
+			Assert.AreEqual (freezableClone.GetValue (ValueProperty), "test",
+				"FreezableClone ValueProperty wasn't copied properly!");
+		}
+
+		// Test that event are properly propagated
+		[Test]
+		public void TestEvents ()
+		{
+			var freezable = new FreezablePoker ();
+			testEventHelper (freezable, false, false, "init");
+			freezable.SetValue (ValueProperty, "test");
+			testEventHelper (freezable, true, false, false, "SetValue");
+			freezable.WritePostscript ();
+			testEventHelper (freezable, true, true, "WritePostscript");
+		}
+
+		private void testEventHelper (FreezablePoker freezable, bool changed,bool onChanged,string state)
+		{
+			if (changed)
+				Assert.IsTrue (freezable.changedCalled, "changed wasn't raised on " + state);
+			else
+				Assert.IsFalse (freezable.changedCalled, "changed was raised on " + state);
+			if (onChanged)
+				Assert.IsTrue (freezable.onChangedCalled, "OnChanged wasn't called on " + state);
+			else
+				Assert.IsFalse (freezable.onChangedCalled, "OnChanged was called on " + state);
+			freezable.changedCalled = false;
+			freezable.onChangedCalled = false;
+		}
+
+		// Test that CanFreeze test child freezable properties and that Freeze also freeze all Freezable childs
+		[Test]
+		public void TestFreezePropagation ()
+		{
+			var freezable = new FreezablePoker ();
+			freezable.SetValue (FreezableProperty, new Unfreezable ());
+			Assert.IsFalse (freezable.CanFreeze, "CanFreeze return true with an unfreezable children!");
+			var childFreezable = new FreezablePoker ();
+			freezable.SetValue (FreezableProperty, childFreezable);
+			freezable.Freeze ();
+			Assert.IsFalse (childFreezable.IsFrozen, "child Freezable is not frozen after it's parent was forzen!");
+		}
 	}
 }
