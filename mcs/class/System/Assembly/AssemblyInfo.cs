11ff8848f3c2d4d6b87ec61692cbf95fe2cb6b2f
Esme Povirk <esme@codeweavers.com>
2021-11-30 15:14:28 -0600

    Move Stack and Queue from mscorlib back to System.

--- mcs/class/System/Assembly/AssemblyInfo.cs
+++ mcs/class/System/Assembly/AssemblyInfo.cs
@@ -94,7 +94,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo ("WebAssembly.Net.WebSockets, PublicKey=00240000048000009400000006020000002400005253413100040000010001004b86c4cb78549b34bab61a3b1800e23bfeb5b3ec390074041536a7e3cbd97f5f04cf0f857155a8928eaa29ebfd11cfbbad3ba70efea7bda3226c6a8d370a4cd303f714486b6ebc225985a638471e6ef571cc92a4613c00b8fa65d61ccee0cbe5f36330c9a01f4183559f1bef24cc2917c6d913e3a541333a1d05d9bed22b38cb")]
 #endif
 
-[assembly: TypeForwardedTo (typeof (System.Collections.Generic.Stack<>))]
-[assembly: TypeForwardedTo (typeof (System.Collections.Generic.Queue<>))]
 [assembly: TypeForwardedTo (typeof (System.IO.Enumeration.FileSystemName))]
 [assembly: TypeForwardedTo (typeof (System.Security.Cryptography.CryptographicOperations))]
