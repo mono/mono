f88ae6dcd80829126d406d4792b5e5bfc0122e69
Alexander Köplinger <alex.koeplinger@outlook.com>
2022-02-24 10:01:58 +0100

    Fix leak in mono_marshal_get_managed_wrapper
    Introduced by f9d44382fbb9331865283d8500f181ae18d99e3b

--- mono/metadata/marshal.c
+++ mono/metadata/marshal.c
@@ -3793,17 +3793,19 @@ mono_marshal_get_managed_wrapper (MonoMethod *method, MonoClass *delegate_klass,
 			mono_error_set_invalid_program (error, "method %s with UnmanagedCallersOnlyAttribute has non-blittable parameters or return type", mono_method_full_name (method, TRUE));
 			return NULL;
 		}
-		mspecs = g_new0 (MonoMarshalSpec*, invoke_sig->param_count + 1);
 	} else {
 		invoke = mono_get_delegate_invoke_internal (delegate_klass);
 		invoke_sig = mono_method_signature_internal (invoke);
-		mspecs = g_new0 (MonoMarshalSpec*, invoke_sig->param_count + 1);
-		mono_method_get_marshal_info (invoke, mspecs);
 	}
 
 	if (invoke_sig->ret->type == MONO_TYPE_GENERICINST) {
 		mono_error_set_generic_error (error, "System.Runtime.InteropServices", "MarshalDirectiveException", "%s", "Cannot marshal 'return value': Non-blittable generic types cannot be marshaled.");
 		return NULL;
+	}
+
+	mspecs = g_new0 (MonoMarshalSpec*, invoke_sig->param_count + 1);
+	if (invoke) {
+		mono_method_get_marshal_info (invoke, mspecs);
 	}
 
 	sig = mono_method_signature_internal (method);
