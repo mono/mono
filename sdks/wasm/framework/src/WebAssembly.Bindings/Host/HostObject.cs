1da2d9803f4e8816a41a2d1aba65ed9011791b83
Alexander Köplinger <alex.koeplinger@outlook.com>
2023-04-24 16:48:34 +0200

    Remove sdks/ directory to reduce confusion (#21647)
    Legacy Android and iOS are building from the 2020-02 branch and modern Android/iOS/WebAssembly are built from the https://github.com/dotnet/runtime repo.

--- sdks/wasm/framework/src/WebAssembly.Bindings/Host/HostObject.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace WebAssembly.Host {
-	public class HostObject : HostObjectBase {
-		public HostObject (string hostName, params object[] _params) : base (Runtime.New(hostName, _params))  
-		{ }
-	}
-}
