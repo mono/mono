47afd83f92aebcc4b213ee45e0bf961b4bc52907
Anton Kruglyakov <antonk@unity3d.com>
2023-09-28 23:30:06 +0100

    Fix build scripts, update GC and enable allocations tracking

--- external/buildscripts/build_runtime_vs.pl
+++ external/buildscripts/build_runtime_vs.pl
@@ -20,6 +20,9 @@ my $clean = 0;
 my $targetArch = "";
 my $debug = 0;
 my $gc = "bdwgc";
+my @vsVersions = (2019, 2022, 2017);
+my @vsEditions = ("Professional", "Enterprise", "Community");
+my @vsBaseFolder = ("ProgramFiles(x86)", "ProgramFiles");
 
 GetOptions(
 	'build=i'=>\$build,
@@ -38,17 +41,26 @@ sub CompileVCProj
 {
 	my $sln = shift;
 	my $config;
-	my $vsInstallRoot = $ENV{"ProgramFiles(x86)"} . "/Microsoft Visual Studio";
 
-	my $msbuild = "$vsInstallRoot/2019/Professional/MSBuild/Current/Bin/MSBuild.exe";
+        my $msbuild = "";
+	MSBUILDLOOP: foreach my $vsFolder (@vsBaseFolder)
+        {
+		foreach my $vsEdition (@vsEditions)
+		{
+			foreach my $vsVersion (@vsVersions)
+			{
+				$msbuild = "$ENV{$vsFolder}/Microsoft Visual Studio/$vsVersion/$vsEdition/MSBuild/Current/Bin/MSBuild.exe";
+				last MSBUILDLOOP if (-e -x $msbuild)
+			}
+		}
+        }
 
 	if (!(-e -x $msbuild))
 	{
-		print (">>> Unable to find executable MSBuild for vs19 at: $msbuild\nFalling back to vs17\n");
-		$msbuild = "$vsInstallRoot/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe";
+		print (">>> Unable to find executable MSBuild\n");
 	}
 
-    $config = $debug ? "Debug" : "Release";
+	$config = $debug ? "Debug" : "Release";
 	my $arch = $targetArch;
 
 	my $target = $clean ? "/t:Clean,Build" :"/t:Build";
